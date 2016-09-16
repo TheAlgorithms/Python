0e60a18cfa1ec25a327b96b8689cccb9d33445db
naveenarun <notatroll.troll@gmail.com>
2016-09-16 14:32:41 -0500

    fix python3 print statement

--- hashes/md5.py
+++ hashes/md5.py
@@ -100,7 +100,7 @@ def md5me(testString):
 def test():
 	assert md5me("") == "d41d8cd98f00b204e9800998ecf8427e"	
 	assert md5me("The quick brown fox jumps over the lazy dog") == "9e107d9d372bb6826bd81d3542a419d6"
-	print "Success."
+	print("Success.")
 
 
 if __name__ == "__main__":
