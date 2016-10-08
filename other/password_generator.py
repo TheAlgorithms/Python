4b43a2f50740bbeab95f64137eb8993ed8ac4617
edawine <sutanto.maxedwin@gmail.com>
2016-10-09 00:09:59 +0700

    Add another randomness into the password generator
    Uses import random for namespace cleanliness
    Uses list instead of string for 'chars' variable in order to shuffle, increases randomness
    Instead of string formatting, uses string concatenation because (currently) it is simpler

--- other/password_generator.py
+++ other/password_generator.py
@@ -1,13 +1,14 @@
 import string
-from random import *
+import random
 
-letters = string.ascii_letters
-digits = string.digits
-symbols = string.punctuation
+letters = [letter for letter in string.ascii_letters]
+digits = [digit for digit in string.digits]
+symbols = [symbol for symbol in string.punctuation]
 chars = letters + digits + symbols
+random.shuffle(chars)
 
 min_length = 8
 max_length = 16
-password = ''.join(choice(chars) for x in range(randint(min_length, max_length)))
-print('Password: %s' % password)
+password = ''.join(random.choice(chars) for x in range(random.randint(min_length, max_length)))
+print('Password: ' + password)
 print('[ If you are thinking of using this passsword, You better save it. ]')
