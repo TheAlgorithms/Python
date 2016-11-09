43513803f078fb1ca0b668ab64accc44ccd1a528
Henocks <hr4574@naver.com>
2016-11-09 23:17:04 +0900

    Slight Performance/Visual Update

--- ciphers/caesar_cipher.py
+++ ciphers/caesar_cipher.py
@@ -2,8 +2,8 @@
 
 def main():
     message = input("Enter message: ")
-    key = int(input("Key [1-26]: "))
-    mode = input("Encrypt or Decrypt [e/d]: ")
+    key     = int(input("Key [1-26]: "))
+    mode    = input("Encrypt or Decrypt [e/d]: ")
 
     if mode.lower().startswith('e'):
         mode = "encrypt"
@@ -11,38 +11,31 @@ def main():
         mode = "decrypt"
 
     translated = encdec(message, key, mode)
-    if mode == "encrypt":
+    if mode ==   "encrypt":
         print("Encryption:", translated)
     elif mode == "decrypt":
         print("Decryption:", translated)
         
 def encdec(message, key, mode):
-    """
-    >>> encdec('Harshil Darji', 12, 'encrypt')
-    'TMDETUX PMDVU'
-
-    >>> encdec('TMDETUX PMDVU', 12, 'decrypt')
-    'HARSHIL DARJI'
-    """
-    message = message.upper()
+    message    = message.upper()
     translated = ""
-    LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    LETTERS    = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     for symbol in message:
         if symbol in LETTERS:
             num = LETTERS.find(symbol)
-            if mode == "encrypt":
+            if mode ==   "encrypt":
                 num = num + key
             elif mode == "decrypt":
                 num = num - key
 
             if num >= len(LETTERS):
-                num = num - len(LETTERS)
+                num -= len(LETTERS)
             elif num < 0:
-                num = num + len(LETTERS)
+                num += len(LETTERS)
 
-            translated = translated + LETTERS[num]
+            translated += LETTERS[num]
         else:
-            translated = translated + symbol
+            translated += symbol
     return translated
 
 if __name__ == '__main__':
