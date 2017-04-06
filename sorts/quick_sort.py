7ae975922016417f507dabd136f960fe775d3840
Mehdi ALAOUI <alaoui.mehdi.1995@gmail.com>
2017-04-06 03:55:28 +0100

    Added some other spaces

--- sorts/quick_sort.py
+++ sorts/quick_sort.py
@@ -29,13 +29,13 @@ def quick_sort(ARRAY):
     >>> quick_sort([-2, -5, -45])
     [-45, -5, -2]
     """
-    ARRAY_LENGTH=len(ARRAY)
+    ARRAY_LENGTH = len(ARRAY)
     if( ARRAY_LENGTH <= 1):
         return ARRAY
     else:
         PIVOT = ARRAY[0]
-        GREATER = [element for element in ARRAY[1:] if element > PIVOT]
-        LESSER = [element for element in ARRAY[1:] if element <= PIVOT]
+        GREATER = [ element for element in ARRAY[1:] if element > PIVOT ]
+        LESSER = [ element for element in ARRAY[1:] if element <= PIVOT ]
         return quick_sort(LESSER) + [PIVOT] + quick_sort(GREATER)
 
 
@@ -50,5 +50,5 @@ if __name__ == '__main__':
         input_function = input
 
     user_input = input_function('Enter numbers separated by a comma:\n')
-    unsorted = [int(item) for item in user_input.split(',')]
-    print(quick_sort(unsorted))
+    unsorted = [ int(item) for item in user_input.split(',') ]
+    print( quick_sort(unsorted) )
