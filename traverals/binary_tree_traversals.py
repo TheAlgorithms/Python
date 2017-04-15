4d872d06a6b4f929eeba63ec44e843373a6e3c80
arpitha-basavaraj <arpitha005@gmail.com>
2017-04-14 19:57:08 -0500

    Updated the binary_tree_traversals.py with fix
    Includes fix for the issues reported Bug#47 and #76.
    User need to specify the size of Tree and input the elements of Tree.
    This code works perfect on Python 2.7.x version.

--- traverals/binary_tree_traversals.py
+++ traverals/binary_tree_traversals.py
@@ -1,9 +1,9 @@
 """
 This is pure python implementation of tree traversal algorithms
 """
-
-import queue
-
+from __future__ import print_function
+import Queue
+import sys
 
 class TreeNode:
     def __init__(self, data):
@@ -12,35 +12,37 @@ class TreeNode:
         self.left = None
 
 
-def build_tree():
-    print("Enter the value of the root node: ", end="")
-    data = eval(input())
+def build_tree(size):
+    print("Enter the value of the root node: ", end=' ')
+    data = eval(input_function())
+	
     if data < 0:
         return None
     else:
-        q = queue.Queue()
-        tree_node = TreeNode(data)
-        q.put(tree_node)
-        while not q.empty():
-            node_found = q.get()
-            print("Enter the left node of %s: " % node_found.data, end="")
-            left_data = eval(input())
-            if left_data >= 0:
-                left_node = TreeNode(left_data)
-                node_found.left = left_node
-                q.put(left_node)
-            print("Enter the right node of %s: " % node_found.data, end="")
-            right_data = eval(input())
-            if right_data >= 0:
-                right_node = TreeNode(right_data)
-                node_found.right = right_node
-                q.put(right_node)
+		q = Queue.Queue()
+		tree_node = TreeNode(data)
+		q.put(tree_node)
+		current_size = q.qsize()
+		while not q.empty() and current_size < size:
+			current_size = q.qsize()
+			node_found = q.get()
+			print("Enter the left node of %s: " % node_found.data, end=' ')
+			left_data = eval(input_function())
+			if left_data >= 0:
+				left_node = TreeNode(left_data)
+				node_found.left = left_node
+				q.put(left_node)
+			print("Enter the right node of %s: " % node_found.data, end=' ')
+			right_data = eval(input_function())
+			if right_data >= 0:
+				right_node = TreeNode(right_data)
+				node_found.right = right_node
+				q.put(right_node)
     return tree_node
 
 
 def pre_order(node):
     if not isinstance(node, TreeNode) or not node:
-        print("Invalid input")
         return
     print(node.data, end=" ")
     pre_order(node.left)
@@ -66,11 +68,11 @@ def post_order(node):
 def level_order(node):
     if not isinstance(node, TreeNode) or not node:
         return
-    q = queue.Queue()
+    q = Queue.Queue()
     q.put(node)
     while not q.empty():
         node_dequeued = q.get()
-        print(node_dequeued.data, end=" ")
+        print(node_dequeued.data, end=' ')
         if node_dequeued.left:
             q.put(node_dequeued.left)
         if node_dequeued.right:
@@ -78,29 +80,31 @@ def level_order(node):
 
 
 if __name__ == '__main__':
-    import sys
-
-    print("\n********* Binary Tree Traversals ************\n")
-    # For python 2.x and 3.x compatibility: 3.x has not raw_input builtin
+	# For python 2.x and 3.x compatibility: 3.x has not raw_input builtin
     # otherwise 2.x's input builtin function is too "smart"
-    if sys.version_info.major < 3:
-        input_function = raw_input
-    else:
-        input_function = input
+	if sys.version_info.major < 3:
+		input_function = raw_input
+	else:
+		input_function = input	
+	print("Enter the size of Tree: ", end=' ')
+	treeSize = eval(input_function())
+	print("Tree size:", treeSize)
+	print("\n********* Binary Tree Traversals ************\n")
 
-    node = build_tree()
-    print("\n********* Pre Order Traversal ************")
-    pre_order(node)
-    print("\n******************************************\n")
 
-    print("\n********* In Order Traversal ************")
-    in_order(node)
-    print("\n******************************************\n")
+	node = build_tree(treeSize)
+	print("\n********* Pre Order Traversal ************")
+	pre_order(node)
+	print("\n******************************************\n")
+	
+	print("\n********* In Order Traversal ************")
+	in_order(node)
+	print("\n******************************************\n")
 
-    print("\n********* Post Order Traversal ************")
-    post_order(node)
-    print("\n******************************************\n")
+	print("\n********* Post Order Traversal ************")
+	post_order(node)
+	print("\n******************************************\n")
 
-    print("\n********* Level Order Traversal ************")
-    level_order(node)
-    print("\n******************************************\n")
+	print("\n********* Level Order Traversal ************")
+	level_order(node)
+	print("\n******************************************\n")
