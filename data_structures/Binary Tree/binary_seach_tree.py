da414d89d953ba91af032ef8a3f3fda627201d70
Francisco Matias <franciscomatias@alu.ufc.br>
2017-06-20 04:43:00 -0300

    fixed error

--- data_structures/Binary Tree/binary_seach_tree.py
+++ data_structures/Binary Tree/binary_seach_tree.py
@@ -75,27 +75,3 @@ class BinarySearchTree:
     def getRoot(self):
         return self.root
 
-
-'''
-Example
-            8
-           / \
-          3   10
-         / \    \
-        1   6    14
-           / \   /
-          4   7 13 
-'''
-
-t = BinarySearchTree()
-t.insert(8)
-t.insert(3)
-t.insert(1)
-t.insert(6)
-t.insert(4)
-t.insert(7)
-t.insert(10)
-t.insert(14)
-t.insert(13)
-
-t.preShow(t.getRoot())
