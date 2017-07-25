9dcd981a5776f5b2d2ff2b5867a17a3d8fcf06d5
mounaim <mounaim.zaryouhi@gmail.com>
2017-07-25 16:08:54 +0100

    Update Breadth_First_Search.py
    Fixed the fact that all nodes except source are marked as visited twice

--- Graphs/Breadth_First_Search.py
+++ Graphs/Breadth_First_Search.py
@@ -15,10 +15,10 @@ class GRAPH:
 
     def bfs(self,s):
         queue=[s]
+        self.visited[s]=1
         while len(queue)!=0:
             x=queue.pop(0)
             print(x)
-            self.visited[x]=1
             for i in range(0,self.nodes):
                 if self.graph[x][i]==1 and self.visited[i]==0:
                     queue.append(i)     
