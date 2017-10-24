7284714d0d6fcf65ad9519d4491fba80b07bac59
Sachin Arora <sachinarora.707@gmail.com>
2017-10-24 21:11:19 +0530

    Project Euler Solutions Added.

---
+++ Project Euler/Problem 04/sol2.py
@@ -0,0 +1,18 @@
+'''
+Problem:
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 x 99.
+Find the largest palindrome made from the product of two 3-digit numbers which is less than N.
+'''
+arr = []
+for i in range(999,100,-1):
+    for j in range(999,100,-1):
+        t = str(i*j)
+        if t == t[::-1]:
+            arr.append(i*j)
+arr.sort()
+
+n=int(input())
+for i in arr[::-1]:
+    if(i<n):
+        print i
+        exit(0)
---
+++ Project Euler/Problem 06/sol2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+'''
+Problem:
+The sum of the squares of the first ten natural numbers is,
+            1^2 + 2^2 + ... + 10^2 = 385
+The square of the sum of the first ten natural numbers is,
+            (1 + 2 + ... + 10)^2 = 552 = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+Find the difference between the sum of the squares of the first N natural numbers and the square of the sum.
+'''
+n = int(input())
+suma = n*(n+1)/2
+suma **= 2
+sumb = n*(n+1)*(2*n+1)/6
+print suma-sumb
