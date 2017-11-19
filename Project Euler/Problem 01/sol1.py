567f2adab87baaec6872e28571a9725e6228f7a9
Christian Bender <christianbender89@web.de>
2017-11-19 14:54:30 +0100

    Correction: Remove semicolons
    I have remove the semicolons in line 8 and line 12

--- Project Euler/Problem 01/sol1.py
+++ Project Euler/Problem 01/sol1.py
@@ -5,8 +5,8 @@ we get 3,5,6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below N.
 '''
 n = int(raw_input().strip())
-sum=0;
+sum=0
 for a in range(3,n):
     if(a%3==0 or a%5==0):
         sum+=a
-print sum;
+print sum
