f920e2a80959eb982e1695cd48ae2da6e1f85f85
nautiyaldeepak <deepaknautiyal07@outlook.com>
2017-11-21 22:04:04 +0530

    ftp client server connection

---
+++ File_Transfer_Protocol/ftp_client_server.py
@@ -0,0 +1,58 @@
+# server
+
+import socket                   # Import socket module
+
+port = 60000                    # Reserve a port for your service.
+s = socket.socket()             # Create a socket object
+host = socket.gethostname()     # Get local machine name
+s.bind((host, port))            # Bind to the port
+s.listen(5)                     # Now wait for client connection.
+
+print 'Server listening....'
+
+while True:
+    conn, addr = s.accept()     # Establish connection with client.
+    print 'Got connection from', addr
+    data = conn.recv(1024)
+    print('Server received', repr(data))
+
+    filename='mytext.txt'
+    f = open(filename,'rb')
+    l = f.read(1024)
+    while (l):
+       conn.send(l)
+       print('Sent ',repr(l))
+       l = f.read(1024)
+    f.close()
+
+    print('Done sending')
+    conn.send('Thank you for connecting')
+    conn.close()
+
+
+# client side server
+
+import socket                   # Import socket module
+
+s = socket.socket()             # Create a socket object
+host = socket.gethostname()     # Get local machine name
+port = 60000                    # Reserve a port for your service.
+
+s.connect((host, port))
+s.send("Hello server!")
+
+with open('received_file', 'wb') as f:
+    print 'file opened'
+    while True:
+        print('receiving data...')
+        data = s.recv(1024)
+        print('data=%s', (data))
+        if not data:
+            break
+        # write data to a file
+        f.write(data)
+
+f.close()
+print('Successfully get the file')
+s.close()
+print('connection closed')
