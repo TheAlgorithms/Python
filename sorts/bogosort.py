37638c0ca1cbc4f9bd241a38c63cfdc0b0cd78ff
fooLDD <1538458441@qq.com>
2018-02-05 13:08:04 +0800

    Update bogosort.py
    In line48,the stript should be strip

--- sorts/bogosort.py
+++ sorts/bogosort.py
@@ -44,6 +44,6 @@ if __name__ == '__main__':
     except NameError:
         raw_input = input  # Python 3
 
-    user_input = raw_input('Enter numbers separated by a comma:\n').stript()
+    user_input = raw_input('Enter numbers separated by a comma:\n').strip()
     unsorted = [int(item) for item in user_input.split(',')]
     print(bogosort(unsorted))
