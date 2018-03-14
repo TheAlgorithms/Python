301c90737674b9724be03fe7d25e5314cf406a04
Daniel Ingram <ingramds@appstate.edu>
2018-03-14 14:55:07 -0400

    Dynamic programming solution to Problem 25

---
+++ Project Euler/Problem 25/sol1.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+
+def fibonacci(n):
+	if n == 1 or type(n) is not int:
+		return 0
+	elif n == 2:
+		return 1
+	else:
+		sequence = [0, 1]
+		for i in xrange(2, n+1):
+			sequence.append(sequence[i-1] + sequence[i-2])
+
+		return sequence[n]
+
+def fibonacci_digits_index(n):
+	digits = 0
+	index = 2
+
+	while digits < n:
+		index += 1
+		digits = len(str(fibonacci(index)))
+
+	return index
+
+if __name__ == '__main__':
+	print(fibonacci_digits_index(1000))
