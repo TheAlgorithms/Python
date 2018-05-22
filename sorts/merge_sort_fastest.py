c74c0b64970d1cce01a5ae920289c7b589b7f4a0
Syed Haseeb Shah <yesiamhasi@gmail.com>
2018-05-22 15:03:59 +0500

    Update merge_sort_fastest.py

--- sorts/merge_sort_fastest.py
+++ sorts/merge_sort_fastest.py
@@ -4,16 +4,51 @@ Takes an average of 0.6 microseconds to sort a list of length 1000 items.
 Best Case Scenario : O(n)
 Worst Case Scenario : O(n)
 '''
+# These modules are required for testing only.
+import random, copy
+import matplotlib.pyplot as plt
+
 def merge_sort(LIST):
+    '''
+    Returns sorted list.
+    '''
+    # Divide list into two parts
     start = []
     end = []
-    while len(LIST) > 1:
-        a = min(LIST)
-        b = max(LIST)
+    a = min(LIST)
+    b = max(LIST)
+    
+    while len(LIST)>1:
         start.append(a)
         end.append(b)
-        LIST.remove(a)
-        LIST.remove(b)
-    if LIST: start.append(LIST[0])
+        try:
+            LIST.remove(a)
+            LIST.remove(b)
+            a = min(LIST)
+            b = max(LIST)
+        except ValueError:
+            continue
     end.reverse()
-    return (start + end)
+    return start + end
+
+def visualize(LIST, SORTED_LIST):
+    '''
+    Plots unsorted and sorted data.
+    '''
+    plt.plot(LIST)
+    plt.xlabel('Random Data')
+    plt.show()
+    plt.plot(SORTED_LIST)
+    plt.xlabel('Merge Sorted Data')
+    plt.show()
+
+if __name__ == '__main__':
+    # Basic Test
+    LIST = [0,5,2,4,3,9,1,7,6,8]
+    print ('Unordered list {}'.format(LIST))
+    print ('Merge sorted list {}'.format(merge_sort(LIST)))
+    # Generate a list of random length between 100 and 1000.
+    # and add random numbers to it.
+    LIST = [random.randint(0, 1000) for i in range(random.randint(100, 1000))]
+    unsortedLIST = copy.deepcopy(LIST)
+    visualize(unsortedLIST, merge_sort(LIST))
