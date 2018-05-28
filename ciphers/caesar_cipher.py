13617225cafdec85cb067c5c7badca28d408eb92
Harshil <darjiharshil2994@gmail.com>
2018-05-28 23:25:48 +0200

    small improvements!

--- ciphers/caesar_cipher.py
+++ ciphers/caesar_cipher.py
@@ -1,3 +1,4 @@
+import sys
 def encrypt(strng, key):
     encrypted = ''
     for x in strng:
@@ -20,50 +21,48 @@ def decrypt(strng, key):
 def brute_force(strng):
     key = 1
     decrypted = ''
-    while key != 96:
+    while key <= 94:
         for x in strng:
             indx = (ord(x) - key) % 256
             if indx < 32:
                 indx = indx + 95
             decrypted = decrypted + chr(indx)
-        print(decrypted)
+        print("Key: {}\t| Message: {}".format(key, decrypted))
         decrypted = ''
         key += 1
     return None
 
 
 def main():
-    print("**Menu**")
+    print('-' * 10 + "\n**Menu**\n" + '-' * 10)
     print("1.Encrpyt")
     print("2.Decrypt")
     print("3.BruteForce")
     print("4.Quit")
     while True:
-        choice = input("what would you like to do")
+        choice = input("What would you like to do?: ")
         if choice not in ['1', '2', '3', '4']:
             print ("Invalid choice")
         elif choice == '1':
-            strng = input("Please enter the string to be ecrypted:")
+            strng = input("Please enter the string to be ecrypted: ")
             while True:
-                key = int(input("Please enter off-set between 1-94"))
-                if key > 0 and key <= 94:
+                key = int(input("Please enter off-set between 1-94: "))
+                if key in range(1, 95):
                     print (encrypt(strng, key))
                     main()
         elif choice == '2':
-            strng = input("Please enter the string to be decrypted:")
+            strng = input("Please enter the string to be decrypted: ")
             while True:
-                key = int(input("Please enter off-set between 1-94"))
+                key = int(input("Please enter off-set between 1-94: "))
                 if key > 0 and key <= 94:
                     print(decrypt(strng, key))
                     main()
         elif choice == '3':
-            strng = input("Please enter the string to be decrypted:")
+            strng = input("Please enter the string to be decrypted: ")
             brute_force(strng)
             main()
         elif choice == '4':
-            print ("GoodBye.")
-            break
+            print ("Goodbye.")
+            sys.exit()
 
 main()
-
-
