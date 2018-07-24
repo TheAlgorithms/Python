52cebaa841fab2789e80221d73768b9a5f00e107
Ambuj81 <dataaccess81@gmail.com>
2018-07-24 06:38:06 +0530

    Update subset_generation.py
    I made all changes I could  . What I mean is I removed all the empty space ....... 
    There some comment extra if you feel removing those comments please do so yourself pls provide spacing as it should be

--- dynamic_programming/subset_generation.py
+++ dynamic_programming/subset_generation.py
@@ -1,12 +1,9 @@
 # python program to print all subset combination of n element in given set of r element .
-
 #arr[]  ---> Input Array
 #data[] ---> Temporary array to store current combination
 # start & end ---> Staring and Ending indexes in arr[]
 # index  ---> Current index in data[]
 #r ---> Size of a combination to be printed 
-
-
 def combinationUtil(arr,n,r,index,data,i):
 #Current combination is ready to be printed,
 # print it
@@ -15,48 +12,28 @@ def combinationUtil(arr,n,r,index,data,i):
    print(data[j],end=" ")
   print(" ")
   return
-
 #  When no more elements are there to put in data[]
-
- if(i>=n):
+ if(i >= n):
   return
-
 #current is included, put next at next
 # location 
-  
  data[index]=arr[i]
  combinationUtil(arr,n,r,index+1,data,i+1)
- 
  # current is excluded, replace it with
  # next (Note that i+1 is passed, but
  # index is not changed)
- 
  combinationUtil(arr,n,r,index,data,i+1)
-
- 
  # The main function that prints all combinations
  #of size r in arr[] of size n. This function 
  #mainly uses combinationUtil()
- 
- 
- 
 def printcombination(arr,n,r):
-
 # A temporary array to store all combination
 # one by one
- data = list(range(r))
- 
+ data = [0]*r
 #Print all combination using temprary
 #array 'data[]'
  combinationUtil(arr,n,r,0,data,0)
-
- 
 # Driver function to check for above function
 arr = [10,20,30,40,50]
-
-r=3
-n= len(arr)
-printcombination(arr,n,r)
- 
- 
+printcombination(arr,len(arr),3)
 #This code is contributed by Ambuj sahu
