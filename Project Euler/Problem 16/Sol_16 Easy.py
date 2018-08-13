26b2e60792f19a66dbd7e38f91a5b8febfdbc740
Sayan Bandyopadhyay <sayan.bandyopadhyay@yahoo.co.in>
2018-08-13 20:50:10 +0530

    Update and rename temp.py to Sol_16 Easy.py

---
+++ Project Euler/Problem 16/Sol_16 Easy.py
@@ -0,0 +1,2 @@
+for i in range(int(input())):
+    print(sum([int(i) for i in list(str(2**int(input())))]))
--- Project Euler/Problem 16/temp.py
+++
@@ -1,3 +0,0 @@
-n=int(input())
-for i in range(n):
-    print(sum([int(i) for i in list(str(2**int(input())))]))
