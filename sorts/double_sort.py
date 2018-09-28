1a1b60bc813c703670c780e9c1dd08818078db5c
ayush246 <37112252+ayush246@users.noreply.github.com>
2018-09-28 21:43:29 +0530

    Update double_sort.py

--- sorts/double_sort.py
+++ sorts/double_sort.py
@@ -1,15 +1,15 @@
 print ("enter the list to be sorted")
-a = [int(x) for x in input().split()]
-n=len(a)
-for i in range(0,int(((n-1)/2)+1)):
-    for j in range(0,n-1):
-        if a[j+1]<a[j]:
-            temp=a[j+1]
-            a[j+1]=a[j]
-            a[j]=temp
-        if (a[n-1-j]<a[n-2-j]):
-            temp=a[n-1-j]
-            a[n-1-j]=a[n-2-j]
-            a[n-2-j]=temp
+lst = [int(x) for x in input().split()] # inputing elements of the list in one line
+no_of_elements=len(lst)
+for i in range(0,int(((no_of_elements-1)/2)+1)): # we dont need to traverse to end of list as
+    for j in range(0,no_of_elements-1):
+        if (lst[j+1]<lst[j]): # applying bubble sort algorithm from left to right (or forwards)
+            temp=lst[j+1]
+            lst[j+1]=lst[j]
+            lst[j]=temp
+        if (lst[no_of_elements-1-j]<lst[no_of_elements-2-j]): # applying bubble sort algorithm from right to left (or backwards)
+            temp=lst[no_of_elements-1-j]
+            lst[no_of_elements-1-j]=lst[no_of_elements-2-j]
+            lst[no_of_elements-2-j]=temp
 print ("the sorted list is")
-print (a)
+print (lst)
