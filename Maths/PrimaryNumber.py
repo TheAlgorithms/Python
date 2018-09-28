80be8f627c9037606af75235f6c3c59fef6e1246
ahmadrezash <ahmad.sharif.abc@gmail.com>
2018-09-28 09:05:43 +0330

    chang name of variable

--- Maths/PrimaryNumber.py
+++ Maths/PrimaryNumber.py
@@ -2,22 +2,19 @@ import sys
 def main():
     print("Calculate primary number from 0 to your number:")
     while True:
-        num = eval(input("To what number?"))
-        print('resualt:' , pr(num))
-
-def pr(num):
-    #we know 2 is a primary number then next number if undivisibie with 2, it is primary
-    #Any number that not divisible by numbers smaller than itself is primary
-    p = [2]
-    i = num
-    for i in range(3,i+1,2):
+        number = eval(input("To what number?"))
+        print('resualt:' , primary(number))
+def primary(num):
+    #we know 2 is a prime number then next number if undivisibie with 2, it is primary
+    #Any number that not divisible by prime numbers smaller than itself is primary
+    prime_numbers = [2]
+    for i in range(3,number+1,2):
         isPrime=True
-        for x in p:
+        for x in prime_numbers:
             if i % x == 0:
                 isPrime = False
                 break
         if isPrime:
             p.append(i)
-    return p
-    
+    return p   
 main()
