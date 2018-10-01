8e0bb93da17d239d8f7a31177c5f94a7300ef5c7
prashant564 <prashantdixit600@gmail.com>
2018-10-02 02:36:11 +0530

    An update get_initial_centroid function

--- machine_learning/k_means_clust.py
+++ machine_learning/k_means_clust.py
@@ -57,14 +57,8 @@ def get_initial_centroids(data, k, seed=None):
     if seed is not None: # useful for obtaining consistent results
         np.random.seed(seed)
     n = data.shape[0] # number of data points
-        
-    # Pick K indices from range [0, N).
-    rand_indices = np.random.randint(0, n, k)
-    
-    # Keep centroids as dense format, as many entries will be nonzero due to averaging.
-    # As long as at least one document in a cluster contains a word,
-    # it will carry a nonzero weight in the TF-IDF vector of the centroid.
-    centroids = data[rand_indices,:]
+  #using vectorized code to find the centroids      
+  centroids = data[np.random.choice(np.arange(n),k), :]
     
     return centroids
 
