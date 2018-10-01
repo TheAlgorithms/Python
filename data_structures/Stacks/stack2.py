9ac3dc425f997feebc85d397116143803fc40170
Rohit Patil <32801968+RohitPatilRRP@users.noreply.github.com>
2018-10-01 23:49:46 +0530

    new

---
+++ data_structures/Stacks/stack2.py
@@ -0,0 +1,11 @@
+
+# Python code to demonstrate Implementing
+# stack using list
+stack = ["Amar", "Akbar", "Anthony"]
+stack.append("Ram")
+stack.append("Iqbal")
+print(stack)
+print(stack.pop())
+print(stack)
+print(stack.pop())
+print(stack)
