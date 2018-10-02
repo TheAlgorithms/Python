526dc0222aa1d1e49b447dd6dbc61521fa83fbb0
Shubhayu Das <43082352+sateslayer@users.noreply.github.com>
2018-10-02 23:48:16 +0530

    Add files via upload

---
+++ ciphers/Atbash.py
@@ -0,0 +1,14 @@
+def Atbash():
+    inp=raw_input("Enter the sentence to be encrypted  ")
+    output=""
+    for i in inp:
+        extract=ord(i)
+        if extract>=65 and extract<=90:
+            output+=(unichr(155-extract))
+        elif extract>=97 and extract<=122:
+            output+=(unichr(219-extract))
+        else:
+            output+=i
+    print output
+
+Atbash()   ;
