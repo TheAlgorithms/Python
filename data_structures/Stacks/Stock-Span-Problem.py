054b4b31f42a2bfe5dafed66fa88cb477aa63098
S-Sanyal <prosanyal123@gmail.com>
2018-10-03 21:52:30 +0530

    Made compatible for all versions of python

--- data_structures/Stacks/Stock-Span-Problem.py
+++ data_structures/Stacks/Stock-Span-Problem.py
@@ -6,6 +6,7 @@ The span Si of the stock’s price on a given day i is defined as the maximum
 number of consecutive days just before the given day, for which the price of the stock 
 on the current day is less than or equal to its price on the given day.
 '''
+from __future__ import print_function
 def calculateSpan(price, S): 
       
     n = len(price) 
