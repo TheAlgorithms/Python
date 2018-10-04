63c7e8ede14ae7200043c59bf4faaf6104c1b13e
Harshil <darjiharshil2994@gmail.com>
2018-10-04 11:34:50 +0200

    Minor update
    Changed two characters which were the root cause of 'SyntaxError: Non-ASCII character '\xe2' in file main.py, but no encoding declared'

--- data_structures/Stacks/Stock-Span-Problem.py
+++ data_structures/Stacks/Stock-Span-Problem.py
@@ -1,8 +1,8 @@
 '''
 The stock span problem is a financial problem where we have a series of n daily 
-price quotes for a stock and we need to calculate span of stock’s price for all n days.
+price quotes for a stock and we need to calculate span of stock's price for all n days.
 
-The span Si of the stock’s price on a given day i is defined as the maximum 
+The span Si of the stock's price on a given day i is defined as the maximum 
 number of consecutive days just before the given day, for which the price of the stock 
 on the current day is less than or equal to its price on the given day.
 '''
