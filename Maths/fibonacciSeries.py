a8a11ccc88a1343d68bef563c4a72b8f651848e5
timgiroux <40814794+timgiroux@users.noreply.github.com>
2018-10-07 15:52:34 -0700

    fixed some spelling and added a different print message

--- Maths/fibonacciSeries.py
+++ Maths/fibonacciSeries.py
@@ -6,11 +6,11 @@ def recur_fibo(n):
    else:
        return(recur_fibo(n-1) + recur_fibo(n-2))
     
-limit = int(input("How many terms to include in fionacci series:"))
+limit = int(input("How many terms to include in fibonacci series: "))
 
 if limit <= 0:
-   print("Plese enter a positive integer")
+   print("Please enter a positive integer: ")
 else:
-   print("Fibonacci series:")
+   print(f"The first {limit} terms of the fibonacci series are as follows")
    for i in range(limit):
        print(recur_fibo(i))
