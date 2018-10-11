e03426b8fd696b8794e21ef52c76a0a5140e1463
rafagarciac <r.gc@hotmail.es>
2018-10-12 00:41:57 +0200

    Improve and Refactor the fibonnaciSeries.py (Recursion)

--- Maths/fibonacciSeries.py
+++ Maths/fibonacciSeries.py
@@ -1,16 +1,18 @@
 # Fibonacci Sequence Using Recursion
 
 def recur_fibo(n):
-   if n <= 1:
-       return n
-   else:
-       return(recur_fibo(n-1) + recur_fibo(n-2))
+    return n if n <= 1 else (recur_fibo(n-1) + recur_fibo(n-2))
     
-limit = int(input("How many terms to include in fibonacci series: "))
+def isPositiveInteger(limit):
+    return limit >= 0
 
-if limit <= 0:
-   print("Please enter a positive integer: ")
-else:
-   print(f"The first {limit} terms of the fibonacci series are as follows")
-   for i in range(limit):
-       print(recur_fibo(i))
+def main():
+    limit = int(input("How many terms to include in fibonacci series: "))
+    if isPositiveInteger:
+        print(f"The first {limit} terms of the fibonacci series are as follows:")
+        print([recur_fibo(n) for n in range(limit)])
+    else:
+        print("Please enter a positive integer: ")
+
+if __name__ == '__main__':
+    main()
