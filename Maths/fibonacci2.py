16f63fa269c024a19b295077bf390ae0b57e05aa
Cabbaryus <44121585+Cabbaryus@users.noreply.github.com>
2018-10-13 22:42:14 +0300

    Update fibonacci2.py

--- Maths/fibonacci2.py
+++ Maths/fibonacci2.py
@@ -5,22 +5,35 @@ def fibonacci(limit):
     first_number = 1
     second_number = 1
 
-    fibonaccci = [first_number , second_number ]
+    fibonaccci = [0, first_number , second_number ]
 
-    for i in range(limit):
+    for i in range(limit-3):
         first_number , second_number = second_number , first_number + second_number
 
         fibonaccci.append(second_number)
-    print(fibonaccci)
+    return fibonaccci
 
 
-number = int(input("How many terms to include in fibonacci series: "))
+while True:
 
-if number <= 0:
-    print("Please enter a positive integer: ")
-else:
-    print(f"The first {number} terms of the fibonacci series are as follows")
-    fibonacci(number)
+    number = int(input("How many terms to include in fibonacci series: "))
+
+
+    if number <= 0:
+        print("Please enter a positive integer: ")
+    elif number == 1:
+        nump = [0]
+        print(nump)
+        break
+    elif number == 2:
+        pump = [0,1]
+        print(pump)
+        break
+    else:
+        print("The first {number} terms of the fibonacci series are as follows")
+        print(fibonacci(number))
+
+        break
 
 
 
