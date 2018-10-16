f9f5d402d390329b5b88b33c9ce4d625a87917cd
Harshil <darjiharshil2994@gmail.com>
2018-10-16 20:39:22 +0200

    Update bubble_sort.py
    Added main method, Made it Python2 suitable, Enabled user input!

--- sorts/bubble_sort.py
+++ sorts/bubble_sort.py
@@ -1,23 +1,23 @@
-def bubbleSort(arr):
-    n = len(arr)
+from __future__ import print_function
+
+def bubble_sort(arr):
+  n = len(arr)
+  # Traverse through all array elements
+  for i in range(n):
+    # Last i elements are already in place
+      for j in range(0, n-i-1):
+        # traverse the array from 0 to n-i-1
+        # Swap if the element found is greater
+        # than the next element
+        if arr[j] > arr[j+1] :
+              arr[j], arr[j+1] = arr[j+1], arr[j]
+  return arr
  
-    # Traverse through all array elements
-    for i in range(n):
- 
-        # Last i elements are already in place
-        for j in range(0, n-i-1):
- 
-            # traverse the array from 0 to n-i-1
-            # Swap if the element found is greater
-            # than the next element
-            if arr[j] > arr[j+1] :
-                arr[j], arr[j+1] = arr[j+1], arr[j]
- 
-# Driver code to test above
-arr = [64, 34, 25, 12, 22, 11, 90]
- 
-bubbleSort(arr)
- 
-print ("Sorted array is:")
-for i in range(len(arr)):
-    print ("%d" %arr[i]),
+if __name__ == '__main__':
+    try:
+        raw_input          # Python 2
+    except NameError:
+        raw_input = input  # Python 3
+    user_input = raw_input('Enter numbers separated by a comma:').strip()
+    unsorted = [int(item) for item in user_input.split(',')]
+    print(*bubble_sort(unsorted), sep=',')
