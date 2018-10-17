17a152ecb99fbc02379353b75c145fa5991808ab
vasutomar <vasutomar26@gmail.com>
2018-10-18 02:32:02 +0530

    Removed unused variables and redundancy

--- Analysis/Compression_Analysis/PSNR.py
+++ Analysis/Compression_Analysis/PSNR.py
@@ -6,7 +6,6 @@ def Representational(r,g,b):
 	return (0.299*r+0.287*g+0.114*b)
 
 def calculate(img):
-	array = np.zeros(img.shape)
 	b,g,r = cv2.split(img)
 	pixelAt = Representational(r,g,b)
 	return pixelAt
@@ -20,19 +19,13 @@ def main():
 	#Getting image height and width
 	height,width = orignal_image.shape[:2]
 
-	#Error initialization
-	error_sum = 0.0
-
-	orignalPixelAt = 0.0
-	compressedPixelAt = 0.0
-
 	orignalPixelAt = calculate(orignal_image)
 	compressedPixelAt = calculate(compressed_image)
+
 	diff = orignalPixelAt - compressedPixelAt
 	error = np.sum(np.abs(diff) ** 2)
 
 	error = error/(height*width)
-
 
 	#MSR = error_sum/(height*width)
 	PSNR = -(10*math.log10(error/(255*255)))
