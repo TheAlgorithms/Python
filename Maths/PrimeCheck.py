b63a1115b23fcd9e32db6f28d6a569b80252d18e
Kiyoto Kai <35162413+Kaiyoto@users.noreply.github.com>
2018-10-19 22:45:53 +0530

    Added a new Algorithm to check if a number is prime or not. (#487)
    * Added a new Algorithm to check if a number is prime or not.
    
    Added a new Algorithm to check if a number is prime or not. It takes one + half the amount of iterations of the square root of the number. Returns Boolean value.
    
    * Fixed possibility of being truncated
    
    Changed the 1/2 with a 0.5
    
    * Fixed Major Error
    
    Instead of 3, 5, 7 The Loop as checking 2, 4, 6 which would cause all odd numbers to show prime. Fixed by subtracting one.
    
    * Fixed Minor Formatting issues
    
    Github Merged the 2 previous and current version to make a weird file.
    
    * Fixed possibility of being truncated
    
    Changed the 1/2 with a 0.5

---
+++ Maths/PrimeCheck.py
@@ -0,0 +1,17 @@
+def primeCheck(number):
+    prime = True
+    for i in range(2, int(number**(0.5)+1), 2):
+        if i != 2:
+            i = i - 1
+        if number % i == 0:
+            prime = False
+            break
+    return prime
+
+def main():
+    print(primeCheck(37))
+    print(primeCheck(100))
+    print(primeCheck(77))
+
+if __name__ == '__main__':
+	main()
