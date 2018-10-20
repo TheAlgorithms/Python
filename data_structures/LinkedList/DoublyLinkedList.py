a0e68176de3b83b862cfd2be1029cbaf175d7fb1
Ashwek Swamy <39827514+ashwek@users.noreply.github.com>
2018-10-20 19:42:09 +0530

    Added Sentinal Nodes
    Added Sentinel  Nodes (Sentinel Head and Sentinel Tail) for simplification of Insertion (Head / Tail) and deletion of (Head / Tail)
    Added 2nd condition (and current!=self.tail) on Line 52.
    Added self.len variable to count the number of nodes in the Linked List, it is also used to check if the LL is empty or not

--- data_structures/LinkedList/DoublyLinkedList.py
+++ data_structures/LinkedList/DoublyLinkedList.py
@@ -8,61 +8,65 @@ from __future__ import print_function
 
 class LinkedList:
     def __init__(self):
-        self.head = None
-        self.tail = None
+        self.len = 0 # count the number of links (Nodes)
+        self.head = Link(None) #Head Sentinel Node
+        self.tail = Link(None) #Tail Sentinel Node
+        self.head.next = self.tail
+        self.tail.previous = self.head #Linking Head & Tail Sentinel Node with each other
         
     def insertHead(self, x):
         newLink = Link(x) #Create a new link with a value attached to it
-        if(self.isEmpty() == True): #Set the first element added to be the tail
-            self.tail = newLink
-        else:
-            self.head.previous = newLink # newLink <-- currenthead(head)
-        newLink.next = self.head # newLink <--> currenthead(head)
-        self.head = newLink # newLink(head) <--> oldhead
+        self.head.next.previous = newLink # newLink <-- currenthead(head)
+        newLink.next = self.head.next # newLink <--> currenthead(head)
+        self.head.next = newLink # newLink = 1st Link (Node)
+        newLink.previous = self.head
+        self.len+=1
     
     def deleteHead(self):
-        temp = self.head
-        self.head = self.head.next # oldHead <--> 2ndElement(head) 
-        self.head.previous = None # oldHead --> 2ndElement(head) nothing pointing at it so the old head will be removed
-        if(self.head is None):
-            self.tail = None
+        if(self.isEmpty()): return None
+        temp = self.head.next
+        self.head.next = self.head.next.next # HeadSentinel --> 2ndElement(head) 
+        self.head.next.previous = self.head # HeadSentinel <-- 2ndElement(head)
+        self.len-=1
         return temp
     
     def insertTail(self, x):
         newLink = Link(x)
-        newLink.next = None # currentTail(tail)    newLink -->
-        self.tail.next = newLink # currentTail(tail) --> newLink -->
-        newLink.previous = self.tail #currentTail(tail) <--> newLink -->
-        self.tail = newLink # oldTail <--> newLink(tail) -->
+        self.tail.previous.next = newLink
+        newLink.previous = self.tail.previous
+        self.tail.previous = newLink
+        newLink.next = self.tail
+        self.len+=1
     
     def deleteTail(self):
-        temp = self.tail
-        self.tail = self.tail.previous # 2ndLast(tail) <--> oldTail --> None
-        self.tail.next = None # 2ndlast(tail) --> None
+        if(self.isEmpty()): return None
+        temp = self.tail.previous
+        temp.previous.next = self.tail # 2ndLast(tail) --> TailSentinel
+        self.tail.previous = temp.previous # 2ndLast(tail) <-- TailSentinel
+        self.len-=1
         return temp
     
     def delete(self, x):
-        current = self.head
+        current = self.head.next
         
-        while(current.value != x): # Find the position to delete
+        while(current.value != x and current!=self.tail): # Find the position to delete
             current = current.next
             
-        if(current == self.head):
-            self.deleteHead()
-            
-        elif(current == self.tail):
-            self.deleteTail()
+        if(current == self.tail):
+            return None
             
         else: #Before: 1 <--> 2(current) <--> 3
             current.previous.next = current.next # 1 --> 3
             current.next.previous = current.previous # 1 <--> 3
+            self.len-=1
+            return current
     
     def isEmpty(self): #Will return True if the list is empty
-        return(self.head is None)
+        return(self.len == 0)
         
     def display(self): #Prints contents of the list
-        current = self.head
-        while(current != None):
+        current = self.head.next
+        while(current != self.tail):
             current.displayLink()
             current = current.next  
         print()
