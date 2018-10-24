ed113841165717d135a001307d06f4282ad16870
Harshil <darjiharshil2994@gmail.com>
2018-10-24 12:50:39 +0200

    Update sol2.py

--- Project Euler/Problem 03/sol2.py
+++ Project Euler/Problem 03/sol2.py
@@ -3,7 +3,13 @@ Problem:
 The prime factors of 13195 are 5,7,13 and 29. What is the largest prime factor of a given number N?
 e.g. for 10, largest prime factor = 5. For 17, largest prime factor = 17.
 '''
+
 from __future__ import print_function
+try:
+    raw_input          # Python 2
+except NameError:
+    raw_input = input  # Python 3
+
 n=int(raw_input())
 prime=1
 i=2
