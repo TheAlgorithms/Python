93ce9109c60a220c7d10aa059661b6dde5ad18a3
Hasitha Amarathunga <haseeamarathunga@gmail.com>
2018-10-26 01:32:53 -0700

    Update bubble_sort.py

--- sorts/bubble_sort.py
+++ sorts/bubble_sort.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 def bubble_sort(arr):
   n = len(arr)
   # Traverse through all array elements
-  for i in range(n):
+  for i in range(0,n):
     # Last i elements are already in place
       for j in range(0, n-i-1):
         # traverse the array from 0 to n-i-1
@@ -18,6 +18,6 @@ if __name__ == '__main__':
         raw_input          # Python 2
     except NameError:
         raw_input = input  # Python 3
-    user_input = raw_input('Enter numbers separated by a comma:').strip()
+    user_input = raw_input('Enter numbers separated from a comma:').strip()
     unsorted = [int(item) for item in user_input.split(',')]
     print(*bubble_sort(unsorted), sep=',')
