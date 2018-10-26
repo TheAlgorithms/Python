f27d18f0ac537daaadc591e58dd56d0469b70a8a
Adit Negi <aditnegi1@gmail.com>
2018-10-26 17:55:32 +0530

    Update FindMax.py

--- Maths/FindMax.py
+++ Maths/FindMax.py
@@ -1,15 +1,15 @@
 # NguyenU
 
 import math
-def find_max(nums):
+def find_max1(nums):
     max = 0
     for x in nums:
-      if x > max:
+      if x > maxs:
         max = x
     print(max)
 
 def main():
-  find_max([2, 4, 9, 7, 19, 94, 5])
+  find_max1([2, 4, 9, 7, 19, 94, 5])
 
 if __name__ == '__main__':
   main()
