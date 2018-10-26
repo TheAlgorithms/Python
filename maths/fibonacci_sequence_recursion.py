1caf881c72865ca1c5c6b9b239c4610e7bcf492f
Nathan Terra Zanuto <37126478+ntzanuto1610@users.noreply.github.com>
2018-10-26 11:12:57 -0300

    commenting again

--- maths/fibonacci_sequence_recursion.py
+++ maths/fibonacci_sequence_recursion.py
@@ -1,5 +1,5 @@
 # Fibonacci Sequence Using Recursion
-
+#returning the elements of the Fibonacci sequence.
 def recur_fibo(n):
     return n if n <= 1 else (recur_fibo(n-1) + recur_fibo(n-2))
     
@@ -7,6 +7,7 @@ def isPositiveInteger(limit):
     return limit >= 0
 
 def main():
+    #user informs how many elements the sequence will have, being positive.
     limit = int(input("How many terms to include in fibonacci series: "))
     if isPositiveInteger(limit):
         print("The first {limit} terms of the fibonacci series are as follows:")
