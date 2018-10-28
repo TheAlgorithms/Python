36ef8c5b7beb8e9d8a4c1ea661d35ede03dd8c71
jahnaviii <44539747+jahnaviii@users.noreply.github.com>
2018-10-28 16:50:49 +0530

    Update FindMax.py

--- Maths/FindMax.py
+++ Maths/FindMax.py
@@ -7,9 +7,5 @@ def find_max(nums):
       if x > max:
         max = x
     print(max)
-
-def main():
-  find_max([2, 4, 9, 7, 19, 94, 5])
-
-if __name__ == '__main__':
-  main()
+L=list(map(int,input().split()))
+find_max(L)
