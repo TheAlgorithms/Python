88b824308dd9ea6c17c725e4328b28fb834962dd
bhavikb07 <43561956+bhavikb07@users.noreply.github.com>
2018-10-29 15:18:08 +0530

    Updated Optimized Bubble sort
    Worst and Average Case Time Complexity : O(n*n). Worst case occurs when array is reverse sorted.
    Best Case Time Complexity : O(n). Best case occurs when array is already sorted.
    Auxiliary Space : O(1)
    Boundary Cases : Bubble sort takes minimum time (Order of n) when elements are already sorted.

--- sorts/bubble_sort.py
+++ sorts/bubble_sort.py
@@ -1,42 +1,33 @@
-from __future__ import print_function
-
-
-def bubble_sort(collection):
-    """Pure implementation of bubble sort algorithm in Python
-
-    :param collection: some mutable ordered collection with heterogeneous
-    comparable items inside
-    :return: the same collection ordered by ascending
-
-    Examples:
-    >>> bubble_sort([0, 5, 3, 2, 2])
-    [0, 2, 2, 3, 5]
-
-    >>> bubble_sort([])
-    []
-
-    >>> bubble_sort([-2, -5, -45])
-    [-45, -5, -2]
-    
-    >>> bubble_sort([-23,0,6,-4,34])
-    [-23,-4,0,6,34]
-    """
-    length = len(collection)
-    for i in range(length-1):
+# An optimized version of Bubble Sort 
+def bubbleSort(arr): 
+    n = len(arr) 
+   
+    # Traverse through all array elements 
+    for i in range(n): 
         swapped = False
-        for j in range(length-1-i):
-            if collection[j] > collection[j+1]:
+  
+        # Last i elements are already 
+        #  in place 
+        for j in range(0, n-i-1): 
+   
+            # traverse the array from 0 to 
+            # n-i-1. Swap if the element  
+            # found is greater than the 
+            # next element 
+            if arr[j] > arr[j+1] : 
+                arr[j], arr[j+1] = arr[j+1], arr[j] 
                 swapped = True
-                collection[j], collection[j+1] = collection[j+1], collection[j]
-        if not swapped: break  # Stop iteration if the collection is sorted.
-    return collection
-
-
-if __name__ == '__main__':
-    try:
-        raw_input          # Python 2
-    except NameError:
-        raw_input = input  # Python 3
-    user_input = raw_input('Enter numbers separated by a comma:').strip()
-    unsorted = [int(item) for item in user_input.split(',')]
-    print(*bubble_sort(unsorted), sep=',')
+  
+        # IF no two elements were swapped 
+        # by inner loop, then break 
+        if swapped == False: 
+            break
+           
+# Driver code to test above 
+arr = [64, 34, 25, 12, 22, 11, 90] 
+   
+bubbleSort(arr) 
+   
+print ("Sorted array :") 
+for i in range(len(arr)): 
+    print ("%d" %arr[i],end=" ")
