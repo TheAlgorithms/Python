17c8c9005f2cfddedfbdd7aa807a55b345aa4a8b
Harshil <darjiharshil2994@gmail.com>
2018-10-29 18:17:31 +0100

    Update Sieve_ of_Eratosthenes.py
    Python2 to Python3!

--- Sieve_ of_Eratosthenes.py
+++ Sieve_ of_Eratosthenes.py
@@ -4,17 +4,17 @@ sys.stdout = open('output.txt', 'w')
 
 M = int(1e6) + 1
 
-composite = [0 for i in xrange(int(M))]
+composite = [0 for i in range(int(M))]
 
 def sieve():
-	for i in xrange(2, M):
+	for i in range(2, M):
 		cnt = 2
 		while cnt*i < M:
 			composite[cnt*i] = 1
 			cnt += 1
 
 if __name__ == '__main__':
-	print composite[2]
-	print composite[5]
-	print composite[6]
-	print composite[12]
+	print(composite[2])
+	print(composite[5])
+	print(composite[6])
+	print(composite[12])
