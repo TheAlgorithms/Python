37da083ac52293fb980e8960867716bde65298c5
bhavikb07 <43561956+bhavikb07@users.noreply.github.com>
2018-10-29 15:08:55 +0530

    Update FindMax.py

--- Maths/FindMax.py
+++ Maths/FindMax.py
@@ -1,13 +1,7 @@
-# NguyenU
-
-import math
 def find_max(nums):
-    max = 0
-    for x in nums:
-      if x > max:
-        max = x
-    print(max)
-
+    m=sorted(nums,reverse=True)
+    print(m[0])
+ 
 def main():
   find_max([2, 4, 9, 7, 19, 94, 5])
 
