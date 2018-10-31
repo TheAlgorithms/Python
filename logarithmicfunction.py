1249c6d5b383e9b22568d98a5a9d26773931fbba
FazilUdupi <faziludupi@gmail.com>
2018-10-31 23:42:47 +0530

    Create logarithmicfunction.py

---
+++ logarithmicfunction.py
@@ -0,0 +1,9 @@
+import math
+
+print('The value of 5^8: ' + str(math.pow(5, 8)))
+print('Square root of 400: ' + str(math.sqrt(400)))
+
+print('The value of 5^e: ' + str(math.exp(5)))
+print('The value of Log(625), base 5: ' + str(math.log(625, 5)))
+print('The value of Log(1024), base 2: ' + str(math.log2(1024)))
+print('The value of Log(1024), base 10: ' + str(math.log10(1024)))
