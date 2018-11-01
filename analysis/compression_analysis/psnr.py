057291fec2e4bb515ed355e27fb76aece7f3aa21
rafagarciac <r.gc@hotmail.es>
2018-11-01 23:48:34 +0100

    Change some Image File names & re-code the psnr algorithm (conserving both methods). Also Added new example.

--- analysis/compression_analysis/psnr.py
+++ analysis/compression_analysis/psnr.py
@@ -1,38 +1,71 @@
-import numpy as np
-import math
-import cv2
+"""
+	Peak signal-to-noise ratio - PSNR - https://en.wikipedia.org/wiki/Peak_signal-to-noise_ratio
+	1º Method: https://tutorials.techonical.com/how-to-calculate-psnr-value-of-two-images-using-python/
+	2º Method: Incorrect ????
+"""
 
-def Representational(r,g,b):
-	return (0.299*r+0.287*g+0.114*b)
+import math
+
+import cv2
+import numpy as np
+
+# This method is the really work as expected, but however I want to preserve the other method (psnr2) 
+def psnr(original, contrast):
+    mse = np.mean((original - contrast) ** 2)
+    if mse == 0:
+        return 100
+    PIXEL_MAX = 255.0
+    PSNR = 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
+    return PSNR
+
+
+def Representational(r, g, b):
+    # Formula to determine brightness of RGB color
+    return (0.299*r+0.287*g+0.114*b)
+
 
 def calculate(img):
-	b,g,r = cv2.split(img)
-	pixelAt = Representational(r,g,b)
-	return pixelAt
+    b, g, r = cv2.split(img)
+    return Representational(r, g, b)
+
+# The 1º Method really works better 
+def psnr2(original, contrast):
+    # Getting image height and width
+    height, width = original.shape[:2]
+
+    # Calculate the RGB Proportion for each Image and get the difference.
+    originalPixelAt = calculate(original)
+    compressedPixelAt = calculate(contrast)
+
+    diff = originalPixelAt - compressedPixelAt
+
+    # Calculate the error
+    error = np.sum(np.abs(diff) ** 2) / (height * width)
+
+    # MSR = error_sum/(height*width)
+    PSNR = -(10*math.log10(error/(255*255)))
+    return format(PSNR)
+
 
 def main():
-	
-	#Loading images (orignal image and compressed image)
-	orignal_image = cv2.imread('orignal_image.png',1)
-	compressed_image = cv2.imread('compressed_image.png',1)
 
-	#Getting image height and width
-	height,width = orignal_image.shape[:2]
+    # Loading images (original image and compressed image)
+    original = cv2.imread('original_image.png')
+    contrast = cv2.imread('compressed_image.png', 1)
 
-	orignalPixelAt = calculate(orignal_image)
-	compressedPixelAt = calculate(compressed_image)
+    original2 = cv2.imread('PSNR-example-base.png')
+    contrast2 = cv2.imread('PSNR-example-comp-10.jpg', 1)
 
-	diff = orignalPixelAt - compressedPixelAt
-	error = np.sum(np.abs(diff) ** 2)
-
-	error = error/(height*width)
-
-	#MSR = error_sum/(height*width)
-	PSNR = -(10*math.log10(error/(255*255)))
-
-	print("PSNR value is {}".format(PSNR))
+    # Value expected: 29.73dB
+    print("-- First Test --")
+    print(f"1º Method: \n PSNR value is {psnr(original, contrast)} dB")
+    print(f"2º Method: \n PSNR value is {psnr2(original, contrast)} dB \n")
+    
+    # # Value expected: 31.53dB (Wikipedia Example)
+    print("-- Second Test --")
+    print(f"1º Method: \n PSNR value is {psnr(original2, contrast2)} dB")
+    print(f"2º Method: \n PSNR value is {psnr2(original2, contrast2)} dB")
 
 
 if __name__ == '__main__':
-	main()
-
+    main()
