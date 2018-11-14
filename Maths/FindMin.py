8abd42cb1828ad094cdec6a19c249e44cdda799a
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2018-11-14 15:31:48 +0530

    Update FindMin.py

--- Maths/FindMin.py
+++ Maths/FindMin.py
@@ -1,10 +1,9 @@
+# Time Complexity: O(n)
+# Space Complexity: O(1) for n-inputs
+
 def main():
     def findMin(x):
-        minNum = x[0]
-        for i in x:
-            if minNum > i:
-                minNum = i
-        return minNum
+        return min(x) # Used built-in function min() on [x]
 
     print(findMin([0,1,2,3,4,5,-3,24,-56])) # = -56
 
