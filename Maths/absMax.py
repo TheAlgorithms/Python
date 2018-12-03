7d47d8cf96d6c6689cb051daaa263d36d698c0e9
wanderer <19929284+AlpineBlack@users.noreply.github.com>
2018-12-03 19:34:37 +0800

    bugs fixed

--- Maths/absMax.py
+++ Maths/absMax.py
@@ -6,9 +6,10 @@ def absMax(x):
     >>absMax([3,-10,-2])
     -10
     """
-    j = x[0]
+    j = absVal(x[0])
     for i in x:
-        if absVal(i) < j:
+        if absVal(i) > absVal(j):
+            # print "at this:",i
             j = i
     return j
     #BUG: i is apparently a list, TypeError: '<' not supported between instances of 'list' and 'int' in absVal
@@ -16,7 +17,9 @@ def absMax(x):
 
 def main():
     a = [1,2,-11]
-    print(absVal(a)) # = -11
+    print(absMax(a)) # = -11
 
 if __name__ == '__main__':
     main()
+
+
