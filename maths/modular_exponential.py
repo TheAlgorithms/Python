1da28639bdf1811edd02bac3c2ba4bb56e9a7c7f
Sanders Lin <45224617+SandersLin@users.noreply.github.com>
2018-12-06 17:34:03 +0800

    update modularExponential() to be more readable
    I believe this makes the original algorithm more readable. Maybe I should write another function instead of altering someone else's code? Let me know how to go about this.

--- maths/modular_exponential.py
+++ maths/modular_exponential.py
@@ -1,16 +1,10 @@
 def modularExponential(base, power, mod):
-	if power < 0:
-		return -1
-	base %= mod
-	result = 1
-
-	while power > 0:
-		if power & 1:
-			result = (result * base) % mod
-		power = power >> 1
-		base = (base * base) % mod
-	return result
-
+    if power < 0:
+        return -1
+    result = 1
+    for i in range(power): 
+        result = result * base % mod
+    return result
 
 def main():
 	print(modularExponential(3, 200, 13))
