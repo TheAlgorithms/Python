29ce68640fe24e64601865275d1ef1a3ef4c48c3
Sanders Lin <45224617+SandersLin@users.noreply.github.com>
2018-12-06 16:19:57 +0800

    Update primeCheck.py to process all integers
    According to wiki page A prime number (or a prime) is a natural number greater than 1 that cannot be formed by multiplying two smaller natural numbers.

--- maths/PrimeCheck.py
+++ maths/PrimeCheck.py
@@ -1,5 +1,7 @@
 import math
 def primeCheck(number):
+    if number < 2 : 
+        return False
     if number % 2 == 0 and number > 2: 
         return False
     return all(number % i for i in range(3, int(math.sqrt(number)) + 1, 2))
@@ -8,6 +10,9 @@ def main():
     print(primeCheck(37))
     print(primeCheck(100))
     print(primeCheck(77))
+    print(primeCheck(0))
+    print(primeCheck(1))
+    print(primeCheck(-1))
 
 if __name__ == '__main__':
 	main()
