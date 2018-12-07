24712525cc42ce47d5e473a81c5c73b057dea114
Sanders Lin <45224617+SandersLin@users.noreply.github.com>
2018-12-07 20:47:20 +0800

    Update sol1.py

--- project_euler/problem_16/sol1.py
+++ project_euler/problem_16/sol1.py
@@ -1,15 +1,6 @@
 power = int(input("Enter the power of 2: "))
 num = 2**power
-
 string_num = str(num)
-
-list_num = list(string_num)
-
-sum_of_num = 0
-
+sum_of_num = sum([int(i) for i in string_num])
 print("2 ^",power,"=",num)
-
-for i in list_num:
-    sum_of_num += int(i)
-
 print("Sum of the digits are:",sum_of_num)
