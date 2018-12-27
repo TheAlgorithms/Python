3677ae08e538fcc927c6b24fb82b355af21a97fd
mikelfried <mikelfried@gmail.com>
2018-12-27 18:00:39 +0200

    avrage.py
    calculate and print the avrage of number list.

---
+++ Maths/avrage.py
@@ -0,0 +1,14 @@
+def avrage(nums):
+    sum = 0
+    n = 0
+    for x in nums:
+      sum += x
+      n += 1
+    avg = sum / n
+    print(avg)
+
+def main():
+  avrage([2, 4, 6, 8, 20, 50, 70])
+
+if __name__ == '__main__':
+  main()
