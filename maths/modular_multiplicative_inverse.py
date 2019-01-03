e7718ae52ca49b98fd459d5a173d9512fe0f2954
Jitendra_Sharma <jitendra.sharma_cs16@gla.ac.in>
2019-01-03 14:21:52 +0530

    all conflicts are resolved

--- maths/modular_multiplicative_inverse.py
+++ maths/modular_multiplicative_inverse.py
@@ -1,7 +1,7 @@
 # python program to find modular inverse of a under modulo m.
 from math import pow
 
-def MMI(int a, int p, int m):
+def MMI(a, p, m):
 	"""
 	According to fermat's little theorem, if m is prime. than,
 	(a^p)%m = a%m
@@ -13,10 +13,10 @@ def MMI(int a, int p, int m):
 	if not p :
 		return 1
 	tmp =  MMI(a, p//2, m) % m;
-	tmp = (tmp**2) % m
-	return tmp if p%2==0 else (temp*p % m)
+	tmp = (tmp*tmp) % m
+	return tmp if p%2==0 else (tmp*a % m)
 
 # Driver Program
 if __name__ == '__main__':
-	a, m = map(int, input().split().strip())
-	print(MMI(a, m))
+	a, m = map(int, input().split())
+	print(MMI(a, m-2, m))
