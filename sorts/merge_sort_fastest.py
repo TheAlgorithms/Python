2e382d00f2d92964883c0baaa655e78de2e9c484
Simon Larsén <slarse@kth.se>
2019-02-25 13:12:23 +0100

    Fix merge_sort_fastest mutating input

--- sorts/merge_sort_fastest.py
+++ sorts/merge_sort_fastest.py
@@ -4,16 +4,17 @@ Takes an average of 0.6 microseconds to sort a list of length 1000 items.
 Best Case Scenario : O(n)
 Worst Case Scenario : O(n)
 '''
-def merge_sort(LIST):
+def merge_sort(lst):
+    lst = list(lst) # copy argument to avoid wrecking it
     start = []
     end = []
-    while len(LIST) > 1:
-        a = min(LIST)
-        b = max(LIST)
+    while len(lst) > 1:
+        a = min(lst)
+        b = max(lst)
         start.append(a)
         end.append(b)
-        LIST.remove(a)
-        LIST.remove(b)
-    if LIST: start.append(LIST[0])
+        lst.remove(a)
+        lst.remove(b)
+    if lst: start.append(lst[0])
     end.reverse()
     return (start + end)
