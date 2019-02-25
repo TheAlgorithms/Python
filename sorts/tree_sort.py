b46d67a4aed4b1046c26b27331d1a69cc84a4d7d
Simon Larsén <slarse@kth.se>
2019-02-25 13:12:36 +0100

    Fix tree sort not handling duplicate elements

--- sorts/tree_sort.py
+++ sorts/tree_sort.py
@@ -1,15 +1,16 @@
 # Tree_sort algorithm
 # Build a BST and in order traverse.
 
-class node():
+
+class node:
     # BST data structure
     def __init__(self, val):
         self.val = val
-        self.left = None 
-        self.right = None 
-    
-    def insert(self,val):
-        if self.val:
+        self.left = None
+        self.right = None
+
+    def insert(self, val):
+        if self.val is not None:
             if val < self.val:
                 if self.left is None:
                     self.left = node(val)
@@ -20,26 +21,31 @@ class node():
                     self.right = node(val)
                 else:
                     self.right.insert(val)
-        else:
-            self.val = val
+            else:  # duplicate value, displace tree left
+                new_left = node(val)
+                new_left.left = self.left
+                self.left = new_left
+
 
 def inorder(root, res):
-    # Recursive travesal 
+    # Recursive travesal
     if root:
-        inorder(root.left,res)
+        inorder(root.left, res)
         res.append(root.val)
-        inorder(root.right,res)
+        inorder(root.right, res)
+
 
 def treesort(arr):
     # Build BST
     if len(arr) == 0:
-        return arr
+        return []
     root = node(arr[0])
-    for i in range(1,len(arr)):
+    for i in range(1, len(arr)):
         root.insert(arr[i])
-    # Traverse BST in order. 
+    # Traverse BST in order.
     res = []
-    inorder(root,res)
+    inorder(root, res)
     return res
 
-print(treesort([10,1,3,2,9,14,13]))
+
+print(treesort([10, 1, 3, 2, 9, 14, 13]))
