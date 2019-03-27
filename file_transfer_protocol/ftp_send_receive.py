8b8a6d881cbda0e6d64aacf5284fbccf27772eec
louisparis <48298425+louisparis@users.noreply.github.com>
2019-03-27 19:46:46 +0200

    reduce indentation (#741)

--- file_transfer_protocol/ftp_send_receive.py
+++ file_transfer_protocol/ftp_send_receive.py
@@ -1,11 +1,11 @@
 """
-	File transfer protocol used to send and receive files using FTP server.
-	Use credentials to provide access to the FTP client
+File transfer protocol used to send and receive files using FTP server.
+Use credentials to provide access to the FTP client
 
-	Note: Do not use root username & password for security reasons
-	  Create a seperate user and provide access to a home directory of the user
-	  Use login id and password of the user created 
-	  cwd here stands for current working directory
+Note: Do not use root username & password for security reasons
+Create a seperate user and provide access to a home directory of the user
+Use login id and password of the user created 
+cwd here stands for current working directory
 """
 
 from ftplib import FTP
@@ -14,8 +14,8 @@ ftp.login(user='username', passwd='password')
 ftp.cwd('/Enter the directory here/')
 
 """
-	The file which will be received via the FTP server
-	Enter the location of the file where the file is received
+The file which will be received via the FTP server
+Enter the location of the file where the file is received
 """
 
 def ReceiveFile():
@@ -25,8 +25,8 @@ def ReceiveFile():
 	ftp.quit()
 
 """
-	The file which will be sent via the FTP server
-	The file send will be send to the current working directory
+The file which will be sent via the FTP server
+The file send will be send to the current working directory
 """
 
 def SendFile():
