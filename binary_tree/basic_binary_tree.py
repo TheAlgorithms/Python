934e919ef7e0df15cb107af6624010fa189d1bbd
kicksent <albert212@gmail.com>
2019-04-07 00:22:40 -0600

    added insert_into_tree and updated tests in main

--- binary_tree/basic_binary_tree.py
+++ binary_tree/basic_binary_tree.py
@@ -5,6 +5,19 @@ class Node: # This is the Class Node with constructor that contains data variabl
         self.right = None
 
 
+def insert_into_tree(tree, val):
+    if tree is None:
+        return Node(val)
+    else:
+        if val < tree.data:
+            tree.left = insert_into_tree(tree.left, val)
+        elif val > tree.data:
+            tree.right = insert_into_tree(tree.right, val)
+        else:
+            print("Could not insert {} value already exists".format(val))
+    return(tree)
+
+
 def depth_of_tree(tree): #This is the recursive function to find the depth of binary tree.
     if tree is None:
         return 0
@@ -29,15 +42,14 @@ def is_full_binary_tree(tree): # This functions returns that is it full binary t
 
 
 def main(): # Main func for testing.
-    tree = Node(1)
-    tree.left = Node(2)
-    tree.right = Node(3)
-    tree.left.left = Node(4)
-    tree.left.right = Node(5)
-    tree.left.right.left = Node(6)
-    tree.right.left = Node(7)
-    tree.right.left.left = Node(8)
-    tree.right.left.left.right = Node(9)
+    #empty tree
+    tree = None
+    #insert nodes with values 1 to 9
+    for i in range(1, 10):
+        tree = insert_into_tree(tree, i)
+    #test print if item exists already
+    tree = insert_into_tree(tree, 1)
+
 
     print(is_full_binary_tree(tree))
     print(depth_of_tree(tree))
