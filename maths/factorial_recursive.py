df04d9454332dc0ea916a6dbe6da2b71d7ae9782
Vysor <vysor@users.noreply.github.com>
2019-04-23 00:53:56 +1000

    Some directories had a capital in their name [fixed]. Added a recursive factorial algorithm. (#763)
    * Renaming directories
    * Adding a recursive factorial algorithm

---
+++ maths/factorial_recursive.py
@@ -0,0 +1,13 @@
+def fact(n):
+	"""
+	Return 1, if n is 1 or below,
+	otherwise, return n * fact(n-1).
+	"""
+	return 1 if n <= 1 else n * fact(n-1)
+
+"""
+Shown factorial for i,
+where i ranges from 1 to 20.
+"""
+for i in range(1,21):
+	print(i, ": ", fact(i), sep='')
