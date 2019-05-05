abba6a0bcd63fe54c534bfb699225180e0737f8e
Mehdi ALAOUI <alaoui.mehdi.1995@gmail.com>
2019-05-05 02:10:30 +0200

    Adding test cases and test functions

--- sorts/tests.py
+++ sorts/tests.py
@@ -21,3 +21,37 @@ from tim_sort import tim_sort
 from topological_sort import topological_sort
 from tree_sort import tree_sort
 from wiggle_sort import wiggle_sort
+
+
+TEST_CASES = [
+    {'input': [8, 7, 6, 5, 4, 3, -2, -5], 'expected': [-5, -2, 3, 4, 5, 6, 7, 8]},
+    {'input': [-5, -2, 3, 4, 5, 6, 7, 8], 'expected': [-5, -2, 3, 4, 5, 6, 7, 8]},
+    {'input': [5, 6, 1, 4, 0, 1, -2, -5, 3, 7], 'expected': [-5, -2, 0, 1, 1, 3, 4, 5, 6, 7]},
+]
+
+
+TEST_FUNCTIONS = [
+    bogo_sort,
+    bubble_sort,
+    bucket_sort,
+    cocktail_shaker_sort,
+    comb_sort,
+    counting_sort,
+    cycle_sort,
+    gnome_sort,
+    heap_sort,
+    insertion_sort,
+    merge_sort_fastest,
+    merge_sort,
+    pancake_sort,
+    quick_sort_3partition,
+    quick_sort,
+    radix_sort,
+    quick_sort_random,
+    selection_sort,
+    shell_sort,
+    tim_sort,
+    topological_sort,
+    tree_sort,
+    wiggle_sort,
+]
