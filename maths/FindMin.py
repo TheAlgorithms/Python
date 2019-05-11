fb74d9b3a644f78fce7a7ca0f40e68029521701a
teenageknight <davidgeorge0227@gmail.com>
2019-05-11 11:00:19 -0400

    style changes
    Added changes to the style of FindMin.py to match FindMax.py

--- maths/FindMin.py
+++ maths/FindMin.py
@@ -1,11 +1,11 @@
-def main():
-    def findMin(x):
-        minNum = x[0]
-        for i in x:
-            if minNum > i:
-                minNum = i
-        return minNum
+def find_min(nums):
+    min = nums[0]
+    for x in nums:
+        if min > x:
+            min = x
+    return min
 
+def main():
     print(findMin([0,1,2,3,4,5,-3,24,-56])) # = -56
 
 if __name__ == '__main__':
