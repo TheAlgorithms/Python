f8a0cbdb1a18f061c9b091d66c81d8fde3b8f593
SiJingZ <SiJingZ@users.noreply.github.com>
2019-05-13 23:06:23 +1000

    Update timsort.py

--- sorts/timsort.py
+++ sorts/timsort.py
@@ -72,11 +72,17 @@ def timsort(lst):
     return sorted_array
 
 
+test_lists = [
+  [],
+  [1],
+  [5, 9, 10, 3, -4, 5, 178, 92, 46, -18, 0, 7],
+  [1, 2, 5, 8, 3, 6, 9, 4, 7],
+]
+
+
 def main():
-    assert timsort([]) == []
-    assert timsort([1]) == [1]
-    assert timsort([5, 9, 10, 3, -4, 5, 178, 92, 46, -18, 0, 7]) == sorted([5, 9, 10, 3, -4, 5, 178, 92, 46, -18, 0, 7])
-    assert timsort([1, 2, 5, 8, 3, 6, 9, 4, 7]) == sorted([1, 2, 5, 8, 3, 6, 9, 4, 7])
+    for test_list in test_lists:
+        assert timsort(test_list) == sorted(test_list)
     lst = [5, 9, 10, 3, -4, 5, 178, 92, 46, -18, 0, 7]
     sorted_lst = timsort(lst)
     print(sorted_lst)
