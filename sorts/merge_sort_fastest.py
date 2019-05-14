26130682722c260e2b7911eb1f21a080b0226294
liuzhen <liuzhen@bookgoal.com>
2019-05-14 12:57:53 +0800

    More elegant coding for merge_sort_fastest

--- sorts/merge_sort_fastest.py
+++ sorts/merge_sort_fastest.py
@@ -1,19 +1,46 @@
 '''
-Python implementation of merge sort algorithm.
+Python implementation of the fastest merge sort algorithm.
 Takes an average of 0.6 microseconds to sort a list of length 1000 items.
 Best Case Scenario : O(n)
 Worst Case Scenario : O(n)
 '''
-def merge_sort(LIST):
-    start = []
-    end = []
-    while len(LIST) > 1:
-        a = min(LIST)
-        b = max(LIST)
-        start.append(a)
-        end.append(b)
-        LIST.remove(a)
-        LIST.remove(b)
-    if LIST: start.append(LIST[0])
+from __future__ import print_function
+
+
+def merge_sort(collection):
+    """Pure implementation of the fastest merge sort algorithm in Python
+
+    :param collection: some mutable ordered collection with heterogeneous
+    comparable items inside
+    :return: a collection ordered by ascending
+
+    Examples:
+    >>> merge_sort([0, 5, 3, 2, 2])
+    [0, 2, 2, 3, 5]
+
+    >>> merge_sort([])
+    []
+
+    >>> merge_sort([-2, -5, -45])
+    [-45, -5, -2]
+    """
+    start, end = [], []
+    while len(collection) > 1:
+        min_one, max_one = min(collection), max(collection)
+        start.append(min_one)
+        end.append(max_one)
+        collection.remove(min_one)
+        collection.remove(max_one)
     end.reverse()
-    return (start + end)
+    return start + collection + end
+
+
+if __name__ == '__main__':
+    try:
+        raw_input          # Python 2
+    except NameError:
+        raw_input = input  # Python 3
+
+    user_input = raw_input('Enter numbers separated by a comma:\n').strip()
+    unsorted = [int(item) for item in user_input.split(',')]
+    print(*merge_sort(unsorted), sep=',')
