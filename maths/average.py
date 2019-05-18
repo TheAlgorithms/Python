3be9e513791a3798c49ea56207ac24f5b53754f7
Abhijeeth S <35632653+abhikmp@users.noreply.github.com>
2019-05-18 11:58:45 +0530

    Update average.py
    Added option to take input from user.

--- maths/average.py
+++ maths/average.py
@@ -10,5 +10,8 @@ def average(nums):
 def main():
   average([2, 4, 6, 8, 20, 50, 70])
 
+  #uncomment this to take input from user.
+  #average(list(map(int, input("Enter a list a numbers seperated by commas: ").split(','))))
+
 if __name__ == '__main__':
   main()
