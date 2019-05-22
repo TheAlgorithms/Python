0da0e68ff16b89baf4278451a1788acef5db46fb
Parisa Mousavi <43146130+parisam83@users.noreply.github.com>
2019-05-22 18:26:57 +0430

    Create insertion_sort_new.py
    This is a new insertion sort algorithm which is much more faster than algorithms shared before!

---
+++ sorts/insertion_sort_new.py
@@ -0,0 +1,24 @@
+"""
+This is a fast insertion sort algorithm in python 
+which it's time complixity is less than other shared
+algorithms.
+
+Just run `insertion_sort` function to see the result.
+"""
+
+def insertion_sort(arr):
+  for i in range(1, len(arr)):
+      j = i - 1
+      for k in range(i):
+          if arr[i] < arr[j]:
+              arr[i], arr[j] = arr[j], arr[i]
+              i = i -1
+              j = j - 1
+
+
+'''create a random list in order to test insertion sort algorithm'''
+from random import shuffle
+arr = list(range(2000))    #very huge list!
+shuffle(arr)
+insertion_sort(arr)
+print(arr)
