0520bce50b00226df42cf018a40a8468fc172735
Erfan Alimohammadi <erfan.aa@gmail.com>
2019-05-27 17:23:33 +0430

    Add fermat's little theorem

---
+++ maths/fermat_little_theorem.py
@@ -0,0 +1,30 @@
+# Python program to show the usage of fermat's little theorem in a division
+# Time complexity: O(log p)
+# According to fermat's little theorem, (a / b) mod p always equals a * (b ^ (p - 2)) mod p
+# Here we assume that p is a prime number, b divides a, and p doesn't divide b
+
+
+def binary_exponentiation(a, n, mod):
+    
+    if (n == 0):
+        return 1
+    
+    elif (n % 2 == 1):
+        return (binary_exponentiation(a, n - 1, mod) * a) % mod
+    
+    else:
+        b = binary_exponentiation(a, n / 2, mod)
+        return (b * b) % mod
+
+
+# a prime number
+p = 701
+
+a = 1000000000
+b = 10
+
+# using binary exponentiation function:
+print((a / b) % p == (a * binary_exponentiation(b, p - 2, p)) % p)
+
+# using Python operators:
+print((a / b) % p == (a * b ** (p - 2)) % p)
