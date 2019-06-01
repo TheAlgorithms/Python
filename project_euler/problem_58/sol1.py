947395c753e07ca4159ee236cc8d151d2156de30
nehamsoni <45124893+nehamsoni@users.noreply.github.com>
2019-06-01 15:35:32 +0530

    solution for problem 58 project euler

---
+++ project_euler/problem_58/sol1.py
@@ -0,0 +1,21 @@
+from sympy import *
+
+numofprime=0
+con=1
+lis=[1]
+i=3
+while True:
+  for j in range(4):
+    con+=i-1
+    lis.append(con)
+  
+  for k in lis[-4:]:
+    if isprime(k)==True:
+      numofprime+=1
+  if numofprime/len(lis)<0.1:
+    print(i)
+    break
+  i+=2
+
+#26241-answer
+
