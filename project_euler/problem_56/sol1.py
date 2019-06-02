517936083d87d7d1d05ce7670288bd30a19de728
nehamsoni <45124893+nehamsoni@users.noreply.github.com>
2019-06-03 02:51:57 +0530

    all done

--- project_euler/problem_56/sol1.py
+++ project_euler/problem_56/sol1.py
@@ -1,6 +1,13 @@
+from __future__ import print_function
+
+try:
+	xrange		#Python 2
+except NameError:
+	xrange = range	#Python 3
+    
 high=0
-for a in range(1,100):
-    for b in range(1,100):
+for a in xrange(1,100):
+    for b in xrange(1,100):
         c=0
         for k in list(str(a**b)):
             c+=int(k)
