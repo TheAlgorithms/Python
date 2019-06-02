69270b302a182666609a1b8d54bf8cb4aef59abc
nehamsoni <45124893+nehamsoni@users.noreply.github.com>
2019-06-03 02:27:32 +0530

    repaired

--- project_euler/problem_58/sol1.py
+++ project_euler/problem_58/sol1.py
@@ -1,21 +1,22 @@
 from sympy import *
-
 numofprime=0
 con=1
 lis=[1]
 i=3
 while True:
-  for j in range(4):
-    con+=i-1
-    lis.append(con)
+    for j in range(4):
+        con+=i-1
+        lis.append(con)
   
-  for k in lis[-4:]:
-    if isprime(k)==True:
-      numofprime+=1
-  if numofprime/len(lis)<0.1:
-    print(i)
-    break
-  i+=2
+    for k in lis[-4:]:
+        if isprime(k)==True:
+            numofprime+=1
+    
+    if numofprime/len(lis)<0.1:
+        print(i)
+        break
+    
+    i+=2
 
 #26241-answer
 
