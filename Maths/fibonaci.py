1104a6a062aa077b97c2a5af46f709df55dcc549
Shivam Mittal <jmjmatter@gmail.com>
2019-06-03 19:46:20 +0530

    Updated fibonacci.py
    Changed the indentation to 4 Spaces

--- Maths/fibonaci.py
+++ Maths/fibonaci.py
@@ -3,7 +3,7 @@ def calculating_fibonacci(number):
 		return 1
 	else: 
 		return calculating_fibonacci(number-1)+ calculating_fibonacci(n-2)
-
+#Changed Indentation to 4 Spaces
 
 def find_first_n_fibonacci(number):
 	for i in range(0,number):
