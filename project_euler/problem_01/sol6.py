b4b2d5610a60c36eddb304d312465803396002d9
John Law <johnlaw.po@gmail.com>
2019-06-05 09:56:42 +0800

    Update sol6.py

--- project_euler/problem_01/sol6.py
+++ project_euler/problem_01/sol6.py
@@ -1,8 +1,9 @@
 a=3
 result=0
-while a<=1000:
-  if(a%3==0 and a%5==0):
-    result+=a
-  elif(a%15==0):
-    result-=a
+while a<1000:
+    if(a%3==0 or a%5==0):
+        result+=a
+    elif(a%15==0):
+        result-=a
+    a+=1
 print(result)
