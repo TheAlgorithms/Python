b3feb066e202fb7665861634b21c8ff06a961080
nehamsoni <45124893+nehamsoni@users.noreply.github.com>
2019-06-08 10:51:02 +0530

    finale

--- project_euler/problem_56/sol1.py
+++ project_euler/problem_56/sol1.py
@@ -1,6 +1,6 @@
 high=0
-for a in xrange(1,100):
-    for b in xrange(1,100):
+for a in range(1,100):
+    for b in range(1,100):
         c=0
         for k in list(str(a**b)):
             c+=int(k)
