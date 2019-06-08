1ca9e24dbc30ceebc6950999cb7d66d735d883ca
nehamsoni <45124893+nehamsoni@users.noreply.github.com>
2019-06-08 20:10:33 +0530

    added url

--- project_euler/problem_63/sol1.py
+++ project_euler/problem_63/sol1.py
@@ -1,3 +1,4 @@
+# https://projecteuler.net/problem=63
 """
 The reason i have choosen 25 is because any number which when is raised to the power of 25 gradually increases from
 8 digits of 2^25 linearly(at first with a diffrence of 4(that is num_of_digit in 2^25 is 8 and 3^25 id 12) which starts 
