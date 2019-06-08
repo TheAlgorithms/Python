a83f18fb2c7b1d2f5181b144c6df1a0df58ff792
cclauss <cclauss@me.com>
2019-06-08 09:29:03 +0200

    https://projecteuler.net/problem=1

--- Project Euler/Problem 01/sol5.py
+++ Project Euler/Problem 01/sol5.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+
 """Find the sum of all the multiples of 3 or 5 below 1000."""
+
+# https://projecteuler.net/problem=1
 
 print(sum(i if 0 in (i % 3, i % 5) else 0 for i in range(1000)))
