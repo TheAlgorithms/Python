12a16d63b7bdc1da0e0b215dfd5d8b938234f4ed
Adeoti Ayodeji <33290249+Lord-sarcastic@users.noreply.github.com>
2019-06-22 05:42:28 +0100

    Update average.py (#908)
    Reduced lines of code and extra processing on the line: n += 1

--- maths/average.py
+++ maths/average.py
@@ -1,11 +1,10 @@
 def average(nums):
     sum = 0
-    n = 0
     for x in nums:
       sum += x
-      n += 1
-    avg = sum / n
+    avg = sum / len(nums)
     print(avg)
+    return avg
 
 def main():
   average([2, 4, 6, 8, 20, 50, 70])
