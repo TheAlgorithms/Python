2d69e46102ea2cbf93c0c58a2c4a4b988e678596
Anshul <anshul.padhi@research.iiit.ac.in>
2019-06-29 07:03:39 +0530

    Update johnsonAPSP.py

--- graphs/johnsonAPSP.py
+++ graphs/johnsonAPSP.py
@@ -3,51 +3,49 @@
 import math
 
 def fillEdges():	
-	for edge in range(numEdge):
-		print('input 3 numbers:starting vertex, ending vertex, weight')
-		u, v, w=input().split()
-		edges.append((int(u), int(v), int(w)))
+    for edge in range(numEdge):
+	print('input 3 numbers:starting vertex, ending vertex, weight')
+	    u, v, w=input().split()
+	    edges.append((int(u), int(v), int(w)))
 
 def fillGraph():
-	for edge in newEdges:
-		u=edge[0]
-		v=edge[1]
-		w=edge[2]
-		if len(str(adjList[int(u)]))>0:
-			adjList[int(u)].append((int(v), int(w)))
-		else:
-			adjList[int(u)]=(int(v), int(w))
+    for edge in newEdges:
+	u=edge[0]
+	v=edge[1]
+	w=edge[2]
+	if len(str(adjList[int(u)]))>0:
+	    adjList[int(u)].append((int(v), int(w)))
+	else:
+	    adjList[int(u)]=(int(v), int(w))
 
 def bellFord(source):
-	minDist[source]=0
-	for outer in range(numVert):
-		for edge in edges:
-			if minDist[int(edge[1])]>minDist[int(edge[0])]+int(edge[2]):
-				minDist[int(edge[1])]=minDist[int(edge[0])]+int(edge[2])
+    minDist[source]=0
+    for outer in range(numVert):
+	for edge in edges:
+	    if minDist[int(edge[1])]>minDist[int(edge[0])]+int(edge[2]):
+		minDist[int(edge[1])]=minDist[int(edge[0])]+int(edge[2])
 
 def reWeigh():
-	for edge in edges:
-		newEdges.append((edge[0], edge[1], edge[2]+minDist[edge[0]]-minDist[edge[1]]))
+    for edge in edges:
+	newEdges.append((edge[0], edge[1], edge[2]+minDist[edge[0]]-minDist[edge[1]]))
 
 def dijk(src):
-	ans=[math.inf]*(numVert+1)
-	ans[src]=0
-	pq=[]
-	pq.append((0, src))
-	while len(pq)>0:
-		pq.sort()
-		u=pq[0][1]
-		del pq[0]
-		for tup in adjList[u]:
-			if ans[tup[0]]>ans[u]+tup[1]:
-				ans[tup[0]]=ans[u]+tup[1]
-				pq.append((ans[tup[0]], tup[0]))
-	print('This is the shortest path of all vertices from {}:{}'.format(src, ans[1:]))
+    ans=[math.inf]*(numVert+1)
+    ans[src]=0
+    pq=[]
+    pq.append((0, src))
+    while len(pq)>0:
+	pq.sort()
+	u=pq[0][1]
+	del pq[0]
+	for tup in adjList[u]:
+	    if ans[tup[0]]>ans[u]+tup[1]:
+		ans[tup[0]]=ans[u]+tup[1]
+		pq.append((ans[tup[0]], tup[0]))
+    print('This is the shortest path of all vertices from {}:{}'.format(src, ans[1:]))
 
-print('input no of vertices')
-numVert=int(input())
-print('input no of edges')
-numEdge=int(input())
+numVert=5
+numEdge=10
 adjList = {new_list: [] for new_list in range(1,numVert+2)} 
 print('initialised graph')
 edges=[]
@@ -55,7 +53,7 @@ fillEdges()
 print('filled edges')
 minDist=[math.inf]*(numVert+2)
 for i in range(1, numVert+1):
-	edges.append((int(numVert+1), i, 0))
+    edges.append((int(numVert+1), i, 0))
 bellFord(numVert+1)
 newEdges=[]
 del edges[numEdge:len(edges)]
@@ -65,4 +63,4 @@ fillGraph()
 print('filled graph')
 minDist=[math.inf]*(numVert+2)
 for src in range(1, numVert+1):
-	dijk(src)
+    dijk(src)
