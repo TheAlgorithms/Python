4ae331e99778ff09888f7e90a0c265a6908fff3b
AugustofCravo <49079453+AugustofCravo@users.noreply.github.com>
2019-07-02 17:50:21 -0300

    Update FirstQuartile.py

--- maths/FirstQuartile.py
+++ maths/FirstQuartile.py
@@ -9,14 +9,34 @@ def FirstQuartile(list):
     quartile1 = st.median(list[:len(list)//2])   
     return quartile1
 
+def SecondQuartile(list):
+    """
+    Returns the second quartile of a list. 
+    Ex.: list = [1,2,3,4,5,6] 
+    second quartile = 3.5
+    """ 
+    quartile2 = st.median(list)   
+    return quartile2
+
+def ThirdQuartile(list):
+    """
+    Returns the third quartile of a list. 
+    Ex.: list = [1,2,3,4,5,6] 
+    third quartile = 5
+    """ 
+    quartile3 = st.median(list[len(list)//2:])   
+    return quartile3
+
 def main():
     a = [1,3,5,7,9,11]
     print(FirstQuartile(a)) # = 3
+    print(SecondQuartile(a)) # = 6
+    print(ThirdQuartile(a)) # = 9
 
 
 if __name__ == '__main__':
     main()
-
+    
 """
-print FirstQuartile
+print Quartiles
 """
