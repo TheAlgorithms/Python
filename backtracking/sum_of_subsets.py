88644d9fc912be5f31e16804c2f8e6a5c5d57e3e
hetzz <hetal.kuvadia@somaiya.edu>
2019-07-04 02:19:04 +0530

    Adding nqueens.py for backtracking

---
+++ backtracking/sum_of_subsets.py
@@ -0,0 +1,24 @@
+def generate_sum_of_subsets_soln(nums, max_sum):
+	result = []
+	path = []
+	num_index = 0
+	remaining_nums_sum = sum(nums)
+	create_state_space_tree(nums, max_sum, num_index, path,result, remaining_nums_sum)
+	return result
+
+def create_state_space_tree(nums,max_sum,num_index,path,result, remaining_nums_sum):
+	if sum(path) > max_sum or (remaining_nums_sum + sum(path)) < max_sum:
+		return
+	if sum(path) == max_sum:
+		result.append(path)
+		return
+	for num_index in range(num_index,len(nums)):
+		create_state_space_tree(nums, max_sum, num_index + 1, path + [nums[num_index]], result, remaining_nums_sum - nums[num_index])
+
+print("Enter the elements")
+nums = list(map(int, input().split()))
+print("Enter max_sum sum")
+max_sum = int(input())
+
+result = generate_sum_of_subsets_soln(nums,max_sum)
+print(result)
