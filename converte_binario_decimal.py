86c8512bac742d85530cdf574cc4f94434bea20b
Dhandarah <dhandarah1996@gmail.com>
2019-07-03 23:09:44 -0300

    Update converte_binario_decimal.py
    Translated to English

--- converte_binario_decimal.py
+++ converte_binario_decimal.py
@@ -1,17 +1,17 @@
-def converterb_d(n):
+def convertb_d(n):
     decimal = 0
     n = str(n)
     n = n[::-1]
-    tam = len(n)
-    for i in range(tam):
+    lenght = len(n)
+    for i in range(lenght):
         if n[i] == "1":
             decimal = decimal + 2**i
     return decimal
 
 def main():
     
-    binar = input('Digite o número binário: ')
-    b_d = converterb_d(binar)
-    print ('O número binário {} corresponde a {} em decimal'.format(binar,b_d ))
+    binar = input('Enter the binary number: ')
+    b_d = convertb_d(binar)
+    print ('The binary number {} corresponds to {} in decimal'.format(binar,b_d ))
     
 main()
