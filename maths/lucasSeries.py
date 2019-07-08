f6de68b2a8d1c9d777539eba3e0568b902940253
PatOnTheBack <51241310+PatOnTheBack@users.noreply.github.com>
2019-07-08 16:13:09 -0400

    Update lucasSeries.py

--- maths/lucasSeries.py
+++ maths/lucasSeries.py
@@ -1,16 +1,13 @@
-"""Lucas Sequence Using Recursion."""
-
+# Lucas Sequence Using Recursion
 
 def recur_luc(n):
-    """Calculate Lucas Sequence Using Recursion."""
-    if n == 1:
-        return n
-    if n == 0:
-        return 2
-    return recur_luc(n - 1) + recur_luc(n - 2)
-
-
-LIMIT = int(input("How many terms to include in Lucas series:"))
+  if n == 1:
+    return n
+  if n == 0:
+    return 2
+  return (recur_luc(n-1) + recur_luc(n-2))
+    
+limit = int(input("How many terms to include in Lucas series:"))
 print("Lucas series:")
-for i in range(LIMIT):
-    print(recur_luc(i))
+for i in range(limit):
+  print(recur_luc(i))
