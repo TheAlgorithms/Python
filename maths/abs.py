897f1d0fb4c4f78dd8a7e82820e843f7f9f38738
PatOnTheBack <51241310+PatOnTheBack@users.noreply.github.com>
2019-07-10 16:09:24 -0400

    Improved Formatting and Style of Math Algos (#960)
    * Improved Formatting and Style
    
    I improved formatting and style to make PyLama happier.
    
    Linters used:
    
    - mccabe
    - pep257
    - pydocstyle
    - pep8
    - pycodestyle
    - pyflakes
    - pylint
    - isort
    
    * Create volume.py
    
    This script calculates the volumes of various shapes.
    
    * Delete lucasSeries.py
    
    * Revert "Delete lucasSeries.py"
    
    This reverts commit 64c19f7a6c8b74e15bed07f0f0337598a001ceb4.
    
    * Update lucasSeries.py

--- maths/abs.py
+++ maths/abs.py
@@ -1,24 +1,25 @@
 """Absolute Value."""
 
 
-def absVal(num):
+def abs_val(num):
     """
     Find the absolute value of a number.
 
-    >>absVal(-5)
+    >>abs_val(-5)
     5
-    >>absVal(0)
+    >>abs_val(0)
     0
     """
     if num < 0:
         return -num
-    else:
-        return num
+
+    # Returns if number is not < 0
+    return num
 
 
 def main():
     """Print absolute value of -34."""
-    print(absVal(-34))  # = 34
+    print(abs_val(-34))  # = 34
 
 
 if __name__ == '__main__':
