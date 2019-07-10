9cc30226ca010028853a58cae4ebff2d5afd542c
Sanders Lin <45224617+SandersLin@users.noreply.github.com>
2019-07-11 02:59:24 +0800

    Update basic_maths.py
    clean code and made fixed

--- maths/basic_maths.py
+++ maths/basic_maths.py
@@ -1,65 +1,40 @@
 """Implementation of Basic Math in Python."""
+from __future__ import division
 import math
-
 
 def prime_factors(n):
     """Find Prime Factors."""
-    pf = []
-    while n % 2 == 0:
-        pf.append(2)
-        n = int(n / 2)
+    if n < 2: 
+        raise ValueError('Prime factors not defined in negative numbers or 1.')
 
-    for i in range(3, int(math.sqrt(n)) + 1, 2):
+    pf = []
+    for i in [2] + list(range(3, int(math.sqrt(n)) + 1, 2)):
         while n % i == 0:
             pf.append(i)
-            n = int(n / i)
-
+            n = n//i
     if n > 2:
         pf.append(n)
-
     return pf
+
+
+def divisors(n):
+    """returns list of divisor"""
+    divisors = []
+    for i in range(1, int(math.sqrt(n) + 1)):
+        if n % i == 0 and i*i != n:
+            divisors.append(i)
+            divisors.append(n // i)
+    return sorted(divisors)
 
 
 def number_of_divisors(n):
     """Calculate Number of Divisors of an Integer."""
-    div = 1
-
-    temp = 1
-    while n % 2 == 0:
-        temp += 1
-        n = int(n / 2)
-    div = div * (temp)
-
-    for i in range(3, int(math.sqrt(n)) + 1, 2):
-        temp = 1
-        while n % i == 0:
-            temp += 1
-            n = int(n / i)
-        div = div * (temp)
-
-    return div
+    return len(divisors(n))
 
 
 def sum_of_divisors(n):
     """Calculate Sum of Divisors."""
-    s = 1
-
-    temp = 1
-    while n % 2 == 0:
-        temp += 1
-        n = int(n / 2)
-    if temp > 1:
-        s *= (2**temp - 1) / (2 - 1)
-
-    for i in range(3, int(math.sqrt(n)) + 1, 2):
-        temp = 1
-        while n % i == 0:
-            temp += 1
-            n = int(n / i)
-        if temp > 1:
-            s *= (i**temp - 1) / (i - 1)
-
-    return s
+    return sum(divisors(n))
 
 
 def euler_phi(n):
@@ -75,6 +50,7 @@ def euler_phi(n):
 def main():
     """Print the Results of Basic Math Operations."""
     print(prime_factors(100))
+    print(divisors(100))
     print(number_of_divisors(100))
     print(sum_of_divisors(100))
     print(euler_phi(100))
