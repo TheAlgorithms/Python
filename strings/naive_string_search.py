a1a3ef94e7599d774d34f9f62eb48ee1e26e6fd6
Pavel Denisov <pashamozg@gmail.com>
2019-07-13 15:40:28 +0300

    Change pattern search mechanism
      1. Change patern search for() loop to list slice -
         code run about 2-5 time faster
      2. Code formating

--- strings/naive_string_search.py
+++ strings/naive_string_search.py
@@ -7,23 +7,24 @@ Complexity : O(n*m)
     n=length of main string
     m=length of pattern string
 """
-def naivePatternSearch(mainString,pattern):
-    patLen=len(pattern)
-    strLen=len(mainString)
-    position=[]
-    for i in range(strLen-patLen+1):
-        match_found=True
-        for j in range(patLen):
-            if mainString[i+j]!=pattern[j]:
-                match_found=False
-                break
-        if match_found:
+
+
+def naivePatternSearch(mainString, pattern):
+    patLen = len(pattern)
+    strLen = len(mainString)
+    position = []
+    if patLen > strLen:
+        return position
+    for i in range(strLen - patLen + 1):
+        if mainString[i : i + patLen + 1] == pattern:
             position.append(i)
     return position
 
-mainString="ABAAABCDBBABCDDEBCABC"
-pattern="ABC"
-position=naivePatternSearch(mainString,pattern)
+
+mainString = "ABAAABCDBBABCDDEBCABC"
+pattern = "ABC"
+position = naivePatternSearch(mainString, pattern)
 print("Pattern found in position ")
 for x in position:
     print(x)
+
