a385ec7b7cb8b39e999bd8494eab19086c15a671
thiru15 <33665749+thiru15@users.noreply.github.com>
2019-07-14 12:57:17 +0530

    Update find_lcm.py
    Added comments and improved the quality of code

--- maths/find_lcm.py
+++ maths/find_lcm.py
@@ -5,10 +5,14 @@
 
 def find_lcm(num_1, num_2):
     """Find the LCM of two numbers."""
-    max_num = num_1 if num_1 > num_2 else num_2
+    if num_1>=num_2:
+        max_num=num_1
+    else:
+        max_num = num2
+    
     lcm = max_num
-    while True:
-        if ((lcm % num_1 == 0) and (lcm % num_2 == 0)):
+    while True: #Run the loop until lcm value becomes divisible by both num_1 and num_2
+        if ((lcm % num_1 == 0) and (lcm % num_2 == 0)): 
             break
         lcm += max_num
     return lcm
@@ -16,9 +20,9 @@ def find_lcm(num_1, num_2):
 
 def main():
     """Use test numbers to run the find_lcm algorithm."""
-    num_1 = 12
-    num_2 = 76
-    print(find_lcm(num_1, num_2))
+    num_1 = int(input())   #get the number1 input from user 
+    num_2 = int(input())   # get the number 2 input from user
+    print(find_lcm(num_1, num_2)) # calls th lcm function
 
 
 if __name__ == '__main__':
