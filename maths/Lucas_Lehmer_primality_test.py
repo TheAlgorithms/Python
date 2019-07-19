f88f03cd372339c03e33376bc3a80f7a5a6d9dc5
obelisk0114 <obelisk0114@gmail.com>
2019-07-19 14:30:02 -0700

    Add Lucas_Lehmer_primality_test

---
+++ maths/Lucas_Lehmer_primality_test.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+        In mathematics, the Lucas–Lehmer test (LLT) is a primality test for Mersenne numbers.
+        https://en.wikipedia.org/wiki/Lucas%E2%80%93Lehmer_primality_test
+        
+        A Mersenne number is a number that is one less than a power of two.
+        That is M_n = 2^n - 1
+        https://en.wikipedia.org/wiki/Mersenne_prime
+        
+        The Lucas–Lehmer test is the primality test used by the 
+        Great Internet Mersenne Prime Search (GIMPS) to locate large primes.
+"""
+
+
+def Lucas_Lehmer_test(p: int) -> bool:
+    """
+    >>> Lucas_Lehmer_test(p=7)
+    True
+    
+    >>> Lucas_Lehmer_test(p=11)
+    False
+    
+    # M_11 = 2^11 - 1 = 2047 = 23 * 89
+    """
+
+    if p < 2:
+        raise ValueError("p should not be less than 2!")
+
+    if p == 2:
+        return True
+
+    s = 4
+    M = (1 << p) - 1
+    for i in range(p - 2):
+        s = ((s * s) - 2) % M
+
+    return s == 0
+
+
+if __name__ == "__main__":
+    p = 7
+    print(Lucas_Lehmer_test(p))
+
+    p = 11
+    print(Lucas_Lehmer_test(p))
