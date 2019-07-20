6393f220f3fbb3a82b888eee9b5f5970158fbd35
QuantumNovice <43876848+QuantumNovice@users.noreply.github.com>
2019-07-20 21:24:45 +0500

    Add files via upload

--- maths/mobius_function.py
+++ maths/mobius_function.py
@@ -1,10 +1,11 @@
-'''
+"""
 Refrences: https://en.wikipedia.org/wiki/M%C3%B6bius_function
 References: wikipedia:square free number
-'''
+python/black : True
+flake8 : True
+"""
 
 from typing import List
-
 
 
 def prime_factors(n: int) -> List[int]:
@@ -30,7 +31,7 @@ def prime_factors(n: int) -> List[int]:
     Traceback (most recent call last):
         ...
     TypeError: '<=' not supported between instances of 'int' and 'str'
-    >>> prime_factors([1,2,'hello'])
+    >>> prime_factors([1, 2, 'hello'])
     Traceback (most recent call last):
         ...
     TypeError: '<=' not supported between instances of 'int' and 'list'
@@ -48,23 +49,24 @@ def prime_factors(n: int) -> List[int]:
         factors.append(n)
     return factors
 
-def is_square_free(factors:List[int]) -> bool:
-    '''
+
+def is_square_free(factors: List[int]) -> bool:
+    """
     This functions takes a list of prime factors as input.
     returns True if the factors are square free.
 
-    >>> is_square_free([1,1,2,3,4])
+    >>> is_square_free([1, 1, 2, 3, 4])
     False
 
     These are wrong but should return some value
     it simply checks for repition in the numb
-    >>> is_square_free([1,3,4,'sd', 0.0])
+    >>> is_square_free([1, 3, 4, 'sd', 0.0])
     True
     
-    >>> is_square_free([1,0.5, 2, 0.0])
+    >>> is_square_free([1, 0.5, 2, 0.0])
     True
     
-    >>> is_square_free([1,2, 2, 5])
+    >>> is_square_free([1, 2, 2, 5])
     False
     
     >>> is_square_free('asd')
@@ -74,14 +76,14 @@ def is_square_free(factors:List[int]) -> bool:
     Traceback (most recent call last):
         ...
     TypeError: 'int' object is not iterable
-    '''
-    i: int
+    """
     if len(set(factors)) != len(factors):
-            return False
+        return False
     return True
 
-def mobius(n:int) -> int:
-    '''
+
+def mobius(n: int) -> int:
+    """
     Mobius function
     >>> mobius(24)
     0
@@ -101,15 +103,14 @@ def mobius(n:int) -> int:
     Traceback (most recent call last):
         ...
     TypeError: '<=' not supported between instances of 'int' and 'list'
-    '''
+    """
     factors = prime_factors(n)
     if is_square_free(factors):
-        if len(factors)%2 == 0:
-            return 1
-        elif len(factors)%2 != 0:
-            return -1
+        return -1 if len(factors) % 2 else 1
     return 0
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import doctest
+
     doctest.testmod()
