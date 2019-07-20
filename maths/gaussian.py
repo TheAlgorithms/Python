024d5fd5a6fb82f9976ff8491e9f28b233542f19
QuantumNovice <43876848+QuantumNovice@users.noreply.github.com>
2019-07-20 16:18:01 +0500

    Update gaussian.py

--- maths/gaussian.py
+++ maths/gaussian.py
@@ -18,8 +18,8 @@ def gaussian(x, mu: float = 0.0, sigma: float = 1.0) -> int:
     >>> gaussian(24)
     3.342714441794458e-126
 
-    # Supports NumPy Arrays
-    # Use numpy.meshgrid with this to generate gaussian blur on images.
+    Supports NumPy Arrays
+    Use numpy.meshgrid with this to generate gaussian blur on images.
     >>> import numpy as np
     >>> x = np.arange(15)
     >>> gaussian(x)
