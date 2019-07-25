ecb79bad40dc89f820fd0dce52d6223959498d28
QuantumNovice <43876848+QuantumNovice@users.noreply.github.com>
2019-07-25 21:04:51 +0500

    pyTests added

--- arithmetic_analysis/in_static_equilibrium.py
+++ arithmetic_analysis/in_static_equilibrium.py
@@ -16,6 +16,10 @@ def polar_force(
     """
     Resolves force along rectangular components.
     (force, angle) => (force_x, force_y)
+    >>> polar_force(10, 45)
+    [7.0710678118654755, 7.0710678118654755]
+    >>> polar_force(10, 3.14, radian_mode=True)
+    [-9.999987317275394, 0.01592652916486828]
     """
     if radian_mode:
         return [magnitude * cos(angle), magnitude * sin(angle)]
@@ -36,6 +40,10 @@ def in_static_equilibrium(
                         [x1, y1],
                         [x2, y2],
                         ....]
+    >>> force = array([[1, 1], [-1, 2]])
+    >>> location = array([[1, 0], [10, 0]])
+    >>> in_static_equilibrium(force, location)
+    False
     """
     # summation of moments is zero
     moments: array = cross(location, forces)
@@ -46,17 +54,12 @@ def in_static_equilibrium(
 if __name__ == "__main__":
     # Test to check if it works
     forces = array(
-        [
-            polar_force(718.4, 180 - 30),
-            polar_force(879.54, 45),
-            polar_force(100, -90)
-        ]
+        [polar_force(718.4, 180 - 30), polar_force(879.54, 45), polar_force(100, -90)]
     )
 
     location = array([[0, 0], [0, 0], [0, 0]])
 
     assert in_static_equilibrium(forces, location)
-   
 
     # Problem 1 in image_data/2D_problems.jpg
     forces = array(
@@ -70,7 +73,6 @@ if __name__ == "__main__":
     location = array([[0, 0], [0, 0], [0, 0]])
 
     assert in_static_equilibrium(forces, location)
-    
 
     # Problem in image_data/2D_problems_1.jpg
     forces = array([[0, -2000], [0, -1200], [0, 15600], [0, -12400]])
@@ -78,4 +80,7 @@ if __name__ == "__main__":
     location = array([[0, 0], [6, 0], [10, 0], [12, 0]])
 
     assert in_static_equilibrium(forces, location)
-    
+
+    import doctest
+
+    doctest.testmod()
