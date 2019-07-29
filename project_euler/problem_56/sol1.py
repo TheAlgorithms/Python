a545998b2ec4f672ac5f9a7229611ab5fedd68eb
John Law <johnlaw.po@gmail.com>
2019-07-29 17:28:31 +0800

    Update sol1.py

--- project_euler/problem_56/sol1.py
+++ project_euler/problem_56/sol1.py
@@ -1,12 +1,12 @@
 # https://projecteuler.net/problem=56
-high=0
+high = 0
 for a in range(1,100):
     for b in range(1,100):
-        c=0
+        c = 0
         for k in list(str(a**b)):
-            c+=int(k)
-        if c>high:
-            high=c
+            c += int(k)
+        if c > high:
+            high = c
 
 print(high)
-#972--answer
+# 972--answer
