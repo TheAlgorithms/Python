0d7f2b304bce2c104244a346fc47f188caf3fdd3
Abhijeeth S <abhikampurath@gmail.com>
2019-07-29 19:33:20 +0530

    Input() statements have been indented.
    input() statements are indented under if __name__ == "__main__":

--- maths/LargestOfVeryLargeNumbers.py
+++ maths/LargestOfVeryLargeNumbers.py
@@ -11,19 +11,21 @@ def res(x,y):
         elif(y==0):
             return 1                       # any number raised to 0 is 1
 
-# Reads two numbers from input and typecasts them to int using map function.
-# Here x is the base and y is the power.
-x1,y1 = map(int, input().split(','))
-x2,y2 = map(int, input().split(','))
+        
+if __name__ == "__main__":                 # Main function
+    # Reads two numbers from input and typecasts them to int using map function.
+    # Here x is the base and y is the power.
+    x1,y1 = map(int, input().split(','))
+    x2,y2 = map(int, input().split(','))
 
-# We find the log of each number, using the function res(), which takes two arguments.
-res1=res(x1,y1)
-res2=res(x2,y2)
+    # We find the log of each number, using the function res(), which takes two arguments.
+    res1=res(x1,y1)
+    res2=res(x2,y2)
 
-# We check for the largest number            
-if(res1>res2):
-    print("Largest number is",x1,'^',y1)
-elif(res2>res1):
-    print("Largest number is", x2, '^', y2)
-else:
-    print("Both are equal")
+    # We check for the largest number            
+    if(res1>res2):
+        print("Largest number is",x1,'^',y1)
+    elif(res2>res1):
+        print("Largest number is", x2, '^', y2)
+    else:
+        print("Both are equal")
