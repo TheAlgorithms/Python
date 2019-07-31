38d459cb1eba49e2c8c8dd4bf5c68388d962e483
John Law <johnlaw.po@gmail.com>
2019-07-31 23:09:03 +0800

    doctest

--- maths/collatz_sequence.py
+++ maths/collatz_sequence.py
@@ -4,6 +4,9 @@ def collatz_sequence(n):
     if the previous term is even, the next term is one half the previous term. 
     If the previous term is odd, the next term is 3 times the previous term plus 1. 
     The conjecture states the sequence will always reach 1 regaardess of starting n.
+    Example:
+    >>> collatz_sequence(43)
+    [43, 130, 65, 196, 98, 49, 148, 74, 37, 112, 56, 28, 14, 7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
     """
     sequence = [n]
     while n != 1:
