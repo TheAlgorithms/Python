7772c04d34a0224ae9f90677b3f758c0a2e17e30
Rakshit Parashar <34675136+rishu2403@users.noreply.github.com>
2019-07-31 21:14:39 +0530

    Update Tkinter_Calculator.py

--- maths/Tkinter_Calculator.py
+++ maths/Tkinter_Calculator.py
@@ -1,60 +1,71 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 # calculator with some exception handling
-#Learn about Tkinter library form https://docs.python.org/3/library/tkinter.html
+# Learn about Tkinter library form https://docs.python.org/3/library/tkinter.html
 
-
-import tkinter as tk 
+import tkinter as tk
 from tkinter import messagebox
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title('Calculator')
 
-root = tk.Tk()
-root.title("Calculator")
-
-label = tk.Label(root, text="Enter first number", pady=(10))
+label = tk.Label(root, text='Enter first number', pady=10)
 label.pack()
 
 first_number_entry = tk.Entry(root)
 first_number_entry.pack()
 
-label2 = tk.Label(root, text="Enter second number")
+label2 = tk.Label(root, text='Enter second number')
 label2.pack()
 
 second_number_entry = tk.Entry(root)
 second_number_entry.pack()
 
-operations = tk.Label(root, text="Operations")
+operations = tk.Label(root, text='Operations')
 operations.pack()
 
+
 def addition():
-    first, second = takeValueInput()
+    (first, second) = takeValueInput()
     result = first + second
+
     # print(first + second)
-    result_label.config(text="Operations result is: " +
-                             str(result))
+
+    result_label.config(text='Operations result is: ' + str(result))
+
 
 def subtract():
-    first, second = takeValueInput()
+    (first, second) = takeValueInput()
     result = first - second
+
     # print(first + second)
-    result_label.config(text="Operations result is: " +
-                             str(result))
+
+    result_label.config(text='Operations result is: ' + str(result))
+
 
 def multiply():
-    first, second = takeValueInput()
+    (first, second) = takeValueInput()
     result = first * second
+
     # print(first + second)
-    result_label.config(text="Operations result is: " +
-                             str(result))
+
+    result_label.config(text='Operations result is: ' + str(result))
+
 
 def divide():
-    first, second = takeValueInput()
+    (first, second) = takeValueInput()
 
     if second == 0:
-        messagebox.showerror("Error", "Cannot divide the value by 0.")
+        messagebox.showerror('Error', 'Cannot divide the value by 0.')
     else:
         result = first / second
+
         # print(first + second)
+
         result = round(result, 2)
-        result_label.config(text="Operations result is: " +
-                                 str(result))
+        result_label.config(text='Operations result is: ' + str(result))
+
 
 def takeValueInput():
     first = first_number_entry.get()
@@ -64,30 +75,30 @@ def takeValueInput():
         first = int(first)
         second = int(second)
 
-        return first, second
+        return (first, second)
     except ValueError:
-        if ((len(first_number_entry.get()) == 0) or (len(second_number_entry.get()) == 0)):
-            messagebox.showerror("Error", "Please enter a value")
+        if len(first_number_entry.get()) == 0 \
+            or len(second_number_entry.get()) == 0:
+            messagebox.showerror('Error', 'Please enter a value')
         else:
-            messagebox.showerror("Error", "Enter only integer value")
+            messagebox.showerror('Error', 'Enter only integer value')
         quit(0)
 
 
-addition_button = tk.Button(root, text="+",
-                            command=lambda : addition())
+addition_button = tk.Button(root, text='+', command=lambda : addition())
 addition_button.pack()
 
-minus_button = tk.Button(root, text="-",
-                         command=lambda : subtract())
+minus_button = tk.Button(root, text='-', command=lambda : subtract())
 minus_button.pack()
 
-multiply_button = tk.Button(root, text="*",
-                            command=lambda : multiply())
+multiply_button = tk.Button(root, text='*', command=lambda : multiply())
 multiply_button.pack()
 
-division_button = tk.Button(root, text="/",
-                            command=lambda : divide())
+division_button = tk.Button(root, text='/', command=lambda : divide())
 division_button.pack()
 
-result_label = tk.Label(root, text="Operations result is:")
+result_label = tk.Label(root, text='Operations result is:')
 result_label.pack()
+
+
+			
