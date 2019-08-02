f2064e08e61213029534edc35b827c76c6d7be7a
Syed Waleed Hyder <syedwaleedhyder@gmail.com>
2019-08-02 20:28:18 +0200

    bin(num) can convert ZERO and negative decimal numbers to binary. Consistent with built-in python bin(x) function.

--- conversions/decimal_to_binary.py
+++ conversions/decimal_to_binary.py
@@ -3,21 +3,43 @@
 
 def decimal_to_binary(num):
     """Convert a Decimal Number to a Binary Number."""
+
+    if num == 0:
+        return 0
+    
+    negative = False
+
+    if num < 0:
+        negative = True
+        num = -num
+
     binary = []
     while num > 0:
         binary.insert(0, num % 2)
         num >>= 1
+
+    if negative:
+        return "-" + "".join(str(e) for e in binary)
+
     return "".join(str(e) for e in binary)
 
 
 def main():
     """Print binary equivelents of decimal numbers."""
+    print("\n0 in binary is:")
+    print(decimal_to_binary(0))  # = 0
     print("\n2 in binary is:")
     print(decimal_to_binary(2))  # = 10
     print("\n7 in binary is:")
     print(decimal_to_binary(7))  # = 111
     print("\n35 in binary is:")
     print(decimal_to_binary(35))  # = 100011
+    print("\n-2 in binary is:")  
+    print(decimal_to_binary(-2))  # = -10
+    print("\n-7 in binary is:")
+    print(decimal_to_binary(7))  # = -111
+    print("\n-35 in binary is:")
+    print(decimal_to_binary(-35))  # = -100011
     print("\n")
 
 
