488abdd9b7dac8e69526c2c6e876b2d8a00ab602
itsvinayak <itssvinayak@gmail.com>
2019-08-07 04:13:38 +0530

    recursion,project_euler(99)

--- project_euler/problem_99/sol1.py
+++ project_euler/problem_99/sol1.py
@@ -1,3 +1,21 @@
+'''
+https://projecteuler.net/problem=99
+
+Problem Statement:
+
+    Comparing two numbers written in index form like 2^11 and 3^7 is not difficult,
+    as any calculator would confirm that 2^11 = 2048 < 3^7 = 2187.
+
+    However, confirming that 632382^518061 > 519432^525806 would be much more difficult,
+    as both numbers contain over three million digits.
+
+    Using base_exp.txt (right click and 'Save Link/Target As...'),
+    a 22K text file containing one thousand lines with a base/exponent pair on each line,
+    determine which line number has the greatest numerical value.
+
+
+'''
+
 import csv
 import math
 
