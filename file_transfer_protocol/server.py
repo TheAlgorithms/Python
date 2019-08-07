317fffcc5aade0883c5ccea888a8615601be9cb1
Alex <opti.jawsome@gmail.com>
2019-08-06 22:01:19 -0400

    'with open() as' syntax

--- file_transfer_protocol/server.py
+++ file_transfer_protocol/server.py
@@ -2,6 +2,7 @@ import socket                   # Import socket module
 
 ONE_CONNECTION_ONLY = True      # Set this to False if you wish to continuously accept connections
 
+filename='mytext.txt'
 port = 12312                    # Reserve a port for your service.
 sock = socket.socket()             # Create a socket object
 host = socket.gethostname()     # Get local machine name
@@ -16,19 +17,18 @@ while True:
     data = conn.recv(1024)
     print('Server received', repr(data))
 
-    filename='mytext.txt'
-    out_file = open(filename,'rb')
-    data = out_file.read(1024)
-    while (data):
-       conn.send(data)
-       print('Sent ',repr(data))
-       data = out_file.read(1024)
-    out_file.close()
+    with open(filename,'rb') as out_file:
+        data = out_file.read(1024)
+        while (data):
+           conn.send(data)
+           print('Sent ',repr(data))
+           data = out_file.read(1024)
 
     print('Done sending')
     conn.send(b'Thank you for connecting')
     conn.close()
     if ONE_CONNECTION_ONLY:  # This is to make sure that the program doesn't hang while testing
         break
+
 sock.shutdown(1)
 sock.close()
