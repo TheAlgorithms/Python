2a6a25bddb145bd53a23e33d0959eeed51dd32c3
Alok Shukla <alok.linuxmail@gmail.com>
2019-08-13 21:55:28 +0530

    Adding Type and Doctest as per guideline

--- project_euler/problem_56/sol1.py
+++ project_euler/problem_56/sol1.py
@@ -1,15 +1,27 @@
-def maximum_digital_sum(a,b):
-    '''
-    Considering natural numbers of the form, a**b, where a, b < 100,
-    what is the maximum digital sum?
-    :param a:
-    :param b:
-    :return:
-    '''
+import typing
+
+
+def maximum_digital_sum(a,b) -> int:
+    """
+        Considering natural numbers of the form, a**b, where a, b < 100,
+        what is the maximum digital sum?
+        :param a:
+        :param b:
+        :return:
+        >>> maximum_digital_sum(10,10)
+        45
+
+        >>> maximum_digital_sum(100,100)
+        972
+
+        >>> maximum_digital_sum(100,200)
+        1872
+    """
+
     # RETURN the MAXIMUM from the list of SUMs of the list of INT converted from STR of BASE raised to the POWER
     return max([sum([int(x) for x in str(base**power)]) for base in range(a) for power in range(b)])
 
 #Tests
-print(maximum_digital_sum(10,10), "is 45")
-print(maximum_digital_sum(100,100), "is 972")
-print(maximum_digital_sum(100,200), "is 1872")
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
