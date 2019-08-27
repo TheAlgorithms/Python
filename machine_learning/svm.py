78cdc4824ceb71929720e8e11a04659edce858bc
Riemann <40825655+anand372@users.noreply.github.com>
2019-08-27 23:16:25 +0530

    update svm.py

--- machine_learning/svm.py
+++ machine_learning/svm.py
@@ -4,7 +4,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 
-def NuSVC(train_x, train_y, test_x):
+def NuSVC(train_x, train_y, test_x, test_y):
     svc_NuSVC = svm.NuSVC()
     svc_NuSVC.fit(train_x, train_y)
     predicted_NuSVC = svc_NuSVC.predict(test_x)
@@ -13,7 +13,7 @@ def NuSVC(train_x, train_y, test_x):
     print("NuSVC score:{} %".format(score_NuSVC * 100))
 
 
-def Linearsvc(train_x, train_y, test_x):
+def Linearsvc(train_x, train_y, test_x, test_y):
     svc_linear = svm.LinearSVC()
     svc_linear.fit(train_x, train_y)
     predicted_svc_linear = svc_linear.predict(test_x)
@@ -22,7 +22,7 @@ def Linearsvc(train_x, train_y, test_x):
     print("Linearsvc score:{} %".format(score_svc_linear * 100))
 
 
-def SVC(train_x, train_y, test_x):
+def SVC(train_x, train_y, test_x, test_y):
     # svm.SVC(C=1.0, kernel='rbf', degree=3, gamma=0.0, coef0=0.0, shrinking=True, probability=False,tol=0.001, cache_size=200, class_weight=None, verbose=False, max_iter=-1, random_state=None)
     # various parameters like "kernal","gamma","C" can effectively tuned for a given machine learning model.
     SVC = svm.SVC()
@@ -33,20 +33,23 @@ def SVC(train_x, train_y, test_x):
     print("SVC score:{} %".format(score_SVC * 100))
 
 
-# loading the iris dataset
-iris = load_iris()
-# splitting the dataset to test and train
-train_x, test_x, train_y, test_y = train_test_split(
-    iris["data"], iris["target"], random_state=4
-)
-print("Target names: \n {} ".format(iris.target_names))
-print("\n Features: \n {}".format(iris.feature_names))
+def main():
+    # loading the iris dataset
+    iris = load_iris()
+    # splitting the dataset to test and train
+    train_x, test_x, train_y, test_y = train_test_split(
+        iris["data"], iris["target"], random_state=4
+    )
+    print("Target names: \n {} ".format(iris.target_names))
+    print("\n Features: \n {}".format(iris.feature_names))
 
-NuSVC(train_x, train_y, test_x)
-Linearsvc(train_x, train_y, test_x)
-SVC(train_x, train_y, test_x)
+    NuSVC(train_x, train_y, test_x, test_y)
+    Linearsvc(train_x, train_y, test_x, test_y)
+    SVC(train_x, train_y, test_x, test_y)
+
 
 if __name__ == "__main__":
     import doctest
-    
+
     doctest.testmod()
+    main()
