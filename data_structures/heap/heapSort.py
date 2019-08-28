8afba80185f6e3410e1dfd475fe45baf7cfeb315
Ujjal Das <ud12@iitbbs.ac.in>
2019-08-28 19:47:13 +0530

    max heap, min heap  , heap sort

---
+++ data_structures/heap/heapSort.py
@@ -0,0 +1,40 @@
+
+def swap(data,index1,index2):
+    temp=data[index1]
+    data[index1]=data[index2]
+    data[index2]=temp
+    return data
+def heapify(data,size,index):
+    left=(2*index)+1
+    right=(2*index)+2
+    largest=index
+
+    if left<size and data[left]>data[largest]:
+        largest=left
+    if right<size and data[right]>data[largest]:
+        largest=right
+
+    if largest!=index:
+        data=swap(data,index,largest)
+        data=heapify(data,size,largest)
+
+    return data
+
+def heapSort(data):
+    n=len(data)
+    leastParent=n
+    for i in range(leastParent,-1,-1):
+        data=heapify(data,n,i)
+
+    for i in range(n-1,0,-1):
+        if(data[0]>data[i]):
+            data=swap(data,0,i)
+            data=heapify(data,i,0)
+    return data
+
+
+from random import randint
+data=[randint(1,1000) for x in range(6)]
+# data=[13, 302, 602, 851, 351, 186]
+data=heapSort(data)
+print(data)
---
+++ data_structures/heap/minHEAPimplementationUSINGarray.py
@@ -0,0 +1,81 @@
+# min heap using array
+
+class maxHeap:
+    def __init__(self):
+        self.data=[]
+        self.length=0
+    def getLength(self):
+        return self.length
+    def leftChild(self,index):
+        if self.getLength()>=(2*index)+1:
+            return self.data[(2*index)+1]
+        else:
+            return None
+    def rightChild(self,index):
+        if self.getLength()>=(2*index)+2:
+            return self.data[(2*index)+2]
+        else:
+            return None
+    def getMin(self):
+        if self.getLength()==0:
+            return None
+        return self.data[0]
+
+    def swap(self,index1,index2):
+        temp=self.data[index1]
+        self.data[index1]=self.data[index2]
+        self.data[index2]=temp
+    def getMinChildIndex(self,index):
+        if 2*index<self.length:
+            if self.length>2*index+2:
+                if self.data[(2*index)+1]<self.data[(2*index)+2]:
+                    return (2*index)+1
+                return (2*index)+2
+        else:
+            return None
+
+
+    def heapify(self,index):
+        if index !=0 and self.data[index//2]>self.data[index]:
+            # this is for upward sorting
+             while index !=0 and self.data[index//2]>self.data[index]:
+                 self.swap(index,index//2)
+                 index=index//2
+        else:
+            check=self.getMinChildIndex(index)
+            while check!=None:
+                if self.data[check]<self.data[index]:
+                    self.swap(index,check)
+                    index=check
+                    check=self.getMinChildIndex(index)
+                else:
+                    break
+
+    def insert(self,value):
+        self.data.append(value)
+        self.length+=1
+        self.heapify(self.length-1)
+        print(self.length,">>>",self.data)
+
+    def delete(self):
+        temp=self.data[0]
+        self.swap(0,self.length-1)
+        self.data=self.data[:self.length-1]
+        self.length-=1
+        self.heapify(0)
+        # print(self.length,">>>",self.data)
+        return temp
+
+
+heap=maxHeap()
+from random import randint
+for i in range(20):
+    heap.insert(randint(1,1000))
+
+# for i in range(19):
+#     print(heap.delete())
+print("\n\n\n")
+for i in range(19):
+    print("min element>>",heap.delete())
+
+
