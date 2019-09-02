e62dbe23fba1bc3c31de8f0ed92f4a33e1caa7e6
jasper256 <jasperseabold@gmail.com>
2019-09-02 19:20:15 -0400

    formatted matrix_class.py with python/black

--- matrix/matrix_class.py
+++ matrix/matrix_class.py
@@ -1,7 +1,8 @@
 # An OOP aproach to representing and manipulating matrices
 
+
 class Matrix:
-    '''
+    """
     Matrix object generated from a 2D array where each element is an array representing a row.
     Rows can contain type int or float.
     Common operations and information available.
@@ -93,10 +94,13 @@ class Matrix:
     [306, 378, 450, 472]
     [414, 513, 612, 640]
 
-    '''
+    """
+
     def __init__(self, rows):
-        error = TypeError('Matrices must be formed from a list of zero or more lists containing at least one and the same number of values, \
-                each of which must be of type int or float')
+        error = TypeError(
+            "Matrices must be formed from a list of zero or more lists containing at least one and the same number of values, \
+                each of which must be of type int or float"
+        )
         if len(rows) != 0:
             cols = len(rows[0])
             if cols == 0:
@@ -110,29 +114,38 @@ class Matrix:
             self.rows = rows
         else:
             self.rows = []
-    
+
     # MATRIX INFORMATION
     @property
     def columns(self):
         return [[row[i] for row in self.rows] for i in range(len(self.rows[0]))]
+
     @property
     def num_rows(self):
         return len(self.rows)
+
     @property
     def num_columns(self):
         return len(self.columns)
+
     @property
     def order(self):
         return (self.num_rows, self.num_columns)
+
     @property
     def is_square(self):
         if self.order[0] == self.order[1]:
             return True
         return False
+
     @property
     def identity(self):
-        values = [[0 if column_num != row_num else 1 for column_num in range(self.num_rows)] for row_num in range(self.num_rows)]
+        values = [
+            [0 if column_num != row_num else 1 for column_num in range(self.num_rows)]
+            for row_num in range(self.num_rows)
+        ]
         return Matrix(values)
+
     @property
     def determinant(self):
         if not self.is_square:
@@ -142,100 +155,170 @@ class Matrix:
         if self.order == (1, 1):
             return self.rows[0][0]
         if self.order == (2, 2):
-            return (self.rows[0][0] * self.rows[1][1]) - (self.rows[0][1] * self.rows[1][0])
-        else: 
-            return sum([self.rows[0][column] * self.cofactors[0][column] for column in range(self.num_columns)])
+            return (self.rows[0][0] * self.rows[1][1]) - (
+                self.rows[0][1] * self.rows[1][0]
+            )
+        else:
+            return sum(
+                [
+                    self.rows[0][column] * self.cofactors[0][column]
+                    for column in range(self.num_columns)
+                ]
+            )
+
     @property
     def is_invertable(self):
         if self.determinant:
             return True
         return False
+
     def get_minor(self, row, column):
-        values = [[self.rows[other_row][other_column] for other_column in range(self.num_columns) if  other_column != column] 
-                for other_row in range(self.num_rows) if other_row != row]
+        values = [
+            [
+                self.rows[other_row][other_column]
+                for other_column in range(self.num_columns)
+                if other_column != column
+            ]
+            for other_row in range(self.num_rows)
+            if other_row != row
+        ]
         return Matrix(values).determinant
+
     def get_cofactor(self, row, column):
         if (row + column) % 2 == 0:
             return self.get_minor(row, column)
         return -1 * self.get_minor(row, column)
+
     @property
     def minors(self):
-        return Matrix([[self.get_minor(row, column) for column in range(self.num_columns)] for row in range(self.num_rows)])
+        return Matrix(
+            [
+                [self.get_minor(row, column) for column in range(self.num_columns)]
+                for row in range(self.num_rows)
+            ]
+        )
+
     @property
     def cofactors(self):
-        return [[self.minors.rows[row][column] if (row + column) % 2 == 0 else self.minors.rows[row][column] * -1 for column in range(self.minors.num_columns)] 
-                for row in range(self.minors.num_rows)]
+        return [
+            [
+                self.minors.rows[row][column]
+                if (row + column) % 2 == 0
+                else self.minors.rows[row][column] * -1
+                for column in range(self.minors.num_columns)
+            ]
+            for row in range(self.minors.num_rows)
+        ]
+
     @property
     def adjugate(self):
-        values = [[self.cofactors[column][row] for column in range(self.num_columns)] for row in range(self.num_rows)]
+        values = [
+            [self.cofactors[column][row] for column in range(self.num_columns)]
+            for row in range(self.num_rows)
+        ]
         return Matrix(values)
+
     @property
     def inverse(self):
         if not self.is_invertable:
             return None
         return self.adjugate * (1 / self.determinant)
+
     def __repr__(self):
         return str(self.rows)
+
     def __str__(self):
         if self.num_rows == 0:
             return str(self.rows)
         if self.num_rows == 1:
             return str(self.rows[0])
-        return '\n'.join([str(row) for row in self.rows])
+        return "\n".join([str(row) for row in self.rows])
 
     # MATRIX MANIPULATION
-    def add_row(self, row, position = None):
-        type_error = TypeError('Row must be a list containing all ints and/or floats')
+    def add_row(self, row, position=None):
+        type_error = TypeError("Row must be a list containing all ints and/or floats")
         if not isinstance(row, list):
             raise type_error
         for value in row:
             if not isinstance(value, (int, float)):
                 raise type_error
         if len(row) != self.num_columns:
-            raise ValueError('Row must be equal in length to the other rows in the matrix')
+            raise ValueError(
+                "Row must be equal in length to the other rows in the matrix"
+            )
         if position is None:
             self.rows.append(row)
         else:
             self.rows = self.rows[0:position] + [row] + self.rows[position:]
-    def add_column(self, column, position = None):
-        type_error = TypeError('Column must be a list containing all ints and/or floats')
+
+    def add_column(self, column, position=None):
+        type_error = TypeError(
+            "Column must be a list containing all ints and/or floats"
+        )
         if not isinstance(column, list):
             raise type_error
         for value in column:
             if not isinstance(value, (int, float)):
                 raise type_error
         if len(column) != self.num_rows:
-            raise ValueError('Column must be equal in length to the other columns in the matrix')
+            raise ValueError(
+                "Column must be equal in length to the other columns in the matrix"
+            )
         if position is None:
             self.rows = [self.rows[i] + [column[i]] for i in range(self.num_rows)]
         else:
-            self.rows = [self.rows[i][0:position] + [column[i]] + self.rows[i][position:] for i in range(self.num_rows)]
+            self.rows = [
+                self.rows[i][0:position] + [column[i]] + self.rows[i][position:]
+                for i in range(self.num_rows)
+            ]
 
     # MATRIX OPERATIONS
     def __add__(self, other):
         if self.order != other.order:
-            raise ValueError('Addition requires matrices of the same order')
-        return Matrix([[self.rows[i][j] + other.rows[i][j] for j in range(self.num_columns)] for i in range(self.num_rows)])
+            raise ValueError("Addition requires matrices of the same order")
+        return Matrix(
+            [
+                [self.rows[i][j] + other.rows[i][j] for j in range(self.num_columns)]
+                for i in range(self.num_rows)
+            ]
+        )
+
     def __sub__(self, other):
         if self.order != other.order:
-            raise ValueError('Subtraction requires matrices of the same order')
-        return Matrix([[self.rows[i][j] - other.rows[i][j] for j in range(self.num_columns)] for i in range(self.num_rows)])
+            raise ValueError("Subtraction requires matrices of the same order")
+        return Matrix(
+            [
+                [self.rows[i][j] - other.rows[i][j] for j in range(self.num_columns)]
+                for i in range(self.num_rows)
+            ]
+        )
+
     def __mul__(self, other):
         if not isinstance(other, (int, float, Matrix)):
-            raise TypeError('A matrix can only be multiplied by an int, float, or another matrix')
+            raise TypeError(
+                "A matrix can only be multiplied by an int, float, or another matrix"
+            )
         if type(other) in (int, float):
             return Matrix([[element * other for element in row] for row in self.rows])
         if type(other) is Matrix:
             if self.num_columns != other.num_rows:
-                raise ValueError('The number of columns in the first matrix must be equal to the number of rows in the second')
-            return Matrix([[Matrix.dot_product(row, column) for column in other.columns] for row in self.rows])
+                raise ValueError(
+                    "The number of columns in the first matrix must be equal to the number of rows in the second"
+                )
+            return Matrix(
+                [
+                    [Matrix.dot_product(row, column) for column in other.columns]
+                    for row in self.rows
+                ]
+            )
 
     @classmethod
     def dot_product(cls, row, column):
         return sum([row[i] * column[i] for i in range(len(row))])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import doctest
+
     test = doctest.testmod()
     print(test)
