f97cc0037da26aefccb5af11a795cdcb333bcd4f
anirudnits <aniruddha97bhatt@gmail.com>
2019-09-25 15:53:10 +0530

    Implemented the requested changes

--- maths/matrix_exponentiation.py
+++ maths/matrix_exponentiation.py
@@ -11,124 +11,122 @@ https://www.hackerearth.com/practice/notes/matrix-exponentiation-1/
 
 
 class Matrix(object):
-	def __init__(self, arg):
-		if isinstance(arg,list): #Initialzes a matrix identical to the one provided.
-			self.t = arg
-			self.n = len(arg)
+    def __init__(self, arg):
+        if isinstance(arg, list):  # Initialzes a matrix identical to the one provided.
+            self.t = arg
+            self.n = len(arg)
 
-		else: #Initializes a square matrix of the given size and set the values to zero.
-			self.n = arg
-			self.t = [[0 for _ in range(self.n)] for _ in range(self.n)]
+        else:  # Initializes a square matrix of the given size and set the values to zero.
+            self.n = arg
+            self.t = [[0 for _ in range(self.n)] for _ in range(self.n)]
 
-	def __mul__(self, b):
-		c = Matrix(self.n)
+    def __mul__(self, b):
+        c = Matrix(self.n)
 
-		for i in range(self.n):
-			for j in range(self.n):
-				for k in range(self.n):
-					c.t[i][j] += self.t[i][k]*b.t[k][j]
+        for i in range(self.n):
+            for j in range(self.n):
+                for k in range(self.n):
+                    c.t[i][j] += self.t[i][k] * b.t[k][j]
 
-		return c
+        return c
+
 
 def modular_exponentiation(a, b):
-	ret = Matrix([[1,0], [0,1]])
+    ret = Matrix([[1, 0], [0, 1]])
 
-	while b > 0:
-		if b&1:
-			ret = ret*a
+    while b > 0:
+        if b & 1:
+            ret = ret * a
 
-		a = a*a
-		b >>= 1
+        a = a * a
+        b >>= 1
 
-	return ret
+    return ret
+
 
 def fibonacci_with_matrix_exponentiation(n, f1, f2):
-	
-	#Trivial Cases
-	if n == 1:
-		return f1
 
-	if n == 2:
-		return f2
+    # Trivial Cases
+    if n == 1:
+        return f1
 
-	a = Matrix([[1,1], [1,0]])
+    if n == 2:
+        return f2
 
-	m = modular_exponentiation(a, n-2)
+    a = Matrix([[1, 1], [1, 0]])
 
-	return f2*m.t[0][0] + f1*m.t[0][1]
+    m = modular_exponentiation(a, n - 2)
+
+    return f2 * m.t[0][0] + f1 * m.t[0][1]
+
 
 def simple_fibonacci(n, f1, f2):
 
-	#Trival Cases
-	if n == 1:
-		return f1
+    # Trival Cases
+    if n == 1:
+        return f1
 
-	if n == 2:
-		return f2
+    if n == 2:
+        return f2
 
-	fn_1 = f1
-	fn_2 = f2
+    fn_1 = f1
+    fn_2 = f2
 
-	n -= 2
+    n -= 2
 
-	while n > 0:
-		fn = fn_1 + fn_2
+    while n > 0:
+        fn = fn_1 + fn_2
 
-		fn_2 = fn_1
-		fn_1 = fn
+        fn_2 = fn_1
+        fn_1 = fn
 
-		n -= 1
+        n -= 1
 
-	return fn
+    return fn
+
 
 def matrix_exponentiation_time():
-	setup = '''
+    setup = """
 from random import randint
 from __main__ import fibonacci_with_matrix_exponentiation
-'''
+"""
 
-	code = '''
-n = randint(1,70000)
-
+    code = """
 f1 = 1
 f2 = 1
 
 
-fibonacci_with_matrix_exponentiation(n, f1, f2)
-'''
-	exec_time = timeit.timeit(setup = setup,
-						  stmt = code,
-						  number = 100)
+fibonacci_with_matrix_exponentiation(randint(1,70000), f1, f2)
+"""
+    exec_time = timeit.timeit(setup=setup, stmt=code, number=100)
 
-	print ("With matrix exponentiation the average execution time is ", exec_time/100)
+    print("With matrix exponentiation the average execution time is ", exec_time / 100)
 
 
 def simple_fibonacci_time():
-	setup = '''
+    setup = """
 from random import randint
 from __main__ import simple_fibonacci
-'''
+"""
 
-	code = '''
-n = randint(1,70000)
-
+    code = """
 f1 = 1
 f2 = 1
 
 
-simple_fibonacci(n, f1, f2)
-'''
-	exec_time = timeit.timeit(setup = setup,
-						  stmt = code,
-						  number = 100)
+simple_fibonacci(randint(1,70000), f1, f2)
+"""
+    exec_time = timeit.timeit(setup=setup, stmt=code, number=100)
 
-	print ("Without matrix exponentiation the average execution time is ", exec_time/100)
+    print(
+        "Without matrix exponentiation the average execution time is ", exec_time / 100
+    )
 
-	
+
 def main():
-	matrix_exponentiation_time()
-	simple_fibonacci_time()
+    matrix_exponentiation_time()
+    simple_fibonacci_time()
 
 
 if __name__ == "__main__":
-	main()
+    main()
