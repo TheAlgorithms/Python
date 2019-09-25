21820acd1d473c7709a640e0fc323ca9e92d611a
Charitoc <37042130+Charitoc@users.noreply.github.com>
2019-09-25 15:23:53 +0300

    Adding stooge sort

---
+++ sorts/stooge_sort.py
@@ -0,0 +1,37 @@
+def stoogesort(arr):
+    stooge(arr,0,len(arr)-1)
+
+def stooge(arr, i, h):
+
+    """
+
+>>> arr = [2, 4, 5, 3, 1]
+>>> stoogesort(arr)
+>>> print(arr)
+[1, 2, 3, 4, 5]
+
+"""
+
+
+ 
+    if i >= h:
+        return
+  
+    # If first element is smaller than the last then swap them
+    if arr[i]>arr[h]:
+        arr[i], arr[h] = arr[h], arr[i]
+  
+    # If there are more than 2 elements in the array
+    if h-i+1 > 2: 
+        t = (int)((h-i+1)/3)
+  
+        # Recursively sort first 2/3 elements
+        stooge(arr, i, (h-t))
+  
+        # Recursively sort last 2/3 elements
+        stooge(arr, i+t, (h))
+  
+        # Recursively sort first 2/3 elements
+        stooge(arr, i, (h-t))
+
+        
