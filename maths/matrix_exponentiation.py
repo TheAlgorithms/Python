8e64f021cc0ad35494bb130f77ec7a46d24f65b0
Christian Clauss <cclauss@me.com>
2019-09-25 20:08:29 +0200

    Update matrix_exponentiation.py

--- maths/matrix_exponentiation.py
+++ maths/matrix_exponentiation.py
@@ -15,71 +15,53 @@ class Matrix(object):
         if isinstance(arg, list):  # Initialzes a matrix identical to the one provided.
             self.t = arg
             self.n = len(arg)
-
         else:  # Initializes a square matrix of the given size and set the values to zero.
             self.n = arg
             self.t = [[0 for _ in range(self.n)] for _ in range(self.n)]
 
     def __mul__(self, b):
-        c = Matrix(self.n)
-
+        matrix = Matrix(self.n)
         for i in range(self.n):
             for j in range(self.n):
                 for k in range(self.n):
-                    c.t[i][j] += self.t[i][k] * b.t[k][j]
-
-        return c
+                    matrix.t[i][j] += self.t[i][k] * b.t[k][j]
+        return matrix
 
 
 def modular_exponentiation(a, b):
-    ret = Matrix([[1, 0], [0, 1]])
-
+    matrix = Matrix([[1, 0], [0, 1]])
     while b > 0:
         if b & 1:
-            ret = ret * a
-
-        a = a * a
+            matrix *= a
+        a *= a
         b >>= 1
-
-    return ret
+    return matrix
 
 
 def fibonacci_with_matrix_exponentiation(n, f1, f2):
-
     # Trivial Cases
     if n == 1:
         return f1
-
-    if n == 2:
+    elif n == 2:
         return f2
-
-    a = Matrix([[1, 1], [1, 0]])
-
-    m = modular_exponentiation(a, n - 2)
-
-    return f2 * m.t[0][0] + f1 * m.t[0][1]
+    matrix = Matrix([[1, 1], [1, 0]])
+    matrix = modular_exponentiation(matrix, n - 2)
+    return f2 * matrix.t[0][0] + f1 * matrix.t[0][1]
 
 
 def simple_fibonacci(n, f1, f2):
-
     # Trival Cases
     if n == 1:
         return f1
-
-    if n == 2:
+    elif n == 2:
         return f2
 
     fn_1 = f1
     fn_2 = f2
-
     n -= 2
 
     while n > 0:
-        fn = fn_1 + fn_2
-
-        fn_2 = fn_1
-        fn_1 = fn
-
+        fn_1, fn_2 = fn_1 + fn_2, fn_1
         n -= 1
 
     return fn
@@ -90,17 +72,10 @@ def matrix_exponentiation_time():
 from random import randint
 from __main__ import fibonacci_with_matrix_exponentiation
 """
-
-    code = """
-f1 = 1
-f2 = 1
-
-
-fibonacci_with_matrix_exponentiation(randint(1,70000), f1, f2)
-"""
+    code = "fibonacci_with_matrix_exponentiation(randint(1,70000), 1, 1)"
     exec_time = timeit.timeit(setup=setup, stmt=code, number=100)
-
     print("With matrix exponentiation the average execution time is ", exec_time / 100)
+    return exec_time
 
 
 def simple_fibonacci_time():
@@ -108,19 +83,11 @@ def simple_fibonacci_time():
 from random import randint
 from __main__ import simple_fibonacci
 """
-
-    code = """
-f1 = 1
-f2 = 1
-
-
-simple_fibonacci(randint(1,70000), f1, f2)
-"""
+    code = "simple_fibonacci(randint(1,70000), 1, 1)"
     exec_time = timeit.timeit(setup=setup, stmt=code, number=100)
-
-    print(
-        "Without matrix exponentiation the average execution time is ", exec_time / 100
-    )
+    print("Without matrix exponentiation the average execution time is ",
+          exec_time / 100)
+    return exec_time
 
 
 def main():
