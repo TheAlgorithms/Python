67cfb523b7f4a417665986d01d658e62ee33c605
Raj <raj1998211@gmail.com>
2019-09-25 00:44:03 -0700

    doctest added

--- data_structures/heap/min_heap.py
+++ data_structures/heap/min_heap.py
@@ -2,36 +2,58 @@
 # with decrease key functionality - in O(log(n)) time
 
 
+class Node:
+    def __init__(self, val, name):
+        self.val = val
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+    def __lt__(self, other):
+        return self.val < other.val
+
+
 class MinHeap:
+    """
+    >>> r = Node(-1, "R")
+    >>> b = Node(6, "B")
+    >>> a = Node(3, "A")
+    >>> x = Node(1, "X")
+    >>> e = Node(4, "E")
+    >>> myMinHeap = MinHeap([r, b, a, x, e])
+    >>> myMinHeap.decrease_key(b, -17)
+    """
+
     def __init__(self, array):
         self.idx_of_element = {}
-        self.heap = self.buildHeap(array)
+        self.heap = self.build_heap(array)
 
-    def getParentIdx(self, idx):
+    def get_parent_idx(self, idx):
         return (idx - 1) // 2
 
-    def getLeftChildIdx(self, idx):
+    def get_left_child_idx(self, idx):
         return idx * 2 + 1
 
-    def getRightChildIdx(self, idx):
+    def get_right_child_idx(self, idx):
         return idx * 2 + 2
 
-    def buildHeap(self, array):
+    def build_heap(self, array):
         lastIdx = len(array) - 1
-        startFrom = self.getParentIdx(lastIdx)
+        startFrom = self.get_parent_idx(lastIdx)
 
         for idx, i in enumerate(array):
             self.idx_of_element[i] = idx
 
         for i in range(startFrom, -1, -1):
-            self.siftDown(i, array)
+            self.sift_down(i, array)
         return array
 
     # this is min-heapify method
-    def siftDown(self, idx, array):
+    def sift_down(self, idx, array):
         while True:
-            l = self.getLeftChildIdx(idx)
-            r = self.getRightChildIdx(idx)
+            l = self.get_left_child_idx(idx)
+            r = self.get_right_child_idx(idx)
 
             smallest = idx
             if l < len(array) and array[l] < array[idx]:
@@ -51,8 +73,8 @@ class MinHeap:
             else:
                 break
 
-    def siftUp(self, idx):
-        p = self.getParentIdx(idx)
+    def sift_up(self, idx):
+        p = self.get_parent_idx(idx)
         while p >= 0 and self.heap[p] > self.heap[idx]:
             self.heap[p], self.heap[idx] = self.heap[idx], self.heap[p]
             self.idx_of_element[self.heap[p]], self.idx_of_element[self.heap[idx]] = (
@@ -60,7 +82,7 @@ class MinHeap:
                 self.idx_of_element[self.heap[p]],
             )
             idx = p
-            p = self.getParentIdx(idx)
+            p = self.get_parent_idx(idx)
 
     def peek(self):
         return self.heap[0]
@@ -74,35 +96,23 @@ class MinHeap:
 
         x = self.heap.pop()
         del self.idx_of_element[x]
-        self.siftDown(0, self.heap)
+        self.sift_down(0, self.heap)
         return x
 
     def insert(self, value):
         self.heap.append(value)
         self.idx_of_element[value] = len(self.heap) - 1
-        self.siftUp(len(self.heap) - 1)
+        self.sift_up(len(self.heap) - 1)
 
-    def isEmpty(self):
+    def is_empty(self):
         return True if len(self.heap) == 0 else False
 
-    def decreaseKey(self, key, newValue):
+    def decrease_key(self, key, newValue):
         assert (
             self.heap[self.idx_of_element[key]].val > newValue
         ), "newValue must be less that current value"
         key.val = newValue
-        self.siftUp(self.idx_of_element[key])
-
-
-class Node:
-    def __init__(self, val, name):
-        self.val = val
-        self.name = name
-
-    def __str__(self):
-        return self.name
-
-    def __lt__(self, other):
-        return self.val < other.val
+        self.sift_up(self.idx_of_element[key])
 
 
 ## USAGE
@@ -113,12 +123,10 @@ a = Node(3, "A")
 x = Node(1, "X")
 e = Node(4, "E")
 
-arr = [r, b, a, x, e]
-
 # Use one of these two ways to generate Min-Heap
 
 # Generating Min-Heap from array
-myMinHeap = MinHeap(arr)
+myMinHeap = MinHeap([r, b, a, x, e])
 
 # Generating Min-Heap by Insert method
 # myMinHeap.insert(a)
@@ -133,8 +141,14 @@ for i in myMinHeap.heap:
     print(i, i.val)
 
 print("Min Heap - After decrease key of node [B -> -17]")
-myMinHeap.decreaseKey(b, -17)
+myMinHeap.decrease_key(b, -17)
 
 # After
 for i in myMinHeap.heap:
     print(i, i.val)
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
