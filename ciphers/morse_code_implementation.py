a61d624ada46c8c97d51938c711c6824ae8727d7
Jonathan Gómez <jonicgp97@gmail.com>
2019-10-01 07:58:10 -0500

    Update morse_code_implementation.py
    Delete blank spaces to make the code more legible

--- ciphers/morse_code_implementation.py
+++ ciphers/morse_code_implementation.py
@@ -23,48 +23,28 @@ def encrypt(message):
     cipher = ''
     for letter in message:
         if letter != ' ':
-
-
             cipher += MORSE_CODE_DICT[letter] + ' '
         else:
-
             cipher += ' '
-
     return cipher
 
 
 def decrypt(message):
-
     message += ' '
-
     decipher = ''
     citext = ''
     for letter in message:
-
         if (letter != ' '):
-
-
             i = 0
-
-
             citext += letter
-
         else:
-
             i += 1
-
-
             if i == 2 :
-
-
                 decipher += ' '
             else:
-
-
                 decipher += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT
                 .values()).index(citext)]
                 citext = ''
-
     return decipher
 
 
@@ -72,7 +52,7 @@ def main():
     message = "Morse code here"
     result = encrypt(message.upper())
     print(result)
-
+    
     message = result
     result = decrypt(message)
     print(result)
