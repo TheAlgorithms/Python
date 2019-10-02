445f3591e1af30b45b79c2d93a5eb8e76e78cac5
DanDevX <DanDevX@users.noreply.github.com>
2019-10-02 18:43:32 +0100

    Modified caesar_cipher.py to work correctly

--- ciphers/caesar_cipher.py
+++ ciphers/caesar_cipher.py
@@ -1,65 +1,86 @@
 def encrypt(strng, key):
     encrypted = ''
     for x in strng:
-        indx = (ord(x) + key) % 256
-        if indx > 126:
-            indx = indx - 95
-        encrypted = encrypted + chr(indx)
+        if x.isalpha():
+            if x.isupper():
+                x = ord(x) - 65
+                x = (x + key) % 26 + 65
+                encrypted += chr(x)
+            else:
+                x = ord(x) - 97
+                x = (x + key) % 26 + 97
+                encrypted += chr(x)
+        else:
+            encrypted += x
     return encrypted
-
 
 def decrypt(strng, key):
     decrypted = ''
     for x in strng:
-        indx = (ord(x) - key) % 256
-        if indx < 32:
-            indx = indx + 95
-        decrypted = decrypted + chr(indx)
+        if x.isalpha():
+            if x.isupper():
+                x = ord(x) - 65
+                x = (x - key) % 26 + 65
+                decrypted += chr(x)
+            else:
+                x = ord(x) - 97
+                x = (x - key) % 26 + 97
+                decrypted += chr(x)
+        else:
+            decrypted += x
     return decrypted
 
 def brute_force(strng):
-    key = 1
+    key = 0
     decrypted = ''
-    while key <= 94:
+    while key < 26:
         for x in strng:
-            indx = (ord(x) - key) % 256
-            if indx < 32:
-                indx = indx + 95
-            decrypted = decrypted + chr(indx)
+            if x.isalpha():
+                if x.isupper():
+                    x = ord(x) - 65
+                    x = (x - key) % 26 + 65
+                    decrypted += chr(x)
+                else:
+                    x = ord(x) - 97
+                    x = (x - key) % 26 + 97
+                    decrypted += chr(x)
+            else:
+                decrypted += x
         print("Key: {}\t| Message: {}".format(key, decrypted))
         decrypted = ''
         key += 1
     return None
 
-
 def main():
     while True:
         print('-' * 10 + "\n**Menu**\n" + '-' * 10)
-        print("1.Encrpyt")
+        print("1.Encrypt")
         print("2.Decrypt")
         print("3.BruteForce")
         print("4.Quit")
         choice = input("What would you like to do?: ")
         if choice not in ['1', '2', '3', '4']:
-            print("Invalid choice, please enter a valid choice")
+            print ("Invalid choice")
         elif choice == '1':
             strng = input("Please enter the string to be encrypted: ")
-            key = int(input("Please enter off-set between 1-94: "))
-            if key in range(1, 95):
-                print(encrypt(strng.lower(), key))
+            key = int(input("Please enter off-set between 0-25: "))
+            if key in range(0, 26):
+                print (encrypt(strng, key))
+            else:
+                print("Invalid off-set")
         elif choice == '2':
             strng = input("Please enter the string to be decrypted: ")
-            key = int(input("Please enter off-set between 1-94: "))
-            if key in range(1,95):
+            key = int(input("Please enter off-set between 0-25: "))
+            if key in range(0, 26):
                 print(decrypt(strng, key))
+            else:
+                print("Invalid off-set")
         elif choice == '3':
             strng = input("Please enter the string to be decrypted: ")
             brute_force(strng)
-            main()
         elif choice == '4':
             print("Goodbye.")
             break
-
 
 if __name__ == '__main__':
     main()
