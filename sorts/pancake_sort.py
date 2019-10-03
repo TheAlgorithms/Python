7f0fc29c7a7e241528d58c2be261683d169faf5b
nityaprakash10 <43900148+nityaprakash10@users.noreply.github.com>
2019-10-03 18:58:27 +0530

    Update pancake_sort.py

--- sorts/pancake_sort.py
+++ sorts/pancake_sort.py
@@ -1,38 +1,22 @@
-"""
-This is a pure python implementation of the pancake sort algorithm
-For doctests run following command:
-python3 -m doctest -v pancake_sort.py
-or
-python -m doctest -v pancake_sort.py
-For manual testing run:
-python pancake_sort.py
-"""
+from random import randint
 
-def pancake_sort(arr):
-    """Sort Array with Pancake Sort.
-    :param arr: Collection containing comparable items
-    :return: Collection ordered in ascending order of items
-    Examples:
-    >>> pancake_sort([0, 5, 3, 2, 2])
-    [0, 2, 2, 3, 5]
-    >>> pancake_sort([])
-    []
-    >>> pancake_sort([-2, -5, -45])
-    [-45, -5, -2]
-    """
-    cur = len(arr)
-    while cur > 1:
-        # Find the maximum number in arr
-        mi = arr.index(max(arr[0:cur]))
-        # Reverse from 0 to mi
-        arr = arr[mi::-1] + arr[mi + 1:len(arr)]
-        # Reverse whole list
-        arr = arr[cur - 1::-1] + arr[cur:len(arr)]
-        cur -= 1
-    return arr
+def pancake_sort(cakes):
+  if not cakes or len(cakes) == 1:
+        return cakes
+  cake_index = 0
+  while cake_index < len(cakes)-1:
+    prev_cake_size, current_cake_size = cakes[cake_index], cakes[cake_index+1]
+    if prev_cake_size > current_cake_size:
+      '''move the highest element to the beginning by reversing
+      the cakes from 0 to highest element(including) and then move
+      the highest element to the end by reversing the subarray'''
+      cakes[0:cake_index+2] = (cakes[0:cake_index+1][::-1] + [cakes[cake_index+1]])[::-1]
+      cake_index = 0
+      continue
+    cake_index += 1
+  return cakes
 
-
-if __name__ == '__main__':
-    user_input = input('Enter numbers separated by a comma:\n').strip()
-    unsorted = [int(item) for item in user_input.split(',')]
-    print(pancake_sort(unsorted))
+for i in range(randint(0, 5)):
+  data = [randint(0, 100) for _ in range(randint(0, 40))]
+  if pancake_sort(data[:]) != sorted(data[:]):
+    print("failed for", data[:])
