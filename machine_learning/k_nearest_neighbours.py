42b37adac88d0814b6823797540f32deff95a4ae
Parth Paradkar <parthparadkar3@gmail.com>
2019-10-05 19:49:59 +0530

    doctest added

--- machine_learning/k_nearest_neighbours.py
+++ machine_learning/k_nearest_neighbours.py
@@ -5,8 +5,6 @@ from sklearn.model_selection import train_test_split
 
 data = datasets.load_iris()
 
-# print(data)
-
 X = np.array(data['data'])
 y = np.array(data['target'])
 classes = data['target_names']
@@ -27,6 +25,17 @@ def classifier(train_data, train_target, classes, point, k=5):
     """
     Classifies the point using the KNN algorithm
     k closest points are found (ranked in ascending order of euclidean distance)
+    Params:
+    :train_data: Set of points that are classified into two or more classes
+    :train_target: List of classes in the order of train_data points
+    :classes: Labels of the classes
+    :point: The data point that needs to be classifed
+
+    >>> X_train = [[0, 0], [1, 0], [0, 1], [0.5, 0.5], [3, 3], [2, 3], [3, 2]]
+    >>> y_train = [0, 0, 0, 0, 1, 1, 1]
+    >>> classes = ['A','B']; point = [1.2,1.2]
+    >>> classifier(X_train, y_train, classes,point)
+    'A'
     """
     data = zip(train_data, train_target)
     # List of distances of all points from the point to be classified
@@ -36,10 +45,11 @@ def classifier(train_data, train_target, classes, point, k=5):
         distances.append((distance, data_point[1]))
     # Choosing 'k' points with the least distances. 
     votes = [i[1] for i in sorted(distances)[:k]]
-    # Most common class occuring among them is chosen to be the class into which the point is classified
+    # Most commonly occuring class among them 
+    # is the class into which the point is classified
     result = Counter(votes).most_common(1)[0][0]
     return classes[result]
 
 
 if __name__ == "__main__":
-    print(classifier([[0, 0], [1, 0], [0, 1], [0.5, 0.5], [3, 3], [2, 3], [3, 2]], [0, 0, 0, 0, 1, 1, 1], ['A', 'B'], [1.2, 1.2]))
+    print(classifier(X_train, y_train, classes, [4.4, 3.1, 1.3, 1.4]))
