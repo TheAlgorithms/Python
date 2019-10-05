0ae6ccd9a8dc9932cc438017ce510ad118e1097a
Parth Paradkar <parthparadkar3@gmail.com>
2019-10-05 18:42:54 +0530

    Comments and test case added

--- machine_learning/k_nearest_neighbours.py
+++ machine_learning/k_nearest_neighbours.py
@@ -29,13 +29,17 @@ def classifier(train_data, train_target, classes, point, k=5):
     k closest points are found (ranked in ascending order of euclidean distance)
     """
     data = zip(train_data, train_target)
+    # List of distances of all points from the point to be classified
     distances = []
     for data_point in data:
         distance = euclidean_distance(data_point[0], point)
         distances.append((distance, data_point[1]))
+    # Choosing 'k' points with the least distances. 
     votes = [i[1] for i in sorted(distances)[:k]]
+    # Most common class occuring among them is chosen to be the class into which the point is classified
     result = Counter(votes).most_common(1)[0][0]
     return classes[result]
 
+
 if __name__ == "__main__":
-    print(classifier(X_train, y_train, classes, [4.4, 3.1, 1.3, 1.4]))
+    print(classifier([[0, 0], [1, 0], [0, 1], [0.5, 0.5], [3, 3], [2, 3], [3, 2]], [0, 0, 0, 0, 1, 1, 1], ['A', 'B'], [1.2, 1.2]))
