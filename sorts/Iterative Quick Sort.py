e9893dd57f360fe05187ff0f9ffad0288e3d1eed
Pranjal1751 <pmishra@mt.iitr.ac.in>
2019-10-06 13:15:48 +0530

    Add comments

--- sorts/Iterative Quick Sort.py
+++ sorts/Iterative Quick Sort.py
@@ -4,8 +4,7 @@
 # places the pivot element at its correct 
 # position in sorted array, and places all 
 # smaller (smaller than pivot) to left of 
-# pivot and all greater elements to right 
-# of pivot 
+# pivot and all greater elements to right of pivot
 def partition(arr, low, high): 
 	i = (low - 1)		 # index of smaller element 
 	pivot = arr[high]	 # pivot 
