d9c51cdec734de6223b06cbd7b75004760881e4f
Fazeel Usmani <fazeelusmani18@gmail.com>
2019-10-06 15:02:04 +0530

    Create reverse_linked_list.py

---
+++ data_structures/linked_list/reverse_linked_list.py
@@ -0,0 +1,32 @@
+# Node class for node creation
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+# A function to print a Linked List using recursion
+def print_linkedlist_spl(head):
+
+  if head is None:
+    return None
+   
+  return print_linkedlist_spl(head.next), print(head.data, end = " ")    
+    
+# Converting an input array to a linked list
+def ll(arr):
+    if len(arr)==0:
+        return None
+    head = Node(arr[0])
+    last = head
+    for data in arr[1:]:
+        last.next = Node(data)
+        last = last.next
+    return head
+
+
+from sys import setrecursionlimit
+setrecursionlimit(10000)            # setting recursion limit as 10**4 (at pax)
+
+arr=list(int(i) for i in input().strip().split(' '))          # reading input array
+l = ll(arr[:-1])                                              # passing input array to linked list creation 
+print_linkedlist_spl(l)
