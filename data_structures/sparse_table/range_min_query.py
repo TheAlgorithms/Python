301b613c56b58bbb82cab8f3242138f27220690a
Mayank Chaudhary <49724597+chaudhary19@users.noreply.github.com>
2019-10-06 12:11:44 +0530

    create sparse table
    created range minimum query 1

---
+++ data_structures/sparse_table/range_min_query.py
@@ -0,0 +1,35 @@
+from math import log
+
+class SparseTable:
+
+    def __init__(self, arr):  # create Sparse Table using given Array
+        self.arr = arr
+        self.size = len(self.arr)     # length of the Array
+        self.stable = [[0 for j in range(int(log(self.size, 2)) + 1)] for i in range(self.size)]
+
+        for i in range(self.size):     # initialize first column with corresponding row number
+            self.stable[i][0] = i
+
+        j = 1
+        while (1 << j) <= self.size:   # Build the Sparse table
+            i = 0
+            while (i + (1 << j) - 1) < self.size:
+                if arr[self.stable[i][j - 1]] < arr[self.stable[i + (1 << (j - 1))][j - 1]]:
+                    self.stable[i][j] = self.stable[i][j - 1]
+                else:
+                    self.stable[i][j] = self.stable[i + (1 << (j - 1))][j - 1]
+                i += 1
+            j += 1
+
+    def min_query(self,l ,r):   # Range minimum Query  :  [l,r)
+        length = r-l+1
+        k = int(log(length, 2))
+        ans = min(self.arr[self.stable[l][k]], self.arr[self.stable[l + length - (1 << k)][k]])
+        return ans              # return the minimum number in the range
+
+if __name__ == '__main__':
+
+    arr = [1,2,3,4,5,6,7,8,9]
+    s_obj = SparseTable(arr)
+    print(s_obj.min_query(2,5))
+    print(s_obj.min_query(1,8))
