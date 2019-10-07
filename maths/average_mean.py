2ddaec34843e4eea1772e247d3d89f6353e32dbf
Maram Sumanth <maram.sumanth@gmail.com>
2019-10-07 17:40:29 +0530

    Update average_mean.py

--- maths/average_mean.py
+++ maths/average_mean.py
@@ -3,17 +3,13 @@
 
 def average(nums):
     """Find mean of a list of numbers."""
-    sum = 0
-    for x in nums:
-        sum += x
-    avg = sum / len(nums)
-    print(avg)
+    avg = sum(nums) / len(nums)
     return avg
 
 
 def main():
     """Call average module to find mean of a specific list of numbers."""
-    average([2, 4, 6, 8, 20, 50, 70])
+    print(average([2, 4, 6, 8, 20, 50, 70]))
 
 
 if __name__ == "__main__":
