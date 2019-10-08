458aa9b860d035fd36d1d540b5075140a3221353
Rishabh Kumar <rishabh0098@gmail.com>
2019-10-08 16:22:31 +0530

    Update karatsuba.py
    Added doctests and divmod

--- maths/karatsuba.py
+++ maths/karatsuba.py
@@ -1,16 +1,19 @@
 """ Multiply two numbers using Karatsuba algorithm """
 
 def karatsuba(a, b):
+    """
+    >>> karatsuba(15463, 23489) == 15463 * 23489
+    True
+    >>> karatsuba(3, 9) == 3 * 9
+    """
     if len(str(a)) == 1 or len(str(b)) == 1:
         return (a * b)
     else:
         m1 = max(len(str(a)), len(str(b)))
         m2 = m1 // 2
 
-        a1 = a // 10**(m2)
-        a2 = a % 10**(m2)
-        b1 = b // 10**(m2)
-        b2 = b % 10**(m2)
+        a1, a2 = divmod(a, 10**m2)
+        b1, b2 = divmod(b, 10**m2)
 
         x = karatsuba(a2, b2)
         y = karatsuba((a1 + a2), (b1 + b2))
