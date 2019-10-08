7964b41afe652f1844b31b4d16519d079c12e31c
higgsboson1209 <45105988+higgsboson1209@users.noreply.github.com>
2019-10-09 03:05:16 +0530

    Update rsa_key_generator.py

--- ciphers/rsa_key_generator.py
+++ ciphers/rsa_key_generator.py
@@ -8,27 +8,69 @@ def main():
     print("Key files generation successful.")
 
 
-def generateKey(keySize):
-    print("Generating prime p...")
-    p = rabinMiller.generateLargePrime(keySize)
-    print("Generating prime q...")
-    q = rabinMiller.generateLargePrime(keySize)
-    n = p * q
+    
+from random import randrange, getrandbits
+def is_prime(n, k=128):
+    """ Test if a number is prime
+        Args:
+            n -- int -- the number to test
+            k -- int -- the number of tests to do
+        return True if n is prime
+    """
+    # Test if n is not even.
+    # But care, 2 is prime !
+    if n == 2 or n == 3:
+        return True
+    if n <= 1 or n % 2 == 0:
+        return False
+    # find r and s
+    s = 0
+    r = n - 1
+    while r & 1 == 0:
+        s += 1
+        r //= 2
+    # do k tests
+    for _ in range(k):
+        a = randrange(2, n - 1)
+        x = pow(a, r, n)
+        if x != 1 and x != n - 1:
+            j = 1
+            while j < s and x != n - 1:
+                x = pow(x, 2, n)
+                if x == 1:
+                    return False
+                j += 1
+            if x != n - 1:
+                return False
+    return True
+def generate_prime_candidate(length):
+    """ Generate an odd integer randomly
+        Args:
+            length -- int -- the length of the number to generate, in bits
+        return a integer
+    """
+    # generate random bits
+    p = getrandbits(length)
+    # apply a mask to set MSB and LSB to 1
+    p |= (1 << length - 1) | 1
+    return p
+def generate_prime_number(length=1024):
+    """ Generate a prime
+        Args:
+            length -- int -- length of the prime to generate, in          bits
+        return a prime
+    """
+    p = 4
+    # keep generating while the primality test fail
+    while not is_prime(p, 128):
+        p = generate_prime_candidate(length)
+    return p
 
-    print("Generating e that is relatively prime to (p - 1) * (q - 1)...")
-    while True:
-        e = random.randrange(2 ** (keySize - 1), 2 ** (keySize))
-        if cryptoMath.gcd(e, (p - 1) * (q - 1)) == 1:
-            break
+p = generate_prime_number()
 
-    print("Calculating d that is mod inverse of e...")
-    d = cryptoMath.findModInverse(e, (p - 1) * (q - 1))
+q = generate_prime_number()
 
-    publicKey = (n, e)
-    privateKey = (n, d)
-    return (publicKey, privateKey)
-
-
+"""From Scratch implementation of Generating large prime_numbers"""
 def makeKeyFiles(name, keySize):
     if os.path.exists("%s_pubkey.txt" % (name)) or os.path.exists(
         "%s_privkey.txt" % (name)
