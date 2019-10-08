2ab692d55c0a7117bfca1aa7cef31e4f1b3caf2c
archit kaushal <kaushalarchit5@gmail.com>
2019-10-09 01:49:58 +0530

    solves #1317

---
+++ networking_flow/socket1.py
@@ -0,0 +1,14 @@
+import socket
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(),end='')
+
+mysock.close()
