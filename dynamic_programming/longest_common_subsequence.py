6ef5dd4ea379a45e166fb6258dd4160408645e39
jpg-130 <jigsgandhi97@gmail.com>
2019-10-09 12:19:47 +0530

    Modified longest_common_ssubsequence.py for successful doctests

--- dynamic_programming/longest_common_subsequence.py
+++ dynamic_programming/longest_common_subsequence.py
@@ -76,6 +76,7 @@ if __name__ == "__main__":
     expected_subseq = "GTAB"
 
     ln, subseq = longest_common_subsequence(a, b)
-    assert expected_ln == ln
-    assert expected_subseq == subseq
-    print("len =", ln, ", sub-sequence =", subseq)
+##    print("len =", ln, ", sub-sequence =", subseq)
+    import doctest
+
+    doctest.testmod()
