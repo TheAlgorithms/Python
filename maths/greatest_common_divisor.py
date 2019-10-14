b190c8f629d1fb8371fb9ea434d986a843b2ecab
Aliabbas Merchant <merchantaliabbas@gmail.com>
2019-10-15 00:05:51 +0530

    Rename GCD File (#1354)

--- maths/greatest_common_divisor.py
+++ maths/greatest_common_divisor.py
@@ -1,12 +1,12 @@
 """
-Greater Common Divisor.
+Greatest Common Divisor.
 
 Wikipedia reference: https://en.wikipedia.org/wiki/Greatest_common_divisor
 """
 
 
 def gcd(a, b):
-    """Calculate Greater Common Divisor (GCD)."""
+    """Calculate Greatest Common Divisor (GCD)."""
     return b if a == 0 else gcd(b % a, a)
 
 
@@ -16,9 +16,9 @@ def main():
         nums = input("Enter two Integers separated by comma (,): ").split(",")
         num_1 = int(nums[0])
         num_2 = int(nums[1])
+        print(f"gcd({num_1}, {num_2}) = {gcd(num_1, num_2)}")
     except (IndexError, UnboundLocalError, ValueError):
         print("Wrong Input")
-    print(f"gcd({num_1}, {num_2}) = {gcd(num_1, num_2)}")
 
 
 if __name__ == "__main__":
