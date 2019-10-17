fd6f80afe3650bdb77f01352d9f4da397fd133ea
Ghulam Mohiyuddin <subahani98@gmail.com>
2019-10-18 01:42:23 +0530

    Another method added for GCD

--- maths/greatest_common_divisor.py
+++ maths/greatest_common_divisor.py
@@ -5,6 +5,15 @@ Wikipedia reference: https://en.wikipedia.org/wiki/Greatest_common_divisor
 """
 
 
+
+# This method is more efficient not acquire more memory cause is no use of any stacks like in recursive as next below mentioned. 
+def gcd_by_iterative(x,y):
+    while y:x,y=y,x%y
+    	
+    return x
+    
+    
+    
 def gcd(a, b):
     """Calculate Greatest Common Divisor (GCD)."""
     return b if a == 0 else gcd(b % a, a)
@@ -16,10 +25,14 @@ def main():
         nums = input("Enter two Integers separated by comma (,): ").split(",")
         num_1 = int(nums[0])
         num_2 = int(nums[1])
+        
         print(f"gcd({num_1}, {num_2}) = {gcd(num_1, num_2)}")
+        print(f"By iterative gcd({num_1}, {num_2}) = {gcd_by_iterative(num_1, num_2)}")
+       
     except (IndexError, UnboundLocalError, ValueError):
         print("Wrong Input")
 
-
+    
 if __name__ == "__main__":
     main()
+    
