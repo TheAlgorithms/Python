8919e3d2fd4fe0fe4a9a942d179b816d0a13530e
Jai Kumar Dewani <jai.dewani.99@gmail.com>
2019-10-17 22:48:39 +0530

    Added doctest in printNGE()

--- data_structures/stacks/next_greater_element.py
+++ data_structures/stacks/next_greater_element.py
@@ -1,7 +1,13 @@
-# Function to print element and Next Greatest Element (NGE) pair for all elements of list
-# NGE - Maximum element present afterwards the current one which is also greater than current one
 def printNGE(arr):
-
+    """
+    Function to print element and Next Greatest Element (NGE) pair for all elements of list
+    NGE - Maximum element present afterwards the current one which is also greater than current one
+    >>> printNGE([11,13,21,3])
+    11 -- 21
+    13 -- 21 
+    21 -- -1
+    3 -- -1
+    """
     for i in range(0, len(arr), 1):
 
         next = -1
@@ -14,12 +20,5 @@ def printNGE(arr):
 
 
 # Driver program to test above function
-'''
-For the given list the ouput should be something like this:
-11 -- 21
-13 -- 21
-21 -- -1
-3 -- -1
-'''
 arr = [11, 13, 21, 3]
 printNGE(arr)
