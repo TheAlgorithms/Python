aba0539a96c451cdf1ce88a356ae5b6222fb2788
mani9k <42939900+mani9k@users.noreply.github.com>
2019-10-17 20:57:52 +0530

    Update longest_repeated_subsequent.py

--- longest_repeated_subsequent.py
+++ longest_repeated_subsequent.py
@@ -1,25 +1,23 @@
-"""Longest Repeated Subsequence
-Given a string, print the longest repeating subsequence such that the two subsequence don’t have same string character at same position, 
-i.e., any i’th character in the two subsequences shouldn’t have the same index in the original string.
 
+"""Given a string, print the longest repeating subsequence such that the two subsequence don’t have same string character at same position, 
+i.e., any i’th character in the two subsequences shouldn’t have the same index in the original string."""
 
+#Input: str = "aabb"
+#Output: "ab"
 
-Input: str = "aabb"
-Output: "ab"
-
-Input: str = "aab"
-Output: "a"
-The two subsequence are 'a'(first) and 'a' 
+#Input: str = "aab"
+#Output: "a"
+"""The two subsequence are 'a'(first) and 'a' 
 (second). Note that 'b' cannot be considered 
 as part of subsequence as it would be at same
-index in both.
+index in both."""
 
-This problem is just the modification of Longest Common Subsequence problem. The idea is to find the LCS(str, str) where str 
+"""This problem is just the modification of Longest Common Subsequence problem. The idea is to find the LCS(str, str) where str 
 is the input string with the restriction that when both the characters are same, 
-they shouldn’t be on the same index in the two strings.
+they shouldn’t be on the same index in the two strings."""
 
 
-PROGRAM:"""
+
 
 def findLongestRepeatingSubSeq(str): 
     n = len(str) 
