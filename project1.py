5c1fbeaff3c0fb327af4ff6c0254ad6610375ad6
Christian Clauss <cclauss@me.com>
2019-10-18 21:32:33 +0200

    noqa: F401 and reformat with black

--- project1.py
+++ project1.py
@@ -1,21 +1,28 @@
+import sys
+import webbrowser
+
 from bs4 import BeautifulSoup
 import requests
-#from urllib.request import urlopen
-import webbrowser
-import sys
-from fake_useragent import UserAgent  #This is needed for intialazing the User agent of the system otherwise it act like bot
-ua = {"UserAgent":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0"}
-print("Googling.....")
-res=requests.get('https://www.google.com/search?q='+ ' '.join(sys.argv[1:]),headers=ua)
-#res.raise_for_status()
-file=open('project1a.html','wb') #only for knowing the class
-for i in res.iter_content(10000):
-    file.write(i)
-soup= BeautifulSoup(res.text,'lxml')
-linkele=soup.select('.eZt8xd')
 
-num=min(5,len(linkele))
-print(num)
-for i in range(num):
-    
-    webbrowser.open('http://google.com' + linkele[i].get('href'))
+# fake_useragent is needed for so that this script is not viewed as a bot
+from fake_useragent import UserAgent  # noqa: F401
+
+user_agent = {
+    "UserAgent": (
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:69.0) "
+        "Gecko/20100101 Firefox/69.0"
+    )
+}
+print("Googling.....")
+url = "https://www.google.com/search?q=" + " ".join(sys.argv[1:])
+res = requests.get(url, headers=user_agent)
+# res.raise_for_status()
+with open("project1a.html", "wb") as out_file:  # only for knowing the class
+    for data in res.iter_content(10000):
+        out_file.write(data)
+soup = BeautifulSoup(res.text, "lxml")
+links = list(soup.select(".eZt8xd"))[:5]
+
+print(len(links))
+for link in links:
+    webbrowser.open(f"http://google.com{links.get('href')}")
