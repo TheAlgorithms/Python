5a5fdf746ddb9d484495ba4a893fa194c8804002
Nicola Amadio <amadionicola@gmail.com>
2019-10-18 15:12:00 +0200

    Update g_topological_sort.py
    I had a version which I think is better then the one which was here. It also handles the case in which there are cycles in the graph, giving as output an empty list (= impossible scheduling). It also has a graph class which I think it's nice. I think it can be improved, I was actually curious if somebody had a better version of it.

--- graphs/g_topological_sort.py
+++ graphs/g_topological_sort.py
@@ -1,47 +1,41 @@
-# Author: Phyllipe Bezerra (https://github.com/pmba)
+def topological_sort(jobs, deps):
+    g = Graph(jobs, deps)
+    visited = {}
+    finished = []
+    for s in jobs:          
+        if s not in visited:
+            parent_nodes = [False for el in range(len(jobs) + 1)]
+            visited, finished = DFS_rec(s, g, visited, finished, parent_nodes)
+            if visited == None and finished == None:        # there's a cycle
+                return []
+    return list(reversed(finished))
 
-clothes = {
-    0: "underwear",
-    1: "pants",
-    2: "belt",
-    3: "suit",
-    4: "shoe",
-    5: "socks",
-    6: "shirt",
-    7: "tie",
-    8: "clock",
-}
+def DFS_rec(s, g, visited, finished, parent_nodes):
+    parent_nodes[s] = True
+    visited[s] = True
+    if s in g.adj:
+        for v in g.adj[s]:
+            if parent_nodes[v]:
+                return None, None
+            if v not in visited:
+                visited, finished = DFS_rec(v, g, visited, finished, parent_nodes)
+            if visited == None and finished == None:        # there's a cycle
+                return None, None            
+    finished.append(s)
+    parent_nodes[s] = False
+    return visited, finished
 
-graph = [[1, 4], [2, 4], [3], [], [], [4], [2, 7], [3], []]
-
-visited = [0 for x in range(len(graph))]
-stack = []
-
-
-def print_stack(stack, clothes):
-    order = 1
-    while stack:
-        cur_clothe = stack.pop()
-        print(order, clothes[cur_clothe])
-        order += 1
-
-
-def dfs(u, visited, graph):
-    visited[u] = 1
-    for v in graph[u]:
-        if not visited[v]:
-            dfs(v, visited, graph)
-
-    stack.append(u)
-
-
-def top_sort(graph, visited):
-    for v in range(len(graph)):
-        if not visited[v]:
-            dfs(v, visited, graph)
-
-
-if __name__ == "__main__":
-    top_sort(graph, visited)
-    print(stack)
-    print_stack(stack, clothes)
+class Graph:
+    def __init__(self, jobs, deps):
+        self.adj = {}
+        for edge in deps:
+            v, u = edge[0], edge[1]
+            if v not in self.adj:
+                self.adj[v] = [u]
+            else:
+                self.adj[v].append(u)
+                
+if __name__ == '__main__':
+    jobs = [1, 2, 3, 4, 5, 6, 7, 8]
+    deps = [[3, 1], [8, 1], [8, 7], [5, 7], [5, 2], [1, 4], [6, 7], [1, 2], [7, 6]]
+    print(topological_sort(jobs, deps))
