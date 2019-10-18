d8af953d9d64df32122867922dd1760156de6804
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2019-10-19 00:48:36 +0530

    Update activity_selection.py

--- other/activity_selection.py
+++ other/activity_selection.py
@@ -4,10 +4,10 @@ are already sorted according to their finish time"""
 """Prints a maximum set of activities that can be done by a 
 single person, one at a time"""
 # n --> Total number of activities 
-# s[]--> An array that contains start time of all activities 
-# f[] --> An array that contains finish time of all activities 
+# start[]--> An array that contains start time of all activities 
+# finish[] --> An array that contains finish time of all activities 
   
-def printMaxActivities(s, f ): 
+def printMaxActivities(start, finish): 
     n = len(f) 
     print("The following activities are selected")
   
@@ -21,14 +21,14 @@ def printMaxActivities(s, f ):
         # If this activity has start time greater than 
         # or equal to the finish time of previously 
         # selected activity, then select it 
-        if s[j] >= f[i]: 
+        if start[j] >= finish[i]: 
             print(j)
             i = j 
   
 # Driver program to test above function 
-s = [1, 3, 0, 5, 8, 5] 
-f = [2, 4, 6, 7, 9, 9] 
-printMaxActivities(s, f) 
+start = [1, 3, 0, 5, 8, 5] 
+finish = [2, 4, 6, 7, 9, 9] 
+printMaxActivities(start, finish) 
 
 
 
