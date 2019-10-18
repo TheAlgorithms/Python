8b5078284fc38648e1e546100c17a17926fda09a
Warlock001 <alfinwilliam@gmail.com>
2019-10-18 19:52:23 +0530

    added docstrings

--- maths/hardy_ramanujanalgo.py
+++ maths/hardy_ramanujanalgo.py
@@ -1,4 +1,10 @@
+
+#It's a theorem that states the number of prime factors
+# of n will approximately be log(log(n)) for most 
+#natural numbers n
+
 import math 
+
 
 def exactPrimeFactorCount(n) : 
 	count = 0
@@ -6,6 +12,8 @@ def exactPrimeFactorCount(n) :
 		count = count + 1
 		while (n % 2 == 0) : 
 			n = int(n / 2) 
+	#the n input value must be odd so that 
+	#we can skip one element ( ie i = i + 2)
 
 	i = 3
 	
@@ -16,11 +24,15 @@ def exactPrimeFactorCount(n) :
 				n = int(n / i) 
 		i = i + 2
 
+	#this condition checks the prime
+	#number n is greater than 2
+
 	if (n > 2) : 
 		count = count + 1
 	return count 
 
 n = 51242183
+#explicitly define value of n here
 print ("The number of distinct prime factors is/are {}". 
 	format(exactPrimeFactorCount(n), end = "\n")) 
 print ("The value of log(log(n)) is {0:.4f}"
