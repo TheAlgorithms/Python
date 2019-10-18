32d9d3915858ebaa8100f06df672b4b3c6064970
Christian Clauss <cclauss@me.com>
2019-10-18 23:18:24 +0200

    Add a doctest

--- other/activity_selection.py
+++ other/activity_selection.py
@@ -7,7 +7,14 @@ single person, one at a time"""
 # start[]--> An array that contains start time of all activities 
 # finish[] --> An array that contains finish time of all activities 
   
-def printMaxActivities(start, finish): 
+def printMaxActivities(start, finish):
+    """
+    >>> start = [1, 3, 0, 5, 8, 5] 
+    >>> finish = [2, 4, 6, 7, 9, 9] 
+    >>> printMaxActivities(start, finish)
+    The following activities are selected
+    0 1 3 4
+    """
     n = len(finish) 
     print("The following activities are selected")
   
@@ -30,9 +37,7 @@ start = [1, 3, 0, 5, 8, 5]
 finish = [2, 4, 6, 7, 9, 9] 
 printMaxActivities(start, finish) 
 
-
-
-
-
-"""The following activities are selected
-0 1 3 4"""
+"""
+The following activities are selected
+0 1 3 4
+"""
