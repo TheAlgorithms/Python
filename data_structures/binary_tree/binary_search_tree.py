ab67d4686bd811c8ab37e53c110444c4518cc575
Hocnonsense <48747984+Hocnonsense@users.noreply.github.com>
2019-10-18 22:56:43 +0800

    Update binary_search_tree.py

--- data_structures/binary_tree/binary_search_tree.py
+++ data_structures/binary_tree/binary_search_tree.py
@@ -1,74 +1,53 @@
-"""
+'''
 A binary search Tree
-"""
-
+'''
 class Node:
-    def __init__(self, data, parent):
-        self.data = data
+    def __init__(self, value, parent):
+        self.value = value
         self.left = None
         self.right = None
         self.parent = parent    # Added in order to delete a node easier
 
-    def get_data(self):
-        return self.data
+    def __repr__(self):
+        from pprint import pformat
 
-    def getleft(self):
-        return self.left
-
-    def getright(self):
-        return self.right
-
-    def getparent(self):
-        return self.parent
-
-    def setdata(self, data):
-        self.data = data
-
-    def setleft(self, left):
-        self.left = left
-
-    def setRight(self, right):
-        self.right = right
-
-    def setparent(self, parent):
-        self.parent = parent
+        if self.left is None and self.right is None:
+            return "'%s" % (self.value)
+        return pformat(
+            {
+                "%s"
+                % (self.value): (self.left, self.right)
+            },
+            indent=1,
+        )
 
 class BinarySearchTree:
-    def __init__(self):
-        self.root = None
+    def __init__(self, root = None):
+        self.root = root
 
     def __str__(self):
         """
-            Return a string of all the Nodes. 
-            In Order Traversal
+        Return a string of all the Nodes. 
+        In Order Traversal
         """
-        nodelist = self.__InOrderTraversal(self.root)
-        str = " ".join([repr(i.get_data()) for i in nodelist])
-        return str
+        return str(self.root)
 
-    def __InOrderTraversal(self, node):
-        nodeList = list()
-        if node is not None:
-            nodeList.insert(0, node)    # Preorder Traversal
-            nodeList = nodeList + self.__InOrderTraversal(node.getleft()) + self.__InOrderTraversal(node.getright())
-        return nodeList
+    def __reassign_nodes(self, node, newChildren):
+        if(newChildren is not None):    # reset its kids
+            newChildren.parent = node.parent
+        if(node.parent is not None):    # reset its parenst
+            if(self.is_right(node)):    # If it is the Right Children
+                node.parent.right = newChildren
+            else:
+                node.parent.left = newChildren
+        else:
+            self.root = newChildren
 
-    def __isRightChildren(self, node):
-        if(node == node.getparent().getright()):
+    def is_right(self, node):
+        if(node == node.parent.right):
             return True
         else:
             return False
-
-    def __reassignNodes(self, node, newChildren):
-        if(newChildren is not None):    # reset its kids
-            newChildren.setparent(node.getparent())
-        if(node.getparent() is not None):   # reset its parenst
-            if(self.__isRightChildren(node)):   # If it is the Right Children
-                node.getparent().setRight(newChildren)
-            else:   # Else it is the left children
-                node.getparent().setleft(newChildren)
-        else:   # no parent => root
-            self.root = newChildren
 
     def empty(self):
         if self.root is None:
@@ -76,104 +55,112 @@ class BinarySearchTree:
         else:
             return False
 
-    def insert(self, data):
+    def __insert(self, value):
         """
-            Insert a new node in Binary Search Tree with value label
+        Insert a new node in Binary Search Tree with value label
         """
-        new_node = Node(data, None) # Create a new Node
+        new_node = Node(value, None)    # Create a new Node
         if self.empty():    # If Tree is empty
-            self.root = new_node    # set its root
+            self.root = new_node    # 则作为根节点 set its root
         else:   # If Tree is not empty
             parent_node = self.root # from root
             while True: # While we don't get to a leaf
-                if data < parent_node.get_data():    # We go left
-                    if parent_node.getleft() == None:
-                        parent_node.setleft(new_node)   # We insert the new node in a leaf
+                if value < parent_node.value:   # We go left
+                    if parent_node.left == None:
+                        parent_node.left = new_node # We insert the new node in a leaf
                         break
                     else:
-                        parent_node = parent_node.getleft()
-                else:   # Else we go right
-                    if parent_node.getright() == None:
-                        parent_node.setRight(new_node)
-                        break
-                    else:
-                        parent_node = parent_node.getright()
-            new_node.setparent(parent_node) # Set parent to the new node
-
-    def getNode(self, data):
-        if self.empty():
-            raise IndexError("Warning: Tree is empty! please use another. ")
-        else:   # If the tree is not empty
-            node = self.getRoot()
-            while node is not None and node.get_data() is not data:    # using lazy evaluation here to avoid NoneType Attribute error
-                if data < node.get_data():
-                    node = node.getleft()   # We go left
+                        parent_node = parent_node.left
                 else:
-                    node = node.getright()  # Else we go right
+                    if parent_node.right == None:
+                        parent_node.right = new_node
+                        break
+                    else:
+                        parent_node = parent_node.right
+            new_node.parent = parent_node
+
+    def insert(self, *values):
+        for value in values:
+            self.__insert(value)
+        return self
+
+    def search(self, value):
+        if self.empty():
+            raise IndexError("Warning: Tree is empty! please use another. ")  # Warning: Tree is empty! please use another.
+        else:
+            node = self.root
+            while node is not None and node.value is not value:    # Using lazy evaluation here to avoid NoneType Attribute error
+                if value < node.value:
+                    node = node.left
+                else:
+                    node = node.right
             return node
 
-    def getRoot(self):
-        return self.root
-
-    def getMax(self, node = None):
+    def get_max(self, node = None):
         """
-            We go deep on the right branch
+        We go deep on the right branch
         """
         if(node is None):
-            node = self.getRoot()
+            node = self.root
         if(not self.empty()):
-            while(node.getright() is not None):
-                node = node.getright()
+            while(node.right is not None):
+                node = node.right
         return node
 
-    def getMin(self, node = None):
+    def get_min(self, node = None):
         """
-            We go deep on the left branch
+        We go deep on the left branch
         """
         if(node is None):
-            node = self.getRoot()
+            node = self.root
         if(not self.empty()):
-            node = self.getRoot()
-            while(node.getleft() is not None):
-                node = node.getleft()
+            node = self.root
+            while(node.left is not None):
+                node = node.left
         return node
 
-    def delete(self, data):
-        node = self.getNode(data)   # Look for the node with that label
+    def remove(self, value):
+        node = self.search(value)    # Look for the node with that label
         if(node is not None):
-            if(node.getleft() is None and node.getright() is None): # If it has no children
-                self.__reassignNodes(node, None)
+            if(node.left is None and node.right is None):   # If it has no children
+                self.__reassign_nodes(node, None)
                 node = None
-            elif(node.getleft() is None):   # Has only right children
-                self.__reassignNodes(node, node.getright())
-            elif(node.getright() is None):  # Has only left children
-                self.__reassignNodes(node, node.getleft())
-            else:   # Has two children
-                tmpNode = self.getMax(node.getleft())   # Gets the max value of the left branch
-                self.delete(tmpNode.get_data())
-                node.setdata(tmpNode.get_data()) # Assigns the value to the node to delete and keesp tree structure
+            elif(node.left is None):    # Has only right children
+                self.__reassign_nodes(node, node.right)
+            elif(node.right is None):   # Has only left children
+                self.__reassign_nodes(node, node.left)
+            else:
+                tmpNode = self.get_max(node.left)   # Gets the max value of the left branch
+                self.remove(tmpNode.value)
+                node.value = tmpNode.value  # Assigns the value to the node to delete and keesp tree structure
+        
+    def preorder_traverse(self, node):
+        if node is not None:
+            yield node    # Preorder Traversal
+            yield from self.preorder_traverse(node.left)
+            yield from self.preorder_traverse(node.right)
 
-    def traversalTree(self, traversalFunction = None, root = None):
+    def traversal_tree(self, traversalFunction = None):
         """
-            This function traversal the tree.
-            You can pass a function to traversal the tree as needed by client code
+        This function traversal the tree.
+        You can pass a function to traversal the tree as needed by client code
         """
-        if(traversalFunction is None):  # default
-            return self.__InOrderTraversal(self.root)
+        if(traversalFunction is None):
+            return self.preorder_traverse(self.root)
         else:
             return traversalFunction(self.root)
 
-def postOrder(curr_node):
+def postorder(curr_node):
     """
-        postOrder (left, right, self)
+    postOrder (left, right, self)
     """
     nodeList = list()
     if curr_node is not None:
-        nodeList = postOrder(curr_node.getleft()) + postOrder(curr_node.getright()) + [curr_node]
+        nodeList = postorder(curr_node.left) + postorder(curr_node.right) + [curr_node]
     return nodeList
 
 def binary_search_tree():
-    r"""
+    r'''
     Example
                   8
                  / \
@@ -182,38 +169,45 @@ def binary_search_tree():
               1   6    14
                  / \   /
                 4   7 13
-    """
+
+        >>> t = BinarySearchTree().insert(8, 3, 6, 1, 10, 14, 13, 4, 7)
+        >>> print(" ".join([repr(i.value) for i in t.traversal_tree()]))
+        8 3 1 6 4 7 10 14 13
+        >>> print(" ".join([repr(i.value) for i in t.traversal_tree(postorder)]))
+        1 4 7 6 3 13 14 10 8
+        >>> BinarySearchTree().search(6)
+        Traceback (most recent call last):
+        ...
+        IndexError: Warning: Tree is empty! please use another. 
+    '''
     t = BinarySearchTree()
     testlist = [8, 3, 6, 1, 10, 14, 13, 4, 7]
     for i in testlist:
         t.insert(i)
 
-    #Prints all the elements of the list in order traversal
+    # Prints all the elements of the list in order traversal
     print(t)
-    #Gets all the elements of the tree In pre order
-    #And it prints them
-    nodelist = t.traversalTree(postOrder, t.root)
-    print(" ".join([repr(i.get_data()) for i in nodelist]))
     
-    if(t.getNode(6) is not None):
-        print("The data 6 exists")
+    if(t.search(6) is not None):
+        print("The value 6 exists")
     else:
-        print("The data 6 doesn't exist")
+        print("The value 6 doesn't exist")
 
-    if(t.getNode(-1) is not None):
-        print("The data -1 exists")
+    if(t.search(-1) is not None):
+        print("The value -1 exists")
     else:
-        print("The data -1 doesn't exist")
+        print("The value -1 doesn't exist")
 
     if(not t.empty()):
-        print(("Max Value: ", t.getMax().get_data()))
-        print(("Min Value: ", t.getMin().get_data()))
+        print("Max Value: ", t.get_max().value)
+        print("Min Value: ", t.get_min().value)
 
     for i in testlist:
-        t.delete(i)
+        t.remove(i)
         print(t)
-
-    t.getNode(6)
+二叉搜索树 = binary_search_tree
 
 if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
     binary_search_tree()
