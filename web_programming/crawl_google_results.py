4600feab6bdb27558530330624192fe4f4c5a795
Christian Clauss <cclauss@me.com>
2019-10-18 21:47:09 +0200

    Update crawl_google_results.py

--- web_programming/crawl_google_results.py
+++ web_programming/crawl_google_results.py
@@ -2,20 +2,12 @@ import sys
 import webbrowser
 
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 import requests
 
-# fake_useragent is needed for so that this script is not viewed as a bot
-from fake_useragent import UserAgent  # noqa: F401
-
-user_agent = {
-    "UserAgent": (
-        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:69.0) "
-        "Gecko/20100101 Firefox/69.0"
-    )
-}
 print("Googling.....")
 url = "https://www.google.com/search?q=" + " ".join(sys.argv[1:])
-res = requests.get(url, headers=user_agent)
+res = requests.get(url, headers=UserAgent().random)
 # res.raise_for_status()
 with open("project1a.html", "wb") as out_file:  # only for knowing the class
     for data in res.iter_content(10000):
