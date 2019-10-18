18403f6df0f824964dd057ea1e5c8a750d403d3e
Prathamesh <pprathamesh1594@gmail.com>
2019-10-18 16:23:41 +0530

    Make amendments in the gcd method
    -  Added doctest

--- maths/greatest_common_divisor_alt.py
+++ maths/greatest_common_divisor_alt.py
@@ -4,16 +4,46 @@ https://en.wikipedia.org/wiki/Euclidean_algorithm#Implementations
 """
 
 
-def gcd(a, b):
-    """Calculate Greater Common Divisor (GCD)."""
-    if a = 0:
-       return b
-    while b != 0:
-        if a > b:
-           a = a − b
+def gcd(x, y):
+    """
+    Computes the greatest common divisor of x and y.  For example:
+    >>> gcd(40,16)
+    8
+    >>> gcd(24,15)
+    3
+    >>> gcd(12,85)
+    1
+
+    Both arguments must be positive integers.
+
+    >>> gcd(3.5,4.2)
+    Traceback (most recent call last):
+    ...
+    TypeError: Arguments must be integers
+    >>> gcd(-4,7)
+    Traceback (most recent call last):
+    ...
+    ValueError: Arguments must be positive integers
+
+    Long integers may also be used. In this case, the returned value is also a
+long.
+
+    >>> gcd(23748928388L, 6723884L)
+    4L
+
+    """
+    if not (isinstance(x,(int,long)) and isinstance(y,(int,long))):
+       raise TypeError, "Arguments must be integers"
+    if x <= 0 or y <= 0:
+       raise ValueError, "Arguments must be positive integers"
+    if x == 0:
+       return y
+    while y != 0:
+        if x > y:
+           x = x − y
         else:
-           b = b − a
-    return a
+           b = y − x
+    return x
 
 
 def main():
