dd627aeb4a64354a6541691099dff3fedec07717
Christian Clauss <cclauss@me.com>
2019-10-18 23:14:25 +0200

    headers={"UserAgent": UserAgent().random}

--- web_programming/crawl_google_results.py
+++ web_programming/crawl_google_results.py
@@ -7,7 +7,7 @@ import requests
 
 print("Googling.....")
 url = "https://www.google.com/search?q=" + " ".join(sys.argv[1:])
-res = requests.get(url, headers=UserAgent().random)
+res = requests.get(url, headers={"UserAgent": UserAgent().random})
 # res.raise_for_status()
 with open("project1a.html", "wb") as out_file:  # only for knowing the class
     for data in res.iter_content(10000):
