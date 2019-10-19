72cf5d220837c153706224f13891d6fe1b7c6f18
anubhav1306 <anubhav.sharma@reserach.iiit.ac.in>
2019-10-19 23:33:27 +0530

    largest subarray sum

---
+++ other/largest_subarray_sum.py
@@ -0,0 +1,18 @@
+from sys import maxint 
+def maxSubArraySum(a, size): 
+       
+    max_so_far = -maxint - 1
+    max_ending_here = 0
+       
+    for i in range(0, size): 
+        max_ending_here = max_ending_here + a[i] 
+        if (max_so_far < max_ending_here): 
+            max_so_far = max_ending_here 
+  
+        if max_ending_here < 0: 
+            max_ending_here = 0   
+    return max_so_far 
+   
+# Driver function to check the above function  
+a = [-13, -3, -25, -20, -3, -16, -23, -12, -5, -22, -15, -4, -7] 
+print "Maximum contiguous sum is", maxSubArraySum(a, len(a)) 
