b528a5f0a70d614d4965558a15d9f1054a70f965
Christian Clauss <cclauss@me.com>
2019-10-19 05:57:57 +0200

    Run Python black on the code

--- maths/hardy_ramanujanalgo.py
+++ maths/hardy_ramanujanalgo.py
@@ -1,46 +1,45 @@
+# This theorem states that the number of prime factors of n
+# will be approximately log(log(n)) for most natural numbers n
 
-#It's a theorem that states the number of prime factors
-# of n will approximately be log(log(n)) for most 
-#natural numbers n
-
-import math 
+import math
 
 
-def exactPrimeFactorCount(n) : 
-	count = 0
-	if (n % 2 == 0) : 
-		count = count + 1
-		while (n % 2 == 0) : 
-			n = int(n / 2) 
-	#the n input value must be odd so that 
-	#we can skip one element ( ie i = i + 2)
+def exactPrimeFactorCount(n):
+    """
+    >>> exactPrimeFactorCount(51242183)
+    3
+    """
+    count = 0
+    if n % 2 == 0:
+        count += 1
+        while n % 2 == 0:
+            n = int(n / 2)
+    # the n input value must be odd so that
+    # we can skip one element (ie i += 2)
 
-	i = 3
-	
-	while (i <= int(math.sqrt(n))) : 
-		if (n % i == 0) :	 
-			count = count + 1
-			while (n % i == 0) : 
-				n = int(n / i) 
-		i = i + 2
+    i = 3
 
-	#this condition checks the prime
-	#number n is greater than 2
+    while i <= int(math.sqrt(n)):
+        if n % i == 0:
+            count += 1
+            while n % i == 0:
+                n = int(n / i)
+        i = i + 2
 
-	if (n > 2) : 
-		count = count + 1
-	return count 
+    # this condition checks the prime
+    # number n is greater than 2
 
-n = 51242183
-#explicitly define value of n here
-print ("The number of distinct prime factors is/are {}". 
-	format(exactPrimeFactorCount(n), end = "\n")) 
-print ("The value of log(log(n)) is {0:.4f}"
-			.format(math.log(math.log(n)))) 
+    if n > 2:
+        count += 1
+    return count
 
-"""
-n = 51242183
-The number of distinct prime factors is/are 3
-The value of log(log(n)) is 2.8765
-"""
 
+if __name__ == "__main__":
+    n = 51242183
+    print(f"The number of distinct prime factors is/are {exactPrimeFactorCount(n)}")
+    print("The value of log(log(n)) is {0:.4f}".format(math.log(math.log(n))))
+
+    """
+    The number of distinct prime factors is/are 3
+    The value of log(log(n)) is 2.8765
+    """
