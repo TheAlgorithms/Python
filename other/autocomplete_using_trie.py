d5c6c693b01d012fbb59a7032d947355afa4e9be
Christian Clauss <cclauss@me.com>
2019-10-20 10:36:27 +0200

    Run the code through Black and add doctest

--- other/autocomplete_using_trie.py
+++ other/autocomplete_using_trie.py
@@ -1,8 +1,10 @@
-END = '#'
+END = "#"
+
+
 class Trie:
     def __init__(self):
         self._trie = {}
-    
+
     def insert_word(self, text):
         trie = self._trie
         for char in text:
@@ -10,37 +12,53 @@ class Trie:
                 trie[char] = {}
             trie = trie[char]
         trie[END] = True
-    
+
     def find_word(self, prefix):
         trie = self._trie
         for char in prefix:
             if char in trie:
                 trie = trie[char]
             else:
-                return[]
+                return []
         return self._elements(trie)
 
     def _elements(self, d):
         result = []
         for c, v in d.items():
             if c == END:
-                subresult = [' ']
+                subresult = [" "]
             else:
                 subresult = [c + s for s in self._elements(v)]
             result.extend(subresult)
-        return result
+        return tuple(result)
+
 
 trie = Trie()
-words = ['depart', 'detergent', 'daring', 'dog', 'deer', 'deal']
+words = ("depart", "detergent", "daring", "dog", "deer", "deal")
 for word in words:
     trie.insert_word(word)
 
+
 def autocomplete_using_trie(s):
+    """
+    >>> trie = Trie()
+    >>> for word in words:
+    ...     trie.insert_word(word)
+    ...
+    >>> matches = autocomplete_using_trie("de")
+
+    "detergent " in matches
+    True
+    "dog " in matches
+    False
+    """
     suffixes = trie.find_word(s)
-    return [s + w for w in suffixes]
+    return tuple(s + w for w in suffixes)
+
 
 def main():
-    print(autocomplete_using_trie('de'))
+    print(autocomplete_using_trie("de"))
+
 
 if __name__ == "__main__":
     main()
