a0054415c37741c9f0aac836bc1ba246c37ae76d
Christian Clauss <cclauss@me.com>
2019-10-21 11:57:20 +0200

    graph.addEdge(3, 4)

--- graphs/cycle_detection_undirected_graph.py
+++ graphs/cycle_detection_undirected_graph.py
@@ -63,6 +63,7 @@ def isCyclic(g,n):
     >>> graph.addEdge(0, 1)
     >>> graph.addEdge(1, 2)
     >>> graph.addEdge(2, 3)
+    >>> graph.addEdge(3, 4)
     >>> isCyclic(graph.graph, 5)
     0
     >>> graph.addEdge(4, 2)
