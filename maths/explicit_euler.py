8c2e748a7c6783225ace7a275d7e805926eeee41
Stephen <24819660+infrontoftheforest@users.noreply.github.com>
2019-10-21 17:12:05 +0000

    update explicit_euler.py variable names

--- maths/explicit_euler.py
+++ maths/explicit_euler.py
@@ -1,17 +1,17 @@
 import numpy as np
 
 
-def explicit_euler(f, y0, x0, h, x_end):
+def explicit_euler(ode_func, y0, x0, stepsize, x_end):
     """
     Calculate numeric solution at each step to an ODE using Euler's Method
 
     https://en.wikipedia.org/wiki/Euler_method
 
     Arguments:
-    f -- The ode as a function of x and y
+    ode_func -- The ode as a function of x and y
     y0 -- the initial value for y
     x0 -- the initial value for x
-    h -- the stepsize
+    stepsize -- the increment value for x
     x_end -- the end value for x
 
     >>> # the exact solution is math.exp(x)
@@ -22,14 +22,14 @@ def explicit_euler(f, y0, x0, h, x_end):
     >>> y[-1]
     144.77277243257308
     """
-    N = int(np.ceil((x_end - x0)/h))
+    N = int(np.ceil((x_end - x0)/stepsize))
     y = np.zeros((N + 1,))
     y[0] = y0
     x = x0
 
     for k in range(N):
-        y[k + 1] = y[k] + h*f(x, y[k])
-        x += h
+        y[k + 1] = y[k] + stepsize*ode_func(x, y[k])
+        x += stepsize
 
     return y
 
