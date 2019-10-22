60aeb5103f588d80f6001b97f9821480992f16c5
Christian Clauss <cclauss@me.com>
2019-10-22 11:20:24 +0200

    Update greatest_common_divisor.py

--- maths/greatest_common_divisor.py
+++ maths/greatest_common_divisor.py
@@ -5,20 +5,19 @@ Wikipedia reference: https://en.wikipedia.org/wiki/Greatest_common_divisor
 """
 
 
-def gcd(a, b):
+def greatest_common_divisor(a, b):
     """
     Calculate Greatest Common Divisor (GCD).
-    >>> gcd(24, 40)
+    >>> greatest_common_divisor(24, 40)
     8
     """
     return b if a == 0 else gcd(b % a, a)
 
 
 """
-Below method is more efficient.
-This method is not acquire more memory cause is no use of any stacks(chunk of a memory
-space).  While above method is good one but acquire more memory for huge number because
-of more recursive call to evaluate GCD.
+Below method is more memory efficient because it does not use the stack (chunk of memory).
+While above method is good, uses more memory for huge numbers because of the recursive calls
+required to calculate the greatest common divisor.
 """
 
 
@@ -26,7 +25,7 @@ def gcd_by_iterative(x, y):
     """
     >>> gcd_by_iterative(24, 40)
     8
-    >>> gcd(24, 40) == gcd_by_iterative(24, 40)
+    >>> greatest_common_divisor(24, 40) == gcd_by_iterative(24, 40)
     True
     """
     while y:  # --> when y=0 then loop will terminate and return x as final GCD.
@@ -35,12 +34,12 @@ def gcd_by_iterative(x, y):
 
 
 def main():
-    """Call GCD function."""
+    """Call Greatest Common Divisor function."""
     try:
         nums = input("Enter two integers separated by comma (,): ").split(",")
         num_1 = int(nums[0])
         num_2 = int(nums[1])
-        print(f"gcd({num_1}, {num_2}) = {gcd(num_1, num_2)}")
+        print(f"greatest_common_divisor({num_1}, {num_2}) = {greatest_common_divisor(num_1, num_2)}")
         print(f"By iterative gcd({num_1}, {num_2}) = {gcd_by_iterative(num_1, num_2)}")
     except (IndexError, UnboundLocalError, ValueError):
         print("Wrong input")
