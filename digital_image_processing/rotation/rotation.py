0b792c5842d9a1396541ed78084a558acd98bc47
Matheus Guimaraes <matheusguimr@gmail.com>
2019-10-22 15:12:53 -0300

    Add type hints in get_rotation() and  enumerate() in loop

--- digital_image_processing/rotation/rotation.py
+++ digital_image_processing/rotation/rotation.py
@@ -1,9 +1,11 @@
 from matplotlib import pyplot as plt
-from numpy import float32
+import numpy as np
 import cv2
 
 
-def get_rotation(img, pt1, pt2, rows, cols):
+def get_rotation(
+    img: np.array, pt1: np.float32, pt2: np.float32, rows: int, cols: int
+) -> np.array:
     """
     Get image rotation
     :param img: np.array
@@ -26,10 +28,10 @@ if __name__ == "__main__":
     img_rows, img_cols = gray_img.shape
 
     # set different points to rotate image
-    pts1 = float32([[50, 50], [200, 50], [50, 200]])
-    pts2 = float32([[10, 100], [200, 50], [100, 250]])
-    pts3 = float32([[50, 50], [150, 50], [120, 200]])
-    pts4 = float32([[10, 100], [80, 50], [180, 250]])
+    pts1 = np.float32([[50, 50], [200, 50], [50, 200]])
+    pts2 = np.float32([[10, 100], [200, 50], [100, 250]])
+    pts3 = np.float32([[50, 50], [150, 50], [120, 200]])
+    pts4 = np.float32([[10, 100], [80, 50], [180, 250]])
 
     # add all rotated images in a list
     images = [
@@ -42,8 +44,8 @@ if __name__ == "__main__":
     # plot different image rotations
     fig = plt.figure(1)
     titles = ["Original", "Rotation 1", "Rotation 2", "Rotation 3"]
-    for i in range(0, len(images)):
-        plt.subplot(2, 2, i + 1), plt.imshow(images[i], "gray")
+    for i, image in enumerate(images):
+        plt.subplot(2, 2, i + 1), plt.imshow(image, "gray")
         plt.title(titles[i])
         plt.axis("off")
         plt.subplots_adjust(left=0.0, bottom=0.05, right=1.0, top=0.95)
