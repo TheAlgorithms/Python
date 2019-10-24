a8a3c8b2ad3282810dede60ed578d5b7adfc0444
pingings <56961474+pingings@users.noreply.github.com>
2019-10-24 16:44:00 +0100

    Update sol1.py

--- project_euler/problem_33/sol1.py
+++ project_euler/problem_33/sol1.py
@@ -1,3 +1,23 @@
+"""
+
+Problem:
+
+The fraction 49/98 is a curious fraction, as an inexperienced 
+mathematician in attempting to simplify it may incorrectly believe 
+that 49/98 = 4/8, which is correct, is obtained by cancelling the 9s.
+
+We shall consider fractions like, 30/50 = 3/5, to be trivial examples.
+
+There are exactly four non-trivial examples of this type of fraction, 
+less than one in value, and containing two digits in the numerator 
+and denominator.
+
+If the product of these four fractions is given in its lowest common 
+terms, find the value of the denominator.
+
+"""
+
+
 def isDigitCancelling(num,den):
     if num != den:
         if num % 10 == den // 10:
