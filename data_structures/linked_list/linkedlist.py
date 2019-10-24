c72542bfc7af81b83fcd77f46ad0d07782221f59
Ritesh8732 <47283395+Ritesh8732@users.noreply.github.com>
2019-10-24 14:47:54 +0530

    create linkedlist

---
+++ data_structures/linked_list/linkedlist.py
@@ -0,0 +1,38 @@
+class Node: 
+  
+    # Function to initialise the node object 
+    def __init__(self, data): 
+        self.data = data  # Assign data 
+        self.next = None  # Initialize next as null 
+  
+  
+# Linked List class contains a Node object 
+class LinkedList: 
+  
+    # Function to initialize head 
+    def __init__(self): 
+        self.head = None
+  
+    # This function prints contents of linked list 
+    # starting from head 
+    def printList(self): 
+        temp = self.head 
+        while (temp): 
+            print temp.data, 
+            temp = temp.next
+  
+  
+# Code execution starts here 
+if __name__=='__main__': 
+  
+    # Start with the empty list 
+    llist = LinkedList() 
+  
+    llist.head = Node(1) 
+    second = Node(2) 
+    third = Node(3) 
+  
+    llist.head.next = second; # Link first node with second 
+    second.next = third; # Link second node with the third node 
+  
+    llist.printList() 
