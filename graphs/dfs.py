d05e4225763bae8fc3bc294194fb3887686f60ea
percy07 <56677891+percy07@users.noreply.github.com>
2019-10-25 01:36:28 +0530

    Update dfs.py
    Optimized the DFS Algorithm.It is also working for Disconnected Graph.

--- graphs/dfs.py
+++ graphs/dfs.py
@@ -1,44 +1,60 @@
-"""pseudo-code"""
+# Python program to print DFS traversal for complete graph 
+from collections import defaultdict 
 
-"""
-DFS(graph G, start vertex s):
-// all nodes initially unexplored
-mark s as explored
-for every edge (s, v):
-    if v unexplored:
-        DFS(G, v)
-"""
+# This class represents a directed graph using adjacency 
+# list representation 
+class Graph: 
 
+    # Constructor 
+    def __init__(self): 
 
-def dfs(graph, start):
-    """The DFS function simply calls itself recursively for every unvisited child of its argument. We can emulate that
-     behaviour precisely using a stack of iterators. Instead of recursively calling with a node, we'll push an iterator
-      to the node's children onto the iterator stack. When the iterator at the top of the stack terminates, we'll pop
-       it off the stack."""
-    explored, stack = set(), [start]
-    while stack:
-        v = (
-            stack.pop()
-        )  # one difference from BFS is to pop last element here instead of first one
+        # default dictionary to store graph 
+        self.graph = defaultdict(list) 
 
-        if v in explored:
-            continue
+    # function to add an edge to graph 
+    def addEdge(self,u,v): 
+        self.graph[u].append(v) 
 
-        explored.add(v)
+    # A function used by DFS 
+    def DFSUtil(self, v, visited): 
 
-        for w in graph[v]:
-            if w not in explored:
-                stack.append(w)
-    return explored
+        # Mark the current node as visited and print it 
+        visited[v]= True
+        print(v) 
+        
+
+        # Recur for all the vertices adjacent to 
+        # this vertex 
+        for i in self.graph[v]: 
+            if visited[i] == False: 
+                self.DFSUtil(i, visited) 
 
 
-G = {
-    "A": ["B", "C"],
-    "B": ["A", "D", "E"],
-    "C": ["A", "F"],
-    "D": ["B"],
-    "E": ["B", "F"],
-    "F": ["C", "E"],
-}
+    # The function to do DFS traversal. It uses 
+    # recursive DFSUtil() 
+    def DFS(self): 
+        V = len(self.graph) #total vertices 
 
-print(dfs(G, "A"))
+        # Mark all the vertices as not visited 
+        visited =[False]*(V) 
+
+        # Call the recursive helper function to print 
+        # DFS traversal starting from all vertices one 
+        # by one 
+        for i in range(V): 
+            if visited[i] == False: 
+                self.DFSUtil(i, visited) 
+
+
+# Driver code 
+# Create a graph given in the above diagram 
+g = Graph() 
+g.addEdge(0, 1) 
+g.addEdge(0, 2) 
+g.addEdge(1, 2) 
+g.addEdge(2, 0) 
+g.addEdge(2, 3) 
+g.addEdge(3, 3) 
+
+print("Following is Depth First Traversal")
+g.DFS() 
