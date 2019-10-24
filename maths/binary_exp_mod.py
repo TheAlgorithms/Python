52f80f33233140877c12191364d1cc4c1b5a9d97
Anzoteh96 <anzoteh@hotmail.com>
2019-10-23 21:22:41 -0400

    Added binary exponentiaion with respect to modulo

---
+++ maths/binary_exp_mod.py
@@ -0,0 +1,23 @@
+import math
+
+def bin_exp_mod(a, n, b):
+  # mod b
+  assert(not (b == 0)), "This cannot accept modulo that is == 0"
+  if (n == 0):
+    return 1
+
+  if (n % 2 == 1):
+    return (bin_exp_mod(a, n - 1, b) * a) % b
+
+  r = bin_exp_mod(a, n/2, b)
+  return (r * r) % b
+
+if __name__ == '__main__':
+  try:
+    BASE = int(input("Enter Base : ").strip())
+    POWER = int(input("Enter Power : ").strip())
+    MODULO = int(input("Enter Modulo : ").strip())
+  except ValueError:
+    print("Invalid literal for integer")
+
+  print(bin_exp_mod(BASE, POWER, MODULO))
