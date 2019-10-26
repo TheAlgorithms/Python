6944309993c30ae39e8b1dad7dc42a698b090079
DevilLord9967 <DevilLord9967@users.noreply.github.com>
2019-10-26 15:08:26 +0530

    Update circular_queue.py

--- data_structures/queue/circular_queue.py
+++ data_structures/queue/circular_queue.py
@@ -5,7 +5,7 @@ class CircularQueue:
         self.queue = list()
         self.head = 0
         self.tail = 0
-        self.maxSize = 8
+        self.max_size = 8
 
     #Adding elements to the queue
     def enqueue(self,data):
@@ -17,10 +17,10 @@ class CircularQueue:
         >>> CQ.size()
         1
         """
-        if self.size() == self.maxSize-1:
+        if self.size() == self.max_size-1:
             return ("Queue Full!")
         self.queue.append(data)
-        self.tail = (self.tail + 1) % self.maxSize
+        self.tail = (self.tail + 1) % self.max_size
         return True
 
     #Removing elements from the queue
@@ -38,7 +38,7 @@ class CircularQueue:
         if self.size()==0:
             return ("Queue Empty!")
         data = self.queue[self.head]
-        self.head = (self.head + 1) % self.maxSize
+        self.head = (self.head + 1) % self.max_size
         return data
 
     #Calculating the size of the queue
@@ -52,7 +52,7 @@ class CircularQueue:
         """
         if self.tail>=self.head:
             return (self.tail-self.head)
-        return (self.maxSize - (self.head-self.tail))
+        return (self.max_size - (self.head-self.tail))
 
 q = CircularQueue()
 
