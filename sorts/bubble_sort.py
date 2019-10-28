09bcd1c7318239301a08001ef308059ced77da5e
mahimadubey <54871679+mahimadubey@users.noreply.github.com>
2019-10-28 13:07:24 +0530

    Update bubble_sort.py

--- sorts/bubble_sort.py
+++ sorts/bubble_sort.py
@@ -1,43 +1,11 @@
-def bubble_sort(collection):
-    """Pure implementation of bubble sort algorithm in Python
-
-    :param collection: some mutable ordered collection with heterogeneous
-    comparable items inside
-    :return: the same collection ordered by ascending
-
-    Examples:
-    >>> bubble_sort([0, 5, 2, 3, 2])
-    [0, 2, 2, 3, 5]
-
-    >>> bubble_sort([])
-    []
-
-    >>> bubble_sort([-2, -45, -5])
-    [-45, -5, -2]
-
-    >>> bubble_sort([-23, 0, 6, -4, 34])
-    [-23, -4, 0, 6, 34]
-
-    >>> bubble_sort([-23, 0, 6, -4, 34]) == sorted([-23, 0, 6, -4, 34])
-    True
-    """
-    length = len(collection)
-    for i in range(length - 1):
-        swapped = False
-        for j in range(length - 1 - i):
-            if collection[j] > collection[j + 1]:
-                swapped = True
-                collection[j], collection[j + 1] = collection[j + 1], collection[j]
-        if not swapped:
-            break  # Stop iteration if the collection is sorted.
-    return collection
-
-
-if __name__ == "__main__":
-    import time
-
-    user_input = input("Enter numbers separated by a comma:").strip()
-    unsorted = [int(item) for item in user_input.split(",")]
-    start = time.process_time()
-    print(*bubble_sort(unsorted), sep=",")
-    print(f"Processing time: {time.process_time() - start}")
+def bubbleSort(a):
+    n = len(a)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if a[j] > a[j+1] :
+                a[j], a[j+1] = a[j+1], a[j]
+l= [64, 34, 25, 12, 22, 11, 90]
+bubbleSort(l)
+print ("Sorted array is:")
+for i in range(len(l)):
+    print ("%d" %l[i])
