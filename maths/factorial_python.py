9fc2a9a2ad098b9254dab4f6904c1ae7e9d40b28
Deekshaesha <57080015+Deekshaesha@users.noreply.github.com>
2019-10-28 11:25:50 +0530

    Update factorial_python.py

--- maths/factorial_python.py
+++ maths/factorial_python.py
@@ -1,21 +1,5 @@
-def factorial(input_number: int) -> int:
-    """
-        Non-recursive algorithm of finding factorial of the
-        input number.
-        >>> factorial(1)
-        1
-        >>> factorial(6)
-        720
-        >>> factorial(0)
-        1
-    """
-
-    if input_number < 0:
-        raise ValueError("Input input_number should be non-negative")
-    elif input_number == 0:
-        return 1
-    else:
-        result = 1
-        for i in range(input_number):
-            result = result * (i + 1)
-    return result
+n = int(input("Enter a number")
+s=1
+for i in range(1,n+1):
+        s*=i
+print (s)
