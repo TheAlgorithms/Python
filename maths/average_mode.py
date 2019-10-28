42c11f27dcec381bc8aa4c5488c20bedb9636847
LokiUvaraj <54948079+LokiUvaraj@users.noreply.github.com>
2019-10-28 14:48:35 +0530

    Update average_mode.py

--- maths/average_mode.py
+++ maths/average_mode.py
@@ -1,18 +1,22 @@
-def mode(inputlist):                    					#Defining function "mode."
+def mode(input_list):                    					#Defining function "mode."
 	"""
 	This function returns the mode(Mode as in the measures of central tendency) of the input data.
-	
-	>>> mode([2,3,4,5,3,4,2,5,2,2,4,2,2,2])
+
+	>>> input_list = [2,3,4,5,3,4,2,5,2,2,4,2,2,2]
+	>>> mode(input_list)
 	2
+	>>> import statistics
+	>>> mode(input_list) == statistics.mode(input_list)
+	True
 	
 	the input list may contain any Datastructure or any Datatype.
 	"""
-	checklist = inputlist.copy()    					#Copying inputlist to check with the index number later.
+	check_list = input_list.copy()    					#Copying inputlist to check with the index number later.
 	result = list()								#Empty list to store the counts of elements in inputlist
 	for x in inputlist:
-		result.append(inputlist.count(x))		
-		inputlist.remove(x)
+		result.append(input_list.count(x))		
+		input_list.remove(x)
 		y=max(result)							#Gets the maximum value in the result list.
-		return checklist[result.index(y)]				#Returns the value with the maximum number of repetitions.
+		return check_list[result.index(y)]				#Returns the value with the maximum number of repetitions.
 data=[1,2,4,3,1,6,4,2,5,1,1,2,3]
 print(mode(data))
