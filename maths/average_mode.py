8579199e304e029f18440adb7a1b4103f0648173
LokiUvaraj <54948079+LokiUvaraj@users.noreply.github.com>
2019-10-28 12:43:33 +0530

    Update average_mode.py

--- maths/average_mode.py
+++ maths/average_mode.py
@@ -1,11 +1,10 @@
-def mode(inputlist):
-	countlist = []
-	checklist = inputlist.copy()
-	result = list()
+def mode(inputlist):                    #Defining function "mode."
+	checklist = inputlist.copy()    #Copying inputlist to check with the index number later.
+	result = list()			#Empty list to store the counts of elements in inputlist
 	for x in inputlist:
-		result.append(inputlist.count(x))
+		result.append(inputlist.count(x))		
 		inputlist.remove(x)
-		y=max(result)
-		return checklist[result.index(y)]
-data=[1,2,3,4,5,1,1,1,1,1]
+		y=max(result)				#Gets the maximum value in the result list.
+		return checklist[result.index(y)]	#Returns the value with the maximum number of repetitions.
+data=[1,2,4,3,1,6,4,2,5,1,1,2,3]
 print(mode(data))
