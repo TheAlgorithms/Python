a49ec029096c1f2d7c49082f57229fcbb3d75215
mahimadubey <54871679+mahimadubey@users.noreply.github.com>
2019-10-28 12:32:51 +0530

    arithmetic sum of numbers
    just made it easier and user friendly

--- maths/sum_of_arithmetic_series.py
+++ maths/sum_of_arithmetic_series.py
@@ -1,23 +1,13 @@
 # DarkCoder
-def sum_of_series(first_term, common_diff, num_of_terms):
-    """
-    Find the sum of n terms in an arithmetic progression.
-
-    >>> sum_of_series(1, 1, 10)
-    55.0
-    >>> sum_of_series(1, 10, 100)
-    49600.0
-    """
-    sum = (num_of_terms / 2) * (2 * first_term + (num_of_terms - 1) * common_diff)
-    # formula for sum of series
+def arith_sum(a,n,d):
+    an=a+(n-1)*d
+    if d==0:
+        sum=n*a
+    else:
+        sum=(n/2)*(a+an)
     return sum
-
-
-def main():
-    print(sum_of_series(1, 1, 10))
-
-
-if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod()
+start=int(input("enter the starting no."))
+no_of_terms=int(input("enter the no of terms"))
+common_diff=int(input("enter the common diff"))
+sum=arith_sum(start,no_of_terms,common_diff)
+print("arithmetic sum=",sum)
