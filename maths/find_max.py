8efac7fb04ac2f008a9acbfbcee9ef32dc42438a
Christian Clauss <cclauss@me.com>
2019-10-28 09:14:35 +0100

    Format with psf/black and add doctests

--- maths/find_max.py
+++ maths/find_max.py
@@ -1,5 +1,25 @@
-n = [34, 54, 56, 90]
-n.sort()
-n = set(n)
-n = list(n)
-print("Largest number : ", n[len(n)-2])
+# NguyenU
+
+
+def find_max(nums):
+    """
+    >>> for nums in ([3, 2, 1], [-3, -2, -1], [3, -3, 0], [3.0, 3.1, 2.9]):
+    ...     find_max(nums) == max(nums)
+    True
+    True
+    True
+    True
+    """
+    max = nums[0]
+    for x in nums:
+        if x > max:
+            max = x
+    return max
+
+
+def main():
+    print(find_max([2, 4, 9, 7, 19, 94, 5]))  # 94
+
+
+if __name__ == "__main__":
+    main()
