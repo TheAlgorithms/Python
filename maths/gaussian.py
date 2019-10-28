ccf59d18c931ea7c3852b2ef5dd8a4c875b146c0
= <=>
2019-10-28 15:52:25 +0530

    Fixed The Gaussian Function . Issue #1375

--- maths/gaussian.py
+++ maths/gaussian.py
@@ -50,7 +50,8 @@ def gaussian(x, mu: float = 0.0, sigma: float = 1.0) -> int:
     >>> gaussian(2523, mu=234234, sigma=3425)
     0.0
   """
-    return 1 / sqrt(2 * pi * sigma ** 2) * exp(-(x - mu) ** 2 / 2 * sigma ** 2)
+    b = float(x - mu)/sigma
+    return math.exp(-b*b/2.0) / math.sqrt(2.0*math.pi)/sigma
 
 
 if __name__ == "__main__":
