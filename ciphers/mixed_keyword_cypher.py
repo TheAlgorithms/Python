5685138f46a1fd90def4f57b92d7fa9565c0d249
Christian Clauss <cclauss@me.com>
2019-10-28 13:03:11 +0100

    Update mixed_keyword_cypher.py

--- ciphers/mixed_keyword_cypher.py
+++ ciphers/mixed_keyword_cypher.py
@@ -1,7 +1,4 @@
-key="college"
-pt="UNIVERSITY"
-
-def mixed_keyword(key,pt):
+def mixed_keyword(key="college", pt="UNIVERSITY"):
     """
 
     For key:hello
@@ -14,79 +11,61 @@ def mixed_keyword(key,pt):
     U V W X
     Y Z
     and map vertically
-    
-    
-    
-    
-    Enter key: COLLEGE 
-    Enter the plaintext:UNIVERSITY
-    {'A': 'C', 'B': 'A', 'C': 'I', 'D': 'P', 'E': 'U', 'F': 'Z', 'G': 'O', 'H': 'B', 'I': 'J', 'J': 'Q', 'K': 'V', 'L': 'L', 'M': 'D', 'N': 'K', 'O': 'R', 'P': 'W', 'Q': 'E', 'R': 'F', 'S': 'M', 'T': 'S', 'U': 'X', 'V': 'G', 'W': 'H', 'X': 'N', 'Y': 'T', 'Z': 'Y'}
-    XKJGUFMJST
-    """
-    key=key.upper()
-    pt=pt.upper()
-    l=len(key)
-    temp=[]
 
+    >>> mixed_keyword("college", "UNIVERSITY")  # doctest: +NORMALIZE_WHITESPACE
+    {'A': 'C', 'B': 'A', 'C': 'I', 'D': 'P', 'E': 'U', 'F': 'Z', 'G': 'O', 'H': 'B',
+     'I': 'J', 'J': 'Q', 'K': 'V', 'L': 'L', 'M': 'D', 'N': 'K', 'O': 'R', 'P': 'W',
+     'Q': 'E', 'R': 'F', 'S': 'M', 'T': 'S', 'U': 'X', 'V': 'G', 'W': 'H', 'X': 'N',
+     'Y': 'T', 'Z': 'Y'}
+    'XKJGUFMJST'
+    """
+    key = key.upper()
+    pt = pt.upper()
+    temp = []
     for i in key:
         if i not in temp:
             temp.append(i)
-
-    l=len(temp)
-    #print(temp)
-
-    alpha=[]
-    modalpha=[]
-    #modalpha.append(temp)
-
-    dic=dict()
-
-    c=0
-    for i in range(65,91):
-        t=chr(i)
+    l = len(temp)
+    # print(temp)
+    alpha = []
+    modalpha = []
+    # modalpha.append(temp)
+    dic = dict()
+    c = 0
+    for i in range(65, 91):
+        t = chr(i)
         alpha.append(t)
         if t not in temp:
             temp.append(t)
-
-    #print(temp)
-
-    r=int(26/4)
-    #print(r)
-
-    k=0;
+    # print(temp)
+    r = int(26 / 4)
+    # print(r)
+    k = 0
     for i in range(r):
-        t=[]
+        t = []
         for j in range(l):
             t.append(temp[k])
-            if not(k<25):
+            if not (k < 25):
                 break
-            k+=1
-            
+            k += 1
         modalpha.append(t)
-    #print(modalpha)
-    d=dict()
-
-    j=0
-    k=0
+    # print(modalpha)
+    d = dict()
+    j = 0
+    k = 0
     for j in range(l):
         for i in modalpha:
-            if not (len(i)-1>=j):
+            if not (len(i) - 1 >= j):
                 break
-            d[alpha[k]]=i[j]
-            if not k<25:
+            d[alpha[k]] = i[j]
+            if not k < 25:
                 break
-            k+=1
-            
-            
+            k += 1
     print(d)
-    cypher=""
-
+    cypher = ""
     for i in pt:
-            cypher+=d[i]
-            
-    return(cypher)
-
-print(mixed_keyword(key,pt))
+        cypher += d[i]
+    return cypher
 
 
-
+print(mixed_keyword("college", "UNIVERSITY"))
