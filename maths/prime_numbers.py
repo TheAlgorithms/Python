4add0813c56f7e67c882242b4fa5f54b2553120f
Christian Clauss <cclauss@me.com>
2019-10-29 21:28:33 +0100

    Update prime_numbers.py

--- maths/prime_numbers.py
+++ maths/prime_numbers.py
@@ -1,7 +1,18 @@
 """Prime numbers calculation."""
 
 
-def primes(max):
+def primes(max: int) -> int:
+    """
+    Return a list of all primes up to max.
+    >>> primes(10)
+    [2, 3, 5, 7]
+    >>> primes(11)
+    [2, 3, 5, 7, 11]
+    >>> primes(25)
+    [2, 3, 5, 7, 11, 13, 17, 19, 23]
+    >>> primes(1_000_000)[-1]
+    999983
+    """
     max += 1
     numbers = [False] * max
     ret = []
@@ -13,12 +24,5 @@ def primes(max):
     return ret
 
 
-
-def main():
-    print("Calculate primes up to:\n>> ", end="")
-    n = int(input())
-    print(primes(n))
-
-
 if __name__ == "__main__":
-    main()
+    print(primes(int(input("Calculate primes up to:\n>> "))))
