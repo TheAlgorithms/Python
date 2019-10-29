adc9a0cf432cd58d0f178e8f508e125f4b23cef2
Christian Clauss <cclauss@me.com>
2019-10-29 23:42:27 +0100

    Add a space

--- data_structures/heap/binomial_heap.py
+++ data_structures/heap/binomial_heap.py
@@ -75,7 +75,7 @@ class BinomialHeap:
     Deleting - delete() test
     >>> for i in range(25):
     ...     print(first_heap.deleteMin(), end=" ")
-    0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
+    0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 
 
     Create a new Heap
     >>> second_heap = BinomialHeap()
