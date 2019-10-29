4e4c4b8efc5f5cb52f9b1e329a76cd7cc5df64b4
Christian Clauss <cclauss@me.com>
2019-10-29 23:13:13 +0100

    Fix mypy error in perceptron.py

--- neural_network/perceptron.py
+++ neural_network/perceptron.py
@@ -95,8 +95,7 @@ class Perceptron:
         ...
         >>> perceptron.sort([-0.6508, 0.1097, 4.0009]) # doctest: +ELLIPSIS
         ('Sample: ', ...)
-        classification: P1
-
+        classification: P...
         """
         if len(self.sample) == 0:
             raise AttributeError("Sample data can not be empty")
