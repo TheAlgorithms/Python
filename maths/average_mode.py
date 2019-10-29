b4913b977615205be199121aa2a1a6b04c662667
Christian Clauss <cclauss@me.com>
2019-10-30 00:17:39 +0100

    Update average_mode.py

--- maths/average_mode.py
+++ maths/average_mode.py
@@ -1,22 +1,31 @@
-def mode(input_list):                    					#Defining function "mode."
-	"""
-	This function returns the mode(Mode as in the measures of central tendency) of the input data.
+import statistics
 
-	>>> input_list = [2,3,4,5,3,4,2,5,2,2,4,2,2,2]
-	>>> mode(input_list)
-	2
-	>>> import statistics
-	>>> mode(input_list) == statistics.mode(input_list)
+
+def mode(input_list):  # Defining function "mode."
+    """This function returns the mode(Mode as in the measures of
+    central tendency) of the input data.
+
+    The input list may contain any Datastructure or any Datatype.
+
+    >>> input_list = [2, 3, 4, 5, 3, 4, 2, 5, 2, 2, 4, 2, 2, 2]
+    >>> mode(input_list)
+    2
+    >>> input_list = [2, 3, 4, 5, 3, 4, 2, 5, 2, 2, 4, 2, 2, 2]
+    >>> mode(input_list) == statistics.mode(input_list)
 	True
-	
-	the input list may contain any Datastructure or any Datatype.
 	"""
-	check_list = input_list.copy()    					#Copying inputlist to check with the index number later.
-	result = list()								#Empty list to store the counts of elements in inputlist
-	for x in input_list:
-		result.append(input_list.count(x))		
-		input_list.remove(x)
-		y=max(result)							#Gets the maximum value in the result list.
-		return check_list[result.index(y)]				#Returns the value with the maximum number of repetitions.
-data=[1,2,4,3,1,6,4,2,5,1,1,2,3]
-print(mode(data))
+    # Copying inputlist to check with the index number later.
+    check_list = input_list[:]
+    result = list()  # Empty list to store the counts of elements in input_list
+    for x in input_list:
+        result.append(input_list.count(x))
+        input_list.remove(x)
+        y = max(result)  # Gets the maximum value in the result list.
+        # Returns the value with the maximum number of repetitions.
+        return check_list[result.index(y)]
+
+
+if __name__ == "__main__":
+    data = [2, 3, 4, 5, 3, 4, 2, 5, 2, 2, 4, 2, 2, 2]
+    print(mode(data))
+    print(statistics.mode(data))
