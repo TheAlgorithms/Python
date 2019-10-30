d4b560da864cac20ba378ac10805f62f7f250069
Sri Suma <41474384+srisumapasumarthi@users.noreply.github.com>
2019-10-30 16:31:55 +0530

    Update sdes.py

--- other/sdes.py
+++ other/sdes.py
@@ -5,11 +5,7 @@ def apply_table(inp, table):
     return res
 
 def left_shift(data):
-    li = []
-    for i in range(len(data)-1):
-        li.append(data[i+1])
-    li.append(data[0])
-    return ''.join(li)
+    return data[1:]+data[0]
 
 def XOR(a, b):
     res = ''
@@ -38,49 +34,51 @@ def function(expansion, s0, s1, key, message):
     temp = XOR(left, temp)
     return temp+right
     
+if __name__ == "__main__":
 
-key = input('Enter 10 bit key: ')
-message = input('Enter 8 bit message: ')
+    key = input('Enter 10 bit key: ')
+    message = input('Enter 8 bit message: ')
 
-p8_table = [6, 3, 7, 4, 8, 5, 10, 9]
-p10_table = [3, 5, 2, 7, 4, 10, 1, 9, 8, 6]
-p4_table = [2, 4, 3, 1]
-IP = [2, 6, 3, 1, 4, 8, 5, 7]
-IP_inv = [4, 1, 3, 5, 7, 2, 8, 6]
-expansion = [4, 1, 2, 3, 2, 3, 4, 1]
-s0 = [[1, 0, 3, 2],
-      [3, 2, 1, 0],
-      [0, 2, 1, 3],
-      [3, 1, 3, 2]]
-s1 = [[0, 1, 2, 3],
-      [2, 0, 1, 3],
-      [3, 0, 1, 0],
-      [2, 1, 0, 3]]
+    p8_table = [6, 3, 7, 4, 8, 5, 10, 9]
+    p10_table = [3, 5, 2, 7, 4, 10, 1, 9, 8, 6]
+    p4_table = [2, 4, 3, 1]
+    IP = [2, 6, 3, 1, 4, 8, 5, 7]
+    IP_inv = [4, 1, 3, 5, 7, 2, 8, 6]
+    expansion = [4, 1, 2, 3, 2, 3, 4, 1]
+    s0 = [[1, 0, 3, 2],
+          [3, 2, 1, 0],
+          [0, 2, 1, 3],
+          [3, 1, 3, 2]]
+    s1 = [[0, 1, 2, 3],
+          [2, 0, 1, 3],
+          [3, 0, 1, 0],
+          [2, 1, 0, 3]]
 
-#encryption
-temp = apply_table(key, p10_table)
-left = temp[:5]
-right = temp[5:]
-left = left_shift(left)
-right = left_shift(right)
-key1 = apply_table(left+right, p8_table)
-left = left_shift(left)
-right = left_shift(right)
-left = left_shift(left)
-right = left_shift(right)
-key2 = apply_table(left+right, p8_table)
+    #key generation
+    temp = apply_table(key, p10_table)
+    left = temp[:5]
+    right = temp[5:]
+    left = left_shift(left)
+    right = left_shift(right)
+    key1 = apply_table(left+right, p8_table)
+    left = left_shift(left)
+    right = left_shift(right)
+    left = left_shift(left)
+    right = left_shift(right)
+    key2 = apply_table(left+right, p8_table)
 
-temp = apply_table(message, IP)
-temp = function(expansion, s0, s1, key1, temp)
-temp = temp[4:]+temp[:4]
-temp = function(expansion, s0, s1, key2, temp)
-CT = apply_table(temp, IP_inv)
-print('Cipher text is: ', CT)
+    #encryption
+    temp = apply_table(message, IP)
+    temp = function(expansion, s0, s1, key1, temp)
+    temp = temp[4:]+temp[:4]
+    temp = function(expansion, s0, s1, key2, temp)
+    CT = apply_table(temp, IP_inv)
+    print('Cipher text is: ', CT)
 
-#decryption
-temp = apply_table(CT, IP)
-temp = function(expansion, s0, s1, key2, temp)
-temp = temp[4:]+temp[:4]
-temp = function(expansion, s0, s1, key1, temp)
-PT = apply_table(temp, IP_inv)
-print('Plain text after decypting is: ', PT)
+    #decryption
+    temp = apply_table(CT, IP)
+    temp = function(expansion, s0, s1, key2, temp)
+    temp = temp[4:]+temp[:4]
+    temp = function(expansion, s0, s1, key1, temp)
+    PT = apply_table(temp, IP_inv)
+    print('Plain text after decypting is: ', PT)
