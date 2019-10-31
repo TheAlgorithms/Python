785663f96994905b16c91a04565941cfe1fe0038
Christian Clauss <cclauss@me.com>
2019-10-31 09:44:22 +0100

    Added doctests, type hints, f-strings, URLs

--- graphs/knapsack.py
+++ graphs/knapsack.py
@@ -1,33 +1,60 @@
-def fractional_knapsack(value, weight, capacity):
+# https://en.wikipedia.org/wiki/Continuous_knapsack_problem
+# https://www.guru99.com/fractional-knapsack-problem-greedy.html
+# https://medium.com/walkinthecode/greedy-algorithm-fractional-knapsack-problem-9aba1daecc93
 
+from typing import List, Tuple
+
+
+def fractional_knapsack(
+    value: List[int], weight: List[int], capacity: int
+) -> Tuple[int, List[int]]:
+    """
+    >>> value = [1, 3, 5, 7, 9]
+    >>> weight = [0.9, 0.7, 0.5, 0.3, 0.1]
+    >>> fractional_knapsack(value, weight, 5)
+    (25, [1, 1, 1, 1, 1])
+    >>> fractional_knapsack(value, weight, 15)
+    (25, [1, 1, 1, 1, 1])
+    >>> fractional_knapsack(value, weight, 25)
+    (25, [1, 1, 1, 1, 1])
+    >>> fractional_knapsack(value, weight, 26)
+    (25, [1, 1, 1, 1, 1])
+    >>> fractional_knapsack(value, weight, -1)
+    (-90.0, [0, 0, 0, 0, -10.0])
+    >>> fractional_knapsack([1, 3, 5, 7], weight, 30)
+    (16, [1, 1, 1, 1])
+    >>> fractional_knapsack(value, [0.9, 0.7, 0.5, 0.3, 0.1], 30)
+    (25, [1, 1, 1, 1, 1])
+    >>> fractional_knapsack([], [], 30)
+    (0, [])
+    """
     index = list(range(len(value)))
-    ratio = [v/w for v, w in zip(value, weight)]
+    ratio = [v / w for v, w in zip(value, weight)]
     index.sort(key=lambda i: ratio[i], reverse=True)
 
     max_value = 0
-    fractions = [0]*len(value)
+    fractions = [0] * len(value)
     for i in index:
         if weight[i] <= capacity:
             fractions[i] = 1
             max_value += value[i]
             capacity -= weight[i]
         else:
-            fractions[i] = capacity/weight[i]
-            max_value += value[i]*capacity/weight[i]
+            fractions[i] = capacity / weight[i]
+            max_value += value[i] * capacity / weight[i]
             break
 
     return max_value, fractions
 
 
-n = int(input('Enter number of items: '))
-value = input('Enter the values of the {} item(s) in order: '
-              .format(n)).split()
-value = [int(v) for v in value]
-weight = input('Enter the positive weights of the {} item(s) in order: '
-               .format(n)).split()
-weight = [int(w) for w in weight]
-capacity = int(input('Enter maximum weight: '))
+if __name__ == "__main__":
+    n = int(input("Enter number of items: "))
+    value = input(f"Enter the values of the {n} item(s) in order: ").split()
+    value = [int(v) for v in value]
+    weight = input(f"Enter the positive weights of the {n} item(s) in order: ".split())
+    weight = [int(w) for w in weight]
+    capacity = int(input("Enter maximum weight: "))
 
-max_value, fractions = fractional_knapsack(value, weight, capacity)
-print('The maximum value of items that can be carried:', max_value)
-print('The fractions in which the items should be taken:', fractions)
+    max_value, fractions = fractional_knapsack(value, weight, capacity)
+    print("The maximum value of items that can be carried:", max_value)
+    print("The fractions in which the items should be taken:", fractions)
