5de2ec0c59a774cd041e5e89fa4f09321e9ff41c
Marvin M. Michum <mrvnmchm@gmail.com>
2019-11-02 00:45:50 -0400

    add porta cipher for #1492

---
+++ ciphers/porta_cipher.py
@@ -0,0 +1,113 @@
+alphabet = {
+    "A": [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"],
+          ["N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]], "B": [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"], ["N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]],
+    "C": [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"],
+          ["Z", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y"]], "D": [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"], ["Z", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y"]],
+    "E": [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"],
+          ["Y", "Z", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X"]], "F": [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"], ["Y", "Z", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X"]],
+    "G": [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"],
+          ["X", "Y", "Z", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W"]], "H": [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"], ["X", "Y", "Z", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W"]],
+    "I": [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"],
+          ["W", "X", "Y", "Z", "N", "O", "P", "Q", "R", "S", "T", "U", "V"]], "J": [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"], ["W", "X", "Y", "Z", "N", "O", "P", "Q", "R", "S", "T", "U", "V"]],
+    "K": [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"],
+          ["V", "W", "X", "Y", "Z", "N", "O", "P", "Q", "R", "S", "T", "U"]], "L": [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"], ["V", "W", "X", "Y", "Z", "N", "O", "P", "Q", "R", "S", "T", "U"]],
+    "M": [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"],
+          ["U", "V", "W", "X", "Y", "Z", "N", "O", "P", "Q", "R", "S", "T"]], "N": [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"], ["U", "V", "W", "X", "Y", "Z", "N", "O", "P", "Q", "R", "S", "T"]],
+    "O": [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"],
+          ["T", "U", "V", "W", "X", "Y", "Z", "N", "O", "P", "Q", "R", "S"]], "P": [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"], ["T", "U", "V", "W", "X", "Y", "Z", "N", "O", "P", "Q", "R", "S"]],
+    "Q": [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"],
+          ["S", "T", "U", "V", "W", "X", "Y", "Z", "N", "O", "P", "Q", "R"]], "R": [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"], ["S", "T", "U", "V", "W", "X", "Y", "Z", "N", "O", "P", "Q", "R"]],
+    "S": [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"],
+          ["R", "S", "T", "U", "V", "W", "X", "Y", "Z", "N", "O", "P", "Q"]], "T": [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"], ["R", "S", "T", "U", "V", "W", "X", "Y", "Z", "N", "O", "P", "Q"]],
+    "U": [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"],
+          ["Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "N", "O", "P"]], "V": [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"], ["Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "N", "O", "P"]],
+    "W": [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"],
+          ["P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "N", "O"]], "X": [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"], ["P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "N", "O"]],
+    "Y": [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"],
+          ["O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "N"]], "Z": [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"], ["O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "N"]]}
+
+
+def generateTable(key):
+    """
+    >>> generateTable('marvin')
+    [[['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'], ['U', 'V', 'W', 'X', 'Y', 'Z', 'N', 'O', 'P', 'Q', 'R', 'S', 'T']], [['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'], ['N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']], [['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'], ['S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'N', 'O', 'P', 'Q', 'R']], [['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'], ['Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'N', 'O', 'P']], [['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'], ['W', 'X', 'Y', 'Z', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V']], [['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'], ['U', 'V', 'W', 'X', 'Y', 'Z', 'N', 'O', 'P', 'Q', 'R', 'S', 'T']]]
+    """
+    table = []
+    for char in key.upper():
+        table.append(alphabet[char])
+    return table
+
+
+def encrypt(key, words):
+    """
+    >>> encrypt('marvin', 'jessica')
+    'QRACRWU'
+    """
+    cipher = ""
+    count = 0
+    table = generateTable(key)
+    for char in words.upper():
+        cipher += getOpponent(table[count], char)
+        count = (count + 1) % len(table)
+    return cipher
+
+
+def decrypt(key, words):
+    """
+    >>> decrypt('marvin', 'QRACRWU')
+    'JESSICA'
+    """
+    return encrypt(key, words)
+
+
+def getPosition(table, char):
+    """
+    >>> table = [[['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'], ['U', 'V', 'W', 'X', 'Y', 'Z', 'N', 'O', 'P', 'Q', 'R', 'S', 'T']], [['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'], ['N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']], [['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'], ['S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'N', 'O', 'P', 'Q', 'R']], [['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'], ['Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'N', 'O', 'P']], [['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'], ['W', 'X', 'Y', 'Z', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V']], [['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'], ['U', 'V', 'W', 'X', 'Y', 'Z', 'N', 'O', 'P', 'Q', 'R', 'S', 'T']]]
+    >>> getPosition(table, 'A')
+    (None, None)
+    """
+    row = -1
+
+    if char in table[0]:
+        row = 0
+    elif char in table[1]:
+        row = 1
+
+    if row != -1:
+        return (row, table[row].index(char))
+    else:
+        return (None, None)
+
+
+def getOpponent(table, char):
+    """
+    >>> table = [[['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'], ['U', 'V', 'W', 'X', 'Y', 'Z', 'N', 'O', 'P', 'Q', 'R', 'S', 'T']], [['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'], ['N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']], [['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'], ['S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'N', 'O', 'P', 'Q', 'R']], [['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'], ['Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'N', 'O', 'P']], [['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'], ['W', 'X', 'Y', 'Z', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V']], [['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'], ['U', 'V', 'W', 'X', 'Y', 'Z', 'N', 'O', 'P', 'Q', 'R', 'S', 'T']]]
+    >>> getOpponent(table, 'A')
+    'A'
+    """
+    row, col = getPosition(table, char.upper())
+
+    if row == 1:
+        return table[0][col]
+    elif row == 0:
+        return table[1][col]
+    else:
+        return char
+
+
+if __name__ == "__main__":
+    """
+    ENTER KEY: marvin
+    ENTER TEXT TO ENCRYPT: jessica
+    ENCRYPTED: QRACRWU
+    DECRYPTED WITH KEY: JESSICA
+    """
+    key = input("ENTER KEY: ")
+    text = input("ENTER TEXT TO ENCRYPT: ")
+    cipher_text = encrypt(key, text)
+
+    print("ENCRYPTED: " + cipher_text)
+    print("DECRYPTED WITH KEY: " + decrypt(key, cipher_text))
+
+    import doctest
+    doctest.testmod()
