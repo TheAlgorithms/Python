81f7d9af81c53a73ce437cbfbcda9bd4f9cbef63
John Law <johnlaw.po@gmail.com>
2019-11-05 00:20:33 +0800

    Format longest_increasing_subsequence.py to PEP8

--- dynamic_programming/longest_increasing_subsequence.py
+++ dynamic_programming/longest_increasing_subsequence.py
@@ -4,50 +4,50 @@ Author  : Mehdi ALAOUI
 This is a pure Python implementation of Dynamic Programming solution to the longest increasing subsequence of a given sequence.
 
 The problem is  :
-Given an ARRAY, to find the longest and increasing sub ARRAY in that given ARRAY and return it.
+Given an array, to find the longest and increasing sub ARRAY in that given array and return it.
 Example: [10, 22, 9, 33, 21, 50, 41, 60, 80] as input will return [10, 22, 33, 41, 60, 80] as output
 """
 from typing import List
 
 
-def longestSub(ARRAY: List[int]) -> List[int]:  # This function is recursive
+def longest_subsequence(Array: List[int]) -> List[int]:  # This function is recursive
     """
     Some examples
-    >>> longestSub([10, 22, 9, 33, 21, 50, 41, 60, 80])
+    >>> longest_subsequence([10, 22, 9, 33, 21, 50, 41, 60, 80])
     [10, 22, 33, 41, 60, 80]
-    >>> longestSub([4, 8, 7, 5, 1, 12, 2, 3, 9])
+    >>> longest_subsequence([4, 8, 7, 5, 1, 12, 2, 3, 9])
     [1, 2, 3, 9]
-    >>> longestSub([9, 8, 7, 6, 5, 7])
+    >>> longest_subsequence([9, 8, 7, 6, 5, 7])
     [8]
-    >>> longestSub([1, 1, 1])
+    >>> longest_subsequence([1, 1, 1])
     [1, 1, 1]
     """
-    ARRAY_LENGTH = len(ARRAY)
+    ArrayLength = len(Array)
     if (
-        ARRAY_LENGTH <= 1
+        ArrayLength <= 1
     ):  # If the array contains only one element, we return it (it's the stop condition of recursion)
-        return ARRAY
+        return Array
         # Else
-    PIVOT = ARRAY[0]
+    Pivot = Array[0]
     isFound = False
     i = 1
-    LONGEST_SUB = []
-    while not isFound and i < ARRAY_LENGTH:
-        if ARRAY[i] < PIVOT:
+    LongestSubseq = []
+    while not isFound and i < ArrayLength:
+        if Array[i] < Pivot:
             isFound = True
-            TEMPORARY_ARRAY = [element for element in ARRAY[i:] if element >= ARRAY[i]]
-            TEMPORARY_ARRAY = longestSub(TEMPORARY_ARRAY)
-            if len(TEMPORARY_ARRAY) > len(LONGEST_SUB):
-                LONGEST_SUB = TEMPORARY_ARRAY
+            TempArray = [element for element in Array[i:] if element >= Array[i]]
+            TempArray = longest_subsequence(TempArray)
+            if len(TempArray) > len(LongestSubseq):
+                LongestSubseq = TempArray
         else:
             i += 1
 
-    TEMPORARY_ARRAY = [element for element in ARRAY[1:] if element >= PIVOT]
-    TEMPORARY_ARRAY = [PIVOT] + longestSub(TEMPORARY_ARRAY)
-    if len(TEMPORARY_ARRAY) > len(LONGEST_SUB):
-        return TEMPORARY_ARRAY
+    TempArray = [element for element in Array[1:] if element >= Pivot]
+    TempArray = [Pivot] + longest_subsequence(TempArray)
+    if len(TempArray) > len(LongestSubseq):
+        return TempArray
     else:
-        return LONGEST_SUB
+        return LongestSubseq
     
 
 if __name__ == "__main__":
