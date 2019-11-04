92f624c9f952e17bad864545132e6b38d205d626
shellhub <shellhub.me@gmail.com>
2019-11-04 15:20:18 +0800

    ceil and floor

--- maths/ceil.py
+++ maths/ceil.py
@@ -6,19 +6,7 @@ def ceil(x) -> int:
     :return: the smallest integer >= x.
 
     >>> import math
-    >>> ceil(1) == math.ceil(1)
-    True
-    >>> ceil(-1) == math.ceil(-1)
-    True
-    >>> ceil(0) == math.ceil(0)
-    True
-    >>> ceil(-0) == math.ceil(-0)
-    True
-    >>> ceil(1.1) == math.ceil(1.1)
-    True
-    >>> ceil(-1.1) == math.ceil(-1.1)
-    True
-    >>> ceil(-1.0) == math.ceil(-1.0)
+    >>> all(ceil(n) == math.ceil(n) for n in (1, -1, 0, -0, 1.1, -1.1, 1.0, -1.0, 1_000_000_000))
     True
     """
     return x if isinstance(x, int) or x - int(x) == 0 else int(x + 1) if x > 0 else int(x)
--- maths/floor.py
+++ maths/floor.py
@@ -6,19 +6,7 @@ def floor(x) -> int:
     :return: the largest integer <= x.
 
     >>> import math
-    >>> floor(1) == math.floor(1)
-    True
-    >>> floor(-1) == math.floor(-1)
-    True
-    >>> floor(0) == math.floor(0)
-    True
-    >>> floor(-0) == math.floor(-0)
-    True
-    >>> floor(1.1) == math.floor(1.1)
-    True
-    >>> floor(-1.1) == math.floor(-1.1)
-    True
-    >>> floor(-1.0) == math.floor(-1.0)
+    >>> all(floor(n) == math.floor(n) for n in (1, -1, 0, -0, 1.1, -1.1, 1.0, -1.0, 1_000_000_000))
     True
     """
     return x if isinstance(x, int) or x - int(x) == 0 else int(x) if x > 0 else int(x - 1)
