82a11d7f31d06f781e3902984fa2902c0676816a
JakobZhao <52325554+JakobMusik@users.noreply.github.com>
2019-11-10 17:01:38 +0800

    Fix bug in bellman_ford.py (#1544)

--- graphs/bellman_ford.py
+++ graphs/bellman_ford.py
@@ -13,14 +13,14 @@ def BellmanFord(graph, V, E, src):
     mdist[src] = 0.0
 
     for i in range(V - 1):
-        for j in range(V):
+        for j in range(E):
             u = graph[j]["src"]
             v = graph[j]["dst"]
             w = graph[j]["weight"]
 
             if mdist[u] != float("inf") and mdist[u] + w < mdist[v]:
                 mdist[v] = mdist[u] + w
-    for j in range(V):
+    for j in range(E):
         u = graph[j]["src"]
         v = graph[j]["dst"]
         w = graph[j]["weight"]
