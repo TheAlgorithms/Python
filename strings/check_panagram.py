88f41b402e762f634d58c8199f6908d9f25d22de
Sarath Kaul <sarath.kaul@searce.com>
2019-11-12 18:38:36 +0530

    Panagram Script Added

--- strings/check_panagram.py
+++ strings/check_panagram.py
@@ -1,30 +1,24 @@
 # Created by sarathkaul on 12/11/19
 
 
-def check_panagram(
-    input_str: str = "The quick brown fox jumps over the lazy dog",
-) -> bool:
+def check_panagram(input_str: str) -> None:
     """
     A Panagram String contains all the alphabets at least once.
     >>> check_panagram("The quick brown fox jumps over the lazy dog")
-    True
+    The quick brown fox jumps over the lazy dog is a Panagram String
     >>> check_panagram("My name is Unknown")
-    False
-    >>> check_panagram("The quick brown fox jumps over the la_y dog")
-    False
+    My name is Unknown is not a Panagram String
     """
     frequency = set()
-    input_str = input_str.replace(
-        " ", ""
-    )  # Replacing all the Whitespaces in our sentence
-    for alpha in input_str:
-        if "a" <= alpha.lower() <= "z":
-            frequency.add(alpha.lower())
 
-    return True if len(frequency) == 26 else False
+    for alpha in input_str:
+        if alpha != " ":
+            frequency.add(alpha.lower())
+    if len(frequency) == 26:
+        print(f"{input_str} is a Panagram String")
+    else:
+        print(f"{input_str} is not a Panagram String")
 
 
 if __name__ == "main":
-    check_str = "INPUT STRING"
-    status = check_panagram(check_str)
-    print(f"{check_str} is {'not ' if status else ''}a panagram string")
+    check_panagram("INPUT STRING")
