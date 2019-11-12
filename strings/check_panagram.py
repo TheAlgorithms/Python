6baab22759d9b9d795926d26d7d47ff932fbe8e6
Sarath Kaul <sarath.kaul@searce.com>
2019-11-12 22:05:12 +0530

    Anagram Script Changes

--- strings/check_panagram.py
+++ strings/check_panagram.py
@@ -1,24 +1,27 @@
 # Created by sarathkaul on 12/11/19
 
 
-def check_panagram(input_str: str) -> None:
+def check_panagram(
+    input_str: str = "The quick brown fox jumps over the lazy dog",
+) -> bool:
     """
     A Panagram String contains all the alphabets at least once.
     >>> check_panagram("The quick brown fox jumps over the lazy dog")
-    The quick brown fox jumps over the lazy dog is a Panagram String
+    True
     >>> check_panagram("My name is Unknown")
-    My name is Unknown is not a Panagram String
+    False
     """
     frequency = set()
-
+    input_str = input_str.replace(
+        " ", ""
+    )  # Replacing all the Whitespaces in our sentence
     for alpha in input_str:
-        if alpha != " ":
-            frequency.add(alpha.lower())
-    if len(frequency) == 26:
-        print(f"{input_str} is a Panagram String")
-    else:
-        print(f"{input_str} is not a Panagram String")
+        frequency.add(alpha.lower())
+
+    return True if len(frequency) == 26 else False
 
 
 if __name__ == "main":
-    check_panagram("INPUT STRING")
+    check_str = "INPUT STRING"
+    status = check_panagram(check_str)
+    print(f"{check_str} is {'not ' if status else ''}a panagram string")
