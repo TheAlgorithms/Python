8d74643237063b66764fe9b826a0cd605f84f6a3
Sarath Kaul <sarath.kaul@searce.com>
2019-11-12 15:32:17 +0530

    Slack Message Revision Changes

--- web_programming/slack_message.py
+++ web_programming/slack_message.py
@@ -1,17 +1,15 @@
 # Created by sarathkaul on 12/11/19
 
-import json
 import requests
 
 
-def send_slack_message(message_body, slack_url):
-    slack_data = {"text": message_body}
+def send_slack_message(message_body: str, slack_url: str) -> None:
     headers = {"Content-Type": "application/json"}
-    response = requests.post(slack_url, data=json.dumps(slack_data), headers=headers)
+    response = requests.post(slack_url, json={"text": message_body}, headers=headers)
     if response.status_code != 200:
         raise ValueError(
-            "Request to slack returned an error %s, the response is:\n%s"
-            % (response.status_code, response.text)
+            f"Request to slack returned an error {response.status_code}, "
+            f"the response is:\n{response.text}"
         )
 
 
