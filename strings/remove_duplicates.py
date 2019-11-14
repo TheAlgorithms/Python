ac9c09fd2ec22d03734a787088bef9f7052e0949
Sarath Kaul <sarath.kaul@searce.com>
2019-11-14 16:58:16 +0530

    Remove Duplicates Updated

--- strings/remove_duplicates.py
+++ strings/remove_duplicates.py
@@ -1,22 +1,20 @@
 # Created by sarathkaul on 14/11/19
 
-from ordered_set import OrderedSet
-
 
 def remove_duplicates(sentence: str) -> str:
     """
     Reomove duplicates from sentence
     >>> remove_duplicates("Python is great and Java is also great")
-    'Python is great and Java also'
+    'Java Python also and great is'
     """
     sen_list = sentence.split(" ")
-    check = OrderedSet()
+    check = set()
 
     for a_word in sen_list:
         check.add(a_word)
 
-    return " ".join(check)
+    return " ".join(sorted(check))
 
 
 if __name__ == "__main__":
-    print(remove_duplicates("INPUT_SENTENCE"))
+    print(remove_duplicates("INPUT_STRING"))
