059c9d793bb633f6e518ff02cb284b1bee0bc659
Fanzil <fan.hakimov2010@yandex.ru>
2019-11-14 13:44:13 +0500

    Create tests running script

---
+++ data_structures/linked_list/run_tests.py
@@ -0,0 +1,6 @@
+from tests.test_doubly_linked_list import TestDList
+from tests.test_singly_linked_list import TestList
+
+if __name__ == "__main__":
+    TestList.run_test()
+    TestDList.run_test()
