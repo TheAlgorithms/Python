568e80888279d40c9354a5207dca04e85e345420
Himanshu Bhatnagar <33115688+Himan10@users.noreply.github.com>
2019-11-16 19:34:09 +0530

    Create circular_queue.py
    Circular Queue implementation using python list with fixed range.

---
+++ data_structures/queue/circular_queue.py
@@ -0,0 +1,41 @@
+# Implementation of Circular Queue (using python lists)
+
+class CircularQueue:
+    """ Circular queue with fixed capacity """
+
+    def __init__(self, n):
+        self.n = n
+        self.array = [None]*self.n
+        self.front = 0              # For first ele. Index
+        self.rear = 0
+        self.size = 0
+
+    def __len__(self):
+        return self.size
+
+    def is_empty(self):
+        return self.size == 0
+
+    def first(self):
+        if self.is_empty():
+            return False
+        return self.array[self.front]
+
+    def enqueue(self, data):
+        if self.size >= self.n:
+            raise "QUEUE IS FULL"
+
+        self.array[self.rear] = data
+        self.rear = (self.rear+1)%self.n
+        self.size += 1
+        return self
+
+    def dequeue(self):
+        if self.size == 0:
+            raise "UNDERFLOW"
+
+        temp = self.array[self.front]
+        self.array[self.front] = None
+        self.front = (self.front + 1)%self.n
+        self.size -= 1
+        return temp
