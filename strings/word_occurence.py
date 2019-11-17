fa0f04c830c6fb5b332d36ea1c6bc31b77c65886
Sarath Kaul <sarath.kaul@searce.com>
2019-11-17 14:08:36 +0530

    Word Occurence Script Added

---
+++ strings/word_occurence.py
@@ -0,0 +1,20 @@
+# Created by sarathkaul on 17/11/19
+
+
+from collections import defaultdict
+
+
+def word_occurence(sentence: str) -> dict:
+    occurence = defaultdict(int)
+
+    sentence = sentence.split(" ")
+    # Creating a dictionary containing count of each word
+    for a_word in sentence:
+        occurence[a_word] += 1
+    return occurence
+
+
+if __name__ == "__main__":
+    count = word_occurence("INPUT STRING")
+    for word, count in count.items():
+        print(f"{word}: {count}")
