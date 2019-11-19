67b2c641cbb56f868bddb3b7a3f25d6bc3485344
onlinejudge95 <onlinejudge95@gmail.com>
2019-11-19 06:57:58 +0530

    Adds, delete from front of the list

--- data_structures/linked_list/circular_linked_list.py
+++ data_structures/linked_list/circular_linked_list.py
@@ -77,9 +77,29 @@ class CircularLinkedList:
 
         self.length += 1
 
+    def delete_front(self):
+        if self.head is None:
+            raise IndexError()
+
+        current_node = self.head
+
+        if current_node.get_next() == current_node:
+            self.head, self.length = None, 0
+        else:
+            while current_node.get_next() != self.head:
+                current_node = current_node.get_next()
+
+            current_node.set_next(self.head.get_next())
+            self.head = self.head.get_next()
+
+        self.length -= 1
+
 
 if __name__ == "__main__":
     temp = CircularLinkedList()
-    for i in range(10):
+    for i in range(1):
         temp.prepend(i)
+    print(temp)
+
+    temp.delete_front()
     print(temp)
