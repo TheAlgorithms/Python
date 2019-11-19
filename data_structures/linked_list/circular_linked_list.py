53d6ba96cc263698499b1367ba317728c2ac5608
onlinejudge95 <onlinejudge95@gmail.com>
2019-11-19 07:17:13 +0530

    Adds, method documentations

--- data_structures/linked_list/circular_linked_list.py
+++ data_structures/linked_list/circular_linked_list.py
@@ -1,33 +1,70 @@
 class Node:
+    """
+    Class to represent a single node.
+
+    Each node has following attributes
+    * data
+    * next_ptr
+    """
+
     def __init__(self, data):
         self.data = data
         self.next_ptr = None
 
     def set_data(self, value):
+        """
+        Set the data field of the node to given value.
+        """
         self.data = value
 
     def get_data(self):
+        """
+        Returns the data field of the current node,
+        """
         return self.data
 
     def set_next(self, value):
+        """
+        Sets the next pointer of the current node.
+        """
         self.next_ptr = value
 
     def get_next(self):
+        """
+        Returns the next pointer of the current node.
+        """
         return self.next_ptr
 
     def has_next(self):
+        """
+        Checks if the current node has a valid next node.
+        """
         return self.next_ptr is not None
 
 
 class CircularLinkedList:
+    """
+    Class to represent the CircularLinkedList.
+
+    CircularLinkedList has following attributes.
+    * HEAD
+    * length
+    """
+
     def __init__(self):
         self.head = None
         self.length = 0
 
     def __len__(self):
+        """
+        Dunder method to return length of the CircularLinkedList
+        """
         return self.length
 
     def __str__(self):
+        """
+        Dunder method to represent the string representation of the CircularLinkedList
+        """
         current_node = self.head
         if not current_node:
             return "Empty linked list"
@@ -42,6 +79,9 @@ class CircularLinkedList:
         return " => ".join(result)
 
     def append(self, data):
+        """
+        Adds a node with given data to the end of the CircularLinkedList
+        """
         current_node = self.head
 
         new_node = Node(data)
@@ -59,6 +99,9 @@ class CircularLinkedList:
         self.length += 1
 
     def prepend(self, data):
+        """
+        Adds a ndoe with given data to the front of the CircularLinkedList
+        """
         current_node = self.head
 
         new_node = Node(data)
@@ -78,6 +121,9 @@ class CircularLinkedList:
         self.length += 1
 
     def delete_front(self):
+        """
+        Removes the 1st node from the CircularLinkedList
+        """
         if self.head is None:
             raise IndexError()
 
@@ -95,6 +141,9 @@ class CircularLinkedList:
         self.length -= 1
 
     def delete_rear(self):
+        """
+        Removes the last node from the CircularLinkedList
+        """
         if self.head is None:
             raise IndexError()
 
@@ -110,13 +159,3 @@ class CircularLinkedList:
             temp_node.set_next(current_node.get_next())
 
         self.length -= 1
-
-
-if __name__ == "__main__":
-    temp = CircularLinkedList()
-    for i in range(10):
-        temp.prepend(i)
-    print(temp)
-
-    temp.delete_rear()
-    print(temp)
