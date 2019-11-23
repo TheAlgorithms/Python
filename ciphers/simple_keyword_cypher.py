c5f5ae43056a80efa03de00224cc89156d4b26a1
Christian Clauss <cclauss@me.com>
2019-11-23 16:51:10 +0100

    Update simple_keyword_cypher.py

--- ciphers/simple_keyword_cypher.py
+++ ciphers/simple_keyword_cypher.py
@@ -1,17 +1,18 @@
-def main():
+def remove_duplicates(key: str) -> str:
     """
-    Handles I/O
-    :return: void
+    Removes duplicate alphabetic characters in a keyword (letter is ignored after its
+        first appearance).
+    :param key: Keyword to use
+    :return: String with duplicates removed
+    >>> remove_duplicates('Hello World!!')
+    'Helo Wrd'
     """
-    message = input("Enter message to encode or decode: ").strip().upper()
-    key = input("Enter keyword: ").strip().upper()
-    option = input("Encipher or decipher? E/D:").strip()[0].lower()
-    try:
-        func = {"e": encipher, "d": decipher}[option]
-    except KeyError:
-        raise KeyError("invalid input option")
-    cipher_map = create_cipher_map(key)
-    print(func(message, cipher_map))
+
+    key_no_dups = ""
+    for ch in key:
+        if ch == " " or ch not in key_no_dups and ch.isalpha():
+            key_no_dups += ch
+    return key_no_dups
 
 
 def create_cipher_map(key: str) -> dict:
@@ -20,18 +21,16 @@ def create_cipher_map(key: str) -> dict:
     :param key: keyword to use
     :return: dictionary cipher map
     """
-    cipher_alphabet = {}
     # Create alphabet list
     alphabet = [chr(i + 65) for i in range(26)]
     # Remove duplicate characters from key
-    key = remove_duplicates(key)
+    key = remove_duplicates(key.upper())
     offset = len(key)
     # First fill cipher with key characters
-    for i, char in enumerate(key):
-        cipher_alphabet[alphabet[i]] = char
+    cipher_alphabet = {alphabet[i]: char for i, char in enumerate(key)}
     # Then map remaining characters in alphabet to
     # the alphabet from the beginning
-    for i in range(len(cipher_alphabet.keys()), 26):
+    for i in range(len(cipher_alphabet), 26):
         char = alphabet[i - offset]
         # Ensure we are not mapping letters to letters previously mapped
         while char in key:
@@ -41,35 +40,16 @@ def create_cipher_map(key: str) -> dict:
     return cipher_alphabet
 
 
-def remove_duplicates(key: str) -> str:
-    """
-    Removes duplicate alphabetic characters in a keyword (removed after first appearance).
-    :param key: Keyword to use
-    :return: String with duplicates removed
-    >>> remove_duplicates('Hello World!!')
-    'Helo Wrd'
-    """
-
-    key_no_dups = ""
-    for ch in key:
-        if (ch not in key_no_dups and ch.isalpha()) or ch == " ":
-            key_no_dups += ch
-    return key_no_dups
-
-
 def encipher(message: str, cipher_map: dict) -> str:
     """
     Enciphers a message given a cipher map.
     :param message: Message to encipher
     :param cipher_map: Cipher map
     :return: enciphered string
-    >>> encipher('HELLO WORLD!!', create_cipher_map('GOODBYE!!'))
+    >>> encipher('Hello World!!', create_cipher_map('Goodbye!!'))
     'CYJJM VMQJB!!'
     """
-    enciphered_message = ""
-    for ch in message:
-        enciphered_message += cipher_map.get(ch, ch)
-    return enciphered_message
+    return "".join(cipher_map.get(ch, ch) for ch in message.upper())
 
 
 def decipher(message: str, cipher_map: dict) -> str:
@@ -78,15 +58,29 @@ def decipher(message: str, cipher_map: dict) -> str:
     :param message: Message to decipher
     :param cipher_map: Dictionary mapping to use
     :return: Deciphered string
-    >>> decipher(encipher('HELLO WORLD!!', create_cipher_map('GOODBYE!!')), create_cipher_map('GOODBYE!!'))
+    >>> cipher_map = create_cipher_map('Goodbye!!')
+    >>> decipher(encipher('Hello World!!', cipher_map), cipher_map)
     'HELLO WORLD!!'
     """
     # Reverse our cipher mappings
     rev_cipher_map = {v: k for k, v in cipher_map.items()}
-    deciphered_message = ""
-    for ch in message:
-        deciphered_message += rev_cipher_map.get(ch, ch)
-    return deciphered_message
+    return "".join(rev_cipher_map.get(ch, ch) for ch in message.upper())
+
+
+def main():
+    """
+    Handles I/O
+    :return: void
+    """
+    message = input("Enter message to encode or decode: ").strip()
+    key = input("Enter keyword: ").strip()
+    option = input("Encipher or decipher? E/D:").strip()[0].lower()
+    try:
+        func = {"e": encipher, "d": decipher}[option]
+    except KeyError:
+        raise KeyError("invalid input option")
+    cipher_map = create_cipher_map(key)
+    print(func(message, cipher_map))
 
 
 if __name__ == "__main__":
