fa6703297e794bb9912717b61f88449e9fb39fcd
Luke Polson <47739849+Luke-Polson@users.noreply.github.com>
2019-11-28 11:45:54 +1300

    additional doctest and error handling

--- maths/recursive_binomial_coefficient.py
+++ maths/recursive_binomial_coefficient.py
@@ -3,15 +3,22 @@ def recursive_binomial_coefficient(n,k):
     Time complexity is O(k), so can calculate fairly quickly for large values of k.
 
     >>> recursive_binomial_coefficient(5,0)
-    0
+    1
 
     >>> recursive_binomial_coefficient(8,2)
     28
 
     >>> recursive_binomial_coefficient(500,300)
     5054949849935535817667719165973249533761635252733275327088189563256013971725761702359997954491403585396607971745777019273390505201262259748208640
+
+    >>> recursive_binomial_coefficient(4,5)
+    Invalid Inputs
+
     """
 
+    if k>n:
+        return 'Invalid Inputs'
+        #function is only defined for n>=k
     if k == 0 or n == k:
         #C(n,0) = C(n,n) = 1, so this is our base case.
         return 1
@@ -21,5 +28,4 @@ def recursive_binomial_coefficient(n,k):
     else:
         #else, we know C(n,k) = (n/k)C(n-1,k-1), so we can use this to reduce our problem size.
         return int((n/k)*recursive_binomial_coefficient(n-1,k-1))
-
 
