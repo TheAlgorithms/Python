6664422a4ecbe151e7bcfe1d115dd80011812b64
Christian Clauss <cclauss@me.com>
2019-12-03 01:13:22 +0100

    Wrap lines for readability

--- hashes/full_domain_hash.py
+++ hashes/full_domain_hash.py
@@ -1,10 +1,15 @@
 from math import ceil
 from hashlib import sha256
 
-def fdh(string, n=256):
-    '''full domain hash of 'string' using SHA256 with digest of n-1 bits. Generates a hash with an arbitrary bit size given as input (n)
-       The function generates a hash digest of bitsize = n bits.   
-       This is achieved by repeatedly appending an incremental value at the end of input string and calculation of SHA256 hash. These hash digests are concatenated to generate hash of desired bit size'''
+def fdh(string, n: int = 256):
+    '''
+    full domain hash of 'string' using SHA256 with digest of n-1 bits.
+    Generates a hash with an arbitrary bit size given as input (n)
+    The function generates a hash digest of bitsize = n bits.   
+    This is achieved by repeatedly appending an incremental value at the
+    end of input string and calculation of SHA256 hash. These hash
+    digests are concatenated to generate hash of desired bit size
+    '''
     
     result = []
     
