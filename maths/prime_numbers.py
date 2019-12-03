369a16920889808d3362e77e330a32728cbf7c28
Níkolas Vargas <vargasnikolass@gmail.com>
2019-12-02 22:08:57 -0300

    fix doctests

--- maths/prime_numbers.py
+++ maths/prime_numbers.py
@@ -3,14 +3,14 @@ from typing import Generator, List
 def primes(max: int) -> Generator[int, None, None]:
     """
     Return a list of all primes numbers up to max.
-    >>> primes(10)
-    [2, 3, 5, 7]
-    >>> primes(11)
-    [2, 3, 5, 7, 11]
-    >>> primes(25)
+    >>> list(primes(25))
     [2, 3, 5, 7, 11, 13, 17, 19, 23]
-    >>> primes(1_000_000)[-1]
-    999983
+    >>> list(primes(11))
+    [2, 3, 5, 7, 11]
+    >>> list(primes(33))
+    [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
+    >>> list(primes(10000))[-1]
+    9973
     """
     numbers: Generator = (i for i in range(1, (max + 1)))
     for i in filter(lambda x: x > 1, numbers):
@@ -22,6 +22,6 @@ def primes(max: int) -> Generator[int, None, None]:
 
 
 if __name__ == "__main__":
-    number = int(input("Calculate primes up to:\n>> "))
+    number = int(input("Calculate primes up to:\n>> ").strip())
     for ret in primes(number):
         print(ret)
