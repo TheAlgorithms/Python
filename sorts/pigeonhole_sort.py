4d9f5471bc2ca92be3587634e7b7d6f99bc946d3
Christian Clauss <cclauss@me.com>
2019-12-06 07:34:04 +0100

    Add doctest

--- sorts/pigeonhole_sort.py
+++ sorts/pigeonhole_sort.py
@@ -1,40 +1,44 @@
-# Python program to implement Pigeonhole Sorting in python 
+# Python program to implement Pigeonhole Sorting in python
 
-#Algorithm for the pigeonhole sorting
+# Algorithm for the pigeonhole sorting
 
-def pigeonhole_sort(a):  
-	# size of range of values in the list (ie, number of pigeonholes we need)
-  
-	min_val = min(a) # min()finds the minimum value
-	max_val = max(a) # max() finds the maximum value
-  
-	size = max_val - min_val + 1 # size is difference of max and min values plus one 
 
-	# list of pigeonholes of size equal to the variable size
-	holes = [0] * size 
+def pigeonhole_sort(a):
+    """
+    >>> a = [8, 3, 2, 7, 4, 6, 8]
+    >>> b = sorted(a)  # a nondestructive sort
+    >>> pigeonhole_sort(a)  # a distructive sort
+    >>> a == b
+    True
+    """
+    # size of range of values in the list (ie, number of pigeonholes we need)
 
-	# Populate the pigeonholes. 
-	for x in a: 
-		assert type(x) is int, "integers only please"
-		holes[x - min_val] += 1
+    min_val = min(a)  # min() finds the minimum value
+    max_val = max(a)  # max() finds the maximum value
 
-	# Putting the elements back into the array in an order. 
-	i = 0
-	for count in range(size): 
-		while holes[count] > 0: 
-			holes[count] -= 1
-			a[i] = count + min_val 
-			i += 1
-			
+    size = max_val - min_val + 1  # size is difference of max and min values plus one
+
+    # list of pigeonholes of size equal to the variable size
+    holes = [0] * size
+
+    # Populate the pigeonholes.
+    for x in a:
+        assert isinstance(x, int), "integers only please"
+        holes[x - min_val] += 1
+
+    # Putting the elements back into the array in an order.
+    i = 0
+    for count in range(size):
+        while holes[count] > 0:
+            holes[count] -= 1
+            a[i] = count + min_val
+            i += 1
+
+
 def main():
-    
-    a = [8, 3, 2, 7, 4, 6, 8] 
-    print("Sorted order is : ", end = ' ') 
+    pigeonhole_sort([8, 3, 2, 7, 4, 6, 8])
+    print("Sorted order is: ", " ", join(a))
 
-    pigeonhole_sort(a) 
-		
-    for i in range(0, len(a)): 
-	    print(a[i], end = ' ') 
-	
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
