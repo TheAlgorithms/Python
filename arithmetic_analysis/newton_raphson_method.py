284e532b7f8b35eef2a9676cf2358008d2f57d1f
Rohit Joshi <joshi.rk11166@gmail.com>
2019-12-14 19:12:02 +0545

    Bug Fixed

--- arithmetic_analysis/newton_raphson_method.py
+++ arithmetic_analysis/newton_raphson_method.py
@@ -4,31 +4,35 @@
 # quickly find a good approximation for the root of a real-valued function
 from sympy import diff
 from decimal import Decimal
+from math import *
+
+
+PRECISION = 10 ** -10
 
 
 def NewtonRaphson(func, a):
     """ Finds root from the point 'a' onwards by Newton-Raphson method """
+    x = a
     while True:
-        c = Decimal(a) - (Decimal(eval(func)) / Decimal(eval(str(diff(func)))))
 
-        a = c
+        x = Decimal(x) - (Decimal(eval(func)) / Decimal(eval(str(diff(func)))))
 
         # This number dictates the accuracy of the answer
-        if abs(eval(func)) < 10 ** -15:
-            return c
+        if abs(eval(func)) < PRECISION:
+            return x
 
 
 # Let's Execute
 if __name__ == "__main__":
     # Find root of trigonometric function
     # Find value of pi
-    print("sin(x) = 0", NewtonRaphson("sin(x)", 2))
+    print("The root of sin(x) = 0 is ", NewtonRaphson("sin(x)", 2))
 
     # Find root of polynomial
-    print("x**2 - 5*x +2 = 0", NewtonRaphson("x**2 - 5*x +2", 0.4))
+    print("The root of x**2 - 5*x +2 = 0 is ", NewtonRaphson("x**2 - 5*x +2", 0.4))
 
     # Find Square Root of 5
-    print("x**2 - 5 = 0", NewtonRaphson("x**2 - 5", 0.1))
+    print("The root of log(x) - 1 = 0 is ", NewtonRaphson("log(x)- 1", 2))
 
     # Exponential Roots
-    print("exp(x) - 1 = 0", NewtonRaphson("exp(x) - 1", 0))
+    print("The root of exp(x) - 1 = 0 is", NewtonRaphson("exp(x) - 1", 0))
