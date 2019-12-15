788626db24f27b6786cdf1919a333e3f0a6225b8
Christian Clauss <cclauss@me.com>
2019-12-15 06:52:12 +0100

    Update newton_raphson_method.py

--- arithmetic_analysis/newton_raphson_method.py
+++ arithmetic_analysis/newton_raphson_method.py
@@ -2,15 +2,13 @@
 # Author: Syed Haseeb Shah (github.com/QuantumNovice)
 # The Newton-Raphson method (also known as Newton's method) is a way to
 # quickly find a good approximation for the root of a real-valued function
-from sympy import diff
+
 from decimal import Decimal
-from math import *
+from math import *  # noqa: F821
+from sympy import diff
 
 
-PRECISION = 10 ** -10
-
-
-def NewtonRaphson(func, a):
+def NewtonRaphson(func: str, a: int, precision=int: 10 ** -10) -> float:
     """ Finds root from the point 'a' onwards by Newton-Raphson method
     >>> NewtonRaphson("sin(x)", 2))
     3.141592653680804119362982272
@@ -21,14 +19,11 @@ def NewtonRaphson(func, a):
     >>> NewtonRaphson("log(x)- 1", 2))
     2.718281828458937968804105873
     """
-    
     x = a
     while True:
-
         x = Decimal(x) - (Decimal(eval(func)) / Decimal(eval(str(diff(func)))))
-
         # This number dictates the accuracy of the answer
-        if abs(eval(func)) < PRECISION:
+        if abs(eval(func)) < precision:
             return x
 
 
@@ -36,13 +31,10 @@ def NewtonRaphson(func, a):
 if __name__ == "__main__":
     # Find root of trigonometric function
     # Find value of pi
-    print("The root of sin(x) = 0 is ", NewtonRaphson("sin(x)", 2))
-
+    print(f"The root of sin(x) = 0 is {NewtonRaphson('sin(x)', 2)}")
     # Find root of polynomial
-    print("The root of x**2 - 5*x +2 = 0 is ", NewtonRaphson("x**2 - 5*x +2", 0.4))
-
+    print(f"The root of x**2 - 5*x + 2 = 0 is {NewtonRaphson('x**2 - 5*x + 2', 0.4)}")
     # Find Square Root of 5
-    print("The root of log(x) - 1 = 0 is ", NewtonRaphson("log(x)- 1", 2))
-    
+    print(f"The root of log(x) - 1 = 0 is {NewtonRaphson('log(x) - 1', 2)}")
     # Exponential Roots
-    print("The root of exp(x) - 1 = 0 is", NewtonRaphson("exp(x) - 1", 0))
+    print(f"The root of exp(x) - 1 = 0 is {NewtonRaphson('exp(x) - 1', 0)}")
