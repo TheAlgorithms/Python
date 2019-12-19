e2d6fe25175687a0e18930e9663ecef06881c026
Kyle <kylepw@gmail.com>
2019-12-19 14:24:57 +0900

    Add docs and type alias to bfs_shortest_path.py

--- graphs/bfs_shortest_path.py
+++ graphs/bfs_shortest_path.py
@@ -1,15 +1,37 @@
-graph = {
-    "A": ["B", "C", "E"],
-    "B": ["A", "D", "E"],
-    "C": ["A", "F", "G"],
-    "D": ["B"],
-    "E": ["A", "B", "D"],
-    "F": ["C"],
-    "G": ["C"],
-}
+"""Breadth-first search shortest path implementations.
+
+    doctest:
+    python -m doctest -v bfs_shortest_path.py
+
+    Manual test:
+    python bfs_shortest_path.py
+"""
 
 
-def bfs_shortest_path(graph, start, goal):
+def bfs_shortest_path(graph, start, goal) -> str:
+    """Find shortest path between `start` and `goal` nodes.
+
+        Args:
+            graph (dict): node/list of neighboring nodes key/value pairs.
+            start: start node.
+            goal: target node.
+
+        Returns:
+            Shortest path between `start` and `goal` nodes as a string of nodes.
+            'Not found' string if no path found.
+
+        Example:
+            >>> bfs_shortest_path(
+            ... {'A': ['B', 'C', 'E'],
+            ... 'B': ['A', 'D', 'E'],
+            ... 'C': ['A', 'F', 'G'],
+            ... 'D': ['B'],
+            ... 'E': ['A', 'B', 'D'],
+            ... 'F': ['C'],
+            ... 'G': ['C']
+            ... }, 'G', 'D')
+            ['G', 'C', 'A', 'B', 'D']
+    """
     # keep track of explored nodes
     explored = []
     # keep track of all the paths to be checked
@@ -44,4 +66,14 @@ def bfs_shortest_path(graph, start, goal):
     return "So sorry, but a connecting path doesn't exist :("
 
 
-bfs_shortest_path(graph, "G", "D")  # returns ['G', 'C', 'A', 'B', 'D']
+if __name__ == "__main__":
+    graph = {
+        "A": ["B", "C", "E"],
+        "B": ["A", "D", "E"],
+        "C": ["A", "F", "G"],
+        "D": ["B"],
+        "E": ["A", "B", "D"],
+        "F": ["C"],
+        "G": ["C"],
+    }
+    bfs_shortest_path(graph, "G", "D")  # returns ['G', 'C', 'A', 'B', 'D']
