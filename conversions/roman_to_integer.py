90d17d291f65b74bd4d96547b270b2d1628319be
Christian Clauss <cclauss@me.com>
2019-12-21 00:18:01 +0100

    Update roman_to_integer.py

--- conversions/roman_to_integer.py
+++ conversions/roman_to_integer.py
@@ -1,14 +1,12 @@
 def roman_to_int(roman: str) -> int:
     """
     LeetCode No. 13 Roman to Integer
-    Given a roman numeral, convert it to an integer. 
+    Given a roman numeral, convert it to an integer.
     Input is guaranteed to be within the range from 1 to 3999.
-    >>> roman_to_int("III")
-    3
-    >>> roman_to_int("CLIV")
-    154
-    >>> roman_to_int("MMD")
-    2500
+    https://en.wikipedia.org/wiki/Roman_numerals
+    >>> tests = {"III": 3, "CLIV": 154, "MIX": 1009, "MMD": 2500, "MMMCMXCIX": 3999}
+    >>> all(roman_to_int(key) == value for key, value in tests.items())
+    True
     """
     vals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     total = 0
