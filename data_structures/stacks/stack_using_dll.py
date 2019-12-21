35e545ebba49286ec4696219769fa8cdc0e18c12
Christian Clauss <cclauss@me.com>
2019-12-21 02:18:46 +0100

    Test with doctests

--- data_structures/stacks/stack_using_dll.py
+++ data_structures/stacks/stack_using_dll.py
@@ -1,24 +1,42 @@
 # A complete working Python program to demonstrate all  
 # stack operations using a doubly linked list  
   
-# Node class  
 class Node: 
-  
-# Function to initialise the node object 
     def __init__(self, data): 
         self.data = data # Assign data 
         self.next = None # Initialize next as null 
         self.prev = None # Initialize prev as null         
           
-# Stack class contains a Node object 
-class Stack: 
-    # Function to initialize head  
+class Stack:
+    """
+    >>> stack = Stack()
+    >>> stack.is_empty()
+    True
+    >>> stack.print_stack()
+    stack elements are:
+    >>> for i in range(4):
+    ...     stack.push(i)
+    ...
+    >>> stack.is_empty()
+    False
+    >>> stack.printstack()
+    stack elements are:
+    3->2->1->0->
+    >>> stack.top()
+    3
+    >>> stack.size()
+    4
+    >>> stack.pop()
+    3
+    >>> stack.printstack()
+    stack elements are:
+    2->1->0->
+    """
     def __init__(self): 
         self.head = None
           
-# Function to add an element data in the stack  
-    def push(self, data): 
-  
+    def push(self, data):
+        """add a Node to the stack"""
         if self.head is None: 
             self.head = Node(data) 
         else: 
@@ -26,12 +44,10 @@ class Stack:
             self.head.prev = new_node 
             new_node.next = self.head 
             new_node.prev = None
-            self.head = new_node 
+            self.head = new_node             
               
-              
-# Function to pop top element and return the element from the stack  
-    def pop(self): 
-  
+    def pop(self):
+        """pop the top element off the stack"""
         if self.head is None: 
             return None
         else: 
@@ -40,36 +56,22 @@ class Stack:
             self.head.prev = None
             return temp 
   
-  
-# Function to return top element in the stack  
-    def top(self): 
-  
-        return self.head.data 
-  
-  
-# Function to return the size of the stack  
-    def size(self): 
-  
+    def top(self):
+        """return the top element of the stack"""
+        return self.head.data
+
+    def __len__(self): 
         temp = self.head 
         count = 0
         while temp is not None: 
-            count = count + 1
+            count += 1
             temp = temp.next
         return count 
-      
-      
-# Function to check if the stack is empty or not   
-    def isEmpty(self): 
-  
-        if self.head is None: 
-           return True
-        else: 
-           return False
-              
-  
-# Function to print the stack 
-    def printstack(self): 
-          
+
+    def is_empty(self):
+        return self.head is None: 
+
+    def print_stack(self): 
         print("stack elements are:") 
         temp = self.head 
         while temp is not None: 
@@ -80,40 +82,39 @@ class Stack:
 # Code execution starts here          
 if __name__=='__main__':  
   
-# Start with the empty stack 
-  stack = Stack() 
+    # Start with the empty stack 
+    stack = Stack() 
   
-# Insert 4 at the beginning. So stack becomes 4->None  
-  print("Stack operations using Doubly LinkedList") 
-  stack.push(4) 
+    # Insert 4 at the beginning. So stack becomes 4->None  
+    print("Stack operations using Doubly LinkedList") 
+    stack.push(4) 
   
-# Insert 5 at the beginning. So stack becomes 4->5->None  
-  stack.push(5) 
+    # Insert 5 at the beginning. So stack becomes 4->5->None  
+    stack.push(5) 
   
-# Insert 6 at the beginning. So stack becomes 4->5->6->None  
-  stack.push(6) 
+    # Insert 6 at the beginning. So stack becomes 4->5->6->None  
+    stack.push(6) 
   
-# Insert 7 at the beginning. So stack becomes 4->5->6->7->None  
-  stack.push(7) 
+    # Insert 7 at the beginning. So stack becomes 4->5->6->7->None  
+    stack.push(7) 
   
-# Print the stack 
-  stack.printstack() 
+    # Print the stack 
+    stack.print_stack() 
   
-# Print the top element 
-  print("\nTop element is ", stack.top()) 
+    # Print the top element 
+    print("\nTop element is ", stack.top()) 
   
-# Print the stack size 
-  print("Size of the stack is ", stack.size()) 
+    # Print the stack size 
+    print("Size of the stack is ", stack.size()) 
   
-# pop the top element 
-  stack.pop() 
+    # pop the top element 
+    stack.pop() 
   
-# pop the top element 
-  stack.pop() 
+    # pop the top element 
+    stack.pop() 
     
-# two elements are popped 
-# Print the stack 
-  stack.printstack() 
+    # two elements have now been popped off
+    stack.print_stack() 
     
-# Print True if the stack is empty else False 
-  print("\nstack is empty:", stack.isEmpty()) 
+    # Print True if the stack is empty else False 
+    print("\nstack is empty:", stack.is_empty()) 
