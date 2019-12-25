24181c2261c79d0f4554b2452bb63becf8b0f031
bluedistro <bineykingsley36@gmail.com>
2019-12-25 15:35:37 +0000

    simple face_detection algorithm suitable for API services

---
+++ Image_Processing/face_detection/detect_face.py
@@ -0,0 +1,17 @@
+# simple face detectin algorithm to detect the presence of at least a face in an image provided.
+import cv2, os
+
+''' 
+    :input => image path
+    :output => boolean corresponding to the detection of a face or not
+    TRUE if face(s) detected. else FALSE
+'''
+
+def detect_face(image):
+    gray = cv2.cvtColor(cv2.imread(image), cv2.COLOR_BGR2GRAY)
+    cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+    faces = cascade.detectMultiScale(gray, 1.03, 3)
+    return len(faces) > 0
+
+if __name__ == "__main__":
+    print(detect_face('im1.jpg'))
