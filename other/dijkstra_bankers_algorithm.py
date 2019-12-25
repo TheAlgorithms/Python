23476c483a153075f1bd15c81a200b9da9a8942d
bluedistro <bineykingsley36@gmail.com>
2019-12-25 20:37:17 +0000

    Applied all requested changes

--- other/dijkstra_bankers_algorithm.py
+++ other/dijkstra_bankers_algorithm.py
@@ -1,7 +1,7 @@
-# A python implementation of the Banker's Algorithm in Operating Systems using
+# A Python implementation of the Banker's Algorithm in Operating Systems using
 # Processes and Resources
 # {
-# "Author: "Biney Kingsley (bluedistro@github.io)",
+# "Author: "Biney Kingsley (bluedistro@github.io), bineykingsley36@gmail.com",
 # "Date": 28-10-2018
 # }
 '''
@@ -15,80 +15,143 @@ before deciding whether allocation should be allowed to continue.
  (https://rosettacode.org/wiki/Banker%27s_algorithm)
 '''
 
-from __future__ import print_function
 import numpy as np
 import time
 
 
 class Bankers_algorithm:
 
-    def __init__(self, claim_vector, allocated_resources_table, maximum_claim_table):
+    def __init__(self, claim_vector: list, allocated_resources_table: list, maximum_claim_table: list) -> None:
         '''
-        :param claim_vector: The maximum (in values) amount of each resources
+        :param claim_vector: A nxn/nxm list depicting the amount of each resources
          (eg. memory, interface, semaphores, etc.) available.
-        :param allocated_resources_table: How much (in value) of each resource
+        :param allocated_resources_table: A nxn/nxm list depicting the amount of each resource
         each process is currently holding
-        :param maximum_claim_table: How much of each resource the system
+        :param maximum_claim_table: A nxn/nxm list depicting how much of each resource the system
         currently has available
         '''
         self.__claim_vector = claim_vector
         self.__allocated_resources_table = allocated_resources_table
         self.__maximum_claim_table = maximum_claim_table
 
-    # check for allocated resources in line with each resource in the claim vector
-    def __processes_resource_summation(self):
+    def __processes_resource_summation(self) -> list:
+        '''
+        This method checks for allocated resources in line with each resource in the claim vector
+        '''
         allocated_resources = list()
         for i in range(len(self.__allocated_resources_table[0])):
-            # dummy to perform addition of specific resources
             sum = 0
             for p_item in self.__allocated_resources_table:
                 sum += p_item[i]
             allocated_resources.append(sum)
         return allocated_resources
 
-    # check for available resources in line with each resource in the claim vector
-    def __available_resources(self):
+    def __available_resources(self) -> list:
+        '''
+        This method checks for available resources in line with each resource in the claim vector
+        '''
         return np.array(self.__claim_vector) - np.array(self.__processes_resource_summation())
 
-    # implement safety checker by ensuring that max_claim[i][j] - alloc_table[i][j] <= avail[j]
-    # in order words, calculating for the needs
-    def __need(self):
+    def __need(self) -> list:
+    '''
+    This method implements safety checker by ensuring that
+    max_claim[i][j] - alloc_table[i][j] <= avail[j]
+    In order words, calculating for the needs
+    '''
         need = list()
         for i in range(len(self.__allocated_resources_table)):
             need.append(list(np.array(self.__maximum_claim_table[i])
                                      - np.array(self.__allocated_resources_table[i])))
         return need
 
-    # build an index control dictionary to track original ids/indices
-    #  of processes during removals in core operation
-    def __need_index_manager(self):
+    def __need_index_manager(self) -> dict:
+        '''
+        This function builds an index control dictionary to track original ids/indices
+        of processes when altered during execution of method "main"
+            Return: {0: [a: int, b: int], 1: [c: int, d: int]}
+        >>> __need_index_manager([[1, 2, 0, 3], [0, 1, 3, 1], [1, 1, 0, 2], [1, 3, 2, 0], [2, 0, 0, 3]])
+        {0: [1, 2, 0, 3], 1: [0, 1, 3, 1], 2: [1, 1, 0, 2], 3: [1, 3, 2, 0], 4: [2, 0, 0, 3]}
+        >>> __need_index_manager([[1, 3, 0, 1], [2, 0, 3, 1], [1, 0, 3, 2], [1, 1, 2, 3], [2, 1, 2, 3]])
+        {0: [1, 3, 0, 1], 1: [2, 0, 3, 1], 2: [1, 0, 3, 2], 3: [1, 1, 2, 3], 4: [2, 1, 2, 3]}
+        '''
         ni_manager = dict()
         for i in self.__need():
             ni_manager.update({self.__need().index(i): i})
         return ni_manager
 
-    # core operation of the banker's algorithm
-    def main(self, **kwargs):
+    def main(self, **kwargs) -> None:
+        ''' 
+        This method utilized the various methods in this class to simulate the Banker's algorithm
+        Return: None
+        >>> Bankers_algorithm([8, 5, 9, 7], [[2, 0, 1, 1],
+                                [0, 1, 2, 1],
+                                [4, 0, 0, 3],
+                                [0, 2, 1, 0],
+                                [1, 0, 3, 0]], [[3, 2, 1, 4],
+                                [0, 2, 5, 2],
+                                [5, 1, 0, 5],
+                                [1, 5, 3, 0],
+                                [3, 0, 3, 3]]).main(describe=true)
+              Allocated Resource Table
+        P1    2        0        1        1     
+
+        P2    0        1        2        1     
+
+        P3    4        0        0        3     
+
+        P4    0        2        1        0     
+
+        P5    1        0        3        0     
+
+            System Resource Table
+        P1    3        2        1        4     
+
+        P2    0        2        5        2     
+
+        P3    5        1        0        5     
+
+        P4    1        5        3        0     
+
+        P5    3        0        3        3     
+
+        Current Usage by Active Processes:     8 5 9 7
+        Initial Available Resources:         1 2 2 2
+        __________________________________________________
+
+        Process 3 is executing.
+        Updated available resource stack for processes: 5 2 2 5
+        The process is in a safe state.
+
+        Process 1 is executing.
+        Updated available resource stack for processes: 7 2 3 6
+        The process is in a safe state.
+
+        Process 2 is executing.
+        Updated available resource stack for processes: 7 3 5 7
+        The process is in a safe state.
+
+        Process 4 is executing.
+        Updated available resource stack for processes: 7 5 6 7
+        The process is in a safe state.
+
+        Process 5 is executing.
+        Updated available resource stack for processes: 8 5 9 7
+        The process is in a safe state.                                
+        '''
         need_list = self.__need()
         alloc_resources_table = self.__allocated_resources_table
         available_resources = self.__available_resources()
         need_index_manager = self.__need_index_manager()
-        # display information if needed
-        # get the parameter that handles display
         for kw, val in kwargs.items():
             if kw and val is True:
                 self.__pretty_data()
-            elif kw and val is False:
-                print ("[Warning] Muting data description.\n")
-            else:
-                print ("[Warning] Muting data description.\n")
         print("{:_^50}\n".format("_"))
         while len(need_list) != 0:
             safe = False
             for each_need in need_list:
                 execution = True
-                for i in range(len(each_need)):
-                    if each_need[i] > available_resources[i]:
+                for index, need in enumerate(each_need):
+                    if need > available_resources[index]:
                         execution = False
                         break
 
@@ -98,64 +161,50 @@ class Bankers_algorithm:
                     for original_need_index, need_clone in need_index_manager.items():
                         if each_need == need_clone:
                             process_number = original_need_index
-                    # display the process id based on entry
                     print('Process {} is executing.'.format(process_number + 1))
                     # remove the process run from stack
                     need_list.remove(each_need)
                     # update available/freed resources stack
                     available_resources = np.array(available_resources) + \
                                                          np.array(alloc_resources_table[process_number])
-                    # display available memory
                     print("Updated available resource stack for processes: {}"
                           .format(" ".join([str(x) for x in available_resources])))
                     break
-                else:
-                    pass
-
             if safe:
                 print("The process is in a safe state.\n")
-
-            if not safe:
+            else:
                 print("System in unsafe state. Aborting...\n")
                 break
 
-    # pretty display description of the allocated, maximum and claim
-    # vector of the resources available
     def __pretty_data(self):
-
-        # Information on Allocation Resource Table
+        '''
+        Properly align display of the algorithm's solution
+        '''
         print("{:>8}".format("      Allocated Resource Table"))
-        # print("{:^11} {:^8} {:^8} {:^8} {:^8}".format("  A", "B", "C", "D", "E"))
-        # print("{:_^11} {:_^8} {:_^8} {:_^8} {:_^8}".format("  ", "_", "_", "_", "_"))
         for item in self.__allocated_resources_table:
             print("P{}".format(str(self.__allocated_resources_table.index(item) + 1)), end=" ")
             for it in item:
                 print("{:^8}".format(it), end=" ")
             print("\n")
 
-        # Information on Current System Resources
         print("{:>8}".format("      System Resource Table"))
-        # print("{:^11} {:^8} {:^8} {:^8} {:^8}".format("  A", "B", "C", "D", "E"))
-        # print("{:_^11} {:_^8} {:_^8} {:_^8} {:_^8}".format("  ", "_", "_", "_", "_"))
         for item in self.__maximum_claim_table:
             print("P{}".format(str(self.__maximum_claim_table.index(item) + 1)), end=" ")
             for it in item:
                 print("{:^8}".format(it), end=" ")
             print("\n")
 
-        # Information on Current Resource Usage by processes
         print("Current Usage by Active Processes: {:>11}"
               .format(str(" ".join([str(x) for x in self.__claim_vector]))))
-        # Information on Current Resource Usage by processes
+
         print("Initial Available Resources: {:>15}"
               .format(str(" ".join([str(x) for x in
                                     self.__available_resources()]))))
-        # make table visible during display
         time.sleep(1)
 
 
 # test algorithm
-def main():
+def test():
     # test 1
     test_1_claim_vector = [8, 5, 9, 7]
     test_1_allocated_res_table = [[2, 0, 1, 1],
@@ -181,4 +230,4 @@ def main():
                     test_1_maximum_claim_table).main(describe=True)
 
 if __name__ == "__main__":
-    main()
+    test()
