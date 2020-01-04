f48cc9094319ac0740ba3876a414b0d6e21dd734
harsh patel <hkp27299@gmail.com>
2020-01-04 16:07:57 +0530

    Update recursive_bubble_sort.py
    Added examples and remove input()

--- sorts/recursive_bubble_sort.py
+++ sorts/recursive_bubble_sort.py
@@ -1,4 +1,21 @@
-def bubble_sort(list1): 
+def bubble_sort(list1):
+    """It is similar is bubble sort but recursive.
+    :param list1: mutable ordered sequence of elements
+    :return: the same list in ascending order    
+    Examples:
+    
+    >>> bubble_sort([22,66,33,11,99])
+    Sorted array:
+    11 22 33 66 99
+    
+    >>> bubble_sort([])
+    Empty list
+    
+    >>> bubble_sort([-22,-99,-11])
+    Sorted array:
+    -99 -22 -11
+    """ 
+
     for i, num in enumerate(list1): 
         try: 
             if list1[i+1] < num: 
@@ -8,24 +25,13 @@ def bubble_sort(list1):
         except IndexError: 
             pass
     return list1 
-  
-list1 = []
-print("Enter length of list elements:")
-n = int(input())
 
-for i in range(0,n):
-    print("Enter",i+1,"element of list")
-    element = input()
-    list1.append(element)
-
-    
-bubble_sort(list1) 
-
-if(len(list1) == 0):
-    print("Empty list")
-    
-else:
-  
-    print("Sorted array:"); 
-    for i in range(0, len(list1)): 
-        print(list1[i], end=' ') 
+if __name__ == "__main__":     
+    list1 = [22,66,33,99,11]    
+    bubble_sort(list1) 
+    if(len(list1) == 0):
+        print("Empty list")
+    else:
+        print("Sorted array:")
+        for i in range(0, len(list1)): 
+            print(list1[i], end=' ') 
