5f18bcc803fb8005b060d5cdca23209659312801
Christian Clauss <cclauss@me.com>
2020-01-05 22:22:45 +0100

    get_imdb_top_250_movies()

--- web_programming/get_imdb_top_250_movies_csv.py
+++ web_programming/get_imdb_top_250_movies_csv.py
@@ -1,45 +1,29 @@
-import requests
 import csv
-import os
+from typing import Dict
+
+import requests
 from bs4 import BeautifulSoup
 
 
-def movies(filename,path):
-   
-    os.chdir(filename)
-    connect=requests.get(""" https://www.imdb.com/chart/top/?ref_=nv_mv_250 """)
-    soup=BeautifulSoup(connect.text,'html.parser')
-    elements_title=soup.find_all('td',attrs='titleColumn')
-    elements_rating=soup.find_all('td',class_='ratingColumn imdbRating')
-    Movies={}
+def get_imdb_top_250_movies(url: str = "") -> Dict[str, float]:
+    url = url or "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
+    soup = BeautifulSoup(requests.get(url).text, "html.parser")
+    titles = soup.find_all("td", attrs="titleColumn")
+    ratings = soup.find_all("td", class_="ratingColumn imdbRating")
+    return {
+        title.a.text: float(rating.strong.text)
+        for title, rating in zip(titles, ratings)
+    }
 
 
-    for i in range(len(elements_title)):
-        title=elements_title[i].find('a').text
-        rating=elements_rating[i].find('strong').text
-        Movies[title]=Movies.get(title,rating)
-
-
-    with open ('Top_250_Movies_IMDB.csv','w') as newfile:
-        Writer=csv.writer(newfile)
-        for Movie,Ratings in Movies.items():
-            Writer.writerow([Movie,Ratings])
-    
+def write_movies(filename: str = "IMDb_Top_250_Movies.csv") -> None:
+    movies = get_imdb_top_250_movies()
+    with open(filename, "w", newline="") as out_file:
+        writer = csv.writer(out_file)
+        writer.writerow(["Movie title", "IMDb rating"])
+        for title, rating in movies.items():
+            writer.writerow([title, rating])
 
 
 if __name__ == "__main__":
-
-
-
-    try:
-        filename=input("Please enter a name for the file to be stored:\n")+".csv"
-        path=input('Enter the path you want to store the file:\n')
-        movies(filename,path)
-        print("Done")
-
-
-    except:
-        filename='Top_250_Movies_IMDB.csv'
-        path=os.path.join(os.environ["HOMEPATH"], "Desktop")
-        movies(filename,path)
-        print("Done!")
+    write_movies()
