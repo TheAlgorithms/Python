38b1590bd0edd5427c809034fb02ed3315c79819
nishithshowri006 <58651995+nishithshowri006@users.noreply.github.com>
2020-01-06 01:05:52 +0530

    Create get_imdb_top_250_movies_csv.py

---
+++ web_programming/get_imdb_top_250_movies_csv.py
@@ -0,0 +1,47 @@
+import requests
+import csv
+import os
+from bs4 import BeautifulSoup
+
+
+def movies(filename,path):
+   
+    os.chdir(filename)
+    titlelist=[]
+    connect=requests.get(""" https://www.imdb.com/chart/top/?ref_=nv_mv_250 """)
+    soup=BeautifulSoup(connect.text,'html.parser')
+    elements_title=soup.find_all('td',attrs='titleColumn')
+    elements_rating=soup.find_all('td',class_='ratingColumn imdbRating')
+    Movies={}
+
+
+    for i in range(len(elements_title)):
+        title=elements_title[i].find('a').text
+        rating=elements_rating[i].find('strong').text
+        Movies[title]=Movies.get(title,rating)
+
+
+    with open ('Top_250_Movies_IMDB.csv','w') as newfile:
+        Writer=csv.writer(newfile)
+        for Movie,Ratings in Movies.items():
+            Writer.writerow([Movie,Ratings])
+        newfile.close()
+    
+
+
+if __name__ == "__main__":
+
+
+
+    try:
+        filename=input("Please enter a name for the file to be stored:\n")+".csv"
+        path=input('Enter the path you want to store the file:\n')
+        movies(filename,path)
+        print("Done")
+
+
+    except:
+        filename='Top_250_Movies_IMDB.csv'
+        path=os.path.join(os.environ["HOMEPATH"], "Desktop")
+        movies(filename,path)
+        print("Done!")
