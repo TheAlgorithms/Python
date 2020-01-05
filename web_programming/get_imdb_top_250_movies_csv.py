a7aec61506a539637bc9bcece0b266b43c8e5702
nishithshowri006 <58651995+nishithshowri006@users.noreply.github.com>
2020-01-06 01:23:10 +0530

    Update get_imdb_top_250_movies_csv.py

--- web_programming/get_imdb_top_250_movies_csv.py
+++ web_programming/get_imdb_top_250_movies_csv.py
@@ -7,7 +7,6 @@ from bs4 import BeautifulSoup
 def movies(filename,path):
    
     os.chdir(filename)
-    titlelist=[]
     connect=requests.get(""" https://www.imdb.com/chart/top/?ref_=nv_mv_250 """)
     soup=BeautifulSoup(connect.text,'html.parser')
     elements_title=soup.find_all('td',attrs='titleColumn')
