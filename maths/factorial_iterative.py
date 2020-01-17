7da46aabad0139a221430e49cad3296f4de6ae28
Pooja <pooja016mca19@igdtuw.ac.in>
2020-01-17 21:09:25 +0530

    Update factorial_iterative.py

--- maths/factorial_iterative.py
+++ maths/factorial_iterative.py
@@ -2,11 +2,13 @@
 
 
 n=int(input("Enter a number"))
-
 def factorial(n):
     fact=1
     for i in range(1,n+1):
         fact*=i
     return fact
 
-print(factorial(n))
+if n>=0:
+    print(factorial(n))
+else:
+    print("number should be positive")
