e333d0d1d0071243b1d5d871934e29e9691b68cf
Pooja <pooja016mca19@igdtuw.ac.in>
2020-01-18 12:41:05 +0530

    Update factorial_recursive.py

--- factorial_recursive.py
+++ factorial_recursive.py
@@ -2,7 +2,7 @@ def factorial(n):
     if n==0:
         return 1
     elif n<0:
-        print("ValueError: factorial() not defined for negative  values")
+        raise ValueError("ValueError: factorial() not defined for negative  values")
     else:
         return n*factorial(n-1)
     
