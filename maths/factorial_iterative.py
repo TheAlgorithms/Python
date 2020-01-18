9f75648b66af95d467f35b8d8ae3c65eb2478cda
Pooja <pooja016mca19@igdtuw.ac.in>
2020-01-18 13:01:57 +0530

    Update factorial_iterative.py

--- maths/factorial_iterative.py
+++ maths/factorial_iterative.py
@@ -1,7 +1,7 @@
 # factorial of a number
 
-
-n=int(input("Enter a number"))
+if __name__=="__main__":
+    n=int(input("Enter a number"))
 def factorial(n):
     fact=1
     for i in range(1,n+1):
@@ -12,6 +12,9 @@ if n>=0:
     print(factorial(n))
 else:
     raise ValueError("ValueError: factorial() not defined for negative vlaues")
+    
+
+    
     
 
     
