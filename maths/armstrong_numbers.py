8acfe7977dc024f34db1ec0799ea4c980376b7e1
Christian Clauss <cclauss@me.com>
2020-01-22 16:35:09 +0100

    Update armstrong_numbers.py

--- maths/armstrong_numbers.py
+++ maths/armstrong_numbers.py
@@ -1,4 +1,5 @@
-""" An Armstrong number is a number that is equal to the sum of the cubes of its digits.
+"""
+An Armstrong number is a number that is equal to the sum of the cubes of its digits.
 For example, 370 is an Armstrong number because 3*3*3 + 7*7*7 + 0*0*0 = 370.
 An Armstrong number is often called Narcissistic number.
 """
@@ -6,39 +7,39 @@ An Armstrong number is often called Narcissistic number.
 
 def armstrong_number(n: int) -> bool:
     """
-        This function checks if a number is Armstrong or not.
+    This function checks if a number is Armstrong or not.
 
-        >>> armstrong_number(153)
-        True
-        >>> armstrong_number(200)
-        False
-        >>> armstrong_number(1634)
-        True
-        >>> armstrong_number(0)
-        False
-        >>> armstrong_number(-1)
-        False
-        >>> armstrong_number(1.2)
-        False
+    >>> armstrong_number(153)
+    True
+    >>> armstrong_number(200)
+    False
+    >>> armstrong_number(1634)
+    True
+    >>> armstrong_number(0)
+    False
+    >>> armstrong_number(-1)
+    False
+    >>> armstrong_number(1.2)
+    False
     """
-    if isinstance(n, float) or n < 1:
+    if not isinstance(n, int) or n < 1:
         return False
-    else:
-        # Initialization of sum and number of digits.
-        sum = 0
-        number_of_digits = 0
-        temp = n
-        # Calculation of digits of the number
-        while temp > 0:
-            number_of_digits += 1
-            temp //= 10
-        # Dividing number into separate digits and find Armstrong number
-        temp = n
-        while temp > 0:
-            rem = temp % 10
-            sum += (rem ** number_of_digits)
-            temp //= 10
-        return n == sum
+    
+    # Initialization of sum and number of digits.
+    sum = 0
+    number_of_digits = 0
+    temp = n
+    # Calculation of digits of the number
+    while temp > 0:
+        number_of_digits += 1
+        temp //= 10
+    # Dividing number into separate digits and find Armstrong number
+    temp = n
+    while temp > 0:
+        rem = temp % 10
+        sum += (rem ** number_of_digits)
+        temp //= 10
+    return n == sum
 
 
 # In main function user inputs a number to find out if it's an Armstrong or not. Th function armstrong_number is called.
@@ -52,4 +53,3 @@ if __name__ == '__main__':
 
     doctest.testmod()
     main()
-
