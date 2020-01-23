058bc4e64d6e7bf6179830461c5f73c97341c89f
Christian Clauss <cclauss@me.com>
2020-01-23 15:28:55 +0100

    Update and rename google_news.py to web_programming/google_news.py

--- web_programming/google_news.py
+++ web_programming/google_news.py
@@ -1,69 +1,61 @@
 """
-a simple terminal program to find new about certain topic by web scraping site.
-site used :
+A simple terminal program to find news about a certain topic by web scraping site.
+site used:
 1. Times of India,
    link : https://timesofindia.indiatimes.com/india/
 2. India's Today,
    link : https://www.indiatoday.in/topic/
 """
 
+import time
+import webbrowser
 import requests
 from bs4 import BeautifulSoup
-import webbrowser
-import time
+
+BOLD_START = "\033[1m"
+BOLD_END = "\033[0m"
 
 
-def Times_of_India(userInput, ua):
-    bold_start = "\033[1m"
-    bold_end = "\033[0m"
-
-    url = "https://timesofindia.indiatimes.com/india/"
-    url += userInput
+def times_of_india(userInput, ua):
+    url = "https://timesofindia.indiatimes.com/india/" + userInput
 
     res = requests.post(url, headers=ua)
     soup = BeautifulSoup(res.content, "html.parser")
     data = soup.find_all(class_="w_tle")
 
-    if len(data) > 0:
-        print("News avaliable :", "\N{slightly smiling face}")
-    if len(data) == 0:
+    if not data:
         return 0
 
-    for item in range(len(data)):
-        print(bold_start, "\033[1;32;40m \nNEWS : ", item + 1, bold_end, end="  ")
-        data1 = data[item].find("a")
-        print(bold_start, data1.get_text(), bold_end)
+   print("News available :", "\N{slightly smiling face}")
+    for i, article in enumerate(data):
+        print(BOLD_START, "\033[1;32;40m \nNEWS : ", i + 1, BOLD_END, end="  ")
+        data1 = article.find("a")
+        print(BOLD_START, data1.get_text(), BOLD_END)
 
-        bol = input("For more details ->(y) (y/n) :: ")
+        bol = input("For more details ->(y) (y/n) :: ".strip().lower())
         if bol == "y":
             url += data1.get("href")
             print("%s" % url)
-
             webbrowser.open(url)
-
     return len(data)
 
 
 def india_today(userInput, ua):
-    bold_start = "\033[1m"
-    bold_end = "\033[0m"
-
-    url = "https://www.indiatoday.in/topic/"
-    url += userInput
+    url = "https://www.indiatoday.in/topic/" + userInput
 
     res = requests.get(url, headers=ua)
     soup = BeautifulSoup(res.content, "html.parser")
     data = soup.find_all(class_="field-content")
 
     if len(data) > 0:
-        print("\nNews avaliable : ", "\N{slightly smiling face}")
+        print("\nNews available : ", "\N{slightly smiling face}")
     k = 0
     for i in range(len(data)):
         data1 = data[i].find_all("a")
         for j in range(len(data1)):
-            print(bold_start, "\033[1;32;40m\nNEWS ", k + 1, bold_end, end=" : ")
+            print(BOLD_START, "\033[1;32;40m\nNEWS ", k + 1, BOLD_END, end=" : ")
             k += 1
-            print(bold_start, data1[j].get_text(), bold_end)
+            print(BOLD_START, data1[j].get_text(), BOLD_END)
             bol = input("\nFor more details ->(y) (y/n) :: ")
             if bol == "y" or bol == "Y":
                 data2 = data[i].find("a")
@@ -74,40 +66,37 @@ def india_today(userInput, ua):
 
 
 if __name__ == "__main__":
-
-    bold_start = "\033[1m"
-    bold_end = "\033[0m"
     print("\033[5;31;40m")
     print(
-        bold_start,
+        BOLD_START,
         "                 HERE YOU WILL GET ALL THE NEWS JUST IN ONE SEARCH                   ",
-        bold_end,
+        BOLD_END,
     )
     print("\n")
     localtime = time.asctime(time.localtime(time.time()))
-    print(bold_start, localtime, bold_end)
+    print(BOLD_START, localtime, BOLD_END)
 
     ua = {
         "UserAgent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0"
     }
     print(
-        bold_start,
+        BOLD_START,
         "\n\033[1;35;40m Search any news (state , city ,Country , AnyThings etc) : ",
-        bold_end,
+        BOLD_END,
         end=" ",
     )
 
     userInput = input()
 
-    print(bold_start, "\033[1;33;40m \n")
+    print(BOLD_START, "\033[1;33;40m \n")
     print("Which news channel data would you prefer")
-    print("1. Times of india")
-    print("2. India's Today", bold_end)
+    print("1. Times of India")
+    print("2. India's Today", BOLD_END)
 
     say = int(input())
 
     if say == 1:
-        length = Times_of_India(userInput, ua)
+        length = times_of_india(userInput, ua)
         if length == 0:
             print("Sorry Here No News Available", "\N{expressionless face}")
             print("\n")
@@ -116,7 +105,7 @@ if __name__ == "__main__":
                 "\N{thinking face}",
                 end="  ",
             )
-            speak = input()
+            speak = input().strip().lower()
             if speak == "y":
                 length = india_today(userInput, ua)
                 if length == 0:
