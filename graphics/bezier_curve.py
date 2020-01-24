59694ce4361fe91caf69b31faaf65d7302676e93
fk03983 <fk03983@st.habib.edu.pk>
2020-01-24 16:07:45 -0600

    updated documentation for readablitity

--- graphics/bezier_curve.py
+++ graphics/bezier_curve.py
@@ -7,25 +7,29 @@ from scipy.special import comb
 
 class BezierCurve:
     """
-    A class to generate bezier curves for a given set of points. The implementation works only for 2d points.
+    Generate bezier curves for a given set of points.
+    The implementation works only for 2d points.
     """
 
-    def __init__(self, list_of_points: List[Tuple]):
+    def __init__(self, list_of_points: List[Tuple[float, float]]):
         """
         The constructor of the Bezier Curve class.
-            list_of_points: List of tuples on which to interpolate.
+            list_of_points: Data points in the xy plane on which to interpolate. These
+            basically serve as the control points (control the behavior of curve),
+            of the bezier curve.
         """
         self.list_of_points = list_of_points
         self.degree = (
-            len(list_of_points) - 1
-        )  # degree of the curve produced will be n - 1
+            len(list_of_points)
+            - 1  # determines the flexibility of the curve. Degree = 1 produces a straight line.
+        )
 
     def basis_function(self, t: float) -> List[float]:
         """
-        The function to cater the need for the basis function.
-            t: the 1-d co-ordinate at which to evaluate the basis function(s).
-            returns a returns list of floating point numbers, a number at index i represents the value of
-            basis function i at t.
+        Bezier Curve is a weighted sum of the control points. Weight of each control point, as
+            a function of time t is called the basis function.
+            t: time value between 0 and 1 inclusive at which to evaluate the basis of the curve.
+        returns the x, y values of basis function at time t
 
         >>> curve1 = BezierCurve([(1,1), (1,2)])
         >>> curve1.basis_function(0)
@@ -33,15 +37,18 @@ class BezierCurve:
         >>> curve1.basis_function(1)
         [0.0, 1.0]
         """
+        assert 0 <= t <= 1  # t must be between 0 and 1
         output_values: List[float] = list()
         for i in range(len(self.list_of_points)):
+            # basis function for each i
             output_values.append(
                 comb(self.degree, i) * ((1 - t) ** (self.degree - i)) * (t ** i)
-            )  # basis function for each i
+            )
 
         assert (
-            round(sum(output_values), 5) == 1
-        )  # the basis must sum up to 1 for it to produce a valid bezier curve.
+            round(sum(output_values), 5)
+            == 1  # the basis must sum up to 1 for it to produce a valid bezier curve.
+        )
         return output_values
 
     def bezier_curve_function(self, t: float) -> Tuple[float, float]:
@@ -49,7 +56,9 @@ class BezierCurve:
         The function to produce the values of the bezier curve at time t. At t = 0,
         it must return the first point, and at t = 1, it must return the last point.
             t: the value of t at which to evaluate the bezier function
-        returns a 2-d tuple of floats, representing the value of the bezier curve at the given t.
+        Returns the x, y coordinates of the Bezier curve at time t.
+            The first point in the curve is when t = 0.
+            The last point in the curve is when t = 1.
 
         >>> curve1 = BezierCurve([(1,1), (1,2)])
         >>> curve1.bezier_curve_function(0)
@@ -57,12 +66,14 @@ class BezierCurve:
         >>> curve1.bezier_curve_function(1)
         (1.0, 2.0)
         """
+
+        assert 0 <= t <= 1  # t must be between 0 and 1.
+
         basis_function = self.basis_function(t)
         x = 0.0
         y = 0.0
-        for i in range(
-            len(self.list_of_points)
-        ):  # summing up the product of i-th basis function and ith point, for all points
+        for i in range(len(self.list_of_points)):
+            # summing up the product of i-th basis function and i-th point, for all points.
             x += basis_function[i] * self.list_of_points[i][0]
             y += basis_function[i] * self.list_of_points[i][1]
 
@@ -71,8 +82,8 @@ class BezierCurve:
     def plot_curve(self, step_size: float = 0.01):
         """
         plots the bezier curve using matplotlib plotting function.
-            step_size: defines the step(s) at which to evaluate the bezier curve and plot. The smaller the step size,
-            the finer the curve produced.
+            step_size: defines the step(s) at which to evaluate the bezier curve and plot.
+            The smaller the step size, the finer the curve produced.
         """
         import matplotlib.pyplot as plt
 
@@ -89,8 +100,13 @@ class BezierCurve:
         x = [i[0] for i in self.list_of_points]
         y = [i[1] for i in self.list_of_points]
 
+        plt.plot(
+            to_plot_x,
+            to_plot_y,
+            color="blue",
+            label="Curve of Degree " + str(self.degree),
+        )
         plt.scatter(x, y, color="red", label="Control Points")
-        plt.plot(to_plot_x, to_plot_y, color="blue", label="Curve")
         plt.legend()
         plt.show()
 
@@ -100,8 +116,8 @@ if __name__ == "__main__":
 
     doctest.testmod()
 
-    curve1 = BezierCurve([(1, 2), (3, 5)])
-    curve1.plot_curve()
+    BezierCurve([(1, 2), (3, 5)]).plot_curve()  # degree 1
 
-    curve1 = BezierCurve([(0, 0), (5, 5), (5, 0)])
-    curve1.plot_curve()
+    BezierCurve([(0, 0), (5, 5), (5, 0)]).plot_curve()  # degree 2
+
+    BezierCurve([(0, 0), (5, 5), (5, 0), (2.5, -2.5)]).plot_curve()  # degree 3
