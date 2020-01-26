e0c1b062a3587a2a700a5d04f110a62e486cafb8
lanzhiwang <hzhilamp@163.com>
2020-01-26 18:56:59 +0800

    unified processing sum min max segment tre

--- data_structures/binary_tree/segment_tree_other.py
+++ data_structures/binary_tree/segment_tree_other.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Segment_tree creates a segment tree with a given array and function,
+allowing queries to be done later in log(N) time
+function takes 2 values and returns a same type value
+"""
 
 from queue import Queue
 from collections.abc import Sequence
@@ -18,9 +23,10 @@ class SegmentTreeNode(object):
         return 'val: %s, start: %s, end: %s' % (self.val, self.start, self.end)
 
 
-class NumArray:
-    """NumArray is sum tree of object. Parent node is sum of two child nodes.
-    >>> num_arr = NumArray([2, 1, 5, 3, 4])
+class SegmentTree(object):
+    """
+    >>> import operator
+    >>> num_arr = SegmentTree([2, 1, 5, 3, 4], operator.add)
     >>> for node in num_arr.traverse():
     ...     print(node)
     ...
@@ -48,77 +54,14 @@ class NumArray:
     val: 2, start: 0, end: 0
     val: 5, start: 1, end: 1
     >>>
-    >>> num_arr.sum_range(3, 4)
+    >>> num_arr.query_range(3, 4)
     7
-    >>> num_arr.sum_range(2, 2)
+    >>> num_arr.query_range(2, 2)
     5
-    >>> num_arr.sum_range(1, 3)
+    >>> num_arr.query_range(1, 3)
     13
     >>>
-    """
-    def __init__(self, collection: Sequence):
-        self.collection = collection
-        if self.collection:
-            self.root = self._build_tree(0, len(collection) - 1)
-
-    def update(self, i, val):
-        self._update_tree(self.root, i, val)
-
-    def sum_range(self, i, j):
-        return self._sum_range(self.root, i, j)
-
-    def _build_tree(self, start, end):
-        if start == end:
-            return SegmentTreeNode(start, end, self.collection[start])
-        mid = (start + end) // 2
-        left = self._build_tree(start, mid)
-        right = self._build_tree(mid + 1, end)
-        return SegmentTreeNode(start, end, left.val + right.val, left, right)
-
-    def _update_tree(self, root, i, val):
-        if root.start == i and root.end == i:
-            root.val = val
-            return
-        if i <= root.mid:
-            self._update_tree(root.left, i, val)
-        else:
-            self._update_tree(root.right, i, val)
-        root.val = root.left.val + root.right.val
-
-    def _sum_range(self, root, i, j):
-        if root.start == i and root.end == j:
-            return root.val
-        """
-         [i, j] [i, j] [i, j]
-        [start mid] [mid+1 end]
-        """
-        if j <= root.mid:
-            return self._sum_range(root.left, i, j)
-        elif i > root.mid:
-            return self._sum_range(root.right, i, j)
-        else:
-            return self._sum_range(root.left, i, root.mid) + self._sum_range(root.right, root.mid + 1, j)
-
-    def traverse(self):
-        result = []
-        if self.root is not None:
-            queue = Queue()
-            queue.put(self.root)
-            while not queue.empty():
-                node = queue.get()
-                result.append(node)
-
-                if node.left is not None:
-                    queue.put(node.left)
-
-                if node.right is not None:
-                    queue.put(node.right)
-            return result
-
-
-class MaxArray:
-    """MaxArray is max tree of object. Parent node is max of two child nodes.
-    >>> max_arr = MaxArray([2, 1, 5, 3, 4])
+    >>> max_arr = SegmentTree([2, 1, 5, 3, 4], max)
     >>> for node in max_arr.traverse():
     ...     print(node)
     ...
@@ -146,77 +89,14 @@ class MaxArray:
     val: 2, start: 0, end: 0
     val: 5, start: 1, end: 1
     >>>
-    >>> max_arr.max_range(3, 4)
+    >>> max_arr.query_range(3, 4)
     4
-    >>> max_arr.max_range(2, 2)
+    >>> max_arr.query_range(2, 2)
     5
-    >>> max_arr.max_range(1, 3)
+    >>> max_arr.query_range(1, 3)
     5
     >>>
-    """
-    def __init__(self, collection: Sequence):
-        self.collection = collection
-        if self.collection:
-            self.root = self._build_tree(0, len(collection) - 1)
-
-    def update(self, i, val):
-        self._update_tree(self.root, i, val)
-
-    def max_range(self, i, j):
-        return self._max_range(self.root, i, j)
-
-    def _build_tree(self, start, end):
-        if start == end:
-            return SegmentTreeNode(start, end, self.collection[start])
-        mid = (start + end) // 2
-        left = self._build_tree(start, mid)
-        right = self._build_tree(mid + 1, end)
-        return SegmentTreeNode(start, end, max([left.val, right.val]), left, right)
-
-    def _update_tree(self, root, i, val):
-        if root.start == i and root.end == i:
-            root.val = val
-            return
-        if i <= root.mid:
-            self._update_tree(root.left, i, val)
-        else:
-            self._update_tree(root.right, i, val)
-        root.val = max([root.left.val, root.right.val])
-
-    def _max_range(self, root, i, j):
-        if root.start == i and root.end == j:
-            return root.val
-        """
-         [i, j] [i, j] [i, j]
-        [start mid] [mid+1 end]
-        """
-        if j <= root.mid:
-            return self._max_range(root.left, i, j)
-        elif i > root.mid:
-            return self._max_range(root.right, i, j)
-        else:
-            return max([self._max_range(root.left, i, root.mid), self._max_range(root.right, root.mid + 1, j)])
-
-    def traverse(self):
-        result = []
-        if self.root is not None:
-            queue = Queue()
-            queue.put(self.root)
-            while not queue.empty():
-                node = queue.get()
-                result.append(node)
-
-                if node.left is not None:
-                    queue.put(node.left)
-
-                if node.right is not None:
-                    queue.put(node.right)
-            return result
-
-
-class MinArray:
-    """MinArray is min tree of object. Parent node is min of two child nodes.
-    >>> min_arr = MinArray([2, 1, 5, 3, 4])
+    >>> min_arr = SegmentTree([2, 1, 5, 3, 4], min)
     >>> for node in min_arr.traverse():
     ...     print(node)
     ...
@@ -244,24 +124,26 @@ class MinArray:
     val: 2, start: 0, end: 0
     val: 5, start: 1, end: 1
     >>>
-    >>> min_arr.min_range(3, 4)
+    >>> min_arr.query_range(3, 4)
     3
-    >>> min_arr.min_range(2, 2)
+    >>> min_arr.query_range(2, 2)
     5
-    >>> min_arr.min_range(1, 3)
+    >>> min_arr.query_range(1, 3)
     3
     >>>
+
     """
-    def __init__(self, collection: Sequence):
+    def __init__(self, collection: Sequence, function):
         self.collection = collection
+        self.fn = function
         if self.collection:
             self.root = self._build_tree(0, len(collection) - 1)
 
     def update(self, i, val):
         self._update_tree(self.root, i, val)
 
-    def min_range(self, i, j):
-        return self._min_range(self.root, i, j)
+    def query_range(self, i, j):
+        return self._query_range(self.root, i, j)
 
     def _build_tree(self, start, end):
         if start == end:
@@ -269,7 +151,7 @@ class MinArray:
         mid = (start + end) // 2
         left = self._build_tree(start, mid)
         right = self._build_tree(mid + 1, end)
-        return SegmentTreeNode(start, end, min([left.val, right.val]), left, right)
+        return SegmentTreeNode(start, end, self.fn(left.val, right.val), left, right)
 
     def _update_tree(self, root, i, val):
         if root.start == i and root.end == i:
@@ -279,9 +161,9 @@ class MinArray:
             self._update_tree(root.left, i, val)
         else:
             self._update_tree(root.right, i, val)
-        root.val = min([root.left.val, root.right.val])
+        root.val = self.fn(root.left.val, root.right.val)
 
-    def _min_range(self, root, i, j):
+    def _query_range(self, root, i, j):
         if root.start == i and root.end == j:
             return root.val
         """
@@ -289,11 +171,11 @@ class MinArray:
         [start mid] [mid+1 end]
         """
         if j <= root.mid:
-            return self._min_range(root.left, i, j)
+            return self._query_range(root.left, i, j)
         elif i > root.mid:
-            return self._min_range(root.right, i, j)
+            return self._query_range(root.right, i, j)
         else:
-            return min([self._min_range(root.left, i, root.mid), self._min_range(root.right, root.mid + 1, j)])
+            return self.fn(self._query_range(root.left, i, root.mid), self._query_range(root.right, root.mid + 1, j))
 
     def traverse(self):
         result = []
@@ -313,49 +195,20 @@ class MinArray:
 
 
 if __name__ == '__main__':
-    print('求和线段树')
-    num_arr = NumArray([2, 1, 5, 3, 4])
-    for node in num_arr.traverse():
-        print(node)
-    print()
+    import operator
+    for fn in [operator.add, max, min]:
+        print('*' * 50)
+        arr = SegmentTree([2, 1, 5, 3, 4], fn)
+        for node in arr.traverse():
+            print(node)
+        print()
 
-    num_arr.update(1, 5)
-    for node in num_arr.traverse():
-        print(node)
-    print()
+        arr.update(1, 5)
+        for node in arr.traverse():
+            print(node)
+        print()
 
-    print(num_arr.sum_range(3, 4))  # 7
-    print(num_arr.sum_range(2, 2))  # 5
-    print(num_arr.sum_range(1, 3))  # 13
-
-    print()
-    print('求最大值线段树')
-    max_arr = MaxArray([2, 1, 5, 3, 4])
-    for node in max_arr.traverse():
-        print(node)
-    print()
-
-    max_arr.update(1, 5)
-    for node in max_arr.traverse():
-        print(node)
-    print()
-
-    print(max_arr.max_range(3, 4))  # 4
-    print(max_arr.max_range(2, 2))  # 5
-    print(max_arr.max_range(1, 3))  # 5
-
-    print()
-    print('求最小值线段树')
-    min_arr = MinArray([2, 1, 5, 3, 4])
-    for node in min_arr.traverse():
-        print(node)
-    print()
-
-    min_arr.update(1, 5)
-    for node in min_arr.traverse():
-        print(node)
-    print()
-
-    print(min_arr.min_range(3, 4))  # 3
-    print(min_arr.min_range(2, 2))  # 5
-    print(min_arr.min_range(1, 3))  # 3
+        print(arr.query_range(3, 4))  # 7
+        print(arr.query_range(2, 2))  # 5
+        print(arr.query_range(1, 3))  # 13
+        print()
