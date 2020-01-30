3a5271d8ef38ebaacd555c97410c0c075da66674
faizan2700 <syedfaizan824@gmail.com>
2020-01-30 22:43:09 +0530

    manacher algorithm updated

--- strings/manacher.py
+++ strings/manacher.py
@@ -5,14 +5,14 @@ def palindromic_string(input_string):
     >>> palindromic_string('ababa')
     'ababa'
 
-    Manacher’s algorithm which finds Longest Palindromic Substring in linear time.
+    Manacher’s algorithm which finds Longest palindromic Substring in linear time.
 
     1. first this convert input_string("xyx") into new_string("x|y|x") where odd
         positions are actual input characters.
     2. for each character in new_string it find corresponding length and store the length
         and l,r to store previously calculated info.(please look the explanation for details)
         
-    3. return correspoding output_string by removing all "|"
+    3. return corresponding output_string by removing all "|"
     """
     max_length = 0
 
@@ -79,24 +79,24 @@ i) we have stored the length of palindromic substring which has center at a3 (st
     is the furthest ending till now, and it has ending after a6
 ii) a2 and a4 are equally distant from a3 so char(a2) == char(a4)
 iii) a0 and a6 are equally distant from a3 so char(a0) == char(a6)
-iv) a1 is corresponding equal character of a5 in palindrom with center a3 (remember that in below derivation of a4==a6)
+iv) a1 is corresponding equal character of a5 in palindrome with center a3 (remember that in below derivation of a4==a6)
 
 now for a5 we will calculate the length of palindromic substring with center as a5 but can we use previously
 calculated information in some way?
-Yes, look the above string we know that a5 is inside the palindrom with center a3 and previously we have
+Yes, look the above string we know that a5 is inside the palindrome with center a3 and previously we have
 have calculated that
-a0==a2 (palindrom of center a1)
-a2==a4 (palindrom of center a3)
-a0==a6 (palindrom of center a3)
+a0==a2 (palindrome of center a1)
+a2==a4 (palindrome of center a3)
+a0==a6 (palindrome of center a3)
 so a4==a6
 
-so we can say that palindrom at center a5 is at least as long as palindrom at center a1
-but this only holds if a0 and a6 are inside the limits of palindrom centered at a3 so finally ..
+so we can say that palindrome at center a5 is at least as long as palindrome at center a1
+but this only holds if a0 and a6 are inside the limits of palindrome centered at a3 so finally ..
 
-len_of_palindrom__at(a5) = min(len_of_palindrom_at(a1), r-a5)
+len_of_palindrome__at(a5) = min(len_of_palindrome_at(a1), r-a5)
 where a3 lies from l to r and we have to keep updating that
 
-and if the a5 lies outside of l,r boundary we calculate length of palindrom with bruteforce and update
+and if the a5 lies outside of l,r boundary we calculate length of palindrome with bruteforce and update
 l,r.
 
 it gives the linear time complexity just like z-function
