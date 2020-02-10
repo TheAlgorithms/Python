de6e494bfd8bee9ae598a340c2c332ac063a4703
billpaps <37051006+billpaps@users.noreply.github.com>
2020-02-10 23:27:35 +0200

    Update bisection.py

--- maths/bisection.py
+++ maths/bisection.py
@@ -1,24 +1,20 @@
-"""Given a function on floating point number f(x) and two numbers ‘a’ and ‘b’
+"""Given a function f(x) on floating number x and two numbers ‘a’ and ‘b’
 such that f(a)*f(b) < 0 and f(x) is continuous in [a, b]. Here f(x) represents
-algebraic or transcendental equation.
-Find root of the function in the interval [a, b] (Or find a value of x such
+ algebraic or transcendental equation.
+Find root of function in interval [a, b] (Or find a value of x such
 that f(x) is 0)
-
-https://en.wikipedia.org/wiki/Bisection_method
 """
-def equation(x: int) -> int:
+import doctest
+
+def equation(x: float) -> float:
     """
-    >>> equation(5)
-    -15
     >>> equation(0)
     10
-    >>> equation(-5)
-    -15
+
     """
     return 10 - x * x
 
-
-def bisection(a: int, b: int) -> float:
+def bisection(a: float, b: float) -> float:
     """
     >>> bisection(-2, 5)
     3.1611328125
@@ -49,8 +45,7 @@ def bisection(a: int, b: int) -> float:
 
 
 if __name__ == '__main__':
-    import doctest
-
-    doctest.testmod()
     print(bisection(-2, 5))
     print(bisection(0, 6))
+
+    doctest.testmod()
