8c6b55c72bedc977c2da49e2e9bd0992f1848e5d
billpaps <37051006+billpaps@users.noreply.github.com>
2020-02-10 22:07:30 +0200

    Update bisection.py

--- maths/bisection.py
+++ maths/bisection.py
@@ -1,43 +1,43 @@
 """Given a function f(x) on floating number x and two numbers ‘a’ and ‘b’
-such that f(a)*f(b) < 0 and f(x) is continuous in [a, b]. Here f(x) represents algebraic or transcendental equation.
-Find root of function in interval [a, b] (Or find a value of x such that f(x) is 0)
+such that f(a)*f(b) < 0 and f(x) is continuous in [a, b]. Here f(x) represents
+ algebraic or transcendental equation.
+Find root of function in interval [a, b] (Or find a value of x such
+that f(x) is 0)
 """
 import doctest
 
-
 def equation(x: int) -> int:
-    return 10 - x * x
+    """
+    >>> equation(0)
+    10
 
+    """
+    return 10 - x * x
 
 def bisection(a: int, b: int) -> float:
     """
-    >>> bisection(-2,5)
+    >>> bisection(-2, 5)
     3.1611328125
     >>> bisection(0, 6)
     3.158203125
-    >>> bisection(2,3)
+    >>> bisection(2, 3)
     False
     """
     # Bolzano theory in order to find if there is a root between a and b
     if equation(a) * equation(b) >= 0:
         return False
-
     c = a
     while (b - a) >= 0.01:
-
         # Find middle point
         c = (a + b) / 2
-
         # Check if middle point is root
         if equation(c) == 0.0:
             break
-
         # Decide the side to repeat the steps
         if equation(c) * equation(a) < 0:
             b = c
         else:
             a = c
-
     return c
 
 
