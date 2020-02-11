786079ef93af7415ae742a6d647da0f219d32336
QuantumNovice <43876848+QuantumNovice@users.noreply.github.com>
2020-02-11 11:51:48 +0500

    Update and rename RayleighQuotient.py to rayleigh_quotient.py

--- linear_algebra/src/rayleigh_quotient.py
+++ linear_algebra/src/rayleigh_quotient.py
@@ -1,11 +1,11 @@
-'''
+"""
 https://en.wikipedia.org/wiki/Rayleigh_quotient
-'''
+"""
 import numpy as np
 
 
 def is_hermitian(matrix) -> bool:
-    '''
+    """
     Checks if a matrix is Hermitian.
 
     >>> import numpy as np
@@ -15,11 +15,18 @@ def is_hermitian(matrix) -> bool:
     ... [4,    -1j,  1]])
     >>> is_hermitian(A)
     True
-    '''
+    >>> A = np.matrix([
+    ... [2,    2+1j, 4+1j],
+    ... [2-1j,  3,  1j],
+    ... [4,    -1j,  1]])
+    >>> is_hermitian(A)
+    False
+    """
     return np.array_equal(matrix, matrix.H)
 
+
 def rayleigh_quotient(A, v) -> float:
-    '''
+    """
     Returns the Rayleigh quotient of a Hermitian matrix A and
     vector v.
     >>> import numpy as np
@@ -35,36 +42,26 @@ def rayleigh_quotient(A, v) -> float:
     ... ])
     >>> rayleigh_quotient(A, v)
     matrix([[3.]])
-    '''
+    """
     v_star = v.H
-    return (v_star*A*v)/(v_star*v)
+    return (v_star * A * v) / (v_star * v)
 
 
 def tests() -> None:
-    A = np.matrix([
-    [2,    2+1j, 4],
-    [2-1j,  3,  1j],
-    [4,    -1j,  1]
-    ])
+    A = np.matrix([[2, 2 + 1j, 4], [2 - 1j, 3, 1j], [4, -1j, 1]])
 
-    v = np.matrix([
-    [1],
-    [2],
-    [3]
-    ])
+    v = np.matrix([[1], [2], [3]])
 
     assert is_hermitian(A) == True
-    print( rayleigh_quotient(A, v))
+    print(rayleigh_quotient(A, v))
 
-    A = np.matrix([
-    [1,  2, 4],
-    [2,  3,  -1],
-    [4, -1,  1]
-    ])
+    A = np.matrix([[1, 2, 4], [2, 3, -1], [4, -1, 1]])
     assert is_hermitian(A) == True
     assert rayleigh_quotient(A, v) == float(3)
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     import doctest
+
     doctest.testmod()
     tests()
