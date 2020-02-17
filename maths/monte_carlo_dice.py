e2d145bc1b9d961176e13bd91ea49bb42a008463
Naveen M V <naveenmv7@gmail.com>
2020-02-17 11:17:03 +0530

    Add tests to throw_dice

--- maths/monte_carlo_dice.py
+++ maths/monte_carlo_dice.py
@@ -1,35 +1,47 @@
 import random
 
-"""
 
-"""
-
-class Dice():
+class Dice:
     NUM_SIDES = 6
+
     def __init__(self):
-        self.sides = []
-        for i in range(1, 7):
-            self.sides.append(i)
-        self.odds = len(self.sides)
-    
+        self.sides = list(range(1, Dice.NUM_SIDES + 1))
+
     def roll(self):
         return random.choice(self.sides)
-    
+
     def _str_(self):
-        return 'Fair Dice'
+        return "Fair Dice"
 
-def play_dice(num_dice, num_rolls):
+
+def throw_dice(num_throws, num_dice=2):
+    """
+    Return probability list of all possible sums when throwing dice.
+
+    >>> random.seed(0)
+    >>> throw_dice(10, 1)
+    [10.0, 0.0, 30.0, 50.0, 10.0, 0.0]
+    >>> throw_dice(100, 1)
+    [19.0, 17.0, 17.0, 11.0, 23.0, 13.0]
+    >>> throw_dice(1000, 1)
+    [18.8, 15.5, 16.3, 17.6, 14.2, 17.6]
+    >>> throw_dice(10000, 1)
+    [16.35, 16.89, 16.93, 16.6, 16.52, 16.71]
+    >>> throw_dice(10000, 2)
+    [2.74, 5.6, 7.99, 11.26, 13.92, 16.7, 14.44, 10.63, 8.05, 5.92, 2.75]
+    """
+
     dices = [Dice() for i in range(num_dice)]
-    occurrence = [0 for i in range(0, len(dices)*Dice.NUM_SIDES+1)]
+    count_of_sum = [0] * (len(dices) * Dice.NUM_SIDES + 1)
 
-    for i in range(num_rolls):
-        occurrence[sum([dice.roll() for dice in dices])] += 1
+    for i in range(num_throws):
+        count_of_sum[sum([dice.roll() for dice in dices])] += 1
 
-    probability = [count/num_rolls for count in occurrence]
-    print(f"Number of rolls = {num_rolls}") 
-    for idx, val in enumerate(probability):
-        print(f"{idx} {round(val*100, 2)}%")
+    probability = [round((count * 100) / num_throws, 2) for count in count_of_sum]
+    return probability[num_dice:]  # remove counts of sums that never appear
 
-play_dice(1, 100000)
-play_dice(2, 100000)
-play_dice(3, 100000)
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
