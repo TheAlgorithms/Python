f96ba4522daf2cee569211dca7e0e0a136a61aba
Praveen <praveennadiminti@gmail.com>
2020-02-24 08:13:51 +0530

    changed variable names

--- digital_image_processing/filters/bilateral_filter.py
+++ digital_image_processing/filters/bilateral_filter.py
@@ -16,38 +16,48 @@ import math
 import sys
 
 
-def vec_gaussian(img: np.ndarray, var: float) -> np.ndarray:
+def vec_gaussian(img: np.ndarray, variance: float) -> np.ndarray:
     # For applying gaussian function for each element in matrix.
-    sigma = math.sqrt(var)
+    sigma = math.sqrt(variance)
     cons = 1 / (sigma * math.sqrt(2 * math.pi))
     fImg = cons * np.exp(-((img / sigma) ** 2) * 0.5)
     return fImg
 
 
-def getSlice(img: np.ndarray, x: int, y: int, N: int) -> np.ndarray:
-    return img[x - N // 2 : x + N // 2 + 1, y - N // 2 : y + N // 2 + 1]
+def getSlice(img: np.ndarray, x: int, y: int, kernel_size: int) -> np.ndarray:
+    return img[
+        x - kernel_size // 2 : x + kernel_size // 2 + 1,
+        y - kernel_size // 2 : y + kernel_size // 2 + 1,
+    ]
 
 
-def getGaussKernel(N: int, varS: float) -> np.ndarray:
+def getGaussKernel(kernel_size: int, spatial_variance: float) -> np.ndarray:
     # Creates a gaussian kernel of given dimension.
-    arr = np.zeros((N, N))
-    for i in range(0, N):
-        for j in range(0, N):
-            arr[i, j] = math.sqrt(abs(i - N // 2) ** 2 + abs(j - N // 2) ** 2)
-    arr = vec_gaussian(arr, varS)
+    arr = np.zeros((kernel_size, kernel_size))
+    for i in range(0, kernel_size):
+        for j in range(0, kernel_size):
+            arr[i, j] = math.sqrt(
+                abs(i - kernel_size // 2) ** 2 + abs(j - kernel_size // 2) ** 2
+            )
+    arr = vec_gaussian(arr, spatial_variance)
     return arr
 
 
-def bilateral_filter(img: np.ndarray, varS: float, varI: float, N: int) -> np.ndarray:
+def bilateral_filter(
+    img: np.ndarray,
+    spatial_variance: float,
+    intensity_variance: float,
+    kernel_size: int,
+) -> np.ndarray:
     img2 = np.zeros(img.shape)
-    gaussKer = getGaussKernel(N, varS)
+    gaussKer = getGaussKernel(kernel_size, spatial_variance)
     sizeX, sizeY = img.shape
-    for i in range(N // 2, sizeX - N // 2):
-        for j in range(N // 2, sizeY - N // 2):
+    for i in range(kernel_size // 2, sizeX - kernel_size // 2):
+        for j in range(kernel_size // 2, sizeY - kernel_size // 2):
 
-            imgS = getSlice(img, i, j, N)
-            imgI = imgS - imgS[N // 2, N // 2]
-            imgIG = vec_gaussian(imgI, varI)
+            imgS = getSlice(img, i, j, kernel_size)
+            imgI = imgS - imgS[kernel_size // 2, kernel_size // 2]
+            imgIG = vec_gaussian(imgI, intensity_variance)
             weights = np.multiply(gaussKer, imgIG)
             vals = np.multiply(imgS, weights)
             val = np.sum(vals) / np.sum(weights)
@@ -57,25 +67,25 @@ def bilateral_filter(img: np.ndarray, varS: float, varI: float, N: int) -> np.nd
 
 if __name__ == "__main__":
     filename = "../image_data/lena.jpg"
-    varS = 1.0
-    varI = 1.0
-    N = 5
+    spatial_variance = 1.0
+    intensity_variance = 1.0
+    kernel_size = 5
     if len(sys.argv) >= 2:
         filename = sys.argv[1]
     if len(sys.argv) >= 3:
-        varS = float(sys.argv[2])
+        spatial_variance = float(sys.argv[2])
     if len(sys.argv) >= 4:
-        varI = float(sys.argv[3])
+        intensity_variance = float(sys.argv[3])
     if len(sys.argv) >= 5:
-        N = int(sys.argv[4])
-        N = N + abs(N % 2 - 1)
+        kernel_size = int(sys.argv[4])
+        kernel_size = kernel_size + abs(kernel_size % 2 - 1)
 
     img = cv2.imread(filename, 0)
     cv2.imshow("input image", img)
 
     out = img / 255
     out = out.astype("float32")
-    out = bilateral_filter(out, varS, varI, N)
+    out = bilateral_filter(out, spatial_variance, intensity_variance, kernel_size)
     out = out * 255
     out = np.uint8(out)
     cv2.imshow("output image", out)
