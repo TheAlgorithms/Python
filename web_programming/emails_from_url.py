090cd66ef2006c2f2ef6514aa05eb420e6fc63f4
vinayak <itssvinayak@gmail.com>
2020-02-25 11:08:49 +0530

    Update emails_from_url.py

--- web_programming/emails_from_url.py
+++ web_programming/emails_from_url.py
@@ -6,16 +6,17 @@ __maintainer__ = "Muhammad Umer Farooq"
 __email__ = "contact@muhammadumerfarooq.me"
 __status__ = "Production"
 
-from html.parser import HTMLParser
-import requests
 import re
-from urllib.parse import urlparse
-
+import requests
 from urllib import parse
+from html.parser import HTMLParser
 
 
 class Parser(HTMLParser):
-
+    ''' class use to parse HTML
+         handle_starttag  function takes url from
+         anchor tag
+    '''
     def __init__(self, domain):
         HTMLParser.__init__(self)
         self.data = []
@@ -36,62 +37,51 @@ class Parser(HTMLParser):
 
 # Get main domain name (example.com)
 def get_domain_name(url):
-    try:
-        u = get_sub_domain_name(url).split('.')
-        return u[-2] + '.' + u[-1]
-    except:
-        return ""
+    return '.'.join(get_sub_domain_name(url).split('.')[-2:])
 
 
 # Get sub domain name (sub.example.com)
 def get_sub_domain_name(url):
-    try:
-        return urlparse(url).netloc
-    except:
-        return ''
+        return parse.urlparse(url).netloc
 
-# Get the url
-url = "https://github.com"
-# Get the base domain from the url
-domain = get_domain_name(url)
+if __name__ == '__main__':
+    # Get the url
+    url = "https://github.com"
+    # Get the base domain from the url
+    domain = get_domain_name(url)
 
-# Initialize the parser
-parser = Parser(domain)
+    # Initialize the parser
+    parser = Parser(domain)
 
-# Validate Email regx.
-emailRegx = '[a-zA-Z0-9]+@' + domain
-try:
+    # Validate Email regx.
+    emailRegx = '[a-zA-Z0-9]+@' + domain
+
     # Open URL
     r = requests.get(url)
-except:
-    print("Please provide the valid url")
 
-# pass the raw HTML to the parser to get links
-parser.feed(r.text)
+    # pass the raw HTML to the parser to get links
+    parser.feed(r.text)
 
-# Store Email Data structure.
-Emails = []
-# Get links and loop through
-for link in parser.data:
-    # open URL.
-    # read = requests.get(link)
-    try:
+    # Store Email Data structure.
+    Emails = []
+    # Get links and loop through
+    for link in parser.data:
+        # open URL.
+        # read = requests.get(link)
         read = requests.get(link)
         # Get the valid email.
         email = re.findall(emailRegx, read.text)
         # If not in list then append it.
         if email not in Emails:
             Emails.append(email)
-    except:
-        pass
 
-ValidEmails = []
+    ValidEmails = []
 
-# Remove duplicates email address.
-for Email in Emails:
-    for e in Email:
-        if e not in ValidEmails:
-            ValidEmails.append(e)
+    # Remove duplicates email address.
+    for Email in Emails:
+        for e in Email:
+            if e not in ValidEmails:
+                ValidEmails.append(e)
 
-# Finally print list of email.
-print(ValidEmails)
+    # Finally print list of email.
+    print(ValidEmails)
