3e840390bc771df7a7746f257ce5e04174718b7e
matkosoric <soric.matko@gmail.com>
2020-03-04 13:11:03 +0100

    import dependency - bug fix

--- scripts/validate_filenames.py
+++ scripts/validate_filenames.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 import os
-from build_directory_md import good_filepaths
+from build_directory_md import good_file_paths
 
-filepaths = list(good_filepaths())
-assert filepaths, "good_filepaths() failed!"
+filepaths = list(good_file_paths())
+assert filepaths, "good_file_paths() failed!"
 
 
 upper_files = [file for file in filepaths if file != file.lower()]
