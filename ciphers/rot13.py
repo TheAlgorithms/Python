7d724031602689942dadeb913bc27ec8342d6727
Christian Clauss <cclauss@me.com>
2020-03-08 06:58:18 +0100

    Type hints, doctests, URL to Wikipedia

--- ciphers/rot13.py
+++ ciphers/rot13.py
@@ -1,9 +1,19 @@
-def dencrypt(s, n):
+def dencrypt(s: str, n: int=13):
+    """
+    https://en.wikipedia.org/wiki/ROT13
+
+    >>> msg = "My secret bank account number is 173-52946 so don't tell anyone!!"
+    >>> s = dencrypt(msg)
+    >>> s
+    "Zl frperg onax nppbhag ahzore vf 173-52946 fb qba'g gryy nalbar!!"
+    >>> dencrypt(s) == msg
+    True
+    """
     out = ""
     for c in s:
-        if c >= "A" and c <= "Z":
+        if "A" <= c <= "Z":
             out += chr(ord("A") + (ord(c) - ord("A") + n) % 26)
-        elif c >= "a" and c <= "z":
+        elif "a" <= c <= "z":
             out += chr(ord("a") + (ord(c) - ord("a") + n) % 26)
         else:
             out += c
@@ -14,10 +24,10 @@ def main():
     s0 = input("Enter message: ")
 
     s1 = dencrypt(s0, 13)
-    print("Encryption: "+s1)  
+    print("Encryption:", s1)  
 
     s2 = dencrypt(s1, 13)
-    print("Decryption: "+s2)  
+    print("Decryption: ", s2)  
 
 
 if __name__ == "__main__":
