e342b57a4346fa044026ecccf16c5a5d5d1a2b50
Vaibhav Singh <45447817+itsvaibhav01@users.noreply.github.com>
2020-03-26 11:25:31 +0530

    Updated to linear time complex solution.
    Extra detail added for two different possible solutions, one Naive and another using stacks.

--- data_structures/stacks/next_greater_element.py
+++ data_structures/stacks/next_greater_element.py
@@ -7,17 +7,29 @@ def printNGE(arr):
     13 -- 21
     21 -- -1
     3 -- -1
+    
+    Naive way to solve this is to take two loops and check for the next bigger number but that will make the
+    time complexity as O(n^2). The better way to solve this would be to use a stack to keep track of maximum
+    number givig a linear time complex solution.
+    
     """
-    for i in range(0, len(arr), 1):
-
-        next = -1
-        for j in range(i + 1, len(arr), 1):
-            if arr[i] < arr[j]:
-                next = arr[j]
-                break
-
-        print(str(arr[i]) + " -- " + str(next))
-
+    stack = []              
+    result = [-1]*len(arr)
+    
+    for index in reversed(range(len(arr))):
+        if len(stack):
+            while stack[-1] < arr[index]:
+                stack.pop()
+                if len(stack) == 0:
+                    break
+                    
+        if len(stack) != 0:
+            result[index] = stack[-1]
+            
+        stack.append(arr[index])
+        
+    for x in range(len(arr)):
+        print("{} -- {}".format(arr[x], result[x]))
 
 # Driver program to test above function
 arr = [11, 13, 21, 3]
