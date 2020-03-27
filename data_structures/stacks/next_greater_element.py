e767f9c26a4968a971f635019d98db3aa2e64fb9
Christian Clauss <cclauss@me.com>
2020-03-27 06:30:41 +0100

    Add benchmark using timeit
    https://docs.python.org/3/library/timeit.html
    
    The performance delta between these two implementation is quite small...
    ```
    next_greatest_element_slow(): 1.843442126
         next_greatest_element(): 1.828941414
    ```

--- data_structures/stacks/next_greater_element.py
+++ data_structures/stacks/next_greater_element.py
@@ -2,7 +2,7 @@ def next_greatest_element_slow(arr):
     """
     Function to get Next Greatest Element (NGE) pair for all elements of list
     Maximum element present afterwards the current one which is also greater than current one
-    >>> next_greatest_element_slow([11,13,21,3])
+    >>> next_greatest_element_slow([11, 13, 21, 3])
     [13, 21, -1, -1]
     """
     result = []
@@ -26,7 +26,7 @@ def next_greatest_element(arr):
     time complexity as O(n^2). The better way to solve this would be to use a stack to keep track of maximum
     number givig a linear time complex solution.
     
-    >>> next_greatest_element([11,13,21,3])
+    >>> next_greatest_element([11, 13, 21, 3])
     [13, 21, -1, -1]
     """
     stack = []              
@@ -47,8 +47,16 @@ def next_greatest_element(arr):
     return result
 
 
+if __name__ == "__main__":
+    from timeit import timeit
 
-# Driver program to test above function
-arr = [11, 13, 21, 3]
-next_greatest_element_slow(arr)
-next_greatest_element(arr)
+    arr = [11, 13, 21, 3]
+    print(next_greatest_element_slow(arr))
+    print(next_greatest_element(arr))
+
+    setup = ("from __main__ import arr, next_greatest_element_slow, "
+             "next_greatest_element")
+    print("next_greatest_element_slow():",
+          timeit("next_greatest_element_slow(arr)", setup=setup))
+    print("     next_greatest_element():",
+          timeit("next_greatest_element(arr)", setup=setup))
