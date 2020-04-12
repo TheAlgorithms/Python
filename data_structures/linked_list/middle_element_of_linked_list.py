b80618dab47bdfb847712eafcdcf4995b2035c45
Christian Clauss <cclauss@me.com>
2020-04-12 16:36:25 +0200

    Whack the trailing whitespace

--- data_structures/linked_list/middle_element_of_linked_list.py
+++ data_structures/linked_list/middle_element_of_linked_list.py
@@ -1,16 +1,19 @@
 class Node:
-    def __init__(self, data:int) -> int:  
-        self.data = data  
+    def __init__(self, data: int) -> int:
+        self.data = data
         self.next = None
 
+
 class LinkedList:
-    def __init__(self): 
+    def __init__(self):
         self.head = None
+
     def push(self, new_data:int) -> int:
         new_node = Node(new_data) 
         new_node.next = self.head 
         self.head = new_node
         return self.head.data
+
     def middle_element(self) -> int:
         '''
         >>> link = LinkedList()
@@ -44,19 +47,18 @@ class LinkedList:
         '''
         slow_pointer = self.head
         fast_pointer = self.head
-        if self.head is not None: 
-            while (fast_pointer is not None and fast_pointer.next is not None): 
+        if self.head: 
+            while fast_pointer and fast_pointer.next: 
                 fast_pointer = fast_pointer.next.next
                 slow_pointer = slow_pointer.next
             return slow_pointer.data
         else:
             print("No element found.")
-            
+
+
 if __name__ == "__main__":
     link = LinkedList()
-    N = int(input().strip())
-
-    for i in range(N):
+    for i in range(int(input().strip())):
         data = int(input().strip())
         link.push(data)
     print(link.middle_element())
