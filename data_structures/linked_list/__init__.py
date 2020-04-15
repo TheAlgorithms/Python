1cd0e54478b99dd17d68461b900add135ccc1794
Lakshmikanth2001 <52835045+Lakshmikanth2001@users.noreply.github.com>
2020-04-15 23:48:11 +0530

    Update __init__.py
    please add a function to get length of linked list

--- data_structures/linked_list/__init__.py
+++ data_structures/linked_list/__init__.py
@@ -2,6 +2,7 @@ class Node:
     def __init__(self, item, next):
         self.item = item
         self.next = next
+        self.size=0
 
 class LinkedList:
     def __init__(self):
@@ -9,6 +10,7 @@ class LinkedList:
 
     def add(self, item):
         self.head = Node(item, self.head)
+        self.size+=1
 
     def remove(self):
         if self.is_empty():
@@ -16,7 +18,10 @@ class LinkedList:
         else:
             item = self.head.item
             self.head = self.head.next
+            self.size-=1
             return item
 
     def is_empty(self):
         return self.head is None
+    def size(self):
+        return self.size
