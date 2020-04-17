398d00be274d4e740ac4d76a87b1c7823e68ce98
John Law <johnlaw.po@gmail.com>
2020-04-17 19:03:48 +0200

    Add doctest for DFS

--- graphs/dfs.py
+++ graphs/dfs.py
@@ -21,6 +21,15 @@ def depth_first_search(graph: Dict, start: str) -> Set[int]:
        :param graph: directed graph in dictionary format
        :param vertex: starting vectex as a string
        :returns: the trace of the search
+       >>> G = { "A": ["B", "C", "D"], "B": ["A", "D", "E"], 
+       ... "C": ["A", "F"], "D": ["B", "D"], "E": ["B", "F"],
+       ... "F": ["C", "E", "G"], "G": ["F"] }
+       >>> start = "A"
+       >>> output_G = list({'A', 'B', 'C', 'D', 'E', 'F', 'G'})
+       >>> all(x in output_G for x in list(depth_first_search(G, "A")))
+       True
+       >>> all(x in output_G for x in list(depth_first_search(G, "G")))
+       True
     """
     explored, stack = set(start), [start]
     while stack:
@@ -33,14 +42,11 @@ def depth_first_search(graph: Dict, start: str) -> Set[int]:
     return explored
 
 
-G = {
-    "A": ["B", "C"],
-    "B": ["A", "D", "E"],
-    "C": ["A", "F"],
-    "D": ["B"],
-    "E": ["B", "F"],
-    "F": ["C", "E"],
-}
+G = { "A": ["B", "C", "D"], "B": ["A", "D", "E"], 
+      "C": ["A", "F"], "D": ["B", "D"], "E": ["B", "F"],
+      "F": ["C", "E", "G"], "G": ["F"] }
 
 if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
     print(depth_first_search(G, "A"))
