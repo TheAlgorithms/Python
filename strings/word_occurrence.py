098be3594bb65d3d19cb4bd1f3394ee522303b16
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-04-21 20:58:54 +0530

    fix: space count in strings/word_occurrence.py (#1896)
    * fix: space count in strings/word_occurrence.py
    
    * Update strings/word_occurrence.py
    
    Co-Authored-By: Christian Clauss <cclauss@me.com>
    
    * Update strings/word_occurrence.py
    
    Co-Authored-By: Christian Clauss <cclauss@me.com>
    
    * Update strings/word_occurrence.py
    
    Co-Authored-By: Christian Clauss <cclauss@me.com>
    
    * Update word_occurrence.py
    
    Seems like, there is no need o `occurrence.pop('', None)`
    
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- strings/word_occurrence.py
+++ strings/word_occurrence.py
@@ -1,4 +1,5 @@
 # Created by sarathkaul on 17/11/19
+# Modified by Arkadip Bhattacharya(@darkmatter18) on 20/04/2020
 from collections import defaultdict
 
 
@@ -10,10 +11,12 @@ def word_occurence(sentence: str) -> dict:
     >>> all(occurence_dict[word] == count for word, count
     ...     in Counter(SENTENCE.split()).items())
     True
+    >>> dict(word_occurence("Two  spaces"))
+    {'Two': 1, 'spaces': 1}
     """
     occurrence = defaultdict(int)
     # Creating a dictionary containing count of each word
-    for word in sentence.split(" "):
+    for word in sentence.split():
         occurrence[word] += 1
     return occurrence
 
