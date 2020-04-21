b7c1a93a8ba2802b311796970d291454a5d21805
siva1098 <32545976+siva1098@users.noreply.github.com>
2020-04-21 15:02:35 +0530

    Update frequent_pattern_graph_miner.py

--- graphs/frequent_pattern_graph_miner.py
+++ graphs/frequent_pattern_graph_miner.py
@@ -38,7 +38,7 @@ def get_bitcode(edge_array: List[List[str]],distinct_edge: List[str]) -> str:
     '''
     Return bitcode of distinct_edge
     >>> get_bitcode(edge_array, get_distinct_edge(edge_array)[0])
-    '11111'
+    '11101'
     '''
     bitcode=['0'] * len(edge_array)
 
