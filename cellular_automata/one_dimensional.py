ccc0b48e06b89e484fb51fbea8de7ea7dcc4ccb7
Christian Clauss <cclauss@me.com>
2020-04-26 02:39:20 +0200

    Update one_dimensional.py

--- cellular_automata/one_dimensional.py
+++ cellular_automata/one_dimensional.py
@@ -1,95 +1,79 @@
-from PIL import Image
-'''
-Returns an image of 16 generations of
-one-dimensional cellular automata 
-based on a given ruleset number
+"""
+Return an image of 16 generations of one-dimensional cellular automata based on a given
+ruleset number
 https://mathworld.wolfram.com/ElementaryCellularAutomaton.html
-'''
-def format_ruleset(ruleset: int) -> list:
-    '''
+"""
+
+from typing import List
+
+from PIL import Image
+
+# Define the first generation of cells
+# fmt: off
+CELLS = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+# fmt: on
+
+
+def format_ruleset(ruleset: int) -> List[int]:
+    """
     >>> format_ruleset(11100)
     [0, 0, 0, 1, 1, 1, 0, 0]
     >>> format_ruleset(0)
     [0, 0, 0, 0, 0, 0, 0, 0]
     >>> format_ruleset(11111111)
     [1, 1, 1, 1, 1, 1, 1, 1]
-    '''
+    """
     return [int(c) for c in f"{ruleset:08}"[:8]]
 
-# Defines the first generation of cells
 
-CELLS = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-          0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,]]
-
-# Mainloop
-
-def new_generation(CELLS,RULE,time):
-    new_row = []
-
+def new_generation(cells: List[List[int]], rule: List[int], time: int) -> List[int]:
+    next_generation = []
     for i in range(31):
-
-        # Gets neighbors of a cell
-
-        # If leftmost cell is in consideration
-
-        if i == 0:
+        # Get the neighbors of each cell
+        if i == 0:  # leftmost cell
             left_neighbor = 0
-            right_neighbor = CELLS[time][i + 1]
-        elif i == 30:
-
-        # If rightmost cell is in consideration
-
-            left_neighbor = CELLS[time][i - 1]
+            right_neighbor = cells[time][i + 1]
+        elif i == 30:  # rightmost cell
+            left_neighbor = cells[time][i - 1]
             right_neighbor = 0
-        else:
+        else:  # All other cells
+            left_neighbor = cells[time][i - 1]
+            right_neighbor = cells[time][i + 1]
+        # Define a new cell and add it to the new generation
+        situation = 7 - int(f"{left_neighbor}{cells[time][i]}{right_neighbor}", 2)
+        next_generation.append(rule[situation])
+    return next_generation
 
-        # All other cells
 
-            left_neighbor = CELLS[time][i - 1]
-            right_neighbor = CELLS[time][i + 1]
-
-        # Defines new cell in and adds it to the new generation
-
-        SITUATION = 7 - int(str(left_neighbor) + str(CELLS[time][i])
-                            + str(right_neighbor), 2)
-        new_row.append(RULE[SITUATION])
-    #returns new generation
-    return new_row
-
-def generate_image(CELLS: list) -> Image:
+def generate_image(cells: List[List[int]]) -> Image.Image:
     """
-    Convert the cells into a PIL.Image.Image and return it to the caller.
-    >>> from random import randint
-    >>> cells = [[randint(0, 1) for w in range(31)] for h in range(16)]
+    Convert the cells into a greyscale PIL.Image.Image and return it to the caller.
+    >>> from random import random
+    >>> cells = [[random() for w in range(31)] for h in range(16)]
     >>> img = generate_image(cells)
-    >>> isinstance(img, PIL.Image.Image)
+    >>> isinstance(img, Image.Image)
     True
-    >>> img.size
+    >>> img.width, img.height
     (31, 16)
     """
-    # Creates the outputting image
-
-    RESULT = Image.new('RGB', (31, 16))
-    PIXELS = RESULT.load()
-
+    # Create the output image
+    img = Image.new("RGB", (len(cells[0]), len(cells)))
+    pixels = img.load()
     # Generates image
-    for w in range(31):
-        for h in range(16):
-            color = 255 - 255 * CELLS[h][w]
-            PIXELS[w, h] = (color, color, color)
-    return RESULT
-    # Uncomment for saving the image
-    # RESULT.save('RULE '+str(RULE_NUM))
+    for w in range(img.width):
+        for h in range(img.height):
+            color = 255 - int(255 * cells[h][w])
+            pixels[w, h] = (color, color, color)
+    return img
 
-    # Shows the image
-    RESULT.show()
 
-if __name__ == '__main__':
-
-    RULE_NUM = bin(int(input('Rule\n')))[2:]
-    RULE = format_ruleset(RULE_NUM)
-
+if __name__ == "__main__":
+    rule_num = bin(int(input("Rule:\n").strip()))[2:]
+    rule = format_ruleset(int(rule_num))
     for time in range(16):
-        CELLS.append(new_generation(CELLS,RULE,time))
-
-    generate_image(CELLS)
+        CELLS.append(new_generation(CELLS, rule, time))
+    img = generate_image(CELLS)
+    # Uncomment for saving the image
+    # img.save(f"rule_{rule_num}.png")
+    img.show()
