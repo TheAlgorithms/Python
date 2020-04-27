b64027bcd6317ee7647c9951f14e5e2c3d0e011d
John Law <johnlaw.po@gmail.com>
2020-04-27 18:05:25 +0200

    Reduce side effect of `shortest_path`
    For the sake of future testing and documentation -

--- graphs/breadth_first_search_shortest_path.py
+++ graphs/breadth_first_search_shortest_path.py
@@ -1,13 +1,18 @@
+"""Breath First Search (BFS) can be used when finding the shortest path 
+from a given source node to a target node in an unweighted graph.
+"""
 class Graph:
     def __init__(self, graph, source_vertex):
-        """graph is implemented as dictionary of adjancency lists"""
+        """Graph is implemented as dictionary of adjancency lists. Also,
+        Source vertex have to be defined upon initialization.
+        """
         self.graph = graph
         # mapping node to its parent in resulting breadth first tree
         self.parent = {}
         self.source_vertex = source_vertex
 
     def breath_first_search(self):
-        """
+        """This function is a helper for running breath first search on this graph.
         """
         visited = {self.source_vertex}
         self.parent[self.source_vertex] = None
@@ -21,14 +26,18 @@ class Graph:
                     self.parent[adjancent_vertex] = vertex
                     queue.append(adjancent_vertex)
 
-    def print_shortest_path(self, target_vertex):
+    def shortest_path(self, target_vertex):
+        """This shortest path function returns a string, describing the result:
+        1.) No path is found. The string is a human readable message to indicate this.
+        2.) The shortest path is found. The string is in the form `v1(->v2->v3->...->vn)`,
+            where v1 is the source vertex and vn is the target vertex, if it exists seperately.
+        """
         if target_vertex == self.source_vertex:
-            print(self.source_vertex, end="")
+            return f"{self.source_vertex}"
         elif target_vertex not in self.parent or not self.parent[target_vertex]:
-            print(f"No path from vertex:{self.source_vertex} to vertex:{target_vertex}")
+            return f"No path from vertex:{self.source_vertex} to vertex:{target_vertex}"
         else:
-            self.print_shortest_path(self.parent[target_vertex])
-            print(f"->{target_vertex}", end="")
+            return self.shortest_path(self.parent[target_vertex]) + f"->{target_vertex}"
 
 
 if __name__ == "__main__":
@@ -43,8 +52,6 @@ if __name__ == "__main__":
     }
     g = Graph(graph, "G")
     g.breath_first_search()
-    g.print_shortest_path("D")
-    print()
-    g.print_shortest_path("G")
-    print()
-    g.print_shortest_path("Foo")
+    print(g.shortest_path("D"))
+    print(g.shortest_path("G"))
+    print(g.shortest_path("Foo"))
