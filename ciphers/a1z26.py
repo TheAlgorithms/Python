0d09165f765b483f75c574cb02b949872ff51f29
LethargicLeprechaun <LethargicLeprechaun@gmail.com>
2020-04-29 21:34:38 +0100

    removed tabs, spaces instead

--- ciphers/a1z26.py
+++ ciphers/a1z26.py
@@ -1,30 +1,30 @@
 def encode(plain : str) -> list:
-	"""
-	>>> encode("MyName")
-	Encoded:  [13, 25, 14, 1, 13, 5]
-	"""
-	result = []
-	for elem in plain:
-		result.append(ord(elem) - 96)
-	return result
+    """
+    >>> encode("MyName")
+    Encoded:  [13, 25, 14, 1, 13, 5]
+    """
+    result = []
+    for elem in plain:
+        result.append(ord(elem) - 96)
+    return result
 
 def decode(encoded : list) -> str:
-	"""
-	>>> decode([13, 25, 14, 1, 13, 5])
-	Decoded: myname
-	"""
-	result = ""
-	for elem in encoded:
-		result += chr(elem + 96)
-	return result
+    """
+    >>> decode([13, 25, 14, 1, 13, 5])
+    Decoded: myname
+    """
+    result = ""
+    for elem in encoded:
+        result += chr(elem + 96)
+    return result
 
 def main():
-	inp = input("->")
-	lowered = inp.lower()
-	encoded = encode(lowered)
-	print("Encoded: ", encoded)
-	decoded = decode(encoded)
-	print("Decoded:", decoded)
+    inp = input("->")
+    lowered = inp.lower()
+    encoded = encode(lowered)
+    print("Encoded: ", encoded)
+    decoded = decode(encoded)
+    print("Decoded:", decoded)
 
 if __name__ == "__main__":
-	main()
+    main()
