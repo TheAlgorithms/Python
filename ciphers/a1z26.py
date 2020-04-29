d3bddb6f1cf64284df6aed69f2b1487d9af9da7f
LethargicLeprechaun <LethargicLeprechaun@gmail.com>
2020-04-29 21:28:42 +0100

    Added Doctests

--- ciphers/a1z26.py
+++ ciphers/a1z26.py
@@ -1,10 +1,18 @@
 def encode(plain : str) -> list:
+	"""
+	>>> encode("MyName")
+	Encoded:  [13, 25, 14, 1, 13, 5]
+	"""
 	result = []
 	for elem in plain:
 		result.append(ord(elem) - 96)
 	return result
 
 def decode(encoded : list) -> str:
+	"""
+	>>> decode([13, 25, 14, 1, 13, 5])
+	Decoded: myname
+	"""
 	result = ""
 	for elem in encoded:
 		result += chr(elem + 96)
