aa4b4ad9c338eddcd66a328cc0bb35317ab11656
LethargicLeprechaun <LethargicLeprechaun@gmail.com>
2020-04-29 21:20:34 +0100

    Added type hints

--- ciphers/a1z26.py
+++ ciphers/a1z26.py
@@ -1,10 +1,10 @@
-def encode(plain):
+def encode(plain : str) -> list:
 	result = []
 	for elem in plain:
 		result.append(ord(elem) - 96)
 	return result
 
-def decode(encoded):
+def decode(encoded : list) -> str:
 	result = ""
 	for elem in encoded:
 		result += chr(elem + 96)
