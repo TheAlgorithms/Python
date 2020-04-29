6166e0cecfcdebb78537e95e370aae65105b7989
Christian Clauss <cclauss@me.com>
2020-04-29 23:58:01 +0200

    Nice one!

--- ciphers/a1z26.py
+++ ciphers/a1z26.py
@@ -1,33 +1,29 @@
 """
+Convert a string of characters to a sequence of numbers
+corresponding to the character's position in the alphabet.
 
-Converts a string of characters to a sequence of numbers corresponding to the character's position in the alphabet.
-
-Information URLs:
 https://www.dcode.fr/letter-number-cipher
 http://bestcodes.weebly.com/a1z26.html
 """
 
-def encode(plain : str) -> list:
+def encode(plain: str) -> list:
     """
     >>> encode("myname")
     [13, 25, 14, 1, 13, 5]
     """
     return [ord(elem) - 96 for elem in plain]
 
-def decode(encoded : list) -> str:
+def decode(encoded: list) -> str:
     """
     >>> decode([13, 25, 14, 1, 13, 5])
     'myname'
     """
-    return "".join(map(lambda elem : chr(elem + 96), encoded))
+    return "".join(chr(elem + 96) for elem in encoded)
 
 def main():
-    inp = input("->")
-    lowered = inp.lower()
-    encoded = encode(lowered)
+    encoded = encode(input("->").strip().lower())
     print("Encoded: ", encoded)
-    decoded = decode(encoded)
-    print("Decoded:", decoded)
+    print("Decoded:", decode(encoded))
 
 if __name__ == "__main__":
     main()
