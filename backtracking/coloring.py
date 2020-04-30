457af3b6de260160695ae55e1b7e71b55ad1417c
CSenshi <saba.pochkhua@gmail.com>
2020-04-30 21:46:32 +0400

    add skeleton code

---
+++ backtracking/coloring.py
@@ -0,0 +1,8 @@
+"""
+    Graph Coloring also called "m coloring problem"
+    consists of coloring given graph with at most m colors
+    such that no adjacent vertices are assigned same color
+"""
+
+def color(graph, m):
+    pass
