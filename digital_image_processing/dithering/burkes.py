823a98fa6e765fca109cf833a392ab43a850775e
Mateusz Zawadzki <mtszzwdzk@gmail.com>
2020-04-30 11:42:29 +0200

    Fix get_greyscale method

--- digital_image_processing/dithering/burkes.py
+++ digital_image_processing/dithering/burkes.py
@@ -38,7 +38,7 @@ class Burkes:
     def get_greyscale(cls, blue: int, green: int, red: int) -> float:
         """
         >>> Burkes.get_greyscale(5, 4, 3)
-        3.753
+        3.5557999999999996
         """
         return 0.114 * blue + 0.587 * green + 0.2126 * red
 
