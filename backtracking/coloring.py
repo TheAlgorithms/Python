2d821c867b2cc7663398933ad90eb64b437558e3
CSenshi <saba.pochkhua@gmail.com>
2020-04-30 22:13:11 +0400

    add mainc function pseudo code and tests (ToDo: write Implementation)

--- backtracking/coloring.py
+++ backtracking/coloring.py
@@ -4,8 +4,42 @@
     such that no adjacent vertices are assigned same color
 """
 
-def color(graph, m):
+
+def util_color(graph, max_colors, colored_vertices, index):
+    """ 
+    Pseudo-Code
+
+    Itterates over each color:
+        Check if coloring is possible:
+            1. Color given vertex
+            2. Do recursive call check if this coloring leads to solving problem
+            4. if coloring is valid return
+            5. Uncolor given vertex
+
+    >>> graph = [[0, 1, 0, 0, 0],
+    ...          [1, 0, 1, 0, 1],
+    ...          [0, 1, 0, 1, 0],
+    ...          [0, 1, 1, 0, 0],
+    ...          [0, 1, 0, 0, 0]]
+    >>> total_colors = 3
+    >>> colored_vertices = [0, 1, 0, 0, 0]
+    >>> index = 3
+    
+    >>> util_color(graph, max_colors, colored_vertices, index)
+    True
+
+    >>> total_colors = 2
+    >>> util_color(graph, max_colors, colored_vertices, index)
+    False
     """
+    pass
+
+
+def color(graph, max_colors):
+    """ Wrapper function to call subroutine called util_color
+        which will either return True or False.
+        If True is returned colored_vertices list is filled with correct colorings
+        
     >>> graph = [[0, 1, 0, 0, 0],
     ...          [1, 0, 1, 0, 1],
     ...          [0, 1, 0, 1, 0],
@@ -20,4 +54,9 @@ def color(graph, m):
     >>> color(graph, total_colors)
     (False, None)
     """
-    pass
+    colored_vertices = [-1] * len(graph)
+
+    if util_color(graph, max_colors, colored_vertices, 0):
+        return True, colored_vertices
+
+    return False, None
