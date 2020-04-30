46127ee992ae8bc08ea6178a07e3adae58e076d1
CSenshi <saba.pochkhua@gmail.com>
2020-04-30 21:50:43 +0400

    add doctests

--- backtracking/coloring.py
+++ backtracking/coloring.py
@@ -5,4 +5,19 @@
 """
 
 def color(graph, m):
+    """
+    >>> graph = [[0, 1, 0, 0, 0],
+    ...          [1, 0, 1, 0, 1],
+    ...          [0, 1, 0, 1, 0],
+    ...          [0, 1, 1, 0, 0],
+    ...          [0, 1, 0, 0, 0]]
+
+    >>> total_colors = 3
+    >>> color(graph, total_colors)
+    (True, [1, 2, 1, 3, 1])
+
+    >>> total_colors = 2
+    >>> color(graph, total_colors)
+    (False, None)
+    """
     pass
