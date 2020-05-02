0258a68fb669463021cfa70f18be0d063de121bf
CSenshi <saba.pochkhua@gmail.com>
2020-05-02 20:18:00 +0400

    add skeleton code

---
+++ backtracking/hamiltonian_cycle.py
@@ -0,0 +1,12 @@
+"""
+    A Hamiltonian cycle (Hamiltonian circuit) is a graph cycle 
+    through a graph that visits each node exactly once.
+    Determining whether such paths and cycles exist in graphs 
+    is the 'Hamiltonian path problem', which is NP-complete.
+    
+    Wikipedia: https://en.wikipedia.org/wiki/Hamiltonian_path
+"""
+
+
+def hamilton_cycle(graph):
+    pass
