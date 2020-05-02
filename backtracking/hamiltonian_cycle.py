978a4ff6b4e3fee42fadf3a7653767051d56a9ee
CSenshi <saba.pochkhua@gmail.com>
2020-05-02 20:19:16 +0400

    add tests for util function + implement wrapper

--- backtracking/hamiltonian_cycle.py
+++ backtracking/hamiltonian_cycle.py
@@ -8,8 +8,22 @@
 """
 
 
-def util_hamilton_cycle(graph):
-    pass
+def util_hamilton_cycle(graph, path, start_index, current_index):
+    """
+    >>> graph = [[0, 1, 0, 1, 0],
+    ...          [1, 0, 1, 1, 1],
+    ...          [0, 1, 0, 0, 1],
+    ...          [1, 1, 0, 0, 1],
+    ...          [0, 1, 1, 1, 0]]
+    >>> path = [0, 1, 2, -1, -1, -1]
+    >>> start_index = 0
+    >>> current_index = 3
+    >>> util_hamilton_cycle(graph, path, start_index, current_index)
+    True
+    >>> print(path)
+    [0, 1, 2, 4, 3, 0]
+    """
+    return True
 
 
 def hamilton_cycle(graph):
@@ -17,7 +31,7 @@ def hamilton_cycle(graph):
     Wrapper function to call subroutine called util_hamilton_cycle,
     which will either return array of vertices indicating hamiltonian cycle
     or an empty list indicating that hamiltonian cycle was not found.
-    Test 1: 
+    Case 1: 
     Following graph consists of 5 edges. 
     If we look closely, we can see that there are multiple Hamiltonian cycles.
     For example one result is when we iterate like: 
@@ -36,7 +50,7 @@ def hamilton_cycle(graph):
     ...          [0, 1, 1, 1, 0]]
     >>> hamilton_cycle(graph)
     [0, 1, 2, 4, 3, 0]
-    Test 2:
+    Case 2:
     Following Graph is exactly what it was before, but edge 3-4 is removed.
     Result is that there is no Hamiltonian Cycle anymore.
     
@@ -55,4 +69,12 @@ def hamilton_cycle(graph):
     []
     
     """
+    # Initialize path with -1, indicating that we have not visited them yet
+    path = [-1] * (len(graph) + 1)
+    # initialize start and end of path with starting index
+    path[0] = path[-1] = 0
+
+    # evaluate and if we find answer return path either return empty array
+    if util_hamilton_cycle(graph, path, 0, 1):
+        return path
     return []
