743eead592c580a89597a426693eae6c5e1cee62
Alok Shukla <20066073+shuklalok@users.noreply.github.com>
2020-05-04 00:50:55 +0530

    Solution for Euler Problem 26

---
+++ project_euler/problem_26/sol1.py
@@ -0,0 +1,49 @@
+"""
+Eular Problem 26
+https://projecteuler.net/problem=26
+Find the value of d < 1000 for which 1/d contains the longest recurring cycle
+in its decimal fraction part.
+"""
+
+NUM = 1
+
+
+def find_digit(numerator: int, digit: int) -> int:
+    """
+    Considering any range can be provided,
+    becasue as per the problem, the digit d < 1000
+    :param n:
+    :param d:
+    :return:
+    >>> find_digit(1, 10)
+    7
+    >>> find_digit(10, 100)
+    97
+    >>> find_digit(10, 1000)
+    983
+    """
+    global NUM
+    NUM = numerator
+    the_digit = 1
+    longest_list_length = 0
+
+    for divide_by_number in range(numerator, digit + 1):
+        has_been_divided = []
+        now_divide = NUM
+        for division_cycle in range(1, digit + 1):
+            if now_divide in has_been_divided:
+                if longest_list_length < len(has_been_divided):
+                    longest_list_length = len(has_been_divided)
+                    the_digit = divide_by_number
+            else:
+                has_been_divided.append(now_divide)
+                now_divide = now_divide * 10 % divide_by_number
+
+    return the_digit
+
+
+# Tests
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
