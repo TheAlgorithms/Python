ff5e07d3cf988fdbdac0e5f180986dea0860cb80
Alok Shukla <20066073+shuklalok@users.noreply.github.com>
2020-05-04 01:34:02 +0530

    Update project_euler/problem_26/sol1.py
    typo error fix
    
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- project_euler/problem_26/sol1.py
+++ project_euler/problem_26/sol1.py
@@ -1,5 +1,5 @@
 """
-Eular Problem 26
+Euler Problem 26
 https://projecteuler.net/problem=26
 Find the value of d < 1000 for which 1/d contains the longest recurring cycle
 in its decimal fraction part.
