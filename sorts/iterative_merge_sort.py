4e41a6000630870c2c1ec379b61873f22966697d
Christian Clauss <cclauss@me.com>
2020-05-11 16:27:10 +0200

    Add the ability to sort strings

--- sorts/iterative_merge_sort.py
+++ sorts/iterative_merge_sort.py
@@ -1,5 +1,5 @@
 """
-This is pure implementation of iterative merge sort in python
+Implementation of iterative merge sort in Python
 Author: Aman Gupta
 
 For doctests run following command:
@@ -12,63 +12,62 @@ python3 iterative_merge_sort.py
 from typing import List
 
 
-def merge(inputlist: List, low: int, mid: int, high: int) -> List:
+def merge(input_list: List, low: int, mid: int, high: int) -> List:
     """
     sorting left-half and right-half individually
     then merging them into result
     """
     result = []
-    left, right = inputlist[low:mid], inputlist[mid : high + 1]
+    left, right = input_list[low:mid], input_list[mid : high + 1]
     while left and right:
         result.append((left if left[0] <= right[0] else right).pop(0))
-    inputlist[low : high + 1] = result + left + right
-    return inputlist
+    input_list[low : high + 1] = result + left + right
+    return input_list
 
 
 # iteration over the unsorted list
-def iter_merge_sort(inputlist: List) -> List:
+def iter_merge_sort(input_list: List) -> List:
     """
-    Returns sorted list of the input numbers list
+    Return a sorted copy of the input list
 
-    Examples
-
-    >>> iter_merge_sort([5,9,8,1,2,7])
-    [1, 2, 5, 7, 8, 9]
-
+    >>> iter_merge_sort([5, 9, 8, 7, 1, 2, 7])
+    [1, 2, 5, 7, 7, 8, 9]
     >>> iter_merge_sort([6])
     [6]
-
     >>> iter_merge_sort([])
     []
-
-    >>> iter_merge_sort([-2,-9,-1,-4])
+    >>> iter_merge_sort([-2, -9, -1, -4])
     [-9, -4, -2, -1]
-    
+    >>> iter_merge_sort([1.1, 1, 0.0, -1, -1.1])
+    [-1.1, -1, 0.0, 1, 1.1]
+    >>> iter_merge_sort(['c', 'b', 'a'])
+    ['a', 'b', 'c']
+    >>> iter_merge_sort('cba')
+    ['a', 'b', 'c']
     """
-
-    if len(inputlist) <= 1:
-        return inputlist
+    if len(input_list) <= 1:
+        return input_list
+    input_list = list(input_list)
 
     # iteration for two-way merging
     p = 2
-    while p < len(inputlist):
+    while p < len(input_list):
         # getting low, high and middle value for merge-sort of single list
-        for i in range(0, len(inputlist), p):
+        for i in range(0, len(input_list), p):
             low = i
             high = i + p - 1
             mid = (low + high + 1) // 2
-            inputlist = merge(inputlist, low, mid, high)
+            input_list = merge(input_list, low, mid, high)
         # final merge of last two parts
-        if p * 2 >= len(inputlist):
+        if p * 2 >= len(input_list):
             mid = i
-            inputlist = merge(inputlist, 0, mid, len(inputlist) - 1)
-
+            input_list = merge(input_list, 0, mid, len(input_list) - 1)
         p *= 2
 
-    return inputlist
+    return input_list
 
 
 if __name__ == "__main__":
     user_input = input("Enter numbers separated by a comma:\n").strip()
     unsorted = [int(item.strip()) for item in user_input.split(",")]
-    print(*iter_merge_sort(unsorted), sep=",")
+    print(iter_merge_sort(unsorted))
