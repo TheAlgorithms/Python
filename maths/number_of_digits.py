656a26bdc6fde1a1a0168a36e4547c39a4a8f119
Vignesh <ravivignesh2000@gmail.com>
2020-05-13 16:55:02 +0530

    Update number_of_digits.py

--- maths/number_of_digits.py
+++ maths/number_of_digits.py
@@ -1,9 +1,18 @@
 def num_digits(n):
-    digits=0;
-    while(n>0):
-        n=n//10;
-        digits=digits+1
+    """
+        Find the number of digits in a number.
+        >>> num_digits(12345)
+        5
+        >>> num_digits(123)
+        3
+    """
+    digits = 0
+    while(n > 0):
+        n = n // 10
+        digits = digits + 1
     return digits
 
-num=12345
-print("Number of digits in " + str(num) + " is : " + str(num_digits(num)))
+
+if __name__ == "__main__":
+    num = 12345
+    print(num_digits(num)) # ===> 5
