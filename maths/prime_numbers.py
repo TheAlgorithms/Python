33e375dd7f965affb48fdea0ae5f0ad5b7ec86b5
Steven Qu <stevenqu2001@gmail.com>
2020-05-14 17:45:13 -0400

    added old version as slow_primes() and named new, faster version primes()

--- maths/prime_numbers.py
+++ maths/prime_numbers.py
@@ -2,6 +2,33 @@ from typing import Generator
 import math
 
 
+def slow_primes(max: int) -> Generator[int, None, None]:
+    """
+    Return a list of all primes numbers up to max.
+    >>> list(primes(0))
+    []
+    >>> list(primes(-1))
+    []
+    >>> list(primes(-10))
+    []
+    >>> list(primes(25))
+    [2, 3, 5, 7, 11, 13, 17, 19, 23]
+    >>> list(primes(11))
+    [2, 3, 5, 7, 11]
+    >>> list(primes(33))
+    [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
+    >>> list(primes(10000))[-1]
+    9973
+    """
+    numbers: Generator = (i for i in range(1, (max + 1)))
+    for i in (n for n in numbers if n > 1):
+        for j in range(2, i):
+            if (i % j) == 0:
+                break
+        else:
+            yield i
+
+
 def primes(max: int) -> Generator[int, None, None]:
     """
     Return a list of all primes numbers up to max.
@@ -23,7 +50,7 @@ def primes(max: int) -> Generator[int, None, None]:
     numbers: Generator = (i for i in range(1, (max + 1)))
     for i in (n for n in numbers if n > 1):
         # only need to check for factors up to sqrt(i)
-        bound = int(math.sqrt(i))+1
+        bound = int(math.sqrt(i)) + 1
         for j in range(2, bound):
             if (i % j) == 0:
                 break
