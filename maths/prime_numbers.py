f7fb16ecda6995fec5ca9ee0d64823ae9ba0bf62
Steven Qu <stevenqu2001@gmail.com>
2020-05-14 17:53:10 -0400

    fixed docstring in slow_primes

--- maths/prime_numbers.py
+++ maths/prime_numbers.py
@@ -5,19 +5,19 @@ import math
 def slow_primes(max: int) -> Generator[int, None, None]:
     """
     Return a list of all primes numbers up to max.
-    >>> list(primes(0))
+    >>> list(slow_primes(0))
     []
-    >>> list(primes(-1))
+    >>> list(slow_primes(-1))
     []
-    >>> list(primes(-10))
+    >>> list(slow_primes(-10))
     []
-    >>> list(primes(25))
+    >>> list(slow_primes(25))
     [2, 3, 5, 7, 11, 13, 17, 19, 23]
-    >>> list(primes(11))
+    >>> list(slow_primes(11))
     [2, 3, 5, 7, 11]
-    >>> list(primes(33))
+    >>> list(slow_primes(33))
     [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
-    >>> list(primes(10000))[-1]
+    >>> list(slow_primes(10000))[-1]
     9973
     """
     numbers: Generator = (i for i in range(1, (max + 1)))
