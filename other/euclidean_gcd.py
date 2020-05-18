201da1435f0aa2f563941dabae4f52ff17b34649
mateuszz0000 <mtszzwdzk@gmail.com>
2020-05-18 14:50:05 +0200

    Added commit suggestions

--- other/euclidean_gcd.py
+++ other/euclidean_gcd.py
@@ -11,9 +11,7 @@ def euclidean_gcd(a: int, b: int) -> int:
     3
     """
     while b:
-        t = b
-        b = a % b
-        a = t
+        a, b = b, a % b
     return a
 
 
@@ -28,18 +26,21 @@ def euclidean_gcd_recursive(a: int, b: int) -> int:
     >>> euclidean_gcd_recursive(6, 3)
     3
     """
-    if b == 0:
-        return a
-    else:
-        return euclidean_gcd_recursive(b, a % b)
+    return a if b == 0 else euclidean_gcd_recursive(b, a % b)
 
 
 def main():
-    print("GCD(3, 5) = " + str(euclidean_gcd(3, 5)))
-    print("GCD(5, 3) = " + str(euclidean_gcd(5, 3)))
-    print("GCD(1, 3) = " + str(euclidean_gcd(1, 3)))
-    print("GCD(3, 6) = " + str(euclidean_gcd(3, 6)))
-    print("GCD(6, 3) = " + str(euclidean_gcd(6, 3)))
+    print(f"euclidean_gcd(3, 5) = {euclidean_gcd(3, 5)}")
+    print(f"euclidean_gcd(5, 3) = {euclidean_gcd(5, 3)}")
+    print(f"euclidean_gcd(1, 3) = {euclidean_gcd(1, 3)}")
+    print(f"euclidean_gcd(3, 6) = {euclidean_gcd(3, 6)}")
+    print(f"euclidean_gcd(6, 3) = {euclidean_gcd(6, 3)}")
+
+    print(f"euclidean_gcd_recursive(3, 5) = {euclidean_gcd_recursive(3, 5)}")
+    print(f"euclidean_gcd_recursive(5, 3) = {euclidean_gcd_recursive(5, 3)}")
+    print(f"euclidean_gcd_recursive(1, 3) = {euclidean_gcd_recursive(1, 3)}")
+    print(f"euclidean_gcd_recursive(3, 6) = {euclidean_gcd_recursive(3, 6)}")
+    print(f"euclidean_gcd_recursive(6, 3) = {euclidean_gcd_recursive(6, 3)}")
 
 
 if __name__ == "__main__":
