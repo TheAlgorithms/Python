46a374874fe9db0ce9f568dc24d9fc5e9adc7d30
mateuszz0000 <mtszzwdzk@gmail.com>
2020-05-19 13:28:37 +0200

    Commit suggestions

--- other/gauss_easter.py
+++ other/gauss_easter.py
@@ -21,28 +21,39 @@ def gauss_easter(year: int) -> datetime:
     >>> gauss_easter(2021)
     datetime.datetime(2021, 4, 4, 0, 0)
     """
-    a = year % 19
-    b = year % 4
-    c = year % 7
-    k = math.floor(year / 100)
-    p = math.floor((13 + 8 * k) / 25)
-    q = k / 4
-    M = (15 - p + k - q) % 30
-    N = (4 + k - q) % 7
-    d = (19 * a + M) % 30
-    e = (2 * b + 4 * c + 6 * d + N) % 7
+    metonic_cycle = year % 19
+    julian_leap_year = year % 4
+    non_leap_year = year % 7
+    leap_day_inhibits = math.floor(year / 100)
+    lunar_orbit_correction = math.floor((13 + 8 * leap_day_inhibits) / 25)
+    leap_day_reinstall_number = leap_day_inhibits / 4
+    secular_moon_shift = (
+        15 - lunar_orbit_correction + leap_day_inhibits - leap_day_reinstall_number
+    ) % 30
+    century_starting_point = (4 + leap_day_inhibits - leap_day_reinstall_number) % 7
 
-    if d == 29 and e == 6:
+    # days to be added to March 21
+    days_to_add = (19 * metonic_cycle + secular_moon_shift) % 30
+
+    # PHM -> Paschal Full Moon
+    days_from_phm_to_sunday = (
+        2 * julian_leap_year
+        + 4 * non_leap_year
+        + 6 * days_to_add
+        + century_starting_point
+    ) % 7
+
+    if days_to_add == 29 and days_from_phm_to_sunday == 6:
         return datetime(year, 4, 19)
-    elif d == 28 and e == 6:
+    elif days_to_add == 28 and days_from_phm_to_sunday == 6:
         return datetime(year, 4, 18)
     else:
-        return datetime(year, 3, 22) + timedelta(days=int(d + e))
+        return datetime(year, 3, 22) + timedelta(
+            days=int(days_to_add + days_from_phm_to_sunday)
+        )
 
 
-if __name__ == '__main__':
-    print(f"Easter in 2023 will be {gauss_easter(2023)}")
-    print(f"Easter in 2021 will be {gauss_easter(2021)}")
-    print(f"Easter in 2010 was {gauss_easter(2010)}")
-    print(f"Easter in 1994 was {gauss_easter(1994)}")
-    print(f"Easter in 2000 was {gauss_easter(2000)}")
+if __name__ == "__main__":
+    for year in (1994, 2000, 2010, 2021, 2023):
+        tense = "will be" if year > datetime.now().year else "was"
+        print(f"Easter in {year} {tense} {gauss_easter(year)}")
