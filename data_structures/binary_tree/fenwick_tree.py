4cd04aaf36d1a431533c8dac81cf78984ad562b1
Shubham Kumar Jha <skjha832@gmail.com>
2020-05-20 17:51:07 +0530

    Updated FenwickTree ADT for better interface

--- data_structures/binary_tree/fenwick_tree.py
+++ data_structures/binary_tree/fenwick_tree.py
@@ -1,28 +1,84 @@
 class FenwickTree:
-    def __init__(self, SIZE):  # create fenwick tree with size SIZE
-        self.Size = SIZE
-        self.ft = [0 for i in range(0, SIZE)]
+    def __init__(self, arg):
+        """
+        Creates a Fenwick Tree
 
-    def update(self, i, val):  # update data (adding) in index i in O(lg N)
-        while i < self.Size:
-            self.ft[i] += val
+        args: arg(two ways)
+            1. If arg is an integer, it creates a Fenwick tree
+               of size arg
+            2. If arg is of type (list, tuple, set), it creates
+               a Fenwick tree with the contents in the list
+        """
+        if isinstance(arg, int):
+            self.size = arg
+        else:
+            self.size = len(arg) + 1
+        self.tree = [0 for i in range(self.size)]
+
+        if not isinstance(arg, int):
+            for i, val in enumerate(arg):
+                self.update(i + 1, val)
+
+    def add(self, i, val):
+        """
+        Increases (/decreases) the value at index i with <val>
+
+        While updating a list consider 1-indexing (add 1 to 0-indexing)
+        """
+        while i < self.size:
+            self.tree[i] += val
             i += i & (-i)
 
-    def query(self, i):  # query cumulative data from index 0 to i in O(lg N)
-        ret = 0
+    def update(self, i, val):
+        """
+        Updates the value at index i with <val>
+
+        While updating a list consider 1-indexing (add 1 to 0-indexing)
+        """
+        orig = self.range_query(i, i)  # value at i-index
+        self.add(i, val - orig)
+
+    def query(self, i):
+        """
+        Queries the cumulative sum from index 1 to i (inclusive) 
+        (indices in 1-indexing)
+        """
+        res = 0
         while i > 0:
-            ret += self.ft[i]
+            res += self.tree[i]
             i -= i & (-i)
-        return ret
+
+        return res
+
+    def range_query(self, i, j):
+        """
+        Queries the total sum from i-index to j-index (inclusive)
+
+        (indices in 1-indexing)
+        """
+        return self.query(j) - self.query(i - 1)
 
 
 if __name__ == "__main__":
     f = FenwickTree(100)
-    f.update(1, 20)
-    f.update(4, 4)
+    f.add(1, 20)
+    f.add(4, 4)
     print(f.query(1))
     print(f.query(3))
     print(f.query(4))
-    f.update(2, -5)
+    f.add(2, -5)
     print(f.query(1))
     print(f.query(3))
+    f.update(2, 10)
+    print(f.query(1))
+    print(f.query(2))
+
+    # List arguments
+    fen = FenwickTree([1, 2, 3, 4, 5])
+    fen.add(1, 1)
+    print(fen.query(1))
+    print(fen.query(5))
+    fen.update(1, 10)
+    print(fen.query(1))
+    print(fen.query(3))
+    print(fen.range_query(3, 5))
