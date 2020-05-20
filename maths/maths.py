5b13018246f5ba5e7856ca9ac5d95ed62a210e21
zhbi98 <3049824528@qq.com>
2020-05-20 21:53:01 +0800

    Modify notes

--- maths/maths.py
+++ maths/maths.py
@@ -7,18 +7,22 @@ Pi = 3.14159265358979323846
 
 
 def _abs(value):
-    # >>> _abs(-10)
-    # 10
-    # >>> _abs(10)
-    # 10
+    '''
+    >>> _abs(-10)
+    10
+    >>> _abs(10)
+    10
+    '''
     return -value if value < 0 else value
 
 
 def _power(value, n):
-    # >>> _power(-10, 2)
-    # 100
-    # >>> _power(10, 2)
-    # 100
+    '''
+    >>> _power(-10, 2)
+    100
+    >>> _power(10, 2)
+    100
+    '''
     result = 1
 
     if n == 0:
@@ -30,45 +34,60 @@ def _power(value, n):
     return result
 
 
-# newton iteration method
 def _sqrt(value):
+    '''
+    newton iteration method:
+    X(k+1) = 1/2 * (X(k) + vale/X(k))
+
+    >>> _sqrt(9)
+    3
+    >>> _sqrt(25)
+    5
+    '''
     if value < 0:
         print("_sqrt: Value must be greater than or equal to 0")
     else:
         x = value
-        t = 0
-        # X(k+1) = 1/2 * (X(k) + vale/X(k))
+        t = 0 
         while _abs(x - t) > 1e-15:
             t = x
             x = 0.5 * (x + (value / x))
-    
-    # >>> _sqrt(9)
-    # 3
-    # >>> _sqrt(25)
-    # 5
+
     return x
 
 
 # factorial
 def _factor(value):
-    # regulations 0! = 1
+    """
+    regulations 0! = 1
+    >>> from math import factorial
+    >>> for i in range 360:
+    >>>     assert _factor(i) == factorial(i), (i, _factor(i), factorial(i))
+    
+    >>> _factor(5)
+    120
+    >>> _factor(2)
+    2  
+    """
     f = 1
     if value:
         for i in range(value):
             f = f * (i + 1)
     else:
         f = 1
-    
-    # >>> _factor(5)
-    # 120
-    # >>> _factor(2)
-    # 2  
+
     return f
 
 
 # Another way to 
 # implement sin(x)
 def __sin(value):
+    '''
+    >>> __sin(90)
+    1
+    >>> __sin(0)
+    0  
+    '''
     value = value * Pi / 180
     t = value
     x = 0
@@ -81,15 +100,17 @@ def __sin(value):
 
     if x > 0 and x < 1e-15:
         x = 0
-
-    # >>> _sin(90)
-    # 1
-    # >>> _sin(0)
-    # 0    
+  
     return x
 
 
 def _sin(value):
+    '''
+    >>> _sin(90)
+    1
+    >>> _sin(0)
+    0
+    '''
     value = value * Pi / 180
     t = 1
     x = 0
@@ -100,14 +121,16 @@ def _sin(value):
         t = -1 * t
         n = n + 2
 
-    # >>> _sin(90)
-    # 1
-    # >>> _sin(0)
-    # 0 
     return x
 
 
 def _cos(value):
+    '''
+    >>> _cos(90)
+    0
+    >>> _cos(0)
+    1
+    '''
     value = value * Pi / 180
     n = 0
     t = 1 
@@ -117,19 +140,18 @@ def _cos(value):
         x += t * _power(value, n) / _factor(n)
         t = -1 * t
         n = n + 2
-
-    # >>> _cos(90)
-    # 0
-    # >>> _cos(0)
-    # 1     
+    
     return x
 
 
 def _tan(value):
-    x = _sin(value) / _cos(value)
-    
-    # >>> _tan(90)
-    # 0
-    # >>> _tan(45)
-    # 1      
-    return x
+    """
+    >>> _tan(90)
+    0
+    >>> _tan(45)
+    1
+    >>> from math import tan
+    >>> for i in range 360:
+    >>>     assert _tan(i) == tan(i), (i, _tan(i), tan(i))
+    """
+    return _sin(value) / _cos(value)
