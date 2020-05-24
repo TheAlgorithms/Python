f3b04ed7a782a5c6bb4268146c592a1f1a763f5d
Aditya Bhardwaj <63164037+aditya-190@users.noreply.github.com>
2020-05-24 09:36:25 +0530

    Update mergesort.py

--- divide_and_conquer/mergesort.py
+++ divide_and_conquer/mergesort.py
@@ -1,55 +1,56 @@
 import doctest
 
 def mergeSort(array):
-	"""
-	This Function Implements Merge Sorting on the array passed as an parameter.
-	Merge Sort is a sorting algorithm, which is commonly used in computer science.
-	Merge Sort is a divide and conquer algorithm.
-	It works by recursively breaking down a problem into two or more sub-problems of the same or related type,
-	until these become simple enough to be solved directly.
-	The solutions to the sub-problems are then combined to give a solution to the original problem.
-	So Merge Sort first divides the array into equal halves and then combines them in a sorted manner.
-	""" 
-	if len(array) >1: 
-		middle_index = len(array)//2 						  #Finding the middle_index of the array
-		left_index = array[:middle_index] 					  # Dividing the array elements 
-		right_index = array[middle_index:]	 	 	 		  # into 2 halves - Left_index & Right Index. 
+    """
+     This Function Implements Merge Sorting on the array passed as an parameter.
+     Merge Sort is a sorting algorithm, which is commonly used in computer science.
+     Merge Sort is a divide and conquer algorithm.
+     It works by recursively breaking down a problem into two or more sub-problems of the same or related type,
+     until these become simple enough to be solved directly.
+     The solutions to the sub-problems are then combined to give a solution to the original problem.
+     So Merge Sort first divides the array into equal halves and then combines them in a sorted manner.
+     """ 
 
-		mergeSort(left_index) 							  # Sorting the first half of the array. 
-		mergeSort(right_index) 							  # Sorting the second half of the array.
+    if len(array) >1: 
+        middle_index = len(array)//2 						  #Finding the middle_index of the array
+        left_index = array[:middle_index] 					  # Dividing the array elements 
+        right_index = array[middle_index:]	 	 	 		  # into 2 halves - Left_index & Right Index. 
 
-		i = j = k = 0
+        mergeSort(left_index)                                                     # Sorting the first half of the array. 
+        mergeSort(right_index)                                                    # Sorting the second half of the array.
+
+        i = j = k = 0
 		
-		while i < len(left_index) and j < len(right_index):   # Copy data to temporary array. 
-			if left_index[i] < right_index[j]: 
-				array[k] = left_index[i] 
-				i+=1
-			else: 
-				array[k] = right_index[j] 
-				j+=1
-			k+=1
+        while i < len(left_index) and j < len(right_index):                       # Copy data to temporary array. 
+            if left_index[i] < right_index[j]: 
+                array[k] = left_index[i] 
+                i+=1
+            else: 
+                array[k] = right_index[j] 
+                j+=1
+            k+=1
 		 
-		while i < len(left_index): 						  # Checking if any element was left
-			array[k] = left_index[i] 
-			i+=1
-			k+=1
+        while i < len(left_index): 					          # Checking if any element was left
+            array[k] = left_index[i] 
+            i+=1
+            k+=1
 		
-		while j < len(right_index): 
-			array[k] = right_index[j] 
-			j+=1
-			k+=1
-	sorted_output(array) 
+        while j < len(right_index):
+            array[k] = right_index[j] 
+            j+=1
+            k+=1
+    return sorted_output(array)
 
-def sorted_output(array):								  # Returns the sorted array.
+def sorted_output(array):							  # Returns the sorted array.
     """
     >>> mergeSort([3,2,1])
     [1, 2, 3]
     >>> mergeSort([3,2,1,0,1,2,3,5,4])
     [0, 1, 1, 2, 2, 3, 3, 4, 5]
     """
-	for i in range(len(array)):		 
-		sorted = (array[i],end=" ") 
-	return sorted 
+    for i in range(len(array)):		 
+        print(array[i], end=', ') 
+
 
 
 if __name__ == "__main__":
