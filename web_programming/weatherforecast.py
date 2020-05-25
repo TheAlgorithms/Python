c097dc7bbfc08fa2ac8a144161edb12a3a589ba7
Swapnanil Dutta <47251193+swapnanildutta@users.noreply.github.com>
2020-05-25 17:49:28 +0530

    Update weatherforecast.py

--- web_programming/weatherforecast.py
+++ web_programming/weatherforecast.py
@@ -2,31 +2,35 @@ import json
 import urllib.request, urllib.parse, urllib.error
 import ssl
 
-ctx=ssl.create_default_context()
-ctx.check_hostname=False
-ctx.verify_mode=ssl.CERT_NONE
-serviceurl="http://api.openweathermap.org/data/2.5/weather?q="
-key="&APPID=" #ADD YOUR KEY
-Valid="True"
-while Valid.lower()== "true":
-    address=input("Enter the location:")
-    if len(address)<1: exit(0)
-    url=serviceurl+address+key
+def weather_forecast()
+    ctx=ssl.create_default_context()
+    ctx.check_hostname=False
+    ctx.verify_mode=ssl.CERT_NONE
+    serviceurl="http://api.openweathermap.org/data/2.5/weather?q="
+    key="&APPID=" #ADD YOUR KEY
+    Valid="True"
+    while Valid.lower()== "true":
+        address=input("Enter the location:")
+        if len(address)<1: exit(0)
+        url=serviceurl+address+key
 
-    print("Retrieving",url)
-    uh=urllib.request.urlopen(url,context=ctx)
-    data=uh.read().decode()
-    print("Retrieved",len(data),'characters')
+        print("Retrieving",url)
+        uh=urllib.request.urlopen(url,context=ctx)
+        data=uh.read().decode() 
+        print("Retrieved",len(data),'characters')
 
-    try:
-        js=json.loads(data)
-    except:
-        js=None
+        try:
+            js=json.loads(data)
+        except:
+            js=None
     
-    if not js or 'cod' not in js or js['cod']!=200:
-        print("===========Failure to Retrieve===========")
-        print(data)
-        exit(0)
-        continue
-    print(js)
-    Valid=input('Do you want to continue?')
+        if not js or 'cod' not in js or js['cod']!=200:
+            print("===========Failure to Retrieve===========")
+            print(data)
+            exit(0)
+            continue
+        print(js)
+        Valid=input('Do you want to continue?')
+        
+if __name__=="__main__":
+    weather_forecast()
