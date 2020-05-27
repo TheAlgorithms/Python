bc32a9f8d28ad4226a2204ee73b3e293ac048c46
mateuszz0000 <mtszzwdzk@gmail.com>
2020-05-27 14:52:25 +0200

    Added doctests

--- strings/jaro_winkler.py
+++ strings/jaro_winkler.py
@@ -4,13 +4,31 @@ import math
 
 def jaro_winkler(first_string: str, second_string: str) -> float:
     """
-    >>> jaro_winkler("something", "gmosehitt")
-    52
+    Jaro–Winkler distance is a string metric measuring an edit distance between two sequences.
+    Output value is between 0.0 and 1.0.
+
+    >>> jaro_winkler("martha", "marhta")
+    0.9611111111111111
+
+    >>> jaro_winkler("CRATE", "TRACE")
+    0.6222222222222222
+
+    >>> jaro_winkler("test", "dbdbdbdb")
+    0.0
+
+    >>> jaro_winkler("test", "test")
+    1.0
+
+    >>> jaro_winkler("hello world", "HeLLo W0rlD")
+    0.5303030303030303
+
+    >>> jaro_winkler("test", "")
+    0.0
     """
     max_distance = math.floor(max(len(first_string), len(second_string)) / 2) - 1
-    match_count = 0
 
     # matching characters
+    match_count = 0
     for i, c1 in enumerate(first_string):
         for j, c2 in enumerate(second_string):
             if c1 == c2 and abs(i - j) < max_distance:
@@ -25,9 +43,12 @@ def jaro_winkler(first_string: str, second_string: str) -> float:
     if not match_count:
         jaro = 0
     else:
-        jaro = 1/3 * (match_count/len(first_string) + match_count/len(second_string) + (match_count - not_match/2)/match_count)
+        jaro = 1/3 * (
+                match_count/len(first_string) +
+                match_count/len(second_string) +
+                (match_count - not_match/2)/match_count)
 
-    # common prefix
+    # common prefix up to 4 characters
     prefix_len = 0
     for c1, c2 in zip(first_string[:4], second_string[:4]):
         if c1 == c2:
@@ -39,4 +60,4 @@ def jaro_winkler(first_string: str, second_string: str) -> float:
 
 
 if __name__ == '__main__':
-    print(jaro_winkler("martha", "marhta"))
+    print(jaro_winkler("hello", "world"))
