b3030e519143d43127b6a4d4eade0f70845b2aea
gkarwchan <ghassan.karwchan@gmail.com>
2020-05-29 16:15:08 -0600

    another solution to the first problem

---
+++ project_euler/problem_01/sol8.py
@@ -0,0 +1,25 @@
+"""
+Problem Statement:
+If we list all the natural numbers below 10 that are multiples of 3 or 5,
+we get 3,5,6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below N.
+"""
+
+
+def solution(n):
+    """Returns the sum of all the multiples of 3 or 5 below n.
+
+    >>> solution(3)
+    0
+    >>> solution(4)
+    3
+    >>> solution(10)
+    23
+    >>> solution(600)
+    83700
+    """
+    return sum(set(range(0,n,3)).union(set(range(0,n,5))))
+
+
+if __name__ == "__main__":
+    print(solution(int(input().strip())))
