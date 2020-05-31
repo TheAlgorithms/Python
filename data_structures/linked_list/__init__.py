ffb58d2a373666ea62f09fcd4897ea78b30b7297
Christian Clauss <cclauss@me.com>
2020-05-31 09:30:03 +0200

    Add doctests to LinkedList().__len__()

--- data_structures/linked_list/__init__.py
+++ data_structures/linked_list/__init__.py
@@ -2,6 +2,8 @@ class Node:
     def __init__(self, item, next):
         self.item = item
         self.next = next
+
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -19,7 +21,26 @@ class LinkedList:
             self.head = self.head.next
             self.size-=1
             return item
+    
     def is_empty(self):
         return self.head is None
+
     def __len__(self):
+        """
+        >>> linked_list = LinkedList()
+        >>> len(linked_list)
+        0
+        >>> linked_list.add("a")
+        >>> len(linked_list)
+        1
+        >>> linked_list.add("b")
+        >>> len(linked_list)
+        2
+        >>> _ = linked_list.remove()
+        >>> len(linked_list)
+        1
+        >>> _ = linked_list.remove()
+        >>> len(linked_list)
+        0
+        """
         return self.size
