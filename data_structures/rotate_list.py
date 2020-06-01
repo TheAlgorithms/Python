fbe17c4a5604fbaf6f739ebf014e8a09d10dad0c
Udit Maherwal <51200625+uditmaherwal@users.noreply.github.com>
2020-06-01 14:05:37 +0530

    Update rotate_list.py

--- data_structures/rotate_list.py
+++ data_structures/rotate_list.py
@@ -12,21 +12,24 @@ rotate 1 steps to the right: [7, 1, 2, 3, 4, 5, 6]
 rotate 2 steps to the right: [6, 7, 1, 2, 3, 4, 5]
 rotate 3 steps to the right: [5, 6, 7, 1, 2, 3, 4]
 """
-from typing import List
-
-
-def rotate(nums: List[int], rotate_index: int) -> List[int]:
+def rotate(nums: [int], rotate_index: int) -> [int]:
     """
     >>> rotate([1, 2, 3, 4, 5, 6, 7], 3)
     [5, 6, 7, 1, 2, 3, 4]
     >>> rotate([1, 2, 3, 4, 5, 6, 7], -3)
-    [1, 2, 3, 4, 5, 6, 7]
+    Invalid input provided
     >>> rotate([1, 2, 3, 4, 5, 6, 7], 3.0)
-    ["?"]
+    Invalid input provided
     >>> rotate("TheAlgorithms/Python", -6)
-    ["?"]
+    Invalid input provided
     """
-    return nums[len(nums) - rotate_index:] + nums[:len(nums) - rotate_index]
+    if rotate_index < 0 or nums != list:
+        return "Invalid input provided"
+    elif rotate_index >= 0:
+        nums[:] = nums[len(nums) - rotate_index:] + nums[:len(nums) - rotate_index]
+        return nums
+    else:
+        return 'Invalid input provided'
 
 
 if __name__ == "__main__":
