a2da6d5e4f4ee5b54762cda1165cf5e425f504be
Christian Clauss <cclauss@me.com>
2020-06-01 09:55:00 +0200

    def rotate(nums: List[int], rotate_index: int) -> List[int]:

--- data_structures/rotate_list.py
+++ data_structures/rotate_list.py
@@ -12,9 +12,10 @@ rotate 1 steps to the right: [7, 1, 2, 3, 4, 5, 6]
 rotate 2 steps to the right: [6, 7, 1, 2, 3, 4, 5]
 rotate 3 steps to the right: [5, 6, 7, 1, 2, 3, 4]
 """
+from typing import List
 
 
-def rotate(nums: List[int], rotate_index: int):
+def rotate(nums: List[int], rotate_index: int) -> List[int]:
     """
     >>> rotate([1, 2, 3, 4, 5, 6, 7], 3)
     [5, 6, 7, 1, 2, 3, 4]
@@ -32,4 +33,3 @@ if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
-    
