239fdfb756e62c3d05e8bfca0f3dc0e7dad6bae7
Vignesh <ravivignesh2000@gmail.com>
2020-06-02 22:33:17 +0530

    create sum_of_digits.py
    create sum_of_digits.py to find the sum of digits of a number
    digit_sum(12345) ---> 15
    digit_sum(12345) ---> 10

---
+++ maths/sum_of_digits.py
@@ -0,0 +1,19 @@
+def digit_sum(n: int) -> int:
+    """
+    Find the sum of digits of a number.
+
+    >>> digit_sum(12345)
+    15
+    >>> digit_sum(123)
+    6
+    """
+    res=0
+    while n > 0:
+        res += n % 10
+        n = n // 10
+    
+    return res
+
+
+if __name__ == "__main__":
+    print(digit_sum(12345))   # ===> 15
