adbd4a79a5a4e1b75818a33962cb96ad19d4ea1a
Vignesh <ravivignesh2000@gmail.com>
2020-06-03 22:09:10 +0530

    Update odd_or_even.py

--- bit_manipulation/odd_or_even.py
+++ bit_manipulation/odd_or_even.py
@@ -1,7 +1,6 @@
 def odd_or_even(n: int) -> str:
     """
     whether a given number is odd or even using bitwise AND
-    
     >>> odd_or_even(1)
     Odd
     >>> sum_of_digits(14)
@@ -14,5 +13,5 @@ def odd_or_even(n: int) -> str:
 
 
 if(__name__=='__main__'):
-    num=11
-    print(num, " is ", odd_or_even(num))
+    num = 11
+    print(num , " is " , odd_or_even(num))
