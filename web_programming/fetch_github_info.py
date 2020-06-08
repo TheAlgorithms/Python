d4e08c522ba71344edd9557c690cbd95d377506f
Christian Clauss <cclauss@me.com>
2020-06-08 12:35:32 +0200

    Algorithms should not print

--- web_programming/fetch_github_info.py
+++ web_programming/fetch_github_info.py
@@ -1,17 +1,25 @@
-# Created by sarathkaul on 14/11/19
+#!/usr/bin/env python3
+
+"""
+Created by sarathkaul on 14/11/19
+
+Depending on your settings, you may need a two-factor authentication OTP code.
+Documentation_url: https://developer.github.com/v3/auth#working-with-two-factor-authentication
+"""
+
 
 import requests
 
 _GITHUB_API = "https://api.github.com/user"
 
 
-def fetch_github_info(auth_user: str, auth_pass: str) -> None:
-    # fetching github info using requests
-    info = requests.get(_GITHUB_API, auth=(auth_user, auth_pass))
-
-    for a_info, a_detail in info.json().items():
-        print(f"{a_info}: {a_detail}")
+def fetch_github_info(auth_user: str, auth_pass: str) -> dict:
+    """
+    Fetch GitHub info using the requests module
+    """
+    return requests.get(_GITHUB_API, auth=(auth_user, auth_pass)).json()
 
 
 if __name__ == "__main__":
-    fetch_github_info("<USER NAME>", "<PASSWORD>")
+    for key, value in fetch_github_info("<USER NAME>", "<PASSWORD>").items():
+        print(f"{key}: {value}")
