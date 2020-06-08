7205ad5c55ac6cdc2ac6138378951393e108c70e
Apoorve73 <apoorve73@gmail.com>
2020-06-08 16:22:33 +0530

    Update Variable and Links

--- greedy_method/knapsack_problem.py
+++ greedy_method/knapsack_problem.py
@@ -1,23 +1,26 @@
+# Refer https://en.wikipedia.org/wiki/Greedy_algorithm#:~:text=A%20greedy%20algorithm%20is%20any,of%20finding%20a%20global%20optimum.
+# to get an insight into Greedy Algorithm 
+
 # Knapsack is a common Greedy Algo problem.
 """
 You have bags of wheat with different weights and a different profits for each.
 eg.
 profit 5 8 7 1 12 3 4
 weight 2 7 1 6 4  2 5
-max_weight 100
-Calculate the max profit the shopkeeper can make for the given max_weight that could be carried.
+max_Weight 100
+Calculate the max profit the shopkeeper can make for the given max_Weight that could be carried.
 """
 
 # Importing copy module for deepcopy operations
 import copy
 
 
-def calc_Profit(profit: list, weight: list, max_weight: int) -> int:
+def calc_Profit(profit: list, weight: list, max_Weight: int) -> int:
     """
     Function description is as follows-
     :param profit: Take a list of profits
     :param weight: Take a list of weight if bags corresponding to the profits
-    :param max_weight: Max. weight that could be carried
+    :param max_Weight: Max. weight that could be carried
     :return: Max expected gain
 
     >>> calc_Profit([1,2,3], [3,4,5], 15)
@@ -29,17 +32,17 @@ def calc_Profit(profit: list, weight: list, max_weight: int) -> int:
 
     # List created to store profit gained for the 1kg in case of each weight
     # respectively
-    prof_by_wei = list()
+    profit_By_Weight = list()
 
     # Calculate and append profit/weight for each
     for i in range(len(weight)):
-        prof_by_wei.append(profit[i] / weight[i])
+        profit_By_Weight.append(profit[i] / weight[i])
 
     # Creating a copy of the list and sorting proit/weight in ascending order
-    temp_pbw = sorted(copy.deepcopy(prof_by_wei))
+    temp_PBW = sorted(copy.deepcopy(profit_By_Weight))
 
     # declaring useful variables
-    l = len(temp_pbw)
+    l = len(temp_PBW)
     limit = 0
     gain = 0
     i = 0
@@ -47,17 +50,17 @@ def calc_Profit(profit: list, weight: list, max_weight: int) -> int:
     # loop till the total weight do not reach max limit i.e. 15 kg and till i<l
     while limit <= 15 and i < l:
 
-        # flag value for encountered greatest element in temp_pbw
-        flag = temp_pbw[l - i - 1]
+        # flag value for encountered greatest element in temp_PBW
+        flag = temp_PBW[l - i - 1]
         """
-        calculating index of the same flag in prof_by_wei list.
+        calculating index of the same flag in profit_By_Weight list.
         This will give the index of the first encountered element which is same as of flag.
         There may be one or more values same as that of flag but .index always encounter the very first element only.
-        To curb this, alter the values in prof_by_wei once they are used. Here it is done to -1 because neither profit
+        To curb this, alter the values in profit_By_Weight once they are used. Here it is done to -1 because neither profit
         nor weight can be in negative.
         """
-        index = prof_by_wei.index(flag)
-        prof_by_wei[index] = -1
+        index = profit_By_Weight.index(flag)
+        profit_By_Weight[index] = -1
 
         # check if the weight encountered is less than the total weight
         # encountered before.
@@ -80,16 +83,16 @@ def calc_Profit(profit: list, weight: list, max_weight: int) -> int:
 
 if __name__ == "__main__":
 
-    # Input profit, weight and max_weight (all positive values).
+    # Input profit, weight and max_Weight (all positive values).
 
     profit = [int(x) for x in input().split()]
     weight = [int(x) for x in input().split()]
-    max_weight = int(input())
+    max_Weight = int(input())
 
     if len(profit) != len(weight):
         print("The length of both the arrays must be same! Try again!")
 
-    if max_weight < 0:
+    if max_Weight < 0:
         print("Gotcha! Weight is a positive quantity")
 
     for i in range(len(profit)):
@@ -97,4 +100,4 @@ if __name__ == "__main__":
             print("Ono! Input positive values only! Better luck next time")
             break
         else:
-            calc_Profit(profit, weight, max_weight)
+            calc_Profit(profit, weight, max_Weight)
