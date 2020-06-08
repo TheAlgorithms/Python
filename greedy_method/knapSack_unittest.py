746d19ff6326dff8598ed2498dd56120e61b42fb
Apoorve73 <apoorve73@gmail.com>
2020-06-08 17:01:51 +0530

    Fixed flake8 bugs

--- greedy_method/knapSack_unittest.py
+++ greedy_method/knapSack_unittest.py
@@ -1,12 +1,20 @@
 import unittest
 import knapsack_problem as kp
 
+
 class TestClass(unittest.TestCase):
+    """
+    Test cases for knapsack_problem
+    """
 
     def test_sorted(self):
-        gain = kp.calc_Profit([10,20,30,40,50,60], [2, 4, 6, 8, 10, 12],100)
+        """
+        kp.calc_Profit takes the required argument (profit, weight, max_weight)
+        and returns whether the answer matches to the expected ones
+        """
+        gain = kp.calc_Profit([10, 20, 30, 40, 50, 60], [2, 4, 6, 8, 10, 12], 100)
         # self.assertTrue(result)
-        self.assertEqual(gain,75)
+        self.assertEqual(gain, 75)
 
 
 if __name__ == "__main__":
--- greedy_method/knapsack_problem.py
+++ greedy_method/knapsack_problem.py
@@ -1,7 +1,7 @@
-# Refer https://en.wikipedia.org/wiki/Greedy_algorithm#:~:text=A%20greedy%20algorithm%20is%20any,of%20finding%20a%20global%20optimum.
-# to get an insight into Greedy Algorithm 
+# To get an insight into Greedy Algorithm
+# Knapsack is a common Greedy Algo problem
 
-# Knapsack is a common Greedy Algo problem.
+
 """
 You have bags of wheat with different weights and a different profits for each.
 eg.
@@ -42,22 +42,22 @@ def calc_Profit(profit: list, weight: list, max_Weight: int) -> int:
     temp_PBW = sorted(copy.deepcopy(profit_By_Weight))
 
     # declaring useful variables
-    l = len(temp_PBW)
+    length = len(temp_PBW)
     limit = 0
     gain = 0
     i = 0
 
     # loop till the total weight do not reach max limit i.e. 15 kg and till i<l
-    while limit <= 15 and i < l:
+    while limit <= 15 and i < length:
 
         # flag value for encountered greatest element in temp_PBW
-        flag = temp_PBW[l - i - 1]
+        flag = temp_PBW[length - i - 1]
         """
         calculating index of the same flag in profit_By_Weight list.
         This will give the index of the first encountered element which is same as of flag.
         There may be one or more values same as that of flag but .index always encounter the very first element only.
-        To curb this, alter the values in profit_By_Weight once they are used. Here it is done to -1 because neither profit
-        nor weight can be in negative.
+        To curb this alter the values in profit_By_Weight once they are used Here it is done to -1
+        because neither profit nor weight can be in negative.
         """
         index = profit_By_Weight.index(flag)
         profit_By_Weight[index] = -1
@@ -72,7 +72,8 @@ def calc_Profit(profit: list, weight: list, max_Weight: int) -> int:
             gain += 1 * profit[index]
 
         else:
-            # Since the weight encountered is greater than limit, therefore take the required number of remaining kgs and calculate profit for it.
+            # Since the weight encountered is greater than limit, therefore take the required number of remaining kgs
+            # and calculate profit for it.
             # weight remaining/ weight[index]
             gain += ((15 - limit) / weight[index]) * profit[index]
             break
