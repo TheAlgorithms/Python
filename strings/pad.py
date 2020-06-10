c7a63c6ee36c76f562661cf1414088a886217875
urkidoo <urkidoo@gmail.com>
2020-06-10 20:32:23 +0300

    cleaning the .py file

--- strings/pad.py
+++ strings/pad.py
@@ -13,16 +13,3 @@ def pad(txt, char_to_pad, final_length):
 	remaining_chars_to_pad = final_length - len(txt)
 	return ((char_to_pad * remaining_chars_to_pad) + txt)[0:final_length]
 	
-	
-if __name__ == "__main__":
-	
-	# for i in range(20):
-		
-		# random_str = 'abc' * random.randint(-2, 2)
-		# char_to_pad = '*'
-		# min_final_length = random.randint(-20, 20)
-		# print("str: \t" + random_str)
-		# print("len: \t" + '*'*min_final_length + ' \t\t' + str(min_final_length))
-		# print( "res: \t" + pad(random_str, char_to_pad, min_final_length) )
-		# print()
-	
