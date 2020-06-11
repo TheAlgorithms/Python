fe0174ad5ed0f56ea8451ad4808ff18fc47cda2c
bnMikheili <mnazg16@freeuni.edu.ge>
2020-06-11 22:26:08 +0400

    fix bug: styling

--- hashes/sdbm.py
+++ hashes/sdbm.py
@@ -1,5 +1,5 @@
 """
-    This algorithm was created for sdbm (a public-domain reimplementation of ndbm) database library. 
+    This algorithm was created for sdbm (a public-domain reimplementation of ndbm) database library.
     It was found to do well in scrambling bits, causing better distribution of the keys and fewer splits.
     It also happens to be a good general hashing function with good distribution.
     The actual function (pseudo code) is:
@@ -7,7 +7,8 @@
             hash(i) = hash(i - 1) * 65599 + str[i];
 
     What is included below is the faster version used in gawk. [there is even a faster, duff-device version]
-    The magic constant 65599 was picked out of thin air while experimenting with different constants, and turns out to be a prime.
+    The magic constant 65599 was picked out of thin air while experimenting with different constants.
+    It turns out to be a prime.
     This is one of the algorithms used in berkeley db (see sleepycat) and elsewhere.
 
     source: http://www.cse.yorku.ca/~oz/hash.html
