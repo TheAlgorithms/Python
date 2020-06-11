409521c0cc5639b86ee9a50fdde668e60ff9efd8
nikalosa <nlosa16@freeuni.edu.ge>
2020-06-11 20:55:29 +0400

    Add additional function as an example of z-function usage, change docstrings for functions

--- strings/z_function.py
+++ strings/z_function.py
@@ -1,25 +1,24 @@
 """
 https://cp-algorithms.com/string/z-function.html
 
-For given string this algorithm computes value for each index,
-which represents the maximal length substring starting from the index
-and is same as the prefix of the same size
+Z-function or Z algorithm
 
-e.x.  for string 'abab' for second index value would be 2
-
-The main adventage of the algorithm is that it's linear, using dynamic programming
+Efficient algorithm for pattern occurrence in a string
 
 Time Complexity: O(n)  - where n is the length of the string
-
-
-For the value of the first element the algorithm always returns 0
 
 """
 
 
 def z_function(input_str: str) -> list:
     """
-    Will convert the entire string to uppercase letters
+    For given string this function computes value for each index,
+    which represents the maximal length substring starting from the index
+    and is same as the prefix of the same size
+
+    e.x.  for string 'abab' for second index value would be 2
+
+    For the value of the first element the algorithm always returns 0
 
     >>> z_function("abracadabra")
     [0, 0, 0, 1, 0, 1, 0, 4, 0, 0, 1]
@@ -44,16 +43,49 @@ def z_function(input_str: str) -> list:
         while go_next(i, z_result, input_str):
             z_result[i] += 1
 
-        # if new index's result gives us more right interval, we've to update left_pointer and right_pointer
+        # if new index's result gives us more right interval,
+        # we've to update left_pointer and right_pointer
         if i + z_result[i] - 1 > right_pointer:
             left_pointer, right_pointer = i, i + z_result[i] - 1
 
     return z_result
 
 
-# helping function which checks if following elements are equal or not
 def go_next(i, z_result, s):
+    """
+    Helping function, which checks if we have to
+    move forward to the next characters or not
+    """
     return i + z_result[i] < len(s) and s[z_result[i]] == s[i + z_result[i]]
+
+
+def find_pattern(pattern: str, input_str: str) -> int:
+    """
+    Example of using z-function for pattern occurrence
+    Given function returns the number of times 'pattern' 
+    appears in 'input_str' as a substring
+
+    >>> find_pattern("abr", "abracadabra")
+    2
+    >>> find_pattern("a", "aaaa")
+    4
+    >>> find_pattern("xz", "zxxzxxz")
+    2
+
+    """
+    answer = 0
+    # concatenate 'pattern' and 'input_str' and call z_function
+    # with concatenated string
+    z_result = z_function(pattern + input_str)
+
+    for val in z_result:
+        # if value is greater then length of the pattern string
+        # that means this index is starting position of substring
+        # which is equal to pattern string
+        if val >= len(pattern):
+            answer += 1
+
+    return answer
 
 
 if __name__ == "__main__":
