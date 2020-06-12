6674903e1a4a6dbd4615cefb0b245874af831f3c
Akash Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-06-12 17:21:09 +0530

    Find non-duplicate
    Find the non-duplicate number using bit manipulation!

---
+++ bit_manipulation/unique_in_array_xor.py
@@ -0,0 +1,13 @@
+def find_non_duplicate(nums: list) -> int:
+    '''
+    >>>find_non_duplicate([1, 2, 1, 2, 3, 4, 3])
+    4
+    This occurs as:
+    1. Any number xor itself is 0.
+    2. Any number xor 0 is itself.
+    See more: https://en.wikipedia.org/wiki/XOR_gate#:~:text=XOR%20gate%20(sometimes%20EOR%2C%20or,to%20the%20gate%20is%20true.
+    '''
+    x = 0
+    for num in nums:
+        x ^= num
+    return x
