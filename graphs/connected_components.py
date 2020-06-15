817b56de7ed773c4b6688c96ebab922332c365ba
Nika Losaberidze <nlosa16@freeuni.edu.ge>
2020-06-15 18:48:46 +0400

    Add doctests and typehints

--- graphs/connected_components.py
+++ graphs/connected_components.py
@@ -25,10 +25,14 @@ test_graph_2 = {
 }
 
 
-def dfs(graph, vert, visited):
+def dfs(graph: dict, vert: int, visited: list) -> list:
     """
     Use depth first search to find all vertexes
     being in the same component as initial vertex
+    >>> dfs(test_graph_1, 0, 5 * [False])
+    [0, 1, 3, 2]
+    >>> dfs(test_graph_2, 0, 6 * [False])
+    [0, 1, 3, 2]
     """
 
     visited[vert] = True
