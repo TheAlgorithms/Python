1bea890ae5867f4ec1a0988d9162c834288062ee
Christian Clauss <cclauss@me.com>
2020-06-16 20:54:55 +0200

    open_file() --> analyze_text()

--- maths/entropy.py
+++ maths/entropy.py
@@ -1,24 +1,17 @@
+#!/usr/bin/env python3
+
 """
-    Implementation of entropy of information
-    https://en.wikipedia.org/wiki/Entropy_(information_theory)
+Implementation of entropy of information
+https://en.wikipedia.org/wiki/Entropy_(information_theory)
 """
 
 import math
 from collections import Counter
+from string import ascii_lowercase
+from typing import Tuple
 
 
-def create_alpha_array():
-    """
-    This method creates array of our alphabet to print probabilities later on.
-    :return:
-    """
-    my_alphas = [" "]
-    for x in range(ord("a"), ord("z") + 1):
-        my_alphas += chr(x)
-    return my_alphas
-
-
-def calculate_prob(text):
+def calculate_prob(text: str) -> None:
     """
     This method takes path and two dict as argument
     and than calculates entropy of them.
@@ -30,8 +23,8 @@ def calculate_prob(text):
     3) print Entropy of H(X n∣Xn−1)
 
     Text from random books. Also, random quotes.
-    >>> text = ("Behind Winston’s back the voice"
-    ...         "from the telescreen was still"
+    >>> text = ("Behind Winston’s back the voice "
+    ...         "from the telescreen was still "
     ...         "babbling and the overfulfilment")
     >>> calculate_prob(text)
     4.0
@@ -44,88 +37,74 @@ def calculate_prob(text):
     4.0
     5.0
     1.0
-    >>> text = ("Had repulsive dashwoods suspicion sincerity but advantage now him. Remark easily garret nor nay."
-    ...         "Civil those mrs enjoy shy fat merry. You greatest jointure saw horrible. He private he on be "
-    ...         "imagine suppose. Fertile beloved evident through no service elderly is. Blind there if every "
-    ...         "no so at. Own neglected you preferred way sincerity delivered his attempted. To of message "
-    ...         "cottage windows do besides against uncivil.  Delightful unreserved impossible few estimating "
-    ...         "men favourable see entreaties. She propriety immediate was improving. He or entrance humoured "
-    ...         "likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we "
-    ...         "offending prevailed discovery. ")
+    >>> text = ("Had repulsive dashwoods suspicion sincerity but advantage now him. "
+    ...         "Remark easily garret nor nay.  Civil those mrs enjoy shy fat merry. "
+    ...         "You greatest jointure saw horrible. He private he on be imagine "
+    ...         "suppose. Fertile beloved evident through no service elderly is. Blind "
+    ...         "there if every no so at. Own neglected you preferred way sincerity "
+    ...         "delivered his attempted. To of message cottage windows do besides "
+    ...         "against uncivil.  Delightful unreserved impossible few estimating "
+    ...         "men favourable see entreaties. She propriety immediate was improving. "
+    ...         "He or entrance humoured likewise moderate. Much nor game son say "
+    ...         "feel. Fat make met can must form into gate. Me we offending prevailed "
+    ...         "discovery.")
     >>> calculate_prob(text)
     4.0
     7.0
     3.0
     """
-    my_dic_fir, my_dic_sec = open_file(text)
-    my_alphas = create_alpha_array()  # create our alpha
+    single_char_strings, two_char_strings = analyze_text(text)
+    my_alphas = list(' ' + ascii_lowercase)
     # what is our total sum of probabilities.
-    values = my_dic_fir.values()
-    all_sum = sum(my_dic_fir.values())
+    all_sum = sum(single_char_strings.values())
 
     # one length string
     my_fir_sum = 0
     # for each alpha we go in our dict and if it is in it we calculate entropy
     for ch in my_alphas:
-        if ch in my_dic_fir:
-            my_str = my_dic_fir[ch]
+        if ch in single_char_strings:
+            my_str = single_char_strings[ch]
             prob = my_str / all_sum
             my_fir_sum += prob * math.log2(prob)  # entropy formula.
 
-    # write entropy to file
+    # print entropy
     print("{0:.1f}".format(round(-1 * my_fir_sum)))
 
     # two len string
-    values = my_dic_sec.values()
-    all_sum = sum(values)  # time for two len probabilities sum
+    all_sum = sum(two_char_strings.values())
     my_sec_sum = 0
     # for each alpha (two in size) calculate entropy.
     for ch0 in my_alphas:
         for ch1 in my_alphas:
             sequence = ch0 + ch1
-            if sequence in my_dic_sec:
-                my_str = my_dic_sec[sequence]
+            if sequence in two_char_strings:
+                my_str = two_char_strings[sequence]
                 prob = int(my_str) / all_sum
                 my_sec_sum += prob * math.log2(prob)
 
-    # write second entropy to the file
+    # print second entropy
     print("{0:.1f}".format(round(-1 * my_sec_sum)))
 
-    # write the difference between them to the file
+    # print the difference between them
     print("{0:.1f}".format(round(((-1 * my_sec_sum) - (-1 * my_fir_sum)))))
 
 
-def open_file(text):
+def analyze_text(text: str) -> Tuple[dict, dict]:
     """
-    This method take path and also two clear dict as argument
-    And later on it open the file at this path and puts frequency on each dictionary
-    in first dictionary it puts one len strings.
-    in second dictionary it puts two len strings.
-    :param text as string type:
-    :return: dict, dict
+    Convert text input into two dicts of counts.
+    The first dictionary stores the frequency of single character strings.
+    The second dictionary stores the frequency of two character strings.
     """
-    my_dic_fir = {}
-    my_dic_sec = {}
-    my_dic_fir[text[len(text) - 1]] = 1
+    single_char_strings = Counter()
+    two_char_strings = Counter()
+    single_char_strings[text[-1]] += 1
 
     # first case when we have space at start.
-    my_dic_sec[" " + text[0]] = 1
-    for _ in range(0, len(text) - 1):
-        sequence_fir = text[_]
-        sequence_sec = text[_ : _ + 2]
-        # continue
-
-        if not Counter(my_dic_fir)[sequence_fir]:
-            my_dic_fir[sequence_fir] = 1
-        else:
-            my_dic_fir[sequence_fir] += 1
-
-        if not Counter(my_dic_sec)[sequence_sec]:
-            my_dic_sec[sequence_sec] = 1
-        else:
-            my_dic_sec[sequence_sec] += 1
-
-    return my_dic_fir, my_dic_sec
+    two_char_strings[" " + text[0]] += 1
+    for i in range(0, len(text) - 1):
+        single_char_strings[text[i]] += 1
+        two_char_strings[text[i : i + 2]] += 1
+    return single_char_strings, two_char_strings
 
 
 def main():
@@ -133,14 +112,16 @@ def main():
 
     doctest.testmod()
     # text = (
-    #     "Had repulsive dashwoods suspicion sincerity but advantage now him. Remark easily garret nor nay."
-    #     "Civil those mrs enjoy shy fat merry. You greatest jointure saw horrible. He private he on be "
-    #     "imagine suppose. Fertile beloved evident through no service elderly is. Blind there if every "
-    #     "no so at. Own neglected you preferred way sincerity delivered his attempted. To of message "
-    #     "cottage windows do besides against uncivil.  Delightful unreserved impossible few estimating "
-    #     "men favourable see entreaties. She propriety immediate was improving. He or entrance humoured "
-    #     "likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we "
-    #     "offending prevailed discovery. "
+    #     "Had repulsive dashwoods suspicion sincerity but advantage now him. Remark "
+    #     "easily garret nor nay. Civil those mrs enjoy shy fat merry. You greatest "
+    #     "jointure saw horrible. He private he on be imagine suppose. Fertile "
+    #     "beloved evident through no service elderly is. Blind there if every no so "
+    #     "at. Own neglected you preferred way sincerity delivered his attempted. To "
+    #     "of message cottage windows do besides against uncivil.  Delightful "
+    #     "unreserved impossible few estimating men favourable see entreaties. She "
+    #     "propriety immediate was improving. He or entrance humoured likewise "
+    #     "moderate. Much nor game son say feel. Fat make met can must form into "
+    #     "gate. Me we offending prevailed discovery. "
     # )
 
     # calculate_prob(text)
