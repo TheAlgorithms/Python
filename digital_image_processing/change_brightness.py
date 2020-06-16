3fae49d6549390436c7641ad9bb3a9af6e1a5a5a
Furkan Atesli <31884209+furkanatesli@users.noreply.github.com>
2020-06-17 01:18:09 +0300

    Update change_brightness.py

--- digital_image_processing/change_brightness.py
+++ digital_image_processing/change_brightness.py
@@ -1,23 +1,24 @@
 from PIL import Image
 
+
 def change_brightness(img: Image, level: float) -> Image:
     """
     Change the brightness of a PIL Image to a given level.
     """
-    factor = (259 + (level + 255)) / (255 + (259 - level))
-
+    
     def brightness(c: int) -> float:
         """
         Fundamental Transformation/Operation that'll be performed on
         every bit.
         """
-        return 128 + factor * (c - 128)
+        return 128 + level + (c - 128)
 
     return img.point(brightness)
+
 
 if __name__ == "__main__":
     # Load image
     with Image.open("image_data/lena.jpg") as img:
-        # Change brightness to 170
-        brigt_img = change_brightness(img, 170)
-        brigt_img.save("image_data/lena_high_contrast.png", format="png")
+        # Change brightness to 100
+        brigt_img = change_brightness(img, 100)
+        brigt_img.save("image_data/lena_brightness.png", format="png")
