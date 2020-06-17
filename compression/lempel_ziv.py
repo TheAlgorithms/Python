9d44acc682bbda82ea1192cc707b58afd544019c
ioane margiani <margianiioane@gmail.com>
2020-06-17 22:24:42 +0400

    Added type hints and some other modifications (Doctests coming up)

--- compression/lempel_ziv.py
+++ compression/lempel_ziv.py
@@ -3,29 +3,31 @@
     https://en.wikipedia.org/wiki/Lempel%E2%80%93Ziv%E2%80%93Welch
 """
 
+import math
 import os
 import sys
-import math
 
 
-def read_file_binary(file):
+def read_file_binary(file_path: str) -> str:
     """
     Reads given file as bytes and returns them as a long string
     """
     result = ""
     try:
-        with open(file, "rb") as binary_file:
+        with open(file_path, "rb") as binary_file:
             data = binary_file.read()
-            for dat in data:
-                curr_byte = "{0:08b}".format(dat)
-                result += curr_byte
+        for dat in data:
+            curr_byte = "{0:08b}".format(dat)
+            result += curr_byte
         return result
     except IOError:
         print("File not accessible")
         sys.exit()
 
 
-def add_key_to_lexicon(lexicon, curr_string, index, last_match_id):
+def add_key_to_lexicon(
+    lexicon: dict, curr_string: str, index: int, last_match_id: int
+) -> None:
     """
     Adds new strings (curr_string + "0",  curr_string + "1") to the lexicon
     """
@@ -33,13 +35,13 @@ def add_key_to_lexicon(lexicon, curr_string, index, last_match_id):
     lexicon[curr_string + "0"] = last_match_id
 
     if math.log2(index).is_integer():
-        for curr_key in lexicon.keys():
+        for curr_key in lexicon:
             lexicon[curr_key] = "0" + lexicon[curr_key]
 
     lexicon[curr_string + "1"] = bin(index)[2:]
 
 
-def compress_data(data_bits):
+def compress_data(data_bits: str) -> str:
     """
     Compresses given data_bits using Lempel–Ziv–Welch compression algorithm
     and returns the result as a string
@@ -50,7 +52,7 @@ def compress_data(data_bits):
 
     for i in range(len(data_bits)):
         curr_string += data_bits[i]
-        if curr_string not in lexicon.keys():
+        if curr_string not in lexicon:
             continue
 
         last_match_id = lexicon[curr_string]
@@ -59,7 +61,7 @@ def compress_data(data_bits):
         index += 1
         curr_string = ""
 
-    while curr_string != "" and curr_string not in lexicon.keys():
+    while curr_string != "" and curr_string not in lexicon:
         curr_string += "0"
 
     if curr_string != "":
@@ -69,24 +71,24 @@ def compress_data(data_bits):
     return result
 
 
-def add_file_length(source, compressed):
+def add_file_length(source_path: str, compressed: str) -> str:
     """
     Adds given file's length in front (using Elias  gamma coding) of the compressed string
     """
-    file_length = os.path.getsize(source)
+    file_length = os.path.getsize(source_path)
     file_length_binary = bin(file_length)[2:]
     length_length = len(file_length_binary)
 
     return "0" * (length_length - 1) + file_length_binary + compressed
 
 
-def write_file_binary(file, to_write):
+def write_file_binary(file_path: str, to_write: str) -> None:
     """
     Writes given to_write string (should only consist of 0's and 1's) as bytes in the file
     """
     byte_length = 8
     try:
-        with open(file, "wb") as opened_file:
+        with open(file_path, "wb") as opened_file:
             result_byte_array = [
                 to_write[i : i + byte_length]
                 for i in range(0, len(to_write), byte_length)
@@ -106,14 +108,14 @@ def write_file_binary(file, to_write):
         sys.exit()
 
 
-def compress(source, destination):
+def compress(source_path, destination_path: str) -> None:
     """
     Reads source file, compresses it and writes the compressed result in destination file
     """
-    data_bits = read_file_binary(source)
+    data_bits = read_file_binary(source_path)
     compressed = compress_data(data_bits)
-    compressed = add_file_length(source, compressed)
-    write_file_binary(destination, compressed)
+    compressed = add_file_length(source_path, compressed)
+    write_file_binary(destination_path, compressed)
 
 
 if __name__ == "__main__":
--- compression/lempel_ziv_decompress.py
+++ compression/lempel_ziv_decompress.py
@@ -3,28 +3,28 @@
     https://en.wikipedia.org/wiki/Lempel%E2%80%93Ziv%E2%80%93Welch
 """
 
-import sys
 import math
+import sys
 
 
-def read_file_binary(file):
+def read_file_binary(file_path: str) -> str:
     """
     Reads given file as bytes and returns them as a long string
     """
     result = ""
     try:
-        with open(file, "rb") as binary_file:
+        with open(file_path, "rb") as binary_file:
             data = binary_file.read()
-            for dat in data:
-                curr_byte = "{0:08b}".format(dat)
-                result += curr_byte
+        for dat in data:
+            curr_byte = "{0:08b}".format(dat)
+            result += curr_byte
         return result
     except IOError:
         print("File not accessible")
         sys.exit()
 
 
-def decompress_data(data_bits):
+def decompress_data(data_bits: str) -> str:
     """
     Decompresses given data_bits using Lempel–Ziv–Welch compression algorithm
     and returns the result as a string
@@ -35,7 +35,7 @@ def decompress_data(data_bits):
 
     for i in range(len(data_bits)):
         curr_string += data_bits[i]
-        if curr_string not in lexicon.keys():
+        if curr_string not in lexicon:
             continue
 
         last_match_id = lexicon[curr_string]
@@ -44,7 +44,7 @@ def decompress_data(data_bits):
 
         if math.log2(index).is_integer():
             newLex = {}
-            for curr_key in list(lexicon.keys()):
+            for curr_key in list(lexicon):
                 newLex["0" + curr_key] = lexicon.pop(curr_key)
             lexicon = newLex
 
@@ -54,13 +54,13 @@ def decompress_data(data_bits):
     return result
 
 
-def write_file_binary(file, to_write):
+def write_file_binary(file_path: str, to_write: str) -> None:
     """
     Writes given to_write string (should only consist of 0's and 1's) as bytes in the file
     """
     byte_length = 8
     try:
-        with open(file, "wb") as opened_file:
+        with open(file_path, "wb") as opened_file:
             result_byte_array = [
                 to_write[i : i + byte_length]
                 for i in range(0, len(to_write), byte_length)
@@ -80,7 +80,7 @@ def write_file_binary(file, to_write):
         sys.exit()
 
 
-def remove_prefix(data_bits):
+def remove_prefix(data_bits: str) -> str:
     """
     Removes size prefix, that compressed file should have
     Returns the result
@@ -96,14 +96,14 @@ def remove_prefix(data_bits):
     return data_bits
 
 
-def compress(source, destination):
+def compress(source_path: str, destination_path: str) -> None:
     """
     Reads source file, decompresses it and writes the result in destination file
     """
-    data_bits = read_file_binary(source)
+    data_bits = read_file_binary(source_path)
     data_bits = remove_prefix(data_bits)
     decompressed = decompress_data(data_bits)
-    write_file_binary(destination, decompressed)
+    write_file_binary(destination_path, decompressed)
 
 
 if __name__ == "__main__":
