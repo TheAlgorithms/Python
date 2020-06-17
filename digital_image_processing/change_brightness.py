d94f7016d77131f13650e2a502b9859d10933942
Christian Clauss <cclauss@me.com>
2020-06-17 07:14:16 +0200

    Update change_brightness.py

--- digital_image_processing/change_brightness.py
+++ digital_image_processing/change_brightness.py
@@ -13,6 +13,8 @@ def change_brightness(img: Image, level: float) -> Image:
         """
         return 128 + level + (c - 128)
 
+    if not -255.0 <= level <= 255.0:
+        raise ValueError("level must be between -255.0 (black) and 255.0 (white)")
     return img.point(brightness)
 
 
