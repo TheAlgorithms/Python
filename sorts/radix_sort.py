765d69de5d1d3f89fa1bde16226999e9d3259985
Christian Clauss <cclauss@me.com>
2020-06-23 15:06:52 +0200

    Update radix_sort.py

--- sorts/radix_sort.py
+++ sorts/radix_sort.py
@@ -10,27 +10,20 @@ def radix_sort(list_of_ints: List[int]) -> List[int]:
     """
     RADIX = 10
     placement = 1
-
-    # get the maximum number
     max_digit = max(list_of_ints)
-
     while placement < max_digit:
-        # declare and initialize buckets
+        # declare and initialize empty buckets
         buckets = [list() for _ in range(RADIX)]
-
-        # split list_of_ints between lists
+        # split list_of_ints between the buckets
         for i in list_of_ints:
             tmp = int((i / placement) % RADIX)
             buckets[tmp].append(i)
-
-        # empty lists into list_of_ints
+        # put each buckets' contents into list_of_ints
         a = 0
         for b in range(RADIX):
-            buck = buckets[b]
-            for i in buck:
+            for i in buckets[b]:
                 list_of_ints[a] = i
                 a += 1
-
         # move to next
         placement *= RADIX
     return list_of_ints
