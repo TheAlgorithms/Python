95084b3c3ab01fc8a2d38bc5bd2cc3380cec638a
mateuszz0000 <mtszzwdzk@gmail.com>
2020-06-24 16:16:28 +0200

    Added doctests

--- sorts/odd_even_transposition_single_threaded.py
+++ sorts/odd_even_transposition_single_threaded.py
@@ -7,11 +7,20 @@ is no better than bubble sort.
 
 
 def OddEvenTransposition(arr):
+    """
+    >>> OddEvenTransposition([5, 4, 3, 2, 1])
+    [1, 2, 3, 4, 5]
+
+    >>> OddEvenTransposition([13, 11, 18, 0, -1])
+    [-1, 0, 11, 13, 18]
+
+    >>> OddEvenTransposition([-.1, 1.1, .1, -2.9])
+    [-2.9, -0.1, 0.1, 1.1]
+    """
     for i in range(0, len(arr)):
         for i in range(i % 2, len(arr) - 1, 2):
             if arr[i + 1] < arr[i]:
                 arr[i], arr[i + 1] = arr[i + 1], arr[i]
-        print(*arr)
 
     return arr
 
