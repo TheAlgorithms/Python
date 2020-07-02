a342617cac067f04c7b2c178fb06cbbcde733eaa
wuyudi <wuyudi119@163.com>
2020-07-02 03:50:03 +0000

    Update matrix_operation.py

--- matrix/matrix_operation.py
+++ matrix/matrix_operation.py
@@ -4,36 +4,43 @@ function based version of matrix operations, which are just 2D arrays
 
 
 def add(matrix_a, matrix_b):
+    '''
+    >>> add([[1,2],[3,4]],[[2,3],[4,5]])
+    [[3, 5], [7, 9]]
+    '''
     if _check_not_integer(matrix_a) and _check_not_integer(matrix_b):
         rows, cols = _verify_matrix_sizes(matrix_a, matrix_b)
-        matrix_c = []
-        for i in range(rows[0]):
-            list_1 = []
-            for j in range(cols[0]):
-                val = matrix_a[i][j] + matrix_b[i][j]
-                list_1.append(val)
-            matrix_c.append(list_1)
+        matrix_c = [[matrix_a[i][j] + matrix_b[i][j]
+                     for j in range(cols[0])] for i in range(rows[0])]
         return matrix_c
 
 
 def subtract(matrix_a, matrix_b):
+    '''
+    >>> subtract([[1,2],[3,4]],[[2,3],[4,5]])
+    [[-1, -1], [-1, -1]]
+    '''
     if _check_not_integer(matrix_a) and _check_not_integer(matrix_b):
         rows, cols = _verify_matrix_sizes(matrix_a, matrix_b)
-        matrix_c = []
-        for i in range(rows[0]):
-            list_1 = []
-            for j in range(cols[0]):
-                val = matrix_a[i][j] - matrix_b[i][j]
-                list_1.append(val)
-            matrix_c.append(list_1)
+        matrix_c = [
+            [matrix_a[i][j] - matrix_b[i][j] for j in range(cols[0])]
+            for i in range(rows[0])]
         return matrix_c
 
 
 def scalar_multiply(matrix, n):
+    '''
+    >>> scalar_multiply([[1,2],[3,4]],5)
+    [[5, 10], [15, 20]]
+    '''
     return [[x * n for x in row] for row in matrix]
 
 
 def multiply(matrix_a, matrix_b):
+    '''
+    >>> multiply([[1,2],[3,4]],[[5,5],[7,5]])
+    [[19, 15], [43, 35]]
+    '''
     if _check_not_integer(matrix_a) and _check_not_integer(matrix_b):
         matrix_c = []
         rows, cols = _verify_matrix_sizes(matrix_a, matrix_b)
@@ -48,7 +55,7 @@ def multiply(matrix_a, matrix_b):
             for j in range(cols[1]):
                 val = 0
                 for k in range(cols[1]):
-                    val = val + matrix_a[i][k] * matrix_b[k][j]
+                    val += matrix_a[i][k] * matrix_b[k][j]
                 list_1.append(val)
             matrix_c.append(list_1)
         return matrix_c
@@ -59,30 +66,42 @@ def identity(n):
     :param n: dimension for nxn matrix
     :type n: int
     :return: Identity matrix of shape [n, n]
+    >>> identity(3)
+    [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
     """
     n = int(n)
     return [[int(row == column) for column in range(n)] for row in range(n)]
 
 
 def transpose(matrix, return_map=True):
+    '''
+    >>> transpose([[1,2],[3,4]]) # doctest: +ELLIPSIS
+    <map object at ...
+    >>> transpose([[1,2],[3,4]], return_map=False)
+    [[1, 3], [2, 4]]
+    '''
     if _check_not_integer(matrix):
         if return_map:
             return map(list, zip(*matrix))
         else:
-            # mt = []
-            # for i in range(len(matrix[0])):
-            #     mt.append([row[i] for row in matrix])
-            # return mt
             return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
 
 
 def minor(matrix, row, column):
-    minor = matrix[:row] + matrix[row + 1 :]
-    minor = [row[:column] + row[column + 1 :] for row in minor]
+    '''
+    >>> minor([[1, 2], [3, 4]], 1, 1)
+    [[1]]
+    '''
+    minor = matrix[:row] + matrix[row + 1:]
+    minor = [row[:column] + row[column + 1:] for row in minor]
     return minor
 
 
 def determinant(matrix):
+    '''
+    >>> determinant([[1, 2], [3, 4]])
+    -2
+    '''
     if len(matrix) == 1:
         return matrix[0][0]
 
@@ -93,11 +112,17 @@ def determinant(matrix):
 
 
 def inverse(matrix):
+    '''
+    >>> inverse([[1, 2], [3, 4]])
+    [[-2.0, 1.0], [1.5, -0.5]]
+    >>> inverse([[1, 1], [1, 1]])
+    '''
+    # https://stackoverflow.com/questions/20047519/python-doctests-test-for-none
     det = determinant(matrix)
     if det == 0:
         return None
 
-    matrix_minor = [[] for _ in range(len(matrix))]
+    matrix_minor = [[] for _ in matrix]
     for i in range(len(matrix)):
         for j in range(len(matrix)):
             matrix_minor[i].append(determinant(minor(matrix, i, j)))
@@ -134,21 +159,23 @@ def _verify_matrix_sizes(matrix_a, matrix_b):
 def main():
     matrix_a = [[12, 10], [3, 9]]
     matrix_b = [[3, 4], [7, 4]]
-    matrix_c = [[11, 12, 13, 14], [21, 22, 23, 24], [31, 32, 33, 34], [41, 42, 43, 44]]
+    matrix_c = [[11, 12, 13, 14], [21, 22, 23, 24],
+                [31, 32, 33, 34], [41, 42, 43, 44]]
     matrix_d = [[3, 0, 2], [2, 0, -2], [0, 1, 1]]
     print(
-        "Add Operation, %s + %s = %s \n"
-        % (matrix_a, matrix_b, (add(matrix_a, matrix_b)))
+        f"Add Operation, {matrix_a} +{matrix_b} = {add(matrix_a, matrix_b)} \n"
     )
     print(
-        "Multiply Operation, %s * %s = %s \n"
-        % (matrix_a, matrix_b, multiply(matrix_a, matrix_b))
+        f"Multiply Operation, {matrix_a} * {matrix_b}",
+        f"= {multiply(matrix_a, matrix_b)} \n"
     )
-    print("Identity:  %s \n" % identity(5))
-    print("Minor of {} = {} \n".format(matrix_c, minor(matrix_c, 1, 2)))
-    print("Determinant of {} = {} \n".format(matrix_b, determinant(matrix_b)))
-    print("Inverse of {} = {}\n".format(matrix_d, inverse(matrix_d)))
+    print(f"Identity:  {identity(5)}\n")
+    print(f"Minor of {matrix_c} = {minor(matrix_c, 1, 2)} \n")
+    print(f"Determinant of {matrix_b} = {determinant(matrix_b)} \n")
+    print(f"Inverse of {matrix_d} = {inverse(matrix_d)}\n")
 
 
 if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
     main()
