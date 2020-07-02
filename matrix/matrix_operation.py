5c0f79c932e8ab99c71e910d549e0c11a8a47853
wuyudi <wuyudi1109@gmail.com>
2020-07-02 23:01:08 +0800

    Update matrix/matrix_operation.py
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- matrix/matrix_operation.py
+++ matrix/matrix_operation.py
@@ -175,7 +175,7 @@ def main():
                 [31, 32, 33, 34], [41, 42, 43, 44]]
     matrix_d = [[3, 0, 2], [2, 0, -2], [0, 1, 1]]
     print(
-        f"Add Operation, {matrix_a} + {matrix_b}",
+        f"Add Operation, {matrix_a} + {matrix_b}"
         f" = {add(matrix_a, matrix_b)} \n")
     print(
         f"Multiply Operation, {matrix_a} * {matrix_b}",
