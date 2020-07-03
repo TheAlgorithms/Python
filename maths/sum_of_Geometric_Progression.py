106a2004d186847ec8282882fd93d9f62551942d
Advik Kulkarni <56193714+AxSmasher44@users.noreply.github.com>
2020-07-04 02:23:41 +0530

    Add files via upload

---
+++ maths/sum_of_Geometric_Progression.py
@@ -0,0 +1,19 @@
+def sum_of_gp(first_term, common_ratio, num_of_terms):
+    """"
+    Find the sum of n terms in a geometric progression.
+    >>> sum_of_gp(1, 2, 10)
+    1023.0
+    >>> sum_of_gp(1, 10, 100)
+    11111.0
+    """
+    if common_ratio == 1:
+        sum = num_of_terms * first_term
+        return sum
+    # formula for sum if common ratio is 1
+
+    sum = (first_term / (1 - common_ratio)) * (1 - common_ratio ** num_of_terms)
+    return sum
+    # Formula for finding sum of n terms of a GeometricProgression
+
+
+print(sum_of_gp(1, 10, 5))
