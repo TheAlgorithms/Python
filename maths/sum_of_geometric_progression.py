b39162ac10b2c1016641cb7ea46fc8245074034a
Advik Kulkarni <56193714+AxSmasher44@users.noreply.github.com>
2020-07-04 14:35:09 +0530

    Update sum_of_geometric_progression.py

--- maths/sum_of_geometric_progression.py
+++ maths/sum_of_geometric_progression.py
@@ -1,6 +1,4 @@
-def sum_of_geometric_progression(
-    first_term: int, common_ratio: int, num_of_terms: int
-) -> float
+def sum_of_geometric_progression(first_term, common_ratio, num_of_terms):
     """"
     Find the sum of n terms in a geometric progression.
     >>> sum_of_gp(1, 2, 10)
@@ -10,13 +8,7 @@ def sum_of_geometric_progression(
     """
     if common_ratio == 1:
         return num_of_terms * first_term
-        return sum
     # formula for sum if common ratio is 1
 
     return (first_term / (1 - common_ratio)) * (1 - common_ratio ** num_of_terms)
-
-    return sum
     # Formula for finding sum of n terms of a GeometricProgression
-
-
-print(sum_of_gp(1, 10, 5))
