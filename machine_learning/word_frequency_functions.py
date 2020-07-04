43bc139bbd71730ea868cc668d7e69d9eb726b2b
ShubhamShaswat <chandragupta4115@gmail.com>
2020-07-05 02:56:07 +0530

    added idf-smooth

--- machine_learning/word_frequency_functions.py
+++ machine_learning/word_frequency_functions.py
@@ -111,7 +111,7 @@ def inverse_document_frequency(df: int, N: int, smoothing=False) -> float:
     if smoothing:
         if N == 0:
             raise ValueError("log10(0) is undefined.")
-        return round(1 + log10(N / (1+df)), 3)
+        return round(1 + log10(N / (1 + df)), 3)
 
     if df == 0:
         raise ZeroDivisionError("df must be > 0")
