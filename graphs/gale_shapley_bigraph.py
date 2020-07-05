054dff2712f3252d56838e2e480b46620d70e6dc
Akash Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-07-05 10:48:02 +0530

    Edited the line lengths

--- graphs/gale_shapley_bigraph.py
+++ graphs/gale_shapley_bigraph.py
@@ -3,8 +3,9 @@ def stable_matching(n: int, donor_pref: list, recipient_pref: list) -> list:
     Finds the stable match in any bipartite graph, i.e a pairing where no 2
     objects prefer each other over their partner.
     The function accepts the preferences of the donors and recipients (where
-    both are assigned numbers from 0 to n-1) and returns a list where the index position
-    corresponds to the donor and value at the index is the organ recipient.
+    both are assigned numbers from 0 to n-1) and returns a list where the index
+    position corresponds to the donor and value at the index is the organ
+    recipient.
     E.g:
     n = 4
     donor_pref = [[0, 1, 3, 2], [0, 2, 3, 1], [1, 0, 2, 3], [0, 3, 1, 2]]
@@ -17,21 +18,21 @@ def stable_matching(n: int, donor_pref: list, recipient_pref: list) -> list:
     '''
     unmatched_donors = [i for i in range(n)]
     donor_record = [None for i in range(n)]  # who the donor has donated to
-    recipient_record = [None for i in range(n)]  # donor received from
+    rec_record = [None for i in range(n)]  # donor received from
     num_donations = [0 for i in range(n)]
-    while undonated_donors:
-        donor = undonated_donors[0]
+    while unmatched_donors:
+        donor = unmatched_donors[0]
         donor_preference = donor_pref[donor]
         recipient = donor_preference[num_donations[donor]]
         num_donations[donor] += 1
-        recipient_preference = recipient_pref[recipient]
-        prev_donor = recipient_record[recipient]
+        rec_preference = recipient_pref[recipient]
+        prev_donor = rec_record[recipient]
         if prev_donor is not None:
-            if recipient_preference.index(prev_donor) > recipient_preference.index(donor):
-                recipient_record[recipient], donor_record[donor] = donor, recipient
-                undonated_donors.append(prev_donor)
-                undonated_donors.remove(donor)
+            if rec_preference.index(prev_donor) > rec_preference.index(donor):
+                rec_record[recipient], donor_record[donor] = donor, recipient
+                unmatched_donors.append(prev_donor)
+                unmatched_donors.remove(donor)
         else:
-            recipient_record[recipient], donor_record[donor] = donor, recipient
-            undonated_donors.remove(donor)
+            rec_record[recipient], donor_record[donor] = donor, recipient
+            unmatched_donors.remove(donor)
     return donor_record
