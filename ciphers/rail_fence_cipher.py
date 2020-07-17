6d048b022cea02599d09bb769d15e8b5113d32a0
Aakash Dinkar <35952953+aakashdinkar@users.noreply.github.com>
2020-07-18 00:48:36 +0530

    Update rail_fence_cipher.py

--- ciphers/rail_fence_cipher.py
+++ ciphers/rail_fence_cipher.py
@@ -1,41 +1,43 @@
-string = input(Enter string to be encrypted:)
+string = input("Enter string to be encrypted:")
 print("Enter key for encryption (works best for small key values) : ", end = "")
 key = int(input())
 
 while key >= len(string):
 	print("Please enter a smaller value: ", end = "")
 	key = int(input())
+	
+def rail_fence_encrypt(string, key):
+	a = [[None for i in range(len(string))] for j in range(key)]
 
-a = [[None for i in range(len(string))] for j in range(key)]
+	curr = 0
+	findex = 0
+	sindex = 0
 
-curr = 0
-findex = 0
-sindex = 0
+	while curr < len(string):
+		if findex < key:
+			while findex < key and curr < len(string):
+				a[findex][sindex] = string[curr]
+				curr += 1
+				findex += 1
+				sindex += 1
 
-while curr < len(string):
-	if findex < key:
-		while findex < key and curr < len(string):
-			a[findex][sindex] = string[curr]
-			curr += 1
-			findex += 1
-			sindex += 1
-
-	if findex == key:
-		findex -= 1
-		while findex > 0 and curr < len(string):
+		if findex == key:
 			findex -= 1
-			a[findex][sindex] = string[curr]
-			curr += 1
-			sindex += 1
-		findex += 1
+			while findex > 0 and curr < len(string):
+				findex -= 1
+				a[findex][sindex] = string[curr]
+				curr += 1
+				sindex += 1
+			findex += 1
+	return a
+
+encrypt_string = rail_fence_encrypt(string, key)
 
 print("Rail fence matrix created: ")
-for i in a:
-	print(i)
 
 encrypted_string = ""
 
-for i in a:
+for i in encrypt_string:
 	l = [x for x in i if x is not None]
 	encrypted_string += "".join(l)
 
