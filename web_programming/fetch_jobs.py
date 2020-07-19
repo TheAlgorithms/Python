20d70b5c9db42d5dd700d6444bcb9577f2507b91
Kaif <kaifkohari10@gmail.com>
2020-07-20 03:10:51 +0530

    Adding job scarping algorithm to web programming

---
+++ web_programming/fetch_jobs.py
@@ -0,0 +1,33 @@
+'''
+Scraping jobs given job title and location from indeed website
+'''
+
+import requests
+from bs4 import BeautifulSoup
+
+ 
+url = "https://www.indeed.co.in/jobs?q=mobile+app+development&l=mumbai"
+
+
+def fetch_jobs():
+    #request the url
+    res = requests.get(url)
+    soup = BeautifulSoup(res.content, 'lxml')
+
+    # This attribute finds out all the specifics listed in a job
+    results = soup.find_all('div', attrs={'data-tn-component': 'organicJob'})
+
+    for x in results:
+        #Job Title
+        job = x.find('a', attrs={'data-tn-element': "jobTitle"})
+        #company name
+        company = x.find('span', {"class" : "company"})
+        print("Job is {} and Company is {}".format(job.text.strip(), company.text.strip()))
+        
+        
+        
+    
+if __name__ == "__main__":
+    fetch_jobs()
+    
+    
