75c46ea474acefe59c1e23228ae6b8e5d7901804
lance-pyles <lance.pyles@gmail.com>
2020-07-21 15:39:43 -0700

    update variable names for consistency using standard formula terms;
    fix flake8 syntax errors;
    
    fix doctests;

--- maths/area.py
+++ maths/area.py
@@ -5,7 +5,7 @@ from math import pi
 from typing import Union
 
 
-def surface_area_cube(side_length: Union[int, float]) -> float:
+def surface_area_cube(side: Union[int, float]) -> float:
     """
     Calculate the Surface Area of a Cube.
 
@@ -14,7 +14,7 @@ def surface_area_cube(side_length: Union[int, float]) -> float:
     >>> surface_area_cube(3)
     54
     """
-    return 6 * pow(side_length, 2)
+    return 6 * pow(side, 2)
 
 
 def surface_area_sphere(radius: float) -> float:
@@ -31,41 +31,41 @@ def surface_area_sphere(radius: float) -> float:
     return 4 * pi * pow(radius, 2)
 
 
-def area_rectangle(base, height):
+def area_rectangle(length, width):
     """
     Calculate the area of a rectangle
 
-    >> area_rectangle(10,20)
+    >>> area_rectangle(10,20)
     200
     """
-    return base * height
+    return length * width
 
 
-def area_square(side_length):
+def area_square(side):
     """
     Calculate the area of a square
 
     >>> area_square(10)
     100
     """
-    return side_length * side_length
+    return pow(side, 2)
 
 
-def area_triangle(length, breadth):
+def area_triangle(base, height):
     """
     Calculate the area of a triangle
 
     >>> area_triangle(10,10)
     50.0
     """
-    return 1 / 2 * length * breadth
+    return (base * height) / 2
 
 
 def area_parallelogram(base, height):
     """
     Calculate the area of a parallelogram
 
-    >> area_parallelogram(10,20)
+    >>> area_parallelogram(10,20)
     200
     """
     return base * height
@@ -75,8 +75,8 @@ def area_trapezium(base1, base2, height):
     """
     Calculate the area of a trapezium
 
-    >> area_trapezium(10,20,30)
-    450
+    >>> area_trapezium(10,20,30)
+    450.0
     """
     return 1 / 2 * (base1 + base2) * height
 
@@ -85,24 +85,29 @@ def area_circle(radius):
     """
     Calculate the area of a circle
 
-    >> area_circle(20)
+    >>> area_circle(20)
     1256.6370614359173
     """
-    return pi * radius * radius
+    return pi * pow(radius, 2)
 
 
 def main():
     print("Areas of various geometric shapes: \n")
-    print(f"Rectangle: {area_rectangle(10, 20)=}")
-    print(f"Square: {area_square(10)=}")
-    print(f"Triangle: {area_triangle(10, 10)=}")
-    print(f"Parallelogram: {area_parallelogram(10, 20)=}")
-    print(f"Trapezium: {area_trapezium(10, 20, 30)=}")
-    print(f"Circle: {area_circle(20)=}")
-    print("Surface Areas of various geometric shapes: \n")
-    print(f"Cube: {surface_area_cube(20)=}")
-    print(f"Sphere: {surface_area_sphere(20)=}")
+    print(f"Rectangle: {area_rectangle(10, 20)}")
+    print(f"Square: {area_square(10)}")
+    print(f"Triangle: {area_triangle(10, 10)}")
+    print(f"Parallelogram: {area_parallelogram(10, 20)}")
+    print(f"Trapezium: {area_trapezium(10, 20, 30)}")
+    print(f"Circle: {area_circle(20)}")
+    print("\nSurface Areas of various geometric shapes: \n")
+    print(f"Cube: {surface_area_cube(20)}")
+    print(f"Sphere: {surface_area_sphere(20)}")
 
 
 if __name__ == "__main__":
+
+    import doctest
+
+    doctest.testmod(verbose=True)  # verbose so we can see methods missing tests
+
     main()
