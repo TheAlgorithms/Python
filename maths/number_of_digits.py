213e764a286a819364be8561d8d8217d74dba266
Vasu Gamdha <40864108+vasugamdha@users.noreply.github.com>
2020-07-21 14:43:57 +0530

    Updated number_of_digits.py
    Added two more methods!

--- maths/number_of_digits.py
+++ maths/number_of_digits.py
@@ -1,6 +1,7 @@
 def num_digits(n: int) -> int:
     """
     Find the number of digits in a number.
+    Time Complexity: O(n)
 
     >>> num_digits(12345)
     5
@@ -13,6 +14,31 @@ def num_digits(n: int) -> int:
         digits += 1
     return digits
 
+def num_digits2(n: int) -> int:
+    """
+    Find the number of digits in a number.
+    Time Complexity: O(n)
+    
+    >>> num_digits2(12345)
+    5
+    >>> num_digits2(123)
+    3
+    """
+    return (len(str(abs(n))))
+
+def num_digits_fast(n: int) -> int: 
+    """
+    Find the number of digits in a number.
+    Time Complexity: O(1)
+    
+    >>> num_digits_fast(12345)
+    5
+    >>> num_digits_fast(123)
+    3
+    """
+    return (math.floor(math.log(abs(n), 10)+1)) #abs as logarithm is not defined for negative numbers.
 
 if __name__ == "__main__":
-    print(num_digits(12345))  # ===> 5
+    print(num_digits(12345))  # ===> 5      #O(n)
+    print(num_digits2(12345))  # ===> 5     #O(n)
+    print(num_digits_fast(12345))  # ===> 5 #O(1)
