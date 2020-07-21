af6c05fe2e509ec2d1b4af923d2d1f9cab1c25fd
Christian Clauss <cclauss@me.com>
2020-07-21 13:38:04 +0200

    Add Python type hints

--- web_programming/fetch_jobs.py
+++ web_programming/fetch_jobs.py
@@ -1,33 +1,23 @@
-'''
+"""
 Scraping jobs given job title and location from indeed website
-'''
+"""
+from typing import Generator, Tuple
 
 import requests
 from bs4 import BeautifulSoup
 
- 
-url = "https://www.indeed.co.in/jobs?q=mobile+app+development&l=mumbai"
+url = "https://www.indeed.co.in/jobs?q=mobile+app+development&l="
 
 
-def fetch_jobs():
-    #request the url
-    res = requests.get(url)
-    soup = BeautifulSoup(res.content, 'lxml')
-
+def fetch_jobs(location: str = "mumbai") -> Generator[Tuple[str, str], None, None]:
+    soup = BeautifulSoup(requests.get(url + location).content, "html.parser")
     # This attribute finds out all the specifics listed in a job
-    results = soup.find_all('div', attrs={'data-tn-component': 'organicJob'})
+    for job in soup.find_all("div", attrs={"data-tn-component": "organicJob"}):
+        job_title = job.find("a", attrs={"data-tn-element": "jobTitle"}).text.strip()
+        company_name = job.find("span", {"class": "company"}).text.strip()
+        yield job_title, company_name
 
-    for x in results:
-        #Job Title
-        job = x.find('a', attrs={'data-tn-element': "jobTitle"})
-        #company name
-        company = x.find('span', {"class" : "company"})
-        print(f"Job is {job.text.strip()} and Company is {company.text.strip()}")
-        
-        
-        
-    
+
 if __name__ == "__main__":
-  fetch_jobs()
-    
-    
+    for i, job in enumerate(fetch_jobs("Bangalore"), 1):
+        print(f"Job {i:>2} is {job[0]} at {job[1]}")
