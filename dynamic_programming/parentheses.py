a1730455dcd15d2f29734be046312d3f037c33f1
Aayush Srivastava <aayu.2312@gmail.com>
2020-07-23 03:22:56 +0530

    Update parentheses.py
    Update parentheses.py and now using built-in addition, subtraction, and multiplication operators and also renamed variables to reflect their purpose.

--- dynamic_programming/parentheses.py
+++ dynamic_programming/parentheses.py
@@ -1,26 +1,25 @@
 import math
+from operator import add, sub, mul
 
 
-def calculate(a, b, op) -> int:
+operators = {"+": add, "-": sub, "*": mul}
+
+
+def calculate(first_operand, second_operand, operator) -> int:
     """
     Return the value after evaluating the expression
     """
-    if op == "+":
-        return a + b
-    elif op == "-":
-        return a - b
-    else:
-        return a * b
+    return operators[operator](first_operand, second_operand)
 
 
-def MinandMax(M, m, i, j, operator) -> int:
+def MinandMax(maximum_numbers, minimum_numbers, i, j, operator) -> int:
     min_value = math.inf
     max_value = -math.inf
     for k in range(i, j):
-        a = calculate(M[i][k], M[k + 1][j], operator[k])
-        b = calculate(M[i][k], m[k + 1][j], operator[k])
-        c = calculate(m[i][k], M[k + 1][j], operator[k])
-        d = calculate(m[i][k], m[k + 1][j], operator[k])
+        a = calculate(maximum_numbers[i][k], maximum_numbers[k + 1][j], operator[k])
+        b = calculate(maximum_numbers[i][k], minimum_numbers[k + 1][j], operator[k])
+        c = calculate(minimum_numbers[i][k], maximum_numbers[k + 1][j], operator[k])
+        d = calculate(minimum_numbers[i][k], minimum_numbers[k + 1][j], operator[k])
         min_value = min(min_value, a, b, c, d)
         max_value = max(max_value, a, b, c, d)
     return min_value, max_value
@@ -29,28 +28,30 @@ def MinandMax(M, m, i, j, operator) -> int:
 def parentheses(operand, operator) -> int:
     """
     Return the maximum value of the expression
-    >>> 1+5
-    6
-    >>> 5-8+7*4-8+9
-    200
     """
-    n = len(operand)
-    m = [[None for x in range(n)] for x in range(n)]
-    M = [[None for x in range(n)] for x in range(n)]
-    for i in range(n):
-        m[i][i] = operand[i]
-        M[i][i] = operand[i]
-    for s in range(1, n):
-        for i in range(0, n - s):
+    number_of_operands = len(operand)
+    minimum_numbers = [
+        [None for x in range(number_of_operands)] for x in range(number_of_operands)
+    ]
+    maximum_numbers = [
+        [None for x in range(number_of_operands)] for x in range(number_of_operands)
+    ]
+    for i in range(number_of_operands):
+        minimum_numbers[i][i] = operand[i]
+        maximum_numbers[i][i] = operand[i]
+    for s in range(1, number_of_operands):
+        for i in range(0, number_of_operands - s):
             j = i + s
-            m[i][j], M[i][j] = MinandMax(M, m, i, j, operator)
-    return M[0][n - 1]
+            minimum_numbers[i][j], maximum_numbers[i][j] = MinandMax(
+                maximum_numbers, minimum_numbers, i, j, operator
+            )
+    return maximum_numbers[0][number_of_operands - 1]
 
 
 expression = input().strip()
 operator, operand = [], []
 for i in expression:
-    if i in ("+", "-", "*"):
+    if i in operators.keys():
         operator.append(i)
     else:
         operand.append(int(i))
