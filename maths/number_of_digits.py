154165429f9c4130d0bb5e5b43fd04a681a9680a
Vasu Gamdha <40864108+vasugamdha@users.noreply.github.com>
2020-07-22 11:22:27 +0530

    Update number_of_digits.py

--- maths/number_of_digits.py
+++ maths/number_of_digits.py
@@ -1,3 +1,5 @@
+import math
+
 def num_digits(n: int) -> int:
     """
     Find the number of digits in a number.
@@ -14,11 +16,13 @@ def num_digits(n: int) -> int:
         digits += 1
     return digits
 
+
 def num_digits2(n: int) -> int:
     """
     Find the number of digits in a number.
     Time Complexity: O(n)
-    
+    abs() is used for negative numbers
+
     >>> num_digits2(12345)
     5
     >>> num_digits2(123)
@@ -26,19 +30,22 @@ def num_digits2(n: int) -> int:
     """
     return (len(str(abs(n))))
 
-def num_digits_fast(n: int) -> int: 
+
+def num_digits_fast(n: int) -> int:
     """
     Find the number of digits in a number.
     Time Complexity: O(1)
-    
+    abs() is used as logarithm for negative numbers is not defined.
+
     >>> num_digits_fast(12345)
     5
     >>> num_digits_fast(123)
     3
     """
-    return (math.floor(math.log(abs(n), 10)+1)) #abs as logarithm is not defined for negative numbers.
+    return (math.floor(math.log(abs(n), 10) + 1))
+
 
 if __name__ == "__main__":
-    print(num_digits(12345))  # ===> 5      #O(n)
-    print(num_digits2(12345))  # ===> 5     #O(n)
-    print(num_digits_fast(12345))  # ===> 5 #O(1)
+    print(num_digits(12345))        # ===> 5 in O(n)
+    print(num_digits2(12345))       # ===> 5 in O(n)
+    print(num_digits_fast(12345))   # ===> 5 in O(1)
