1ad367032108fe0cbdfc276f3d414e5c683b4242
tasbihaasim <ta05227@st.habib.edu.pk>
2020-07-23 14:19:21 +0500

    Add String_mirroring to strings

---
+++ strings/mirroring_using_enque_deque.py
@@ -0,0 +1,32 @@
+
+def enQueue(lst: list, data: str):
+    # Adds element to the back of the queue
+    lst.append(data)
+
+
+def deQueue(lst: list):
+    # Returns element at front of the queue
+    return lst.pop(0)
+
+
+def mirror(qString: str):
+    '''
+    Returns a mirrored form of the string using enque and deque methods
+    >>> mirror("python")
+    'pythonnohtyp'
+    >>> mirror("github")
+    'githubbuhtig'
+    '''
+    qString = qString.strip()
+    qString = list(qString)
+    Reverse = qString[::-1]
+    for i in range(len(Reverse)):
+        enQueue(qString, deQueue(Reverse))
+    str1 = ''
+    qString = str1.join(qString)
+    return (qString)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
