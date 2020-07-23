667ab6cc339c957b73180355916ffa3d0613c3bf
Christian Clauss <cclauss@me.com>
2020-07-23 13:09:00 +0200

    swaped --> swapped

--- matrix/inverse_of_matrix.py
+++ matrix/inverse_of_matrix.py
@@ -31,7 +31,7 @@ def inverse_of_matrix(matrix: List[List[float]]) -> List[List[float]]:
     determinant = D(matrix[0][0]) * D(matrix[1][1]) - D(matrix[1][0]) * D(matrix[0][1])
     if determinant == 0:
         raise ValueError("This matrix has no inverse.")
-    # Creates a copy of the matrix with swaped positions of the elements
+    # Creates a copy of the matrix with swapped positions of the elements
     swapped_matrix = [[0.0, 0.0], [0.0, 0.0]]
     swapped_matrix[0][0], swapped_matrix[1][1] = matrix[1][1], matrix[0][0]
     swapped_matrix[1][0], swapped_matrix[0][1] = -matrix[1][0], -matrix[0][1]
