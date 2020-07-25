9d16dc22752c87de2719c34bebd7ef7f4837aefc
Utsav1999 <utsavakash123@gmail.com>
2020-07-26 02:17:36 +0530

    Added Exponent Finding program

---
+++ maths/exponent_recursion.py
@@ -0,0 +1,27 @@
+'''
+        =============================== Finding Exponent using Recursion =======================
+
+        i/p -->
+        Enter the base: 3
+        Enter the exponent: 4
+        o/p -->
+        3 to the power of 4: 81
+
+
+        i/p -->
+        Enter the base: 2
+        Enter the exponent: 0
+        o/p -->
+        2 to the power of 0: 1
+'''
+def powerCalculation(n: int, p: int)->int:
+    if(p == 0):
+        return 1
+    else:
+        return (n * powerCalculation(n, (p-1)))
+
+if __name__ == "__main__":
+    n = int(input("Enter the base: "))
+    p = int(input("Enter the exponent: "))
+    result = powerCalculation(n, p)
+    print("{} to the power of {}: {}".format(n, p, result))
