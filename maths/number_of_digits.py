0d9ebb7328fbb3ab49cd71d9ab8b2a2d618a5b28
Vasu Gamdha <40864108+vasugamdha@users.noreply.github.com>
2020-07-26 10:57:50 +0530

    Added benchmarks!

--- maths/number_of_digits.py
+++ maths/number_of_digits.py
@@ -1,10 +1,10 @@
 import math
+from timeit import timeit
 
 
 def num_digits(n: int) -> int:
     """
     Find the number of digits in a number.
-    Time Complexity: O(n)
 
     >>> num_digits(12345)
     5
@@ -18,24 +18,9 @@ def num_digits(n: int) -> int:
     return digits
 
 
-def num_digits2(n: int) -> int:
-    """
-    Find the number of digits in a number.
-    Time Complexity: O(n)
-    abs() is used for negative numbers
-
-    >>> num_digits2(12345)
-    5
-    >>> num_digits2(123)
-    3
-    """
-    return (len(str(abs(n))))
-
-
 def num_digits_fast(n: int) -> int:
     """
     Find the number of digits in a number.
-    Time Complexity: O(1)
     abs() is used as logarithm for negative numbers is not defined.
 
     >>> num_digits_fast(12345)
@@ -46,7 +31,36 @@ def num_digits_fast(n: int) -> int:
     return (math.floor(math.log(abs(n), 10) + 1))
 
 
+def num_digits_faster(n: int) -> int:
+    """
+    Find the number of digits in a number.
+    abs() is used for negative numbers
+
+    >>> num_digits2(12345)
+    5
+    >>> num_digits2(123)
+    3
+    """
+    return (len(str(abs(n))))
+
+
 if __name__ == "__main__":
-    print(num_digits(12345))        # ===> 5 in O(n)
-    print(num_digits2(12345))       # ===> 5 in O(n)
-    print(num_digits_fast(12345))   # ===> 5 in O(1)
+    small_num = 262144
+    medium_num = 1125899906842624
+    large_num = 1267650600228229401496703205376
+
+    # Benchmarks    
+    print('\nFor small_num = ', small_num, ':')
+    print("> num_digits()", '\t\tans =', num_digits(small_num), '\ttime =', timeit("z.num_digits(z.small_num)", setup = "import __main__ as z"), "seconds")
+    print("> num_digits_fast()", '\tans =', num_digits_fast(small_num), '\ttime =', timeit("z.num_digits_fast(z.small_num)", setup = "import __main__ as z"), "seconds")
+    print("> num_digits_faster()", '\tans =', num_digits_faster(small_num), '\ttime =', timeit("z.num_digits_faster(z.small_num)", setup = "import __main__ as z"), "seconds")
+
+    print('\nFor medium_num = ', medium_num, ':')
+    print("> num_digits()", '\t\tans =', num_digits(medium_num), '\ttime =', timeit("z.num_digits(z.medium_num)", setup = "import __main__ as z"), "seconds")
+    print("> num_digits_fast()", '\tans =', num_digits_fast(medium_num), '\ttime =', timeit("z.num_digits_fast(z.medium_num)", setup = "import __main__ as z"), "seconds")
+    print("> num_digits_faster()", '\tans =', num_digits_faster(medium_num), '\ttime =', timeit("z.num_digits_faster(z.medium_num)", setup = "import __main__ as z"), "seconds")
+
+    print('\nFor large_num = ', large_num, ':')
+    print("> num_digits()", '\t\tans =', num_digits(large_num), '\ttime =', timeit("z.num_digits(z.large_num)", setup = "import __main__ as z"), "seconds")
+    print("> num_digits_fast()", '\tans =', num_digits_fast(large_num), '\ttime =', timeit("z.num_digits_fast(z.large_num)", setup = "import __main__ as z"), "seconds")
+    print("> num_digits_faster()", '\tans =', num_digits_faster(large_num), '\ttime =', timeit("z.num_digits_faster(z.large_num)", setup = "import __main__ as z"), "seconds")
