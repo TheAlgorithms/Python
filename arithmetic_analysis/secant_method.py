f229a00016f1ffa536d48cdbaab4833accc935b6
spamegg <spamegg@pm.me>
2020-08-02 12:22:06 +0300

    Refactored arithmetic_analysis/secant_method.py
    - added missing Wikipedia link
    - added type hints and few more doctests
    - moved the function `f` to inside `secant_method` as a parameter `func`, instead of calling a globally defined function inside the algorithm body
    - renamed some variables and introduced some intermediate variables, to make it more readable and avoid recalculating `f(x1)`
    - replaced unused for-loop variable `i` with `_`

--- arithmetic_analysis/secant_method.py
+++ arithmetic_analysis/secant_method.py
@@ -1,11 +1,43 @@
 # Implementing Secant method in Python
+# https://en.wikipedia.org/wiki/Secant_method
 # Author: dimgrichr
 
 
 from math import exp
+from typing import Callable
+
+RealFunc = Callable[[float], float]  # type alias for real -> real function
 
 
-def f(x):
+def secant_method(
+    func: RealFunc, lower_bound: float, upper_bound: float, repeats: int
+) -> float:
+    """
+    >>> secant_method(lambda x: 8 * x - 2 * exp(-x), 1, 3, 2)
+    0.2139409276214589
+    >>> secant_method(lambda x: x ** 3 - 2 * x - 5, -3, 3, 10)
+    2.094546116969569
+    >>> secant_method(lambda x: x ** 3 - 1, 0, 5, 5)
+    0.07992494940223759
+    >>> secant_method(lambda x: x ** 3 - 1, 0, 5, 500)
+    Traceback (most recent call last):
+    ...
+    ZeroDivisionError: Could not find root
+    """
+    lower: float = lower_bound
+    upper: float = upper_bound
+    for _ in range(repeats):
+        value: float = func(upper)
+        numerator: float = value * (upper - lower)
+        denominator: float = value - func(lower)
+        try:
+            lower, upper = upper, upper - numerator / denominator
+        except ZeroDivisionError:
+            raise ZeroDivisionError("Could not find root")
+    return upper
+
+
+def f(x: float) -> float:
     """
     >>> f(5)
     39.98652410600183
@@ -13,16 +45,4 @@ def f(x):
     return 8 * x - 2 * exp(-x)
 
 
-def SecantMethod(lower_bound, upper_bound, repeats):
-    """
-    >>> SecantMethod(1, 3, 2)
-    0.2139409276214589
-    """
-    x0 = lower_bound
-    x1 = upper_bound
-    for i in range(0, repeats):
-        x0, x1 = x1, x1 - (f(x1) * (x1 - x0)) / (f(x1) - f(x0))
-    return x1
-
-
-print(f"The solution is: {SecantMethod(1, 3, 2)}")
+print(f"The solution is: {secant_method(f, 1, 3, 2)}")
