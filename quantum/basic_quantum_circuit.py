c393f96a4e12445d0f1960c2200c4615fe003690
sidhant-sriv <sidhant.sriv@gmail.com>
2020-08-02 23:13:19 +0530

    made slight corrections to existing code

--- quantum/basic_quantum_circuit.py
+++ quantum/basic_quantum_circuit.py
@@ -1,25 +1,26 @@
 #This is basic code demonstrating the qiskit library.
 #A Quantum circuit with one Hadamard gate (H gate)
 
-#importing dependancies
+#importing dependencies
 import numpy as np
 from qiskit import (QuantumCircuit, execute , Aer)
 from qiskit.visualization import plot_histogram
 
-#For the simulator
+def q_circuit():
+    #For the simulator
 simulator = Aer.get_backend('qasm_simulator')
 
 #Creating the circuit
 circuit = QuantumCircuit(2,2) # Two qubits
 
-#Adding H-gate on Qubit 0
-circuit.h(0)
+#Adding H-gate on Qubit 1
+circuit.h(1)
 
 # Adding a CX (CNOT) gate on control qubit 0 and target qubit 1
-circuit.cx(0, 1)
+circuit.cx(1,0)
 
 # Mapping the quantum measurement to the classical bits
-circuit.measure([0,1], [0,1])
+circuit.measure([1,0], [0,1])
 
 # Execute the circuit on the qasm simulator
 job = execute(circuit, simulator, shots=1000) #run the simulation 1,000 times
@@ -29,5 +30,6 @@ result = job.result()
 
 #Returning counts
 counts = result.get_counts(circuit)
-print("\nTotal count for 00 and 11 are",counts['00'],' and ',counts['11'])
+return [counts['00'],counts['11']] #returns a count of 00 and 11 respectively
 
+# https://en.wikipedia.org/wiki/Quantum_logic_gate
