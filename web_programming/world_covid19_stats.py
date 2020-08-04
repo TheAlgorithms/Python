6b2d90e940e28c7b610e7a7c228b1ea5e80cd379
Lownish Sookha <lownish.19ms0139@mc.iitism.ac.in>
2020-08-04 14:57:22 +0530

    Performed flake test on Priority Scheduling

--- web_programming/world_covid19_stats.py
+++ web_programming/world_covid19_stats.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-'''
+"""
 Provide the current worldwide COVID-19 statistics.
 This data is being scrapped from 'https://www.worldometers.info/coronavirus/'.
-'''
+"""
 
 import requests
 from bs4 import BeautifulSoup
@@ -13,8 +13,8 @@ def world_covid19_stats(url: str = "https://www.worldometers.info/coronavirus") 
     """
     Return a dict of current worldwide COVID-19 statistics
     """
-    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
-    keys = soup.findAll('h1')
+    soup = BeautifulSoup(requests.get(url).text, "html.parser")
+    keys = soup.findAll("h1")
     values = soup.findAll("div", {"class": "maincounter-number"})
     keys += soup.findAll("span", {"class": "panel-title"})
     values += soup.findAll("div", {"class": "number-table-main"})
