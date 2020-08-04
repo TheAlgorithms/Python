415b4c6db8a3abd589dd5356bc34e636fd8ad722
Lownish Sookha <lownish.19ms0139@mc.iitism.ac.in>
2020-08-05 05:00:49 +0530

    Review changes updated

--- scheduling/priority_scheduling.py
+++ scheduling/priority_scheduling.py
@@ -23,100 +23,101 @@ class process:
         self.waiting_time = None
         self.turn_around_time = None
 
-
-n = int(input("Enter number of processes: ").strip())
-processes = []
-
-
-for i in range(n):
-    process_name = input("Enter process name: ").strip()
-    arrival_time = int(input("Enter arrival time: ").strip())
-    burst_time = int(input("Enter burst time: ").strip())
-    priority = int(input("Enter Priority: ").strip())
-
-    processes.append(process(process_name, arrival_time, burst_time, priority))
-
-"""
-Executed processes from the original array will be deleted
-and will be appended to the array below
-"""
-executed_processes = []
+if __name__ == "__main__":
+        
+    n = int(input("Enter number of processes: ").strip())
+    processes = []
 
 
-"""
-Linear Runtime of the Scheduling
-"""
-current_time = 0
+    for i in range(n):
+        process_name = input("Enter process name: ").strip()
+        arrival_time = int(input("Enter arrival time: ").strip())
+        burst_time = int(input("Enter burst time: ").strip())
+        priority = int(input("Enter Priority: ").strip())
 
-"""
-As long as there exists atleast one element in the original array,
-Priority Scheduling will keep running in loop
-"""
-while len(processes) > 0:
+        processes.append(process(process_name, arrival_time, burst_time, priority))
 
     """
-    To deal with more than one process with same priority level,
-    the following array is created
+    Executed processes from the original array will be deleted
+    and will be appended to the array below
     """
-    processes_with_highest_priority = []
+    executed_processes = []
+
 
     """
-    Retrieves current highest priority in the original array
+    Linear Runtime of the Scheduling
     """
-    highest_priority = max(processes, key=attrgetter("priority")).priority
+    current_time = 0
 
     """
-    To counter issues where the processes with
-    the highest priority has not entered the queue yet
+    As long as there exists at least one element in the original array,
+    Priority Scheduling will keep running in loop
     """
-    while len(processes_with_highest_priority) == 0:
-
-        for process in processes:
-            if (
-                process.priority == highest_priority
-                and process.arrival_time <= current_time
-            ):
-                processes_with_highest_priority.append(process)
+    while len(processes) > 0:
 
         """
-        In case no process of the highest priority was found waiting in the queue,
-        it will move to the priority one step lower
+        To deal with more than one process with same priority level,
+        the following array is created
         """
-        if len(processes_with_highest_priority) == 0:
-            highest_priority -= 1
+        processes_with_highest_priority = []
+
+        """
+        Retrieves current highest priority in the original array
+        """
+        highest_priority = max(processes, key=attrgetter("priority")).priority
+
+        """
+        To counter issues where the processes with
+        the highest priority has not entered the queue yet
+        """
+        while len(processes_with_highest_priority) == 0:
+
+            for process in processes:
+                if (
+                    process.priority == highest_priority
+                    and process.arrival_time <= current_time
+                ):
+                    processes_with_highest_priority.append(process)
+
+            """
+            In case no process of the highest priority was found waiting in the queue,
+            it will move to the priority one step lower
+            """
+            if len(processes_with_highest_priority) == 0:
+                highest_priority -= 1
+
+        """
+        In case of more than one process with the same priority level,
+        appyling FCFS algorithm
+        """
+        process_to_execute = min(
+            processes_with_highest_priority, key=attrgetter("arrival_time")
+        )
+        k = processes.index(process_to_execute)
+
+        """
+        Calculating arrival time and turn around time of the executed process
+        and removing it from the original array
+        """
+        process_to_execute.waiting_time = current_time - process_to_execute.arrival_time
+        process_to_execute.turn_around_time = (
+            process_to_execute.waiting_time + process_to_execute.burst_time
+        )
+        executed_processes.append(process_to_execute)
+
+        current_time += processes[k].burst_time
+        del processes[k]
+
 
     """
-    In case of more than one process with the same priority level,
-    appyling FCFS algorithm
+    Printing Process names in executed order, along with waiting time and turn around time
     """
-    process_to_execute = min(
-        processes_with_highest_priority, key=attrgetter("arrival_time")
-    )
-    k = processes.index(process_to_execute)
-
-    """
-    Calculating arrival time and turn around time of the executed process
-    and removing it from the original array
-    """
-    process_to_execute.waiting_time = current_time - process_to_execute.arrival_time
-    process_to_execute.turn_around_time = (
-        process_to_execute.waiting_time + process_to_execute.burst_time
-    )
-    executed_processes.append(process_to_execute)
-
-    current_time += processes[k].burst_time
-    del processes[k]
-
-
-"""
-Printing Process names in executed order, along with waiting time and turn around time
-"""
-print("Process Name\tWaiting Time\t Turn Around Time")
-for process in executed_processes:
-    print(
-        str(process.process_name)
-        + "\t\t"
-        + str(process.waiting_time)
-        + "\t\t"
-        + str(process.turn_around_time)
-    )
+    print("Process Name\tWaiting Time\t Turn Around Time")
+    for process in executed_processes:
+        print(
+            process.process_name
+            + "\t\t"
+            + str(process.waiting_time)
+            + "\t\t"
+            + str(process.turn_around_time)
+        )
