4c38019b7251d07db1b8b22913f373e59e207e58
Christian Clauss <cclauss@me.com>
2020-08-13 16:51:52 +0200

    Use int(n) instead of floor(n)

--- project_euler/problem_34/sol1.py
+++ project_euler/problem_34/sol1.py
@@ -23,11 +23,9 @@ def factorial(n: int) -> int:
     ValueError: n must be exact integer
     """
 
-    import math
-
     if not n >= 0:
         raise ValueError("n must be >= 0")
-    if math.floor(n) != n:
+    if int(n) != n:
         raise ValueError("n must be exact integer")
     if n + 1 == n:  # catch a value like 1e300
         raise OverflowError("n too large")
