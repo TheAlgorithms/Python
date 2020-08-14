f4b2956d95d0be09b5cc6913a91a9aeef637dc7f
Guillaume <guillaume.rochedix@gmail.com>
2020-08-14 10:49:39 +0200

    type hint improvement: explicitely described in func args

--- sorts/quick_sort_3_partition.py
+++ sorts/quick_sort_3_partition.py
@@ -7,38 +7,40 @@ This algorithm splits at each iteration a mutable ordered collection in 3 parts
 3 : elements greater than the pivot
 
 For doctests run following command:
-python -m doctest -v quick_sort_3partition.py
-or
-python3 -m doctest -v quick_sort_3partition.py
+python3 -m doctest -v quick_sort_3_partition.py
 
 For manual testing run:
-python quick_sort_3partition.py
+python3 quick_sort_3_partition.py
 """
 
+from typing import Optional, List
 
-def quick_sort_3partition(my_list: list, left: int = 0, right: int = None) -> list:
+
+def quick_sort_3_partition(
+    my_list: List[int], left: int = 0, right: Optional[int] = None
+) -> List[int]:
     """
     Implementation of quick sort algorithm, 3-Way variation.
 
     Examples:
-    >>> quick_sort_3partition([0, 5, 3, 2, 2])
+    >>> quick_sort_3_partition([0, 5, 3, 2, 2])
     [0, 2, 2, 3, 5]
 
-    >>> quick_sort_3partition([])
+    >>> quick_sort_3_partition([])
     []
 
-    >>> quick_sort_3partition([-2, -5, -45])
+    >>> quick_sort_3_partition([-2, -5, -45])
     [-45, -5, -2]
     """
     if right is None:
         right = len(my_list) - 1
     if right <= left:
         return my_list
-    i: int = left
-    temp_left: int = left
-    temp_right: int = right
+    i = left
+    temp_left = left
+    temp_right = right
     # Use the first element as pivot
-    pivot: int = my_list[left]
+    pivot = my_list[left]
     while i <= temp_right:
         if my_list[i] < pivot:
             # Permute current element with leftmost element of the current sublist
@@ -52,12 +54,12 @@ def quick_sort_3partition(my_list: list, left: int = 0, right: int = None) -> li
         else:
             # Current element is equal to the pivot
             i += 1
-    quick_sort_3partition(my_list, left, temp_left - 1)
-    quick_sort_3partition(my_list, temp_right + 1, right)
+    quick_sort_3_partition(my_list, left, temp_left - 1)
+    quick_sort_3_partition(my_list, temp_right + 1, right)
     return my_list
 
 
 if __name__ == "__main__":
     user_input = input("Enter numbers separated by a comma:\n").strip()
     unsorted = [int(item) for item in user_input.split(",")]
-    print(quick_sort_3partition(unsorted))
+    print(quick_sort_3_partition(unsorted))
