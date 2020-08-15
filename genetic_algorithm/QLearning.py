602fb42c5409ab870a095bce3ac2cfdfa854ab77
Aryan <aryanraj31599@gmail.com>
2020-08-15 22:19:10 +0530

    New Commit

--- genetic_algorithm/QLearning.py
+++ genetic_algorithm/QLearning.py
@@ -7,24 +7,37 @@ This code was written to automate the classic Atari game snake using the algorit
 """
 
 with open("Qtable.txt", "rb") as f:
+    """
+	This function is responsible for loading Qtable.txt if already present
+    """
     Q_table = pickle.load(f)
     
 def Action(state, Q_table):
+    """
+	This function returns the most suitable action value from the qtable.
+    """
     action = np.argmax(Q_table[state])
     if(action == None):
         action = randomAction()
     return action
 
 def randomAction():
+    """
+	This function returns random action just in case the Qtable values return None.
+    """
     action = np.random.choice([0, 1, 2, 3])
     return action
     
 def StateActionReward(player, x_food, y_food):
+    """
+	This function returns state, action and reward to update the Qtable.
+    """
     x_agent, y_agent = player.body[0].pos
     state = State(player, x_food, y_food)
     action = Action(state, Q_table)
     reward = Reward(player, x_food, y_food)
     return state, action, reward
+
 # States to consider:
 #   * Food's relative positioning wrt the head
 # Checking for food in nine directions
@@ -34,6 +47,14 @@ def StateActionReward(player, x_food, y_food):
 #   * obstruction Ahead, Right, Left
 
 def State(player, x_food, y_food):
+    """
+	This function returns the state of the agent according to the food's relative positioning wrt to the head
+	Checking for food in nine directions
+	    ###
+	    # #
+	    ###
+        Obstructions to consider: Ahead, Right, Left  (As these are the movements allowed for the snake)
+    """
     x_agent, y_agent = player.body[0].pos
     states = []
 
@@ -81,6 +102,9 @@ def State(player, x_food, y_food):
     return state
     
 def dist(x1, y1, x2, y2):
+    """
+       For calculation of Euler Distance.
+    """
     dist = (x1 - x2)**2 + (y1 - y2)**2
     return dist**0.5
 
@@ -91,6 +115,9 @@ def dist(x1, y1, x2, y2):
 #   * -7 for going away from the fruit
           
 def Reward(player, x_food, y_food):
+    """
+	This function assigns the reward to the agent according to the action taken
+    """
     x_agent, y_agent = player.body[0].pos
     if (x_agent == x_food and y_agent == y_food):
         return +10
@@ -102,6 +129,9 @@ def Reward(player, x_food, y_food):
         return -7
 
 def Learn(state, action, reward, next_state, next_action, i,trialNumber, epsilon):
+    """
+	This function is for iteratively updating the Qtable.
+    """
     gamma = 0.9
     learning_rate = 0.7
     Q_tablePrev = Q_table
