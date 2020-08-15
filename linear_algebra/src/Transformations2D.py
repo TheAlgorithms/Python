dea2a398f2cadd057314678b8cabde0fc1c74270
Yukti Khosla <44090430+Yukti-09@users.noreply.github.com>
2020-08-15 17:43:51 +0530

    Update Transformations2D.py

--- linear_algebra/src/Transformations2D.py
+++ linear_algebra/src/Transformations2D.py
@@ -1,41 +1,41 @@
-#2D matrices used for linear transformation in linear algebra 
+#2D Transformations are regularly used in Linear Algebra.
+
+#I have added the codes for reflection, projection, scaling and rotation matrices.
 
 import numpy as np
 import math 
 
-class Transformations:
+def scaling(scaling_factor):
+	return(scaling_factor*(np.identity(2))) #This returns a scaling matrix
 
-	def scaling(self,scaling_factor):
-		return(scaling_factor*(np.identity(2))) #This returns a scaling matrix
+def rotation(angle):
+	arr = np.empty([2, 2])
 
-	def rotation(self,angle):
-		arr = np.empty([2, 2])
+	arr[0][0] = math.cos(angle)
+	arr[0][1] = -math.sin(angle)
+	arr[1][0] = math.sin(angle)
+	arr[1][1] = math.cos(angle)
 
-		arr[0][0] = math.cos(angle)
-		arr[0][1] = -math.sin(angle)
-		arr[1][0] = math.sin(angle)
-		arr[1][1] = math.cos(angle)
-
-		return arr #This returns a rotation matrix
-
-	
-	def projection(self,angle):
-		arr = np.empty([2, 2])
-
-		arr[0][0] = math.cos(angle)*math.cos(angle)
-		arr[0][1] = math.sin(angle)*math.cos(angle)
-		arr[1][0] = math.sin(angle)*math.cos(angle)
-		arr[1][1] = math.sin(angle)*math.sin(angle)
-
-		return arr #This returns a rotation matrix
+	return arr #This returns a rotation matrix
 
 
-	def reflection(self,angle):
-		arr = np.empty([2, 2])
+def projection(angle):
+	arr = np.empty([2, 2])
 
-		arr[0][0] = (2*math.cos(angle)) -1
-		arr[0][1] = 2*math.sin(angle)*math.cos(angle)
-		arr[1][0] = 2*math.sin(angle)*math.cos(angle)
-		arr[1][1] = (2*math.sin(angle)) -1
+	arr[0][0] = math.cos(angle)*math.cos(angle)
+	arr[0][1] = math.sin(angle)*math.cos(angle)
+	arr[1][0] = math.sin(angle)*math.cos(angle)
+	arr[1][1] = math.sin(angle)*math.sin(angle)
 
-		return arr #This returns a reflection matrix
+	return arr #This returns a rotation matrix
+
+
+def reflection(angle):
+	arr = np.empty([2, 2])
+
+	arr[0][0] = (2*math.cos(angle)) -1
+	arr[0][1] = 2*math.sin(angle)*math.cos(angle)
+	arr[1][0] = 2*math.sin(angle)*math.cos(angle)
+	arr[1][1] = (2*math.sin(angle)) -1
+
+	return arr #This returns a reflection matrix
