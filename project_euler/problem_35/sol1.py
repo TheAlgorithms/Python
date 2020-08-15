2ff80b57cd411390c2438d71ecd8f998944ce87c
Kushagra Bansal <kushagrabansalajmer@gmail.com>
2020-08-15 22:20:10 +0530

    Update project_euler/problem_35/sol1.py
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- project_euler/problem_35/sol1.py
+++ project_euler/problem_35/sol1.py
@@ -1,6 +1,5 @@
 """
 The number 197 is called a circular prime because all rotations of the digits:
-
 197, 971, and 719, are themselves prime.
 
 There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73,
