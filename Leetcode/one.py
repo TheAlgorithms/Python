79a65046d3e00742315c35ac45a5501485075180
Yukti Khosla <44090430+Yukti-09@users.noreply.github.com>
2020-08-16 11:23:40 +0530

    Create one.py

---
+++ Leetcode/one.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        l = []
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    l.append(i)
+                    l.append(j)
+                    return l
