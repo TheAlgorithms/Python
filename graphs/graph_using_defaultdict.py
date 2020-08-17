48ceda4b06a42bdb5b93cb65c5106b19933020fe
Modem Naveen <nmodem2k@gmail.com>
2020-08-17 18:10:24 +0530

    Update and rename graph_using_defaultdict to graph_using_defaultdict.py

---
+++ graphs/graph_using_defaultdict.py
@@ -0,0 +1,43 @@
+from collections import defaultdict
+ class Graph:
+  def __init__(self):
+    self.graph=defaultdict(list)
+  def add_edge(self,node1,node2):  # undirected graph
+    self.graph[node1].append(node2)
+    self.graph[node2].append(node1)
+  def print(self):
+    print(self.graph)
+
+  def BFS(self,starting_node):
+    visited=[False]*len(self.graph)
+    queue=[]
+    queue.append(starting_node)
+    visited[starting_node]=True
+    while queue:
+      first_inserted_element_in_queue=queue.pop(0)
+      print(first_inserted_element_in_queue)
+      for i in self.graph[first_inserted_element_in_queue]:
+        if visited[i]!=True:
+          queue.append(i)
+          visited[i]=True
+        
+  def _DFS(self,visited,node):
+    visited[node]=True
+    print(node)
+    for i in self.graph[node]:
+      if visited[i]!=True:
+        self._DFS(visited,i)
+  def DFS(self,node):
+    visited=[False]*len(self.graph)
+    self._DFS(visited,node)
+
+graph=Graph()
+grap.add_edge(0,3)
+graph.add_edge(0,1)
+graph.add_edge(3,1)
+graph.add_edge(2,1)
+graph.add_edge(6,1)
+graph.add_edge(3,2)
+graph.add_edge(2,4)
+graph.add_edge(1,5)
+graph.add_edge(2,5)
