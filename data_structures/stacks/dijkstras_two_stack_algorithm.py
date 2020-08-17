b605ea75b357c7276d5be91c048a333bc74dbab2
echoaj <aljoslin13@yahoo.com>
2020-08-16 21:32:30 -0700

    Made changes to dijkstras two stack algorithm for documentation and testing purposes.

--- data_structures/stacks/dijkstras_two_stack_algorithm.py
+++ data_structures/stacks/dijkstras_two_stack_algorithm.py
@@ -5,21 +5,32 @@ GitHub: github.com/echoaj
 We can use Dijkstra's two stack algorithm to solve an equation
 such as: (5 + ((4 * 2) * (2 + 3)))
 
-These are the algorithm's rules:
-RULE 1: Scan the expression from left to right. When an operand is encountered, push it onto the the operand stack.
-RULE 2: When an operator is encountered in the expression, push it onto the operator stack.
+THESE ARE THE ALGORITHM'S RULES:
+RULE 1: Scan the expression from left to right. When an operand is encountered,
+        push it onto the the operand stack.
+
+RULE 2: When an operator is encountered in the expression,
+        push it onto the operator stack.
+
 RULE 3: When a left parenthesis is encountered in teh expression, ignore it.
-RULE 4: When a right parenthesis is encountered in the expression, pop an operator off the operator stack.
-        The two operands it must operate on must be the last two operands pushed onto the operand stack.
-        We therefore pop the operand stack twice, perform the operation, and push the result back onto the
-        operand stack so it will be available for use as an operand of the next operator popped off the
-        operator stack.
-RULE 5: When the entire infix expression has been scanned, the value left on the operand stack represents
-        the value of the expression.
+
+RULE 4: When a right parenthesis is encountered in the expression,
+        pop an operator off the operator stack.  The two operands it must
+        operate on must be the last two operands pushed onto the operand stack.
+        We therefore pop the operand stack twice, perform the operation,
+        and push the result back onto the operand stack so it will be available
+        for use as an operand of the next operator popped off the operator stack.
+
+RULE 5: When the entire infix expression has been scanned, the value left on
+        the operand stack represents the value of the expression.
+
+NOTE:   It only works with positive numbers.
 """
+__author__ = "Omkar Pathak"
 
 
 class Stack:
+    """ Stack Data Structure Used for the Algorithm"""
     def __init__(self):
         self.__array = []
         self.__len = 0
@@ -36,11 +47,11 @@ class Stack:
         self.__len += 1
 
     def pop(self):
-        if not self.is_empty():  # if empty
+        if not self.is_empty():
             self.__array.pop()
             self.__len -= 1
             self.underflow_error = False
-        else:  # if not empty
+        else:
             self.underflow_error = True
 
     def peek(self):
@@ -53,20 +64,31 @@ class Stack:
         print(self.__array)
 
 
-def main():
-    equation = "(5 + ((4 * 2) * (2 + 3)))"
+def dijkstras_two_stack_algorithm(equation):
+    """
+    DocTests
+    >>> dijkstras_two_stack_algorithm("(5 + 3)")
+    8
+    >>> dijkstras_two_stack_algorithm("((9 - (2 + 9)) + (8 - 1))")
+    5
+    >>> dijkstras_two_stack_algorithm("((((3 - 2) - (2 + 3)) + (2 - 4)) + 3)")
+    -3
+
+    :param equation: a string
+    :return: result: an integer
+    """
     operand_stack = Stack()
     operator_stack = Stack()
 
     for i in equation:
         if i.isdigit():
-            # Rule 1
+            # RULE 1
             operand_stack.push(int(i))
-        elif i in ['+', '*', '-', '/', '%']:
-            # Rule 2
+        elif i in ["+", "*", "-", "/", "%"]:
+            # RULE 2
             operator_stack.push(i)
-        elif i == ')':
-            # Rule 4
+        elif i == ")":
+            # RULE 4
             opr1 = operator_stack.peek()
             operator_stack.pop()
             num1 = operand_stack.peek()
@@ -75,25 +97,32 @@ def main():
             operand_stack.pop()
 
             total = 0
-            if opr1 == '+':
+            if opr1 == "+":
                 total = num2 + num1
-            elif opr1 == '-':
+            elif opr1 == "-":
                 total = num2 - num1
-            elif opr1 == '*':
+            elif opr1 == "*":
                 total = num2 * num1
-            elif opr1 == '/':
+            elif opr1 == "/":
                 total = num2 / num1
-            elif opr1 == '%':
+            elif opr1 == "%":
                 total = num2 % num1
 
             operand_stack.push(total)
         else:
-            # Rule 3
+            # RULE 3
             pass
 
-    # Rule 5
-    print(operand_stack.peek()) # Result -> 45
+    # RULE 5
+    result = operand_stack.peek()
+    return result
 
+
+def main():
+    equation = "(5 + ((4 * 2) * (2 + 3)))"
+    # answer = 45
+    answer = dijkstras_two_stack_algorithm(equation)
+    print(answer)
 
 
 if __name__ == "__main__":
