6948ec4e97b4cf269587f19697ded428572601a5
karan-dhir <kurrandhir@gmail.com>
2020-08-18 18:26:00 +0530

    Added static type checking to Python/linear_algebra/src/power_iteration.py

--- linear_algebra/src/power_iteration.py
+++ linear_algebra/src/power_iteration.py
@@ -1,9 +1,11 @@
-import numpy as np
+from typing import Tuple, List, Union
+
+import numpy as np  # type: ignore
 
 
 def power_iteration(
-    input_matrix: np.array, vector: np.array, error_tol=1e-12, max_iterations=100
-) -> [float, np.array]:
+    input_matrix: np.array, vector: np.array, error_tol: float = 1e-12, max_iterations: int = 100
+) -> Tuple[float, np.array]:
     """
     Power Iteration.
     Find the largest eignevalue and corresponding eigenvector
@@ -42,19 +44,19 @@ def power_iteration(
     # Set convergence to False. Will define convergence when we exceed max_iterations
     # or when we have small changes from one iteration to next.
 
-    convergence = False
-    lamda_previous = 0
-    iterations = 0
-    error = 1e12
+    convergence: bool = False
+    lamda_previous: int = 0
+    iterations: int = 0
+    error: float = 1e12
 
     while not convergence:
         # Multiple matrix by the vector.
-        w = np.dot(input_matrix, vector)
+        w: float = np.dot(input_matrix, vector)
         # Normalize the resulting output vector.
         vector = w / np.linalg.norm(w)
         # Find rayleigh quotient
         # (faster than usual b/c we know vector is normalized already)
-        lamda = np.dot(vector.T, np.dot(input_matrix, vector))
+        lamda: int = np.dot(vector.T, np.dot(input_matrix, vector))
 
         # Check convergence.
         error = np.abs(lamda - lamda_previous) / lamda
@@ -73,25 +75,28 @@ def test_power_iteration() -> None:
     >>> test_power_iteration()  # self running tests
     """
     # Our implementation.
-    input_matrix = np.array([[41, 4, 20], [4, 26, 30], [20, 30, 50]])
-    vector = np.array([41, 4, 20])
-    eigen_value, eigen_vector = power_iteration(input_matrix, vector)
+    input_matrix: np.array = np.array([[41, 4, 20], [4, 26, 30], [20, 30, 50]])
+    vector: np.array = np.array([41, 4, 20])
+    eigen_value, eigen_vector = power_iteration(
+        input_matrix, vector)  # type: float, float
 
     # Numpy implementation.
 
     # Get eigen values and eigen vectors using built in numpy
     # eigh (eigh used for symmetric or hermetian matrices).
-    eigen_values, eigen_vectors = np.linalg.eigh(input_matrix)
+    eigen_values, eigen_vectors = np.linalg.eigh(
+        input_matrix)  # type: np.array, np.array
     # Last eigen value is the maximum one.
-    eigen_value_max = eigen_values[-1]
+    eigen_value_max: float = eigen_values[-1]
     # Last column in this matrix is eigen vector corresponding to largest eigen value.
-    eigen_vector_max = eigen_vectors[:, -1]
+    eigen_vector_max: float = eigen_vectors[:, -1]
 
     # Check our implementation and numpy gives close answers.
     assert np.abs(eigen_value - eigen_value_max) <= 1e-6
     # Take absolute values element wise of each eigenvector.
     # as they are only unique to a minus sign.
-    assert np.linalg.norm(np.abs(eigen_vector) - np.abs(eigen_vector_max)) <= 1e-6
+    assert np.linalg.norm(np.abs(eigen_vector) -
+                          np.abs(eigen_vector_max)) <= 1e-6
 
 
 if __name__ == "__main__":
