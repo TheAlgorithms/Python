064dab1a0eccb308f97a3b4967d2db1d41a18ba3
Christian Clauss <cclauss@me.com>
2020-08-19 18:05:17 +0200

    “” or not

--- strings/check_anagrams.py
+++ strings/check_anagrams.py
@@ -2,11 +2,11 @@ def check_anagrams(a: str, b: str) -> bool:
     """
     Two strings are anagrams if they are made of the same letters
     arranged differently (ignoring the case).
-    >>> check_anagrams('Silent','Listen')
+    >>> check_anagrams('Silent', 'Listen')
     True
-    >>> check_anagrams('This is a string','Is this a string')
+    >>> check_anagrams('This is a string', 'Is this a string')
     True
-    >>> check_anagrams('There','Their')
+    >>> check_anagrams('There', 'Their')
     False
     """
     return sorted(a.lower()) == sorted(b.lower())
@@ -18,5 +18,5 @@ if __name__ == "__main__":
 
     status = check_anagrams(input_A, input_B)
     print(
-        f"{input_A} and {input_B} are {'anagrams.' if status else 'not anagrams.' = }"
+        f"{input_A} and {input_B} are {'' if status else 'not '}anagrams."
     )
