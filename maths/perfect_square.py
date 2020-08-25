022cc8eae6fc91ed4c67c04de41faeb4d91799ed
Iheb Haboubi <iheb.haboubi56@gmail.com>
2020-08-25 11:18:18 +0100

    Add perfect_square_binary_search

--- maths/perfect_square.py
+++ maths/perfect_square.py
@@ -21,6 +21,36 @@ def perfect_square(num: int) -> bool:
     return math.sqrt(num) * math.sqrt(num) == num
 
 
+def perfect_square_binary_search(n: int) -> bool:
+    """
+    Check if a number is perfect square using binary search.
+    Time complexity : O(Log(n))
+    Space complexity: O(1)
+
+    >>> perfect_square(9)
+    True
+    >>> perfect_square(16)
+    True
+    >>> perfect_square(1)
+    True
+    >>> perfect_square(0)
+    True
+    >>> perfect_square(10)
+    False
+    """
+    left = 0
+    right = n
+    while left <= right:
+        mid = (left + right) // 2
+        if mid ** 2 == n:
+            return True
+        elif mid ** 2 > n:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return False
+
+
 if __name__ == "__main__":
     import doctest
 
