3d6a4fe12ca73fe5ec50760202cfebbe092b39c3
Kushagra Bansal <kushagrabansalajmer@gmail.com>
2020-08-28 18:07:54 +0530

    Update triplet_sum.py

--- other/triplet_sum.py
+++ other/triplet_sum.py
@@ -1,15 +1,23 @@
 """
-Given an array of integers and another integer X,
-we are required to find a triplet from the array such that it's sum is equal to X
+Given an array of integers and another integer target,
+we are required to find a triplet from the array such that it's sum is equal to
+the target.
 """
 from typing import List, Tuple
 from itertools import permutations
 from timeit import repeat
+from random import randint
+
+
+def make_dataset() -> Tuple[List[int], int]:
+    arr = [randint(-1000, 1000) for i in range(10)]
+    r = randint(-5000, 5000)
+    return (arr, r)
 
 
 def triplet_sum1(arr: List[int], target: int) -> Tuple[int, int, int]:
     """
-    Returns a triplet in in array with sum equal to X,
+    Returns a triplet in in array with sum equal to target,
     else (0, 0, 0).
     >>> triplet_sum1([13, 29, 7, 23, 5], 35)
     (5, 7, 23)
@@ -26,22 +34,9 @@ def triplet_sum1(arr: List[int], target: int) -> Tuple[int, int, int]:
     return (0, 0, 0)
 
 
-def sol1_time() -> str:
-    setup_code = """ 
-from __main__ import triplet_sum1
-from random import randint"""
-    test_code = """
-arr = [randint(-1000, 1000) for i in range(10)]
-r = randint(-5000, 5000)
-triplet_sum1(arr,r)
+def triplet_sum2(arr: List[int], target: int) -> Tuple[int, int, int]:
     """
-    times = repeat(stmt=test_code, setup=setup_code, repeat=5, number=10000)
-    return f"Naive solution time is {min(times)}"
-
-
-def triplet_sum2(arr: List[int], X: int) -> tuple:
-    """
-    Returns a triplet in in array with sum equal to X,
+    Returns a triplet in in array with sum equal to target,
     else (0, 0, 0).
     >>> triplet_sum2([13, 29, 7, 23, 5], 35)
     (5, 7, 23)
@@ -57,38 +52,36 @@ def triplet_sum2(arr: List[int], X: int) -> tuple:
     for i in range(n - 1):
         left, right = i + 1, n - 1
         while left < right:
-            if arr[i] + arr[left] + arr[right] == X:
+            if arr[i] + arr[left] + arr[right] == target:
                 return (arr[i], arr[left], arr[right])
-            elif arr[i] + arr[left] + arr[right] < X:
+            elif arr[i] + arr[left] + arr[right] < target:
                 left += 1
-            elif arr[i] + arr[left] + arr[right] > X:
+            elif arr[i] + arr[left] + arr[right] > target:
                 right -= 1
     else:
         return (0, 0, 0)
 
 
-def sol2_time() -> str:
-    setup_code = """ 
-from __main__ import triplet_sum2
-from random import randint"""
-    test_code = """
-arr = [randint(-1000, 1000) for i in range(10)]
-r = randint(-5000, 5000)
-triplet_sum2(arr,r)
-    """
-    times = repeat(stmt=test_code, setup=setup_code, repeat=5, number=10000)
-    return f"Optimized solution time is {min(times)}"
+def solution_times() -> Tuple[float, float]:
+    setup_code = """
+from __main__ import make_dataset, triplet_sum1, triplet_sum2
+dataset = make_dataset()
+"""
+    test_code1 = """
+triplet_sum1(*dataset)
+"""
+    test_code2 = """
+triplet_sum2(*dataset)
+"""
+    times1 = repeat(setup=setup_code, stmt=test_code1, repeat=5, number=10000)
+    times2 = repeat(setup=setup_code, stmt=test_code2, repeat=5, number=10000)
+    return (min(times1), min(times2))
 
 
 if __name__ == "__main__":
     from doctest import testmod
 
     testmod()
-    from random import randint
-
-    arr = [randint(-1000, 1000) for i in range(100)]
-    r = randint(-5000, 5000)
-    print(f"{triplet_sum1(arr,r)}")
-    print(f"{sol1_time()}")
-    print(f"{triplet_sum2(arr,r)}")
-    print(f"{sol2_time()}")
+    times = solution_times()
+    print(f"The time for naive implementation is {times[0]}.")
+    print(f"The time for optimized implementation is {times[1]}.")
