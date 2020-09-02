05bfed6ffbd9fefb3f11928ec377f905afe5940a
mohammadreza490 <47437328+mohammadreza490@users.noreply.github.com>
2020-09-03 00:38:51 +0100

    Update capitalize.py

--- strings/capitalize.py
+++ strings/capitalize.py
@@ -1,4 +1,4 @@
-def capitalize(str: sentence) -> str:
+def capitalize(sentence: str) -> str:
     """
     This function will capitalize the first letter of a sentence or a word
     >>> capitalize("hello world")
