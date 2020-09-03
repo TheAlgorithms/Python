36f3fdcfc897083dd631bbec70f79f3040afcde5
mohammadreza490 <47437328+mohammadreza490@users.noreply.github.com>
2020-09-03 13:46:20 +0100

    Update capitalize.py

--- strings/capitalize.py
+++ strings/capitalize.py
@@ -10,11 +10,20 @@ def capitalize(sentence: str) -> str:
     '123 hello world'
     >>> capitalize(" hello world")
     ' hello world'
+    >>> capitalize("a")
+    'A'
+    >>> capitalize("")
+    ''
     """
     lower_upper_dict = {
         lower: upper for lower, upper in zip(ascii_lowercase, ascii_uppercase)
     }
-    return lower_upper_dict.get(sentence[0], sentence[0]) + sentence[1:]
+    return (
+        lower_upper_dict.get(sentence[0], sentence[0])
+        + (sentence[1:] if (len(sentence) >= 1) else "")
+        if (len(sentence) != 0)
+        else ""
+    )
 
 
 if __name__ == "__main__":
