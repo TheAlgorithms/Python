52026cb4739caaa5e393454707b3fe0924d24eab
mohammadreza490 <47437328+mohammadreza490@users.noreply.github.com>
2020-09-03 11:38:25 +0100

    Update capitalize.py

--- strings/capitalize.py
+++ strings/capitalize.py
@@ -1,3 +1,5 @@
+import string
+
 def capitalize(sentence: str) -> str:
     """
     This function will capitalize the first letter of a sentence or a word
@@ -8,8 +10,20 @@ def capitalize(sentence: str) -> str:
     >>> capitalize(" hello world")
     ' hello world'
     """
-    first_char = sentence[0]
-    new_sentence = str.upper(first_char) + sentence[1:]
+    
+    """
+    Creates a new dictionary which keys are lowercase letters and the values are their corresponding uppercase letter.
+    It gets the first character of the sentence and if that character is a key in the dictionary, it gets the value 
+    and use it as the first character of the sentence.
+    """
+    first_letter = sentence[0]
+    lower_upper_dict = dict()
+    for lower, upper in zip(string.ascii_lowercase, string.ascii_uppercase): 
+        lower_upper_dict[lower] = upper
+    if first_letter in lower_upper_dict:
+        new_sentence = lower_upper_dict[first_letter] + sentence[1:] 
+    else:
+        new_sentence = sentence
     return new_sentence
 
 
