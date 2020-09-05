752efd54cf940a27bfaa7730e3e1acc04c66105f
NEERAJ ADITYANANTH POLAMPALLI <65017645+NEERAJAP2001@users.noreply.github.com>
2020-09-05 22:46:15 +0530

    update_binary_exponent

--- maths/binary_exponent.py
+++ maths/binary_exponent.py
@@ -1,5 +1,4 @@
 def bin_exp(num: int, expo: int) -> int:
-    
     """
     Calculate the exponent of 'num' over 'expo' and return a positive number.
     This function does binary exponential hence O(log N) time complexity.
@@ -14,28 +13,20 @@ def bin_exp(num: int, expo: int) -> int:
     0
     >>> bin_exp(1.5, 2.5)
     2
-    >>> bin_exp('a', 'b')
-    'An exception occurred'
     >>> bin_exp(5, None)
     1
     """
-    
     result = 1
     while expo:
-        try:
-            div, mod = divmod(expo, 2)
-            if mod:
-                result *= num
-                expo -= 1
-            else:
-                num *= num
-                expo = div 
-            return result  
-        except:
-            return 'An exception occurred'
- 
-
-
+        div, mod = divmod(expo, 2)
+        if mod:
+            result *= num
+            expo -= 1
+        else:
+            num *= num
+            expo = div 
+    return result  
+    
 # call the testmod function
 if __name__ == "__main__":
     from doctest import testmod
