f96ff689805ac3ba12e6b04163462ddb06543f47
NEERAJ ADITYANANTH POLAMPALLI <65017645+NEERAJAP2001@users.noreply.github.com>
2020-09-05 21:41:50 +0530

    update binary_exponent.py
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- maths/binary_exponent.py
+++ maths/binary_exponent.py
@@ -1,7 +1,6 @@
 def bin_exp(num, expo) -> int:
     """
-    This function calculates the exponent of 'num' over 'expo' and
-    returns a positive number.
+    Calculate the exponent of 'num' over 'expo' and return a positive number.
     This function does binary exponential hence O(log N) time complexity.
     So, It is faster than normal exponential function for huge numbers.
     >>> bin_exp(2, 5)
