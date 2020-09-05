191e613b173af4d533cd39acc9eed22879dfe421
Neeraj Ap <neeruap2001@gmail.com>
2020-09-05 19:18:52 +0530

    added algorith for binary exponent

---
+++ maths/binary_exponent.py
@@ -0,0 +1,25 @@
+def bin_exp(num, expo) -> int:
+    """
+    This function calculates the exponent of 'num' over 'expo' and
+    returns a positive number.
+    This function does binary exponential hence O(log N) time complexity.
+    So, It is faster than normal exponential function for huge numbers.
+    >>> bin_exp(2,5)==32
+    True
+    """
+    result = 1
+    while expo:
+        if expo % 2:
+            result *= num
+            expo = expo - 1
+        else:
+            num *= num
+            expo /= 2
+    return result
+
+
+# call the testmod function
+if __name__ == "__main__":
+    from doctest import testmod
+
+    testmod()
