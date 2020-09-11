0fe06812c7f781cfaf0fc9de3b8149234df6e822
Christian Clauss <cclauss@me.com>
2020-09-11 22:22:28 +0200

    Update worldometers_covid_with_lxml.py

--- web_programming/worldometers_covid_with_lxml.py
+++ web_programming/worldometers_covid_with_lxml.py
@@ -1,20 +1,23 @@
 """
 This is to show simple COVID19 info fetching from worldometers site using lxml
-* The main motivation to use lxml in place of bs4 is that it is faster & therefore more
-convenient to use in Python web projects (e.g. Django based)
+* The main motivation to use lxml in place of bs4 is that it is faster and therefore
+more convenient to use in Python web projects (e.g. Django or Flask-based)
 """
+
+from collections import namedtuple
 
 import requests
 from lxml import html
 
+covid_data = namedtuple("covid_data", "cases deaths recovered")
 
-wld = requests.get("https://www.worldometers.info/coronavirus/")
-wldh = html.fromstring(wld.content)
-wldc = wldh.xpath('//div[@class = "maincounter-number"]/span/text()')
-wcase = wldc[0]
-wdeath = wldc[1]
-wcure = wldc[2]
 
-print('Total COVID19 Cases in the world :' + wcase)
-print('Total Deaths Due to COVID19 in the world :' + wdeath)
-print('Total COVID19 Patients Cured in the world :' + wcure)
+def covid_stats(url: str = "https://www.worldometers.info/coronavirus/") -> covid_data:
+    xpath_str = '//div[@class = "maincounter-number"]/span/text()'
+    return covid_data(*html.fromstring(requests.get(url).content).xpath(xpath_str))
+
+
+fmt = """Total COVID-19 cases in the world: {}
+Total deaths due to COVID-19 in the world: {}
+Total COVID-19 patients recovered in the world: {}"""
+print(fmt.format(*covid_stats()))
