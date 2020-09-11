25239cc65ddf778e7f2ea54bc3986c5570d82e43
santoshrajkumar <rajkumar.nits@gmail.com>
2020-09-12 00:59:52 +0530

    lxmlCovidDataFetch1

--- web_programming/worldometers_covid_with_lxml.py
+++ web_programming/worldometers_covid_with_lxml.py
@@ -1,17 +1,19 @@
+"""
+This is to show simple COVID19 info fetching from worldometers site using lxml
+* The main motivation to use lxml in place of bs4 is that it is faster & therefore more
+convenient to use in Python web projects (e.g. Django based)
+"""
+
 import requests
 from lxml import html
 
-try:
-    wld = requests.get("https://www.worldometers.info/coronavirus/")
-    wldh = html.fromstring(wld.content)
-    wldc = wldh.xpath('//div[@class = "maincounter-number"]/span/text()')
-    wcase = wldc[0]
-    wdeath = wldc[1]
-    wcure = wldc[2]
-except:
-    wcase = 'fetching.....'
-    wdeath ='fetching.....'
-    wcure = 'fetching.....'
+
+wld = requests.get("https://www.worldometers.info/coronavirus/")
+wldh = html.fromstring(wld.content)
+wldc = wldh.xpath('//div[@class = "maincounter-number"]/span/text()')
+wcase = wldc[0]
+wdeath = wldc[1]
+wcure = wldc[2]
 
 print('Total COVID19 Cases in the world :' + wcase)
 print('Total Deaths Due to COVID19 in the world :' + wdeath)
