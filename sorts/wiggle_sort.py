e96f1ecfdc3a7ac895c1cd814a6f2de079a04bd3
Guillaume <guillaume.rochedix@gmail.com>
2020-09-12 21:44:40 +0200

    wiggle sort : type hint + doctest

--- sorts/wiggle_sort.py
+++ sorts/wiggle_sort.py
@@ -9,18 +9,27 @@ one possible Wiggle Sorted answer is [3, 5, 1, 6, 2, 4].
 """
 
 
-def wiggle_sort(nums):
-    """Perform Wiggle Sort."""
+def wiggle_sort(nums: list):
+    """
+    Python implementation of wiggle.
+    Example:
+    >>> merge_sort([0, 5, 3, 2, 2])
+    [0, 5, 2, 3, 2]
+    >>> merge_sort([])
+    []
+    >>> merge_sort([-2, -5, -45])
+    [-45, -2, -5]
+    """
     for i in range(len(nums)):
         if (i % 2 == 1) == (nums[i - 1] > nums[i]):
             nums[i - 1], nums[i] = nums[i], nums[i - 1]
 
 
 if __name__ == "__main__":
-    print("Enter the array elements:\n")
+    print("Enter the array elements:")
     array = list(map(int, input().split()))
-    print("The unsorted array is:\n")
+    print("The unsorted array is:")
     print(array)
     wiggle_sort(array)
-    print("Array after Wiggle sort:\n")
+    print("Array after Wiggle sort:")
     print(array)
