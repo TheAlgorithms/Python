042e4493977e3641320c377c1857640f675bd476
Christian Clauss <cclauss@me.com>
2020-09-13 19:49:39 +0200

    authenticate = login = render = redirect = print

--- web_programming/recaptcha_verification.py
+++ web_programming/recaptcha_verification.py
@@ -1,21 +1,11 @@
 """
-Recaptcha is a free captcha service offered by Google
-in order to secure websites / forms
-https://www.google.com/recaptcha/admin/create (This is the site where
-you can get your recaptcha keys created)
+Recaptcha is a free captcha service offered by Google in order to secure websites and
+forms.  At https://www.google.com/recaptcha/admin/create you can create new recaptcha
+keys and see the keys that your have already created.
 * Keep in mind that recaptcha doesn't work with localhost
-When you register recaptcha for your site,
-you'll get two keys: ClientKey & SecretKey.
-ClientKey is to be kept in the front end
-SecretKey is to be kept at backend
-"""
-# from django.contrib.auth import authenticate, login
-# from django.shortcuts import render, redirect
-import json
-import requests
-
-
-"""
+When you create a recaptcha key, your will get two separate keys: ClientKey & SecretKey.
+ClientKey should be kept in your site's front end
+SecretKey should be kept in your site's  back end
 
 # An example HTML login form with recaptcha tag is shown below
 
@@ -37,52 +27,40 @@ import requests
 
     <!-- Below is the recaptcha script to be kept inside html tag -->
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-Below one Django function based code for views.py file for a login form
-has been shown with recaptcha verification
+
+Below a Django function for the views.py file contains a login form for demonstrating
+recaptcha verification.
 """
+import requests
+
+try:
+    from django.contrib.auth import authenticate, login
+    from django.shortcuts import redirect, render
+except ImportError:
+    authenticate = login = render = redirect = print
 
 
 def login_using_recaptcha(request):
+    # Enter your recaptcha secret key here
+    secret_key = "secretKey"
+    url = "https://www.google.com/recaptcha/api/siteverify"
+
     # when method is not POST, direct user to login page
     if request.method != "POST":
-        print('Welcome to LoginPage')
-        # return render(request, "login.html")
+        return render(request, "login.html")
 
-    # get username, password & client_key from frontend
-    # username = request.POST.get("username")
-    # password = request.POST.get("password")
+    # from the frontend, get username, password, and client_key
+    username = request.POST.get("username")
+    password = request.POST.get("password")
     client_key = request.POST.get("g-recaptcha-response")
 
-    # Keep your recaptcha secret key here
-    secret_key = "secretKey"
-
-    # make json of your captcha data
-    captcha_data = {"secret": secret_key, "response": client_key}
-
-    # post recaptcha response to Google recaptcha api
-    post = requests.post(
-        "https://www.google.com/recaptcha/api/siteverify", data=captcha_data
-    )
-    # read the json response from recaptcha api
-    response = json.loads(post.text)
-    verify = response['success']
-    # if verify is true
-    if verify:
-        # authenticate user with following code
-        # user = authenticate(request, username=username, password=password)
-        user = True
-
-        # if user is in database
-        if user:
-            print('Login success')
-            # login user
-            # login(request, user)
-            # return redirect("/your-webpage")
-        else:
-            print('invalid crednetial')
-            # send user back to the login page again
-            # return render(request, "login.html")
-    else:
-        print('Captcha verification failed')
-        # if verify is not true, send user back to login page
-        # return render(request, "login.html")
+    # post recaptcha response to Google's recaptcha api
+    response = requests.post(url, data={"secret": secret_key, "response": client_key})
+    # if the recaptcha api verified our keys
+    if response.json().get("success", False):
+        # authenticate the user
+        user_in_database = authenticate(request, username=username, password=password)
+        if user_in_database:
+            login(request, user_in_database)
+            return redirect("/your-webpage")
+    return render(request, "login.html")
