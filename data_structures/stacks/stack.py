44b8cb0c81b1c86dc8957d6d218f3643488e84a9
Du Yuanchao <shellhub.me@gmail.com>
2020-09-13 19:56:03 +0800

    Updated Stack (#2414)
    * * Added type hints
    * Added test
    * Formated code
    
    * updating DIRECTORY.md
    
    * Update stack.py
    
    * Test error conditions for pop, peek, and
    
    Co-authored-by: github-actions <${GITHUB_ACTOR}@users.noreply.github.com>
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- data_structures/stacks/stack.py
+++ data_structures/stacks/stack.py
@@ -1,4 +1,5 @@
-__author__ = "Omkar Pathak"
+class StackOverflowError(BaseException):
+    pass
 
 
 class Stack:
@@ -7,18 +8,17 @@ class Stack:
     element to the top of the stack, and pop() removes an element from the top
     of a stack. The order in which elements come off of a stack are
     Last In, First Out (LIFO).
-
     https://en.wikipedia.org/wiki/Stack_(abstract_data_type)
     """
 
-    def __init__(self, limit=10):
+    def __init__(self, limit: int = 10):
         self.stack = []
         self.limit = limit
 
-    def __bool__(self):
+    def __bool__(self) -> bool:
         return bool(self.stack)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self.stack)
 
     def push(self, data):
@@ -29,21 +29,20 @@ class Stack:
 
     def pop(self):
         """ Pop an element off of the top of the stack."""
-        if self.stack:
-            return self.stack.pop()
-        else:
-            raise IndexError("pop from an empty stack")
+        return self.stack.pop()
 
     def peek(self):
         """ Peek at the top-most element of the stack."""
-        if self.stack:
-            return self.stack[-1]
+        return self.stack[-1]
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         """ Check if a stack is empty."""
         return not bool(self.stack)
 
-    def size(self):
+    def is_full(self) -> bool:
+        return self.size() == self.limit
+
+    def size(self) -> int:
         """ Return the size of the stack."""
         return len(self.stack)
 
@@ -52,24 +51,54 @@ class Stack:
         return item in self.stack
 
 
-class StackOverflowError(BaseException):
-    pass
+def test_stack() -> None:
+    """
+    >>> test_stack()
+    """
+    stack = Stack(10)
+    assert bool(stack) is False
+    assert stack.is_empty() is True
+    assert stack.is_full() is False
+    assert str(stack) == "[]"
+
+    try:
+        _ = stack.pop()
+        assert False  # This should not happen
+    except IndexError:
+        assert True  # This should happen
+
+    try:
+        _ = stack.peek()
+        assert False  # This should not happen
+    except IndexError:
+        assert True  # This should happen
+
+    for i in range(10):
+        assert stack.size() == i
+        stack.push(i)
+
+    assert bool(stack) is True
+    assert stack.is_empty() is False
+    assert stack.is_full() is True
+    assert str(stack) == str(list(range(10)))
+    assert stack.pop() == 9
+    assert stack.peek() == 8
+
+    stack.push(100)
+    assert str(stack) == str([0, 1, 2, 3, 4, 5, 6, 7, 8, 100])
+
+    try:
+        stack.push(200)
+        assert False  # This should not happen
+    except StackOverflowError:
+        assert True  # This should happen
+
+    assert stack.is_empty() is False
+    assert stack.size() == 10
+
+    assert 5 in stack
+    assert 55 not in stack
 
 
 if __name__ == "__main__":
-    stack = Stack()
-    for i in range(10):
-        stack.push(i)
-
-    print("Stack demonstration:\n")
-    print("Initial stack: " + str(stack))
-    print("pop(): " + str(stack.pop()))
-    print("After pop(), the stack is now: " + str(stack))
-    print("peek(): " + str(stack.peek()))
-    stack.push(100)
-    print("After push(100), the stack is now: " + str(stack))
-    print("is_empty(): " + str(stack.is_empty()))
-    print("size(): " + str(stack.size()))
-    num = 5
-    if num in stack:
-        print(f"{num} is in stack")
+    test_stack()
