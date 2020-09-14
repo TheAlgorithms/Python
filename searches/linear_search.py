dbf677fd84d3a5c5e71b8c6a56cd59b995edcfc7
Ashley Jeji George <40469421+Ashley-J-George@users.noreply.github.com>
2020-09-14 15:31:58 +0530

    Update linear_search.py
    Both the functions return the index if the target is found and -1 if it is not found
    The rec_linear_search raises an exception if there is an indexing problem
    Made changes in the doc comments

--- searches/linear_search.py
+++ searches/linear_search.py
@@ -30,47 +30,48 @@ def linear_search(sequence: list, target: int) -> int:
     1
 
     >>> linear_search([0, 5, 7, 10, 15], 6)
-
+    -1
     """
     for index, item in enumerate(sequence):
         if item == target:
             return index
-    return None
+    return -1
 
 
 def rec_linear_search(sequence: list, low: int, high: int, target: int) -> int:
-    '''
+    """
     Pure implementation of recursive linear search algorithm in Python
 
-    :param sequence: An array of items
+    :param sequence: a collection with comparable items (as sorted items not required
+        in Linear Search)
     :param low: Lower bound of the array
     :param high: Higher bound of the array
     :param target: The element to be found
-    :return: Index of the key or -1 if key not found or None in case of an exception
+    :return: Index of the key or -1 if key not found
 
     Examples:
     >>> rec_linear_search([0, 30, 500, 100, 700], 0, 4, 0)
-    The element 0 is present at index 0
-    
+    0
+
     >>> rec_linear_search([0, 30, 500, 100, 700], 0, 4, 700)
-    The element 700 is present at index 4
-    
+    4
+
     >>> rec_linear_search([0, 30, 500, 100, 700], 0, 4, 30)
-    The element 5 is present at index 1
-    
+    1
+
     >>> rec_linear_search([0, 30, 500, 100, 700], 0, 4, -6)
-    The element -6 is not present in the array
-    '''
+    -1
+    """
+    if (0 > high or high >= len(sequence)) or (0 > low or low >= len(sequence)):
+        raise Exception("Invalid upper or lower bound!")
+
     if high < low:
         return -1
-    try:
-        if sequence[low] == target:
-            return low
-        if sequence[high] == target:
-            return high
-        return rec_linear_search(sequence, low + 1, high - 1, target)
-    except IndexError:
-        print('Invalid upper or lower bound!')
+    if sequence[low] == target:
+        return low
+    if sequence[high] == target:
+        return high
+    return rec_linear_search(sequence, low + 1, high - 1, target)
 
 
 if __name__ == "__main__":
