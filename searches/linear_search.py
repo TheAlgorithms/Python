852069491a12bd148ad35c0a87c4f6548f5ee585
Christian Clauss <cclauss@me.com>
2020-09-14 12:37:55 +0200

    Update linear_search.py

--- searches/linear_search.py
+++ searches/linear_search.py
@@ -2,17 +2,15 @@
 This is pure Python implementation of linear search algorithm
 
 For doctests run following command:
-python -m doctest -v linear_search.py
-or
 python3 -m doctest -v linear_search.py
 
 For manual testing run:
-python linear_search.py
+python3 linear_search.py
 """
 
 
 def linear_search(sequence: list, target: int) -> int:
-    """Pure implementation of linear search algorithm in Python
+    """A pure Python implementation of a linear search algorithm
 
     :param sequence: a collection with comparable items (as sorted items not required
         in Linear Search)
@@ -22,13 +20,10 @@ def linear_search(sequence: list, target: int) -> int:
     Examples:
     >>> linear_search([0, 5, 7, 10, 15], 0)
     0
-
     >>> linear_search([0, 5, 7, 10, 15], 15)
     4
-
     >>> linear_search([0, 5, 7, 10, 15], 5)
     1
-
     >>> linear_search([0, 5, 7, 10, 15], 6)
     -1
     """
@@ -40,7 +35,7 @@ def linear_search(sequence: list, target: int) -> int:
 
 def rec_linear_search(sequence: list, low: int, high: int, target: int) -> int:
     """
-    Pure implementation of recursive linear search algorithm in Python
+    A pure Python implementation of a recursive linear search algorithm
 
     :param sequence: a collection with comparable items (as sorted items not required
         in Linear Search)
@@ -52,19 +47,15 @@ def rec_linear_search(sequence: list, low: int, high: int, target: int) -> int:
     Examples:
     >>> rec_linear_search([0, 30, 500, 100, 700], 0, 4, 0)
     0
-
     >>> rec_linear_search([0, 30, 500, 100, 700], 0, 4, 700)
     4
-
     >>> rec_linear_search([0, 30, 500, 100, 700], 0, 4, 30)
     1
-
     >>> rec_linear_search([0, 30, 500, 100, 700], 0, 4, -6)
     -1
     """
-    if (0 > high or high >= len(sequence)) or (0 > low or low >= len(sequence)):
+    if not (0 <= high < len(sequence) and 0 <= low < len(sequence)):
         raise Exception("Invalid upper or lower bound!")
-
     if high < low:
         return -1
     if sequence[low] == target:
@@ -76,12 +67,11 @@ def rec_linear_search(sequence: list, low: int, high: int, target: int) -> int:
 
 if __name__ == "__main__":
     user_input = input("Enter numbers separated by comma:\n").strip()
-    sequence = [int(item) for item in user_input.split(",")]
+    sequence = [int(item.strip()) for item in user_input.split(",")]
 
-    target_input = input("Enter a single number to be found in the list:\n")
-    target = int(target_input)
+    target = int(input("Enter a single number to be found in the list:\n").strip())
     result = linear_search(sequence, target)
-    if result is not None:
-        print(f"{target} found at position : {result}")
+    if result != -1:
+        print(f"linear_search({sequence}, {target}) = {result}")
     else:
-        print("Not found")
+        print(f"{target} was not found in {sequence}")
