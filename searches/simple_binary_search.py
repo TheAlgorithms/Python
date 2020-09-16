0801878fb23296fb21a085868ab1aac35fdfda24
poloso <osoriopabl@gmail.com>
2020-09-16 17:46:21 -0500

    Apply suggestions from code review
    - Delete documentation line to run doctests
    - Delete type hints for variables that comes from functions
    
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- searches/simple_binary_search.py
+++ searches/simple_binary_search.py
@@ -2,19 +2,16 @@
 Pure Python implementation of the binary search algorithm.
 
 For doctests run following command:
-python -m doctest -v simple_binary_search.py
-or
 python3 -m doctest -v simple_binary_search.py
 
 For manual testing run:
-python simple_binary_search.py
+python3 simple_binary_search.py
 """
 from typing import List
 
 
 def binary_search(a_list: List[int], item: int) -> bool:
     """
-    Pure python implementation of a binary search of a number is in a list.
 
     >>> test_list = [0, 1, 2, 8, 13, 17, 19, 32, 42]
     >>> print(binary_search(test_list, 3))
@@ -34,10 +31,10 @@ def binary_search(a_list: List[int], item: int) -> bool:
 
 
 if __name__ == "__main__":
-    user_input: str = input("Enter numbers separated by comma:\n").strip()
-    sequence: List[int] = [int(item.strip()) for item in user_input.split(",")]
+    user_input = input("Enter numbers separated by comma:\n").strip()
+    sequence = [int(item.strip()) for item in user_input.split(",")]
     target_str = input("Enter the number to be found in the list:\n").strip()
-    target: int = int(target_str)
+    target = int(target_str)
     result = binary_search(sequence, target)
     if result:
         print(f"{target} was found in {sequence}")
