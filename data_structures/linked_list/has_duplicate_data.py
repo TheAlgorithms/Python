cbd04a6ef37d46eb4111766cf54eed730c26e315
Christian Clauss <cclauss@me.com>
2020-09-18 22:20:43 +0200

    Update has_duplicate_data.py

--- data_structures/linked_list/has_duplicate_data.py
+++ data_structures/linked_list/has_duplicate_data.py
@@ -15,32 +15,33 @@ class Node:
     @property
     def has_duplicate_data(self) -> bool:
         """
-        >>> node1 = Node(1)
-        >>> node1.next_node = Node(2)
-        >>> node1.next_node.next_node = Node(3)
-        >>> node1.next_node.next_node.next_node = Node(4)
-        >>> node.has_duplicate_data
+        >>> root_node = Node(1)
+        >>> root_node.next_node = Node(2)
+        >>> root_node.next_node.next_node = Node(3)
+        >>> root_node.next_node.next_node.next_node = Node(4)
+        >>> root_node.has_duplicate_data
         False
-        >>> node1.next_node.next_node.next_node = node1.next_node
-        >>> node.has_duplicate_data
+        >>> root_node.next_node.next_node.next_node = root_node.next_node
+        >>> root_node.has_duplicate_data
         True
         """
         return len(list(self)) != len(set(self))
 
 
 if __name__ == "__main__":
-    node1 = Node(1)
-    node1.next_node = Node(2)
-    node1.next_node.next_node = Node(3)
-    node1.next_node.next_node.next_node = Node(4)
-    node1.next_node.next_node.next_node = node1.next_node
-    print(node1.has_duplicate_data)
+    root_node = Node(1)
+    root_node.next_node = Node(2)
+    root_node.next_node.next_node = Node(3)
+    root_node.next_node.next_node.next_node = Node(4)
+    print(root_node.has_duplicate_data)  # False
+    root_node.next_node.next_node.next_node = root_node.next_node
+    print(root_node.has_duplicate_data)  # True
 
-    node2 = Node(5)
-    node2.next_node = Node(6)
-    node2.next_node.next_node = Node(5)
-    node2.next_node.next_node.next_node = Node(6)
-    print(node2.has_duplicate_data)
+    root_node = Node(5)
+    root_node.next_node = Node(6)
+    root_node.next_node.next_node = Node(5)
+    root_node.next_node.next_node.next_node = Node(6)
+    print(root_node.has_duplicate_data)  # True
 
-    node3 = Node(1)
-    print(node3.has_duplicate_data)
+    root_node = Node(1)
+    print(root_node.has_duplicate_data)  # False
