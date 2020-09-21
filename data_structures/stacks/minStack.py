5d289ad8abb7c90034d7ec8c7ee6872ed19a2918
Soumen sinha mahapatra <soumen1696@gmail.com>
2020-09-21 13:07:54 +0530

    Create minStack.py

--- data_structures/stacks/minStack.py
+++ data_structures/stacks/minStack.py
@@ -1,45 +1,37 @@
-'''
-    So MinStack is a Stack through which we can get minimum element in Constant time, we will maintain another stack to return Minimum 
-    Element in O(1) time 
-
-'''
 class MinStack:
-    
-
     def __init__(self):
-        ''' Creating stack and min Stack, and size variable to keep track of no of elements in stack'''
+        """ Creating stack minStack and size Variable"""
         self.stack = []
-        self.minStack =[]
-        self.size =0
+        self.minStack = []
+        self.size = 0
 
-    def push(self, data:int) -> None:
-        if self.size == 0: 
+    def push(self, data: int) -> None:
+        if self.size == 0:
             self.minStack.append(data)
         elif data <= self.minStack[-1]:
             self.minStack.append(data)
         self.stack.append(data)
-        self.size+=1
-    
-    def pop(self)-> None:
-        ''' Removes the topmost element from the Stack  ''' 
-        top =  self.stack.pop()
-        self.size-=1
+        self.size = self.size + 1
+
+    def pop(self) -> None:
+        """Removes the topmost element from the Stack """
+        top = self.stack.pop()
+        self.size = self.size - 1
         if top <= self.minStack[-1]:
             self.minStack.pop()
-        
-    
-    def top(self) ->None:
-        ''' Returns the top element from the stack '''
+
+    def top(self) -> None:
+        """Returns the top element from the stack """
         return self.stack[-1]
 
     def getMin(self):
-        ''' Returns the top element from the  Minstack '''
+        """Returns the top element from the  Minstack """
         return self.minStack[-1]
+
 
 # Code execution starts here
 if __name__ == "__main__":
-    
-   
+
     # Creating a min Stack
     stack = MinStack()
 
