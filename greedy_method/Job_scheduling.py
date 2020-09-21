461a38d3d294e7969d3e1c8e144a3d4c40d5110c
Mohit Kumar <47894634+Mohitkumar6122@users.noreply.github.com>
2020-09-21 15:12:25 +0530

    Update Job_scheduling.py

--- greedy_method/Job_scheduling.py
+++ greedy_method/Job_scheduling.py
@@ -1,14 +1,15 @@
-''' Author : Mohit Kumar 
-    
-    Job Sequencing Problem implemented in python
-
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+''' 
+Author : Mohit Kumar 
+Job Sequencing Problem implemented in python
 '''
 
-from operator import itemgetter # Return a callable object that fetches item from its operand using the operand’s __getitem__() method.
-                                # If multiple items are specified, returns a tuple of lookup values
+from operator import itemgetter
 
 
 class Scheduling:
+
     def __init__(self, jobs):
         self.jobs = jobs
 
@@ -22,7 +23,7 @@ class Scheduling:
             self.K.append(self.jobs[self.x])
             self.x = self.x + 1
             self.feasibility = self.feasible(self.K, self.sdl)
-            if self.feasibility == True:
+            if self.feasibility is True:
                 self.J = self.K.copy()
 
         return self.J
@@ -35,14 +36,14 @@ class Scheduling:
         self.j = 1
         self.k = 0
 
-
         while self.i < len(self.tmp):
             while self.j < len(self.tmp):
                 self.index1 = self.jobs.index(self.tmp[self.i])
                 self.index2 = self.jobs.index(self.tmp[self.j])
                 self.j = self.j + 1
-                if (fdl[self.index1] > fdl[self.index2]):
-                    self.tmp[self.i], self.tmp[self.j] = self.tmp[self.j], self.tmp[self.i]
+                if fdl[self.index1] > fdl[self.index2]:
+                    (self.tmp[self.i], self.tmp[self.j]) = \
+                        (self.tmp[self.j], self.tmp[self.i])
             self.i = self.i + 1
 
         while self.k < len(self.tmp):
@@ -51,47 +52,63 @@ class Scheduling:
             self.dlineval = fdl[self.jobindex]
             self.ftest = self.k + 1
             self.k = self.k + 1
-            if (self.dlineval < self.ftest):
+            if self.dlineval < self.ftest:
                 self.isFeasible = False
                 break
 
         return self.isFeasible
 
-def main():
-    n = 7       # Number of jobs
-    jobs =     [0, 1, 2, 3, 4, 5, 6]  # jobs id
-    deadline = [0, 2, 4, 3, 3, 2, 1]  # deadline to job[i]
-    profit =   [0 , 46, 52, 30, 36 ,56, 40] # profit associated wrt job[i]
 
-    midresult = [list(x) for x in zip(deadline, profit ,jobs)]
+def main():
+    n = 7  # Number of jobs
+    jobs = [  # jobs id
+        0,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        ]
+    deadline = [  # deadline to job[i]
+        0,
+        2,
+        4,
+        3,
+        3,
+        2,
+        1,
+        ]
+    profit = [  # profit associated wrt job[i]
+        0,
+        46,
+        52,
+        30,
+        36,
+        56,
+        40,
+        ]
+
+    midresult = [list(x) for x in zip(deadline, profit, jobs)]
     midresult.sort(key=lambda k: (k[0], -k[1]))
-    deadline, profit, jobs = map(list, zip(*midresult))
+    (deadline, profit, jobs) = map(list, zip(*midresult))
 
     sins = Scheduling(jobs)
     sjobs = sins.schedule(n, deadline)
-    print("\n Jobs", sjobs)
+    print ('\n Jobs', sjobs)
 
     finalprofit = []
     finaldl = []
 
     for c in range(len(sjobs)):
-      item = sjobs[c]
-      jobsindex = jobs.index(item)
-      finalprofit.append(profit[jobsindex])
-      finaldl.append(deadline[jobsindex])
+        item = sjobs[c]
+        jobsindex = jobs.index(item)
+        finalprofit.append(profit[jobsindex])
+        finaldl.append(deadline[jobsindex])
 
-    print("\n profit", finalprofit)
-    print("\n Deadline", finaldl)
+    print ('\n profit', finalprofit)
+    print ('\n Deadline', finaldl)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
-    
-'''  
-    OUTPUT : 
-    Jobs [6, 5, 4, 2]
-
-    profit [40, 56, 36, 52]
-
-    Deadline [1, 2, 3, 4]
-'''
- 
