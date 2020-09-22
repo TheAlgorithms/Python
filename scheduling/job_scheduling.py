05632be315e817105ba8b59dc90235f2386cbeda
Mohit Kumar <47894634+Mohitkumar6122@users.noreply.github.com>
2020-09-22 23:00:39 +0530

    Update job_scheduling.py

--- scheduling/job_scheduling.py
+++ scheduling/job_scheduling.py
@@ -5,27 +5,56 @@ Author : Mohit Kumar
 Job Sequencing Problem implemented in python
 """
 from collections import namedtuple
+from typing import List
 
 
 class Scheduling:
-    def __init__(self, jobs):
+    def __init__(self, jobs: int) -> None:
+        """
+        Assign jobs as instance of class Scheduling
+        """
         self.jobs = jobs
 
-    def schedule(self, n, deadline):
-        self.sdl = deadline
-        self.J = [self.jobs[1]]
+    def schedule(self, total_jobs: int, deadline: List[int]) -> List[int]:
+        """
+        Parameteres  : total_jobs  and list of deadline of jobs
+
+        Returns : List of jobs_id which are profitable  and can be done before
+                  deadline
+
+        Scheduling(3).schedule(3, [3, 4, 5])
+        >>> [1, 2]
+
+        Scheduling(4).schedule(4, [13, 2 33, 16])
+        >>> [0, 3, 2]
+
+        """
+        self.j = [self.jobs[1]]
         self.x = 2
-        while self.x < n:
-            self.K = self.J.copy()
-            self.K.append(self.jobs[self.x])
-            self.x = self.x + 1
-            if self.feasible(self.K, self.sdl):
-                self.J = self.K.copy()
+        while self.x < total_jobs:
+            self.k = self.j.copy()
+            self.k.append(self.jobs[self.x])
+            self.x += 1
+            if self.feasible(self.k, deadline):
+                self.j = self.k.copy()
 
-        return self.J
+        return self.j
 
-    def feasible(self, K, fdl):
-        self.tmp = K
+    def feasible(self, profit_jobs: List[int], deadline: List[int]) -> bool:
+        """
+        Parameters : list of current profitable jobs within deadline
+                     list of deadline of jobs
+
+        Returns : true if k[-1] job is profitable to us else false
+
+        Scheduling(7).feasible( [6, 5], [0, 1, 2, 2, 3, 3, 4] )
+        >>> True
+
+        Scheduling(7).feasible( [6, 5, 1],  [0, 1, 2, 2, 3, 3, 4] )
+        >>> False
+
+        """
+        self.tmp = profit_jobs
         self.is_feasible = True
 
         i = 0
@@ -37,7 +66,7 @@ class Scheduling:
                 self.index1 = self.jobs.index(self.tmp[i])
                 self.index2 = self.jobs.index(self.tmp[j])
                 j += 1
-                if fdl[self.index1] > fdl[self.index2]:
+                if deadline[self.index1] > deadline[self.index2]:
                     (self.tmp[i], self.tmp[j]) = (
                         self.tmp[j],
                         self.tmp[i],
@@ -47,19 +76,17 @@ class Scheduling:
         while k < len(self.tmp):
             self.job = self.tmp[k]
             self.jobindex = self.jobs.index(self.job)
-            self.dlineval = fdl[self.jobindex]
+            self.dlineval = deadline[self.jobindex]
             self.ftest = k + 1
             k += 1
             if self.dlineval < self.ftest:
                 self.is_feasible = False
                 break
-
         return self.is_feasible
 
 
 def main():
-    n = 7  # Number of jobs
-    job = namedtuple("job", "jobs deadline profit")
+    job = namedtuple("job", "job_id deadline profit")
     jobs = [
         job(0, 0, 0),
         job(1, 2, 46),
@@ -69,23 +96,23 @@ def main():
         job(5, 2, 56),
         job(6, 1, 40),
     ]
-
+    # midresult stores jobs in sorting order of deadline
     midresult = []
-    for i in range(n):
-        cur_job = []
-        cur_job.extend((jobs[i].deadline, jobs[i].profit, jobs[i].jobs))
-        midresult.append(cur_job)
+    for i in range(len(jobs)):
+        current_job = []
+        current_job.extend((jobs[i].deadline, jobs[i].profit, jobs[i].job_id))
+        midresult.append(current_job)
     midresult.sort(key=lambda k: (k[0], -k[1]))
     (deadline, profit, jobs) = map(list, zip(*midresult))
 
-    sins = Scheduling(jobs)
-    sjobs = sins.schedule(n, deadline)
-    print("\n Jobs", sjobs)
+    scheduling_jobs = Scheduling(jobs)
+    scheduled_jobs = scheduling_jobs.schedule(len(jobs), deadline)
+    print(f"\n Jobs {scheduled_jobs}")
 
     finalprofit = []
     finaldl = []
 
-    for i, item in enumerate(sjobs):
+    for i, item in enumerate(scheduled_jobs):
         jobsindex = jobs.index(item)
         finalprofit.append(profit[jobsindex])
         finaldl.append(deadline[jobsindex])
