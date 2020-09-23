e3aa1b3a04f5d5336d401b0dbe083194980388fc
spamegg <4255997+spamegg1@users.noreply.github.com>
2020-09-23 22:42:05 +0300

    Update other/two_sum.py
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- other/two_sum.py
+++ other/two_sum.py
@@ -11,7 +11,7 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 """
-from typing import List
+from __future__ import annotations
 
 
 def two_sum(nums: List[int], target: int) -> List[int]:
