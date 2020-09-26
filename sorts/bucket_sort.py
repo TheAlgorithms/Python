cca7cdbca5db49d97edf21c523b2a9e15a469403
shellhub <shellhub.me@gmail.com>
2020-09-27 04:53:49 +0800

    delete blank line

--- sorts/bucket_sort.py
+++ sorts/bucket_sort.py
@@ -63,6 +63,5 @@ if __name__ == "__main__":
     from doctest import testmod
 
     testmod()
-    
     assert bucket_sort([4, 5, 3, 2, 1]) == [1, 2, 3, 4, 5]
     assert bucket_sort([0, 1, -10, 15, 2, -2]) == [-10, -2, 0, 1, 2, 15]
