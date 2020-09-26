54928d5a28882cd7205431180a0fc1fcd0a0c7c5
Soumen sinha mahapatra <soumen1696@gmail.com>
2020-09-26 17:32:01 +0530

    Update min_stack.py

--- data_structures/stacks/min_stack.py
+++ data_structures/stacks/min_stack.py
@@ -1,4 +1,6 @@
 class MinStack:
+    """ Designing a Stack that can return Minimum element in Constant time"""
+
     def __init__(self):
         """ Creating stack minStack and size Variable"""
         self.stack = []
@@ -6,6 +8,12 @@ class MinStack:
         self.size = 0
 
     def push(self, data: int) -> None:
+        """
+        Push helps to put a element a the end of the stack
+        >>> s = MinStack()
+        >>> s.push(1)
+        >>> assert s.size == 1
+        """
         if self.size == 0:
             self.minStack.append(data)
         elif data <= self.minStack[-1]:
@@ -14,18 +22,33 @@ class MinStack:
         self.size = self.size + 1
 
     def pop(self) -> None:
-        """Removes the topmost element from the Stack """
+        """Removes the topmost element from the Stack
+        >>> s = MinStack()
+        >>> s.push(1)
+        >>> s.push(2)
+        >>> assert s.pop() == 2
+        """
         top = self.stack.pop()
         self.size = self.size - 1
         if top <= self.minStack[-1]:
             self.minStack.pop()
+        return top
 
     def top(self) -> None:
-        """Returns the top element from the stack """
+        """Returns the top element from the stack
+        >>> s = MinStack()
+        >>> s.push(3)
+        >>> s.push(2)
+        >>> assert s.top() == 2
+        """
         return self.stack[-1]
 
-    def getMin(self):
-        """Returns the top element from the  Minstack """
+    def get_min(self):
+        """Returns the top element from the  Minstack
+        >>> s = MinStack()
+        >>> s.push(5)
+        >>> assert s.get_min() == 5
+        """
         return self.minStack[-1]
 
 
@@ -36,28 +59,25 @@ if __name__ == "__main__":
     stack = MinStack()
 
     stack.push(6)
-    print(stack.getMin())  # prints  6
+    print(stack.get_min())  # prints  6
 
     stack.push(7)
-    print(stack.getMin())  # prints 6
+    print(stack.get_min())  # prints 6
 
     stack.push(8)
-    print(stack.getMin())  # prints 6
+    print(stack.get_min())  # prints 6
 
     stack.push(5)
-    print(stack.getMin())  # prints 5
+    print(stack.get_min())  # prints 5
 
     stack.push(3)
-    print(stack.getMin())  # prints 3
+    print(stack.get_min())  # prints 3
 
     stack.pop()
-    print(stack.getMin())  # prints 5
+    print(stack.get_min())  # prints 5
 
     stack.push(10)
-    print(stack.getMin())  # prints 5
+    print(stack.get_min())  # prints 5
 
     stack.pop()
-    print(stack.getMin())  # prints 5
-
-    stack.pop()
-    print(stack.getMin())  # prints 6
+    print(stack.get_min())  # prints 5
