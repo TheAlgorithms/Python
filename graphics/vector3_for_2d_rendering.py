f5e9d4ea2e86f65a64bfada41d137ceb8a8ad1e2
xcodz-dot <71920621+xcodz-dot@users.noreply.github.com>
2020-09-28 13:44:47 +0530

    Update vector3_for_2d_rendering.py
    Added enough doctests

--- graphics/vector3_for_2d_rendering.py
+++ graphics/vector3_for_2d_rendering.py
@@ -18,6 +18,14 @@ def convert_to_2d(x: float, y: float, z: float, scale: float,
 
     >>> convert_to_2d(1.0, 2.0, 3.0, 10.0, 10.0)
     (7.6923076923076925, 15.384615384615385)
+
+    >>> convert_to_2d(1, 2, 3, 10, 10)
+    (7.6923076923076925, 15.384615384615385)
+
+    >>> convert_to_2d("1", 2, 3, 10, 10)  # '1' is str
+    Traceback (most recent call last):
+        ...
+    TypeError: Input values must either be float or int: ['1', 2, 3, 10, 10]
     """
     if not all(isinstance(val, (float, int)) for val in locals().values()):
         raise TypeError("Input values must either be float or int: "
@@ -36,6 +44,25 @@ def rotate(x: float, y: float, z: float, axis: str,
 
     >>> rotate(1.0, 2.0, 3.0, 'y', 90.0)
     (3.130524675073759, 2.0, 0.4470070007889556)
+
+    >>> rotate(1, 2, 3, "z", 180)
+    (0.999736015495891, -2.0001319704760485, 3)
+
+    >>> rotate('1', 2, 3, "z", 90.0)  # '1' is str
+    Traceback (most recent call last):
+        ...
+    TypeError: Input values except axis must either be float or int: ['1', 2, 3, 90.0]
+
+    >>> rotate(1, 2, 3, "n", 90)  # 'n' is not a valid axis
+    Traceback (most recent call last):
+        ...
+    ValueError: not a valid axis, choose one of 'x', 'y', 'z'
+
+    >>> rotate(1, 2, 3, "x", -90)
+    (1, -2.5049096187183877, -2.5933429780983657)
+
+    >>> rotate(1, 2, 3, "x", 450)  # 450 wrap around to 90
+    (1, 3.5776792428178217, -0.44744970165427644)
     """
     if not isinstance(axis, str):
         raise TypeError("Axis must be a str")
@@ -45,8 +72,6 @@ def rotate(x: float, y: float, z: float, axis: str,
         raise TypeError("Input values except axis must either be float or int: "
                         f"{list(input_variables.values())}")
     angle %= 360  # wrap around if angle >= 360
-    if not 0 <= angle < 360:
-        raise ValueError("Angle is supposed to be in between 0, 360")
     angle = angle / 450 * 180 / math.pi
     if axis == 'z':
         new_x = x * math.cos(angle) - y * math.sin(angle)
