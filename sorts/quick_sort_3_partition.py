a30a2831e66cdd1d4e87d161b26c19512cadb738
Lewis Tian <taseikyo@gmail.com>
2020-09-29 11:32:10 +0800

    Update sorts/quick_sort_3partition.py
    Another quick sort algorithm, returns a new sorted list

--- sorts/quick_sort_3_partition.py
+++ sorts/quick_sort_3_partition.py
@@ -1,4 +1,4 @@
-def quick_sort_3partition(sorting, left, right):
+def quick_sort_3partition(sorting: list, left: int, right: int) -> list:
     if right <= left:
         return
     a = i = left
@@ -18,7 +18,33 @@ def quick_sort_3partition(sorting, left, right):
     quick_sort_3partition(sorting, b + 1, right)
 
 
+def quick_sort_3partition(sorting: list) -> list:
+    """
+    Another quick sort algorithm, returns a new sorted list
+
+    >>> quick_sort_3partition([])
+    []
+    >>> quick_sort_3partition([1])
+    [1]
+    >>> quick_sort_3partition([-5, -2, 1, -2, 0, 1])
+    [-5, -2, -2, 0, 1, 1]
+    >>> quick_sort_3partition([1, 2, 5, 1, 2, 0, 0, 5, 2, -1])
+    [-1, 0, 0, 1, 1, 2, 2, 2, 5, 5]
+    """
+    if len(sorting) <= 1:
+        return sorting
+    return (
+        quick_sort_3partition([i for i in sorting if i < sorting[0]])
+        + [i for i in sorting if i == sorting[0]]
+        + quick_sort_3partition([i for i in sorting if i > sorting[0]])
+    )
+
+
 if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod(verbose=True)
+
     user_input = input("Enter numbers separated by a comma:\n").strip()
     unsorted = [int(item) for item in user_input.split(",")]
     quick_sort_3partition(unsorted, 0, len(unsorted) - 1)
