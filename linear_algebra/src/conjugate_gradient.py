322cfa4d8f9cb20cb054390125e83e3fd541be95
Zeyad Zaky <zeyadzaky@Zeyads-MacBook-Pro.local>
2020-09-28 23:40:42 -0700

    Added documentation links, chnaged variable names to lower case and more descriptive naming, added check for symmetry in _is_matrix_spd

--- linear_algebra/src/conjugate_gradient.py
+++ linear_algebra/src/conjugate_gradient.py
@@ -1,33 +1,40 @@
 import numpy as np
 
+# https://en.wikipedia.org/wiki/Conjugate_gradient_method
+# https://en.wikipedia.org/wiki/Definite_symmetric_matrix
 
-def _is_matrix_spd(A: np.array) -> bool:
+
+def _is_matrix_spd(matrix: np.array) -> bool:
 
     """
-    Returns True if input matrix A is symmetric positive definite.
+    Returns True if input matrix is symmetric positive definite.
     Returns False otherwise.
 
     For a matrix to be SPD, all eigenvalues must be positive.
 
     >>> import numpy as np
-    >>> A = np.array([
+    >>> matrix = np.array([
     ... [4.12401784, -5.01453636, -0.63865857],
     ... [-5.01453636, 12.33347422, -3.40493586],
     ... [-0.63865857, -3.40493586,  5.78591885]])
-    >>> _is_matrix_spd(A)
+    >>> _is_matrix_spd(matrix)
     True
-    >>> A = np.array([
+    >>> matrix = np.array([
     ... [0.34634879,  1.96165514,  2.18277744],
     ... [0.74074469, -1.19648894, -1.34223498],
     ... [-0.7687067 ,  0.06018373, -1.16315631]])
-    >>> _is_matrix_spd(A)
+    >>> _is_matrix_spd(matrix)
     False
     """
     # Ensure matrix is square.
-    assert np.shape(A)[0] == np.shape(A)[1]
+    assert np.shape(matrix)[0] == np.shape(matrix)[1]
+
+    # If matrix not symmetric, exit right away.
+    if np.allclose(matrix, matrix.T) is False:
+        return False
 
     # Get eigenvalues and eignevectors for a symmetric matrix.
-    eigen_values, _ = np.linalg.eigh(A)
+    eigen_values, _ = np.linalg.eigh(matrix)
 
     # Check sign of all eigenvalues.
     return np.all(eigen_values > 0)
@@ -38,41 +45,41 @@ def _create_spd_matrix(N: np.int64) -> np.array:
     Returns a symmetric positive definite matrix given a dimension.
 
     Input:
-    N is an integer.
+    N gives the square matrix dimension.
 
     Output:
-    A is an NxN symmetric positive definite (SPD) matrix.
+    spd_matrix is an NxN symmetric positive definite (SPD) matrix.
 
     >>> import numpy as np
     >>> N = 3
-    >>> A = _create_spd_matrix(N)
-    >>> _is_matrix_spd(A)
+    >>> spd_matrix = _create_spd_matrix(N)
+    >>> _is_matrix_spd(spd_matrix)
     True
     """
 
-    A = np.random.randn(N, N)
-    A = np.dot(A, A.T)
+    random_matrix = np.random.randn(N, N)
+    spd_matrix = np.dot(random_matrix, random_matrix.T)
 
-    assert _is_matrix_spd(A)
+    assert _is_matrix_spd(spd_matrix)
 
-    return A
+    return spd_matrix
 
 
 def conjugate_gradient(
-    A: np.array, b: np.array, max_iterations=1000, tol=1e-8
+    spd_matrix: np.array, b: np.array, max_iterations=1000, tol=1e-8
 ) -> np.array:
     """
-    Returns solution to the linear system Ax = b.
+    Returns solution to the linear system np.dot(spd_matrix, x) = b.
 
     Input:
-    A is an NxN Symmetric Positive Definite (SPD) matrix.
-    b is an Nx1 vector.
+    spd_matrix is an NxN Symmetric Positive Definite (SPD) matrix.
+    b is an Nx1 vector that is the load vector.
 
     Output:
-    x is an Nx1 vector.
+    x is an Nx1 vector that is the solution vector.
 
     >>> import numpy as np
-    >>> A = np.array([
+    >>> spd_matrix = np.array([
     ... [8.73256573, -5.02034289, -2.68709226],
     ... [-5.02034289,  3.78188322,  0.91980451],
     ... [-2.68709226,  0.91980451,  1.94746467]])
@@ -80,15 +87,15 @@ def conjugate_gradient(
     ... [-5.80872761],
     ... [ 3.23807431],
     ... [ 1.95381422]])
-    >>> conjugate_gradient(A,b)
+    >>> conjugate_gradient(spd_matrix,b)
     array([[-0.63114139],
            [-0.01561498],
            [ 0.13979294]])
     """
     # Ensure proper dimensionality.
-    assert np.shape(A)[0] == np.shape(A)[1]
-    assert np.shape(b)[0] == np.shape(A)[0]
-    assert _is_matrix_spd(A)
+    assert np.shape(spd_matrix)[0] == np.shape(spd_matrix)[1]
+    assert np.shape(b)[0] == np.shape(spd_matrix)[0]
+    assert _is_matrix_spd(spd_matrix)
 
     N = np.shape(b)[0]
 
@@ -108,7 +115,7 @@ def conjugate_gradient(
     while error > tol:
 
         # Save this value so we only calculate the matrix-vector product once.
-        w = np.dot(A, p0)
+        w = np.dot(spd_matrix, p0)
 
         # The main algorithm.
 
@@ -147,15 +154,15 @@ def test_conjugate_gradient() -> None:
 
     # Create linear system with SPD matrix and known solution x_true.
     N = 3
-    A = _create_spd_matrix(N)
+    spd_matrix = _create_spd_matrix(N)
     x_true = np.random.randn(N, 1)
-    b = np.dot(A, x_true)
+    b = np.dot(spd_matrix, x_true)
 
     # Numpy solution.
-    x_numpy = np.linalg.solve(A, b)
+    x_numpy = np.linalg.solve(spd_matrix, b)
 
     # Our implementation.
-    x_conjugate_gradient = conjugate_gradient(A, b)
+    x_conjugate_gradient = conjugate_gradient(spd_matrix, b)
 
     # Ensure both solutions are close to x_true (and therefore one another).
     assert np.linalg.norm(x_numpy - x_true) <= 1e-6
