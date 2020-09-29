3e929232e2e026786d706ed0678e560df72598cc
Christian Clauss <cclauss@me.com>
2020-09-29 15:47:14 +0200

    Add doctests

--- web_programming/instagram_crawler.py
+++ web_programming/instagram_crawler.py
@@ -1,167 +1,138 @@
 #!/usr/bin/env python3
+from __future__ import annotations
+
+import json
+
 import requests
 from bs4 import BeautifulSoup
-import json
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) "
     "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
 }
 
-# Usage
-"""
->>> user = Instagram("github")
->>> user.is_verified
-True
->>> user.get_biography
-Built for developers.
 
-"""
+def extract_user_profile(script) -> dict:
+    """
+    May raise json.decoder.JSONDecodeError
+    """
+    data = script.contents[0]
+    info = json.loads(data[data.find('{"config"') : -1])
+    return info["entry_data"]["ProfilePage"][0]["graphql"]["user"]
 
 
 class InstagramUser:
     """
     Class Instagram crawl instagram user information
+
+    Usage:
+    >>> instagram_user = InstagramUser("github")
+    >>> instagram_user.is_verified
+    True
+    >>> instagram_user.biography
+    'Built for developers.'
     """
 
     def __init__(self, username):
-        self.username = username
         self.url = f"https://www.instagram.com/{username}/"
+        self.user_data = self.get_json()
 
     def get_json(self) -> dict:
         """
-        return json of user information
+        Return a dict of user information
         """
-
-        html = requests.get(self.url, headers=headers)
-        soup = BeautifulSoup(html.text, "html.parser")
+        html = requests.get(self.url, headers=headers).text
+        scripts = BeautifulSoup(html, "html.parser").find_all("script")
         try:
-            return html_1(soup)
+            return extract_user_profile(scripts[4])
         except json.decoder.JSONDecodeError:
-            return html_2(soup)
+            return extract_user_profile(scripts[3])
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.username})"
+
+    def __str__(self) -> str:
+        return f"Instagram user {self.fullname} ({self.username}) is {self.biography}"
 
     @property
-    def number_of_followers(self) -> int:
-        """
-        return number of followers
-        """
-
-        info = self.get_json()
-        return info["edge_followed_by"]["count"]
-
-    @property
-    def no_of_followings(self) -> int:
-        """
-        return number of followings
-        """
-
-        info = self.get_json()
-        return info["edge_follow"]["count"]
-
-    @property
-    def no_of_posts(self) -> int:
-        """
-        return number of posts
-        """
-
-        info = self.get_json()
-        return info["edge_owner_to_timeline_media"]["count"]
-
-    @property
-    def biography(self) -> str:
-        """
-        return biography of user
-        """
-
-        info = self.get_json()
-        return info["biography"]
+    def username(self) -> str:
+        return self.user_data["username"]
 
     @property
     def fullname(self) -> str:
-        """
-        return fullname of the user
-        """
-
-        info = self.get_json()
-        return info["full_name"]
+        return self.user_data["full_name"]
 
     @property
-    def get_username(self) -> str:
-        """
-        return the username of the user
-        """
+    def biography(self) -> str:
+        return self.user_data["biography"]
 
-        info = self.get_json()
-        return info["username"]
+    @property
+    def email(self) -> str:
+        return self.user_data["business_email"]
+
+    @property
+    def website(self) -> str:
+        return self.user_data["external_url"]
+
+    @property
+    def number_of_followers(self) -> int:
+        return self.user_data["edge_followed_by"]["count"]
+
+    @property
+    def number_of_followings(self) -> int:
+        return self.user_data["edge_follow"]["count"]
+
+    @property
+    def number_of_posts(self) -> int:
+        return self.user_data["edge_owner_to_timeline_media"]["count"]
 
     @property
     def profile_picture_url(self) -> str:
-        """
-        return the link of profile picture
-        """
-
-        info = self.get_json()
-        return info["profile_pic_url_hd"]
-
-    @property
-    def get_website(self) -> str:
-        """
-        return the users's website link
-        """
-
-        info = self.get_json()
-        return info["external_url"]
-
-    @property
-    def get_email(self) -> str:
-        """
-        return the email id of user if
-        available
-        """
-
-        info = self.get_json()
-        return info["business_email"]
+        return self.user_data["profile_pic_url_hd"]
 
     @property
     def is_verified(self) -> bool:
-        """
-        check the user is verified
-        """
-
-        info = self.get_json()
-        return info["is_verified"]
+        return self.user_data["is_verified"]
 
     @property
     def is_private(self) -> bool:
-        """
-        check user is private
-        """
-
-        info = self.get_json()
-        return info["is_private"]
+        return self.user_data["is_private"]
 
 
-def html_1(soup):
-    scripts = soup.find_all("script")
-    main_scripts = scripts[4]
-    data = main_scripts.contents[0]
-    info_object = data[data.find('{"config"') : -1]
-    info = json.loads(info_object)
-    info = info["entry_data"]["ProfilePage"][0]["graphql"]["user"]
-    return info
-
-
-def html_2(soup):
-    scripts = soup.find_all("script")
-    main_scripts = scripts[3]
-    data = main_scripts.contents[0]
-    info_object = data[data.find('{"config"') : -1]
-    info = json.loads(info_object)
-    info = info["entry_data"]["ProfilePage"][0]["graphql"]["user"]
-    return info
+def test_instagram_user(username: str = "github") -> None:
+    """
+    A self running doctest
+    >>> test_instagram_user()
+    """
+    instagram_user = InstagramUser(username)
+    assert instagram_user.user_data
+    assert isinstance(instagram_user.user_data, dict)
+    assert instagram_user.username == username
+    if username != "github":
+        return
+    assert instagram_user.fullname == "GitHub"
+    assert instagram_user.biography == "Built for developers."
+    assert instagram_user.number_of_posts > 150
+    assert instagram_user.number_of_followers > 120000
+    assert instagram_user.number_of_followings > 15
+    assert instagram_user.email == "support@github.com"
+    assert instagram_user.website == "https://github.com/readme"
+    assert instagram_user.profile_picture_url.startswith("https://instagram.")
+    assert instagram_user.is_verified is True
+    assert instagram_user.is_private is False
 
 
 if __name__ == "__main__":
-    user = InstagramUser("github")
-    print(f"{user.is_verified = }")
-    print(f"{user.get_biography = }")
+    import doctest
+
+    doctest.testmod()
+    instagram_user = InstagramUser("github")
+    print(instagram_user)
+    print(f"{instagram_user.number_of_posts = }")
+    print(f"{instagram_user.number_of_followers = }")
+    print(f"{instagram_user.number_of_followings = }")
+    print(f"{instagram_user.email = }")
+    print(f"{instagram_user.website = }")
+    print(f"{instagram_user.profile_picture_url = }")
+    print(f"{instagram_user.is_verified = }")
+    print(f"{instagram_user.is_private = }")
