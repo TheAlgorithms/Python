75a5782a69a499c25ad1523e15dc327b2cc627b5
Ajay Singh Panwar <64411148+AjaySinghPanwar@users.noreply.github.com>
2020-10-01 00:46:10 +0530

    Update Handling_Imabalanced_Data.py

--- machine_learning/Handling_Imabalanced_Data.py
+++ machine_learning/Handling_Imabalanced_Data.py
@@ -17,11 +17,7 @@ from collections import Counter
 
 
 # Creating the dataset
-X, y = make_classification(n_classes=2, class_sep=2,
-                 weights=[0.1, 0.9], n_informative=3, 
-                 n_redundant=1, flip_y=0,
-                 n_features=20, n_clusters_per_class=1, 
-                 n_samples=1000, random_state=10)
+X, y = make_classification(n_classes=2, class_sep=2, weights=[0.1, 0.9], n_informative=3, n_redundant=1, flip_y=0, n_features=20, n_clusters_per_class=1, n_samples=1000, random_state=10)
 
 print('Original dataset shape %s' % Counter(y))
 """
