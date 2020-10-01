8cd50101a83243e5c3ced3bb111842baac44bad5
YOGESHWARAN R <yogeshin247@gmail.com>
2020-10-01 17:34:53 +0530

    Create get_links_of_url.py
    Function get all links from the website

---
+++ web_programming/get_links_of_url.py
@@ -0,0 +1,46 @@
+import re
+
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import urllib.request
+
+
+def is_valid_url(url):
+    """
+    Funtion Vaild the url
+    """
+
+    regex = re.compile(
+        r"^(?:http|ftp)s?://"
+        r"""(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)
+        +(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|"""
+        r"localhost|"
+        r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"
+        r"(?::\d+)?"
+        r"(?:/?|[/?]\S+)$",
+        re.IGNORECASE,
+    )
+    return re.match(regex, url) is not None
+
+
+def get_links(url):
+    """
+    Funtion get all links in website
+    """
+
+    soup = BeautifulSoup(urllib.request.urlopen(url).read(), "html.parser")
+    data = [link.get("href") for link in soup.find_all("a")]
+    links = []
+    for link in data:
+        if not is_valid_url(link):
+            links.append(urljoin(url, link))
+        else:
+            links.append(link)
+    return links
+
+
+if __name__ == "__main__":
+    links = get_links("https://www.github.com")
+    print(f"Total {len(links)} are found")
+    for link in links:
+        print(link)
