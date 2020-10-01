570de226de000d002d0f2d0b03b67d18556ab19b
Samuel da Costa Araujo Nunes <samueldacostaaraujonunes@gmail.com>
2020-10-01 15:12:38 -0300

    Arrumando alguns detalhes!

--- maths/fibonacci_sequence_recursion_with_memoization .py
+++ maths/fibonacci_sequence_recursion_with_memoization .py
@@ -1,32 +1,29 @@
 # Fibonacci Sequence Using Recursion and Memoization
 
-cache = dict() #Cria um dicionário que guardará os resultados dos números já calculados para melhorar a perfomance.
-def recur_fibo(n):
+cache = dict() #Creates a dictionary that will store the results of the numbers already calculated to improve performance.
+def fibonacci(n):
     """
-    >>> [recur_fibo(i) for i in range(12)]
+    >>> [fibonacci(i) for i in range(12)]
     [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
     """
     aux = n
 
-    if aux in cache: #Verifica se este número já foi calculado anteriormente, para avaliar se é necessário calcular seu valor fibonacci novamente.
+    if aux in cache: #Checks if this number has already been calculated before, to assess whether it is necessary to calculate its fibonacci value again.
         return cache[aux] #Existindo, o seu valor fibonacci é retornado
 
-    if (n == 0) or (n == 1): #Caso pertença a um caso base, ele guarda o "n" em uma variavel auxiliar.("Ans -> Answer")
+    if (n == 0) or (n == 1): #If it belongs to a base case, it stores the "n" in an auxiliary variable.("Ans -> Answer")
         ans = n
     else:
-        ans = recur_fibo(n-1) + recur_fibo(n-2) #Caso não pertença a um caso base, ele faz uma chamada recursiva, guardando seu retorno na variavel "Ans"
-    cache[aux] = ans #A resposta é guardada no dicionário.
+        ans = fibonacci(n-1) + fibonacci(n-2) #If it does not belong to a base case, he makes a recursive call, keeping his return in the variable "Ans"
+    cache[aux] = ans #The answer is saved in the dictionary.
 
-    return ans    #E por fim, e retornado para o user.
+    return ans    #And finally, and returned to the user.
 
 
 def main() -> None:
-    limit = int(input("How many terms to include in fibonacci series: "))
-    if limit > 0:
-        print(f"The first {limit} terms of the fibonacci series are as follows:")
-        print([recur_fibo(n) for n in range(limit)])
-    else:
-        print("Please enter a positive integer: ")
+    lenght = int(input("Enter an integer greater than or equal to 0: "))
+    print(f"The first {lenght} Fibbonaci sequence terms are:")
+    print([fibonacci(n) for n in range(lenght)])
 
 
 if __name__ == "__main__":
