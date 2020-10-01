f80dad1e573ba13321e74778303a5da073eb1ce8
Samuel da Costa Araujo Nunes <samueldacostaaraujonunes@gmail.com>
2020-10-01 15:05:36 -0300

    Add DocTest

--- maths/fibonacci_sequence_recursion_with_memoization .py
+++ maths/fibonacci_sequence_recursion_with_memoization .py
@@ -1,13 +1,17 @@
 # Fibonacci Sequence Using Recursion and Memoization
 
-cache = dict() #Cria um dicionario que guardará os resultados dos numeros já calculados para melhorar a perfomance.
-def recur_fibo(n): 
+cache = dict() #Cria um dicionário que guardará os resultados dos números já calculados para melhorar a perfomance.
+def recur_fibo(n):
+    """
+    >>> [recur_fibo(i) for i in range(12)]
+    [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    """
     aux = n
 
-    if aux in cache: #Verifica se este número já foi calculado anteriormente, para avaliar se é necessario calcular seu valor fibonacci novamente.
+    if aux in cache: #Verifica se este número já foi calculado anteriormente, para avaliar se é necessário calcular seu valor fibonacci novamente.
         return cache[aux] #Existindo, o seu valor fibonacci é retornado
 
-    if (n == 0) or (n == 1): #Caso pertença a um caso base, ele guarda o "n" em uma varival auxiliar.("Ans -> Answer")
+    if (n == 0) or (n == 1): #Caso pertença a um caso base, ele guarda o "n" em uma variavel auxiliar.("Ans -> Answer")
         ans = n
     else:
         ans = recur_fibo(n-1) + recur_fibo(n-2) #Caso não pertença a um caso base, ele faz uma chamada recursiva, guardando seu retorno na variavel "Ans"
