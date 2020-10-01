baeeaf06a888e4c958aaaf73bd5821c07c1390d9
Vivek Patel <vicky.p8980@gmail.com>
2020-10-01 12:35:57 +0530

    updated file

--- bit_manipulation/binary_count_setbits.py
+++ bit_manipulation/binary_count_setbits.py
@@ -1,30 +1,30 @@
 
-def count_setbits(a: int) -> int:
+def binary_count_setbits(a: int) -> int:
     """
     Take in 1 integer, return a number that is
     the number of 1's in binary representation of that number.
 
-    >>> count_setbits(25)
+    >>> binary_count_setbits(25)
     3
-    >>> count_setbits(36)
+    >>> binary_count_setbits(36)
     2
-    >>> count_setbits(16)
+    >>> binary_count_setbits(16)
     1
-    >>> count_setbits(58)
+    >>> binary_count_setbits(58)
     4
-    >>> count_setbits(4294967295)
+    >>> binary_count_setbits(4294967295)
     32
-    >>> count_setbits(0)
+    >>> binary_count_setbits(0)
     0
-    >>> count_setbits(-10)
+    >>> binary_count_setbits(-10)
     Traceback (most recent call last):
         ...
     ValueError: Input value must be a positive integer
-    >>> count_setbits(0.8)
+    >>> binary_count_setbits(0.8)
     Traceback (most recent call last):
         ...
     TypeError: Input value must be a 'int' type
-    >>> count_setbits("0")
+    >>> binary_count_setbits("0")
     Traceback (most recent call last):
         ...
     TypeError: '<' not supported between instances of 'str' and 'int'
