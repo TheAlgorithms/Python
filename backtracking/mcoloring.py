b0ec835414337930e376b8975280949c20432354
optimistic-coder-001 <72167494+optimistic-coder-001@users.noreply.github.com>
2020-10-01 18:45:36 +0530

    updated m-coloring.py

--- backtracking/mcoloring.py
+++ backtracking/mcoloring.py
@@ -2,7 +2,7 @@ class Graph():
 
 	def __init__(self, vertices):
 		self.V = vertices
-		self.graph = [[0 for column in range(vertices)]\
+		self.graph = [[0 for column in range(vertices)]
 							for row in range(vertices)]
 
 	# A utility function to check if the current color assignment
