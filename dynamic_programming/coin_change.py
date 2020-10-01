19fa1f05f7b1d1d04bea93512fc9bb34af040e35
Mohit Kumar <mkay6123@gmail.com>
2020-10-01 23:03:11 +0530

    Update coin_change.py

--- dynamic_programming/coin_change.py
+++ dynamic_programming/coin_change.py
@@ -39,8 +39,10 @@ def dp_count(S, m, n):
 
 if __name__ == "__main__":
 
-    coins = [1, 2, 3, 4, 9]
-    m = len(coins)
-    sum = 15
+    m = int(input(f"\n Enter Total number of coins\n"))
+    print(f"\n Enter value of each coin in space separated way\n")
+
+    coins = list(map(int, input().split()))
+    sum = int(input(f"\n Enter required sum\n"))
 
     print(f"\n Total ways are {dp_count(coins, m, sum)} ")
