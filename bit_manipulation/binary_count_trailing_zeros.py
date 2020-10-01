4828ab8038ab51eacd815058af6c9053d3cae153
Vivek Patel <vicky.p8980@gmail.com>
2020-10-01 10:06:59 +0530

    updated binary_count_trailing_zeros.py file

--- bit_manipulation/binary_count_trailing_zeros.py
+++ bit_manipulation/binary_count_trailing_zeros.py
@@ -6,17 +6,17 @@ def binary_count_trailing_zeros(a: int):
     Take in 1 integer, return a number that is the number of trailing zeros in binary representation of that number.
 
     >>> binary_count_trailing_zeros(25)
-    '0'
+    0
     >>> binary_count_trailing_zeros(36)
-    '2'
+    2
     >>> binary_count_trailing_zeros(16)
-    '4'
+    4
     >>> binary_count_trailing_zeros(58)
-    '1'
+    1
     >>> binary_count_trailing_zeros(4294967296)
-    '32'
+    32
     >>> binary_count_trailing_zeros(0)
-    '0'
+    0
     >>> binary_count_trailing_zeros(-10)
     Traceback (most recent call last):
         ...
@@ -33,8 +33,7 @@ def binary_count_trailing_zeros(a: int):
     if a < 0:
         raise ValueError("the value of input must be positive")
 
-    # counting trailing zeros in O(1) time complexity
-    return (0 if (a == 0) else int(log2(a & -a)))
+    return 0 if (a == 0) else int(log2(a & -a))
 
 
 if __name__ == "__main__":
