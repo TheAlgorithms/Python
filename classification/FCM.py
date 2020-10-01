35c33a1135cb782bc7f9c24e60f46a34181e0d56
⎛⎝ L∆RBI ⎠⎞ <48409226+L4RBI@users.noreply.github.com>
2020-10-01 01:27:43 +0100

    Create FCM.py
    fuzzy c means algorithm

---
+++ classification/FCM.py
@@ -0,0 +1,51 @@
+from tools import *
+import datetime
+
+
+class FuzzyCMeans:
+    def __init__(self, n_clusters, initial_centers, histogram, max_iter=250, m=2, error=1e-5):
+        assert m > 1
+        #assert initial_centers.shape[0] == n_clusters
+        self.U = None
+        self.centers = initial_centers
+        self.max_iter = max_iter
+        self.m = m
+        self.error = error
+        self.histogram = histogram
+
+    def membership(self, histogram, centers):
+        U_temp = cdist(histogram, centers, 'euclidean')
+        U_temp = numpy.power(U_temp, 2/(self.m - 1))
+        denominator_ = U_temp.reshape(
+            (histogram.shape[0], 1, -1)).repeat(U_temp.shape[-1], axis=1)
+        denominator_ = U_temp[:, :, numpy.newaxis] / denominator_
+        return 1 / denominator_.sum(2)
+
+    def Centers(self, histogram, U):
+        um = U ** self.m
+        return (histogram.T @ um / numpy.sum(um, axis=0)).T
+
+    def newImage(self, U, centers, im):
+        best = numpy.argmax(self.U, axis=-1)
+        # print(best)
+        # numpy.round()
+        image = im.astype(int)
+        for i in range(256):
+            image = numpy.where(image == float(i), centers[best[i]][0], image)
+        return image
+
+    def compute(self):
+        self.U = self.membership(self.histogram, self.centers)
+
+        past_U = numpy.copy(self.U)
+        begin_time = datetime.datetime.now()
+        for i in range(self.max_iter):
+
+            self.centers = self.Centers(self.histogram, self.U)
+            self.U = self.membership(self.histogram, self.centers)
+
+            if norm(self.U - past_U) < self.error:
+                break
+            past_U = numpy.copy(self.U)
+        x = datetime.datetime.now() - begin_time
+        return self.centers, self.U, x
