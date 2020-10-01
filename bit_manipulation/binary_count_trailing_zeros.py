44446053fd6aee19dc9fed904faa0cd92a4c6840
Vivek Patel <vicky.p8980@gmail.com>
2020-10-01 11:44:57 +0530

    resolved pre-commit error

--- bit_manipulation/binary_count_trailing_zeros.py
+++ bit_manipulation/binary_count_trailing_zeros.py
@@ -34,7 +34,7 @@ def binary_count_trailing_zeros(a: int) -> int:
     if a < 0:
         raise ValueError("Input value must be a positive integer")
     elif isinstance(a, float):
-        raise TypeError("Input value must be a \'int\' type")
+        raise TypeError("Input value must be a 'int' type")
     return 0 if (a == 0) else int(log2(a & -a))
 
 
