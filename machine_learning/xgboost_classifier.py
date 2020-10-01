081e3ccdef57048f0f19890e368261bffe0c2279
Kabir Nagpal <21kabirnagpal@gmail.com>
2020-10-01 20:13:38 +0530

    Update xgboost_classifier.py

--- machine_learning/xgboost_classifier.py
+++ machine_learning/xgboost_classifier.py
@@ -1,7 +1,7 @@
 # XGboost Classifier Example
 from matplotlib import pyplot as plt
 from sklearn.datasets import load_iris
-from sklearn.metrics import plot_confusion_matrix
+from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 from xgboost import XGBClassifier
 
@@ -18,9 +18,7 @@ def main():
     # Split dataset into train and test data
     X = iris["data"]  # features
     Y = iris["target"]
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, Y, test_size=0.3, random_state=42
-    )
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
     
     model = XGBClassifier(silent=True,
                       booster = 'gbtree',
@@ -37,16 +35,9 @@ def main():
               eval_metric=eval_metric, 
               eval_set=eval_set)
     
-    plot_confusion_matrix(
-        model,
-        X_test,
-        y_test,
-        display_labels=iris["target_names"],
-        cmap="Blues",
-        normalize="true",
-    )
-    plt.title("Normalized Confusion Matrix - IRIS Dataset")
-    plt.show()
+    y_pred = model.predict(X_test)
+    
+    print(classification_report(y_test, y_pred))
 
 
 if __name__ == "__main__":
