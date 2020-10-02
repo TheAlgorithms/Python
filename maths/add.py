995b78af5422217d599876b74d2b3c785b6e6d03
CodeWithPython57 <68240991+CodeWithPython57@users.noreply.github.com>
2020-10-02 17:24:46 +0530

    Update add.py
    Algorithm for addition of n numbers. 
    N can be 2 or 6 or 600 .
    If you use string than you get Addition invalid error.

--- maths/add.py
+++ maths/add.py
@@ -1,19 +1,16 @@
 """
-Just to check
+Addition of n numbers.
 """
 
 
-def add(a, b):
-    """
-    >>> add(2, 2)
-    4
-    >>> add(2, -2)
-    0
-    """
-    return a + b
+def add(*a):
+    a=[*a]
+    try:
+        total=sum(a)
+        print('Sum is  ' + str(total))
+    except:
+        print('Addition valid for only numbers')
 
-
-if __name__ == "__main__":
-    a = 5
-    b = 6
-    print(f"The sum of {a} + {b} is {add(a, b)}")
+add(2,3) # output is 5
+add(5,3,2,1) # output is 11
+add(23,45,56,76,43,21) #output is 264.
