aa43c2627dcd80f4d7875e887470cb67d72d4dae
Ishan Sharma <51106690+Ishan7390@users.noreply.github.com>
2020-10-02 12:01:02 +0530

    Added solution of Problem 38 of Project Euler

--- project_euler/problem_38/sol1.py
+++ project_euler/problem_38/sol1.py
@@ -13,29 +13,37 @@ What is the largest 1 to 9 pandigital 9-digit number that can be formed as the c
 
 '''
 def is_pandigital():
-  '''
+	'''
   The function finds the largest pandigital number by looping from 1 to 1000, and concatinating the multiples untill a 9 digit number is achieved. It then using an if condition to check if the length of num1 is 9 without repititive digits and without the occurence of 0 in it.
   
   If these conditions hold true, it assigns num1 to the largest variable.
 
   This largest variable is the answer of problem 38.
-  '''
-  largest = 0 #this variable will be the largest 1 to 9 pandigital 9-digit number
-  for i in range(1,1000):
-    num1 = "" #a string is used and not int as we'll later be concatinating it to generate a 9 digital number.
-
-    num2 = 1 
-
-    while len(num1) < 9:
-      num1 += str(i*num2) #concatinating the multiples until num1 has 9 digits.
-      num2 += 1
-
-    if len(num1) == 9 and len(set(num1)) == 9 and 0 not in num1:
-
-      if int(num1) > largest:
-        largest = int(num1)
   
-  return largest
+  >>>is_pandigital()
+  932718654
+  '''
+	largest = 0 #this variable will be the largest 1 to 9 pandigital number
+
+	for i in range(1,10000):
+	  
+	  num1 = '' #a string is used and not int as we'll later be concatinating it to generate a 9 digital number.
+    
+	  num2 = 1
+	  
+	  while len(num1) < 9:
+      
+	    num1 += str(i*num2) #concatinating the multiples until num1 has 9 digits.
+	    
+	    num2 += 1
+	    
+	  if ((len(num1) == 9) and (len(set(num1)) == 9) #to check if all 1-9 digits are present and 0 is not in the number.
+	    and ('0' not in num1)):
+	  
+	    if int(num1) > largest:
+	      largest = int(num1)
+        
+	return largest
 
 if __name__ == "__main__":
-    print(f"The largest pandigital number is {is_pandigital()}")
+    print(f'The largest pandigital number is {is_pandigital()}')
