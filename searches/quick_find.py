2fb75b87ff82029749124fbccb703f4afe6ff6c3
fisher60 <wakefollower@gmail.com>
2020-10-01 18:29:22 -0700

    fix test again

--- searches/quick_find.py
+++ searches/quick_find.py
@@ -68,7 +68,7 @@ class QuickUnion:
 def run_test(n=None, connections=None):
     """
     >>> run_test(connections=[(1, 1), (3, 4), (0, 1), (4, 2), (4, 3)])
-    [1, 1, 2, 2, 2]
+    '[1, 1, 2, 2, 2]'
     """
     test = QuickUnion(n=n, connections=connections)
     for connection in test.connections:
