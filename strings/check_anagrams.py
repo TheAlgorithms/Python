138acedd0eeb229fc27a7abf29163e42e614b45b
pointer-04 <59029087+pointer-04@users.noreply.github.com>
2020-10-02 17:09:22 +0530

    Update check_anagrams.py

--- strings/check_anagrams.py
+++ strings/check_anagrams.py
@@ -21,6 +21,43 @@ def check_anagrams(first_str: str, second_str: str) -> bool:
         == "".join(sorted(second_str.lower())).strip()
     )
 
+def checkAnagramsAlternate(first_str: str, second_str: str) -> bool:
+    '''
+    Strategy is to:
+    (a) first check whether the length of both of the srings are same. If not, they can never be anagrams.
+    (b) second check to make is whether all the characters in both the strings occur the same number of times
+    
+    Case 1: SILENT and LIST
+    >>> not anagrams because (a) failed
+    Case 2: SILENT and LISTED
+    >>> not anagrams because (b) failed
+    Case 3: SILENT and LISTEN 
+    >>> anagrams because both (a) and (b) passed
+    
+    We will be using a hashmap to keep track of the number of letters of each type
+    '''
+    if len(first_str)!=len(second_str):
+        return False
+    letterCount=dict()
+    #traversing through first string and storing the count of different variables
+    for letter in first_str:
+        if letter not in letterCount:
+            letterCount[letter]=1
+        else:
+            letterCount[letter]+=1
+    #traversing through second string and checking whether the letters and letter counts match
+    for letter in second_str:
+        if letter not in letterCount:
+            return False
+        else:
+            letterCount[letter]-=1
+    #traversing through the hashmap to check if all the entries have a value=0
+    for entry in letterCount:
+        if letterCount[entry]!=0:
+            return False
+        else:
+            continue
+    return True
 
 if __name__ == "__main__":
     from doctest import testmod
@@ -29,5 +66,7 @@ if __name__ == "__main__":
     input_A = input("Enter the first string ").strip()
     input_B = input("Enter the second string ").strip()
 
-    status = check_anagrams(input_A, input_B)
+    status1 = check_anagrams(input_A, input_B)
     print(f"{input_A} and {input_B} are {'' if status else 'not '}anagrams.")
+    status2 = checkAnagramsAlternate(input_A,input_B)
+    print(status2)
