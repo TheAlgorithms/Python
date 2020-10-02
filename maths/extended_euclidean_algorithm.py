478b0ac50ea5c8d014723f1cc0816ff8500c9d65
Joyce <jycyeo@yahoo.com.sg>
2020-10-02 14:16:38 +0800

    change algorithm for negative numbers

--- maths/extended_euclidean_algorithm.py
+++ maths/extended_euclidean_algorithm.py
@@ -3,18 +3,42 @@ Extended Euclidean Algorithm.
 
 Finds 2 numbers a and b such that it satisfies
 the equation am + bn = gcd(m, n) (a.k.a Bezout's Identity)
+
+https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
 """
 
 # @Author: S. Sharma <silentcat>
 # @Date:   2019-02-25T12:08:53-06:00
 # @Email:  silentcat@protonmail.com
-# @Last modified by:   PatOnTheBack
-# @Last modified time: 2019-07-05
+# @Last modified by:   pikulet
+# @Last modified time: 2020-10-02
 
 import sys
 
 
-def extended_euclidean_algorithm(m: int, n: int) -> (int, int):
+def sign(n: int) -> int:
+    """
+    Returns sign of number for correction of negative numbers in algorithm
+
+    >>> sign(4)
+    1
+
+    >>> sign(0)
+    0
+
+    >>> sign(-8)
+    -1
+
+    """
+    if n > 0:
+        return 1
+    elif n < 0:
+        return -1
+    else:
+        return 0
+
+
+def extended_euclidean_algorithm(a: int, b: int) -> (int, int):
     """
     Extended Euclidean Algorithm.
 
@@ -26,42 +50,40 @@ def extended_euclidean_algorithm(m: int, n: int) -> (int, int):
 
     >>> extended_euclidean_algorithm(8, 14)
     (2, -1)
+
+    >>> extended_euclidean_algorithm(240, 46)
+    (-9, 47)
+
+    >>> extended_euclidean_algorithm(1, -4)
+    (1, 0)
+
+    >>> extended_euclidean_algorithm(-2, -4)
+    (-1, 0)
+
+    >>> extended_euclidean_algorithm(0, -4)
+    (0, -1)
+
+    >>> extended_euclidean_algorithm(2, 0)
+    (1, 0)
+
     """
-    a = 0
-    a_prime = 1
-    b = 1
-    b_prime = 0
-    q = 0
-    r = 0
-    if m > n:
-        c = m
-        d = n
+    old_r, r = a, b
+    old_s, s = 1, 0
+    old_t, t = 0, 1
+
+    while r != 0:
+        quotient = old_r // r
+        old_r, r = r, old_r - quotient * r
+        old_s, s = s, old_s - quotient * s
+        old_t, t = t, old_t - quotient * t
+
+    # sign correction for negative numbers
+    if abs(a) == 1:
+        return sign(a), 0
+    elif abs(b) == 1:
+        return 0, sign(b)
     else:
-        c = n
-        d = m
-
-    while True:
-        q = int(c / d)
-        r = c % d
-        if r == 0:
-            break
-        c = d
-        d = r
-
-        t = a_prime
-        a_prime = a
-        a = t - q * a
-
-        t = b_prime
-        b_prime = b
-        b = t - q * b
-
-    pair = None
-    if m > n:
-        pair = (a, b)
-    else:
-        pair = (b, a)
-    return pair
+        return (sign(a) * old_s, sign(b) * old_t)
 
 
 def main():
@@ -69,9 +91,9 @@ def main():
     if len(sys.argv) < 3:
         print("2 integer arguments required")
         exit(1)
-    m = int(sys.argv[1])
-    n = int(sys.argv[2])
-    print(extended_euclidean_algorithm(m, n))
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
+    print(extended_euclidean_algorithm(a, b))
 
 
 if __name__ == "__main__":
