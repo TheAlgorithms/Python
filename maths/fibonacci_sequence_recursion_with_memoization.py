82364bab36a6a67b6f4041ccdbfc4177fd49fadc
Samuel da Costa Araujo Nunes <samueldacostaaraujonunes@gmail.com>
2020-10-02 12:43:51 -0300

    Correções

--- maths/fibonacci_sequence_recursion_with_memoization.py
+++ maths/fibonacci_sequence_recursion_with_memoization.py
@@ -6,30 +6,42 @@
 cache = dict()
 
 
-def fibonacci(n):
+def fibonacci(n: int) -> int:
     """
     Return number of index n in sequence fibonacci
     >>> [fibonacci(i) for i in range(12)]
     [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    >>> fibonacci(12)
+    89
     """
     aux = n
 
-    if aux in cache:
-        return cache[aux]
+    try:
+        if aux in cache:
+            return cache[aux]
 
-    if (n == 0) or (n == 1):
-        ans = n
-    else:
-        ans = fibonacci(n - 1) + fibonacci(n - 2)
-    cache[aux] = ans
+        if n in {0, 1}:
+            ans = n
+        else:
+            ans = fibonacci(n - 1) + fibonacci(n - 2)
+        cache[aux] = ans
 
-    return ans
+        return ans
+    except TypeError:
+        print("Error: An integer value is expected")
+    except ValueError:
+        print("Error: The value must be greater than or equal to zero")
+
 
 
 def main() -> None:
-    number = int(input("Enter an integer greater than or equal to 0: "))
-    print(f"The first {number} Fibonacci sequence terms are:")
-    print([fibonacci(n) for n in range(number)])
+    try:
+        number = int(input("Enter an integer greater than or equal to 0: "))
+        print(f"The first {number} Fibonacci sequence terms are:")
+        print([fibonacci(n) for n in range(number)])
+        print(f"The {number} index of the fibonacci sequence corresponds to {fibonacci(number)}")
+    except ValueError:
+        print("Error: Input value should be an integer greater than or equal to 0")
 
 
 if __name__ == "__main__":
