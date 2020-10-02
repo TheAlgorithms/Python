e304a74a98fb20b49404d881d70bea1e84331936
MANYAJAIN195 <manyajain195@gmail.com>
2020-10-03 02:05:11 +0530

    Issue #2661 fixed redo

--- maths/add.py
+++ maths/add.py
@@ -1,43 +1,40 @@
-ans="yes"
-l1=[]
-Sum=i=0
+'''
+Find the sum of n numbers
+'''
+def add():
+	ans="y"
+    list1=[]
+    sum1=i=0
 '''
 Enter n numbers
 '''
-while ans=="yes":
-  '''
-  check for invalid input
-  '''	
-  try:
-    n=int(input("Enter the number :"))
-    l1.append(n)      #adding elements to the list
-  except:
-    i+=1
-    if i>=1:
-        print("Enter a valid integer !")
-        continue
+    while ans=="y":
+'''
+check for invalid input
+'''	
+      try:
+        num=int(input("Enter the number :").strip())
+        list1.append(num)      #adding elements to the list
+      except:
+        i+=1
+        if i>=1:
+            print("Enter a valid integer !")
+            continue
+        try:
+          print("Enter a valid integer !")  
+          num=int(input("Enter the number :").strip())
+          list1.append(num)    #adding elements to the list
+        except:
+            continue
     
-    try:
-      print("Enter a valid integer !")  
-      n=int(input("Enter the number :"))
-      l1.append(n)    #adding elements to the list
-    except:
-        continue
     
-    
-  ans=input("Do you want to add more numbers? (yes/no) :")
+      ans=input("Do you want to add more numbers? (y/n) :").strip()
 '''
 finding the sum n numbers
 '''
-for i in range(0,len(l1)):
-  Sum+=l1[i]
-print("Sum of numbers is :",Sum)
+    for i in range(0,len(list1)):
+      sum1+=list1[i]
+    print("Sum of numbers is :",sum1)
 
-        
-    
-
-
-
-
-
-
+if __name__=="__main__":
+	add()
