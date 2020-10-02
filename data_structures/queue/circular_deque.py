d4b5775b5521791f3d6c8da6af44f5d6b7659196
Siddharth Choudhary <60484636+siddharthzs@users.noreply.github.com>
2020-10-03 03:20:29 +0530

    fixed black error

--- data_structures/queue/circular_deque.py
+++ data_structures/queue/circular_deque.py
@@ -1,92 +1,86 @@
 class MyCircularDeque:
     """
-    MyCircularDeque circularDeque = new MycircularDeque(3); // set the size to be 3
-    circularDeque.insertLast(1);			// return true
-    circularDeque.insertLast(2);			// return true
-    circularDeque.insertFront(3);			// return true
-    circularDeque.insertFront(4);			// return false, the queue is full
-    circularDeque.getRear();  			// return 2
-    circularDeque.isFull();				// return true
-    circularDeque.deleteLast();			// return true
-    circularDeque.insertFront(4);			// return true
-    circularDeque.getFront();			// return 4
+    Doubly-linklist implementation of Deque
+    Deque is a double ended queue which can
+    insert, remove and get in front element in: O(1)
+    insert, remove and get in back element in: O(1)
     """
 
-    class __Node:
+    class Node:
         def __init__(self, value):
             self.value = value
             self.previous = self.next = None
 
     def __init__(self, k: int):
-        self.__capacity = k
-        self.__size = 0
-        self.__dummyRoot = self.__Node(None)
-        self.__connect(self.__dummyRoot, self.__dummyRoot)
+        self.capacity = k
+        self.size = 0
+        self.dummyRoot = self.Node(None)
+        self.connect(self.dummyRoot, self.dummyRoot)
 
     def insertFront(self, value: int) -> bool:
         if self.isFull():
             return False
 
-        node = self.__Node(value)
-        self.__connect(node, self.__dummyRoot.next)
-        self.__connect(self.__dummyRoot, node)
-        self.__size += 1
+        node = self.Node(value)
+        self.connect(node, self.dummyRoot.next)
+        self.connect(self.dummyRoot, node)
+        self.size += 1
         return True
 
     def insertLast(self, value: int) -> bool:
         if self.isFull():
             return False
 
-        node = self.__Node(value)
-        self.__connect(self.__dummyRoot.previous, node)
-        self.__connect(node, self.__dummyRoot)
-        self.__size += 1
+        node = self.Node(value)
+        self.connect(self.dummyRoot.previous, node)
+        self.connect(node, self.dummyRoot)
+        self.size += 1
         return True
 
     def deleteFront(self) -> bool:
         if self.isEmpty():
             return False
 
-        self.__connect(self.__dummyRoot, self.__dummyRoot.next.next)
-        self.__size -= 1
+        self.connect(self.dummyRoot, self.dummyRoot.next.next)
+        self.size -= 1
         return True
 
     def deleteLast(self) -> bool:
         if self.isEmpty():
             return False
 
-        self.__connect(self.__dummyRoot.previous.previous, self.__dummyRoot)
-        self.__size -= 1
+        self.connect(self.dummyRoot.previous.previous, self.dummyRoot)
+        self.size -= 1
         return True
 
     def getFront(self) -> int:
         if self.isEmpty():
             return -1
-        return self.__dummyRoot.next.value
+        return self.dummyRoot.next.value
 
     def getRear(self) -> int:
         if self.isEmpty():
             return -1
-        return self.__dummyRoot.previous.value
+        return self.dummyRoot.previous.value
 
     def isEmpty(self) -> bool:
-        return self.__size == 0
+        return self.size == 0
 
     def isFull(self) -> bool:
-        return self.__size == self.__capacity
+        return self.size == self.capacity
 
-    def __connect(self, node: __Node, following: __Node) -> None:
+    def connect(self, node: Node, following: Node) -> None:
         node.next = following
         following.previous = node
 
 
-circularDeque = MyCircularDeque(3)   # set the size to be 3
-print(circularDeque.insertLast(1))	 # return true
-print(circularDeque.insertLast(2))	 # return true
-print(circularDeque.insertFront(3))	 # return true
-print(circularDeque.insertFront(4))	 # return false, the queue is full
-print(circularDeque.getRear()) 		 # return 2
-print(circularDeque.isFull())		 # return true
-print(circularDeque.deleteLast())  	 # return true
-print(circularDeque.insertFront(4))  # return true
-print(circularDeque.getFront()) 	 # return 4
+circularDeque = MyCircularDeque(3)
+print(circularDeque.insertLast(1))
+print(circularDeque.insertLast(2))
+print(circularDeque.insertFront(3))
+print(circularDeque.insertFront(4))
+print(circularDeque.getRear())
+print(circularDeque.isFull())
+print(circularDeque.deleteLast())
+print(circularDeque.insertFront(4))
+print(circularDeque.getFront())
