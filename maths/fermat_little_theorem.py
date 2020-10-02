31467b1a0ff54b02f8829894a005a473eeff71ce
Joyce <jycyeo@yahoo.com.sg>
2020-10-02 15:36:37 +0800

    update proof statement

--- maths/fermat_little_theorem.py
+++ maths/fermat_little_theorem.py
@@ -1,12 +1,24 @@
-# Python program to show the usage of Fermat's little theorem in a division
-# According to Fermat's little theorem, (a / b) mod p always equals
-# a * (b ^ (p - 2)) mod p
-# Here we assume that p is a prime number, b divides a, and p doesn't divide b
-# Wikipedia reference: https://en.wikipedia.org/wiki/Fermat%27s_little_theorem
+"""
+Python program to show the usage of Fermat's little theorem in a division
+
+If p is a prime number, then given any integer a,
+(a raised to the power of p) is congruent to (a) modulo the prime p.
+
+Wikipedia reference: https://en.wikipedia.org/wiki/Fermat%27s_little_theorem
+"""
 
 
-def binary_exponentiation(a, n, mod):
+def binary_exponentiation(a: int, n: int, mod: int) -> int:
+    """
+    Find a to the power of n, modulo mod.
 
+    >>> binary_exponentiation(4, 3, 7)
+    1
+
+    >>> binary_exponentiation(2, 9, 9)
+    8
+
+    """
     if n == 0:
         return 1
 
@@ -18,14 +30,25 @@ def binary_exponentiation(a, n, mod):
         return (b * b) % mod
 
 
-# a prime number
-p = 701
+def check_fermat_little_thm(a: int, p: int) -> bool:
+    """
+    Verifies the fermat little theorem
 
-a = 1000000000
-b = 10
+    # Example prime: 13
+    >>> check_fermat_little_thm(400, 13)
+    True
 
-# using binary exponentiation function, O(log(p)):
-print((a / b) % p == (a * binary_exponentiation(b, p - 2, p)) % p)
+    >>> check_fermat_little_thm(22, 13)
+    True
 
-# using Python operators:
-print((a / b) % p == (a * b ** (p - 2)) % p)
+    # Example prime: 701
+    >>> check_fermat_little_thm(13, 701)
+    True
+
+    >>> check_fermat_little_thm(22, 701)
+    True
+    """
+    a_power_p = binary_exponentiation(a, p, p)
+    a_mod_p = binary_exponentiation(a, 1, p)  # or just a % p
+
+    return a_power_p == a_mod_p
