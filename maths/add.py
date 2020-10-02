c3ff3d9dce074391a37199d2bdc561b2311a9ffe
MANYAJAIN195 <manyajain195@gmail.com>
2020-10-03 00:35:43 +0530

    Issue #2661 fixed

--- maths/add.py
+++ maths/add.py
@@ -1,19 +1,34 @@
-"""
-Just to check
-"""
+ans="yes"
+l1=[]
+Sum=i=0
+while ans=="yes":
+  try:
+    n=int(input("Enter the number :"))
+    l1.append(n)
+  except:
+    i+=1
+    if i>=1:
+        print("Enter a valid integer !")
+        continue
+    
+    try:
+      print("Enter a valid integer !")  
+      n=int(input("Enter the number :"))
+      l1.append(n)
+    except:
+        continue
+    
+    
+  ans=input("Do you want to add more numbers? (yes/no) :")
+for i in range(0,len(l1)):
+  Sum+=l1[i]
+print("Sum of numbers is :",Sum)
+
+        
+    
 
 
-def add(a, b):
-    """
-    >>> add(2, 2)
-    4
-    >>> add(2, -2)
-    0
-    """
-    return a + b
 
 
-if __name__ == "__main__":
-    a = 5
-    b = 6
-    print(f"The sum of {a} + {b} is {add(a, b)}")
+
+
