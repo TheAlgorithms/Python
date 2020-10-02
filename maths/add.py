e0087bfc8ec4480cf66d615bcca093d8758844cc
MANYAJAIN195 <manyajain195@gmail.com>
2020-10-03 01:03:36 +0530

    Added comments to add.py

--- maths/add.py
+++ maths/add.py
@@ -1,10 +1,16 @@
 ans="yes"
 l1=[]
 Sum=i=0
+'''
+Enter n numbers
+'''
 while ans=="yes":
+  '''
+  check for invalid input
+  '''	
   try:
     n=int(input("Enter the number :"))
-    l1.append(n)
+    l1.append(n)      #adding elements to the list
   except:
     i+=1
     if i>=1:
@@ -14,12 +20,15 @@ while ans=="yes":
     try:
       print("Enter a valid integer !")  
       n=int(input("Enter the number :"))
-      l1.append(n)
+      l1.append(n)    #adding elements to the list
     except:
         continue
     
     
   ans=input("Do you want to add more numbers? (yes/no) :")
+'''
+finding the sum n numbers
+'''
 for i in range(0,len(l1)):
   Sum+=l1[i]
 print("Sum of numbers is :",Sum)
