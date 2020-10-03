5118372d94534f9c80b075f3be951d4efe5e71c5
Shouvik-99 <72292341+Shouvik-99@users.noreply.github.com>
2020-10-03 23:52:55 +0530

    brute_force.py
    my_string_op.py

--- strings/is_palindrome.py
+++ strings/is_palindrome.py
@@ -1,3 +1,4 @@
+import time
 def is_palindrome(s: str) -> bool:
     """
     Determine whether the string is palindrome
@@ -16,9 +17,29 @@ def is_palindrome(s: str) -> bool:
     """
     # Since Punctuation, capitalization, and spaces are usually ignored while checking
     # Palindrome,  we first remove them from our string.
+    t1=time.time()
     s = "".join([character for character in s.lower() if character.isalnum()])
+    t2=time.time()
+    print(t2-t1)
     return s == s[::-1]
 
+
+def my_palindrome(s):
+    t1=time.time()
+    s=s.lower()
+    s1=''
+    for i in s:
+        if ord(i)>=97 and ord(i)<=122:
+            s1+=i
+    s2=''
+    for i in range(len(s1)-1,-1,-1):
+        s2+=s1[i]
+    t2=time.time()
+    print(t2-t1)
+    if s2==s1:
+        print('Given string is palindrome')
+    else:
+        print('Given string is not palindrome')
 
 if __name__ == "__main__":
     s = input("Enter string to determine whether its palindrome or not: ").strip()
@@ -26,3 +47,14 @@ if __name__ == "__main__":
         print("Given string is palindrome")
     else:
         print("Given string is not palindrome")
+        
+ 
+    my_palindrome(s)
+    
+    
+    '''
+    Brute Force approach:
+                   with same time complexity(O(n)),
+                   well understandable,
+                   Same Execution Time.
+   '''
