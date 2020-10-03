769d907cc1c96b21db3a4dad6b880a8ec93f0977
jck <ashishdwivedi086@gmail.com>
2020-10-03 17:56:16 +0530

    update problem_58 in project_euler

--- project_euler/problem_58/sol1.py
+++ project_euler/problem_58/sol1.py
@@ -8,32 +8,44 @@ def is_prime(n):
     return True
 
 
-i = 0   # iterator for the numbers
 
-gap = 1   # gap for every four iterations
+def solution():
+    i = 0   # iterator for the numbers
 
-ratio = 1 # ratio of lenght of primes to length of diagonals
+    gap = 1   # gap for every four iterations
 
-primes = []  # prime numbers on the diagonal
+    ratio = 1 # ratio of length of primes to length of diagonals
 
-all_numbers = [1]  # all the numbers, including 1
+    primes = []  # prime numbers on the diagonal
 
-# While loop, to loop until we reach the last number
-while ratio > 0.1:
-    for j in range(4):
-        # generating the value of n for 2n+1
-        i += gap
-        # generating the odd number
-        present_number = 2*i + 1
-        # appending the number to all_numbers
-        all_numbers.append(present_number)
-        # appending to prime if the number is prime
-        if is_prime(present_number):
-            primes.append(2*i + 1)
-    # changing the value of ratio at the end of loop
-    ratio = float(len(primes))/len(all_numbers)
-    # increasing the gap after every 4 numbers
-    gap += 1
+    all_numbers = [1]  # all the numbers, including 1
+
+    # While loop, to loop until we reach the last number
+    while ratio > 0.1:
+        for j in range(4):
+            # generating the value of n for 2n+1
+            i += gap
+            # generating the odd number
+            present_number = 2*i + 1
+            # appending the number to all_numbers
+            all_numbers.append(present_number)
+            # appending to prime if the number is prime
+            if is_prime(present_number):
+                primes.append(2*i + 1)
+        # changing the value of ratio at the end of loop
+        ratio = float(len(primes))/len(all_numbers)
+        # increasing the gap after every 4 numbers
+        gap += 1
     
+    return int((2*i+1)**0.5)
+
+
 if __name__ == "__main__":
-    print( int((2*i+1)**0.5))
+    from doctest import testmod
+    testmod()
+    print(solution())
+
+
+
+
+    
