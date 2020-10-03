2ccfc712595f8f1d72cad0565e41922d2949bfeb
Morten Kragh Nielsen <morten@mnit.dk>
2020-10-03 22:39:37 +0200

    blacked the code

--- project_euler/problem_57/sol1.py
+++ project_euler/problem_57/sol1.py
@@ -4,25 +4,30 @@ Square root convergents
 url: https://projecteuler.net/problem=57
 
 """
+
+
 def square_root_convergents():
-	"""Returns the number of fractions where the numerator has more digits than the denominator.
+    """Returns the number of fractions
+         where the numerator has more digits than the denominator.
+
     >>> square_root_convergents()
     153
     """
-	n1 = 1
-	n2 = 1
-	result = 0
-	for x in range(1,1001):
-		numerator = n1 + n2*2
-		denominator = n1 + n2
+    n1 = 1
+    n2 = 1
+    result = 0
+    for x in range(1, 1001):
+        numerator = n1 + n2 * 2
+        denominator = n1 + n2
 
-		if len(str(numerator)) > len(str(denominator)):
-			result += 1
+        if len(str(numerator)) > len(str(denominator)):
+            result += 1
 
-		n1 = numerator
-		n2 = denominator
+        n1 = numerator
+        n2 = denominator
 
-	return result
+    return result
+
 
 if __name__ == "__main__":
-	print(square_root_convergents())
+    print(square_root_convergents())
