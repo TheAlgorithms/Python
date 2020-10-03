e2f56a95aaee2238fa58ce3b7c251876a06ab560
shellhub <shellhub.me@gmail.com>
2020-10-03 21:11:53 +0800

    using enumerate()

--- sorts/insertion_sort.py
+++ sorts/insertion_sort.py
@@ -40,13 +40,12 @@ def insertion_sort(collection: list) -> list:
     True
     """
 
-    for i in range(1, len(collection)):
-        insert_index = i - 1
-        insert_value = collection[i]
+    for insert_index, insert_value in enumerate(collection[1:]):
+        temp_index = insert_index
         while insert_index >= 0 and insert_value < collection[insert_index]:
             collection[insert_index + 1] = collection[insert_index]
             insert_index -= 1
-        if insert_index != i - 1:
+        if insert_index != temp_index:
             collection[insert_index + 1] = insert_value
     return collection
 
