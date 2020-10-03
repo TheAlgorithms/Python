13300e718e9669b21b5ace8d2c184e20a81a1589
MALYA BANSAL <62214575+malyabansal@users.noreply.github.com>
2020-10-03 19:18:52 +0530

    Create Simple_Strings.py

---
+++ strings/Simple_Strings.py
@@ -0,0 +1,21 @@
+String1 = 'Welcome to the Malya's World'
+print("String with the use of Single Quotes: ") 
+print(String1) 
+  
+# Creating a String 
+# with double Quotes 
+String1 = "I'm a Malya"
+print("\nString with the use of Double Quotes: ") 
+print(String1) 
+  
+# Creating a String 
+# with triple Quotes 
+String1 = '''I'm Malya and I live in a world of "TechGeeks"'''
+print("\nString with the use of Triple Quotes: ") 
+print(String1) 
+  
+# Creating String with triple 
+# Quotes allows multiple lines 
+String1 = '''Malya is the Best'''
+print("\nCreating a multiline String: ") 
+print(String1) 
