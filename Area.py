efd445f2c8fae6877ec4101a835615edf8db4050
UdhavKumar <61343874+UdhavKumar@users.noreply.github.com>
2020-10-03 22:30:25 +0530

    A file to find out the area

---
+++ Area.py
@@ -0,0 +1,5 @@
+x = eval(input("Enter length : ")) # This statement asks the uer to enter the length
+y = eval(input("Enter breadth : ")) # This statement asks the user to enter the breadth
+ans = x * y
+print("Your answer is ")
+print(ans)
