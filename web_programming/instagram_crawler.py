8187e9ffc196e330af35f752bcf82f4f54cb3920
Dmytro Litvinov <litvinov.dmytro.it@gmail.com>
2020-10-03 18:08:45 +0300

    Fixes: #2699. Fix Insragram crawler website link

--- web_programming/instagram_crawler.py
+++ web_programming/instagram_crawler.py
@@ -118,7 +118,7 @@ def test_instagram_user(username: str = "github") -> None:
     assert instagram_user.number_of_followers > 120000
     assert instagram_user.number_of_followings > 15
     assert instagram_user.email == "support@github.com"
-    assert instagram_user.website == "https://github.com/readme"
+    assert instagram_user.website == "https://github.com/readme/wes-mckinney"
     assert instagram_user.profile_picture_url.startswith("https://instagram.")
     assert instagram_user.is_verified is True
     assert instagram_user.is_private is False
