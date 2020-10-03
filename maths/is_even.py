0e280830662f48c6f3f0c1c162bb0ded2003f8db
AlefAdonis <alefadonis10@gmail.com>
2020-10-02 23:04:03 -0300

    Adding a test of if is even

---
+++ maths/is_even.py
@@ -0,0 +1,17 @@
+""" The number is even """
+
+def even(number = 0): 
+
+    """For all integers numbers, if their divisions by 2 
+    are exacts, so this number is even."""
+   
+    if number % 2 == 0:
+        return print("Is even")
+    
+    else:
+        return print("Is odd")
+
+if __name__ == "__main__":
+    
+    number = int(input())
+    even(number)
