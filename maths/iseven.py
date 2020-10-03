4082eee5fc73a39fc7fb22bf69a80f6e9c08c253
AlefAdonis <alefadonis10@gmail.com>
2020-10-02 23:43:08 -0300

    I forgot the use of black

--- maths/iseven.py
+++ maths/iseven.py
@@ -1,17 +1,19 @@
 """ The number is even """
 
-def even(number = 0): 
 
-    """For all integers numbers, if their divisions by 2 
+def even(number=0):
+
+    """For all integers numbers, if their divisions by 2
     are exacts, so this number is even."""
-   
+
     if number % 2 == 0:
         return print("Is even")
-    
+
     else:
         return print("Is odd")
 
+
 if __name__ == "__main__":
-    
+
     number = int(input().strip())
     even(number)
