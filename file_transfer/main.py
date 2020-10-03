081181958f4578198a0795e24de8c5ade2556690
Scott Little <swlittle7@gmail.com>
2020-10-03 15:39:51 -0500

    Add menu

---
+++ file_transfer/main.py
@@ -0,0 +1,22 @@
+from send_file.py import send_file
+from receive_file import receive
+def main():
+
+    print("File transfer:")
+    print("(R)eceive?")
+    print("(T)rasnfer?")
+    mode = input("? ").lower()
+    if mode not in ('r', 't'):
+        print("Invalid mode.")
+        main()
+    if mode == 'r':
+        save = input('Save location? ')
+        host = input("Override host (empty to use default)? ")
+        port = input("Override port (empty to use default)? ")
+        receive(save, host, port)
+    elif mode == 't':
+        fn = input("File to send? ")
+        send_file(fn)
+
+if __name__=="__main__":
+    main()
--- file_transfer/receive_file.py
+++ file_transfer/receive_file.py
@@ -1,19 +1,20 @@
-if __name__ == "__main__":
+def receive(saveas, host='', port='', debug=False):
     import socket  # Import socket module
 
     sock = socket.socket()  # Create a socket object
-    host = socket.gethostname()  # Get local machine name
-    port = 12312
+    host = socket.gethostname() if not host else host  # Get local machine name
+    port = 12312 if not port else port
 
     sock.connect((host, port))
     sock.send(b"Hello server!")
 
-    with open("Received_file", "wb") as out_file:
+    with open(saveas, "wb") as out_file:
         print("File opened")
         print("Receiving data...")
         while True:
             data = sock.recv(1024)
-            print(f"data={data}")
+            if debug:
+                print(f"data={data}")
             if not data:
                 break
             out_file.write(data)  # Write data to a file
