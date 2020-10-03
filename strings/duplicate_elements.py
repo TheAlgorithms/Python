1bb74d8d4bd88a4ce234b3b8b495722cdec146ae
VISMAYA <vismayak28@gmail.com>
2020-10-03 19:32:41 +0530

    added program to find duplicate elements

---
+++ strings/duplicate_elements.py
@@ -0,0 +1,14 @@
+#Created by Vismaya 
+#This python program finds the duplicate elements in any string
+
+
+string=input("Enter a string:")
+set_dupli=set({})
+for i in string:
+    count=0
+    for j in string:
+        if(i==j):
+            count+=1
+    if(count>1):
+        set_dupli.add(i)
+print("Duplicate elements are: {}".format(set_dupli))
