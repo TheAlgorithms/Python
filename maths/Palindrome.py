f119acdc668cb970c26860c670648235085d515b
Swatigupta-droid <64798478+Swatigupta-droid@users.noreply.github.com>
2020-10-03 23:48:36 +0530

    Create Palindrome.py

---
+++ maths/Palindrome.py
@@ -0,0 +1,27 @@
+# function which return reverse of a string
+
+ 
+
+def isPalindrome(s):
+
+    return s == s[::-1]
+
+ 
+
+ 
+
+# Driver code
+
+s = "malayalam"
+
+ans = isPalindrome(s)
+
+ 
+
+if ans:
+
+    print("Yes")
+
+else:
+
+    print("No")
