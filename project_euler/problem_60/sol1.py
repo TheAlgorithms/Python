1febc750139a7f01f22f6bd948340bf1d66aa146
gupta41sandeep@gmail.com <gupta41sandeep@gmail.com>
2020-10-04 18:39:38 +0530

    adding eulers problem 60

---
+++ project_euler/problem_60/sol1.py
@@ -0,0 +1,76 @@
+'''
+    Euler Problem : 60 
+    @author : sandeep gupta
+    @time   : 4 October 2020, 18:30
+
+    @Problem: The primes 3, 7, 109, and 673, are quite remarkable. By taking any two primes and concatenating them in 
+              any order the result will always be prime. For example, taking 7 and 109, both 7109 and 1097 are prime. 
+              The sum of these four primes, 792, represents the lowest sum for a set of four primes with this property.
+              Find the lowest sum for a set of five primes for which any two primes concatenate to produce another prime.
+
+    @Solution: BRUTE - calculated all the prime numbers using seive and stored them in global to avoid any re-calculation
+               after that in each iteration tried to optimze at each step to avoid re-dundant work.
+    
+    @ans     : sum(13, 5197, 5701, 6733, 8389) = 26033
+
+'''
+
+max_num = 100000000
+prime_arr = []
+prime = []
+
+def is_compound_prime(a,b):
+    global prime_arr
+    global prime
+    prefix = int(str(a) + str(b))
+    suffix = int(str(b) + str(a))
+    if prefix < max_num and suffix < max_num and prime[prefix] and prime[suffix]:
+        return True
+    return False
+
+def generate_prime():
+    global prime_arr
+    global prime
+    prime = [True for i in range(max_num+1)] 
+    p = 2
+    while (p * p <= max_num): 
+          
+        # If prime[p] is not changed, then it is a prime 
+        if (prime[p] == True): 
+              
+            # Update all multiples of p 
+            for i in range(p * p, max_num+1, p): 
+                prime[i] = False
+        p += 1
+      
+    # Print all prime numbers 
+    i = 0
+    for p in range(2, max_num+1): 
+        if prime[p]: 
+            i+=1
+            prime_arr.append(p)
+
+def solution():                               # function without any parameters
+    # ... calculations ...
+    answer = 1000000000000000
+    max_limit = 1100
+    generate_prime()
+    global prime_arr
+    global prime
+    for i in range(0,max_limit):
+        for j in range(i+1,max_limit):
+            if (is_compound_prime(prime_arr[i], prime_arr[j])):
+                for k in range(j+1,max_limit):
+                    if (is_compound_prime(prime_arr[k],prime_arr[i]) and is_compound_prime(prime_arr[k],prime_arr[j])):
+                        for l in range(k+1,max_limit):
+                            if (is_compound_prime(prime_arr[l],prime_arr[i]) and is_compound_prime(prime_arr[l],prime_arr[j]) and is_compound_prime(prime_arr[l],prime_arr[k])):
+                                for m in range(l+1,max_limit):
+                                    if (is_compound_prime(prime_arr[m],prime_arr[i]) and is_compound_prime(prime_arr[m],prime_arr[j]) and is_compound_prime(prime_arr[m],prime_arr[k]) and is_compound_prime(prime_arr[m],prime_arr[l])):
+                                        # print (prime_arr[i],prime_arr[j],prime_arr[k],prime_arr[l],prime_arr[m])
+                                        cur_list = [prime_arr[i],prime_arr[j],prime_arr[k],prime_arr[l],prime_arr[m]]
+                                        # print (cur_list)
+                                        answer = min(answer, sum(cur_list))
+
+    return answer
+
+print (solution())
