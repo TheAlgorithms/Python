fd07da2b762abd995b2e2465c38e2be3d20ac0c0
Chakradhar Palaparthi <chakradhar51.p@gmail.com>
2020-10-04 13:55:54 +0530

    Update file_utils.py
    Correcting black and flake8 changes

--- other/file_utils.py
+++ other/file_utils.py
@@ -1,8 +1,8 @@
 import json
 import sys
 
-
 # A utility file for all basic plain text and JSON file IO
+
 
 def touch_file(file_name: str) -> None:
     """
@@ -26,7 +26,7 @@ def touch_file(file_name: str) -> None:
 def clear_file(file_name: str) -> None:
     """
     Clear file:
-    A utility function to clear the contents of the file file_name
+    A utility function to clear the contents of the file file_name.
     """
     try:
         with open(file_name, "w"):
@@ -60,13 +60,15 @@ def load_json_from_file(file_name: str) -> str:
         return readfile.read() or str({})
 
 
-def read_lines_strip_return(file_name: str, split: str = None, index: int = None) -> list:
+def read_lines_strip_return(
+        file_name: str, split: str = None, index: int = None
+) -> list:
     """
     Read Lines from file:
     - Read the content of the file file_name using readlines.
     - If the parameter split is specified then each line is split using it.
-    - If the index is specified then after splitting the line corresponding index from the line is returned
-    - Returns list of lines from the file file_name matching the split and index conditions
+    - If the index is specified, index from the above split line is returned.
+    - Returns list of lines from file_name matching the split and index conditions.
     """
     try:
         with open(file_name) as readfile:
@@ -108,8 +110,8 @@ def append_line_to_file(file_name: str, line: str) -> None:
     """
     Append Line to file:
     - Checks if the file exists and creates if not.
-    - Opens the file in append mode and appends the line at the ending of file file_name.
-    """    
+    - Opens the file in append mode and appends the line at the ending of file_name.
+    """
     touch_file(file_name)
     with open(file_name, "a") as append_file:
         append_file.write(line + "\n")
@@ -121,7 +123,7 @@ def append_line_to_file_if_doesnt_exist(file_name: str, line: str) -> None:
     - Checks if the file exists and creates if not.
     - Checks if the line already exists in the file file_name.
     - Only appends the line if nor already present in file_name.
-    """   
+    """
     touch_file(file_name)
     lines = read_lines_strip_return(file_name)
     if line not in lines:
@@ -147,8 +149,8 @@ def check_file_exists(file_name: str) -> bool:
 def convert_int_to_dotted_str(number: int) -> str:
     """
     Convert int into dotted str:
-    - This function takes an integer and converts it into a dotted str
-    - Useful in converting a number into version 
+    - This function takes an integer and converts it into a dotted str.
+    - Useful in converting a number into version.
     - Eg: 123 -> "1.2.3"
     """
     res = list()
@@ -160,8 +162,8 @@ def convert_int_to_dotted_str(number: int) -> str:
 def convert_dotted_str_to_int(dotted_str: str) -> int:
     """
     Convert dotted str into int:
-    - This function takes a dotted str and converts into an int
-    - Useful in converting version into a number 
+    - This function takes a dotted str and converts into an int.
+    - Useful in converting version into a number.
     - Eg: "1.2.3" -> 123
     """
     return int("".join(dotted_str.split(".")))
