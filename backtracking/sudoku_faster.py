f4b624ea68f9d0d64410d3c8d62010852722b398
Crypticsy <animesh19basnet@gmail.com>
2020-10-04 20:22:28 +0545

    tested black and flask8

--- backtracking/sudoku_faster.py
+++ backtracking/sudoku_faster.py
@@ -1,9 +1,10 @@
 import sys
 import time
-sys.setrecursionlimit(10**6) 
+
+sys.setrecursionlimit(10 ** 6)
 
 
-def findemptycells():       # Finds all the empty cells and adds it for later
+def findemptycells():  # Finds all the empty cells and adds it for later
     """
     This function checks the grid to see if each row,
     column, and the 3x3 subgrids contain the digit 'n'.
@@ -13,12 +14,12 @@ def findemptycells():       # Finds all the empty cells and adds it for later
     for x in range(9):
         for y in range(9):
             if grid[x][y] == 0:
-                empty.append((x,y))
+                empty.append((x, y))
             else:
                 row[x].add(grid[x][y])
                 col[y].add(grid[x][y])
-                a,b = x//3,y//3
-                table[a*3+b].add(grid[x][y])
+                a, b = x // 3, y // 3
+                table[a * 3 + b].add(grid[x][y])
 
 
 def solver(i=0, j=0, n=0):
@@ -27,29 +28,32 @@ def solver(i=0, j=0, n=0):
     all unassigned locations in such a way to meet the requirements
     for Sudoku solution (non-duplication across rows, columns, and boxes)
     """
-    x,y = (-1,-1) if n>=len(empty) else empty[n]
-    if x == -1:return True
-        
-    for z in range(1,10):
-        temp = (x//3)*3+y//3
+    x, y = (-1, -1) if n >= len(empty) else empty[n]
+    if x == -1:
+        return True
+
+    for z in range(1, 10):
+        temp = (x // 3) * 3 + y // 3
         if z not in row[x] and z not in col[y] and z not in table[temp]:
             grid[x][y] = z
-            row[x].add(z)       # the new value is added to that specific row
-            col[y].add(z)       # the new value is added to that specific column
-            table[temp].add(z)       # the new value is added to that mini table
+            row[x].add(z)  # new value is added to that specific row
+            col[y].add(z)  # new value is added to that specific column
+            table[temp].add(z)  # new value is added to that mini table
 
-            if solver(x,y,n+1):return True
-            
+            if solver(x, y, n + 1):
+                return True
+
             grid[x][y] = 0
-            row[x].remove(z)       # the new value is removed from that specific row
-            col[y].remove(z)       # the new value is removed from that specific column
-            table[temp].remove(z)       # the new value is removed from that specific mini table
-            
-    return False 
-    
+            row[x].remove(z)  # new value is removed from that specific row
+            col[y].remove(z)  # new value is removed from that specific column
+            table[temp].remove(
+                z
+            )  # the new value is removed from that specific mini table
+
+    return False
 
 
-#Testing
+# Testing
 
 # assigning initial values to the grid (World's hardest sudoku problem)
 initial_grid = [
@@ -96,13 +100,15 @@ no_solution = [
 if __name__ == "__main__":
     for grid in (initial_grid, no_solution):
         start_time = time.time()
-        
-        grid,empty = list(map(list, grid)), []
-        row, col, table = [{x:set() for x in range(9)} for x in range(3)]
+
+        grid, empty = list(map(list, grid)), []
+        row, col, table = [{x: set() for x in range(9)} for x in range(3)]
 
         findemptycells()
-        if solver():        # recurses till a solution is found otherwise returns the same table
-            print("---- Solved in %s seconds ----" % (time.time() - start_time))
+        if (
+            solver()
+        ):  # check if a solution exists
+            print("Solved in %s seconds" % (time.time() - start_time))
             [print(*x) for x in grid]
         else:
             print("Cannot find a solution.")
