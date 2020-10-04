847071e1e6b4337963350c6d374fa6a3736e8f03
KiranAkadas <akadask@gmail.com>
2020-10-05 00:35:32 +0530

    Update sol.py

--- project_euler/problem_80/sol.py
+++ project_euler/problem_80/sol.py
@@ -2,25 +2,25 @@ import decimal
 from math import floor
 
 def solution() -> int:
-    n=100
-    p=100
-    tot=0
+    n = 100
+    p = 100
+    tot = 0
 
-    for i in range(1,n+1):
-        sm=0
-        decimal.getcontext().prec=p+10
-        fv=decimal.Decimal(i).sqrt()
-        if fv-floor(fv)==0:
+    for i in range(1, n + 1):
+        sm = 0
+        decimal.getcontext().prec = p + 10
+        fv = decimal.Decimal(i).sqrt()
+        if fv - floor(fv) == 0:
             continue
         else:
-            fv=str(fv).replace('.','')
-            fv=fv[0:p]
-            fv=int(fv)
-            while(fv!=0):
-                sm=sm+(fv%10)
-                fv=fv//10
-            tot+=sm
+            fv = str(fv).replace(".", "")
+            fv = fv[0:p]
+            fv = int(fv)
+            while fv != 0:
+                sm = sm + (fv%10)
+                fv = fv // 10
+            tot += sm
     return tot
 
-res=solution()
+res = solution()
 print(res)
