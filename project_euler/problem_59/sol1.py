b9bac29aa0bdc8120b5317646e7e0c236566c15b
gupta41sandeep@gmail.com <gupta41sandeep@gmail.com>
2020-10-05 01:37:38 +0530

    added try catch block

--- project_euler/problem_59/sol1.py
+++ project_euler/problem_59/sol1.py
@@ -37,30 +37,33 @@ def isAcceptable(myChar):
 
 
 def solution() -> int:
-    with open("cipher.txt", "r") as file:
-        data = file.read().replace("\n", "")
-        data = data.split(",")
-        data = [int(x) for x in data]
-        answer = 0
-        for i in range(97, 123):
-            for j in range(97, 123):
-                for k in range(97, 123):
-                    possible_lists = set(list(permutations([i, j, k], 3)))
-                    for possible_answer in possible_lists:
-                        round_robin_count = 0
-                        words = ""
-                        valid = True
-                        for d in data:
-                            if check_english(d, possible_answer[round_robin_count]):
-                                words += chr(d ^ possible_answer[round_robin_count])
-                            else:
-                                valid = False
-                                break
-                            round_robin_count = (round_robin_count + 1) % 3
-                        if valid:
-                            for word in words:
-                                answer += ord(word)
-                            return answer
+    try:
+        with open("cipher.txt", "r") as file:
+            data = file.read().replace("\n", "")
+            data = data.split(",")
+            data = [int(x) for x in data]
+            answer = 0
+            for i in range(97, 123):
+                for j in range(97, 123):
+                    for k in range(97, 123):
+                        possible_lists = set(list(permutations([i, j, k], 3)))
+                        for possible_answer in possible_lists:
+                            round_robin_count = 0
+                            words = ""
+                            valid = True
+                            for d in data:
+                                if check_english(d, possible_answer[round_robin_count]):
+                                    words += chr(d ^ possible_answer[round_robin_count])
+                                else:
+                                    valid = False
+                                    break
+                                round_robin_count = (round_robin_count + 1) % 3
+                            if valid:
+                                for word in words:
+                                    answer += ord(word)
+                                return answer
+    except BaseException:
+        print("An exception occured while reading file")
     return answer
 
 
