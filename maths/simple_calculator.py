06b6ecebd52bacf5d1aa0eb6735b038ed09ebac7
Jowin John Chemban <29167718+jowinjohnchemban@users.noreply.github.com>
2020-10-05 01:32:13 +0530

    Update simple_calculator.py

--- maths/simple_calculator.py
+++ maths/simple_calculator.py
@@ -11,19 +11,19 @@ def div(a,b): #Division ÷
 	return(a/b)
 
 n=int(raw_input("\n 1. Addition\n 2. Subtraction\n 3. Multiplication\n 4. Division\n 0. Exit\n\n Enter your Choice!\n")) #MENU
-if(n==0):
+if(n == 0):
 	exit()
 a=int(raw_input("\n\n First Number : ")) #First Input
 b=int(raw_input("\n\n Second Number : ")) #Second Input
 
  #Output
-if(n==1):
-	print"\n Sum=",add(a,b)
-elif(n==2):
-	print"\n Difference=",sub(a,b)
-elif(n==3):
-	print"\n Product=",prd(a,b)
-elif(n==4):
-	print"\n Quotient=",div(a,b)
+if(n == 1):
+	print("\n Sum=",add(a,b))
+elif(n == 2):
+	print("\n Difference=",sub(a,b))
+elif(n == 3):
+	print("\n Product=",prd(a,b))
+elif(n == 4):
+	print("\n Quotient=",div(a,b))
 else:
-	print"Invalid Entry!"
+	print("Invalid Entry!")
