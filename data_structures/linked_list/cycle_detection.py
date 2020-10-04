b88b053c9df3920724beec1def7775b1c287d51e
Akshat Gadhwal <akshatgadhwal1@gmail.com>
2020-10-04 14:34:51 +0530

    eddited cycle_detection

--- data_structures/linked_list/cycle_detection.py
+++ data_structures/linked_list/cycle_detection.py
@@ -49,33 +49,4 @@ def has_cycle(head):
     return False
 
 
-##  inputs
-    tests = int(input())
-
-    for tests_itr in range(tests):
-        index = int(input())
-
-        llist_count = int(input())
-
-        llist = SinglyLinkedList()
-
-        for _ in range(llist_count):
-            llist_item = int(input())
-            llist.insert_node(llist_item)
-
-        extra = SinglyLinkedListNode(-1);
-        temp = llist.head;
-
-        for i in range(llist_count):
-            if i == index:
-                extra = temp
-
-            if i != llist_count-1:
-                temp = temp.next
-
-        temp.next = extra
-
-        result = has_cycle(llist.head)
-        print(result)
-
 
