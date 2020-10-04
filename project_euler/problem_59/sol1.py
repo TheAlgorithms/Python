f8920686ab1f0a24b142c076d4e5f70f5d97e244
gupta41sandeep@gmail.com <gupta41sandeep@gmail.com>
2020-10-05 01:08:35 +0530

    init for problem 59 project euler problem

---
+++ project_euler/problem_59/sol1.py
@@ -0,0 +1,76 @@
+from itertools import permutations
+import enchant
+
+words_checker = enchant.Dict("en_US")
+
+
+def check_english(ascii1, ascii2):
+    """
+    function to check if the
+    xor(exclusive or) of two ascii
+    numbers entered is only letters
+    used in common english
+    """
+    xor = ascii1 ^ ascii2
+    return isAcceptable(chr(xor))
+
+    # print (chr(xor))
+    return False
+
+
+def isAcceptable(myChar):
+    return (
+        "a" <= myChar <= "z"
+        or "A" <= myChar <= "Z"
+        or myChar == " "
+        or myChar == "'"
+        or myChar == ","
+        or myChar == '"'
+        or myChar == "["
+        or myChar == "]"
+        or myChar == ":"
+        or "0" <= myChar <= "9"
+        or myChar == "/"
+        or myChar == "."
+        or myChar == "("
+        or myChar == ")"
+        or myChar == ";"
+        or myChar == "+"
+    )
+
+
+def solution():
+    with open("cipher.txt", "r") as file:
+        data = file.read().replace("\n", "")
+        data = data.split(",")
+        data = [int(x) for x in data]
+        answer = 0
+        for i in range(97, 123):
+            for j in range(97, 123):
+                for k in range(97, 123):
+                    possible_lists = set(list(permutations([i, j, k], 3)))
+                    for possible_answer in possible_lists:
+                        round_robin_count = 0
+                        words = ""
+                        valid = True
+                        for d in data:
+                            if check_english(d, possible_answer[round_robin_count]):
+                                words += chr(d ^ possible_answer[round_robin_count])
+                            else:
+                                valid = False
+                                break
+                            round_robin_count = (round_robin_count + 1) % 3
+                        if valid:
+                            for word in words:
+                                answer += ord(word)
+                            return answer
+    return answer
+
+
+print(solution())
+
+# Tests
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
