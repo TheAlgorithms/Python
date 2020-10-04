c6071aa5e28e088929cbf03b1361a5ebd8defe94
Dr. Tobias Schröder <tobias.krauthoff@gmail.com>
2020-10-04 09:46:28 +0200

    Fix shadowing

--- knappsack/knappsack.py
+++ knappsack/knappsack.py
@@ -3,16 +3,16 @@ from typing import List
 
 # A naive recursive implementation of 0-1 Knapsack Problem
 
-def knapsack(capacity: int, weights: List[int], value: List[int], counter) -> int:
+def knapsack(capacity: int, weights: List[int], values: List[int], counter) -> int:
     """
-    Returns the maximum value that can be put in a knapsack of a capacity,
-    whereby each weight has a specific value.
+    Returns the maximum value that can be put in a knapsack of a capacity cap,
+    whereby each weight w has a specific value val.
 
-    >>> capactiy = 50
-    >>> values = [60, 100, 120]
-    >>> weights = [10, 20, 30]
-    >>> counter = len(values)
-    >>> print(knapsack(capactiy, weights, values, counter))
+    >>> cap = 50
+    >>> val = [60, 100, 120]
+    >>> w = [10, 20, 30]
+    >>> c = len(values)
+    >>> print(knapsack(cap, w, val, c))
     50
 
     The result is 50 cause the values of 100 and 120 got the weight of 50
@@ -29,11 +29,11 @@ def knapsack(capacity: int, weights: List[int], value: List[int], counter) -> in
     #   (1) nth item included
     #   (2) not included
     if weights[counter - 1] > capacity:
-        return knapsack(capacity, weights, value, counter - 1)
+        return knapsack(capacity, weights, values, counter - 1)
     else:
         left_capacity = capacity - weights[counter - 1]
-        new_value_included = value[counter - 1] + knapsack(left_capacity, weights, value, counter - 1)
-        without_new_value = knapsack(capacity, weights, value, counter - 1)
+        new_value_included = values[counter - 1] + knapsack(left_capacity, weights, values, counter - 1)
+        without_new_value = knapsack(capacity, weights, values, counter - 1)
         return max(new_value_included, without_new_value)
 
 
