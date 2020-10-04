319347e74afbfffc4e9e8b89b17d2f0280c21077
KiranAkadas <akadask@gmail.com>
2020-10-05 00:55:55 +0530

    Update sol.py

--- project_euler/problem_80/sol.py
+++ project_euler/problem_80/sol.py
@@ -1,16 +1,21 @@
 import decimal
 from math import floor
+
 """
 Square root digital expansion
-It is well known that if the square root of a natural number is not an integer, then it is irrational. The decimal expansion of such square roots is infinite without any repeating pattern at all.
+It is well known that if the square root of a natural number is not an integer, then it is irrational.
+The decimal expansion of such square roots is infinite without any repeating pattern at all.
 
-The square root of two is 1.41421356237309504880..., and the digital sum of the first one hundred decimal digits is 475.
+The square root of two is 1.41421356237309504880..., and the digital sum of the first one hundred
+decimal digits is 475.
 
-For the first one hundred natural numbers, find the total of the digital sums of the first one hundred decimal digits for all the irrational square roots.
+For the first one hundred natural numbers, find the total of the digital sums of the first one hundred
+decimal digits for all the irrational square roots.
 
 >>> solution()
 40886
 """
+
 
 def solution() -> int:
     """Return the total of the digital sums of the first one hundred decimal digits for all the irrational square roots.
@@ -18,7 +23,7 @@ def solution() -> int:
     >>> solution()
     40886
     """
-    
+
     n = 100
     p = 100
     tot = 0
@@ -41,5 +46,5 @@ def solution() -> int:
 
 
 if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+    res=solution()
+    print(res)
