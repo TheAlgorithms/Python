77069e70d0592073cc20beafc1d90f9cb55f4e4d
Siddharth Choudhary <60484636+siddharthzs@users.noreply.github.com>
2020-10-05 16:06:28 +0530

    All requested changes has been made. Details below
    1. all names in snake_case
    2. function call inside __name__ == __main__
    3.  Testcase has been added to doc string

--- traversals/morris_traversal.py
+++ traversals/morris_traversal.py
@@ -5,47 +5,59 @@ class Node:
         self.right = None
 
 
-def Morris_traversal(root):
+def morris_traversal(root):
     """
     Morris(InOrder) travaersal is a tree traversal algorithm that does not employ
      the use of recursion or a stack. In this traversal, links are created as
     successors and nodes are printed using these links.
     Finally, the changes are reverted back to restore the original tree.
+    root = Node(4)
+    temp = root
+    temp.left = Node(2)
+    temp.right = Node(8)
+    temp = temp.left
+    temp.left = Node(1)
+    temp.right = Node(5)
     """
-    InorderTraversal = []
+    inorder_traversal = []
     # set current to root of binary tree
-    curr = root
+    current = root
 
-    while curr is not None:
-        if curr.left is None:
-            InorderTraversal.append(curr.data)
-            curr = curr.right
+    while current is not None:
+        if current.left is None:
+            inorder_traversal.append(current.data)
+            current = current.right
         else:
             # find the previous (prev) of curr
-            prev = curr.left
-            while prev.right is not None and prev.right != curr:
-                prev = prev.right
+            previous = current.left
+            while previous.right is not None and previous.right != current:
+                previous = previous.right
 
             # make curr as right child of its prev
-            if prev.right is None:
-                prev.right = curr
-                curr = curr.left
+            if previous.right is None:
+                previous.right = current
+                current = current.left
 
             # firx the right child of prev
             else:
-                prev.right = None
-                InorderTraversal.append(curr.data)
-                curr = curr.right
+                previous.right = None
+                inorder_traversal.append(current.data)
+                current = current.right
 
-    return InorderTraversal
+    return inorder_traversal
 
 
-root = Node(4)
-temp = root
-temp.left = Node(2)
-temp.right = Node(8)
-temp = temp.left
-temp.left = Node(1)
-temp.right = Node(5)
+def main():
+    root = Node(4)
+    temp = root
+    temp.left = Node(2)
+    temp.right = Node(8)
+    temp = temp.left
+    temp.left = Node(1)
+    temp.right = Node(5)
+    print(morris_traversal(root))
 
-print(Morris_traversal(root))
+
+if __name__ == "__main__":
+    main()
+    
