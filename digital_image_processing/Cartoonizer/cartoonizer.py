f067f329f1f001f50a12f7e8deae8e02104a9713
Ankur <34418414+Ankuraxz@users.noreply.github.com>
2020-10-05 15:29:19 +0530

    Update and rename Cartoonizer.py to cartoonizer.py

--- digital_image_processing/Cartoonizer/cartoonizer.py
+++ digital_image_processing/Cartoonizer/cartoonizer.py
@@ -22,18 +22,18 @@ def cartoonizer(img,num_down = 2, num_bi = 5):
     #Params
     #num_down = 2 #DOWNSAMPLE STEPS
     #num_bi = 5 # BILATERAL FILTERING STEPS
-    img_c = img
+    img_color = img
     for ix in range(num_down):
-        img_c = cv2.pyrDown(img)# Pyramid Down : Downsampling
+        img_color = cv2.pyrDown(img)# Pyramid Down : Downsampling
     # print(img_c.shape)
     
     for iy in range(num_bi):
-        img_c = cv2.bilateralFilter(img_c,d=9,sigmaColor=9,sigmaSpace=7) #Filtering
+        img_color = cv2.bilateralFilter(img_color,d=9,sigmaColor=9,sigmaSpace=7) #Filtering
     # print(img_c.shape)
     
     #UPSAMPLING
     for ix in range(num_down):
-        img_c = cv2.pyrUp(img_c)# Pyramid Down : Downsampling
+        img_color = cv2.pyrUp(img_color)# Pyramid Down : Downsampling
     # print(img_c.shape)
     
     #BLUR and Threshold
@@ -41,12 +41,12 @@ def cartoonizer(img,num_down = 2, num_bi = 5):
     img_blur = cv2.medianBlur(img_gray,7) #MEDIAN BLUR
     img_edge = cv2.adaptiveThreshold(img_blur,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,blockSize=9,C=2)
 
-    img_c = cv2.resize(img_c,(800,800))
+    img_color = cv2.resize(img_color,(800,800))
     #RGB CONVERSION + BITWISE &
     img_edge = cv2.cvtColor(img_edge,cv2.COLOR_GRAY2RGB)
     # print(img_c.shape)
     # print(img_edge.shape)
-    img_cartoon = cv2.bitwise_and(img_c,img_edge)
+    img_cartoon = cv2.bitwise_and(img_color,img_edge)
 
     stack = np.hstack([img,img_cartoon])
     return stack
