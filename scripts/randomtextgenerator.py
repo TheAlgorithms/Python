833a01490faef43fd7af2a9e5e1666d7d1670065
Sarthak Kalpande <65620738+sakisrealop@users.noreply.github.com>
2020-10-05 15:20:46 +0530

    Update randomtextgenerator.py

--- scripts/randomtextgenerator.py
+++ scripts/randomtextgenerator.py
@@ -6,8 +6,7 @@ N = 7
   
 # using random.choices() 
 # generating random strings  
-res = ''.join(random.choices(string.ascii_uppercase +
-                             string.digits, k = N)) 
+res = ''.join(random.choices(string.ascii_uppercase + string.digits, k = N)) 
   
 # print result 
 print("The generated random string : " + str(res)) 
