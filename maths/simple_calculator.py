674886640ec9f31a386756d4cd5292cb43484ee6
Jowin John Chemban <29167718+jowinjohnchemban@users.noreply.github.com>
2020-10-05 11:17:37 +0530

    Update simple_calculator.py

--- maths/simple_calculator.py
+++ maths/simple_calculator.py
@@ -9,25 +9,27 @@ def prd(a,b): #Multiplication ×
 
 def div(a,b): #Division ÷
 	return (a/b)
-try:
+
+if __name__ == "__main__":
+
 	n=int(input("\n 1. Addition\n 2. Subtraction\n 3. Multiplication\n 4. Division\n 0. Exit\n\n Enter your Choice!\n")) #MENU
 	if(n == 0):
 		exit()
 	a=int(input("\n\n First Number : ")) #First Input
 	b=int(input("\n\n Second Number : ")) #Second Input
+	try:
+		 #Output
+		if(n == 1):
+			print("\n Sum=",add(a,b))
+		elif(n == 2):
+			print("\n Difference=",sub(a,b))
+		elif(n == 3):
+			print("\n Product=",prd(a,b))
+		elif(n == 4):
+			print("\n Quotient=",div(a,b))
+		else:
+			print("Unknown Entry!")
 
-	 #Output
-	if(n == 1):
-		print("\n Sum=",add(a,b))
-	elif(n == 2):
-		print("\n Difference=",sub(a,b))
-	elif(n == 3):
-		print("\n Product=",prd(a,b))
-	elif(n == 4):
-		print("\n Quotient=",div(a,b))
-	else:
-		print("Unknown Entry!")
+	except:
+		print("\n\n E : Exception Occured")
 
-except:
-	print("\n\n E : Exception Occured")
-	
