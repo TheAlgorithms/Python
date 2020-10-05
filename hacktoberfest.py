6c21bf738c3655d6f85eafc560e3627975500f69
Slayking1965 <69784108+Slayking1965@users.noreply.github.com>
2020-10-05 17:11:19 +0530

    hacktober

---
+++ hacktoberfest.py
@@ -0,0 +1,102 @@
+# Awesome Hacktoberfest [![Awesome](https://cdn.rawgit.com/sindresorhus/awesome/d7305f38d29fed78fa85652e3a63e154dd8e8829/media/badge.svg)](https://github.com/sindresorhus/awesome)
+> [Hacktoberfest](https://hacktoberfest.digitalocean.com/) is a chance to level up your coding skills and get small goodies in reward.
+> This page lists links to awesome background information and links to awesome contribution possibilities.
+[![Hacktoberfest 2019 logo](https://hacktoberfest.digitalocean.com/assets/logo-hf19-header-8245176fe235ab5d942c7580778a914110fa06a23c3d55bf40e2d061809d8785.svg)](https://benbarth.github.io/hacktoberfest-swag/)
+## Official website for registration and tips on how to get started
+- [Hacktoberfest Official Website](https://hacktoberfest.digitalocean.com/)
+- [Hacktoberfest Tutorial](https://youtu.be/0mjJS1Y8wrI) - Introduction to Hacktoberfest
+- [Hacktoberfest Swag List](https://benbarth.github.io/hacktoberfest-swag/) - List of all goodies given away by companies
+## Blogs entries
+- [Digitial Ocean's Blog Entry for Hacktoberfest 2019](https://blog.digitalocean.com/hacktoberfest-is-back-for-year-six/) - provides background information and explains **Participating for the right reasons**
+- [Hacktoberfest 2019 — Time to Start your Open-Source Journey!](https://medium.com/@swap/hacktoberfest-2019-time-to-start-your-open-source-journey-8227e10fac33) - Article on the basics of Hacktoberfest
+## Finding issues
+Finding a good issue to work on takes time.
+Here, you find possibilities to ease the search.
+### Search engines
+
+- [Issue Finder](http://hacktoberfest-finder.netlify.com) - finds issues made especially for hacktoberfest, also allows filtering by language
+- [Up For Grabs](https://up-for-grabs.net/#/) - find beginner-friendly projects and issues
+- [Hacktoberfest Finder](https://hacktoberfest-finder.netlify.app/) - easy way to find issues to work on during hacktoberfest, including the ability to filter by language and least comments.
+
+### Using labels
+
+With these labels, one finds issues to level up his skills in hacktoberfest.
+- [hacktoberfest](https://github.com/search?q=label%3Ahacktoberfest+state%3Aopen+type%3Aissue) - label especially created for hacktoberfest
+- [first-timers-only](https://github.com/search?q=label%3Afirst-timers-only+state%3Aopen+type%3Aissue)
+- [for-new-contributors](https://github.com/search?q=label%3Afor-new-contributors+state%3Aopen+type%3Aissue)
+- [good-first-issue](https://github.com/search?q=label%3Agood-first-issue+state%3Aopen+type%3Aissue)
+## Projects friendly to Hacktoberfest 1st time contributors
+You can start browsing [Awesome First PR Opportunities](https://github.com/MunGell/awesome-for-beginners) to find projects which are beginner-friendly. To learn more about how to make contributios visit [First Timers Only](https://firsttimersonly.com).
+In case you are an experienced developer, projects have prepared special issues for hacktoberfest.
+Below you will find awesome projects that have been put notable efforts into Hacktoberfest.
+They also try to be very welcoming for 1st-time contributors.
+### Any Project
+[Create Pull request here on any projects you have made](https://github.com/vijaygupta18/Hacktober-2020)
+### CSS
+([all CSS projects with hacktoberfest issues](https://github.com/search?utf8=%E2%9C%93&q=label%3Ahacktoberfest+state%3Aopen+type%3Aissue+language%3AJava&type=Issues&ref=advsearch&l=CSS))
+- [shopee_design](https://github.com/bluetch/shopee_design)
+- [crystal-ann](https://github.com/crystal-community/crystal-ann)
+### Dart / Flutter
+([all dart projects with hacktoberfest issues](https://github.com/search?utf8=%E2%9C%93&q=label%3Ahacktoberfest+state%3Aopen+type%3Aissue+language%3ADart&type=Issues&ref=advsearch&l=Dart&l=))
+- [The Public Transport](https://github.com/thepublictransport/thepublictransport-app)
+### HTML
+([all HTML projects with hacktoberfest issues](https://github.com/search?utf8=%E2%9C%93&q=label%3Ahacktoberfest+state%3Aopen+type%3Aissue+language%3AJava&type=Issues&ref=advsearch&l=HTML))
+- [league_app](https://github.com/connorphee/league_app)
+### JavaScript
+([all JavaScript projects with hacktoberfest issues](https://github.com/search?utf8=%E2%9C%93&q=label%3Ahacktoberfest+state%3Aopen+type%3Aissue+language%3AJava&type=Issues&ref=advsearch&l=JavaScript))
+- [jest-extended](https://github.com/mattphillips/jest-extended)
+- [vue-dropzone](https://github.com/rowanwins/vue-dropzone)
+### Java
+([all Java projects with hacktoberfest issues](https://github.com/search?utf8=%E2%9C%93&q=label%3Ahacktoberfest+state%3Aopen+type%3Aissue+language%3AJava&type=Issues&ref=advsearch&l=Java&l=))
+- [JabRef](https://www.jabref.org/hacktoberfest/2019.html)
+### PHP
+([all PHP projects with hacktoberfest issues](https://github.com/search?utf8=%E2%9C%93&q=label%3Ahacktoberfest+state%3Aopen+type%3Aissue+language%3APHP&type=Issues&ref=advsearch&l=&l=))
+- [hacktoberfest-status](https://github.com/niclasleonbock/hacktoberfest-status)
+### Python
+([all python projects with hacktoberfest issues](https://github.com/search?utf8=%E2%9C%93&q=label%3Ahacktoberfest+state%3Aopen+type%3Aissue+language%3APython&type=Issues&ref=advsearch&l=Dart&l=))
+- [django-gamification](https://github.com/mattjegan/django-gamification)
+- [howlong](https://github.com/mattjegan/HowLong)
+- [python-ds](https://github.com/prabhupant/python-ds) - datastructures implemented in PYthon
+- [quick-net](https://github.com/Zwork101/quick-net)
+- [Ralph](https://github.com/allegro/ralph)
+- [Tipboard](https://github.com/allegro/tipboard)
+- [wtfuzz](https://github.com/mattjegan/wtfuzz)
+### React
+([all React projects with hacktoberfest issues](https://github.com/search?utf8=%E2%9C%93&q=label%3Ahacktoberfest+state%3Aopen+type%3Aissue+language%3ADart&type=Issues&ref=advsearch&l=React&l=))
+- [Hacktoberfest Checker](https://hacktoberfestchecker.jenko.me/) - check your Hacktoberfest progress. [[Source](https://github.com/jenkoian/hacktoberfest-checker)]
+### React Native
+- [Hacktoberfest Tracker App](https://hacktoberfestchecker.jenko.me/) - Keep track of your and your friends' hacktoberfest progress. [[Source](https://github.com/KeyboardNinjas/hacktoberfest-mobileapp)]
+### SVG
+- [atom-icons](https://github.com/HackeSta/atom-icons)
+### TypeScript
+([all TypeScript projects with hacktoberfest issues](https://github.com/search?utf8=%E2%9C%93&q=label%3Ahacktoberfest+state%3Aopen+type%3Aissue+language%3ATypescript&type=Issues&ref=advsearch&l=Typescript))
+## Projects made for Hacktoberfest
+Here you find projects that were made especially for hacktoberfest.
+- [hacktoberfest-status](https://github.com/niclasleonbock/hacktoberfest-status)
+- [Issue Finder](http://hacktoberfest-finder.netlify.com) - finds issues made especially for hacktoberfest, also allows filtering by language
+## Companies participating in Hacktoberfest
+- [Accord](https://www.accordproject.org/events/hacktoberfest-2019/)
+- [Appwrite](https://medium.com/appwrite-io/hacktoberfest-2019-is-almost-here-lets-celebrate-it-together-24b311236dd)
+- [Aqua Security](https://blog.aquasec.com/aqua-open-source-security-hacktoberfest)
+- [Circle CI](https://circleci-public.github.io/hacktoberfest/#/)
+- [Coding Blocks](https://blog.codingblocks.com/2019/cb-hacktoberfest-2019/)
+- [Devfolio](https://devfolio.co/blog/hacktoberfest-2019-devfolio/)
+- [Gatsby](https://github.com/gatsbyjs/store.gatsbyjs.org)
+- [Hasura](https://blog.hasura.io/hasura-joins-hacktoberfest-2019/)
+- [Sendgrid](https://sendgrid.com/blog/hacktoberfest-2018-hack-on-sendgrid-open-source-projects/)
+- [Source{d}](https://go.sourced.tech/hacktoberfest)
+## Nonprofits participating in Hacktoberfest
+- [If-Me](https://github.com/ifmeorg/ifme/labels/hacktoberfest)
+- [Oppia](https://github.com/oppia/oppia/labels/Hacktoberfest)
+## Misc resources
+- [AwesomeSearch](https://awesomelists.top/) - search engine for [the awesome list](https://github.com/sindresorhus/awesome/blob/master/readme.md) providing useful programming resources.
+- [Build a Hacktoberfest Checker Clone](https://pybit.es/codechallenge38.html)
+- [Reading List](https://github.com/mattjegan/reading-list)
+## Contribute
+Contributions are welcome!
+Please read the [contribution guidelines](contributing.md) first.
+Especially have the motto "quality over quantity" in mind.
+Hacktoberfest is there to level up coding skills, not the capability to submit arbitrary files to arbitrary repositories.
+## License
+[![CC0](http://mirrors.creativecommons.org/presskit/buttons/88x31/svg/cc-zero.svg)](http://creativecommons.org/publicdomain/zero/1.0)
+To the extent possible under law, Matthew Egan has waived all copyright and related or neighbouring rights to this work.
