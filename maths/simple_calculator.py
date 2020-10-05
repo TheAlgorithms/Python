892bb3473c6a2070798429fc685ddd76dcff1edb
Jowin John Chemban <29167718+jowinjohnchemban@users.noreply.github.com>
2020-10-05 11:26:37 +0530

    Update simple_calculator.py

--- maths/simple_calculator.py
+++ maths/simple_calculator.py
@@ -1,35 +1,22 @@
-def add(a,b): #Addition +
-	return (a+b)
+def simple_calculator(a,n,b):
+try:
+	if(n == '+'):
+		print("\n Sum=",(a+b))
+	elif(n == '-'):
+		print("\n Difference=",(a-b))
+	elif(n == '*'):
+		print("\n Product=",(a*b))
+	elif(n == '/'):
+		print("\n Quotient=",(a/b))
+	else:
+		print("Unknown Entry!")
+except:
+	print("\n\n E : Exception Occured")
 
-def sub(a,b): #Subtraction -
-	return (a-b)
-
-def prd(a,b): #Multiplication ×
-	return (a*b)
-
-def div(a,b): #Division ÷
-	return (a/b)
-
+	
 if __name__ == "__main__":
-
-	n=int(input("\n 1. Addition\n 2. Subtraction\n 3. Multiplication\n 4. Division\n 0. Exit\n\n Enter your Choice!\n")) #MENU
-	if(n == 0):
-		exit()
-	a=int(input("\n\n First Number : ")) #First Input
-	b=int(input("\n\n Second Number : ")) #Second Input
-	try:
-		 #Output
-		if(n == 1):
-			print("\n Sum=",add(a,b))
-		elif(n == 2):
-			print("\n Difference=",sub(a,b))
-		elif(n == 3):
-			print("\n Product=",prd(a,b))
-		elif(n == 4):
-			print("\n Quotient=",div(a,b))
-		else:
-			print("Unknown Entry!")
-
-	except:
-		print("\n\n E : Exception Occured")
-
+	simple_calculator(1,'+',1)
+	simple_calculator(1,'-',1)
+	simple_calculator(1,'*',1)
+	simple_calculator(1,'/',1)
+	
