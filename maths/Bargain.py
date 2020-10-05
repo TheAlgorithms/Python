6826194640db565aa65afbccacb4961218498883
Bhaskar Joshi <56031616+BhaskarJoshi-01@users.noreply.github.com>
2020-10-05 23:10:24 +0530

    Bargain.py
    This is a python code for a math problem in Codeforces

---
+++ maths/Bargain.py
@@ -0,0 +1,53 @@
+# This is a wonderful math problem . Link is https://codeforces.com/contest/1422/problem/C
+
+MOD=10**9+7
+
+digits=[int(x) for x in input()]
+n=len(digits)
+
+
+right=[0 for _ in range(n)]
+r=0
+mul=1
+for i in range(n-1,-1,-1):
+    r+=digits[i]*mul
+    mul*=10
+    mul%=MOD
+    r%=MOD
+    right[i]=r
+
+
+left=[0 for _ in range(n)]
+l=0
+tot=0
+for i in range(n):
+    l*=10
+    l+=digits[i]
+    l%=MOD
+    tot+=l
+    tot%=MOD
+    left[i]=tot
+
+ans=0
+for rr in range(1,n+1):
+    leftIdx=rr-2#must have gap
+    if leftIdx==-1 and rr==n: #len of input is 1
+        break
+    if leftIdx==-1:
+        ans+=1*right[rr]
+        ans%=MOD
+#        print('FIRST {}'.format(1*right[rr]))
+    elif rr==n:
+        nLeft=leftIdx+2
+        ans+=left[leftIdx]
+        ans%=MOD
+#        print('LAST {}'.format(left[leftIdx]))
+    else:
+        nLeft=leftIdx+2
+#        print('rr:{} left[leftIdx]:{} n-rr:{} increment:{}'.format(rr,left[leftIdx],n-rr,left[leftIdx]*(10**(n-rr))+nLeft*right[rr]))
+        ans+=(left[leftIdx]%MOD)*pow(10,n-rr,MOD)+(nLeft*right[rr])%MOD
+        ans%=MOD
+
+#print(left)
+#print(right)
+print(ans)
