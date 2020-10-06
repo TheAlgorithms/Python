95d53f3ab8446d220356b1ca892f153c4bc546fc
Abhishek Maletha <67141747+Abhishek-photon@users.noreply.github.com>
2020-10-06 11:56:15 +0530

    Create sigmoid.py

---
+++ maths/sigmoid.py
@@ -0,0 +1,28 @@
+# This is a sigmoid function
+# x i function sigmoid is function variable
+# a is the gain
+#https://en.wikipedia.org/wiki/Sigmoid_function
+import math
+
+
+def sigmoid(x, a):
+
+    """
+    >>> sigmoid(0.5, 1)
+    0.6224593312018546
+    >>> add(2, 0.5)
+    0.7310585786300049
+    """
+    p = math.exp(-a * x)
+    return 1 / (1 + p)
+
+
+def main():
+    # enter values of gain and x
+    x = 2
+    gain = 0.5
+    print(sigmoid(x, gain))
+
+
+if __name__ == "__main__":
+    main()
