4dfedcc1193ab4c26789016b30033abdf7497f35
LAPTOP-PIGVD7CM\Komal <99kkaur@gmail.com>
2020-10-06 14:25:22 +0530

    slicing

---
+++ maths/slicing.py
@@ -0,0 +1,10 @@
+book_titles=[
+    'Exam WArriors',
+    'Evil in the Mahabharat',
+    '6 TIMES THINNER',
+    'The Driver in the Driverless Car',
+    'Evolution'
+]
+print(book_titles)
+book_titles[4:]
+print(book_titles[4:])
