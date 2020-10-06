2fa521b08cb1983b9b1803e7a5f985bfd655294a
Michael D <michael@drueing.de>
2020-10-06 19:44:52 +0200

    split out modexpt() function, and rename parameters

--- project_euler/problem_188/sol1.py
+++ project_euler/problem_188/sol1.py
@@ -15,8 +15,24 @@ Find the last 8 digits of 1777↑↑1855.
 """
 
 
-def solution(a=1777, k=1855, digits=8):
-    """Returns the last 8 digits of the hyperexponentiation of a by k.
+# small helper function for modular exponentiation
+def modexpt(base, exponent, modulo_value):
+    """Returns the modular exponentiation, that is the value
+    of `base ** exponent % modulo_value`, without calculating
+    the actual number
+    """
+
+    if exponent == 1:
+        return base
+    if exponent % 2 == 0:
+        x = modexpt(base, exponent / 2, modulo_value) % modulo_value
+        return (x * x) % modulo_value
+    else:
+        return (base * modexpt(base, exponent - 1, modulo_value)) % modulo_value
+
+def solution(base=1777, height=1855, digits=8):
+    """Returns the last 8 digits of the hyperexponentiation of base by
+    height, i.e. the number base↑↑height:
 
     >>> solution()
     95962097
@@ -30,21 +46,10 @@ def solution(a=1777, k=1855, digits=8):
     # last 8 digits
     modulo_value = 10 ** digits
 
-    # small helper function for modular exponentiation, to keep the result
-    # values small enough
-    def modexpt(base, exponent):
-        if exponent == 1:
-            return base
-        if exponent % 2 == 0:
-            x = modexpt(base, exponent / 2) % modulo_value
-            return (x * x) % modulo_value
-        else:
-            return (base * modexpt(base, exponent - 1)) % modulo_value
-
-    # calculate a ↑↑ k (mod modulo_value)
-    result = a
-    for i in range(1, k):
-        result = modexpt(a, result) % modulo_value
+    # calculate base↑↑height (mod modulo_value)
+    result = base
+    for i in range(1, height):
+        result = modexpt(base, result, modulo_value)
 
     return result
 
