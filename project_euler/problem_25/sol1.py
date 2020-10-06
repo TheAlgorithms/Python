f8feca3836b7347e9f7a1b734aa8c7a0ff4eb3be
nstoik <nelsonstoik@gmail.com>
2020-10-06 08:37:03 -0600

    update sol1 after running black

--- project_euler/problem_25/sol1.py
+++ project_euler/problem_25/sol1.py
@@ -31,15 +31,15 @@ def fibonacci(n: int) -> int:
     and creating an array of ints using the Fibonacci formula.
     Returns the nth element of the array.
 
-    >>> fibonacci(2)  
+    >>> fibonacci(2)
     1
-    >>> fibonacci(3)  
+    >>> fibonacci(3)
     2
-    >>> fibonacci(5) 
+    >>> fibonacci(5)
     5
-    >>> fibonacci(10) 
+    >>> fibonacci(10)
     55
-    >>> fibonacci(12) 
+    >>> fibonacci(12)
     144
 
     """
@@ -60,14 +60,14 @@ def fibonacci_digits_index(n: int) -> int:
     Computes incrementing Fibonacci numbers starting from 3 until the length
     of the resulting Fibonacci result is the input value n. Returns the term
     of the Fibonacci sequence where this occurs.
-    
+
     >>> fibonacci_digits_index(1000)
     4782
-    >>> fibonacci_digits_index(100)  
+    >>> fibonacci_digits_index(100)
     476
-    >>> fibonacci_digits_index(50)  
+    >>> fibonacci_digits_index(50)
     237
-    >>> fibonacci_digits_index(3)  
+    >>> fibonacci_digits_index(3)
     12
     """
     digits = 0
