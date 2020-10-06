bbd291f4775faf0510331edfaf9d6655441ddb53
Carlos Meza <carlos@digitalr00ts.com>
2020-10-05 17:38:19 -0700

    rename var to avoid redefining builtin

--- project_euler/problem_01/sol2.py
+++ project_euler/problem_01/sol2.py
@@ -19,14 +19,14 @@ def solution(n: int = 1000) -> int:
     83700
     """
 
-    sum = 0
+    total = 0
     terms = (n - 1) // 3
-    sum += ((terms) * (6 + (terms - 1) * 3)) // 2  # sum of an A.P.
+    total += ((terms) * (6 + (terms - 1) * 3)) // 2  # total of an A.P.
     terms = (n - 1) // 5
-    sum += ((terms) * (10 + (terms - 1) * 5)) // 2
+    total += ((terms) * (10 + (terms - 1) * 5)) // 2
     terms = (n - 1) // 15
-    sum -= ((terms) * (30 + (terms - 1) * 15)) // 2
-    return sum
+    total -= ((terms) * (30 + (terms - 1) * 15)) // 2
+    return total
 
 
 if __name__ == "__main__":
