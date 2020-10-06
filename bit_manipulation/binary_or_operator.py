fca2200898a22614ae8b3431b35191787008cfbc
Shreya <shreya.saxena00@gmail.com>
2020-10-06 22:08:38 +0530

    Working Bit manipulation-2

--- bit_manipulation/binary_or_operator.py
+++ bit_manipulation/binary_or_operator.py
@@ -13,32 +13,32 @@ def binary_or(a, b):
     """
     if a < 0 or b < 0:
         raise ValueError("the value of both input must be positive")
-        
+
     c = a | b
     decToBinary(c)
 
-def decToBinary(n): 
-      
-    # array to store binary number 
-    if (n == 0):
+
+def decToBinary(n):
+
+    # array to store binary number
+    if n == 0:
         print("0")
-    binaryNum = [0] * n; 
-  
-    # counter for binary array 
-    i = 0; 
-    while (n > 0):  
-  
-        # storing remainder in binary array 
-        binaryNum[i] = n % 2; 
-        n = int(n / 2); 
-        i += 1; 
-  
-    # printing binary array in reverse order 
+    binaryNum = [0] * n
+
+    # counter for binary array
+    i = 0
+    while n > 0:
+
+        # storing remainder in binary array
+        binaryNum[i] = n % 2
+        n = int(n / 2)
+        i += 1
+
+    # printing binary array in reverse order
 
     for j in range(i - 1, -1, -1):
-        print(binaryNum[j], end = "");
+        print(binaryNum[j], end="")
     print()
-
 
 
 if __name__ == "__main__":
