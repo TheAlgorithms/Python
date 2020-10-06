0b9e97cae6b5321f6e172d8998fbe346e2805634
Shreya <shreya.saxena00@gmail.com>
2020-10-06 20:28:14 +0530

    Working Bit manipulation

--- bit_manipulation/binary_and_operator.py
+++ bit_manipulation/binary_and_operator.py
@@ -1,52 +1,46 @@
-# https://www.tutorialspoint.com/python3/bitwise_operators_example.htm
-
-
-def binary_and(a: int, b: int):
+def binary_and(a, b):
     """
-    Take in 2 integers, convert them to binary,
-    return a binary number that is the
-    result of a binary and operation on the integers provided.
+    Take in 2 integers, perform binary & operation,
+    convert the number to binary and print.
 
-    >>> binary_and(25, 32)
-    '0b000000'
-    >>> binary_and(37, 50)
-    '0b100000'
-    >>> binary_and(21, 30)
-    '0b10100'
-    >>> binary_and(58, 73)
-    '0b0001000'
-    >>> binary_and(0, 255)
-    '0b00000000'
-    >>> binary_and(256, 256)
-    '0b100000000'
-    >>> binary_and(0, -1)
-    Traceback (most recent call last):
-        ...
+    binary_and(25, 32)
+    000000
+    binary_and(37, 50)
+    100000
+    binary_and(0, -1)
+    Traceback (most recent call last): ...
     ValueError: the value of both input must be positive
-    >>> binary_and(0, 1.1)
-    Traceback (most recent call last):
-        ...
-    TypeError: 'float' object cannot be interpreted as an integer
-    >>> binary_and("0", "1")
-    Traceback (most recent call last):
-        ...
-    TypeError: '<' not supported between instances of 'str' and 'int'
     """
     if a < 0 or b < 0:
         raise ValueError("the value of both input must be positive")
+        
+    c = a & b
+    decToBinary(c)
 
-    a_binary = str(bin(a))[2:]  # remove the leading "0b"
-    b_binary = str(bin(b))[2:]  # remove the leading "0b"
+def decToBinary(n): 
+      
+    # array to store binary number 
+    if (n == 0):
+        print("0")
+    binaryNum = [0] * n; 
+  
+    # counter for binary array 
+    i = 0; 
+    while (n > 0):  
+  
+        # storing remainder in binary array 
+        binaryNum[i] = n % 2; 
+        n = int(n / 2); 
+        i += 1; 
+  
+    # printing binary array in reverse order 
 
-    max_len = max(len(a_binary), len(b_binary))
+    for j in range(i - 1, -1, -1):
+        print(binaryNum[j], end = "");
+    print()
 
-    return "0b" + "".join(
-        str(int(char_a == "1" and char_b == "1"))
-        for char_a, char_b in zip(a_binary.zfill(max_len), b_binary.zfill(max_len))
-    )
 
 
 if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod()
+    binary_and(25, 32)
+    binary_and(37, 50)
--- bit_manipulation/binary_or_operator.py
+++ bit_manipulation/binary_or_operator.py
@@ -1,48 +1,46 @@
-# https://www.tutorialspoint.com/python3/bitwise_operators_example.htm
-
-
-def binary_or(a: int, b: int):
+def binary_or(a, b):
     """
-    Take in 2 integers, convert them to binary, and return a binary number that is the
-    result of a binary or operation on the integers provided.
+    Take in 2 integers, perform binary & operation,
+    convert the number to binary and print.
 
-    >>> binary_or(25, 32)
-    '0b111001'
-    >>> binary_or(37, 50)
-    '0b110111'
-    >>> binary_or(21, 30)
-    '0b11111'
-    >>> binary_or(58, 73)
-    '0b1111011'
-    >>> binary_or(0, 255)
-    '0b11111111'
-    >>> binary_or(0, 256)
-    '0b100000000'
-    >>> binary_or(0, -1)
-    Traceback (most recent call last):
-        ...
+    binary_or(25, 32)
+    111001
+    binary_or(37, 50)
+    110111
+    binary_or(0, -1)
+    Traceback (most recent call last): ...
     ValueError: the value of both input must be positive
-    >>> binary_or(0, 1.1)
-    Traceback (most recent call last):
-        ...
-    TypeError: 'float' object cannot be interpreted as an integer
-    >>> binary_or("0", "1")
-    Traceback (most recent call last):
-        ...
-    TypeError: '<' not supported between instances of 'str' and 'int'
     """
     if a < 0 or b < 0:
         raise ValueError("the value of both input must be positive")
-    a_binary = str(bin(a))[2:]  # remove the leading "0b"
-    b_binary = str(bin(b))[2:]
-    max_len = max(len(a_binary), len(b_binary))
-    return "0b" + "".join(
-        str(int("1" in (char_a, char_b)))
-        for char_a, char_b in zip(a_binary.zfill(max_len), b_binary.zfill(max_len))
-    )
+        
+    c = a | b
+    decToBinary(c)
+
+def decToBinary(n): 
+      
+    # array to store binary number 
+    if (n == 0):
+        print("0")
+    binaryNum = [0] * n; 
+  
+    # counter for binary array 
+    i = 0; 
+    while (n > 0):  
+  
+        # storing remainder in binary array 
+        binaryNum[i] = n % 2; 
+        n = int(n / 2); 
+        i += 1; 
+  
+    # printing binary array in reverse order 
+
+    for j in range(i - 1, -1, -1):
+        print(binaryNum[j], end = "");
+    print()
+
 
 
 if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod()
+    binary_or(25, 32)
+    binary_or(37, 50)
