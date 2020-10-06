ec1ca1bc6af775d1e4bb38ddafbeeb8913aec134
Aryaz <aryaz.egh@gmail.com>
2020-10-06 06:59:05 +0330

    Removed rank
    Since any union_set operation on x connects x to root, rank of both x and y are always equal to 1
    Removed rank for simplicity and fewer operations

--- data_structures/disjoint_set/disjoint_set.py
+++ data_structures/disjoint_set/disjoint_set.py
@@ -13,25 +13,15 @@ def make_set(x):
     """
     make x as a set.
     """
-    # rank is the distance from x to its' parent
-    # root's rank is 0
-    x.rank = 0
     x.parent = x
 
 
 def union_set(x, y):
     """
     union two sets.
-    set with bigger rank should be parent, so that the
-    disjoint set tree will be more flat.
     """
     x, y = find_set(x), find_set(y)
-    if x.rank > y.rank:
-        y.parent = x
-    else:
-        x.parent = y
-        if x.rank == y.rank:
-            y.rank += 1
+    y.parent = x
 
 
 def find_set(x):
