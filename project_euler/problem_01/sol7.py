ee4ca6e519fb4c3440d19ff906caa26700607a6f
Carlos Meza <carlos@digitalr00ts.com>
2020-10-05 17:50:26 -0700

    clean up module docstr

--- project_euler/problem_01/sol7.py
+++ project_euler/problem_01/sol7.py
@@ -1,7 +1,7 @@
 """
 Problem Statement:
 If we list all the natural numbers below 10 that are multiples of 3 or 5,
-we get 3,5,6 and 9. The sum of these multiples is 23.
+we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below N.
 """
 
