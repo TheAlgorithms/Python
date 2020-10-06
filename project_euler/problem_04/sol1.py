621c6a2641e875675eef2216ae5a9a60d1a9b145
Archaengel <god11341258@gmail.com>
2020-10-06 11:38:27 -0700

    Add style improvements to solutions for PE problem 04

--- project_euler/problem_04/sol1.py
+++ project_euler/problem_04/sol1.py
@@ -8,10 +8,9 @@ is less than N.
 """
 
 
-def solution(n):
+def solution(n: int = 998001) -> int:
     """Returns the largest palindrome made from the product of two 3-digit
-    numbers which is less than n.
-
+    numbers which is less than n or -1 if no such product exists
     >>> solution(20000)
     19591
     >>> solution(30000)
@@ -19,33 +18,23 @@ def solution(n):
     >>> solution(40000)
     39893
     >>> solution(10000)
-    Traceback (most recent call last):
-    ...
-    ValueError: That number is larger than our acceptable range.
+    -1
     """
     # fetches the next number
     for number in range(n - 1, 9999, -1):
-
         # converts number into string.
         str_number = str(number)
-
         # checks whether 'str_number' is a palindrome.
         if str_number == str_number[::-1]:
-
             divisor = 999
-
             # if 'number' is a product of two 3-digit numbers
             # then number is the answer otherwise fetch next number.
             while divisor != 99:
                 if (number % divisor == 0) and (len(str(number // divisor)) == 3.0):
                     return number
                 divisor -= 1
-    raise ValueError("That number is larger than our acceptable range.")
+    return -1
 
 
 if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod()
-
     print(solution(int(input().strip())))
--- project_euler/problem_04/sol2.py
+++ project_euler/problem_04/sol2.py
@@ -8,24 +8,25 @@ is less than N.
 """
 
 
-def solution(n):
+def solution(n: int = 998001) -> int:
     """Returns the largest palindrome made from the product of two 3-digit
-    numbers which is less than n.
-
+    numbers which is less than n or -1 if no such product exists.
     >>> solution(20000)
     19591
     >>> solution(30000)
     29992
     >>> solution(40000)
     39893
+    >>> solution(10000)
+    -1
     """
     answer = 0
     for i in range(999, 99, -1):  # 3 digit numbers range from 999 down to 100
         for j in range(999, 99, -1):
-            t = str(i * j)
-            if t == t[::-1] and i * j < n:
+            product_string = str(i * j)
+            if product_string == product_string[::-1] and i * j < n:
                 answer = max(answer, i * j)
-    return answer
+    return -1 if answer == 0 else answer
 
 
 if __name__ == "__main__":
