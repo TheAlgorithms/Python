369cb25ec2a83d55a0f4dde2eab7f6ac5dc2aac6
LAPTOP-PIGVD7CM\Komal <99kkaur@gmail.com>
2020-10-06 12:45:12 +0530

    using trim mean

--- maths/trim_mean.py
+++ maths/trim_mean.py
@@ -1,6 +1,36 @@
-from statistics import mean
 from scipy import stats
-marks=[1,2,3,4,5,34,25,34,6,5,3,23,34,23,12,45,6,78,98,76,87,98,45,34,56,78,65,78,65,52]
+marks = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    34,
+    25,
+    34,
+    6,
+    5,
+    3,
+    23,
+    34,
+    23,
+    12,
+    45,
+    6,
+    78,
+    98,
+    76,
+    87,
+    98,
+    45,
+    34,
+    56,
+    78,
+    65,
+    78,
+    65,
+    52
+    ]
 print(len(marks))
-m=stats.trim_mean(marks,0.02)
+m = stats.trim_mean(marks, 0.02)
 print(m)
