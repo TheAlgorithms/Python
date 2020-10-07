147f6f719f2596ae2094c14e6de83cf6ea31a300
Abhishek Maletha <67141747+Abhishek-photon@users.noreply.github.com>
2020-10-07 15:34:15 +0530

    Update sigmoid.py

--- maths/sigmoid.py
+++ maths/sigmoid.py
@@ -3,7 +3,6 @@ This is a sigmoid function
 x i function sigmoid is function variable
 a is the gain
 https://en.wikipedia.org/wiki/Sigmoid_function
-
 """
 
 import math
@@ -13,10 +12,10 @@ def sigmoid(x, a):
 
     """
     Returns value corresponding to sigmoid function.
-    
+
     >>> sigmoid(0.5, 1)
     0.6224593312018546
-    >>> add(2, 0.5)
+    >>> sigmoid(2, 0.5)
     0.7310585786300049
     """
     p = math.exp(-a * x)
@@ -26,4 +25,4 @@ def sigmoid(x, a):
 if __name__ == "__main__":
     import doctest
 
-    doctest.testmod()
+    doctest.testmod(verbose=True)
