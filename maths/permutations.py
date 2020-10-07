dcbfc29a20d95ed6ac353d5596b2cdb3911ac0b6
InMDev <inzaghi.moniaga@gmail.com>
2020-10-07 13:11:03 +0000

    permutations

---
+++ maths/permutations.py
@@ -0,0 +1,17 @@
+#nCr = n! / (n - r)! https://en.wikipedia.org/wiki/Permutation
+
+#Calculate factorial (!)
+def fact(n):
+    count = 1
+    for i in range (2, n+1):
+        count *= i
+    return count
+
+#Calculate nPr (Permutations)
+def nPr(n, r):
+    return (fact(n) / fact(n - r))
+
+#Asking for n & r value
+n = int(input("n = "))
+r = int(input("r = "))
+print(int(nPr(n, r)))  
