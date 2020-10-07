99a04861032532c25fb0d640361784d6eb1eada8
Wen Hong <lamwenhong@yahoo.com.sg>
2020-10-08 00:41:26 +0800

    remove solution arrows

--- project_euler/problem_37/sol1.py
+++ project_euler/problem_37/sol1.py
@@ -89,10 +89,8 @@ def compute_truncated_primes(count: int = 11) -> list[int]:
 def solution() -> int:
     """
     Returns the sum of truncated primes
-    >>> solution()
-    748317
     """
     return sum(compute_truncated_primes(11))
 
 if __name__ == "__main__":
-    print(f"{sum(compute_truncated_primes(11)) = }")
+     print(f"{sum(compute_truncated_primes(11)) = }")
