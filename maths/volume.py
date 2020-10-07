410e484ea4eeef2d222129a32ecde9859388bb8d
Sabari Ganesh <64348740+SabariGanesh-K@users.noreply.github.com>
2020-10-07 20:14:30 +0530

    Update volume.py

--- maths/volume.py
+++ maths/volume.py
@@ -138,7 +138,7 @@ def main():
     print("Pyramid: " + str(vol_pyramid(2, 2)))  # ~= 1.33
     print("Sphere: " + str(vol_sphere(2)))  # ~= 33.5
     print("Circular Cylinder: " + str(vol_circular_cylinder(2, 2)))  # ~= 25.1
-    print("Trapezoidal Prism: " + str(vol_trapezoid_prism(2, 2, 2, 2))) # = 8
+    print("Trapezoidal Prism: " + str(vol_trapezoid_prism(2, 2, 2, 2)))  # = 8
 
 
 if __name__ == "__main__":
