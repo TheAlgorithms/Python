e6cac3137c714d046627511d0241f96604fec4f2
nstoik <nelsonstoik@gmail.com>
2020-10-06 22:20:39 -0600

    add full problem statement and solution function with typehint and doctest

--- project_euler/problem_26/sol1.py
+++ project_euler/problem_26/sol1.py
@@ -1,6 +1,24 @@
 """
 Euler Problem 26
 https://projecteuler.net/problem=26
+
+Problem Statement:
+
+A unit fraction contains 1 in the numerator. The decimal representation of the
+unit fractions with denominators 2 to 10 are given:
+
+1/2	= 	0.5
+1/3	= 	0.(3)
+1/4	= 	0.25
+1/5	= 	0.2
+1/6	= 	0.1(6)
+1/7	= 	0.(142857)
+1/8	= 	0.125
+1/9	= 	0.(1)
+1/10	= 	0.1
+Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be
+seen that 1/7 has a 6-digit recurring cycle.
+
 Find the value of d < 1000 for which 1/d contains the longest recurring cycle
 in its decimal fraction part.
 """
@@ -35,8 +53,22 @@ def find_digit(numerator: int, digit: int) -> int:
     return the_digit
 
 
-# Tests
-if __name__ == "__main__":
-    import doctest
+def solution(n: int = 1000) -> int:
+    """
+    Find the value of n < 1000 for which 1/n contains the longest recurring cycle
+    in its decimal fraction part.
 
-    doctest.testmod()
+    >>> solution(10)
+    7
+    >>> solution(100)
+    97
+    >>> solution(1000)
+    983
+
+    """
+
+    return find_digit(1, n)
+
+
+if __name__ == "__main__":
+    print(solution(int(str(input()).strip())))
