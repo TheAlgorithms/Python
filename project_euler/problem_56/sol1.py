2dacfdba676f0c0359613859ca8130d073a01617
Dhruv <dhruvmanila@gmail.com>
2020-10-08 18:59:00 +0530

    Update sol1.py

--- project_euler/problem_56/sol1.py
+++ project_euler/problem_56/sol1.py
@@ -1,4 +1,16 @@
-def solution(a: int, b: int) -> int:
+"""
+Project Euler Problem 56: https://projecteuler.net/problem=56
+
+A googol (10^100) is a massive number: one followed by one-hundred zeros; 
+100^100 is almost unimaginably large: one followed by two-hundred zeros. 
+Despite their size, the sum of the digits in each number is only 1.
+
+Considering natural numbers of the form, ab, where a, b < 100, 
+what is the maximum digital sum?
+"""
+
+
+def solution(a: int = 100, b: int = 100) -> int:
     """
     Considering natural numbers of the form, a**b, where a, b < 100,
     what is the maximum digital sum?
