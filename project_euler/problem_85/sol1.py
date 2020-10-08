c92327214209c0a989d0c1b7e364ebccfeb7fd6f
osoba <osoba@users.noreply.github.com>
2020-10-08 06:46:32 +0200

    add problem 85

---
+++ project_euler/problem_85/sol1.py
@@ -0,0 +1,18 @@
+import math
+
+def solution() -> int:
+  goal = 8000000
+  root = int(math.sqrt(8000000)+1)
+  distance, resa, resb = goal, 0, 0
+
+  for a in range(1, root+1):
+    for b in range(1, math.ceil(root/a)+1):
+      current_distance = abs((a*b+a)*(a*b+b) - goal)
+      if current_distance < distance:
+        distance = current_distance
+        resa, resb = a, b
+
+  return resa*resb
+
+if __name__ == "__main__":
+    print("Area of the grid is ", solution())
