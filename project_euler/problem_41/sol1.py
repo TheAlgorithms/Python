0c3c157ebcfcb58253155b96a1f8201ec13a7d6c
joan.rosellr <joan.rosellr@gmail.com>
2020-10-08 08:38:52 +0200

    update solution to return the actual solution instead of a list
    Signed-off-by: joan.rosellr <joan.rosellr@gmail.com>

--- project_euler/problem_41/sol1.py
+++ project_euler/problem_41/sol1.py
@@ -36,20 +36,21 @@ def is_prime(n: int) -> bool:
     return True
 
 
-def solution(n: int = 7) -> list[int]:
+def solution(n: int = 7) -> int:
     """
     Returns a list of all pandigital prime numbers of length n.
     >>> solution(2)
-    []
-    >>> max(solution(4))
+    0
+    >>> solution(4)
     4231
-    >>> max(solution(7))
+    >>> solution(7)
     7652413
     """
     pandigital_str = "".join(str(i) for i in range(1, n + 1))
     perm_list = [int("".join(i)) for i in permutations(pandigital_str, n)]
-    return [num for num in perm_list if is_prime(num)]
+    pandigitals = [num for num in perm_list if is_prime(num)]
+    return max(pandigitals) if pandigitals else 0
 
 
 if __name__ == "__main__":
-    print(f"{max(solution()) = }")
+    print(f"{solution() = }")
