389e281b368022e085322a4a159eb1c21e5642d8
fa1l <fa1l@users.noreply.github.com>
2020-10-08 11:46:01 +0500

    improvements for project euler task 56

--- project_euler/problem_56/sol1.py
+++ project_euler/problem_56/sol1.py
@@ -1,33 +1,36 @@
+"""
+https://projecteuler.net/problem=56
+
+A googol (10**100) is a massive number: one followed by one-hundred zeros;
+100**100 is almost unimaginably large: one followed by two-hundred zeros.
+Despite their size, the sum of the digits in each number is only 1.
+
+Considering natural numbers of the form, ab, where a, b < 100, what is the maximum digital sum?
+"""
+
+
 def maximum_digital_sum(a: int, b: int) -> int:
     """
-    Considering natural numbers of the form, a**b, where a, b < 100,
-    what is the maximum digital sum?
-    :param a:
-    :param b:
-    :return:
+    Returns the maximum from the list of SUMs of the list of INT converted from STR of BASE raised to the POWER
     >>> maximum_digital_sum(10,10)
     45
-
     >>> maximum_digital_sum(100,100)
     972
-
     >>> maximum_digital_sum(100,200)
     1872
     """
 
-    # RETURN the MAXIMUM from the list of SUMs of the list of INT converted from STR of
-    # BASE raised to the POWER
     return max(
-        [
-            sum([int(x) for x in str(base ** power)])
-            for base in range(a)
-            for power in range(b)
-        ]
+        sum(int(x) for x in str(base ** power))
+        for base in range(a)
+        for power in range(b)
     )
 
 
-# Tests
-if __name__ == "__main__":
-    import doctest
+def solution(a: int = 100, b: int = 100) -> int:
+    """Returns maximum digital sum"""
+    return maximum_digital_sum(a, b)
 
-    doctest.testmod()
+
+if __name__ == "__main__":
+    print(f"{solution(100, 100)}")
