654beeda61812140f51d5b42c7f5f8c0db98655c
Meysam Azad <MeysamAzad81@yahoo.com>
2020-10-09 16:13:38 +0330

    graphs/kruskal: fix max-line-length violation

--- graphs/minimum_spanning_tree_kruskal.py
+++ graphs/minimum_spanning_tree_kruskal.py
@@ -1,7 +1,9 @@
 from typing import List, Tuple
 
 
-def kruskal(num_nodes: int, num_edges: int, edges: List[Tuple[int, int, int]]) -> int:
+def kruskal(
+    num_nodes: int, num_edges: int, edges: List[Tuple[int, int, int]]
+) -> int:
     """
     >>> kruskal(4, 3, [(0, 1, 3), (1, 2, 5), (2, 3, 1)])
     [(2, 3, 1), (0, 1, 3), (1, 2, 5)]
@@ -9,7 +11,8 @@ def kruskal(num_nodes: int, num_edges: int, edges: List[Tuple[int, int, int]]) -
     >>> kruskal(4, 5, [(0, 1, 3), (1, 2, 5), (2, 3, 1), (0, 2, 1), (0, 3, 2)])
     [(2, 3, 1), (0, 2, 1), (0, 1, 3)]
 
-    >>> kruskal(4, 6, [(0, 1, 3), (1, 2, 5), (2, 3, 1), (0, 2, 1), (0, 3, 2), (2, 1, 1)])
+    >>> kruskal(4, 6, [(0, 1, 3), (1, 2, 5), (2, 3, 1), (0, 2, 1), (0, 3, 2),
+    ... (2, 1, 1)])
     [(2, 3, 1), (0, 2, 1), (2, 1, 1)]
     """
     edges = sorted(edges, key=lambda edge: edge[2])
