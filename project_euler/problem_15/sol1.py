396d59a5ab993b5b44803bf79892a364b319cb48
Dhruv <dhruvmanila@gmail.com>
2020-10-09 08:38:05 +0530

    Update sol1.py

--- project_euler/problem_15/sol1.py
+++ project_euler/problem_15/sol1.py
@@ -1,4 +1,6 @@
 """
+Problem 15: https://projecteuler.net/problem=15
+
 Starting in the top left corner of a 2×2 grid, and only being able to move to
 the right and down, there are exactly 6 routes to the bottom right corner.
 How many such routes are there through a 20×20 grid?
