24ec0b2d62c793c127c52f1cbe9984ed6b85bfe9
RGHV122 <46723077+RGHV122@users.noreply.github.com>
2020-10-09 23:26:36 +0530

    Update coin_change_greedy.py

--- greedy_method/coin_change_greedy.py
+++ greedy_method/coin_change_greedy.py
@@ -1,4 +1,13 @@
 def coin_change_greedy(n,coins):
+  """returns minimum coin needed for change with greedy"""
+  '''
+  >>Enter Target: 10
+  Enter Available Coins: 1 5 3 7
+  7 3 
+  >>Enter Target: 44
+  Enter Available Coins: 1 10 20 2
+  20 20 2 2
+      '''
   i = 0
   coins = sorted(coins,reverse=True)
   if(n<0):
