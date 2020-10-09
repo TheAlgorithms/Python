46ffc5d20a75eaab47f696c214e05d6c4506cd6e
cybov <brainiac2244@gmail.com>
2020-10-09 18:16:02 +0100

    added to the conversions folder a file to convert decimals to binary using recursion method

--- conversions/decimal_to_binary_using_recursion.py
+++ conversions/decimal_to_binary_using_recursion.py
@@ -1,43 +1,31 @@
-"""
-Converts decimal number to its binary equivalent using recursion
-~$ python Decimal_to_binary_using_recursion.py
-Enter a decimal number to be converted to binary form: 0
-'0b0'
-~$ python Decimal_to_binary_using_recursion.py
-Enter a decimal number to be converted to binary form: 40
-'0b101000'
-~$ python Decimal_to_binary_using_recursion.py
-Enter a decimal number to be converted to binary form: -40
-'-0b101000'
-~$ python Decimal_to_binary_using_recursion.py
-Enter a decimal number to be converted to binary form: forty
-'The value entered is not a decimal number'
-
-"""
-import sys
-
+# This function converts positive integer values to their binary equivalent
 def bin_recursive(decimal: int) -> str:
     """
-    Converts a positive integer decimal number to its binary equivalent using recursion
-    >>>bin_recursive(0)
-    '0b0'
-    >>>bin_recursive(40)
-    '0b101000'
+    Converts a positive integer decimal number to its binary equivalent using recursion.
+    The funtion takes in an integer number via the parameter "decimal" and returns a string.
     """
-
+    # Initialize exit base of the recursion function
     if decimal == 1 or decimal == 0: return str(decimal)
     
     half = decimal // 2
     remainder = decimal % 2
     return bin_recursive(half) + str(remainder)
-    
-try:
-    number = int(input("Enter a decimal number to be converted to binary form: ").strip())
-except:
-    print("The value entered is not a decimal number")
-    sys.exit()
 
-if number < 0:
-    number = -number
-    print("-0b" + bin_recursive(number))
-else: print("0b" + bin_recursive(number))
+# This funtion handles wrong inputs, calls the funtion above, adds "0b" & "-0b" to positive and negative decimal inputs respectively and then prints out the output.
+def main(number) -> str:
+    try:
+        # try converting number to an integer
+        number = int(number)
+    except Exception:
+        # Handle exception raised and return nothing
+        print("Input value is not an integer")
+        return
+    
+    if number < 0:
+        number = -number
+        print("-0b" + bin_recursive(number))
+    else: print("0b" + bin_recursive(number))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
