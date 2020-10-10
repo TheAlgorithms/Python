c4ce596239123bf7f4bc51d0b716b887599e92c5
jck <ashishdwivedi086@gmail.com>
2020-10-10 17:54:42 +0530

    reformated problem_114 in project_euler

--- project_euler/problem_114/sol1.py
+++ project_euler/problem_114/sol1.py
@@ -1,15 +1,17 @@
-#Project Euler Problem 114
+# Project Euler Problem 114
+
 
 def solution(n):
 
-    #min block size
+    # min block size
     m = 3
 
-    ways = [1]*(m) + [0]*(n-m+1)
-    for k in range(m, n+1):
-        ways[k] = ways[k-1] + sum(ways[:k-m]) + 1
-    
+    ways = [1] * (m) + [0] * (n - m + 1)
+    for k in range(m, n + 1):
+        ways[k] = ways[k - 1] + sum(ways[: k - m]) + 1
+
     return ways[n]
+
 
 if __name__ == "__main__":
     from doctest import testmod
