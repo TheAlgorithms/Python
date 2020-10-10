c3afb3d4931b4670a4d91c2bfe87c795e8c9ae24
mateuszkowalke <mateusz.kowalke@gmail.com>
2020-10-10 17:11:55 +0200

    File now passes flake8 test.

--- project_euler/problem_38/sol1.py
+++ project_euler/problem_38/sol1.py
@@ -5,11 +5,16 @@ Take the number 192 and multiply it by each of 1, 2, and 3:
 192 × 1 = 192
 192 × 2 = 384
 192 × 3 = 576
-By concatenating each product we get the 1 to 9 pandigital, 192384576. We will call 192384576 the concatenated product of 192 and (1,2,3)
+By concatenating each product we get the 1 to 9 pandigital, 192384576.
+We will call 192384576 the concatenated product of 192 and (1,2,3)
 
-The same can be achieved by starting with 9 and multiplying by 1, 2, 3, 4, and 5, giving the pandigital, 918273645, which is the concatenated product of 9 and (1,2,3,4,5).
+The same can be achieved by starting with 9
+and multiplying by 1, 2, 3, 4, and 5, giving the pandigital, 918273645,
+which is the concatenated product of 9 and (1,2,3,4,5).
 
-What is the largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer with (1,2, ... , n) where n > 1?
+What is the largest 1 to 9 pandigital 9-digit number
+that can be formed as the concatenated product of
+an integer with (1,2, ... , n) where n > 1?
 """
 
 
