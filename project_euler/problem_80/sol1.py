24c3b7ec9c69d690d3e35812b8468c51a29c4b09
gupta41sandeep@gmail.com <gupta41sandeep@gmail.com>
2020-10-10 16:27:29 +0530

    updating sol as per comments

--- project_euler/problem_80/sol1.py
+++ project_euler/problem_80/sol1.py
@@ -1,31 +1,37 @@
 """
-    Euler Problem : 80
-    @author : Sandeep Gupta
-    @time   : 5 October 2020, 18:30
-    @Solution: Used decimal python module to calculate the decimal
-    places up to 100, the most important thing would be take calculate
-    a few extra places for decimal otherwise there will be rounding
-    error.
-    @answer     : 40886
+Euler Problem : 80
+author : Sandeep Gupta
+problem : For the first one hundred natural numbers, find the total of
+the digital sums of the first one hundred decimal digits for all the irrational
+square roots.
+time   : 5 October 2020, 18:30
 """
 import decimal
 
 
 def solution() -> int:
+    """
+    To evaluate the sum, Used decimal python module to calculate the decimal
+    places up to 100, the most important thing would be take calculate
+    a few extra places for decimal otherwise there will be rounding
+    error.
+
+    >>> solution()
+    40886
+    """
     answer = 0
-    dot100 = decimal.Context(prec=105)
+    decimal_context = decimal.Context(prec=105)
     for i in range(2, 100):
         number = decimal.Decimal(i)
-        sqrt_number = number.sqrt(dot100)
+        sqrt_number = number.sqrt(decimal_context)
         if len(str(sqrt_number)) > 1:
             answer += int(str(sqrt_number)[0])
             sqrt_number = str(sqrt_number)[2:101]
             answer += sum([int(x) for x in sqrt_number])
-        return answer
+    return answer
 
 
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
-    print(solution())
