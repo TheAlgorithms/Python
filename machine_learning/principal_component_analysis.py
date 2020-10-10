7235916b731ed9cc61613fdb1eed2a1a25136a9a
duy quang <hyouka266@gmail.com>
2020-10-11 01:19:53 +0700

    @cdefga Added principal_component_analysis.py

--- machine_learning/principal_component_analysis.py
+++ machine_learning/principal_component_analysis.py
@@ -1,12 +1,12 @@
 """
 Principal Component Analysis, or PCA, is a dimensionality-reduction method
-that is often used to reduce the dimensionality of large data sets, 
-by transforming a large set of variables into a smaller one that still 
-contains most of the information in the large set. Reducing the number of variables 
-of a data set naturally comes at the expense of accuracy, but the trick in 
-dimensionality reduction is to trade a little accuracy for simplicity. 
+that is often used to reduce the dimensionality of large data sets,
+by transforming a large set of variables into a smaller one that still
+contains most of the information in the large set. Reducing the number of variables
+of a data set naturally comes at the expense of accuracy, but the trick in
+dimensionality reduction is to trade a little accuracy for simplicity.
 Because smaller data sets are easier to explore and visualize and make analyzing
-data much easier and faster for machine learning algorithms without extraneous 
+data much easier and faster for machine learning algorithms without extraneous
 variables to process.
 """
 
@@ -20,7 +20,7 @@ def center(data: np.ndarray) -> np.ndarray:
 
     >>> center(np.array([[0.5, 1], [1,4]]))
     array([[-0.25, -1.5 ],
-        [ 0.25,  1.5 ]])
+           [ 0.25,  1.5 ]])
     """
     N = data.shape[0]
     mean = np.sum(data, axis=0) / N
@@ -33,7 +33,7 @@ def pca(data: np.ndarray, n_components=2) -> np.ndarray:
     >>> data = np.array([[0.5, 1], [1,4]])
     >>> pca(data, 1)
     array([[-1.52069063],
-        [ 1.52069063]])
+           [ 1.52069063]])
     """
     X = center(data)  # center the data
     N = X.shape[0]  # number of observations
