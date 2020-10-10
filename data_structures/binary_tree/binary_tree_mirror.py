3bf3c6e69bfd37c7c55235a7cbb5fff60a25d3b8
svedire <VedireSusmith_Reddy@intuit.com>
2020-10-10 21:57:04 +0530

    Added binary tree mirror algorithm

---
+++ data_structures/binary_tree/binary_tree_mirror.py
@@ -0,0 +1,37 @@
+# Created by susmith98
+
+
+"""
+Problem Description:
+Given a binary tree, return it's mirror.
+"""
+
+
+def binaryTreeMirror(root: int, binary_tree: dict = {}, binary_tree_mirror: dict = {}):
+    """
+    >>> binaryTreeMirror(1, { 1: [2,3], 2: [4,5], 3: [6,7], 7: [8,9]})
+    {1: [3, 2], 2: [5, 4], 3: [7, 6], 7: [9, 8]}
+    >>> binaryTreeMirror(5, { 1: [2,3], 2: [4,5], 3: [6,7], 7: [8,9]})
+    {}
+    >>> binaryTreeMirror\
+    (1, { 1: [2,3], 2: [4,5], 3: [6,7], 4: [10,11], 5: [12,13],7: [8,9]})
+    {1: [3, 2], 2: [5, 4], 4: [11, 10], 5: [13, 12], 3: [7, 6], 7: [9, 8]}
+    """
+    if root is None or root not in binary_tree.keys():
+        return
+    left_child = binary_tree[root][0]
+    right_child = binary_tree[root][1]
+    binary_tree_mirror[root] = [right_child, left_child]
+    binaryTreeMirror(left_child, binary_tree, binary_tree_mirror)
+    binaryTreeMirror(right_child, binary_tree, binary_tree_mirror)
+
+
+if __name__ == "__main__":
+    binary_tree = {1: [2, 3], 2: [4, 5], 3: [6, 7], 7: [8, 9]}
+    root = 1
+    print("Binary tree:", sep=" ")
+    print(binary_tree)
+    binary_tree_mirror = {}
+    binaryTreeMirror(root, binary_tree, binary_tree_mirror)
+    print(" binary tree mirror:", sep=" ")
+    print(binary_tree_mirror)
