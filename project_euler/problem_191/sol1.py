a44e1934804eabc2c83826932244c5cd7e42ec60
Michael D <michael@drueing.de>
2020-10-10 19:33:13 +0200

    Address requested changes
    - update documentation
    - split out helper function but mark it with an underscore
    - remove redundant comments or make them more explicit/helpful

--- project_euler/problem_191/sol1.py
+++ project_euler/problem_191/sol1.py
@@ -26,8 +26,74 @@ References:
 """
 
 
+# a cache to speed up calculation
+cache = {}
+
+
+def _calculate(days: int, absent: int, late: int) -> int:
+    """
+    A small helper function for the recursion, mainly to have
+    a clean interface for the solution() function below.
+
+    It should get called with the number of days (corresponding
+    to the desired length of the 'prize strings'), and the
+    initial values for the number of consecutive absent days and
+    number of total late days.
+
+    >>> _calculate(days=4, absent=0, late=0)
+    43
+    >>> _calculate(days=30, absent=2, late=0)
+    0
+    >>> _calculate(days=30, absent=1, late=0)
+    98950096
+    """
+
+    # if we are absent twice, or late 3 consecutive days,
+    # no further prize strings are possible
+    if late == 3 or absent == 2:
+        return 0
+
+    # if we have no days left, and have not failed any other rules,
+    # we have a prize string
+    if days == 0:
+        return 1
+
+    # No easy solution, so now we need to do the recursive calculation
+
+    # First, check if the combination is already in the cache, and
+    # if yes, return the stored value from there since we already
+    # know the number of possible prize strings from this point on
+    key = (days, absent, late)
+    if key in cache:
+        return cache[key]
+
+    # now we calculate the three possible ways that can unfold from
+    # this point on, depending on our attendance today
+
+    # 1) if we are late (but not absent), the "absent" counter stays as
+    # it is, but the "late" counter increases by one
+    state_late = _calculate(days - 1, absent, late + 1)
+
+    # 2) if we are absent, the "absent" counter increases by 1, and the
+    # "late" counter resets to 0
+    state_absent = _calculate(days - 1, absent + 1, 0)
+
+    # 3) if we are on time, this resets the "late" counter and keeps the
+    # absent counter
+    state_ontime = _calculate(days - 1, absent, 0)
+
+    prizestrings = state_late + state_absent + state_ontime
+
+    # keep the value we calculated in the cache, so that later we do not
+    # need to recurse again. This is the main improvement which brings
+    # runtime down from seconds/minutes to usually below 1 second.
+    cache[key] = prizestrings
+    return prizestrings
+
+
 def solution(days: int = 30) -> int:
-    """Returns the number of possible prize strings for a particular number
+    """
+    Returns the number of possible prize strings for a particular number
     of days, using a simple recursive function with caching to speed it up.
 
     >>> solution()
@@ -36,51 +102,9 @@ def solution(days: int = 30) -> int:
     43
     """
 
-    # a cache to speed up calculation
-    cache = {}
-
-    # we will be using a simple recursive function
-    def calculate(days: int, absent: int, late: int) -> int:
-        # if we are absent twice, or late 3 consecutive days,
-        # no further prize strings are possible
-        if late == 3 or absent == 2:
-            return 0
-
-        # if we have no days left, and have not failed any other rules,
-        # we have a prize string
-        if days == 0:
-            return 1
-
-        # No easy solution, so now we need to do the recursive calculation
-
-        # First, check if the combination is already in the cache, and
-        # if yes, return the stored value from there since we already
-        # know the number of possible prize strings from this point on
-        key = (days, absent, late)
-        if key in cache:
-            return cache[key]
-
-        # if we are late (but not absent), the "absent" counter stays as
-        # it is, but the "late" counter increases by one
-        state_late = calculate(days - 1, absent, late + 1)
-
-        # if we are absent, the "absent" counter increases by 1, and the
-        # "late" counter resets to 0
-        state_absent = calculate(days - 1, absent + 1, 0)
-
-        # if we are on time, this resets the "late" counter and keeps the
-        # absent counter
-        state_ontime = calculate(days - 1, absent, 0)
-
-        # the total number of prize strings is the sum of these three
-        # possible states
-        prizestrings = state_late + state_absent + state_ontime
-
-        # now store the value we calculated in the cache
-        cache[key] = prizestrings
-        return prizestrings
-
-    return calculate(days, 0, 0)
+    # we call the calculate function above, with initially
+    # no absent days and no late days.
+    return _calculate(days, absent=0, late=0)
 
 
 if __name__ == "__main__":
