927e14e7f2698ea4ac4525161ca5afbf351f22ab
Carlos Meza <hire@carlosmeza.com>
2020-10-09 22:33:00 -0700

    Cleanup Project Euler Problem 01 (#2900)
    * mv str statement into docstr
    
    * rename var to avoid redefining builtin
    
    * clean up module docstr

--- project_euler/problem_01/sol5.py
+++ project_euler/problem_01/sol5.py
@@ -1,15 +1,14 @@
 """
 Problem Statement:
 If we list all the natural numbers below 10 that are multiples of 3 or 5,
-we get 3,5,6 and 9. The sum of these multiples is 23.
+we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below N.
 """
-
-"""A straightforward pythonic solution using list comprehension"""
 
 
 def solution(n: int = 1000) -> int:
     """Returns the sum of all the multiples of 3 or 5 below n.
+       A straightforward pythonic solution using list comprehension.
 
     >>> solution(3)
     0
