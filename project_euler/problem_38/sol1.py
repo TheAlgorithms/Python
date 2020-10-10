e5922312d2181c9f54d115f16aaae4f65fdc6b3d
mateuszkowalke <mateusz.kowalke@gmail.com>
2020-10-10 16:43:39 +0200

    Fix styling issue

--- project_euler/problem_38/sol1.py
+++ project_euler/problem_38/sol1.py
@@ -41,7 +41,7 @@ def is_pandigital(num):
 
 def solution():
     for num in range(9487, 9233, -1):
-        num_to_check = int(str(num * 1)+str(num * 2))
+        num_to_check = int(str(num * 1) + str(num * 2))
         if is_pandigital(num_to_check):
             return num_to_check
 
