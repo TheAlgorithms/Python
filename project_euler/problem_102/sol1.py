738e93036db31dc196a1dab2bef613fe10c366b7
pastre <pastre68@gmail.com>
2020-10-11 02:43:13 -0300

    Properly commented everything

--- project_euler/problem_102/sol1.py
+++ project_euler/problem_102/sol1.py
@@ -22,32 +22,54 @@ NOTE: The first two examples in the file represent
 """
 
 
-# Class to represent a point with x and y coordinates
+# Class representing a 2D point with x and y coordinates
 class Point:
     def __init__(self, x, y):
         self.x = float(x)
         self.y = float(y)
 
-    # Calculates the difference between two points
     def minus(self, p):
+        """ Returns the difference between self and another 2D point
+
+        >>> p = Point(2, 3)
+        >>> p.minus( Point(4, 5))
+        p.x == 2 and p.y == 2
+        """
+
         return Point(p.x - self.x, p.y - self.y)
 
     def __str__(self):
         return f"( {self.x}, {self.y})"
+    def __unicode__(self):
+        return f"( {self.x}, {self.y})"
+    def __repr__(self):
+        return f"( {self.x}, {self.y})"
 
 
-# Calculates the distance between p1 and p2, or one side of a triangle
 def side(p1, p2):
+    """ Returns the distance between p1 and p2
+
+    >>> cross_product(Point(0, 3), Point(4, 0))
+    5.0
+    """
     return (abs(p2.x - p1.x) ** 2.0 + abs(p2.y - p1.y) ** 2.0) ** 0.5
 
 
-# Calculates the cross product between two points
 def cross_product(p1, p2):
+    """ Returns the cross product between two 2D points
+
+    >>> cross_product(Point(1, 2), Point(3, 4))
+    -2.0
+    """
     return p1.x * p2.y - p1.y * p2.x
 
-
-# Calculates the area of an triangle with vertices on points p1, p2, p3
 def area(p1, p2, p3):
+    """ Returns the area of an triangle of vertices on points p1, p2, p3
+
+    >>> area(Point(1, 2), Point(-2, 3), Point(3, 4))
+    4.000000000000002
+    """
+
     a = side(p1, p2)
     b = side(p2, p3)
     c = side(p3, p1)
@@ -60,9 +82,15 @@ def area(p1, p2, p3):
     s = (a + b + c) / 2.0
     return (s * (s - a) * (s - b) * (s - c)) ** 0.5
 
-
-# Returns wheter a triangle contains (0, 0) or not
 def triangle_contains_origin(triangle):
+    """ Returns wheter a triangle contains (0, 0) or not
+
+    >>> triangle_contains_origin([Point(-175,41), Point(-421,-714), Point(574,-645)])
+    False
+
+    >>> triangle_contains_origin([Point(-340,495), Point(-153,-910), Point(835,-947)])
+    True
+    """
 
     p1, p2, p3 = triangle
     p0 = Point(0.0, 0.0)
@@ -77,17 +105,26 @@ def triangle_contains_origin(triangle):
     return round(big_area, 5) == round(sum(smaller_areas), 5)
 
 
-# Loads the triangles from the file provided by Project Euler
+def build_triangle(fromStr):
+    """ Returns an array of 2D Points composing a triangle from a comma separated string
+
+    >>> len(build_triangle('-340,495,-153,-910,835,-947'))
+    3
+    """
+    splitted = fromStr.split(",")
+    asInt = [float(i) for i in splitted]
+    triangle = []
+    for i in range(0, 6, 2):
+        new_point = Point(asInt[i], asInt[i + 1])
+        triangle.append(new_point)
+    return triangle
+
 def triangles_from_file(fileName):
-    # Builds 3 vertices from a line
-    def build_triangle(fromStr):
-        splitted = fromStr.split(",")
-        asInt = [float(i) for i in splitted]
-        triangle = []
-        for i in range(0, 6, 2):
-            new_point = Point(asInt[i], asInt[i + 1])
-            triangle.append(new_point)
-        return triangle
+    """ Returns an array of triangles from the file provided by Project Euler
+
+    >>> len(triangles_from_file('triangles_euler102.txt'))
+    1000
+    """
 
     data = open(fileName, "r").read()
     tests = data.split("\n")
@@ -97,6 +134,11 @@ def triangles_from_file(fileName):
 
 
 def count_triangles():
+    """ Returns the count of triangles that contains the origin
+    
+    >>> count_triangles()
+    228
+    """
     triangles = triangles_from_file("triangles_euler102.txt")
 
     return len([True for i in triangles if triangle_contains_origin(i)])
