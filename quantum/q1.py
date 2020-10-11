e05c4d38693daf98b87ba5ce09d014d1d8a6b336
Abhishek Jaisingh <abhi2254015@gmail.com>
2020-10-11 23:35:17 +0530

    Address Review Comments

--- quantum/q1.py
+++ quantum/q1.py
@@ -1,28 +1,41 @@
+#!/usr/bin/env python3
+"""
+Build a simple bare-minimum quantum circuit that starts with a single 
+qubit (by default, in state 0), runs the experiment 1000 times, and 
+finally prints the total count of the states finally observed.
+Qiskit Docs: https://qiskit.org/documentation/getting_started.html
+"""
+
+
 import qiskit as q
 
-"""
-Build a simple bare-minimum quantum
-circuit that starts with a single qubit
-(by default in state 0), runs the experiment
-1000 times, and finally prints the total 
-count of the states finally observed.
-"""
 
-# Use Aer's qasm_simulator
-simulator = q.Aer.get_backend('qasm_simulator')
+def single_qubit_measure() -> q.result.counts.Counts:
+	"""
+	>>> single_qubit_measure()
+	{'0': 1000}
+	"""
+	# Use Aer's qasm_simulator
+	simulator = q.Aer.get_backend('qasm_simulator')
 
-# Create a Quantum Circuit acting on the q register
-circuit = q.QuantumCircuit(1, 1)
+	# Create a Quantum Circuit acting on the q register
+	circuit = q.QuantumCircuit(1, 1)
 
-# Map the quantum measurement to the classical bits
-circuit.measure([0], [0])
+	# Map the quantum measurement to the classical bits
+	circuit.measure([0], [0])
 
-# Execute the circuit on the qasm simulator
-job = q.execute(circuit, simulator, shots=1000)
+	# Execute the circuit on the qasm simulator
+	job = q.execute(circuit, simulator, shots=1000)
 
-# Grab results from the job
-result = job.result()
+	# Grab results from the job
+	result = job.result()
 
-# Returns counts
-counts = result.get_counts(circuit)
-print("\nTotal count for varopis staes are:", counts)
+	# Get the histogram data of an experiment.
+	counts = result.get_counts(circuit)
+
+	return counts
+
+
+if __name__ == '__main__':
+	counts = single_qubit_measure()
+	print("Total count for various states are:", counts)
