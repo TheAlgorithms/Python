e33c00678dbe4533bd817dae34c7f6a2d5271717
Diya Acharya <55196217+diyaacharya@users.noreply.github.com>
2020-10-12 00:33:28 +0530

    Update pearson_correlation.py

--- maths/pearson_correlation.py
+++ maths/pearson_correlation.py
@@ -2,9 +2,8 @@ import sys
 from scipy.stats import pearsonr 
   
 
-list1 = list(map(int,input("\nEnter the numbers in first list: ").strip().split()))
-list2 = list(map(int,input("\nEnter the numbers in second list : ").strip().split())) 
-
+list1 = [1,2,4,7,8]
+list2 = [3,7,8,7,90]
   
 # Apply the pearsonr() 
 corr, _ = pearsonr(list1, list2) 
