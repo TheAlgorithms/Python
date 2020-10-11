97582b50e9a5f58e0e0339ecafc0e2f24ca4273e
nishchalnishant <40529054+nishchalnishant@users.noreply.github.com>
2020-10-11 18:57:33 +0530

    solution for problem no. 116

--- project_euler/problem_116/sol1.py
+++ project_euler/problem_116/sol1.py
@@ -6,7 +6,6 @@ name :Red, green or blue tiles
 
 Since, we need to count total ways for 50 units of black 
 colored square tiles, say k = 50.
-
 Here, we are initializing our function solution() which holds 
 the logic of the solution to the problem.The function solution 
 has two parameters i = number of black coloured square tiles 
@@ -21,11 +20,11 @@ def solution(i, k):
     fifty units in length be replaced if colours cannot 
     be mixed and at least one coloured
     tile must be used?
-    >> > solution(2, 50)
+    >>> solution(2, 50)
     20365011073
-    >> > solution(3, 50)
+    >>> solution(3, 50)
     122106096
-    >> > solution(4, 50)
+    >>> solution(4, 50)
     5453760
     """
     ways = [1] * i + [0] * (k-i+1)
