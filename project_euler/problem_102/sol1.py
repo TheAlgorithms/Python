b0e3ce3767240f7cc311ebeeda8570100f9fc034
pastre <pastre68@gmail.com>
2020-10-11 02:01:04 -0300

    Adds problem description

--- project_euler/problem_102/sol1.py
+++ project_euler/problem_102/sol1.py
@@ -1,3 +1,21 @@
+"""
+Problem 102: https://projecteuler.net/problem=102
+
+Three distinct points are plotted at random on a Cartesian plane, for which -1000 ≤ x, y ≤ 1000, such that a triangle is formed.
+
+Consider the following two triangles:
+
+A(-340,495), B(-153,-910), C(835,-947)
+
+X(-175,41), Y(-421,-714), Z(574,-645)
+
+It can be verified that triangle ABC contains the origin, whereas triangle XYZ does not.
+
+Using triangles.txt (right click and 'Save Link/Target As...'), a 27K text file containing the co-ordinates of one thousand "random" triangles, find the number of triangles for which the interior contains the origin.
+
+NOTE: The first two examples in the file represent the triangles in the example given above.
+"""
+
 class Point:
     def __init__(self, x, y):
         self.x = float(x)
