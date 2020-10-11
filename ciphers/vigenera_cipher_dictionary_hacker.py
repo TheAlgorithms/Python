c91fe77aed220c9d8f4903ea961f8d45fa2ec956
RadadiyaMohit <30775542+radadiyamohit81@users.noreply.github.com>
2020-10-11 22:45:38 +0530

    created new cipher
    if you like my code, merge it and add the label as `hacktoberfest-accepted`

---
+++ ciphers/vigenera_cipher_dictionary_hacker.py
@@ -0,0 +1,36 @@
+import detectEnglish, vigenereCipher, pyperclip
+
+def main():
+    ciphertext = """Tzx isnz eccjxkg nfq lol mys bbqq I lxcz."""
+    hackedMessage = hackVigenere(ciphertext)
+
+    if hackedMessage != None:
+        print('Copying hacked message to clipboard:')
+        print(hackedMessage)
+        pyperclip.copy(hackedMessage)
+    else:
+        print('Failed to hack encryption.')
+
+
+def hackVigenere(ciphertext):
+    fo = open('dictionary.txt')
+    words = fo.readlines()
+    fo.close()
+
+    for word in words:
+        word = word.strip()
+        decryptedText = vigenereCipher.decryptMessage(word, ciphertext)
+        if detectEnglish.isEnglish(decryptedText, wordPercentage=40):
+            # Check with user to see if the decrypted key has been found.
+            print()
+            print('Possible encryption break:')
+            print('Key ' + str(word) + ': ' + decryptedText[:100])
+            print()
+            print('Enter D for done, or just press Enter to continue breaking:')
+            response = input('> ')
+
+            if response.upper().startswith('D'):
+                return decryptedText
+
+if __name__ == '__main__':
+    main()
