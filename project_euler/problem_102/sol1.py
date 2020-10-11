e6b0f65531cd06ff2adadae18e574e2904452de0
pastre <pastre68@gmail.com>
2020-10-11 02:07:49 -0300

    Added comments

--- project_euler/problem_102/sol1.py
+++ project_euler/problem_102/sol1.py
@@ -16,35 +16,34 @@ Using triangles.txt (right click and 'Save Link/Target As...'), a 27K text file 
 NOTE: The first two examples in the file represent the triangles in the example given above.
 """
 
+# Class to represent a point with x and y coordinates
 class Point:
     def __init__(self, x, y):
         self.x = float(x)
         self.y = float(y)
 
-    def isZero(self):
-        return self.x == 0 and self.y == 0
-
+    # Calculates the difference between two points   
     def minus(self, p):
         return Point(p.x - self.x, p.y - self.y)
 
     def __str__(self):
         return f"( {self.x}, {self.y})"
 
-
+# Calculates the distance between p1 and p2, or one side of a triangle
 def side(p1, p2):
     return (abs(p2.x - p1.x) ** 2.0 + abs(p2.y - p1.y) ** 2.0) ** 0.5
 
-
-def crossproduct(p1, p2):
+# Calculates the cross product between two points
+def cross_product(p1, p2):
     return p1.x * p2.y - p1.y * p2.x
 
-
+# Calculates the area of an triangle with vertices on points p1, p2, p3 
 def area(p1, p2, p3):
     a = side(p1, p2)
     b = side(p2, p3)
     c = side(p3, p1)
 
-    cp = crossproduct(p1.minus(p2), p1.minus(p3))
+    cp = cross_product(p1.minus(p2), p1.minus(p3))
 
     if cp == 0:
         return 0
@@ -52,8 +51,10 @@ def area(p1, p2, p3):
     s = (a + b + c) / 2.0
     return (s * (s - a) * (s - b) * (s - c)) ** 0.5
 
+# Returns wheter a triangle contains (0, 0) or not
+def triangle_contains_origin(triangle):
 
-def contains(p1, p2, p3):
+    p1, p2, p3 = triangle
     p0 = Point(0.0, 0.0)
 
     bigArea = area(p1, p2, p3)
@@ -65,13 +66,9 @@ def contains(p1, p2, p3):
 
     return round(bigArea, 5) == round(sum(smallerAreas), 5)
 
-
-def triangleContainsOrigin(triangle):
-    p1, p2, p3 = triangle
-    return contains(p1, p2, p3)
-
-
+# Loads the triangles from the file provided by Project Euler
 def triangles_from_file(fileName):
+    # Builds 3 vertices from a line
     def build_triangle(fromStr):
         splitted = fromStr.split(",")
         asInt = [float(i) for i in splitted]
@@ -88,10 +85,12 @@ def triangles_from_file(fileName):
     return triangles
 
 
-def run():
+def count_triangles():
     triangles = triangles_from_file("triangles_euler102.txt")
 
-    count = [True for i in triangles if triangleContainsOrigin(i)]
+    return len([True for i in triangles if triangle_contains_origin(i)])
 
 
-run()
+
+if __name__ == "__main__":
+    print(f"Triangles containing the origin: {count_triangles()}")
