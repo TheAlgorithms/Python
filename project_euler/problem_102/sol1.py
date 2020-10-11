349c901ac5e799e325d8f1c711ca2b051d7402f5
pastre <pastre68@gmail.com>
2020-10-11 02:10:42 -0300

    Ran flake8

--- project_euler/problem_102/sol1.py
+++ project_euler/problem_102/sol1.py
@@ -1,7 +1,8 @@
 """
 Problem 102: https://projecteuler.net/problem=102
 
-Three distinct points are plotted at random on a Cartesian plane, for which -1000 ≤ x, y ≤ 1000, such that a triangle is formed.
+Three distinct points are plotted at random on a Cartesian plane,
+ for which -1000 ≤ x, y ≤ 1000, such that a triangle is formed.
 
 Consider the following two triangles:
 
@@ -11,10 +12,15 @@ X(-175,41), Y(-421,-714), Z(574,-645)
 
 It can be verified that triangle ABC contains the origin, whereas triangle XYZ does not.
 
-Using triangles.txt (right click and 'Save Link/Target As...'), a 27K text file containing the co-ordinates of one thousand "random" triangles, find the number of triangles for which the interior contains the origin.
+Using triangles.txt (right click and 'Save Link/Target As...'),
+ a 27K text file containing the co-ordinates of one thousand "random"
+ triangles, find the number of triangles for which the
+ interior contains the origin.
 
-NOTE: The first two examples in the file represent the triangles in the example given above.
+NOTE: The first two examples in the file represent
+ the triangles in the example given above.
 """
+
 
 # Class to represent a point with x and y coordinates
 class Point:
