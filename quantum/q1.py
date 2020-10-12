59b0f1ed3d2f87759db3ff8366c53a7e23b96626
Christian Clauss <cclauss@me.com>
2020-10-12 21:34:20 +0200

    Update q1.py

--- quantum/q1.py
+++ quantum/q1.py
@@ -10,16 +10,16 @@ Qiskit Docs: https://qiskit.org/documentation/getting_started.html
 import qiskit as q
 
 
-def single_qubit_measure() -> q.result.counts.Counts:
+def single_qubit_measure(qubits: int, classical_bits: int) -> q.result.counts.Counts:
     """
-    >>> single_qubit_measure()
+    >>> single_qubit_measure(1, 1)
     {'0': 1000}
     """
     # Use Aer's qasm_simulator
     simulator = q.Aer.get_backend("qasm_simulator")
 
     # Create a Quantum Circuit acting on the q register
-    circuit = q.QuantumCircuit(1, 1)
+    circuit = q.QuantumCircuit(qubits, classical_bits)
 
     # Map the quantum measurement to the classical bits
     circuit.measure([0], [0])
@@ -27,15 +27,9 @@ def single_qubit_measure() -> q.result.counts.Counts:
     # Execute the circuit on the qasm simulator
     job = q.execute(circuit, simulator, shots=1000)
 
-    # Grab results from the job
-    result = job.result()
-
-    # Get the histogram data of an experiment.
-    counts = result.get_counts(circuit)
-
-    return counts
+    # Return the histogram data of the results of the experiment.
+    return job.result().get_counts(circuit)
 
 
 if __name__ == "__main__":
-    counts = single_qubit_measure()
-    print("Total count for various states are:", counts)
+    print(f"Total count for various states are: {single_qubit_measure(1, 1)}")
