1aeb19105422f1e3c0c5410358d274715c9ee1eb
Vivek Das <56256802+heyyviv@users.noreply.github.com>
2020-10-12 20:26:58 +0530

    added solution of question 081 in project euler

---
+++ project_euler/problem_81/problem_81.py
@@ -0,0 +1,52 @@
+```
+question link - https://projecteuler.net/problem=81
+
+Find the minimal path sum from the top left to the bottom right by
+only moving right and down in matrix.txt 
+
+
+
+
+
+
+```
+
+
+
+import os
+
+
+
+def solution():
+    script_dir=os.path.abspath(os.path.dirname(__file__))
+    p081_matrix=os.path.join(script_dir,"p081_matrix.txt")
+    matrix=[]
+    with open("p081_matrix.txt","r") as file_hand:
+        for line in file_hand:
+            vector=list(map(int,line.split(',')))
+            matrix.append(vector)
+    row=len(matrix)
+    col=len(matrix[0])
+
+    for i in range(1, col):
+	       matrix[0][i] += matrix[0][i-1]
+
+    for i in range(1, row):
+	       matrix[i][0] += matrix[i-1][0]
+
+    for i in range(1, row):
+	       for j in range(1, col):
+	              matrix[i][j] += min(matrix[i-1][j], matrix[i][j-1])
+
+    return matrix[row-1][col-1]
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print(solution())
