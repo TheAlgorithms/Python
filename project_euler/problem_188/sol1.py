780ebe0477fdd9534bd9a705b64e735201ee4422
Michael D <michael@drueing.de>
2020-10-12 22:17:34 +0200

    Fix doctests and remove/improve redundant comments

--- project_euler/problem_188/sol1.py
+++ project_euler/problem_188/sol1.py
@@ -49,23 +49,19 @@ def solution(base: int = 1777, height: int = 1855, digits: int = 8) -> int:
     Returns the last 8 digits of the hyperexponentiation of base by
     height, i.e. the number base↑↑height:
 
-    >>> solution()
-    95962097
-    >>> solution(3, 2)
+    >>> solution(base=3, height=2)
     27
-    >>> solution(3, 3)
+    >>> solution(base=3, height=3)
     97484987
+    >>> solution(base=123, height=456, digits=4)
+    2547
     """
 
-    # we calculate everything modulo 10^8, since we only care about the
-    # last 8 digits
-    modulo_value = 10 ** digits
-
-    # calculate base↑↑height (mod modulo_value) by repeated modular
-    # exponentiation 'height' times
+    # calculate base↑↑height by right-assiciative repeated modular
+    # exponentiation
     result = base
     for i in range(1, height):
-        result = _modexpt(base, result, modulo_value)
+        result = _modexpt(base, result, 10 ** digits)
 
     return result
 
