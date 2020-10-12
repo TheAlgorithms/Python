36ddceb379fef70da26f5638fcad88a7371c3de7
Adam Hackbarth <fearchaos@gmail.com>
2020-10-12 01:26:39 -0400

    Reformatted file with black

--- maths/runge_kutta.py
+++ maths/runge_kutta.py
@@ -39,27 +39,28 @@ def runge_kutta(f, y0, x0, h, x_end):
 
     return y
 
+
 def butchers_runge_kutta(f, y0, x0, h, x_end):
     """
-     Calculate the numeric solution at each step to the ODE f(x, y) using:
+    Calculate the numeric solution at each step to the ODE f(x, y) using:
 
-     Butcher's Fifth Order Runge-Kutta Method
+    Butcher's Fifth Order Runge-Kutta Method
 
-     Arguments:
-     f -- The ode as a function of x and y
-     y0 -- the initial value for y
-     x0 -- the initial value for x
-     h -- the stepsize
-     x_end -- the end value for x
+    Arguments:
+    f -- The ode as a function of x and y
+    y0 -- the initial value for y
+    x0 -- the initial value for x
+    h -- the stepsize
+    x_end -- the end value for x
 
-     >>> # the exact solution is math.exp(x)
-     >>> def f(x, y):
-     ...     return y
-     >>> y0 = 1
-     >>> y = butchers_runge_kutta(f, y0, 0.0, 0.01, 5)
-     >>> y[-1]
-     148.41315910258956
-     """
+    >>> # the exact solution is math.exp(x)
+    >>> def f(x, y):
+    ...     return y
+    >>> y0 = 1
+    >>> y = butchers_runge_kutta(f, y0, 0.0, 0.01, 5)
+    >>> y[-1]
+    148.41315910258956
+    """
 
     N = int(np.ceil((x_end - x0) / h))
     y = np.zeros((N + 1,))
@@ -68,15 +69,21 @@ def butchers_runge_kutta(f, y0, x0, h, x_end):
 
     for k in range(N):
         f1 = f(x, y[k])
-        f2 = f(x + ((1/4) * h), y[k] + ((1/4) * h * f1))
-        f3 = f(x + ((1/4) * h), y[k] + ((1/8) * h * f1) + ((1/8) * h * f2))
-        f4 = f(x + ((1/2) * h), y[k] - ((1/2) * h * f2) + (h * f3))
-        f5 = f(x + ((3/4) * h), y[k] + ((3/16) * h * f1) + ((9/16) * h * f4))
-        f6 = f(x + h,
-               y[k] - ((3/7) * h * f1) + ((2/7) * h * f2) +
-               ((12/7) * h * f3) - ((12/7) * h * f4) + ((8/7) * h * f5))
+        f2 = f(x + ((1 / 4) * h), y[k] + ((1 / 4) * h * f1))
+        f3 = f(x + ((1 / 4) * h), y[k] + ((1 / 8) * h * f1) + ((1 / 8) * h * f2))
+        f4 = f(x + ((1 / 2) * h), y[k] - ((1 / 2) * h * f2) + (h * f3))
+        f5 = f(x + ((3 / 4) * h), y[k] + ((3 / 16) * h * f1) + ((9 / 16) * h * f4))
+        f6 = f(
+            x + h,
+            y[k]
+            - ((3 / 7) * h * f1)
+            + ((2 / 7) * h * f2)
+            + ((12 / 7) * h * f3)
+            - ((12 / 7) * h * f4)
+            + ((8 / 7) * h * f5),
+        )
 
-        y[k + 1] = y[k] + (1 / 90) * h * (7*f1 + 32*f3 + 12*f4 + 32*f5 + 7*f6)
+        y[k + 1] = y[k] + (1 / 90) * h * (7 * f1 + 32 * f3 + 12 * f4 + 32 * f5 + 7 * f6)
 
         x += h
 
