f410669bb9648ca24cf319966f073c746d3629fa
svedire <VedireSusmith_Reddy@intuit.com>
2020-10-13 13:32:57 +0530

    Resolved comments

--- data_structures/binary_tree/binary_tree_mirror.py
+++ data_structures/binary_tree/binary_tree_mirror.py
@@ -7,35 +7,36 @@ Given a binary tree, return it's mirror.
 """
 
 
-def binaryTreeMirrorDict(root: int, binary_tree: dict = {}, binary_tree_mirror: dict = {}):
-    if root is None or root not in binary_tree.keys():
+def binary_tree_mirror_dict(root: int, binary_tree_mirror_dictionary: dict):
+    if not root or root not in binary_tree_mirror_dictionary:
         return
-    left_child = binary_tree[root][0]
-    right_child = binary_tree[root][1]
-    binary_tree_mirror[root] = [right_child, left_child]
-    binaryTreeMirrorDict(left_child, binary_tree, binary_tree_mirror)
-    binaryTreeMirrorDict(right_child, binary_tree, binary_tree_mirror)
+    left_child = binary_tree_mirror_dictionary[root][0]
+    right_child = binary_tree_mirror_dictionary[root][1]
+    binary_tree_mirror_dictionary[root] = [right_child, left_child]
+    binary_tree_mirror_dict(left_child, binary_tree_mirror_dictionary)
+    binary_tree_mirror_dict(right_child, binary_tree_mirror_dictionary)
 
-def binaryTreeMirror(root: int, binary_tree: dict = {}) -> dict:
+
+def binary_tree_mirror(binary_tree: dict = {}, root: int = 1) -> dict:
     """
-    >>> binaryTreeMirror(1, { 1: [2,3], 2: [4,5], 3: [6,7], 7: [8,9]})
+    >>> binary_tree_mirror(1, { 1: [2,3], 2: [4,5], 3: [6,7], 7: [8,9]})
     {1: [3, 2], 2: [5, 4], 3: [7, 6], 7: [9, 8]}
-    >>> binaryTreeMirror(5, { 1: [2,3], 2: [4,5], 3: [6,7], 7: [8,9]})
+    >>> binary_tree_mirror(5, { 1: [2,3], 2: [4,5], 3: [6,7], 7: [8,9]})
     {}
-    >>> binaryTreeMirror\
-    (1, { 1: [2,3], 2: [4,5], 3: [6,7], 4: [10,11], 5: [12,13],7: [8,9]})
-    {1: [3, 2], 2: [5, 4], 4: [11, 10], 5: [13, 12], 3: [7, 6], 7: [9, 8]}
+    >>> binary_tree_mirror(1, { 1: [2,3], 2: [4,5], 3: [6,7], 4: [10,11]})
+    {1: [3, 2], 2: [5, 4], 3: [7, 6], 4: [11, 10]}
     """
-    binary_tree_mirror = {}
-    binaryTreeMirrorDict(root, binary_tree, binary_tree_mirror)
-    return binary_tree_mirror
+    if not binary_tree:
+        raise ValueError("binary tree cannot be empty")
+    if root not in binary_tree:
+        raise ValueError("root is present in the binary_tree")
+    binary_tree_mirror_dictionary = dict(binary_tree)
+    binary_tree_mirror_dict(root, binary_tree_mirror_dictionary)
+    return binary_tree_mirror_dictionary
 
 
 if __name__ == "__main__":
-    binary_tree = { 1: [2,3], 2: [4,5], 3: [6,7], 4: [10,11], 5: [12,13],7: [8,9]}
-    root = 1
-    print("Binary tree:", sep=" ")
-    print(binary_tree)
-    binary_tree_mirror = binaryTreeMirror(root, binary_tree)
-    print(" binary tree mirror:", sep=" ")
-    print(binary_tree_mirror)
+    binary_tree = {1: [2, 3], 2: [4, 5], 3: [6, 7], 7: [8, 9]}
+    print("Binary tree: ", binary_tree)
+    binary_tree_mirror_dictionary = binary_tree_mirror(binary_tree, 1)
+    print("Binary tree mirror: ", binary_tree_mirror_dictionary)
