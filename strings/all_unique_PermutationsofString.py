451b4d008b49ef1ee9d6f7a318b166718d112c11
Inderjeet0003 <53380463+Inderjeet0003@users.noreply.github.com>
2020-10-13 09:34:05 +0530

    Create all_unique_PermutationsofString.py

---
+++ strings/all_unique_PermutationsofString.py
@@ -0,0 +1,24 @@
+# program will generate all unique permutation of string
+def permutation(lst): 
+  
+    if len(lst) == 0: 
+        return [] 
+  
+    if len(lst) == 1: 
+        return [lst] 
+  
+  
+    l = [] 
+  
+    for i in range(len(lst)): 
+       m = lst[i] 
+       remLst = lst[:i] + lst[i+1:] 
+  
+       for p in permutation(remLst): 
+           l.append([m] + p) 
+    return l 
+  
+inp = input()
+data = list(in) 
+for p in permutation(data): 
+    print p 
