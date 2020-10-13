2dab878f06810fd3170ff4eebe1bcc2cc4db990b
svedire <VedireSusmith_Reddy@intuit.com>
2020-10-13 13:46:57 +0530

    Minor Changes

--- data_structures/binary_tree/binary_tree_mirror.py
+++ data_structures/binary_tree/binary_tree_mirror.py
@@ -19,11 +19,9 @@ def binary_tree_mirror_dict(root: int, binary_tree_mirror_dictionary: dict):
 
 def binary_tree_mirror(binary_tree: dict = {}, root: int = 1) -> dict:
     """
-    >>> binary_tree_mirror(1, { 1: [2,3], 2: [4,5], 3: [6,7], 7: [8,9]})
+    >>> binary_tree_mirror({ 1: [2,3], 2: [4,5], 3: [6,7], 7: [8,9]}, 1)
     {1: [3, 2], 2: [5, 4], 3: [7, 6], 7: [9, 8]}
-    >>> binary_tree_mirror(5, { 1: [2,3], 2: [4,5], 3: [6,7], 7: [8,9]})
-    {}
-    >>> binary_tree_mirror(1, { 1: [2,3], 2: [4,5], 3: [6,7], 4: [10,11]})
+    >>> binary_tree_mirror({ 1: [2,3], 2: [4,5], 3: [6,7], 4: [10,11]}, 1)
     {1: [3, 2], 2: [5, 4], 3: [7, 6], 4: [11, 10]}
     """
     if not binary_tree:
