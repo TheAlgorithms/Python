d4afbc6d2ba68303c3a2b9e782f535efd9ad976e
Ben Smith <bh2smith@gmail.com>
2020-10-13 14:24:20 +0200

    add problem title to doc string

--- project_euler/problem_62/sol1.py
+++ project_euler/problem_62/sol1.py
@@ -1,5 +1,5 @@
 """
-Problem 62:
+Problem 62: Cubic Permutations
 The cube, 41063625 (3453), can be permuted to produce two other cubes: 56623104 (3843) and 66430125 (4053). 
 In fact, 41063625 is the smallest cube which has exactly three permutations of its digits which are also cube.
 
