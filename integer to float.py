f517ccc89b7aae107868e06552a152aad5fcf87a
aksh191 <55920775+aksh191@users.noreply.github.com>
2020-10-13 11:49:46 +0530

    Create integer to float.py

---
+++ integer to float.py
@@ -0,0 +1,10 @@
+num_int = 123
+num_flo = 1.23
+
+num_new = num_int + num_flo
+
+print("datatype of num_int:",type(num_int))
+print("datatype of num_flo:",type(num_flo))
+
+print("Value of num_new:",num_new)
+print("datatype of num_new:",type(num_new))
