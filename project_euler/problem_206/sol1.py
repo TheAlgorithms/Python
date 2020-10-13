995ba18e25674dfcc3b706733db9a993b138383b
nico <esistegal-aber@gmx.de>
2020-10-13 22:18:22 +0200

    add url to Project Euler problem

--- project_euler/problem_206/sol1.py
+++ project_euler/problem_206/sol1.py
@@ -1,4 +1,7 @@
 """
+Project Euler problem 205:
+https://projecteuler.net/problem=205
+
 Problem Statement:
 Find the unique positive integer whose square has the form 1_2_3_4_5_6_7_8_9_0,
 where each “_” is a single digit.
