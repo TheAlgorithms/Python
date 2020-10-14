27d854fceb37ee974e2c070c79ae43f1f6eb7af2
Anshraj Shrivastava <42239140+rajansh87@users.noreply.github.com>
2020-10-15 03:25:27 +0530

    Update binary_tree_traversals.py

--- data_structures/binary_tree/binary_tree_traversals.py
+++ data_structures/binary_tree/binary_tree_traversals.py
@@ -152,11 +152,7 @@ def main():  # Main function for testing.
     """
     Create binary tree.
     """
-    root = Node(1)
-    root.left = Node(2)
-    root.right = Node(3)
-    root.left.left = Node(4)
-    root.left.right = Node(5)
+    make_tree()
 
     """
     All Traversals of the binary are as follows:
