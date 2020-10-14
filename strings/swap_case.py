ef736199d854b178fbf96905b23f4f67a204076f
mayur200 <mayur.pardeshi96@gmail.com>
2020-10-15 02:12:37 +0530

    added True and fixed comment

--- strings/swap_case.py
+++ strings/swap_case.py
@@ -13,26 +13,24 @@ For example:
 """
 import re
 
-# This re.compile() function saves the pattern from 'a' to 'z' and 'A' to 'Z'
-# into 'regexp' variable
+# This re.compile() function saves the pattern from 'a' to 'z' and 'A' to 'Z' into 'regexp' variable
 regexp = re.compile("[^a-zA-Z]+")
 
 
 def swap_case(sentence):
     """
-    This function will convert all lowercase letters to uppercase letters and
-    vice versa.
+    This function will convert all lowercase letters to uppercase letters and vice versa.
 
     >>> swap_case('Algorithm.Python@89')
     aLGORITHM.pYTHON@89
     """
     new_string = ""
     for char in sentence:
-        if char.isupper():
+        if char.isupper() == True:
             new_string += char.lower()
-        if char.islower():
+        if char.islower() == True:
             new_string += char.upper()
-        if regexp.search(char):
+        if regexp.search(char) == True:
             new_string += char
 
     return new_string
