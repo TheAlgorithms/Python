da98d2d81eb6fc6e9ae28790c3642d5cd6c3a5bb
Lukas Kleybolte <lukas.kleybolte@hs-augsburg.de>
2020-10-14 15:57:39 +0200

    add natural_sort.py

---
+++ sorts/natural_sort.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+import re
+
+
+def natural_sort(input_list: list[str]) -> list[str]:
+    """
+    Sort the given list of strings in the way that humans expect.
+
+    The normal python sort algorithm sorts lexicographically,
+    so you might not get the results that you expect
+    >>> example1 = ['2 ft 7 in', '1 ft 5 in', '10 ft 2 in', '2 ft 11 in', '7 ft 6 in']
+    >>> example2 = ['Elm11', 'Elm12', 'Elm2', 'elm0', 'elm1', 'elm10', 'elm13', 'elm9']
+    >>> sorted(example1)
+    ['1 ft 5 in', '10 ft 2 in', '2 ft 11 in', '2 ft 7 in', '7 ft 6 in']
+
+    >>> sorted(example2)
+    ['Elm11', 'Elm12', 'Elm2', 'elm0', 'elm1', 'elm10', 'elm13', 'elm9']
+
+    The natural sort algorith sort based on meaning and not computer code point.
+    >>> natural_sort(example1)
+    ['1 ft 5 in', '2 ft 7 in', '2 ft 11 in', '7 ft 6 in', '10 ft 2 in']
+
+    >>> natural_sort(example2)
+    ['elm0', 'elm1', 'Elm2', 'elm9', 'elm10', 'Elm11', 'Elm12', 'elm13']
+
+    """
+
+    def alphanum_key(key: str) -> list:
+        return [
+            int(text) if text.isdigit() else text.lower()
+            for text in re.split("([0-9]+)", key)
+        ]
+
+    return sorted(input_list, key=alphanum_key)
