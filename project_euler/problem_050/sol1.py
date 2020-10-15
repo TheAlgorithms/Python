39c5fa757f4ffb59d9a2c8c941471315540ec102
Abhishek Jaisingh <abhi2254015@gmail.com>
2020-10-15 18:53:27 +0530

    Adhere To Coding Style

--- project_euler/problem_050/sol1.py
+++ project_euler/problem_050/sol1.py
@@ -1,4 +1,6 @@
 """
+Problem 50: https://projecteuler.net/problem=50
+
 The prime 41, can be written as the sum of six consecutive primes:
 
 41 = 2 + 3 + 5 + 7 + 11 + 13
@@ -10,12 +12,15 @@ Which prime, below one-million, can be written as the sum of the most consecutiv
 """
 
 
-def get_primes(upper_limit):
-    """
-    Returns the list & set of primes up to the below upper_limit
+def get_primes(limit: int) -> (set, list):
+    """ 
+    Returns the set & list(ordered) of primes up to the below limit
     using Sieve of Eratosthenes 
+
+    >> get_primes(10)
+    ({2, 3, 5, 7}, [2, 3, 5, 7])
     """
-    SV_MAX = upper_limit
+    SV_MAX = limit
     sv = [True for i in range(SV_MAX)]
     prime_set = set()
     primes = []
@@ -33,14 +38,19 @@ def get_primes(upper_limit):
     return prime_set, primes
 
 
-def solution(upper_limit=1000000):
+def solution(limit: int =1000000) -> int:
     """
+    First, generate a list of all primes upto limit
+    Then, try all sets of consecutive primes to see if they add up to 
+    some other prime, successively trying smaller size sets if bigger 
+    ones don't have any valid consecutive primes
+
     >>> solution(1000)
     953
     >>> solution(1000000)
     997651
     """
-    prime_set, primes = get_primes(upper_limit)
+    prime_set, primes = get_primes(limit)
     prefix = []
 
     curr = 0
@@ -61,7 +71,7 @@ def solution(upper_limit=1000000):
             p = prefix[j]
             if i >= 1:
                 p -= prefix[i-1]
-            if p >= upper_limit:
+            if p >= limit:
                 break
             if p in prime_set:
                 curr_l = j-i+1
@@ -71,6 +81,9 @@ def solution(upper_limit=1000000):
     return max_prime
 
 
-if __name__ == "__main__":
-    print(solution(int(input().strip())))
+# if __name__ == "__main__":
+#     print(solution())
 
+
+if __name__ == "__main__":
+    print(f"solution = {solution()}")
