bdf8006152df6ca64198195c9393a1ae98d8b9d8
vedantpople <vedantpople4@gmail.com>
2020-10-15 12:45:48 +0530

    new algorithms

---
+++ machine_learning/apriori.py
@@ -0,0 +1,20 @@
+# Apriori
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Data Preprocessing
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+transactions = []
+for i in range(0, 7501):
+    transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
+
+# Training the Apriori model on the dataset
+from apyori import apriori
+rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
+
+# Visualising the results
+results = list(rules)
+print(results)
---
+++ neural_network/artificial_neural_network.py
@@ -0,0 +1,71 @@
+# Importing the libraries
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+tf.__version__
+
+# Part 1 - Data Preprocessing
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:-1].values
+y = dataset.iloc[:, -1].values
+print(X)
+print(y)
+
+# Encoding categorical data
+# Label Encoding the "Gender" column
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+X[:, 2] = le.fit_transform(X[:, 2])
+print(X)
+# One Hot Encoding the "Geography" column
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+print(X)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X = sc.fit_transform(X)
+print(X)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Part 2 - Building the ANN
+
+# Initializing the ANN
+ann = tf.keras.models.Sequential()
+
+# Adding the input layer and the first hidden layer
+ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+
+# Adding the second hidden layer
+ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+
+# Adding the output layer
+ann.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+
+# Part 3 - Training the ANN
+
+# Compiling the ANN
+ann.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Training the ANN on the Training set
+ann.fit(X_train, y_train, batch_size = 32, epochs = 100)
+
+# Part 4 - Making the predictions and evaluating the model
+
+# Predicting the Test set results
+y_pred = ann.predict(X_test)
+y_pred = (y_pred > 0.5)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
