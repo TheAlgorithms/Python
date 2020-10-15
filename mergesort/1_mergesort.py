0889b8d00c5fffd03bda4ae1a91633d947df2733
Leo Lima <leonardo.lima.silva@ccc.ufcg.edu.br>
2020-10-15 13:14:53 -0300

    fix identation

--- mergesort/1_mergesort.py
+++ mergesort/1_mergesort.py
@@ -5,20 +5,20 @@ def merge(left_list, right_list, aux_array):
 
     while i < len(left_list) and j < len(right_list):
         if left_list[i] < right_list[j]:
-            aux_array[k]=left_list[i]
+            aux_array[k] = left_list[i]
             i += 1
         else:
-            aux_array[k]=right_list[j]
+            aux_array[k] = right_list[j]
             j += 1
         k += 1
 
     while i < len(left_list):
-        aux_array[k]=left_list[i]
+        aux_array[k] = left_list[i]
         i += 1
         k += 1
 
     while j < len(right_list):
-        aux_array[k]=right_list[j]
+        aux_array[k] = right_list[j]
         j += 1
         k += 1
 
@@ -32,7 +32,7 @@ def merge_sort(array, aux_array=[]):
 
         merge_sort(left_list, array)
         merge_sort(right_list, array)
-        merge(left_list,right_list, array)
+        merge(left_list, right_list, array)
 
 numbers = [100, 55, 80, 20, 15, 98, 76, 500, 480]
 
