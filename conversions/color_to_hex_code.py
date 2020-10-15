3a12a2d1a680ce24aa68abb2b1f0624eb4ba1591
Yusuf <mohamadyusufrizaldi@gmail.com>
2020-10-15 09:12:01 +0700

    Code reformatted

--- conversions/color_to_hex_code.py
+++ conversions/color_to_hex_code.py
@@ -6,31 +6,28 @@
 """
 
 dec_hex_dict = {
-    0: '0',
-    1: '1',
-    2: '2',
-    3: '3',
-    4: '4',
-    5: '5',
-    6: '6',
-    7: '7',
-    8: '8',
-    9: '9',
-    10: 'A',
-    11: 'B',
-    12: 'C',
-    13: 'D',
-    14: 'E',
-    15: 'F'}
-
-
+    0: "0",
+    1: "1",
+    2: "2",
+    3: "3",
+    4: "4",
+    5: "5",
+    6: "6",
+    7: "7",
+    8: "8",
+    9: "9",
+    10: "A",
+    11: "B",
+    12: "C",
+    13: "D",
+    14: "E",
+    15: "F",
+}
 
 
 def dec_to_hex_color(val: int) -> str:
     hex = dec_hex_dict[int(val / 16)] + dec_hex_dict[val % 16]
     return hex
-
-
 
 
 def rgb_acc_from_hue(hue: int, C: float, X: float) -> [float, float, float]:
@@ -53,14 +50,15 @@ def rgb_acc_from_hue(hue: int, C: float, X: float) -> [float, float, float]:
     return red_acc, green_acc, blue_acc
 
 
-
-
-def cmyk_to_rgb(cyan: float, magenta: float, yellow: float, black: float) -> [int, int, int]:
+def cmyk_to_rgb(
+    cyan: float, magenta: float, yellow: float, black: float
+) -> [int, int, int]:
     black_percent = black / 100
-    red   = round(255 * (1 - (cyan / 100)) * (1 - black_percent))
+    red = round(255 * (1 - (cyan / 100)) * (1 - black_percent))
     green = round(255 * (1 - (magenta / 100)) * (1 - black_percent))
-    blue  = round(255 * (1 - (yellow / 100)) * (1 - black_percent))
+    blue = round(255 * (1 - (yellow / 100)) * (1 - black_percent))
     return red, green, blue
+
 
 def hsl_to_rgb(hue: int, saturation: float, lightness: float) -> [int, int, int]:
     hue = hue if hue < 360 else 0
@@ -73,10 +71,11 @@ def hsl_to_rgb(hue: int, saturation: float, lightness: float) -> [int, int, int]
 
     red_acc, green_acc, blue_acc = rgb_acc_from_hue(hue, C, X)
 
-    red   = round((red_acc + m) * 255)
+    red = round((red_acc + m) * 255)
     green = round((green_acc + m) * 255)
-    blue  = round((blue_acc + m) * 255)
+    blue = round((blue_acc + m) * 255)
     return red, green, blue
+
 
 def hsv_to_rgb(hue: int, saturation: float, value: float) -> [int, int, int]:
     hue = hue if hue < 360 else 0
@@ -89,28 +88,39 @@ def hsv_to_rgb(hue: int, saturation: float, value: float) -> [int, int, int]:
 
     red_acc, green_acc, blue_acc = rgb_acc_from_hue(hue, C, X)
 
-    red   = round((red_acc + m) * 255)
+    red = round((red_acc + m) * 255)
     green = round((green_acc + m) * 255)
-    blue  = round((blue_acc + m) * 255)
+    blue = round((blue_acc + m) * 255)
     return red, green, blue
 
 
-
-
-def rgb_to_hex_color_code(red: int, green: int, blue: int, with_hash: bool = True) -> str:
-    hex_color_code = dec_to_hex_color(red) + dec_to_hex_color(green) + dec_to_hex_color(blue)
-    prefix = '#' if with_hash else ''
+def rgb_to_hex_color_code(
+    red: int, green: int, blue: int, with_hash: bool = True
+) -> str:
+    hex_color_code = (
+        dec_to_hex_color(red) + dec_to_hex_color(green) + dec_to_hex_color(blue)
+    )
+    prefix = "#" if with_hash else ""
     return prefix + hex_color_code
 
-def cmyk_to_hex_color_code(cyan: float, magenta: float, yellow: float, black: float, with_hash: bool = True) -> str:
+
+def cmyk_to_hex_color_code(
+    cyan: float, magenta: float, yellow: float, black: float, with_hash: bool = True
+) -> str:
     red, green, blue = cmyk_to_rgb(cyan, magenta, yellow, black)
     return rgb_to_hex_color_code(red, green, blue, with_hash)
 
-def hsl_to_hex_color_code(hue: int, saturation: float, lightness: float, with_hash: bool = True) -> str:
+
+def hsl_to_hex_color_code(
+    hue: int, saturation: float, lightness: float, with_hash: bool = True
+) -> str:
     red, green, blue = hsl_to_rgb(hue, saturation, lightness)
     return rgb_to_hex_color_code(red, green, blue, with_hash)
 
-def hsv_to_hex_color_code(hue: int, saturation: float, value: float, with_hash: bool = True) -> str:
+
+def hsv_to_hex_color_code(
+    hue: int, saturation: float, value: float, with_hash: bool = True
+) -> str:
     red, green, blue = hsv_to_rgb(hue, saturation, value)
     return rgb_to_hex_color_code(red, green, blue, with_hash)
 
