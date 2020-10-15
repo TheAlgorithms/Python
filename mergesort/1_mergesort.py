fb84bce51e1d82a6edf53d3e146bd08537cc8bbd
Leo Lima <leonardo.lima.silva@ccc.ufcg.edu.br>
2020-10-15 12:57:37 -0300

    fix identation

--- mergesort/1_mergesort.py
+++ mergesort/1_mergesort.py
@@ -4,31 +4,28 @@ def merge(left_list, right_list, aux_array):
   k = 0
 
   while i < len(left_list) and j < len(right_list):
-
-      if left_list[i] < right_list[j]:
-          aux_array[k]=left_list[i]
-          i += 1
-      else:
-          aux_array[k]=right_list[j]
-          j += 1
-      k += 1
+    if left_list[i] < right_list[j]:
+        aux_array[k]=left_list[i]
+        i += 1
+    else:
+        aux_array[k]=right_list[j]
+        j += 1
+    k += 1
 
   while i < len(left_list):
-
-      aux_array[k]=left_list[i]
-      i += 1
-      k += 1
+    aux_array[k]=left_list[i]
+    i += 1
+    k += 1
 
   while j < len(right_list):
-      aux_array[k]=right_list[j]
-      j += 1
-      k += 1
-
+    aux_array[k]=right_list[j]
+    j += 1
+    k += 1
 
 def merge_sort(array, aux_array=[]):
   if len(array) > 1:
 
-    half = len(array)//2
+    half = int(len(array) / 2)
 
     left_list = array[:half]
     right_list = array[half:]
@@ -37,7 +34,7 @@ def merge_sort(array, aux_array=[]):
     merge_sort(right_list, array)
     merge(left_list,right_list, array)
 
-numbers = [100,55,80,20,15,98,76,500,480]
+numbers = [100, 55, 80, 20, 15, 98, 76, 500, 480]
 
 merge_sort(numbers)
 print(numbers)
