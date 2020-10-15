791e366900139fd404b944267ab52f3206a6a49e
Bünyamin Mete <metebunyaminn@gmail.com>
2020-10-15 04:42:32 +0300

    Bug fixed

--- problem_135/135.py
+++ problem_135/135.py
@@ -1,12 +1,12 @@
-'''
+"""
 Problem 135: https://projecteuler.net/problem=135
 
 Given the positive integers, x, y, and z, are consecutive terms of an arithmetic progression, the least value of the positive integer, n, for which the equation, x2 − y2 − z2 = n, has exactly two solutions is n = 27:
 342 − 272 − 202 = 122 − 92 − 62 = 27
 It turns out that n = 1155 is the least value which has exactly ten solutions.
 How many values of n less than one million have exactly ten distinct solutions?
+"""
 
-'''
 def solution():
 	lim = 10**6
 	solutions = [0] * lim
@@ -23,3 +23,4 @@ def solution():
 
 if __name__ == "__main__":
 	print(solution())
+	
