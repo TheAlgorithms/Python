c8c7faf93fc63e1981aea0430e2f22b1c9bc2db8
Aman Garg <43947335+aman-garg0001@users.noreply.github.com>
2020-10-15 22:22:33 +0530

    Draws a rectangle on the detected face

---
+++ computer_vision/face_detector_using_OpenCV.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+
+path = 'front_face.xml' # name of file with XML tags
+faceCascade = cv2.CascadeClassifier(path)
+
+cap = cv2.VideoCapture(0) # capture video using webcam
+cap.set(3,640) # set Width
+cap.set(4,480) # set Height
+while True:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,     
+        scaleFactor=1.2,
+        minNeighbors=5,     
+        minSize=(20, 20)
+    )
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]  
+    cv2.imshow('video',img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27: # press 'ESC' to quit
+        break
+cap.release()
+cv2.destroyAllWindows()
