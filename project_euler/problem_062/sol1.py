9acfcb69138361e381e35ac85221019f878e9d24
Freddy Pringle <freddyjepringle@gmail.com>
2020-10-15 10:32:51 +0200

    Update formatting and docstrings. Reference: #3256

--- project_euler/problem_062/sol1.py
+++ project_euler/problem_062/sol1.py
@@ -1,4 +1,6 @@
 """
+Project Euler Problem 62: https://projecteuler.net/problem=62
+
 The cube, 41063625 (345^3), can be permuted to produce two other cubes: 56623104 (384^3)
 and 66430125 (405^3). In fact, 41063625 is the smallest cube which has exactly three
 permutations of its digits which are also cube.
@@ -11,9 +13,11 @@ from collections import defaultdict
 from math import ceil, floor
 
 
-def solution() -> int:
+def solution(num_perms: int = 5) -> int:
     """
     Return the smallest cube for which exactly five permutations of its digits are cube.
+    >>> solution(3)
+    41063625
     """
     cube_len = 1
     while True:
@@ -25,11 +29,13 @@ def solution() -> int:
             cube = cube_base * cube_base * cube_base
             s = "".join(sorted(list(str(cube))))
             d[s].append(cube_base)
-            if len(d[s]) == 5:
+
+        for s in d:
+            if len(d[s]) == num_perms:
                 return d[s][0] ** 3
 
         cube_len += 1
 
 
 if __name__ == "__main__":
-    print(solution())
+    print(f"{solution() = }")
