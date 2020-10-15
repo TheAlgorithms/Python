18e778ec9754a7b51dc5018f253ee7783abf911e
Christian Clauss <cclauss@me.com>
2020-10-15 13:31:06 +0200

    Update natural_sort.py

--- sorts/natural_sort.py
+++ sorts/natural_sort.py
@@ -8,7 +8,8 @@ def natural_sort(input_list: list[str]) -> list[str]:
     Sort the given list of strings in the way that humans expect.
 
     The normal Python sort algorithm sorts lexicographically,
-    so you might not get the results that you expect
+    so you might not get the results that you expect...
+
     >>> example1 = ['2 ft 7 in', '1 ft 5 in', '10 ft 2 in', '2 ft 11 in', '7 ft 6 in']
     >>> sorted(example1)
     ['1 ft 5 in', '10 ft 2 in', '2 ft 11 in', '2 ft 7 in', '7 ft 6 in']
@@ -21,14 +22,10 @@ def natural_sort(input_list: list[str]) -> list[str]:
     ['Elm11', 'Elm12', 'Elm2', 'elm0', 'elm1', 'elm10', 'elm13', 'elm9']
     >>> natural_sort(example2)
     ['elm0', 'elm1', 'Elm2', 'elm9', 'elm10', 'Elm11', 'Elm12', 'elm13']
-
     """
 
     def alphanum_key(key):
-        return [
-            int(text) if text.isdigit() else text.lower()
-            for text in re.split("([0-9]+)", key)
-        ]
+        return [int(s) if s.isdigit() else s.lower() for s in re.split("([0-9]+)", key)]
 
     return sorted(input_list, key=alphanum_key)
 
