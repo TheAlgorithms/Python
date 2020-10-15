44667d4ace80ecfda91d425f12a813e20ba9a44d
Freddy Pringle <freddyjepringle@gmail.com>
2020-10-15 10:06:15 +0200

    Update formatting, doctest and annotations. Reference: #3256

--- project_euler/problem_173/sol1.py
+++ project_euler/problem_173/sol1.py
@@ -1,4 +1,6 @@
 """
+Project Euler Problem 173: https://projecteuler.net/problem=173
+
 We shall define a square lamina to be a square outline with a square "hole" so that
 the shape possesses vertical and horizontal symmetry. For example, using exactly
 thirty-two square tiles we can form two different square laminae:
@@ -13,17 +15,18 @@ Using up to one million tiles how many different square laminae can be formed?
 from math import ceil, sqrt
 
 
-def solution():
+def solution(limit: int = 1000000) -> int:
     """
     Return the number of different square laminae that can be formed using up to
     one million tiles.
+    >>> solution(100)
+    41
     """
-    LIMIT = 10 ** 6
     answer = 0
 
-    for outer_width in range(3, (LIMIT // 4) + 2):
-        if outer_width ** 2 > LIMIT:
-            hole_width_lower_bound = max(ceil(sqrt(outer_width ** 2 - LIMIT)), 1)
+    for outer_width in range(3, (limit // 4) + 2):
+        if outer_width ** 2 > limit:
+            hole_width_lower_bound = max(ceil(sqrt(outer_width ** 2 - limit)), 1)
         else:
             hole_width_lower_bound = 1
         if (outer_width - hole_width_lower_bound) % 2:
@@ -35,4 +38,4 @@ def solution():
 
 
 if __name__ == "__main__":
-    print(solution())
+    print(f"{solution() = }")
