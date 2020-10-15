caa5f38c1359349dc234717a04def4a89d618f43
Freddy Pringle <freddyjepringle@gmail.com>
2020-10-15 10:23:01 +0200

    Update docstring and 0-padding in directory name. Reference: #3256

--- project_euler/problem_050/sol1.py
+++ project_euler/problem_050/sol1.py
@@ -1,4 +1,6 @@
 """
+Project Euler Problem 50: https://projecteuler.net/problem=50
+
 The prime 41, can be written as the sum of six consecutive primes:
 
 41 = 2 + 3 + 5 + 7 + 11 + 13
@@ -49,4 +51,4 @@ def solution(limit: int = 1000000) -> int:
 
 
 if __name__ == "__main__":
-    print(solution())
+    print(f"{solution() = }")
