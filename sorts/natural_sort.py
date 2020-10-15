ad346f2fdbd97b2e3faf5d406e105055f79a2766
Mozartus <32893711+Mozartuss@users.noreply.github.com>
2020-10-15 12:55:48 +0200

    Update sorts/natural_sort.py
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- sorts/natural_sort.py
+++ sorts/natural_sort.py
@@ -10,17 +10,15 @@ def natural_sort(input_list: list[str]) -> list[str]:
     The normal Python sort algorithm sorts lexicographically,
     so you might not get the results that you expect
     >>> example1 = ['2 ft 7 in', '1 ft 5 in', '10 ft 2 in', '2 ft 11 in', '7 ft 6 in']
-    >>> example2 = ['Elm11', 'Elm12', 'Elm2', 'elm0', 'elm1', 'elm10', 'elm13', 'elm9']
     >>> sorted(example1)
     ['1 ft 5 in', '10 ft 2 in', '2 ft 11 in', '2 ft 7 in', '7 ft 6 in']
-
-    >>> sorted(example2)
-    ['Elm11', 'Elm12', 'Elm2', 'elm0', 'elm1', 'elm10', 'elm13', 'elm9']
-
-    The natural sort algorithm sort based on meaning and not computer code point.
+    >>> # The natural sort algorithm sort based on meaning and not computer code point.
     >>> natural_sort(example1)
     ['1 ft 5 in', '2 ft 7 in', '2 ft 11 in', '7 ft 6 in', '10 ft 2 in']
 
+    >>> example2 = ['Elm11', 'Elm12', 'Elm2', 'elm0', 'elm1', 'elm10', 'elm13', 'elm9']
+    >>> sorted(example2)
+    ['Elm11', 'Elm12', 'Elm2', 'elm0', 'elm1', 'elm10', 'elm13', 'elm9']
     >>> natural_sort(example2)
     ['elm0', 'elm1', 'Elm2', 'elm9', 'elm10', 'Elm11', 'Elm12', 'elm13']
 
