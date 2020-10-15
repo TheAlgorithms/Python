9102f3e99b0fb6b928a8c37321046fa77c0af7ce
Andrew <54547089+ac-tam@users.noreply.github.com>
2020-10-15 17:31:07 -0400

    Update distance_formula.py

--- maths/distance_formula.py
+++ maths/distance_formula.py
@@ -1,4 +1,4 @@
-def distance_formula(x1,y1,x2,y2):
+def distance_formula(x1, y1, x2, y2):
     """
     Calculate the distance between the two coordinates (x1,y1) and (x2,y2).
     >>> distance_formula(0, 0, 2, 2)
@@ -8,8 +8,8 @@ def distance_formula(x1,y1,x2,y2):
     >>> distance_formula(0, 0, 5, 0)
     5.0
     """
-    return ((y2-y1) ** 2  + (x2-x1) ** 2) ** (1/2)
+    return ((y2 - y1) ** 2  + (x2 - x1) ** 2) ** (1 / 2)
 
 
 if __name__ == "__main__":
-    print(distance_formula(0,0,2,2))
+    print(distance_formula(0, 0, 2, 2))
