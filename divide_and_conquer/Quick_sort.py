4c313327aa28f08a4bbcba4cc059374aa40f18a5
Saket Thenua <saket.11902197@lpu.in>
2020-10-16 13:38:35 +0530

    newalgo

--- divide_and_conquer/Quick_sort.py
+++ divide_and_conquer/Quick_sort.py
@@ -1,49 +1,29 @@
 
 
-def partition(arr, low, high):
-	i = (low-1)		 # index of smaller element
-	pivot = arr[high]	 # pivot
-
-	for j in range(low, high):
-
-		# If current element is smaller than or
-		# equal to pivot
-		if arr[j] <= pivot:
-
-			# increment index of smaller element
-			i = i+1
-			arr[i], arr[j] = arr[j], arr[i]
-
-	arr[i+1], arr[high] = arr[high], arr[i+1]
-	return (i+1)
-
-# The main function that implements QuickSort
-# arr[] --> Array to be sorted,
-# low --> Starting index,
-# high --> Ending index
-
-# Function to do Quick sort
-
-
-def quickSort(arr, low, high):
-	if len(arr) == 1:
-		return arr
-	if low < high:
-
-		# pi is partitioning index, arr[p] is now
-		# at right place
-		pi = partition(arr, low, high)
-
-		# Separately sort elements before
-		# partition and after partition
-		quickSort(arr, low, pi-1)
-		quickSort(arr, pi+1, high)
-
-
-# Driver code to test above
-arr = [10, 7, 8, 9, 1, 5]
+# divide function
+def partition(arr,low,high):
+   i = ( low-1 )
+   pivot = arr[high] # pivot element
+   for j in range(low , high):
+      # If current element is smaller
+      if arr[j] <= pivot:
+         # increment
+         i = i+1
+         arr[i],arr[j] = arr[j],arr[i]
+   arr[i+1],arr[high] = arr[high],arr[i+1]
+   return ( i+1 )
+# sort
+def quickSort(arr,low,high):
+   if low < high:
+      # index
+      pi = partition(arr,low,high)
+      # sort the partitions
+      quickSort(arr, low, pi-1)
+      quickSort(arr, pi+1, high)
+# main
+arr = [2,5,3,8,6,5,4,7]
 n = len(arr)
-quickSort(arr, 0, n-1)
-print("Sorted array is:")
+quickSort(arr,0,n-1)
+print ("Sorted array is:")
 for i in range(n):
-	print("%d" % arr[i])
+   print (arr[i],end=" ")
