0451a5b5b436d25fc06f6ba9fd738f559840facf
Andrew <54547089+ac-tam@users.noreply.github.com>
2020-10-16 10:24:57 -0400

    v1 - > v2

--- maths/euclidean_distance.py
+++ maths/euclidean_distance.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def euclidean_distance(v1, v2) -> int:
     """
-    Calculate the distance between the two = endpoints of two vectors, v1 and v2. v1 and v2 must be the same length.
+    Calculate the distance between the two = endpoints of two vectors, v1 and v2.
     >>> euclidean_distance(np.array([0, 0]), np.array([2, 2]))
     2.8284271247461903
     >>> euclidean_distance(np.array([0, 0, 0]), np.array([2, 2, 2]))
@@ -10,12 +10,13 @@ def euclidean_distance(v1, v2) -> int:
     >>> euclidean_distance(np.array([1, 2, 3, 4]), np.array([5, 6, 7, 8]))
     8.0
     """
+
     sumSquared = 0 
     for i in range(len(v1)):
-        sumSquared += (v1[i] - v1[i]) ** 2
+        sumSquared += (v2[i] - v1[i]) ** 2
     return sumSquared ** (1 / 2)
 
 if __name__ == "__main__":
-    point = np.array([1, 2, 3, 4])
-    point2 = np.array([5, 6, 7, 8])
-    print(euclidean_distance(point, point2))   
+    point = np.array([2, 2])
+    point2 = np.array([0, 0])
+    print(euclidean_distance(point, point2))
