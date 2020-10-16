f2dc129e0540604e304907d4d223f1cb6eed4e08
Vivek Das <56256802+heyyviv@users.noreply.github.com>
2020-10-16 11:22:56 +0530

    Add more solutions to Project Euler problems #2695

---
+++ project_euler/problem_085/sol1.py
@@ -0,0 +1,49 @@
+"""
+problem 85 -  https://projecteuler.net/problem=85
+
+By counting carefully it can be seen that a rectangular grid measuring 3 by 2 contains eighteen rectangles
+
+Although there exists no rectangular grid that contains exactly two million rectangles,
+find the area of the grid with the nearest solution.
+
+
+
+
+"""
+
+def solution():
+
+    """
+
+    i want to take the error in finding a rectangle with 2Million possible rectangle as minimum
+    as possible so i have took min_diff 9000
+
+    no of possible rectangle is n(n+1)/2 in each side
+    so total possible triangle is n1(n1+1) * n2(n2+1) /4
+
+    """
+
+    min_diff=9000
+
+    area=0
+    L=2000000
+
+    for x in range(2,102):
+        for y in range(2,102):
+            rec=((x**2 + x)*(y**2 + y))/4
+            diff_rec=abs(rec-L)
+            if diff_rec < min_diff:
+                min_diff=diff_rec
+                area=x*y
+    return area
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print(solution())
