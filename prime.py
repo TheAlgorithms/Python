79ebd40956c78bfd8d30d474b4bee0611799e97f
sandeepdixit0401 <72873808+sandeepdixit0401@users.noreply.github.com>
2020-10-16 05:43:37 -1200

    prime
    find prime no. of given

---
+++ prime.py
@@ -0,0 +1,17 @@
+num = 407
+
+
+
+if num > 1:
+   
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"is not a prime number")
+           print(i,"times",num//i,"is",num)
+           break
+   else:
+       print(num,"is a prime number")
+       
+
+else:
+   print(num,"is not a prime number")
