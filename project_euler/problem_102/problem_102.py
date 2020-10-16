3491637be9d871d8083c0e26228c623c4fe6be8b
Patrik Holopainen <oncerecursive@gmail.com>
2020-10-16 21:57:01 +0300

    Cleaned problem_102 to fit requirements.

--- project_euler/problem_102/problem_102.py
+++ project_euler/problem_102/problem_102.py
@@ -1,21 +1,48 @@
-def solution() -> int:
+"""
+Project Euler Problem 102: https://projecteuler.net/problem=102
+Three distinct points are plotted at random
+on a Cartesian plane, for which -1000 ≤ x, y ≤ 1000,
+such that a triangle is formed.
+
+Consider the following two triangles:
+
+A(-340,495), B(-153,-910), C(835,-947)
+
+X(-175,41), Y(-421,-714), Z(574,-645)
+
+It can be verified that triangle ABC
+contains the origin, whereas triangle XYZ does not.
+
+Using triangles.txt, a 27K text file containing
+the co-ordinates of one thousand "random" triangles,
+find the number of triangles for which the interior
+contains the origin.
+"""
+import math
+
+
+def solution(file_path: str = "triangles.txt") -> int:
     """
-    Return the number of triangles from triangles.txt that
-    include the origin.
+    Return number of triangles that include the origin.
+    Coordinates are given in the parameter text file path.
     >>> solution()
     228
     """
-    import math
 
-    f = open("triangles.txt")
+    f = open(file_path)
     list_of_lines = f.readlines()
 
     def Area(a1: int, a2: int, b1: int, b2: int, c1: int, c2: int) -> int:
+        """
+        Return the area of a triangle defined by given coordinates.
+        >>> Area(0,0,0,2,2,0)
+        2
+        """
+
         a = math.sqrt((b1 - c1) ** 2 + (b2 - c2) ** 2)
         b = math.sqrt((a1 - c1) ** 2 + (a2 - c2) ** 2)
         c = math.sqrt((b1 - a1) ** 2 + (b2 - a2) ** 2)
         s = (a + b + c) / 2
-        # print(a,b,c,s)
         A = math.sqrt(s * (s - a) * (s - b) * (s - c))
         return A
 
@@ -26,10 +53,8 @@ def solution() -> int:
         A1 = Area(0, 0, b1, b2, c1, c2)
         A2 = Area(a1, a2, 0, 0, c1, c2)
         A3 = Area(a1, a2, b1, b2, 0, 0)
-        # print(A1+A2+A3-A)
         if math.isclose(A, A1 + A2 + A3):
             count += 1
-
     return count
 
 
