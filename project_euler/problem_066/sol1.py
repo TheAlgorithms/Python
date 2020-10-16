e441946f15cae45a4d12cb7ded040e3711e21eb1
Kalyani123kumari <kalyanikumari187@gmail.com>
2020-10-16 17:43:03 +0530

    added problem 66

--- project_euler/problem_066/sol1.py
+++ project_euler/problem_066/sol1.py
@@ -1,20 +1,5 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct 16 02:05:22 2020
-
-@author: kalyani
 """
 
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct 16 01:06:49 2020
-
-@author: kalyani
-"""
-
-"""
 
 Project Euler 66
 https://projecteuler.net/problem=66
@@ -25,11 +10,14 @@ Consider quadratic Diophantine equations of the form:
 
 x^2 – Dy^2 = 1
 
-For example, when D=13, the minimal solution in x is 6492 – 13×1802 = 1.
+For example, when D=13, the minimal
+solution in x is 6492 – 13×1802 = 1.
 
-It can be assumed that there are no solutions in positive integers when D is square.
+It can be assumed that there are no solutions in
+positive integers when D is square.
 
-By finding minimal solutions in x for D = {2, 3, 5, 6, 7}, we obtain the following:
+By finding minimal solutions in x for
+D = {2, 3, 5, 6, 7}, we obtain the following:
 
 3^2 – 2×2^2 = 1
 2^2 – 3×1^2 = 1
@@ -37,9 +25,11 @@ By finding minimal solutions in x for D = {2, 3, 5, 6, 7}, we obtain the followi
 5^2 – 6×2^2 = 1
 8^2 – 7×3^2 = 1
 
-Hence, by considering minimal solutions in x for D ≤ 7, the largest x is obtained when D=5.
+Hence, by considering minimal solutions in x for D ≤ 7,
+the largest x is obtained when D=5.
 
-Find the value of D ≤ 1000 in minimal solutions of x for which the largest value of x is obtained.
+Find the value of D ≤ 1000 in minimal solutions of
+x for which the largest value of x is obtained.
 
 """
 
@@ -104,7 +94,8 @@ def solution(limit: int = 1001) -> int:
     """
     function to find value of D in minimal solutions of x
     for which x is largest.
-
+    """
+    """
     >>>solution(7)
     5
 
@@ -132,7 +123,7 @@ def solution(limit: int = 1001) -> int:
                 largest = i, u
 
     # print the largest value
-    print(largest[0])
+    # print(largest[0])
     return largest[0]
 
 
