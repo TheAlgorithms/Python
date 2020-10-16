e710058bca4693d56b8d8fac8cdbc88b80469f5a
Patrik Holopainen <oncerecursive@gmail.com>
2020-10-16 21:53:34 +0300

    Corrected format and styling according to requirements.

--- project_euler/problem_058/problem_58.py
+++ project_euler/problem_058/problem_58.py
@@ -30,6 +30,50 @@ Stop as soon as the ratio falls below 10%.
 """
 import math
 
+
+def prime(n: int) -> bool:
+    """
+    Check if n is a prime.
+    >>> prime(3)
+    True
+    >>> prime(4)
+    False
+    """
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n > 2 and n % 2 == 0:
+        return False
+
+    max_div = math.floor(math.sqrt(n))
+    for i in range(3, 1 + max_div, 2):
+        if n % i == 0:
+            return False
+    return True
+
+
+def f(k: int, n_primes: int, x: int) -> (int, int):
+    """
+    Return total number of primes after adding the kth
+    layer to the spiral and the last number added.
+
+    >>> f(1,0,1)
+    (3, 9)
+    """
+    diff_corners = (k - 1) * 2 + 1
+    corner_numbers = [
+        x + diff_corners + 1,
+        x + diff_corners * 2 + 2,
+        x + diff_corners * 3 + 3,
+        x + diff_corners * 4 + 4,
+    ]
+    return (
+        n_primes + sum([prime(corner) for corner in corner_numbers]),
+        corner_numbers[3],
+    )
+
+
 def solution(limit: int = 0.1) -> int:
     """Return the smallest side length of the square spiral
     where the fraction of primes in the diagonals is < 0.1
@@ -37,49 +81,6 @@ def solution(limit: int = 0.1) -> int:
     >>> solution()
     26241
     """
-
-
-    def prime(n: int) -> bool:
-        """
-        Check if n is a prime.
-        >>> prime(3)
-        True
-        >>> prime(4)
-        False
-        """
-        if n <= 1:
-            return False
-        if n == 2:
-            return True
-        if n > 2 and n % 2 == 0:
-            return False
-
-        max_div = math.floor(math.sqrt(n))
-        for i in range(3, 1 + max_div, 2):
-            if n % i == 0:
-                return False
-        return True
-
-    def f(k: int, n_primes: int, x: int) -> (int, int):
-        """
-        Return total number of primes after adding the kth
-        layer to the spiral and the last number added.
-        
-        >>> f(1,0,1)
-        (3,9)
-
-        """
-        diff_corners = (k - 1) * 2 + 1
-        corner_numbers = [
-            x + diff_corners + 1,
-            x + diff_corners * 2 + 2,
-            x + diff_corners * 3 + 3,
-            x + diff_corners * 4 + 4,
-        ]
-        return (
-            n_primes + sum([prime(corner) for corner in corner_numbers]),
-            corner_numbers[3],
-        )
 
     p = 1
     k = 1
