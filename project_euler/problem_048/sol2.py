8f3b3a46f35ccf8c6e4d3eb4063fec4025ee6903
Satyam Koshta <58166232+satyamkoshta340@users.noreply.github.com>
2020-10-16 19:45:41 +0530

    Create sol2.py
    use of power function and slicing to get the answer

---
+++ project_euler/problem_048/sol2.py
@@ -0,0 +1,22 @@
+"""
+Self Powers
+Problem 48
+The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+"""
+
+
+def solution():
+    """
+    this is simple using power method to calculate answer
+    
+    solution()
+    "9110846700"
+    """
+    t_sum = 0
+    for i in range(1, 1001):
+        t_sum += pow(i, i)
+    return str(t_sum)[-10:]
+
+if __name__ == "__main__":
+    print(solution())
