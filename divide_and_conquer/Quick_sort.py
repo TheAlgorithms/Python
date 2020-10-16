784c40920d6154806901e474fad1b6579b51fd4c
Saket Thenua <saket.11902197@lpu.in>
2020-10-16 11:27:22 +0530

    new algo

--- divide_and_conquer/Quick_sort.py
+++ divide_and_conquer/Quick_sort.py
@@ -1,4 +1,5 @@
 
+
 def partition(arr, low, high):
 	i = (low-1)		 # index of smaller element
 	pivot = arr[high]	 # pivot
@@ -16,6 +17,10 @@ def partition(arr, low, high):
 	arr[i+1], arr[high] = arr[high], arr[i+1]
 	return (i+1)
 
+# The main function that implements QuickSort
+# arr[] --> Array to be sorted,
+# low --> Starting index,
+# high --> Ending index
 
 # Function to do Quick sort
 
@@ -41,4 +46,4 @@ n = len(arr)
 quickSort(arr, 0, n-1)
 print("Sorted array is:")
 for i in range(n):
-	print("%d" % arr[i]),
+	print("%d" % arr[i])
