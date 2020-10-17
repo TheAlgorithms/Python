3df88f686c3b58a95fe2e59e2e5d07fa56c9448b
RadadiyaMohit <30775542+radadiyamohit81@users.noreply.github.com>
2020-10-17 19:47:44 +0530

    update file

--- ciphers/vigenera_cipher_dictionary_hacker.py
+++ ciphers/vigenera_cipher_dictionary_hacker.py
@@ -3,22 +3,20 @@ import detectEnglish, vigenereCipher, pyperclip
 
 def main():
     ciphertext = "Tzx isnz eccjxkg nfq lol mys bbqq I lxcz"
-    hackedMessage = hackVigenere(ciphertext)
+    hacked_message = hack_vigenere(ciphertext)
 
     if hackedMessage != None:
-        print("Copying hacked message to clipboard:")
-        print(hackedMessage)
+        print(f"Copying hacked message to clipboard: {hackedMessage}")
         pyperclip.copy(hackedMessage)
     else:
-        print("Failed to hack encryption.")
+        print(f"Failed to hack encryption")
 
 
 def hackVigenere(ciphertext: str) -> str:
     """
     >>> hackVigenere("Tzx isnz eccjxkg nfq lol mys bbqq I lxcz")
     'Hjsg sfaty shsj hjasg vzfwt jnal'
-    
-    
+   
     Return the decrypted text
     """
     with open("dictionary.txt") as in_file:
@@ -29,10 +27,10 @@ def hackVigenere(ciphertext: str) -> str:
         if detectEnglish.isEnglish(decryptedText, wordPercentage=40):
             # Check with user to see if the decrypted key has been found.
             print()
-            print("Possible encryption break:")
-            print("Key " + str(word) + ": " + decryptedText[:100])
+            print(f"Possible encryption break:")
+            print(f"Key {str(word)} : {decryptedText[:100]}")
             print()
-            print("Enter D for done, or just press Enter to continue breaking:")
+            print(f"Enter D for done, or just press Enter to continue breaking:")
             response = input("> ")
 
             if response.upper().startswith("D"):
