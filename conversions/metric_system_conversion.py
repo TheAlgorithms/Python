1d7761fe7b4aa98c26d6a3ea78e088893bc4b198
Darkirondan <dev.darkirondan@gmail.com>
2020-10-17 13:36:32 -0500

    Make upgrades on sintax

--- conversions/metric_system_conversion.py
+++ conversions/metric_system_conversion.py
@@ -7,12 +7,12 @@ import sys
 def m_to_cm(meters):
     """
     Print the conversion from meters to centimeters
-    >>> m_to_cm(5)
-    23.0 m = 2300.0 cm
-    >>> m_to_cm(12)
-    421.0 m = 42100.0 cm
+    >>> m_to_cm(23)
+    '23 m = 2300 cm'
+    >>> m_to_cm(421)
+    '421 m = 42100 cm'
     >>> m_to_cm(2.36)
-    2.36 m = 236.0 cm
+    '2.36 m = 236.0 cm'
     """
     centimeters = meters * 100
     result = f"{meters} m = {centimeters} cm"
@@ -23,11 +23,11 @@ def cm_to_m(centimeters):
     """
     Print the conversion from centimeters to meters
     >>> cm_to_m(5)
-    5.0 cm = 0.05 m
-    >>> cm_to_m(135) 
-    135.0 cm = 1.35 m
+    '5 cm = 0.05 m'
+    >>> cm_to_m(135)
+    '135 cm = 1.35 m'
     >>> cm_to_m(11235.125)
-    11235.125 cm = 112.35125 m
+    '11235.125 cm = 112.35125 m'
     """
     meters = centimeters / 100
     result = f"{centimeters} cm = {meters} m"
@@ -45,6 +45,8 @@ def menu():
 
 
 """"Using a dictionary for switching"""
+
+
 def switch(case):
 
     if case == 0:
@@ -53,10 +55,7 @@ def switch(case):
 
     print("Numbre to convert:")
     number = float(input().strip())
-    switch_opciones = {
-        1: m_to_cm(number),
-        2: cm_to_m(number)
-    }
+    switch_opciones = {1: m_to_cm(number), 2: cm_to_m(number)}
 
     return switch_opciones.get(case, default())
 
@@ -69,4 +68,3 @@ if __name__ == "__main__":
         menu()
         case = int(input().strip())
         print(switch(case))
-
