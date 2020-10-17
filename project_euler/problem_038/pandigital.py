3437c604cc683dfef1b2dea3d1a2edb9f87f3f4f
Dark00infinity <47387653+dark00infinity@users.noreply.github.com>
2020-10-17 19:30:55 +0530

    Update pandigital.py
    removed spaces

--- project_euler/problem_038/pandigital.py
+++ project_euler/problem_038/pandigital.py
@@ -17,29 +17,22 @@ def solution():
     # largest pandigital number
     largest = 0 
     # for loop to loop till 4 digits
-    for i in range(1,10000):
-    	
+    for i in range(1,10000):	
     	# value for concatenated string
     	multiplication = ''   	
     	integer = 1
-    	
     	# if the multiplication < 9 digits
-    	while len(multiplication) < 9:
-    		
+    	while len(multiplication) < 9:  		
     		# concatenating the product at each stage
     		multiplication += str(i*integer)       
-    		integer += 1
-    		
+    		integer += 1    		
     	# check for digits less than 9
     	# check for all 1-9 numbers
     	# check if '0' not in concatenated string
-    	if ((len(multiplication) == 9) and (len(set(multiplication)) == 9) 
-    		and ('0' not in multiplication)):
-    	
+    	if ((len(multiplication) == 9) and (len(set(multiplication)) == 9) and ('0' not in multiplication)):    	
     		# check if multiplication is greater than largest
     		if int(multiplication) > largest:
-    			largest = int(multiplication)
-    
+    			largest = int(multiplication)    
     # return the largest
     return(largest)
 solution()
