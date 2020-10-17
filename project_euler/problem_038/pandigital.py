cd8a015d2c313c508b2599592e4c021b6831dcfc
Dark00infinity <47387653+dark00infinity@users.noreply.github.com>
2020-10-17 18:59:21 +0530

    Update pandigital.py

--- project_euler/problem_038/pandigital.py
+++ project_euler/problem_038/pandigital.py
@@ -15,15 +15,13 @@ What is the largest 1 to 9 pandigital 9-digit number that can be formed as the c
 """
 def solution():
     # largest pandigital number
-    largest = 0
-    
+    largest = 0 
     # for loop to loop till 4 digits
     for i in range(1,10000):
     	
-    	# value for concatenated product
+    	# value for concatenated string
     	multiplication = ''
     	
-    	# (1,2,3,4,.....n)
     	integer = 1
     	
     	# if the multiplication < 9 digits
@@ -31,13 +29,12 @@ def solution():
     		
     		# concatenating the product at each stage
     		multiplication += str(i*integer)
-    		
-    		# incrementing (1,2,3,4,....n)
+            
     		integer += 1
     		
     	# check for digits less than 9
     	# check for all 1-9 numbers
-    	# check if '0' not in concatenated sting
+    	# check if '0' not in concatenated string
     	if ((len(multiplication) == 9) and (len(set(multiplication)) == 9) 
     		and ('0' not in multiplication)):
     	
