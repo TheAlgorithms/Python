696707bcb08f8fb9393f60be05680d85fba3c7ab
RadadiyaMohit <30775542+radadiyamohit81@users.noreply.github.com>
2020-10-17 14:24:16 +0530

    update as per black formatter

--- ciphers/beaufort_cipher.py
+++ ciphers/beaufort_cipher.py
@@ -1,14 +1,60 @@
-dict1 = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4,
-         'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9,
-         'K': 10, 'L': 11, 'M': 12, 'N': 13, 'O': 14,
-         'P': 15, 'Q': 16, 'R': 17, 'S': 18, 'T': 19,
-         'U': 20, 'V': 21, 'W': 22, 'X': 23, 'Y': 24, 'Z': 25}
+dict1 = {
+    "A": 0,
+    "B": 1,
+    "C": 2,
+    "D": 3,
+    "E": 4,
+    "F": 5,
+    "G": 6,
+    "H": 7,
+    "I": 8,
+    "J": 9,
+    "K": 10,
+    "L": 11,
+    "M": 12,
+    "N": 13,
+    "O": 14,
+    "P": 15,
+    "Q": 16,
+    "R": 17,
+    "S": 18,
+    "T": 19,
+    "U": 20,
+    "V": 21,
+    "W": 22,
+    "X": 23,
+    "Y": 24,
+    "Z": 25,
+}
 
-dict2 = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E',
-         5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J',
-         10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O',
-         15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T',
-         20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z'}
+dict2 = {
+    0: "A",
+    1: "B",
+    2: "C",
+    3: "D",
+    4: "E",
+    5: "F",
+    6: "G",
+    7: "H",
+    8: "I",
+    9: "J",
+    10: "K",
+    11: "L",
+    12: "M",
+    13: "N",
+    14: "O",
+    15: "P",
+    16: "Q",
+    17: "R",
+    18: "S",
+    19: "T",
+    20: "U",
+    21: "V",
+    22: "W",
+    23: "X",
+    24: "Y",
+    25: "Z",
+}
 
 
 # This function generates the key in
@@ -30,13 +76,13 @@ def generate_key(message: str, key: str) -> str:
 # This function returns the encrypted text
 # generated with the help of the key
 def cipherText(message: str, key_new: str) -> str:
-    cipher_text = ''
+    cipher_text = ""
     i = 0
     for letter in message:
-        if letter == ' ':
-            cipher_text += ' '
+        if letter == " ":
+            cipher_text += " "
         else:
-            x = (dict1[letter]-dict1[key_new[i]]) % 26
+            x = (dict1[letter] - dict1[key_new[i]]) % 26
             i += 1
             cipher_text += dict2[x]
     return cipher_text
@@ -45,25 +91,27 @@ def cipherText(message: str, key_new: str) -> str:
 # This function decrypts the encrypted text
 # and returns the original text
 def originalText(cipher_text: str, key_new: str) -> str:
-    or_txt = ''
+    or_txt = ""
     i = 0
     for letter in cipher_text:
-        if letter == ' ':
-            or_txt += ' '
+        if letter == " ":
+            or_txt += " "
         else:
-            x = (dict1[letter]+dict1[key_new[i]]+26) % 26
+            x = (dict1[letter] + dict1[key_new[i]] + 26) % 26
             i += 1
             or_txt += dict2[x]
     return or_txt
 
 
 def main():
-    message = 'THE GERMAN ATTACK'
-    key = 'SECRET'
+    message = "THE GERMAN ATTACK"
+    key = "SECRET"
     key_new = generate_key(message, key)
     cipher_text = cipherText(message, key_new)
     original_text = originalText(cipher_text, key_new)
     print("Encrypted Text =", cipher_text)
     print("Original Text =", original_text)
 
-main()
+
+if __name__ == "__main__":
+    main()
