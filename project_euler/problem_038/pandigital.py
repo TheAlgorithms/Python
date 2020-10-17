f5181cdcb000384d02cc826714d18ee634482a42
Dark00infinity <47387653+dark00infinity@users.noreply.github.com>
2020-10-17 18:45:01 +0530

    problem_038 solution
    solution for Euler problem_038 (pandigital multiples)

---
+++ project_euler/problem_038/pandigital.py
@@ -0,0 +1,50 @@
+"""
+Project Euler Problem [problem_038]: https://projecteuler.net/problem=38
+
+...[Problem statement: Take the number 192 and multiply it by each of 1, 2, and 3:
+
+                    192 × 1 = 192
+                    192 × 2 = 384
+                    192 × 3 = 576
+By concatenating each product we get the 1 to 9 pandigital, 192384576. We will call 192384576 the concatenated product of 192 and (1,2,3)
+
+The same can be achieved by starting with 9 and multiplying by 1, 2, 3, 4, and 5, giving the pandigital, 918273645, which is the concatenated product of 9 and (1,2,3,4,5).
+
+What is the largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer with (1,2, ... , n) where n > 1?] ...
+    
+"""
+def solution():
+    # largest pandigital number
+    largest = 0
+    
+    # for loop to loop till 4 digits
+    for i in range(1,10000):
+    	
+    	# value for concatenated product
+    	multiplication = ''
+    	
+    	# (1,2,3,4,.....n)
+    	integer = 1
+    	
+    	# if the multiplication < 9 digits
+    	while len(multiplication) < 9:
+    		
+    		# concatenating the product at each stage
+    		multiplication += str(i*integer)
+    		
+    		# incrementing (1,2,3,4,....n)
+    		integer += 1
+    		
+    	# check for digits less than 9
+    	# check for all 1-9 numbers
+    	# check if '0' not in concatenated sting
+    	if ((len(multiplication) == 9) and (len(set(multiplication)) == 9) 
+    		and ('0' not in multiplication)):
+    	
+    		# check if multiplication is greater than largest
+    		if int(multiplication) > largest:
+    			largest = int(multiplication)
+    
+    # return the largest
+    return(largest)
+solution()
