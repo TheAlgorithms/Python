66c8839218741b5656fd23901f7b869ea6665fe4
Dark00infinity <47387653+dark00infinity@users.noreply.github.com>
2020-10-17 22:59:23 +0530

    Update sol2.py

--- project_euler/problem_038/sol2.py
+++ project_euler/problem_038/sol2.py
@@ -12,21 +12,23 @@ def solution():
     # for loop to loop till 4 digits
     for i in range(1, 10000):
         # value for concatenated string
-        mul = ''
+        mul = ""
         int_eger = 1
         # if the multiplication < 9 digits
         while len(mul) < 9:
             # concatenating the product at each stage
-            mul += str(i*int_eger)
+            mul += str(i * int_eger)
             int_eger += 1
         # check for digits less than 9
         # check for all 1-9 numbers
         # check if '0' not in concatenated string
-        if ((len(mul) == 9) and (len(set(mul)) == 9) and ('0' not in mul)):
+        if (len(mul) == 9) and (len(set(mul)) == 9) and ("0" not in mul):
             # check if multiplication is greater than largest
             if int(mul) > largest:
                 largest = int(mul)
     # return the largest
-    return(largest)
+    return largest
+
+
 if __name__ == "__main__":
     print(solution())
