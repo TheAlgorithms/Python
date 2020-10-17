e1b459a9e0f9bb3242882b68a7184ff795d68c21
sudheer-kumar-kapalavai <66019976+sudheer-kumar-kapalavai@users.noreply.github.com>
2020-10-17 17:11:33 +0530

    Fixes: #3362

--- other/reverse_order_of_vowels.py
+++ other/reverse_order_of_vowels.py
@@ -3,7 +3,7 @@ Given a string 'S' reverse the vowels order in that string.
 return the modified string where the position
 of constants are static and vowels got reversed.
 
-You may assume that each input would have exactly one solution, 
+You may assume that each input would have exactly one solution,
 if there are no vowels return the input.
 
 Example:
@@ -12,6 +12,7 @@ Example:
 'hilla progrommeng'
 """
 import doctest
+
 
 def is_vowel(char: str) -> bool:
     """
@@ -23,11 +24,11 @@ def is_vowel(char: str) -> bool:
         >>> is_vowel('f')
         False
     """
-    vowels = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u']
+    vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
     # Check for empty string
-    if(not char):
+    if not char:
         return False
-    return (char[0] in vowels)
+    return char[0] in vowels
 
 
 def get_vowels(string: str) -> list:
@@ -47,16 +48,17 @@ def get_vowels(string: str) -> list:
     length = len(string)
 
     while index < length:
-        if(is_vowel(string[index])):
+        if is_vowel(string[index]):
             vowels.append(string[index])
         index += 1
 
     return vowels
 
+
 def reverse_vowels(string: str) -> str:
     """
         Reverses the order of vowels in the given string
-        
+
         >>> reverse_vowels('Hello World')
         'Hollo Werld'
         >>> reverse_vowels('Algo & DS')
@@ -71,7 +73,7 @@ def reverse_vowels(string: str) -> str:
     length = len(string)
 
     while index < length:
-        if(is_vowel(string[index])):
+        if is_vowel(string[index]):
             answer = answer + vowels.pop()
         else:
             answer = answer + string[index]
@@ -79,8 +81,8 @@ def reverse_vowels(string: str) -> str:
 
     return answer
 
+
 if __name__ == "__main__":
     doctest.testmod()
-
     inputString = input().strip()
     print(f"{reverse_vowels(inputString)}")
