5bb6b250876f4bb73e37a92acdd816ccf29f2ba3
Dark00infinity <47387653+dark00infinity@users.noreply.github.com>
2020-10-17 20:46:45 +0530

    Update pandigital.py

--- project_euler/problem_038/pandigital.py
+++ project_euler/problem_038/pandigital.py
@@ -2,19 +2,19 @@
 problem_038: https://projecteuler.net/problem=38
 Problem statement: What is the largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer with (1,2, ... , n) where n > 1
 """
-def solution(n):
+def solution():
     # largest pandigital number
     largest = 0 
     # for loop to loop till 4 digits
-    for i in range(1,n):   	
+    for i in range(1,10000):   	
     	# value for concatenated string
     	multiplication = '' 	
-    	integer = 1  	
+    	int_eger = 1  	
     	# if the multiplication < 9 digits
     	while len(multiplication) < 9:  		
     		# concatenating the product at each stage
-    		multiplication += str(i*integer)        
-    		integer += 1
+    		multiplication += str(i*int_eger)        
+    		int_eger += 1
     	# check for digits less than 9
     	# check for all 1-9 numbers
     	# check if '0' not in concatenated string
@@ -22,8 +22,9 @@ def solution(n):
     		and ('0' not in multiplication)):	
     		# check if multiplication is greater than largest
     		if int(multiplication) > largest:
-    			largest = int(multiplication)  
+    			largest = int(multiplication) 
     # return the largest
     return(largest)
 if __name__=="__main__":
-    print(solution(10000))
+    print(solution())
+
