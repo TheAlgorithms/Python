69abfb90c8db725ebb656cc10e66cd6bf6c0949d
Sumagna Dey <49372356+sumagnadey@users.noreply.github.com>
2020-10-17 12:58:26 +0530

    Update wine_prob.py

--- dynamic_programming/wine_prob.py
+++ dynamic_programming/wine_prob.py
@@ -1,4 +1,12 @@
-"""Maximum profit from sale of wines"""
+"""Maximum profit from sale of wines
+Given n wines in a row, with integers denoting the cost of each wine respectively. 
+Each year you can sale the first or the last wine in the row. 
+However, the price of wines increases over time. 
+Let the initial profits from the wines be P1, P2, P3…Pn. On the Yth year, 
+the profit from the ith wine will be Y*Pi. For each year, 
+your task is to print “beg” or “end” denoting whether first or last wine should be sold. 
+Also, calculate the maximum profit from all the wines.
+"""
 
 def count_max_sl_td(winePr, i, j, yr, dp):
     if i > j:
