7d41624b2ef88ca47a5e11515e1ac0da05cd6b8f
RadadiyaMohit <30775542+radadiyamohit81@users.noreply.github.com>
2020-10-17 19:17:36 +0530

    update file

--- ciphers/mono_alphabetic_ciphers.py
+++ ciphers/mono_alphabetic_ciphers.py
@@ -1,50 +1,9 @@
 LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
-def main():
-    myMessage = "When you do the common things in life in an uncommon way, you will command the attention of the world"
-    myKey = "QWERTYUIOPASDFGHJKLZXCVBNM"
-    myMode = "decrypt"  # set to 'encrypt' or 'decrypt'
-
-    checkValidKey(myKey)
-
-    if myMode == "encrypt":
-        translated = encryptMessage(myKey, myMessage)
-    elif myMode == "decrypt":
-        translated = decryptMessage(myKey, myMessage)
-    print("Using key %s" % (myKey))
-    print("The %sed message is:" % (myMode))
-    print(translated)
-
-
-def checkValidKey(key: str):
-    keyList = list(key)
-    lettersList = list(LETTERS)
-    keyList.sort()
-    lettersList.sort()
-    if keyList != lettersList:
-        sys.exit("There is an error in the key or symbol set.")
-
-
-def encryptMessage(key: str, message: str) -> str:
+def translate_message(key, message, mode):
     """
-    >>> encryptMessage("QWERTYUIOPASDFGHJKLZXCVBNM", "When you do the common things in life in an uncommon way, you will command the attention of the world")
-    'Vitf ngx rg zit egddgf zioful of soyt of qf xfegddgf vqn, ngx voss egddqfr zit qzztfzogf gy zit vgksr'
-    """
-    return translateMessage(key, message, "encrypt")
-
-
-def decryptMessage(key, message):
-    """
-    >>> decryptMessage("QWERTYUIOPASDFGHJKLZXCVBNM", "When you do the common things in life in an uncommon way, you will command the attention of the world")
-    'Bpcy fig mi epc vizziy ephyol hy shnc hy ky gyvizziy bkf, fig bhss vizzkym epc keecyehiy in epc bidsm'
-    """
-    return translateMessage(key, message, "decrypt")
-
-
-def translateMessage(key, message, mode):
-    """
-    >>> translateMessage("QWERTYUIOPASDFGHJKLZXCVBNM", "When you do the common things in life in an uncommon way, you will command the attention of the world", "encrypt")
+    >>> translate_message("QWERTYUIOPASDFGHJKLZXCVBNM", "When you do the common things in life in an uncommon way, you will command the attention of the world", "encrypt")
     'Vitf ngx rg zit egddgf zioful of soyt of qf xfegddgf vqn, ngx voss egddqfr zit qzztfzogf gy zit vgksr'
     """
     translated = ""
@@ -67,6 +26,47 @@ def translateMessage(key, message, mode):
             # symbol is not in LETTERS, just add it
             translated += symbol
     return translated
+
+
+def encrypt_message(key: str, message: str) -> str:
+    """
+    >>> encrypt_message("QWERTYUIOPASDFGHJKLZXCVBNM", "When you do the common things in life in an uncommon way, you will command the attention of the world")
+    'Vitf ngx rg zit egddgf zioful of soyt of qf xfegddgf vqn, ngx voss egddqfr zit qzztfzogf gy zit vgksr'
+    """
+    return translate_message(key, message, "encrypt")
+
+
+def decrypt_message(key, message):
+    """
+    >>> decrypt_message("QWERTYUIOPASDFGHJKLZXCVBNM", "When you do the common things in life in an uncommon way, you will command the attention of the world")
+    'Bpcy fig mi epc vizziy ephyol hy shnc hy ky gyvizziy bkf, fig bhss vizzkym epc keecyehiy in epc bidsm'
+    """
+    return translate_message(key, message, "decrypt")
+
+
+def check_validKey(key: str):
+    keyList = list(key)
+    lettersList = list(LETTERS)
+    keyList.sort()
+    lettersList.sort()
+    if keyList != lettersList:
+        sys.exit("There is an error in the key or symbol set.")
+
+
+def main():
+    myMessage = "When you do the common things in life in an uncommon way, you will command the attention of the world"
+    myKey = "QWERTYUIOPASDFGHJKLZXCVBNM"
+    myMode = "decrypt"  # set to 'encrypt' or 'decrypt'
+
+    check_validKey(myKey)
+
+    if myMode == "encrypt":
+        translated = encrypt_message(myKey, myMessage)
+    elif myMode == "decrypt":
+        translated = decrypt_message(myKey, myMessage)
+    print("Using key %s" % (myKey))
+    print("The %sed message is:" % (myMode))
+    print(translated)
 
 
 if __name__ == "__main__":
