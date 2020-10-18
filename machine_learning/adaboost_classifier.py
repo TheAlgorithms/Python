27ed08f9c9bb9daf3bdd9ac9762a648d85cf887d
deril2605 <derilraju.com>
2020-10-18 11:42:32 +0530

    adding adaboost classifier from scratch

---
+++ machine_learning/adaboost_classifier.py
@@ -0,0 +1,125 @@
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+# Input data files are available in the read-only "../input/" directory
+# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+#import libraries
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+#acc metric
+def accuracy(y_true, y_pred):
+    accuracy = np.sum(y_true == y_pred) / len(y_true)
+    return accuracy
+
+#dataset load
+data = datasets.load_breast_cancer()
+X = data.data
+y = data.target
+y[y==0]=-1
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+
+# Decision stump used as weak classifier
+class DecisionStump():
+    def __init__(self):
+        self.polarity = 1
+        self.feature_idx = None
+        self.threshold = None
+        self.alpha = None
+
+    def predict(self, X):
+        n_samples = X.shape[0]
+        X_column = X[:, self.feature_idx]
+        predictions = np.ones(n_samples)
+        if self.polarity == 1:
+            predictions[X_column < self.threshold] = -1
+        else:
+            predictions[X_column > self.threshold] = -1
+
+        return predictions
+
+#Adaboost classifier
+class Adaboost():
+
+    def __init__(self, n_clf=5):
+        self.n_clf = n_clf
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+
+        # Initialize weights to 1/N
+        w = np.full(n_samples, (1 / n_samples))
+
+        self.clfs = []
+        # Iterate through classifiers
+        for _ in range(self.n_clf):
+            clf = DecisionStump()
+
+            min_error = float('inf')
+            # greedy search to find best threshold and feature
+            for feature_i in range(n_features):
+                X_column = X[:, feature_i]
+                thresholds = np.unique(X_column)
+
+                for threshold in thresholds:
+                    # predict with polarity 1
+                    p = 1
+                    predictions = np.ones(n_samples)
+                    predictions[X_column < threshold] = -1
+
+                    # Error = sum of weights of misclassified samples
+                    misclassified = w[y != predictions]
+                    error = sum(misclassified)
+
+                    if error > 0.5:
+                        error = 1 - error
+                        p = -1
+
+                    # store the best configuration
+                    if error < min_error:
+                        clf.polarity = p
+                        clf.threshold = threshold
+                        clf.feature_idx = feature_i
+                        min_error = error
+
+            # calculate alpha
+            EPS = 1e-10
+            clf.alpha = 0.5 * np.log((1.0 - min_error + EPS) / (min_error + EPS))
+
+            # calculate predictions and update weights
+            predictions = clf.predict(X)
+
+            w *= np.exp(-clf.alpha * y * predictions)
+            # Normalize to one
+            w /= np.sum(w)
+
+            # Save classifier
+            self.clfs.append(clf)
+
+    def predict(self, X):
+        clf_preds = [clf.alpha * clf.predict(X) for clf in self.clfs]
+        y_pred = np.sum(clf_preds, axis=0)
+        y_pred = np.sign(y_pred)
+
+        return y_pred
+
+#Instantiate class & train
+clf = myAdaBoost()
+clf.fit(X_train, y_train)
+
+# predict classes
+y_pred = clf.predict(X_test)
+acc = accuracy(y_test, y_pred)
+print(acc)
+
