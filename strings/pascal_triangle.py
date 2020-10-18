8b887a1a79a88f69ca8decbd791c7495dffd4519
Mahi Monga <61409868+mahimonga@users.noreply.github.com>
2020-10-19 00:31:27 +0530

    Update pascal_triangle.py

--- strings/pascal_triangle.py
+++ strings/pascal_triangle.py
@@ -12,10 +12,14 @@ Input: Integer
 Output: String
 Sample Input: 4 
 Sample Output: "1 4 6 4 1"
+
+
 '''
 #my newbie solution:
 def pascals_triangle(row):
     new_string = ""
+
+    
     for i in range (0,row+1):
         new_string+=str(bin_coeff(row,i))+" "
     return new_string.rstrip()
@@ -25,8 +29,12 @@ def pascals_triangle(row):
    
 def bin_coeff(row,col):
     result = 1
+    
     if col > (row-col):
         col = row - col
+    
+    
+    
     for i in range (0,col):
         result *=(row -i)
         result//=(i+1)
