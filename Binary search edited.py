0776fd459eed04e71446bb1fde045d0de1f7946c
adityaverma121 <73019438+adityaverma121@users.noreply.github.com>
2020-10-18 11:19:41 +0530

    Add files via upload

---
+++ Binary search edited.py
@@ -0,0 +1,39 @@
+# Python3 Program for recursive binary search. 
+  
+# Returns index of x in arr if present, else -1 
+def binarySearch (arr, l, r, x): 
+  
+    # Check base case 
+    if r >= l: 
+  
+        mid = l + (r - l) // 2
+  
+        # If element is present at the middle itself 
+        if arr[mid] == x: 
+            return mid 
+          
+        # If element is smaller than mid, then it  
+        # can only be present in left subarray 
+        elif arr[mid] > x: 
+            return binarySearch(arr, l, mid-1, x) 
+  
+        # Else the element can only be present  
+        # in right subarray 
+        else: 
+            return binarySearch(arr, mid + 1, r, x) 
+  
+    else: 
+        # Element is not present in the array 
+        return -1
+  
+# Driver Code 
+arr = [ 2, 3, 4, 10, 40 ] 
+x = 10
+  
+# Function call 
+result = binarySearch(arr, 0, len(arr)-1, x) 
+  
+if result != -1: 
+    print ("Element is present at index % d" % result) 
+else: 
+    print ("Element is not present in array") 
