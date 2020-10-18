ca51403d98bc031c353a761bae370381f57cadef
Basil Victor <basilbennedict@gmail.com>
2020-10-18 21:52:14 +0530

    Added program to calculate distance between two coordinates

---
+++ maths/distance.py
@@ -0,0 +1,20 @@
+"""
+Finding distance between a pair of coordinates (x1, y1) and (x2, y2)
+"""
+
+
+def getDistance(x1, y1, x2, y2):
+    """
+    Using the standard distance Formula
+    **2 is to perform square operation
+    **5 is to perform square root operation
+    """
+    return ((x2 - x1)**2 + (y2 - y1)**2) **0.5
+
+
+if __name__ == "__main__":
+    x1 = 2
+    y1 = 4
+    x2 = 7
+    y2 = 16
+    print(f"The distance between ({x1}, {y1}) and ({x2}, {y2}) is {getDistance(x1, y1, x2, y2)} units")
