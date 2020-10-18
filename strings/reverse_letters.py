3cde411a35aecd564142278d95a8816c80804f00
Snimerjot Singh <snimersingh1729@gmail.com>
2020-10-18 15:38:12 +0530

    Added reverse_letters.py

--- strings/reverse_letters.py
+++ strings/reverse_letters.py
@@ -1,6 +1,6 @@
 def reverse_letters(input_str: str) -> str:
     '''
-    Reverses letters in a given string without adjusting the postion of the words
+    Reverses letters in a given string without adjusting the position of the words
     >>> reverse_letters('The cat in the hat')
     'ehT tac ni eht tah'
     >>> reverse_letters('The quick brown fox jumped over the lazy dog.')
@@ -8,10 +8,10 @@ def reverse_letters(input_str: str) -> str:
     >>> reverse_letters('Is this true?')
     'sI siht ?eurt'
     '''
-    arr = []  # create an empty list
-    for i in input_str.split(' '):  # Runs a for loop in a list of the string separated by space
-        arr.append(i[::-1])  # Appends reversed letter of a single word
-    return ' '.join(arr) # returns the arr joined with spaces
+    reversed_letters_list = []
+    for i in input_str.split(' '):
+        reversed_letters_list.append(i[::-1])
+    return ' '.join(reverse_letters)
 
 
 if __name__ == '__main__':
