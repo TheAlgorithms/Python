7adb75fee8da8b3f4dfdfe506118bf085a1949e6
DhyeyThakkar27 <70805890+DhyeyThakkar27@users.noreply.github.com>
2020-10-18 15:26:07 +0530

    Update balanced_brackets.py

--- data_structures/stacks/balanced_brackets.py
+++ data_structures/stacks/balanced_brackets.py
@@ -1,2 +1,14 @@
+from .stack import Stack
+
+__author__ = "Dhyey Thakkar"
+
 def balanced_brackets(brackets):
+  """
+  >>>Use a Stack to check if the given string of brackets is balanced or not
+  
+  """
+  brackets_stack = Stack(len(brackets))
+  for bracket in brackets:
+    if bracket in "{[(":
+      bracket_stack.push(bracket)
   
