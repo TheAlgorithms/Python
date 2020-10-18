bd569c188f95aaed11dc3b2a7a385165894bfff3
DhyeyThakkar27 <70805890+DhyeyThakkar27@users.noreply.github.com>
2020-10-18 15:44:35 +0530

    Updated balanced_brackets

--- data_structures/stacks/balanced_brackets.py
+++ data_structures/stacks/balanced_brackets.py
@@ -4,11 +4,26 @@ __author__ = "Dhyey Thakkar"
 
 def balanced_brackets(brackets):
   """
-  >>>Use a Stack to check if the given string of brackets is balanced or not
+  >>>Use a Stack to check if the given string of brackets is balanced or not 
+     P.S it is updated part of balanced_parentheses.In this you can use "[{(<>)}]"
+  >>>	balanced_brackets("{{[]}}()(<>)")
+      returns True
+  
+  >>> balanced_brackets("[{]()<>")
+      retruns False
+	
   
   """
   brackets_stack = Stack(len(brackets))
   for bracket in brackets:
-    if bracket in "{[(":
+    if bracket in "{[(<":
       bracket_stack.push(bracket)
+    elif bracket in ">]})":
+      if bracket_stack.is_empty() or bracket_stack.peek() != bracket:
+        return False
+      bracket_stack.pop()
+  return bracket_stack.is_empty()
+
+
+
   
