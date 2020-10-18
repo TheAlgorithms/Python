eb4f4c9a306b4392703b2a40e7618264cb2e0397
RadadiyaMohit <30775542+radadiyamohit81@users.noreply.github.com>
2020-10-18 10:39:56 +0530

    update sol1

--- project_euler/problem_117/sol1.py
+++ project_euler/problem_117/sol1.py
@@ -2,10 +2,10 @@
 Project Euler Problem 104: https://projecteuler.net/problem=117
 Using a combination of grey square tiles and oblong tiles chosen from:
 red tiles (measuring two units), green tiles (measuring three units),
-and blue tiles (measuring four units), it is possible to tile
-a row measuring five units in length in exactly fifteen different ways.
+and blue tiles (measuring four units), it is possible to tile a row
+measuring five units in length in exactly fifteen different ways.
 
-Que. How many ways can a row n units long be filled with
+How many ways can a row n units long be filled with
 """
 
 
@@ -16,7 +16,7 @@ def solution(length: int) -> str:
     """
     ways = [1] + [0] * length
     for n in range(1, len(ways)):
-        ways[n] += sum(ways[max(n - 4, 0):n])
+        ways[n] += sum(ways[max(n-4, 0):n])
     return str(ways[-1])
 
 
