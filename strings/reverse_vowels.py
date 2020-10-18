c843a35d8d352eeedb2e3d8e8e14d35afadaa8b1
shivani-yamavaram <sreeshivanijuly07@gmail.com>
2020-10-18 17:18:53 +0530

    added reverse_vowels.py

--- strings/reverse_vowels.py
+++ strings/reverse_vowels.py
@@ -1,28 +1,17 @@
-
-def reverseVowels( s):
-    chars = list(s)
-    idx = []
-    v = []
-
-    for i in range(len(chars)):
-        if chars[i] in ['a','e','i','o','u']:
-            v.append(chars[i])
-            idx.append(i)
-
-    v = v[::-1]
-    final = []
-
-    ind = 0
-    for i in range(len(chars)):
-        if i in idx:
-            final.append(v[ind])
-            ind += 1
+def reversevowel(s):
+    st =[]
+    k = ""
+    for i in range(len(s)):
+        if s[i] in ['a','e','i','o','u']:
+            st.append(s[i])
+    for i in range(len(s)):
+        if s[i] in ['a','e','i','o','u']:
+            k+= st.pop()
         else:
-            final.append(chars[i])
+            k+=s[i]
 
-    str1 = ""
-    return str1.join(final)
+    return k
 
-
-s=input()
-print(reverseVowels(s))
+if __name__ == "__main__":
+    s=input()
+    print(reversevowel(s))
