1d6eb49b9d134fcde45a02f74b7f39f33a857096
tanyonghe <tanyonghe@u.nus.edu>
2020-10-19 13:36:33 +0800

    Add doctest and remove input() usage

--- ciphers/base16.py
+++ ciphers/base16.py
@@ -1,13 +1,21 @@
 import base64
+import doctest
 
 
-def main():
-    inp = input("->")
+def encode_to_b16(inp):
+    """
+    Encodes a given utf-8 string into base-16.
+    >>> encode_to_b16('Hello World!')
+    b'48656C6C6F20576F726C6421'
+    >>> encode_to_b16('HELLO WORLD!')
+    b'48454C4C4F20574F524C4421'
+    >>> encode_to_b16('')
+    b''
+    """
     encoded = inp.encode("utf-8")  # encoded the input (we need a bytes like object)
     b16encoded = base64.b16encode(encoded)  # b16encoded the encoded string
-    print(b16encoded)
-    print(base64.b16decode(b16encoded).decode("utf-8"))  # decoded it
+    return b16encoded
 
 
 if __name__ == "__main__":
-    main()
+    doctest.testmod()
