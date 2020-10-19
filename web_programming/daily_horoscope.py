76e43bdfa65e31c9485c095833ba40ceab31bb2c
Noopur <41565380+naman1303@users.noreply.github.com>
2020-10-19 23:34:46 +0530

    Update daily_horoscope.py
    Added validation on user input for zodiac sign and day. Also used tuple and made minor changes.

--- web_programming/daily_horoscope.py
+++ web_programming/daily_horoscope.py
@@ -14,7 +14,7 @@ def horoscope(zodiac_sign: int, day: str) -> str:
 if __name__ == "__main__":
     print("Daily Horoscope. \n")
     print(
-        "enter your Zodiac sign number:\n",
+        "Zodiac signs:\n",
         "1. Aries\n",
         "2. Taurus\n",
         "3. Gemini\n",
@@ -28,8 +28,18 @@ if __name__ == "__main__":
         "11. Aquarius\n",
         "12. Pisces\n",
     )
-    zodiac_sign = int(input("number> ").strip())
-    print("choose some day:\n", "yesterday\n", "today\n", "tomorrow\n")
-    day = input("enter the day> ")
-    horoscope_text = horoscope(zodiac_sign, day)
-    print(horoscope_text)
+    zodiac_sign = int(input("Select a sign [1-12] > ").strip())
+    if zodiac_sign not in range(0, 13):
+        print("Invalid choice of Zodian sign!")
+    else:
+        days_list = {"1": "Yesterday", "2": "Today", "3": "Tomorrow"}
+        print("\nDays available:")
+        for day in days_list.items():
+            print("{}. {}".format(day[0], day[1]))
+        inp_day = input("Choose a day [1-3] > ")
+        if inp_day not in days_list:
+            print("Invalid choice of day!")
+        else:
+            day = days_list[inp_day].lower()
+            horoscope_text = horoscope(zodiac_sign, day)
+            print(horoscope_text)
