c8eff0d10bf3927f6891867af0f01ad1d9beab12
devangi2000 <devangidp2000@gmail.com>
2020-10-19 13:23:45 +0530

    added CNN implementation of MNIST dataset

---
+++ neural_network/mnist_train_cnn.py
@@ -0,0 +1,87 @@
+from __future__ import print_function
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+import json
+from keras.models import model_from_json, load_model, save_model
+
+batch_size = 128
+num_classes = 10
+epochs = 12
+# input image dimensions
+img_rows, img_cols = 28, 28
+# the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+import requests
+requests.packages.urllib3.disable_warnings()
+import ssl
+try:
+   _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+   # Legacy Python that doesn't verify HTTPS certificates by default
+   pass
+else:
+   # Handle target environment that doesn't support HTTPS verification
+   ssl._create_default_https_context = _create_unverified_https_context
+
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+#Neural networks perform much better when the output label is fed as a
+# sparse matrix so we convert the y-label for both train and test data as a
+# sparse matrix.
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+  json_file.write(model_json)
+model.save_weights("model.h5")
+
+
+
