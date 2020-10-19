de643925cd7fed99ac8dd01ac0cd4d2306770c02
Andrew <54547089+ac-tam@users.noreply.github.com>
2020-10-18 21:41:58 -0400

    Update euclidean_distance.py

--- maths/euclidean_distance.py
+++ maths/euclidean_distance.py
@@ -1,20 +1,21 @@
 import numpy as np
+from typing import Union, List, Tuple
 
-def euclidean_distance(v1, v2) -> double:
+Vector = Union[List[float], List[int], Tuple[float], Tuple[int], np.ndarray]
+VectorOut = Union[np.float64, int, float]
+
+def euclidean_distance(vector_1: Vector, vector_2: Vector) -> VectorOut:
     """
-    Calculate the distance between the two = endpoints of two vectors, v1 and v2.
-    >>> euclidean_distance(np.array([0, 0]), np.array([2, 2]))
+    Calculate the distance between the two endpoints of two vectors. A vector is defined as a list, tuple, or numpy 1D array.
     2.8284271247461903
     >>> euclidean_distance(np.array([0, 0, 0]), np.array([2, 2, 2]))
     3.4641016151377544
     >>> euclidean_distance(np.array([1, 2, 3, 4]), np.array([5, 6, 7, 8]))
     8.0
+    >>> euclidean_distance([1, 2, 3, 4], [5, 6, 7, 8])
+    8.0
     """
-
-    sumSquared = 0 
-    for i in range(len(v1)):
-        sumSquared += (v2[i] - v1[i]) ** 2
-    return sumSquared ** (1 / 2)
+    return np.sqrt(np.sum((np.asarray(vector_1) - np.asarray(vector_2))**2))
 
 if __name__ == "__main__":
     point = np.array([2, 2])
