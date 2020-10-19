052795fb1062812af00e619e8812ebfed5f48792
Abhishek Jain <55635206+fiddlytompkus@users.noreply.github.com>
2020-10-19 12:35:54 +0530

    Update length_of_longest_palindromic_subsequence.py

--- dynamic_programming/length_of_longest_palindromic_subsequence.py
+++ dynamic_programming/length_of_longest_palindromic_subsequence.py
@@ -1,30 +1,33 @@
 def longestPalindrome(X, i, j):
 
-	# base condition
-	if i > j:
-		return 0
+   # base condition
+   if i > j:
+       return 0
 
-	# if X has only one character, it is palindrome
-	if i == j:
-		return 1
+   # if X has only one character, it is palindrome
+   if i == j:
+       return 1
+       
+   # if last character of the string is same as the first character
+   if X[i] == X[j]:
+       # include first and last characters in palindrome
+       # and recur for the remaining substring X[i+1, j-1]
+       return longestPalindrome(X, i + 1, j - 1) + 2
 
-	# if last character of the string is same as the first character
-	if X[i] == X[j]:
-		# include first and last characters in palindrome
-		# and recur for the remaining substring X[i+1, j-1]
-		return longestPalindrome(X, i + 1, j - 1) + 2
+   # if last character of string is different to the first character
 
-	# if last character of string is different to the first character
-
-	# 1. Remove last character & recur for the remaining substring X[i, j-1]
-	# 2. Remove first character & recur for the remaining substring X[i+1, j]
-	# return maximum of the two values
-	return max(longestPalindrome(X, i, j - 1), longestPalindrome(X, i + 1, j))
+   # 1. Remove last character & recur for the remaining substring X[i, j-1]
+   # 2. Remove first character & recur for the remaining substring X[i+1, j]
+   # return maximum of the two values
+   return max(longestPalindrome(X, i, j - 1), longestPalindrome(X, i + 1, j))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-	X = "ABBDCACB"
-	n = len(X)
+   X = "ABBDCACB"
+   n = len(X)
 
-	print("The length of Longest Palindromic Subsequence is",longestPalindrome(X, 0, n - 1))
+   print(
+       "The length of Longest Palindromic Subsequence is",
+       longestPalindrome(X, 0, n - 1)
+   )
