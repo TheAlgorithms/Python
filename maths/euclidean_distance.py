b5152b23f15622feca7515fb69ffb13e6dc84ed3
Andrew <54547089+ac-tam@users.noreply.github.com>
2020-10-18 21:51:20 -0400

    Update euclidean_distance.py

--- maths/euclidean_distance.py
+++ maths/euclidean_distance.py
@@ -4,9 +4,11 @@ from typing import Union, List, Tuple
 Vector = Union[List[float], List[int], Tuple[float], Tuple[int], np.ndarray]
 VectorOut = Union[np.float64, int, float]
 
+
 def euclidean_distance(vector_1: Vector, vector_2: Vector) -> VectorOut:
     """
-    Calculate the distance between the two endpoints of two vectors. A vector is defined as a list, tuple, or numpy 1D array.
+    Calculate the distance between the two endpoints of two vectors.
+    A vector is defined as a list, tuple, or numpy 1D array.
     2.8284271247461903
     >>> euclidean_distance(np.array([0, 0, 0]), np.array([2, 2, 2]))
     3.4641016151377544
@@ -16,6 +18,7 @@ def euclidean_distance(vector_1: Vector, vector_2: Vector) -> VectorOut:
     8.0
     """
     return np.sqrt(np.sum((np.asarray(vector_1) - np.asarray(vector_2)) ** 2))
+
 
 if __name__ == "__main__":
     point = np.array([2, 2])
