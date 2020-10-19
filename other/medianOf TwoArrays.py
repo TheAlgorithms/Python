8db9cef4280df49ba28144acc80a78d1b0423dfa
anneCoder1805 <66819522+anneCoder1805@users.noreply.github.com>
2020-10-19 16:55:52 +0530

    Create medianOf TwoArrays.py
    This code finds the median of two arrays (which may or may not be sorted initially).
    Example: 
    Enter elements of an array: 1 5 4 2
    Enter elements of another array: 1 7 4 2 7
    The median of two arrays is :  4

---
+++ other/medianOf TwoArrays.py
@@ -0,0 +1,15 @@
+# This code finds the median of two arrays, even if they are not sorted initially
+def findMedianArrays(nums1, nums2):
+    list3 = nums1+nums2
+    list3.sort()
+    if len(list3)%2==1:
+        a = int(len(list3)/2)
+        return list3[a]
+    else:
+        a = int(len(list3)/2)
+        return  (list3[a] + list3[a - 1])/2
+    
+if __name__ == "__main__":
+    n1 = list(map(int, input('Enter elements of an array: ').split()))
+    n2 = list(map(int, input('Enter elements of another array: ').split()))
+    print('The median of two arrays is: ', findMedianArrays(n1,n2))
