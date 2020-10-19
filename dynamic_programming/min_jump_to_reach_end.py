bcfcbac3a4768188eb36e107f11d56ef2a0ed485
hieetesh <dharmadhikarihitesh@gmail.com>
2020-10-19 13:52:47 +0530

    made changes black and flake8

--- dynamic_programming/min_jump_to_reach_end.py
+++ dynamic_programming/min_jump_to_reach_end.py
@@ -1,15 +1,8 @@
 """
-Minimum number of jumps to reach end using DYNAMIC PROGRAMMING approach 
-.
+Minimum number of jumps to reach end using DYNAMIC PROGRAMMING approach
 Time Complexity: O(n)
 Space Complexity: O(1)
-.
-Description: 
-            Given an array of integers where each element represents the max number of steps that can be made forward 
-            from that element. Write a function to return the minimum number of jumps to reach the end of the array 
-            (starting from the first element). If an element is 0, then we cannot move through that element.
-
-For Example: 
+For Example:
             Input:  arr[] = {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9}
             Output: 3 (1-> 3 -> 8 -> 9)
 """
@@ -23,48 +16,48 @@ def min_jump_to_reach_end(arr: list) -> int:
 
     n = len(arr)
 
-    # The number of jumps needed to reach the starting index is 0 
-    if (n <= 1): 
+    # The number of jumps needed to reach the starting index is 0
+    if n <= 1:
         return 0
-   
-    # Return -1 if not possible to jump 
-    if (arr[0] == 0): 
+
+    # Return -1 if not possible to jump
+    if arr[0] == 0:
         return -1
-    
-    # initialization 
-    # stores all time the maximal reachable index in the array 
-    maxReach = arr[0]   
-    # stores the amount of steps we can still take 
-    step = arr[0] 
-    # stores the amount of jumps necessary to reach that maximal reachable position 
-    jump = 1 
-    
-    # Start traversing array 
-    
-    for i in range(1, n): 
-        # Check if we have reached the end of the array 
-        if (i == n-1): 
-            return jump 
-    
-        # updating maxReach 
-        maxReach = max(maxReach, i + arr[i]) 
-    
-        # we use a step to get to the current index 
-        step -= 1; 
-    
-        # If no further steps left 
-        if (step == 0): 
-        # we must have used a jump 
+
+    # initialization
+    # stores all time the maximal reachable index in the array
+    maxReach = arr[0]
+    # stores the amount of steps we can still take
+    step = arr[0]
+    # stores the jumps required to reach maximal reachable position
+    jump = 1
+
+    # Start traversing array
+
+    for i in range(1, n):
+        # Check if we have reached the end of the array
+        if i == n - 1:
+            return jump
+
+        # updating maxReach
+        maxReach = max(maxReach, i + arr[i])
+
+        # we use a step to get to the current index
+        step -= 1
+
+        # If no further steps left
+        if step == 0:
+            # we must have used a jump
             jump += 1
-            
-            # Check if the current index / position or lesser index 
-            # is the maximum reach point from the previous indexes 
-            if(i >= maxReach): 
+
+            # Check if the current index / position or lesser index
+            # is the maximum reach point from the previous indexes
+            if i >= maxReach:
                 return -1
-        
-            # re-initialize the steps to the amount 
-            # of steps to reach maxReach from position i. 
-            step = maxReach - i; 
+
+            # re-initialize the steps to the amount
+            # of steps to reach maxReach from position i.
+            step = maxReach - i
     return -1
 
 
