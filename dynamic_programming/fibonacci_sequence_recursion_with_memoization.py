da7a7419f0755ecbb277267bbf924282da2e236a
Samuel da Costa Araujo Nunes <samueldacostaaraujonunes@gmail.com>
2020-10-19 11:14:31 -0300

    Adicionando Ideias de @poyea

--- dynamic_programming/fibonacci_sequence_recursion_with_memoization.py
+++ dynamic_programming/fibonacci_sequence_recursion_with_memoization.py
@@ -3,10 +3,8 @@
 # Memoization Concept: https://en.wikipedia.org/wiki/Memoization
 # Fibonacci Concept : https://pt.wikipedia.org/wiki/Sequ%C3%AAncia_de_Fibonacci
 
-cache: dict = dict()
 
-
-def fibonacci(n: int) -> int:
+def fibonacci(n: int, cache: dict = dict()) -> int:
     """
     Return number of index n in sequence fibonacci
     >>> [fibonacci(i) for i in range(12)]
@@ -23,7 +21,7 @@ def fibonacci(n: int) -> int:
         if n in {0, 1}:
             ans = n
         else:
-            ans = fibonacci(n - 1) + fibonacci(n - 2)
+            ans = fibonacci(n - 1, cache) + fibonacci(n - 2, cache)
         cache[aux] = ans
 
         return ans
@@ -38,11 +36,13 @@ def main() -> None:
         number = int(input("Enter an integer greater than or equal to 0: "))
         print(f"The first {number} Fibonacci sequence terms are:")
         print([fibonacci(n) for n in range(number)])
-        print(f"The {number} index of the fibonacci sequence:")
+        print(f"The {number} index of the fibonacci sequence: ", end="")
         print(fibonacci(number))
     except ValueError:
         print("Error: Input value should be an integer greater than or equal to 0")
 
 
 if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
     main()
