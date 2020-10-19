69e86bf64ced8b2edb6ede94c3d270a32a844ee7
Ankit Tiwari <tankit651@gmail.com>
2020-10-19 16:33:54 +0530

    some code of this folder Data_strutures has been change

---
+++ data_structures/stacks/infix to postfix.py
@@ -0,0 +1,95 @@
+# Python program to convert infix expression to postfix 
+
+# Class to convert the expression 
+class Conversion: 
+	
+	# Constructor to initialize the class variables 
+	def __init__(self, capacity): 
+		self.top = -1
+		self.capacity = capacity 
+		# This array is used a stack 
+		self.array = [] 
+		# Precedence setting 
+		self.output = [] 
+		self.precedence = {'+':1, '-':1, '*':2, '/':2, '^':3} 
+	
+	# check if the stack is empty 
+	def isEmpty(self): 
+		return True if self.top == -1 else False
+	
+	# Return the value of the top of the stack 
+	def peek(self): 
+		return self.array[-1] 
+	
+	# Pop the element from the stack 
+	def pop(self): 
+		if not self.isEmpty(): 
+			self.top -= 1
+			return self.array.pop() 
+		else: 
+			return "$"
+	
+	# Push the element to the stack 
+	def push(self, op): 
+		self.top += 1
+		self.array.append(op) 
+
+	# A utility function to check is the given character 
+	# is operand 
+	def isOperand(self, ch): 
+		return ch.isalpha() 
+
+	# Check if the precedence of operator is strictly 
+	# less than top of stack or not 
+	def notGreater(self, i): 
+		try: 
+			a = self.precedence[i] 
+			b = self.precedence[self.peek()] 
+			return True if a <= b else False
+		except KeyError: 
+			return False
+			
+	# The main function that converts given infix expression 
+	# to postfix expression 
+	def infixToPostfix(self, exp): 
+		
+		# Iterate over the expression for conversion 
+		for i in exp: 
+			# If the character is an operand, 
+			# add it to output 
+			if self.isOperand(i): 
+				self.output.append(i) 
+			
+			# If the character is an '(', push it to stack 
+			elif i == '(': 
+				self.push(i) 
+
+			# If the scanned character is an ')', pop and 
+			# output from the stack until and '(' is found 
+			elif i == ')': 
+				while( (not self.isEmpty()) and self.peek() != '('): 
+					a = self.pop() 
+					self.output.append(a) 
+				if (not self.isEmpty() and self.peek() != '('): 
+					return -1
+				else: 
+					self.pop() 
+
+			# An operator is encountered 
+			else: 
+				while(not self.isEmpty() and self.notGreater(i)): 
+					self.output.append(self.pop()) 
+				self.push(i) 
+
+		# pop all the operator from the stack 
+		while not self.isEmpty(): 
+			self.output.append(self.pop()) 
+
+		print "".join(self.output) 
+
+# Driver program to test above function 
+exp = "a+b*(c^d-e)^(f+g*h)-i"
+obj = Conversion(len(exp)) 
+obj.infixToPostfix(exp) 
+
+# This code is contributed by Ankit Tiwari
---
+++ data_structures/trie/Thread_binary_tree.py
@@ -0,0 +1,43 @@
+# Python3 program to find the all 
+# full nodes in a given binary tree 
+
+# Binary Tree Node 
+""" utility that allocates a newNode 
+with the given key """
+class newNode: 
+
+	# Construct to create a newNode 
+	def __init__(self, key): 
+		self.data = key 
+		self.left = None
+		self.right = None
+
+# Traverses given tree in Inorder 
+# fashion and prints all nodes that 
+# have both children as non-empty. 
+def findFullNode(root) : 
+
+	if (root != None) : 
+	
+		findFullNode(root.left) 
+		if (root.left != None and
+			root.right != None) : 
+			print(root.data, end = " ") 
+		findFullNode(root.right) 
+
+# Driver Code 
+if __name__ == '__main__': 
+
+	root = newNode(1) 
+	root.left = newNode(2) 
+	root.right = newNode(3) 
+	root.left.left = newNode(4) 
+	root.right.left = newNode(5) 
+	root.right.right = newNode(6) 
+	root.right.left.right = newNode(7) 
+	root.right.right.right = newNode(8) 
+	root.right.left.right.left = newNode(9) 
+	findFullNode(root) 
+
+# This code is contributed by 
+#Ankit Tiwari
