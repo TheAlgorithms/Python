39019799a0c771db9478bf72abcde57707634665
Nirdosh Gandhi <49865067+ngandhi369@users.noreply.github.com>
2020-10-19 12:07:15 +0530

    Update mutations.py

--- strings/mutations.py
+++ strings/mutations.py
@@ -7,10 +7,20 @@ abracadabra
 Output:-
 abrackdabra
 
-Hackerank Link:-
-https://www.hackerrank.com/challenges/python-mutations/problem """
+Hackerank Link: https://www.hackerrank.com/challenges/python-mutations/problem """
 
 def mutate_string(string, position, character):
     string = string[:position] + character + string[position+1:]
     return string
 
+if __name__ == "__main__":
+    s = input()
+    #input string
+    
+    i, c = input().split()
+    #input position and character
+    
+    #call a function
+    s_new = mutate_string(s, int(i), c)
+    
+    print(s_new)
