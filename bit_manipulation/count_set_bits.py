6a373fc55efe966cad8f687ad05d04a81f8cc86b
Harshit <ag01.harshit@gmail.com>
2020-10-19 20:10:14 +0530

    Fixed some tests

--- bit_manipulation/count_set_bits.py
+++ bit_manipulation/count_set_bits.py
@@ -14,19 +14,7 @@ def count_set_bits(n):
     3
     >>> count_set_bits(8)
     1
-    Traceback (most recent call last):
-    File "bin1.py", line 44, in <module>
-    count_set_bits(-2)
-    File "bin1.py", line 37, in count_set_bits
-    raise my_error
-    ValueError: The input should neither be a negative nor a fractional number
-    >>> count_set_bits(6.8)
-    Traceback (most recent call last):
-    File "bin1.py", line 44, in <module>
-    count_set_bits(6.8)
-    File "bin1.py", line 36, in count_set_bits
-    raise my_error
-    ValueError: The input should neither be a negative nor a fractional number
+
 
     """
 
