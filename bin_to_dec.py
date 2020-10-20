d1aaa25c3467d12d01184f5f23d24a07c7517346
varsha6kannan <62871024+varsha6kannan@users.noreply.github.com>
2020-10-20 13:50:29 +0530

    data structure- binary to decimal

---
+++ bin_to_dec.py
@@ -0,0 +1,16 @@
+from pythonds.basic import Stack
+
+def divideBy2(decNumber):
+    remstack = Stack()
+
+    while decNumber > 0:
+        rem = decNumber % 2
+        remstack.push(rem)
+        decNumber = decNumber // 2
+
+    binString = ""
+    while not remstack.isEmpty():
+        binString = binString + str(remstack.pop())
+
+    return binString
+
