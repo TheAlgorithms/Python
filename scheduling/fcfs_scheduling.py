f12970934e0bf3d207561c823f7a2cff8a67cbf4
Dasith U Edirisinghe <58045206+DasithEdirisinghe@users.noreply.github.com>
2020-10-21 02:10:43 +0530

    Update fcfs_scheduling.py

--- scheduling/fcfs_scheduling.py
+++ scheduling/fcfs_scheduling.py
@@ -4,24 +4,27 @@ the process which arrives first, gets executed first, or we can say
 that the process which requests the CPU first, gets the CPU allocated first.
 """
 
-def find_waiting_time(processes : list[int], n, bt, wt) -> None:
+
+def find_waiting_time(processes, n, bt, wt) -> None:
     """
     Find the waiting time
     n = number of processes
     wt = waiting time
     bt = burst time
-    """
-    """
-    waiting time for  
-    first process is 0 
+    waiting time for
+    first process is 0
     """
     wt[0] = 0
-    """
-    calculating waiting time
-    waiting time = burst time[previous process]+waiting time[previous process]
-    """
+
     for i in range(1, n):
+        """
+        calculating waiting time
+        waiting time =
+        burst time[previous process]+waiting time[previous process]
+        """
+
         wt[i] = bt[i - 1] + wt[i - 1]
+
 
 def find_turn_around_time(processes, n, bt, wt, tat) -> None:
     """
@@ -30,6 +33,7 @@ def find_turn_around_time(processes, n, bt, wt, tat) -> None:
     """
     for i in range(n):
         tat[i] = bt[i] + wt[i]
+
 
 def find_avg_time(processes, n, bt) -> None:
     """
@@ -41,21 +45,23 @@ def find_avg_time(processes, n, bt) -> None:
     total_wt = 0
     total_tat = 0
     find_waiting_time(processes, n, bt, wt)
-    find_turn_around_time(processes, n,bt, wt, tat)
-    print("Processes Burst time " +
-          " Waiting time " +
-          " Turn around time")
+    find_turn_around_time(processes, n, bt, wt, tat)
+    print("Processes Burst time " + " Waiting time " + " Turn around time")
     for i in range(n):
         total_wt = total_wt + wt[i]
         total_tat = total_tat + tat[i]
-        print(" " + str(i + 1) + "\t\t" +
-              str(bt[i]) + "\t " +
-              str(wt[i]) + "\t\t " +
-              str(tat[i]))
-    print("Average waiting time = " +
-          str(total_wt / n))
-    print("Average turn around time = " +
-          str(total_tat / n))
+        print(
+            " "
+            + str(i + 1)
+            + "\t\t"
+            + str(bt[i])
+            + "\t "
+            + str(wt[i])
+            + "\t\t "
+            + str(tat[i])
+        )
+    print("Average waiting time = " + str(total_wt / n))
+    print("Average turn around time = " + str(total_tat / n))
 
 
 if __name__ == "__main__":
