27e65ec0dbe0b9dd1346be7fd7547a69ce1c4afb
Dasith U Edirisinghe <58045206+DasithEdirisinghe@users.noreply.github.com>
2020-10-20 22:39:43 +0530

    Update fleurys_algorithm.py

--- graphs/fleurys_algorithm.py
+++ graphs/fleurys_algorithm.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 class Graph:
 
     def __init__(self, vertices):
-        self.V = vertices
+        self.v = vertices
         self.graph = defaultdict(list)
 
     def addEdge(self, u, v):
