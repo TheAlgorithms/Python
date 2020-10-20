996306915253c92a9d2fba4713f5d17d1bde8f79
Noopur <41565380+naman1303@users.noreply.github.com>
2020-10-20 23:24:17 +0530

    Update imdb_top.py
    1. Incorporated review comments - added hints and doctests in imdb_top() function.
    2. I have removed the int conversion and validation from main function, because it is happening inside imdb_top() function itself (to make it self sufficient for handling doctests).
    
    I have tested the code locally.

--- web_programming/imdb_top.py
+++ web_programming/imdb_top.py
@@ -1,39 +1,60 @@
-import helper
 import requests
 from bs4 import BeautifulSoup
+import doctest
 
-
-def validate_input(movie_count):
+def imdb_top(imdb_top_n: int) -> None:
+    r"""
+    >>> imdb_top(2)             # doctest: +NORMALIZE_WHITESPACE
+    1. The Shawshank Redemption
+    <BLANKLINE>
+    Drama
+    9.3
+    https://www.imdb.com/title/tt0111161/
+    ----------------------------------------
+    2. The Dark Knight
+    <BLANKLINE>
+    Action, Crime, Drama
+    9.0
+    https://www.imdb.com/title/tt0468569/
+    ----------------------------------------
+    >>> imdb_top('jskjksas')    # doctest: +NORMALIZE_WHITESPACE
+    Sorry! couldn't understand your choice. Let's show you the highest rated movie details.
+    1. The Shawshank Redemption
+    <BLANKLINE>
+    Drama
+    9.3
+    https://www.imdb.com/title/tt0111161/
+    ----------------------------------------
+    >>> imdb_top('')    # doctest: +NORMALIZE_WHITESPACE
+    Sorry! couldn't understand your choice. Let's show you the highest rated movie details.
+    1. The Shawshank Redemption
+    <BLANKLINE>
+    Drama
+    9.3
+    https://www.imdb.com/title/tt0111161/
+    ----------------------------------------
+    """
     try:
-        if not movie_count:
-            raise UnboundLocalError("Count must not be empty")
-        int_count = int(movie_count)
-        return int_count
-    except (UnboundLocalError, ValueError, TypeError) as e:
-        with helper.disable_exception_traceback():
-            raise (e)
+        if not imdb_top_n:
+            raise UnboundLocalError()
+        imdb_top_n = int(imdb_top_n)
+    except (ValueError, UnboundLocalError):
+        print("\nSorry! couldn't understand your choice. Let's show you the highest rated movie details.\n")
+        imdb_top_n = 1
 
-
-def imdb_top(imdb_top_n):
-    """
-    Args:
-        imdb_top_n: Movie count provided by user. Integer value.
-    Returns:
-        None
-    """
     base_url = (
         f"https://www.imdb.com/search/title?title_type="
         f"feature&sort=num_votes,desc&count={imdb_top_n}"
     )
     source = BeautifulSoup(requests.get(base_url).content, "html.parser")
     for i, m in enumerate(source.findAll("div", class_="lister-item mode-advanced")):
-        print(f"\n{i+1}. {m.h3.a.text}")  # serial no and movie's name
+        print(f"{i+1}. {m.h3.a.text}")  # serial number followed by movie name
         print(m.find("span", attrs={"class": "genre"}).text)  # genre
         print(m.strong.text)  # movie's rating
         print(f"https://www.imdb.com{m.a.get('href')}")  # movie's page link
-        print("*" * 40)
-
+        print("-" * 40)
 
 if __name__ == "__main__":
-    movie_count = input("How many movies would you like to see? ")
-    imdb_top(validate_input(movie_count))
+    doctest.testmod()
+    movie_count = input("How many movies would you like to see [default set to 1]? ").strip()
+    imdb_top(movie_count)
