472040c1a5e7329a7717d168cd11537695f06ea7
anneCoder1805 <66819522+anneCoder1805@users.noreply.github.com>
2020-10-20 12:51:37 +0530

    Update median_of_two_arrays.py

--- other/median_of_two_arrays.py
+++ other/median_of_two_arrays.py
@@ -8,11 +8,11 @@ def find_median_arrays(nums1, nums2):
         return list3[a]
     else:
         a = int(len(list3) / 2)
-        return  (list3[a] + list3[a - 1]) / 2
+    return  (list3[a] + list3[a - 1]) / 2
   
 
 if __name__ == "__main__":
     n1 = [int(x) for x in input("Enter the elements of first array: ").split()]
     n2 = [int(x) for x in input("Enter the elements of second array: ").split()]
-    m = find_media_arrays(n1, n2)
+    m = find_median_arrays(n1, n2)
     print(f'The median of two arrays is: {m}')    
