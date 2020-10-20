a205c7fcb86b584a9fe37a8ca8ccf6e4c6432d4b
Noopur <41565380+naman1303@users.noreply.github.com>
2020-10-20 19:45:59 +0530

    Create helper.py
    Created this helper file where common functions can be added, required across the files.
    Added function to suppress traceback information whenever exception is to be raised. This is right now used in imdb_top.py file.

---
+++ web_programming/helper.py
@@ -0,0 +1,12 @@
+import sys
+from contextlib import contextmanager
+
+@contextmanager
+def disable_exception_traceback():
+    """
+    All traceback information is suppressed and only the exception type and value are printed
+    """
+    default_value = getattr(sys, "tracebacklimit", 1000)  # `1000` is a Python's default value
+    sys.tracebacklimit = 0
+    yield
+    sys.tracebacklimit = default_value
