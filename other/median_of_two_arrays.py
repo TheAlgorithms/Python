c608218c1ca16490ea34618170b63db756318556
anneCoder1805 <66819522+anneCoder1805@users.noreply.github.com>
2020-10-20 13:34:30 +0530

    Update median_of_two_arrays.py

--- other/median_of_two_arrays.py
+++ other/median_of_two_arrays.py
@@ -1,18 +1,17 @@
 # This code finds the median of two arrays, even if they are not sorted initially
 def find_median_arrays(nums1, nums2):
-    list3 = nums1 + nums2
-    list3 = nums1 + nums2
-    list3.sort()
-    if divmod(len(list3), 2).[1] == 1:
-        a = int(len(list3) / 2)
-        return list3[a]
+    all_numbers = nums1 + nums2
+    all_numbers.sort()
+    div, mod = divmod(len(all_numbers), 2)
+    if mod == 1:
+        a = int(len(all_numbers) / 2)
+        return all_numbers[a]
     else:
-        a = int(len(list3) / 2)
-    return  (list3[a] + list3[a - 1]) / 2
-  
+        a = int(div)
+    return  (all_numbers[a] + all_numbers[a - 1]) / 2
+
 
 if __name__ == "__main__":
-    n1 = [int(x) for x in input("Enter the elements of first array: ").split()]
-    n2 = [int(x) for x in input("Enter the elements of second array: ").split()]
-    m = find_median_arrays(n1, n2)
-    print(f'The median of two arrays is: {m}')    
+    array_1 = [int(x) for x in input("Enter the elements of first array: ").split()]
+    array_2 = [int(x) for x in input("Enter the elements of second array: ").split()] 
+    print(f'The median of two arrays is: {find_median_arrays(array_1, array_2)}')    
