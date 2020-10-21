1464c702ed01d8fced5968965fd97e50036c4a29
Harshit-lab <ag01.harshit@gmail.com>
2020-10-21 18:35:17 +0530

    Solved the Doctest Issues

--- bit_manipulation/count_set_bits.py
+++ bit_manipulation/count_set_bits.py
@@ -1,33 +1,27 @@
 import math
-
-def count_set_bits(n: int) -> int:
-
+def count_set_bits(n):
     """
-    The function accepts a whole number stored in the variable n
-    return the number of set bits which means the number of 1's in the binary representation
-    this value is stored in the variable count initialized to 0
-    The function uses Brian Kerninghan's Algorithm to achieve the same
-    The primary expression is n=n&(n-1) which returns the leftmost set bit
-    Some examples : Binary representation of 10 = 1010
-    Sample I/O:
-    >>> count_set_bits(7)
-    3
     >>> count_set_bits(8)
     1
-
-
+    >>> count_set_bits(7)
+    3
+    >>> count_set_bits(2.5)
+    Traceback (most recent call last):
+        ...
+    ValueError: The input should be positive integer
+    >>> count_set_bits(-2)
+    Traceback (most recent call last):
+        ...
+    ValueError: The input should be positive integer
     """
-    count = 0
+    if (n<0 or n!=math.floor(n)):
+        raise ValueError("The input should be positive integer")
+    c = 0
     while (n!=0):
-        n = n&(n-1)
-        count = count + 1
-    return count
+        n=n&(n-1)
+        c=c+1
+    return c
 
-
-
-if __name__ == "__main__" :
+if __name__ == "main":
     import doctest
-
     doctest.testmod()
-
-
