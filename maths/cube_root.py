778a49d5c276ed4c9b5c64eda563f717569e35af
rajgupta81 <bharitigupta123@gmail.com>
2020-10-21 15:12:47 +0530

    added cube_root.py

---
+++ maths/cube_root.py
@@ -0,0 +1,10 @@
+
+def cube_root(a: float) ->float:
+    """
+    it just give cube root
+    """
+    return a ** ( 1 / 3 )
+
+if __name__ == "__main__":
+    """just for testing"""
+    print(cube_root(27))
