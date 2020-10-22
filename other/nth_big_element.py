f0410b1abd605031208cf1a04f652b914f39afc1
Snimerjot Singh <snimersingh1729@gmail.com>
2020-10-22 13:53:23 +0530

    Added nth_big_element.py
    Finds the nth largest element in a list.

---
+++ other/nth_big_element.py
@@ -0,0 +1,17 @@
+def find_nth_largest(nums: list, k: int) -> int:
+    """
+    Finds the nth largest element in a list.
+    >>> find_nth_largest([3, 5, 2, 4, 6, 8], 2)
+    6
+    >>> find_nth_largest([70, 140, 99, 16, 17, 56], 5)
+    17
+    """
+    nums = list(dict.fromkeys(nums))
+    nums.sort(reverse = True)
+    return nums[k-1]
+
+
+if __name__ == "__main__":
+    from doctest import testmod
+
+    testmod()
