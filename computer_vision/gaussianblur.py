b681a58fa9bf0700a0b6be15824c61ceb5c80dda
SanaaShah <sanashah.ss13@gmail.com>
2020-10-22 21:24:16 +0500

    added gaussian blur to computer vision

--- computer_vision/gaussianblur.py
+++ computer_vision/gaussianblur.py
@@ -1,25 +1,28 @@
 """
 Gaussian Blur applied for blurring an image
 More information on:
-https://en.wikipedia.org/wiki/Gaussian_blur"""
+https://en.wikipedia.org/wiki/Gaussian_blur
+"""
 
 import numpy as np
 import cv2
 
-def gaussianblur(size, img, sigma = 1):
+
+def gaussian_blur(size, img, sigma=1):
 
     size = int(size) // 2
-    X, Y = np.mgrid[-size:size + 1, -size:size + 1]
+    x1, y1 = np.mgrid[-size: size + 1, -size: size + 1]
     constant = 1 / (2.0 * np.pi * sigma ** 2)
-    guassian = np.exp(-((X ** 2 + Y ** 2) / (2.0 * sigma ** 2))) * constant
-    blurred = cv2.filter2D(gray, -1, guassian)
+    gaussian = np.exp(-((x1 ** 2 + y1 ** 2) / (2.0 * sigma ** 2))) * constant
+    blurred = cv2.filter2D(img, -1, gaussian)
     return blurred
+
 
 if __name__ == "__main__":
 
-    img = cv2.imread(r'path_to_image')
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blurred = gaussianblur(4, gray, 1.3)
-    cv2.imshow('Blurred Image', blurred)
+    image = cv2.imread(r"path_to_image")
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred_image = gaussian_blur(4, gray_image, 1.3)
+    cv2.imshow("Blurred Image", blurred_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
