fcc162caf0b3cdb517f065f988ed7837a36074f2
GantaChaitra <gantachaitra@gmail.com>
2020-10-22 16:15:53 +0530

    HacktoberFest: Added a solution for Project Euler problem_024 after resolving pre-commit check

--- project_euler/problem_024/sol2.py
+++ project_euler/problem_024/sol2.py
@@ -4,13 +4,16 @@
 What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 """
 from itertools import permutations
+
+
 def solution():
     """ 
     >>> solution()
     '2783915460'
     """
-    lexi_perm = ''.join(list(permutations('0123456789'))[999999])
+    lexi_perm = "".join(list(permutations("0123456789"))[999999])
     return lexi_perm
+
 
 if __name__ == "__main__":
     print(solution())
