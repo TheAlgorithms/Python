28c9ca67f1bd254394bf8c793378b34732dff0f6
GantaChaitra <gantachaitra@gmail.com>
2020-10-24 01:00:10 +0530

    HacktoberFest: Added a solution for Project Euler problem_024

--- project_euler/problem_024/sol2.py
+++ project_euler/problem_024/sol2.py
@@ -1,4 +1,5 @@
-"""A permutation is an ordered arrangement of objects.
+"""
+A permutation is an ordered arrangement of objects.
 For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4.
 If all of the permutations are listed numerically or alphabetically,
 we call it lexicographic order.
@@ -8,10 +9,8 @@ The lexicographic permutations of 0, 1 and 2 are:
 
 What is the millionth lexicographic permutation of the digits
 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
-
 """
 from itertools import permutations
-
 
 def solution():
     """
