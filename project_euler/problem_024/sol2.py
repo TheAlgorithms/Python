23b5fb4bb7e72db555d088c26c441725f9f64dda
GantaChaitra <gantachaitra@gmail.com>
2020-10-23 22:42:16 +0530

    HacktoberFest: Added a solution for Project Euler problem_024

--- project_euler/problem_024/sol2.py
+++ project_euler/problem_024/sol2.py
@@ -1,7 +1,7 @@
 """A permutation is an ordered arrangement of objects.
 For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4.
-If all of the permutations are listed numerically or alphabetically
-, we call it lexicographic order.
+If all of the permutations are listed numerically or alphabetically,
+we call it lexicographic order.
 The lexicographic permutations of 0, 1 and 2 are:
 
 012   021   102   120   201   210
@@ -25,18 +25,4 @@ def solution():
 if __name__ == "__main__":
     print(solution())
 
-"""
-Output: $ python3 -m doctest -v sol2.py
-    Trying:
-    solution()
-Expecting:
-    '2783915460'
-ok
-1 items had no tests:
-    sol2
-1 items passed all tests:
-   1 tests in sol2.solution
-1 tests in 2 items.
-1 passed and 0 failed.
-Test passed.
-"""
+
