e7ffd0c0533d032457c90115c3f4ecc4df5448fe
willtryagain <41161981+willtryagain@users.noreply.github.com>
2020-10-23 16:00:45 +0530

    Add files via upload

--- project_euler/problem_050/sol1.py
+++ project_euler/problem_050/sol1.py
@@ -7,9 +7,11 @@ The prime 41, can be written as the sum of six consecutive primes:
 
 This is the longest sum of consecutive primes that adds to a prime below one-hundred.
 
-The longest sum of consecutive primes below one-thousand that adds to a prime, contains 21 terms, and is equal to 953.
+The longest sum of consecutive primes below one-thousand that adds to a
+ prime, contains 21 terms, and is equal to 953.
 
-Which prime, below one-million, can be written as the sum of the most consecutive primes?
+Which prime, below one-million, can be written as the sum of the 
+most consecutive primes?
 """
 
 
