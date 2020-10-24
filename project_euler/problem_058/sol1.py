f9b972fd48dbf41ccc754d6388a683d40b74404d
katepap <katerinapapoudarichatziavgeri@gmail.com>
2020-10-24 13:47:58 +0300

    Added problem_058 to project euler

--- project_euler/problem_058/sol1.py
+++ project_euler/problem_058/sol1.py
@@ -12,6 +12,8 @@
     It is interesting to note that the odd squares lie along the bottom right diagonal, but what is more interesting is that 8 out of the 13 numbers lying along both diagonals are prime; that is, a ratio of 8/13 ≈ 62%.
 
     If one complete new layer is wrapped around the spiral above, a square spiral with side length 9 will be formed. If this process is continued, what is the side length of the square spiral for which the ratio of primes along both diagonals first falls below 10%?
+
+    problem link: https://projecteuler.net/problem=58
 """
 def isPrime(n) -> bool:
     if n==2 or n==3: return True
