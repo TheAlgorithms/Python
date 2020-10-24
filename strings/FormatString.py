51889ed652c0b2d8af5ba17ce6d5b285cd697255
Tejus W <73341091+TejusW@users.noreply.github.com>
2020-10-24 22:11:52 +0530

    Adding example to format string

---
+++ strings/FormatString.py
@@ -0,0 +1,5 @@
+print("Harry is awesome".capitalize())
+print("HARRY IS KING".lower())
+print("harry is hot".upper())
+print("Harry potter and the half-blood prince".title())
+print("AVENGER:the war of infinity".swapcase())
