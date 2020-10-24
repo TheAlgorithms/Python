39b346e237d5f93e02e808cd778810641061cebb
Cho Yin Yong <choyiny@users.noreply.github.com>
2020-10-24 01:16:01 -0400

    fix formatting issues

--- divide_and_conquer/max_difference_pair.py
+++ divide_and_conquer/max_difference_pair.py
@@ -6,10 +6,10 @@ def max_difference(a: List[int]) -> (int, int):
     We are given an array A[1..n] of integers, n >= 1. We want to
     find a pair of indices (i, j) such that
     1 <= i <= j <= n and A[j] - A[i] is as large as possible.
-    
+
     Explanation:
     https://www.geeksforgeeks.org/maximum-difference-between-two-elements/
-    
+
     >>> max_difference([5, 11, 2, 1, 7, 9, 0, 7])
     (1, 9)
     """
@@ -18,8 +18,8 @@ def max_difference(a: List[int]) -> (int, int):
         return a[0], a[0]
     else:
         # split A into half.
-        first = a[:len(a)//2]
-        second = a[len(a)//2:]
+        first = a[: len(a)//2]
+        second = a[len(a)//2 :]
 
         # 2 sub problems, 1/2 of original size.
         small1, big1 = max_difference(first)
@@ -33,10 +33,7 @@ def max_difference(a: List[int]) -> (int, int):
         # 3 cases, either (small1, big1),
         # (min_first, max_second), (small2, big2)
         # constant comparisons
-        if (
-            big2 - small2 > max_second - min_first
-            and big2 - small2 > big1 - small1
-        ):
+        if big2 - small2 > max_second - min_first and big2 - small2 > big1 - small1:
             return small2, big2
         elif big1 - small1 > max_second - min_first:
             return small1, big1
