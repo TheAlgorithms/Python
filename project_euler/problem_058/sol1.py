ce1fe8eb3676ae0ff5310eeb029bfc42b5ac022a
katepap <katerinapapoudarichatziavgeri@gmail.com>
2020-10-24 16:29:25 +0300

    Removed whitespaces trailing

--- project_euler/problem_058/sol1.py
+++ project_euler/problem_058/sol1.py
@@ -1,5 +1,5 @@
 """
-    Starting with 1 and spiralling anticlockwise in the following way, 
+    Starting with 1 and spiralling anticlockwise in the following way,
     a square spiral with side length 7 is formed.
 
     37 36 35 34 33 32 31
@@ -10,13 +10,13 @@
     42 21 22 23 24 25 26
     43 44 45 46 47 48 49
 
-    It is interesting to note that the odd squares lie along 
-    the bottom right diagonal, but what is more interesting is 
-    that 8 out of the 13 numbers lying along both 
+    It is interesting to note that the odd squares lie along
+    the bottom right diagonal, but what is more interesting is
+    that 8 out of the 13 numbers lying along both
     diagonals are prime; that is, a ratio of 8/13 ≈ 62%.
 
-    If one complete new layer is wrapped around the spiral 
-    above, a square spiral with side length 9 will be formed. 
+    If one complete new layer is wrapped around the spiral
+    above, a square spiral with side length 9 will be formed.
     If this process is continued, what is the side length of the square spiral for 
     which the ratio of primes along both diagonals first falls below 10%?
 
@@ -37,8 +37,8 @@ def isPrime(n) -> bool:
 
 
 """
-    Instead of calculation all numbers we are only calculating the sides of the square. 
-    we find these numbers by adding to the last digit we had, 
+    Instead of calculation all numbers we are only calculating the sides of the square.
+    we find these numbers by adding to the last digit we had,
     for example 9 the current side length
     minus 1. For example:
 
