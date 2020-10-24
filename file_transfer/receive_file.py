8c106ceff614de1c5ff38f2dbdb0daf2b979e301
vikram singh <58397405+Sparta-9@users.noreply.github.com>
2020-10-24 23:28:13 +0530

    Update receive_file.py
    there was some indentation error

--- file_transfer/receive_file.py
+++ file_transfer/receive_file.py
@@ -1,13 +1,10 @@
 if __name__ == "__main__":
     import socket  # Import socket module
-
     sock = socket.socket()  # Create a socket object
     host = socket.gethostname()  # Get local machine name
     port = 12312
-
     sock.connect((host, port))
     sock.send(b"Hello server!")
-
     with open("Received_file", "wb") as out_file:
         print("File opened")
         print("Receiving data...")
@@ -17,7 +14,6 @@ if __name__ == "__main__":
             if not data:
                 break
             out_file.write(data)  # Write data to a file
-
     print("Successfully got the file")
     sock.close()
     print("Connection closed")
