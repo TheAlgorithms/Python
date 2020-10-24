663aeaa06624d0ac53d18de63e6a82180316d822
Jake Gerber <jake.j.gerber@gmail.com>
2020-10-24 01:09:11 -0700

    Update decimal_isolate.py

--- maths/decimal_isolate.py
+++ maths/decimal_isolate.py
@@ -17,7 +17,7 @@ def decimal_isolate(number, digitAmount):
     return round(number - int(number), digitAmount)
   return number - int(number)
 
-  if __name__ == "__main__":
+if __name__ == "__main__":
     print(decimal_isolate(35.345, 1))
     print(decimal_isolate(35.345, 2))
     print(decimal_isolate(35.345, 3))
