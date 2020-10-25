95f77023f2b23e7ceadc05837a74406faa520b2e
pardaz-banu <45086957+pardaz-banu@users.noreply.github.com>
2020-10-25 22:04:40 +0530

    Update binary_to_decimal_recursion.py

--- conversions/binary_to_decimal_recursion.py
+++ conversions/binary_to_decimal_recursion.py
@@ -1,4 +1,4 @@
-def binaryToDecimal(b, i = 0):
+def binaryToDecimal(b, i):
     n = len(b)
     if i == n - 1:
         return int(b[i]) - 0
@@ -6,4 +6,5 @@ def binaryToDecimal(b, i = 0):
     return (((int(b[i]) - 0) << (n - i - 1)) + binaryToDecimal(b, i + 1))
 
 binary_string = input()
-print(binaryToDecimal(binary_string))
+i = 0
+print(binaryToDecimal(binary_string, i))
