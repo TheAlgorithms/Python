12ece8a8e236409558f40314c60304195c8239fb
Abhishek Jaisingh <abhi2254015@gmail.com>
2020-10-26 03:29:49 +0530

    Address Further Review Comments

--- project_euler/problem_050/sol1.py
+++ project_euler/problem_050/sol1.py
@@ -11,8 +11,10 @@ The longest sum of consecutive primes below one-thousand that adds to a prime, c
 Which prime, below one-million, can be written as the sum of the most consecutive primes?
 """
 
+from typing import List, Set
 
-def get_primes(limit: int) -> (set, list):
+
+def get_primes(limit: int) -> (Set[int], List[int]):
     """ 
     Returns the set & list(ordered) of primes up to the below limit
     using Sieve of Eratosthenes 
@@ -20,25 +22,24 @@ def get_primes(limit: int) -> (set, list):
     >> get_primes(10)
     ({2, 3, 5, 7}, [2, 3, 5, 7])
     """
-    SV_MAX = limit
-    sv = [True for i in range(SV_MAX)]
+    sieve = [True for i in range(limit)]
     prime_set = set()
     primes = []
 
     # 1 is not prime
-    sv[0] = False
+    sieve[0] = False
 
-    for i in range(2, SV_MAX+1):
+    for i in range(2, limit+1):
         # if current number is prime
-        if sv[i-1]:
+        if sieve[i-1]:
             primes.append(i)
             prime_set.add(i)
-            for j in range(2*i, SV_MAX+1, i):
-                sv[j-1] = False
+            for j in range(2*i, limit+1, i):
+                sieve[j-1] = False
     return prime_set, primes
 
 
-def solution(limit: int =1000000) -> int:
+def solution(limit: int =1000) -> int:
     """
     First, generate a list of all primes upto limit
     Then, try all sets of consecutive primes to see if they add up to 
@@ -53,19 +54,19 @@ def solution(limit: int =1000000) -> int:
     prime_set, primes = get_primes(limit)
     prefix = []
 
-    curr = 0
-    for p in primes:
-        curr += p
-        prefix.append(curr)
+    curr_prefix_sum = 0
+    for prime in primes:
+        curr_prefix_sum += prime
+        prefix.append(curr_prefix_sum)
 
-    l = len(prefix)
+    len_prefix = len(prefix)
 
-    max_prime, max_l = -1, -1
+    max_prime, max_len = -1, -1
 
-    for i in range(l-1):
+    for i in range(len_prefix-1):
         # check if we can generate a solution with i terms
         # decrease i successively if we don't find a solution
-        for j in range(i+1, l):
+        for j in range(i+1, len_prefix):
             # iterate over each sequence of i primes starting at
             # jth index
             p = prefix[j]
@@ -75,14 +76,10 @@ def solution(limit: int =1000000) -> int:
                 break
             if p in prime_set:
                 curr_l = j-i+1
-                if curr_l > max_l:
+                if curr_l > max_len:
                     max_prime = p
-                    max_l = curr_l
+                    max_len = curr_l
     return max_prime
-
-
-# if __name__ == "__main__":
-#     print(solution())
 
 
 if __name__ == "__main__":
