d0d12852d44856e16dfa5d92a613961bc4853f10
Ricardo Bortolucci <Ricardo@localhost.localdomain>
2020-10-25 20:14:31 -0300

    Ignore text after '#'

--- strings/reading_commented_text.py
+++ strings/reading_commented_text.py
@@ -1,15 +1,30 @@
 # coding=utf-8
+"""
+This program read the part of text before comment char ('#') with selective_reading()
 
-def selective_reading():
-    archive = "insert the path here"
-    with open(archive, 'r') as arch:
-        for line in arch:
-            line = line.partition('#')[0] # split the line into elements before "#" and after "#" 
-            line = line.rstrip() # remove the after "#"
-            print(line)
+>>> selective_reading("hello #world")
+'hello'
+"""
+def selective_reading(line):
+    """
+    will capture only before #
 
-def main():
-    selective_reading()
+    >>> selective_reading("Hello #world")
+    'Hello'
+    >>> selective_reading("Hell#o world")
+    'Hell'
+    >>> selective_reading("#Hello world")
+    ''
+    >>> selective_reading("Hello world")
+    'Hello world'
+    >>> selective_reading("Hello world #")
+    'Hello world'
+    """
 
+    line = line.partition('#')[0] # split the line into elements before "#" and after "#" 
+    line = line.rstrip() # remove the after "#"
+    return line
+        
 if __name__ == "__main__":
-    main()
+    import doctest
+    doctest.testmod()
