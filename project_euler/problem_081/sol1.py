ef3a87da4e6cc31ecf1e288360876197140224ab
Stefan Radu <stefan270101@gmail.com>
2020-10-25 03:28:46 +0200

    removed local input file
    * use a get request to get it from the website

--- project_euler/problem_081/sol1.py
+++ project_euler/problem_081/sol1.py
@@ -46,6 +46,9 @@ TIME 0.22s
 
 """
 
+import os
+import requests
+
 
 def solve_matrix(lines: int, columns: int, matrix: list) -> int:
     """
@@ -89,13 +92,22 @@ def solution() -> int:
     427337
     """
 
-    matrix = []
+    input_url = "https://projecteuler.net/project/resources/p081_matrix.txt"
+    response = requests.get(input_url)
 
+    # download and write input file
+    with open("./p081_matrix.txt", "wb") as file:
+        file.write(response.content)
+
+    matrix = []
     with open("./p081_matrix.txt", "r") as file:
         matrix = file.readlines()
         for i, line in enumerate(matrix):
             line[i].strip("\n")
             matrix[i] = [int(x) for x in line.split(",")]
+
+    # remove file as it's no longer needed
+    os.remove("./p081_matrix.txt")
 
     return solve_matrix(len(matrix), len(matrix[-1]), matrix)
 
