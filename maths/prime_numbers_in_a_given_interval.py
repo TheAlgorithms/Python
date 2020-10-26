644301ad0d922987344f78940bb8a8ef0265dbc4
Shashank KR <shashucr567@gmail.com>
2020-10-26 20:04:11 +0530

    New file prime_numbers_in_a_given_interval.py has been

---
+++ maths/prime_numbers_in_a_given_interval.py
@@ -0,0 +1,11 @@
+#Take the input from the user:   
+lower = int(input("Enter lower range: "))  
+upper = int(input("Enter upper range: "))  
+  
+for num in range(lower,upper + 1):  
+   if num > 1:  
+       for i in range(2,num):  
+           if (num % i) == 0:  
+               break  
+       else:  
+           print(num) 
