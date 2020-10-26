fde601bb3da6970b686ab6f4cf3ed5bd54e68581
Gonzalo Gamez <takosaga@gmail.com>
2020-10-26 14:32:52 -0500

    black formated

--- project_euler/problem_179/sol1.py
+++ project_euler/problem_179/sol1.py
@@ -12,7 +12,8 @@ number for every number between 1 and 10,000,000
 
 import math
 
-def countDivisors(n) -> int :
+
+def countDivisors(n) -> int:
 
     """
     Counting divisors from:
@@ -25,14 +26,14 @@ def countDivisors(n) -> int :
     """
 
     cnt = 0
-    for i in range(1, (int)(math.sqrt(n)) + 1) :
-        if (n % i == 0) :
+    for i in range(1, (int)(math.sqrt(n)) + 1):
+        if n % i == 0:
 
             # If divisors are equal,
             # count only one
-            if (n / i == i) :
+            if n / i == i:
                 cnt = cnt + 1
-            else : # Otherwise count both
+            else:  # Otherwise count both
                 cnt = cnt + 2
 
     return cnt
@@ -47,13 +48,14 @@ def solution() -> int:
     consecutive_positive_divisors = 0
     temp_divisors = 0
 
-    for number in range(2,10_000_000):
+    for number in range(2, 10_000_000):
         divisors = countDivisors(number)
         if divisors == temp_divisors:
             consecutive_positive_divisors += 1
 
         temp_divisors = divisors
     return consecutive_positive_divisors
+
 
 if __name__ == "__main__":
     print(f"{solution() = }")
