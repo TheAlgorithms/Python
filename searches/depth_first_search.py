d45bd9cb16ca85b41bcf6532bada85940018f598
Nathan <nathan@Nathans-MacBook-Pro.local>
2020-10-27 12:19:58 -0400

    Add Depth First Search Algorithm

--- searches/depth_first_search.py
+++ searches/depth_first_search.py
@@ -1,11 +1,13 @@
+# The depth first search is a tree searching algorithm that outputs the final
+# tree from a graph that is inputed. More explanation can be found here:
+# https://en.wikipedia.org/wiki/Depth-first_search#:~:text=Depth%2Dfirst%20search%20(DFS),along%20each%20branch%20before%20backtracking.
+
 def dfs(visited: list, graph: dict, start: str) -> list:
     """
     Returns a list of the visited nodes using a depth first search
     from inputed graph in alphabetical order.
     
-    >>> graph = {'A' : ['B', 'C', 'D', 'G'], 'B' : ['A', 'C', 'F', 'G'], 
-    'C' : ['A', 'B', 'D', 'E', 'F', 'G'], 'D' : ['A', 'C', 'E', 'F'], 
-    'E' : ['C', 'D', 'F'], 'F' : ['B', 'C', 'D', 'E'], 'G' : ['A', 'B', 'C']}
+    >>> graph = {'A' : ['B', 'C', 'D', 'G'], 'B' : ['A', 'C', 'F', 'G'], 'C' : ['A', 'B', 'D', 'E', 'F', 'G'], 'D' : ['A', 'C', 'E', 'F'], 'E' : ['C', 'D', 'F'], 'F' : ['B', 'C', 'D', 'E'], 'G' : ['A', 'B', 'C']}
 
     dfs(graph, 'A')
 
@@ -35,5 +37,9 @@ graph2 = {'0' : ['1'], '1' : ['0', '3'], '2' : ['3', '4'], '3' : ['1', '2', '4']
 
 print(dfs(lst1, graph1, 'E'))
 print(dfs(lst2, graph2, '2'))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
 
 # Can be used with any type of graph, but it must be a string set!!
