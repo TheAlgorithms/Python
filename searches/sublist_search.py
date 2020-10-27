f215ccf9a0a21c901f6696517a7b996a5cbe84e2
Pratham-123 <54859695+Pratham-123@users.noreply.github.com>
2020-10-27 11:12:24 +0530

    Added sublist search

---
+++ searches/sublist_search.py
@@ -0,0 +1,26 @@
+def is_Sublist(l, s):
+	sub_set = False
+	if s == []:
+		sub_set = True
+	elif s == l:
+		sub_set = True
+	elif len(s) > len(l):
+		sub_set = False
+
+	else:
+		for i in range(len(l)):
+			if l[i] == s[0]:
+				n = 1
+				while (n < len(s)) and (l[i+n] == s[n]):
+					n += 1
+				
+				if n == len(s):
+					sub_set = True
+
+	return sub_set
+
+a = [2,4,3,5,7]
+b = [4,3]
+c = [3,7]
+print(is_Sublist(a, b))
+print(is_Sublist(a, c))
