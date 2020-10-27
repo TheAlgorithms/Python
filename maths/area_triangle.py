3e18f55736bd16f56cfb762e1471a3872bd72973
kasa10 <kirill.kas@mail.ru>
2020-10-27 08:52:17 +0300

    Update area_triangle.py
    V2.0 Area of a triangle

--- maths/area_triangle.py
+++ maths/area_triangle.py
@@ -1,10 +1,9 @@
 #S= √ (p * (p - a)*(p - b)*(p - c))
 #p=(a+b+c)/2;
 #a,b,c - sides triangle
-
+#calculates the area of a triangle on three sides. a,b,c - sides triangle
 
 def treug(a,b,c):
-	"""calculates the area of a triangle on three sides. a,b,c - sides triangle"""
-    p=(a+b+c)/2
-    S=(p * (p - a)*(p - b)*(p - c))**(1/2)
-    return(S)
+	p=(a+b+c)/2
+	S=(p * (p - a)*(p - b)*(p - c))**(1/2)
+	return(S)
