4288f835a2f9e562f02675ea7b8bdb3288affacf
Nathan <nathan@Nathans-MacBook-Pro.local>
2020-10-27 15:06:29 -0400

    Add Depth First Search Algorithm

--- searches/depth_first_search.py
+++ searches/depth_first_search.py
@@ -2,20 +2,22 @@
 # tree from a graph that is inputed. More explanation can be found here:
 # https://en.wikipedia.org/wiki/Depth-first_search#:~:text=Depth%2Dfirst%20search%20(DFS),along%20each%20branch%20before%20backtracking.
 
-def dfs(visited: list, graph: dict, start: str) -> list:
+def depth_first_search(visited: list, graph: dict, start: str) -> list:
     """
     Returns a list of the visited nodes using a depth first search
     from inputed graph in alphabetical order.
     
-    >>> graph = {'A' : ['B', 'C', 'D', 'G'], 'B' : ['A', 'C', 'F', 'G'], 'C' : ['A', 'B', 'D', 'E', 'F', 'G'], 'D' : ['A', 'C', 'E', 'F'], 'E' : ['C', 'D', 'F'], 'F' : ['B', 'C', 'D', 'E'], 'G' : ['A', 'B', 'C']}
+    >>> lst = []
+    graph = {'A' : ['B', 'C', 'D', 'G'], 'B' : ['A', 'C', 'F', 'G'], 'C' : ['A', 'B', 'D', 'E', 'F', 'G'], 'D' : ['A', 'C', 'E', 'F'], 'E' : ['C', 'D', 'F'], 'F' : ['B', 'C', 'D', 'E'], 'G' : ['A', 'B', 'C']}
 
-    dfs(graph, 'A')
+    depth_first_search(lst, graph, 'A')
 
     ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 
-    >>> graph = {'A' : ['B'], 'B' : ['D'], 'C' : ['D', 'E'], 'D' : ['B'], 'E' : ['C', 'D']}
+    >>> lst = []
+    graph = {'A' : ['B'], 'B' : ['D'], 'C' : ['D', 'E'], 'D' : ['B'], 'E' : ['C', 'D']}
 
-    dfs(graph, 'D')
+    depth_first_search(lst, graph, 'D')
 
     ['D', 'B', 'A', 'C', 'E']
     """
@@ -23,8 +25,12 @@ def dfs(visited: list, graph: dict, start: str) -> list:
     if start not in visited:
         visited.append(start)
         for neighbour in graph[start]:
-            dfs(visited, graph, neighbour)
+            depth_first_search(visited, graph, neighbour)
     
     return visited
     
 lst = []
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
