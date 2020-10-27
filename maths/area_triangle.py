639d1b0331137cb816026d43ba669637fe58f202
kasa10 <kirill.kas@mail.ru>
2020-10-27 20:18:19 +0300

    Update area_triangle.py
    v3.0

--- maths/area_triangle.py
+++ maths/area_triangle.py
@@ -1,9 +1,6 @@
-#S=√(p*(p-a)*(p-b)*(p-c))
-#p=(a+b+c)/2
-#a,b,c-sidestriangle
-#calculatestheareaofatriangleonthreesides.a,b,c-sidestriangle
-def treug(a,b,c):
-	p=(a+b+c)/2
-	S=(p*(p-a)*(p-b)*(p-c))**(1/2)
-	return(S)
-print(treug(5,5,5))
+def treug(a, b, c):
+	p = (a + b + c) / 2 #Semi-Perimeter
+	S = (p * (p - a) * (p - b) * (p - c)) ** (1 / 2) #Area by Heron's Formula
+	return S
+
+print(treug(5, 5, 5))
