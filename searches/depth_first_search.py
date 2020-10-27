1cb91b9d164bbda439bd3a9346bd9775a2299b8b
Nathan <nathan@Nathans-MacBook-Pro.local>
2020-10-27 15:49:28 -0400

    Add Depth First Search

--- searches/depth_first_search.py
+++ searches/depth_first_search.py
@@ -2,6 +2,7 @@
 # tree from a graph that is inputed. More explanation can be found here:
 # https://en.wikipedia.org/wiki/Depth-first_search#:~:text=Depth%2Dfirst%20search%20(DFS),along%20each%20branch%20before%20backtracking.
 
+
 def depth_first_search(visited: list, graph: dict, start: str) -> list:
     """
     Returns a list of the visited nodes using a depth first search
@@ -9,14 +10,23 @@ def depth_first_search(visited: list, graph: dict, start: str) -> list:
     """
 
     if start not in visited:
-        visited.append(start)
+        visited.append(start)  # Puts the first visited letter in the sequence
         for neighbour in graph[start]:
-            depth_first_search(visited, graph, neighbour)
-    
+            depth_first_search(
+                visited, graph, neighbour
+            )  # Uses recursion to check the if statement again. If the if statement is not met, it returns the existing list.
+
     return visited
-    
+
+
 lst = []
 
-graph = {'A': ['B'], 'B': ['A', 'D'], 'C': ['D', 'E'], 'D': ['B', 'C', 'E'], 'E': ['C', 'D']}
+graph = {
+    "A": ["B"],
+    "B": ["A", "D"],
+    "C": ["D", "E"],
+    "D": ["B", "C", "E"],
+    "E": ["C", "D"],
+}
 
-print(depth_first_search(lst, graph, 'D'))
+print(depth_first_search(lst, graph, "D"))
