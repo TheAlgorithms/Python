f53a4389a79f3b8513997661f44ac30bcc6c94f8
kasa10 <kirill.kas@mail.ru>
2020-10-27 09:07:03 +0300

    Update area_triangle.py
    delete white space

--- maths/area_triangle.py
+++ maths/area_triangle.py
@@ -1,12 +1,10 @@
-#S= √ (p * (p - a)*(p - b)*(p - c))
-#p=(a+b+c)/2;
-#a,b,c - sides triangle
-#calculates the area of a triangle on three sides. a,b,c - sides triangle
+#S=√(p*(p-a)*(p-b)*(p-c))
+#p=(a+b+c)/2
+#a,b,c-sidestriangle
+#calculatestheareaofatriangleonthreesides.a,b,c-sidestriangle
 
 def treug(a,b,c):
 	p=(a+b+c)/2
-	S=(p * (p - a)*(p - b)*(p - c))**(1/2)
+	S=(p*(p-a)*(p-b)*(p-c))**(1/2)
 	return(S)
-	
 
-treug(5,5,5)
