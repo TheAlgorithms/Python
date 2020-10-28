cf4d377fb57df4a167a04c8ba7111c6464976956
tboonma <tboonma@hotmail.com>
2020-10-28 15:15:14 +0700

    Add doctest

--- strings/word_count.py
+++ strings/word_count.py
@@ -1,10 +1,27 @@
 def word_count(txt):
+    """
+    >>> word_count("Hello")
+    1
+    >>> word_count("Hello, World")
+    2
+    """
     x = txt.split()
     return len(x)
 
 def char_count(txt):
+    """
+    >>> char_count("Hello")
+    5
+    >>> char_count("Hello, World")
+    12
+    """
     return len(txt)
+
 
 txt = input("Input your text to count all word\n>>> ")
 
 print(word_count(txt), "word(s) ", char_count(txt), "character(s)")
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
