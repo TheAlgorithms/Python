030a8f766be3c2baf9b8becb7684d06d8d298507
Du Yuanchao <shellhub.me@gmail.com>
2020-10-29 16:44:12 +0800

    Update data_structures/stacks/balanced_parentheses.py
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- data_structures/stacks/balanced_parentheses.py
+++ data_structures/stacks/balanced_parentheses.py
@@ -13,7 +13,7 @@ def balanced_parentheses(parentheses: str) -> bool:
     stack = Stack()
     bracket_pairs = {"(": ")", "[": "]", "{": "}"}
     for bracket in parentheses:
-        if bracket in ("(", "[", "{"):
+        if bracket in bracket_pairs:
             stack.push(bracket)
         elif bracket in (")", "]", "}"):
             if stack.is_empty() or bracket_pairs[stack.pop()] != bracket:
