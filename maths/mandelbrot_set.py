214d7e7613bc1080835b3d907503a0ef11b00d8f
Jr Miranda <sjrmiranda@gmail.com>
2020-10-30 04:15:23 -0300

    Adding Mandelbrot Set

---
+++ maths/mandelbrot_set.py
@@ -0,0 +1,40 @@
+from PIL import Image, ImageDraw
+
+# Define constants
+WIDTH = 400
+HEIGHT = 300
+ITER = 80
+RE_RANGE = (-2, 1)
+IM_RANGE = (-1, 1)
+
+# Create new image
+im = Image.new('RGB', (WIDTH, HEIGHT), (0, 0, 0))
+draw = ImageDraw.Draw(im)
+
+
+def calc_mandelbrot(z):
+  # Check if it converges
+  z = 0
+  n = 0
+  
+  while abs(z) <= 2 and n < ITER:
+    z = z*z + c
+    n += 1
+  return n
+
+
+# Paint each pixel
+for x in range(WIDTH):
+  for y in range(HEIGHT):
+    real = RE_RANGE[0] + (x/WIDTH) * (RE_RANGE[1] - RE_RANGE[0])
+    imag = IM_RANGE[0] + (y/HEIGHT)*(IM_RANGE[1]-IM_RANGE[0])
+    c = complex(real, imag)
+    
+    mandelbrot = calc_mandelbrot(c)
+    color = 255 - int(mandelbrot*255/ITER)
+    
+    draw.point((x, y), (color, color, color))
+
+
+# Save image
+im.save('images/mandelbrot.png')
