e16e0ecb155e7bf4848c871652c9b2f5251ba182
Sherri <sherrichuah4@gmail.com>
2020-10-30 18:36:31 +0000

    Integer to roman conversion

--- conversions/integer_to_roman.py
+++ conversions/integer_to_roman.py
@@ -6,6 +6,7 @@ def int_to_roman(number: int) -> str:
     'MMMCDLVI'
     >>> int_to_roman(3999)
     'MMMCMXCIX'
+    
     """
     vals = {"M": 1000, "CM": 900, "D": 500, "CD": 400, "C": 100, "XC": 90, "L": 50, "XL":40, "X": 10, "IX": 9, "V": 5, "IV":4, "I": 1}
     roman_num = ""
