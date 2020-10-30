0289322796507327ef1edec2b201db3624790e8f
mohamed-m-sabir <mohamed.m.sabir@gmail.com>
2020-10-30 19:13:46 +0200

    modified meanthreshold.py script

--- computer_vision/meanthreshold.py
+++ computer_vision/meanthreshold.py
@@ -1,30 +1,26 @@
-from PIL import Image
-
-"""
-Mean thresholding algorithm for image processing
-https://en.wikipedia.org/wiki/Thresholding_(image_processing)
-"""
-
-
-def mean_threshold(image: Image) -> Image:
-    """
-    image: is a grayscale PIL image object
-    """
-    height, width = image.size
-    mean = 0
-    pixels = image.load()
-    for i in range(width):
-        for j in range(height):
-            pixel = pixels[j, i]
-            mean += pixel
-    mean //= width * height
-
-    for j in range(width):
-        for i in range(height):
-            pixels[i, j] = 255 if pixels[i, j] > mean else 0
-    return image
-
-
-if __name__ == "__main__":
-    image = mean_threshold(Image.open("path_to_image").convert("L"))
-    image.save("output_image_path")
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+img = cv.imread('noisy2.png',0)
+# global thresholding
+ret1,th1 = cv.threshold(img,127,255,cv.THRESH_BINARY)
+# Otsu's thresholding
+ret2,th2 = cv.threshold(img,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+# Otsu's thresholding after Gaussian filtering
+blur = cv.GaussianBlur(img,(5,5),0)
+ret3,th3 = cv.threshold(blur,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+# plot all the images and their histograms
+images = [img, 0, th1,
+          img, 0, th2,
+          blur, 0, th3]
+titles = ['Original Noisy Image','Histogram','Global Thresholding (v=127)',
+          'Original Noisy Image','Histogram',"Otsu's Thresholding",
+          'Gaussian filtered Image','Histogram',"Otsu's Thresholding"]
+for i in xrange(3):
+    plt.subplot(3,3,i*3+1),plt.imshow(images[i*3],'gray')
+    plt.title(titles[i*3]), plt.xticks([]), plt.yticks([])
+    plt.subplot(3,3,i*3+2),plt.hist(images[i*3].ravel(),256)
+    plt.title(titles[i*3+1]), plt.xticks([]), plt.yticks([])
+    plt.subplot(3,3,i*3+3),plt.imshow(images[i*3+2],'gray')
+    plt.title(titles[i*3+2]), plt.xticks([]), plt.yticks([])
+plt.show()
