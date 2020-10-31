47fe4824c6435c9e965f38525f3a7d920fcfe32c
Romil Jain <romilj012@gmail.com>
2020-10-31 23:43:18 +0530

    Decimal_To_AllConversions

---
+++ conversions/decimal_to_allconversions.py
@@ -0,0 +1,7 @@
+# Python program to convert decimal into other number systems
+dec = 344
+
+print("The decimal value of", dec, "is:")
+print(bin(dec), "in binary.")
+print(oct(dec), "in octal.")
+print(hex(dec), "in hexadecimal.")
