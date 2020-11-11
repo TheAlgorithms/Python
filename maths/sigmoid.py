d5f02bc9250eec91162b9c88798f374b729e2512
sukyung99 <tnrudsla413@gmail.com>
2020-11-12 02:26:30 +0900

    Add Maths / Sigmoid Function

---
+++ maths/sigmoid.py
@@ -0,0 +1,38 @@
+"""
+This script demonstrates the implementation of the Sigmoid function.
+
+The function takes a vector of K real numbers as input and then 1 / (1 + exp(-x)).
+After through Sigmoid, the element of the vector mostly 0 between 1. or 1 between -1.
+
+Script inspired from its corresponding Wikipedia article
+https://en.wikipedia.org/wiki/Sigmoid_function
+"""
+
+import numpy as np
+
+
+def sigmoid(vector: float):
+    """
+    Implements the sigmoid function
+
+    Parameters:
+        vector (np.array): A  numpy array of shape (1,n)
+        consisting of real values
+
+
+    Returns:
+        sigmoid_vec (np.array): The input numpy array, after applying
+        sigmoid.
+
+    >>> vec = np.array([-1.0, 1.0, 2.0])
+    >>> sigmoid(vec)
+    array([0.26894142, 0.73105858, 0.88079708])
+    """
+
+    return 1 / (1 + np.exp(-vector))
+
+
+if __name__ == "__main__":
+    print(
+        sigmoid(np.array([-1.0, 1.0, 2.0]))
+    )  # --> [0.26894142, 0.73105858, 0.88079708]
