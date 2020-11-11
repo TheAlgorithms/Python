a614c9d4b823c6caa73df8d17fdd17df68d6da6a
boyun <boyoon54@gmail.com>
2020-11-12 00:33:33 +0900

    pass isort

--- web_programming/crawl_google_scholar_citation.py
+++ web_programming/crawl_google_scholar_citation.py
@@ -1,24 +1,26 @@
-
 """
 Get the citation from google scholar
 using title and year of publication, and volume and pages of journal.
 """
 
-from bs4 import BeautifulSoup
-import requests
 import re
+
+import requests
+from bs4 import BeautifulSoup
 
 
 def create_url(title: str, journal: str, volume: str, page: str, year: str) -> str:
     """
     Return the url.
     """
-    url = f"http://scholar.google.com/scholar_lookup?hl=en&" \
-          f"title={title}" \
-          f"&journal={journal}" \
-          f"&volume={volume}" \
-          f"&pages={page}" \
-          f"&publication_year={year}"
+    url = (
+        f"http://scholar.google.com/scholar_lookup?hl=en&"
+        f"title={title}"
+        f"&journal={journal}"
+        f"&volume={volume}"
+        f"&pages={page}"
+        f"&publication_year={year}"
+    )
     url = remove_tag(url)
     return url.replace(" ", "%")
 
@@ -28,8 +30,8 @@ def remove_tag(url: str) -> str:
     Remove the html tags in 'url'.
     Return the url removed the html tags.
     """
-    tag = re.compile('<.*?>')
-    clean_url = re.sub(tag, '', url)
+    tag = re.compile("<.*?>")
+    clean_url = re.sub(tag, "", url)
     return clean_url
 
 
@@ -39,20 +41,20 @@ def get_citation(url: str) -> str:
     """
     url = requests.get(url).text
     soup = BeautifulSoup(url, "html.parser")
-    get_div = soup.find(u"div", attrs={u"class": u"gs_ri"})
-    get_a_tag = get_div.find(u"div", attrs={u"class": u"gs_fl"}).findAll('a')
+    get_div = soup.find("div", attrs={"class": "gs_ri"})
+    get_a_tag = get_div.find("div", attrs={"class": "gs_fl"}).findAll("a")
     citation = get_a_tag[2].get_text()
-    if 'Cited' not in citation:
-        citation = 'Cited by 0'
+    if "Cited" not in citation:
+        citation = "Cited by 0"
 
     return citation.replace("Cited by ", "")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """
     You have to fill following values: title, journal_name, volume, page, year.
     For example,
-    title = "abcde"
+    title = "abc de"
     journal_name = "fgh"
     volume = "30"
     page = "3979-3990"
