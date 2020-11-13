6c3d24d3880497471211b184c74c8b18a5f3ec8c
boyun <boyoon54@gmail.com>
2020-11-13 20:35:42 +0900

    change how to build url

--- web_programming/crawl_google_scholar_citation.py
+++ web_programming/crawl_google_scholar_citation.py
@@ -3,44 +3,15 @@ Get the citation from google scholar
 using title and year of publication, and volume and pages of journal.
 """
 
-import re
-
 import requests
 from bs4 import BeautifulSoup
 
 
-def create_url(title: str, journal: str, volume: str, page: str, year: str) -> str:
-    """
-    Return the url.
-    """
-    url = (
-        f"http://scholar.google.com/scholar_lookup?hl=en&"
-        f"title={title}"
-        f"&journal={journal}"
-        f"&volume={volume}"
-        f"&pages={page}"
-        f"&publication_year={year}"
-    )
-    url = remove_tag(url)
-    return url.replace(" ", "%")
-
-
-def remove_tag(url: str) -> str:
-    """
-    Remove the html tags in 'url'.
-    Return the url removed the html tags.
-    """
-    tag = re.compile("<.*?>")
-    clean_url = re.sub(tag, "", url)
-    return clean_url
-
-
-def get_citation(url: str) -> str:
+def get_citation(base_url: str, params: dict) -> str:
     """
     Return the citation number.
     """
-    url = requests.get(url).text
-    soup = BeautifulSoup(url, "html.parser")
+    soup = BeautifulSoup(requests.get(base_url, params=params).content, "html.parser")
     get_div = soup.find("div", attrs={"class": "gs_ri"})
     get_a_tag = get_div.find("div", attrs={"class": "gs_fl"}).findAll("a")
     citation = get_a_tag[2].get_text()
@@ -51,18 +22,16 @@ def get_citation(url: str) -> str:
 
 
 if __name__ == "__main__":
-    """
-    You have to fill following values: title, journal_name, volume, page, year.
-    """
-    title = (
-        "Precisely geometry controlled microsupercapacitors"
-        " for ultrahigh areal capacitance,"
-        " volumetric capacitance, and energy density"
-    )
-    journal_name = "Chem. Mater."
-    volume = "30"
-    page = "3979-3990"
-    year = "2018"
+    params = {
+        "title": (
+            "Precisely geometry controlled microsupercapacitors"
+            " for ultrahigh areal capacitance,"
+            " volumetric capacitance, and energy density"
+        ),
+        "journal_name": "Chem. Mater.",
+        "volume": "30",
+        "page": "3979-3990",
+        "year": "2018",
+    }
 
-    citation = get_citation(create_url(title, journal_name, volume, page, year))
-    print(citation)
+    print(get_citation("http://scholar.google.com/scholar_lookup?hl=en&", params=params))
