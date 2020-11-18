ad6d4748ad64dc758f815c0e66f69128d6a02734
Dhruv Manilawala <dhruvmanila@gmail.com>
2020-11-18 11:59:40 +0530

    Update upper.py

--- strings/upper.py
+++ strings/upper.py
@@ -13,7 +13,7 @@ def upper(word: str) -> str:
     """
 
     # Converting to ascii value int value and checking to see if char is a lower letter
-    # if it is a lowercase letter it is getting shift by 32 which makes it an uppercase 
+    # if it is a lowercase letter it is getting shift by 32 which makes it an uppercase
     # case letter
     return "".join(chr(ord(char) - 32) if "a" <= char <= "z" else char for char in word)
 
