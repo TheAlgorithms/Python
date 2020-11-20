0b36986b9fd62f6617e376934b6bc45850bb7549
Lewis Tian <taseikyo@gmail.com>
2020-11-20 15:46:48 +0800

    add doctests

--- graphs/breadth_first_search.py
+++ graphs/breadth_first_search.py
@@ -6,22 +6,48 @@ from typing import Set
 
 
 class Graph:
-    def __init__(self):
+    def __init__(self) -> None:
         self.vertices = {}
 
     def print_graph(self) -> None:
-        """prints adjacency list representation of graaph"""
+        """
+        prints adjacency list representation of graaph
+        >>> g = Graph()
+        >>> g.print_graph()
+        >>> g.add_edge(0, 1)
+        >>> g.print_graph()
+        0  :  1
+        """
         for i in self.vertices:
             print(i, " : ", " -> ".join([str(j) for j in self.vertices[i]]))
 
     def add_edge(self, from_vertex: int, to_vertex: int) -> None:
-        """adding the edge between two vertices"""
+        """
+        adding the edge between two vertices
+        >>> g = Graph()
+        >>> g.print_graph()
+        >>> g.add_edge(0, 1)
+        >>> g.print_graph()
+        0  :  1
+        """
         if from_vertex in self.vertices:
             self.vertices[from_vertex].append(to_vertex)
         else:
             self.vertices[from_vertex] = [to_vertex]
 
     def bfs(self, start_vertex: int) -> Set[int]:
+        """
+        >>> g = Graph()
+        >>> g.add_edge(0, 1)
+        >>> g.add_edge(0, 1)
+        >>> g.add_edge(0, 2)
+        >>> g.add_edge(1, 2)
+        >>> g.add_edge(2, 0)
+        >>> g.add_edge(2, 3)
+        >>> g.add_edge(3, 3)
+        >>> sorted(g.bfs(2))
+        [0, 1, 2, 3]
+        """
         # initialize set for storing already visited vertices
         visited = set()
 
@@ -44,6 +70,10 @@ class Graph:
 
 
 if __name__ == "__main__":
+    from doctest import testmod
+
+    testmod(verbose=True)
+
     g = Graph()
     g.add_edge(0, 1)
     g.add_edge(0, 2)
