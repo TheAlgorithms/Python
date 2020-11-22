fa26af697bd9f321b0f0f37b78b4c3edabe30706
Erdum <erdumadnan@gmail.com>
2020-11-22 22:50:28 +0500

    Errors resolvedc

--- Electronics/ohms_law.py
+++ Electronics/ohms_law.py
@@ -1,7 +1,22 @@
+# Importing doctest for testing our function
+from doctest import testmod
+
 # Defining function
+
+
 def ohms_law(v: float = 0, i: float = 0, r: float = 0) -> float:
+
+    """
+    This function calculates the any one
+    of the three fundemental value, of
+    electronics.
+    >>> ohms_law(v=10, r=5)
+    2.0
+    >>> ohms_law(i=1, r=10)
+    10.0
+    """
     if v == 0:
-        result = i * r
+        result = float(i * r)
         return result
     elif i == 0:
         result = v / r
@@ -11,3 +26,7 @@ def ohms_law(v: float = 0, i: float = 0, r: float = 0) -> float:
         return result
     else:
         return 0
+
+
+if __name__ == "__main__":
+    testmod(name="ohms_law", verbose=True)
