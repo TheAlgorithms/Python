a86e5b0ed464bb96829cc96595178cc8d8f9591b
Dhruv Manilawala <dhruvmanila@gmail.com>
2020-11-23 12:57:06 +0530

    Apply suggestions from code review

--- ciphers/base16.py
+++ ciphers/base16.py
@@ -1,8 +1,7 @@
 import base64
-import doctest
 
 
-def encode_to_b16(inp):
+def encode_to_b16(inp: str) -> bytes:
     """
     Encodes a given utf-8 string into base-16.
     >>> encode_to_b16('Hello World!')
@@ -18,4 +17,6 @@ def encode_to_b16(inp):
 
 
 if __name__ == "__main__":
+    import doctest
+
     doctest.testmod()
