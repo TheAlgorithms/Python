cfb87dc31cf2c234bb5c2c181931ec528eec5785
lawric1 <67882089+lawric1@users.noreply.github.com>
2020-11-24 16:41:02 -0300

    Add descriptive dict type hint, change headers format to f-string

--- web_programming/fetch_github_info.py
+++ web_programming/fetch_github_info.py
@@ -6,16 +6,18 @@ Authentication will be made via access token.
 To generate your personal access token visit https://github.com/settings/tokens.
 """
 
+
 import requests
+from typing import Dict
 
 _GITHUB_API = "https://api.github.com/user"
 USER_TOKEN = "" # provide your access token
 
-def fetch_github_info(auth_token: str) -> dict:
+def fetch_github_info(auth_token: str) -> Dict[any, any]:
     """
     Fetch GitHub info of a user using the requests module
     """
-    token = {"Authorization": "token {}".format(auth_token)}
+    token = {"Authorization": f"token {auth_token}"}
     return requests.get(_GITHUB_API, headers=token).json()
 
 
@@ -24,4 +26,4 @@ if __name__ == "__main__":
         for key, value in fetch_github_info(USER_TOKEN).items():
             print(f"{key}: {value}")
     else:
-        print("token should not be empty")
+        print("token should not be empty.")
