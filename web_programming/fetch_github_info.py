7b1433474ae969d9359bee7cf05cb06767639c29
lawric1 <67882089+lawric1@users.noreply.github.com>
2020-11-24 13:21:35 -0300

    Fixed docstring failure in pre-commit, Fixed request.get params to GitHub REST API standards

--- web_programming/fetch_github_info.py
+++ web_programming/fetch_github_info.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python3
-import requests
-
 """
 Created by sarathkaul on 14/11/19
 
 Basic authentication using an API password is deprecated and will soon no longer work.
 Visit https://developer.github.com/changes/2020-02-14-deprecating-password-auth
 for more information around suggested workarounds and removal dates.
+
+
+Updated by lawric1 on 24/11/20
+
+Authentication will be made via access token.
+To generate your personal access token visit https://github.com/settings/tokens.
 """
 
-"""
-Created by lawric1 on 24/11/20
-
-To generate your personal access token visit https://github.com/settings/tokens
-"""
-
+import requests
 
 _GITHUB_API = "https://api.github.com/user"
 
 
-def fetch_github_info(auth_user: str, auth_token: str) -> dict:
+def fetch_github_info(auth_token: str) -> dict:
     """
     Fetch GitHub info of a user using the requests module
     """
-    return requests.get(_GITHUB_API, auth=(auth_user, auth_token)).json()
+    token = {'Authorization': 'token {}'.format(auth_token)}
+    return requests.get(_GITHUB_API, headers=token).json()
 
 
 if __name__ == "__main__":
-    for key, value in fetch_github_info("<USER NAME>", "<TOKEN>").items():
+    for key, value in fetch_github_info("<TOKEN>").items():
         print(f"{key}: {value}")
