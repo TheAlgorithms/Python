7ab78478e9d0b52c2f848baa720a8b2d27adf298
lawric1 <67882089+lawric1@users.noreply.github.com>
2020-11-24 08:52:31 -0300

    fixes #3944 authentication error

--- web_programming/fetch_github_info.py
+++ web_programming/fetch_github_info.py
@@ -8,19 +8,24 @@ Visit https://developer.github.com/changes/2020-02-14-deprecating-password-auth
 for more information around suggested workarounds and removal dates.
 """
 
+"""
+Created by lawric1 on 24/11/20
+
+To generate your personal access token visit https://github.com/settings/tokens
+"""
 
 import requests
 
 _GITHUB_API = "https://api.github.com/user"
 
 
-def fetch_github_info(auth_user: str, auth_pass: str) -> dict:
+def fetch_github_info(auth_user: str, auth_token: str) -> dict:
     """
     Fetch GitHub info of a user using the requests module
     """
-    return requests.get(_GITHUB_API, auth=(auth_user, auth_pass)).json()
+    return requests.get(_GITHUB_API, auth=(auth_user, auth_token)).json()
 
 
 if __name__ == "__main__":
-    for key, value in fetch_github_info("<USER NAME>", "<PASSWORD>").items():
+    for key, value in fetch_github_info("<USER NAME>", "<TOKEN>").items():
         print(f"{key}: {value}")
