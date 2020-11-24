e211eb40f8a7b433b6ce532d685f91f8d60deefb
Sullivan <38718448+Epic-R-R@users.noreply.github.com>
2020-11-24 19:22:53 +0330

    isort

--- web_programming/instagram_pic.py
+++ web_programming/instagram_pic.py
@@ -1,13 +1,14 @@
-import requests
-from bs4 import BeautifulSoup
 from datetime import datetime
 
+import requests
+from bs4 import BeautifulSoup
 
 if __name__ == "__main__":
     url = input("Enter image url: ").strip()
     print(f"Downloading image from {url} ...")
     soup = BeautifulSoup(requests.get(url).content, "html.parser")
-    # The image URL is in the content field of the first meta tag with the property og:image
+    # The image URL is in the content field
+    # of the first meta tag with the property og:image
     image_url = soup.find("meta", {"property": "og:image"})["content"]
     image_data = requests.get(image_url).content
     file_name = f"{datetime.now():%Y-%m-%d_%H:%M:%S}.jpg"
