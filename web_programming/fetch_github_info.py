8454ae13c03e4146ac1f7ed81955ec2050d09eb6
Dhruv Manilawala <dhruvmanila@gmail.com>
2020-11-25 10:18:47 +0530

    Remove test function from main file

--- web_programming/fetch_github_info.py
+++ web_programming/fetch_github_info.py
@@ -42,30 +42,6 @@ def fetch_github_info(auth_token: str) -> Dict[Any, Any]:
     return requests.get(AUTHENTICATED_USER_ENDPOINT, headers=headers).json()
 
 
-def test_fetch_github_info(monkeypatch):
-    class FakeResponse:
-        def __init__(self, content) -> None:
-            assert isinstance(content, (bytes, str))
-            self.content = content
-        
-        def json(self):
-            import json
-
-            return json.loads(self.content)
-    
-    def mock_response(*args, **kwargs):
-        assert args[0] == AUTHENTICATED_USER_ENDPOINT
-        assert "Authorization" in kwargs["headers"]
-        assert kwargs["headers"]["Authorization"].startswith("token ")
-        assert "Accept" in kwargs["headers"]
-        return FakeResponse(b'{"login":"test","id":1}')
-
-    monkeypatch.setattr(requests, "get", mock_response)
-    result = fetch_github_info("token")
-    assert result["login"] == "test"
-    assert result["id"] == 1
-
-
 if __name__ == "__main__":
     if USER_TOKEN:
         for key, value in fetch_github_info(USER_TOKEN).items():
