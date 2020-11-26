947a783096794dbc92890ef523f143de49d1fe6d
Ariel Sperduti <arielbmx@gmail.com>
2020-11-26 13:04:18 -0300

    Fix length

--- other/text_to_speech.py
+++ other/text_to_speech.py
@@ -70,8 +70,8 @@ def request_fromtexttospeech(
     headers = {
         "Accept-Language": "en-US,en;q=0.5",
         "Upgrade-Insecure-Requests": "1",
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:66.0) Gecko/20100101 " +
-                      "Firefox/66.0",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:66.0) Gecko/20100101 "
+        + "Firefox/66.0",
         "Content-Type": "application/x-www-form-urlencoded",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
         "Referer": "http://www.fromtexttospeech.com/",
