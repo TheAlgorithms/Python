f0cd9d5428201fad4fd75d40019c92f33845bbf8
Meysam Azad <MeysamAzad81@yahoo.com>
2020-11-26 10:59:18 +0330

    Merge branch 'master' into 'test/string-conversion'

---
+++ project_euler/problem_074/sol2.py
@@ -0,0 +1,126 @@
+"""
+    Project Euler Problem 074: https://projecteuler.net/problem=74
+
+    Starting from any positive integer number
+    it is possible to attain another one summing the factorial of its digits.
+
+    Repeating this step, we can build chains of numbers.
+    It is not difficult to prove that EVERY starting number
+    will eventually get stuck in a loop.
+
+    The request is to find how many numbers less than one million
+    produce a chain with exactly 60 non repeating items.
+
+    Solution approach:
+    This solution simply consists in a loop that generates
+    the chains of non repeating items.
+    The generation of the chain stops before a repeating item
+    or if the size of the chain is greater then the desired one.
+    After generating each chain, the length is checked and the
+    counter increases.
+"""
+
+factorial_cache = {}
+factorial_sum_cache = {}
+
+
+def factorial(a: int) -> int:
+    """Returns the factorial of the input a
+    >>> factorial(5)
+    120
+
+    >>> factorial(6)
+    720
+
+    >>> factorial(0)
+    1
+    """
+
+    # The factorial function is not defined for negative numbers
+    if a < 0:
+        raise ValueError("Invalid negative input!", a)
+
+    if a in factorial_cache:
+        return factorial_cache[a]
+
+    # The case of 0! is handled separately
+    if a == 0:
+        factorial_cache[a] = 1
+    else:
+        # use a temporary support variable to store the computation
+        temporary_number = a
+        temporary_computation = 1
+
+        while temporary_number > 0:
+            temporary_computation *= temporary_number
+            temporary_number -= 1
+
+        factorial_cache[a] = temporary_computation
+    return factorial_cache[a]
+
+
+def factorial_sum(a: int) -> int:
+    """Function to perform the sum of the factorial
+    of all the digits in a
+
+    >>> factorial_sum(69)
+    363600
+    """
+    if a in factorial_sum_cache:
+        return factorial_sum_cache[a]
+    # Prepare a variable to hold the computation
+    fact_sum = 0
+
+    """ Convert a in string to iterate on its digits
+        convert the digit back into an int
+        and add its factorial to fact_sum.
+    """
+    for i in str(a):
+        fact_sum += factorial(int(i))
+    factorial_sum_cache[a] = fact_sum
+    return fact_sum
+
+
+def solution(chain_length: int = 60, number_limit: int = 1000000) -> int:
+    """Returns the number of numbers that produce
+        chains with exactly 60 non repeating elements.
+    >>> solution(10, 1000)
+    26
+    """
+
+    # the counter for the chains with the exact desired length
+    chain_counter = 0
+
+    for i in range(1, number_limit + 1):
+
+        # The temporary list will contain the elements of the chain
+        chain_set = {i}
+        len_chain_set = 1
+        last_chain_element = i
+
+        # The new element of the chain
+        new_chain_element = factorial_sum(last_chain_element)
+
+        # Stop computing the chain when you find a repeating item
+        # or the length it greater then the desired one.
+
+        while new_chain_element not in chain_set and len_chain_set <= chain_length:
+            chain_set.add(new_chain_element)
+
+            len_chain_set += 1
+            last_chain_element = new_chain_element
+            new_chain_element = factorial_sum(last_chain_element)
+
+        # If the while exited because the chain list contains the exact amount
+        # of elements increase the counter
+        if len_chain_set == chain_length:
+            chain_counter += 1
+
+    return chain_counter
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
+    print(f"{solution()}")
---
+++ project_euler/problem_188/sol1.py
@@ -0,0 +1,68 @@
+"""
+Project Euler Problem 188: https://projecteuler.net/problem=188
+
+The hyperexponentiation of a number
+
+The hyperexponentiation or tetration of a number a by a positive integer b,
+denoted by a↑↑b or b^a, is recursively defined by:
+
+a↑↑1 = a,
+a↑↑(k+1) = a(a↑↑k).
+
+Thus we have e.g. 3↑↑2 = 3^3 = 27, hence 3↑↑3 = 3^27 = 7625597484987 and
+3↑↑4 is roughly 103.6383346400240996*10^12.
+
+Find the last 8 digits of 1777↑↑1855.
+
+References:
+    - https://en.wikipedia.org/wiki/Tetration
+"""
+
+
+# small helper function for modular exponentiation
+def _modexpt(base: int, exponent: int, modulo_value: int) -> int:
+    """
+    Returns the modular exponentiation, that is the value
+    of `base ** exponent % modulo_value`, without calculating
+    the actual number.
+    >>> _modexpt(2, 4, 10)
+    6
+    >>> _modexpt(2, 1024, 100)
+    16
+    >>> _modexpt(13, 65535, 7)
+    6
+    """
+
+    if exponent == 1:
+        return base
+    if exponent % 2 == 0:
+        x = _modexpt(base, exponent / 2, modulo_value) % modulo_value
+        return (x * x) % modulo_value
+    else:
+        return (base * _modexpt(base, exponent - 1, modulo_value)) % modulo_value
+
+
+def solution(base: int = 1777, height: int = 1855, digits: int = 8) -> int:
+    """
+    Returns the last 8 digits of the hyperexponentiation of base by
+    height, i.e. the number base↑↑height:
+
+    >>> solution(base=3, height=2)
+    27
+    >>> solution(base=3, height=3)
+    97484987
+    >>> solution(base=123, height=456, digits=4)
+    2547
+    """
+
+    # calculate base↑↑height by right-assiciative repeated modular
+    # exponentiation
+    result = base
+    for i in range(1, height):
+        result = _modexpt(base, result, 10 ** digits)
+
+    return result
+
+
+if __name__ == "__main__":
+    print(f"{solution() = }")
