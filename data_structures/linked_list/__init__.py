959738236e20c4963ea61c088150e5121e07d838
arif599 <59244462+arif599@users.noreply.github.com>
2020-11-26 10:38:45 -0800

    Adding type hints

--- data_structures/linked_list/__init__.py
+++ data_structures/linked_list/__init__.py
@@ -1,19 +1,26 @@
+"""
+Linked Lists consists of Nodes.
+Nodes contain data and also may link to other nodes:
+    - Head Node: First node, the address of the head node gives us access of the complete list
+    - Last node: points to null
+"""
+
 class Node:
-    def __init__(self, item, next):
+    def __init__(self, item: any, next):
         self.item = item
         self.next = next
 
 
 class LinkedList:
-    def __init__(self):
+    def __init__(self) -> None:
         self.head = None
         self.size = 0
 
-    def add(self, item):
+    def add(self, item:any ) -> None:
         self.head = Node(item, self.head)
         self.size += 1
 
-    def remove(self):
+    def remove(self) -> any:
         if self.is_empty():
             return None
         else:
@@ -22,10 +29,10 @@ class LinkedList:
             self.size -= 1
             return item
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         return self.head is None
 
-    def __str__(self):
+    def __str__(self) -> str:
         """
         >>> linked_list = LinkedList()
         >>> linked_list.add(23)
@@ -41,15 +48,14 @@ class LinkedList:
             item_str = ""
             item_list = []
             while iterate:
-                item_list.append(iterate.item)
+                item_list.append(str(iterate.item))
                 iterate = iterate.next
 
-            item_list = [str(item) for item in item_list]
             item_str = " --> ".join(item_list)
 
             return item_str
 
-    def __len__(self):
+    def __len__(self) -> int:
         """
         >>> linked_list = LinkedList()
         >>> len(linked_list)
