03a7aa0a6e28ee8876706db1177e7d9de0204f07
shellhub <shellhub.me@gmail.com>
2020-11-27 09:59:38 +0800

    fixed LGTM fixed typehint

--- maths/lucas_series.py
+++ maths/lucas_series.py
@@ -3,7 +3,7 @@ https://en.wikipedia.org/wiki/Lucas_number
 """
 
 
-def recursive_lucas_number(n):
+def recursive_lucas_number(n: int) -> int:
     """
     Returns the nth lucas number
     >>> recursive_lucas_number(1)
@@ -19,12 +19,12 @@ def recursive_lucas_number(n):
         ...
     TypeError: recursive_lucas_number accepts only integer arguments.
     """
-    if n == 1:
-        return n
-    if n == 0:
-        return 2
     if not isinstance(n, int):
         raise TypeError("recursive_lucas_number accepts only integer arguments.")
+    if n == 0:
+        return 2
+    if n == 1:
+        return 1
 
     return recursive_lucas_number(n - 1) + recursive_lucas_number(n - 2)
 
@@ -47,10 +47,6 @@ def dynamic_lucas_number(n: int) -> int:
     """
     if not isinstance(n, int):
         raise TypeError("dynamic_lucas_number accepts only integer arguments.")
-    if n == 0:
-        return 2
-    if n == 1:
-        return 1
     a, b = 2, 1
     for i in range(n):
         a, b = b, a + b
@@ -61,7 +57,6 @@ if __name__ == "__main__":
     from doctest import testmod
 
     testmod()
-    n = int(input("Enter the number of terms in lucas series:\n").strip())
     n = int(input("Enter the number of terms in lucas series:\n").strip())
     print("Using recursive function to calculate lucas series:")
     print(" ".join(str(recursive_lucas_number(i)) for i in range(n)))
