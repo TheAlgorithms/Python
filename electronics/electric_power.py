9b2826e10a2cde49a09cf21751e4a319a7cc7476
Erdum <erdumadnan@gmail.com>
2020-11-28 13:41:30 +0500

    updated as suggested by cclauss

--- electronics/electric_power.py
+++ electronics/electric_power.py
@@ -3,7 +3,8 @@
 
 def electric_power(voltage: float, current: float, power: float) -> float:
     """
-    This function can calculate any one of the three (voltage, current, power), fundamental value of electrical system.
+    This function can calculate any one of the three (voltage, current, power),
+    fundamental value of electrical system.
     examples are below:
     >>> electric_power(voltage=0, current=2, power=5)
     {'voltage': 2.5}
@@ -15,20 +16,18 @@ def electric_power(voltage: float, current: float, power: float) -> float:
     if (voltage, current, power).count(0) != 1:
         raise ValueError("Only one argument must be 0")
     elif power < 0:
-        raise ValueError("Power cannot be negative in any electrical/electronics system")
+        raise ValueError(
+            "Power cannot be negative in any electrical/electronics system"
+        )
     elif voltage == 0:
-        return {'voltage': power / current}
+        return {"voltage": power / current}
     elif current == 0:
-        return {'current': power / voltage}
+        return {"current": power / voltage}
     elif power == 0:
-        # I created this variable because i want to remove negative sign from the power value
-        result = (float(voltage * current))
-        if result < 0:
-            result = result * -1
-        return {'power': result}
+        return {"power": abs(float(voltage * current))}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
