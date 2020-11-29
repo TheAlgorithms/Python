9ce187e17b7c1af4d7000a32d5df378a8178122f
Sullivan <38718448+Epic-R-R@users.noreply.github.com>
2020-11-29 13:44:14 +0330

    Some Change

--- web_programming/instagram_video.py
+++ web_programming/instagram_video.py
@@ -1,17 +1,18 @@
-import json
 from datetime import datetime
 
 import requests
 
+
+def download_video(url: str) -> bytes:
+    url = f"https://downloadgram.net/wp-json/wppress/video-downloader/video?url={url}"
+    json_data = requests.get(url).json()
+    video_data = requests.get(json_data[0]["urls"][0]["src"]).content
+    return video_data
+
 if __name__ == "__main__":
     link = input("Enter Video/IGTV url: ").strip()
-    url = f"https://downloadgram.net/wp-json/wppress/video-downloader/video?url={link}"
-    req = requests.get(url)
-    res = req.content
-    json_data = json.loads(res)
     print(f"Downloading video from {link} ...")
-    video_data = requests.get(json_data[0]["urls"][0]["src"]).content
     file_name = f"{datetime.now():%Y-%m-%d_%H:%M:%S}.mp4"
     with open(file_name, "wb") as fp:
-        fp.write(video_data)
+        fp.write(download_video(link))
     print(f"Done. Video saved to disk as {file_name}.")
