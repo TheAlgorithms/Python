a43af896b6a913e71768bdaaee8337375afbf3bf
Sullivan <38718448+Epic-R-R@users.noreply.github.com>
2020-11-29 13:45:00 +0330

    Update

--- web_programming/instagram_video.py
+++ web_programming/instagram_video.py
@@ -11,7 +11,6 @@ def download_video(url: str) -> bytes:
 
 if __name__ == "__main__":
     link = input("Enter Video/IGTV url: ").strip()
-    print(f"Downloading video from {link} ...")
     file_name = f"{datetime.now():%Y-%m-%d_%H:%M:%S}.mp4"
     with open(file_name, "wb") as fp:
         fp.write(download_video(link))
