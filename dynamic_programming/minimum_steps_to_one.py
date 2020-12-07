99e0bec2e9efe5879f77062fd4d7d5cb50661a12
Alex Joslin <aljoslin13@yahoo.com>
2020-12-06 20:28:30 -0800

    Update minimum_steps_to_one.py
    Made the parameter "n" more descriptive.  Changed it to number

--- dynamic_programming/minimum_steps_to_one.py
+++ dynamic_programming/minimum_steps_to_one.py
@@ -27,7 +27,7 @@ from __future__ import annotations
 __author__ = "Alexander Joslin"
 
 
-def min_steps_to_one(n: int) -> int:
+def min_steps_to_one(number: int) -> int:
     """
     Implemented using tabulation.
 
@@ -45,22 +45,22 @@ def min_steps_to_one(n: int) -> int:
     :return int:
     """
 
-    if n <= 0:
-        raise ValueError(f"n must be greater than 0. Got n = {n}")
+    if number <= 0:
+        raise ValueError(f"n must be greater than 0. Got n = {number}")
 
-    table = [n + 1] * (n + 1)
+    table = [number + 1] * (number + 1)
 
     # starting position
     table[1] = 0
-    for i in range(1, n):
+    for i in range(1, number):
         table[i + 1] = min(table[i + 1], table[i] + 1)
         # check if out of bounds
-        if i * 2 <= n:
+        if i * 2 <= number:
             table[i * 2] = min(table[i * 2], table[i] + 1)
         # check if out of bounds
-        if i * 3 <= n:
+        if i * 3 <= number:
             table[i * 3] = min(table[i * 3], table[i] + 1)
-    return table[n]
+    return table[number]
 
 
 if __name__ == "__main__":
