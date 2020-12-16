6c32a6d2d35a2467577193adcd12e93e65829b93
shellhub <shellhub.me@gmail.com>
2020-12-16 18:45:33 +0800

    fixed shell sort

--- sorts/shell_sort.py
+++ sorts/shell_sort.py
@@ -19,26 +19,28 @@ def shell_sort(collection):
 
     >>> shell_sort([0, 5, 3, 2, 2])
     [0, 2, 2, 3, 5]
-
     >>> shell_sort([])
     []
-
     >>> shell_sort([-2, -5, -45])
     [-45, -5, -2]
     """
     # Marcin Ciura's gap sequence
     gaps = [701, 301, 132, 57, 23, 10, 4, 1]
-
     for gap in gaps:
         for i in range(gap, len(collection)):
-            j = i
-            while j >= gap and collection[j] < collection[j - gap]:
-                collection[j], collection[j - gap] = collection[j - gap], collection[j]
+            insert_value = collection[i]
+            j = i - gap
+            while j >= 0 and insert_value < collection[j]:
+                collection[j + gap] = collection[j]
                 j -= gap
+            collection[j + gap] = insert_value
     return collection
 
 
 if __name__ == "__main__":
+    from doctest import testmod
+
+    testmod()
     user_input = input("Enter numbers separated by a comma:\n").strip()
     unsorted = [int(item) for item in user_input.split(",")]
     print(shell_sort(unsorted))
