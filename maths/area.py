45da82461ccf8d5a4a6a27f7539b572954714555
SiddhantJain15 <siddhant.jain150397@gmail.com>
2020-12-27 00:57:13 +0530

    Update area.py
    Modified area of triangle function. Added a new algorithm to calculate area when 3 sides are known

--- maths/area.py
+++ maths/area.py
@@ -1,13 +1,12 @@
 """
 Find the area of various geometric shapes
 """
-from math import pi
+from math import pi, sqrt
 
 
 def surface_area_cube(side_length: float) -> float:
     """
     Calculate the Surface Area of a Cube.
-
     >>> surface_area_cube(1)
     6
     >>> surface_area_cube(3)
@@ -27,7 +26,6 @@ def surface_area_sphere(radius: float) -> float:
     Calculate the Surface Area of a Sphere.
     Wikipedia reference: https://en.wikipedia.org/wiki/Sphere
     :return 4 * pi * r^2
-
     >>> surface_area_sphere(5)
     314.1592653589793
     >>> surface_area_sphere(1)
@@ -45,7 +43,6 @@ def surface_area_sphere(radius: float) -> float:
 def area_rectangle(length: float, width: float) -> float:
     """
     Calculate the area of a rectangle
-
     >>> area_rectangle(10, 20)
     200
     >>> area_rectangle(-1, -2)
@@ -69,7 +66,6 @@ def area_rectangle(length: float, width: float) -> float:
 def area_square(side_length: float) -> float:
     """
     Calculate the area of a square
-
     >>> area_square(10)
     100
     >>> area_square(-1)
@@ -82,34 +78,54 @@ def area_square(side_length: float) -> float:
     return side_length ** 2
 
 
-def area_triangle(base: float, height: float) -> float:
+def area_isosceles_triangle(base: float, height: float) -> float:
     """
-    Calculate the area of a triangle
-
-    >>> area_triangle(10, 10)
+    Calculate the area of an isosceles triangle
+    >>> area_isosceles_triangle(10, 10)
     50.0
-    >>> area_triangle(-1, -2)
+    >>> area_isosceles_triangle(-1, -2)
     Traceback (most recent call last):
         ...
-    ValueError: area_triangle() only accepts non-negative values
-    >>> area_triangle(1, -2)
+    ValueError: area_isosceles_triangle() only accepts non-negative values
+    >>> area_isosceles_triangle(1, -2)
     Traceback (most recent call last):
         ...
-    ValueError: area_triangle() only accepts non-negative values
-    >>> area_triangle(-1, 2)
+    ValueError: area_isosceles_triangle() only accepts non-negative values
+    >>> area_isosceles_triangle(-1, 2)
     Traceback (most recent call last):
         ...
-    ValueError: area_triangle() only accepts non-negative values
+    ValueError: area_isosceles_triangle() only accepts non-negative values
     """
     if base < 0 or height < 0:
-        raise ValueError("area_triangle() only accepts non-negative values")
+        raise ValueError("area_isosceles_triangle() only accepts non-negative values")
     return (base * height) / 2
 
+def area_triangle(side1: float, side2: float, side3: float) -> float:
+    """
+    Calculate area of triangle when 3 sides are known
+
+    >>> area_triangle(5, 12, 13)
+    30.0
+    >>> area_triangle(-1, -2, -1)
+    Traceback (most recent call last):
+        ...
+    ValueError: area_triangle() only accepts non-negative values
+    >>> area_triangle(1, -2, 1)
+    Traceback (most recent call last):
+        ...
+    ValueError: area_triangle() only accepts non-negative values
+    """
+    if side1 < 0 or side2 < 0 or side3 < 0:
+        raise ValueError("area_triangle() only accepts non-negative values")
+    elif side1 + side2 < side3 or side1 + side3 < side2 or side2 + side3 < side1:
+        raise ValueError("Given three sides do not form a triangle")
+    semi_perimeter = (side1 + side2 + side3) / 2;
+    area = sqrt(semi_perimeter * (semi_perimeter - side1) * (semi_perimeter - side2) * (semi_perimeter - side3))
+    return area
 
 def area_parallelogram(base: float, height: float) -> float:
     """
     Calculate the area of a parallelogram
-
     >>> area_parallelogram(10, 20)
     200
     >>> area_parallelogram(-1, -2)
@@ -133,7 +149,6 @@ def area_parallelogram(base: float, height: float) -> float:
 def area_trapezium(base1: float, base2: float, height: float) -> float:
     """
     Calculate the area of a trapezium
-
     >>> area_trapezium(10, 20, 30)
     450.0
     >>> area_trapezium(-1, -2, -3)
@@ -173,7 +188,6 @@ def area_trapezium(base1: float, base2: float, height: float) -> float:
 def area_circle(radius: float) -> float:
     """
     Calculate the area of a circle
-
     >>> area_circle(20)
     1256.6370614359173
     >>> area_circle(-1)
@@ -189,7 +203,6 @@ def area_circle(radius: float) -> float:
 def area_ellipse(radius_x: float, radius_y: float) -> float:
     """
     Calculate the area of a ellipse
-
     >>> area_ellipse(10, 10)
     314.1592653589793
     >>> area_ellipse(10, 20)
@@ -215,7 +228,6 @@ def area_ellipse(radius_x: float, radius_y: float) -> float:
 def area_rhombus(diagonal_1: float, diagonal_2: float) -> float:
     """
     Calculate the area of a rhombus
-
     >>> area_rhombus(10, 20)
     100.0
     >>> area_rhombus(-1, -2)
@@ -240,7 +252,8 @@ def main():
     print("Areas of various geometric shapes: \n")
     print(f"Rectangle: {area_rectangle(10, 20)}")
     print(f"Square: {area_square(10)}")
-    print(f"Triangle: {area_triangle(10, 10)}")
+    print(f"Triangle: {area_isosceles_triangle(10, 10)}")
+    print(f"Triangle: {area_triangle(5, 12, 13)}")
     print(f"Parallelogram: {area_parallelogram(10, 20)}")
     print(f"Trapezium: {area_trapezium(10, 20, 30)}")
     print(f"Circle: {area_circle(20)}")
