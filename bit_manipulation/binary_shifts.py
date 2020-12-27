919af320fdaf3ef67873697d3ccd883383a3ca9f
Leyza <dragonzgirl11@gmail.com>
2020-12-27 00:13:05 -0500

    Fixed formatting

--- bit_manipulation/binary_shifts.py
+++ bit_manipulation/binary_shifts.py
@@ -56,7 +56,7 @@ def logical_right_shift(number: int, shift_amount: int) -> str:
     binary_number = str(bin(number))[2:]
     if shift_amount >= len(binary_number):
         return "0b0"
-    shifted_binary_number = binary_number[:len(binary_number) - shift_amount]
+    shifted_binary_number = binary_number[: len(binary_number) - shift_amount]
     return "0b" + shifted_binary_number
 
 
@@ -84,13 +84,19 @@ def arithmetic_right_shift(number: int, shift_amount: int) -> str:
     else:  # Get binary (2's complement) representation of negative number
         binary_number_length = len(bin(number)[3:])  # Find 2's complement of number
         binary_number = bin(abs(number) - (1 << binary_number_length))[3:]
-        binary_number = ("1" + "0" * (binary_number_length - len(binary_number)) +
-                         binary_number) if number < 0 else "0"
+        binary_number = (
+            ("1" + "0" * (binary_number_length - len(binary_number)) + binary_number)
+            if number < 0
+            else "0"
+        )
 
     if shift_amount >= len(binary_number):
         return "0b" + binary_number[0] * len(binary_number)
-    return "0b" + binary_number[0] * shift_amount + \
-           binary_number[:len(binary_number) - shift_amount]
+    return (
+            "0b"
+            + binary_number[0] * shift_amount
+            + binary_number[:len(binary_number) - shift_amount]
+    )
 
 
 if __name__ == "__main__":
--- bit_manipulation/binary_twos_complement.py
+++ bit_manipulation/binary_twos_complement.py
@@ -22,9 +22,15 @@ def twos_complement(number: int) -> str:
         raise ValueError("input must be a negative integer")
     binary_number_length = len(bin(number)[3:])
     twos_complement_number = bin(abs(number) - (1 << binary_number_length))[3:]
-    twos_complement_number = ("1" + "0" *
-                              (binary_number_length - len(twos_complement_number)) +
-                              twos_complement_number) if number < 0 else "0"
+    twos_complement_number = (
+        (
+            "1"
+            + "0" * (binary_number_length - len(twos_complement_number))
+            + twos_complement_number
+        )
+        if number < 0
+        else "0"
+    )
     return "0b" + twos_complement_number
 
 
