ec0fe34ffcb014949ba707293423e6eb032db74f
Leyza <dragonzgirl11@gmail.com>
2020-12-26 23:13:00 -0500

    Fixed problem representing 0 wrong

--- bit_manipulation/binary_twos_complement.py
+++ bit_manipulation/binary_twos_complement.py
@@ -3,6 +3,8 @@ def twos_complement(a: int) -> str:
     Take in a negative integer 'a'.
     Return the two's complement representation of 'a'.
 
+    >>> twos_complement(0)
+    '0b0'
     >>> twos_complement(-1)
     '0b11'
     >>> twos_complement(-5)
@@ -18,9 +20,10 @@ def twos_complement(a: int) -> str:
     """
     if a > 0:
         raise ValueError("input must be a negative integer")
-    bit_a_length = len(bin(a)[3:])
-    twos_complement_a = bin(abs(a) - (1 << bit_a_length))[3:]
-    twos_complement_a = '1' + '0' * (bit_a_length - len(twos_complement_a)) + twos_complement_a
+    binary_a_length = len(bin(a)[3:])
+    twos_complement_a = bin(abs(a) - (1 << binary_a_length))[3:]
+    twos_complement_a = ('1' + '0' * (binary_a_length - len(twos_complement_a)) +
+                         twos_complement_a) if a < 0 else '0'
     return '0b' + twos_complement_a
 
 
