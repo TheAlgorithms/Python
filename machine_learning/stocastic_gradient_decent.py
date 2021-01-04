6c40eca7da6f0eaa9b0b89904a184f08ba9bda25
MiClaire <narayan.sachan19@tutanota.com>
2021-01-04 19:12:18 +0530

    added plot func

--- machine_learning/stocastic_gradient_decent.py
+++ machine_learning/stocastic_gradient_decent.py
@@ -3,6 +3,7 @@ Implementation of stocastic gradient decent algorithm for minimizing cost
 of a Mean Square Error function [MSE]
 """
 import numpy as np
+import matplotlib.pyplot as plt
 
 """
 function looks like y = w_0 + w_1 * x [For a single Feature[Column]]
@@ -16,14 +17,16 @@ or simply : y = W * X                   :y = output value
 """
 def Generate_data(seed, takeout=0.7):
 
-    total_no = 20
+    total_no = 100
     np.random.seed(seed)
-    _data = np.random.randint(0, 10, size=(total_no, 4))
+    X_linear = 2 * np.random.randn(total_no, 1)
+    y_linear = 4 + 10 * X_linear + np.random.randn(total_no, 1) * 10
+    _data = np.c_[X_linear, y_linear]
 
     bias_column = np.ones(total_no)
     data_modified = np.c_[bias_column, _data]
 
-    amount = int(20 * 0.7)
+    amount = int(total_no * 0.7)
     train_data, test_data = data_modified[:amount, :], data_modified[amount: , :]
 
     np.random.shuffle(train_data)
@@ -34,7 +37,7 @@ def Generate_data(seed, takeout=0.7):
 train_data, test_data = Generate_data(0)
 
 m = train_data.shape[0]
-n = train_data.shape[1]
+n = train_data.shape[1] - 1
 
 parameter_vector = np.random.randn(n, 1)
 LEARNING_RATE = 0.001
@@ -75,22 +78,36 @@ def _calc_gradient(x_index, y_index, param_vec):
     result = np.dot(x_index_transpose, np.dot(x_index, param_vec) - y_index)
     return result
 
-def stocastic_gradient_decent(data_set, param_vec, seed):
+def stocastic_gradient_decent(data_set, param_vec, seed, epochs=1):
     """
     """
     np.random.seed(seed)
     np.random.shuffle(data_set)
-    for _ in range(m):
-        random_index = np.random.randint(m)
-        x_i = data_set[random_index:random_index + 1]
-        y_i= _output_val(random_index, data_set)
-        gradient = _calc_gradient(x_i, y_i, param_vec)
-        param_vec -= LEARNING_RATE * gradient
     
-    global parameter_vector
-    parameter_vector = param_vec
+    for _ in range(epochs):
 
+        for _ in range(m):
+
+            random_index = np.random.randint(m)
+
+            x_i = data_set[random_index:random_index + 1]
+            y_i= _output_val(random_index, data_set)
+
+            gradient = _calc_gradient(x_i, y_i, param_vec)
+            param_vec -= LEARNING_RATE * gradient
+
+            # print("loss: ", mse_error(random_index, data_set, param_vec))
+    
+    return param_vec
+
+def plot_hypothesis(data_set, output):
+    #plt.scatter(data_set[:, -1], output)
+    plt.plot(data_set @ parameter_vector, output)
+    plt.show()
 
 if __name__ == "__main__":
     X = train_data[:, :n]
-    stocastic_gradient_decent(X, parameter_vector, 1907)
+    y = train_data[:, -1]
+    parameter_vector = stocastic_gradient_decent(X, parameter_vector, 1907)
+    plot_hypothesis(X, y)
+    # test = mse_error(1, test_data[:, :n], parameter_vector)
