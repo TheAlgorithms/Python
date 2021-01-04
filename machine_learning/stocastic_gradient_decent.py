f51a5bc9e431eb4b777be4f275fe96c39dddd3ac
MiClaire <narayan.sachan19@tutanota.com>
2021-01-04 17:14:08 +0530

    added SGD implementation

--- machine_learning/stocastic_gradient_decent.py
+++ machine_learning/stocastic_gradient_decent.py
@@ -14,18 +14,29 @@ or simply : y = W * X                   :y = output value
                                         :W = Whole Parameter Vector
                                         :* = dot product
 """
-np.random.seed(1907)
-train_data = np.random.randint(0, 10, size=(10, 4))
-test_data = np.random.randint(0, 10, size=(4, 4))
+def Generate_data(seed, takeout=0.7):
+
+    total_no = 20
+    np.random.seed(seed)
+    _data = np.random.randint(0, 10, size=(total_no, 4))
+
+    bias_column = np.ones(total_no)
+    data_modified = np.c_[bias_column, _data]
+
+    amount = int(20 * 0.7)
+    train_data, test_data = data_modified[:amount, :], data_modified[amount: , :]
+
+    np.random.shuffle(train_data)
+    np.random.shuffle(test_data)
+
+    return (train_data, test_data)
+
+train_data, test_data = Generate_data(0)
 
 m = train_data.shape[0]
-n = test_data.shape[1]
+n = train_data.shape[1]
 
-bias_column = np.ones(m)
-train_data_modified = np.c_[bias_column, train_data]
-test_data_modified = np.c_[bias_column, train_data]
-
-paramater_vector = np.random.randn(n, 1)
+parameter_vector = np.random.randn(n, 1)
 LEARNING_RATE = 0.001
 
 def _output_val(record_no: int, data_set: np.ndarray) -> np.ndarray:
@@ -56,6 +67,23 @@ def _calc_hypothesis_val(record_no, data_set, param_vec):
     hypothesis_val = np.dot(data_set[record_no, :], param_vec)
     return hypothesis_val
 
+def _calc_gradient(X_index, y_index, param_vec):
+    pass
+
+def stocastic_gradient_decent(data_set, param_vec, seed):
+    """
+    """
+    np.random.seed(seed)
+    np.random.shuffle(data_set)
+    for i in range(m):
+        random_index = np.random.randint(m)
+        data_point_i = data_set[random_index:random_index + 1]
+        output_val_i = data_set[random_index:random_index + 1]
+        gradient = _calc_gradient(data_point_i, output_val_i, parameter_vector)
+        param_vec -= LEARNING_RATE * gradient
+
+
+
 if __name__ == "__main__":
-    X = train_data_modified[:, :n]
-    print(mse_error(1, X, paramater_vector))
+    X = train_data[:, :n]
+    stocastic_gradient_decent(X, parameter_vector, 1907)
