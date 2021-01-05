c8de38e7a22f02271ab03501da986cba60711526
MiClaire <narayan.sachan19@tutanota.com>
2021-01-05 14:01:45 +0530

    added doctests

--- machine_learning/stocastic_gradient_decent.py
+++ machine_learning/stocastic_gradient_decent.py
@@ -12,15 +12,20 @@ function looks like y = w_0 + w_1 * x [For a single Feature[Column]]
 """
 
 
-def generate_data(seed: int, takeout: int) -> Tuple:
+def generate_data(total_no: int, seed: int, takeout: int) -> Tuple:
     """
     param seed: to seed to random randn function to get reproducible result
     param takeout: to divide the data set into train and test sets
     e.g [here its 0.7 to divide the set int o 70% train and 30% test]
     Note:
     here bias column is added which is column full of ones
+
+    >>> train_set, test_set = generate_data(2 , 19, 0.5)
+    >>> train_set
+    [[ 1.          0.44200653 -0.9978138 ]]
+    >>> test_set
+    [[ 1.         -0.68093002 10.40526862]]
     """
-    total_no = 100
     np.random.seed(seed)
     x_linear = 2 * np.random.randn(total_no, 1)
     y_linear = 4 - 10 * x_linear + np.random.randn(total_no, 1)
@@ -43,6 +48,10 @@ def _output_val(record_no: int, data_set: np.ndarray) -> np.ndarray:
     param record_no: data point no. in data_set
     param data_set: dataset in which record 's output needed
     return : output for that specific data point value
+
+    >>> output_value = _output_val(1, np.array([[1, 2], [3, 4] ,[5, 6]]))
+    >>> output_value
+    4
     """
     return data_set[record_no, -1]
 
@@ -52,6 +61,11 @@ def mse_error(record_no: int, data_set: np.ndarray, param_vec: np.ndarray) -> fl
     param record_no: data point no.
     param dataset: data_set whose data points needs to be taken
     param_vec: parameter_vector
+
+    >>> error = mse_error(1, np.array([[1, 2], [3, 4] ,[5, 6]]), np.array([1, 2]))
+    >>> error
+    24.5
+
     """
     error = np.power(
         _calc_hypothesis_val(record_no, data_set, param_vec)
@@ -69,6 +83,13 @@ def _calc_hypothesis_val(
     param record_no: data point no.
     param dataset: data_set whose data points needs to be taken
     param_vec: parameter_vector
+
+    >>> data = np.array([[1, 2], [3, 4] ,[5, 6]])
+    >>> parameter_vector = np.array([1, 2])
+    >>> hypo_val = _calc_hypothesis_val(1, data, parameter_vector)
+    >>> hypo_val
+    11
+
     """
     data_set = data_set.reshape(-1, n)
     hypothesis_val = np.dot(data_set[record_no, :], param_vec)
@@ -85,6 +106,15 @@ def _calc_gradient(
     Note:
     the expression below is the derivative of the MSE function
     with respect to all parameters
+
+    >>> data = np.array([1, 3]).reshape(1, 2)
+    >>> output = np.array([1])
+    >>> parameter_vector = np.array([1, 2])
+    >>> grad = _calc_gradient(data, output, parameter_vector)
+    >>> grad
+    [[ 6]
+    [18]]
+
     """
     x_index_transpose = np.transpose(data)
 
@@ -96,13 +126,18 @@ def learning_rate_decay(initial_learning_rate: float, epoch_no: int) -> float:
     """
     param intial_learning_rate: the learning rate in the previous epoch
     param epoch_no: current epoch_no
+
+    >>> lr = learning_rate_decay(1, 2)
+    >>> lr
+    0.0001
+
     """
     decay_rate = 0.01
     new_learning_rate = initial_learning_rate * decay_rate ** epoch_no
     return new_learning_rate
 
 
-def stocastic_gradient_decent(
+def stochastic_gradient_descent(
     data_set: np.ndarray, param_vec: np.ndarray, seed: int, epochs: int
 ) -> np.ndarray:
     """
@@ -111,6 +146,16 @@ def stocastic_gradient_decent(
     param seed: to seed the random shuffle function to get reproducible results
     param epochs: how many times to loop over the data set
     return: parameter vector
+
+    >>> data = np.array([1, 3], dtype=np.float32).reshape(1, 2)
+    >>> output = np.array([1])
+    >>> parameter_vector = np.array([[1], [2]], dtype=np.float32)
+    >>> parameter_vector = stochastic_gradient_descent(data, parameter_vector,
+        seed=1907, epochs=100)
+    >>> paramter_vector
+    [[0.6       ]
+    [0.79999995]]
+
     """
     global learning_rate
     np.random.seed(seed)
@@ -132,7 +177,7 @@ def stocastic_gradient_decent(
 
 
 # Create the train and test data sets
-train_data, test_data = generate_data(seed=0, takeout=0.7)
+train_data, test_data = generate_data(100, seed=0, takeout=0.7)
 
 m = train_data.shape[0]
 n = train_data.shape[1] - 1
@@ -143,7 +188,7 @@ learning_rate = 0.1
 if __name__ == "__main__":
 
     print("Testing Stochastic Gradient Descent...")
-    parameter_vector = stocastic_gradient_decent(
+    parameter_vector = stochastic_gradient_descent(
         train_data, parameter_vector, seed=1907, epochs=100
     )
     print("Converged.")
