ba437d65c86071864da358b1080981deef9c3baf
Yash Jain <yash0307jain@gmail.com>
2021-01-09 02:12:36 +0530

    correcting build bugs

--- dynamic_programming/smallest_element.py
+++ dynamic_programming/smallest_element.py
@@ -1,24 +1,25 @@
+ 
 # You have given some element with its weight in an array, you task is to find the
 # minimum possible weight element and print the weight.
 # You can combine two elements and the resultant element will be the abs
 # difference of the weight of these two elements.
 
 
-def min_element(arr: [int], minimum_value: int, count: int) -> int:
+def min_element(arr) -> int:
     """
-    >>> min_element([1, 1, 2, 6, 24, 120], 1, 0)
+    >>> min_element([1, 1, 2, 6, 24, 120])
     0
-    >>> min_element([30, 24, 10, 3, 1], 30, 0)
+    >>> min_element([30, 24, 10, 3, 1])
     0
-    >>> min_element([11, 1, 20], 11, 0)
-    1
     """
+
+    global min, count
 
     if len(arr) == 1:
-        return minimum_value
+        return min
 
-    if minimum_value == 0:
-        return minimum_value
+    if min == 0:
+        return min
 
     for i in range(0, len(arr) - 1):
         for j in range(i + 1, len(arr)):
@@ -28,23 +29,21 @@ def min_element(arr: [int], minimum_value: int, count: int) -> int:
             temp.pop(i)
             temp.append(abs(arr[i] - arr[j]))
 
-            if minimum_value > abs(arr[i] - arr[j]):
-                minimum_value = abs(arr[i] - arr[j])
+            if min > abs(arr[i] - arr[j]):
+                min = abs(arr[i] - arr[j])
 
-                if minimum_value == 0:
-                    return minimum_value
+                if min == 0:
+                    return min
 
-            min_element(temp, minimum_value, count)
+            min_element(temp)
 
-    return minimum_value
+    return min
 
 
 if __name__ == "__main__":
+    min = 0
     count = 0
-    elements = [1, 10, 2, 6, 24, 120]
-    minimum_value = elements[0]
-    min_element_value = min_element(elements, minimum_value, count)
-    elements.append(min_element_value)
-
-    answer = min(elements)
+    arr = [100, 90, 19, 88, 95]
+    min = arr[0] + 1
+    answer = min_element(arr)
     print(answer)
