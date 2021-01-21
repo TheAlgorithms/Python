2f72cadd0eb3b6287f2fc3150a8e0251dc217281
siddharth <77455890+siddharth-143@users.noreply.github.com>
2021-01-21 17:18:18 +0530

    Update abs.py

--- maths/abs.py
+++ maths/abs.py
@@ -1,28 +1,14 @@
-"""Absolute Value."""
-
+# Absolute Value :
 
 def abs_val(num):
-    """
-    Find the absolute value of a number.
-
-    >>> abs_val(-5.1)
-    5.1
-    >>> abs_val(-5) == abs_val(5)
-    True
-    >>> abs_val(0)
-    0
-    """
     return -num if num < 0 else num
 
-
 def test_abs_val():
-    """
-    >>> test_abs_val()
-    """
     assert 0 == abs_val(0)
-    assert 34 == abs_val(34)
-    assert 100000000000 == abs_val(-100000000000)
-
-
+    assert 45 == abs_val(45)
+    assert 100000000 == abs_val(-100000000)
+    
 if __name__ == "__main__":
-    print(abs_val(-34))  # --> 34
+    num = int(input("Enter a number : "))
+    print(abs_val(num))
+
