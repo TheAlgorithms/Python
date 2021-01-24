fc119e4b7da26f2d91820c396dd789a17464c27e
barklan <buzin.gb@phystech.edu>
2021-01-24 10:44:48 +0300

    Add doctest for predict function

--- machine_learning/logistic_elastic_regression.py
+++ machine_learning/logistic_elastic_regression.py
@@ -96,6 +96,14 @@ class MyElasticLogisticRegression(object):
         return sigmoid(logit(inputs_modified, self.w))
 
     def predict(self, inputs: Any, threshold: float = 0.5) -> Any:
+        """
+        >>> from sklearn.datasets import load_iris
+        >>> from sklearn.linear_model import LogisticRegression
+        >>> X, y = load_iris(return_X_y=True)
+        >>> clf = LogisticRegression(random_state=0).fit(X, y)
+        >>> clf.predict(X[:2, :])
+        array([0, 0])
+        """
         return self.predict_proba(inputs) >= threshold
 
     def get_weights(self) -> Any:
