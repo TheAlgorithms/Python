602958d40a807a361b81ef0425bc14a213069151
Ayush <scientificnationwords@gmail.com>
2021-02-06 17:26:36 +0530

    Better Code for emails_from_url.py

--- web_programming/emails_from_url.py
+++ web_programming/emails_from_url.py
@@ -1,103 +1,86 @@
-"""Get the site emails from URL."""
-__author__ = "Muhammad Umer Farooq"
-__license__ = "MIT"
-__version__ = "1.0.0"
-__maintainer__ = "Muhammad Umer Farooq"
-__email__ = "contact@muhammadumerfarooq.me"
-__status__ = "Alpha"
+"""
+Author - AyushSehrawat
+"""
 
-import re
-from html.parser import HTMLParser
-from urllib import parse
-
+# Imports the needed modules
+from bs4 import BeautifulSoup
 import requests
+import requests.exceptions
+import urllib.parse
+from collections import deque
+import re
 
-
-class Parser(HTMLParser):
-    def __init__(self, domain: str):
-        HTMLParser.__init__(self)
-        self.data = []
-        self.domain = domain
-
-    def handle_starttag(self, tag: str, attrs: str) -> None:
-        """
-        This function parse html to take takes url from tags
-        """
-        # Only parse the 'anchor' tag.
-        if tag == "a":
-            # Check the list of defined attributes.
-            for name, value in attrs:
-                # If href is defined, and not empty nor # print it.
-                if name == "href" and value != "#" and value != "":
-                    # If not already in data.
-                    if value not in self.data:
-                        url = parse.urljoin(self.domain, value)
-                        self.data.append(url)
-
-
-# Get main domain name (example.com)
-def get_domain_name(url: str) -> str:
+""" Function to get emails from the url """
+def getMails(num : int) -> None:
+    count = 0
     """
-    This function get the main domain name
-
-    >>> get_domain_name("https://a.b.c.d/e/f?g=h,i=j#k")
-    'c.d'
-    >>> get_domain_name("Not a URL!")
-    ''
+    Keep a check on range
     """
-    return ".".join(get_sub_domain_name(url).split(".")[-2:])
-
-
-# Get sub domain name (sub.example.com)
-def get_sub_domain_name(url: str) -> str:
-    """
-    >>> get_sub_domain_name("https://a.b.c.d/e/f?g=h,i=j#k")
-    'a.b.c.d'
-    >>> get_sub_domain_name("Not a URL!")
-    ''
-    """
-    return parse.urlparse(url).netloc
-
-
-def emails_from_url(url: str = "https://github.com") -> list:
-    """
-    This function takes url and return all valid urls
-    """
-    # Get the base domain from the url
-    domain = get_domain_name(url)
-
-    # Initialize the parser
-    parser = Parser(domain)
-
     try:
-        # Open URL
-        r = requests.get(url)
+        while len(urls):
+            count += 1
+            if count == num:
+                break
+            url = urls.popleft()
+            scraped_urls.add(url)
+            # Add to the set
 
-        # pass the raw HTML to the parser to get links
-        parser.feed(r.text)
+            parts = urllib.parse.urlsplit(url)
+            base_url = "{0.scheme}://{0.netloc}".format(parts)
+            # Forms a base url
 
-        # Get links and loop through
-        valid_emails = set()
-        for link in parser.data:
-            # open URL.
-            # read = requests.get(link)
+            path = url[:url.rfind('/') + 1] if '/' in parts.path else url
+
             try:
-                read = requests.get(link)
-                # Get the valid email.
-                emails = re.findall("[a-zA-Z0-9]+@" + domain, read.text)
-                # If not in list then append it.
-                for email in emails:
-                    valid_emails.add(email)
-            except ValueError:
-                pass
-    except ValueError:
-        exit(-1)
+                response = requests.get(url)
+            except (requests.exceptions.MissingSchema,
+                    requests.exceptions.ConnectionError):
+                continue
 
-    # Finally return a sorted list of email addresses with no duplicates.
-    return sorted(valid_emails)
+            new_emails = set(
+                re.findall(r"[a-zA-Z0-9+._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+",
+                        response.text, re.I))
+            # A regex search to find emails
+            emails.update(new_emails)
+
+            soup = BeautifulSoup(response.text, features='lxml')
+
+            """
+            Find links
+            """
+            for anchor in soup.find_all("a"):
+                link = anchor.attrs['href'] if 'href' in anchor.attrs else ''
+                if link.startswith('/'):
+                    link = base_url + link
+                elif not link.startswith('http'):
+                    link = path + link
+                if not link in urls and not link in scraped_urls:
+                    urls.append(link)
+
+    except KeyboardInterrupt:
+        print('[-] Closing')
 
 
-if __name__ == "__main__":
-    emails = emails_from_url("https://github.com")
-    print(f"{len(emails)} emails found:")
-    print("\n".join(sorted(emails)))
+if __name__ == '__main__':
+    user_url = "https://google.com" # Example Url
+    urls = deque([user_url])
+
+    """
+    These set store emails and prevent repeating emails 
+    """
+    scraped_urls = set()
+    emails = set()
+
+    """
+    Num = How much loops you want )
+
+    """
+    num = 10
+    getMails(num)
+
+    """
+    Finally prints the mails
+    You can also return the emails ( optional)
+    """
+    for mail in emails:
+        print(mail)
