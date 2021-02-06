df6ab5b025a18fdb4917c17c464a132718f36150
Ayush <scientificnationwords@gmail.com>
2021-02-06 19:38:43 +0530

    Formatted code

--- web_programming/emails_from_url.py
+++ web_programming/emails_from_url.py
@@ -2,16 +2,20 @@
 Author - AyushSehrawat
 """
 
-# Imports the needed modules
-from bs4 import BeautifulSoup
-import requests
-import requests.exceptions
+import re
 import urllib.parse
 from collections import deque
-import re
+
+import requests
+import requests.exceptions
+
+# Imports the needed modules
+from bs4 import BeautifulSoup
 
 """ Function to get emails from the url """
-def getMails(num : int) -> None:
+
+
+def getMails(num: int) -> None:
     count = 0
     """
     Keep a check on range
@@ -29,44 +33,50 @@ def getMails(num : int) -> None:
             base_url = "{0.scheme}://{0.netloc}".format(parts)
             # Forms a base url
 
-            path = url[:url.rfind('/') + 1] if '/' in parts.path else url
+            path = url[: url.rfind("/") + 1] if "/" in parts.path else url
 
             try:
                 response = requests.get(url)
-            except (requests.exceptions.MissingSchema,
-                    requests.exceptions.ConnectionError):
+            except (
+                requests.exceptions.MissingSchema,
+                requests.exceptions.ConnectionError,
+            ):
                 continue
 
             new_emails = set(
-                re.findall(r"[a-zA-Z0-9+._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+",
-                        response.text, re.I))
+                re.findall(
+                    r"[a-zA-Z0-9+._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+",
+                    response.text,
+                    re.I,
+                )
+            )
             # A regex search to find emails
             emails.update(new_emails)
 
-            soup = BeautifulSoup(response.text, features='lxml')
+            soup = BeautifulSoup(response.text, features="lxml")
 
             """
             Find links
             """
             for anchor in soup.find_all("a"):
-                link = anchor.attrs['href'] if 'href' in anchor.attrs else ''
-                if link.startswith('/'):
+                link = anchor.attrs["href"] if "href" in anchor.attrs else ""
+                if link.startswith("/"):
                     link = base_url + link
-                elif not link.startswith('http'):
+                elif not link.startswith("http"):
                     link = path + link
-                if not link in urls and not link in scraped_urls:
+                if not (link in urls or link in scraped_urls):
                     urls.append(link)
 
     except KeyboardInterrupt:
-        print('[-] Closing')
+        print("[-] Closing")
 
 
-if __name__ == '__main__':
-    user_url = "https://google.com" # Example Url
+if __name__ == "__main__":
+    user_url = "https://google.com"  # Example Url
     urls = deque([user_url])
 
     """
-    These set store emails and prevent repeating emails 
+    These set store emails and prevent repeating emails
     """
     scraped_urls = set()
     emails = set()
