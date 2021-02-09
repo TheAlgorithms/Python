efb3abdbb22b8e6d2102d344dd231cb9125fba54
Abdoulayegk <balde9745@outlook.com>
2021-02-09 23:00:01 +0530

    add lightgbm classifer to predict breast cancer

---
+++ machine_learning/lightgbm_classifier.py
@@ -0,0 +1,33 @@
+from sklearn.datasets import load_breast_cancer
+import lightgbm as lgb
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+import numpy as np
+
+"""
+To use lightGBMClassifier to predict the breast cancer dataset
+"""
+
+
+def model():
+    df = load_breast_cancer()
+    # selection of X and y for train the model
+    print(df.keys())
+    X, y = df.data, df.target
+    print("The number of classes:", np.unique(y))
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=1
+    )
+    classifier = lgb.LGBMClassifier()
+    classifier.fit(X_train, y_train)
+    y_pred = classifier.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    print("lightGBM model accuracy socore is:", accuracy)
+    # classification report
+    clr = classification_report(y_test, y_pred)
+    print("Classification report is:", clr)
+
+
+if __name__ == "__main__":
+    model()
