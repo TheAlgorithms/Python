22991aca84735c32b5c37878ca583e356acc9ce3
daretohack <cseanamika3@gmail.com>
2021-02-13 02:02:02 +0530

    Added a new implementation for singly linked list data structure

--- data_structures/linked_list/singly_linked_list_method2.py
+++ data_structures/linked_list/singly_linked_list_method2.py
@@ -2,26 +2,45 @@
 #NOTE:- LL means Linked List (Singly/Simple Linked List)
 
 class Node:
-	def __init__(self,data):
+	def __init__(self,data)->None:
 		self.data=data
 		self.ref=None
 
 
 class LinkedList:
-	def __init__(self):
+	def __init__(self)->None:
 		self.__head=None
 		self.__size=0
 
 
-	def isEmpty(self):
+	def is_empty(self)->bool:
+		"""
+		Returns whether the linked list is empty or not
+		>>> LL1 = LinkedList()
+		>>> LL1.is_empty()
+		True
+		>>> LL1.add_at_end("head")
+		>>> LL1.is_empty()
+		False
+		>>> LL1.add_at_begin("head")
+		>>> LL1.is_empty()
+		False
+		>>> LL1.del_at_end()
+		>>> LL1.is_empty()
+		False
+		>>> LL1.del_at_end()
+		>>> LL1.is_empty()
+		True
+		"""
 		return(self.__head==None)
 
 
-	def display(self):
+	def display(self)->None:
 		"""
 		Visualization of a Linked Lists
+		Prints the nodes of the Linked List
 		"""
-		if self.isEmpty():
+		if self.is_empty():
 			print("LL is empty.")
 		else:
 			n=self.__head
@@ -29,26 +48,26 @@ class LinkedList:
 				print(n.data,'--->',end=' ')
 				n=n.ref
 		print()
-		self.size_of_LL()
+		self.size_of_ll()
 
 
-	def size_of_LL(self):
+	def size_of_ll(self)->None:
 		"""
 		Returns length of linked list i.e, number of nodes
 		>>> LL1 = LinkedList()
-		>>> LL1.size_of_LL
+		>>> LL1.size_of_ll
 		Size of the LL: 0
 		>>> LL1.add_at_end("head")
-		>>> LL1.size_of_LL
+		>>> LL1.size_of_ll
 		Size of the LL: 1
 		>>> LL1.add_at_begin("head")
-		>>> LL1.size_of_LL
+		>>> LL1.size_of_ll
 		Size of the LL: 2
 		>>> LL1.del_at_end()
-		>>> LL1.size_of_LL
+		>>> LL1.size_of_ll
 		Size of the LL: 1
 		>>> LL1.del_at_end()
-		>>> LL1.size_of_LL
+		>>> LL1.size_of_ll
 		Size of the LL: 0
 		"""
 
@@ -57,14 +76,15 @@ class LinkedList:
 		print("Size of the LL:",self.__size)
 
 
-	def add_at_begin(self,data): #adds the given element as the head in the Linked List
+	def add_at_begin(self,data)->None: #adds the given element as the head in the Linked List
 		new_node=Node(data)
 		new_node.ref=self.__head
 		self.__head=new_node
 		self.__size+=1
 
-	def del_from_beg(self): #deletes the head of the Linked List
-		if self.isEmpty():
+	def del_from_beg(self)->None: #deletes the head of the Linked List
+
+		if self.is_empty():
 			print("Can't delete elements, LL is empty.")
 		else:
 			n=self.__head
@@ -72,8 +92,8 @@ class LinkedList:
 			n.ref=self.__head
 			self.__size-=1
 
-	def add_at_end(self,data): #adds the given element as the tail in the Linked List
-		if self.isEmpty():
+	def add_at_end(self,data)->None: #adds the given element as the tail in the Linked List
+		if self.is_empty():
 			print("The given element was not found, since the list is empty.")
 		else:
 			n=self.__head
@@ -83,8 +103,8 @@ class LinkedList:
 			n.ref=new_node
 			self.__size+=1
 
-	def del_at_end(self): #deletes the tail of the Linked List
-		if self.isEmpty():
+	def del_at_end(self)->None: #deletes the tail of the Linked List
+		if self.is_empty():
 			print("Can't delete elements, LL is empty.")
 		elif self.__head.ref==None:
 			self.__head=None
@@ -98,18 +118,18 @@ class LinkedList:
 			self.__size-=1
 
 
-	def add_after_node(self,data,x): #adds a node with the provided data just after the given node
-	#NOTE that here, 'x' is the specified node, after which a node with the given data has to be added.
-		if self.isEmpty():
-			print("Can't add after specified node",x,"Since, the LL is empty.")
+	def add_after_node(self,data,given_node)->None: #adds a node with the provided data just after the given node
+	#NOTE that here, 'given_node' is the specified node, after which a node with the given data has to be added.
+		if self.is_empty():
+			print("Can't add after specified node",given_node,"Since, the LL is empty.")
 			return
 		n=self.__head
 		while n is not None:
-			if n.data==x:
+			if n.data==given_node:
 				break
 			n=n.ref
 		if n is None:
-			print("Given node",x,"not found in LL.")
+			print("Given node",given_node,"not found in LL.")
 			return
 		elif n.ref==None: #add after the last node
 			new_node=Node(data)
@@ -121,18 +141,18 @@ class LinkedList:
 		self.__size+=1
 
 
-	def del_after_node(self,x): #deletes a node just after the given node
-	#NOTE that here, 'x' is the specified node, after which the node has to be deleted.
-		if self.isEmpty():
+	def del_after_node(self,given_node)->None: #deletes a node just after the given node
+	#NOTE that here, 'given_node' is the specified node, after which the node has to be deleted.
+		if self.is_empty():
 			print("Can't delete elements, LL is empty.")
 		else:
 			n=self.__head
 			while n.ref is not None:
-				if n.data==x:
+				if n.data==given_node:
 					break
 				n=n.ref
 			if n.ref is None:
-				print("Given node",x,"not found in the LL.")
+				print("Given node",given_node,"not found in the LL.")
 				return
 			else:
 				n.ref=n.ref.ref
@@ -140,11 +160,11 @@ class LinkedList:
 			self.__size-=1
 				
 
-	def add_before_node(self,data,x): #adds a node with the provided data just before the given node
-	#NOTE that here, 'x' is the specified node, before which a node with the given data has to be added.
-		if self.isEmpty():
+	def add_before_node(self,data,given_node)->None: #adds a node with the provided data just before the given node
+	#NOTE that here, 'given_node' is the specified node, before which a node with the given data has to be added.
+		if self.is_empty():
 			print("LL is empty, so can't perform the add_before_node method.")
-		elif self.__head.data==x: #adding before first node
+		elif self.__head.data==given_node: #adding before first node
 			new_node=Node(data)
 			new_node.ref=self.__head
 			self.__head=new_node
@@ -152,11 +172,11 @@ class LinkedList:
 		else:
 			n=self.__head
 			while n.ref is not None: #if n.ref is None, it means that we have to insert before the last node
-				if n.ref.data==x:
+				if n.ref.data==given_node:
 					break
 				n=n.ref
 			if n.ref is None:
-				print("Given node",x,"was not found in the LL")
+				print("Given node",given_node,"was not found in the LL")
 				return
 			else:
 				new_node=Node(data)
@@ -164,41 +184,41 @@ class LinkedList:
 				n.ref=new_node
 		self.__size+=1
 
-	def del_before_node(self,x): #deletes a node with the provided data just before the given node
-	#NOTE that here, 'x' is the specified node, before which the node has to be deleted.
-		if self.isEmpty():
+	def del_before_node(self,given_node)->None: #deletes a node with the provided data just before the given node
+	#NOTE that here, 'given_node' is the specified node, before which the node has to be deleted.
+		if self.is_empty():
 			print("LL is empty, so can't perform the del_before_node method.")
 			return
-		if self.__head.data==x: #deleting before the first node; not possible
+		if self.__head.data==given_node: #deleting before the first node; not possible
 			print("Can't delete anything before the FIRST node.")
 			return
 		n=self.__head
 		while n.ref.ref is not None:
-			if n.ref.data==x:
+			if n.ref.data==given_node:
 				break
 			n=n.ref
 		if n.ref is None:
-			print("Given node",x,"not present in the LL.")
+			print("Given node",given_node,"not present in the LL.")
 		else:
 			self.del_by_value(n.data)
 		self.__size-=1
 
 
-	def del_by_value(self,x): #deletes teh node whose data has been provided
-	#NOTE that here, 'x' is the specified node, whose data has to be deleted.
-		if self.isEmpty():
+	def del_by_value(self,given_node)->None: #deletes teh node whose data has been provided
+	#NOTE that here, 'given_node' is the specified node, whose data has to be deleted.
+		if self.is_empty():
 			print("LL is empty, so cannot delete anything.")
 			return
-		if self.__head.data==x: #deleting the fist node
+		if self.__head.data==given_node: #deleting the fist node
 			self.__head=self.__head.ref
 			return
 		n=self.__head
 		while n.ref is not None:
-			if n.ref.data==x:
+			if n.ref.data==given_node:
 				break
 			n=n.ref
 		if n.ref is None: #we have reached to the last node
-			print("Given node",x,"not found in the LL.")
+			print("Given node",given_node,"not found in the LL.")
 		else:
 			n.ref=n.ref.ref
 		self.__size-=1
@@ -211,7 +231,7 @@ Ofcourse you can implement your own part as well, and change data as you wouuld 
 
 
 LL1=LinkedList()
-'''LL1.add_at_begin(15)
+LL1.add_at_begin(15)
 LL1.add_at_begin(35)
 LL1.add_after_node(1500,15)
 LL1.add_after_node(3500,35)
@@ -220,8 +240,8 @@ LL1.add_at_end(456)
 LL1.add_before_node(7800,15)
 LL1.add_before_node(7811,35)
 LL1.add_before_node(7822,456)
-LL1.add_before_node(92793,6387)'''
-'''LL1.del_by_value(7811)
+LL1.add_before_node(92793,6387)
+LL1.del_by_value(7811)
 LL1.del_by_value(456)
 LL1.del_by_value(900)
 LL1.del_before_node(15)
@@ -230,10 +250,10 @@ LL1.del_after_node(7811)
 LL1.del_after_node(456)
 LL1.del_after_node(7822)
 LL1.del_from_beg()
-LL1.del_at_end()'''
-'''LL1.del_at_end()
 LL1.del_at_end()
-LL1.del_at_end()'''
+LL1.del_at_end()
+LL1.del_at_end()
+LL1.del_at_end()
 
 print("\nFrom first node:")
 LL1.display()
