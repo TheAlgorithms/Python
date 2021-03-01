e1833f6eb5fa2b86549954a787dbac12d9693965
sudip777sharma <60563255+sudip777sharma@users.noreply.github.com>
2021-03-02 04:18:28 +0530

    Update determinant_of_nxn_matrix.py

--- matrix/determinant_of_nxn_matrix.py
+++ matrix/determinant_of_nxn_matrix.py
@@ -10,11 +10,17 @@
     -3.0
     >>> determinant_of_nxn_matrix([[1, 2, 3], [4, 5, 6], [7, 8, 4]])
     15.0
-    >>> determinant_of_nxn_matrix([[1, 2, 3, 7], [4, 5, 6, 6], [7, 8, 1, 5], [1, 2, 3, 4]])
+    >>> determinant_of_nxn_matrix([[1, 2, 3, 7], [4, 5, 6, 6],
+    [7, 8, 1, 5], [1, 2, 3, 4]])
     -72.0
-    >>> determinant_of_nxn_matrix([[1, 2, 3, 7, 13], [4, 5, 6, 6, 90], [7, 8, 1, 5, 76], [1, 2, 3, 4, 12], [9, 6, 3, 7, 4]])
+    >>> determinant_of_nxn_matrix([[1, 2, 3, 7, 13], [4, 5, 6, 6, 90],
+    [7, 8, 1, 5, 76],
+    [1, 2, 3, 4, 12], [9, 6, 3, 7, 4]])
     19848.0
-    >>> determinant_of_nxn_matrix([[1, 2,  3, 7,  13, 23], [4, 44, 6, 6,  90, 12], [7, 8,  1, 5,  6,  98], [1, 2,  3, 4,  12, 4], [9, 6,  3, 7,  4,  9], [2, 47, 8, 91, 36, 9]])
+    >>> determinant_of_nxn_matrix([[1, 2,  3, 7,  13, 23],
+    [4, 44, 6, 6,  90, 12],
+    [7, 8,  1, 5,  6,  98], [1, 2,  3, 4,  12, 4],
+    [9, 6,  3, 7,  4,  9], [2, 47, 8, 91, 36, 9]])
     -20981553.999999993
 """
 from __future__ import annotations
@@ -22,9 +28,9 @@ from __future__ import annotations
 from copy import deepcopy
 
 
-def determinant_of_nxn_matrix(matrix: list[list[float]]) -> float:
-    N = int(len(matrix))
-    matrix_copy = deepcopy(matrix)
+def determinant_of_nxn_matrix(a: list[list[float]]) -> float:
+    N = int(len(a))
+    a_copy = deepcopy(a)
     n = 0
     m = 1
     c1 = 1
@@ -32,13 +38,13 @@ def determinant_of_nxn_matrix(matrix: list[list[float]]) -> float:
     for k in range(1, (2 * (N - 1)) + 1):
         for i in range(c1, N):
             for j in range(0, N):
-                if (matrix[c1 - 1][c2]) != 0:
-                    matrix_copy[i][j] = matrix[i][j] - (
-                        (matrix[m][n]) * ((matrix[c2][j]) / (matrix[c1 - 1][c2]))
+                if (a[c1 - 1][c2]) != 0:
+                    a_copy[i][j] = a[i][j] - (
+                        (a[m][n]) * ((a[c2][j]) / (a[c1 - 1][c2]))
                     )
             m = m + 1
 
-        matrix = deepcopy(matrix_copy)
+        a = deepcopy(a_copy)
         n = n + 1
         m = n + 1
         c1 = c1 + 1
@@ -48,6 +54,6 @@ def determinant_of_nxn_matrix(matrix: list[list[float]]) -> float:
     for i in range(0, N):
         for j in range(0, N):
             if i == j:
-                determinant = determinant * matrix[i][j]
+                determinant = determinant * a[i][j]
 
     return determinant
