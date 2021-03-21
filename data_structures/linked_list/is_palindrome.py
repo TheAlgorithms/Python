143837689997b72af24408d07ba732194e44ecd3
Yordan Grigorov <yoreei.grigorov@gmail.com>
2021-03-21 18:43:07 +0100

    Revert "Added documentation and tests to is_palindrome.py"
    This reverts commit d7e819d26b48fa11f89471ade384378fa901162a.

--- data_structures/linked_list/is_palindrome.py
+++ data_structures/linked_list/is_palindrome.py
@@ -1,123 +1,64 @@
-"""
-"A palindrome is a word, number, phrase, or other sequence of characters
-which reads the same backward as forward, such as madam or racecar"
-https://en.wikipedia.org/wiki/Palindrome
-"""
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-def traverse(head):
-    if head.next is None:
-        return head.data
-    else:
-        return head.data+traverse(head.next)
-
-def reverse(head):
-    """
-    Reverses the linked list IN PLACE and returns the new head
-    >>> traverse(reverse(str2sll('abc')))=='cba'
-    True
-    >>> traverse(reverse(str2sll('0')))=='0'
-    True
-    """
-    prev = None
-    next_node=head
-
-    # (1 -> 2 -> None) becomes (None <- 1 <- 2); return 2
-    while current:= next_node:
-        next_node = current.next
-        current.next = prev
-        prev = current
-    # Return prev in order to put the head at the end
-    return prev
-
-def midpoint(head):
-    """
-    Finds the midpoint in N/2 iterations. That's still O(N)
-    >>> l=str2sll('1234'); midpoint(l).data
-    '3'
-    >>> l=str2sll('12345'); midpoint(l).data
-    '3'
-    >>> l=str2sll('1'); midpoint(l).data
-    '1'
-    """
-    slow = fast = head
-    while fast and fast.next:
-        fast, slow = fast.next.next, slow.next
-    return slow
-
 def is_palindrome(head):
-    """
-    >>> l=str2sll('abccba'); is_palindrome(l)
-    True
-    >>> l=str2sll('abcba'); is_palindrome(l)
-    True
-    >>> l=str2sll('abc'); is_palindrome(l)
-    False
-    """
     if not head:
         return True
-
-    second=midpoint(head)
-
-    second_rev = reverse(second)
+    # split the list to two parts
+    fast, slow = head.next, head
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+    second = slow.next
+    slow.next = None  # Don't forget here! But forget still works!
+    # reverse the second part
+    node = None
+    while second:
+        nxt = second.next
+        second.next = node
+        node = second
+        second = nxt
     # compare two parts
-    # len(second_rev)*2==len(head) or len(second_rev)*2 -1==len(head)
-    while second_rev:
-        if second_rev.data != head.data:
+    # second part has the same or one less node
+    while node:
+        if node.val != head.val:
             return False
-        second_rev = second_rev.next
+        node = node.next
         head = head.next
     return True
 
+
 def is_palindrome_stack(head):
-    """
-    >>> l=str2sll('abccba'); is_palindrome_stack(l)
-    True
-    >>> l=str2sll('abcba'); is_palindrome_stack(l)
-    True
-    >>> l=str2sll('abc'); is_palindrome_stack(l)
-    False
-    """
     if not head or not head.next:
         return True
 
-    second = midpoint(head)
+    # 1. Get the midpoint (slow)
+    slow = fast = cur = head
+    while fast and fast.next:
+        fast, slow = fast.next.next, slow.next
 
-    # Pushing the second half to a stack is equivalent to a reverse()
-    stack = [second.data]
-    while second.next:
-        second = second.next
-        stack.append(second.data)
+    # 2. Push the second half into the stack
+    stack = [slow.val]
+    while slow.next:
+        slow = slow.next
+        stack.append(slow.val)
 
-    # Comparison
+    # 3. Comparison
     while stack:
-        if stack.pop() != head.data:
+        if stack.pop() != cur.val:
             return False
-        head = head.next
+        cur = cur.next
 
     return True
 
 
 def is_palindrome_dict(head):
-    """
-    >>> l=str2sll('aaabb'); is_palindrome_dict(l)
-    False
-    >>> l=str2sll('abccba'); is_palindrome_dict(l)
-    True
-    >>> l=str2sll('abcba'); is_palindrome_dict(l)
-    True
-    >>> l=str2sll('abc'); is_palindrome_dict(l)
-    False
-    """
     if not head or not head.next:
         return True
     d = {}
     pos = 0
     while head:
-        d.setdefault(head.data, []).append(pos)
+        if head.val in d.keys():
+            d[head.val].append(pos)
+        else:
+            d[head.val] = [pos]
         head = head.next
         pos += 1
     checksum = pos - 1
@@ -126,31 +67,11 @@ def is_palindrome_dict(head):
         if len(v) % 2 != 0:
             middle += 1
         else:
-            for step, pos in enumerate(v):
-                if pos + v[len(v) - 1 - step] != checksum:
+            step = 0
+            for i in range(0, len(v)):
+                if v[i] + v[len(v) - 1 - step] != checksum:
                     return False
+                step += 1
         if middle > 1:
             return False
     return True
-
-
-def str2sll(input: str) -> Node:
-    """
-    We assume that len(input)>=1
-    >>> traverse(str2sll('abccba'))=='abccba'
-    True
-    >>> traverse(str2sll('0'))=='1'
-    False
-    """
-    head = Node(input[0])
-    cursor = head
-    for char in input[1:]:
-        cursor.next = Node(char)
-        cursor = cursor.next
-    return head
-
-
-if __name__ == "__main__":
-    from doctest import testmod
-
-    testmod()
