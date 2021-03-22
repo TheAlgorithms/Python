a671f542822b0ccc9841084371fc164fa386da96
Yordan Grigorov <yoreei.grigorov@gmail.com>
2021-03-22 20:32:15 +0100

    added benchmark to quantify optimization

--- data_structures/linked_list/has_loop.py
+++ data_structures/linked_list/has_loop.py
@@ -1,8 +1,17 @@
+import timeit
 from typing import Any
 
 
 class ContainsLoopError(Exception):
     pass
+
+
+class ListWithAdd(list):
+    def add(self, *args, **kwargs):
+        return self.append(*args, **kwargs)
+
+
+SETORLIST = set
 
 
 class Node:
@@ -12,7 +21,7 @@ class Node:
 
     def __iter__(self):
         node = self
-        visited = set()
+        visited = SETORLIST()
         while node:
             if node in visited:  # set().__contains__ is O(1)
                 raise ContainsLoopError
@@ -24,14 +33,14 @@ class Node:
     def has_loop(self) -> bool:
         """
         A loop is when the exact same Node appears more than once in a linked list.
-        >>> root_node = Node(1)
-        >>> root_node.next_node = Node(2)
-        >>> root_node.next_node.next_node = Node(3)
-        >>> root_node.next_node.next_node.next_node = Node(4)
-        >>> root_node.has_loop
+        >>> root = Node(1)
+        >>> root.next_node = Node(2)
+        >>> root.next_node.next_node = Node(3)
+        >>> root.next_node.next_node.next_node = Node(4)
+        >>> root.has_loop
         False
-        >>> root_node.next_node.next_node.next_node = root_node.next_node
-        >>> root_node.has_loop
+        >>> root.next_node.next_node.next_node = root.next_node
+        >>> root.has_loop
         True
         """
         try:
@@ -42,19 +51,43 @@ class Node:
 
 
 if __name__ == "__main__":
-    root_node = Node(1)
-    root_node.next_node = Node(2)
-    root_node.next_node.next_node = Node(3)
-    root_node.next_node.next_node.next_node = Node(4)
-    print(root_node.has_loop)  # False
-    root_node.next_node.next_node.next_node = root_node.next_node
-    print(root_node.has_loop)  # True
+    root = Node(1)
+    root.next_node = Node(2)
+    root.next_node.next_node = Node(3)
+    root.next_node.next_node.next_node = Node(4)
+    print(root.has_loop)  # False
+    root.next_node.next_node.next_node = root.next_node
+    print(root.has_loop)  # True
 
-    root_node = Node(5)
-    root_node.next_node = Node(6)
-    root_node.next_node.next_node = Node(5)
-    root_node.next_node.next_node.next_node = Node(6)
-    print(root_node.has_loop)  # False
+    root = Node(5)
+    root.next_node = Node(6)
+    root.next_node.next_node = Node(5)
+    root.next_node.next_node.next_node = Node(6)
+    print(root.has_loop)  # False
 
-    root_node = Node(1)
-    print(root_node.has_loop)  # False
+    root = Node(1)
+    print(root.has_loop)  # False
+
+
+    # Performance comparison between using a set or a list
+    def bench_f() -> bool:
+        return root.has_loop
+
+
+    for exp in range(1, 4 + 1):
+        root = Node(1)
+        for i in range(10 ** exp):
+            new_node = Node(i)
+            new_node.next_node = root
+            root = new_node
+
+        SETORLIST = set
+        time_set: float = timeit.timeit(bench_f, number=5)
+        SETORLIST = ListWithAdd
+        time_list: float = timeit.timeit(bench_f, number=5)
+
+        print(f'{time_set=}, {time_list=}, {time_set<time_list=}')
+        # time_set = 2.269999999993111e-05, time_list = 3.430000000004263e-05, time_set < time_list = True
+        # time_set = 0.00012579999999995373, time_list = 0.0004275000000000251, time_set < time_list = True
+        # time_set = 0.0008853999999999251, time_list = 0.033791399999999916, time_set < time_list = True
+        # time_set = 0.014864699999999953, time_list = 2.8534654, time_set < time_list = True
