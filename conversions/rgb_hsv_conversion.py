b5ce71a5bd624b475c7a31d5a53b80779e78bca6
algobytewise <algobytewise@gmail.com>
2021-04-15 15:46:21 +0530

    snake-case

--- conversions/rgb_hsv_conversion.py
+++ conversions/rgb_hsv_conversion.py
@@ -49,27 +49,27 @@ def hsv_to_rgb(hue: float, saturation: float, value: float) -> list[int]:
         raise Exception("value should be between 0 and 1")
 
     chroma = value * saturation
-    hueSection = hue / 60
-    second_largest_component = chroma * (1 - abs(hueSection % 2 - 1))
+    hue_section = hue / 60
+    second_largest_component = chroma * (1 - abs(hue_section % 2 - 1))
     match_value = value - chroma
 
-    if hueSection >= 0 and hueSection <= 1:
+    if hue_section >= 0 and hue_section <= 1:
         red = round(255 * (chroma + match_value))
         green = round(255 * (second_largest_component + match_value))
         blue = round(255 * (match_value))
-    elif hueSection > 1 and hueSection <= 2:
+    elif hue_section > 1 and hue_section <= 2:
         red = round(255 * (second_largest_component + match_value))
         green = round(255 * (chroma + match_value))
         blue = round(255 * (match_value))
-    elif hueSection > 2 and hueSection <= 3:
+    elif hue_section > 2 and hue_section <= 3:
         red = round(255 * (match_value))
         green = round(255 * (chroma + match_value))
         blue = round(255 * (second_largest_component + match_value))
-    elif hueSection > 3 and hueSection <= 4:
+    elif hue_section > 3 and hue_section <= 4:
         red = round(255 * (match_value))
         green = round(255 * (second_largest_component + match_value))
         blue = round(255 * (chroma + match_value))
-    elif hueSection > 4 and hueSection <= 5:
+    elif hue_section > 4 and hue_section <= 5:
         red = round(255 * (second_largest_component + match_value))
         green = round(255 * (match_value))
         blue = round(255 * (chroma + match_value))
