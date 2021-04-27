7981c4a99961fd6df86e2a16b299ef8b96c017fb
9harshit <43147421+9harshit@users.noreply.github.com>
2021-04-27 17:38:46 +0530

    Update cnn_classification.py

--- computer_vision/cnn_classification/cnn_classification.py
+++ computer_vision/cnn_classification/cnn_classification.py
@@ -61,10 +61,6 @@ train_datagen = ImageDataGenerator(
     rescale=1.0 / 255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True
 )
 
-train_datagen = ImageDataGenerator(
-    rescale=1.0 / 255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True
-)
-
 test_datagen = ImageDataGenerator(rescale=1.0 / 255)
 
 training_set = train_datagen.flow_from_directory(
