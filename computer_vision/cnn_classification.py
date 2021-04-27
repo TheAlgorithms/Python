96a770e7f9b295b23d9ecd47e04cb534e1e45ec7
9harshit <43147421+9harshit@users.noreply.github.com>
2021-04-27 17:10:22 +0530

    added dataset link

--- computer_vision/cnn_classification.py
+++ computer_vision/cnn_classification.py
@@ -1,6 +1,7 @@
 # Convolutional Neural Network
 
 # Resources : https://en.wikipedia.org/wiki/Convolutional_neural_network
+# Download dataset from : https://lhncbc.nlm.nih.gov/LHC-publications/pubs/TuberculosisChestXrayImageDataSets.html
 
 # Installing Tensorflow
 # pip install tensorflow
@@ -46,51 +47,4 @@ classifier.add(Dense(units=1, activation="sigmoid"))
 classifier.compile(optimizer="adam", loss="binary_crossentropy",
                    metrics=["accuracy"])
 
-# Part 2 - Fitting the CNN to the images
-
-
-# Load Trained model weights
-
-# from keras.models import load_model
-# regressor=load_model('cnn.h5')
-
-
-train_datagen = ImageDataGenerator(
-    rescale=1.0 / 255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True
-)
-
-train_datagen = ImageDataGenerator(
-    rescale=1.0 / 255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True
-)
-
-test_datagen = ImageDataGenerator(rescale=1.0 / 255)
-
-training_set = train_datagen.flow_from_directory(
-    "dataset/training_set", target_size=(64, 64), batch_size=32,
-    class_mode="binary"
-)
-
-test_set = test_datagen.flow_from_directory(
-    "dataset/test_set", target_size=(64, 64), batch_size=32,
-    class_mode="binary"
-)
-
-classifier.fit_generator(
-    training_set, steps_per_epoch=5, epochs=30, validation_data=test_set
-)
-# classifier.save("cnn.h5")
-
-# Part 3 - Making new predictions
-
-
-test_image = image.load_img("dataset/single_prediction/image.png",
-                            target_size=(64, 64))
-test_image = image.load_img(image, target_size=(64, 64))
-test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis=0)
-result = classifier.predict(test_image)
-training_set.class_indices
-if result[0][0] == 0:
-    prediction = "Normal"
-if result[0][0] == 1:
-    prediction = "Abnormality detected"
+# Part 
