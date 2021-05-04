9af688e79ea9c7e7b356a535debc5fe7dd18b655
sudip777sharma <60563255+sudip777sharma@users.noreply.github.com>
2021-05-04 19:50:39 +0530

    Update determinant_of_nxn_matrix.py
    descriptive name of list[list[]] as matrix

--- matrix/determinant_of_nxn_matrix.py
+++ matrix/determinant_of_nxn_matrix.py
@@ -7,7 +7,7 @@ from __future__ import annotations
 from copy import deepcopy
 
 
-def determinant_of_nxn_matrix(a: list[list[float]]) -> float:
+def determinant_of_nxn_matrix(matrix: list[list[float]]) -> float:
     """>>> determinant_of_nxn_matrix([[10, 5], [3, 2.5]])
         10.0
         >>> determinant_of_nxn_matrix([[1, 2, 3], [4, 5, 6], [7, 8, 10]])
@@ -27,31 +27,31 @@ def determinant_of_nxn_matrix(a: list[list[float]]) -> float:
         [9, 6,  3, 7,  4,  9], [2, 47, 8, 91, 36, 9]])
         -20981553.999999993"""
     
-    N = int(len(a))
-    a_copy = deepcopy(a)
+    size = int(len(matrix))
+    matrix_copy = deepcopy(matrix)
     n = 0
     m = 1
     c1 = 1
     c2 = 0
-    for k in range(1, (2 * (N - 1)) + 1):
-        for i in range(c1, N):
-            for j in range(0, N):
-                if (a[c1 - 1][c2]) != 0:
-                    a_copy[i][j] = a[i][j] - (
-                        (a[m][n]) * ((a[c2][j]) / (a[c1 - 1][c2]))
+    for k in range(1, (2 * (size - 1)) + 1):
+        for i in range(c1, size):
+            for j in range(0, size):
+                if (matrix[c1 - 1][c2]) != 0:
+                    matrix_copy[i][j] = matrix[i][j] - (
+                        (matrix[m][n]) * ((matrix[c2][j]) / (matrix[c1 - 1][c2]))
                     )
             m = m + 1
 
-        a = deepcopy(a_copy)
+        matrix = deepcopy(matrix_copy)
         n = n + 1
         m = n + 1
         c1 = c1 + 1
         c2 = c2 + 1
 
     determinant = float(1)
-    for i in range(0, N):
-        for j in range(0, N):
+    for i in range(0, size):
+        for j in range(0, size):
             if i == j:
-                determinant = determinant * a[i][j]
+                determinant = determinant * matrix[i][j]
 
     return determinant
