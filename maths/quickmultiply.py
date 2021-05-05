4bd0fb845d1deda2236b830d18f7599c7e0a9938
Rohit Barua <83600150+Rohit-beep-droid@users.noreply.github.com>
2021-05-05 12:28:21 -0400

    Create quickmultiply.py

---
+++ maths/quickmultiply.py
@@ -0,0 +1,32 @@
+"""Mutiplication of two integers."""
+
+"""
+Find the mutiplication of two integers
+
+>>> quick_multiply(-3,3)
+-9
+>>> quick_multiply(-100,-2)
+200
+>>> quick_multiply(0,0)
+0
+"""
+
+
+def quick_multiply(num: int, num2: int) -> int:
+  return num*num2
+
+
+def test_quick_multiply()
+"""
+>>> test_quick_multiply()
+"""
+assert -25 == test_quick_mutiply(-5,5)
+assert 1000 == test_quick_mutiply(-10,-10)
+assert 0 == test_quick_multiply(0,1000)
+assert 81 == test_quick_multiply(9,9)
+
+
+if __name__ == "__main__":
+  print(quick_multiply(5,6)) # ---> 30
+  print(quick_multiply(-1,10)) # ---> -10
+  print(quick_multiply(-1,-10)) # ---> 10
