1a6b40e6fbc1cfd6bbbf097288f1795da882d5fb
MR. TEJAS DIXIT <tejasdixit17@gmail.com>
2021-05-12 00:58:47 +0530

    Code is updated with doctest

--- searches/linear_search_v2.py
+++ searches/linear_search_v2.py
@@ -1,33 +1,14 @@
-import os 
-os.system("clear")
-def lsearch(array:list, search_element:any)->None:
-	print("\nMethod:linear search")
-	print("You entered:  ",list(array),type(array))
-	print("Search Element is:  ",search_element,type(search_element))
-	print("Searching started ")
-	print("Checking..")
-	i = 0
-	for i in range(len(array)):
-		if array[i] == search_element:
-			
-			print("Element is Found at",  i,"th index and",  i+1,  "th position")
-			
-			print("Completed chacking. ")
-			
-			print("Searching ended")
-			break
-		else:
-			pass
-			if i==len(array) -1:
-				print("Element not found ")
-				print("Completed chacking.")
-				print("Searching ended")
-		i = i+1
-
-
-l1 = lsearch([1,2,3,4,5,6,7],'tejas')
-l2 = lsearch(['tejas','kunal','vignesh','datta','saif'],5)
-l3 = lsearch(['appale',1,2,3,'mango'],'mango')
+def lsearch(array:list, key:any)-> any:
+    size =len(array)
+    found = False
+    i= 0
+    for i in range(size):
+        if array[i] == key:
+            print("Element found at ",i)
+        else :
+            if i == size-1:
+                return f"Not found"
+        i = i+1        
 if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+    import doctest 
+    assert lsearch([45,90,1,24],1)
