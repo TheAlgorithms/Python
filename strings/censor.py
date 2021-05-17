e7d4102bbb5a90ea144f9a5880a92722dc5cd20e
Margaret <62753112+meg-1@users.noreply.github.com>
2021-05-17 17:40:48 +0300

    Added comments to the code

--- strings/censor.py
+++ strings/censor.py
@@ -1,10 +1,14 @@
 txt = input("Please enter the text from which you would like to censor a certain word: \n")
 word = input("Please enter the word you would like to be censored from the text: \n")
 txt_lst = txt.split()
+
+#here we loop through a list which consists of words from the text we should censor
 for word1 in txt_lst:
   if word1 == word:
     txt_lst[txt_lst.index(word1)] = "*" * len(word1)
 txt = ""
+
+#here we combine all of the split words in txt_lst into one string
 for word1 in txt_lst:
   txt += word1 + " "
 txt = txt[:-1]
