e794056c2b95f077bc535735911a915519ee9c16
TANMAY SRIVASTAVA <77936821+ktsrivastava29@users.noreply.github.com>
2021-05-18 13:27:18 +0530

    Added WhiteSpace around operators.

--- conversions/hex-bin.py
+++ conversions/hex-bin.py
@@ -1,4 +1,4 @@
-#convert hexadecimal into binary
+#  CONVERT HEXADECIMAL TO BINARY
 from doctest import testmod
 import math
 
@@ -7,6 +7,14 @@ def convert(num: str) -> str:
     """
     Convert a hexadecimal value to its decimal equivalent
     #https://stackoverflow.com/questions/1425493/convert-hex-to-binary
+    Here, we have used " >> " bitwise operator
+    Bitwise right shift: 
+    Shifts the bits of the number to the right and fills 0 on voids left as a result. 
+    Similar effect as of dividing the number with some power of two.
+    Example: 
+    a = 10
+    a >> 1 = 5 
+    
     >>> convert("AC")
     10101100
     >>> convert("9A4")
@@ -26,38 +34,31 @@ def convert(num: str) -> str:
     
     """
 
-    bin_str : str=""
+    bin_str :str=""
     flag :bool = 0
     hex_str :str = num.strip()
     
     if not hex_str:
-        raise ValueError("Empty string was passed to the function")
+        return False
+    
     is_negative : bool= hex_str[0] == "-"
      
     if is_negative:
         flag = 1
         hex_string = hex_str[1:]
     
-    num2 :int = int(hex_str, 16)
-
-    """
-    Here, we have used " >> " bitwise operator
-    Bitwise right shift: 
-    Shifts the bits of the number to the right and fills 0 on voids left as a result. 
-    Similar effect as of dividing the number with some power of two.
-    Example: 
-    a = 10
-    a >> 1 = 5 
-
-    """
+    try:
+        num2 :int = int(hex_str, 16)
+    except ValueError:
+        return False
     
-    while num2>0:
-        num3 :str = str(num2%2)
+    while num2 > 0:
+        num3 :str = str(num2 % 2)
         bin_str = num3 + bin_str
-        num2=num2>>1
+        num2 = num2 >> 1
 
     if flag: 
-        return "-"+bin_str
+        return "-" + bin_str
     else:
         return bin_str
         
