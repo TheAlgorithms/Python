d022cfa5317aeedc49fabca8d50cc637354e8f9e
Benjamin Fein <benaaronfein@gmail.com>
2021-05-28 19:53:13 -0400

    Fixes naming conventions

--- sorts/recursive_mergesort_array.py
+++ sorts/recursive_mergesort_array.py
@@ -1,5 +1,4 @@
-## Documentation: https://en.wikipedia.org/wiki/Merge_sort
-def merge(arr):
+def merge(arr: list[int]) -> list[int]:
 	"""Return a sorted array.
 	>>> merge([10,9,8,7,6,5,4,3,2,1])
 	[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -9,30 +8,30 @@ def merge(arr):
 	[1, 2, 3, 9, 10, 15, 22, 23]
 	"""
 	if(len(arr) > 1):
-		middle = len(arr) // 2
-		left = arr[:middle]
-		right = arr[middle:]
-		leftSize = len(left)
-		rightSize = len(right)
-		l = merge(left)
-		r = merge(right)
+		middle_length = len(arr) // 2
+		left_array = arr[:middle_length]
+		right_array = arr[middle_length:]
+		left_size = len(left_array)
+		right_size = len(right_array)
+		l = merge(left_array)
+		r = merge(right_array)
 		i = 0
 		j = 0
 		k = 0
-		while(i < leftSize and j < rightSize):
-			if(left[i] < right[j]):
-				arr[k] = left[i]
+		while(i < left_size and j < right_size):
+			if(left_array[i] < right_array[j]):
+				arr[k] = left_array[i]
 				i = i + 1
 			else:
-				arr[k] = right[j]
+				arr[k] = right_array[j]
 				j = j + 1
 			k = k + 1
-		while(i < leftSize):
-				arr[k] = left[i]
+		while(i < left_size):
+				arr[k] = left_array[i]
 				i = i + 1
 				k = k + 1
-		while(j < rightSize):
-				arr[k] = right[j]
+		while(j < right_size):
+				arr[k] = right_array[j]
 				j = j + 1
 				k = k + 1
 	return arr
