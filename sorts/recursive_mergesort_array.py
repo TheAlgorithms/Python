751e9b8aeb86280b77d9f86e81d83303638f4067
Benjamin Fein <benfein78@icloud.com>
2021-05-29 13:33:41 -0400

    Fixes variable names and adds documentation

--- sorts/recursive_mergesort_array.py
+++ sorts/recursive_mergesort_array.py
@@ -1,3 +1,8 @@
+"""A merge sort which accepts an array as input and recursively splits an array in half and sorts and combines them. """
+
+"""https://en.wikipedia.org/wiki/Merge_sort """
+
+
 def merge(arr: list[int]) -> list[int]:
     """Return a sorted array.
     >>> merge([10,9,8,7,6,5,4,3,2,1])
@@ -8,32 +13,42 @@ def merge(arr: list[int]) -> list[int]:
     [1, 2, 3, 9, 10, 15, 22, 23]
     """
     if len(arr) > 1:
-        middle_length = len(arr) // 2
-        left_array = arr[:middle_length]
-        right_array = arr[middle_length:]
+        middle_length = len(arr) // 2  ##Finds the middle of the array
+        left_array = arr[
+            :middle_length
+        ]  ##Creates an array of the elements in the first half.
+        right_array = arr[
+            middle_length:
+        ]  ##Creates an array of the elements in the second half.
         left_size = len(left_array)
         right_size = len(right_array)
-        merge(left_array)
-        merge(right_array)
-        i = 0
-        j = 0
-        k = 0
-        while i < left_size and j < right_size:
-            if left_array[i] < right_array[j]:
-                arr[k] = left_array[i]
-                i = i + 1
+        merge(left_array)  ## Starts sorting the left.
+        merge(right_array)  ##Starts sorting the right
+        leftIndex = 0  ##Left Counter
+        rightIndex = 0  ## Right Counter
+        index = 0  ## Position Counter
+        while (
+            leftIndex < left_size and rightIndex < right_size
+        ):  ##Runs until the lowers size of the left and right are sorted.
+            if left_array[leftIndex] < right_array[rightIndex]:
+                arr[index] = left_array[leftIndex]
+                leftIndex = leftIndex + 1
             else:
-                arr[k] = right_array[j]
-                j = j + 1
-            k = k + 1
-        while i < left_size:
-            arr[k] = left_array[i]
-            i = i + 1
-            k = k + 1
-        while j < right_size:
-            arr[k] = right_array[j]
-            j = j + 1
-            k = k + 1
+                arr[index] = right_array[rightIndex]
+                rightIndex = rightIndex + 1
+            index = index + 1
+        while (
+            leftIndex < left_size
+        ):  ##Adds the left over elements in the left half of the array
+            arr[index] = left_array[leftIndex]
+            leftIndex = leftIndex + 1
+            index = index + 1
+        while (
+            rightIndex < right_size
+        ):  ##Adds the left over elements in the right half of the array
+            arr[index] = right_array[rightIndex]
+            rightIndex = rightIndex + 1
+            index = index + 1
     return arr
 
 
