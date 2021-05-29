f087db3dae696bb830fb9dddf8f412b5ee1c1d62
Benjamin Fein <benfein78@icloud.com>
2021-05-29 13:46:26 -0400

    Fixes variable names to use snake_case.

--- sorts/recursive_mergesort_array.py
+++ sorts/recursive_mergesort_array.py
@@ -11,6 +11,10 @@ def merge(arr: list[int]) -> list[int]:
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     >>> merge([10,22,1,2,3,9,15,23])
     [1, 2, 3, 9, 10, 15, 22, 23]
+    >>> merge([100])
+    [100]
+    >>> merge([])
+    []
     """
     if len(arr) > 1:
         middle_length = len(arr) // 2  ##Finds the middle of the array
@@ -24,30 +28,30 @@ def merge(arr: list[int]) -> list[int]:
         right_size = len(right_array)
         merge(left_array)  ## Starts sorting the left.
         merge(right_array)  ##Starts sorting the right
-        leftIndex = 0  ##Left Counter
-        rightIndex = 0  ## Right Counter
+        left_index = 0  ##Left Counter
+        right_index = 0  ## Right Counter
         index = 0  ## Position Counter
         while (
-            leftIndex < left_size and rightIndex < right_size
+            left_index < left_size and right_index < right_size
         ):  ##Runs until the lowers size of the left and right are sorted.
-            if left_array[leftIndex] < right_array[rightIndex]:
-                arr[index] = left_array[leftIndex]
-                leftIndex = leftIndex + 1
+            if left_array[left_index] < right_array[right_index]:
+                arr[index] = left_array[left_index]
+                left_index = left_index + 1
             else:
-                arr[index] = right_array[rightIndex]
-                rightIndex = rightIndex + 1
+                arr[index] = right_array[right_index]
+                right_index = right_index + 1
             index = index + 1
         while (
-            leftIndex < left_size
+            left_index < left_size
         ):  ##Adds the left over elements in the left half of the array
-            arr[index] = left_array[leftIndex]
-            leftIndex = leftIndex + 1
+            arr[index] = left_array[left_index]
+            left_index = left_index + 1
             index = index + 1
         while (
-            rightIndex < right_size
+            right_index < right_size
         ):  ##Adds the left over elements in the right half of the array
-            arr[index] = right_array[rightIndex]
-            rightIndex = rightIndex + 1
+            arr[index] = right_array[right_index]
+            right_index = right_index + 1
             index = index + 1
     return arr
 
