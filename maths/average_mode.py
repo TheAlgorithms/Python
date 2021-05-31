fe6ca24cbd124e1aba15feefa2b9405a4df1d7da
Grigoriy Hanin <43445998+haningrisha@users.noreply.github.com>
2021-05-31 11:30:03 +0300

    Suggestions resolved
    * output typing changed to Any
    * test cases added

--- maths/average_mode.py
+++ maths/average_mode.py
@@ -1,7 +1,8 @@
 import statistics
+from typing import Any
 
 
-def mode(input_list: list) -> int:  # Defining function "mode."
+def mode(input_list: list) -> Any:  # Defining function "mode."
     """This function returns the mode(Mode as in the measures of
     central tendency) of the input data.
 
@@ -10,21 +11,28 @@ def mode(input_list: list) -> int:  # Defining function "mode."
     >>> input_list = [2, 3, 4, 5, 3, 4, 2, 5, 2, 2, 4, 2, 2, 2]
     >>> mode(input_list)
     2
+    >>> input_list = [3, 4, 5, 3, 4, 2, 5, 2, 2, 4, 4, 2, 2, 2]
+    >>> mode(input_list)
+    2
+    >>> input_list = ["x", "y", "y", "z"]
+    >>> mode(input_list) 
+    'y'
     >>> input_list = [2, 3, 4, 5, 3, 4, 2, 5, 2, 2, 4, 2, 2, 2]
+    >>> mode(input_list) == statistics.mode(input_list)
+    True
+    >>> input_list = ["x", "y", "y", "z"]
     >>> mode(input_list) == statistics.mode(input_list)
     True
     """
     # Copying input_list to check with the index number later.
-    check_list = input_list.copy()
     result = list()  # Empty list to store the counts of elements in input_list
     for x in input_list:
         result.append(input_list.count(x))
     y = max(result)  # Gets the maximum value in the result list.
     # Returns the value with the maximum number of repetitions.
-    return check_list[result.index(y)]
+    return input_list[result.index(y)]
 
 
 if __name__ == "__main__":
-    data = [2, 3, 4, 5, 3, 4, 2, 5, 2, 2, 4, 2, 2, 2]
-    print(mode(data))
-    print(statistics.mode(data))
+    import doctest
+    doctest.testmod()
