c036083d1eb6de64b7e9ea6b6d61de218963726c
Grigoriy Hanin <43445998+haningrisha@users.noreply.github.com>
2021-06-05 20:06:31 +0300

    Average mean refactor
    Added doctests and type hints to average_mean

--- maths/average_mean.py
+++ maths/average_mean.py
@@ -1,20 +1,18 @@
-"""Find mean of a list of numbers."""
+def mean(nums: list) -> float:
+    """
+    Find mean of a list of numbers.
 
-
-def average(nums):
-    """Find mean of a list of numbers."""
+    >>> mean([3, 6, 9, 12, 15, 18, 21])
+    12.0
+    >>> mean([5, 10, 15, 20, 25, 30, 35])
+    20.0
+    >>> mean([1, 2, 3, 4, 5, 6, 7, 8])
+    4.5
+    """
     return sum(nums) / len(nums)
 
 
-def test_average():
-    """
-    >>> test_average()
-    """
-    assert 12.0 == average([3, 6, 9, 12, 15, 18, 21])
-    assert 20 == average([5, 10, 15, 20, 25, 30, 35])
-    assert 4.5 == average([1, 2, 3, 4, 5, 6, 7, 8])
-
-
 if __name__ == "__main__":
-    """Call average module to find mean of a specific list of numbers."""
-    print(average([2, 4, 6, 8, 20, 50, 70]))
+    import doctest
+
+    doctest.testmod()
