840e8ddfe3b5096daef3424cae4f5389c23182f6
QuantumNovice <43876848+QuantumNovice@users.noreply.github.com>
2021-06-08 18:50:52 +0500

    Update luhn.py

--- hashes/luhn.py
+++ hashes/luhn.py
@@ -5,22 +5,22 @@ from typing import List
 def is_luhn(string: str) -> bool:
     """
     Perform Luhn validation on input string
-    https://en.wikipedia.org/wiki/Luhn_algorithm
     Algorithm:
     * Double every other digit starting from 2nd last digit.
     * Subtract 9 if number is greater than 9.
     * Sum the numbers
     *
     >>> test_cases = [79927398710, 79927398711, 79927398712, 79927398713,
-    ...     79927398714, 79927398715, 79927398716, 79927398717, 79927398718, 79927398719]
+    ...     79927398714, 79927398715, 79927398716, 79927398717, 79927398718,
+    ...     79927398719]
     >>> test_cases = list(map(str, test_cases))
     >>> list(map(is_luhn, test_cases))
     [False, False, False, True, False, False, False, False, False, False]
     """
     check_digit: int
-    vector: List[str] = list(string)
-    vector, check_digit = vector[:-1], int(vector[-1])
-    vector: List[int] = [*map(int, vector)]
+    _vector: List[str] = list(string)
+    __vector, check_digit = _vector[:-1], int(_vector[-1])
+    vector: List[int] = [*map(int, __vector)]
 
     vector.reverse()
     for idx, i in enumerate(vector):
@@ -41,6 +41,6 @@ def is_luhn(string: str) -> bool:
 
 if __name__ == "__main__":
     import doctest
-    
+
     doctest.testmod()
     assert is_luhn("79927398713")
