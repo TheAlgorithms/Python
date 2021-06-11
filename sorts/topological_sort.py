55e62a1ea2637468da28784c81848072d6f84700
saikumar-ramavath <58340442+saikumar-ramavath@users.noreply.github.com>
2021-06-11 20:23:56 +0530

    Removed Redundant/unused code,  corrected print function
    Previous algorithm works only for given inputs, made algorithm generic works for any inputs of graph.
    corrected print function of topo sort
    Optimized look up in visited dictionary
    Remove Redundant/unused code

--- sorts/topological_sort.py
+++ sorts/topological_sort.py
@@ -8,28 +8,39 @@
 edges = {"a": ["c", "b"], "b": ["d", "e"], "c": [], "d": [], "e": []}
 vertices = ["a", "b", "c", "d", "e"]
 
+sort = []
+visited = {}
+
 
 def topological_sort(start, visited, sort):
-    """Perform topolical sort on a directed acyclic graph."""
+    """Perform topological sort on a directed acyclic graph."""
     current = start
     # add current to visited
-    visited.append(current)
+    visited[current] = 1
     neighbors = edges[current]
     for neighbor in neighbors:
         # if neighbor not in visited, visit
-        if neighbor not in visited:
+        if visited[neighbor] == 0:
             sort = topological_sort(neighbor, visited, sort)
     # if all neighbors visited add current to sort
-    sort.append(current)
-    # if all vertices haven't been visited select a new one to visit
-    if len(visited) != len(vertices):
-        for vertice in vertices:
-            if vertice not in visited:
-                sort = topological_sort(vertice, visited, sort)
+    sort.append(current)    
     # return sort
     return sort
 
+def print_graph(sort):
+    #reverse the sort 
+    sort = sort[::-1]
+    print(sort)
 
 if __name__ == "__main__":
-    sort = topological_sort("a", [], [])
-    print(sort)
+    #Adding vertices to visited and mark them as 0
+    # 0 value indicates that vertex is unvisited
+    # 1 value indicates that vertex is visited
+    for vertex in vertices:
+        visited.update({vertex: 0})
+    
+    for vertex in visited:
+        if visited[vertex] == 0:
+            sort = topological_sort(vertex, visited, sort)
+    
+    print_graph(sort)
