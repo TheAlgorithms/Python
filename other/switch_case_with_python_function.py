22963d11faece15184ff6e5310cc694838cbb686
suyash shrivastava <suyash2796@gmail.com>
2021-06-11 08:34:19 +0530

    general code format

--- other/switch_case_with_python_function.py
+++ other/switch_case_with_python_function.py
@@ -3,7 +3,8 @@
 '''
 import sys
 
-def switch_case_mapper(case_key:str, attr1:int, attr2:int) -> int:
+
+def switch_case_mapper(case_key: str, attr1: int, attr2: int) -> int:
     """
     >>> switch_case_mapper('add',1,2)
     3
@@ -18,38 +19,44 @@ def switch_case_mapper(case_key:str, attr1:int, attr2:int) -> int:
     >>> switch_case_mapper('invalid case',1,2)
     'Case Not Found!'
     """
-    method = getattr(sys.modules[__name__], case_key, lambda  attr1,attr2: "Case Not Found!")
+    method = getattr(sys.modules[__name__], case_key,
+                     lambda attr1, attr2: "Case Not Found!")
     return method(attr1, attr2)
 
-def add(attr1:int, attr2:int) -> int:
+
+def add(attr1: int, attr2: int) -> int:
     """
     >>> add(1,2)
     3
     """
     return attr1+attr2
 
-def subtract(attr1:int, attr2:int) -> int:
+
+def subtract(attr1: int, attr2: int) -> int:
     """
     >>> subtract(1,2)
     -1
     """
     return attr1-attr2
 
-def multiply(attr1:int, attr2:int) -> int:
+
+def multiply(attr1: int, attr2: int) -> int:
     """
     >>> multiply(1,2)
     2
     """
     return attr1*attr2
 
-def modulo(attr1:int, attr2:int) -> int:
+
+def modulo(attr1: int, attr2: int) -> int:
     """
     >>> modulo(1,2)
     1
     """
-    return attr1%attr2
+    return attr1 % attr2
 
-def divide(attr1:int, attr2:int) -> int:
+
+def divide(attr1: int, attr2: int) -> int:
     """
     >>> divide(1,2)
     0
@@ -58,9 +65,9 @@ def divide(attr1:int, attr2:int) -> int:
 
 
 if __name__ == "__main__":
-    switch_case_mapper('multiply',1,2)
-    switch_case_mapper('add',1,2)
-    switch_case_mapper('subtract',1,2)
-    switch_case_mapper('divide',1,2)
-    switch_case_mapper('modulo',1,2)
-    switch_case_mapper('invalid case',1,2)
+    switch_case_mapper('multiply', 1, 2)
+    switch_case_mapper('add', 1, 2)
+    switch_case_mapper('subtract', 1, 2)
+    switch_case_mapper('divide', 1, 2)
+    switch_case_mapper('modulo', 1, 2)
+    switch_case_mapper('invalid case', 1, 2)
