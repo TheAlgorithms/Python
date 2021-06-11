417f0d020b5e87d4d2c1f44a1ffc8e439099e698
suyash shrivastava <suyash2796@gmail.com>
2021-06-11 10:56:22 +0530

    formtting code

--- other/switch_case_with_python_function.py
+++ other/switch_case_with_python_function.py
@@ -1,6 +1,6 @@
-'''
+"""
 @author : Suyash Shrivastava
-'''
+"""
 import sys
 
 
@@ -19,8 +19,9 @@ def switch_case_mapper(case_key: str, attr1: int, attr2: int) -> int:
     >>> switch_case_mapper('invalid case',1,2)
     'Case Not Found!'
     """
-    method = getattr(sys.modules[__name__], case_key,
-                     lambda attr1, attr2: "Case Not Found!")
++   method = getattr(
++       sys.modules[__name__], case_key, lambda attr1, attr2: "Case Not Found!"
++   )
     return method(attr1, attr2)
 
 
@@ -29,7 +30,7 @@ def add(attr1: int, attr2: int) -> int:
     >>> add(1,2)
     3
     """
-    return attr1+attr2
+    return attr1 + attr2
 
 
 def subtract(attr1: int, attr2: int) -> int:
@@ -37,7 +38,7 @@ def subtract(attr1: int, attr2: int) -> int:
     >>> subtract(1,2)
     -1
     """
-    return attr1-attr2
+    return attr1 - attr2
 
 
 def multiply(attr1: int, attr2: int) -> int:
@@ -45,7 +46,7 @@ def multiply(attr1: int, attr2: int) -> int:
     >>> multiply(1,2)
     2
     """
-    return attr1*attr2
+    return attr1 * attr2
 
 
 def modulo(attr1: int, attr2: int) -> int:
@@ -61,13 +62,13 @@ def divide(attr1: int, attr2: int) -> int:
     >>> divide(1,2)
     0
     """
-    return attr1//attr2
+    return attr1 // attr2
 
 
 if __name__ == "__main__":
-    switch_case_mapper('multiply', 1, 2)
-    switch_case_mapper('add', 1, 2)
-    switch_case_mapper('subtract', 1, 2)
-    switch_case_mapper('divide', 1, 2)
-    switch_case_mapper('modulo', 1, 2)
-    switch_case_mapper('invalid case', 1, 2)
+    switch_case_mapper("multiply", 1, 2)
+    switch_case_mapper("add", 1, 2)
+    switch_case_mapper("subtract", 1, 2)
+    switch_case_mapper("divide", 1, 2)
+    switch_case_mapper("modulo", 1, 2)
+    switch_case_mapper("invalid case", 1, 2)
