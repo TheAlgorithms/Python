b433734d6eb70346a5b299e6a506cdab6e620220
Alexandre Xavier <ale.bxsantos@gmail.com>
2021-06-13 08:30:30 -0300

    fix(typing): adds correct typing to variables

--- maths/average_mode.py
+++ maths/average_mode.py
@@ -1,3 +1,6 @@
+from typing import List, Union
+
+
 def mode(input_list: list) -> list:  # Defining function "mode."
     """This function returns the mode(Mode as in the measures of
     central tendency) of the input data.
@@ -20,14 +23,16 @@ def mode(input_list: list) -> list:  # Defining function "mode."
     >>> mode(input_list)
     ['x', 'y']
     """
-    result = list()  # Empty list to store the counts of elements in input_list
+    result: List[
+        Union[str, int, float]
+    ] = []  # Empty list to store the counts of elements in input_list
     for x in input_list:
         result.append(input_list.count(x))
     if not result:
         return []
     y = max(result)  # Gets the maximum value in the result list.
     # Gets values of modes
-    result = {input_list[i] for i, value in enumerate(result) if value == y}
+    result = list({input_list[i] for i, value in enumerate(result) if value == y})
     return sorted(result)
 
 
