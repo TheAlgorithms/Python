ef955c30bbd82cefa34cb535152f8fc1ec60b32e
sai kumar ramavath <58340442+saikumar-ramavath@users.noreply.github.com>
2021-06-13 23:40:46 +0530

    Update topological_sort.py

--- sorts/topological_sort.py
+++ sorts/topological_sort.py
@@ -8,7 +8,9 @@
 edges = {"a": ["c", "b"], "b": ["d", "e"], "c": [], "d": [], "e": []}
 vertices = ["a", "b", "c", "d", "e"]
 
+# sort stores the topological sort of graph
 sort = []
+# visited dictionary keep tracks of which nodes of graph are visited
 visited = {}
 
 
@@ -23,24 +25,22 @@ def topological_sort(start, visited, sort):
         if visited[neighbor] == 0:
             sort = topological_sort(neighbor, visited, sort)
     # if all neighbors visited add current to sort
-    sort.append(current)    
+    sort.append(current)
     # return sort
     return sort
 
-def print_graph(sort):
-    #reverse the sort 
-    sort = sort[::-1]
-    print(sort)
 
 if __name__ == "__main__":
-    #Adding vertices to visited and mark them as 0
+    # Adding vertices to visited and mark them as 0
     # 0 value indicates that vertex is unvisited
     # 1 value indicates that vertex is visited
     for vertex in vertices:
         visited.update({vertex: 0})
-    
+
     for vertex in visited:
         if visited[vertex] == 0:
             sort = topological_sort(vertex, visited, sort)
-    
-    print_graph(sort)
+
+    # reverse the sort
+    sort = sort[::-1]
+    print(sort)
