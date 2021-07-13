18e090f7c3e196eb209a34bb67dfce8f2bb36788
J.Rakesh <53005533+RakeshJarupula@users.noreply.github.com>
2021-07-13 08:02:35 +0530

    'Add tyep hint for variables'

--- machine_learning/linear_regression_GD.py
+++ machine_learning/linear_regression_GD.py
@@ -5,16 +5,17 @@ class LinearRegressionGD():
         self.eta: float = eta
         self.n_iter: int = n_iter
 
-    def net_input(self, X):
-        return np.dot(X, self.w_[1:]) + self.w_[0]
+    def net_input(self, X_matrix):
+        '''Given an input n dimenstional array(X), returns weighted sum of inputs with weights (i.e. W.X + b)'''
+        return np.dot(X_matrix, self.w_[1:]) + self.w_[0]
 
-    def fit(self, X, y):
-        self.w_ = np.zeros(1 + X.shape[1])
+    def fit(self, X_matrix, target_y):
+        self.w_ = np.zeros(1 + X_matrix.shape[1])
         self.cost_ = []
         for i in range(self.n_iter):
-            output=self.net_input(X)
-            errors = (y-output)
-            self.w_[1:] += self.eta * X.T.dot(errors)
+            output=self.net_input(X_matrix)
+            errors = (target_y - output)
+            self.w_[1:] += self.eta * X_matrix.T.dot(errors)
             self.w_[0] += self.eta * errors.sum()
             cost = (errors**2).sum() / 2.0
             self.cost_.append(cost)
