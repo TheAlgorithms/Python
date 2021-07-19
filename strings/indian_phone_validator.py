dd25a7efe656582eab9254fc62ee2a0940f416a8
Suryapratap Singh <suryprtaps@gmail.com>
2021-07-19 20:52:04 +0530

    change the phone_validator to indian_phone_validator

--- strings/indian_phone_validator.py
+++ strings/indian_phone_validator.py
@@ -1,30 +1,28 @@
 import re
-def phone_validator(phone: str) -> bool:
+def indian_phone_validator(phone: str) -> bool:
     """
     Determine whether the string is a valid phone number or not
     :param phone:
     :return: Boolean
-    >>> phone_validator("+91123456789")
+    >>> indian_phone_validator("+91123456789")
     False
-    >>> phone_validator("+919876543210")
+    >>> indian_phone_validator("+919876543210")
     True
-    >>> phone_validator("01234567896")
+    >>> indian_phone_validator("01234567896")
     False
-    >>> phone_validator("919876543218")
+    >>> indian_phone_validator("919876543218")
     True
-    >>> phone_validator("+91-1234567899")
+    >>> indian_phone_validator("+91-1234567899")
     False
     """
     # Created a regex pattern to produce a valid phone number
     pat = re.compile(r"^(\+91[\-\s]?)?[0]?(91)?[789]\d{9}$")
     # Find their matches
     match = re.search(pat, phone)
-    # if match are present
+    # if the match is present to check for the phone string is valid or not.
     if match:
-        # the match is the same as the phone string it means they are valid phone number.
         return match.string == phone
-    # if match are none so return False.
     return False
 
 if __name__ == "__main__":
-    print(phone_validator('+91123456789'))
+    print(indian_phone_validator('+918827897895'))
