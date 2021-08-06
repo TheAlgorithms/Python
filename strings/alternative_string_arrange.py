799f8407947aea27a5588b2a17370011bb14036c
Suryapratap Singh <suryprtaps@gmail.com>
2021-08-06 15:36:55 +0530

    changed the variable name li to output_list

--- strings/alternative_string_arrange.py
+++ strings/alternative_string_arrange.py
@@ -18,13 +18,13 @@ def alternative_string_arrange(first_str: str, second_str: str) -> str:
     abs_length: int = (
         first_str_length if first_str_length > second_str_length else second_str_length
     )
-    li: list = []
+    output_list: list = []
     for char_count in range(abs_length):
         if char_count < first_str_length:
-            li.append(first_str[char_count])
+            output_list.append(first_str[char_count])
         if char_count < second_str_length:
-            li.append(second_str[char_count])
-    return "".join(li)
+            output_list.append(second_str[char_count])
+    return "".join(output_list)
 
 
 if __name__ == "__main__":
