c17470c50ab4a65f499391532bbca5e2777ce3a4
Kiran Hipparagi <49370990+KiranHipparagi@users.noreply.github.com>
2021-08-20 14:00:38 +0530

    Added doctest cases

--- sorts/dnf_sort.py
+++ sorts/dnf_sort.py
@@ -3,7 +3,15 @@ Pure implementation of Dutch national flag problem(dnf) sort algorithm in Python
 Dutch National Flag algorithm is a popular algorithm originally designed by Edsger Dijkstra.
 It is the most optimal sort for 3 unique values (eg. 0,1,2) in an Array.
 dnf sort can sort an array of n size with [0<=a[i]<=2] at guaranteed O(n) complexity in a single pass.
-  
+
+The flag of the Netherlands consists of three colors: white, red, and blue. 
+The task is to randomly arrange balls of white, red, and blue in such a way that balls of the same color are placed together. 
+For DNF (Dutch National Flag), we sort an array of 0, 1, and 2's in linear time that does not consume any extra space. 
+We have to keep in mind that this algorithm can be implemented only on an array that has three unique elements.
+
+1)Time complexity is O(n).
+2)Space complexity is O(1).
+
 More info on: https://en.wikipedia.org/wiki/Dutch_national_flag_problem
 
 For doctests run following command:
@@ -19,7 +27,10 @@ python dnf_sort.py
 
 # Python program to sort an array with
 # 0, 1 and 2 in a single pass
-
+red = 0 # In this case this is the first color of the flag.
+white = 1 # This is the second color of the flag.
+blue = 2 # This is the third color of the flag.
+colors = [red, white, blue]
 # Function to sort array
 def dnf_sort( sequence:list, arr_size:int)->list:
     """
@@ -32,19 +43,26 @@ def dnf_sort( sequence:list, arr_size:int)->list:
     Input: dnf_sort([0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1],12)
     Output: [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2]
     """
+    if sequence is None: return
+    if len(sequence) <= 1: return
     low = 0
     high = arr_size - 1
     mid = 0
     while mid <= high:
-        if sequence[mid] == 0:
+        if sequence[mid] == colors[0]:
             sequence[low], sequence[mid] = sequence[mid], sequence[low]
             low = low + 1
             mid = mid + 1
-        elif sequence[mid] == 1:
+            continue
+        if sequence[mid] == colors[1]:
             mid = mid + 1
-        else:
+            continue
+        if sequence[mid] == colors[2]:
             sequence[mid], sequence[high] = sequence[high], sequence[mid]
             high = high - 1
+            continue
+            
+        raise ValueError(f"The elements inside the sequence must contains only {colors} values")    
     return sequence
 
 if __name__ == "__main__":
