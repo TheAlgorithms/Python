3cd9ba12636cb0e941bcaffe1e6adf56cecca6dd
Kiran Hipparagi <49370990+KiranHipparagi@users.noreply.github.com>
2021-08-20 09:51:16 +0530

    Added descriptive name and type hint
    Added descriptive name and type hint  for parameter with doctest for the function dnf_sort.

--- sorts/dnf_sort.py
+++ sorts/dnf_sort.py
@@ -21,38 +21,39 @@ python dnf_sort.py
 # 0, 1 and 2 in a single pass
 
 # Function to sort array
-def dnf_sort( a, arr_size):
+def dnf_sort( sequence:list, arr_size:int)->list:
     """
      Pure implementation of dnf sort algorithm in Python
     :param data: 3 unique values (eg. 0,1,2) in an Array
     :return: the same collection in ascending order
     Examples:
-    Input: dnf_sort([2, 1, 0, 0,1, 2])
+    Input: dnf_sort([2, 1, 0, 0,1, 2],6)
     Output: [0, 0, 1, 1, 2, 2]
-    Input: dnf_sort([0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1])
+    Input: dnf_sort([0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1],12)
     Output: [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2]
     """
     low = 0
     high = arr_size - 1
     mid = 0
     while mid <= high:
-        if a[mid] == 0:
-            a[low], a[mid] = a[mid], a[low]
+        if sequence[mid] == 0:
+            sequence[low], sequence[mid] = sequence[mid], sequence[low]
             low = low + 1
             mid = mid + 1
-        elif a[mid] == 1:
+        elif sequence[mid] == 1:
             mid = mid + 1
         else:
-            a[mid], a[high] = a[high], a[mid]
+            sequence[mid], sequence[high] = sequence[high], sequence[mid]
             high = high - 1
-    return a
+    return sequence
 
 if __name__ == "__main__":
     from doctest import testmod
 
     testmod()
 
-    
+    assert dnf_sort([1, 0, 0, 2, 1],5) == [0,0,1, 1, 2]
+    assert dnf_sort([0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1],12) == [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2]
     user_input = input("Enter numbers separated by a comma:\n").strip()
     unsorted = [int(item) for item in user_input.split(",")]
-print(dnf_sort(unsorted,len(unsorted)))
+print(f"{dnf_sort(unsorted,len(unsorted))}")
