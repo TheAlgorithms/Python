edbcc14bde2ea146a913bb0026580906b9751c9d
gpapadok <gpapadokostakis@protonmail.ch>
2021-08-23 15:15:42 +0300

    fix zero_crossing function and doctests

--- digital_image_processing/edge_detection/marr_hildreth.py
+++ digital_image_processing/edge_detection/marr_hildreth.py
@@ -54,47 +54,42 @@ def zero_crossing(image: np.ndarray, threshold: float = 0) -> np.ndarray:
     their difference is above a threshold it is marked as an edge pixel (255).
     Otherwise it is marked as a background pixel (0).
     Examples:
-    >>> lena = cv2.imread("../image_data/lena.jpg", 0)
-    >>> laplacian = img_convolve(img_convolve(lena, gaussian_kernel(1.)), \
-                                 laplacian_kernel)
-    >>> zero_crossing(laplacian)
-    array([[  0,   0,   0, ...,   0,   0,   0],
-           [  0, 255, 255, ...,   0, 255,   0],
-           [  0, 255, 255, ...,   0, 255,   0],
-           ...,
-           [  0, 255, 255, ...,   0, 255,   0],
-           [  0, 255, 255, ...,   0, 255,   0],
-           [  0,   0,   0, ...,   0,   0,   0]], dtype=uint8)
+    >>> a = np.array([[-1,0,1],[0,0,0],[0,0,1]])
+    >>> zero_crossing(a)
+    array([[  0, 255,   0],
+           [  0, 255,   0],
+           [  0,   0,   0]], dtype=uint8)
     """
     N, M = image.shape
 
     edges = np.zeros_like(image, dtype=np.uint8)
-    for i in range(1, N - 1):
-        for j in range(1, M - 1):
-            left = image[i - 1, j]
-            right = image[i + 1, j]
-            up = image[i, j + 1]
-            down = image[i, j - 1]
-            up_left = image[i - 1, j - 1]
-            down_right = image[i + 1, j + 1]
-            down_left = image[i - 1, j + 1]
-            up_right = image[i + 1, j - 1]
-
-            # left/right
-            if left * right < 0 and np.abs(left - right) > threshold:
-                edges[i, j] = 255
-
-            # up/down
-            elif up * down < 0 and np.abs(up - down) > threshold:
-                edges[i, j] = 255
-
-            # diagonalw
-            elif up_left * down_right < 0 and np.abs(up_left - down_right) > threshold:
-                edges[i, j] = 255
-
-            elif down_left * up_right < 0 and np.abs(down_left - up_right) > threshold:
-                edges[i, j] = 255
-
+    for i in range(N):
+        for j in range(M):
+            if i > 0 and i < N - 1:
+                left = image[i - 1, j]
+                right = image[i + 1, j]
+                if left * right < 0 and np.abs(left - right) > threshold:
+                    edges[i, j] = 255
+            if j > 0 and j < M - 1:
+                up = image[i, j + 1]
+                down = image[i, j - 1]
+                if up * down < 0 and np.abs(up - down) > threshold:
+                    edges[i, j] = 255
+            if (i > 0 and i < N - 1) and (j > 0 and j < M - 1):
+                up_left = image[i - 1, j - 1]
+                down_right = image[i + 1, j + 1]
+                down_left = image[i - 1, j + 1]
+                up_right = image[i + 1, j - 1]
+                if (
+                    up_left * down_right < 0
+                    and np.abs(up_left - down_right) > threshold
+                ):
+                    edges[i, j] = 255
+                elif (
+                    down_left * up_right < 0
+                    and np.abs(down_left - up_right) > threshold
+                ):
+                    edges[i, j] = 255
     return edges
 
 
@@ -110,15 +105,11 @@ def marr_hildreth(
     a Gaussian kernel then with a Laplacian. Results are the same.
     Source: https://en.wikipedia.org/wiki/Marr%E2%80%93Hildreth_algorithm
     Examples:
-    >>> lena = cv2.imread("../image_data/lena.jpg", 0)
-    >>> marr_hildreth(lena)
-    array([[  0,   0,   0, ...,   0,   0,   0],
-           [  0,   0,   0, ...,   0, 255,   0],
-           [  0,   0,   0, ...,   0, 255,   0],
-           ...,
-           [  0,   0,   0, ...,   0,   0,   0],
-           [  0,   0,   0, ...,   0,   0,   0],
-           [  0,   0,   0, ...,   0,   0,   0]], dtype=uint8)
+    >>> a = np.array([[0,0,255],[0,0,255],[0,0,255]])
+    >>> marr_hildreth(a)
+    array([[  0, 255,   0],
+           [  0, 255,   0],
+           [  0, 255,   0]], dtype=uint8)
     """
 
     # Filter image with Gaussian kernel
