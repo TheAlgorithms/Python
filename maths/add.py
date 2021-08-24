9cf1a180550470018a536355a5bdc0dcf4531aa0
Vibhor0 <78290877+Vibhor0@users.noreply.github.com>
2021-08-25 01:22:57 +0530

    Update add.py

--- maths/add.py
+++ maths/add.py
@@ -1,19 +1,5 @@
-"""
-Just to check
-"""
-
-
-def add(a, b):
-    """
-    >>> add(2, 2)
-    4
-    >>> add(2, -2)
-    0
-    """
-    return a + b
-
-
-if __name__ == "__main__":
-    a = 5
-    b = 6
-    print(f"The sum of {a} + {b} is {add(a, b)}")
+a = input("Enter first number : ")   
+b = input("Enter second number : ")
+a = int(a)      # Converting it from char to int to prevent it from concatenate
+b = int(b)
+print("Sum = ", a+b)
