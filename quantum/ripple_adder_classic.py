fa71c3ae91fa5b4599d1f648221a9b0942ff1dd4
Christian Clauss <cclauss@me.com>
2021-08-31 07:51:23 +0200

    Update ripple_adder_classic.py

--- quantum/ripple_adder_classic.py
+++ quantum/ripple_adder_classic.py
@@ -55,7 +55,7 @@ def full_adder(
 # The default value for **backend** is the result of a function call which is not
 # normally recommended and causes flake8-bugbear to raise a B008 error. However,
 # in this case, this is accptable because `Aer.get_backend()` is called when the
-# function is defined and that same backend is then reused for function calls.
+# function is defined and that same backend is then reused for all function calls.
 
 
 def ripple_adder(
