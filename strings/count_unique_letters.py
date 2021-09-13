a603051454b33fc0cb70b645a18ee8d9991f9324
Aswin Sankar P D <you@example.com>
2021-09-13 21:39:27 +0530

    added a new program to find and return the occurences of letters in a word

---
+++ strings/count_unique_letters.py
@@ -0,0 +1,25 @@
+def unique_letters(word):
+    """
+    >>> unique_letters('abbccc')
+    {'a': 1, 'b': 2, 'c': 3}
+    >>> unique_letters('mellow')
+    {'m': 1, 'e': 1, 'l': 2, 'o': 1, 'w': 1}
+    """
+    dct = {}
+    for i in word:
+        if i == " ":
+            pass
+        else:
+            if i in dct.keys():
+                dct[i] += 1
+            else:
+                dct[i] = 1
+    print(dct)
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
+
+    unique_letters(input("enter word: "))
