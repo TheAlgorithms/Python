75fda09c66abc6a4139ef51ef2257eb7ee0f9d26
Alexandre De Zotti <alexn11@users.noreply.github.com>
2021-09-28 08:14:32 +0100

    Update fractals/julia_sets.py
    Co-authored-by: John Law <johnlaw.po@gmail.com>

--- fractals/julia_sets.py
+++ fractals/julia_sets.py
@@ -189,7 +189,7 @@ if __name__ == "__main__":
         z_0,
         infinity=1.1 * escape_radius,
     )
-    show_results("z²+c", c_polynomial_1, escape_radius, z_final)
+    show_results("z^2+c", c_polynomial_1, escape_radius, z_final)
 
     nb_iterations = 161
     escape_radius = 2 * abs(c_polynomial_2) + 1
