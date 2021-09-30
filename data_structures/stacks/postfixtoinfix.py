4283df35f383a9f6d7afceb369e7cf12b3b4a3da
kuroyukihime <78816968+KhushiChaudhary744@users.noreply.github.com>
2021-10-01 00:18:19 +0530

    Add files via upload
    Describe your change:
    
    [x] Add an algorithm?
    [ ]  Fix a bug or typo in an existing algorithm?
    [ ] Documentation change?
    
    Checklist:
    
    [x] I have read CONTRIBUTING.md.
    [x] This pull request is all my own work -- I have not plagiarized.
    [x] I know that pull requests will not be merged if they fail the automated tests.
    [x] This PR only changes one algorithm file. To ease review, please open separate PRs for separate algorithms.
    [x] All new Python files are placed inside an existing directory.
    [x] All filenames are in all lowercase characters with no spaces or dashes.
    [x] All functions and variable names follow Python naming conventions.
    [x] All function parameters and return values are annotated with Python type hints.
    [x] All functions have doctests that pass the automated testing.
    [x] All new algorithms have a URL in its comments that points to Wikipedia or other similar explanation.
    [x] If this pull request resolves one or more open issues then the commit message contains Fixes: #{$ISSUE_NO}.

---
+++ data_structures/stacks/postfixtoinfix.py
@@ -0,0 +1,22 @@
+def isOperand(x): #check wheather the character is am operamd or not 
+
+	return (((x>='A') and(x<='Z')) or ((x >= 'a') and (x <= 'z')))
+
+def convert_Infix(expression): #converts the expression
+	s = []                     #create stack to store operators
+	for i in expression:	
+		if (isOperand(i)) :		
+			s.insert(0, i)           #if encountered character is a operand it is placed in the stack.
+		else:	                     #else 
+			operator1 = s[0]               #the first element is popped and storred
+			s.pop(0)
+			operator2 = s[0]               #the second element is popped and storred
+			s.pop(0)
+			s.insert(0,"("+operator2+i+operator1+ ")")           #the operands are arranged to be in infix order around the element
+
+	return s[0]
+
+if __name__ == '__main__':
+	expression = input()       #string
+	print(convert_Infix(expression.strip()))   #call the function
+
