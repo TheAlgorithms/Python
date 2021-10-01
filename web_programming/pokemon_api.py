c1c7f8ef3766470ca52bfa76f6715dd904a82aea
Rithic Hariharan <60564768+gr8rithic@users.noreply.github.com>
2021-10-01 12:05:00 +0530

    Update pokemon_api.py

--- web_programming/pokemon_api.py
+++ web_programming/pokemon_api.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     url = f"https://api.pokemontcg.io/v1/cards?name={name}"
     print(url)
     response = requests.get(url)
-    json = response.json()
-    recieved_data=json(name)
+    recieved_data = response.json()
+    print(recieved_data)
     url_data=requests.get(recieved_data["cards"][1]["imageUrl"])
     with open("001.png","wb") as f:
         for item in url_data.iter_content(1024):
