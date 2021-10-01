be1c08ae3aab8d95cecf937792d7649fc3becd3b
appledora <nazia89@student.sust.edu>
2021-10-01 20:17:10 +0600

    Added large changes to web_programming/crawl_google_results.py with documentations and doctests

--- web_programming/crawl_google_results.py
+++ web_programming/crawl_google_results.py
@@ -5,20 +5,28 @@ import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
+def crawl_and_store_google_search_result(query = "potato", filename=""):
+    """This function will crawl google search result for the given query and store it in a file.
+
+    Args:
+        query (str, optional): The search term provided by the user. Defaults to "potato".
+        filename (str, optional): The name of the file to be saved with the search results. Defaults to "".
+    """
+    if filename == "" :
+        filename = query+"-query.html"
+    elif not filename.endswith(".html"):
+        filename = filename+".html"
+
+    ua = UserAgent()
+    headers = {'User-Agent': ua.random}
+    url = 'https://www.google.com/search?q=' + query
+    response = requests.get(url, headers=headers)
+    with open(filename, "wb") as out_file:  # only for knowing the class
+        for data in response.iter_content(10000):
+            out_file.write(data)
+    print(f"File saved in {filename}")
+
 if __name__ == "__main__":
     print("Googling.....")
-    url = "https://www.google.com/search?q=" + " ".join(sys.argv[1:])
-    res = requests.get(url, headers={"UserAgent": UserAgent().random})
-    # res.raise_for_status()
-    with open("project1a.html", "wb") as out_file:  # only for knowing the class
-        for data in res.iter_content(10000):
-            out_file.write(data)
-    soup = BeautifulSoup(res.text, "html.parser")
-    links = list(soup.select(".eZt8xd"))[:5]
-
-    print(len(links))
-    for link in links:
-        if link.text == "Maps":
-            webbrowser.open(link.get("href"))
-        else:
-            webbrowser.open(f"http://google.com{link.get('href')}")
+    crawl_and_store_google_search_result(sys.argv[1]) 
+ 
