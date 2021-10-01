e0ec4a168284567edd88d430c70aa4df4306b7a9
Vaibhav <ervaibhavkumar@outlook.com>
2021-10-01 10:17:43 +0530

    Created simple function for subtraction

---
+++ maths/subtract.py
@@ -0,0 +1,15 @@
+
+def subtract(a, b):
+    """
+    >>> subtract(3, 2)
+    1
+    >>> subtract(2, -2)
+    4
+    """
+    return a - b
+
+
+if __name__ == "__main__":
+    a = 6
+    b = 5
+    print(f"The difference of of {a} and {b} is {subtract(a, b)}")
