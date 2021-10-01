6de0769cf2120891566217b67cb090076ce7af8f
Rithic Hariharan <60564768+gr8rithic@users.noreply.github.com>
2021-10-01 08:39:15 +0530

    Update pokemon_api.py

--- web_programming/pokemon_api.py
+++ web_programming/pokemon_api.py
@@ -1,17 +1,19 @@
 import requests
 import json
-def get_poke_data() -> None:
-    name = "pikachu"
-    url = f"https://api.pokemontcg.io/v1/cards?name={name}" 
-    response = requests.get(url)
-    return response.json()
-recieved_data=get_poke_data(name)
-
 import matplotlib.pyplot as plt
-url_data=requests.get(recieved_data["cards"][1]["imageUrl"])
-with open("001.png","wb") as f:
-    for item in url_data.iter_content(1024):
-        f.write(item)
-image_data=plt.imread("./001.png")
-plt.imshow(image_data)
-plt.show()
+
+if __name__ == "__main__":
+    name = "pikachu"
+    # The image URL is in the content field of the first meta tag
+    url = f"https://api.pokemontcg.io/v1/cards?name={name}"
+    print(url)
+    response = requests.get(url)
+    json = response.json()
+    recieved_data=json(name)
+    url_data=requests.get(recieved_data["cards"][1]["imageUrl"])
+    with open("001.png","wb") as f:
+        for item in url_data.iter_content(1024):
+            f.write(item)
+    image_data=plt.imread("./001.png")
+    plt.imshow(image_data)
+    plt.show()
