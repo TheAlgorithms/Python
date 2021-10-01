dec909c87ee7ce98d3a486aa7c9d21b17c8c7d9b
appledora <nazia89@student.sust.edu>
2021-10-01 20:32:01 +0600

    Added large changes to web_programming/crawl_google_results.py with documentations and doctests

--- web_programming/crawl_google_results.py
+++ web_programming/crawl_google_results.py
@@ -1,32 +1,39 @@
 import sys
-import webbrowser
-
 import requests
-from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
-def crawl_and_store_google_search_result(query = "potato", filename=""):
-    """This function will crawl google search result for the given query and store it in a file.
+
+def crawl_and_store_google_search_result(query="potato", filename=""):
+    """This function will crawl google search result for the given query and
+    store it in a file.
 
     Args:
-        query (str, optional): The search term provided by the user. Defaults to "potato".
-        filename (str, optional): The name of the file to be saved with the search results. Defaults to "".
+        query (str, optional): The search term provided by the user. Defaults
+        to "potato".
+        filename (str, optional): The name of the file to be saved with the
+        search results. Defaults to "".
+    >>> crawl_and_store_google_search_result ("hacktober", "hacktober")
+    File saved in hacktober.html
+    >>> crawl_and_store_google_search_result ("hacktober", "hacktober.html")
+    File saved in hacktober.html
+    >>> crawl_and_store_google_search_result ("hacktober")
+    File saved in hacktober-query.html
     """
-    if filename == "" :
-        filename = query+"-query.html"
+    print(f"Searching Google for {query} ....")
+    if filename == "":
+        filename = query + "-query.html"
     elif not filename.endswith(".html"):
-        filename = filename+".html"
+        filename = filename + ".html"
 
     ua = UserAgent()
-    headers = {'User-Agent': ua.random}
-    url = 'https://www.google.com/search?q=' + query
+    headers = {"User-Agent": ua.random}
+    url = "https://www.google.com/search?q=" + query
     response = requests.get(url, headers=headers)
     with open(filename, "wb") as out_file:  # only for knowing the class
         for data in response.iter_content(10000):
             out_file.write(data)
-    print(f"File saved in {filename}")
+    print(f"File saved as {filename}")
+
 
 if __name__ == "__main__":
-    print("Googling.....")
-    crawl_and_store_google_search_result(sys.argv[1]) 
- 
+    crawl_and_store_google_search_result(sys.argv[1])
