6b98b0ba62dee140276d4d790e4d99dc32dca9e7
appledora <nazia89@student.sust.edu>
2021-10-02 12:36:45 +0600

    fixed typo in test

--- web_programming/get_imdbtop.py
+++ web_programming/get_imdbtop.py
@@ -2,77 +2,43 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def print_imdb_topn_movies(num_movies: int = 5) -> None:
+def parse_dictionary(movie_dict: dict = {}) -> None:
+    """Prints the movie dictionary.
+
+    Args:
+        movie_dict: The dictionary to print.
+    """
+    print(f"\nMovie: {movie_dict['name']}")
+    print(f"Genre: {movie_dict['genre']}")
+    print(f"Rating: {movie_dict['rating']}")
+    print(f"Page Link: {movie_dict['page link']}")
+
+
+def crawl_imdb_topn_movies(num_movies: int = 5) -> None:
     """Crawls IMDB and prints the top n movies with their respective titles, genre,
     ratings, and imdb page link released.
 
     Args:
         num_movies: The number of movies to crawl. Defaults to 5.
-
-    >>> print_imdb_topn_movies()
-    The Shawshank Redemption
-
-    Drama
-    9.3
-    https://www.imdb.com/title/tt0111161/
-    ****************************************
-
-    The Dark Knight
-
-    Action, Crime, Drama
-    9.0
-    https://www.imdb.com/title/tt0468569/
-    ****************************************
-
-    Inception
-
-    Action, Adventure, Sci-Fi
-    8.8
-    https://www.imdb.com/title/tt1375666/
-    ****************************************
-
-    Fight Club
-
-    Drama
-    8.8
-    https://www.imdb.com/title/tt0137523/
-    ****************************************
-
-    Pulp Fiction
-
-    Crime, Drama
-    8.9
-    https://www.imdb.com/title/tt0110912/
-    ****************************************
-
-    >>> print_imdb_topn_movies(2)
-    The Shawshank Redemption
-
-    Drama
-    9.3
-    https://www.imdb.com/title/tt0111161/
-    ****************************************
-
-    The Dark Knight
-
-    Action, Crime, Drama
-    9.0
-    https://www.imdb.com/title/tt0468569/
-    ****************************************
     """
-    print(f"Crawling {num_movies} movies from the IMDB databse ...")
+
+    print(f"Crawling {num_movies} movies from the IMDB database...")
     base_url = (
         f"https://www.imdb.com/search/title?title_type="
         f"feature&sort=num_votes,desc&count={num_movies}"
     )
     source = BeautifulSoup(requests.get(base_url).content, "html.parser")
-    for m in source.findAll("div", class_="lister-item mode-advanced"):
-        print("\n" + m.h3.a.text)  # movie's name
-        print(m.find("span", attrs={"class": "genre"}).text)  # genre
-        print(m.strong.text)  # movie's rating
-        print(f"https://www.imdb.com{m.a.get('href')}")  # movie's page link
-        print("*" * 40)
+    movies = source.find_all("div", class_="lister-item mode-advanced")
+    movie_dict = {}
+    for m in movies:
+        movie_dict["name"] = "\n" + m.h3.a.text  # movie's name
+        movie_dict["genre"] = m.find("span", attrs={"class": "genre"}).text  # genre
+        movie_dict["rating"] = m.strong.text  # movie's rating
+        movie_dict[
+            "page link"
+        ] = f"https://www.imdb.com{m.a.get('href')}"  # movie's page link
+        parse_dictionary(movie_dict)
 
 
 if __name__ == "__main__":
-    print_imdb_topn_movies(input("How many movies would you like to see? "))
+    crawl_imdb_topn_movies(input("How many movies would you like to see? "))
