09aaeae04867c31fc770263d7bb35f998bce5313
K Shakthi Dhar Reddy <55318956+Shakthi-Dhar@users.noreply.github.com>
2021-10-02 14:44:16 +0530

    reddit hot threads crawler
    created a simple python script using praw library to extract data from reddit

---
+++ web_programming/reddit_crawler_hot_topics.py
@@ -0,0 +1,26 @@
+import praw
+
+# creating an instance for praw reddit
+# get all the user cridentials form the reddit page: https://www.reddit.com/prefs/apps
+reddit = praw.Reddit(
+    # Client ID
+    client_id="Enter client id here",
+    # Client Secret Key
+    client_secret="Enter client secret here",
+    # User Agent
+    user_agent="Enter user agent here",
+    check_for_async=False
+)
+
+
+def crawler(subreddit) -> None:
+    hot = reddit.subreddit(subreddit).hot(limit=100)
+    for post in hot:
+        print(post.title)
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
+    crawler("all")
