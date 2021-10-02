6b774c2c59094a67832b5e876cc7a67b44c6522e
appledora <nazia89@student.sust.edu>
2021-10-02 21:02:51 +0600

    Fixed doctest error, removed print() from method, created new script as suggested

---
+++ web_programming/get_google_search_results.py
@@ -0,0 +1,44 @@
+import requests
+from fake_useragent import UserAgent
+
+
+def get_google_search_results(query="potato", filename=""):
+    """Reads google search result page for the given query and
+    stores it in a file.
+
+    Args:
+        query: The search term provided by the user. Defaults
+        to "potato".
+        filename (str, optional): The name of the file to be saved with the
+        search results. Defaults to "".
+    >>> get_google_search_results ("hacktober", "hacktober") != None
+    True
+    >>> get_google_search_results ("hacktober","") != None
+    True
+    >>> get_google_search_results ("", "hacktober") != None
+    True
+    >>> get_google_search_results ("", "") != None
+    True
+
+    """
+    if filename == "":
+        filename = query + "-query.html"
+    elif not filename.endswith(".html"):
+        filename = filename + ".html"
+
+    ua = UserAgent()
+    headers = {"User-Agent": ua.random}
+    url = "https://www.google.com/search?q=" + query
+    response = requests.get(url, headers=headers)
+    with open(filename, "wb") as out_file:  # only for knowing the class
+        for data in response.iter_content(10000):
+            out_file.write(data)
+    return filename
+
+
+if __name__ == "__main__":
+    query = input("Enter query: ")
+    filename = input("Enter filename: ")
+    print(f"Searching Google for {query} ....")
+    filename = get_google_search_results(query, filename)
+    print(f"File saved as {filename}")
