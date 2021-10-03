6f82f39fe7c82890c6095f3159a392512707fba0
Luke Banicevic <60857954+banaboi@users.noreply.github.com>
2021-10-03 23:30:35 +1100

    Added delete node without access to head algorithm

---
+++ data_structures/linked_list/delete_node_linked_list.py
@@ -0,0 +1,51 @@
+"""
+Write a function to delete a node in a singly-linked list.
+You will not be given access to the head of the list,
+instead you will be given access to the node to be deleted directly.
+"""
+
+
+class ListNode:
+    def __init__(self, val) -> None:
+        self.val = val
+        self.next = None
+
+
+def make_linked_list(numbers: list = []) -> ListNode:
+    """Creates a Linked List given a list of numbers, returns
+    the head of the Linked List
+
+    >>> make_linked_list([])
+    Traceback (most recent call last):
+        ...
+    Exception: The Numbers List is empty
+    >>> print(make_linked_list([1,2,3]).val)
+    1
+    >>> print(make_linked_list([0,2,15]).val)
+    0
+    """
+
+    if not numbers:
+        raise Exception("The Numbers List is empty")
+
+    current = head = ListNode(numbers[0])
+
+    for i in range(1, len(numbers)):
+        current.next = ListNode(numbers[i])
+        current = current.next
+    return head
+
+
+def delete_node(node: ListNode) -> None:
+    """Deletes a given node from a linked list
+    without accessing the head of the linked list.
+    """
+
+    node.val = node.next.val
+    node.next = node.next.next
+
+
+if __name__ == "__main__":
+    from doctest import testmod
+
+    testmod()
