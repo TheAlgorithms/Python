05f8fec3334eb96ead8b507849c13b1808950ba6
AlternateWalls <71268229+AlternateWalls@users.noreply.github.com>
2021-10-03 13:42:42 -0400

    Update ascii_const_addition_cipher.py

--- ciphers/ascii_const_addition_cipher.py
+++ ciphers/ascii_const_addition_cipher.py
@@ -1,32 +1,33 @@
 choice = input("Choose an option: (1) - Encrypt a message (2) - Decrypt a message: ")
-i = 0
-x = 0
-string_1 = ""
-string_2 = ""
+choice1_location = 0
+choice2_location = 0
+choice_1_string = ""
+choice_2_string = ""
 
 if choice == "1":
   user_input1 = input("Enter the  message here: ")
   user_constant = input("Enter the constant number here: ")
+
   for z in user_input1:
-     asciicode = ord(user_input1[i])
-     i += 1
+     asciicode = ord(user_input1[choice1_location])
+     choice1_location += 1
      asciicode += int(user_constant)
-     string_1 += str(asciicode)
-     string_1 += " "
-     if i == len(user_input1):
-       print(string_1)
+     choice_1_string += str(asciicode)
+     choice_1_string += " "
+     if choice1_location == len(user_input1):
+       print(choice_1_string)
 elif choice == "2":
   user_input2 = input("Enter the message here: ")
   user_constant2 = input("Enter the constant number: ")
   while True:
     split = user_input2.split()
-    english = int(split[x])
+    english = int(split[choice2_location])
     english += - int(user_constant2)
     english = chr(english)
-    x += 1
-    string_2 += str(english)
-    if x == len(split):
-      print(string_2)
+    choice2_location += 1
+    choice_2_string += str(english)
+    if choice2_location == len(split):
+      print(choice_2_string)
       break
 else:
   print("You were supposed to type 1 or 2, not %s!" % choice)
