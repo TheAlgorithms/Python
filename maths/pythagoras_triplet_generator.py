1f0778fc11d5256b979c9e818ad206542b07da8b
Naman <75575170+Naman-Singla777@users.noreply.github.com>
2021-10-03 12:28:33 +0530

    Added Pythagoras Generator

---
+++ maths/pythagoras_triplet_generator.py
@@ -0,0 +1,33 @@
+"""
+Pythagoras Triplet Generator
+
+Input: n = 5
+Output: 5 12 13
+
+Input: n = 123456789
+Output: 123456789 7620789375095260 7620789375095261
+
+I've written this algorithm myself so can't really find an article but this guy explains it really well-
+https://www.youtube.com/watch?v=n6vL2KiWrD4
+
+"""
+
+def PythagorasGenerator(n):
+    """
+    It gives you the pythagoras triplet of the given number
+    It works literally on every number greater than 2
+    """
+    
+    if n % 2 == 0:
+        n = n // 2
+        return 2*n, (n**2-1), (n**2+1)
+    else:
+        return n, (n**2-1)//2, (n**2+1)//2
+      
+n = int(input())
+
+triplet = PythagorasGenerator(n)
+
+a, b, c = triplet
+
+print(a, b, c)
