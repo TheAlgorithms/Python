88b68ca14413a768131ccf1855bf2e76b075f594
thedublinboy <nialljcarrick@gmail.com>
2021-10-03 15:38:15 +0100

    subset_sum.py

--- maths/subset_sum.py
+++ maths/subset_sum.py
@@ -4,15 +4,15 @@
 # Returns false if you cannot add two numbers in the set to create the passed sum
 
 #array must be an array
-#sumToCheckAgainst must be an int
+#desired_sum must be an int
 
-def is_Subset_For_Sum(array: list, sum_To_Check_Against: int) -> None:
+def Sum_InSubset(array: list, desired_sum: int) -> None:
 
     """
     In an array see if a subset adds up to a sum
 
     :param array: list
-    :param sum_To_Check_Against: int
+    :param desired_sum: int
     :return: boolean
 
     >>> is_Subset_For_Sum((1,2,5,6),7)
@@ -25,13 +25,13 @@ def is_Subset_For_Sum(array: list, sum_To_Check_Against: int) -> None:
         raise ValueError("Parameter 1, invalid file type. Value should be list")  
 
     try:
-        sumToCheckAgainst = int(sum_To_Check_Against)
+        sumToCheckAgainst = int(desired_sum)
     except:
         raise ValueError("Parameter 2, invalid file type. Value should be int")  
         
     
 
-    num_Of_Items_in_Array = len(array)
+    num_Array = len(array)
     
     """
     The value of subset[i][ii] will be true when there's
@@ -39,28 +39,28 @@ def is_Subset_For_Sum(array: list, sum_To_Check_Against: int) -> None:
     """
 
     subset = [
-        [False for i in range(sum_To_Check_Against + 1)]
-        for i in range(num_Of_Items_in_Array + 1)
+        [False for i in range(desired_sum + 1)]
+        for i in range(num_Array + 1)
     ]
 
     # If sumToCheckAgainst is 0, then answer is true
-    for i in range(num_Of_Items_in_Array + 1):
+    for i in range(num_Array + 1):
         subset[i][0] = True
 
     # If sumToCheckAgainst is not 0 and set is empty,
     # then answer is false
-    for i in range(1, sum_To_Check_Against + 1):
+    for i in range(1, desired_sum + 1):
         subset[0][i] = False
 
     # Fill the subset table in bottom up manner
-    for i in range(1, num_Of_Items_in_Array + 1):
-        for ii in range(1, sum_To_Check_Against + 1):
+    for i in range(1, num_Array + 1):
+        for ii in range(1, desired_sum + 1):
             if ii < array[i - 1]:
                 subset[i][ii] = subset[i - 1][ii]
             if ii >= array[i - 1]:
                 subset[i][ii] = subset[i - 1][ii] or subset[i - 1][ii - array[i - 1]]
 
-    return subset[num_Of_Items_in_Array][sum_To_Check_Against]
+    return subset[num_Array][desired_sum]
 
 
 if __name__ == "__main__":
