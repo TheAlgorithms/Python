736be1e93836a7be502d2fd466711b85ff7c4b08
Naman <75575170+Naman-Singla777@users.noreply.github.com>
2021-10-03 12:49:02 +0530

    Added testcase

--- maths/pythagoras_triplet_generator.py
+++ maths/pythagoras_triplet_generator.py
@@ -16,6 +16,11 @@ def pythagoras_generator(n):
     """
     It gives you the pythagoras triplet of the given number
     It works literally on every number greater than 2
+    
+    >>> pythagoras_generator(10):
+    10 24 26
+    >>> pythagoras_generator(21):
+    21 220 221
     """
     
     if n % 2 == 0:
