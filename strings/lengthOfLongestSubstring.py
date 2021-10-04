59766aa110b56535d103a7033c6530ff47a4a2a6
Rohan <cristinorocks@gmail.com>
2021-10-04 16:08:35 +0530

    Update lengthOfLongestSubstring.py

--- strings/lengthOfLongestSubstring.py
+++ strings/lengthOfLongestSubstring.py
@@ -1,4 +1,4 @@
-def length_of_longest_substring(s: str) -> int:
+def length_of_longest_substring(string: str) -> int:
 	"""
 	Given a string s, returnss the length of the longest substring without repeating characters.
 
@@ -15,12 +15,12 @@ def length_of_longest_substring(s: str) -> int:
 	"""
 	start = max_len = 0
 	used_char = {}
-	for i in range(len(s)):
-		if s[i] in used_char and start <= used_char[s[i]]:
-			start = used_char[s[i]] + 1
+	for i in range(len(string)):
+		if string[i] in used_char and start <= used_char[string[i]]:
+			start = used_char[string[i]] + 1
 		else:
 			max_len = max(max_len, i - start + 1)
-		used_char[s[i]] = i
+		used_char[string[i]] = i
 	return max_len
   
 if __name__ == "__main__":
