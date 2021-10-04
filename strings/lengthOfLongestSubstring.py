58dcfe9ce6a812a3a197a7b49a06540e97b1d5f1
Rohan <cristinorocks@gmail.com>
2021-10-04 16:07:00 +0530

    Update lengthOfLongestSubstring.py

--- strings/lengthOfLongestSubstring.py
+++ strings/lengthOfLongestSubstring.py
@@ -1,27 +1,27 @@
-def lengthOfLongestSubstring(s: str) -> int:
+def length_of_longest_substring(s: str) -> int:
 	"""
 	Given a string s, returnss the length of the longest substring without repeating characters.
 
-	>>> lengthOfLongestSubstring("abcabcbb")
+	>>> length_of_longest_substring("abcabcbb")
 	3
-	>>> lengthOfLongestSubstring("bbbbb")
+	>>> length_of_longest_substring("bbbbb")
 	1
-	>>> lengthOfLongestSubstring("pwwkew")
+	>>> length_of_longest_substring("pwwkew")
 	3
-	>>> lengthOfLongestSubstring("")
+	>>> length_of_longest_substring("")
 	0
-	>>> lengthOfLongestSubstring("a")
+	>>> length_of_longest_substring("a")
 	1
 	"""
 	start = maxLength = 0
-	usedChar = {}
+	used_char = {}
 	for i in range(len(s)):
-		if s[i] in usedChar and start <= usedChar[s[i]]:
-			start = usedChar[s[i]] + 1
+		if s[i] in used_char and start <= used_char[s[i]]:
+			start = used_char[s[i]] + 1
 		else:
-			maxLength = max(maxLength, i - start + 1)
-		usedChar[s[i]] = i
-	return maxLength
+			max_len = max(max_len, i - start + 1)
+		used_char[s[i]] = i
+	return max_len
   
 if __name__ == "__main__":
 	from doctest import testmod
