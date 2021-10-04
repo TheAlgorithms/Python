8826e64a2fd04f51e48586b31d5fb651f8c37407
Al-Mubin Khan Nabil <thetwin11100@gmail.com>
2021-10-05 03:24:55 +0600

    Update kahns_algorithm_topo.py
    added comments explaining steps of the algorithm
    added doctests
    returning the topologically sorted list instead of printing
    raising ValueError on cyclic graph instead of just printing the error
    changed camelcase variable naming to underscored naming

--- graphs/kahns_algorithm_topo.py
+++ graphs/kahns_algorithm_topo.py
@@ -1,36 +1,55 @@
-def topologicalSort(graph):
+def topologicalSort(graph: dict) -> list:
     """
     Kahn's Algorithm is used to find Topological ordering of Directed Acyclic Graph
     using BFS
-    """
-    indegree = [0] * len(graph)
-    queue = []
-    topo = []
-    cnt = 0
 
+    Return topologically sorted node list of graph
+    >>> topologicalSort({0: [1, 2], 1: [3], 2: [3], 3: [4, 5], 4: [], 5: []})
+    [0, 1, 2, 3, 4, 5]
+    >>> topologicalSort({0: [1, 2], 1: [0, 3], 2: [3], 3: [4, 5], 4: [], 5: []})
+    Traceback (most recent call last):
+    ValueError: Cycle exists in graph
+    """
+    in_degree = [0] * len(graph)
+    queue = []
+    topo_sorted = []
+    count = 0
+
+    # graph array contains out_nodes of i-th node. calculating in_degree of each node from this information
     for key, values in graph.items():
         for i in values:
-            indegree[i] += 1
+            in_degree[i] += 1
 
-    for i in range(len(indegree)):
-        if indegree[i] == 0:
+    for i in range(len(in_degree)):
+        if in_degree[i] == 0:
             queue.append(i)
 
-    while queue:
+    while (
+        queue
+    ):  # keep iterating until all the nodes with zero in_deg has been processed
         vertex = queue.pop(0)
-        cnt += 1
-        topo.append(vertex)
-        for x in graph[vertex]:
-            indegree[x] -= 1
-            if indegree[x] == 0:
-                queue.append(x)
+        count += 1
+        topo_sorted.append(vertex)  # every time we deque a node from to_process, that means
+        # the returned node has zero in_deg so it can go into the topo list
+        for node in graph[vertex]:
+            in_degree[
+                node
+            ] -= 1  # now that we have added current_node into sorted list we can
+            # decrease in_degree of every node that depends on it by 1
+            if in_degree[node] == 0:
+                queue.append(
+                    node
+                )  # if the node that we decreased in_degree of has reached zero in_degree,
+                # that means this node is ready to be processed and added to topo list
 
-    if cnt != len(graph):
-        print("Cycle exists")
+    # if the graph contained any cycle in_degree of some of the nodes will never
+    # be zero so length of sorted list will be smaller than nodes on the graph
+    if count != len(graph):
+        raise ValueError("Cycle exists in graph")
     else:
-        print(topo)
+        return topo_sorted
 
 
 # Adjacency List of Graph
 graph = {0: [1, 2], 1: [3], 2: [3], 3: [4, 5], 4: [], 5: []}
-topologicalSort(graph)
+print(topologicalSort(graph))
