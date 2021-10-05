c6ae4eeb3ec3ec3ad08c1177780727f495d617f7
sai_sudha <51985990+SaiSudhaV@users.noreply.github.com>
2021-10-06 00:06:54 +0530

    Update continuous_height.py

--- data_structures/binary_tree/continuous_height.py
+++ data_structures/binary_tree/continuous_height.py
@@ -11,6 +11,7 @@ class BST:
         self.root = None
 
     def create(self, value : int) -> None:  #value is the element to be inserted to BST
+        doctest.testmod()
         if self.root == None:
             self.root = node(value)
         else:
@@ -32,6 +33,7 @@ class BST:
                     break
 
 def height_of_tree(root : Node) -> int:
+    doctest.testmod()
     if not root:
         return -1
     return 1 + max(height_of_tree(root.right), height_of_tree(root.left))
