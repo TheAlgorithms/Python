ce6b36964092958e57a3ffd91b995e8fdedef74d
Soggy_Pancake <54160598+Soggy-Pancake@users.noreply.github.com>
2021-10-05 15:33:56 -0700

    Use DocTest

--- strings/UniqueCharacter.py
+++ strings/UniqueCharacter.py
@@ -1,23 +1,31 @@
-def CharUnique(S):
+def CharUnique(S: str):
+	"""
+	>>> CharUnique('deacidified')
+	False
+	>>> CharUnique('keraunoscopia')
+	False
+	>>> CharUnique('layout')
+	True
+	>>> CharUnique('brand')
+	True
+	>>> CharUnique('texture')
+	False
+	>>> CharUnique('ovalness')
+	False
+	>>> CharUnique('unglove')
+	True
+	"""
+
 	#for i scan ahead for same char
 	for i in range(len(S)):
 		#c is the one that i is compared against
 		for c in range(i + 1, len(S)):
-			"""
-			Ex: Hello!
-			i = 0 and C starts at i + 1 so the same char isn't compared against itself
-			S[0] = H and S[1] = e
-			
-			H is compared to every other char ahead of it
-			c loop finishes then e is compared to everything ahead of it
-			
-			S[2] = l S[3] = l; 2 characters are repeated and not unique return false
-			if nothing is found by end of string return true
-			"""
 			if S[i] == S[c]:
 				return(False)
 	return(True)
 
-#Given a string "S", Check if all characters in the string is unique or not
-S = input("Input String: ")
-print(CharUnique(S))
+#Given a string "S", Check if all characters in the string are unique or not if not return False
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
