6da76d2cd523923bf5e5b9bb70c12aac5a7c1f5e
appledora <nazia89@student.sust.edu>
2021-10-05 19:58:57 +0600

    removed session related libraries because problem with mypy

--- web_programming/get_google_search_results.py
+++ web_programming/get_google_search_results.py
@@ -1,18 +1,21 @@
-import urllib.parse
-
 import requests
 from bs4 import BeautifulSoup
-from requests import Session
 
 BASE_URL = "https://www.google.com"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+    + "AppleWebKit/537.36 (KHTML, like Gecko)"
+    + "Chrome/72.0.3538 102 Safari/537.36 Edge/18.19582"
+}
 
 
-def parse_results(query: str = "") -> list:
+def parse_results(query: str = "", num_q: int = 20) -> list:
     """ "Reads google search result page for the given query and
     stores all the results in with title, link and descriptions
     in a list.
     Args:
-        query (str): The search term provided by the user.
+        query : The search term provided by the user.
+        num_q : The number of search results to fetch.
     Returns:
         list: list with search results
 
@@ -21,22 +24,30 @@ def parse_results(query: str = "") -> list:
     >>> len(parse_results("")) == 0
     True
     """
-    query = urllib.parse.quote_plus(query)
-    url = "https://www.google.com/search?q=" + query
+    params = {"q": str(query)}
     response = None
     try:
-        session = Session()
-        response = session.get(url)
-
+        response = requests.get(BASE_URL + "/search", headers=headers, params=params)
     except requests.exceptions.RequestException:
         return []
-
     css_identifier_result = ".tF2Cxc"
     css_identifier_title = "h3"
     css_identifier_link = ".yuRUbf a"
-    css_identifier_text = ".IsZvec"
-    results = response.html.find(css_identifier_result)
-    soup = BeautifulSoup(response.html.html, "html.parser")
+    css_identifier_text = "#rso .lyLwlc"
+
+    soup = BeautifulSoup(response.text, "lxml")
+    results = soup.select(css_identifier_result)
+    output = []
+    for result in results:
+        title = result.select_one(css_identifier_title).text
+        link = result.select_one(css_identifier_link)["href"]
+        try:
+            snippet = result.select_one(css_identifier_text).text
+        except Exception:
+            snippet = None
+        item = {"title": title, "link": link, "text": snippet}
+        output.append(item)
+    stat_table = BeautifulSoup(response.content, "html.parser").find("table")
     stat_table = soup.find("table")
     if stat_table is None:
         return []
@@ -44,42 +55,29 @@ def parse_results(query: str = "") -> list:
     next_page = []
 
     for item in table_data:
-        next_page.append(item.attrs["href"])
-
-    output = []
-
-    for result in results:
-
-        item = {
-            "title": result.find(css_identifier_title, first=True).text,
-            "link": result.find(css_identifier_link, first=True).attrs["href"],
-            "text": result.find(css_identifier_text, first=True).text,
-        }
-
-        output.append(item)
-
+        next_page.append(item["href"])
     for next_page_link in next_page:
         new_link = BASE_URL + next_page_link
         try:
-            session = HTMLSession()
-            response = session.get(new_link)
+            response = requests.get(new_link, headers=headers)
 
         except requests.exceptions.RequestException:
             continue
-        results = response.html.find(css_identifier_result)
+        soup = BeautifulSoup(response.text, "lxml")
+        results = soup.select(css_identifier_result)
         for result in results:
-
-            item = {
-                "title": result.find(css_identifier_title, first=True).text,
-                "link": result.find(css_identifier_link, first=True).attrs["href"],
-                "text": result.find(css_identifier_text, first=True).text,
-            }
-
+            title = result.select_one(css_identifier_title).text
+            link = result.select_one(css_identifier_link)["href"]
+            try:
+                snippet = result.select_one(css_identifier_text).text
+            except Exception:
+                snippet = None
+            item = {"title": title, "link": link, "text": snippet}
             output.append(item)
     return output
 
 
-def write_google_search_results(query: str = "potato", filename: str = "") -> str:
+def write_google_search_results(query: str = "", filename: str = "") -> str:
     """Writes the Google search result page for the given query into a local file.
     Args:
         query: The search term provided by the user. Default is "potato".
@@ -97,13 +95,15 @@ def write_google_search_results(query: str = "potato", filename: str = "") -> st
     True
     >>> write_google_search_results("", "") != None
     True
-    >>> "python" in write_google_search_results("python", "test")
+    >>> "test" in write_google_search_results("python", "test")
     True
-    >>> "test" in write_google_search_results("", "test")
+    >>> "test1" in write_google_search_results("", "test1")
     True
     >>> "potato" in write_google_search_results("", "")
     True
     """
+    if query == "":
+        query = "potato"
     if filename == "":
         filename = query + "-query.txt"
     elif not filename.endswith(".txt"):
@@ -120,7 +120,7 @@ def write_google_search_results(query: str = "potato", filename: str = "") -> st
 
 if __name__ == "__main__":
     query = input("Enter query: ") or "potato"
-    filename = input("Enter optional filename: ")
+    filename = input("Enter optional filename: ") or "smt"
     print(f"Searching Google for {query} ....")
     filename = write_google_search_results(query, filename)
     print(f"File saved into {filename}")
