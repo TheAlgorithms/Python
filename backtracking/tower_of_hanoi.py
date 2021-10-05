6288b53a2308a577ab458ec810fd1cf67662776a
Ayushmaan <ayushmaan1362@gmail.com>
2021-10-05 22:51:03 +0530

    Added a recursive algorithm for the tower of hanoi problem

---
+++ backtracking/tower_of_hanoi.py
@@ -0,0 +1,13 @@
+'''
+    Number of turns it takes it takes to move n rings across 3 rods A,B and C from source rod A to destination rod C.
+    
+'''
+def tower_of_hanoi(n, source, destination, aux):
+    if(n == 1):
+        print("Move disk ",n," from ", source, " to ",destination)
+        return
+    tower_of_hanoi(n-1,source, aux, destination)
+    print("Move disk ",n," from ",source," to ",destination)
+    tower_of_hanoi(n-1,aux,destination,source)
+
+tower_of_hanoi(5,'A','B','C')
