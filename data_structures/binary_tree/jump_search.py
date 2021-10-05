b65254d4a3b1f87469a8895296345ddd8c1ede48
Divyanshu Kumar <53843818+divyanshugit@users.noreply.github.com>
2021-10-05 05:40:05 +0530

    Update jump_search.py

--- data_structures/binary_tree/jump_search.py
+++ data_structures/binary_tree/jump_search.py
@@ -1,6 +1,19 @@
+"""
+Jump Search Algorithm
+https://en.wikipedia.org/wiki/Jump_search
+Author @divyanshugit 
+"""
 import math
-
 def JumpSearch (lst, val): #lst->list, val->value
+    """
+    -At first, It determines the jump size by computing math.sqrt(len(lys)).
+    -Next, it computes the value of the right variable, which is the minimum of the length of the array minus 1, or the value of left+jump.
+    -It checks whether our search element.
+    -In the end we calculate the position of element.
+    >>>JumpSearch([1,2,3,4,5,6,7,8,9], 5)
+    4
+    """
+    
     length = len(lst)
     jump = int(math.sqrt(length))
     left, right = 0, 0
@@ -18,3 +31,4 @@ def JumpSearch (lst, val): #lst->list, val->value
             return i
         i += 1
     return -1
+
