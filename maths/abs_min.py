48b56a5280d821b655c0bd5dc031cd303fff0213
Anshu Saini <65893784+anshu189@users.noreply.github.com>
2021-10-07 03:09:28 +0530

    More Simplified and new Algorithm for abs min
    Remove unused/invalid imports
    More user friendly and simple

--- maths/abs_min.py
+++ maths/abs_min.py
@@ -1,6 +1,19 @@
+import doctest
+    
 class abshead:
     def abs_min(self, x: list[int]) -> int:
         
+        '''
+        >> abs_min([4,73,12,0])
+        0
+        >> abs_min([-4,-73,12,-1])
+        -1
+        >>> abs_max([])
+        Traceback (most recent call last):
+        ValueError: abs_min() arg is an empty sequence
+        
+        '''
+                
         if len(x) == 0:
             raise ValueError("abs_min() arg is an empty sequence")
         j = x[0]
@@ -9,7 +22,7 @@ class abshead:
                 j = i
         return j
 
-a1 = abshead()
+a1 = abshead()  #defining class object
 
 def main():
     a = [-3, -4, 2, -1, -11]
@@ -17,4 +30,6 @@ def main():
 
 
 if __name__ == "__main__":
+    
+    doctest.testmod(verbose=True)
     main()
