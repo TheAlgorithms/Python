5ba558e27836d3ed0e04ce2ee18c2534917d59fd
Anshu Saini <65893784+anshu189@users.noreply.github.com>
2021-10-07 02:54:28 +0530

    More Simplified Algorithm for abs min
    Remove unused/invalid imports
    More user friendly and simple

--- maths/abs_min.py
+++ maths/abs_min.py
@@ -1,35 +1,20 @@
-from __future__ import annotations
+class abshead:
+    def abs_min(self, x: list[int]) -> int:
+        
+        if len(x) == 0:
+            raise ValueError("abs_min() arg is an empty sequence")
+        j = x[0]
+        for i in x:
+            if abs(i) < abs(j):
+                j = i
+        return j
 
-from .abs import abs_val
-
-
-def abs_min(x: list[int]) -> int:
-    """
-    >>> abs_min([0,5,1,11])
-    0
-    >>> abs_min([3,-10,-2])
-    -2
-    >>> abs_min([])
-    Traceback (most recent call last):
-        ...
-    ValueError: abs_min() arg is an empty sequence
-    """
-    if len(x) == 0:
-        raise ValueError("abs_min() arg is an empty sequence")
-    j = x[0]
-    for i in x:
-        if abs_val(i) < abs_val(j):
-            j = i
-    return j
-
+a1 = abshead()
 
 def main():
-    a = [-3, -1, 2, -11]
-    print(abs_min(a))  # = -1
+    a = [-3, -4, 2, -1, -11]
+    print(a1.abs_min(a))  # abs_min = -1
 
 
 if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod(verbose=True)
     main()
