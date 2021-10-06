3e5c635cc9b9f94dc7cdf2a01662584a8c1d0eda
Krish <68506145+KrishAgarwal2811@users.noreply.github.com>
2021-10-06 17:29:47 +0530

    added: shifting string's character
    I know it's useless but it could be helpful to someone who is getting started with ASCII :)

---
+++ strings/shift_string.py
@@ -0,0 +1,32 @@
+# Shifts the charcter by 1 through it's ACII
+
+
+def shiftString(originalStr: str, shift: int = 1):
+    """
+    Shifts the charcter by 1 through it's ACII
+    >>> shiftString('Hello')
+    'Ifmmp'
+
+    >>> shiftString('something')
+    'tpnfuijoh'
+
+    >>> shiftString('heyaaa', 2)
+    'jg{ccc'
+    """
+    if shift < 1:
+        return Exception("Shift can't be less than 1")
+    if len(originalStr) < 1:
+        return Exception("String can't be empty")
+    newStr = ""
+    # ord function gives the ascii of the charcter for ex. ord(a) gives 97
+    # and it's inverse is chr which will turn 97 to a
+    for i in originalStr:
+        asc = ord(i)
+        newStr += chr(asc + shift)
+    return newStr
+
+
+if __name__ == "__main__":
+    from doctest import testmod
+
+    testmod()
