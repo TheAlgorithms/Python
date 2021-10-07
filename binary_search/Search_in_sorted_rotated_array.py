52f29fbfa7d82aeb32b94ffcd7b681db1c9994cb
Harshit Kumar <kumarharshit850@gmail.com>
2021-10-07 16:52:22 +0530

    search in rotated sorted array is added

---
+++ binary_search/Search_in_sorted_rotated_array.py
@@ -0,0 +1,29 @@
+class Solution:
+    # @param {integer[]} numss
+    # @param {integer} target
+    # @return {integer}
+    # Algorithmic Concept: Binary Search
+    def search(self, nums, target):
+        if not nums:
+            return -1
+
+        low, high = 0, len(nums) - 1
+
+        while low <= high:
+            mid = (low + high) / 2
+            if target == nums[mid]:
+                return mid
+
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target <= nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] <= target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+
+        # Target not foound!
+        return -1
