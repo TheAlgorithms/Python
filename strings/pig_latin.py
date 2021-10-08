cbd67d0e796308da1ef2c9d4454a73dba9f58d1b
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2021-10-08 19:38:23 +0530

    added type hints and corrected variables and functions to follow snake_case

--- strings/pig_latin.py
+++ strings/pig_latin.py
@@ -5,22 +5,23 @@ A word in english is provided, you have to convert it into pig latin.
 Pig Latin takes the word and checks if the word begins with a vowel, if it does then just add "way" at the end of the string. Else take the consonant (or the consonant cluster) move it to the end of the word and suffix an "ay". 
 """
 
-def translatePigLatin(str, charPos = 0) -> str:
+def translate_pig_latin(str: str, char_pos: int = 0) -> str:
     """
     This function will convert english word to pig latin word
     """
     arr = ['a', 'e', 'i', 'o', 'u']
     if(str[0] in arr):
-        if(charPos == 0):
+        if(char_pos == 0):
             return str + 'way'
         return str + "ay"
     else:
-        if(charPos == len(str)):
+        if(char_pos == len(str)):
             return str + 'ay'
-        translatePigLatin(str[1:] + str[0], charPos + 1)
+        translate_pig_latin(str[1:] + str[0], char_pos + 1)
 
 
 if __name__ == "__main__":
     from doctest import testmod
 
+    translate_pig_latin("owl")
     testmod()
