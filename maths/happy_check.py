36b85f832128250f3989a373c54afa9239a034a5
sire-ambrose <68953964+sire-ambrose@users.noreply.github.com>
2021-10-08 06:25:10 +0100

    Update happy_check.py

--- maths/happy_check.py
+++ maths/happy_check.py
@@ -2,19 +2,20 @@
 
 
 def check_happy() -> string:
-    
-    #A happy number is a number which eventually reaches 1 when replaced by the sum of the square of each digit. 
-    #For instance, 13 is a happy number because 1**2 + 3**2 = 10 , and 1**2 + 0**2 =1 . 
-    #On the other hand, 4 is not a happy number because the sequence starting with 4**2 = 16 and 1**2 + 6**2 = 37 eventually reaches 2**2 + 0**2 = 4, the number that started the sequence, 
+    '''
+    A happy number is a number which eventually reaches 1 when replaced by the sum of the square of each digit. 
+    For instance, 13 is a happy number because 1**2 + 3**2 = 10 , and 1**2 + 0**2 =1 . 
+    On the other hand, 4 is not a happy number because the sequence starting with 4**2 = 16 and 1**2 + 6**2 = 37 eventually reaches 2**2 + 0**2 = 4, the number that started the sequence, 
     #and so the process continues in an infinite cycle without ever reaching 1. A number which is not happy is called sad or unhappy. 
     
-    #   >>> Enter number: 13
-    #   13 is a happy number
+    >>> Enter number: 13
+    13 is a happy number
     
-    #   >>> Enter number: 45
-    #   45 is a sad number
+    >>> Enter number: 45
+    45 is a sad number
     
-    #visit https://en.wikipedia.org/wiki/Happy_number for more clarification
+    visit https://en.wikipedia.org/wiki/Happy_number for more clarification
+    '''
 
 
     n=int(input('Enter number: '))
