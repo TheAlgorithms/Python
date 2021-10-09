da80b4f3722c2ebb175728369a5bd10b02cc4f03
Dharmendra <78801686+ProgrammingPirates@users.noreply.github.com>
2021-10-09 10:04:57 +0530

    Create Jump Search.py

---
+++ searches/Jump Search.py
@@ -0,0 +1,20 @@
+import math
+
+def JumpSearch (lys, val):
+    length = len(lys)
+    jump = int(math.sqrt(length))
+    left, right = 0, 0
+    while left < length and lys[left] <= val:
+        right = min(length - 1, left + jump)
+        if lys[left] <= val and lys[right] >= val:
+            break
+        left += jump;
+    if left >= length or lys[left] > val:
+        return -1
+    right = min(length - 1, right)
+    i = left
+    while i <= right and lys[i] <= val:
+        if lys[i] == val:
+            return i
+        i += 1
+    return -1
