a11f66bae61282664aa3849c32639d3a5527ff28
Rishi <anotherishi@gmail.com>
2021-10-10 00:43:27 +0530

    pre-commit issue

--- project_euler/problem_073/sol1.py
+++ project_euler/problem_073/sol1.py
@@ -1,12 +1,16 @@
 """
 Project Euler Problem 73: https://projecteuler.net/problem=73
 
-Consider the fraction, n/d, where n and d are positive integers. If n<d and HCF(n,d)=1, it is called a reduced proper fraction.
-If we list the set of reduced proper fractions for d ≤ 8 in ascending order of size, we get:
-1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5, 5/8, 2/3, 5/7, 3/4, 4/5, 5/6, 6/7, 7/8
+Consider the fraction, n/d, where n and d are positive integers. If n<d and HCF(n,d)=1,
+it is called a reduced proper fraction.
+If we list the set of reduced proper fractions for d ≤ 8 in ascending order of size,
+we get:
+1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5, 5/8, 2/3, 5/7,
+3/4, 4/5, 5/6, 6/7, 7/8
 It can be seen that there are 3 fractions between 1/3 and 1/2.
 
-How many fractions lie between 1/3 and 1/2 in the sorted set of reduced proper fractions for d ≤ 12,000?
+How many fractions lie between 1/3 and 1/2 in the sorted set of reduced
+proper fractions for d ≤ 12,000?
 
 References
   - https://en.wikipedia.org/wiki/Euclidean_division
@@ -18,7 +22,8 @@ from math import gcd
 
 def solution(d: int = 12000) -> int:
     """
-    Returns the number of fractions lying between 1/3 and 1/2 in the sorted set of reduced proper fractions for d ≤ 12,000.
+    Returns the number of fractions lying between 1/3 and 1/2 in the sorted set of
+    reduced proper fractions for d ≤ 12,000.
 
     >>> solution(500)
     12687
@@ -37,7 +42,7 @@ def solution(d: int = 12000) -> int:
         for denominator in range(numerator + 1, d + 1):
             # count only if the fractions are reduced and lie in the range (1/3, 1/2)
             if (
-                1/3 < numerator / denominator < 0.5
+                1 / 3 < numerator / denominator < 0.5
                 and gcd(numerator, denominator) == 1
             ):
                 result += 1
