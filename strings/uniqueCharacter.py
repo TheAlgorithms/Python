2440d503c55235f64c6a379c5929f46eca226239
RUDRA SHAH <heysiri876@gmail.com>
2021-10-11 01:05:14 +0530

    Added Unique Character Algorithm

--- strings/uniqueCharacter.py
+++ strings/uniqueCharacter.py
@@ -1,6 +1,8 @@
 def uniqueness(string: str):
-    if(set(string)==len(string)):
+    if(len(set(string))==len(string)):
         return string+" - CONTAINS ALL UNIQUE CHARACTERS"
     else:
         return string+" - DOSEN'T CONTAINS UNIQUE CHARACTERS"
     return None
+
+print(uniqueness("abcd"))
