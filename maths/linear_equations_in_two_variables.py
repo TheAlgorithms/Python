dffc7898759be59e65f9f08e876748985f3b23ab
Rohanrbharadwaj <89947037+Rohanrbharadwaj@users.noreply.github.com>
2021-10-11 02:42:40 +0530

    Create linear_equations_in_two_variables.py

---
+++ maths/linear_equations_in_two_variables.py
@@ -0,0 +1,20 @@
+'''
+Solves linear equations in two variables
+Inputs : Co-efficients of x and y in a pair of equations, Dependents
+Outputs : Values of x and y
+'''
+
+def solve(x_coefficient_1: float, y_coefficient_1: float, dependent_1: float, x_coefficient_2: float, y_coefficient_2: float, dependent_2: float) -> list:
+  '''
+  >>> solve(4,5,20,1,2,13)
+  [-8.333333333333332, 10.666666666666666]
+  '''
+  import numpy
+  coefficients = numpy.array([ [x_coefficient_1, y_coefficient_1], [x_coefficient_2, y_coefficient_2] ])
+  dependents = numpy.array([dependent_1, dependent_2])
+  answers = numpy.linalg.solve(coefficients, dependents)
+  return list(answers)
+
+if __name__ == '__main__':
+  from doctest import testmod
+  testmod()
