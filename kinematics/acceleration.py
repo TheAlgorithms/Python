7d5c3a1e649d70b7952e9ee108086b35fdce1a19
SaucyGames05 <47290046+SaucyGames05@users.noreply.github.com>
2021-10-11 01:59:36 -0700

    Update acceleration.py

--- kinematics/acceleration.py
+++ kinematics/acceleration.py
@@ -12,7 +12,9 @@
 # very close. Typically you would only show sig 
 # figs when you calculate with these equations.
 
-# Doctests are all tested with initial_velocity=0, final_velocity=104.96, x0=0, final_position=1720, a=3.2, t=32.8, r=2
+# Doctests are all tested with initial_velocity=0, final_velocity=104.96, x0=0, 
+# final_position=1720, acceleration=3.2, time_elapsed=32.8, r=2
+
 # These were calculated by hand so the output for distance is different, but the outputs 
 # are more accurate than the one used for the example.
 
