0071c8256479aa1eca2f1e2eaa1e982ed5837ecd
SaucyGames05 <47290046+SaucyGames05@users.noreply.github.com>
2021-10-11 03:37:07 -0700

    velocity.py correctly formatted

--- kinematics/velocity.py
+++ kinematics/velocity.py
@@ -5,117 +5,152 @@
 # Here is an explanation on the "big 4 equations", all of which were used to make this
 # algorithm: https://www.physicsclassroom.com/class/1dkin/Lesson-6/Kinematic-Equations
 
-# These are how we represent specific values when calculating using kinematic equations
-# x0 = Initial Position
-# x = Final Postion
-# v0 = Intial Velocity
-# v = Final Velocity
-# a = acceleration
-# t = time
-
-# r = rounding place
-
-# Output will always be a float, so if you need to round, set r to whatever place you 
+# Output will always be a float, so if you need to round, set r to whatever place you
 # are rounding to
 
-# NOTE: Not all of these equations will give you an exactly same answer, but will be 
-# very close. Typically you would only show sig 
+# NOTE: Not all of these equations will give you an exactly same answer, but will be
+# very close. Typically you would only show sig
 # figs when you calculate with these equations.
 
-# Doctests are all tested with v0=0, v=104.96, x0=0, x=1720, a=3.2, t=32.8, r=2
-# These were calculated by hand so the output for distance is different, but the outputs 
+# Doctests are all tested with intial_velocity=0, v=104.96, intial_position=0,
+# final_position=1720, acceleration=3.2, time_elapsed=32.8, retaining_wall=2
+# These were calculated by hand so the output for distance is different, but the outputs
 # are more accurate than the one used for the example.
 
 # Typing hints
 from typing import Union
 
+
 # Square root function just so I don't have to import math and it looks cleaner
-def sqrt(num):
+def sqrt(num: Union[int, float]) -> Union[int, float]:
     if num < 0:
         multiplier = -1
         final_result = abs(num)
     else:
         multiplier = 1
         final_result = num
-        
-    return multiplier*(final_result**0.5)
+
+    return multiplier * (final_result ** 0.5)
+
 
 # FINDING VELOCITY #
 
+
 class NotEnoughInfo(Exception):
     pass
-  
-def velocity(x0: Union[int,float] = None, x: Union[int,float] = None, 
-             v0: Union[int,float] = None, a: Union[int,float] = None, 
-             t: Union[int,float] = None, r: int = None) -> float:
+
+
+def velocity(
+    intial_position: Union[int, float] = None,
+    final_position: Union[int, float] = None,
+    intial_velocity: Union[int, float] = None,
+    acceleration: Union[int, float] = None,
+    time_elapsed: Union[int, float] = None,
+    retaining_wall: int = None,
+) -> float:
     """
-    Find velocity with given v0, x0, x, a, or t.
-    
-    :param v0: int, float
-    :param x0: int, float
-    :param x: int, float
-    :param a: int, float
-    :param t: int, float
-    :param r: int
+    Find velocity with given intial_velocity, intial_position, final_position,
+    acceleration, or time_elapsed.
+
+    :param intial_velocity: int, float
+    :param intial_position: int, float
+    :param final_position: int, float
+    :param acceleration: int, float
+    :param time_elapsed int, float
+    :param retaining_wall: int
     :return: float
-    
-    >>> velocity(v0=0, a=3.2, t=32.8, r=2)
+
+    >>> velocity(intial_velocity=0, acceleration=3.2, time_elapsed=32.8,
+    ... retaining_wall=2)
     104.96
-    >>> velocity(v0=0, x0=0, x=1720, t=32.8, r=2)
+    >>> velocity(intial_velocity=0, intial_position=0, final_position=1720,
+    ... time_elapsed=32.8, retaining_wall=2)
     104.88
-    >>> velocity(v0=0, x0=0, x=1720, a=3.2, r=2)
+    >>> velocity(intial_velocity=0, intial_position=0, final_position=1720,
+    ... acceleration=3.2, retaining_wall=2)
     104.92
-    
+
     The variable 'r' can only be an integer.
-    >>> velocity(v0=0, x0=0, x=1720, a=3.2, r=[2])
+    >>> velocity(intial_velocity=0, intial_position=0, final_position=1720,
+    ... acceleration=3.2, retaining_wall=[2])
     Traceback (most recent call last):
         ...
     ValueError: r cannot be type 'list'
-    
+
     When not given enough information, you will get an error.
     Ex. Trying to find velocity without initial position, final position
     and time
-    >>> velocity(v0=0, a=3.2, r=2)
+    >>> velocity(intial_velocity=0, acceleration=3.2, retaining_wall=2)
     Traceback (most recent call last):
         ...
     NotEnoughInfo: Not enough information to complete calculation.
     """
-    if not x0 and not x:
-        if None in (v0,a,t):
+    if not intial_position and not final_position:
+        if None in (intial_velocity, acceleration, time_elapsed):
             raise NotEnoughInfo("Not enough information to complete calculation.")
         else:
-            if r is not None:
-                if type(r) != int:
-                    raise ValueError(f"r cannot be type '{type(r).__name__}'")
+            if retaining_wall is not None:
+                if type(retaining_wall) != int:
+                    raise ValueError(
+                        f"r cannot be type '{type(retaining_wall).__name__}'"
+                    )
                 else:
-                    return round(float(v0+(a*t)), r)
+                    return round(
+                        float(intial_velocity + (acceleration * time_elapsed)),
+                        retaining_wall,
+                    )
             else:
-                return float(v0+(a*t))
+                return float(intial_velocity + (acceleration * time_elapsed))
     else:
-        if not a and not t:
+        if not acceleration and not time_elapsed:
             raise NotEnoughInfo("Not enough information to complete calculation.")
         else:
-            if not a:
-                if None in (v0,x0,x):
-                    raise NotEnoughInfo("Not enough information to complete calculation.")
+            if not acceleration:
+                if None in (intial_velocity, intial_position, final_position):
+                    raise NotEnoughInfo(
+                        "Not enough information to complete calculation."
+                    )
                 else:
-                    if r is not None:
-                        if type(r) != int:
-                            raise ValueError(f"r cannot be type '{type(r).__name__}'")
+                    if retaining_wall is not None:
+                        if type(retaining_wall) != int:
+                            raise ValueError(
+                                f"r cannot be type '{type(retaining_wall).__name__}'"
+                            )
                         else:
-                            return round(float((((x-x0)/t)*2)-v0), r)
+                            return round(
+                                float(
+                                    (
+                                        (
+                                            (final_position - intial_position)
+                                            / time_elapsed
+                                        )
+                                        * 2
+                                    )
+                                    - intial_velocity
+                                ),
+                                retaining_wall,
+                            )
                     else:
-                        return float((((x-x0)/t)*2)-v0)
+                        return float(
+                            (((final_position - intial_position) / time_elapsed) * 2)
+                            - intial_velocity
+                        )
             else:
-                if None in (v0,x0,x):
-                    raise NotEnoughInfo("Not enough information to complete calculation.")
+                if None in (intial_velocity, intial_position, final_position):
+                    raise NotEnoughInfo(
+                        "Not enough information to complete calculation."
+                    )
                 else:
-                    solution = v0**2+(2*a*(x-x0))
+                    solution = intial_velocity ** 2 + (
+                        2 * acceleration * (final_position - intial_position)
+                    )
                     final_result = sqrt(solution)
-                    if r is not None:
-                        if type(r) != int:
-                            raise ValueError(f"r cannot be type '{type(r).__name__}'")
+                    if retaining_wall is not None:
+                        if type(retaining_wall) != int:
+                            raise ValueError(
+                                f"r cannot be type '{type(retaining_wall).__name__}'"
+                            )
                         else:
-                            return round(float(final_result), r)
+                            return round(float(final_result), retaining_wall)
                     else:
                         return float(final_result)
