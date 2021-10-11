fbdad8c5084e640a833cee374ef1aecf553fdad2
SaucyGames05 <47290046+SaucyGames05@users.noreply.github.com>
2021-10-11 03:12:00 -0700

    distance.py correctly formatted

--- kinematics/distance.py
+++ kinematics/distance.py
@@ -7,17 +7,18 @@
 
 # r = rounding place
 
-# Output will always be a float, so if you need to round, set r to whatever place you 
+# Output will always be a float, so if you need to round, set r to whatever place you
 # are rounding to
 
-# NOTE: Not all of these equations will give you an exactly same answer, but will be 
-# very close. Typically you would only show sig 
+# NOTE: Not all of these equations will give you an exactly same answer, but will be
+# very close. Typically you would only show sig
 # figs when you calculate with these equations.
 
-# Doctests are all tested with initial_velocity=0, final_velocity=104.96, initial_position=0,
-# final_position=1720, acceleration=3.2, time_elapsed=32.8, r=2
+# Doctests are all tested with initial_velocity=0, final_velocity=104.96,
+# initial_position=0, final_position=1720, acceleration=3.2, time_elapsed=32.8,
+# retaining_wall=2
 
-# These were calculated by hand so the output for distance is different, but the outputs 
+# These were calculated by hand so the output for distance is different, but the outputs
 # are more accurate than the one used for the example.
 
 # In all cases distance is final_position-initial_position
@@ -29,61 +30,97 @@ from typing import Union
 
 # FINDING DISTANCE #
 
+
 class NotEnoughInfo(Exception):
     pass
 
-# r is rounding place
-def distance(initial_velocity: Union[int,float] = None, final_velocity: Union[int,float] = None, 
-             initial_position: Union[int,float] = None, acceleration: Union[int,float] = None, 
-             time_elapsed: Union[int,float] = None, r: int = None) -> float:
+
+# retaining_wall is rounding place
+def distance(
+    initial_velocity: Union[int, float] = None,
+    final_velocity: Union[int, float] = None,
+    initial_position: Union[int, float] = None,
+    acceleration: Union[int, float] = None,
+    time_elapsed: Union[int, float] = None,
+    retaining_wall: int = None,
+) -> float:
     """
-    Find distance for given initial_position, initial_velocity, final_velocity, acceleration or time_elapsed.
-    
+    Find distance for given initial_position, initial_velocity, final_velocity,
+    acceleration or time_elapsed.
+
     :param initial_velocity: int, float
     :param initial_position: int, float
     :param final_velocity: int, float
     :param acceleration: int, float
     :param time_elapsed: int, float
-    :param r: int
+    :param retaining_wall: int
     :return: float
-    
-    >>> distance(initial_velocity=0, initial_position=0, acceleration=3.2, time_elapsed=32.8, r=2)
+
+    >>> distance(initial_velocity=0, initial_position=0, acceleration=3.2,
+    ... time_elapsed=32.8, retaining_wall=2)
     1721.34
-    >>> distance(initial_velocity=0, final_velocity=104.96, initial_position=0, time_elapsed=32.8, r=2)
+    >>> distance(initial_velocity=0, final_velocity=104.96, initial_position=0,
+    ... time_elapsed=32.8, retaining_wall=2)
     1721.34
-    
+
     The variable 'r' can only be an integer.
-    >>> distance(initial_velocity=0, final_velocity=104.96, initial_position=0, time_elapsed=32.8, r='2')
+    >>> distance(initial_velocity=0, final_velocity=104.96, initial_position=0,
+    ... time_elapsed=32.8, retaining_wall='2')
     Traceback (most recent call last):
         ...
     ValueError: r cannot be type 'str'
-    
+
     When not given enough information, you will get an error.
     Ex. Trying to find distance without time
-    >>> distance(initial_velocity=0, final_velocity=104.96, initial_position=0, r=2)
+    >>> distance(initial_velocity=0, final_velocity=104.96, initial_position=0,
+    ... retaining_wall=2)
     Traceback (most recent call last):
         ...
     NotEnoughInfo: Not enough information to complete calculation.
     """
     if not acceleration:
-        if None in (initial_position,initial_velocity,final_velocity,time_elapsed):
+        if None in (initial_position, initial_velocity, final_velocity, time_elapsed):
             raise NotEnoughInfo("Not enough information to complete calculation.")
         else:
-            if r is not None:
-                if type(r) != int:
-                    raise ValueError(f"r cannot be type '{type(r).__name__}'")
+            if retaining_wall is not None:
+                if type(retaining_wall) != int:
+                    raise ValueError(
+                        f"r cannot be type '{type(retaining_wall).__name__}'"
+                    )
                 else:
-                    return round(float(initial_position+((final_velocity+initial_velocity)/2)*time_elapsed), r)
+                    return round(
+                        float(
+                            initial_position
+                            + ((final_velocity + initial_velocity) / 2) * time_elapsed
+                        ),
+                        retaining_wall,
+                    )
             else:
-                return float(initial_position+((final_velocity+initial_velocity)/2)*time_elapsed)
+                return float(
+                    initial_position
+                    + ((final_velocity + initial_velocity) / 2) * time_elapsed
+                )
     else:
-        if None in (initial_velocity,initial_position,acceleration*time_elapsed):
+        if None in (initial_velocity, initial_position, acceleration * time_elapsed):
             raise NotEnoughInfo("Not enough information to complete calculation.")
         else:
-            if r is not None:
-                if type(r) != int:
-                    raise ValueError(f"r cannot be type '{type(r).__name__}'")
+            if retaining_wall is not None:
+                if type(retaining_wall) != int:
+                    raise ValueError(
+                        f"r cannot be type '{type(retaining_wall).__name__}'"
+                    )
                 else:
-                    return round(float(initial_position+(initial_velocity*time_elapsed)+((acceleration*(time_elapsed**2))*0.5)), r)
+                    return round(
+                        float(
+                            initial_position
+                            + (initial_velocity * time_elapsed)
+                            + ((acceleration * (time_elapsed ** 2)) * 0.5)
+                        ),
+                        retaining_wall,
+                    )
             else:
-                return float(initial_position+(initial_velocity*time_elapsed)+((acceleration*(time_elapsed**2))*0.5))
+                return float(
+                    initial_position
+                    + (initial_velocity * time_elapsed)
+                    + ((acceleration * (time_elapsed ** 2)) * 0.5)
+                )
