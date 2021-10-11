e9db37a6405b68c34fa9f50c7dec018cf26698cf
SaucyGames05 <47290046+SaucyGames05@users.noreply.github.com>
2021-10-11 02:58:06 -0700

    Update acceleration.py to follow formatting rules
    Only issue is line 107 which can't be reformatted in any other way that I know of currently and is 2 spaces beyond 88

--- kinematics/acceleration.py
+++ kinematics/acceleration.py
@@ -5,17 +5,18 @@
 # Here is an explanation on the "big 4 equations", all of which were used to make this
 # algorithm: https://www.physicsclassroom.com/class/1dkin/Lesson-6/Kinematic-Equations
 
-# Output will always be a float, so if you need to round, set r to whatever place you 
-# are rounding to
+# Output will always be a float, so if you need to round, set retaining_wall to
+# whatever place you are rounding to
 
-# NOTE: Not all of these equations will give you an exactly same answer, but will be 
-# very close. Typically you would only show sig 
+# NOTE: Not all of these equations will give you an exactly same answer, but will be
+# very close. Typically you would only show sig
 # figs when you calculate with these equations.
 
-# Doctests are all tested with initial_velocity=0, final_velocity=104.96, x0=0, 
-# final_position=1720, acceleration=3.2, time_elapsed=32.8, r=2
+# Doctests are all tested with initial_velocity=0, final_velocity=104.96,
+# initial_position=0, final_position=1720, acceleration=3.2,
+# time_elapsed=32.8, retaining_wall=2
 
-# These were calculated by hand so the output for distance is different, but the outputs 
+# These were calculated by hand so the output for distance is different, but the outputs
 # are more accurate than the one used for the example.
 
 # In all cases distance is final_position-initial_position
@@ -27,66 +28,94 @@ from typing import Union
 
 # FINDING ACCELERATION
 
+
 class NotEnoughInfo(Exception):
     pass
 
-# r is the rounding place
+
+# retaining_wall is the rounding place
 def acceleration(
     initial_velocity: Union[int, float, None] = None,
-    final_velocity: Union[int, float, None] = None, 
+    final_velocity: Union[int, float, None] = None,
     final_position: Union[int, float, None] = None,
-    initial_position: Union[int, float, None] = None, 
+    initial_position: Union[int, float, None] = None,
     time_elapsed: Union[int, float, None] = None,
     retaining_wall: int = None,
 ) -> float:
     """
-    Find acceleration for given initial_position, final_position, initial_velocity, final_velocity, or time_elapsed.
-    
+    Find acceleration for given initial_position, final_position, initial_velocity,
+    final_velocity, or time_elapsed.
+
     :param initial_position: int, float
     :param final_position: int, float
     :param initial_velocity: int, float
     :param final_velocity: int, float
-    :param t: int, float
-    :param r: int
+    :param time_elapsed: int, float
+    :param retaining_wall: int
     :return: float
-    
-    >>> acceleration(initial_position=0, final_position=1720, initial_velocity=0, final_velocity=104.96, r=2)
+
+    >>> acceleration(initial_position=0, final_position=1720, initial_velocity=0,
+    ... final_velocity=104.96, retaining_wall=2)
     3.2
-    >>> acceleration(initial_velocity=0, final_velocity=104.96, time_elapsed=32.8, r=2)
+    >>> acceleration(initial_velocity=0, final_velocity=104.96, time_elapsed=32.8,
+    ... retaining_wall=2)
     3.2
-    
-    The variable 'r' can only be an integer.
-    >>> acceleration(initial_velocity=0, final_velocity=104.96, time_elapsed=32.8, r=2.1)
+
+    The variable 'retaining_wall' can only be an integer.
+    >>> acceleration(initial_velocity=0, final_velocity=104.96, time_elapsed=32.8,
+    ... retaining_wall=2.1)
     Traceback (most recent call last):
         ...
-    ValueError: r cannot be type 'float'
-    
+    ValueError: retaining_wall cannot be type 'float'
+
     When not given enough information, you will get an error.
     Ex. Trying to find acceleration with final velocity and time only
-    >>> acceleration(final_velocity=104.96, time_elapsed=32.8, r=2)
+    >>> acceleration(final_velocity=104.96, time_elapsed=32.8, retaining_wall=2)
     Traceback (most recent call last):
         ...
     NotEnoughInfo: Not enough information to complete calculation.
     """
     if not time_elapsed:
-        if None in (initial_velocity,final_velocity,final_position,initial_position):
+        if None in (initial_velocity, final_velocity, final_position, initial_position):
             raise NotEnoughInfo("Not enough information to complete calculation.")
         else:
-            if r is not None:
-                if type(r) != int:
-                    raise ValueError(f"r cannot be type {type(r).__name__}")
+            if retaining_wall is not None:
+                if type(retaining_wall) != int:
+                    raise ValueError(
+                        f"retaining_wall cannot be type {type(retaining_wall).__name__}"
+                    )
                 else:
-                    return round(float((final_velocity**2-initial_velocity**2)/(2*(final_position-initial_position))), r)
+                    return round(
+                        float(
+                            (final_velocity**2 - initial_velocity**2)
+                            / (2 * (final_position - initial_position))
+                        ),
+                        retaining_wall
+                    )
             else:
-                return float((final_velocity**2-initial_velocity**2)/(2*(final_position-initial_position)))
+                return float(
+                    (final_velocity**2 - initial_velocity**2)
+                    / (2 * (final_position - initial_position))
+                )
     else:
-        if None in (initial_velocity,final_velocity,time_elapsed):
+        if None in (initial_velocity, final_velocity, time_elapsed):
             raise NotEnoughInfo("Not enough information to complete calculation.")
         else:
-            if r is not None:
-                if type(r) != int:
-                    raise ValueError(f"r cannot be type '{type(r).__name__}'")
+            if retaining_wall is not None:
+                if type(retaining_wall) != int:
+                    raise ValueError(
+                        f"retaining_wall cannot be type '{type(retaining_wall).__name__}'"
+                    )
                 else:
-                    return round(float((final_velocity-initial_velocity)/time_elapsed), r)
+                    return round(
+                        float(
+                            (final_velocity - initial_velocity)
+                            / time_elapsed
+                        ),
+                        retaining_wall
+                    )
             else:
-                return float((final_velocity-initial_velocity)/time_elapsed)
+                return float(
+                    (final_velocity - initial_velocity)
+                    / time_elapsed
+                )
