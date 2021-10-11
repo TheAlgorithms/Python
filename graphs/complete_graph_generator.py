377f378b9d10fd3fdfde31bc02ecde49eb241c70
manueldilullo <manuel.dilullo99@gmail.com>
2021-10-11 20:25:59 +0200

    validated using pre-commit

--- graphs/complete_graph_generator.py
+++ graphs/complete_graph_generator.py
@@ -1,15 +1,17 @@
 """
 * Author: Manuel Di Lullo (https://github.com/manueldilullo)
 * Python Version: 3.9
-* Description: Complete graphs generator. Uses graphs represented with an adjacency list 
+* Description: Complete graphs generator. Uses graphs represented with an adjacency list
 
 URL: https://en.wikipedia.org/wiki/Complete_graph
 """
 
-def complete_graph(vertices_number:int) -> dict:
+
+def complete_graph(vertices_number: int) -> dict:
     """
-    function that generate a complete graph with n vertices
-    @input: n (number of vertices), directed (False if the graph is undirected, True otherwise)
+    Generates a complete graph with n vertices
+    @input: n (number of vertices),
+            directed (False if the graph is undirected, True otherwise)
     @example:
     >>> print(complete_graph(3))
     {0: [1, 2], 1: [2], 2: []}
@@ -17,13 +19,15 @@ def complete_graph(vertices_number:int) -> dict:
     g = {}
     for i in range(vertices_number):
         g[i] = []
-        for j in range(0,vertices_number):
+        for j in range(0, vertices_number):
             if i != j:
                 g[i].append(j)
     return g
 
+
 if __name__ == "__main__":
     import doctest
+
     doctest.testmod()
 
     # print(f"Complete graph:\n{complete_graph(5)}")
