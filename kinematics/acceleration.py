b10a742d722da0a6178b568a04502d531bb6680b
SaucyGames05 <47290046+SaucyGames05@users.noreply.github.com>
2021-10-10 21:55:46 -0700

    Update acceleration.py

--- kinematics/acceleration.py
+++ kinematics/acceleration.py
@@ -19,28 +19,48 @@
 # very close. Typically you would only show sig 
 # figs when you calculate with these equations.
 
+# Doctests are all tested with v0=0, v=104.96, x0=0, x=1720, a=3.2, t=32.8, r=2
+# These were calculated by hand so the output for distance is different, but the outputs 
+# are more accurate than the one used for the example.
+
 class NotEnoughInfo(Exception):
     pass
 
 def acceleration(v0=None, v=None, x=None, x0=None, t=None, r=None):
+    """
+    Find acceleration for given x0, x, v0, v, or t.
+    
+    :param x0: int, float
+    :param x: int, float
+    :param v0: int, float
+    :param v: int, float
+    :param t: int, float
+    :param r: int
+    :return: float
+    
+    >>> acceleration(x0=0, x=1720, v0=0, v=104.96, r=2)
+    3.2
+    >>> acceleration(v0=0, v=104.96, t=32.8, r=2)
+    3.2
+    """
     if not t:
-        if not v0 or not v or not x0 or not x:
+        if None in (v0,v,x,x0):
             raise NotEnoughInfo("Not enough information to complete calculation.")
         else:
             if r is not None:
                 if type(r) != int:
-                    raise ValueError(f"r cannot be type {type(r)}")
+                    raise ValueError(f"r cannot be type {type(r).__name__}")
                 else:
                     return round(float((v**2-v0**2)/(2*(x-x0))), r)
             else:
                 return float((v**2-v0**2)/(2*(x-x0)))
     else:
-        if not v0 or not v or not t:
+        if None in (v0,v,t):
             raise NotEnoughInfo("Not enough information to complete calculation.")
         else:
             if r is not None:
                 if type(r) != int:
-                    raise ValueError(f"r cannot be type {type(r)}")
+                    raise ValueError(f"r cannot be type '{type(r).__name__}'")
                 else:
                     return round(float((v-v0)/t), r)
             else:
