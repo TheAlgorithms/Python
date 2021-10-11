03b29167b0b3eeae294cf7cac8d4940261d60495
deepu14d <deepanshu2002d@gmail.com>
2021-10-12 00:01:32 +0530

    Accuracy and one more array sample added

--- machine_learning/knn_sklearn.py
+++ machine_learning/knn_sklearn.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
 
 # Load iris file
 iris = load_iris()
@@ -10,22 +11,34 @@ iris.keys()
 print(f"Target names: \n {iris.target_names} ")
 print(f"\n Features: \n {iris.feature_names}")
 
-# Train set e Test set
+# Train set and Test set
 X_train, X_test, y_train, y_test = train_test_split(
     iris["data"], iris["target"], random_state=4
 )
 
 # KNN
-
 knn = KNeighborsClassifier(n_neighbors=1)
 knn.fit(X_train, y_train)
+y_predict=knn.predict(X_test)
 
-# new array to test
-X_new = [[1, 2, 1, 4], [2, 3, 4, 5]]
+#Accuracy
+print(metrics.accuracy_score(y_test, y_predict))
+#Accuracy = 94.73%(approx)
 
-prediction = knn.predict(X_new)
+# new array sample to test
+sample = [[1, 2, 1, 4], [2, 3, 4, 5]]
+prediction = knn.predict(sample)
 
 print(
-    f"\nNew array: \n {X_new}\n\nTarget Names Prediction: \n"
+    f"\nNew array: \n {sample}\n\nTarget Names Prediction: \n"
     f" {iris['target_names'][prediction]}"
+)
+
+# one more array sample to test
+sample2 = [[2,3,4,5], [3,4,5,6]]
+prediction2 = knn.predict(samples)
+
+print(
+    f"\nNew array: \n {sample2}\n\nTarget Names Prediction: \n"
+    f" {iris['target_names'][prediction2]}"
 )
