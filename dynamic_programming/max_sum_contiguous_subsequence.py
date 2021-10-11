83a62122f4ea2f3909aa167036fc15594dd1932f
AAYUSHI KUNWAR <51057675+aayushi-kunwar13@users.noreply.github.com>
2021-10-11 18:43:06 +0530

    Added the problem statement and explanation
    Added problem statement and the explanation of the code for the problem Max Subarray Sum

--- dynamic_programming/max_sum_contiguous_subsequence.py
+++ dynamic_programming/max_sum_contiguous_subsequence.py
@@ -1,3 +1,15 @@
+"""
+ Problem Statement - Find the sum of contiguous subarray within a one-dimensional array of numbers that has the largest sum. 
+ Example - [6 , 9, -1, 3, -7, -5, 10]   Output - 17 
+ Time Complexity - O(n) 
+
+Approach  - The approach is to look for all positive contiguous segments of the array (s is used for this).
+And keep track of maximum sum contiguous segment among all positive segments (res is used for this).
+Each time we compare s with res and update res if it is greater than res 
+"""
+
+
+# Function to find the maximum contiguous subarray
 def max_subarray_sum(nums: list) -> int:
     """
     >>> max_subarray_sum([6 , 9, -1, 3, -7, -5, 10])
@@ -15,6 +27,7 @@ def max_subarray_sum(nums: list) -> int:
     return res
 
 
+# Driver function to check the above function
 if __name__ == "__main__":
     nums = [6, 9, -1, 3, -7, -5, 10]
     print(max_subarray_sum(nums))
