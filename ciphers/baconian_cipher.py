8045dd713ea191436e30a59d77a30a9cff31ac98
Rohanrbharadwaj <89947037+Rohanrbharadwaj@users.noreply.github.com>
2021-10-12 01:54:24 +0530

    Update baconian_cipher.py

--- ciphers/baconian_cipher.py
+++ ciphers/baconian_cipher.py
@@ -53,7 +53,7 @@ def encode(word: str) -> str:
     """
     encoded = ''
     for letter in word.lower():
-        if letter.isalpha() or letter == " ":
+	if letter.isalpha() or letter == " ":
 	    encoded += encode_dict[letter]
         else:
             raise Exception("encode() accepts only alphabets")
