d4c98b836ace1dcb57fcd99a2e57614365499762
Usajjad123 <usajjad@hotmail.co.uk>
2021-10-12 05:20:46 +0500

    fixed naming conventions and added comments

--- project_euler/problem_090/sol1.py
+++ project_euler/problem_090/sol1.py
@@ -1,34 +1,39 @@
 from itertools import permutations, combinations
 
-def setToBit(s):
+def set_to_bit(s):
     """
         returns bit representation of a given iterable, preferably set
+        @param s - set representing the 1 bits
     """
     res = 0
     for i in s:
         res |= 1<<i
     return res
 
-def isBitSet(n, c):
+def is_bit_set(n, c):
     """
         checks if a given bit is 1 in the given number (work around for 6 and 9)
+        @param n - the number/set to search in
+        @param c - the index to look for
     """
     if c == 6 or c == 9:
         return (1<<6 & n) or (1<<9 & n)
     return 1<<c & n
 
-def validateCubes(cubes, sq):
+def validate_cubes(cubes, sq):
     """
         verifies whether or not the selected combination of cubes is valid, by iterating through
-        all square values
+        all square values.
+        @param cubes - array of cubes represented by numbers (having six 1 bits (0-9))
+        @param sq - list of squares to validate
     """
     for s in sq:
         res = False
         for p in permutations(s):
-            curRes = True
+            cur_res = True
             for i, c in enumerate(map(int, p)):
-                curRes = curRes and isBitSet(cubes[i], c)
-            res = res or curRes
+                cur_res = cur_res and is_bit_set(cubes[i], c)
+            res = res or cur_res
         if not res:
             return False
     return True
@@ -37,17 +42,17 @@ def solution(n=9, m=2):
     """
         returns the solution of problem 90 using helper functions
         e.g 1217 for the default argument values
-        
+
         @param n - the number of squares to validate.
         @param m - the number of dice to use.
     """
     sq = [str(i**2).zfill(m) for i in range(1, n+1)]
-    allDices = [setToBit(c) for c in combinations(range(10), 6)]
-    dices = [p for p in combinations(allDices, m)]
+    all_dices = [set_to_bit(c) for c in combinations(range(10), 6)]
+    dices = [p for p in combinations(all_dices, m)]
 
     res = 0
     for d in dices:
-        if validateCubes(d, sq):
+        if validate_cubes(d, sq):
             res += 1
     return res
 
