c468cf4a412b31059c8934e96ef43b50a1f354bc
sultan arif <53237271+sultanarif-p@users.noreply.github.com>
2021-10-12 20:20:49 +0300

    Create Balanced_binary_tree.py

---
+++ data_structures/binary_tree/Balanced_binary_tree.py
@@ -0,0 +1,29 @@
+# Checking if a binary tree is height balanced in Python
+
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.left = self.right = None
+
+
+class Height:
+    def __init__(self):
+        self.height = 0
+
+
+def isHeightBalanced(root, height):
+
+    left_height = Height()
+    right_height = Height()
+
+    if root is None:
+        return True
+
+    l = isHeightBalanced(root.left, left_height)
+    r = isHeightBalanced(root.right, right_height)
+
+    height.height = max(left_height.height, right_height.height) + 1
+
+    if abs(left_height.height - right_height.height) 
