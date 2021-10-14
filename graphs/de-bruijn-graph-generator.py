045384b14b485b2a37d7b073a46551d18029e806
Mishmash <barshag@post.bgu.ac.il>
2021-10-15 02:14:54 +0300

    adding de-brujin prototpye

---
+++ graphs/de-bruijn-graph-generator.py
@@ -0,0 +1,19 @@
+
+from itertools import permutations
+#De brujin graph via matrix
+
+alphaBet = ['a','b','c']
+
+
+def generate_de_bruijn_graph(alphaBet,n):
+    return list(permutations(alphaBet,len(alphaBet)))
+    pass
+
+
+
+##Tests...
+print(generate_de_bruijn_graph(alphaBet,2))
+
+print("###################################3")
+
+print(generate_de_bruijn_graph(alphaBet,3))
