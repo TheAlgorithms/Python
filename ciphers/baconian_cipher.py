79ff3ef905d5350b80b4afc7cabeb15f2c5871d7
Rohan R Bharadwaj <89947037+Rohanrbharadwaj@users.noreply.github.com>
2021-10-14 18:34:25 +0530

    Update baconian_cipher.py

--- ciphers/baconian_cipher.py
+++ ciphers/baconian_cipher.py
@@ -73,7 +73,7 @@ def decode(coded: str) -> str:
     """
     if set(coded) - {"A", "B", " "} != set() and set(coded) - {"A", "B"} != set():
         raise Exception("decode accepts only 'A', 'B' and ' '")
-        decoded = ""
+    decoded = ""
     for word in coded.split():
         while len(word) != 0:
             decoded += decode_dict[word[:5]]
