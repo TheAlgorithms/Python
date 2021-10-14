2935f452a926b367212d68eb6f33197f240bb6cb
Christian Clauss <cclauss@me.com>
2021-10-14 12:31:21 +0200

    Update optimal_merge_pattern.py

--- greedy_methods/optimal_merge_pattern.py
+++ greedy_methods/optimal_merge_pattern.py
@@ -4,13 +4,7 @@ reference: https://www.geeksforgeeks.org/optimal-file-merge-patterns/
 
 For doctests run following command:
 python3 -m doctest -v greedy_merge_sort.py
-or
-python -m doctest -v greedy_merge_sort.py
 
-"""
-
-
-"""
 Objective
 Merge a set of sorted files of different length into a single sorted file.
 We need to find an optimal solution, where the resultant file
@@ -36,30 +30,23 @@ def optimal_merge_pattern(files: list) -> float:
         optimal_merge_cost [int]: Optimal cost to merge all those files
 
     Examples:
-    >>> optimal_merge_pattern([2,3,4])
+    >>> optimal_merge_pattern([2, 3, 4])
     14
-
-    >>> optimal_merge_pattern([5,10,20,30,30])
+    >>> optimal_merge_pattern([5, 10, 20, 30, 30])
     205
-
-    >>> optimal_merge_pattern([8,8,8,8,8])
+    >>> optimal_merge_pattern([8, 8, 8, 8, 8])
     96
     """
-
     optimal_merge_cost = 0
-
     while len(files) > 1:
         temp = 0
-
         # Consider two files with minimum cost to be merged
         for i in range(2):
             min_index = files.index(min(files))
             temp += files[min_index]
             files.pop(min_index)
-
         files.append(temp)
         optimal_merge_cost += temp
-
     return optimal_merge_cost
 
 
