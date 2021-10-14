c96d22b36137d923cf338dbae069a2eafb1a6d90
Soumil Arora <soumil789@gmail.com>
2021-10-14 19:47:28 +0530

    Added a new bug-free Morse Code Translator which can encode as well decode morse code

---
+++ Morse_Code_Translator/Morse_Code_Translator.py
@@ -0,0 +1,107 @@
+'''
+VARIABLE KEY
+'cipher' -> 'stores the morse translated form of the english string'
+'decipher' -> 'stores the english translated form of the morse string'
+'citext' -> 'stores morse code of a single character'
+'i' -> 'keeps count of the spaces between morse characters'
+'message' -> 'stores the string to be encoded or decoded'
+'''
+
+# Dictionary representing the morse code chart
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+					'C':'-.-.', 'D':'-..', 'E':'.',
+					'F':'..-.', 'G':'--.', 'H':'....',
+					'I':'..', 'J':'.---', 'K':'-.-',
+					'L':'.-..', 'M':'--', 'N':'-.',
+					'O':'---', 'P':'.--.', 'Q':'--.-',
+					'R':'.-.', 'S':'...', 'T':'-',
+					'U':'..-', 'V':'...-', 'W':'.--',
+					'X':'-..-', 'Y':'-.--', 'Z':'--..',
+					'1':'.----', '2':'..---', '3':'...--',
+					'4':'....-', '5':'.....', '6':'-....',
+					'7':'--...', '8':'---..', '9':'----.',
+					'0':'-----', ', ':'--..--', '.':'.-.-.-',
+					'?':'..--..', '/':'-..-.', '-':'-....-',
+					'(':'-.--.', ')':'-.--.-'}
+
+# Function to encrypt the string
+# according to the morse code chart
+def encrypt(message):
+	cipher = ''
+	for letter in message:
+		if letter != ' ':
+
+			# Looks up the dictionary and adds the
+			# correspponding morse code
+			# along with a space to separate
+			# morse codes for different characters
+			cipher += MORSE_CODE_DICT[letter] + ' '
+		else:
+			# 1 space indicates different characters
+			# and 2 indicates different words
+			cipher += ' '
+
+	return cipher
+
+# Function to decrypt the string
+# from morse to english
+def decrypt(message):
+
+	# extra space added at the end to access the
+	# last morse code
+	message += ' '
+
+	decipher = ''
+	citext = ''
+	for letter in message:
+
+		# checks for space
+		if (letter != ' '):
+
+			# counter to keep track of space
+			i = 0
+
+			# storing morse code of a single character
+			citext += letter
+
+		# in case of space
+		else:
+			# if i = 1 that indicates a new character
+			i += 1
+
+			# if i = 2 that indicates a new word
+			if i == 2 :
+
+				# adding space to separate words
+				decipher += ' '
+			else:
+
+				# accessing the keys using their values (reverse of encryption)
+				decipher += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT
+				.values()).index(citext)]
+				citext = ''
+
+	return decipher
+
+# Function to run the program
+def main():
+
+	x = input("Do you want to encode or decode a morse code? ")
+
+	# If the user wants to encode in a morse code
+	if x == "encode" or x == "Encode" or x == "ENCODE":
+		message = input("Enter the English string: ")
+		result = encrypt(message.upper())
+		print ("The given English string in morse code is ", result)
+
+	# If the user wants to decode a morse code
+	elif x == "decode" or x == "Decode" or x == "DECODE":
+		message = input("Enter the morse code: ")
+		result = decrypt(message)
+		print ("The given morse code in English language is ", result)
+
+	else:
+		print("Please input by writing encode or decode /nIf you want to exit the program, just press Ctrl+C")
+
+# Executes the main function
+main()
