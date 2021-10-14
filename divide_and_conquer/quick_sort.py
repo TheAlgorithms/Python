d5ab0b2fe1c48b2a34c4776b7e68d29ed2abc617
rsh-raj <rishiraj45035@gmail.com>
2021-10-14 20:24:02 +0530

    Added doctest for functions

--- divide_and_conquer/quick_sort.py
+++ divide_and_conquer/quick_sort.py
@@ -1,10 +1,21 @@
 # Implementation of Quick sort algo-> https://en.wikipedia.org/wiki/Quicksort
 class QuickSort():
+   
     def __init__(self, array:list,asc:bool=True)->None:
         self.asc=asc
         self.array = array
 
     def partition(self, start:int, end:int)->int:  # Will sort in ascending order by default
+        """ 
+        >>> arr=[2,1,4,5,6,1]
+        >>> q_s=QuickSort(arr)
+        >>> q_s.partition(0,len(arr))
+        2
+        >>> arr=[2,3,4,5,6,9]
+        >>> q_s=QuickSort(arr)
+        >>> q_s.partition(0,len(arr))
+        0
+        """
         '''Normal partition routine read more about it here: google Lomuto partition algorithm. '''
         i = start
         j = start+1
@@ -22,19 +33,23 @@ class QuickSort():
         self.array[start], self.array[i] = self.array[i], self.array[start]
         return i
     def sort(self,start:int,end:int)->None: #recursive implementation of quick sort
+        """ 
+        >>> arr=[2,1,4,5,6,1]
+        >>> q_s=QuickSort(arr)
+        >>> q_s.sort(0,len(arr))
+        >>> print(arr)
+        [1, 1, 2, 4, 5, 6]
+        """
         if(start>=end):
             return
         i=self.partition(start,end)
         self.sort(start,i)
         self.sort(i+1,end)
 
+if __name__=='__main__':
+    import doctest
 
-ls = [3, 2, 1, 4, 3, 0, 5, 9,0,99,88,77,66,55,1]
-#sort in ascending order
-q_s=QuickSort(ls)
-q_s.sort(start=0,end=len(ls))
-print(ls)
-#descending
-q_s=QuickSort(ls,False)
-q_s.sort(start=0,end=len(ls))
-print(ls)
+    doctest.testmod()
+    ls = [3, 2, 1, 4, 3, 0, 5, 9,0,99,88,77,66,55,1]
+    #sort in ascending order
+    
