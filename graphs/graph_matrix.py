21a081fedcb5df97d26b7a83861011e86390f399
Priya Rawat <83421508+priyaa-rawat@users.noreply.github.com>
2021-10-15 22:46:06 +0530

    Update graph_matrix.py

--- graphs/graph_matrix.py
+++ graphs/graph_matrix.py
@@ -4,18 +4,16 @@ class Graph:
         self.graph = [[0] * vertex for i in range(vertex)]
 
     def add_edge(self, u, v):
-        try:
-            self.graph[u - 1][v - 1] = 1
-            self.graph[v - 1][u - 1] = 1
-        except IndexError as e:
-            print(e)
+         if u>self.vertex or v>self.vertex or u<1 or v<1:
+            raise IndexError("Invalid Edge")
+        self.graph[u - 1][v - 1] = 1
+        self.graph[v - 1][u - 1] = 1
         
     def remove_edge(self,u,v):
-        try:
-            self.graph[u - 1][v - 1] = 0
-            self.graph[v - 1][u - 1] = 0
-        except IndexError as e:
-            print(e)
+        if u>self.vertex or v>self.vertex or u<1 or v<1:
+            raise IndexError("Invalid Edge")
+        self.graph[u - 1][v - 1] = 0
+        self.graph[v - 1][u - 1] = 0
 
     def show(self):
 
