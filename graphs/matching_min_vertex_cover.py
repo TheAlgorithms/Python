1e7c9bc93c610f6e23ef79448245fcbd805dbe98
Christian Clauss <cclauss@me.com>
2021-10-15 16:47:07 +0200

    Update matching_min_vertex_cover.py

--- graphs/matching_min_vertex_cover.py
+++ graphs/matching_min_vertex_cover.py
@@ -12,7 +12,7 @@ def matching_min_vertex_cover(graph: dict) -> set:
     """
     APX Algorithm for min Vertex Cover using Matching Approach
     @input: graph (graph stored in an adjacency list where each vertex
-            is represented with an integer)
+            is represented as an integer)
     @example:
     >>> graph = {0: [1, 3], 1: [0, 3], 2: [0, 3, 4], 3: [0, 1, 2], 4: [2, 3]}
     >>> matching_min_vertex_cover(graph)
@@ -23,25 +23,24 @@ def matching_min_vertex_cover(graph: dict) -> set:
     # edges = list of graph's edges
     edges = get_edges(graph)
 
-    # while there are still elements in edges' list
-    # take an arbitrary edge (from_node,to_node) and add his extremity to s
-    # then remove all arcs adjacent to the from_node and to_node
+    # While there are still elements in edges list, take an arbitrary edge
+    # (from_node, to_node) and add his extremity to chosen_vertices and then
+    # remove all arcs adjacent to the from_node and to_node
     while edges:
         from_node, to_node = edges.pop()
         chosen_vertices.add(from_node)
         chosen_vertices.add(to_node)
         for edge in edges.copy():
-            if (from_node in edge) or (to_node in edge):
+            if from_node in edge or to_node in edge:
                 edges.discard(edge)
     return chosen_vertices
 
 
 def get_edges(graph: dict) -> set:
     """
-    Function that returns a set of couples that represents
-    the set of all the edges
-    @input: graph (graph stored in an adjacency list where each vertex
-            is represented with an integer)
+    Return a set of couples that represents all of the edges.
+    @input: graph (graph stored in an adjacency list where each vertex is
+            represented as an integer)
     @example:
     >>> graph = {0: [1, 3], 1: [0, 3], 2: [0, 3], 3: [0, 1, 2]}
     >>> get_edges(graph)
