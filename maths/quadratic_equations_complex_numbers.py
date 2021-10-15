d11c9523031238e3cdc08c4337c32eba0af5cd45
Bithiah Koshy <63845509+vintagemind@users.noreply.github.com>
2021-10-15 21:06:28 +1100

    Quadratic Root Calculator
    A simple quadratic root calculator

--- maths/quadratic_equations_complex_numbers.py
+++ maths/quadratic_equations_complex_numbers.py
@@ -36,3 +36,27 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+"""
+Quadratic equation root calculator
+"""
+
+def roots(a,b,c):
+
+  D = (b*b - 4*a*c) ** 0.5
+  x_1 = (-b + D) / (2*a)
+  x_2 = (-b - D) /( 2*a)
+
+  print("x1 : {0}" .format(x_1))
+  print("x2 : {0}" .format(x_2))
+
+if __name__ == "__main__":
+  a = input()
+  b = input()
+  c = input()
+
+  roots(float(a), float(b), float(c))
+
+  
+   
+  
