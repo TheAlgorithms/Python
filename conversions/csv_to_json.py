d11eab93cc8776f027e29ce877e77bc10851444f
Sreelakshmi G <sreelakshmig009@gmail.com>
2021-10-16 22:18:18 +0530

    Updates made

--- conversions/csv_to_json.py
+++ conversions/csv_to_json.py
@@ -5,65 +5,76 @@ import json
 
 
 # Function to convert a CSV file to JSON file
+
 # Takes the path of the files to be converted as arguments
-def convert_csv_to_json(csvPath:Any, jsonPath:Any)-> Any:
 
-            """
-            I used a csv file called ratings from this link : https://github.com/gangtao/datasets/blob/master/csv/ratings.csv
-	    I assumed the key user to be the primary key
-            >>> convert_csv_to_json(ratings.csv,ratings.json)
-            {
-                    "1": {
-                            "user": "1",
-                            "movie": "5060",
-                            "rating": "5.0",
-                            "timestamp": "964984002"
-                    },
-                    "2": {
-                            "user": "2",
-                            "movie": "131724",
-                            "rating": "5.0",
-                            "timestamp": "1445714851"
-                   },
-                   ...
-                   "122": {
-                           "user": "122",
-                           "movie": "134130",
-                           "rating": "4.0",
-                           "timestamp": null
-                   }
-            }
-            Traceback (most recent call last):
-            ...
-            KeyError: 'wrong-key'
-            """
-	
-	# create a empty dictionary
-	data = {}
-	
-	# read csv file using in-built function of csv module called DictReader
-	with open(csvPath, encoding='utf-8') as csv_file: # don't forget to mention utf-8 encoding
-		csvReader = csv.DictReader(csv_file)
-		
-		# Convert each row into a dictionary and add it to data
-		for rows in csvReader:
-			
-			# Assuming a column named 'id' to be the primary key
-			# change the primary key name according to your csv file
-			key = rows['id']
-			data[key] = rows
+def convert_csv_to_json(csv_path: Any, json_path: Any) -> Any:
+    """
+    I used a csv file called ratings from this link : https://github.com/gangtao/datasets/blob/master/csv/ratings.csv
+    >>> convert_csv_to_json(ratings.csv,ratings.json)
+    {
+        "1": {
+                "user": "1",
+                "movie": "5060",
+                "rating": "5.0",
+                "timestamp": "964984002"
+        },
+        "2": {
+                "user": "2",
+                "movie": "131724",
+                "rating": "5.0",
+                "timestamp": "1445714851"
+        },
+        ...
+        "122": {
+                "user": "122",
+                "movie": "134130",
+                "rating": "4.0",
+                "timestamp": null
+        }
+    }
+    Traceback (most recent call last):
+    ...
+    KeyError: 'wrong-key'
+    """
+    # create a empty dictionary
 
-	# Open a json writer and use the json.dumps() function to dump data
-	with open(jsonPath, 'w', encoding='utf-8') as json_file: 
-		json_file.write(json.dumps(data, indent=3)) # indent is used to improve the readability of the josn file, can be None also
-		# https://docs.python.org/3/library/json.html read this documentation for difference between json.dump() and json.dumps()
+    data = {}
+
+    # read csv file using in-built function of csv module called DictReader
+
+     # don't forget to mention utf-8 encoding
+    with open(csv_path, encoding="utf-8") as csv_file:
+        csv_reader = csv.DictReader(csvf)
+
+        # Convert each row into a dictionary and add it to data
+
+        for rows in csv_reader:
+
+            # Assuming a column named 'id' to be the primary key
+            # change the primary key name according to your csv file
+
+            key = rows["id"]
+
+            data[key] = rows
+
+    # Open a json writer and use the json.dumps() function to dump data
+
+    with open(json_path, "w", encoding="utf-8") as json_file:
+
+        jsonf.write(
+            json.dumps(data, indent=3)
+        )  # indent is used to improve the readability of the json file, can be None also
+
+        # https://docs.python.org/3/library/json.html read this documentation for difference between json.dump() and json.dumps()
+
 
 # Driver Code
 
 # Decide the two file paths according to your computer device
-csvPath = r'path/to/csv/file'
-jsonPath = r'path/to/json/file' # specify the path where your converted json file should be stored
+csv_path = r"path/to/csv/file"
+json_path = r"path/to/json/file"  # specify the path where your converted json file should be stored
 
 
 # Call the convert_csv_to_json function
-convert_csv_to_json(csvPath, jsonPath)
+convert_csv_to_json(csv_path, json_path)
