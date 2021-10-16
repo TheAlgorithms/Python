3d26ffe2d272434b95b4ad3e0f828089cc967fad
Christian Clauss <cclauss@me.com>
2021-10-16 03:19:40 +0200

    Update get_imdbtop.py

--- web_programming/get_imdbtop.py
+++ web_programming/get_imdbtop.py
@@ -1,45 +1,53 @@
+import bs4
 import requests
-from bs4 import BeautifulSoup
 
 
-def crawl_imdb_topn_movies(num_movies: int = 5) -> list:
-    """Crawls IMDB and return a list of tuples containing
-    the top n movies with their respective titles, genre,
-    ratings, and imdb page link released.
+def get_movie_data_from_soup(soup: bs4.element.ResultSet) -> dict[str, str]:
+    return {
+        "name": soup.h3.a.text,
+        "genre": soup.find("span", class_="genre").text.strip(),
+        "rating": soup.strong.text,
+        "page_link": f"https://www.imdb.com{soup.a.get('href')}",
+    }
+
+
+def get_imdb_top_movies(num_movies: int = 5) -> tuple:
+    """Get the top num_movies most highly rated movies from IMDB and
+    return a tuple of dicts discribing each movie's name, genre, rating, and URL.
 
     Args:
-        num_movies: The number of movies to crawl. Defaults to 5.
+        num_movies: The number of movies to get. Defaults to 5.
 
     Returns:
         A list of tuples containing information about the top n movies.
 
-    >>> len(crawl_imdb_topn_movies(5)) == int(5)
-    True
-    >>> len(crawl_imdb_topn_movies(-3)) == int(0)
-    True
-    >>> len(crawl_imdb_topn_movies(4.6)) == int(4)
-    True
+    >>> len(get_imdb_top_movies(5))
+    5
+    >>> len(get_imdb_top_movies(-3))
+    0
+    >>> len(get_imdb_top_movies(4.99999))
+    4
     """
     num_movies = int(float(num_movies))
-    if int(num_movies) < 1:
+    if num_movies < 1:
         return []
     base_url = (
-        f"https://www.imdb.com/search/title?title_type="
+        "https://www.imdb.com/search/title?title_type="
         f"feature&sort=num_votes,desc&count={num_movies}"
     )
-    source = BeautifulSoup(requests.get(base_url).content, "html.parser")
-    movies = source.find_all("div", class_="lister-item mode-advanced")
-    total_movies_list = []
-    movie_dict = {}
-    for m in movies:
-        movie_dict["name"] = "\n" + m.h3.a.text  # movie's name
-        movie_dict["genre"] = m.find("span", attrs={"class": "genre"}).text
-        movie_dict["rating"] = m.strong.text  # movie's rating
-        movie_dict["page link"] = f"https://www.imdb.com{m.a.get('href')}"
-        total_movies_list.append(movie_dict)
-    return total_movies_list
+    source = bs4.BeautifulSoup(requests.get(base_url).content, "html.parser")
+    return tuple(
+        get_movie_data_from_soup(movie)
+        for movie in source.find_all("div", class_="lister-item mode-advanced")
+    )
 
 
 if __name__ == "__main__":
-    num_movies = int(float(input("How many movies would you like to see? ")))
-    crawl_imdb_topn_movies(num_movies)
+    import json
+
+    num_movies = int(input("How many movies would you like to see? "))
+    print(
+        ", ".join(
+            json.dumps(movie, indent=4) for movie in get_imdb_top_movies(num_movies)
+        )
+    )
