c7687e493020230477a7df6c387b830a3a6fc414
Kiran KS <kirankrish792@gmail.com>
2021-10-17 22:10:06 +0530

    First fit updated algo

--- Memory_allocation/firstfit.py
+++ Memory_allocation/firstfit.py
@@ -1,7 +1,7 @@
 
 # Function to allocate memory to  
 # blocks as per First fit algorithm  
-def firstFit(blockSize, m, processSize, n): 
+def first_fit(block_size, m, process_size, n): 
       
     # Stores block id of the  
     # block allocated to a process  
@@ -13,19 +13,19 @@ def firstFit(blockSize, m, processSize, n):
     # according to its size ad assign to it  
     for i in range(n): 
         for j in range(m): 
-            if blockSize[j] >= processSize[i]: 
+            if block_size[j] >= process_size[i]: 
                   
                 # allocate block j to p[i] process  
                 allocation[i] = j  
   
                 # Reduce available memory in this block.  
-                blockSize[j] -= processSize[i]  
+                block_size[j] -= process_size[i]  
   
                 break
   
     print(" Process No. Process Size      Block no.") 
     for i in range(n): 
-        print(" ", i + 1, "      ", processSize[i],  
+        print(" ", i + 1, "      ", process_size[i],  
                           "      ", end = " ")  
         if allocation[i] != -1:  
             print(allocation[i] + 1)  
@@ -34,9 +34,9 @@ def firstFit(blockSize, m, processSize, n):
   
 # Driver code  
 if __name__ == '__main__':  
-    blockSize = [100, 500, 200, 300, 600]  
-    processSize = [212, 417, 112, 426] 
-    m = len(blockSize) 
-    n = len(processSize) 
+    block_size = [100, 500, 200, 300, 600]  
+    process_size = [212, 417, 112, 426] 
+    m = len(block_size) 
+    n = len(process_size) 
   
-    firstFit(blockSize, m, processSize, n) 
+    first_fit(block_size, m, process_size, n) 
