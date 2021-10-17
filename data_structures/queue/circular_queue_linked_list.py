daec8737c5b628db69b9ebe5e6808f181cf24839
Víctor (Bit-Man) Rodríguez <victor.bitman@gmail.com>
2021-10-17 18:48:42 -0300

    CircularQueueLinkedList: single element list TheAlgorithms#5361

--- data_structures/queue/circular_queue_linked_list.py
+++ data_structures/queue/circular_queue_linked_list.py
@@ -1,11 +1,22 @@
 class CircularQueueLinkedList:
+    def __init__(self):
+        self.is_empty_queue = True
+        self.elem = None
+
     def is_empty(self) -> bool:
         """
         >>> cq = CircularQueueLinkedList()
         >>> cq.is_empty()
         True
+        >>> cq.enqueue('a')
+        >>> cq.is_empty()
+        False
+        >>> cq.dequeue()
+        'a'
+        >>> cq.is_empty()
+        True
         """
-        return True
+        return self.is_empty_queue
 
     def first(self):
         """
@@ -14,11 +25,24 @@ class CircularQueueLinkedList:
         Traceback (most recent call last):
            ...
         Exception: Empty Queue
+        >>> cq.enqueue('a')
+        >>> cq.first()
+        'a'
+        >>> cq.dequeue()
+        'a'
+        >>> cq.first()
+        Traceback (most recent call last):
+           ...
+        Exception: Empty Queue
         """
-        raise Exception("Empty Queue")
+        if self.elem is None:
+            raise Exception("Empty Queue")
+
+        return self.elem
 
     def enqueue(self, data):
-        pass
+        self.is_empty_queue = False
+        self.elem = data
 
     def dequeue(self):
         """
@@ -27,9 +51,21 @@ class CircularQueueLinkedList:
         Traceback (most recent call last):
            ...
         Exception: Empty Queue
+        >>> cq.enqueue('a')
+        >>> cq.dequeue()
+        'a'
+        >>> cq.dequeue()
+        Traceback (most recent call last):
+           ...
+        Exception: Empty Queue
         """
+        if self.elem is None:
+            raise Exception("Empty Queue")
 
-        raise Exception("Empty Queue")
+        self.is_empty_queue = True
+        elem = self.elem
+        self.elem = None
+        return elem
 
 
 if __name__ == "__main__":
