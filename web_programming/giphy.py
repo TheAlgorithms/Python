633327ce4bb9a3f858f16d517c0ff9f865efea23
Jaydeep Das <jaydeepjd.8914@gmail.com>
2021-10-17 20:32:19 +0530

    Added giphy.py to fetch gifs on a given topic

---
+++ web_programming/giphy.py
@@ -0,0 +1,29 @@
+#!/bin/python
+import random
+import json
+import requests
+
+
+api_key = "YOUR GIPHY API KEY"
+# Can be fetched from https://developers.giphy.com/dashboard/
+
+
+def getgif(query: str) -> str:
+    """
+    Get 1 URL of GIF on a given `query`
+
+    ❯ python giphy.py
+      Topic: space+ship
+      Total urls received:  50
+      https://giphy.com/gifs/startrekfleetcommand-BPVIRni1Z70QO2nJMX
+    """
+    qu: str = '+'.join(query.split(" "))
+    print("Topic: ", qu)
+    url: str = f"http://api.giphy.com/v1/gifs/search?q={qu}&api_key={api_key}"
+    jsdata = requests.get(url)
+    data: dict = json.loads(jsdata.content)
+    print("Total urls received: ", len(data['data']))
+    return data['data'][random.randint(0, 49)]['url']
+
+
+print(getgif("space ship"))
