273f94f5e5b7882f652981a6f48de1974c103a47
Vinicius Cordeiro <marcos.cordeiroal@gmail.com>
2021-10-17 10:59:36 -0300

    Edit polybius.py to suit naming conventions

--- ciphers/polybius.py
+++ ciphers/polybius.py
@@ -1,70 +1,96 @@
 #!/usr/bin/env python3
-#!/usr/bin/env python3
 
-'''
+"""
 A Polybius Square is a table that allows someone to translate letters into numbers.
 
 https://www.braingle.com/brainteasers/codes/polybius.php
-'''
+"""
 
 import numpy as np
 
-class PolybiusCipher():
-    
+
+class PolybiusCypher:
     def __init__(self) -> None:
-        Square = [['a','b','c','d','e'],
-                  ['f','g','h','i','k'],
-                  ['l','m','n','o','p'],
-                  ['q','r','s','t','u'],
-                  ['v','w','x','y','z']]
-        self.Square = np.array(Square)
-    
-    def LetterToNumbers(self, Letter: str) -> tuple:
-        Index1, Index2 = np.where(self.Square == Letter)
-        Indexes = np.concatenate([Index1, Index2])
-        return Indexes
+        SQUARE = [
+            ["a", "b", "c", "d", "e"],
+            ["f", "g", "h", "i", "k"],
+            ["l", "m", "n", "o", "p"],
+            ["q", "r", "s", "t", "u"],
+            ["v", "w", "x", "y", "z"],
+        ]
+        self.SQUARE = np.array(SQUARE)
 
-    def NumbersToLetter(self, Index1: int, Index2: int) -> str:
-        Letter = self.Square[Index1, Index2]
-        return Letter
-        
-    def Encode(self, Message: str) -> str:
-        Message = Message.lower()
-        
-        EncodedMessage = ''
-        for LetterIndex in range(len(Message)):
-            if Message[LetterIndex] != ' ':
-                Numbers = self.LetterToNumbers(Message[LetterIndex])
-                EncodedMessage = EncodedMessage + str(Numbers[0] + 1) + str(Numbers[1] + 1)
-            elif Message[LetterIndex] == ' ':
-                EncodedMessage = EncodedMessage + ' '
-        
-        return EncodedMessage
-    
-    def Decode(self, Message: str) -> str:
-        Message = Message.replace(' ', '  ')
-        DecodedMessage = ''
-        for NumbersIndex in range(int(len(Message)/2)):
-            if Message[NumbersIndex * 2] != ' ':
-                Index1 = Message[NumbersIndex * 2]
-                Index2 = Message[NumbersIndex * 2 + 1]
-                
-                Letter = self.NumbersToLetter(int(Index1) - 1, int(Index2) - 1)
-                DecodedMessage = DecodedMessage + Letter
-            elif Message[NumbersIndex * 2] == ' ':
-                DecodedMessage = DecodedMessage + ' '
-            
-        return DecodedMessage
+    def letter_to_numbers(self, letter: str) -> np.ndarray:
+        """
+        Return the pair of numbers that represents the given letter in the
+        polybius square
+        >>> letter_to_numbers('a')
+        [1, 1]
 
-    def TestCipher(self) -> None:
-        CipherTest = PolybiusCipher()
+        >>> letter_to_numbers('u')
+        [4, 5]
+        """
+        index1, index2 = np.where(self.SQUARE == letter)
+        indexes = np.concatenate([index1 + 1, index2 + 1])
+        return indexes
 
-        (CipherTest.Encode('testmessage') == '4415434432154343112215')
-        assert (CipherTest.Encode('test message') == '44154344 32154343112215')
-        assert (CipherTest.Encode('Test Message') == '44154344 32154343112215')
-        assert (CipherTest.Encode('') == '')
+    def numbers_to_letter(self, index1: int, index2: int) -> str:
+        """
+        Return the letter corresponding to the position [index1, index2] in
+        the polybius square
 
-        assert (CipherTest.Decode('44154344 32154343112215') == 'test message')
-        assert (CipherTest.Decode('4415434432154343112215') == 'testmessage')
-        assert (CipherTest.Decode('') == '')
-        
+        >>> numbers_to_letters(4, 5)
+        "u"
+
+        >>> numbers_to_letters(1, 1)
+        "a"
+        """
+        letter = self.SQUARE[index1 - 1, index2 - 1]
+        return letter
+
+    def encode(self, message: str) -> str:
+        """
+        Return the encoded version of message according to the polybius cypher
+
+        >>> encode("test message")
+        "44154344 32154343112215"
+
+        >>> encode("Test Message")
+        44154344 32154343112215"
+        """
+        message = message.lower()
+        message = message.replace("j", "i")
+
+        encoded_message = ""
+        for letter_index in range(len(message)):
+            if message[letter_index] != " ":
+                numbers = self.letter_to_numbers(message[letter_index])
+                encoded_message = encoded_message + str(numbers[0]) + str(numbers[1])
+            elif message[letter_index] == " ":
+                encoded_message = encoded_message + " "
+
+        return encoded_message
+
+    def decode(self, message: str) -> str:
+        """
+        Return the decoded version of message according to the polybius cypher
+
+        >>> decode("44154344 32154343112215")
+        "test message"
+
+        >>> decode("4415434432154343112215")
+        testmessage"
+        """
+        message = message.replace(" ", "  ")
+        decoded_message = ""
+        for numbers_index in range(int(len(message) / 2)):
+            if message[numbers_index * 2] != " ":
+                index1 = message[numbers_index * 2]
+                index2 = message[numbers_index * 2 + 1]
+
+                letter = self.numbers_to_letter(int(index1), int(index2))
+                decoded_message = decoded_message + letter
+            elif message[numbers_index * 2] == " ":
+                decoded_message = decoded_message + " "
+
+        return decoded_message
