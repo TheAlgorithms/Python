6e2ef628701ffe081812aa036b9c12d63f4959aa
MohdElgaar <mohdelgaar@gmail.com>
2021-10-17 00:52:32 -0400

    fix snake_case

--- maths/fibonacci_repeated_squaring.py
+++ maths/fibonacci_repeated_squaring.py
@@ -6,40 +6,40 @@ https://en.wikipedia.org/wiki/Fibonacci_number#Matrix_form
 PRIME = 30011
 
 
-def matmul(matrix_A: list, matrix_B: list) -> list:
+def matmul(matrix_a: list, matrix_b: list) -> list:
     """
     >>> matmul([[1, 0], [0, 1]], [[1, 0], [0, 1]])
     [[1, 0], [0, 1]]
     """
     result = list()
-    for i in range(len(matrix_A)):
+    for i in range(len(matrix_a)):
         result.append(list())
-        for j in range(len(matrix_B[0])):
-            column = [x[j] for x in matrix_B]
-            result[i].append(dot(matrix_A[i], column))
+        for j in range(len(matrix_b[0])):
+            column = [x[j] for x in matrix_b]
+            result[i].append(dot(matrix_a[i], column))
     return result
 
 
-def dot(x: list, y: list) -> list:
+def dot(vector_x: list, vector_y: list) -> list:
     """
     >>> dot([1,2,3,4,5], [1,1,1,1,1])
     15
     """
     result = 0
-    for xi, yi in zip(x, y):
+    for xi, yi in zip(vector_x, vector_y):
         result += xi * yi
     return result % PRIME
 
 
-def square(matrix_A: list) -> list:
+def square(matrix_a: list) -> list:
     """
     >>> square([[1, 0], [0, 1]])
     [[1, 0], [0, 1]]
     """
-    return matmul(matrix_A, matrix_A)
+    return matmul(matrix_a, matrix_a)
 
 
-def power(matrix_X: list, n: int) -> list:
+def power(matrix_x: list, n: int) -> list:
     """
     >>> power([[1, 0], [0, 1]], 1)
     [[1, 0], [0, 1]]
@@ -49,15 +49,15 @@ def power(matrix_X: list, n: int) -> list:
     [[1, 0], [0, 1]]
     """
     if n < 0:
-        return matrix_X
+        return matrix_x
     if n == 0:
         return [[1, 0], [0, 1]]
     if n == 1:
         return X
     if n % 2 == 0:
-        return square(power(matrix_X, n / 2))
+        return square(power(matrix_x, n / 2))
     if n % 2 != 0:
-        return matmul(square(power(matrix_X, (n - 1) / 2)), matrix_X)
+        return matmul(square(power(matrix_x, (n - 1) / 2)), matrix_x)
 
 
 def fib_repeated_squaring(n: int) -> int:
