20b6be4edd271618c87daca4131f4136967b3141
Devarsh Panchal <devarshpanchal@rediffmail.com>
2021-10-18 21:01:25 +0530

    Added decimal converter

---
+++ conversions/decimal_converter.py
@@ -0,0 +1,6 @@
+dec = int(input("Enter the decimal value: "))
+
+print("\nThe decimal value of", dec, "is:")
+print(bin(dec), "in binary.")
+print(oct(dec), "in octal.")
+print(hex(dec), "in hexadecimal.")
