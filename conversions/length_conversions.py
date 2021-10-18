0968b11827d9d2cae010e0c56570e9fbe48a9a7d
Christian Clauss <cclauss@me.com>
2021-10-18 23:18:03 +0200

    Update length_conversions.py

--- conversions/length_conversions.py
+++ conversions/length_conversions.py
@@ -6,9 +6,10 @@ USAGE :
 -> Import this file into their respective project.
 -> Use the function length_conversion() for conversion of length units.
 -> Parameters :
+    -> value : The number of from units you want to convert
     -> from_type : From which type you want to convert
     -> to_type : To which type you want to convert
-    -> value : the value which you want to convert
+    
 
 REFERENCES :
 -> Wikipedia reference: https://en.wikipedia.org/wiki/Meter
@@ -22,95 +23,80 @@ REFERENCES :
 -> Wikipedia reference: https://en.wikipedia.org/wiki/Millimeter
 """
 
-METER_CHART: dict[str, float] = {
-    "meter": 1,
-    "kilometer": 0.001,
-    "feet": 3.28084,
-    "inch": 39.3701,
-    "centimeter": 100,
-    "yard": 1.09361,
-    "foot": 3.28084,
-    "mile": 0.000621371,
-    "millimeter": 1000,
-}
+from collections import namedtuple
 
-LENGTH_TYPE_CHART: dict[str, float] = {
-    "meter": 1,
-    "kilometer": 1000,
-    "feet": 0.3048,
-    "inch": 0.0254,
-    "centimeter": 0.01,
-    "yard": 0.9144,
-    "foot": 0.3048,
-    "mile": 1609.34,
-    "millimeter": 0.001,
+from_to = namedtuple("from_to", "from_ to")
+
+METRIC_CONVERSION = {
+    "meter": from_to(1, 1),
+    "kilometer": from_to(1000, 0.001),
+    "feet": from_to(0.3048, 3.28084),
+    "inch": from_to(0.0254, 39.3701),
+    "centimeter": from_to(0.01, 100),
+    "yard": from_to(0.9144, 1.09361),
+    "foot": from_to(0.3048, 3.28084),
+    "mile": from_to(1609.34, 0.000621371),
+    "millimeter": from_to(0.001, 1000),
 }
 
 
-def length_conversion(from_type: str, to_type: str, value: float) -> float:
+def length_conversion(value: float, from_type: str, to_type: str) -> float:
     """
-    Conversion of length unit with the help of LENGTH_CHART
-     "meter": 1,
-    "kilometer": 0.001,
-    "feet": 3.28084,
-    "inch": 39.3701,
-    "centimeter": 100,
-    "yard": 1.09361,
-    "foot": 3.28084,
-    "mile": 0.000621371,
-    "millimeter": 1000,
-    >>> length_conversion("meter","feet",4)
+    Conversion between length units.
+ 
+    >>> length_conversion(4, "meter", "feet")
     13.12336
-    >>> length_conversion("meter","kilometer",1)
+    >>> length_conversion(1, "meter", "kilometer")
     0.001
-    >>> length_conversion("kilometer","inch",1)
+    >>> length_conversion(1, "kilometer", "inch")
     39370.1
-    >>> length_conversion("kilometer","mile",3)
+    >>> length_conversion(3, "kilometer", "mile")
     1.8641130000000001
-    >>> length_conversion("feet","meter",2)
+    >>> length_conversion(2, "feet", "meter")
     0.6096
-    >>> length_conversion("feet","yard",4)
+    >>> length_conversion(4, "feet", "yard")
     1.333329312
-    >>> length_conversion("inch","meter",1)
+    >>> length_conversion(1, "inch", "meter")
     0.0254
-    >>> length_conversion("inch","mile",2)
+    >>> length_conversion(2, "inch", "mile")
     3.15656468e-05
-    >>> length_conversion("centimeter","millimeter",2)
+    >>> length_conversion(2, "centimeter", "millimeter")
     20.0
-    >>> length_conversion("centimeter","yard",2)
+    >>> length_conversion(2, "centimeter", "yard")
     0.0218722
-    >>> length_conversion("yard","meter",4)
+    >>> length_conversion(4, "yard", "meter")
     3.6576
-    >>> length_conversion("yard","kilometer",4)
+    >>> length_conversion(4, "yard", "kilometer")
     0.0036576
-    >>> length_conversion("foot","meter",3)
+    >>> length_conversion(3, "foot", "meter")
     0.9144000000000001
-    >>> length_conversion("foot","inch",3)
+    >>> length_conversion(3, "foot", "inch")
     36.00001944
-    >>> length_conversion("mile","kilometer",4)
+    >>> length_conversion(4, "mile", "kilometer")
     6.43736
-    >>> length_conversion("mile","inch",2)
+    >>> length_conversion(2, "mile", "inch")
     126719.753468
-    >>> length_conversion("millimeter","centimeter",3)
+    >>> length_conversion(3, "millimeter", "centimeter")
     0.3
-    >>> length_conversion("millimeter","inch",3)
+    >>> length_conversion(3, "millimeter", "inch")
     0.1181103
-    >>> length_conversion("wrongUnit","inch",4 )
+    >>> length_conversion(4, "wrongUnit", "inch")
     Traceback (most recent call last):
-      File "/usr/lib/python3.8/doctest.py", line 1336, in __run
-        exec(compile(example.source, filename, "single",
-      File "<doctest __main__.length_conversion[18]>", line 1, in <module>
-        length_conversion("wrongUnit","inch",4 )
-      File "<string>", line 104, in length_conversion
-    ValueError: Invalid 'from_type' or 'to_type' value: 'wrongUnit', 'inch'
+      ...
+    ValueError: Invalid 'from_type' value: 'wrongUnit'
     Supported values are: meter, kilometer, feet, inch, centimeter, yard, foot, mile, millimeter
     """
-    if to_type not in METER_CHART or from_type not in LENGTH_TYPE_CHART:
+    if from_type not in METRIC_CONVERSION:
         raise ValueError(
-            f"Invalid 'from_type' or 'to_type' value: {from_type!r}, {to_type!r}\n"
-            f"Supported values are: {', '.join(LENGTH_TYPE_CHART)}"
+            f"Invalid 'from_type' value: {from_type!r}\n"
+            f"Supported values are: {', '.join(METRIC_CONVERSION)}"
         )
-    return value * METER_CHART[to_type] * LENGTH_TYPE_CHART[from_type]
+    if to_type not in METRIC_CONVERSION:
+        raise ValueError(
+            f"Invalid 'to_type' value: {to_type!r}\n"
+            f"Supported values are: {', '.join(METRIC_CONVERSION)}"
+        )
+    return value * METRIC_CONVERSION[from_type].from_ * METRIC_CONVERSION[to_type].to
 
 
 if __name__ == "__main__":
