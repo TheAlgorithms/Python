5e075ef9412acc57d4946f3c61c9bbd67c39a604
Satyajit Mishra <smish_@Soumyajeets-MacBook-Air.local>
2021-10-18 22:00:59 +0530

    Made modification to jumble words, added doctest

--- Jumble Words/jumbleWords.py
+++ Jumble Words/jumbleWords.py
@@ -1,4 +1,17 @@
-def unjumble(word):
+def unjumble(word) -> None:
+
+    r"""
+    This function converts given input jumbled word into its lowercase equivalent, sorts it, and
+    then cross verifies with the same using each word from the file "words.txt", following which,
+    it returns a list of possible correct outcomes.
+
+    Define input and expected output:
+    >>> unjumble('pplea')
+    ['appel\n', 'apple\n', 'pepla\n']
+    >>> unjumble('roltonc')
+    ['control\n']
+    """
+
     str = []
     str2 = []
     res = []
@@ -25,7 +38,10 @@ def unjumble(word):
 
 
     print(res)
-
-word = input("Enter the jumbled word : ")
-word=word.lower()
-unjumble(word)
+  
+if __name__ == '__main__':
+    import doctest
+    import typing
+    word = input("Enter the jumbled word : ") # type: str
+    word = word.lower()
+    doctest.testmod(name ='unjumble', verbose = True)
