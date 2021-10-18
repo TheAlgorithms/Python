d68350a2ce97dcf75df7231392233a4789129c75
Satyajit Mishra <smish_@Soumyajeets-MacBook-Air.local>
2021-10-19 01:55:43 +0530

    Added comments for easier understanding

--- Jumble Words/jumbleWords.py
+++ Jumble Words/jumbleWords.py
@@ -1,4 +1,4 @@
-def unjumble(word: str) -> None:
+def unjumble(word: str) -> None:        # Function to show possible unjumbled words
 
     r"""
     This function converts given input jumbled word into its lowercase equivalent, sorts it, and
@@ -17,31 +17,35 @@ def unjumble(word: str) -> None:
     res = []
 
     for i in word:
-        str.append(i)
+        str.append(i)       # converts the entered word into a list
 
-    str.sort()
-    f = open("words.txt","r")
+    str.sort()      # sorting the list in ascending order
+    f = open("words.txt","r")       # accessing the txt file containing all the dictionary words
 
 
     for line in f:
-        i = line
-        for x in i:
-            str2.append(x)
+        i = line        # getting each word from the txt file 
+        for x in i:         
+            str2.append(x)      # Converting each word from the txt file and storing it in another list   
 
 
-        str2.pop()
-        str2.sort()
-        if str == str2:
-            res.append(line)
+        str2.pop()      # removing the '\n' character from the list for every word from the file
+        str2.sort()     # sorting the second list in ascending order as well
+
+        # making both lists sorted enables quick comparision between both of them
+
+        if str == str2:     # checking if the list from the original input jumbled word matched with the list from the txt file
+            res.append(line)    # if it matches, Then adding the particular word to result list
+
         str2=[]
 
 
 
-    print(res)
+    print(res)      # printing the resultant list containing all the possible outcomes for the input word
   
 if __name__ == '__main__':
     import doctest
     import typing
-    word = input("Enter the jumbled word : ") # type: str
-    word = word.lower()
+    word = input("Enter the jumbled word : ")       # Asking user to enter the jubled word
+    word = word.lower()     # Converting the input word to lower case
     doctest.testmod(name ='unjumble', verbose = True)
