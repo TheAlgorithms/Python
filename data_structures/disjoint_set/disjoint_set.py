2382a094837c519289a7d4efcbf60216044f3419
rsh-raj <rishiraj45035@gmail.com>
2021-10-19 01:08:39 +0530

    Update disjoint_set.py

--- data_structures/disjoint_set/disjoint_set.py
+++ data_structures/disjoint_set/disjoint_set.py
@@ -15,8 +15,8 @@ def make_set(x: Node) -> None:
     """
     Make x as a set.
     """
-    # rank is the upper bound for hieght but not height
-    # We store rank instead of hieght because hieght can change during find operation and we will have to do some extra calculations to keep the hieght correct.
+    # rank is the upper bound for hieght but not always equal to height
+    # We store rank instead of hieght because hieght can change during find operation and then we will have to do some extra calculations to keep the hieght correct.
     x.rank = 0 #Initially all nodes rank is taken as zero by convention
     x.parent = x
 
