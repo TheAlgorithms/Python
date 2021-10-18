7c73af813f2ecf592b7e76f32d228d1c5cb4128a
Rohan R Bharadwaj <89947037+Rohanrbharadwaj@users.noreply.github.com>
2021-10-18 14:34:20 +0530

    Update join.py

--- strings/join.py
+++ strings/join.py
@@ -5,15 +5,15 @@ Program to join a list of strings with a given separator
 
 def join(separator: str = "", separated: list) -> str:
     """
-    >>> join(["a", "b", "c", "d"])
+    >>> join(separated = ["a", "b", "c", "d"])
     'abcd'
-    >>> join(["a", "b", "c", "d"], "#")
+    >>> join("#", ["a", "b", "c", "d"])
     'a#b#c#d'
-    >>> join("a", "#")
+    >>> join("#", "a")
     'a'
-    >>> join(["You", "are", "amazing!"], " ")
+    >>> join(" ", ["You", "are", "amazing!"])
     'You are amazing!'
-    >>> join(["a", "b", "c", 1], "#")
+    >>> join("#", ["a", "b", "c", 1])
     Traceback (most recent call last):
     ...
     Exception: join() accepts only strings to be joined
