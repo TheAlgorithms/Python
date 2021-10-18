1f86f2ea8973d4e83fe3b91318f6f8c6afa3ccf7
patrick-steve <75305373+patrick-steve@users.noreply.github.com>
2021-10-18 15:41:23 +0530

    Added checkISBN

---
+++ strings/checkISBN.py
@@ -0,0 +1,37 @@
+# Python code to check if a
+# given ISBN is valid or not.
+
+def isValidISBN(isbn):
+
+	# check for length
+	if len(isbn) != 10:
+		return False
+	
+	# Computing weighted sum
+	# of first 9 digits
+	_sum = 0
+	for i in range(9):
+		if 0 <= int(isbn[i]) <= 9:
+			_sum += int(isbn[i]) * (10 - i)
+		else:
+			return False
+		
+	# Checking last digit
+	if(isbn[9] != 'X' and
+	0 <= int(isbn[9]) <= 9):
+		return False
+	
+	# If last digit is 'X', add
+	# 10 to sum, else add its value.
+	_sum += 10 if isbn[9] == 'X' else int(isbn[9])
+	
+	# Return true if weighted sum of
+	# digits is divisible by 11
+	return (_sum % 11 == 0)
+
+# Test
+isbn = "007462542X"
+if isValidISBN(isbn):
+	print('Valid')
+else:
+	print("Invalid")
