026fd5e08687642bec2cf3e39a6db87f1ce311d7
Matthew Wisdom <matthewwisdom11@gmail.com>
2021-10-19 20:10:19 +0100

    Update nevilles_method.py

--- maths/nevilles_method.py
+++ maths/nevilles_method.py
@@ -45,15 +45,15 @@ te
         for i in range(2, n):
     TypeError: unsupported operand type(s) for -: 'str' and 'int'
     """
-    n = len(x)
+    n = len(x_points)
     q = [[0] * n for i in range(n)]
     for i in range(n):
-        q[i][1] = y[i]
+        q[i][1] = y_points[i]
 
     for i in range(2, n):
         for j in range(i, n):
-            q[j][i] = ((x0 - x[j - i + 1]) * q[j][i - 1] - (
-                x0 - x[j]) * q[j - 1][i - 1]) / (x[j] - x[j - i + 1])
+            q[j][i] = ((x0 - x_points[j - i + 1]) * q[j][i - 1] - (
+                x0 - x_points[j]) * q[j - 1][i - 1]) / (x_points[j] - x_points[j - i + 1])
 
     return [q[n - 1][n - 1], q]
 
