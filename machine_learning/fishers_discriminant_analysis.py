913b1066bd69fcd4fa1815b351c96caa93b49c95
Mudit <mudit.verma2014@gmail.com>
2021-10-19 14:30:30 -0700

    fixes to names and type hints

--- machine_learning/fishers_discriminant_analysis.py
+++ machine_learning/fishers_discriminant_analysis.py
@@ -32,12 +32,12 @@ import numpy as np
 from numpy.linalg import inv
 
 
-def get_XT() -> tuple:
+def get_xt() -> tuple:
     """
     Placeholder code to generate the training data, X, Y.
     (Y is also referred to as T in the code / docs).
 
-    >>> get_XT()
+    >>> get_xt()
     (array([[  0,   0],
            [  1,   0],
            [  2,   0],
@@ -114,9 +114,9 @@ def get_XT() -> tuple:
     return (X, T)
 
 
-def get_X_by_T(X, T) -> tuple:
+def get_x_by_t(X_input: np.ndarray, T_input: np.ndarray) -> tuple:
     """
-    >>> get_X_by_T([[1,1],[2,2]], [[1,0],[0,1]])
+    >>> get_x_by_t([[1,1],[2,2]], [[1,0],[0,1]])
     (array([[1, 1]]), array([[2, 2]]))
     """
 
@@ -124,19 +124,19 @@ def get_X_by_T(X, T) -> tuple:
     # learning purposes the code needn't be over-complex.
     # We have two classes.
 
-    X_1 = []
-    X_2 = []
+    x_1 = []
+    x_2 = []
 
-    for ix, t in enumerate(T):
+    for ix, t in enumerate(T_input):
         if t[0] == 1:
-            X_1.append(X[ix])
+            x_1.append(X_input[ix])
         else:
-            X_2.append(X[ix])
+            x_2.append(X_input[ix])
 
-    X_1 = np.array(X_1)
-    X_2 = np.array(X_2)
+    x_1 = np.array(x_1)
+    x_2 = np.array(x_2)
 
-    return (X_1, X_2)
+    return (x_1, x_2)
 
 
 def main() -> None:
@@ -148,11 +148,11 @@ def main() -> None:
     <BLANKLINE>
     <BLANKLINE>
     Step 2: Get X by classes in T, i.e. all X points of
-            class 1 as X_1, all points of class 2 as X_2.
+            class 1 as x_1, all points of class 2 as x_2.
     <BLANKLINE>
     <BLANKLINE>
     Step 3: Get means of X by class. i.e. m1_mat
-            and m2_mat as means of data X_1 and X_2 respectively.
+            and m2_mat as means of data x_1 and x_2 respectively.
     Mean 1 : [[13.70588235  1.23529412]]
     Mean 2 : [[28.  3.]]
     <BLANKLINE>
@@ -182,21 +182,21 @@ def main() -> None:
     Prediction Label : [1]
     """
     print("\n\nStep 1: Get X, T training data.")
-    X, T = get_XT()
+    X, T = get_xt()
 
     print(
         """\n\nStep 2: Get X by classes in T, i.e. all X points of
-        class 1 as X_1, all points of class 2 as X_2."""
+        class 1 as x_1, all points of class 2 as x_2."""
     )
-    X_1, X_2 = get_X_by_T(X, T)
+    x_1, x_2 = get_x_by_t(X, T)
 
     print(
         """\n\nStep 3: Get means of X by class. i.e. m1_mat
-        and m2_mat as means of data X_1 and X_2 respectively."""
+        and m2_mat as means of data x_1 and x_2 respectively."""
     )
 
-    m1_mat = np.mean(X_1, axis=0).reshape(2, 1)
-    m2_mat = np.mean(X_2, axis=0).reshape(2, 1)
+    m1_mat = np.mean(x_1, axis=0).reshape(2, 1)
+    m2_mat = np.mean(x_2, axis=0).reshape(2, 1)
     print("Mean 1 :", np.transpose(m1_mat))
     print("Mean 2 :", np.transpose(m2_mat))
 
@@ -208,14 +208,14 @@ def main() -> None:
 
     s1w = np.zeros((2, 2))
 
-    for xn in X_1:
+    for xn in x_1:
         ff = xn.reshape(2, 1) - m1_mat.reshape(2, 1)
         ans = np.matmul(ff, np.transpose(ff))
         s1w += ans
 
     s2w = np.zeros((2, 2))
 
-    for xn in X_2:
+    for xn in x_2:
         ff = xn.reshape(2, 1) - m2_mat.reshape(2, 1)
         ans = np.matmul(ff, np.transpose(ff))
         s2w += ans
@@ -231,9 +231,9 @@ def main() -> None:
     print("\n\nStep 7: Calculate Fisher's Criterion J(w)")
     num = np.matmul(np.matmul(np.transpose(w), s_b), w)
     den = np.matmul(np.matmul(np.transpose(w), sw), w)
-    J_w = num / den
+    j_w = num / den
 
-    print(J_w)
+    print(j_w)
 
     print("\n\nStep 8: Perform Prediction on last data point in the training set.")
     point = X[0].reshape(2, 1)
