08eabb42e662010a8ced09fdfa4edc75d6d8da12
Matthew Wisdom <matthewwisdom11@gmail.com>
2021-10-19 20:22:16 +0100

    Fixed some linting issues

--- maths/nevilles_method.py
+++ maths/nevilles_method.py
@@ -12,36 +12,28 @@
 def neville_interpolate(x_points: list, y_points: list, x0: int) -> list:
     """
        Interpolate and evaluate a polynomial using Neville's method.
-
        Arguments:
-           x_points, y_points: Iterables of x and corresponding y points through which the
-                 polynomial passes.
+           x_points, y_points: Iterables of x and corresponding y points through
+            which the polynomial passes.
            x0: The value of x to evaluate the polynomial for.
-
        Return Value: A list of the approximated value and the Neville iterations
            table respectively.
-
     >>> import pprint
-
     >>> neville_interpolate((1,2,3,4,6), (6,7,8,9,11), 5)[0]
     10.0
-
     >>> pprint.pprint(neville_interpolate((1,2,3,4,6), (6,7,8,9,11), 99)[1])
     [[0, 6, 0, 0, 0],
      [0, 7, 0, 0, 0],
      [0, 8, 104.0, 0, 0],
      [0, 9, 104.0, 104.0, 0],
      [0, 11, 104.0, 104.0, 104.0]]
-
-
     >>> neville_interpolate((1,2,3,4,6), (6,7,8,9,11), 99)[0]
     104.0
-
     >>> neville_interpolate((1,2,3,4,6), (6,7,8,9,11), '')
     Traceback (most recent call last):
       File "<stdin>", line 1, in <module>
-      File "/home/mattwisdom/thealgo/nevilles_method.py", line 35, in neville_interpola\
-te
+      File "/home/mattwisdom/thealgo/nevilles_method.py", line 35, in neville_interpo\
+late
         for i in range(2, n):
     TypeError: unsupported operand type(s) for -: 'str' and 'int'
     """
@@ -53,7 +45,8 @@ te
     for i in range(2, n):
         for j in range(i, n):
             q[j][i] = ((x0 - x_points[j - i + 1]) * q[j][i - 1] - (
-                x0 - x_points[j]) * q[j - 1][i - 1]) / (x_points[j] - x_points[j - i + 1])
+                x0 - x_points[j]) * q[j - 1][i - 1]) / (
+                x_points[j] - x_points[j - i + 1])
 
     return [q[n - 1][n - 1], q]
 
