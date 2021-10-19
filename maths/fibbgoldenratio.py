c5ec2429c510fd9eaf7b5a890b8d72a8eb589761
zehra47 <73516108+zehra47@users.noreply.github.com>
2021-10-19 11:19:40 +0530

    Create fibbgoldenratio.py

---
+++ maths/fibbgoldenratio.py
@@ -0,0 +1,16 @@
+PHI = 1.6180339
+f = [ 0, 1, 1, 2, 3, 5 ]
+def fib ( n ):
+ 
+    if n < 6:
+        return f[n]
+    t = 5
+    fn = 5
+     
+    while t < n:
+        fn = round(fn * PHI)
+        t+=1
+     
+    return fn
+n = 9
+print(n, "th Fibonacci Number =", fib(n))
