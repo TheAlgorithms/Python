fb6d746641b40b515e2a02f226923b9747798177
Atharva <atharva7275@gmail.com>
2021-10-20 11:44:46 +0530

    Added algorithm for Longest Common SUBSTRING under Dynammic Programming

--- dynamic_programming/longest_common_substring.py
+++ dynamic_programming/longest_common_substring.py
@@ -3,39 +3,103 @@
 # Name: Atharva Patil
 # Github Profile link:  https://github.com/atharvapatil123 
  
-def longest_common_substring(X, Y, n, m):
+def longest_common_substring(X: str, Y: str, n: int, m: int):
 
-    # res variable is used to store the result: Maximum length of common subtring
-    res = 0
+    # Parameters
+
+    # X: 1st string
+    # Y: 2nd string
+    # n: length of X string
+    # m: length of Y string
+
+    # Returns
+
+    # The funtion longest_common_substring returns 2 things:
+    # 1. length: This is the length of longest common substring of the 2 strings
+    # 2. string: This is the actual string (longest common substring)
+
+
+    # longest_common_substring("coding", "code")
+    # length = 3, string = 'cod')
+
+    # longest_common_substring("fishing", "fighting")
+    # length = 3, string = 'ing')
+
+    # longest_common_substring("health", "wealth")
+    # length = 5, string = 'ealth'
+
+    # longest_common_substring("no", "yes")
+    # length = 0, string = none
+
+    res = 0 # res variable is used to store the result: Maximum length of common subtring
+    string="" # string is used to store the longest common substring
 
     # Create a table to store Longest Common suffixes 
     # Initially, all cells are initialized with -1
-    table = [[-1 for k in range(m+1)] for l in range(n+1)]
+    table = [[-1 for i in range(m+1)] for j in range(n+1)]
 
     # Making first row and column entirely 0
     for i in range(n+1):
         for j in range(m+1):
             if(i==0 or j==0): 
                 table[i][j]=0
+
             
+    # To store the indices of the table with maximum value
+    k=0
+    l=0
 
     for i in range(1,n+1):
         for j in range(1,m+1):
             if(X[i-1]==Y[j-1]):
                 table[i][j] = 1 + table[i-1][j-1]
-                res = max(res, table[i][j]) 
+                if(table[i][j]>res):
+                    res = table[i][j]
+                    k=i # stores row value with maximum value in table
+                    l=j # stores column value with maximum value in table
             else:
                 table[i][j] = 0;
 
-    return res
+    i=k
+    j=l
+    while(i>0 and j>0):
+        if(X[i-1]==Y[j-1]):
+            string = string+X[i-1] # Use to store longest common substring
+        i=i-1
+        j=j-1
+
+    string = "".join(reversed(string))
+
+    return res, string
  
  
 # Driver Code
-X = "abcde"
-Y = "abfcde"
- 
-n = len(X)
-m = len(Y)
 
-print('\nLength of Longest Common Substring is', longest_common_substring(X, Y, n, m))
-# 3 is returned
+if __name__ == "__main__":
+    X = "abcde"
+    Y = "abfcde"
+    
+    # X and Y represent 2 strings whose longest common substring is to be found
+    # That is here, X=abcde and Y=abfcde
+    # Thus, longest common substring possible is cde
+
+    n = len(X) # n represent length of string X
+    m = len(Y) # m represent length of string Y
+
+    
+    length, string = longest_common_substring(X, Y, n, m)
+    # The function returns 2 things
+    # 1. length: This is the length of longest common substring of the 2 strings
+    # 2. string: This is the actual string (longest common substring)
+
+    # If length=0, then no common substring is present between the two strings
+    if(length==0):
+        print('\nThere is no longest common substring possible')
+
+    else:        
+        print('\nLength of Longest Common Substring is', length)
+        print('Longest Common Substring is', string)
+
+    import doctest
+
+    doctest.testmod()
