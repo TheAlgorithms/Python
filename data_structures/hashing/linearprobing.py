d44b486fd52e4bad002c1a65f896156d9909ac6e
Harsha Sri Sameera <70195328+HarshaSri-Sameera@users.noreply.github.com>
2021-10-21 00:12:33 +0530

    Added Linear Probing program in python
    For more detailed information on the linear probing visit, https://en.wikipedia.org/wiki/Linear_probing

--- data_structures/hashing/linearprobing.py
+++ data_structures/hashing/linearprobing.py
@@ -1,47 +1,59 @@
 import numpy as np
 
-# Linear Probing (we linearly probe for next slot)
+# Linear probing program
 class Hash_table() :
     
     def __init__(self,size) :
         self.size = size
         self.hashtable = np.array([None]*self.size)
-        
-    def hash(self,key) :
-        
-        # Hash function h(x) = x%10
+
+    def hash(self,key: int) -> int:
+        """
+        Hash function h(x) = x%10
+        returns the index of the hashtable
+        :param key value
+        """
         index = key%10 
-        
+
         if self.hashtable[index] == None :
             return index
         else :
-            
+
             # Implementing linear probing
             while self.hashtable[index] != None :
                 index = (index+1)%10
-                
+
             return index
-        
-    def insert(self,key) :
-        
+
+    def insert(self,key: int):
+        """
+        this function inserts an element into the hash table.
+        :param: key
+        """
         index = self.hash(key)
         self.hashtable[index] = key
-        
-    def search(self,key) :
-        
+
+    def search(self,key: int) -> int:
+        """
+        searching the key element present in the hash table or not
+        returns: index of the element (if founded)
+                 else None
+        """
         index = key%10
-        
+
         if self.hashtable[index] != key :
             while self.hashtable[index] != key and self.hashtable[index] != None :
                 index = (index+1)%10
-                
+
         if self.hashtable[index] == key :
             return index
         else :
             return None
-    
+
     def print_hashtable(self) :
-        
+        """
+        prints the hash table when called
+        """
         print("Hash table is :-\n\nindex \t value")
         for x in range(len(self.hashtable)) :
             print(x,"\t",self.hashtable[x])
@@ -64,3 +76,21 @@ if index!= None :
     print("\nGiven key is present at index",index)
 else :
     print("\nGiven key is not present in the hash table")
+    
+    
+# output
+'''Hash table is :-
+
+index 	 value
+0 	 10
+1 	 90
+2 	 None
+3 	 None
+4 	 None
+5 	 25
+6 	 5
+7 	 35
+8 	 None
+9 	 None
+
+Given key is present at index 7'''
