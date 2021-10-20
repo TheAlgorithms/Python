7d6767e8bbd99900955f75d5bbbdead144d835cd
Atharva <atharva7275@gmail.com>
2021-10-20 12:00:07 +0530

    Added algorithm for Longest Common SUBSTRING under Dynammic Programming

--- dynamic_programming/longest_common_substring.py
+++ dynamic_programming/longest_common_substring.py
@@ -1,41 +1,50 @@
-# Dynamic Programming (DP) : Algorithm for finding Longest Common Substring in python
+"""
+Dynamic Programming (DP) : Algorithm for finding Longest Common Substring in python
 
-# Name: Atharva Patil
-# Github Profile link:  https://github.com/atharvapatil123 
+LONGEST COMMON SUBSTRING PROBLEM:  Given two strings, find the length of longest substring
+present in both of them.  A substring is a sequence that appears in the same relative
+order and is necessarily continuous.
+
+Name: Atharva Patil
+Github Profile link:  https://github.com/atharvapatil123 
+"""
  
 def longest_common_substring(X: str, Y: str, n: int, m: int):
+    """
+    Parameters
 
-    # Parameters
+    X: 1st string
+    Y: 2nd string
+    n: length of X string
+    m: length of Y string
 
-    # X: 1st string
-    # Y: 2nd string
-    # n: length of X string
-    # m: length of Y string
+    Returns
 
-    # Returns
-
-    # The funtion longest_common_substring returns 2 things:
-    # 1. length: This is the length of longest common substring of the 2 strings
-    # 2. string: This is the actual string (longest common substring)
+    The funtion longest_common_substring returns 2 things:
+    1. length: This is the length of longest common substring of the 2 strings
+    2. string: This is the actual string (longest common substring)
 
 
-    # longest_common_substring("coding", "code")
-    # length = 3, string = 'cod')
+    longest_common_substring("coding", "code")
+    length = 3, string = 'cod')
 
-    # longest_common_substring("fishing", "fighting")
-    # length = 3, string = 'ing')
+    longest_common_substring("fishing", "fighting")
+    length = 3, string = 'ing')
 
-    # longest_common_substring("health", "wealth")
-    # length = 5, string = 'ealth'
+    longest_common_substring("health", "wealth")
+    length = 5, string = 'ealth'
 
-    # longest_common_substring("no", "yes")
-    # length = 0, string = none
+    longest_common_substring("no", "yes")
+    length = 0, string = none
+    """
 
     res = 0 # res variable is used to store the result: Maximum length of common subtring
     string="" # string is used to store the longest common substring
 
-    # Create a table to store Longest Common suffixes 
-    # Initially, all cells are initialized with -1
+    """
+    Create a table to store Longest Common suffixes 
+    Initially, all cells are initialized with -1
+    """
     table = [[-1 for i in range(m+1)] for j in range(n+1)]
 
     # Making first row and column entirely 0
@@ -79,20 +88,30 @@ if __name__ == "__main__":
     X = "abcde"
     Y = "abfcde"
     
-    # X and Y represent 2 strings whose longest common substring is to be found
-    # That is here, X=abcde and Y=abfcde
-    # Thus, longest common substring possible is cde
+    """
+    X and Y represent 2 strings whose longest common substring is to be found
+    That is here, X=abcde and Y=abfcde
+    Thus, longest common substring possible is cde
+    """
 
-    n = len(X) # n represent length of string X
-    m = len(Y) # m represent length of string Y
+    """
+    n represent length of string X
+    m represent length of string Y
+    """
+    n = len(X)
+    m = len(Y) 
 
     
     length, string = longest_common_substring(X, Y, n, m)
-    # The function returns 2 things
-    # 1. length: This is the length of longest common substring of the 2 strings
-    # 2. string: This is the actual string (longest common substring)
+    """
+    The function returns 2 things
+    1. length: This is the length of longest common substring of the 2 strings
+    2. string: This is the actual string (longest common substring)
+    """
 
-    # If length=0, then no common substring is present between the two strings
+    """
+    If length=0, then no common substring is present between the two strings
+    """
     if(length==0):
         print('\nThere is no longest common substring possible')
 
