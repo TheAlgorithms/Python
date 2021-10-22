af5888726f76ed3099efff62a39e0c4d9121981a
Studiex <80968515+Studiex@users.noreply.github.com>
2021-10-23 01:04:59 +0400

    Update sha256.py

--- hashes/sha256.py
+++ hashes/sha256.py
@@ -137,7 +137,7 @@ class SHA256HashTest(unittest.TestCase):
     Test class for the SHA256 class. Inherits the TestCase class from unittest
     """
 
-    def testMatchHashes(self) -> None:
+    def test_match_hashes(self) -> None:
         msg = bytes("Test String", "utf-8")
         self.assertEqual(SHA256(msg).hash, hashlib.sha256(msg).hexdigest())
 
@@ -148,7 +148,7 @@ def main() -> None:
     and prints the calculated SHA-256 hash
     """
 
-    # unittest.main()
+    unittest.main()
 
     import doctest
 
