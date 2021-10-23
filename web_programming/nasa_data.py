098cf751a85d330f0f31b3c9eff33323996b8d55
Jaydeep Das <jaydeepjd.8914@gmail.com>
2021-10-23 15:58:37 +0530

    Modified as per review

--- web_programming/nasa_data.py
+++ web_programming/nasa_data.py
@@ -1,5 +1,4 @@
 import requests
-from requests.exceptions import HTTPError
 
 
 def get_apod_data(api_key: str) -> dict:
@@ -7,24 +6,17 @@ def get_apod_data(api_key: str) -> dict:
     Get the APOD(Astronomical Picture of the day) data
     Get the API Key from : https://api.nasa.gov/
     """
-    try:
-        return requests.get(
-            f"https://api.nasa.gov/planetary/apod/?api_key={api_key}"
-        ).json()
-    except requests.exceptions.HTTPError:
-        raise HTTPError
+    return requests.get(
+        f"https://api.nasa.gov/planetary/apod/?api_key={api_key}"
+    ).json()
 
 
 def get_archive_data(query: str) -> dict:
     """
     Get the data of a particular query from NASA archives
     """
-    formatted_query = query.strip().replace(" ", "%")
-    endpoint = f"https://images-api.nasa.gov/search?q={formatted_query}"
-    try:
-        return requests.get(endpoint).json()
-    except requests.exceptions.HTTPError:
-        raise HTTPError
+    endpoint = "https://images-api.nasa.gov/search"
+    return requests.get(endpoint, params={"q": query}).json()
 
 
 if __name__ == "__main__":
