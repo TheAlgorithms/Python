5096492d328df3ebf8eb485b3955a8c8fd7cbc44
Christopher Swenson <swenson@swenson.io>
2021-10-22 21:46:19 -0700

    Wrap long lines in rsa_factorization doctest; remove unnecessary annotations import

--- ciphers/rsa_factorization.py
+++ ciphers/rsa_factorization.py
@@ -6,8 +6,6 @@ public key e.
 Source: on page 3 of https://crypto.stanford.edu/~dabo/papers/RSA-survey.pdf
 More readable source: https://www.di-mgt.com.au/rsa_factorize_n.html
 """
-from __future__ import annotations
-
 import math
 import random
 from typing import List
@@ -30,8 +28,12 @@ def rsafactor(d: int, e: int, N: int) -> List[int]:
     [113, 241]
     >>> rsafactor(4021, 13, 17711)
     [89, 199]
-    >>> rsafactor(0x123c5b61ba36edb1d3679904199a89ea80c09b9122e1400c09adcf7784676d01d23356a7d44d6bd8bd50e94bfc723fa87d8862b75177691c11d757692df8881, 0x10001, 0xa66791dc6988168de7ab77419bb7fb0c001c62710270075142942e19a8d8c51d053b3e3782a1de5dc5af4ebe99468170114a1dfe67cdc9a9af55d655620bbab)
-    [23234950162188993388155927630085331316851060055334470382368804331834850828939, 23443439767333138692938389505422341860387525814723848738690073331642118819681]
+    >>> rsafactor(int("123c5b61ba36edb1d3679904199a89ea80c09b9122e1400c09adcf77846" +
+    ...    "76d01d23356a7d44d6bd8bd50e94bfc723fa87d8862b75177691c11d757692df8881", 16),
+    ...    0x10001,
+    ...    int("a66791dc6988168de7ab77419bb7fb0c001c62710270075142942e19a8d8c51d05" +
+    ...    "3b3e3782a1de5dc5af4ebe99468170114a1dfe67cdc9a9af55d655620bbab", 16))[0]
+    23234950162188993388155927630085331316851060055334470382368804331834850828939
     >>> rsafactor(4, 3, 13)
     Traceback (most recent call last):
     ...
