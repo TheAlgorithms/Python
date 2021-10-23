9c3595ed59bb1e2188b03ad8fab7b935499b4cb7
Christopher Swenson <swenson@swenson.io>
2021-10-22 19:54:16 -0700

    Speed improvements and bug fixes in ciphers/rsa_factorization.py
    * Using `(g ** t) % N` is significantly slower than `pow(g, t, N)` as
      the values get larger. Replacing this allows even large examples to
      run nearly instantly.
    * Added doctests from the cited sources
    * Remove unnecessary extra `while`/`if`.
    * Added a `ValueError` if the arguments are invalid. `e * d % ((p - 1) *
      (q - 1))` should be equal to 1, where `p` and `q` are primes greater
      than 2. Hence, `(p - 1) * (q - 1)` is even, and `e * d - 1` will also
      be even if they are valid.
    
    The underlying algorithm was not changed.

--- ciphers/rsa_factorization.py
+++ ciphers/rsa_factorization.py
@@ -5,15 +5,15 @@ The program can efficiently factor RSA prime number given the private key d and
 public key e.
 Source: on page 3 of https://crypto.stanford.edu/~dabo/papers/RSA-survey.pdf
 More readable source: https://www.di-mgt.com.au/rsa_factorize_n.html
-large number can take minutes to factor, therefore are not included in doctest.
 """
 from __future__ import annotations
 
 import math
 import random
+from typing import List
 
 
-def rsafactor(d: int, e: int, N: int) -> list[int]:
+def rsafactor(d: int, e: int, N: int) -> List[int]:
     """
     This function returns the factors of N, where p*q=N
       Return: [p, q]
@@ -30,25 +30,29 @@ def rsafactor(d: int, e: int, N: int) -> list[int]:
     [113, 241]
     >>> rsafactor(4021, 13, 17711)
     [89, 199]
+    >>> rsafactor(0x123c5b61ba36edb1d3679904199a89ea80c09b9122e1400c09adcf7784676d01d23356a7d44d6bd8bd50e94bfc723fa87d8862b75177691c11d757692df8881, 0x10001, 0xa66791dc6988168de7ab77419bb7fb0c001c62710270075142942e19a8d8c51d053b3e3782a1de5dc5af4ebe99468170114a1dfe67cdc9a9af55d655620bbab)
+    [23234950162188993388155927630085331316851060055334470382368804331834850828939, 23443439767333138692938389505422341860387525814723848738690073331642118819681]
+    >>> rsafactor(4, 3, 13)
+    Traceback (most recent call last):
+    ...
+    ValueError: e and d are not valid RSA exponents
     """
     k = d * e - 1
-    p = 0
-    q = 0
-    while p == 0:
+    if k & 1 == 1:
+        raise ValueError("e and d are not valid RSA exponents")
+
+    while True:
         g = random.randint(2, N - 1)
         t = k
-        while True:
-            if t % 2 == 0:
-                t = t // 2
-                x = (g ** t) % N
-                y = math.gcd(x - 1, N)
-                if x > 1 and y > 1:
-                    p = y
-                    q = N // y
-                    break  # find the correct factors
-            else:
-                break  # t is not divisible by 2, break and choose another g
-    return sorted([p, q])
+        while t & 1 == 0:
+            t >>= 1
+            x = pow(g, t, N)
+            y = math.gcd(x - 1, N)
+            if x > 1 and y > 1:
+                # we found the correct factors
+                p = y
+                q = N // y
+                return sorted([p, q])
 
 
 if __name__ == "__main__":
