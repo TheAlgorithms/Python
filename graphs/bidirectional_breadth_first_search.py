c1950ddf1533d99b3450af12f46e68dfc0aab240
Christian Clauss <cclauss@me.com>
2021-10-23 17:01:02 +0200

    Comment out slow tests

--- graphs/bidirectional_breadth_first_search.py
+++ graphs/bidirectional_breadth_first_search.py
@@ -34,16 +34,19 @@ class Node:
 
 class BreadthFirstSearch:
     """
-    >>> bfs = BreadthFirstSearch((0, 0), (len(grid) - 1, len(grid[0]) - 1))
-    >>> (bfs.start.pos_y + delta[3][0], bfs.start.pos_x + delta[3][1])
+    # Comment out slow pytests...
+    # 9.15s call     graphs/bidirectional_breadth_first_search.py:: \
+    #                graphs.bidirectional_breadth_first_search.BreadthFirstSearch 
+    # >>> bfs = BreadthFirstSearch((0, 0), (len(grid) - 1, len(grid[0]) - 1))
+    # >>> (bfs.start.pos_y + delta[3][0], bfs.start.pos_x + delta[3][1])
     (0, 1)
-    >>> [x.pos for x in bfs.get_successors(bfs.start)]
+    # >>> [x.pos for x in bfs.get_successors(bfs.start)]
     [(1, 0), (0, 1)]
-    >>> (bfs.start.pos_y + delta[2][0], bfs.start.pos_x + delta[2][1])
+    # >>> (bfs.start.pos_y + delta[2][0], bfs.start.pos_x + delta[2][1])
     (1, 0)
-    >>> bfs.retrace_path(bfs.start)
+    # >>> bfs.retrace_path(bfs.start)
     [(0, 0)]
-    >>> bfs.search()  # doctest: +NORMALIZE_WHITESPACE
+    # >>> bfs.search()  # doctest: +NORMALIZE_WHITESPACE
     [(0, 0), (1, 0), (2, 0), (3, 0), (3, 1), (4, 1),
      (5, 1), (5, 2), (5, 3), (5, 4), (5, 5), (6, 5), (6, 6)]
     """
@@ -106,17 +109,14 @@ class BreadthFirstSearch:
 
 class BidirectionalBreadthFirstSearch:
     """
-    # Comment out slow pytests...
-    # 9.15s call     graphs/bidirectional_breadth_first_search.py:: \
-    #                graphs.bidirectional_breadth_first_search.BreadthFirstSearch
-    # >>> bd_bfs = BidirectionalBreadthFirstSearch((0, 0), (len(grid) - 1,
+    >>> bd_bfs = BidirectionalBreadthFirstSearch((0, 0), (len(grid) - 1,
     ...                                                   len(grid[0]) - 1))
-    # >>> bd_bfs.fwd_bfs.start.pos == bd_bfs.bwd_bfs.target.pos
+    >>> bd_bfs.fwd_bfs.start.pos == bd_bfs.bwd_bfs.target.pos
     True
-    # >>> bd_bfs.retrace_bidirectional_path(bd_bfs.fwd_bfs.start,
+    >>> bd_bfs.retrace_bidirectional_path(bd_bfs.fwd_bfs.start,
     ...                                     bd_bfs.bwd_bfs.start)
     [(0, 0)]
-    # >>> bd_bfs.search()  # doctest: +NORMALIZE_WHITESPACE
+    >>> bd_bfs.search()  # doctest: +NORMALIZE_WHITESPACE
     [(0, 0), (0, 1), (0, 2), (1, 2), (2, 2), (2, 3),
      (2, 4), (3, 4), (3, 5), (3, 6), (4, 6), (5, 6), (6, 6)]
     """
