170a818259d1f60fd4893075b4296d93f8ecebf7
Jaydeep Das <jaydeepjd.8914@gmail.com>
2021-10-23 16:32:01 +0530

    Added a feature to download images.

--- web_programming/nasa_data.py
+++ web_programming/nasa_data.py
@@ -1,13 +1,23 @@
+import shutil
 import requests
 
 
-def get_apod_data(api_key: str) -> dict:
+def get_apod_data(api_key: str, download: bool = False, path: str = ".") -> dict:
     """
     Get the APOD(Astronomical Picture of the day) data
     Get the API Key from : https://api.nasa.gov/
     """
     url = "https://api.nasa.gov/planetary/apod/"
-    return requests.get(url, params={"api_key": api_key}).json()
+    data = requests.get(url, params={"api_key": api_key}).json()
+    if download:
+        img_url = data["url"]
+        img_name = img_url.split("/")[-1]
+        response = requests.get(img_url, stream=True)
+
+        with open(f"{path}/{img_name}", "wb+") as img_file:
+            shutil.copyfileobj(response.raw, img_file)
+        del response
+    return data
 
 
 def get_archive_data(query: str) -> dict:
