bf416b0f983ba47aa69cf6f03dfc29fda616aa30
Studiex <80968515+Studiex@users.noreply.github.com>
2021-10-23 13:04:36 +0400

    Update sha256.py

--- hashes/sha256.py
+++ hashes/sha256.py
@@ -33,24 +33,85 @@ class SHA256:
     def __init__(self, data: bytes) -> None:
         self.data = data
 
-        # fmt: off
-
         # Initialize hash values
-        self.h = [0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19]
+        self.h = [
+            0x6A09E667,
+            0xBB67AE85,
+            0x3C6EF372,
+            0xA54FF53A,
+            0x510E527F,
+            0x9B05688C,
+            0x1F83D9AB,
+            0x5BE0CD19,
+        ]
 
         # Initialize round constants
         self.round_constants = [
-            0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
-            0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,
-            0xe49b69c1,0xefbe4786,0x0fc19dc6,0x240ca1cc,0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
-            0x983e5152,0xa831c66d,0xb00327c8,0xbf597fc7,0xc6e00bf3,0xd5a79147,0x06ca6351,0x14292967,
-            0x27b70a85,0x2e1b2138,0x4d2c6dfc,0x53380d13,0x650a7354,0x766a0abb,0x81c2c92e,0x92722c85,
-            0xa2bfe8a1,0xa81a664b,0xc24b8b70,0xc76c51a3,0xd192e819,0xd6990624,0xf40e3585,0x106aa070,
-            0x19a4c116,0x1e376c08,0x2748774c,0x34b0bcb5,0x391c0cb3,0x4ed8aa4a,0x5b9cca4f,0x682e6ff3,
-            0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2
+            0x428A2F98,
+            0x71374491,
+            0xB5C0FBCF,
+            0xE9B5DBA5,
+            0x3956C25B,
+            0x59F111F1,
+            0x923F82A4,
+            0xAB1C5ED5,
+            0xD807AA98,
+            0x12835B01,
+            0x243185BE,
+            0x550C7DC3,
+            0x72BE5D74,
+            0x80DEB1FE,
+            0x9BDC06A7,
+            0xC19BF174,
+            0xE49B69C1,
+            0xEFBE4786,
+            0x0FC19DC6,
+            0x240CA1CC,
+            0x2DE92C6F,
+            0x4A7484AA,
+            0x5CB0A9DC,
+            0x76F988DA,
+            0x983E5152,
+            0xA831C66D,
+            0xB00327C8,
+            0xBF597FC7,
+            0xC6E00BF3,
+            0xD5A79147,
+            0x06CA6351,
+            0x14292967,
+            0x27B70A85,
+            0x2E1B2138,
+            0x4D2C6DFC,
+            0x53380D13,
+            0x650A7354,
+            0x766A0ABB,
+            0x81C2C92E,
+            0x92722C85,
+            0xA2BFE8A1,
+            0xA81A664B,
+            0xC24B8B70,
+            0xC76C51A3,
+            0xD192E819,
+            0xD6990624,
+            0xF40E3585,
+            0x106AA070,
+            0x19A4C116,
+            0x1E376C08,
+            0x2748774C,
+            0x34B0BCB5,
+            0x391C0CB3,
+            0x4ED8AA4A,
+            0x5B9CCA4F,
+            0x682E6FF3,
+            0x748F82EE,
+            0x78A5636F,
+            0x84C87814,
+            0x8CC70208,
+            0x90BEFFFA,
+            0xA4506CEB,
+            0xBEF9A3F7,
+            0xC67178F2,
         ]
-
-        # fmt: on
 
         self.preprocessed_data = self.preprocessing()
         self.final_hash()
