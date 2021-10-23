414a2717ba5321c1d88e58623867cffd5b0a2653
Christian Clauss <cclauss@me.com>
2021-10-23 12:50:43 +0200

    print(get_archive_data("apollo 2011")["collection"]["items"][0]["data"][0]["description"])

--- web_programming/nasa_data.py
+++ web_programming/nasa_data.py
@@ -20,4 +20,4 @@ def get_archive_data(query: str) -> dict:
 
 if __name__ == "__main__":
     print(get_apod_data("YOUR API KEY"))
-    # print(get_archive_data("apollo 2011"))
+    print(get_archive_data("apollo 2011")["collection"]["items"][0]["data"][0]["description"])
