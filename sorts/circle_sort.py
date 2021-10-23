5560fcead60c95612992088811a7f8ba94cba76a
Kanakalatha Vemuru <kanakalathav99@gmail.com>
2021-10-23 15:21:21 +0530

    Added modifications

--- sorts/circle_sort.py
+++ sorts/circle_sort.py
@@ -40,7 +40,10 @@ def circle_sort(collection: list) -> list:
 
         while left < right:
             if collection[left] > collection[right]:
-                collection[left], collection[right] = collection[right], collection[left]
+                collection[left], collection[right] = (
+                    collection[right],
+                    collection[left]
+                )
                 swapped = True
 
             left += 1
@@ -48,11 +51,14 @@ def circle_sort(collection: list) -> list:
 
         if left == right:
             if collection[left] > collection[right + 1]:
-                collection[left], collection[right + 1] = collection[right + 1], collection[left]
+                collection[left], collection[right + 1] = (
+                    collection[right + 1], 
+                    collection[left]
+                )
                 
                 swapped = True
                 
-        mid = low + int((high - low)/2)
+        mid = low + int((high - low) / 2)
         left_swap = circle_sort_util(collection, low, mid)
         right_swap = circle_sort_util(collection, mid + 1, high)
 
