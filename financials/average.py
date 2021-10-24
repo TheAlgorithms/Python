ff0cde76e69efb1ee36b258d633009dce1fceeac
tarcisiobruni <tarcisiobruni@hotmail.com>
2021-10-24 15:57:11 -0300

    refactor: make pull request items requirements

--- financials/average.py
+++
@@ -1,5 +0,0 @@
-def weighted(ratio,rates):
-    result = 0
-    for index in range(len(ratio)):
-        result = result + ratio[index]*rates[index]
-    return result
--- financials/interest.py
+++ financials/interest.py
@@ -1,8 +1,23 @@
-def simple_interest(principle, daily_interest_rate, number_of_days_between_payment):
+from __future__ import annotations
+
+def simple_interest(principle, daily_interest_rate, number_of_days_between_payment) -> float:
+    """
+    >>> simple_interest(18000,0.06,3)
+    3240.0
+    """
     result = principle * daily_interest_rate * number_of_days_between_payment
     return result
 
-def compound_interest(principle, nominal_annual_interest_rate_percentage , number_of_compounding_periods):
+def compound_interest(principle, nominal_annual_interest_rate_percentage , number_of_compounding_periods)-> float:
+    """
+    >>> compound_interest(10000,0.05,3)
+    1576.2500000000014
+    """
     result = principle * ((1 + nominal_annual_interest_rate_percentage) ** number_of_compounding_periods - 1)
     return result
 
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
