e00a8750012f61ee3194449a7d2a42b3b525c714
tarcisiobruni <tarcisiobruni@hotmail.com>
2021-10-24 16:01:33 -0300

    refactor: provides type hint for parameters

--- financials/interest.py
+++ financials/interest.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-def simple_interest(principle, daily_interest_rate, number_of_days_between_payment) -> float:
+def simple_interest(principle:float, daily_interest_rate:float, number_of_days_between_payment:int) -> float:
     """
     >>> simple_interest(18000,0.06,3)
     3240.0
@@ -8,7 +8,7 @@ def simple_interest(principle, daily_interest_rate, number_of_days_between_payme
     result = principle * daily_interest_rate * number_of_days_between_payment
     return result
 
-def compound_interest(principle, nominal_annual_interest_rate_percentage , number_of_compounding_periods)-> float:
+def compound_interest(principle:float, nominal_annual_interest_rate_percentage:float, number_of_compounding_periods:int)-> float:
     """
     >>> compound_interest(10000,0.05,3)
     1576.2500000000014
