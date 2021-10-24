1e5cc3043ce7daae90cf83ba4bad21624861bfa2
tarcisiobruni <tarcisiobruni@hotmail.com>
2021-10-24 16:13:06 -0300

    refactor: applies code review suggestions

--- financials/interest.py
+++ financials/interest.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 
-def simple_interest(principle:float, daily_interest_rate:float, number_of_days_between_payment:int) -> float:
+def simple_interest(principle:float, daily_interest_rate:float, number_of_days_between_payments:int) -> float:
     """
     >>> simple_interest(18000,0.06,3)
     3240.0
     """
-    result = principle * daily_interest_rate * number_of_days_between_payment
+    result = principle * daily_interest_rate * number_of_days_between_payments
     return result
 
 def compound_interest(principle:float, nominal_annual_interest_rate_percentage:float, number_of_compounding_periods:int)-> float:
