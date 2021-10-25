184709eb3c8e6216a9119250b2764bbc3f9ef8a1
Prakhar Gurunani <prakhargurunani@gmail.com>
2021-10-25 18:45:40 +0530

    Reformat code with black

--- web_programming/get_user_tweets.py
+++ web_programming/get_user_tweets.py
@@ -1,7 +1,7 @@
-import tweepy 
+import tweepy
 import csv
 
-#Twitter API credentials
+# Twitter API credentials
 consumer_key = ""
 consumer_secret = ""
 access_key = ""
@@ -9,51 +9,53 @@ access_secret = ""
 
 
 def get_all_tweets(screen_name):
-    
-    #authorize twitter, initialize tweepy
+
+    # authorize twitter, initialize tweepy
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_key, access_secret)
     api = tweepy.API(auth)
-    
-    #initialize a list to hold all the tweepy Tweets
-    alltweets = []  
-    
-    #make initial request for most recent tweets (200 is the maximum allowed count)
-    new_tweets = api.user_timeline(screen_name = screen_name,count=200)
-    
-    #save most recent tweets
+
+    # initialize a list to hold all the tweepy Tweets
+    alltweets = []
+
+    # make initial request for most recent tweets (200 is the maximum allowed count)
+    new_tweets = api.user_timeline(screen_name=screen_name, count=200)
+
+    # save most recent tweets
     alltweets.extend(new_tweets)
-    
-    #save the id of the oldest tweet less one
+
+    # save the id of the oldest tweet less one
     oldest = alltweets[-1].id - 1
-    
-    #keep grabbing tweets until there are no tweets left to grab
+
+    # keep grabbing tweets until there are no tweets left to grab
     while len(new_tweets) > 0:
         print(f"getting tweets before {oldest}")
-        
-        #all subsiquent requests use the max_id param to prevent duplicates
-        new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
-        
-        #save most recent tweets
+
+        # all subsiquent requests use the max_id param to prevent duplicates
+        new_tweets = api.user_timeline(
+            screen_name=screen_name, count=200, max_id=oldest
+        )
+
+        # save most recent tweets
         alltweets.extend(new_tweets)
-        
-        #update the id of the oldest tweet less one
+
+        # update the id of the oldest tweet less one
         oldest = alltweets[-1].id - 1
-        
+
         print(f"...{len(alltweets)} tweets downloaded so far")
-    
-    #transform the tweepy tweets into a 2D array that will populate the csv 
+
+    # transform the tweepy tweets into a 2D array that will populate the csv
     outtweets = [[tweet.id_str, tweet.created_at, tweet.text] for tweet in alltweets]
-    
-    #write the csv  
-    with open(f'new_{screen_name}_tweets.csv', 'w') as f:
+
+    # write the csv
+    with open(f"new_{screen_name}_tweets.csv", "w") as f:
         writer = csv.writer(f)
-        writer.writerow(["id","created_at","text"])
+        writer.writerow(["id", "created_at", "text"])
         writer.writerows(outtweets)
-    
+
     pass
 
 
-if __name__ == '__main__':
-	#pass in the username of the account you want to download
-	get_all_tweets("FirePing32")
+if __name__ == "__main__":
+    # pass in the username of the account you want to download
+    get_all_tweets("FirePing32")
