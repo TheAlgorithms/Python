33d2b31275accabbeb0f314a438e7e930b5a4af2
Mohammad Firmansyah <76118762+dimasdh842@users.noreply.github.com>
2021-10-25 14:51:21 +0000

    Update index.py

--- python_codewars_disemvowel/index.py
+++ python_codewars_disemvowel/index.py
@@ -1,3 +1,12 @@
+"""
+This is the Test of SpinWords method.
+
+TheFunction will spin your words
+
+>>> spin_words("Hey wollef sroirraw")
+'Hey fellow warriors'
+"""
+
 def spin_words(sentence):
     sentences = sentence.split(" ")
 
@@ -7,7 +16,14 @@ def spin_words(sentence):
             newWord = "".join([(word[len(word) - e])  for e in range(1,len(sentences[i]) + 1)])
             sentences.remove(sentences[i])
             sentences.insert(i, newWord)
-    
-    return " ".join(sentences)
+    result = " ".join(sentences)
+    return result
+
 
 print(spin_words("Hey wollef sroirraw"))
+
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
