805fee5f3f3824b4cd3e48fa2ecba73595864771
henri <henri.gasc31@gmail.com>
2021-10-25 14:57:44 +0200

    Improved prime_numbers.py

--- maths/prime_numbers.py
+++ maths/prime_numbers.py
@@ -58,6 +58,38 @@ def primes(max: int) -> Generator[int, None, None]:
             yield i
 
 
+def fast_primes(max: int) -> Generator[int, None, None]:
+    """
+    Return a list of all primes numbers up to max.
+    >>> list(fast_primes(0))
+    []
+    >>> list(fast_primes(-1))
+    []
+    >>> list(fast_primes(-10))
+    []
+    >>> list(fast_primes(25))
+    [2, 3, 5, 7, 11, 13, 17, 19, 23]
+    >>> list(fast_primes(11))
+    [2, 3, 5, 7, 11]
+    >>> list(fast_primes(33))
+    [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
+    >>> list(fast_primes(10000))[-1]
+    9973
+    """
+    numbers: Generator = (i for i in range(1, (max + 1), 2))
+    # It's useless to test even numbers as they will not be prime
+    if max > 2:
+        yield 2  # Because 2 will not be tested, it's necessary to yield it now
+    for i in (n for n in numbers if n > 1):
+        bound = int(math.sqrt(i)) + 1
+        for j in range(3, bound, 2):
+            # As we removed the even numbers, we don't need them now
+            if (i % j) == 0:
+                break
+        else:
+            yield i
+
+
 if __name__ == "__main__":
     number = int(input("Calculate primes up to:\n>> ").strip())
     for ret in primes(number):
