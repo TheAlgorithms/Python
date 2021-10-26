129f823a57b6500745e1a29caa6afec0507bb34a
Dylan Buchi <devybuchi@gmail.com>
2021-10-26 14:48:42 -0300

    Update data_structures/binary_tree/binary_tree_traversals.py
    Co-authored-by: John Law <johnlaw.po@gmail.com>

--- data_structures/binary_tree/binary_tree_traversals.py
+++ data_structures/binary_tree/binary_tree_traversals.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 from collections import deque
 from dataclasses import dataclass
-from typing import Optional, Sequence
+from typing import Sequence
 
 
 @dataclass
