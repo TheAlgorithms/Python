44c6d8fb2cb2989f93a7f3b899077b853a63ec4e
Makrand SB <bhondemakrand7@gmail.com>
2021-10-26 13:01:14 +0530

    easy implementation of deleting a node from linked list

--- data_structures/linked_list/delete_Node.py
+++ data_structures/linked_list/delete_Node.py
@@ -5,19 +5,19 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
-    def pushAtHead(self, data):
-        newNode = Node(data)
-        newNode.next = self.head
-        self.head = newNode
+    def push_at_head(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
     # **deletion code starts**
-    def deleteNode(self, key):
+    def delete_node(self, key):
         temp = self.head
         while temp.next:
             if temp.next.data == key:       #if next node is the node what we want to delete
                 temp.next = temp.next.next  #breaking the link of node which has to be deleted
             temp = temp.next                #if condition not satisy then go to next node
     # **deletion code ends**
-    def printList(self):
+    def print_list(self):
         temp = self.head
         while temp:
             print(temp.data, end="->")
@@ -25,15 +25,15 @@ class LinkedList:
         print("NULL")
 
 if __name__=='__main__':
-    myObj = LinkedList()
-    myObj.pushAtHead(4)
-    myObj.pushAtHead(3)       
-    myObj.pushAtHead(2)       
-    myObj.pushAtHead(1)
-    myObj.pushAtHead(0)
+    myobj = LinkedList()
+    myobj.push_at_head(4)
+    myobj.push_at_head(3)       
+    myobj.push_at_head(2)       
+    myobj.push_at_head(1)
+    myobj.push_at_head(0)
     print("List before deleting the node: ")
-    myObj.printList()
-    myObj.deleteNode(3)
+    myobj.print_list()
+    myobj.delete_node(3)
     print("List after deleting the node: ")
-    myObj.printList()
+    myobj.print_list()
 
