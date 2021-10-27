de91676c311d4e0f635350526095063fe96a5ddc
TheHanyu <91917207+TheHanyu@users.noreply.github.com>
2021-10-26 20:21:24 -0700

    Create find-videogames-to-play.py

---
+++ find-videogames-to-play.py
@@ -0,0 +1,18 @@
+print("Are you trying to find new videogames to play?")
+print("let me help you!")
+print("do you like shooting games, yes or no")
+shooting=input()
+if shooting = "yes"
+  print("do you like battle royale games?")
+  br=input()
+    if br="yes"
+      print("you should try out call of duty!")
+    else if br="no"
+      print("you should try overwatch!")
+  else if shooting="no"
+    print("do you like sports games, yes or no")
+    sports=input()
+    if sports="yes"
+      print("try out Fifa or NBA2k!")
+      else if sports="no"
+        print("I know, try out rocket league!")
