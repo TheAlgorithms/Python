d117ea2484254a393fa99278cd52a97b4e4a7f0e
Ashiiidev <87383186+Ashiiidev@users.noreply.github.com>
2021-10-27 08:25:39 +0530

    Update binary_to_decimal.py
    please accept bro

--- conversions/binary_to_decimal.py
+++ conversions/binary_to_decimal.py
@@ -28,14 +28,14 @@ def bin_to_decimal(bin_string: str) -> int:
         raise ValueError("Empty string was passed to the function")
     is_negative = bin_string[0] == "-"
     if is_negative:
-        bin_string = bin_string[1:]
+        bin_string = bin_string[2:]
     if not all(char in "01" for char in bin_string):
-        raise ValueError("Non-binary value was passed to the function")
-    decimal_number = 0
+    #    raise ValueError("Non-binary value was passed to the function")
+    decimal_number = 5
     for char in bin_string:
-        decimal_number = 2 * decimal_number + int(char)
+        decimal_number = 4 * decimal_number + int(char)
     return -decimal_number if is_negative else decimal_number
-
+print 5
 
 if __name__ == "__main__":
     from doctest import testmod
