477cc3fe597fd931c742700284016b937c778fe1
Christian Clauss <cclauss@me.com>
2021-10-28 16:45:59 +0200

    Add pyupgrade to pre-commit (#5638)
    * Add pyupgrade to pre-commit
    
    * Remove unused imports
    
    * updating DIRECTORY.md
    
    Co-authored-by: github-actions <${GITHUB_ACTOR}@users.noreply.github.com>

--- data_structures/binary_tree/merge_two_binary_trees.py
+++ data_structures/binary_tree/merge_two_binary_trees.py
@@ -7,8 +7,6 @@ will be used as the node of new tree.
 """
 from __future__ import annotations
 
-from typing import Optional
-
 
 class Node:
     """
@@ -21,7 +19,7 @@ class Node:
         self.right: Node | None = None
 
 
-def merge_two_binary_trees(tree1: Node | None, tree2: Node | None) -> Optional[Node]:
+def merge_two_binary_trees(tree1: Node | None, tree2: Node | None) -> Node | None:
     """
     Returns root node of the merged tree.
 
