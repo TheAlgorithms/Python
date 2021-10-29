a89840e5f8267b2b9fbd38c010a051fa895f2cf2
kkumuda <73311121+kkumuda@users.noreply.github.com>
2021-10-29 15:27:47 +0530

    added code to calculate third side of right angled triangle using pythagorean theorem

--- linear_algebra/src/pythagorean_theorem.py
+++ linear_algebra/src/pythagorean_theorem.py
@@ -2,7 +2,7 @@ import math
 print("ENTER THE LENGHTS OF 2 SIDES OF THE RIGHT ANGLED TRIANGLE :")
 s1=int(input("ENTER SIDE S1 : "))
 s2=int(input("ENTER SIDE S2 : "))
-ss= s1**2+s2**2
-s3=math.sqrt(ss)
+sq= s1**2+s2**2
+s3=math.sqrt(sq)
 print("LENGHT OF SIDE 3 IS : ")
 print(s3)
