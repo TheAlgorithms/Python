1a970d3376386c3fe32bbea6d02d10a603304956
Aryaz Eghbali <64126826+AryazE@users.noreply.github.com>
2021-10-30 21:00:05 +0200

    Update alternate_disjoint_set.py
    Improved the efficiency by removing the rank attribute as 
    with Path Compression there is no need for Union by rank.
    Also added amortized running time description.

--- data_structures/disjoint_set/alternate_disjoint_set.py
+++ data_structures/disjoint_set/alternate_disjoint_set.py
@@ -1,6 +1,8 @@
 """
-Implements a disjoint set using Lists and some added heuristics for efficiency
-Union by Rank Heuristic and Path Compression
+Implements a disjoint set using Lists and Path Compression.
+Path compression allows O(log*(n)) amortized running time and
+there is no need for Union by rank heuristic for added
+efficiency
 """
 
 
@@ -8,17 +10,15 @@ class DisjointSet:
     def __init__(self, set_counts: list) -> None:
         """
         Initialize with a list of the number of items in each set
-        and with rank = 1 for each set
         """
         self.set_counts = set_counts
         self.max_set = max(set_counts)
         num_sets = len(set_counts)
-        self.ranks = [1] * num_sets
         self.parents = list(range(num_sets))
 
     def merge(self, src: int, dst: int) -> bool:
         """
-        Merge two sets together using Union by rank heuristic
+        Merge two sets together
         Return True if successful
         Merge two disjoint sets
         >>> A = DisjointSet([1, 1, 1])
@@ -35,25 +35,18 @@ class DisjointSet:
         if src_parent == dst_parent:
             return False
 
-        if self.ranks[dst_parent] >= self.ranks[src_parent]:
-            self.set_counts[dst_parent] += self.set_counts[src_parent]
-            self.set_counts[src_parent] = 0
-            self.parents[src_parent] = dst_parent
-            if self.ranks[dst_parent] == self.ranks[src_parent]:
-                self.ranks[dst_parent] += 1
-            joined_set_size = self.set_counts[dst_parent]
-        else:
-            self.set_counts[src_parent] += self.set_counts[dst_parent]
-            self.set_counts[dst_parent] = 0
-            self.parents[dst_parent] = src_parent
-            joined_set_size = self.set_counts[src_parent]
-
+        self.set_counts[dst_parent] += self.set_counts[src_parent]
+        self.set_counts[src_parent] = 0
+        self.parents[src_parent] = dst_parent
+        joined_set_size = self.set_counts[dst_parent]
+        
         self.max_set = max(self.max_set, joined_set_size)
         return True
 
     def get_parent(self, disj_set: int) -> int:
         """
-        Find the Parent of a given set
+        Find the Parent of a given set and perform
+        path compression
         >>> A = DisjointSet([1, 1, 1])
         >>> A.merge(1, 2)
         True
