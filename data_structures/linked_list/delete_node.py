c4f01e1d4a42c2d85e315b85cadd53ba8dd04b2c
makrandbhonde <60532803+makrandbhonde@users.noreply.github.com>
2021-10-30 12:07:31 +0530

    suggested doc test included

--- data_structures/linked_list/delete_node.py
+++ data_structures/linked_list/delete_node.py
@@ -1,25 +1,39 @@
 from typing import Any
+import doctest
 
 class Node:
-    def __init__(self, data: Any)-> None:
+    def __init__(self, data: Any) -> None:
         self.data = data
         self.next = None
 class Linkedlist:
     def __init__(self)-> None:
         self.head = None
-    def push_at_head(self, data: Any)-> None:
+    def push_at_head(self, data: Any) -> None:
         new_node = Node(data)
         new_node.next = self.head
         self.head = new_node
     # **deletion code starts**
-    def delete_node(self, key)-> None:
+    def delete_node(self, key) -> None:
         temp = self.head
         while temp.next:
             if temp.next.data == key:       #if next node is the node what we want to delete
                 temp.next = temp.next.next  #breaking the link of node which has to be deleted
             temp = temp.next                #if condition not satisy then go to next node
     # **deletion code ends**
-    def print_list(self)-> None:
+    def print_list(self) -> None:
+        """
+        >>> linked_list = Linkedlist()
+
+        >>> linked_list.push_at_head(2)
+
+        >>> linked_list.push_at_head(1)
+
+        >>> linked_list.push_at_head(0)
+
+        >>> linked_list.print_list()
+        0->1->2->NULL
+        
+        """
         temp = self.head
         while temp:
             print(temp.data, end="->")
@@ -27,15 +41,5 @@ class Linkedlist:
         print("NULL")
 
 if __name__=='__main__':
-    myobj = Linkedlist()
-    myobj.push_at_head(4)
-    myobj.push_at_head(3)       
-    myobj.push_at_head(2)       
-    myobj.push_at_head(1)
-    myobj.push_at_head(0)
-    print("List before deleting the node: ")
-    myobj.print_list()
-    myobj.delete_node(3)
-    print("List after deleting the node: ")
-    myobj.print_list()
-
+   
+    doctest.testmod()
