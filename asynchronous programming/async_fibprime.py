8ad981371cad06e21a47d6f265af3987df29671e
souvikghosh05 <souvikghosh872@gmail.com>
2021-10-30 15:03:45 +0530

    async doctest added

--- asynchronous programming/async_fibprime.py
+++ asynchronous programming/async_fibprime.py
@@ -2,33 +2,56 @@ from typing import Union
 from async_lru import alru_cache
 from asyncio import gather, run
 from math import sqrt
+import doctest
 
 @alru_cache(maxsize=256)  # using memoization to speed up the process
-async def fib(n: int) -> Union[int, str]:
-    if n < 0:
+async def fib(digit: int) -> Union[int, str]:
+    
+    """
+    >>> run(fib(10))
+    55
+    >>> run(fib(20))
+    6765
+    >>> run(fib(-7))
+    'Number must be a positive integer!'
+    >>> run(fib(1))
+    1
+    
+    """
+    
+    if digit < 0:
         return "Number must be a positive integer!"
-    elif n==0:
+    elif digit==0:
         return 0
-    elif n==1:
+    elif digit==1:
         return 1
     else:
-        return (await fib(n-1) + await fib(n-2))
+        return (await fib(digit-1) + await fib(digit-2))
     
-async def is_prime(n: int) -> bool:
+async def is_prime(digit: int) -> bool:
+    
+    """
+    >>> run(is_prime(70))
+    False
+    >>> run(is_prime(2))
+    True
+    
+    """
+    
     prime_flag = 0
 
-    if n <= 1:
+    if digit <= 1:
         return False
 
-    for i in range(2, int(sqrt(n)) + 1):
-        if (n % i == 0):
+    for i in range(2, int(sqrt(digit)) + 1):
+        if (digit % i == 0):
             prime_flag = 1
             break
     return (prime_flag == 0)
         
 
 
-async def main():
+async def main() -> None:
     try:
         put_num=int(input("Enter a number: "))
         ret_fib, ret_prime = await gather(fib(put_num), is_prime(put_num))
@@ -37,5 +60,7 @@ async def main():
         print(f"{put_num} is a prime number") if ret_prime else print(f"{put_num} is not a prime number") 
     except ValueError:
         print("Invalid input!")
-        
-run(main())
+
+if __name__ == "__main__":
+    doctest.testmod(verbose=True)
+    run(main())       
