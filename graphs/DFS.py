3f4f2ca25f8c9b14d9ce1ef4f55c6a36d871278e
Kanika-Tyagi <70576709+Kanika-Tyagi@users.noreply.github.com>
2021-10-30 12:45:05 +0530

    Create DFS.py

---
+++ graphs/DFS.py
@@ -0,0 +1,22 @@
+# Using a Python dictionary to act as an adjacency list
+graph = {
+  '5' : ['3','7'],
+  '3' : ['2', '4'],
+  '7' : ['8'],
+  '2' : [],
+  '4' : ['8'],
+  '8' : []
+}
+
+visited = set() # Set to keep track of visited nodes of graph.
+
+def dfs(visited, graph, node):  #function for dfs 
+    if node not in visited:
+        print (node)
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+
+# Driver Code
+print("Following is the Depth-First Search")
+dfs(visited, graph, '5')
