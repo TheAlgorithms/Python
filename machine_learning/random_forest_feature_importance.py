e82c3ab6bbc9b4fffa5e21774a89f1df5a9f5e33
Hugo Abonizio <hugo_abonizio@hotmail.com>
2021-10-31 10:38:32 -0300

    Skip main script verification since it does not make sence to import it from other files

--- machine_learning/random_forest_feature_importance.py
+++ machine_learning/random_forest_feature_importance.py
@@ -6,44 +6,33 @@ from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
+"""
+Random Forest Feature Importance Example using sklearn function.
+Iris type dataset is used to demonstrate algorithm.
+https://scikit-learn.org/stable/auto_examples/ensemble/plot_forest_importances.html
+"""
 
-def main() -> None:
+# Load Iris dataset
+iris = load_iris()
 
-    """
-    Random Forest Feature Importance Example using sklearn function.
-    Iris type dataset is used to demonstrate algorithm.
-    https://scikit-learn.org/stable/auto_examples/ensemble/plot_forest_importances.html
-    """
+# Split dataset into train and test data
+X = iris["data"]
+Y = iris["target"]
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=1)
 
-    # Load Iris dataset
-    iris = load_iris()
+# Random Forest Classifier
+rand_for = RandomForestClassifier(random_state=42, n_estimators=100)
+rand_for.fit(x_train, y_train)
 
-    # Split dataset into train and test data
-    X = iris["data"]
-    Y = iris["target"]
-    x_train, x_test, y_train, y_test = train_test_split(
-        X, Y, test_size=0.3, random_state=1
-    )
+# Average importances and standard deviations
+importances = rand_for.feature_importances_
+std = np.std([tree.feature_importances_ for tree in rand_for.estimators_], axis=0)
 
-    # Random Forest Classifier
-    rand_for = RandomForestClassifier(random_state=42, n_estimators=100)
-    rand_for.fit(x_train, y_train)
-
-    # Average importances and standard deviations
-    importances = rand_for.feature_importances_
-    std = np.std([tree.feature_importances_ for tree in rand_for.estimators_], axis=0)
-
-    # Plot
-    forest_importances = pd.Series(importances, index=iris["feature_names"])
-    fig, ax = plt.subplots()
-    forest_importances.plot.bar(yerr=std, ax=ax)
-    ax.set_title("Feature importances using MDI")
-    ax.set_ylabel("Mean decrease in impurity")
-    fig.tight_layout()
-    plt.show()
-
-
-if __name__ == "__main__":
-    import doctest
-    main()
-    doctest.testmod()
+# Plot
+forest_importances = pd.Series(importances, index=iris["feature_names"])
+fig, ax = plt.subplots()
+forest_importances.plot.bar(yerr=std, ax=ax)
+ax.set_title("Feature importances using MDI")
+ax.set_ylabel("Mean decrease in impurity")
+fig.tight_layout()
+plt.show()
