cd01fb93324dd7a535ae32c7556f31ad4efe931b
Jahnavi Aaki <51069244+Jahnavi1201@users.noreply.github.com>
2021-10-31 12:34:08 +0530

    Updated zig_zag_pattern.py
    Used chain.from_iterable to flatten resultant 2D list as specified. And added a test for the case when the input matrix doesn't have rows or columns of the same length.

--- matrix/zig_zag_pattern.py
+++ matrix/zig_zag_pattern.py
@@ -4,19 +4,25 @@ in zig-zag fashion.
 '''
 
 from __future__ import annotations
+from itertools import chain # To flatten a 2D list
 
-def get_zig_zag_pattern(matrix: list[list[float]]) -> list[list[float]]:
+def get_zig_zag_pattern(matrix: list[list[int]]) -> list[int]:
     
     """
     >>> get_zig_zag_pattern([[12,22],[31,42]])
-    [[12], [22, 31], [42]]
+    [12, 22, 31, 42]
     >>> get_zig_zag_pattern([[1]])
-    [[1]]
+    [1]
     >>> get_zig_zag_pattern([0])
     Traceback (most recent call last):
         ...
     TypeError: object of type 'int' has no len()
-    
+    >>> get_zig_zag_pattern([[0, 1, 0],[1, 1, 1],[0, 1],[1, 1, 1]])
+    Traceback (most recent call last):
+        ...
+    IndexError: list index out of range
+    >>> get_zig_zag_pattern([[1, 0, 1],[0, 1 ,1]])
+    [1, 0, 0, 1, 1, 1]
     """
     #Storing dimensions of the input matrix
     rows=len(matrix)
@@ -36,20 +42,15 @@ def get_zig_zag_pattern(matrix: list[list[float]]) -> list[list[float]]:
                 result[ind].append(matrix[i][j])
                 
     #returns resultant matrix(in zig-zag fashion)
-    return result
+    return list(chain.from_iterable(result))
             
 if __name__ == "__main__":
     
     import doctest
     doctest.testmod()
     
-    result=get_zig_zag_pattern([
+    print(get_zig_zag_pattern([
             [ 1, 2, 3,],
             [ 4, 5, 6 ],
             [ 7, 8, 9 ],
-        ])
-    for i in result:
-        for j in i:
-            print(j,end=" ")
-    
-    
+        ]))
