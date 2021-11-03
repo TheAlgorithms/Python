b2251b1b17f9ccd3be5939249a02e0be51c7322a
Christian Clauss <cclauss@me.com>
2021-11-03 21:21:42 +0100

    Update sock_merchant.py

--- maths/sock_merchant.py
+++ maths/sock_merchant.py
@@ -1,27 +1,20 @@
-import doctest
 from collections import Counter
 
 
-def sock_merchant(ar: list[int]) -> int:
+def sock_merchant(colors: list[int]) -> int:
     """
     >>> sock_merchant([10, 20, 20, 10, 10, 30, 50, 10, 20])
     3
     >>> sock_merchant([1, 1, 3, 3])
     2
     """
-
-    i = 0
-    occur = Counter(ar)
-
-    for x in occur.values():
-        i += x // 2
-    return i
+    return sum(socks_by_color // 2 for socks_by_color in Counter(colors).values())
 
 
 if __name__ == "__main__":
+    import doctest
 
-    array = list(map(int, input().rstrip().split()))
-
-    result = sock_merchant(array)
-    print(result)
     doctest.testmod()
+
+    colors = [int(x) for x in input("Enter socks by color :").rstrip().split()]
+    print(f"sock_merchant({colors}) = {sock_merchant(colors)}")
