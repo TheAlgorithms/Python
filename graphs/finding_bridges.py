9723bad855c44a0c6f8eaf2819e8b0eedbc071e3
Boris Galochkin <spore17@mail.ru>
2021-11-04 17:13:58 +0300

    Fix finding_bridges algorithms + tests

--- graphs/finding_bridges.py
+++ graphs/finding_bridges.py
@@ -1,5 +1,42 @@
-# Finding Bridges in Undirected Graph
-def computeBridges(graph):
+def __get_demo_graph(index):
+    return [
+        {
+            0: [1, 2],
+            1: [0, 2],
+            2: [0, 1, 3, 5],
+            3: [2, 4],
+            4: [3],
+            5: [2, 6, 8],
+            6: [5, 7],
+            7: [6, 8],
+            8: [5, 7],
+        },
+        {
+            0: [6],
+            1: [9],
+            2: [4, 5],
+            3: [4],
+            4: [2, 3],
+            5: [2],
+            6: [0, 7],
+            7: [6],
+            8: [],
+            9: [1],
+        },
+    ][index]
+
+
+def compute_bridges(graph: dict) -> list:
+    """
+    Return the list of undirected graph bridges [(a1, b1), ..., (ak, bk)]; ai <= bi
+    >>> compute_bridges(__get_demo_graph(0))
+    [(3, 4), (2, 3), (2, 5)]
+    >>> compute_bridges(__get_demo_graph(1))
+    [(6, 7), (0, 6), (1, 9), (3, 4), (2, 4), (2, 5)]
+    >>> compute_bridges({})
+    []
+    """
+
     id = 0
     n = len(graph)  # No of vertices in graph
     low = [0] * n
@@ -15,28 +52,14 @@ def computeBridges(graph):
             elif not visited[to]:
                 dfs(to, at, bridges, id)
                 low[at] = min(low[at], low[to])
-                if at < low[to]:
-                    bridges.append([at, to])
+                if id <= low[to]:
+                    bridges.append((at, to) if at < to else (to, at))
             else:
                 # This edge is a back edge and cannot be a bridge
-                low[at] = min(low[at], to)
+                low[at] = min(low[at], low[to])
 
     bridges = []
     for i in range(n):
         if not visited[i]:
             dfs(i, -1, bridges, id)
-    print(bridges)
-
-
-graph = {
-    0: [1, 2],
-    1: [0, 2],
-    2: [0, 1, 3, 5],
-    3: [2, 4],
-    4: [3],
-    5: [2, 6, 8],
-    6: [5, 7],
-    7: [6, 8],
-    8: [5, 7],
-}
-computeBridges(graph)
+    return bridges
