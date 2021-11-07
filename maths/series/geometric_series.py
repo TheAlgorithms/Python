85b0ba87b73cca65bb621a0a1051ace6f7f20202
Rohan R Bharadwaj <rohanrbharadwaj@gmail.com>
2021-11-07 19:45:23 +0530

    Update geometric_series.py

--- maths/series/geometric_series.py
+++ maths/series/geometric_series.py
@@ -45,8 +45,8 @@ def geometric_series(
     """
     if not all((nth_term, start_term_a, common_ratio_r)):
         return []
-    series: list[float] = []
-    power: int = 1
+    series = []
+    power = 1
     multiple = common_ratio_r
     for _ in range(int(nth_term)):
         if series == []:
@@ -60,14 +60,13 @@ def geometric_series(
 
 if __name__ == "__main__":
     import doctest
+
     doctest.testmod()
 
     nth_term = float(input("Enter the last number (n term) of the Geometric Series"))
     start_term_a = float(input("Enter the starting term (a) of the Geometric Series"))
     common_ratio_r = float(
-        input(
-        "Enter the common ratio between two terms (r) of the Geometric Series"
-        )
+        input("Enter the common ratio between two terms (r) of the Geometric Series")
     )
     print("Formula of Geometric Series => a + ar + ar^2 ... +ar^n")
     print(geometric_series(nth_term, start_term_a, common_ratio_r))
