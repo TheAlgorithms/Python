effeab809d0e8f128fa6fd3294e726a61330494d
Bartechnika <steamp4nk@gmail.com>
2021-11-08 17:05:32 +0000

    Added another persistence function

--- maths/persistence.py
+++ maths/persistence.py
@@ -1,25 +1,25 @@
-def persistence(num: int) -> int:
+def multiplicative_persistence(num: int) -> int:
     """
     Return the persistence of a given number.
 
     https://en.wikipedia.org/wiki/Persistence_of_a_number
 
-    >>> persistence(217)
+    >>> multiplicative_persistence(217)
     2
-    >>> persistence(-1)
+    >>> multiplicative_persistence(-1)
     Traceback (most recent call last):
         ...
-    ValueError: persistence() does not accept negative values
-    >>> persistence("long number")
+    ValueError: multiplicative_persistence() does not accept negative values
+    >>> multiplicative_persistence("long number")
     Traceback (most recent call last):
         ...
-    ValueError: persistence() only accepts integral values
+    ValueError: multiplicative_persistence() only accepts integral values
     """
 
     if not isinstance(num, int):
-        raise ValueError("persistence() only accepts integral values")
+        raise ValueError("multiplicative_persistence() only accepts integral values")
     if num < 0:
-        raise ValueError("persistence() does not accept negative values")
+        raise ValueError("multiplicative_persistence() does not accept negative values")
 
     steps = 0
     num_string = str(num)
@@ -30,6 +30,45 @@ def persistence(num: int) -> int:
         total = 1
         for i in range(0, len(numbers)):
             total *= numbers[i]
+
+        num_string = str(total)
+
+        steps += 1
+    return steps
+
+
+def additive_persistence(num: int) -> int:
+    """
+    Return the persistence of a given number.
+
+    https://en.wikipedia.org/wiki/Persistence_of_a_number
+
+    >>> additive_persistence(199)
+    3
+    >>> additive_persistence(-1)
+    Traceback (most recent call last):
+        ...
+    ValueError: additive_persistence() does not accept negative values
+    >>> additive_persistence("long number")
+    Traceback (most recent call last):
+        ...
+    ValueError: additive_persistence() only accepts integral values
+    """
+
+    if not isinstance(num, int):
+        raise ValueError("additive_persistence() only accepts integral values")
+    if num < 0:
+        raise ValueError("additive_persistence() does not accept negative values")
+
+    steps = 0
+    num_string = str(num)
+
+    while len(num_string) != 1:
+        numbers = [int(i) for i in num_string]
+
+        total = 0
+        for i in range(0, len(numbers)):
+            total += numbers[i]
 
         num_string = str(total)
 
