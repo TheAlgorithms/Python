ffdd52944256bdda7112584e6d05fc74feb99096
ReisgoldmanX <46068107+reisgoldmanX@users.noreply.github.com>
2021-11-09 22:55:05 +0300

    Added liquid_pressure
    Added liquid_pressure algo to the pressure.py

--- physics/pressure.py
+++ physics/pressure.py
@@ -2,12 +2,19 @@
 Description :
 https://en.wikipedia.org/wiki/Pressure
 
-Pressure is the amount of force applied at right angles to the surface of an object per unit area.
+- Solid Pressure
 P = F/A
-
 P is the pressure,
 F is the magnitude of the normal force,
 A is the area of the surface on contact.
+
+- Liquid Pressure
+https://en.wikipedia.org/wiki/Pressure#Liquid_pressure
+P = p*g*h
+P is liquid pressure,
+g is gravity at the surface of overlaying material,
+ρ is density of liquid,
+h is height of liquid column or depth within a substance.
 """
 
 
@@ -45,8 +52,42 @@ def solid_pressure(force: float, area: float) -> float:
         return force / area
 
 
-if __name__ == "__main__":
+def liquid_pressure(density: float, gravity: float, height: float) -> float:
+    """
+    >>> liquid_pressure(1, 10, 2)
+    20
 
+    >>> liquid_pressure(3.7, 6.2, 9.1)
+    208.754
+
+    >>> liquid_pressure(-1, 10, 2)
+    Traceback (most recent call last):
+    ...
+    ValueError: Density must be > 0
+
+    >>> liquid_pressure(2, -5, 7)
+    Traceback (most recent call last):
+    ...
+    ValueError: Gravity must be > 0
+
+    >>> liquid_pressure(5, 23, -5)
+    Traceback (most recent call last):
+    ...
+    ValueError: Height must be > 0
+    """
+    if density <= 0 and gravity <= 0 and height <= 0:
+        raise ValueError("Density, gravity and height must be > 0")
+    elif density <= 0:
+        raise ValueError("Density must be > 0")
+    elif gravity <= 0:
+        raise ValueError("Gravity must be > 0")
+    elif height <= 0:
+        raise ValueError("Height must be > 0")
+    else:
+        return density * gravity * height
+
+
+if __name__ == "__main__":
     import doctest
 
     # run doctest
