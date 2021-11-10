ccb0928cc005430cbf23f74a2b7990cc45998b29
MaximSmolskiy <mithridatus@mail.ru>
2021-11-10 09:30:06 +0300

    Fix statement

--- project_euler/problem_074/sol2.py
+++ project_euler/problem_074/sol2.py
@@ -1,23 +1,38 @@
 """
-    Project Euler Problem 074: https://projecteuler.net/problem=74
+Project Euler Problem 074: https://projecteuler.net/problem=74
 
-    Starting from any positive integer number
-    it is possible to attain another one summing the factorial of its digits.
+The number 145 is well known for the property that the sum of the factorial of its
+digits is equal to 145:
 
-    Repeating this step, we can build chains of numbers.
-    It is not difficult to prove that EVERY starting number
-    will eventually get stuck in a loop.
+1! + 4! + 5! = 1 + 24 + 120 = 145
 
-    The request is to find how many numbers less than one million
-    produce a chain with exactly 60 non repeating items.
+Perhaps less well known is 169, in that it produces the longest chain of numbers that
+link back to 169; it turns out that there are only three such loops that exist:
 
-    Solution approach:
-    This solution simply consists in a loop that generates
-    the chains of non repeating items.
-    The generation of the chain stops before a repeating item
-    or if the size of the chain is greater then the desired one.
-    After generating each chain, the length is checked and the
-    counter increases.
+169 → 363601 → 1454 → 169
+871 → 45361 → 871
+872 → 45362 → 872
+
+It is not difficult to prove that EVERY starting number will eventually get stuck in a
+loop. For example,
+
+69 → 363600 → 1454 → 169 → 363601 (→ 1454)
+78 → 45360 → 871 → 45361 (→ 871)
+540 → 145 (→ 145)
+
+Starting with 69 produces a chain of five non-repeating terms, but the longest
+non-repeating chain with a starting number below one million is sixty terms.
+
+How many chains, with a starting number below one million, contain exactly sixty
+non-repeating terms?
+
+Solution approach:
+This solution simply consists in a loop that generates
+the chains of non repeating items.
+The generation of the chain stops before a repeating item
+or if the size of the chain is greater then the desired one.
+After generating each chain, the length is checked and the
+counter increases.
 """
 
 factorial_cache: dict[int, int] = {}
