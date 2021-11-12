4b2bf1702564a072fa994bc85b1eb9e07ac39095
Rohan R Bharadwaj <rohanrbharadwaj@gmail.com>
2021-11-13 00:05:00 +0530

    Improve Project Euler Problem 145 Solution 1

--- project_euler/problem_145/sol1.py
+++ project_euler/problem_145/sol1.py
@@ -19,42 +19,6 @@ Solution:
 Here a brute force solution is used to find and count the reversible numbers.
 
 """
-from __future__ import annotations
-
-
-def check_if_odd(sum: int = 36) -> int:
-    """
-    Check if the last digit in the sum is even or odd. If even return 0.
-    If odd then floor division by 10 is used to remove the last number.
-    Process continues until sum becomes 0 because no more numbers.
-    >>> check_if_odd(36)
-    0
-    >>> check_if_odd(33)
-    1
-    """
-    while sum > 0:
-        if (sum % 10) % 2 == 0:
-            return 0
-        sum = sum // 10
-    return 1
-
-
-def find_reverse_number(number: int = 36) -> int:
-    """
-    Reverses the given number. Does not work with number that end in zero.
-    >>> find_reverse_number(36)
-    63
-    >>> find_reverse_number(409)
-    904
-    """
-    reverse = 0
-
-    while number > 0:
-        temp = number % 10
-        reverse = reverse * 10 + temp
-        number = number // 10
-
-    return reverse
 
 
 def solution(number: int = 1000000000) -> int:
@@ -63,11 +27,9 @@ def solution(number: int = 1000000000) -> int:
     Checks if they have ending zeros which disqualifies them from being reversible.
     If that condition is passed it generates the reversed number.
     Then sum up n and reverse(n).
-    Then check if all the numbers in the sum are odd. If true add to the answer.
+    Then check if no number in the sum is even. If true add 1 to the answer.
     >>> solution(1000000000)
     608720
-    >>> solution(1000000)
-    18720
     >>> solution(1000000)
     18720
     >>> solution(1000)
@@ -75,11 +37,12 @@ def solution(number: int = 1000000000) -> int:
     """
     answer = 0
     for x in range(1, number):
-        if x % 10 != 0:
-            reversed_number = find_reverse_number(x)
-            sum = x + reversed_number
-            answer += check_if_odd(sum)
-
+        if x % 10 == 0:
+            continue
+        x_reverse = int(str(x)[::-1])
+        summ = x + x_reverse
+        if all([i not in str(summ) for i in ['2', '4', '6', '8', '0']]):
+            answer += 1        
     return answer
 
 
