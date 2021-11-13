3f8859898bc5a239b1280afd88f1354f4c983b50
MaximSmolskiy <mithridatus@mail.ru>
2021-11-13 18:35:59 +0300

    Add tests

--- project_euler/problem_074/sol2.py
+++ project_euler/problem_074/sol2.py
@@ -42,9 +42,28 @@ def digit_factorial_sum(number: int) -> int:
     """
     Function to perform the sum of the factorial of all the digits in number
 
+    >>> digit_factorial_sum(69.0)
+    Traceback (most recent call last):
+        ...
+    TypeError: Parameter number must be int
+
+    >>> digit_factorial_sum(-1)
+    Traceback (most recent call last):
+        ...
+    ValueError: Parameter number must be greater than or equal to 0
+
+    >>> digit_factorial_sum(0)
+    1
+
     >>> digit_factorial_sum(69)
     363600
     """
+    if not isinstance(number, int):
+        raise TypeError("Parameter number must be int")
+
+    if number < 0:
+        raise ValueError("Parameter number must be greater than or equal to 0")
+
     # Converts number in string to iterate on its digits and adds its factorial.
     return sum(DIGIT_FACTORIAL[digit] for digit in str(number))
 
@@ -54,9 +73,37 @@ def solution(chain_length: int = 60, number_limit: int = 1000000) -> int:
     Returns the number of numbers below number_limit that produce chains with exactly
     chain_length non repeating elements.
 
+    >>> solution(10.0, 1000)
+    Traceback (most recent call last):
+        ...
+    TypeError: Parameters chain_length and number_limit must be int
+
+    >>> solution(10, 1000.0)
+    Traceback (most recent call last):
+        ...
+    TypeError: Parameters chain_length and number_limit must be int
+
+    >>> solution(0, 1000)
+    Traceback (most recent call last):
+        ...
+    ValueError: Parameters chain_length and number_limit must be greater than 0
+
+    >>> solution(10, 0)
+    Traceback (most recent call last):
+        ...
+    ValueError: Parameters chain_length and number_limit must be greater than 0
+
     >>> solution(10, 1000)
     26
     """
+
+    if not isinstance(chain_length, int) or not isinstance(number_limit, int):
+        raise TypeError("Parameters chain_length and number_limit must be int")
+
+    if chain_length <= 0 or number_limit <= 0:
+        raise ValueError(
+            "Parameters chain_length and number_limit must be greater than 0"
+        )
 
     # the counter for the chains with the exact desired length
     chains_counter = 0
