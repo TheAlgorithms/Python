71e424b33d519b96cab083bf39e868adbe51307f
Rohan R Bharadwaj <rohanrbharadwaj@gmail.com>
2021-11-13 23:56:13 +0530

    Update count_islands_in_matrix.py

--- matrix/count_islands_in_matrix.py
+++ matrix/count_islands_in_matrix.py
@@ -9,7 +9,7 @@ class matrix:  # Public class to implement a graph
         self.COL = col
         self.graph = graph
 
-    def is_safe(self, i, j, visited) -> bool:
+    def is_safe(self, i: int, j: int, visited: list[list]) -> bool:
         return (
             0 <= i < self.ROW
             and 0 <= j < self.COL
@@ -17,7 +17,8 @@ class matrix:  # Public class to implement a graph
             and self.graph[i][j]
         )
 
-    def diffs(self, i, j, visited):  # Checking all 8 elements surrounding nth element
+    def diffs(self, i: int, j: int, visited: list[list]) -> None:
+        # Checking all 8 elements surrounding nth element
         rowNbr = [-1, -1, -1, 0, 0, 1, 1, 1]  # Coordinate order
         colNbr = [-1, 0, 1, -1, 1, -1, 0, 1]
         visited[i][j] = True  # Make those cells visited
