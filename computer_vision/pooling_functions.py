868e42abde0e8995cbc322020d4ebb7cde08dc02
Navaneeth Sharma <63489382+Navaneeth-Sharma@users.noreply.github.com>
2021-11-15 20:00:21 +0530

    pooling.py: Adding Pooling Algorithms

--- computer_vision/pooling_functions.py
+++ computer_vision/pooling_functions.py
@@ -2,24 +2,37 @@
 # Importing the libraries
 import numpy as np
 from PIL import Image
-
+from doctest import testmod
 
 # Maxpooling Function
 def maxpooling(arr:list, size:int, stride:int) -> list:
     """
     This function is used to perform maxpooling on the input array of 2D matrix(image)
-
     Args:
         arr: numpy array
         size: size of pooling matrix
         stride: the number of pixels shifts over the input matrix
     Returns:
         numpy array of maxpooled matrix
+    
+    Sample Input Output:
+    >>> maxpooling([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]], 2, 2)
+    array([[ 6.,  8.],
+           [14., 16.]])
+    
+    >>> maxpooling([[136,228,0,166,51],[148,42,89,146,230],[3,37,215,145,69],[83,43,20,22,80],[172,171,233,165,48]], 3, 1)
+    array([[228., 228., 230.],
+           [215., 215., 230.],
+           [233., 233., 233.]])
     """
+    arr = np.array(arr)
     if arr.shape[0] != arr.shape[1]:
         raise ValueError("The input array is not a square matrix")
     i = 0
     j = 0
+    max_mat_i = 0
+    max_mat_j = 0
+
     # compute the shape of the output matrix
     maxpool_shape = (arr.shape[0] - size) // stride + 1
     # initialize the output matrix with zeros of shape maxpool_shape
@@ -34,14 +47,18 @@ def maxpooling(arr:list, size:int, stride:int) -> list:
             if j + size > arr.shape[1]:
                 break
             # compute the maximum of the pooling matrix
-            updated_arr[i][j] = np.max(arr[i:i + size, j:j + size])
+            updated_arr[max_mat_i][max_mat_j] = np.max(arr[i:i + size, j:j + size])
             # shift the pooling matrix by stride of column pixels
             j += stride
+            max_mat_j += 1
 
         # shift the pooling matrix by stride of row pixels
         i += stride
+        max_mat_i += 1
+
         # reset the column index to 0
         j = 0
+        max_mat_j = 0
 
     return updated_arr
 
@@ -50,6 +67,15 @@ def maxpooling(arr:list, size:int, stride:int) -> list:
 def avgpooling(arr:list, size:int, stride:int) -> list:
     """
     This function is used to perform avgpooling on the input array of 2D matrix(image)
+    >>> avgpooling([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]], 2, 2)
+    array([[ 3.,  5.],
+           [11., 13.]])
+
+    >>> avgpooling([[136,228,0,166,51],[148,42,89,146,230],[3,37,215,145,69],[83,43,20,22,80],[172,171,233,165,48]], 3, 1)
+    array([[ 99., 118., 123.],
+           [ 75.,  84., 112.],
+           [108., 116., 110.]])
+
     Args:
         arr: numpy array
         size: size of pooling matrix
@@ -57,10 +83,14 @@ def avgpooling(arr:list, size:int, stride:int) -> list:
     Returns:
         numpy array of avgpooled matrix
     """
+    arr = np.array(arr)
     if arr.shape[0] != arr.shape[1]:
         raise ValueError("The input array is not a square matrix")
     i = 0
     j = 0
+    avg_mat_i = 0
+    avg_mat_j = 0
+
     # compute the shape of the output matrix
     avgpool_shape = (arr.shape[0] - size) // stride + 1
     # initialize the output matrix with zeros of shape avgpool_shape
@@ -75,20 +105,26 @@ def avgpooling(arr:list, size:int, stride:int) -> list:
             if j + size > arr.shape[1]:
                 break
             # compute the average of the pooling matrix
-            updated_arr[i][j] = int(np.average(arr[i:i + size, j:j + size]))
+            updated_arr[avg_mat_i][avg_mat_j] = int(np.average(arr[i:i + size, j:j + size]))
             # shift the pooling matrix by stride of column pixels
             j += stride
+            avg_mat_j += 1
 
         # shift the pooling matrix by stride of row pixels
         i += stride
+        avg_mat_i += 1
         # reset the column index to 0
         j = 0
+        avg_mat_j = 0
 
     return updated_arr
 
 
 # Main Function
 if __name__ == "__main__":
+    # test the functions
+    testmod(name ='avgpooling', verbose = True)
+
     # Loading the image
     image = Image.open("path_to_image")
 
