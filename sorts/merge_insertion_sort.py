0e08304cbdb6f2d906b1b273fdae9d2b90337a63
yellowsto <79023119+yellowsto@users.noreply.github.com>
2021-11-24 15:34:06 +0100

    Update merge_insertion_sort.py
    Fixes #5774
    
    added permutation range from 0 to 4
    
    Co-Authored-By: AilisOsswald <44617437+AilisOsswald@users.noreply.github.com>

--- sorts/merge_insertion_sort.py
+++ sorts/merge_insertion_sort.py
@@ -30,6 +30,12 @@ def merge_insertion_sort(collection: list[int]) -> list[int]:
 
     >>> merge_insertion_sort([-2, -5, -45])
     [-45, -5, -2]
+
+    Testing with all permutations on range(0,5):
+    >>> import itertools
+    >>> permutations = list(itertools.permutations([0, 1, 2, 3, 4]))
+    >>> [x for x in permutations if merge_insertion_sort(x) != [0, 1, 2, 3, 4]]
+    []
     """
 
     def binary_search_insertion(sorted_list, item):
