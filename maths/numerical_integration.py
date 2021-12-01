78ab0af9524c7ba3aae15b13144d8a8ae030c61b
Noah Schiro <noahschiro@rocketmail.com>
2021-12-01 17:18:33 -0500

    Simpson's rule, testing, more comments

--- maths/numerical_integration.py
+++ maths/numerical_integration.py
@@ -5,14 +5,19 @@ numerical integration see here:
 
 https://en.wikipedia.org/wiki/Numerical_integration
 
-The methods used are described in order from most 
-accurate to least accurate:
+The methods used are described in order from least 
+accurate to most accurate:
 1. Right endpoint rule
 2. Left endpoint rule
 3. Midpoint rule
-4. Trapezoidal rule
-5. Simpson's rule
+4. Simpson's rule
+5. Trapezoidal rule
 
+For those looking to implement an algorithm for 
+integration, the trapezoidal rule is the best fit. 
+Generally, if your steps are equal to the size of 
+your interval * 10,000, it will yield a fairly accurate
+algorithm for a definite integral
 """
 from __future__ import annotations
 
@@ -130,6 +135,59 @@ def midpoint_rule(
 
     return area
 
+def simpsons_rule(
+    fnc: Callable[[int | float], int | float],
+    x_start: int | float,
+    x_end: int | float,
+    steps: int = 100,
+) -> float:
+
+    """
+    For simpson's rule, there must be an even number of subintervals.
+    The exact derivation for this rule is complicated; the motivated 
+    can find an explanation here:
+    https://en.wikipedia.org/wiki/Simpson%27s_rule
+
+    Note that this is NOT the Simpson's 1/3 rule or the simpson's 3/8th
+    rule. The algrorithm employed is the general form which allows for an
+    arbitrary amount of steps
+    """
+
+    # Steps must be even
+    if steps % 2 != 0:
+        steps += 1
+
+    dx = (x_end - x_start) / steps
+    sum = 0.0
+
+    # Loop from begining of interval to end of interval 
+    # and sum up according to simpsons rule
+    i = x_start
+    counter = 1
+    while i <= x_end:
+        
+        # If interval is at beginning or end, 
+        # it doesn't have a multiplier
+        if i == x_start or i == x_end:
+            sum += fnc(i)
+
+        # If interval is an odd number (the first, third,
+        # fifth, ...), then the multiplier is 4
+        elif counter % 2 == 0:
+            sum += (4*fnc(i))
+
+        # If interval is an even number (the second, 
+        # fourth, ...), then the multiplier is 2
+        else:
+            sum += (2*fnc(i))
+        
+        # Increment our interval and counter
+        i += dx
+        counter += 1
+    
+    # Simpson's rule also says that the area is dx/3 * sum
+    return (dx/3) * sum
+
 def trapezoidal_area(
     fnc: Callable[[int | float], int | float],
     x_start: int | float,
@@ -174,32 +232,29 @@ def trapezoidal_area(
         # Increment step
         x1 = x2
         fx1 = fx2
-    return area
-
-def simpsons_rule(
-    fnc: Callable[[int | float], int | float],
-    x_start: int | float,
-    x_end: int | float,
-    steps: int = 100,
-) -> float:
-
-    pass
+    return area     
 
 if __name__ == "__main__":
 
+    # Define a function for testing
     def f(x):
         return x ** 3
 
     print("f(x) = x^3")
     print("The area between the curve, x = 0, x = 5 and the x axis is:")
+
+    # Test each approximation with a variety of intervals
     i = 10
     while i <= 100000:
         r_area = right_endpt_rule(f, 0, 5, i)
         l_area = left_endpt_rule (f, 0, 5, i)
         m_area = midpoint_rule   (f, 0, 5, i)
         t_area = trapezoidal_area(f, 0, 5, i)
+        s_area = simpsons_rule   (f, 0, 5, i)
         print(f"Right endpoint rule ({i} steps): \t {r_area}")
         print(f"Left endpoint rule ({i} steps):  \t {l_area}")
         print(f"Midpoint rule ({i} steps):       \t {m_area}")
+        print(f"Simpson's rule ({i} steps):      \t {s_area}")
         print(f"Trapezoidal rule ({i} steps):    \t {t_area}\n")
+
         i *= 10
