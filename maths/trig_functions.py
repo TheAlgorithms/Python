6e9eb3ce0534cc7a6e3fb480a57f30264817be13
Noah Schiro <noahschiro@rocketmail.com>
2021-12-01 20:41:41 -0500

    Init

---
+++ maths/trig_functions.py
@@ -0,0 +1,159 @@
+"""
+The 6 normal trig functions and their inverses. In theory, I could 
+have used a CORDIC algorithm https://en.wikipedia.org/wiki/CORDIC
+but this requires a load of pre-calculated values stored in some
+list. In keeping with the spirit of what this repository aims to
+achieve, I decided to use a taylor series expansion so that everything
+is clear
+"""
+
+def sine(
+    x: float,
+    accuracy: int = 100
+) -> float:
+
+    """
+    Algorithm used: taylor series
+    https://en.wikipedia.org/wiki/Taylor_series
+
+    Doctests:
+    """
+    pass
+
+def cosine(
+    x: float,
+    accuracy: int = 100
+) -> float:
+    """
+    Algorithm used: taylor series
+    https://en.wikipedia.org/wiki/Taylor_series
+
+    Doctests:
+    """
+    pass
+
+def tangent(
+    x: float,
+    accuracy: int = 100
+) -> float:
+    """
+    Algorithm used: taylor series
+    https://en.wikipedia.org/wiki/Taylor_series
+
+    Doctests:
+    """
+    pass
+
+def cosecant(
+    x: float,
+    accuracy: int = 100
+) -> float:
+    """
+    Algorithm used: taylor series
+    https://en.wikipedia.org/wiki/Taylor_series
+
+    Doctests:
+    """
+    pass
+
+def secant(
+    x: float,
+    accuracy: int = 100
+) -> float:
+    """
+    Algorithm used: taylor series
+    https://en.wikipedia.org/wiki/Taylor_series
+
+    Doctests:
+    """
+    pass
+
+def cotangent(
+    x: float,
+    accuracy: int = 100
+) -> float:
+    """
+    Algorithm used: taylor series
+    https://en.wikipedia.org/wiki/Taylor_series
+
+    Doctests:
+    """
+    pass
+
+def arc_sine(
+    x: float,
+    accuracy: int = 100
+) -> float:
+    """
+    Algorithm used: taylor series
+    https://en.wikipedia.org/wiki/Taylor_series
+
+    Doctests:
+    """
+    pass
+
+def arc_cosine(
+    x: float,
+    accuracy: int = 100
+) -> float:
+    """
+    Algorithm used: taylor series
+    https://en.wikipedia.org/wiki/Taylor_series
+
+    Doctests:
+    """
+    pass
+
+def arc_tangent(
+    x: float,
+    accuracy: int = 100
+) -> float:
+    """
+    Algorithm used: taylor series
+    https://en.wikipedia.org/wiki/Taylor_series
+
+    Doctests:
+    """
+    pass
+
+def arc_cosecant(
+    x: float,
+    accuracy: int = 100
+) -> float:
+    """
+    Algorithm used: taylor series
+    https://en.wikipedia.org/wiki/Taylor_series
+
+    Doctests:
+    """
+    pass
+
+def arc_secant(
+    x: float,
+    accuracy: int = 100
+) -> float:
+    """
+    Algorithm used: taylor series
+    https://en.wikipedia.org/wiki/Taylor_series
+
+    Doctests:
+    """
+    pass
+
+def arc_cotangent(
+    x: float,
+    accuracy: int = 100
+) -> float:
+    """
+    Algorithm used: taylor series
+    https://en.wikipedia.org/wiki/Taylor_series
+
+    Doctests:
+    """
+    pass
+
+if __name__ == "__main__":
+
+    # do some testing stuff
+
+    pass
