58fd7239359cc5b931425f1619537a6fcbd8bd53
Noah Schiro <noahschiro@rocketmail.com>
2021-12-02 15:36:25 -0500

    Sine function is completely functional

--- maths/trig_functions.py
+++ maths/trig_functions.py
@@ -12,11 +12,12 @@ from math import pi
 # a factorial in their expressions. This
 # is just a simple O(n) algo for factorial
 def factorial(x: int) -> int:
+
     value = 1
-    for n in range(2,x):
-        value *= n
-    
+    for i in range(1, x + 1):
+        value *= i
     return value
+
 
 def sine(
     x: float,
@@ -26,16 +27,42 @@ def sine(
     Algorithm used: taylor series
     https://en.wikipedia.org/wiki/Taylor_series#Trigonometric_functions#Trigonometric_functions
 
+    Accuracy cannot exceed 85 for 
+    this algorithm because python 
+    runs into issues with converting 
+    huge factorials into floats.
     Doctests:
     """
 
+    # Value to be returned
     sum = 0.0
+    
+    # Python imposed limitation
+    if accuracy > 85:
+        accuracy = 85
+
+    # We can reduce our range of angles to just 
+    # be from [0, 2pi] because trig functions are periodic
+    x = x%(2*pi)
+
+    # We can further reduce our range of inputs to just [-pi/2, pi/2]
+    if x > (pi/2) and pi < ((3*pi)/2):
+        # Output must then be negative if it's in the second or third
+        # quadrant
+        sum *= -1
+        # Move the value out of that quadrant
+        x += pi
+
+    # And we can once again reduce the range of inputs to [0, pi/2]
+    if x >= ((3*pi)/2):
+        x = (2*pi)-x
+
 
     for n in range(accuracy):
-        top = (-1)**n * pow(x, (2*n)+1)
-        bottom = factorial((2*n)+1)
-        sum += float(top) / float(bottom)
-        
+        numerator = (-1)**n * pow(x, (2*n)+1)
+        denominator = factorial((2*n)+1)
+        sum += float(numerator) / float (denominator)
+
     return sum
 
 def cosine(
@@ -173,4 +200,12 @@ def arc_cotangent(
 if __name__ == "__main__":
 
     # do some testing stuff
+    print(sine(0))
+    print(sine(pi/3))
+    print(sine(pi/2))
+    print(sine((3*pi)/4))
+    print(sine(pi))
+    print(sine((5*pi)/4))
+    print(sine((3*pi)/2))
+    print(sine((7*pi)/3))
     print(sine(2*pi))
