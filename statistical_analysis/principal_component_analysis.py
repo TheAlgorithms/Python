ecf02a76dfc926da9cb304de7fc23c26e7a73c8f
Anvit Garg <anvit25@gmail.com>
2021-12-10 03:27:24 +0530

    added type hints

--- statistical_analysis/principal_component_analysis.py
+++ statistical_analysis/principal_component_analysis.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def centering_matrix(n: int):
+def centering_matrix(n: int) -> np.ndarray:
     """
     Returns the centering matrix H = I - (1/n)J
     where I is the identity matrix and J is the matrix
@@ -17,30 +17,32 @@ def centering_matrix(n: int):
     return centering
 
 
-def principal_comoponent_analysis(X, n_components: int):
+def principal_comoponent_analysis(
+    data_matrix: np.ndarray, n_components: int
+) -> np.ndarray:
     """
     Input
-    X  -  is (n, p) array-like where n is the number of observations
-          and p is the number of differnt variables.
-    n_components  -  is the number of principal components to be 
-                     included
+    data_matrix   - is (n, p) array-like where n is the number of observations
+                    and p is the number of different variables.
+    n_components  - is the number of principal components to be
+                    included
 
     Returns
-    Data matrix of size (n, n_components), array like 
+    Data matrix of size (n, n_components), array like
 
 
     https://en.wikipedia.org/wiki/Principal_component_analysis
     """
-    n = len(X)
+    n = len(data_matrix)
     centering = centering_matrix(n)
 
-    if n_components > min(X.shape):
+    if n_components > min(data_matrix.shape):
         raise ValueError(
             "Number of components cannot be more than"
             " the number of observations or variables"
         )
 
-    covariance_matrix = X.transpose() @ centering @ X / (n - 1)
+    covariance_matrix = data_matrix.transpose() @ centering @ data_matrix / (n - 1)
 
     eigen_values, eigen_vectors = np.linalg.eig(covariance_matrix)
     # Eigen Values are not necessarily in decreasing order, so need
@@ -50,9 +52,9 @@ def principal_comoponent_analysis(X, n_components: int):
     # eigen_values = eigen_values[order]
     eigen_vectors = eigen_vectors[:, order]
 
-    return centering @ X @ eigen_vectors[:, :n_components]
+    return np.array(centering @ data_matrix @ eigen_vectors[:, :n_components])
 
 
 if __name__ == "__main__":
-    X = np.array([[1, 0], [2, 1], [3, 2], [4, 3]])
-    print(principal_comoponent_analysis(X, 1))
+    data_matrix = np.array([[1, 0], [2, 1], [3, 2], [4, 3]])
+    print(principal_comoponent_analysis(data_matrix, 1))
