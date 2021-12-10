95a3236cbfcfee143096cc90183d97f974b8d5ba
Anvit Garg <anvit25@gmail.com>
2021-12-10 12:09:25 +0530

    Added doctest

--- statistical_analysis/principal_component_analysis.py
+++ statistical_analysis/principal_component_analysis.py
@@ -1,24 +1,31 @@
 import numpy as np
 
 
-def centering_matrix(n: int) -> np.ndarray:
+def centering_matrix(n_dim: int) -> np.ndarray:
     """
     Returns the centering matrix H = I - (1/n)J
     where I is the identity matrix and J is the matrix
     that has 1 in every entry.
+
+    >>> centering_matrix(2)
+    array([[ 0.5, -0.5],
+           [-0.5,  0.5]])
+    >>> centering_matrix(1)
+    array([[0.]])
     """
+
     identity = np.diag(
         np.ones(
-            n,
+            n_dim
         )
     )
-    ones = np.ones((n, n))
-    centering = identity - ones / n
+    ones = np.ones((n_dim, n_dim))
+    centering = identity - ones / n_dim
     return centering
 
 
 def principal_comoponent_analysis(
-    data_matrix: np.ndarray, n_components: int
+    data_matrix: np.ndarray, n_components: int = 2
 ) -> np.ndarray:
     """
     Input
@@ -30,11 +37,16 @@ def principal_comoponent_analysis(
     Returns
     Data matrix of size (n, n_components), array like
 
+    >>> principal_comoponent_analysis(np.array([[1, 0], [2, 1], [3, 2], [4, 3]]))
+    array([[-2.12132034e+00,  2.22044605e-16],
+           [-7.07106781e-01,  5.55111512e-17],
+           [ 7.07106781e-01, -5.55111512e-17],
+           [ 2.12132034e+00, -2.22044605e-16]])
 
     https://en.wikipedia.org/wiki/Principal_component_analysis
     """
-    n = len(data_matrix)
-    centering = centering_matrix(n)
+    n_obs = len(data_matrix)
+    centering = centering_matrix(n_obs)
 
     if n_components > min(data_matrix.shape):
         raise ValueError(
@@ -42,19 +54,26 @@ def principal_comoponent_analysis(
             " the number of observations or variables"
         )
 
-    covariance_matrix = data_matrix.transpose() @ centering @ data_matrix / (n - 1)
+    covariance_matrix = data_matrix.transpose() @ centering @ data_matrix / (n_obs - 1)
 
     eigen_values, eigen_vectors = np.linalg.eig(covariance_matrix)
     # Eigen Values are not necessarily in decreasing order, so need
     # to reorder them
 
     order = np.argsort(eigen_values)[::-1]
-    # eigen_values = eigen_values[order]
     eigen_vectors = eigen_vectors[:, order]
 
     return np.array(centering @ data_matrix @ eigen_vectors[:, :n_components])
 
 
 if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
     data_matrix = np.array([[1, 0], [2, 1], [3, 2], [4, 3]])
-    print(principal_comoponent_analysis(data_matrix, 1))
+    result = principal_comoponent_analysis(data_matrix, 1)
+
+    print("The first Principal component of ")
+    print(data_matrix)
+    print('is')
+    print(result)
