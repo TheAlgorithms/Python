652f63ed0c260f5520322aae943d8f5931c103c2
Vaibhav <72079538+Vaibhav19102008@users.noreply.github.com>
2021-12-13 11:53:30 +0530

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,25 @@
+# Program to display the Fibonacci sequence up to n-th term
+
+nterms = int(input("How many terms? "))
+
+# first two terms
+n1, n2 = 0, 1
+count = 0
+
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Please enter a positive integer")
+# if there is only one term, return n1
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+# generate fibonacci sequence
+else:
+   print("Fibonacci sequence:")
+   while count < nterms:
+       print(n1)
+       nth = n1 + n2
+       # update values
+       n1 = n2
+       n2 = nth
+       count += 1
