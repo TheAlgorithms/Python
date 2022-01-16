052ae65c6c838af7c7d37117b64031fb48ab7737
nirbhayChaplot <nirc34@gmail.com>
2022-01-16 18:33:13 +0530

    Implement Algo Fully

--- ciphers/a51.py
+++ ciphers/a51.py
@@ -1,6 +1,5 @@
 # implementing A5/1 cipher algorithm
-
-from re import I
+from collections import Counter
 
 
 reg1_len = 19
@@ -32,7 +31,7 @@ def set_register(type:int, i:int, a:str):
         intermediate_xor = LFSR1[tapped_bits_1[0]] ^ LFSR1[tapped_bits_1[1]] ^ LFSR1[tapped_bits_1[2]] ^ LFSR1[tapped_bits_1[3]]
         if i < 19:
             LFSR1[i] = intermediate_xor ^ int(a)
-            
+
     if (type == 2):
         # register 2
         intermediate_xor = LFSR2[tapped_bits_1[0]] ^ LFSR2[tapped_bits_1[1]] 
@@ -46,6 +45,12 @@ def set_register(type:int, i:int, a:str):
             LFSR3[i] = intermediate_xor ^ int(a)
 
 
+def majority(arr):
+    freq = Counter(arr)
+    for (key,val) in freq.items():
+        if(val > 1):
+            return key
+
 #Example of 64-bit key: 0101001000011010110001110001100100101001000000110111111010110111
 incomming_str = "0101001000011010110001110001100100101001000000110111111010110111"
 # 64
@@ -54,8 +59,34 @@ for i in range(len(incomming_str)):
     set_register(2, i, incomming_str[i])
     set_register(3, i, incomming_str[i])
 
+key = []
 
+for i in range(64):
+    # majority counting function
+    majority_value = majority([LFSR1[8], LFSR2[10], LFSR3[10]])
+    if (majority_value == LFSR1[8]):
+        intermediate_xor = LFSR1[tapped_bits_1[0]] ^ LFSR1[tapped_bits_1[1]] ^ LFSR1[tapped_bits_1[2]] ^ LFSR1[tapped_bits_1[3]]
+        LFSR1.insert(0, intermediate_xor)
+        LFSR1.pop()
+    if (majority_value == LFSR2[10]):
+        intermediate_xor = LFSR2[tapped_bits_1[0]] ^ LFSR2[tapped_bits_1[1]] 
+        LFSR2.insert(0, intermediate_xor)
+        LFSR2.pop()
+    if (majority_value == LFSR3[10]):
+        intermediate_xor = LFSR3[tapped_bits_1[0]] ^ LFSR3[tapped_bits_1[1]] ^ LFSR3[tapped_bits_1[2]] ^ LFSR3[tapped_bits_1[3]]
+        LFSR3.insert(0, intermediate_xor)
+        LFSR3.pop()
 
-print(LFSR1)
-print(LFSR2)
-print(LFSR3)
+    output:int = LFSR1[18]^LFSR2[21]^LFSR3[22]
+    key.append(output)
+
+cipher = []
+for i in key:
+    cipher.append(key[i] ^ int(incomming_str[i]))
+
+print("LFSR1: ", LFSR1)
+print("LFSR2: ", LFSR2)
+print("LFSR3: ", LFSR3)
+print("KEY: ", key)
+print("Cipher: ", cipher)
+print(len(cipher))
