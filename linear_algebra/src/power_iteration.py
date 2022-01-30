9fff131b7a5a598ecad54646f635b74101217c2d
Calvin McCarter <calvin199@gmail.com>
2022-01-30 15:47:58 -0500

    revert switch to unittest

--- linear_algebra/src/power_iteration.py
+++ linear_algebra/src/power_iteration.py
@@ -1,5 +1,3 @@
-import unittest
-
 import numpy as np
 
 
@@ -83,47 +81,44 @@ def power_iteration(
     return lamda, vector
 
 
-class TestPowerIteration(unittest.TestCase):
-    def setUp(self) -> None:
-        self.real_input_matrix = np.array([[41, 4, 20], [4, 26, 30], [20, 30, 50]])
-        self.real_vector = np.array([41, 4, 20])
-        self.complex_input_matrix = self.real_input_matrix.astype(np.complex128)
-        imag_matrix = np.triu(1j * self.complex_input_matrix, 1)
-        self.complex_input_matrix += imag_matrix
-        self.complex_input_matrix += -1 * imag_matrix.T
-        self.complex_vector = np.array([41, 4, 20]).astype(np.complex128)
+def test_power_iteration() -> None:
+    """
+    >>> test_power_iteration()  # self running tests
+    """
+    real_input_matrix = np.array([[41, 4, 20], [4, 26, 30], [20, 30, 50]])
+    real_vector = np.array([41, 4, 20])
+    complex_input_matrix = real_input_matrix.astype(np.complex128)
+    imag_matrix = np.triu(1j * complex_input_matrix, 1)
+    complex_input_matrix += imag_matrix
+    complex_input_matrix += -1 * imag_matrix.T
+    complex_vector = np.array([41, 4, 20]).astype(np.complex128)
 
+    for problem_type in ["real", "complex"]:
+        if problem_type == "real":
+            input_matrix = real_input_matrix
+            vector = real_vector
+        elif problem_type == "complex":
+            input_matrix = complex_input_matrix
+            vector = complex_vector
 
-    def test_power_iteration(self) -> None:
-        """
-        >>> test_power_iteration()  # self running tests
-        """
-        for problem_type in ["real", "complex"]:
-            if problem_type == "real":
-                input_matrix = self.real_input_matrix
-                vector = self.real_vector
-            elif problem_type == "complex":
-                input_matrix = self.complex_input_matrix
-                vector = self.complex_vector
+        # Our implementation.
+        eigen_value, eigen_vector = power_iteration(input_matrix, vector)
 
-            # Our implementation.
-            eigen_value, eigen_vector = power_iteration(input_matrix, vector)
+        # Numpy implementation.
 
-            # Numpy implementation.
+        # Get eigen values and eigen vectors using built in numpy
+        # eigh (eigh used for symmetric or hermetian matrices).
+        eigen_values, eigen_vectors = np.linalg.eigh(input_matrix)
+        # Last eigen value is the maximum one.
+        eigen_value_max = eigen_values[-1]
+        # Last column in this matrix is eigen vector corresponding to largest eigen value.
+        eigen_vector_max = eigen_vectors[:, -1]
 
-            # Get eigen values and eigen vectors using built in numpy
-            # eigh (eigh used for symmetric or hermetian matrices).
-            eigen_values, eigen_vectors = np.linalg.eigh(input_matrix)
-            # Last eigen value is the maximum one.
-            eigen_value_max = eigen_values[-1]
-            # Last column in this matrix is eigen vector corresponding to largest eigen value.
-            eigen_vector_max = eigen_vectors[:, -1]
-
-            # Check our implementation and numpy gives close answers.
-            assert np.abs(eigen_value - eigen_value_max) <= 1e-6
-            # Take absolute values element wise of each eigenvector.
-            # as they are only unique to a minus sign.
-            assert np.linalg.norm(np.abs(eigen_vector) - np.abs(eigen_vector_max)) <= 1e-6
+        # Check our implementation and numpy gives close answers.
+        assert np.abs(eigen_value - eigen_value_max) <= 1e-6
+        # Take absolute values element wise of each eigenvector.
+        # as they are only unique to a minus sign.
+        assert np.linalg.norm(np.abs(eigen_vector) - np.abs(eigen_vector_max)) <= 1e-6
 
 
 if __name__ == "__main__":
