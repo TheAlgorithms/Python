2fda9b1afebdc8530fa7fd1a68f2ed2d3e0a533b
Shehzad Shah <64668205+shehzad02@users.noreply.github.com>
2022-01-30 22:53:56 -0500

    Add documentation and testing

--- strings/validate_parentheses_brackets.py
+++ strings/validate_parentheses_brackets.py
@@ -1,15 +1,42 @@
-def isValid(self, s: str) -> bool:
-    stack: list = []
-    dic: dict = {
+def valid_parentheses_brackets(input_string: str) -> bool:
+    """
+    Determine whether the brackets, braces, and parentheses in a string are valid.
+    Works only on strings containing only brackets, braces, and parentheses.
+
+    :param input_string:
+    :return: Boolean
+
+    >>> valid_parentheses_brackets('()')
+    True
+    >>> valid_parentheses_brackets('()[]{}')
+    True
+    >>> valid_parentheses_brackets('{[()]}')
+    True
+    >>> valid_parentheses_brackets('(})')
+    False
+
+    Time complexity: O(n) where n is the length of the input string.
+    Space complexity: O(n) where n is the length of the input string.
+    """
+    open_stack: list = []
+    map_close_to_open: dict = {
         ')': '(',
         '}': '{',
         ']': '['
     }
-    for ch in s:
-        if ch in dic:
-            x = stack.pop()
-            if dic[ch] != x:
+    for character in input_string:
+        if character in map_close_to_open:
+            if open_stack and open_stack.pop() == map_close_to_open[character]:
+                pass
+            else:
                 return False
-        stack.append(ch)
+        else:
+            open_stack.append(character)
     
-    return True
+    return False if open_stack else True
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
