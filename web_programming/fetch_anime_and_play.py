d5f33c56116522f76a4c757a26a6cf5e4c56ba0e
saptarshi1996 <saptarshisg96@gmail.com>
2022-02-02 03:23:00 +0530

    remove unussed exception

--- web_programming/fetch_anime_and_play.py
+++ web_programming/fetch_anime_and_play.py
@@ -3,7 +3,6 @@ from xml.dom import NotFoundErr
 import requests
 from bs4 import BeautifulSoup, NavigableString
 from fake_useragent import UserAgent
-from requests.exceptions import RequestException
 
 BASE_URL = "https://ww1.gogoanime2.org"
 
@@ -31,15 +30,12 @@ def search_scraper(anime_name: str) -> list:
     # concat the name to form the search url.
     search_url = f"{BASE_URL}/search/{anime_name}"
 
-    try:
-        response = requests.get(
-            search_url, headers={"UserAgent": UserAgent().chrome}
-        )  # request the url.
+    response = requests.get(
+        search_url, headers={"UserAgent": UserAgent().chrome}
+    )  # request the url.
 
-        # Is the response ok?
-        response.raise_for_status()
-    except RequestException as e:
-        raise e
+    # Is the response ok?
+    response.raise_for_status()
 
     # parse with soup.
     soup = BeautifulSoup(response.text, "html.parser")
@@ -89,13 +85,8 @@ def search_anime_episode_list(episode_endpoint: str) -> list:
 
     request_url = f"{BASE_URL}{episode_endpoint}"
 
-    try:
-        response = requests.get(
-            url=request_url, headers={"UserAgent": UserAgent().chrome}
-        )
-        response.raise_for_status()
-    except RequestException as e:
-        raise e
+    response = requests.get(url=request_url, headers={"UserAgent": UserAgent().chrome})
+    response.raise_for_status()
 
     soup = BeautifulSoup(response.text, "html.parser")
 
@@ -142,12 +133,10 @@ def get_anime_episode(episode_endpoint: str) -> list:
 
     episode_page_url = f"{BASE_URL}{episode_endpoint}"
 
-    try:
-        response = requests.get(
-            url=episode_page_url, headers={"User-Agent": UserAgent().chrome}
-        )
-    except RequestException as e:
-        raise e
+    response = requests.get(
+        url=episode_page_url, headers={"User-Agent": UserAgent().chrome}
+    )
+    response.raise_for_status()
 
     soup = BeautifulSoup(response.text, "html.parser")
 
