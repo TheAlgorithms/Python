27524fe09a1564de98935ef6564e7b57777c0070
zaakeer <zaakeer1.1@gmail.com>
2022-02-03 20:14:20 +0530

    Update unique_paths_in_obstacle_grid.py

--- dynamic_programming/unique_paths_in_obstacle_grid.py
+++ dynamic_programming/unique_paths_in_obstacle_grid.py
@@ -7,59 +7,59 @@ We can only move right or down
 """
 
 
-def uniquePathsWithObstacles(obstacleGrid: list[list[int]]) -> int:
+def unique_paths_with_obstacles(obstacle_grid: list[list[int]]) -> int:
     """
-    >>> print(uniquePathsWithObstacles([[0,0,0],[0,1,0],[0,0,0]]))
+    >>> print(unique_paths_with_obstacles([[0,0,0],[0,1,0],[0,0,0]]))
     2
-    >>> print(uniquePathsWithObstacles([[0,1],[0,0]]))
+    >>> print(unique_paths_with_obstacles([[0,1],[0,0]]))
     1
-    >>> print(uniquePathsWithObstacles([[0,1,0],[0,0,0],[0,0,1]]))
+    >>> print(unique_paths_with_obstacles([[0,1,0],[0,0,0],[0,0,1]]))
     0
     """
 
     # If the obstacle grid is not given as input
-    if not obstacleGrid:
+    if not obstacle_grid:
         return 0
 
     # If the starting tile is blocked there is no way to enter the grid
-    if obstacleGrid[0][0] == 1:
+    if obstacle_grid[0][0] == 1:
         return 0
 
-    rows = len(obstacleGrid)
-    cols = len(obstacleGrid[0])
+    rows = len(obstacle_grid)
+    cols = len(obstacle_grid[0])
 
     # Creating a 2D list to memorise unique paths to a tile
-    uniquePaths = [[0 for col in range(cols)] for row in range(rows)]
+    unique_paths = [[0 for col in range(cols)] for row in range(rows)]
 
     # There is only one way to reach the starting the tile
-    uniquePaths[0][0] = 1
+    unique_paths[0][0] = 1
 
     # Finding no of ways to reach all the tiles in top row
     for col in range(1, cols):
-        if obstacleGrid[0][col] == 1:
-            uniquePaths[0][col] = 0
+        if obstacle_grid[0][col] == 1:
+            unique_paths[0][col] = 0
         else:
-            uniquePaths[0][col] = uniquePaths[0][col - 1]
+            unique_paths[0][col] = unique_paths[0][col - 1]
 
     # Finding no of ways to reach all the tiles in left most column
     for row in range(1, rows):
-        if obstacleGrid[row][0] == 1:
-            uniquePaths[row][0] = 0
+        if obstacle_grid[row][0] == 1:
+            unique_paths[row][0] = 0
         else:
-            uniquePaths[row][0] = uniquePaths[row - 1][0]
+            unique_paths[row][0] = unique_paths[row - 1][0]
 
     # Calculating no of unique paths for rest of the tiles
     for row in range(1, rows):
         for col in range(1, cols):
-            if obstacleGrid[row][col] == 1:
-                uniquePaths[row][col] = 0
+            if obstacle_grid[row][col] == 1:
+                unique_paths[row][col] = 0
             else:
-                uniquePaths[row][col] = (
-                    uniquePaths[row - 1][col] + uniquePaths[row][col - 1]
+                unique_paths[row][col] = (
+                    unique_paths[row - 1][col] + unique_paths[row][col - 1]
                 )
 
-    return uniquePaths[-1][-1]
+    return unique_paths[-1][-1]
 
 
 if __name__ == "__main__":
-    testmod(name="uniquePathsWithObstacles", verbose=True)
+    testmod(name="unique_paths_with_obstacles", verbose=True)
