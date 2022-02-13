2f3681fecbd84b55354050cd7bb31c28cbefe5f7
Christian Clauss <cclauss@me.com>
2022-02-13 18:06:12 +0100

    Update points_are_collinear_3d.py

--- maths/points_are_collinear_3d.py
+++ maths/points_are_collinear_3d.py
@@ -46,7 +46,6 @@ def create_vector(end_point1: Point3d, end_point2: Point3d) -> Vector3d:
     x = end_point2[0] - end_point1[0]
     y = end_point2[1] - end_point1[1]
     z = end_point2[2] - end_point1[2]
-
     return (x, y, z)
 
 
@@ -72,7 +71,6 @@ def get_3d_vectors_cross(ab: Vector3d, ac: Vector3d) -> Vector3d:
     x = ab[1] * ac[2] - ab[2] * ac[1]  # *i
     y = (ab[0] * ac[2] - ab[2] * ac[0]) * -1  # *j
     z = ab[0] * ac[1] - ab[1] * ac[0]  # *k
-
     return (x, y, z)
 
 
@@ -125,5 +123,4 @@ def are_collinear(a: Point3d, b: Point3d, c: Point3d, accuracy: int = 10) -> boo
     """
     ab = create_vector(a, b)
     ac = create_vector(a, c)
-
     return is_zero_vector(get_3d_vectors_cross(ab, ac), accuracy)
