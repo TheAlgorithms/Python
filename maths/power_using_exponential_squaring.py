3c202c4469cdcaf49c7021e29086fa5b1768a7f1
fffzlfk <1319933925qq@gmail.com>
2022-03-01 22:28:53 +0800

    Add power_using_exponential_squaring

---
+++ maths/power_using_exponential_squaring.py
@@ -0,0 +1,30 @@
+def power_using_exponential_squaring(base: int, exponent: int) -> int:
+    """Returns the result of base^exponent
+
+    >>> power_using_exponential_squaring(3, 3)
+    27
+    >>> power_using_exponential_squaring(2, 0)
+    1
+    >>> power_using_exponential_squaring(2, 10)
+    1024
+    >>> power_using_exponential_squaring(10, 6)
+    1000000
+    """
+
+    assert base >= 1
+
+    result = 1
+    while exponent > 0:
+        if exponent & 1:
+            result *= base
+
+        base *= base
+        exponent >>= 1
+
+    return result
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
