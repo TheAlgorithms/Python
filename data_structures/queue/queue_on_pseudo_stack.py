d650f9565b8521cb7735d4b61ac6ad02ae7a24b2
aissata <aimaiga2@gmail.com>
2022-03-03 10:53:07 +0100

    fix: test coverage of queue_on_pseudo_stack.py

--- data_structures/queue/queue_on_pseudo_stack.py
+++ data_structures/queue/queue_on_pseudo_stack.py
@@ -7,6 +7,18 @@ class Queue:
         self.length = 0
 
     def __str__(self):
+        """
+        >>> q = Queue()
+        >>> q.put(3)
+        >>> q.put(5)
+        >>> q.put(7)
+        >>> print(q)
+        <3, 5, 7>
+
+        >>> q = Queue()
+        >>> print(q)
+        <>
+        """
         printed = "<" + str(self.stack)[1:-1] + ">"
         return printed
 
@@ -15,6 +27,12 @@ class Queue:
         item to enqueue"""
 
     def put(self, item):
+        """
+        >>> q = Queue()
+        >>> q.put(3)
+        >>> assert q.stack == [3]
+        >>> assert q.length == 1
+        """
         self.stack.append(item)
         self.length = self.length + 1
 
@@ -24,6 +42,37 @@ class Queue:
         item that was dequeued"""
 
     def get(self):
+        """
+        Testing get() and inner call to rotate()
+
+        >>> q = Queue()
+        >>> q.put(3)
+        >>> q.put(5)
+        >>> q.put(7)
+        >>> n = q.get()
+        >>> assert n == 3
+        >>> assert q.stack == [5,7]
+
+        >>> q2 = Queue()
+        >>> q2.put(3)
+        >>> n = q2.get()
+        >>> n = q2.get()
+        Traceback (most recent call last):
+        ...
+        IndexError: The queue is empty
+
+        >>> q3 = Queue()
+        >>> n = q3.get()
+        Traceback (most recent call last):
+        ...
+        IndexError: The queue is empty
+
+        >>> q4 = Queue()
+        >>> q4.put(3)
+        >>> n = q4.get()
+        >>> assert n == 3
+        """
+
         self.rotate(1)
         dequeued = self.stack[self.length - 1]
         self.stack = self.stack[:-1]
@@ -33,10 +82,22 @@ class Queue:
 
     """Rotates the queue {@code rotation} times
     @param rotation
+    @requirement: |self.length| > 0
         number of times to rotate queue"""
 
     def rotate(self, rotation):
+        """
+        >>> q = Queue()
+        >>> q.put(3)
+        >>> q.put(5)
+        >>> q.put(7)
+        >>> q.rotate(2)
+        >>> assert q.stack == [7,3,5]
+        """
+
         for i in range(rotation):
+            if self.length == 0:
+                raise IndexError("The queue is empty")
             temp = self.stack[0]
             self.stack = self.stack[1:]
             self.put(temp)
@@ -46,6 +107,18 @@ class Queue:
     @return item at front of self.stack"""
 
     def front(self):
+        """
+        >>> q = Queue()
+        >>> q.put(3)
+        >>> n = q.front()
+        >>> assert n == 3
+
+        >>> q2 = Queue()
+        >>> n = q2.front()
+        Traceback (most recent call last):
+        ...
+        IndexError: The queue is empty
+        """
         front = self.get()
         self.put(front)
         self.rotate(self.length - 1)
@@ -54,4 +127,15 @@ class Queue:
     """Returns the length of this.stack"""
 
     def size(self):
+        """
+        >>> q = Queue()
+        >>> q.size()
+        0
+        """
         return self.length
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
