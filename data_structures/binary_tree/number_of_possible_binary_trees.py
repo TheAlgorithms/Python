80c349ea0cc35fa4b9073469056b04c4cb41d909
YenChen <b06902047@ntu.edu.tw>
2022-03-04 15:31:03 +0100

    Fixes: #11 run pre-commit before push

--- data_structures/binary_tree/number_of_possible_binary_trees.py
+++ data_structures/binary_tree/number_of_possible_binary_trees.py
@@ -1,9 +1,7 @@
 """
 Hey, we are going to find an exciting number called Catalan number which is use to find
 the number of possible binary search trees from tree of a given number of nodes.
-
 We will use the formula: t(n) = SUMMATION(i = 1 to n)t(i-1)t(n-i)
-
 Further details at Wikipedia: https://en.wikipedia.org/wiki/Catalan_number
 """
 """
@@ -24,7 +22,6 @@ def binomial_coefficient(n: int, k: int) -> int:
     :param n: 2 times of Number of nodes
     :param k: Number of nodes
     :return:  Integer Value
-
     >>> binomial_coefficient(4, 2)
     6
     >>> binomial_coefficient(5, 3)
@@ -45,11 +42,9 @@ def catalan_number(node_count: int) -> int:
     """
     We can find Catalan number many ways but here we use Binomial Coefficient because it
     does the job in O(n)
-
     return the Catalan number of n using 2nCn/(n+1).
     :param n: number of nodes
     :return: Catalan number of n nodes
-
     >>> catalan_number(5)
     42
     >>> catalan_number(6)
@@ -63,7 +58,6 @@ def factorial(n: int) -> int:
     Return the factorial of a number.
     :param n: Number to find the Factorial of.
     :return: Factorial of n.
-
     >>> import math
     >>> all(factorial(i) == math.factorial(i) for i in range(10))
     True
@@ -85,7 +79,6 @@ def binary_tree_count(node_count: int) -> int:
     Return the number of possible of binary trees.
     :param n: number of nodes
     :return: Number of possible binary trees
-
     >>> binary_tree_count(5)
     5040
     >>> binary_tree_count(6)
@@ -94,10 +87,13 @@ def binary_tree_count(node_count: int) -> int:
     return catalan_number(node_count) * factorial(node_count)
 
 
+if __name__ == "__main__":
+    node_count = int(input("Enter the number of nodes: ").strip() or 0)
+
+
 def number_of_binary_trees(node_count: int) -> None:
     """
     :param n: number of nodes
-
     >>> number_of_binary_trees(-5)
     Traceback (most recent call last):
     ...
