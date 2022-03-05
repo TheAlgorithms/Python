f43534dbe9fc549d940152d69687d5a5f7ac7f25
Gautam Manek <gautam007@e.ntu.edu.sg>
2022-03-06 00:05:18 +0100

    feat: added unit tests to quadratic_probing

--- data_structures/hashing/quadratic_probing.py
+++ data_structures/hashing/quadratic_probing.py
@@ -11,7 +11,37 @@ class QuadraticProbing(HashTable):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def _collision_resolution(self, key, data=None):
+    def _collision_resolution(self, key):
+        """
+        >>> hashTable = QuadraticProbing(7)
+        >>> hashTable.insert_data(0)
+        >>> hashTable.values
+        [0, None, None, None, None, None, None]
+        >>> hashTable.insert_data(1)
+        >>> hashTable.values
+        [0, 1, None, None, None, None, None]
+        >>> hashTable._collision_resolution(7)
+        4
+        >>> hashTable.insert_data(7)
+        >>> hashTable.values
+        [0, 1, None, None, 7, None, None]
+        >>> hashTable.bulk_insert([2,3,5,6])
+        step 1
+        [0, 1, 2, 3, 4, 5, 6]
+        [0, 1, 2, None, 7, None, None]
+        step 2
+        [0, 1, 2, 3, 4, 5, 6]
+        [0, 1, 2, 3, 7, None, None]
+        step 3
+        [0, 1, 2, 3, 4, 5, 6]
+        [0, 1, 2, 3, 7, 5, None]
+        step 4
+        [0, 1, 2, 3, 4, 5, 6]
+        [0, 1, 2, 3, 7, 5, 6]
+        >>> hashTable.balanced_factor()
+        1.0
+        >>> hashTable._collision_resolution(7)
+        """
         i = 1
         new_key = self.hash_function(key + i * i)
 
