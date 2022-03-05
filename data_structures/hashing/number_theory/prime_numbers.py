76bc87be19af2e8f7e4bcbe6a2a676365c869967
Gautam Manek <gautam007@e.ntu.edu.sg>
2022-03-05 21:41:20 +0100

    feat: added unit test for number_theory/prime_number.py

--- data_structures/hashing/number_theory/prime_numbers.py
+++ data_structures/hashing/number_theory/prime_numbers.py
@@ -6,18 +6,37 @@
 
 def check_prime(number):
     """
-    it's not the best solution
+    checks that the given number is a prime number.
+    >>> check_prime(1)
+    False
+    >>> check_prime(7)
+    True
+    >>> check_prime(11)
+    True
+    >>> check_prime(8)
+    False
     """
-    special_non_primes = [0, 1, 2]
-    if number in special_non_primes[:2]:
-        return 2
-    elif number == special_non_primes[-1]:
-        return 3
-
-    return all([number % i for i in range(2, number)])
+    if number < 2:
+        return False
+    for factor in range(2, number // 2):
+        if number % factor == 0:
+            return False
+    return True
 
 
 def next_prime(value, factor=1, **kwargs):
+    """
+    Starting from value * factor, finds the first prime number above/ below.
+    If an argument of desc=True is passed, finds the first prime number below.
+    >>> next_prime(1)
+    2
+    >>> next_prime(6)
+    7
+    >>> next_prime(11)
+    13
+    >>> next_prime(8, 1, desc=True)
+    7
+    """
     value = factor * value
     first_value_val = value
 
