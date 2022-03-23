d00d1356838fdd0cb0857f0b42066c197e279610
Margaret <62753112+meg-1@users.noreply.github.com>
2022-03-23 17:43:11 +0200

    added a descriptive name

--- maths/is_narcissistic.py
+++ maths/is_narcissistic.py
@@ -1,4 +1,4 @@
-def is_narcissistic(i: int) -> bool:
+def is_narcissistic(num: int) -> bool:
 
   """
     >>> is_narcissistic(1527)
@@ -13,10 +13,10 @@ def is_narcissistic(i: int) -> bool:
 
   """In number theory, a narcissistic number is a number that is 
   the sum of its own digits each raised to the power of the number of digits.
-  this alogrithm finds out if given number 'i' is narcissistic or not"""
+  this alogrithm finds out if given number 'num' is narcissistic or not"""
 
-  i = abs(int(i))
-  return sum([int(char)**len(str(i)) for char in str(i)]) == i
+  num = abs(int(num))
+  return sum([int(char)**len(str(num)) for char in str(num)]) == num
 
 
 if __name__ == "__main__":
