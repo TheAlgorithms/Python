8e0f78f1038efdff16ba3c6962df34cb92265784
Durgesh Rao <77789927+DURGESH716@users.noreply.github.com>
2022-03-24 21:00:44 +0530

    Create Stack_using_list.py

---
+++ data_structures/stacks/Stack_using_list.py
@@ -0,0 +1,29 @@
+# Python program to
+# demonstrate stack implementation
+# using list
+
+stack = []
+
+# append() function to push
+# element in the stack
+stack.append('a')
+stack.append('b')
+stack.append('c')
+
+print('Initial stack')
+print(stack)
+
+# pop() function to pop
+# element from stack in
+# LIFO order
+print('\nElements popped from stack:')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+print('\nStack after elements are popped:')
+print(stack)
+
+# uncommenting print(stack.pop())
+# will cause an IndexError
+# as the stack is now empty
