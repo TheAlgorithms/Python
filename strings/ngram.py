2bf6ea8b28be972bc6c1eba9edf6b1ee83ba7572
Harshit Agarwal <43147421+9harshit@users.noreply.github.com>
2022-03-30 20:20:18 -0300

    Update strings/ngram.py
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- strings/ngram.py
+++ strings/ngram.py
@@ -18,10 +18,7 @@ def create_ngram(sentence: str, ngram_size: int) -> list:
     ['I ', ' a', 'am', 'm ', ' a', 'an', 'n ', ' N', 'NL', 'LP', 'Pe', 'er']
     """
 
-    ngrams = [
-        sentence[i : i + ngram_size] for i in range(len(sentence) - ngram_size + 1)
-    ]
-    return ngrams
+    return [sentence[i : i + ngram_size] for i in range(len(sentence) - ngram_size + 1)]
 
 
 if __name__ == "__main__":
