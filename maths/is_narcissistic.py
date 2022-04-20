1c3c89c5f3148c493aa009175be3781cb64a9148
Margaret <62753112+meg-1@users.noreply.github.com>
2022-04-20 17:29:38 +0300

    added indendation
    added indentation to the code

--- maths/is_narcissistic.py
+++ maths/is_narcissistic.py
@@ -1,24 +1,24 @@
 def is_narcissistic(num: int) -> bool:
 
-  """
-    >>> is_narcissistic(1527)
-    False
-    >>> is_narcissistic(-1527)
-    False
-    >>> is_narcissistic(153)
-    True
-    >>> is_narcissistic("153")
-    True
-  """
+    """
+      >>> is_narcissistic(1527)
+      False
+      >>> is_narcissistic(-1527)
+      False
+      >>> is_narcissistic(153)
+      True
+      >>> is_narcissistic("153")
+      True
+    """
 
-  """In number theory, a narcissistic number is a number that is 
-  the sum of its own digits each raised to the power of the number of digits.
-  this alogrithm finds out if given number 'num' is narcissistic or not"""
+    """In number theory, a narcissistic number is a number that is 
+    the sum of its own digits each raised to the power of the number of digits.
+    this alogrithm finds out if given number 'num' is narcissistic or not"""
 
-  num = abs(int(num))
-  return sum([int(char)**len(str(num)) for char in str(num)]) == num
+    num = abs(int(num))
+    return sum([int(char)**len(str(num)) for char in str(num)]) == num
 
 
-  if __name__ == "__main__":
-      import doctest
-      doctest.testmod()
+    if __name__ == "__main__":
+        import doctest
+        doctest.testmod()
