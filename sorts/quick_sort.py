63d86ef573f589a0e932a3cde9fd0baf71b85080
lt77777 <64245101+lt77777@users.noreply.github.com>
2022-04-28 16:42:07 -0400

    Update quick_sort.py
    Fixing issue 6095, reduces the chances of O(n^2) by using median as first pivot rather than end.

--- sorts/quick_sort.py
+++ sorts/quick_sort.py
@@ -26,7 +26,7 @@ def quick_sort(collection: list) -> list:
     """
     if len(collection) < 2:
         return collection
-    pivot = collection.pop()  # Use the last element as the first pivot
+    pivot = collection[len(collection)//2]  # Use the median as the first pivot
     greater: list[int] = []  # All elements greater than pivot
     lesser: list[int] = []  # All elements less than or equal to pivot
     for element in collection:
