3746684800945f4c0113d3c813b68d6ecbbeae89
Varunrao Wallajabad <vwallajabad@gmail.com>
2022-05-12 15:00:27 -0500

    Create luhn_check.py

---
+++ financial/luhn_check.py
@@ -0,0 +1,20 @@
+def luhn_check(cardNumber):
+    cardDigits = len(str(cardNumber))
+    totalSum = 0
+    isSecond = False
+
+    for i in range(cardDigits - 1, -1, -1):
+        d = ord(cardNumber[i]) - ord('0')
+
+        if (isSecond == True):
+            d = d * 2
+
+        totalSum += d // 10
+        totalSum += d % 10
+
+        isSecond = not isSecond
+
+    if (totalSum % 10 == 0):
+        return True
+    else:
+        return False
