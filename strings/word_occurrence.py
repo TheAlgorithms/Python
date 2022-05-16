f8e4253d392dcbb4efeabaece32c36291c95588f
Ikko Ashimine <eltociear@gmail.com>
2022-05-16 19:01:04 +0900

    Fix typo in word_occurrence.py
    word_occurence -> word_occurrence

--- strings/word_occurrence.py
+++ strings/word_occurrence.py
@@ -4,15 +4,15 @@ from collections import defaultdict
 from typing import DefaultDict
 
 
-def word_occurence(sentence: str) -> dict:
+def word_occurrence(sentence: str) -> dict:
     """
     >>> from collections import Counter
     >>> SENTENCE = "a b A b c b d b d e f e g e h e i e j e 0"
-    >>> occurence_dict = word_occurence(SENTENCE)
+    >>> occurence_dict = word_occurrence(SENTENCE)
     >>> all(occurence_dict[word] == count for word, count
     ...     in Counter(SENTENCE.split()).items())
     True
-    >>> dict(word_occurence("Two  spaces"))
+    >>> dict(word_occurrence("Two  spaces"))
     {'Two': 1, 'spaces': 1}
     """
     occurrence: DefaultDict[str, int] = defaultdict(int)
@@ -23,5 +23,5 @@ def word_occurence(sentence: str) -> dict:
 
 
 if __name__ == "__main__":
-    for word, count in word_occurence("INPUT STRING").items():
+    for word, count in word_occurrence("INPUT STRING").items():
         print(f"{word}: {count}")
