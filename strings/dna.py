47d65088b153b828b299005f9204d14125e2b28b
Margaret <62753112+meg-1@users.noreply.github.com>
2022-05-18 17:12:18 +0300

    fixed mistakes in dna.py
    fixed indentation

--- strings/dna.py
+++ strings/dna.py
@@ -3,14 +3,14 @@ import re
 def dna(dna: str) -> str:
 
     """
-      >>> dna("GCTA")
-      'CGAT'
-      >>> dna("ATGC")
-      'TACG'
-      >>> dna("CTGA")
-      'GACT'
-      >>> dna("GFGG")
-      'Invalid Strand'
+    >>> dna("GCTA")
+    'CGAT'
+    >>> dna("ATGC")
+    'TACG'
+    >>> dna("CTGA")
+    'GACT'
+    >>> dna("GFGG")
+    'Invalid Strand'
     """
 
     """ https://en.wikipedia.org/wiki/DNA """
@@ -20,10 +20,11 @@ def dna(dna: str) -> str:
     complementary side of said strand"""
 
     r = len(re.findall("[ATCG]", dna)) != len(dna) 
-    val = dna.translate(dna.maketrans("ATCG","TAGC"))
+    val = dna.translate(dna.maketrans("ATCG", "TAGC"))
     return "Invalid Strand" if r else val
 
 
 if __name__ == "__main__":
     import doctest
+    
     doctest.testmod()
