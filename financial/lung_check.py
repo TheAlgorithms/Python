ed532e06a8dce7355f773f27ae146361abcae724
Varunrao Wallajabad <vwallajabad@gmail.com>
2022-05-18 09:09:25 -0500

    Update lung_check.py

--- financial/lung_check.py
+++ financial/lung_check.py
@@ -1,5 +1,34 @@
-#https://en.wikipedia.org/wiki/Luhn_algorithm
+"""
+A pure python implementation of the lugh algortihm.
+
+The lugh algortihm is a simple checksum formula
+used to validate a variety of identification numbers, such as credit card numbers, 
+IMEI numbers, SIM card numbers and more.
+
+from: https://en.wikipedia.org/wiki/Luhn_algorithm
+
+For doctests run following command:
+python3 -m doctest -v lung_check.py
+
+For manual testing run:
+python3 lung_check.py
+
+"""
 def luhn_check(card_no: str) -> bool:
+    """
+    A pure python implementation of the lugh algortihm.
+
+    :param card_no: a card number to be validated
+    :return: True / False
+
+    Examples:
+    >>> luhn_check("7656449456")
+    True
+    >>> luhn_check("7626449456")
+    False
+    >>> luhn_check("7850889192")
+    True
+    """
     card_digits = len(str(card_no))
     luhn_sum = 0
     second = False
@@ -19,3 +48,7 @@ def luhn_check(card_no: str) -> bool:
         return True
     else:
         return False
+
+if __name__ == "__main__":
+    user_input = input("Enter a number:\n").strip()
+    print(luhn_check(user_input))
