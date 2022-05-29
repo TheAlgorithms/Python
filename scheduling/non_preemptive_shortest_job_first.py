e36c13f95850aabef55a327324b0ca770fe1a0c2
BYN <blaccbean@gmail.com>
2022-05-29 20:20:29 +0900

    delete Korean comments

--- scheduling/non_preemptive_shortest_job_first.py
+++ scheduling/non_preemptive_shortest_job_first.py
@@ -13,11 +13,8 @@ from statistics import mean
 
 
 def calculate_waitingtime(
-        arrival_time: list[int], burst_time: list[int], no_of_processes: int
+    arrival_time: list[int], burst_time: list[int], no_of_processes: int
 ) -> list[int]:
-    # Arrival_time : 도착 시간을 담은 리스트
-    # Burst_time : 수행시간
-    # No_of_processes : 프로세스의 개수, the number of processes
 
     """
     Calculate the waiting time of each processes
@@ -31,30 +28,21 @@ def calculate_waitingtime(
     [12, 0, 2]
     """
 
-    # waiting_time : 대기 시간
-    # remaining_time : 남은 수행 시간
     waiting_time = [0] * no_of_processes
     remaining_time = [0] * no_of_processes
 
     """
     Initialize remaining_time to waiting_time.
-    남은 수행 시간을 수행 시간으로 초기화한다.
     """
     for i in range(no_of_processes):
         remaining_time[i] = burst_time[i]
     ready_process = []
 
-    # completed : 수행 완료된 프로세스의 수, The number of processes completed
-    # total_time : 프로세스와 무관하게 진행되는 시간
     completed = 0
     total_time = 0
 
     """
-    수행할 프로세스가 남아 있을 때,
-        도착 시간이 지났고 남아 있는 수행 시간이 있는 프로세스는 ready_process에 들어간다.
-        ready_process에서 가장 짧은 프로세스인 target_process가 실행된다.
-
-    When processes are not completed,
+     When processes are not completed,
         A process whose arrival time has passed \
         and has remaining execution time is put into the ready_process.
         The shortest process in the ready_process, target_process is executed.
@@ -75,9 +63,9 @@ def calculate_waitingtime(
             total_time += burst_time[target_process]
             completed += 1
             remaining_time[target_process] = 0
-            waiting_time[target_process] = \
-                total_time - arrival_time[target_process]\
-                - burst_time[target_process]
+            waiting_time[target_process] = (
+                total_time - arrival_time[target_process] - burst_time[target_process]
+            )
         else:
             total_time += 1
 
@@ -85,7 +73,7 @@ def calculate_waitingtime(
 
 
 def calculate_turnaroundtime(
-        burst_time: list[int], no_of_processes: int, waiting_time: list[int]
+    burst_time: list[int], no_of_processes: int, waiting_time: list[int]
 ) -> list[int]:
     """
     Calculate the turnaround time of each Processes
@@ -101,8 +89,7 @@ def calculate_turnaroundtime(
 
     turn_around_time = [0] * no_of_processes
     for i in range(no_of_processes):
-        turn_around_time[i] = burst_time[i] \
-            + waiting_time[i]
+        turn_around_time[i] = burst_time[i] + waiting_time[i]
     return turn_around_time
 
 
@@ -113,10 +100,10 @@ if __name__ == "__main__":
     burst_time = [2, 5, 3, 7]
     arrival_time = [0, 0, 0, 0]
     processes = list(range(1, 5))
-    waiting_time = calculate_waitingtime(arrival_time,
-                                         burst_time, no_of_processes)
-    turn_around_time = calculate_turnaroundtime(burst_time,
-                                                no_of_processes, waiting_time)
+    waiting_time = calculate_waitingtime(arrival_time, burst_time, no_of_processes)
+    turn_around_time = calculate_turnaroundtime(
+        burst_time, no_of_processes, waiting_time
+    )
 
     # Printing the Result
     print("PID\tBurst Time\tArrival Time\tWaiting Time\tTurnaround Time")
