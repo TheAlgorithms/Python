c946db8835ab8c1b63bdcd71d07ba06ab7ff227e
ANUJ BISHT <anujbisht09@gmail.com>
2022-06-08 10:31:37 +0530

    Create linear_search.py

---
+++ data_structures/searching_algorithm/linear_search.py
@@ -0,0 +1,63 @@
+# Write a program to perform linear search
+
+# function to perform linear search on the list
+def linearSearch(list_1, size, key):
+    for i in range(size):
+        if list_1[i] == key:
+            return i
+    return -1
+
+
+# scanning the size of the list 
+size = int(input("Enter the number of elements to be entered in list: "))
+
+# initialiszing the empty list
+list_1 = []
+
+# scanning the elements of the list
+print(f"Enter the {size} elements of the list")
+for i in range(size):
+    print(f"Element {i+1}:", end=" ")
+    element = int(input())
+    list_1.append(element)
+
+# printing the list
+print("Your List:")
+print(list_1)
+
+# scanning the key element to find in the list
+key = int(input("Enter an element to find in list: "))
+
+# calling the linearSearch() function
+result = linearSearch(list_1, size, key)
+
+# printing the conclusion
+if result == -1:
+    print(f"{key} is not found in the list")
+else:
+    print(f"{key} is found at position {result + 1} in the list")
+
+    
+    
+# OUTPUT
+# Enter the number of elements to be entered in list: 3
+# Enter the 3 elements of the list
+# Element 1: 1
+# Element 2: 5
+# Element 3: 9
+# Your List:
+# [1, 5, 9]
+# Enter an element to find in list: 5
+# 5 is found at position 2 in the list
+
+
+# OUTPUT
+# Enter the number of elements to be entered in list: 3
+# Enter the 3 elements of the list
+# Element 1: 1
+# Element 2: 5
+# Element 3: 9
+# Your List:
+# [1, 5, 9]
+# Enter an element to find in list: 8
+# 8 is not found in the list
