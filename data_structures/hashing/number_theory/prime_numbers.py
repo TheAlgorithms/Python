af18967d15b2c084cee99a21ce5a421b1f9215cb
Nick <ngiachou@gmail.com>
2022-07-05 21:09:17 -0400

    renames prime functions and occurances in comments

--- data_structures/hashing/number_theory/prime_numbers.py
+++ data_structures/hashing/number_theory/prime_numbers.py
@@ -4,7 +4,7 @@
 """
 
 
-def check_prime(number):
+def is_prime(number):
     """
     it's not the best solution
     """
@@ -21,7 +21,7 @@ def next_prime(value, factor=1, **kwargs):
     value = factor * value
     first_value_val = value
 
-    while not check_prime(value):
+    while not is_prime(value):
         value += 1 if not ("desc" in kwargs.keys() and kwargs["desc"] is True) else -1
 
     if value == first_value_val:
