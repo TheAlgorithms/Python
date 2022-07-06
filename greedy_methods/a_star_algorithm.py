014669be402c5933f138571d0a7810f05ea4a3db
Quyen Linh TA <linhtq.sfu@gmail.com>
2022-07-06 18:33:40 +0200

    Addition :: A* Search Algorithm

---
+++ greedy_methods/a_star_algorithm.py
@@ -0,0 +1,82 @@
+def a_star(start_vertex, vertex_target):
+    open_set = set(start_vertex)
+    closed_set = set()
+    dist_from_startnode = {}
+    parents = {}
+
+    dist_from_startnode[start_vertex] = 0
+    parents[start_vertex] = start_vertex
+
+    while len(open_set) > 0:
+        x = None
+        for v in open_set:
+            if x is None or dist_from_startnode[v] + heuristic(v) < dist_from_startnode[x] + heuristic(x):
+                x = v
+
+        if x == vertex_target or Nodes[x] is None:
+            pass
+        else:
+            for (y, weight) in get_neighbors(x):
+                if y not in open_set and y not in closed_set:
+                    open_set.add(y)
+                    parents[y] = x
+                    dist_from_startnode[y] = dist_from_startnode[x] + weight
+                else:
+                    if dist_from_startnode[y] > dist_from_startnode[x] + weight:
+                        dist_from_startnode[y] = dist_from_startnode[x] + weight
+                        parents[y] = x
+                        if y in closed_set:
+                            closed_set.remove(y)
+                            open_set.add(y)
+
+        if x is None:
+            print("Path does not exist!")
+            return None
+
+        # if the current node is the vertex_target
+        # then we begin reconstruct in the path from it to the start_vertex
+        if x == vertex_target:
+            path = []
+
+            while parents[x] != x:
+                path.append(x)
+                x = parents[x]
+
+            path.append(start_vertex)
+
+            path.reverse()
+
+            print('Path found: {}'.format(path))
+            return path
+        open_set.remove(x)
+        closed_set.add(x)
+
+    print("Path does not exist!")
+    return None
+
+
+def get_neighbors(v):
+    if v in Nodes:
+        return Nodes[v]
+    else:
+        return None
+
+
+def heuristic(n):
+    h_distance = {'A': 11,
+                  'B': 6,
+                  'C': 99,
+                  'D': 1,
+                  'E': 7,
+                  'G': 0, }
+    return h_distance[n]
+
+
+Nodes = {'A': [('B', 2), ('E', 3)],
+         'B': [('C', 1), ('G', 9)],
+         'C': None,
+         'E': [('D', 6)],
+         'D': [('G', 1)]}
+
+if __name__ == '__main__':
+    a_star('A', 'G')
