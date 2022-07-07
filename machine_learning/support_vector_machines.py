ee0423bb022091ce326f3bbb4c245760af07baac
Bjiornulf <tpeev01@gmail.com>
2022-07-07 18:01:40 +0200

    better naming and documentation

--- machine_learning/support_vector_machines.py
+++ machine_learning/support_vector_machines.py
@@ -3,72 +3,104 @@ from numpy import ndarray
 from scipy.optimize import Bounds, LinearConstraint, minimize
 
 
+def norm_squared(vector: ndarray) -> float:
+    """
+    Return the squared second norm of vector
+    norm_squared(v) = sum(x * x for x in v)
+
+    Args:
+        vector (ndarray): input vector
+
+    Returns:
+        float: squared second norm of vector
+    """
+    return np.dot(vector, vector)
+
+
 class SVC:
     """
     Support Vector Classifier
 
     Args:
-        kern (str): kernel to use. Default: linear
+        kernel (str): kernel to use. Default: linear
             Possible choices:
                 - linear
-        C: constraint for soft margin (data not linearly separable)
+        regularization: constraint for soft margin (data not linearly separable)
             Default: unbound
 
-    >>> SVC(kern="asdf")
+    >>> SVC(kernel="asdf")
     Traceback (most recent call last):
         ...
     ValueError: Unknown kernel: asdf
 
-    >>> SVC(kern="rbf")
+    >>> SVC(kernel="rbf")
     Traceback (most recent call last):
         ...
     ValueError: rbf kernel requires gamma
 
-    >>> SVC(kern="rbf", gamma=-1)
+    >>> SVC(kernel="rbf", gamma=-1)
     Traceback (most recent call last):
         ...
     ValueError: gamma must be > 0
     """
 
     def __init__(
-        self, *, C: float = np.inf, kern: str = "linear", gamma: float = None
+        self,
+        *,
+        regularization: float = np.inf,
+        kernel: str = "linear",
+        gamma: float = 0,
     ) -> None:
-        self.C = C
+        self.regularization = regularization
         self.gamma = gamma
-        if kern == "linear":
-            self.kern = self.__linear
-        elif kern == "rbf":
-            self.kern = self.__rbf
-            if not self.gamma:
+        if kernel == "linear":
+            self.kernel = self.__linear
+        elif kernel == "rbf":
+            if self.gamma == 0:
                 raise ValueError("rbf kernel requires gamma")
             if not (isinstance(self.gamma, float) or isinstance(self.gamma, int)):
                 raise ValueError("gamma must be float or int")
             if not self.gamma > 0:
                 raise ValueError("gamma must be > 0")
+            self.kernel = self.__rbf
             # in the future, there could be a default value like in sklearn
             # sklear: def_gamma = 1/(n_features * X.var()) (wiki)
             # previously it was 1/(n_features)
         else:
-            raise ValueError(f"Unknown kernel: {kern}")
+            raise ValueError(f"Unknown kernel: {kernel}")
 
     # kernels
-    def __linear(self, x: ndarray, y: ndarray) -> float:
-        return np.dot(x, y)
+    def __linear(self, vector1: ndarray, vector2: ndarray) -> float:
+        """Linear kernel (as if no kernel used at all)"""
+        return np.dot(vector1, vector2)
 
-    def __rbf(self, x: ndarray, y: ndarray) -> float:
-        return np.exp(-(self.gamma * np.dot(x - y, x - y)))
+    def __rbf(self, vector1: ndarray, vector2: ndarray) -> float:
+        """
+        RBF: Radial Basis Function Kernel
 
-    def fit(self, xs: list[ndarray], y: ndarray) -> None:
+        Note: for more information see:
+            https://en.wikipedia.org/wiki/Radial_basis_function_kernel
+
+        Args:
+            vector1 (ndarray): first vector
+            vector2 (ndarray): second vector)
+
+        Returns:
+            float: exp(-(gamma * norm_squared(vector1 - vector2)))
+        """
+        return np.exp(-(self.gamma * norm_squared(vector1 - vector2)))
+
+    def fit(self, observations: list[ndarray], classes: ndarray) -> None:
         """
         Fits the SVC with a set of observations.
 
         Args:
-            xs (list[Vector]): list of observations (each observation is a vector)
-            y (list[int]): classification of each observation (in {1, -1})
+            observations (list[ndarray]): list of observations
+            classes (ndarray): classification of each observation (in {1, -1})
         """
 
-        self.xs = xs
-        self.y = y
+        self.observations = observations
+        self.classes = classes
 
         # using Wolfe's Dual to calculate w.
         # Primal problem: minimize 1/2*norm_squared(w)
@@ -85,38 +117,54 @@ class SVC:
         # Since we use kernels, we only need l_star to calculate b
         # and to classify observations
 
-        (n,) = np.shape(y)
+        (n,) = np.shape(classes)
 
-        # l = v because flake8...
-        def to_minimize(v: ndarray) -> float:
-            """Opposite of the function to maximize"""
+        def to_minimize(candidate: ndarray) -> float:
+            """
+            Opposite of the function to maximize
+
+            Args:
+                candidate (ndarray): candidate array to test
+
+            Return:
+                float: Wolfe's Dual result to minimize
+            """
             s = 0
-            (n,) = np.shape(v)
+            (n,) = np.shape(candidate)
             for i in range(n):
                 for j in range(n):
-                    s += v[i] * v[j] * y[i] * y[j] * self.kern(xs[i], xs[j])
-            return 1 / 2 * s - sum(v)
+                    s += (
+                        candidate[i]
+                        * candidate[j]
+                        * classes[i]
+                        * classes[j]
+                        * self.kernel(observations[i], observations[j])
+                    )
+            return 1 / 2 * s - sum(candidate)
 
-        ly_contraint = LinearConstraint(y, 0, 0)
-        l_bounds = Bounds(0, self.C)
+        ly_contraint = LinearConstraint(classes, 0, 0)
+        l_bounds = Bounds(0, self.regularization)
 
         l_star = minimize(
             to_minimize, np.ones(n), bounds=l_bounds, constraints=[ly_contraint]
         ).x
-        self.l_star = l_star
+        self.optimum = l_star
 
-        b = 0
+        # calculating mean offset of separation plane to points
+        s = 0
         for i in range(n):
             for j in range(n):
-                b += y[i] - y[i] * l_star[i] * self.kern(xs[i], xs[j])
-        self.b = b / n
+                s += classes[i] - classes[i] * self.optimum[i] * self.kernel(
+                    observations[i], observations[j]
+                )
+        self.offset = s / n
 
-    def predict(self, x: ndarray) -> int:
+    def predict(self, observation: ndarray) -> int:
         """
         Get the expected class of an observation
 
         Args:
-            x (Vector): observation
+            observation (Vector): observation
 
         Returns:
             int {1, -1}: expected class
@@ -136,10 +184,12 @@ class SVC:
         -1
         """
         s = sum(
-            self.l_star[n] * self.y[n] * self.kern(self.xs[n], x)
-            for n in range(len(self.y))
+            self.optimum[n]
+            * self.classes[n]
+            * self.kernel(self.observations[n], observation)
+            for n in range(len(self.classes))
         )
-        return 1 if s + self.b >= 0 else -1
+        return 1 if s + self.offset >= 0 else -1
 
 
 if __name__ == "__main__":
