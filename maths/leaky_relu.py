d91eb1c86ad8a9d311d2b319b983931becb079bd
SJTechy <67711756+atomicsorcerer@users.noreply.github.com>
2022-07-19 23:29:30 -0700

    Created leaky_relu.py and implemented the algorithm.

---
+++ maths/leaky_relu.py
@@ -0,0 +1,52 @@
+"""
+This algorithm implements the leaky rectified linear (LReLU).
+
+LReLU is at times used as a substitute to ReLU because it fixes the dying ReLU problem.
+This is done by adding a slight slope to the negative portion of the function.
+The default value for the slope is 0.01.
+The new slope is determined before the network is trained.
+
+Script inspired from its corresponding Wikipedia article
+https://en.wikipedia.org/wiki/Rectifier_(neural_networks)
+"""
+from __future__ import annotations
+
+
+def leaky_relu(vector: float | list[float], slope: float = 0.01) -> float | list[float]:
+    """
+    Implements the leaky rectified linear activation function
+
+    >>> leaky_relu([-5])
+    [-0.05]
+    >>> leaky_relu([-2, 0.8, -0.3])
+    [-0.02, 0.8, -0.003]
+    >>> leaky_relu(-3.0)
+    -0.03
+    >>> leaky_relu(2)
+    Traceback (most recent call last):
+      ...
+    ValueError: leaky_relu() only accepts floats or a list of floats for vector
+    """
+    if isinstance(vector, int):
+        raise ValueError(
+            "leaky_relu() only accepts floats or a list of floats for vector"
+        )
+    if not isinstance(slope, float):
+        raise ValueError("leaky_relu() only accepts a float value for slope")
+
+    if isinstance(vector, float):
+        if vector < 0:
+            return vector * slope
+        return vector
+
+    for index, value in enumerate(vector):
+        if value < 0:
+            vector[index] = value * slope
+
+    return vector
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
