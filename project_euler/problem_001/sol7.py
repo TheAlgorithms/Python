e8d9071fb2df3741c25fba22b174246b1d4179f1
Nidhi <nidhi.roorkee.2003@gmail.com>
2022-07-30 13:57:45 +0530

    sol7.py modified

--- project_euler/problem_001/sol7.py
+++ project_euler/problem_001/sol7.py
@@ -1,7 +1,7 @@
 """
 Project Euler Problem 1: https://projecteuler.net/problem=1
 
-Multiples of 3 and 5
+To find the Multiples of 3 and 5:
 
 If we list all the natural numbers below 10 that are multiples of 3 or 5,
 we get 3, 5, 6 and 9. The sum of these multiples is 23.
