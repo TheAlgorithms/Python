d4a9574a7a939192a43aac5c4ef2d05f7fa016b1
zhexuanl <63616187+zhexuanl@users.noreply.github.com>
2022-08-04 23:25:31 +0800

    Add descriptive name for parameters  x and y

--- digital_image_processing/filters/local_binary_pattern.py
+++ digital_image_processing/filters/local_binary_pattern.py
@@ -2,15 +2,15 @@ import cv2
 import numpy as np
 
 
-def get_neighbors_pixel(image: np.ndarray, x: int, y: int, center: int) -> int:
+def get_neighbors_pixel(image: np.ndarray, x_coordinate: int, y_coordinate: int, center: int) -> int:
     """
     Comparing local neighborhood pixel value with threshold value of centre pixel.
     Exception is required when neighborhood value of a center pixel value is null.
     i.e. values present at boundaries.
 
     :param image: The image we're working with
-    :param x: x-coordinate of the center pixel
-    :param y: The y coordinate of the pixel
+    :param x_coordinate: x-coordinate of the  pixel
+    :param y_coordinate: The y coordinate of the pixel
     :param center: center pixel value
     :return: The value of the pixel is being returned.
     """
@@ -18,7 +18,7 @@ def get_neighbors_pixel(image: np.ndarray, x: int, y: int, center: int) -> int:
     value = 0
 
     try:
-        if image[x][y] >= center:
+        if image[x_coordinate][y_coordinate] >= center:
             value = 1
     except Exception:
         pass
@@ -26,32 +26,32 @@ def get_neighbors_pixel(image: np.ndarray, x: int, y: int, center: int) -> int:
     return value
 
 
-def local_binary_value(image: np.ndarray, x: int, y: int) -> int:
+def local_binary_value(image: np.ndarray, x_coordinate: int, y_coordinate: int) -> int:
     """
     It takes an image, an x and y coordinate, and returns the
     decimal value of the local binary patternof the pixel
     at that coordinate
 
     :param image: the image to be processed
-    :param x: x coordinate of the pixel
-    :param y: the y coordinate of the pixel
+    :param x_coordinate: x coordinate of the pixel
+    :param y_coordinate: the y coordinate of the pixel
     :return: The decimal value of the binary value of the pixels
     around the center pixel.
     """
     binary_value = []
-    center = image[x][y]
+    center = image[x_coordinate][y_coordinate]
     powers = [1, 2, 4, 8, 16, 32, 64, 128]
     decimal_val = 0
 
     # Starting from top right,assigning value to pixels clockwise
-    binary_value.append(get_neighbors_pixel(image, x - 1, y + 1, center))
-    binary_value.append(get_neighbors_pixel(image, x, y + 1, center))
-    binary_value.append(get_neighbors_pixel(image, x - 1, y, center))
-    binary_value.append(get_neighbors_pixel(image, x + 1, y + 1, center))
-    binary_value.append(get_neighbors_pixel(image, x + 1, y, center))
-    binary_value.append(get_neighbors_pixel(image, x + 1, y - 1, center))
-    binary_value.append(get_neighbors_pixel(image, x, y - 1, center))
-    binary_value.append(get_neighbors_pixel(image, x - 1, y - 1, center))
+    binary_value.append(get_neighbors_pixel(image, x_coordinate - 1, y_coordinate + 1, center))
+    binary_value.append(get_neighbors_pixel(image, x_coordinate, y_coordinate + 1, center))
+    binary_value.append(get_neighbors_pixel(image, x_coordinate - 1, y_coordinate, center))
+    binary_value.append(get_neighbors_pixel(image, x_coordinate + 1, y_coordinate + 1, center))
+    binary_value.append(get_neighbors_pixel(image, x_coordinate + 1, y_coordinate, center))
+    binary_value.append(get_neighbors_pixel(image, x_coordinate + 1, y_coordinate - 1, center))
+    binary_value.append(get_neighbors_pixel(image, x_coordinate, y_coordinate - 1, center))
+    binary_value.append(get_neighbors_pixel(image, x_coordinate - 1, y_coordinate - 1, center))
 
     # Converting the binary value to decimal.
     for i in range(len(binary_value)):
