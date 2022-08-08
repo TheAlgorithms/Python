2e413e7eb511419686495ee9cc76fd10e2f0833a
QuekYH <87815473+QuekYH@users.noreply.github.com>
2022-08-08 22:06:35 +0800

    Update octal_to_binary.py
    Added doctest and type hint

--- conversions/octal_to_binary.py
+++ conversions/octal_to_binary.py
@@ -1,17 +1,35 @@
-# Converting Octal number to Binary 
+# Converting Octal number to Binary
 
 # defining a function that returns
 # binary equivalent of the number
-def Octal_To_Binary(oct_num):
-     
-    binary = "" # initialising bin as String
-    
+def octal_to_binary(oct_num: int) -> int:
+
+    """
+    >>> octal_to_binary(1)
+    '001'
+    >>> octal_to_binary(12)
+    '001010'
+    >>> octal_to_binary(2)
+    '010'
+    >>> octal_to_binary(8)
+    Traceback (most recent call last):
+    ...
+    ValueError: Non-octal value was passed to the function
+    >>> octal_to_binary(19)
+    Traceback (most recent call last):
+    ...
+    ValueError: Non-octal value was passed to the function
+
+    """
+
+    binary = ""  # initialising bin as String
+
     # While loop to extract each digit
     while oct_num != 0:
-         
+
         # extracting each digit
         d = int(oct_num % 10)
-        if d == 0:  
+        if d == 0:
             # concatenation of string using join function
             binary = "".join(["000", binary])
 
@@ -20,7 +38,6 @@ def Octal_To_Binary(oct_num):
             binary = "".join(["001", binary])
 
         elif d == 2:
-             
             # concatenation of string using join function
             binary = "".join(["010", binary])
 
@@ -36,43 +53,43 @@ def Octal_To_Binary(oct_num):
             # concatenation of string using join function
             binary = "".join(["101", binary])
 
-        elif d == 6: 
+        elif d == 6:
             # concatenation of string using join function
-            binary = "".join(["110",binary])
-        
+            binary = "".join(["110", binary])
+
         elif d == 7:
             # concatenation of string using join function
             binary = "".join(["111", binary])
 
-        else:  
+        else:
             # an option for invalid input
-            binary = "Non-octal value was passed to the function"
+            raise ValueError("Non-octal value was passed to the function")
             break
- 
+
         # updating the oct for while loop
         oct_num = int(oct_num / 10)
-         
+
     # returning the string binary that stores
     # binary equivalent of the number
     return binary
 
- 
+
 # Driver Code
 print("Enter the Octal Number")
 
-try: 
+try:
     oct_num = int(input())
 except ValueError:
     print("Please enter an integer.")
     exit()
- 
+
 # value of function stored final_bin
-final_bin = "" + Octal_To_Binary(oct_num)
- 
+final_bin = "" + octal_to_binary(oct_num)
+
 # result is printed
 print("Equivalent Binary Value =", final_bin)
 
 if __name__ == "__main__":
-        from doctest import testmod
+    from doctest import testmod
 
 testmod()
