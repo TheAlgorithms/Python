4fb0acedd9f54a6c650396e9b5194f274f8c9f5f
AmirMohammad Hosseini Nasab <awmirhn@gmail.com>
2022-08-12 10:29:09 +0430

    Change MaxFenwickTree into 0-based indexing

--- data_structures/binary_tree/maximum_fenwick_tree.py
+++ data_structures/binary_tree/maximum_fenwick_tree.py
@@ -1,22 +1,22 @@
-class MaxFenwickTreeOneBasedIndexing:
+class MaxFenwickTree:
     """
-    Maximum Fenwick Tree with One-Based Indexing
+    Maximum Fenwick Tree
 
     More info: https://cp-algorithms.com/data_structures/fenwick.html
     ---------
-    >>> ft = MaxFenwickTreeOneBasedIndexing(5)
+    >>> ft = MaxFenwickTree(5)
     >>> ft.query(0, 5)
     0
     >>> ft.update(2, 20)
     >>> ft.query(0, 5)
     20
-    >>> ft.update(5, 10)
+    >>> ft.update(4, 10)
     >>> ft.query(2, 5)
-    10
+    20
     >>> ft.update(2, 0)
     >>> ft.query(0, 5)
     10
-    >>> ft = MaxFenwickTreeOneBasedIndexing(10000)
+    >>> ft = MaxFenwickTree(10000)
     >>> ft.update(255, 30)
     >>> ft.query(0, 10000)
     30
@@ -24,7 +24,7 @@ class MaxFenwickTreeOneBasedIndexing:
 
     def __init__(self, size: int) -> None:
         """
-        Create empty One-Based indexing Maximum Fenwick Tree with specified size
+        Create empty Maximum Fenwick Tree with specified size
 
         Parameters:
             size: size of Array
@@ -35,6 +35,20 @@ class MaxFenwickTreeOneBasedIndexing:
         self.size = size
         self.arr = [0] * (size + 1)
         self.tree = [0] * (size + 1)
+
+    @staticmethod
+    def get_next(index: int) -> int:
+        """
+        Get next index in O(1)
+        """
+        return index + index & -index
+
+    @staticmethod
+    def get_prev(index: int) -> int:
+        """
+        Get previous index in O(1)
+        """
+        return index - index & -index
 
     def update(self, index: int, value: int) -> None:
         """
@@ -48,29 +62,30 @@ class MaxFenwickTreeOneBasedIndexing:
             None
         """
         self.arr[index] = value
+        index += 1  # 1-indexed
         while index < self.size:
-            self.tree[index] = max(value, self.query(index - index & -index, index))
-            index += index & (-index)
+            self.tree[index] = max(value, self.query(self.get_prev(index), index))
+            index = self.get_next(index)
 
     def query(self, left: int, right: int) -> int:
         """
-        Answer the query of maximum range (l, r] in O(lg^2 N)
+        Answer the query of maximum range [l, r) in O(lg^2 N)
 
         Parameters:
-            left: left index of query range (exclusive)
-            right: right index of query range (inclusive)
+            left: left index of query range (inclusive)
+            right: right index of query range (exclusive)
 
         Returns:
-            Maximum value of range (left, right]
+            Maximum value of range [left, right)
         """
         result = 0
         while left < right:
-            current_left = right - right & -right
+            current_left = self.get_prev(right)
             if left < current_left:
                 result = max(result, self.tree[right])
                 right = current_left
             else:
-                result = max(result, self.arr[right])
+                result = max(result, self.arr[right - 1])  # Because of right is exclusive
                 right -= 1
         return result
 
