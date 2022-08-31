32ed068906dd83a0fb1ef16701bd2fd32ddec37e
Chris O <46587501+ChrisO345@users.noreply.github.com>
2022-09-01 10:21:39 +1200

    added support for fractional logarithms

--- maths/logarithm.py
+++ maths/logarithm.py
@@ -3,46 +3,77 @@ https://en.wikipedia.org/wiki/Logarithm
 """
 
 
-def logarithm(end: int, base: int) -> float:
+def logarithm(end: float, base: float, err: float = 0.000001) -> float:
     """
     Return the logarithm with base base of x
     :param end: the number
     :param base: the base of the exponential
     :return:
-
-    >>> logarithm(32, 2)
-    5
-    >>> logarithm(100, 10)
-    2
-    >>> logarithm(2937846, 3)
-    13.556363215077281
+    
+    >>> logarithm(2, 9)
+    0.31546499999999994
+    >>> logarithm(9, 2)
+    3.1699250000000005
+    >>> logarithm(0.2, 0.9)
+    15.275499999999994
+    >>> logarithm(0.9, 0.2)
+    0.065464
+    >>> logarithm(0.2, 9)
+    -0.7324849999999997
+    >>> logarithm(2, 0.9)
+    -6.578809999999997
+    >>> logarithm(9, 0.2)
+    -1.3652123999999999
+    >>> logarithm(0.9, 2)
+    -0.15200200000000003
     """
+    
     if base < 0 or end < 0:
         raise ValueError('logarithm() takes 2 positive values')
     
+    frac = False
+    
+    accuracy = 1
+    
+    if end < 1 and base < 1:
+        accuracy = 1
+        frac = True
+    elif base < 1:
+        accuracy = -1
+    elif end < 1:
+        accuracy = -1
+        frac = True
+    
+
     if base == 0:
         return 1
     start = 0
     prev = 0
-    accuracy = 1
-    e = 0.000001
     while True:
         start = prev + accuracy
         _curr = base ** start
 
-        if _curr >= end:
-            error = _curr - end
-        else:
-            error = end - _curr
-
-        if error <= e:
+        if abs(_curr - end) <= err:
             return start
-
-        if _curr > end:
-            accuracy /= 10
+        if frac:
+            if _curr < end:
+                accuracy /= 10
+            else:
+                prev = start
         else:
-            prev = start
+            if _curr > end:
+                accuracy /= 10
+            else:
+                prev = start
+    return start
 
 
 if __name__ == "__main__":
-    pass
+    print(logarithm(2, 9))
+    print(logarithm(9, 2))
+    print(logarithm(0.2, 0.9))
+    print(logarithm(0.9, 0.2))
+    print(logarithm(0.2, 9))
+    print(logarithm(2, 0.9))
+    print(logarithm(9, 0.2))
+    print(logarithm(0.9, 2))
