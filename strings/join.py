352d82a31edc05011ba581c6eac5bbb8a1cb5d8e
☙ Sohlstyce ❧ <csuryachethanreddy@gmail.com>
2022-09-04 14:47:13 +0530

    Use builtins instead of for loops to improve speed

--- strings/join.py
+++ strings/join.py
@@ -18,15 +18,11 @@ def join(separator: str, separated: list[str]) -> str:
     ...
     Exception: join() accepts only strings to be joined
     """
-    joined = ""
-    for word_or_phrase in separated:
-        if not isinstance(word_or_phrase, str):
-            raise Exception("join() accepts only strings to be joined")
-        joined += word_or_phrase + separator
-    return joined.strip(separator)
+    try:
+        return separator.join(separated)
+    except TypeError:
+        raise Exception("join() accepts only strings to be joined")
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     from doctest import testmod
-
     testmod()
