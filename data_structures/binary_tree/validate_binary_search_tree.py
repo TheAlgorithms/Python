4ddbd0e98bc2f85c00819a4963ed58f176fe4431
10kartik <kartik@plgworks.com>
2022-09-06 12:15:22 +0530

    Resolved reviews

--- data_structures/binary_tree/validate_binary_search_tree.py
+++ data_structures/binary_tree/validate_binary_search_tree.py
@@ -1,9 +1,3 @@
-from __future__ import annotations
-
-# Storing inorder traversal values into this list
-traversed_node_values = []  # type: list[int]
-
-
 class Node:
     """
     A Node has data variable and pointers to Nodes to its left and right.
@@ -15,38 +9,55 @@ class Node:
         self.right: Node | None = None
 
 
-def display(tree: Node | None) -> None:  # In Order traversal of the tree
+def display(tree: Node | None) -> str:
     """
+    In Order traversal of the tree
+
     >>> root = Node(1)
     >>> root.left = Node(0)
     >>> root.right = Node(2)
-    >>> display(root)
-    0
-    1
-    2
-    >>> display(root.right)
+    >>> print(display(root))
+    012
+    >>> print(display(root.right))
     2
     """
-    if tree:
-        display(tree.left)
-        print(tree.data)
-        display(tree.right)
+    inorder_string: str = ""
+
+    def inner_display(tree: Node | None) -> str:
+        nonlocal inorder_string
+        if tree:
+            inner_display(tree.left)
+            inorder_string += str(tree.data)
+            inner_display(tree.right)
+        return inorder_string
+
+    inner_display(tree)
+    return inorder_string
 
 
-def inorder_traversal(tree: Node | None) -> None:
+def inorder_traversal(tree: Node | None) -> list:
     """
-    Inserts value of nodes into list `traversed_node_values` inorder fashion
-
+    Iterate recursively and insert value of nodes into
+    list `traversed_node_values` inorder fashion
     >>> root = Node(10)
     >>> root.left = Node(0)
     >>> root.right = Node(20)
-    >>> inorder_traversal(root)
+    >>> print(inorder_traversal(root))
+    [0, 10, 20]
     """
-    if tree is None:
-        return
-    inorder_traversal(tree.left)
-    traversed_node_values.append(tree.data)
-    inorder_traversal(tree.right)
+    # Storing inorder traversal values into this list
+    traversed_node_values: list[int] = []
+
+    def inner_inorder_traversal(tree: Node | None) -> None:
+        if tree is None:
+            return
+        nonlocal traversed_node_values
+        inner_inorder_traversal(tree.left)
+        traversed_node_values.append(tree.data)
+        inner_inorder_traversal(tree.right)
+
+    inner_inorder_traversal(tree)
+    return traversed_node_values
 
 
 def is_valid_bst(tree: Node) -> bool:
@@ -56,7 +67,6 @@ def is_valid_bst(tree: Node) -> bool:
     >>> tree = Node(20)
     >>> tree.left = Node(10)
     >>> tree.right = Node(30)
-    >>> traversed_node_values.clear()
     >>> is_valid_bst(tree)
     True
     >>> tree = Node(20)
@@ -70,42 +80,38 @@ def is_valid_bst(tree: Node) -> bool:
     >>> is_valid_bst(tree)
     True
     """
-    traversed_node_values.clear()
     if tree is None:
         return True
-    inorder_traversal(tree)
+    traversed_node_values = inorder_traversal(tree)
 
     """
-    Now, if `traversed_node_values` is sorted and doesn't contain any duplicate values
+    Now, if `traversed_node_values` is sorted and
+    doesn't contain any duplicate values,
     it means the given tree is valid binary search tree
     """
-    for i in range(len(traversed_node_values) - 1):
-        if traversed_node_values[i] >= traversed_node_values[i + 1]:
-            return False
-    return True
+    return not any(
+        (value >= traversed_node_values[index + 1])
+        for index, value in enumerate(traversed_node_values[:-1])
+    )
 
 
 def main() -> None:
     """
     Main function for testing.
-
     >>> tree = Node(20)
     >>> tree.left = Node(10)
     >>> tree.right = Node(30)
-    >>> display(tree)
-    10
-    20
-    30
+    >>> print(display(tree))
+    102030
     >>> is_valid_bst(tree)
     True
 
     >>> tree = Node(10)
     >>> tree.left = Node(30)
     >>> tree.right = Node(20)
-    >>> display(tree)
-    30
-    10
-    20
+    >>> print(display(tree))
+    301020
+
     >>> is_valid_bst(tree)
     False
     """
@@ -114,7 +120,7 @@ def main() -> None:
     tree.right = Node(3)
 
     print("Tree is: ")
-    display(tree)
+    print(display(tree))
     print("Tree is valid binary search tree: ", is_valid_bst(tree))
 
 
