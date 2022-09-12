3f75428c042f615d909b9db7277deeafebb25a55
ajinkyac03 <ajinkyachikhale321@gmail.com>
2022-09-12 11:33:25 +0530

    Updated parameter name

--- dynamic_programming/tribonacci.py
+++ dynamic_programming/tribonacci.py
@@ -1,19 +1,19 @@
 # Tribonacci sequence using Dynamic Programming
 
 
-def tribonacci(n: int) -> list:
+def tribonacci(num: int) -> list:
     """
-    Given a number n, return first n Tribonacci Numbers.
+    Given a number, return first n Tribonacci Numbers.
     >>> tribonacci(5)
     [0, 0, 1, 1, 2]
     >>> tribonacci(8)
     [0, 0, 1, 1, 2, 4, 7, 13]
     """
-    dp = [0 for i in range(n)]
+    dp = [0 for i in range(num)]
     dp[0] = dp[1] = 0
     dp[2] = 1
 
-    for i in range(3, n):
+    for i in range(3, num):
         dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
 
     return dp
