bc477e628c28a5e2556f22fe4ac5e30f811efcb5
yznnck <yannickbrenning2@gmail.com>
2022-09-14 14:03:58 +0200

    Add matrix dimension check

--- matrix/inverse_of_matrix.py
+++ matrix/inverse_of_matrix.py
@@ -28,13 +28,19 @@ def inverse_of_matrix(matrix: list[list[float]]) -> list[list[float]]:
     """
 
     D = Decimal  # An abbreviation for conciseness
+
+    if len(matrix) != 2 or len(matrix[0]) != 2 or len(matrix[1]) != 2:
+        raise ValueError("Please provide a matrix of size 2x2.")
+
     # Calculate the determinant of the matrix
     determinant = D(matrix[0][0]) * D(matrix[1][1]) - D(matrix[1][0]) * D(matrix[0][1])
     if determinant == 0:
         raise ValueError("This matrix has no inverse.")
+
     # Creates a copy of the matrix with swapped positions of the elements
     swapped_matrix = [[0.0, 0.0], [0.0, 0.0]]
     swapped_matrix[0][0], swapped_matrix[1][1] = matrix[1][1], matrix[0][0]
     swapped_matrix[1][0], swapped_matrix[0][1] = -matrix[1][0], -matrix[0][1]
+
     # Calculate the inverse of the matrix
     return [[float(D(n) / determinant) or 0.0 for n in row] for row in swapped_matrix]
