e7547031c9eda68db9f8917fca85436d4ebf833c
Google Jr <andilembele020@gmail.com>
2022-09-25 08:48:45 +0200

    Removed comment

--- data_structures/trie/trie.py
+++ data_structures/trie/trie.py
@@ -11,7 +11,7 @@ class TrieNode:
         self.nodes: dict[str, TrieNode] = dict()  # Mapping from char to TrieNode
         self.is_leaf = False
 
-    def insert_many(self, words: list([str])) -> None: # wondering if this is the best way to do this
+    def insert_many(self, words: list([str])) -> None:
         """
         Inserts a list of words into the Trie
         :param words: list of string words
