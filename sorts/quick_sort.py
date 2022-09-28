b2e296b9e72e2088b2c79c14f37dc134d2409224
Ashutosh2613 <ashutoshvp1@gmail.com>
2022-09-28 13:12:04 +0530

    refactor: choose the pivot at a random point then choosing the last element every time.

--- sorts/quick_sort.py
+++ sorts/quick_sort.py
@@ -8,6 +8,7 @@ For manual testing run:
 python3 quick_sort.py
 """
 from __future__ import annotations
+import random
 
 
 def quick_sort(collection: list) -> list:
@@ -26,13 +27,12 @@ def quick_sort(collection: list) -> list:
     """
     if len(collection) < 2:
         return collection
-    pivot = collection.pop()  # Use the last element as the first pivot
-    greater: list[int] = []  # All elements greater than pivot
-    lesser: list[int] = []  # All elements less than or equal to pivot
+    pivot = random.choice(collection)  
+    greater: list[int] = []
+    lesser: list[int] = []
     for element in collection:
         (greater if element > pivot else lesser).append(element)
-    return quick_sort(lesser) + [pivot] + quick_sort(greater)
-
+    return quick_sort(lesser) + quick_sort(greater)
 
 if __name__ == "__main__":
     user_input = input("Enter numbers separated by a comma:\n").strip()
