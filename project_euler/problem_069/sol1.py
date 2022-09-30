89fb25d8cd8d86f0c95ed565bc993d598ddf7281
chashtag <matthew.cundari@gmail.com>
2022-09-30 15:01:23 -0400

    Fix up formatting

--- project_euler/problem_069/sol1.py
+++ project_euler/problem_069/sol1.py
@@ -7,16 +7,16 @@ is used to determine the number of numbers less than n which are relatively prim
 For example, as 1, 2, 4, 5, 7, and 8,
 are all less than nine and relatively prime to nine, φ(9)=6.
 
-n	Relatively Prime	φ(n)	n/φ(n)
-2	1	                1	    2
-3	1,2	                2	    1.5
-4	1,3	                2	    2
-5	1,2,3,4	            4	    1.25
-6	1,5		            2	    3
-7	1,2,3,4,5,6	        6	    1.1666...
-8	1,3,5,7		        4	    2
-9	1,2,4,5,7,8	        6	    1.5
-10	1,3,7,9	            4	    2.5
+n   Relatively Prime    φ(n)    n/φ(n)
+2   1                   1       2
+3   1,2                 2       1.5
+4   1,3                 2       2
+5   1,2,3,4             4       1.25
+6   1,5                 2       3
+7   1,2,3,4,5,6         6       1.1666...
+8   1,3,5,7             4       2
+9   1,2,4,5,7,8         6       1.5
+10  1,3,7,9             4       2.5
 
 It can be seen that n=6 produces a maximum n/φ(n) for n ≤ 10.
 
