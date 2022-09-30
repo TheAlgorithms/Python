f07c11a719470ba327e0d81255a66e8ab2d09c34
rohanr18 <114707091+rohanr18@users.noreply.github.com>
2022-09-30 22:32:24 +0530

    Update count_islands_in_matrix.py

--- matrix/count_islands_in_matrix.py
+++ matrix/count_islands_in_matrix.py
@@ -4,12 +4,12 @@
 
 
 class matrix:  # Public class to implement a graph
-    def __init__(self, row: int, col: int, graph: list[int]) -> None:
+    def __init__(self, row: int, col: int, graph: list[list[bool]]) -> None:
         self.ROW = row
         self.COL = col
         self.graph = graph
 
-    def is_safe(self, i: int, j: int, visited: list[list[int]]) -> bool:
+    def is_safe(self, i: int, j: int, visited: list[list[bool]]) -> bool:
         return (
             0 <= i < self.ROW
             and 0 <= j < self.COL
@@ -17,7 +17,7 @@ class matrix:  # Public class to implement a graph
             and self.graph[i][j]
         )
 
-    def diffs(self, i: int, j: int, visited: list[list[int]]) -> None:
+    def diffs(self, i: int, j: int, visited: list[list[bool]]) -> None:
         # Checking all 8 elements surrounding nth element
         rowNbr = [-1, -1, -1, 0, 0, 1, 1, 1]  # Coordinate order
         colNbr = [-1, 0, 1, -1, 1, -1, 0, 1]
