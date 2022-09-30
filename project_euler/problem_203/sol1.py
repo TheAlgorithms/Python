8c088aeb5bf7325e4ded703baf0614bc78689b7f
chashtag <matthew.cundari@gmail.com>
2022-09-30 15:02:54 -0400

    Fixed up formatting #4

--- project_euler/problem_203/sol1.py
+++ project_euler/problem_203/sol1.py
@@ -5,12 +5,12 @@ The binomial coefficients (n k) can be arranged in triangular form, Pascal's
 triangle, like this:
                             1
                         1       1
-                    1		2       1
-                1		3		3       1
-            1		4		6		4		1
-        1		5		10		10		5		1
-    1		6		15		20		15		6		1
-1		7		21		35		35		21		7		1
+                    1       2       1
+                1       3       3       1
+            1       4       6       4       1
+        1       5       10      10      5       1
+    1       6       15      20      15      6       1
+1       7       21      35      35      21      7      1
                         .........
 
 It can be seen that the first eight rows of Pascal's triangle contain twelve
