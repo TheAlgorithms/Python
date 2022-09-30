89568a399de1f035c20f8452043d2cc937b6a366
lostybtw <58177990+lostybtw@users.noreply.github.com>
2022-09-30 15:47:49 +0000

    fizzbuzz

---
+++ dynamic_programming/fizz_buzz.py
@@ -0,0 +1,11 @@
+#https://en.wikipedia.org/wiki/Fizz_buzz#Programming
+number = 1
+out = ""
+iterations = 100
+
+while number <= int(iterations):
+  if number % 3 == 0: out += "Fizz"
+  if number % 5 == 0: out +="Buzz"
+  if out == "": out = number
+  print(out)
+  number += 1
