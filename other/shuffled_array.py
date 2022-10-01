2b13f8ca934d7b8c84742e35b44bd5cba7e51f65
manuprakash <manu.prakash2020@vitbhopal.ac.in>
2022-10-02 02:34:59 +0530

    added shuffled_array.py documentation better

--- other/shuffled_array.py
+++ other/shuffled_array.py
@@ -6,58 +6,64 @@ To Understand more of random number generation follow https://en.wikipedia.org/w
 
 import random
 import time
-class Solution:
-    def __init__(self,array:list)->None :
-        """
-        >>> Solution().__init__(array=[1,2,3])
-        Traceback (most recent call last):
-                   ....
-        TypeError: __init__() missing 1 required positional argument: 'array'
-        """
-        self.arr = array    
-        self.seed = int(str(time.time())[-1:-5:-1])                  #generating a 4 digit number randomly using decimals of current time
 
-    def prng(self,num:int)->int:                           #pseudo random number generator
-        """
-    >>> Solution([2,3,4,56]).prng(1)
-    0
+
+class Solution:
     """
-        self.seed*=self.seed
+    >>> Solution().__init__(array=[1,2,3])
+    Traceback (most recent call last):
+               ....
+    TypeError: __init__() missing 1 required positional argument: 'array'
+    """
+
+    def __init__(self, array: list) -> None:
+        self.arr = array
+        self.seed = int(str(time.time())[-1:-5:-1])
+
+    # generating a 4 digit number randomly by taking the last four numbers of the system generated time which keeps on changing and therefore making it a pseudo random number but this function generate number less than the size of array
+    def prng(self, num: int) -> int:  # pseudo random number generator
+        """
+        >>> Solution([2,3,4,56]).prng(1)
+        0
+        """
+        self.seed *= self.seed
         any = str(self.seed)
-        if any != '0' :
+        if any != "0":
             self.seed = int(any[2:6])
         else:
             self.seed = int(str(time.time())[-1:-5:-1])
             any = str(self.seed)
-        if int(any[4])<num:
-            return int(any[4])
+        if int(any[3]) < num:
+            return int(any[3])
         return self.prng(num)
 
-    def reset(self)-> None:
+    def reset(self) -> list:
+        # it will return the original given array
         """
-    >>> Solution([2,3,4,56]).reset()
-    [2, 3, 4, 56]
-    """
-
-        print(self.arr)
-
-    
-    def shuffle(self)->list:
+        >>> Solution([2,3,4,56]).reset()
+        [2, 3, 4, 56]
         """
-    >>> Solution([2]).shuffle()
-    [2]
-    """
+        return self.arr
+
+    def shuffle(self) -> list:
+        # generated a pseudo number for each traversal and swapped the traversing value with that number
+        """
+        >>> Solution([2]).shuffle()
+        [2]
+        """
         temp = self.arr.copy()
-        for i in range(1,len(self.arr)):
+        for i in range(1, len(self.arr)):
             a = self.prng(len(self.arr))
-            temp[a],temp[i] = temp[i],temp[a]
+            temp[a], temp[i] = temp[i], temp[a]
         return temp
 
-solclass = Solution([18,2,3,4,5,7,8,10,21])
+
+solclass = Solution([18, 2, 3, 4, 5, 7, 8, 10, 21])
+
 shuffled_arr = solclass.shuffle()
 print(shuffled_arr)
-solclass.reset()
+
+print(solclass.reset())
+
 # import doctest
 # doctest.testmod()
-
-
