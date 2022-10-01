0d63ab0b0b5a22a71e540facdbbb71db85f2dd45
rayrifaira <100701066+rayrifaira@users.noreply.github.com>
2022-10-01 19:00:21 +0700

    AT send SMS

---
+++ other/at_command_send_sms.py
@@ -0,0 +1,33 @@
+import serial
+import argparse
+"""
+Send SMS with command control port
+"""
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--port', default="/dev/ttyUSB0", help="Command Control Port. Windows: COM | Linux: /dev/ttyUSB0")
+parser.add_argument('--baudrate', default=115200, type=int, help="Baudrate")
+parser.add_argument('--to', type=str, default="0811290110921", help="Nomor tujuan. contoh: 082172187218")
+parser.add_argument('--message', type=str, default="test", help="message content")
+
+args, unknown = parser.parse_known_args()
+
+def send_sms(port, baudrate, recipient, message):
+    phone = serial.Serial(port,  baudrate, timeout=0)
+    try:
+        phone.write(b'AT\r\n')
+        phone.write(b'AT+CMGF=1\r\n')
+        phone.write(b'AT+CSCS="GSM"\r\n')
+        phone.write(b'AT+CMGS="' + recipient.encode() + b'"\r\n')
+        phone.write(message.encode() + b'\r\n')
+        time.sleep(0.5)
+        phone.write(bytes([26]))
+        time.sleep(0.5)
+        print("Message Sent")
+    except:
+        print("Failed sending message")
+    finally:
+        phone.close()
+
+send_sms(args.port, args.baudrate, args.to, args.message)
