383b11f08d83400538290e55da82aa1e068c582b
lostybtw <58177990+lostybtw@users.noreply.github.com>
2022-10-01 12:46:40 +0000

    Update fizz_buzz.py

--- dynamic_programming/fizz_buzz.py
+++ dynamic_programming/fizz_buzz.py
@@ -1,7 +1,7 @@
 # https://en.wikipedia.org/wiki/Fizz_buzz#Programming
 
 
-def fizz_buzz(number: int, out: str, iterations: int) -> str:
+def fizz_buzz(number: int, iterations: int) -> str:
     """
     Plays FizzBuzz.
     Prints Fizz if number is a multiple of 3.
@@ -9,18 +9,18 @@ def fizz_buzz(number: int, out: str, iterations: int) -> str:
     Prints FizzBuzz if its a multiple of both 3 and 5 or 15.
     Else Prints The Number Itself.
 
-    >>> fizz_buzz(1,"",7)
+    >>> fizz_buzz(1,7)
     '1 2 Fizz 4 Buzz Fizz 7 '
-    >>> fizz_buzz(1,"",'a')
+    >>> fizz_buzz(1,'a')
     Traceback (most recent call last):
       ...
     ValueError: iterations must be defined as integers
-    >>> fizz_buzz(1,"",0)
+    >>> fizz_buzz(1,0)
     Traceback (most recent call last):
       ...
     ValueError: iterations must be done more
                              than 0 times to play FizzBuzz
-    >>> fizz_buzz('a',"",5)
+    >>> fizz_buzz('a',5)
     Traceback (most recent call last):
       ...
     ValueError: starting number must be
@@ -40,7 +40,7 @@ def fizz_buzz(number: int, out: str, iterations: int) -> str:
             """starting number must be
                          and integer and be more than 0"""
         )
-
+    out = ""
     while number <= iterations:
         if number % 3 == 0:
             out += "Fizz"
