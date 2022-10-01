5f1bc551ee56244908e10bdaf904f0c4f56e4547
lostybtw <58177990+lostybtw@users.noreply.github.com>
2022-10-01 04:30:41 +0000

    Update dynamic_programming/fizz_buzz.py
    Co-authored-by: Caeden <caedenperelliharris@gmail.com>

--- dynamic_programming/fizz_buzz.py
+++ dynamic_programming/fizz_buzz.py
@@ -4,7 +4,8 @@ out = ""
 iterations = 100
 
 while number <= iterations:
-  if number % 3 == 0: out += "Fizz"
+  if number % 3 == 0:
+      out += "Fizz"
   if number % 5 == 0: out +="Buzz"
   if out == "": out = number
   print(out)
