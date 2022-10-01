181d62e75d76fb8b9be56193041bf5d4580254cf
rudrakshkarpe <rudraksh.karpe@gmail.com>
2022-10-01 12:12:48 +0530

    update coin_change.py

--- dynamic_programming/coin_change.py
+++ dynamic_programming/coin_change.py
@@ -16,8 +16,14 @@ import sys
 # to get a total of `target` from set `S`
 def find_minimum_coins(S, target) -> int:
     '''
-    S = { 1, 3, 5, 7 }
-    target = 15
+    >>> find_minimum_coins([1, 2, 3], 4)
+    2
+    >>> find_minimum_coins([1, 2, 3], 7)
+    3
+    >>> find_minimum_coins([2, 5, 3, 6], 10)
+    2
+    >>> find_minimum_coins([10], 99)
+    -1
     '''
 
     # if the total is 0, no coins are needed
@@ -26,20 +32,20 @@ def find_minimum_coins(S, target) -> int:
 
     # return infinity if total becomes negative
     if target < 0:
-        return sys.maximum_size
+        return sys.maxsize
 
     # initialize the minimum number of coins needed to infinity
-    coins = sys.maximum_size
+    coins = sys.maxsize
 
     # do for each coin
     for c in S:
 
         # recur to see if total can be reached by including current coin `c`
-        result = Find_minimum_coins(S, target - c)
+        result = find_minimum_coins(S, target - c)
 
         # update the minimum number of coins needed if choosing the current
         # coin resulted in a solution
-        if result != sys.maximum_size:
+        if result != sys.maxsize:
             coins = min(coins, result + 1)
 
     # return the minimum number of coins needed
@@ -53,7 +59,7 @@ if __name__ == '__main__':
     # total change required
     target = 18
 
-    coins = Find_minimum_coins(S, target)
-    if coins != sys.maximum_size:
+    coins = find_minimum_coins(S, target)
+    if coins != sys.maxsize:
         print('The minimum number of coins required to get the desired change is',
               coins)
