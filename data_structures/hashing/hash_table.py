f42bc1d6a631ca9f7475b5df89f801b5c7854a41
Si Lam <silam@hotmail.com>
2022-10-01 17:39:05 -0500

    Enter the logic for hash table

--- data_structures/hashing/hash_table.py
+++ data_structures/hashing/hash_table.py
@@ -1,3 +1,17 @@
+"""
+Hash Table is a data structure which stores data in an associative manner. 
+In a hash table, data is stored in an array format, where each data value has its own unique index value. 
+Access of data becomes very fast if we know the index of the desired data.
+
+Thus, it becomes a data structure in which insertion and search operations are very fast 
+irrespective of the size of the data. 
+Hash Table uses an array as a storage medium and uses hash technique to generate an index 
+where an element is to be inserted or is to be located from.
+
+Hashing is a technique to convert a range of key values into a range of indexes of an array. 
+We're going to use modulo operator to get a range of key values. 
+
+"""
 #!/usr/bin/env python3
 from .number_theory.prime_numbers import next_prime
 
