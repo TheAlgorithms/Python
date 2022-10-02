154c97d7f1398f95d828a940dd3d28c1447c2f0a
Harsh Wasnik <harshwasnik160602@gmail.com>
2022-10-02 12:08:33 +0530

    Added doctests and typehints

--- linear_algebra/src/gauss_seidel.py
+++ linear_algebra/src/gauss_seidel.py
@@ -1,6 +1,6 @@
 # This is python code to implement Gauss-Seidel method for solving linear equations
 
-# Gauss Seidel Method 
+# Gauss Seidel Method (https://en.wikipedia.org/wiki/Gauss%E2%80%93Seidel_method)
 # initially assumes the solution to be zero
 # Then in each iteration improves the solution by using the previous solution
 
@@ -8,43 +8,74 @@
 # Defining our function as seidel which takes 3 arguments
 # as A matrix, Solution and B matrix
 
-def gauss_seidel(a, x ,b):
-	n = len(a)  # number of variables				
-	for i in range(0, n):		
-	    # Update each variable
-		d = b[i]				
-		for j in range(0, n):	
-			if(i != j):
-				d-=a[i][j] * x[j]	
-		x[i] = d / a[i][i]	
-	return x	
-	
-if __name__ == "__main__":
-    n = int(input("Enter the number of variables: "))	# variables						
-    a = []		# Coefficient Matrix					
-    b = []		# Constant Matrix
-    x = []      # Solution Matrix
-    # initial solution depending on n(here n=3)	
-    print("Enter the coefficient Matrix (nxn)\n")	
-    for i in range(0, n):
-        a.append([])
-        for j in range(0, n):
-            a[i].append(int(input("Enter a["+str(i)+"]["+ str(j)+"]: "))) 
-        x.append(0)
-    for i in range(0, n):
-        b.append(int(input("Enter b["+str(i)+"]: ")))           			
-    # x = [0, 0, 0]						  # Solution Assumption
-    # a = [[4, 1, 2],[3, 5, 1],[1, 1, 3]]   
-    # b = [4,7,3]                           
-    m=1000
-    m = int(input("Enter the number of iterations: "))	# iterations
-    print(x)
-    #loop run for m times depending on m the error value
-    for i in range(0, m):			
-        x = gauss_seidel(a, x, b)
-        #print each time the updated solution
-        print(x)					
 
+def update_solution(a: list[list[float]], x: list[float], b: list[float]) -> list[float]:
+    """
+        Function to update the solution
+        a: coefficient matrix
+        x: solution vector
+        b: constant vector
+
+        Example:
+        >>> a = [[10, -1, 2, 0], [-1, 11, -1, 3], [2, -1, 10, -1], [0, 3, -1, 8]]
+        >>> x = [0, 0, 0, 0]
+        >>> b = [6, 25, -11, 15]
+        >>> update_solution(a, x, b)
+        [0.6, 2.3272727272727276, -0.9872727272727271, 0.8788636363636363]
+    """
+    n = len(a)  # number of variables
+    for i in range(0, n):
+        # Update each variable
+        d = b[i]
+        for j in range(0, n):
+            if(i != j):
+                d -= a[i][j] * x[j]
+        x[i] = d / a[i][i]
+    return x
+
+
+def gauss_seidel(a: list[list[float]], x: list[float], b: list[float], m: int) -> None:
+    """
+        Function to perform Gauss Seidel Iteration
+        a: coefficient matrix
+        x: solution vector
+        b: constant vector
+        m: max iterations
+
+        Example:
+        >>> a = [[10, -1, 2, 0], [-1, 11, -1, 3], [2, -1, 10, -1], [0, 3, -1, 8]]
+        >>> x = [0, 0, 0, 0]
+        >>> b = [6, 25, -11, 15]
+        >>> gauss_seidel(a, x, b, 100)
+        [1.0, 2.0, -1.0, 1.0]
+    """
+    for i in range(0, m):
+        x = update_solution(a, x, b)
+        # print("Iteration {}: {}".format(i, x))
+    print(x)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+# if __name__ == "__main__":
+#     n = int(input("Enter the number of variables: "))	# variables
+#     a = []		# Coefficient Matrix
+#     b = []		# Constant Matrix
+#     x = []      # Solution Matrix
+#     m=1000
+#     # initial solution depending on n(here n=3)
+#     print("Enter the coefficient Matrix (nxn)\n")
+#     for i in range(0, n):
+#         a.append([])
+#         for j in range(0, n):
+#             a[i].append(int(input("Enter a["+str(i)+"]["+ str(j)+"]: ")))
+#         x.append(0)
+#     for i in range(0, n):
+#         b.append(int(input("Enter b["+str(i)+"]: ")))
+#     m = int(input("Enter the number of iterations: "))	# iterations
+#     print("Initial Guess : {}".format(x))
+#     gauss_seidel(a, x, b, m)
 
 # ------------ Sample Input ------------
 # 4x + y + 2z = 4
