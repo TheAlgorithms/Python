54be7d90504ae15fc259a00b5a5df443f8fb9160
Harsh Wasnik <harshwasnik160602@gmail.com>
2022-10-02 10:56:32 +0530

    renamed function to be more meaningful and removed unnecessary comments

--- linear_algebra/src/gauss_seidel.py
+++ linear_algebra/src/gauss_seidel.py
@@ -8,21 +8,15 @@
 # Defining our function as seidel which takes 3 arguments
 # as A matrix, Solution and B matrix
 
-def seidel(a, x ,b):
-	#Finding length of a(3)	
-	n = len(a)				
-	# for loop for 3 times as to calculate x, y , z
-	for j in range(0, n):		
-		# temp variable d to store b[j]
-		d = b[j]				
-		
-		# to calculate respective xi, yi, zi
-		for i in range(0, n):	
-			if(j != i):
-				d-=a[j][i] * x[i]
-		# updating the value of our solution		
-		x[j] = d / a[j][j]
-	# returning our updated solution		
+def gauss_seidel(a, x ,b):
+	n = len(a)  # number of variables				
+	for i in range(0, n):		
+	    # Update each variable
+		d = b[i]				
+		for j in range(0, n):	
+			if(i != j):
+				d-=a[i][j] * x[j]	
+		x[i] = d / a[i][i]	
 	return x	
 	
 if __name__ == "__main__":
@@ -47,7 +41,7 @@ if __name__ == "__main__":
     print(x)
     #loop run for m times depending on m the error value
     for i in range(0, m):			
-        x = seidel(a, x, b)
+        x = gauss_seidel(a, x, b)
         #print each time the updated solution
         print(x)					
 
