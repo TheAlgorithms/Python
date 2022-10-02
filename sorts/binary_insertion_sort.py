2c90877109789df7caee4ed053a52380c48febcd
jaygala <jaygala260@gmail.com>
2022-10-02 18:54:43 +0530

    fixes: #6537 binary insertion sort

---
+++ sorts/binary_insertion_sort.py
@@ -0,0 +1,43 @@
+def binary_search(arr, val, start, end):
+
+    """
+    Algorithm for Binary Search
+    """
+
+    if start == end:
+        if arr[start] > val:
+            return start
+        else:
+            return start + 1
+
+    if start > end:
+        return start
+
+    mid = (start + end) // 2
+    if arr[mid] < val:
+        return binary_search(arr, val, mid + 1, end)
+    elif arr[mid] > val:
+        return binary_search(arr, val, start, mid - 1)
+    else:
+        return mid
+
+
+def insertion_sort(arr):
+
+    """
+    Algorithm for insertion sort
+    >>> insertion_sort([37, 23, 0, 31, 22, 17, 12, 72, 31, 46, 100, 88, 54])
+    [0, 12, 17, 22, 23, 31, 31, 37, 46, 54, 72, 88, 100]
+    """
+
+    for i in range(1, len(arr)):
+        val = arr[i]
+        j = binary_search(arr, val, 0, i - 1)
+        arr = arr[:j] + [val] + arr[j:i] + arr[i + 1 :]
+    return arr
+
+
+if __name__ == "__main__":
+    n = int(input("Enter number of elements : ").strip())
+    array = list(map(int, input("\nEnter the numbers : ").strip().split()))[:n]
+    print(insertion_sort(array))
