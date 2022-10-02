09e5a79d7ca04762cd0aeff2c74169a79920299f
Prerit Das <preritdas@gmail.com>
2022-10-02 15:13:58 -0400

    Remove unused variables.

--- maths/fermat_last_theorem.py
+++ maths/fermat_last_theorem.py
@@ -1,8 +1,9 @@
 # Proving Fermat's last theorem
 # Wikipedia reference: https://en.wikipedia.org/wiki/Fermat%27s_Last_Theorem
 
-import math
 import random
+import math
+
 
 # --- Parameters ----
 HIGHEST_N = 1000
@@ -13,7 +14,7 @@ def test_vals(values: list[int]) -> list[int]:
     Tests the theorem on pre-generated inputted values.
     """
     a, b, c, n = values
-
+    
     if a**n + b**n == c**n:
         return values
 
@@ -22,22 +23,22 @@ def test_vals(values: list[int]) -> list[int]:
 
 def test_values_against_theorem(highest_n: int = HIGHEST_N) -> bool:
     """
-    Tries all possible combinations of a, b, c, and n. Returns
+    Tries all possible combinations of a, b, c, and n. Returns 
     >>> test_values_against_theorem(10)
     False
     >>> test_values_against_theorem(20)
     False
     """
     tried_values: list[list[int]] = []
-
-    for _ in range(max_tries := math.comb(highest_n, 4)):
+    
+    for _ in range(math.comb(highest_n, 4)):
         while (
             values := [random.randint(3, highest_n) for _ in range(4)]
         ) in tried_values:
             pass
 
         # Run the test
-        if solution := test_vals(values):
+        if test_vals(values):
             return True
 
         tried_values.append(values)
