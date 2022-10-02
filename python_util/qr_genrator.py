975e5859eefa4d247b8b74b81c22cd13557ede23
Shashank Bhardwaj <86946068+shashank1623@users.noreply.github.com>
2022-10-02 23:21:34 +0530

    Update qr_genrator.py

---
+++ python_util/qr_genrator.py
@@ -0,0 +1,10 @@
+##qr code using python
+import pyqrcode
+import png
+link = "https://https://github.com/shashank1623"
+qr_code = pyqrcode.create(link)
+qr_code.png("github1.png",scale = 5)
+
+##before running this run this cmd
+## 1 . pip install pyqrcode
+## 2. python -m pip install git+https://gitlab.com/drj11/pypng@pypng-0.20220715.0
