e2fb555163650c974e00302c47147c7eefac6b43
jaygala <jaygala260@gmail.com>
2022-10-02 16:49:41 +0530

    Added maximum subarray sum #6519

---
+++ other/maximum_subarray.py
@@ -0,0 +1,26 @@
+def maxSubArray(nums: list[int]) -> int:
+    """
+    Returns the subarray with maximum sum
+    >>> maxSubArray([1,2,3,4,-2])
+    10
+    >>> maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
+    6
+    """
+
+    curr_max = ans = nums[0]
+
+    for i in range(1, len(nums)):
+        if curr_max >= 0:
+            curr_max = curr_max + nums[i]
+        else:
+            curr_max = nums[i]
+
+        ans = max(curr_max, ans)
+
+    return ans
+
+
+if __name__ == "__main__":
+    n = int(input("Enter number of elements : ").strip())
+    array = list(map(int, input("\nEnter the numbers : ").strip().split()))[:n]
+    print(maxSubArray(array))
