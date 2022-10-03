ab547813416f25d61136c159cbf85d4ef9078a14
Palaniappan R <104909040+palaniappan-r@users.noreply.github.com>
2022-10-03 09:20:17 +0530

    Update byte_to_int.py

--- conversions/bytes_to_int.py
+++ conversions/bytes_to_int.py
@@ -1,38 +1,37 @@
 """
-This script first converts a byetarray to its corresponding sugned/unsigned binary number and the converts the binary number to an integer.
+This script first converts a byetarray to its corresponding sugned/unsigned binary number and the converts the binary number to an integer.    
 """
-
-
-def bytes_to_int(bytes_var: bytearray, signed: bool) -> (int | None):
+def bytes_to_int(bytes_var:bytearray,signed:bool):
     try:
-        binval = ""
+        binval = ''
         for i in bytes_var:
             binnum = str(bin(i))[2:]
-            if len(binnum) < 8:
-                for i in range(0, 8 - len(binnum)):
-                    binnum = "0" + binnum
+            if(len(binnum) < 8):
+                for i in range(0,8-len(binnum)):
+                    binnum = '0' + binnum
             binval = binval + binnum
-
+        
         rslt = 0
-
-        if signed == False:
-            temp = ""
+        
+        if(signed == False):
+            temp = ''
             for i in binval:
-                if i == "0":
-                    temp = temp + "1"
+                if(i == '0'):
+                    temp = temp + '1'
                 else:
-                    temp = temp + "0"
+                    temp = temp + '0'
             binval = temp
-            binval = str(bin(int(binval, 2) + int("1", 2)))[2:]
-
+            binval = str(bin(int(binval, 2) + int('1', 2)))[2:]
+            
         for i in binval:
-            rslt = 2 * rslt + int(i)
-
-        if signed == True:
+                rslt = 2 * rslt + int(i)
+        
+        if(signed == True):
             rslt *= -1
 
         return rslt
-
+    
     except Exception as e:
         print(f"ERROR - {e}")
         return
+        
