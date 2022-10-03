ea7ef67ed73921d039713ddb2aeb057e7d87932d
itzyash01 <91739600+itzyash01@users.noreply.github.com>
2022-10-03 20:46:27 +0530

    Created new file PrefixToInfix.py
    Python code to convert prefix to infix.

---
+++ data_structures/stacks/PrefixToInfix.py
@@ -0,0 +1,24 @@
+def prefixToInfix(prefix):
+	stack = []
+	
+	i = len(prefix) - 1
+	while i >= 0:
+		if not isOperator(prefix[i]):
+			stack.append(prefix[i])
+			i -= 1
+		else:
+			str = "(" + stack.pop() + prefix[i] + stack.pop() + ")"
+			stack.append(str)
+			i -= 1
+	
+	return stack.pop()
+
+def isOperator(c):
+	if c == "*" or c == "+" or c == "-" or c == "/" or c == "^" or c == "(" or c == ")":
+		return True
+	else:
+		return False
+
+if __name__=="__main__":
+	str = "*-A/BC-/ADE"
+	print(prefixToInfix(str))
