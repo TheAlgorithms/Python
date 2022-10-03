81a7cd98041fa8b1ee8484e9ade18bce55f4a005
dimonalik <114773527+dimonalik@users.noreply.github.com>
2022-10-03 13:02:11 +0300

    Create area_of_circle.py
    Script to caclulate circle area by different input measurements

---
+++ maths/area_of_circle.py
@@ -0,0 +1,21 @@
+import math
+i = 1
+while i == 1:
+    mode = input("What will we use to count area of circle? Length of circumference[l], Radius[r] or Diameter[d]?: ")
+    if mode == "d" or mode == "D":
+        d = input("What diameter of circle: ")
+        s = (math.pi*math.pow(int(d),2))/4
+        print("Area of circle is: ", s)
+        break
+    elif mode == "r" or mode == "R":
+        r = input("What radius of circle: ")
+        s = math.pi*(math.pow(int(r),2))
+        print("Area of circle is:", s)
+        break
+    elif mode == "l" or mode == "L":
+        l = input("What length of circumference: ")
+        s = math.pow(int(l),2)/(math.pi*4)
+        print("Area of circle is:", s)
+        break
+    else:
+        print("Wrong input. Try again")
