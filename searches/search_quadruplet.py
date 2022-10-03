9fe384d69180665bfc729668a9e6c2d77b103388
Destiny Erhabor <destinyerhabor6@gmail.com>
2022-10-03 04:15:02 +0000

    Added search quadruplet algorithm

---
+++ searches/search_quadruplet.py
@@ -0,0 +1,64 @@
+
+
+
+def search_quadruplets(sequence, target):
+    """
+    >> > search_quadruplets([4, 1, 2, -1, 1, -3], 1)
+
+    [-3, -1, 1, 4], [-3, 1, 1, 2]
+    Explanation: Both the quadruplets add up to the target.
+
+    >> > search_quadruplets([2, 0, -1, 1, -2, 2], 2)
+    [-2, 0, 2, 2], [-1, 0, 1, 2]
+    Explanation: Both the quadruplets add up to the target.
+
+    Technique: Two Pointers
+    This problem follows the Two Pointers pattern and
+    shares similarities with Triplet Sum to Zero.
+
+    """
+
+    sequence.sort()
+    quadruplets = []
+    for index in range(0, len(sequence) - 3):
+        if index > 0 and sequence[index] == sequence[index - 1]:
+            continue
+        for another_index in range(index + 1, len(sequence) - 2):
+            if (
+                another_index > 0
+                and sequence[another_index] == sequence[another_index - 1]
+            ):
+                continue
+
+            left = another_index + 1
+            right = len(sequence) - 1
+            while left < right:
+                quadruplets_sum = (
+                    sequence[index]
+                    + sequence[another_index]
+                    + sequence[left]
+                    + sequence[right]
+                )
+                if quadruplets_sum == target:
+                    quadruplets.append(
+                        [
+                            sequence[index],
+                            sequence[another_index],
+                            sequence[left],
+                            sequence[right],
+                        ]
+                    )
+                    left += 1
+                    right -= 1
+
+                    while left < right and sequence[left] == sequence[left - 1]:
+                        left += 1
+                    while left < right and sequence[right] == sequence[right + 1]:
+                        right -= 1
+                elif quadruplets_sum < target:
+                    left += 1
+                else:
+                    right -= 1
+    return quadruplets
+
+
