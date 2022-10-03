fbf7a0b47242eb0d80e5c344cfc3f118e0beee6b
giladwo <25708271+giladwo@users.noreply.github.com>
2022-10-03 21:57:51 +0300

    Simplify climbing stairs and use constant memory

--- dynamic_programming/climbing_stairs.py
+++ dynamic_programming/climbing_stairs.py
@@ -30,11 +30,10 @@ def climb_stairs(n: int) -> int:
     ), f"n needs to be positive integer, your input {n}"
     if n == 1:
         return 1
-    dp = [0] * (n + 1)
-    dp[0], dp[1] = (1, 1)
-    for i in range(2, n + 1):
-        dp[i] = dp[i - 1] + dp[i - 2]
-    return dp[n]
+    previous, current = 1, 1
+    for _ in range(n - 1):
+        current, previous = current + previous, current
+    return current
 
 
 if __name__ == "__main__":
