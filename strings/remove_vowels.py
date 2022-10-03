54ca03d1a2b4b88a7cba5dad7a35c439e52f77b6
Mayuresh Choudhary <111348926+Mayuresh-22@users.noreply.github.com>
2022-10-03 11:14:49 +0530

    Update remove_vowels.py

--- strings/remove_vowels.py
+++ strings/remove_vowels.py
@@ -1,36 +1,33 @@
-# This function will remove all the vowels present from the given sentence or a word.
-# returns the modified version of that string.
-
+#This function will remove all the vowels present from the given sentence or a word.
+#returns the modified version of that string.
 
 def remove_vowels(sentence: str) -> str:
     """
-    This removeVowel function will remove all the vowels from a given sentence or a word
-    >>> removeVowels('Hello world')
+    This remove_vowels function will remove all the vowels from a given sentence or a word
+    >>> remove_vowels('Hello world')
     'Hll wrld'
-    >>> removeVowels('Programming is my passion')
+    >>> remove_vowels('Programming is my passion')
     'Prgrmmng s my pssn'
-    >>> removeVowels('I love coding 24 hours')
+    >>> remove_vowels('I love coding 24 hours')
     ' lv cdng 24 hrs'
-    >>> removeVowels('')
+    >>> remove_vowels('')
     ''
     """
 
-    # List of all the vowels
-    vow = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    #List of all the vowels
+    vow=['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
 
-    # iterating over a list of vowels
+    #iterating over a list of vowels
     for vowel in vow:
-        # replacing the vowel with blank
-        sentence = sentence.replace(f"{vowel}", "")
+        #replacing the vowel with blank
+        sentence=sentence.replace(f"{vowel}", "")
 
-    # returning a updated string
+    #returning a updated string
     return sentence
-
 
 if __name__ == "__main__":
     import doctest
-
     doctest.testmod()
 
-    # passing a string to a function removeVowels and printing the result
-    print(remove_vowels("Welcome to HACKTOBERFEST 2022"))
+    #passing a string to a function removeVowels and printing the result
+    print(remove_vowels('Welcome to HACKTOBERFEST 2022'))
