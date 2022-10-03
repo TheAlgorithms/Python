d34b297908d517abb8060e3c30502a58536f2f3d
Ritik Rathor <93708110+ritik-369@users.noreply.github.com>
2022-10-03 07:57:30 +0530

    Update linear_diophantine_check.py

--- maths/linear_diophantine_check.py
+++ maths/linear_diophantine_check.py
@@ -1,4 +1,4 @@
-"""
+""" 
    Program to check for solutions
    of linear diophantine equations
    can be possible or not
@@ -15,72 +15,70 @@ from math import gcd
    ax + by = c
 """
 
-# returns true if solutions exists otherwise false
-# param a,b,c if first,second,third coeff. of equation
-def isPossible(a, b, c):
-    return c % gcd(a, b) == 0
+#returns true if solutions exists otherwise false
+#param a,b,c if first,second,third coeff. of equation
+def isPossible(a, b, c)->bool:
+   return (c % gcd(a, b) == 0)
 
 
 # function for testcase 1
-# return 0 after executing
-def test1():
-    """
-    First example
-    3x + 6y =9
-    """
-    a = 3  # param a is first coeff. of equation
-    b = 6  # param b is second coeff. of equation
-    c = 9  # param c is third coeff. of equation
+def test1()->int:
+   '''
+      First example
+      3x + 6y =9
+   '''
+   a = 3  # param a is first coeff. of equation
+   b = 6  # param b is second coeff. of equation
+   c = 9  # param c is third coeff. of equation
 
-    if isPossible(a, b, c):
-        print("Possible")
-    else:
-        print("Not Possible")
+   if (isPossible(a, b, c)):
+      print("Possible")
+   else:
+      print("Not Possible")
 
-    return 0
-
+   return 0
 
 # function for test case 2
-# return 0 after executing
-def test2():
-    """
-    second example
-    3x + 6y =9
-    """
-    a = 3
-    b = 6
-    c = 9
+def test2()->int:
+   '''
+      second example
+      3x + 6y =9
+   '''
+   a = 3
+   b = 6
+   c = 9
 
-    if isPossible(a, b, c):
-        print("Possible")
-    else:
-        print("Not Possible")
+   if (isPossible(a, b, c)):
+      print("Possible")
+   else:
+      print("Not Possible")
 
-    return 0
+   return 0
 
 
 # function for test case 3
-# return 0 after executing
-def test3():
-    """
-    third example
-    2x + 5y =1
-    """
-    a = 2
-    b = 5
-    c = 1
-    if isPossible(a, b, c):
-        print("Possible")
-    else:
-        print("Not Possible")
+def test3()->int:
+   '''
+      third example
+      2x + 5y =1
+   '''
+   a = 2
+   b = 5
+   c = 1
+   if (isPossible(a, b, c)):
+      print("Possible")
+   else:
+      print("Not Possible")
 
-    return 0
+   return 0
+
 
 
 # main code
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+   
+   test1()  # testcase 1
+   test2()  # testcase 2
+   test3()  # testcase 3
 
-    test1()  # testcase 1
-    test2()  # testcase 2
-    test3()  # testcase 3
