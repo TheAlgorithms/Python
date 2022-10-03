5d9984009c528d6bc5e031aaa95ea55d823df5e0
space monkey <113308290+sofezticated-gentleman@users.noreply.github.com>
2022-10-03 09:53:38 +0530

    Update matrix/matrix_binary_search.py
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- matrix/matrix_binary_search.py
+++ matrix/matrix_binary_search.py
@@ -28,10 +28,10 @@ def mat_bin_search(value: int, matrix: list) -> bool:
     value : value ment to be searched
     matrix = a sorted 2d matrix
     >>> matrix = [[1, 4, 7, 11, 15],
-              [2, 5, 8, 12, 19],
-              [3, 6, 9, 16, 22],
-              [10, 13, 14, 17, 24],
-              [18, 21, 23, 26, 30]]
+    ...           [2, 5, 8, 12, 19],
+    ...           [3, 6, 9, 16, 22],
+    ...           [10, 13, 14, 17, 24],
+    ...           [18, 21, 23, 26, 30]]
     >>> target = 1
     >>> mat_bin_search(target, matrix)
     True
