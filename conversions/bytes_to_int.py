8e219d7f59f05f990c4a399ae75be8a7142fc338
Palaniappan R <palaniappan.r.mail@gmail.com>
2022-10-03 10:45:54 +0530

    Added Bytes to Integer conversion script

--- conversions/bytes_to_int.py
+++ conversions/bytes_to_int.py
@@ -1,9 +1,10 @@
+
 """
 This script first converts a byetarray to its corresponding sugned/unsigned binary number and the converts the binary number to an integer.
 """
 
 
-def bytes_to_int(bytes_var: bytearray, signed: bool):
+def bytes_to_int(bytes_var: bytearray, signed: bool) -> (int):
     try:
         binval = ""
         for i in bytes_var:
@@ -17,22 +18,16 @@ def bytes_to_int(bytes_var: bytearray, signed: bool):
 
         if signed == True:
             temp = ""
-<<<<<<< HEAD
             for i in binval:
                 if i == "0":
                     temp = str(temp) + "1"
-=======
-            for i in range(0,len(binval)):
-                if binval[i] == "0":
-                    temp = str(temp) + str("1")
->>>>>>> d8aa886 (Added Bytes to Integer conversion script)
                 else:
                     temp = str(temp) + "0"
             binval = temp
             binval = str(bin(int(binval, 2) + int("1", 2)))[2:]
 
-        for i in binval:
-            rslt = 2 * rslt + int(i)
+        for k in binval:
+            rslt = 2 * rslt + int(k)
 
         if signed == True:
             rslt *= -1
