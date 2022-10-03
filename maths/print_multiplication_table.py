1382ac27b97bb0ae322e0d849010e3c950828779
Himanshi Tiwari <himanshitiwari0304@gmail.com>
2022-10-03 16:13:35 +0530

    Add print_multiplication_table.py

---
+++ maths/print_multiplication_table.py
@@ -0,0 +1,26 @@
+def print_multiplication_table(number: int, number_of_terms: int):
+    """
+    Prints the multiplication table of a given number till the given number of terms
+    >>> print_multiplication_table(3,5)
+    3 * 1 = 3
+    3 * 2 = 6
+    3 * 3 = 9
+    3 * 4 = 12
+    3 * 5 = 15
+
+    >>> print_multiplication_table(-4,6)
+    -4 * 1 = -4
+    -4 * 2 = -8
+    -4 * 3 = -12
+    -4 * 4 = -16
+    -4 * 5 = -20
+    -4 * 6 = -24
+    """
+    for i in range(1,number_of_terms+1):
+        print(f"{number} * {i} = " + str(number*i))
+        
+
+if __name__ == "__main__":
+    number = 5
+    number_of_terms = 10
+    print_multiplication_table(number,number_of_terms)        
