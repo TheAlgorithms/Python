6510e5f4812f8dd08856ddc1f36f5de886061203
Anil <anil.nagabhiaru@kraftheinz.com>
2022-10-04 14:34:22 -0500

    added group anagram code

---
+++ strings/group_anagram.py
@@ -0,0 +1,23 @@
+import doctest
+
+def find_anagrams(_list: list):
+    emptyDict = {}
+    emptylist=[]
+    for word in a:
+        if ''.join(sorted(word)) in emptyDict.keys():
+            if(emptyDict.get(''.join(sorted(word)))==word):
+                pass
+            else:
+                emptyDict.update({''.join(sorted(word)):emptyDict.get(''.join(sorted(word)))+','+word})
+        else:
+            emptyDict.update({''.join(sorted(word)):word})
+    for key,value in emptyDict.items():
+        listword=value.split(",")
+        if(len(listword)>1):
+            emptylist.append(listword)    
+    return emptylist
+
+if __name__ == "__main__":
+    input = ['could', 'cloud', 'areas', 'arena', 'artsy', 'grips', 'hello', 'parts', 'prigs', 'strap', 'traps']
+    doctest.testfile('group_anagram.txt')
+    find_anagrams(input)
