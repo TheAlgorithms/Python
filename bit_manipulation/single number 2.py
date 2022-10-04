61c49b9cba9a2a9c579c468a8f0ead0846f3130c
Raj kushwaha <87632065+Rajkushwaha0@users.noreply.github.com>
2022-10-04 16:29:20 +0530

    Add files via upload

---
+++ bit_manipulation/single number 2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        d={}
+        for i in range(len(nums)):
+            a=nums.count(nums[i])
+            d[a]=nums[i]
+        for i,j in d.items():
+            if(i==1):
+                return (j)
