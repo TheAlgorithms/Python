426dcbbce315b68ee52f2cb13818921f845403f3
Night101 <91742459+Night-101@users.noreply.github.com>
2022-10-04 21:39:13 +0530

    Create exponential_search.py

---
+++ searches/exponential_search.py
@@ -0,0 +1,33 @@
+"""
+Python program to find an element x
+in a sorted array using Exponential Search
+  
+A recursive binary search function returns 
+location  of x in given array arr[l..r] is 
+present, otherwise -1
+https://en.wikipedia.org/wiki/Exponential_search
+"""
+
+def binarySearch( arr, l, r, x):
+    if r >= l:
+        mid = l + ( r-l ) // 2
+          
+        if arr[mid] == x:
+            return mid
+          
+        if arr[mid] > x:
+            return binarySearch(arr, l, mid - 1, x)
+          
+        return binarySearch(arr, mid + 1, r, x)
+    return -1
+  
+
+def exponentialSearch(arr, n, x):
+    if arr[0] == x:
+        return 0
+
+    i = 1
+    while i < n and arr[i] <= x:
+        i = i * 2
+    return binarySearch( arr, i // 2, min(i, n-1), x)
+      
