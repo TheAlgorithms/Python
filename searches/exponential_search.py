5d177fe980ae1aa94ed247d66813cb34a02890fc
Night101 <91742459+Night-101@users.noreply.github.com>
2022-10-04 21:50:36 +0530

    Update exponential_search.py

--- searches/exponential_search.py
+++ searches/exponential_search.py
@@ -9,7 +9,7 @@ https://en.wikipedia.org/wiki/Exponential_search
 """
 
 
-def binarySearch(arr, l, r, x):
+def binary_search(arr, l, r, x):
     if r >= l:
         mid = l + (r - l) // 2
 
@@ -17,17 +17,27 @@ def binarySearch(arr, l, r, x):
             return mid
 
         if arr[mid] > x:
-            return binarySearch(arr, l, mid - 1, x)
+            return binary_search(arr, l, mid - 1, x)
 
-        return binarySearch(arr, mid + 1, r, x)
+        return binary_search(arr, mid + 1, r, x)
     return -1
 
 
-def exponentialSearch(arr, n, x):
+def exponential_search(arr, n, x):
     if arr[0] == x:
         return 0
 
     i = 1
     while i < n and arr[i] <= x:
         i = i * 2
-    return binarySearch(arr, i // 2, min(i, n - 1), x)
+    return binary_search(arr, i // 2, min(i, n - 1), x)
+
+if __name__ == "__main__":
+    arr = [2, 3, 4, 10, 40]
+    n = len(arr)
+    x = 10
+    result = exponentialSearch(arr, n, x)
+    if result == -1:
+        print ("Element not found in the array")
+    else:
+        print ("Element is present at index %d" %(result))
