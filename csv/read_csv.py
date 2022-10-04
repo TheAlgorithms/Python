e928e8ba681e79856032e61949c0c110b7219d3f
ALONSO <edgar_gonzalezja@hotmail.com>
2022-10-04 14:28:21 -0500

    -new folder for csv -add programming read csv

---
+++ csv/read_csv.py
@@ -0,0 +1,9 @@
+from csv import reader
+# open file in read mode
+with open('csv\students.csv', 'r') as read_obj:
+    # pass the file object to reader() to get the reader object
+    csv_reader = reader(read_obj)
+    # Iterate over each row in the csv using reader object
+    for row in csv_reader:
+        # row variable is a list that represents a row in csv
+        print(row)
