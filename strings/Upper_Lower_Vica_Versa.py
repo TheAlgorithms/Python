bf8b40161feac0dbf285c6b2c8f1b974912f03fc
Sahil Rajesh Shriwardhankar <102847495+notsointresting@users.noreply.github.com>
2022-10-04 17:08:25 +0530

    Add files via upload

---
+++ strings/Upper_Lower_Vica_Versa.py
@@ -0,0 +1,7 @@
+#Python Program to convert Upper to Lower or vica versa
+
+Up = input("Enter String to convert into lowercase---> ")
+Lw = input("Enter String to convert into Uppercase---> ")
+
+print("Lowercase--->",Up.lower())
+print("Uppercase---> ",Lw.upper())
