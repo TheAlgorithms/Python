39d8a7149095291ab281f650c6b5b00d21c26732
Sakshi Agarwal <67694526+7sakshi7@users.noreply.github.com>
2022-10-05 22:28:32 +0530

    Update job_sequencing_problem.py

--- greedy_methods/job_sequencing_problem.py
+++ greedy_methods/job_sequencing_problem.py
@@ -1,5 +1,34 @@
-def print_job_scheduling(x, t):
- 
+"""
+This is a pure Python implementation for Job Sequencing Problem
+reference: https://www.geeksforgeeks.org/job-sequencing-problem/
+For doctests run following command:
+python3 -m doctest -v job_sequencing_problem.py
+Objective
+Given some jobs along with their deadline and profit.
+We need to find maximum profit if all jobs takes unit time to complete
+Approach
+Sort all jobs in decreasing order of profit. 
+Iterate on jobs in decreasing order of profit.For each job , do the following : 
+Find a time slot i, such that slot is empty and i < deadline and i is greatest.Put the job in 
+this slot and mark this slot filled. 
+If no such i exists, then ignore the job. 
+"""
+
+def print_job_scheduling(Jobs : x, MaxDeadline:t) -> List:
+  """Function to print all jobs which can be completed
+    Args:
+        Jobs [list]: A list of all jobs along with deadline and profits
+        MaxDeadline [int]: Maximum deadline among all the jobs
+    Returns:
+        job [list]: Jobs that can be completed
+    Examples:
+    >>> print_job_scheduling [[1, 2, 100],
+           [2, 1, 19],
+           [3, 2, 27],
+           [4, 1, 25],
+           [5, 3, 15]])
+    [2,-1,1,-1,3]
+    """
     n = len(x)
  
     for i in range(n):
@@ -19,14 +48,9 @@ def print_job_scheduling(x, t):
                 result[j] = True
                 job[j] = x[i][0]
                 break
-     """
-    Print the Jobs
-    """
-    print(job)
+    return job
  
- x = [[1, 4, 20],  
-              [2, 1, 20],
-              [3, 1, 40],
-              [4, 1, 30]
- 
-print_job_scheduling(x, 4)
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
