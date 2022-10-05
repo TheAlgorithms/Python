974eecb02add145ad68a97e549353c142cd75266
Samyak <43057462+samyak1999@users.noreply.github.com>
2022-10-05 15:06:28 +0530

    wordbreak.py V2

--- dynamic_programming/word_break.py
+++ dynamic_programming/word_break.py
@@ -1,5 +1,8 @@
-def wordbreak(s, word_dict) -> bool:
+
+def wordbreak(word, word_dict) -> bool:
     """
+    word ---> Input word
+    word_dict ---> Input dictionary
     >>> wordbreak('deepImpact',['deep,'Im','pact'])
     True
     >>> wordbreak('mangoinmango',['mango','in'])
@@ -9,18 +12,17 @@ def wordbreak(s, word_dict) -> bool:
     """
     dict_set = set(word_dict)
     start = [0]
-    # dp[0] is the equivalent of a base-case
-    # from the recursive solution and dp[-1] is the
+    # start[0] is the equivalent of a base-case
+    # from the recursive solution and start[-1] is the
     # overall solution to the complete problem
-    for i in range(len(s)):
+    for i in range(len(word)):
         for j in start:
-            if s[j : i + 1] in dict_set:
+            if word[j : i + 1] in dict_set:
                 start.append(i + 1)  # start of next word in dict
                 break  # - note [1]
-    return start[-1] == len(s)
+    return start[-1] == len(word)
 
 
 if __name__ == "__main__":
     import doctest
-
     doctest.testmod()
