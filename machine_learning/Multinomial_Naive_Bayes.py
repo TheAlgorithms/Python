a5f414e6aa6e5ed2342d7379bf88818d50473789
Sanjay <sanjayprabahar@gmail.com>
2022-10-05 12:46:21 +0530

    Update Multinomial_Naive_Bayes.py

--- machine_learning/Multinomial_Naive_Bayes.py
+++ machine_learning/Multinomial_Naive_Bayes.py
@@ -7,10 +7,7 @@ import numpy as np  # Array and matrix math operations
 import pandas as pd  # Used for reading CSV files
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import cross_val_score, train_test_split
-from sklearn.naive_bayes import (  # sklearn is liberay for Using Machine learning algorithms
-    MultinomialNB,
-)
-
+from sklearn.naive_bayes import MultinomialNB # sklearn is liberay for Using Machine learning algorithms
 
 def main() -> None:
 
@@ -23,7 +20,7 @@ def main() -> None:
 
     # else:
     iris = pd.read_csv("iris.csv")
-
+    
     X = [
         "Sepal.Length",
         "Sepal.Width",
@@ -33,7 +30,7 @@ def main() -> None:
     Y = ["Species"]  # Y is the outputs
 
     # Splitting data into train and test
-    X_train, X_test, y_train, y_test = train_test_split(
+    x_train, x_test, y_train, y_test = train_test_split(
         iris[X], iris[Y], test_size=0.3, random_state=0
     )
 
@@ -41,23 +38,25 @@ def main() -> None:
     MNB = MultinomialNB()
 
     # Building the model
-    pred_mnb = MNB.fit(X_train, y_train.values.ravel())
+    pred_mnb = MNB.fit(x_train, y_train.values.ravel())
 
     # Predict target for test data
-    predict = pred_mnb.predict(X_test)
+    predict = pred_mnb.predict(x_test)
     predict = predict.reshape(len(predict), 1)
-
+    
     # the score, or accuracy of the model
     # print(imnb.score(Xtest, ytest))
-    print(f"Accuracy of the model:\t {MNB.score(X_test, y_test)}")
+    print(f"Accuracy of the model:\t {MNB.score(x_test, y_test)}")
 
     # Confusion matrix MultinomialNB model
     confusion_matrix(y_test, predict)
 
     print(
-        f"Cross Val Accuracy:\t {np.mean(cross_val_score(MNB, X_train, y_train.values.ravel(), cv=10)) }"
+        f"Cross Val Accuracy:\t {np.mean(cross_val_score(MNB, x_train, y_train.values.ravel(), cv=10)) }"
     )
-
-
+    
 if __name__ == "__main__":
     main()
+    import doctest
+
+    doctest.testmod()
