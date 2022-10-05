22688dfee2182abeff03dba8a4441a98956a363e
Deepu Yadav <58137357+Deepshikhayadav@users.noreply.github.com>
2022-10-05 20:47:41 +0530

    added type hint and tests

--- sorts/insertionsort.py
+++ sorts/insertionsort.py
@@ -4,6 +4,26 @@ run on terminal to see the result
 run ->python insertion_sort2.py
 """
 
+'''
+insertion_sort([15, 10, 5, 7, 13, 8, 2])
+Sorted array:
+[2, 5, 7, 8, 10, 13, 15]
+
+
+insertion_sort([-6,-5,-12])
+Sorted array:
+[-12,-6,-5]
+
+
+insertion_sort(['s','h','y'])
+Sorted array:
+['h','s','y']
+
+insertion_sort([])
+Sorted array:
+[]
+
+'''
 
 def insertion_sort(a) -> None:
     for i in range(1, len(a)):
