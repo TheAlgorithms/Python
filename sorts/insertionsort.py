1bb96568c877a7ae6d56be92a3bdc87db2564eab
Deepu Yadav <58137357+Deepshikhayadav@users.noreply.github.com>
2022-10-05 20:56:04 +0530

    Update insertionsort.py

--- sorts/insertionsort.py
+++ sorts/insertionsort.py
@@ -4,28 +4,32 @@ run on terminal to see the result
 run ->python insertion_sort2.py
 """
 
-'''
-insertion_sort([15, 10, 5, 7, 13, 8, 2])
-Sorted array:
-[2, 5, 7, 8, 10, 13, 15]
-
-
-insertion_sort([-6,-5,-12])
-Sorted array:
-[-12,-6,-5]
-
-
-insertion_sort(['s','h','y'])
-Sorted array:
-['h','s','y']
-
-insertion_sort([])
-Sorted array:
-[]
-
-'''
 
 def insertion_sort(a) -> None:
+    
+    '''
+    Examples:
+    >>> a=[15, 10, 5, 7, 13, 8, 2]
+    >>> insertion_sort(a)
+    Sorted array:
+    [2, 5, 7, 8, 10, 13, 15]
+    
+    >>> a=[-6,-5,-12]
+    >>>insertion_sort(a)
+    Sorted array:
+    [-12,-6,-5]
+    
+    >>> a=['s','h','y']
+    >>>insertion_sort(a)
+    Sorted array:
+    ['h','s','y']
+    
+    >>> a=[]
+    >>>insertion_sort(a)
+    Sorted array:
+    []
+    
+    '''
     for i in range(1, len(a)):
         temp = a[i]
         j = i - 1
@@ -33,7 +37,8 @@ def insertion_sort(a) -> None:
             a[j + 1] = a[j]
             j -= 1
         a[j + 1] = temp
-
+    print("Sorted array:")
+    print(a)
 
 # array to be sorted
 a = [15, 10, 5, 7, 13, 8, 2]
@@ -41,5 +46,4 @@ print("Unsorted array:")
 print(a)
 # method call
 insertion_sort(a)
-print("Sorted array:")
-print(a)
+
