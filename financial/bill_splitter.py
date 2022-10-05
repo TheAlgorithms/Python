3e3c411a42b3e60cd7fde9831f0400f900f36055
Samartha <samarthahm@gmail.com>
2022-10-05 20:10:21 +0530

    update testcases

--- financial/bill_splitter.py
+++ financial/bill_splitter.py
@@ -4,18 +4,6 @@ Program to split bills among a group by given
 - Name of contributer and amount of contribution separated by space. eg: John 100
 
 """
-from os import system
-def createContri(s:str):
-  '''
-  >>> createContri('Rohan 500')
-  {'name': 'Rohan', 'contri': 500}
-  >>> createContri('Sam 10')
-  {'name': 'Sam', 'contri': 10}
-  >>> createContri('Peter 0')
-  {'name': 'Peter', 'contri': 0}
-  '''
-  s=s.split(' ')
-  return {'name':s[0],'contri':int(s[1])}
 
 #function to split bill among group
 #pool: [...{'name':str,'contri':int},{'name':str,'contri':int}]
@@ -57,8 +45,23 @@ def splitBill(pool):
     sol.append({'name':i['name'],'payment':m})
   return {'sol':sol,'total':total,'each':each,'pool':pool}
 
+
 #function to print solution in a format
+#result is the value returned by splitBill(pool) function
 def printSolution(result):
+  '''
+  >>> printSolution({'sol': [{'name': 'sam', 'payment': [{'name': 'rohan', 'contri': 50.0}, {'name': 'john', 'contri': 200.0}]}], 'total': 750, 'each': 250.0, 'pool': [{'name': 'sam', 'contri': 500}, {'name': 'rohan', 'contri': 200}, {'name': 'john', 'contri': 50}]})
+  sam  paid    $ 500
+  rohan  paid    $ 200
+  john  paid    $ 50
+  -------------------------------------------------------------------------------
+  Total pool amount  : $ 750
+  Per head           : $ 250.0
+  -------------------------------------------------------------------------------
+  rohan  should pay $ 50.0  to  sam
+  john  should pay $ 200.0  to  sam
+  -------------------------------------------------------------------------------
+  '''
   for i in result['pool']:
     print(i['name'],' paid    $',i['contri'])
   print('-------------------------------------------------------------------------------')
@@ -71,27 +74,7 @@ def printSolution(result):
         print(j['name'],' should pay $',j['contri'],' to ', i['name'])
     print('-------------------------------------------------------------------------------')
 
-def printBanner():
-  banner='''
-███████╗██████╗ ██╗     ██╗████████╗    ██████╗ ██╗██╗     ██╗     ███████╗
-██╔════╝██╔══██╗██║     ██║╚══██╔══╝    ██╔══██╗██║██║     ██║     ██╔════╝
-███████╗██████╔╝██║     ██║   ██║       ██████╔╝██║██║     ██║     ███████╗
-╚════██║██╔═══╝ ██║     ██║   ██║       ██╔══██╗██║██║     ██║     ╚════██║
-███████║██║     ███████╗██║   ██║       ██████╔╝██║███████╗███████╗███████║
-╚══════╝╚═╝     ╚══════╝╚═╝   ╚═╝       ╚═════╝ ╚═╝╚══════╝╚══════╝╚══════╝     
-  '''
-  print(banner)
 
-system('cls')
-printBanner()
-pool=[]
-n=int(input('Number of participants : '))
-for i in range(n):
-  print('Enter name and contribution of participant ',i+1,' : ')
-  pool.append(createContri(input()))
-system('cls')
-printBanner()
-printSolution(splitBill(pool))
 
 if __name__ == "__main__":
     import doctest
