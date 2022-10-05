d0767b0ce3e04bdf6debb3fa8d98dffcf5d1d2bd
SHRIYANSH-GAUR <82903280+SHRIYANSHGAUR@users.noreply.github.com>
2022-10-06 00:59:38 +0530

    Added the famous Golden ratio calculator in python

---
+++ maths/Golden_Ratio.py
@@ -0,0 +1,15 @@
+#Golden ratio
+
+def goldenRatio():
+  """
+  >>> goldenRatio()
+  1.618033988749895
+  
+  """
+  phi = (1 + 5 ** 0.5) / 2
+  return phi
+
+
+if __name__ == "__main__":
+    
+    print(f"The Golden Ratio is {add()}")
