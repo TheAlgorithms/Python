b7705b49555f5801ca5f418489ed81886999d84c
SHRIYANSH-GAUR <82903280+SHRIYANSHGAUR@users.noreply.github.com>
2022-10-06 18:27:13 +0530

    Created the percentage algorthim

---
+++ maths/percent.py
@@ -0,0 +1,20 @@
+"""
+Find the Percentage of two numbers
+wikipdia
+https://en.wikipedia.org/wiki/Percentage
+>>> percent(30.0,75.0)
+    40.0
+"""
+
+def percent(a: float , b: float) -> float:
+    return (a / b) * 100
+
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod(verbose=True)  # verbose so we can see methods missing tests
+
+
+    print(f"Percent of these two numbers -> : {percent(30.0, 75.0) = }")
