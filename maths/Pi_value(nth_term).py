663f4ce890b83656f116cc026417f5575cf33342
Ujjawal Agrawal <72138726+Ujjawal179@users.noreply.github.com>
2022-10-06 17:59:23 +0530

    Update Pi_value(nth_term).py

--- maths/Pi_value(nth_term).py
+++ maths/Pi_value(nth_term).py
@@ -5,24 +5,27 @@ getcontext().prec = 1000
 # max possible digits = 1000
 
 
+
 def pi_d(n_d: int) -> float:
+    if n_d==0:
+        return(0.0)
+    else:
 
-    numerator = Decimal(0)
-    denominator = Decimal(0)
-    result = Decimal(0)
+        numerator = Decimal(0)
+        denominator = Decimal(0)
+        result = Decimal(0)
 
-    for k in range(n_d):
+        for k in range(n_d):
 
-        numerator = ((-1) ** k) * (factorial(6 * k)) * (13591409 + 54510134 * k)
+            numerator = ((-1) ** k) * (factorial(6 * k)) * (13591409 + 54510134 * k)
 
-        denominator = factorial(3 * k) * ((factorial(k)) ** 3) * (640320 ** (3 * k))
+            denominator = factorial(3 * k) * ((factorial(k)) ** 3) * (640320 ** (3 * k))
 
-        result += Decimal(numerator) / Decimal(denominator)
-        result *= 12 / Decimal(640320**1.5)
-        result = result**-1
+            result += Decimal(numerator) / Decimal(denominator)
+            result *= 12 / Decimal(640320**1.5)
+            result = result**-1
 
-        return round(result, n_d)
-
+            return round(result, n_d)
 
 if __name__ == "__main__":
     num = int(input("\nEnter the num of digits (0-999): "))
@@ -30,4 +33,4 @@ if __name__ == "__main__":
         final_result = pi_d(num)
         print(f"Values of pi to the {num} decimal places is :\n\n   {final_result}\n")
     else:
-        print("Enter a value between 0 - 999")
+        print("Enter a value between 1 - 999")
