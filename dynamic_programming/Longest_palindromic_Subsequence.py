898993fd74293ce0c510df853acb4c7be5d9d639
Avimehta14 <avimehta14@gmail.com>
2022-10-06 01:44:36 -0700

    contributed by adding dynamic programming approach towards finding longest palindromicsubsequence in a string

--- dynamic_programming/Longest_palindromic_Subsequence.py
+++ dynamic_programming/Longest_palindromic_Subsequence.py
@@ -1,12 +1,33 @@
+#############################
+# Author: Avi Mehta
+# File: Longest_palindromic_subsequence
+# comments: This programme outputs the Longest Strictly Palindromic Subsequence in
+#           O(NLogN) Where N is the Number of elements in the string
+#############################
+from __future__ import annotations
 
 def longestPalindrome(s):
-    #dp[i][j] means whether s[i] to s[j] is a panlindrome
+    """
+    dp[i][j] means whether s[i] to s[j] is a panlindrome
 
-    dp=[[False for i in range(len(s))] for j in range(len(s))]      # act as base case
+    >>> longestPalindrome("cbbd")
+    2
+    >>> longestPalindrome("bbbab")
+    4
+    >>> longestPalindrome("zzakz")
+    3
+    >>> longestPalindromic(" ")
+    0
+
+    """
+
+    dp=[[False for i in range(len(s))] for j in range(len(s))]    
     for i in range(len(s)):
         dp[i][i]=True
     maxl=1
-    begin=0                     #bottom up DP always start from base case, so we will scan backward
+    begin=0                     
+    
+    #bottom up DP always start from base case, so we will scan backward
     for i in range(len(s)-1,-1,-1):
         for j in range(i+1,len(s)):        
             if s[i]==s[j]:      #if i+1=j and they are same character,of course this is a panlindrome
@@ -21,5 +42,8 @@ def longestPalindrome(s):
 
     return s[begin:begin+maxl]
 
-s = "ababas"
-answer = longestPalindrome(s)
+if __name__ == "__main__":
+    
+    import doctest
+
+    doctest.testmod()
