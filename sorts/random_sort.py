6c426a1d989f8fa2a64ba5a3a0a6ca9745273b7a
Christian L. Moreno <christianluciano.m@gmail.com>
2022-10-06 00:53:45 -0300

    doctest

--- sorts/random_sort.py
+++ sorts/random_sort.py
@@ -1,21 +1,32 @@
 import random
 from typing import List
 
+def is_sorted(lista: List[int]) -> bool:
+    """
+    Given a list of integer, return True if all are ordered
 
-def is_sorted(l: list[int]) -> bool:
-    values = [l[i] <= l[i + 1] for i in range(len(l) - 1)]
+    >>> is_sorted([1, 2, 3])
+    True
+    >>> is_sorted([3, 2, 1])
+    False
+    """
+    values = [lista[i] <= lista[i+1] for i in range(len(lista) - 1)]
     return all(values)
 
-
-def random_sort(l: list[int]) -> list[int]:
+def random_sort(lista: List[int]) -> List[int]:
     """
     efficiency? where are we going we don't need that
-    """
-    while not is_sorted(l):
-        random.shuffle(l)
-        # print(l)
 
-    return l
+    >>> random_sort([1, 2, 3])
+    [1, 2, 3]
+    >>> random_sort([3, 2, 1])
+    [1, 2, 3]
+    """
+    while not is_sorted(lista):
+        random.shuffle(lista)
+        # print(lista)
+
+    return lista
 
 
 if __name__ == "__main__":
