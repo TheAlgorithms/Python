2958b12e09a8a4fd801397180d2d0ac80c2bc06c
adarshdotexe <adarshvarshney@outlook.com>
2022-10-06 22:58:18 +0530

    Clean Formatting for flake8

--- project_euler/problem_127/sol1.py
+++ project_euler/problem_127/sol1.py
@@ -3,7 +3,9 @@ Problem 127: https://projecteuler.net/problem=127
 
 Description:
 
-The radicalical of n, radical(n), is the product of distinct prime factors of n. For example, 504 = 23 × 32 × 7, so radical(504) = 2 × 3 × 7 = 42.
+The radicalical of n, radical(n), is the product of distinct prime
+factors of n. For example, 504 = 23 × 32 × 7,
+so radical(504) = 2 × 3 × 7 = 42.
 
 We shall define the triplet of positive integers (a, b, c) to be an abc-hit if:
 
@@ -17,13 +19,15 @@ GCD(5, 27) = GCD(5, 32) = GCD(27, 32) = 1
 5 < 27
 5 + 27 = 32
 radical(4320) = 30 < 32
-It turns out that abc-hits are quite rare and there are only thirty-one abc-hits for c < 1000, with ∑c = 12523.
+It turns out that abc-hits are quite rare and there are only thirty-one
+abc-hits for c < 1000, with ∑c = 12523.
 
 Find ∑c for c < 120000.
 
 Solution:
 
-A Naive solution would be to generate all the triplets and check if they are abc-hits.
+A Naive solution would be to generate all the triplets and check if they are
+abc-hits.
 
 Observations:
 radical(a*b*c) = radical(a)*radical(b)*radical(c)
@@ -32,49 +36,53 @@ radical(a*b*c) = radical(a)*radical(b)*radical(c)
 
 
 def greatest_common_divisor(a: int, b: int) -> int:
-	"""
-	Returns the greatest common divisor of a and b.
-	>>> greatest_common_divisor(10, 15)
-	5
-	>>> greatest_common_divisor(3, 7)
-	1
-	>>> greatest_common_divisor(10, 10)
-	10
-	"""
-	while b != 0:
-		a, b = b, a % b
-	return a
+    """
+    Returns the greatest common divisor of a and b.
+    >>> greatest_common_divisor(10, 15)
+    5
+    >>> greatest_common_divisor(3, 7)
+    1
+    >>> greatest_common_divisor(10, 10)
+    10
+    """
+    while b != 0:
+        a, b = b, a % b
+    return a
 
 
 def solution(limit: int = 120000) -> int:
-	"""
-	Return the sum of all c < limit such that (a, b, c) is an abc-hit.
-	>>> solution(1000)
-	12523
-	>>> solution(10000)
-	441085
-	>>> solution(100000)
-	14125034
-	>>> solution(120000)
-	18407904
-	"""
+    """
+    Return the sum of all c < limit such that (a, b, c) is an abc-hit.
+    >>> solution(1000)
+    12523
+    >>> solution(10000)
+    441085
+    >>> solution(100000)
+    14125034
+    >>> solution(120000)
+    18407904
+    """
 
-	# Modified Sieve of Eratosthenes (Source: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes) to find Radicals.
-	radicals = [0] + [1] * (limit - 1)
-	for i in range(2, len(radicals)):
-		if radicals[i] == 1:
-			for j in range(i, len(radicals), i):
-				radicals[j] *= i
+    # Modified Sieve of Eratosthenes
+    # (Source: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes)
+    # to find Radicals.
+    radicals = [0] + [1] * (limit - 1)
+    for i in range(2, len(radicals)):
+        if radicals[i] == 1:
+            for j in range(i, len(radicals), i):
+                radicals[j] *= i
 
-
-	ans = 0
-	for c in range(2, limit):
-		for a in range(1, c):
-			b = c - a
-			if a < b and greatest_common_divisor(a, b) == 1 and greatest_common_divisor(a, c) == 1 and greatest_common_divisor(b, c) == 1 and radicals[a] * radicals[b] * radicals[c] < c:
-				ans += c
-	return ans
+    ans = 0
+    for c in range(2, limit):
+        for a in range(1, c):
+            b = c - a
+            if (a < b and greatest_common_divisor(a, b) == 1
+                    and greatest_common_divisor(a, c) == 1
+                    and greatest_common_divisor(b, c) == 1
+                    and radicals[a] * radicals[b] * radicals[c] < c):
+                ans += c
+    return ans
 
 
 if __name__ == "__main__":
-	print(solution())
+    print(solution())
--- project_euler/problem_127/sol2.py
+++ project_euler/problem_127/sol2.py
@@ -3,7 +3,8 @@ Problem 127: https://projecteuler.net/problem=127
 
 Description:
 
-The radicalical of n, radical(n), is the product of distinct prime factors of n. For example, 504 = 23 × 32 × 7, so radical(504) = 2 × 3 × 7 = 42.
+The radicalical of n, radical(n), is the product of distinct prime factors of
+n. For example, 504 = 23 × 32 × 7, so radical(504) = 2 × 3 × 7 = 42.
 
 We shall define the triplet of positive integers (a, b, c) to be an abc-hit if:
 
@@ -17,80 +18,91 @@ GCD(5, 27) = GCD(5, 32) = GCD(27, 32) = 1
 5 < 27
 5 + 27 = 32
 radical(4320) = 30 < 32
-It turns out that abc-hits are quite rare and there are only thirty-one abc-hits for c < 1000, with ∑c = 12523.
+It turns out that abc-hits are quite rare and there are only thirty-one
+abc-hits for c < 1000, with ∑c = 12523.
 
 Find ∑c for c < 120000.
 
 Solution:
 
-A Naive solution would be to generate all the triplets and check if they are abc-hits:
-	for c in range(2, LIMIT):
-		for a in range(1, c):
-			b = c - a
-			if a < b and math.gcd(a, b) == 1 and math.gcd(a, c) == 1 and math.gcd(b, c) == 1 and radical(a * b * c) < c:
-				ans += c
+A Naive solution would be to generate all the triplets and check if they are
+abc-hits:
+    for c in range(2, LIMIT):
+        for a in range(1, c):
+            b = c - a
+            if a < b and math.gcd(a, b) == 1 and math.gcd(a, c) == 1 and
+            math.gcd(b, c) == 1 and radical(a * b * c) < c:
+                ans += c
 
 Observations:
 2 <= radical(n) <= n (for all n > 1).
-gcd(c,b) = gcd(a+b,b) = gcd(a,b) = gcd(a,a+b) = gcd(a,c) (Euclid's GCD algorithm Source: https://en.wikipedia.org/wiki/Euclidean_algorithm).
-radical(a * b * c) = radical(a) * radical(b) * radical(c) (Since {a, b, c} are mutually coprime).
+gcd(c,b) = gcd(a+b,b) = gcd(a,b) = gcd(a,a+b) = gcd(a,c)(Euclid's GCD
+algorithm Source: https://en.wikipedia.org/wiki/Euclidean_algorithm).
+radical(a * b * c) = radical(a) * radical(b) * radical(c) (Since {a, b, c}
+are mutually coprime).
 
 Idea:
-We sort 'a' values in ascending order of radical(a) and stop when radical(a) * radical(c) >= c instead of checking for all values of a in the range [1,c).
-We only find gcd(a,b) instead of gcd(a,b), gcd(a,c) and gcd(b,c) separately.
+We sort 'a' values in ascending order of radical(a) and stop when
+radical(a) * radical(c) >= c instead of checking for all values of
+a in the range [1,c). We only find gcd(a,b) instead of gcd(a,b), gcd(a,c)
+and gcd(b,c) separately.
 """
 
 
 def greatest_common_divisor(a: int, b: int) -> int:
-	"""
-	Returns the greatest common divisor of a and b.
-	>>> greatest_common_divisor(10, 15)
-	5
-	>>> greatest_common_divisor(3, 7)
-	1
-	>>> greatest_common_divisor(10, 10)
-	10
-	"""
-	while b != 0:
-		a, b = b, a % b
-	return a
+    """
+    Returns the greatest common divisor of a and b.
+    >>> greatest_common_divisor(10, 15)
+    5
+    >>> greatest_common_divisor(3, 7)
+    1
+    >>> greatest_common_divisor(10, 10)
+    10
+    """
+    while b != 0:
+        a, b = b, a % b
+    return a
 
 
 def solution(limit: int = 120000) -> int:
-	"""
-	Return the sum of all c < limit such that (a, b, c) is an abc-hit.
-	>>> solution(1000)
-	12523
-	>>> solution(10000)
-	441085
-	>>> solution(100000)
-	14125034
-	>>> solution(120000)
-	18407904
-	"""
+    """
+    Return the sum of all c < limit such that (a, b, c) is an abc-hit.
+    >>> solution(1000)
+    12523
+    >>> solution(10000)
+    441085
+    >>> solution(100000)
+    14125034
+    >>> solution(120000)
+    18407904
+    """
 
-	# Modified Sieve of Eratosthenes (Source: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes) to find Radicals.
-	radicals = [0] + [1] * (limit - 1)
-	for i in range(2, len(radicals)):
-		if radicals[i] == 1:
-			for j in range(i, len(radicals), i):
-				radicals[j] *= i
+    # Modified Sieve of Eratosthenes
+    # (Source: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes)
+    # to find Radicals.
+    radicals = [0] + [1] * (limit - 1)
+    for i in range(2, len(radicals)):
+        if radicals[i] == 1:
+            for j in range(i, len(radicals), i):
+                radicals[j] *= i
 
-	# Sort a values in ascending order of radical(a).
-	sorted_radicals = sorted((radical, n) for (n, radical) in enumerate(radicals))
-	sorted_radicals = sorted_radicals[1:]  # Remove (0, 0) from the list.
+    # Sort a values in ascending order of radical(a).
+    sorted_radicals = sorted((radical, n) for (n, radical) in
+                             enumerate(radicals))
+    sorted_radicals = sorted_radicals[1:]  # Remove (0, 0) from the list.
 
-	ans = 0
-	for c in range(2, limit):
-		for (radical, a) in sorted_radicals:
-			radical *= radicals[c]
-			if radical >= c:
-				break
-			b = c - a
-			if a < b and greatest_common_divisor(a, b) == 1 and radical * radicals[b] < c:
-				ans += c
-	return ans
+    ans = 0
+    for c in range(2, limit):
+        for (radical, a) in sorted_radicals:
+            radical *= radicals[c]
+            if radical >= c:
+                break
+            b = c - a
+            if (a < b and greatest_common_divisor(a, b) == 1
+                    and radical * radicals[b] < c):
+                ans += c
+    return ans
 
 
 if __name__ == "__main__":
-	print(solution())
+    print(solution())
