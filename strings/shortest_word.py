fcd629629b0092d2500eaa002ed188e28615dc71
happiestbee <87628038+happiestbee@users.noreply.github.com>
2022-10-06 08:20:25 -0400

    Add find shortest word in a sentence

---
+++ strings/shortest_word.py
@@ -0,0 +1,26 @@
+def shortest_word(sentence: str) -> str:
+    """
+    Find the shortest word in a sentence. 
+    If there are multiple words that are the same length, return the first one.
+
+    >>> shortest_word("I love Python")
+    'I'
+    >>> shortest_word("Find the shortest word in a sentence.")
+    'a'
+    >>> shortest_word("Black Stack")
+    'Black'
+    >>> shortest_word("funny bunny rabbit")
+    'funny'
+    """
+
+    # Split the sentence into a list of words
+    # Sort by length and take the first element
+    word_list = sentence.split()
+    word_list.sort(key=len)
+    return word_list[0]
+
+
+if __name__ == "__main__":
+    from doctest import testmod
+
+    testmod()
