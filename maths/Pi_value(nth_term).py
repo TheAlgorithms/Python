50a6a7e1a9f6c9fc639cee4f3e08979ea9db963c
Ujjawal Agrawal <72138726+Ujjawal179@users.noreply.github.com>
2022-10-06 18:12:14 +0530

    Update Pi_value(nth_term).py

--- maths/Pi_value(nth_term).py
+++ maths/Pi_value(nth_term).py
@@ -6,25 +6,34 @@ getcontext().prec = 1000
 
 
 def pi_d(n_d: int) -> float:
-    if n_d == 0:
-        return 0.0
-    else:
+    '''
+    Calculate the value of pi till n-digits before decimal
+    >>> pi_d(0)
+    None
+    >>> pi_d(10)
+    3.1415926536
+    >>> pi_d(30)
+    3.141592653589734311542867450467
+    >>> pi_d(3000)
+    Traceback (most recent call last):
+    ...
+    decimal.InvalidOperation: [<class 'decimal.InvalidOperation'>]
+    '''
+    numerator = Decimal(0)
+    denominator = Decimal(0)
+    result = Decimal(0)
 
-        numerator = Decimal(0)
-        denominator = Decimal(0)
-        result = Decimal(0)
+    for k in range(n_d):
 
-        for k in range(n_d):
+        numerator = ((-1) ** k) * (factorial(6 * k)) * (13591409 + 54510134 * k)
 
-            numerator = ((-1) ** k) * (factorial(6 * k)) * (13591409 + 54510134 * k)
+        denominator = factorial(3 * k) * ((factorial(k)) ** 3) * (640320 ** (3 * k))
 
-            denominator = factorial(3 * k) * ((factorial(k)) ** 3) * (640320 ** (3 * k))
+        result += Decimal(numerator) / Decimal(denominator)
+        result *= 12 / Decimal(640320**1.5)
+        result = result**-1
 
-            result += Decimal(numerator) / Decimal(denominator)
-            result *= 12 / Decimal(640320**1.5)
-            result = result**-1
-
-            return round(result, n_d)
+        return round(result, n_d)
 
 
 if __name__ == "__main__":
