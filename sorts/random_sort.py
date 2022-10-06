3fa8d69c6fc593f44a9405bc6ee01b98ab4cbaf8
Christian L. Moreno <christianluciano.m@gmail.com>
2022-10-05 23:55:00 -0300

    add random sort

---
+++ sorts/random_sort.py
@@ -0,0 +1,23 @@
+import random
+
+
+def is_sorted(l):
+    values = [l[i] <= l[i+1] for i in range(len(l) - 1)]
+    return all(values)
+
+def random_sort(l):
+    """
+    efficiency? where are we going we don't need that
+    """
+    while not is_sorted(l):
+        random.shuffle(l)
+        # print(l)
+
+    return l
+
+
+
+if __name__ == "__main__":
+    user_input = input("Enter numbers separated by a comma:\n").strip()
+    unsorted = [int(item) for item in user_input.split(",")]
+    print(random_sort(unsorted))
