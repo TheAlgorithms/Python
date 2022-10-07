bb4d50e9f75918962464172033f1f5bd7c1de095
Ujjawal Agrawal <72138726+Ujjawal179@users.noreply.github.com>
2022-10-07 17:56:57 +0530

    Update coulombs_law.py

--- physics/coulombs_law.py
+++ physics/coulombs_law.py
@@ -12,10 +12,10 @@ Source :
 
 
 # Define the Coulombs Constant K and the function
-Coulombs_CONSTANT = 8.988e9
+coulombs_constant = 8.988e9
 
 
-def Coulombs_law(
+def coulombs_law(
     force: float, charge_1: float, charge_2: float, distance: float
 ) -> dict[str, float]:
 
@@ -31,19 +31,19 @@ def Coulombs_law(
     result : dict name, value pair of the parameter having Zero as it's value
     Returns the value of one of the parameters specified as 0, provided the values of
     other parameters are given.
-    >>> Coulombs_law(force=0, charge_1=10, charge_2=1, distance=1)
+    >>> coulombs_law(force=0, charge_1=10, charge_2=1, distance=1)
     {'force': 8.988e10}
-    >>> Coulombs_law(force=8.988e9, charge_1=0, charge_2=10, distance=0.1)
+    >>> coulombs_law(force=8.988e9, charge_1=0, charge_2=10, distance=0.1)
     {'charge_1': 0.0010000000000000002}
-    >>> Coulombs_law(force=36337.283, charge_1=0, charge_2=0, distance=35584)
+    >>> coulombs_law(force=36337.283, charge_1=0, charge_2=0, distance=35584)
     Traceback (most recent call last):
         ...
     ValueError: One and only one argument must be 0
-    >>> Coulombs_law(force=36337.283, charge_1=-674, charge_2=0, distance=35584)
+    >>> coulombs_law(force=36337.283, charge_1=-674, charge_2=0, distance=35584)
     Traceback (most recent call last):
         ...
     ValueError: Magnitude of Coulombs force can not be negative
-    >>> Coulombs_law(force=-847938e12, charge_1=674, charge_2=0, distance=9374)
+    >>> coulombs_law(force=-847938e12, charge_1=674, charge_2=0, distance=9374)
     Traceback (most recent call last):
         ...
     ValueError: Magnitude of Coulombs force can not be negative
@@ -60,16 +60,16 @@ def Coulombs_law(
     if charge_1 < 0 or charge_2 < 0:
         raise ValueError("Magnitude of charge can not be negative")
     if force == 0:
-        force = Coulombs_CONSTANT * product_of_charge / (distance**2)
+        force = coulombs_constant * product_of_charge / (distance**2)
         return {"force": force}
     elif charge_1 == 0:
-        charge_1 = (force) * (distance**2) / (Coulombs_CONSTANT * charge_2)
+        charge_1 = (force) * (distance**2) / (coulombs_constant * charge_2)
         return {"charge_1": charge_1}
     elif charge_2 == 0:
-        charge_2 = (force) * (distance**2) / (Coulombs_CONSTANT * charge_1)
+        charge_2 = (force) * (distance**2) / (coulombs_constant * charge_1)
         return {"charge_2": charge_2}
     elif distance == 0:
-        distance = (Coulombs_CONSTANT * product_of_charge / (force)) ** 0.5
+        distance = (coulombs_constant * product_of_charge / (force)) ** 0.5
         return {"distance": distance}
     raise ValueError("One and only one argument must be 0")
 
@@ -80,4 +80,4 @@ if __name__ == "__main__":
     charge_1 = float(input("Enter the magnitude of charge_1:"))
     charge_2 = float(input("Enter the magnitude of charge_2:"))
     distance = float(input("Enter the magnitude of distance:"))
-    print(Coulombs_law(force, charge_1, charge_2, distance))
+    print(coulombs_law(force, charge_1, charge_2, distance))
