bf6657963542bfe864ae4cfc9d4d032b6138d84d
Abinash Satapathy <iamabinash98@gmail.com>
2022-10-07 13:31:30 +0200

    Update barcode_validator.py
    Included docstring and updated variables to snake_case

--- strings/barcode_validator.py
+++ strings/barcode_validator.py
@@ -1,4 +1,9 @@
-def evalKey(code: int):
+def eval_key(code: int):
+    '''
+    Returns the last digit of barcode by excluding the last digit first
+    and then computing to reach the actual last digit from the remaining
+    12 digits.
+    '''
     code //= 10  # exclude the last digit
     checker = False
     s = 0
@@ -13,14 +18,22 @@ def evalKey(code: int):
     return (10 - (s % 10)) % 10
 
 
-def isValid(code: int):
-    return len(str(code)) == 13 and evalKey(code) == code % 10
+def is_valid(code: int):
+    '''
+    Checks for length of barcode and last-digit
+    Returns boolean value of validity of barcode
+    '''
+    return len(str(code)) == 13 and eval_key(code) == code % 10
 
 
 if __name__ == "__main__":
+    '''
+    Enter a barcode.
+    Displays whether the entered barcode is valid or invalid.
+    '''
     barcode = input("Enter a barcode: ")
     number_barcode = int(barcode)
-    if isValid(number_barcode):
+    if is_valid(number_barcode):
         print(f"|{n}| is a valid Barcode")
     else:
         print(f"|{n}| is NOT is valid Barcode.")
