57247b683d1fd54e2d1d4ef2691273e23e08c1c7
Abinash Satapathy <iamabinash98@gmail.com>
2022-10-07 14:36:19 +0200

    Update barcode_validator.py
    Included docset and updated bugs

--- strings/barcode_validator.py
+++ strings/barcode_validator.py
@@ -1,9 +1,13 @@
-def eval_key(code: int):
+def eval_key(code: int) -> int:
     """
     Returns the last digit of barcode by excluding the last digit first
     and then computing to reach the actual last digit from the remaining
     12 digits.
+
+    >>> eval_key(8718452538119)
+    9
     """
+
     code //= 10  # exclude the last digit
     checker = False
     s = 0
@@ -18,15 +22,25 @@ def eval_key(code: int):
     return (10 - (s % 10)) % 10
 
 
-def is_valid(code: int):
+def is_valid(code: int) -> bool:
     """
     Checks for length of barcode and last-digit
     Returns boolean value of validity of barcode
+
+    >>> is_valid(8718452538119)
+    True
+
+    >>> is_valid(87184525)
+    False
+
     """
+
     return len(str(code)) == 13 and eval_key(code) == code % 10
 
 
 if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
     """
     Enter a barcode.
     Displays whether the entered barcode is valid or invalid.
@@ -34,6 +48,6 @@ if __name__ == "__main__":
     barcode = input("Enter a barcode: ")
     number_barcode = int(barcode)
     if is_valid(number_barcode):
-        print(f"|{n}| is a valid Barcode")
+        print(f"|{number_barcode}| is a valid Barcode")
     else:
-        print(f"|{n}| is NOT is valid Barcode.")
+        print(f"|{number_barcode}| is NOT is valid Barcode.")
