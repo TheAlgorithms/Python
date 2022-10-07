be9c614b5db8a8fb16282a987aeb4d18d9106498
Rehan Shaikh <70072758+rydanstar01@users.noreply.github.com>
2022-10-08 02:51:25 +0530

    Update binary_insertion_sort.py

--- sorts/binary_insertion_sort.py
+++ sorts/binary_insertion_sort.py
@@ -18,7 +18,6 @@ python binary_insertion_sort.py
 
 """
 
-
 def binary_search(arr: list, length: int, key: int) -> int:
     """
     Pure implementation of binary search algorithm in Python
@@ -33,7 +32,7 @@ def binary_search(arr: list, length: int, key: int) -> int:
     2
     >>> binary_search([7, 3, 9, 5, 2], 4, 9)
     4
-
+    
     """
     low = 0
     high = length
@@ -45,9 +44,8 @@ def binary_search(arr: list, length: int, key: int) -> int:
             high = mid
     return low
 
-
-def insertion_sort(arr: list) -> list:
-    """Pure implementation of binary insertion sort algorithm in Python
+def insertion_sort(arr: list) -> list: 
+    """ Pure implementation of binary insertion sort algorithm in Python
 
     :param arr: list
     :return: list
@@ -55,12 +53,9 @@ def insertion_sort(arr: list) -> list:
     Examples:
     >>> insertion_sort([7, 3, 9, 5, 2])
     [2, 3, 5, 7, 9]
-    >>> insertion_sort([7, 3, 9, 5, 2]) == sorted([7, 3, 9, 5, 2])
-    True
-    >>> insertion_sort([]) == sorted([])
-    True
+    
     """
-    for i in range(1, len(arr)):
+    for i in range (1,len(arr)):
         key = arr[i]
         pos = binary_search(arr, i, key)
         j = i
@@ -70,11 +65,11 @@ def insertion_sort(arr: list) -> list:
         arr[pos] = key
     return arr
 
-
 if __name__ == "__main__":
     from doctest import testmod
 
     testmod()
+
     assert binary_search([7, 3, 9, 5, 2], 4, 5) == 2
     assert binary_search([7, 3, 9, 5, 2], 4, 9) == 4
 
