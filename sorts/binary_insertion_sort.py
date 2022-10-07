7ca81350515eda939e1942152adb365269e7753f
Rehan Shaikh <70072758+rydanstar01@users.noreply.github.com>
2022-10-08 02:45:59 +0530

    Update binary_insertion_sort.py

--- sorts/binary_insertion_sort.py
+++ sorts/binary_insertion_sort.py
@@ -18,7 +18,6 @@ python binary_insertion_sort.py
 
 """
 
-
 def binary_search(arr: list, length: int, key: int) -> int:
     """
     Pure implementation of binary search algorithm in Python
@@ -33,7 +32,7 @@ def binary_search(arr: list, length: int, key: int) -> int:
     2
     >>> binary_search([7, 3, 9, 5, 2], 4, 9)
     4
-
+    
     """
     low = 0
     high = length
@@ -45,9 +44,8 @@ def binary_search(arr: list, length: int, key: int) -> int:
             high = mid
     return low
 
-
-def insertion_sort(arr: list) -> list:
-    """Pure implementation of binary insertion sort algorithm in Python
+def insertion_sort(arr: list) -> list: 
+    """ Pure implementation of binary insertion sort algorithm in Python
 
     :param arr: list
     :return: list
@@ -59,18 +57,8 @@ def insertion_sort(arr: list) -> list:
     True
     >>> insertion_sort([]) == sorted([])
     True
-    >>> insertion_sort([-2, -45, -5]) == sorted([-2, -45, -5])
-    True
-    >>> insertion_sort([-23, 0, 6, -4, 34]) == sorted([-23, 0, 6, -4, 34])
-    True
-    >>> insertion_sort(['d', 'a', 'b', 'e', 'c']) == sorted(['d', 'a', 'b', 'e', 'c'])
-    True
-    >>> import random
-    >>> collection = random.sample(range(-50, 50), 50)
-    >>> insertion_sort(collection) == sorted(collection)
-    True
     """
-    for i in range(1, len(arr)):
+    for i in range (1,len(arr)):
         key = arr[i]
         pos = binary_search(arr, i, key)
         j = i
@@ -79,7 +67,6 @@ def insertion_sort(arr: list) -> list:
             j = j - 1
         arr[pos] = key
     return arr
-
 
 if __name__ == "__main__":
     from doctest import testmod
