abe00665a815ae732a8725fb073df1fec43d136c
Rahul-learner <66640990+Rahul-learner@users.noreply.github.com>
2022-10-08 12:51:13 -0700

    Update Resistance_In_Series_Calculator.py

--- electronics/Resistance_In_Series_Calculator.py
+++ electronics/Resistance_In_Series_Calculator.py
@@ -4,5 +4,5 @@ resistence = resistors.split(",")
 for i in range(0, len(resistence)):
     resistence[i] = int(resistence[i])
 
-total_Resistance = sum(resistence)
-print(f"Your total resistence is {total_Resistance} ohms.")
+total_resistance = sum(resistence)
+print(f"Your total resistence is {total_resistance} ohms.")
