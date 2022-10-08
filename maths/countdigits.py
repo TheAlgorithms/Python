898192af79761e270cd758b890b463cca2ec7f6d
kalmastenitin <nknckalmaste@gmail.com>
2022-10-08 09:06:40 +0530

    added doctest

--- maths/countdigits.py
+++ maths/countdigits.py
@@ -1,23 +1,23 @@
-def count_digits_recursive(n: int) -> int:
+def count_digits_recursive(num: int) -> int:
     """divide each digit by 10 so remainder will be that number"""
-    if n // 10 == 0:
+    if num // 10 == 0:
         return 1
-    return count_digits_recursive(int(n / 10)) + 1
+    return count_digits_recursive(int(num / 10)) + 1
 
 
-def count_digits(n: int) -> int:
+def count_digits(num: int) -> int:
     """non-recursive way of digit count"""
     count = 0
-    while n > 0:
-        n = int(n / 10)
+    while num > 0:
+        num = int(num / 10)
         count += 1
     return count
 
 
 if __name__ == "__main__":
-    n = 1568468461984216484351648351648864306186431654986465151846135
-    print(count_digits_recursive(n))
-    print(count_digits(n))
+    num = 1568468461984216484351648351648864306186431654986465151846135
+    print(count_digits_recursive(num))
+    print(count_digits(num))
     import doctest
 
     doctest.testmod()
