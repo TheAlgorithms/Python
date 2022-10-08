e22c1c5febc5dbefb8ff2a1b8a4abe7c96238ea9
lakshayroop5 <87693528+lavenroop5@users.noreply.github.com>
2022-10-09 05:17:27 +0530

    completed optimised code for job sequencing with deadline problem

---
+++ greedy_methods/job_sequencing_with_deadline.py
@@ -0,0 +1,33 @@
+# INPUT
+N = int(input())
+jobs = []
+for i in range(N):
+    jobs.append(list(map(int, input().split())))
+
+#OUTPUT
+# SORT JOBS ACCORDING TO THEIR PROFIT
+jobs.sort(key=lambda x: x[2], reverse=True)
+
+# FIND MAXIMUM DEADLINE
+max_deadline = max(jobs, key=lambda x: x[1])[1]
+
+# INITIALIZE SLOTS
+slots = [0] * max_deadline
+
+# FIND SLOTS FOR JOBS
+for job in jobs:
+    for i in range(job[1]-1, -1, -1):
+        if slots[i] == 0:
+            slots[i] = job[0]
+            break
+
+# FIND PROFIT AND COUNT OF JOBS
+count = 0
+profit = 0
+for i in slots:
+    if i != 0:
+        count += 1
+        profit += jobs[i-1][2]
+
+# PRINTING NUMBER OF JOBS AND MAXIMUM PROFIT      
+print(count, profit)
