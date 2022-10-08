7aabf8cc1ed929c35564c76d3efb2f950f44c58c
kalmastenitin <nknckalmaste@gmail.com>
2022-10-08 08:52:19 +0530

    added description

--- maths/countdigits.py
+++ maths/countdigits.py
@@ -1,12 +1,12 @@
-def count_digits_recursive(n):
-    # divide each digit by 10 so remainder will be that number
+def count_digits_recursive(n: int) -> int:
+    ''' divide each digit by 10 so remainder will be that number '''
     if n//10 == 0:
         return 1
     return count_digits_recursive(int(n/10)) + 1
 
 
-def count_digits(n):
-    # non-recursive way of digit count
+def count_digits(n: int) -> int:
+    ''' non-recursive way of digit count '''
     count = 0
     while n > 0:
         n = int(n/10)
@@ -18,3 +18,6 @@ if __name__ == "__main__":
     n = input("Enter a number: ")
     print(count_digits_recursive(n))
     print(count_digits(n))
+    import doctest
+
+    doctest.testmod()
