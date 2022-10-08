7e8f486058c6fbfc6d228bda047b8506f2daa2b9
kalmastenitin <nknckalmaste@gmail.com>
2022-10-08 08:41:28 +0530

    count number of digits in a number

---
+++ maths/countdigits.py
@@ -0,0 +1,20 @@
+def count_digits_recursive(n):
+    # divide each digit by 10 so remainder will be that number
+    if n//10 == 0:
+        return 1
+    return count_digits_recursive(int(n/10)) + 1
+
+
+def count_digits(n):
+    # non-recursive way of digit count
+    count = 0
+    while n > 0:
+        n = int(n/10)
+        count += 1
+    return count
+
+
+if __name__ == "__main__":
+    n = input("Enter a number: ")
+    print(count_digits_recursive(n))
+    print(count_digits(n))
