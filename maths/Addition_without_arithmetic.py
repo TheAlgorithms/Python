6900529ae370f11c8b93c6d1ccb7dfe1463ad083
kumarsurajsk <104374726+kumarsurajsk@users.noreply.github.com>
2022-10-08 19:44:15 +0530

    Addition_without_arithmetic

---
+++ maths/Addition_without_arithmetic.py
@@ -0,0 +1,16 @@
+first=int(input("Enter the number for first: "))
+sec=int(input("Enter the number for sec: "))
+def add(first,sec):   #Create a function
+    while(sec != 0):
+
+        c=first&sec
+
+        first=first^sec
+
+        sec=c<<1
+    return first
+
+    
+    
+print("Sum of two numbers",add(first,sec))  #call the function
+# Display sum of two numbers
