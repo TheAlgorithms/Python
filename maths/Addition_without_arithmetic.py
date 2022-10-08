c216f3ea404bf56eec5889132590c385c581b6b0
kumarsurajsk <104374726+kumarsurajsk@users.noreply.github.com>
2022-10-08 20:15:05 +0530

    added_param

--- maths/Addition_without_arithmetic.py
+++ maths/Addition_without_arithmetic.py
@@ -1,8 +1,26 @@
+"""
+Illustrate how to add the integer without arithmetic operation
+Author: suraj Kumar
+Time Complexity: 1
+"""
+
+
 first = int(input("Enter the number for first: "))
 sec = int(input("Enter the number for sec: "))
 
 
-def add(first, sec):  # Create a function
+def add(first, sec) -> int:  # Create a function
+    """
+    implementation of addition of integer
+    :param first: int
+    :param sec: int    
+    :return: int
+    Examples:
+    >>> add(3,5)
+    8
+    >>> add(13,5)
+    18
+    """
     while sec != 0:
 
         c = first & sec
