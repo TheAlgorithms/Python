db69183307d9a8f4b0e7df700a01c53db6f41df8
Karan Bhatt <64679205+kara751@users.noreply.github.com>
2022-10-09 12:06:41 +0530

    Rabin Karp using Python

---
+++ other/Rabin Karp.py
@@ -0,0 +1,39 @@
+d = 10
+
+
+def search(pattern, text, q):
+    m = len(pattern)
+    n = len(text)
+    p = 0
+    t = 0
+    h = 1
+    i = 0
+    j = 0
+
+    for i in range(m-1):
+        h = (h*d) % q
+    for i in range(m):
+        p = (d*p + ord(pattern[i])) % q
+        t = (d*t + ord(text[i])) % q
+
+    for i in range(n-m+1):
+        if p == t:
+            for j in range(m):
+                if text[i+j] != pattern[j]:
+                    break
+
+            j += 1
+            if j == m:
+                print("Pattern is found at position: " + str(i+1))
+
+        if i < n-m:
+            t = (d*(t-ord(text[i])*h) + ord(text[i+m])) % q
+
+            if t < 0:
+                t = t+q
+
+
+text = "ABCCDDAEFG"
+pattern = "CDD"
+q = 13
+search(pattern, text, q)
