9a44b4eddca6761ffbf5ac9480f1d0ac78ce2e4c
VARISH GAUTAM <48176176+Variiiest@users.noreply.github.com>
2022-10-09 17:12:15 +0530

    resolved

--- maths/weird_number.py
+++ maths/weird_number.py
@@ -1,7 +1,7 @@
 from math import sqrt
+from typing import List
 
-
-def factors(n):
+def factors(n : int) -> List[int]:
 
     v = []
     v.append(1)
@@ -15,57 +15,53 @@ def factors(n):
     return v
 
 
-def abundant(n):
-    sum = 0
-    v = factors(n)
-    for i in range(len(v)):
-        sum += v[i]
-    if sum > n:
-        return True
-    else:
-        return False
+def abundant(n : int) -> bool:
+	sum = 0
+	v = factors(n)
+	for i in range(len(v)):
+		sum += v[i]
+	if (sum > n):
+		return True
+	else:
+		return False
 
+def semi_perfect(n: int) -> bool:
+	v = factors(n)
+	v.sort(reverse = False)
+	r = len(v)
+	subset = [[0 for i in range(n + 1)]
+				for j in range(r + 1)]
+	for i in range(r + 1):
+		subset[i][0] = True
+	
+	for i in range(1, n + 1):
+		subset[0][i] = False
+	
+	for i in range(1, r + 1):
+		for j in range(1, n + 1):
+			if j < v[i - 1]:
+				subset[i][j] = subset[i - 1][j]
+			else:
+				subset[i][j] = subset[i - 1][j] or subset[i - 1][j - v[i - 1]]
+				
+	if (subset[r][n]) == 0:
+		return False
+	else:
+		 return True
 
-def semiPerfect(n):
-    v = factors(n)
-    v.sort(reverse=False)
-    r = len(v)
-    subset = [[0 for i in range(n + 1)] for j in range(r + 1)]
+def weird(n : int) -> bool:
+	if (abundant(n) == True and semi_perfect(n) == False):
+		return True
+	else:
+		return False
 
-    for i in range(r + 1):
-        subset[i][0] = True
-
-    for i in range(1, n + 1):
-        subset[0][i] = False
-
-    for i in range(1, r + 1):
-        for j in range(1, n + 1):
-            if j < v[i - 1]:
-                subset[i][j] = subset[i - 1][j]
-            else:
-                subset[i][j] = subset[i - 1][j] or subset[i - 1][j - v[i - 1]]
-
-    if (subset[r][n]) == 0:
-        return False
-    else:
-        return True
-
-
-def weird(n):
-    if abundant(n) == True and semiPerfect(n) == False:
-        return True
-    else:
-        return False
-
-
-def main():
-    n = 70
-    if weird(n):
-        print("Weird Number")
-    else:
-        print("Not Weird Number")
-
-
+def main() -> None:
+	n = 70
+	if (weird(n)):
+		print("Weird Number")
+	else:
+		print("Not Weird Number")
+    
 if __name__ == "__main__":
     import doctest
 
