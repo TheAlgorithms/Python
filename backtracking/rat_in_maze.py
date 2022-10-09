4308e04c12571e2a4b99514e591445a295b0a70d
ADITYA DUBEY <75172576+adidubs@users.noreply.github.com>
2022-10-09 11:18:25 +0530

    Update rat_in_maze.py

--- backtracking/rat_in_maze.py
+++ backtracking/rat_in_maze.py
@@ -1,118 +1,52 @@
-from __future__ import annotations
-
-
-def solve_maze(maze: list[list[int]]) -> bool:
-    """
-    This method solves the "rat in maze" problem.
-    In this problem we have some n by n matrix, a start point and an end point.
-    We want to go from the start to the end. In this matrix zeroes represent walls
-    and ones paths we can use.
-    Parameters :
-        maze(2D matrix) : maze
-    Returns:
-        Return: True if the maze has a solution or False if it does not.
-    >>> maze = [[0, 1, 0, 1, 1],
-    ...         [0, 0, 0, 0, 0],
-    ...         [1, 0, 1, 0, 1],
-    ...         [0, 0, 1, 0, 0],
-    ...         [1, 0, 0, 1, 0]]
-    >>> solve_maze(maze)
-    [1, 0, 0, 0, 0]
-    [1, 1, 1, 1, 0]
-    [0, 0, 0, 1, 0]
-    [0, 0, 0, 1, 1]
-    [0, 0, 0, 0, 1]
-    True
-
-    >>> maze = [[0, 1, 0, 1, 1],
-    ...         [0, 0, 0, 0, 0],
-    ...         [0, 0, 0, 0, 1],
-    ...         [0, 0, 0, 0, 0],
-    ...         [0, 0, 0, 0, 0]]
-    >>> solve_maze(maze)
-    [1, 0, 0, 0, 0]
-    [1, 0, 0, 0, 0]
-    [1, 0, 0, 0, 0]
-    [1, 0, 0, 0, 0]
-    [1, 1, 1, 1, 1]
-    True
-
-    >>> maze = [[0, 0, 0],
-    ...         [0, 1, 0],
-    ...         [1, 0, 0]]
-    >>> solve_maze(maze)
-    [1, 1, 1]
-    [0, 0, 1]
-    [0, 0, 1]
-    True
-
-    >>> maze = [[0, 1, 0],
-    ...         [0, 1, 0],
-    ...         [1, 0, 0]]
-    >>> solve_maze(maze)
-    No solution exists!
-    False
-
-    >>> maze = [[0, 1],
-    ...         [1, 0]]
-    >>> solve_maze(maze)
-    No solution exists!
-    False
-    """
-    size = len(maze)
-    # We need to create solution object to save path.
-    solutions = [[0 for _ in range(size)] for _ in range(size)]
-    solved = run_maze(maze, 0, 0, solutions)
-    if solved:
-        print("\n".join(str(row) for row in solutions))
-    else:
-        print("No solution exists!")
-    return solved
-
-
-def run_maze(maze: list[list[int]], i: int, j: int, solutions: list[list[int]]) -> bool:
-    """
-    This method is recursive starting from (i, j) and going in one of four directions:
-    up, down, left, right.
-    If a path is found to destination it returns True otherwise it returns False.
-    Parameters:
-        maze(2D matrix) : maze
-        i, j : coordinates of matrix
-        solutions(2D matrix) : solutions
-    Returns:
-        Boolean if path is found True, Otherwise False.
-    """
-    size = len(maze)
-    # Final check point.
-    if i == j == (size - 1):
-        solutions[i][j] = 1
+N = 4
+def printSolution( sol ):
+     
+    for i in sol:
+        for j in i:
+            print(str(j) + " ", end ="")
+        print("")
+def isSafe( maze, x, y ):
+     
+    if x >= 0 and x < N and y >= 0 and y < N and maze[x][y] == 1:
         return True
-
-    lower_flag = (not (i < 0)) and (not (j < 0))  # Check lower bounds
-    upper_flag = (i < size) and (j < size)  # Check upper bounds
-
-    if lower_flag and upper_flag:
-        # check for already visited and block points.
-        block_flag = (not (solutions[i][j])) and (not (maze[i][j]))
-        if block_flag:
-            # check visited
-            solutions[i][j] = 1
-
-            # check for directions
-            if (
-                run_maze(maze, i + 1, j, solutions)
-                or run_maze(maze, i, j + 1, solutions)
-                or run_maze(maze, i - 1, j, solutions)
-                or run_maze(maze, i, j - 1, solutions)
-            ):
-                return True
-
-            solutions[i][j] = 0
-            return False
+     
     return False
-
+ 
+def solveMaze( maze ):
+     
+    # Creating a 4 * 4 2-D list
+    sol = [ [ 0 for j in range(4) ] for i in range(4) ]
+     
+    if solveMazeUtil(maze, 0, 0, sol) == False:
+        print("Solution doesn't exist");
+        return False
+     
+    printSolution(sol)
+    return True
+def solveMazeUtil(maze, x, y, sol):
+   
+    if x == N - 1 and y == N - 1:
+        sol[x][y] = 1
+        return True
+         
+  
+    if isSafe(maze, x, y) == True:
+        sol[x][y] = 1
+         
+        if solveMazeUtil(maze, x + 1, y, sol) == True:
+            return True
+             
+        if solveMazeUtil(maze, x, y + 1, sol) == True:
+            return True
+         
+        sol[x][y] = 0
+        return False
 
 if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod()
+    # Initialising the maze
+    maze = [ [1, 0, 0, 0],
+             [1, 1, 0, 1],
+             [0, 1, 0, 0],
+             [1, 1, 1, 1] ]
+              
+    solveMaze(maze)
