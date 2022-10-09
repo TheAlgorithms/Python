51d9345a96c8a0f42a5d9820d183cced22f38025
Lakshay Roopchandani <75477853+lakshayroop5@users.noreply.github.com>
2022-10-09 07:23:25 +0530

    completed optimized code for job sequencing with deadline problem

--- greedy_methods/job_sequencing_with_deadline.py
+++ greedy_methods/job_sequencing_with_deadline.py
@@ -7,12 +7,12 @@ python3 -m doctest -v greedy_merge_sort.py
 """
 
 # FUNCTION
-def job_sequencing_with_deadlines(numJobs: int, jobs: list) -> list:
+def job_sequencing_with_deadlines(num_jobs: int, jobs: list) -> list:
     """
     Function to find the maximum profit by doing jobs in a given time frame
 
     Args:
-        numJobs [int]: Number of jobs
+        num_jobs [int]: Number of jobs
         jobs [list]: A list of tuples of (job_id, deadline, profit)
 
     Returns:
