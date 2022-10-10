5d1ee491fb3ac59e31b3cc27201674480a608889
Aryan Mankame <87220756+AryanMankame@users.noreply.github.com>
2022-10-11 02:46:26 +0530

    Create sleepsort.py
    added sleepsort.py

---
+++ sorts/sleepsort.py
@@ -0,0 +1,11 @@
+import thread
+from time import sleep
+items = []
+n = int(input())
+for i in range(0,n):
+  a = int(input())
+  items.append(a)
+def sleep_sort(i):
+        sleep(i)
+        print i
+[thread.start_new_thread(sleep_sort, (i,)) for i in items]
