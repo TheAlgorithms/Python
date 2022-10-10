f696db71b88a231f8e60f49e2f7711cc85709c5f
Aryan Mankame <87220756+AryanMankame@users.noreply.github.com>
2022-10-10 21:10:32 +0530

    created longestvalidparanthesis.py
    The code for longest valid paranthesis is added!

---
+++ strings/longestvalidparanthesis.py
@@ -0,0 +1,21 @@
+def longestValidParentheses(self, s: str) -> int:
+        stack = []
+        preceeding_matched = 0
+        res = 0
+        for char in s:
+            if char == "(":
+                stack.append(preceeding_matched)
+                preceeding_matched = 0
+            else:
+                if stack:
+                    preceeding_matched += 1 + stack.pop()
+                else:
+                    res = max(res, preceeding_matched)
+                    preceeding_matched = 0
+        
+        res = max(res, preceeding_matched)
+        
+        while stack:
+            res = max(res, stack.pop())
+        
+        return res * 2
