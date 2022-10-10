64cef2effd49b160bd18d245829f5e42f3d16000
karthik <karthik.ayangar7118@gmail.com>
2022-10-10 23:54:43 +0530

    Modified - Matrix/inverse_of_matrix.py - added implementation for inverse of 3x3 matrix

--- matrix/inverse_of_matrix.py
+++ matrix/inverse_of_matrix.py
@@ -6,13 +6,14 @@ from decimal import Decimal
 def inverse_of_matrix(matrix: list[list[float]]) -> list[list[float]]:
     """
     A matrix multiplied with its inverse gives the identity matrix.
-    This function finds the inverse of a 2x2 matrix.
+    This function finds the inverse of a 2x2 and 3x3 matrix.
     If the determinant of a matrix is 0, its inverse does not exist.
 
     Sources for fixing inaccurate float arithmetic:
     https://stackoverflow.com/questions/6563058/how-do-i-use-accurate-float-arithmetic-in-python
     https://docs.python.org/3/library/decimal.html
 
+    Demo for 2x2
     >>> inverse_of_matrix([[2, 5], [2, 0]])
     [[0.0, 0.5], [0.2, -0.2]]
     >>> inverse_of_matrix([[2.5, 5], [1, 2]])
@@ -25,24 +26,73 @@ def inverse_of_matrix(matrix: list[list[float]]) -> list[list[float]]:
     [[0.16666666666666666, -0.0625], [-0.3333333333333333, 0.25]]
     >>> inverse_of_matrix([[10, 5], [3, 2.5]])
     [[0.25, -0.5], [-0.3, 1.0]]
+
+    Demo for 3x3
+    >>> inverse_of_matrix([[2, 5, 7], [2, 0, 1], [1, 2, 3]])
+    [[2, 5, -4], [5, 1, -1], [4, 1, -10]]
+    >>> inverse_of_matrix([[1, 2, 2], [1, 2, 2], [3, 2, -1])
+    Traceback (most recent call last):
+    ...
+    ValueError: This matrix has no inverse.
     """
 
     D = Decimal  # An abbreviation for conciseness
 
     # Check if the provided matrix has 2 rows and 2 columns
     # since this implementation only works for 2x2 matrices
-    if len(matrix) != 2 or len(matrix[0]) != 2 or len(matrix[1]) != 2:
-        raise ValueError("Please provide a matrix of size 2x2.")
+    if len(matrix) == 2 and len(matrix[0]) == 2 and len(matrix[1]) == 2:
+        # Calculate the determinant of the matrix
+        determinant = D(matrix[0][0]) * D(matrix[1][1]) - D(matrix[1][0]) * D(matrix[0][1])
+        if determinant == 0:
+            raise ValueError("This matrix has no inverse.")
 
-    # Calculate the determinant of the matrix
-    determinant = D(matrix[0][0]) * D(matrix[1][1]) - D(matrix[1][0]) * D(matrix[0][1])
-    if determinant == 0:
-        raise ValueError("This matrix has no inverse.")
+        # Creates a copy of the matrix with swapped positions of the elements
+        swapped_matrix = [[0.0, 0.0], [0.0, 0.0]]
+        swapped_matrix[0][0], swapped_matrix[1][1] = matrix[1][1], matrix[0][0]
+        swapped_matrix[1][0], swapped_matrix[0][1] = -matrix[1][0], -matrix[0][1]
 
-    # Creates a copy of the matrix with swapped positions of the elements
-    swapped_matrix = [[0.0, 0.0], [0.0, 0.0]]
-    swapped_matrix[0][0], swapped_matrix[1][1] = matrix[1][1], matrix[0][0]
-    swapped_matrix[1][0], swapped_matrix[0][1] = -matrix[1][0], -matrix[0][1]
+        # Calculate the inverse of the matrix
+        return [[float(D(n) / determinant) or 0.0 for n in row] for row in swapped_matrix]
+    elif len(matrix) == 3 and len(matrix[0]) == 3 and len(matrix[1]) == 3 and len(matrix[2]) == 3:
+        # Calculate the determinant of the matrix using sarrus rule
+        determinant = ((D(matrix[0][0]) * D(matrix[1][1]) * D(matrix[2][2])) + (
+                    D(matrix[0][1]) * D(matrix[1][2]) * D(matrix[2][0])) + (
+                                   D(matrix[0][2]) * D(matrix[1][0]) * D(matrix[2][1]))) - (
+                                  (D(matrix[0][2]) * D(matrix[1][1]) * D(matrix[2][0])) + (
+                                      D(matrix[0][1]) * D(matrix[1][0]) * D(matrix[2][2])) + (
+                                              D(matrix[0][0]) * D(matrix[1][2]) * D(matrix[2][1])))
+        if determinant == 0:
+            raise ValueError("This matrix has no inverse.")
 
-    # Calculate the inverse of the matrix
-    return [[float(D(n) / determinant) or 0.0 for n in row] for row in swapped_matrix]
+        # Creating cofactor matrix
+        cofactor_matrix = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]
+        cofactor_matrix[0][0] = (D(matrix[1][1]) * D(matrix[2][2])) - (D(matrix[1][2]) * D(matrix[2][1]))
+        cofactor_matrix[0][1] = -((D(matrix[1][0]) * D(matrix[2][2])) - (D(matrix[1][2]) * D(matrix[2][0])))
+        cofactor_matrix[0][2] = (D(matrix[1][0]) * D(matrix[2][1])) - (D(matrix[1][1]) * D(matrix[2][0]))
+        cofactor_matrix[1][0] = -((D(matrix[0][1]) * D(matrix[2][2])) - (D(matrix[0][2]) * D(matrix[2][1])))
+        cofactor_matrix[1][1] = (D(matrix[0][0]) * D(matrix[2][2])) - (D(matrix[0][2]) * D(matrix[2][0]))
+        cofactor_matrix[1][2] = -((D(matrix[0][0]) * D(matrix[2][1])) - (D(matrix[0][1]) * D(matrix[2][0])))
+        cofactor_matrix[2][0] = (D(matrix[0][1]) * D(matrix[1][2])) - (D(matrix[0][2]) * D(matrix[1][1]))
+        cofactor_matrix[2][1] = -((D(matrix[0][0]) * D(matrix[1][2])) - (D(matrix[0][2]) * D(matrix[1][0])))
+        cofactor_matrix[2][2] = (D(matrix[0][0]) * D(matrix[1][1])) - (D(matrix[0][1]) * D(matrix[1][0]))
+
+        # Transpose the cofactor matrix (Adjoint matrix)
+        adjoint_matrix = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]
+        for i in range(3):
+            for j in range(3):
+                adjoint_matrix[i][j] = cofactor_matrix[j][i]
+
+        # Inverse of the matrix using the formula (1/determinant) * adjoint matrix = Inverse of matrix
+        inverse_matrix = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]
+        for i in range(3):
+            for j in range(3):
+                inverse_matrix[i][j] = (adjoint_matrix[i][j]) / (determinant)
+
+        # Calculate the inverse of the matrix
+        return [[float(D(n)) or 0.0 for n in row] for row in inverse_matrix]
+    else:
+        raise ValueError("Please provide a matrix of size 2x2 or 3x3.")
+
+
+
+
