9f9bf37bd67d2c0470eabe399c6cf648d3d96187
Shubham Kondekar <kondekarshubham123@gmail.com>
2022-10-10 21:46:08 +0530

    Binary tree problem, height of Binary tree solution added

---
+++ data_structures/binary_tree/height_of_binary_tree.py
@@ -0,0 +1,53 @@
+# Given the root of a binary tree, return its maximum depth.
+# A binary tree's maximum depth is the number of nodes along the longest
+# path from the root node down to the farthest leaf node.
+
+
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def height_of_tree(root: Optional[TreeNode], depth=0) -> int:
+    """
+    Function returns the height of tree
+
+    >>> height_of_tree(None)
+    0
+    """
+    # Base condition of recurrsion, if the root is not present return the current depth
+    if not root:
+        return depth
+    # Same like dfs, go as much as you can in depth and return the maximum possible depth
+    return max(
+        height_of_tree(root.left, depth + 1),
+        height_of_tree(root.right, depth + 1))
+
+
+# Example 1
+
+"""
+    3       ->  1
+   / \
+  9   20    ->  2
+     /  \
+    15   7  ->  3
+"""
+tree_1 = TreeNode(3)
+tree_1.left = TreeNode(9)
+tree_1.right = TreeNode(20)
+tree_1.right.left = TreeNode(15)
+tree_1.right.right = TreeNode(7)
+
+print(height_of_tree(tree_1))  # Output: 3
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
