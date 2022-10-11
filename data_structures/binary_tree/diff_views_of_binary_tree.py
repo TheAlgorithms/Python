51d1918d710ab403ff0646e89245aaee41d8caf6
Shubham Kondekar <kondekarshubham123@gmail.com>
2022-10-11 22:46:10 +0530

    Cases from if block removed

--- data_structures/binary_tree/diff_views_of_binary_tree.py
+++ data_structures/binary_tree/diff_views_of_binary_tree.py
@@ -250,15 +250,4 @@ def binary_tree_bottom_side_view(root: TreeNode | None) -> list[int]:
 if __name__ == "__main__":
     import doctest
 
-    tree_1 = TreeNode(3)
-    tree_1.left = TreeNode(9)
-    tree_1.right = TreeNode(20)
-    tree_1.right.left = TreeNode(15)
-    tree_1.right.right = TreeNode(7)
-
-    print(binary_tree_right_side_view(tree_1))  # Output: [3, 20, 7]
-    print(binary_tree_left_side_view(tree_1))  # Output: [3, 9, 15]
-    print(binary_tree_top_side_view(tree_1))  # Output: [9, 3, 20, 7]
-    print(binary_tree_bottom_side_view(tree_1))  # Output: [9, 15, 20, 7]
-
     doctest.testmod()
