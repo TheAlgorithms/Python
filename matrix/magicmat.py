78165d4f547c8734dde083563e4e0037edeb0fb0
sahilb16 <77878647+sahilb16@users.noreply.github.com>
2022-10-11 12:35:05 +0530

    Magic Matrix Checker
    This algorithm checks if a matrix is Magic Matrix or not

--- matrix/magicmat.py
+++ matrix/magicmat.py
@@ -10,56 +10,62 @@ A n×n square matrix of positive integers is called a magic square if the follow
 
 
 """
-
-
 def magic(mat):
-
+    
     # first get the dimension of the matrix
-
+    
     m = len(mat)
 
+    
     # the sum of the two diagonals
     d1sum, d2sum = 0, 0
-
+    
     # (i, i) goes from top-left -> bottom-right
     # (i, m - i - 1) goes from top-right -> bottom-left
     # note that a single loop is enough; no nesting required
-
+    
     for i in range(m):
-
+        
         d1sum += mat[i][i]
-
+        
         d2sum += mat[i][m - i - 1]
-
+        
     # if the two diagonal sums are unequal, we can return NO
-    # unnecessary computation can be avoided
-
-    if not (d1sum == d2sum):
-        return "NO"
-
+ 	# unnecessary computation can be avoided
+ 	
+    if not(d1sum == d2sum):
+        return 'NO'
+    
     # get row-sum and column-sum
-
+    
     for i in range(m):
         rsum, csum = 0, 0
-
+        
         for j in range(m):
             rsum += mat[i][j]
             csum += mat[j][i]
-
-        if not (rsum == csum == d1sum):
-            return "NO"
-
+            
+        if not(rsum == csum == d1sum):
+            return 'NO'
+        
     # if the code reaches this level
     # then all requirements of a magic-square are satisfied
     # so we can safely return YES
+    
+    return 'YES'
+    # return type of the function is a string
 
-    return "YES"
 
-
-mat = [[1, 2], [2, 1]]
+# Test Cases
+mat=[[1,2],
+     [2,1]]
 print(magic(mat))
 
-# OUTPUT -> NO
-
-mat = [[4, 9, 2], [3, 5, 7], [8, 1, 6]]
+mat=[[4,9,2],
+     [3,5,7],
+     [8,1,6]]
 print(magic(mat))
+
+"""    OUTPUT;
+>>>NO
+   YES
