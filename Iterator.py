1ec383fcd8162f6a3aa44db15e7c25840d1f75b1
Shardendu Kumar <72930487+shardendu930@users.noreply.github.com>
2022-10-11 22:37:15 +0530

    Create Iterator.py
    created iterator in python

---
+++ Iterator.py
@@ -0,0 +1,24 @@
+# define a list
+my_list = [4, 7, 0, 3]
+
+# get an iterator using iter()
+my_iter = iter(my_list)
+
+# iterate through it using next()
+
+# Output: 4
+print(next(my_iter))
+
+# Output: 7
+print(next(my_iter))
+
+# next(obj) is same as obj.__next__()
+
+# Output: 0
+print(my_iter.__next__())
+
+# Output: 3
+print(my_iter.__next__())
+
+# This will raise error, no items left
+next(my_iter)
