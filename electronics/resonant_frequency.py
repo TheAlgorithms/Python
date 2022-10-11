75d431105230430e6942e36d0aa38740b2173e9f
Shashank Kashyap <50551759+SKVKPandey@users.noreply.github.com>
2022-10-11 13:59:26 +0530

    Resonant Frequency of LC Circuit

--- electronics/resonant_frequency.py
+++ electronics/resonant_frequency.py
@@ -1,8 +1,9 @@
 # https://en.wikipedia.org/wiki/LC_circuit
-
 from __future__ import annotations
+
+from math import pi as PI
 from math import sqrt
-from math import pi
+
 
 def resonant_frequency(inductance: float, capacitance: float) -> tuple:
 
@@ -12,21 +13,29 @@ def resonant_frequency(inductance: float, capacitance: float) -> tuple:
 
     Examples are given below:
     >>> resonant_frequency(inductance=0, capacitance=5)
+    Traceback (most recent call last):
+      ...
     ValueError: Inductance cannot be 0 or negative
     >>> resonant_frequency(inductance=10, capacitance=0)
+    Traceback (most recent call last):
+      ...
     ValueError: Capacitance cannot be 0 or negative
     >>> resonant_frequency(inductance=10, capacitance=5)
-    0.022507907903927652
+    ('resonant_frequency', 0.022507907903927652)
     """
 
-    if inductance<=0:
+    if inductance <= 0:
         raise ValueError("Inductance cannot be 0 or negative")
 
-    elif capacitance<=0:
+    elif capacitance <= 0:
         raise ValueError("Capacitance cannot be 0 or negative")
 
     else:
-        return(float(1/(2*pi*(sqrt(inductance*capacitance)))))
+        return (
+            "resonant_frequency",
+            float(1 / (2 * PI * (sqrt(inductance * capacitance)))),
+        )
+
 
 if __name__ == "__main__":
     import doctest
