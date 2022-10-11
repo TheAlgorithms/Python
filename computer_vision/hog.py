b7691cc1e47d72a444a240beecc76a698f3c0b84
Juan Emilio Martínez <33232217+Juane99@users.noreply.github.com>
2022-10-12 00:41:44 +0200

    Update hog.py

--- computer_vision/hog.py
+++ computer_vision/hog.py
@@ -5,7 +5,6 @@ import math
 import os
 import random
 from time import time
-
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
@@ -15,134 +14,157 @@ import tensorflow as tf
 from sklearn.metrics import confusion_matrix
 from sklearn.svm import SVC
 
+"""
+Histogram of oriented gradients
+https://en.wikipedia.org/wiki/Histogram_of_oriented_gradients
+"""
+
 random.seed(1234)
 
 # Establecemos una semilla
 random.seed(1234)
 
-# Función que calcula el HOG de una imagen
-def HOG(imagen):
+class Histogram_Oriented_Gradients:
 
-    # Pasamos la imagen a float (escala 0-1)
-    imagen = np.float32(imagen) / 255.0
+    def __init__(self, imagen: np.float32):
+        """
+        imagen : numpy array of float values
+        """
+        self.imagen = imagen
 
-    # Comenzamos obteniendo las imágenes gradiente. Para ello
-    # convolucionamos la imagen con kernels Sobel 1D.
+    def HOG() -> np.float32:
 
-    Gx = cv2.Sobel(imagen, cv2.CV_32F, 1, 0, ksize=1)
-    Gy = cv2.Sobel(imagen, cv2.CV_32F, 0, 1, ksize=1)
+        """
+            Returns the descriptors vector of am inage
+            output : list of the descriptors values
+        """
 
-    # Calculamos la magnitud
-    magnitud = np.sqrt(Gx**2 + Gy**2)
+        # Pasamos la imagen a float (escala 0-1)
+        imagen = np.float32(imagen) / 255.0
 
-    # Calculamos la orientacion
-    orientacion = np.abs(np.arctan2(Gy, Gx) * 180 / np.pi)
+        # Comenzamos obteniendo las imágenes gradiente. Para ello
+        # convolucionamos la imagen con kernels Sobel 1D.
 
-    # Ahora debemos calcular un histograma de orientación de gradiente
-    # por cada célula 8x8 de nuestra imagen. Para ello crearemos una
-    # matriz de listas que iremos rellenando.
+        Gx = cv2.Sobel(imagen, cv2.CV_32F, 1, 0, ksize=1)
+        Gy = cv2.Sobel(imagen, cv2.CV_32F, 0, 1, ksize=1)
 
-    hog = [
-        [0 for x in range(int(imagen.shape[1] / 8))]
-        for y in range(int(imagen.shape[0] / 8))
-    ]
+        # Calculamos la magnitud
+        magnitud = np.sqrt(Gx**2 + Gy**2)
 
-    # Recorremos cada célula
-    for indice1, r in enumerate(range(0, imagen.shape[0], 8)):
-        for indice2, c in enumerate(range(0, imagen.shape[1], 8)):
+        # Calculamos la orientacion
+        orientacion = np.abs(np.arctan2(Gy, Gx) * 180 / np.pi)
 
-            # Inicializamos el hog de la célula actual
-            hog_aux = [0.0 for i in range(0, 10)]
-            hog_angles = [10, 30, 50, 70, 90, 110, 130, 150, 170, 180]
-            mag_aux = magnitud[r : r + 8, c : c + 8, 1]
-            ori_aux = orientacion[r : r + 8, c : c + 8, 1]
+        # Ahora debemos calcular un histograma de orientación de gradiente
+        # por cada célula 8x8 de nuestra imagen. Para ello crearemos una
+        # matriz de listas que iremos rellenando.
 
-            # Recorremos la célula
-            for i in range(mag_aux.shape[0]):
-                for j in range(mag_aux.shape[1]):
+        hog = [
+            [0 for x in range(int(imagen.shape[1] / 8))]
+            for y in range(int(imagen.shape[0] / 8))
+        ]
 
-                    # Vemos cual es el ángulo más cercano de la lista de hog_angles
-                    indx = (np.abs(hog_angles - ori_aux[i][j])).argmin()
+        # Recorremos cada célula
+        for indice1, r in enumerate(range(0, imagen.shape[0], 8)):
+            for indice2, c in enumerate(range(0, imagen.shape[1], 8)):
 
-                    # Si coincide con el ángulo, añadimos la magnitud del pixel al hog
-                    if ori_aux[i][j] == hog_angles[indx]:
-                        hog_aux[indx] += mag_aux[i][j]
+                # Inicializamos el hog de la célula actual
+                hog_aux = [0.0 for i in range(0, 10)]
+                hog_angles = [10, 30, 50, 70, 90, 110, 130, 150, 170, 180]
+                mag_aux = magnitud[r : r + 8, c : c + 8, 1]
+                ori_aux = orientacion[r : r + 8, c : c + 8, 1]
 
-                    # Si no coincide, dividimos la magnitud entre los ángulos adyacentes
-                    else:
-                        # Si el ángulo es menor
-                        if ori_aux[i][j] < hog_angles[indx]:
+                # Recorremos la célula
+                for i in range(mag_aux.shape[0]):
+                    for j in range(mag_aux.shape[1]):
 
-                            hog_aux[indx - 1] += (
-                                mag_aux[i][j]
-                                * (hog_angles[indx] - ori_aux[i][j])
-                                / (hog_angles[indx] - hog_angles[indx - 1])
-                            )
+                        # Vemos cual es el ángulo más cercano de la lista de hog_angles
+                        indx = (np.abs(hog_angles - ori_aux[i][j])).argmin()
 
-                            hog_aux[indx] += (
-                                mag_aux[i][j]
-                                * (ori_aux[i][j] - hog_angles[indx - 1])
-                                / (hog_angles[indx] - hog_angles[indx - 1])
-                            )
+                        # Si coincide con el ángulo, añadimos la magnitud del pixel al hog
+                        if ori_aux[i][j] == hog_angles[indx]:
+                            hog_aux[indx] += mag_aux[i][j]
 
-                        # Si el ángulo es mayor
+                        # Si no coincide, dividimos la magnitud entre los ángulos adyacentes
                         else:
+                            # Si el ángulo es menor
+                            if ori_aux[i][j] < hog_angles[indx]:
 
-                            hog_aux[indx] += (
-                                mag_aux[i][j]
-                                * (hog_angles[indx + 1] - ori_aux[i][j])
-                                / (hog_angles[indx + 1] - hog_angles[indx])
-                            )
+                                hog_aux[indx - 1] += (
+                                    mag_aux[i][j]
+                                    * (hog_angles[indx] - ori_aux[i][j])
+                                    / (hog_angles[indx] - hog_angles[indx - 1])
+                                )
 
-                            hog_aux[indx + 1] += (
-                                mag_aux[i][j]
-                                * (ori_aux[i][j] - hog_angles[indx])
-                                / (hog_angles[indx + 1] - hog_angles[indx])
-                            )
+                                hog_aux[indx] += (
+                                    mag_aux[i][j]
+                                    * (ori_aux[i][j] - hog_angles[indx - 1])
+                                    / (hog_angles[indx] - hog_angles[indx - 1])
+                                )
 
-            # Almacenamos en hog los valores de hog_aux
-            hog[indice1][indice2] = hog_aux
+                            # Si el ángulo es mayor
+                            else:
 
-    # Agrupamos los histogramas/célula en bloques de histogramas. Cada bloque estará formado
-    # por 4 células.
-    hog_bloques = []
-    for i in range(0, int(imagen.shape[0] / 8) - 1):
-        for j in range(0, int(imagen.shape[1] / 8) - 1):
+                                hog_aux[indx] += (
+                                    mag_aux[i][j]
+                                    * (hog_angles[indx + 1] - ori_aux[i][j])
+                                    / (hog_angles[indx + 1] - hog_angles[indx])
+                                )
 
-            aux = []
+                                hog_aux[indx + 1] += (
+                                    mag_aux[i][j]
+                                    * (ori_aux[i][j] - hog_angles[indx])
+                                    / (hog_angles[indx + 1] - hog_angles[indx])
+                                )
 
-            aux = aux + hog[i][j]
-            aux = aux + hog[i][j + 1]
-            aux = aux + hog[i + 1][j]
-            aux = aux + hog[i + 1][j + 1]
+                # Almacenamos en hog los valores de hog_aux
+                hog[indice1][indice2] = hog_aux
 
-            hog_bloques.append(aux)
+        # Agrupamos los histogramas/célula en bloques de histogramas. Cada bloque estará formado
+        # por 4 células.
+        hog_bloques = []
+        for i in range(0, int(imagen.shape[0] / 8) - 1):
+            for j in range(0, int(imagen.shape[1] / 8) - 1):
 
-    # Normalizamos el hog por bloques
-    for i, bloque in enumerate(hog_bloques):
+                aux = []
 
-        # Calculamos el modulo del bloque actual
+                aux = aux + hog[i][j]
+                aux = aux + hog[i][j + 1]
+                aux = aux + hog[i + 1][j]
+                aux = aux + hog[i + 1][j + 1]
+
+                hog_bloques.append(aux)
+
+        # Normalizamos el hog por bloques
+        for i, bloque in enumerate(hog_bloques):
+
+            # Calculamos el modulo del bloque actual
+            modulo = 0.0
+            for el in bloque:
+                modulo = modulo + el**2
+
+            modulo = math.sqrt(modulo + 0.01)
+
+            # Dividimos el bloque actual entre el módulo
+            hog_bloques[i] = [x / modulo for x in bloque]
+
+        # Concatenamos todos los elementos del hog de bloques
+        # para obtener el vector de descriptores
+        vector_descriptores = [x for l in hog_bloques for x in l]
+
+        # Normalizamos el vector de descriptores
         modulo = 0.0
-        for el in bloque:
+        for el in vector_descriptores:
             modulo = modulo + el**2
 
         modulo = math.sqrt(modulo + 0.01)
 
-        # Dividimos el bloque actual entre el módulo
-        hog_bloques[i] = [x / modulo for x in bloque]
+        # Dividimos cada elemento del vector entre el módulo
+        vector_descriptores = [x / modulo for x in vector_descriptores]
 
-    # Concatenamos todos los elementos del hog de bloques
-    # para obtener el vector de descriptores
-    vector_descriptores = [x for l in hog_bloques for x in l]
+        return vector_descriptores
 
-    # Normalizamos el vector de descriptores
-    modulo = 0.0
-    for el in vector_descriptores:
-        modulo = modulo + el**2
+if __name__ == "__main__":
 
-    modulo = math.sqrt(modulo + 0.01)
-
-    # Dividimos cada elemento del vector entre el módulo
-    vector_descriptores = [x / modulo for x in vector_descriptores]
-
-    return vector_descriptores
+    im = cv2.imread('path',1)
+    hog = Histogram_Oriented_Gradients(im)
+    descriptors = hog.HOG()
