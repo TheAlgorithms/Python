4b455d51cdc94aed3efbb255f30748898053dbfa
Biswajit Panda <75068508+biswajitafk@users.noreply.github.com>
2022-10-12 14:19:14 +0530

    Create divisibility_test.py

---
+++ maths/divisibility_test.py
@@ -0,0 +1,11 @@
+num = int (input (“Enter the number whose divisibility needs to be checked:”))
+
+div = int (input (“Enter the number with which divisibility needs to be checked:”))
+
+if num%div == 0:
+
+              print (“The number is divisible.”)
+
+else:
+
+              print (“The number is not divisible.”)
