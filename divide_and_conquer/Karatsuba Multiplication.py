6613f86e2f1b9ab97766b5e5d94b42d44a2532f0
Devakrishna-Sanil-Kumar <86128166+Devakrishna-Sanil-Kumar@users.noreply.github.com>
2022-10-12 22:15:22 +0530

    Uploaded KaratsubaMultiplication.py
    The Karatsuba algorithm is a fast multiplication algorithm that uses a divide and conquer approach to multiply two numbers.

---
+++ divide_and_conquer/Karatsuba Multiplication.py
@@ -0,0 +1,26 @@
+def karatsuba(x, y):
+    n = len(x)
+    if n < 2:
+        return int(x)*int(y)
+    else:
+        n = n//2
+        al = x[:n]
+        ar = x[n:]
+        bl = y[:n]
+        br = y[n:]
+        a = karatsuba(al, bl)
+        b = karatsuba(ar, br)
+        c = karatsuba(str(int(al)+int(ar)), str(int(bl)+int(br))) - a - b
+        prod = (a * (10 ** (n*2))) + (c * (10 ** n)) + b
+        return prod
+
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+xlen = len(num1)
+ylen = len(num2)
+ma = max(len(num1),len(num2))
+num1.zfill(ma)
+num2.zfill(ma)
+result = karatsuba(num1, num2)
+print(result)
+print(int(num1) * int(num2))
