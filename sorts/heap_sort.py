929e9d2ab24335181e8951c16a862506310e8796
euilee0129 <39470736+euilee0129@users.noreply.github.com>
2022-10-12 13:18:58 -0700

    Update heap_sort.py

--- sorts/heap_sort.py
+++ sorts/heap_sort.py
@@ -13,8 +13,8 @@ python heap_sort.py
 
 def heapify(unsorted, index, heap_size):
     largest = index
-    left_index = 2 * index + 1
-    right_index = 2 * index + 2
+    left_index = (2 * index) + 1
+    right_index = (2 * index) + 2
     if left_index < heap_size and unsorted[left_index] > unsorted[largest]:
         largest = left_index
 
