b973148a8f0abf50373c2c8953fdf75834e60cb6
Christian Clauss <cclauss@me.com>
2022-10-13 00:45:35 +0200

    Update barcode_validator.py

--- strings/barcode_validator.py
+++ strings/barcode_validator.py
@@ -1,3 +1,8 @@
+"""
+https://en.wikipedia.org/wiki/Check_digit#Algorithms
+"""
+
+
 def get_check_digit(barcode: int) -> int:
     """
     Returns the last digit of barcode by excluding the last digit first
@@ -6,9 +11,13 @@ def get_check_digit(barcode: int) -> int:
 
     >>> get_check_digit(8718452538119)
     9
-
+    >>> get_check_digit(87184523)
+    5
+    >>> get_check_digit(87193425381086)
+    9
+    >>> [get_check_digit(x) for x in range(0, 100, 10)]
+    [0, 7, 4, 1, 8, 5, 2, 9, 6, 3]
     """
-
     barcode //= 10  # exclude the last digit
     checker = False
     s = 0
@@ -30,22 +39,17 @@ def is_valid(barcode: int) -> bool:
 
     >>> is_valid(8718452538119)
     True
-
     >>> is_valid(87184525)
     False
-
     >>> is_valid(87193425381089)
     False
-
     >>> is_valid(0)
     False
-
     >>> is_valid(dwefgiweuf)
     Traceback (most recent call last):
     ...
     NameError: name 'dwefgiweuf' is not defined
     """
-
     return len(str(barcode)) == 13 and get_check_digit(barcode) == barcode % 10
 
 
@@ -55,18 +59,15 @@ def get_barcode(barcode: str) -> int:
 
     >>> get_barcode("8718452538119")
     8718452538119
-
     >>> get_barcode("dwefgiweuf")
     Traceback (most recent call last):
     ...
-    ValueError: Barcode 'dwefgiweuf' has alphabets in it
+    ValueError: Barcode 'dwefgiweuf' has alphabetic characters.
     """
     if str(barcode).isalpha():
-        raise ValueError(f"Barcode '{barcode}' has alphabets in it")
-
-    if int(barcode) < 0:
-        raise ValueError("The entered barcode has negative values. Try again.")
-
+        raise ValueError(f"Barcode '{barcode}' has alphabetic characters.")
+    elif int(barcode) < 0:
+        raise ValueError("The entered barcode has a negative value. Try again.")
     else:
         return int(barcode)
 
@@ -82,6 +83,6 @@ if __name__ == "__main__":
     barcode = get_barcode(input("Barcode: ").strip())
 
     if is_valid(barcode):
-        print(f"|{barcode}| is a valid Barcode")
+        print(f"'{barcode}' is a valid Barcode")
     else:
-        print(f"|{barcode}| is NOT is valid Barcode.")
+        print(f"'{barcode}' is NOT is valid Barcode.")
