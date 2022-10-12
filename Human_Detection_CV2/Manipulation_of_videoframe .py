ec3fdc132ec2780116369e12a3085c3c31ef97f5
Prasanna1717 <111585361+Prasanna1717@users.noreply.github.com>
2022-10-12 20:58:15 +0530

    Update Manipulation_of_videoframe .py

--- Human_Detection_CV2/Manipulation_of_videoframe .py
+++ Human_Detection_CV2/Manipulation_of_videoframe .py
@@ -1,5 +1,6 @@
+#these lines can be added as a optional for enhancement of project not necessary option.
 # For greyscale:
- frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+ #frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
  #KINDLY apply threshold values for all pixels with a level larger than 80 are shown in white.
  #And the others will be shown in black
- ret,frame = cv2.threshold(frame,80,255,cv2.THRESH_BINARY)
+ #ret,frame = cv2.threshold(frame,80,255,cv2.THRESH_BINARY)
