ebe7d10efef7ca10c97e67957405f96c2384016f
Aditya Wadkar <67093170+AdityaWadkar@users.noreply.github.com>
2022-10-12 12:54:03 +0530

    Created area_rectangle.py file
    This script will return area of rectangle

---
+++ maths/area_rectangle.py
@@ -0,0 +1,27 @@
+
+"""
+Basic of this concept..
+
+https://en.wikipedia.org/wiki/Rectangle
+
+"""
+
+"""area of rectangle."""
+
+
+def area(length,breadth):
+    """
+    Find the area of recatngle.
+    >>> area(5,6)
+    30
+    >>> area(2,10)
+    20
+    >>> area(30,50)
+    1500
+    """
+    return length*breadth
+
+
+
+if __name__ == "__main__":
+    print(area(10,20))  # --> 10,20
