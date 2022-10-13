471a62be9cf57e47a0f80c58d83120b6c32cf530
Koushik Chakraborty <72178398+KoushikAD1234@users.noreply.github.com>
2022-10-13 22:01:04 +0530

    Vioce Assistant

---
+++ Alexa_Project/alexa.py
@@ -0,0 +1,56 @@
+# install all libraries 
+
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+# import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+def talk(text):
+   engine.say(text)
+   engine.runAndWait()
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+
+    except:
+        pass
+    return command
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        print(time)
+        talk('Current time is ' + time)
+
+    elif 'date' in command:
+        talk('sorry, You are not mature enough')
+
+    elif 'are you single' in command:
+        talk('No, I am in a relationship with Siri')
+
+    else:
+        talk('please say the command again')
+
+while True:
+  run_alexa()
