0d63a429f09943c02f095755038e0b9fdc4687cc
CaedenPH <caedenperelliharris@gmail.com>
2022-10-13 15:12:06 +0100

    refactor: Rename `next_` to `next_item` (#7104)

--- data_structures/stacks/next_greater_element.py
+++ data_structures/stacks/next_greater_element.py
@@ -36,12 +36,12 @@ def next_greatest_element_fast(arr: list[float]) -> list[float]:
     """
     result = []
     for i, outer in enumerate(arr):
-        next_: float = -1
+        next_item: float = -1
         for inner in arr[i + 1 :]:
             if outer < inner:
-                next_ = inner
+                next_item = inner
                 break
-        result.append(next_)
+        result.append(next_item)
     return result
 
 
