5786bd16167815aec186d881436f77999c90ecaa
Christian Clauss <cclauss@me.com>
2022-10-13 17:21:45 +0200

    Update web_programming/open_google_results.py

--- web_programming/open_google_results.py
+++ web_programming/open_google_results.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     res = requests.get(
         url,
         headers={
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) " "Gecko/20100101 Firefox/98.0"
         },
     )
 
