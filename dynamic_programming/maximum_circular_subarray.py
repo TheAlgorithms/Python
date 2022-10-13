8fec400572dcd7e0a7dd949bf84942bece78f2e4
Suman Mondal <94859440+sumanarlert@users.noreply.github.com>
2022-10-13 14:14:05 +0530

    Update maximum_circular_subarray.py

--- dynamic_programming/maximum_circular_subarray.py
+++ dynamic_programming/maximum_circular_subarray.py
@@ -1,4 +1,11 @@
+# The function returns maximum circular contiguous sum in a[]
 def maxCircularSum(arr: list[int], n: int) -> int:
+  """
+  Input: arr[] = {8, -7, 9, -9, 10, -11, 12}
+  Output: 22 
+  Explanation: Subarray 12, 8, -7, 9, -9, 10 gives the maximum sum, that is 23.
+  """    
+    
     # Edge Case
     if (n == 1): return arr[0]
   
