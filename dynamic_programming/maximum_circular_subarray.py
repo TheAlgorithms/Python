854f6319315f4e36bb27e172f2dd38f7930263ba
Suman Mondal <94859440+sumanarlert@users.noreply.github.com>
2022-10-13 14:11:13 +0530

    Create maximum_circular_subarray.py

---
+++ dynamic_programming/maximum_circular_subarray.py
@@ -0,0 +1,29 @@
+def maxCircularSum(arr: list[int], n: int) -> int:
+    # Edge Case
+    if (n == 1): return arr[0]
+  
+    # Sum variable which store total arr_sum of the array.
+    arr_sum = sum(arr)
+  
+    # Every variable stores first value of the array.
+    current_max = max_so_far = current_min = min_so_far = arr[0]
+  
+    # Concept of Kadane's Algorithm
+    for i in range(1, n):
+        # Kadane's Algorithm to find Maximum subarray arr_sum.
+        current_max = max(current_max + arr[i], arr[i])
+        max_so_far = max(max_so_far, current_max)
+  
+        # Kadane's Algorithm to find Minimum subarray arr_sum.
+        current_min = min(current_min + arr[i], arr[i])
+        min_so_far = min(min_so_far, current_min)
+  
+    if (min_so_far == arr_sum): return max_so_far
+  
+    # returning the maximum value
+    return max(max_so_far, arr_sum - min_so_far)
+  
+if __name__ == '__main__':
+    size = int(input("Enter size of array:\t"))
+    arr = list(map(int, input("Enter the elements of the array: ").strip().split()));
+    print(f"Maximum circular sum is {maxCircularSum(arr, size)}")
