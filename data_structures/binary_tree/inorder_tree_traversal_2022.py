7a93e2d0c4819c2a7fb23197b5ad913eac2feda5
GURNEET SINGH <79376134+SinghGurneet21@users.noreply.github.com>
2022-10-13 12:30:34 +0530

    Update inorder_tree_traversal_2022.py

--- data_structures/binary_tree/inorder_tree_traversal_2022.py
+++ data_structures/binary_tree/inorder_tree_traversal_2022.py
@@ -5,6 +5,7 @@ https://www.geeksforgeeks.org/tree-traversals-inorder-preorder-and-postorder/
 """
 
 
+
 class BinaryTreeNode:
     """Defining the structure of BinaryTreeNode"""
 
@@ -36,7 +37,7 @@ def insert(node: BinaryTreeNode, new_value: int) -> BinaryTreeNode:
 
 def inorder(node: None) -> BinaryTreeNode:  # if node is None,return
     """
-    >>> inorder(make())
+    >>> inorder(make_tree())
     6
     10
     14
@@ -45,7 +46,7 @@ def inorder(node: None) -> BinaryTreeNode:  # if node is None,return
     25
     60
     """
-
+    
     if node is None:
         return None
     # traverse left subtree
@@ -55,9 +56,8 @@ def inorder(node: None) -> BinaryTreeNode:  # if node is None,return
     # traverse right subtree
     inorder(node.right_child)
 
-
-def make_tree() -> BinaryTreeNode | None:
-
+def make_tree()-> BinaryTreeNode | None:
+    
     root = insert(None, 15)
     insert(root, 10)
     insert(root, 25)
@@ -67,9 +67,8 @@ def make_tree() -> BinaryTreeNode | None:
     insert(root, 60)
     return root
 
-
-def main():
-    # main function
+def main()-> None:
+    # main function 
     root = make_tree()
     print("Printing values of binary search tree in Inorder Traversal.")
     inorder(root)
