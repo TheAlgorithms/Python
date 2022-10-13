ff4612dd98365275f4e8a1fefc81871cb4e5006f
AkshajV1309 <79909101+AkshajV1309@users.noreply.github.com>
2022-10-13 21:20:38 +0530

    Create norgate.py

---
+++ boolean_algebra/norgate.py
@@ -0,0 +1,26 @@
+''' A NOR Gate is a logic gate in boolean algebra which results to false(0)
+    if any of the input is 1, and True(1) if  both the inputs are 0.
+   Following is the truth table of an NOR Gate:
+   | Input 1 | Input 2 |  Output |
+   |      0      |     0      |      1      |
+   |      0      |     1      |      0      |
+   |      1      |     0      |      0      |
+   |      1      |     1      |      0      |
+'''
+'''Following is the code implementation of the NOR Gate'''
+
+def nor_gate(input_1: int,input_2: int) -> int:
+    if input_1 == input_2 == 0:
+        return 1
+    else:
+        return 0
+    
+if __name__== '__main__':
+    print('Truth Table of NOR Gate:')
+    print('| Input 1 |',' Input 2 |',' Output |')
+    print('|      0      |','     0      |     ',nor_gate(0,0),'     |')
+    print('|      0      |','     1      |     ',nor_gate(0,1),'     |')
+    print('|      1      |','     0      |     ',nor_gate(1,0),'     |')
+    print('|      1      |','     1      |     ',nor_gate(1,1),'     |')
+'''Code provided by Akshaj Vishwanathan'''
+'''Reference: https://www.geeksforgeeks.org/logic-gates-in-python/'''
