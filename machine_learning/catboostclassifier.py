caec6483e9897c2d7965fe209c476a50bd8d316e
Modassir Afzal <modassir2024@gmail.com>
2022-10-13 17:57:58 +0530

    Fixes: #{6911}

---
+++ machine_learning/catboostclassifier.py
@@ -0,0 +1,46 @@
+# CatBoost Classifier Example
+from matplotlib import pyplot as plt
+from sklearn.datasets import load_iris
+from sklearn.metrics import plot_confusion_matrix
+from sklearn.model_selection import train_test_split
+from catboost import CatBoostClassifier
+
+
+def main() -> None:
+
+  """
+    The Url for the algorithm
+    https://catboost.ai/en/docs/concepts/python-reference_catboostclassifier
+    Iris type dataset is used to demonstrate algorithm.
+  """
+
+  # Load Iris dataset
+  iris = load_iris()
+
+  # Split dataset into train and test data
+  x = iris["data"]  # features
+  y = iris["target"]
+  x_train, x_test, y_train, y_test = train_test_split(
+        x, y, test_size=0.25, random_state=1
+    )
+
+  # XGBoost Classifier
+  cbc = CatBoostClassifier()
+  cbc.fit(x_train, y_train)
+
+  # Display Confusion Matrix of Classifier
+  # with both train and test sets
+  plot_confusion_matrix(
+        cbc,
+        x_test,
+        y_test,
+        display_labels=iris["target_names"],
+        cmap="Blues",
+        normalize="true",
+    )
+  plt.title("Normalized Confusion Matrix - IRIS Dataset")
+  plt.show()
+
+
+if __name__ == "__main__":
+    main()
