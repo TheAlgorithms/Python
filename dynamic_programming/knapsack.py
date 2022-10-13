0c48669df7fa90bf50ca4d3325d64de9aa439dc4
CaedenPH <caedenperelliharris@gmail.com>
2022-10-13 16:48:41 +0100

    fix: Correct `knapsack` code (#7127)

--- dynamic_programming/knapsack.py
+++ dynamic_programming/knapsack.py
@@ -31,12 +31,12 @@ def knapsack(w, wt, val, n):
 
     for i in range(1, n + 1):
         for w_ in range(1, w + 1):
-            if wt[i - 1] <= w:
-                dp[i][w_] = max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w_])
+            if wt[i - 1] <= w_:
+                dp[i][w_] = max(val[i - 1] + dp[i - 1][w_ - wt[i - 1]], dp[i - 1][w_])
             else:
                 dp[i][w_] = dp[i - 1][w_]
 
-    return dp[n][w], dp
+    return dp[n][w_], dp
 
 
 def knapsack_with_example_solution(w: int, wt: list, val: list):
