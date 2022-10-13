f19c439bbb432e86e6babe396d6659c7f2f56e9b
Havish <100441982+havishs9@users.noreply.github.com>
2022-10-12 17:06:24 -0700

    Create boolean algebra calculator.py

---
+++ boolean_algebra/boolean algebra calculator.py
@@ -0,0 +1,12 @@
+#ONLY OPERATORS ARE "AND", "OR", "NOT"
+
+print("INSTRUCTIONS: USE 'and' / 'or' / 'not' when entering equation. For booleans use True/False or 1/0")
+
+print()
+while True:
+  try:
+    eq = input("Enter equation: ")
+    
+    print(eval(eq))
+  except:
+    pass
