3698bc62c4790b61971afd013b6d69496260e648
Suman Mondal <94859440+sumanarlert@users.noreply.github.com>
2022-10-13 15:41:21 +0530

    Update maximum_circular_subarray.py

--- dynamic_programming/maximum_circular_subarray.py
+++ dynamic_programming/maximum_circular_subarray.py
@@ -45,8 +45,7 @@ def max_circular_sum(arr: list[int], size: int) -> int:
 
 if __name__ == "__main__":
     import doctest
-
     doctest.testmod()
     size = int(input("Enter size of array:\t"))
     arr = list(map(int, input("Enter the elements of the array: ").strip().split()))
-    print(f"Maximum circular sum is {maxCircularSum(arr, size)}")
+    print(f"Maximum circular sum is {max_circular_sum(arr, size)}")
