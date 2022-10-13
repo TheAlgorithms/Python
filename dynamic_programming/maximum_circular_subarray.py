c28264327dc27da79745fd420a43f711680199f6
Suman Mondal <94859440+sumanarlert@users.noreply.github.com>
2022-10-13 14:19:36 +0530

    Update maximum_circular_subarray.py

--- dynamic_programming/maximum_circular_subarray.py
+++ dynamic_programming/maximum_circular_subarray.py
@@ -3,7 +3,7 @@ def maxCircularSum(arr: list[int], n: int) -> int:
   """
   Input: arr[] = {8, -7, 9, -9, 10, -11, 12}
   Output: 22 
-  Explanation: Subarray 12, 8, -7, 9, -9, 10 gives the maximum sum, that is 23.
+  Explanation: Subarray 12, 8, -7, 9, -9, 10 gives the maximum sum, that is 22.
   """    
     
     # Edge Case
