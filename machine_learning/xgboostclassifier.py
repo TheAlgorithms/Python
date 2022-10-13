d795ac42af4e9e01c3efd1e29ac1103cbb68d620
Modassir Afzal <60973906+Moddy2024@users.noreply.github.com>
2022-10-13 17:59:35 +0530

    Fixes: #{6551}

--- machine_learning/xgboostclassifier.py
+++ machine_learning/xgboostclassifier.py
@@ -23,7 +23,6 @@ def main() -> None:
     x_train, x_test, y_train, y_test = train_test_split(
         x, y, test_size=0.3, random_state=1
     )
-
     # XGBoost Classifier
     xgb = XGBClassifier()
     xgb.fit(x_train, y_train)
