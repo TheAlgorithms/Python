5a66120a7631d46be37e4085341fec1bb4c2fef0
Dexter <67648158+TheGeekDex@users.noreply.github.com>
2022-10-14 06:06:58 +0530

    Update binary_and_text.py
    Fixed bug in docs

--- strings/binary_and_text.py
+++ strings/binary_and_text.py
@@ -3,13 +3,13 @@ def text_to_binary(text: str) -> str:
     Converts any string to binary
 
     >>> text_to_binary('A')
-    1000001
+    '1000001'
 
     >>> text_to_binary('Dexter')
-    1000100 1100101 1111000 1110100 1100101 1110010
+    '1000100 1100101 1111000 1110100 1100101 1110010'
 
     >>> text_to_binary('❤️')
-    10011101100100 1111111000001111
+    '10011101100100 1111111000001111'
     """
     return " ".join(map(lambda char: bin(ord(char))[2:], text))
 
@@ -19,13 +19,13 @@ def binary_to_text(bin: str) -> str:
     Converts any binary to string
 
     >>> binary_to_text('1000001')
-    A
+    'A'
 
     >>> binary_to_text('1000100 1100101 1111000 1110100 1100101 1110010')
-    Dexter
+    'Dexter'
 
     >>> binary_to_text('10011101100100 1111111000001111')
-    ❤️
+    '❤️'
     """
     return "".join(map(lambda bin_text: chr(int(bin_text, base=2)), bin.split()))
 
