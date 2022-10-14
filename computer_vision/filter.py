e166f912388a480862079b6a091c49de8c7b313f
Mouiad Ali <66889657+Mouiad-JRA@users.noreply.github.com>
2022-10-14 12:24:31 +0300

    Update filter.py

--- computer_vision/filter.py
+++ computer_vision/filter.py
@@ -3,17 +3,18 @@ import numpy as np
 
 
 class Filter:
-    def __int__(self, image):
+    def __int__(self, image: str) -> None:
+
         self.image = image
 
-    def image_read(self):
+    def image_read(self) -> np.array:
         """
         Read the image
         :return: The image as numpy array
         """
         return cv2.imread(self.image, 0)
 
-    def get_identity_kernel(self):
+    def get_identity_kernel(self) -> np.array:
         """
         Apply identity kernel
         :return: array with the value of the middle element
@@ -21,7 +22,7 @@ class Filter:
         """
         return np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
 
-    def get_blur_kernel(self):
+    def get_blur_kernel(self) -> np.array:
         """
         Kernel for box blur filter
         :return:  unity matrix which is divided by 9
@@ -34,21 +35,21 @@ class Filter:
             ]
         )
 
-    def get_sharp_kernel(self):
+    def get_sharp_kernel(self) -> np.array:
         """
         Apply kernel for sharpening
         :return: sharpening filter array
         """
         return np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
 
-    def get_emboss_kernel(self):
+    def get_emboss_kernel(self) -> np.array:
         """
         Apply kernel for embossing
         :return: embossing filter array
         """
         return np.array([[-1, 0, 0], [0, 0, 0], [0, 0, 1]])
 
-    def image_shape(self):
+    def image_shape(self) -> tuple[int, int]:
         """
         Get the Image Shape
         :return: The number of rows and columns of the Image
@@ -57,7 +58,7 @@ class Filter:
         rows, columns = image.shape
         return rows, columns
 
-    def get_new_image(self):
+    def get_new_image(self) -> np.array:
         """
         Convolve mask over the image
         :return: New Image as numpy array
@@ -65,14 +66,14 @@ class Filter:
         rows, columns = self.image_shape()
         return np.zeros([rows, columns])
 
-    def average_mask(self):
+    def average_mask(self) -> np.array:
         """
         Averaging filter Mask
         :return: mask with 3×3 dim
         """
         return (np.ones([3, 3], dtype=int)) / 9
 
-    def get_ready(self):
+    def get_ready(self) -> tuple[int, int, np.array, np.array]:
         """
         Read the image, Get the Image Shape, Convolve mask over the image
         :return: row, columns , image and new image
@@ -83,7 +84,7 @@ class Filter:
 
         return rows, columns, image, new_image
 
-    def set_up_filter(self, kernel):
+    def set_up_filter(self, kernel) -> np.array:
         """
         kernel is used for specifying the kernel operation
         :param kernel: choose the kernel we want to apply
@@ -94,7 +95,7 @@ class Filter:
         filter_image = cv2.filter2D(src=image, ddepth=-1, kernel=kernel)
         return filter_image
 
-    def average(self):
+    def average(self) -> np.array:
         """
         :return: image after applying the Average filter
         """
@@ -117,7 +118,7 @@ class Filter:
                 new_image[i, j] = temp
         return new_image.astype(np.uint8)
 
-    def median(self):
+    def median(self) -> np.array:
         """
         Traverse the image find the median of the pixels and
         replace the center pixel by median filter
@@ -142,7 +143,7 @@ class Filter:
                 new_image[i, j] = temp[4]
         return new_image.astype(np.uint8)
 
-    def filter_2d(self):
+    def filter_2d(self) -> np.array:
         """
         Use Identity Kernel with 2D filter
         :return: Filtered image with  Identity Kernel
@@ -150,7 +151,7 @@ class Filter:
         kernel = self.get_identity_kernel()
         return self.set_up_filter(kernel)
 
-    def blur_filter(self):
+    def blur_filter(self) -> np.array:
         """
         Use Kernel Blur with 2D filter
         :return: Blur Filtered image with Blur Kernel
@@ -158,7 +159,7 @@ class Filter:
         kernel = self.get_blur_kernel()
         return self.set_up_filter(kernel)
 
-    def gaussian_blur(self):
+    def gaussian_blur(self) -> np.array:
         """
         Applying Gaussian Blur Filter using Gaussian Blur method
         :return: Gaussian Blur filtered image
@@ -169,7 +170,7 @@ class Filter:
         )
         return gaussian_blur_filter_image
 
-    def median_blur(self):
+    def median_blur(self) -> np.array:
         """
         Applying median Blur Filter using median Blur method
         :return: Median Blur filtered image
@@ -178,7 +179,7 @@ class Filter:
         median_blur_filtered_image = cv2.medianBlur(src=image, ksize=9)
         return median_blur_filtered_image
 
-    def sharp_filter(self):
+    def sharp_filter(self) -> np.array:
         """
         Use Sharp kernel with 2D filter
         :return: Sharpened image with sharp Kernel
@@ -186,7 +187,7 @@ class Filter:
         kernel = self.get_sharp_kernel()
         return self.set_up_filter(kernel)
 
-    def emboss_filter(self):
+    def emboss_filter(self) -> np.array:
         """
         Use Emboss kernel with 2D filter
         :return: Embossed image with emboss Kernel
@@ -197,7 +198,6 @@ class Filter:
 
 # Main Function
 if __name__ == "__main__":
-
     filter_object = Filter()
     filter_object.image = "path/image-name.png"
     cv2.imwrite("average.tif", filter_object.average())
