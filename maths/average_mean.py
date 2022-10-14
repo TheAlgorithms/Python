7ee1a1f493e3bb1dd9121faec9dd1190c6d45056
Soham Chowdhury <chowdhurysoham007@gmail.com>
2022-10-14 23:04:54 +0530

    Update average_mean.py

--- maths/average_mean.py
+++ maths/average_mean.py
@@ -1,28 +1,13 @@
-from __future__ import annotations
+# Python program to demonstrate mean()
+# function from the statistics module
 
+# Importing the statistics module
+import statistics
 
-def mean(nums: list) -> float:
-    """
-    Find mean of a list of numbers.
-    Wiki: https://en.wikipedia.org/wiki/Mean
+# list of positive integer numbers
+data1 = [1, 3, 4, 5, 7, 9, 2]
 
-    >>> mean([3, 6, 9, 12, 15, 18, 21])
-    12.0
-    >>> mean([5, 10, 15, 20, 25, 30, 35])
-    20.0
-    >>> mean([1, 2, 3, 4, 5, 6, 7, 8])
-    4.5
-    >>> mean([])
-    Traceback (most recent call last):
-        ...
-    ValueError: List is empty
-    """
-    if not nums:
-        raise ValueError("List is empty")
-    return sum(nums) / len(nums)
+x = statistics.mean(data1)
 
-
-if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod()
+# Printing the mean
+print("Mean is :", x)
