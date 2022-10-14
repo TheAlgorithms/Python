f775743c421119616d8c3ee6a4ebacea7bb9f6f3
Rajarshi101 <109743720+Rajarshi101@users.noreply.github.com>
2022-10-14 18:22:54 +0530

    Update factors.py

--- maths/factors.py
+++ maths/factors.py
@@ -1,9 +1,9 @@
-list1=[]                                                    # declaring an empty list to store the factors after calculation
-x=int(input("Enter a positive number: "))                   # accepting a number from the user
-for i in range(1,x+1):                                      # calculating and storing the factors in the list
-    if x%i==0:
+list1=[]                                                      # declaring an empty list to store the factors after calculation
+num=int(input("Enter a positive number: "))                   # accepting a number from the user
+for i in range(1,num+1):                                      # calculating and storing the factors in the list
+    if num%i==0:
         list1.append(i)
-print("The factors of the number",x,"are:--\n",list1)       # printing out the factors of the given number in the form of a list
+print("The factors of the number",num,"are:--\n",list1)       # displaying the factors of the given number in the form of a list
 
 """       ---::Output::---
 >>> The factors of the number 1 are:--
