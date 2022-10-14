937f0a3a95cdfe8a5f54e1e8d41184a00c11825a
Anurag Shukla <76862299+anuragshuklajec@users.noreply.github.com>
2022-10-14 13:40:39 +0530

    Update maths/find_unique.py
    Co-authored-by: Caeden <caedenperelliharris@gmail.com>

--- maths/find_unique.py
+++ maths/find_unique.py
@@ -1,6 +1,8 @@
 def find_unique_xor(nums: list) -> float:
     """
-    Find unique from a given list of numbers with 2n+1 entries of which n element appears twice and there is one unique element.
+    Find unique from a given list of numbers with 2n+1 
+    entries of which n element appears twice and there 
+    is one unique element.
     >>> find_unique_xor([1,2,3,2,1])
     3
     >>> find_unique_xor([25,27,26,27,25])
