1e812724de65526714d2c8ba5d5a11098aa2b615
Dexter <67648158+TheGeekDex@users.noreply.github.com>
2022-10-14 12:48:38 +0530

    Update binary_and_text.py
    Added doctest and removed juk.

--- strings/binary_and_text.py
+++ strings/binary_and_text.py
@@ -1,13 +1,10 @@
 def text_to_binary(text: str) -> str:
     """
     Converts any string to binary
-
     >>> text_to_binary('A')
     '1000001'
-
     >>> text_to_binary('Dexter')
     '1000100 1100101 1111000 1110100 1100101 1110010'
-
     >>> text_to_binary('❤️')
     '10011101100100 1111111000001111'
     """
@@ -17,13 +14,10 @@ def text_to_binary(text: str) -> str:
 def binary_to_text(binary: str) -> str:
     """
     Converts any binary to string
-
     >>> binary_to_text('1000001')
     'A'
-
     >>> binary_to_text('1000100 1100101 1111000 1110100 1100101 1110010')
     'Dexter'
-
     >>> binary_to_text('10011101100100 1111111000001111')
     '❤️'
     """
@@ -32,5 +26,5 @@ def binary_to_text(binary: str) -> str:
 
 if __name__ == "__main__":
     import doctest
-
+    
     doctest.testmod()
