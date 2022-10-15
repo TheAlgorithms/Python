4fc22de590e0b7fab012c960465479c016ecf5f2
otavioabreu27 <otavio.abreu96@gmail.com>
2022-10-15 16:57:08 -0300

    Fixing the return type

--- conversions/absolute_conversion.py
+++ conversions/absolute_conversion.py
@@ -40,7 +40,8 @@ def absolute_conversion(interv_start: float,
         return print("Invalid arguments, start and end shoud be different.")
     else:
         native_interval = interv_end - interv_start
-        return (100 * (number - interv_start)) / native_interval
+        value_percentage = float((100*(number - interv_start))/native_interval)
+        return value_percentage
 
 
 if __name__ == "__main__":
