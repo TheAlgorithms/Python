0e12f0223cbaecd4f2c5f6bd10055e91d3a3c20e
Kuldeep Borkar Jr <kuldeepborkarjr765@gmail.com>
2022-10-15 20:45:23 +0530

    Updated with some suggested formatting

--- boolean_algebra/xnorgate.py
+++ boolean_algebra/xnorgate.py
@@ -1,4 +1,5 @@
-""" A XNOR Gate is a logic gate in boolean algebra which results to false(0)
+"""
+A XNOR Gate is a logic gate in boolean algebra which results to false(0)
     if both the inputs are different and results to true(1) if both the inputs are same.
    Following is the truth table of an XNOR Gate:
    | Input 1 | Input 2 |  Output |
@@ -6,8 +7,9 @@
    |      0      |     1      |      0      |
    |      1      |     0      |      0      |
    |      1      |     1      |      1      |
+
+Following is the code implementation of the XNOR Gate
 """
-"""Following is the code implementation of the XNOR Gate"""
 
 
 def xnor_gate(input_1: int, input_2: int) -> int:
@@ -21,17 +23,20 @@ def xnor_gate(input_1: int, input_2: int) -> int:
     >>> xnor_gate(1, 1)
     1
     """
-    return int((not(input_1 != input_2)))
+    return int(input_1 == input_2)
+
 
 def main() -> None:
     print("Truth Table of XNOR Gate:")
     print("| Input 1     |", " Input 2    |", " Output     |")
-    print("|      0      |", "     0      |     ", xnor_gate(0, 0), "     |")
-    print("|      0      |", "     1      |     ", xnor_gate(0, 1), "     |")
-    print("|      1      |", "     0      |     ", xnor_gate(1, 0), "     |")
-    print("|      1      |", "     1      |     ", xnor_gate(1, 1), "     |")
+    print(f"|      0      |      0      |      {xnor_gate(0,0)}      |")
+    print(f"|      0      |      1      |      {xnor_gate(0,1)}      |")
+    print(f"|      1      |      0      |      {xnor_gate(1,0)}      |")
+    print(f"|      1      |      1      |      {xnor_gate(1,1)}      |")
+
 
 if __name__ == "__main__":
     import doctest
+
     doctest.testmod()
     main()
--- boolean_algebra/xorgate.py
+++ boolean_algebra/xorgate.py
@@ -1,4 +1,5 @@
-""" A XOR Gate is a logic gate in boolean algebra which results to true(1)
+"""
+A XOR Gate is a logic gate in boolean algebra which results to true(1)
     only when the two input values are different, and false(0) if the inputs are equal.
    Following is the truth table of an XOR Gate:
    | Input 1 | Input 2 |  Output |
@@ -6,8 +7,9 @@
    |      0      |     1      |      1      |
    |      1      |     0      |      1      |
    |      1      |     1      |      0      |
+
+Following is the code implementation of the XOR Gate
 """
-"""Following is the code implementation of the XOR Gate"""
 
 
 def xor_gate(input_1: int, input_2: int) -> int:
@@ -23,15 +25,18 @@ def xor_gate(input_1: int, input_2: int) -> int:
     """
     return int(input_1 != input_2)
 
+
 def main() -> None:
     print("Truth Table of XOR Gate:")
     print("| Input 1     |", " Input 2    |", " Output     |")
-    print("|      0      |", "     0      |     ", xor_gate(0, 0), "     |")
-    print("|      0      |", "     1      |     ", xor_gate(0, 1), "     |")
-    print("|      1      |", "     0      |     ", xor_gate(1, 0), "     |")
-    print("|      1      |", "     1      |     ", xor_gate(1, 1), "     |")
+    print(f"|      0      |      0      |      {xor_gate(0,0)}      |")
+    print(f"|      0      |      1      |      {xor_gate(0,1)}      |")
+    print(f"|      1      |      0      |      {xor_gate(1,0)}      |")
+    print(f"|      1      |      1      |      {xor_gate(1,1)}      |")
+
 
 if __name__ == "__main__":
     import doctest
+
     doctest.testmod()
     main()
