3a7c31eeb4689c1f28004e40f37cb08db69720f4
Jagdamba Tripathi <54079135+jagdtri2003@users.noreply.github.com>
2022-10-15 22:16:30 +0530

    Create checkJSON.py

---
+++ Python Automation/checkJSON.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import json
+
+if len(sys.argv) > 1:
+    if os.path.exists(sys.argv[1]):
+        file = open(sys.argv[1], "r")
+        json.load(file)
+        file.close()
+        print("Validate JSON!")
+    else:
+        print(sys.argv[1] + " not found")
+else:
+    print("Usage: checkjson.py <file>")
+
+
+"""
+    To run it use :
+
+        checkJSON test.json
+
+"""
