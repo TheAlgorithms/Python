3dd7a8fbcc55263625df9d481719b16e54a66c9c
Ankit Akash <75488501+ankit-akash@users.noreply.github.com>
2022-10-16 00:07:48 +0530

    Create bfs.py

---
+++ data_structures/graphs/bfs.py
@@ -0,0 +1,30 @@
+#Breadth First Search in Python
+
+graph = {
+  '5' : ['3','7'],
+  '3' : ['2', '4'],
+  '7' : ['8'],
+  '2' : [],
+  '4' : ['8'],
+  '8' : []
+}
+
+visited = [] # List for visited nodes.
+queue = []     #Initialize a queue
+
+def bfs(visited, graph, node): #function for BFS
+  visited.append(node)
+  queue.append(node)
+
+  while queue:          # Creating loop to visit each node
+    m = queue.pop(0) 
+    print (m, end = " ") 
+
+    for neighbour in graph[m]:
+      if neighbour not in visited:
+        visited.append(neighbour)
+        queue.append(neighbour)
+
+# Driver Code
+print("Following is the Breadth-First Search")
+bfs(visited, graph, '5')    # function calling
