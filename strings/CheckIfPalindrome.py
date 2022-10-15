93855e604bc54f38f46cc80dbbf290e4aa48ba5f
Pedro Ulisses <pedro.ulisses@aluno.uece.br>
2022-10-14 23:46:09 -0300

    Algorithm to check if the given string is a palindrome

--- strings/CheckIfPalindrome.py
+++ strings/CheckIfPalindrome.py
@@ -1,20 +1,34 @@
-import sys
-
-
-def palindrome(string):
-    string_rev = string[::-1]
-    if string_rev == string:
-        return "Yes"
+def palindrome(s: str) -> str:
+    """
+    Determines whether the given word is a palindrome
+    :param str:
+    :return: string:
+    >>> palindrome(bib)
+    Yes
+    >>>palindrome(nun)
+    Yes
+    >>>palindrome(a man)
+    No
+    >>>palindrome(pedro)
+    No
+    >>>palindrome(madam)
+    Yes
+    """
+    
+    
+    
+    string_rev = s[::-1]
+    if string_rev == s:
+        return 'Yes'
     else:
-        return "No"
+        return 'No'
 
-
-n_inp = int(input())  # Numbers of case test
-
-for item in range(n_inp):
-    size = int(input())  # Size of string
-    n = input()  # String
-    if len(n) > size:
-        print("Oversized or is different.")
-        sys.exit()
-    print(palindrome(n))
+if __name__ == "__main__":
+    n_inp = int(input()) #Numbers of case test
+    for item in range(n_inp):
+        size = int(input()) # Size of string
+        n = input() # String
+        if len(n) > size:
+            print("Oversized or is different.")
+        else:
+            print(palindrome(n))
