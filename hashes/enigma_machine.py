755c9370adfd88cd6400d7f8a00cfea37be27c68
CaedenPH <caedenperelliharris@gmail.com>
2022-10-15 15:16:40 +0100

    refactor: Fix ``flake8-comprehensions`` errors

--- hashes/enigma_machine.py
+++ hashes/enigma_machine.py
@@ -1,8 +1,8 @@
 alphabets = [chr(i) for i in range(32, 126)]
-gear_one = [i for i in range(len(alphabets))]
-gear_two = [i for i in range(len(alphabets))]
-gear_three = [i for i in range(len(alphabets))]
-reflector = [i for i in reversed(range(len(alphabets)))]
+gear_one = range(len(alphabets))
+gear_two = range(len(alphabets))
+gear_three = range(len(alphabets))
+reflector = reversed(range(len(alphabets)))
 code = []
 gear_one_pos = gear_two_pos = gear_three_pos = 0
 
