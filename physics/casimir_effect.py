9df4f719efc7914fdc17cee944ddaa248f0c42fe
Christian Clauss <cclauss@me.com>
2022-10-15 19:35:02 +0200

    from math import pi

--- physics/casimir_effect.py
+++ physics/casimir_effect.py
@@ -37,15 +37,13 @@ Source :
 
 from __future__ import annotations
 
-import math
+from math import pi
 
 # Define the Reduced Planck Constant ℏ (H bar), speed of light C, value of
 # Pi and the function
 REDUCED_PLANCK_CONSTANT = 1.054571817e-34  # unit of ℏ : J * s
 
 SPEED_OF_LIGHT = 3e8  # unit of c : m * s^-1
-
-PI = math.pi
 
 
 def casimir_force(force: float, area: float, distance: float) -> dict[str, float]:
@@ -99,18 +97,18 @@ def casimir_force(force: float, area: float, distance: float) -> dict[str, float
     if area < 0:
         raise ValueError("Area can not be negative")
     if force == 0:
-        force = (REDUCED_PLANCK_CONSTANT * SPEED_OF_LIGHT * PI**2 * area) / (
+        force = (REDUCED_PLANCK_CONSTANT * SPEED_OF_LIGHT * pi**2 * area) / (
             240 * (distance) ** 4
         )
         return {"force": force}
     elif area == 0:
         area = (240 * force * (distance) ** 4) / (
-            REDUCED_PLANCK_CONSTANT * SPEED_OF_LIGHT * PI**2
+            REDUCED_PLANCK_CONSTANT * SPEED_OF_LIGHT * pi**2
         )
         return {"area": area}
     elif distance == 0:
         distance = (
-            (REDUCED_PLANCK_CONSTANT * SPEED_OF_LIGHT * PI**2 * area) / (240 * force)
+            (REDUCED_PLANCK_CONSTANT * SPEED_OF_LIGHT * pi**2 * area) / (240 * force)
         ) ** (1 / 4)
         return {"distance": distance}
     raise ValueError("One and only one argument must be 0")
