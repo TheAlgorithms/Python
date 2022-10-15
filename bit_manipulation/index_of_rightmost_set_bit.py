071639b038d318fb53c4029732571f27b9a98b21
GautamChaurasia <gautamchaurasia501@gmail.com>
2022-10-15 20:06:57 +0530

    applied suggested changes

--- bit_manipulation/index_of_rightmost_set_bit.py
+++ bit_manipulation/index_of_rightmost_set_bit.py
@@ -1,7 +1,6 @@
-"Reference: https://www.geeksforgeeks.org/position-of-rightmost-set-bit/"
+# Reference: https://www.geeksforgeeks.org/position-of-rightmost-set-bit/
 
 def get_index_of_rightmost_set_bit(number: int) -> int:
-
     """
     Take in a positive integer 'number'.
     Returns the zero-based index of first set bit in that 'number' from right.
@@ -21,21 +20,21 @@ def get_index_of_rightmost_set_bit(number: int) -> int:
     ValueError: Input must be a non-negative integer
     """
 
-    if number>=0 and type(number)==int:
-        intermediate = number&~(number-1)
-        index = 0
-        while intermediate:
-            intermediate>>=1
-            index+=1
-        return index-1
-    else: raise ValueError("Input must be a non-negative integer")
+    if 0 > number and not isinstance(number, int):
+        raise ValueError("Input must be a non-negative integer")
 
-"""
-Finding the index of rightmost set bit has some very peculiar use-cases,
-especially in finding missing or/and repeating numbers in a list of positive integers.
-"""
+    intermediate = number&~(number-1)
+    index = 0
+    while intermediate:
+        intermediate>>=1
+        index+=1
+    return index-1
 
 if __name__ == "__main__":
+    """
+    Finding the index of rightmost set bit has some very peculiar use-cases,
+    especially in finding missing or/and repeating numbers in a list of positive integers.
+    """
     import doctest
 
     doctest.testmod()
