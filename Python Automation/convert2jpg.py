716c14b11e75396a17ba4667fc63866417809706
Jagdamba Tripathi <54079135+jagdtri2003@users.noreply.github.com>
2022-10-15 22:17:01 +0530

    Add files via upload

---
+++ Python Automation/convert2jpg.py
@@ -0,0 +1,23 @@
+import os
+import sys
+from PIL import Image
+
+if len(sys.argv) > 1:
+    if os.path.exists(sys.argv[1]):
+        im = Image.open(sys.argv[1])
+        target_name = sys.argv[1] + ".jpg"
+        rgb_im = im.convert('RGB')
+        rgb_im.save(target_name)
+        print("Saved as " + target_name)
+    else:
+        print(sys.argv[1] + " not found")
+else:
+    print("Usage: convert2jpg.py <file>")
+
+
+"""  
+    To run it use:
+
+        convert2jpg test.png
+
+"""
