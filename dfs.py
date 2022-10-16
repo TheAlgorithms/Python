b7fd37fcf72b1560ce4b4e6e78573eb2579cba86
Coding Capricorn <thecodingcapricorn@gmail.com>
2022-10-16 14:29:24 +0530

    Create dfs.py
    depth first search using python

---
+++ dfs.py
@@ -0,0 +1,23 @@
+# DFS algorithm in Python
+
+
+# DFS algorithm
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+
+    print(start)
+
+    for next in graph[start] - visited:
+        dfs(graph, next, visited)
+    return visited
+
+
+graph = {'0': set(['1', '2']),
+         '1': set(['0', '3', '4']),
+         '2': set(['0']),
+         '3': set(['1']),
+         '4': set(['2', '3'])}
+
+dfs(graph, '0')
