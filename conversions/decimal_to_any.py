974476d46d1d085881c250e17e2f4460573f5cb5
Caeden <caedenperelliharris@gmail.com>
2022-10-16 10:03:26 +0100

    Update conversions/decimal_to_any.py
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- conversions/decimal_to_any.py
+++ conversions/decimal_to_any.py
@@ -1,34 +1,8 @@
 """Convert a positive Decimal Number to Any Other Representation"""
 
+from string import ascii_uppercase
 
-ALPHABET_VALUES = {
-    "10": "A",
-    "11": "B",
-    "12": "C",
-    "13": "D",
-    "14": "E",
-    "15": "F",
-    "16": "G",
-    "17": "H",
-    "18": "I",
-    "19": "J",
-    "20": "K",
-    "21": "L",
-    "22": "M",
-    "23": "N",
-    "24": "O",
-    "25": "P",
-    "26": "Q",
-    "27": "R",
-    "28": "S",
-    "29": "T",
-    "30": "U",
-    "31": "V",
-    "32": "W",
-    "33": "X",
-    "34": "Y",
-    "35": "Z",
-}
+ALPHABET_VALUES = {str(ord(c) - 55): c for c in ascii_uppercase}
 
 
 def decimal_to_any(num: int, base: int) -> str:
