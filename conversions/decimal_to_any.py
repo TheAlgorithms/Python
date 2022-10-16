b8bef862a98c61384e9c23253c647757c01c2c98
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-10-16 11:03:31 +0530

    chore: remove fmt: on/off

--- conversions/decimal_to_any.py
+++ conversions/decimal_to_any.py
@@ -1,13 +1,11 @@
 """Convert a positive Decimal Number to Any Other Representation"""
 
 
-# fmt: off
-ALPHABET_VALUES = {'10': 'A', '11': 'B', '12': 'C', '13': 'D', '14': 'E', '15': 'F',  # noqa: E501
+ALPHABET_VALUES = {'10': 'A', '11': 'B', '12': 'C', '13': 'D', '14': 'E', '15': 'F',
                    '16': 'G', '17': 'H', '18': 'I', '19': 'J', '20': 'K', '21': 'L',
                    '22': 'M', '23': 'N', '24': 'O', '25': 'P', '26': 'Q', '27': 'R',
                    '28': 'S', '29': 'T', '30': 'U', '31': 'V', '32': 'W', '33': 'X',
                    '34': 'Y', '35': 'Z'}
-# fmt: on
 
 
 def decimal_to_any(num: int, base: int) -> str:
