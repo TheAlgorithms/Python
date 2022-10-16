2c89234dae2167f214a7cadd1735cd14606c7f6e
Wissam Fawaz <wissamfawaz12@gmail.com>
2022-10-16 10:15:27 +0300

    Added Pascal triangle implementation to the other folder.

--- other/pascal_triangle.py
+++ other/pascal_triangle.py
@@ -8,7 +8,7 @@ for more info about this triangle.
 """
 
 
-def generate_pascal_triangle(num_rows):
+def generate_pascal_triangle(num_rows: int) -> None:
     """
     Print Pascal's triangle for different number of rows
     >>> generate_triangle(1)
@@ -22,14 +22,14 @@ def generate_pascal_triangle(num_rows):
     >>> generate_triangle(5)
     [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
     """
-    triangle = []
+    triangle: list[list[int]] = []
     for current_row_idx in range(num_rows):
         populate_current_row(triangle, current_row_idx)
     print(triangle)
 
 
-def populate_current_row(triangle, current_row_idx):
-    current_row = [None] * (current_row_idx + 1)
+def populate_current_row(triangle: list[list[int]], current_row_idx: int) -> None:
+    current_row = [-1] * (current_row_idx + 1)
     # first and last elements of current row are equal to 1
     current_row[0], current_row[-1] = 1, 1
     for current_col_idx in range(1, current_row_idx):
@@ -39,13 +39,18 @@ def populate_current_row(triangle, current_row_idx):
     triangle.append(current_row)
 
 
-def calculate_current_element(triangle, current_row, current_row_idx, current_col_idx):
+def calculate_current_element(
+    triangle: list[list[int]],
+    current_row: list[int],
+    current_row_idx: int,
+    current_col_idx: int,
+) -> None:
     above_to_left_elt = triangle[current_row_idx - 1][current_col_idx - 1]
     above_to_right_elt = triangle[current_row_idx - 1][current_col_idx]
     current_row[current_col_idx] = above_to_left_elt + above_to_right_elt
 
 
-def main():
+def main() -> None:
     num_rows = int(input("Rows count of Pascal's triangle: ").strip())
     generate_pascal_triangle(num_rows)
 
