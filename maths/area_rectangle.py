a5f47866013858420e4fea2a17e22d2a41238953
Aditya Wadkar <67093170+AdityaWadkar@users.noreply.github.com>
2022-10-16 10:08:38 +0530

    updated create_rectangle.py
    Changes done as per suggestions

--- maths/area_rectangle.py
+++ maths/area_rectangle.py
@@ -1,11 +1,10 @@
 """
 Basic of this concept..
-
+The area of a rectangle is defined as the product  of its two adjacent sides.
+For more information 
 https://en.wikipedia.org/wiki/Rectangle
 
 """
-
-"""area of rectangle."""
 
 
 def area(length, breadth):
@@ -17,9 +16,10 @@ def area(length, breadth):
     20
     >>> area(30,50)
     1500
+    This functio will return integer value
     """
-    return length * breadth
+    return length * breadth 
 
 
 if __name__ == "__main__":
-    print(area(10, 20))  # --> 10,20
+    print(area(10, 20))  # --> 10,20(and two integer values)
