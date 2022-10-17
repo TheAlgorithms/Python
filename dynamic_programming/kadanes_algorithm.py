240e4d526a58f9880cf28cd1e6515dfc7930e14a
atharv-patil <8383455141+atharv-patil@users.noreply.github.com>
2022-10-17 23:02:50 +0530

    added kadanes algo

--- dynamic_programming/kadanes_algorithm.py
+++ dynamic_programming/kadanes_algorithm.py
@@ -1,12 +1,35 @@
 # Code Contributed by Atharv Patil, 2nd year, IIIT Pune
 # kadanes algorithm calculates maximum subarray sum in O(n) time complexity and
-# O(1) space complexity
+# O(1) auxiliary space complexity
 
 
+<<<<<<< Updated upstream
 def kadane(array):
     n = len(array)  # Initialization of n as length of array.
     mxsm = float("-inf")  # Initialization of mxsm as negative infinity.
     tempsm = 0  # Initialization of tempsm as 0.
+=======
+
+def kadane(array: list[int]) -> int:
+    """
+    >>> kadane([1,2,3,5,6,-144,1,3,5,10])
+    19
+    >>> kadane([1,-2,1,1,4,5,6,1,-1,9,10])
+    36
+    >>> kadane([5,6,-10,1,2,3,5,-3,6,-134,1,45])
+    46
+    """
+    n = len(array)              # Initialization of n as length of array.
+    mxsm = float("-inf")        # Initialization of mxsm as negative infinity.
+    tempsm = 0                  # Initialization of tempsm as 0.
+    
+    for i in range(n):
+        
+        tempsm += array[i]
+        
+        if tempsm > mxsm:       # mxsm will take the value of tempsm whenever
+            mxsm = tempsm       # tempsm will exceed it.
+>>>>>>> Stashed changes
 
     for i in range(n):
 
@@ -17,9 +40,10 @@ def kadane(array):
 
         if tempsm < 0:  # tempsm will be 0 whenever it becomes negative
             tempsm = 0
-
+    
     return mxsm
 
+<<<<<<< Updated upstream
 
 a = list(
     map(int, input("Enter the array: ").split())
@@ -28,3 +52,8 @@ a = list(
 print(
     "Maximum subarray sum is", kadane(a)
 )  # kadane function call with the array as argument
+=======
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod() 
+>>>>>>> Stashed changes
