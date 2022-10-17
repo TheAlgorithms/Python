9b5338b1703d8e1d24b8e6d3bf8e47db53d6f2b3
Shubham Kondekar <40213815+kondekarshubham123@users.noreply.github.com>
2022-10-18 00:32:14 +0530

    Update matrix/max_area_of_island.py
    Co-authored-by: Caeden Perelli-Harris <caedenperelliharris@gmail.com>

--- matrix/max_area_of_island.py
+++ matrix/max_area_of_island.py
@@ -1,9 +1,11 @@
-# Given an m x n binary matrix grid. An island is a group of 1's
-# (representing land) connected 4-directionally (horizontal or vertical.)
-# You may assume all four edges of the grid are surrounded by water.
-# The area of an island is the number of cells with a value 1 in the island.
-# Return the maximum area of an island in a grid.
-# If there is no island, return 0.
+"""
+Given an m x n binary matrix grid. An island is a group of 1's
+(representing land) connected 4-directionally (horizontal or vertical.)
+You may assume all four edges of the grid are surrounded by water.
+The area of an island is the number of cells with a value 1 in the island.
+Return the maximum area of an island in a grid.
+If there is no island, return 0.
+"""
 
 
 def is_safe(row: int, col: int, rows: int, cols: int) -> bool:
