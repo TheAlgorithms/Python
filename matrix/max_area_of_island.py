323047ffc01e5bb4de4af4813b39a229a369bafe
Christian Clauss <cclauss@me.com>
2022-10-18 19:58:38 +0200

    Typo

--- matrix/max_area_of_island.py
+++ matrix/max_area_of_island.py
@@ -1,7 +1,7 @@
 """
 Given an two dimensional binary matrix grid. An island is a group of 1's (representing
 land) connected 4-directionally (horizontal or vertical.) You may assume all four edges
-ofthe grid are surrounded by water.  The area of an island is the number of cells with
+of the grid are surrounded by water.  The area of an island is the number of cells with
 a value 1 in the island. Return the maximum area of an island in a grid. If there is no
 island, return 0.
 """
