9da8f0345ae8e0bf0a41229e4e5d78c8c9552660
Sai Ganesh Manda <89340753+mvsg2@users.noreply.github.com>
2022-10-18 23:42:38 +0530

    Update gaussian_naive_bayes.py
    Using the seaborn.heatmap library to plot the confusion matrix instead of the plot_confusion_matrix method from the sklearn.metrics module.

--- machine_learning/gaussian_naive_bayes.py
+++ machine_learning/gaussian_naive_bayes.py
@@ -1,9 +1,11 @@
 # Gaussian Naive Bayes Example
 from matplotlib import pyplot as plt
 from sklearn.datasets import load_iris
-from sklearn.metrics import plot_confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix # The plot_confusion_matrix method will be deprecated in the developed versions of Python 3.10.x according to the warning that it throws when this code is run.
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
+import time
+import seaborn as sns # For plotting a heatmap of the confusion matrix. This way will not throw any warning.
 
 
 def main():
@@ -25,20 +27,21 @@ def main():
 
     # Gaussian Naive Bayes
     nb_model = GaussianNB()
-    nb_model.fit(x_train, y_train)
-
+    model_fit = nb_model.fit(x_train, y_train)
+    y_pred = model_fit.predict(x_test)
     # Display Confusion Matrix
-    plot_confusion_matrix(
-        nb_model,
-        x_test,
-        y_test,
-        display_labels=iris["target_names"],
-        cmap="Blues",
-        normalize="true",
-    )
-    plt.title("Normalized Confusion Matrix - IRIS Dataset")
+    Conf_Matrix = confusion_matrix(y_true=y_test, y_pred=y_pred)
+    sns.heatmap(data = Conf_Matrix, annot = True, cmap = "Greys_r")
     plt.show()
-
-
+    
+    # Printing the seen confusion matrix on the console
+    time.sleep(1.2)
+    print("The confusion matrix is:\n", Conf_Matrix)
+    
+    time.sleep(1.8)
+    # Declaring the overall accuracy of the model
+    final_accuracy = 100*accuracy_score(y_true = y_test, y_pred = y_pred)
+    print(f"The final accuracy of the model is: {round(final_accuracy, 2)}%")
+ 
 if __name__ == "__main__":
     main()
