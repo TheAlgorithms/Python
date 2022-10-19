c0ec5a83d52d4dfcb680111f3cec4c5816bd2828
Pradyumn Singh Rahar <rvrahar@gmail.com>
2022-10-19 19:56:51 +0530

    Reduced Time Complexity to O(sqrt(n))

--- maths/factors.py
+++ maths/factors.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 def factors_of_a_number(num: int) -> list:
     """
     >>> factors_of_a_number(1)
@@ -9,7 +12,21 @@ def factors_of_a_number(num: int) -> list:
     >>> factors_of_a_number(-24)
     []
     """
-    return [i for i in range(1, num + 1) if num % i == 0]
+    facs: list[int] = []
+    if num < 1:
+        return facs
+    facs.append(1)
+    if num == 1:
+        return facs
+    facs.append(num)
+    for i in range(2, int(sqrt(num)) + 1):
+        if num % i == 0:  # If i is a factor of num
+            facs.append(i)
+            d = num // i  # num//i is the other factor of num
+            if d != i:  # If d and i are distinct
+                facs.append(d)  # we have found another factor
+    facs.sort()
+    return facs
 
 
 if __name__ == "__main__":
