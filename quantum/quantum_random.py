d0258f040aa0e2647080f085e8151a869a974e8b
Alan Paul <57307037+Alanzz@users.noreply.github.com>
2022-10-20 14:35:12 +0530

    Update quantum/quantum_random.py
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- quantum/quantum_random.py
+++ quantum/quantum_random.py
@@ -18,16 +18,11 @@ if __name__ == "__main__":
     doctest.testmod()
 
     # This list is used to store our random numbers
-    random_numbers_list = []
-
     # initialises a new quantum backend
     quantum_engine = MainEngine()
 
-    # for loop to generate 10 random numbers
-    for i in range(10):
-        # calling the random number function and append the return to the list
-        random_numbers_list.append(get_random_number(quantum_engine))
-
+    # Generate a list of 10 random numbers
+    random_numbers_list = [get_random_number(quantum_engine) for _ in range(10)]
     # Flushes the quantum engine from memory
     quantum_engine.flush()
 
