fe24dea38c4a7d4a3260d7935e0e5a51409a9ab1
Akshit Gulyan <103456810+AkshitGulyan@users.noreply.github.com>
2022-10-20 23:31:04 +0530

    Created sum_of_harmonic_series.py
    In Maths Section sum of Arithmetic Series as well as Geometric Series were there so I also added sum of Harmonic Series !

---
+++ maths/sum_of_harmonic_series.py
@@ -0,0 +1,18 @@
+def sum_of_hp(first_term: int, common_diff: int, num_of_terms: int) -> float:
+    """
+    Find the sum of n terms in an harmonic progression.
+    """
+    import math
+    total = (1/common_diff)*(math.log((2*(1/first_term)+(2*num_of_terms-1)*common_diff)/(2*first_term-common_diff))
+    # formula for sum of harmonic series
+    return total
+
+
+def main():
+    print(sum_of_hp(6,4,3))
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
