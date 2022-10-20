7439768add190de8d5b56e07c4cf8985dbed67e1
Mayuresh_22 <mayureshchoudhary22@gmail.com>
2022-10-20 20:30:30 +0530

    modified files

--- strings/camel_case_to_snake_case.py
+++ strings/camel_case_to_snake_case.py
@@ -1,6 +1,15 @@
 """
 this function focuses on converting camel case (eg: camelCase)
 to snake case (snake_case)
+-"Camel case" combines words by capitalizing all words following 
+the first word and removing the space eg: userLoginCount 
+This is a very popular way to combine words to form a single concept. 
+It is often used as a convention in variable declaration in many languages.
+-Snake case combines words by replacing each space with an underscore (_) and,
+in the all caps version, all letters are capitalized eg: user_login_count 
+This style, when capitalized, is often used as a convention 
+in declaring constants in many languages. 
+When lower cased, it is used conventionally in declaring database field names
 """
 
 
@@ -17,64 +26,61 @@ def to_snake_case(word: str) -> str:
     'hacktober_fest'
     >>> to_snake_case('')
     Traceback (most recent call last):
-    Exception: Provide camel case eg: helloWorld
+    ...
+    TypeError: Provide camel case eg: helloWorld
     >>> to_snake_case('HelloWorld')
     Traceback (most recent call last):
-    Exception: Provide camel case eg: helloWorld
+    ...
+    TypeError: Provide camel case eg: helloWorld
     >>> to_snake_case(' ')
     Traceback (most recent call last):
     ...
-    Exception: Provide camel case eg: helloWorld
-    """
-
-    """
-    Raising the error if inputted data is not in the required format
+    TypeError: Provide camel case eg: helloWorld
     """
 
     # checks if the word has capital character in it,
     # and first character is not the capital.
-    if word != word.lower() and word[0] == word[0].lower():
-        snake_case = ""
-        index_list = []
-        # check number of Capital letter and append their indexes in list
-        for character in word:
-            if character != character.lower():
-                index_list.append(word.index(character))
-
-        # append one more element
-        index_list.append(100)
-
-        # initializing few variables
-        i = 0
-        temp = 1
-
-        # changing original string to lowercase
-        word = word.lower()
-
-        for index in index_list:  # iterates over list of indexes
-            """
-            # checks if the temp is less than length of the list
-            # (temp variables checks the number of Capital letters index
-            # as we don't want to add underscore at the end
-            # of the return string)"""
-            if temp < len(index_list):
-                # adds substring to snake_case strings
-                snake_case = snake_case + word[i:index] + "_"
-
-            # if temp value more than the length of the index list
-            # it executes this statement
-            # as we don't want to add (_) at the end.
-            elif temp >= len(index_list):
-                snake_case = snake_case + word[i:]
-            temp += 1
-            i = index
-
-        # Returns the string
-        return snake_case
-
-    else:
+    if word == word.lower() or word[0] == word[0].upper():
+        # Raising the error if inputted data is not in the required format
         raise TypeError("Provide camel case eg: helloWorld")
 
+    snake_case = ""
+    index_list = []
+    # check number of Capital letter and append their indexes in list
+    for character in word:
+        if character != character.lower():
+            index_list.append(word.index(character))
+
+    # append one more element
+    index_list.append(100)
+
+    # initializing few variables
+    i = 0
+    temp = 1
+
+    # changing original string to lowercase
+    word = word.lower()
+
+    for index in index_list:  # iterates over list of indexes
+        """
+        # checks if the temp is less than length of the list
+        # (temp variables checks the number of Capital letters index
+        # as we don't want to add underscore at the end
+        # of the return string)"""
+        if temp < len(index_list):
+            # adds substring to snake_case strings
+            snake_case = snake_case + word[i:index] + "_"
+
+        # if temp value more than the length of the index list
+        # it executes this statement
+        # as we don't want to add (_) at the end.
+        elif temp >= len(index_list):
+            snake_case = snake_case + word[i:]
+        temp += 1
+        i = index
+
+    # Returns the string
+    return snake_case
 
 if __name__ == "__main__":
     import doctest
--- strings/remove_vowels.py
+++ strings/remove_vowels.py
@@ -1,11 +1,19 @@
-# This function removes all the vowels present in the given sentence or a word.
-# returns the modified version of that string.
+"""
+A vowel is a sound such as the ones represented in writing 
+by the letters 'a', 'e', 'i', ' o', and 'u', 
+which you pronounce with your mouth open, allowing the air to flow through it.
+
+This function removes all the vowels present in the given sentence or a word.
+returns the modified version of that string.
+eg: HEllO World >> HLL Wrld
+"""
 
 
 def remove_vowels(sentence: str) -> str:
     """
     This remove_vowels function removes all the vowels
     from a given sentence or a word
+    
     >>> remove_vowels('Hello world')
     'Hll wrld'
     >>> remove_vowels('Programming is my passion')
@@ -25,7 +33,7 @@ def remove_vowels(sentence: str) -> str:
         sentence = sentence.replace(vowel, "")
 
     # returning a updated string
-    return sentence  # return --str
+    return sentence
 
 
 if __name__ == "__main__":
