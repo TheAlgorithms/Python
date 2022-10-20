cfdebf36c1e2e514712bc7ec20f66c30faa7a54d
Alan Paul <57307037+Alanzz@users.noreply.github.com>
2022-10-20 11:25:45 +0530

    Create quantum_random.py

---
+++ quantum/quantum_random.py
@@ -0,0 +1,21 @@
+from projectq.ops import H, Measure
+from projectq import MainEngine
+
+def get_random_number(quantum_engine):
+    qubit = quantum_engine.allocate_qubit()
+    H | qubit
+    Measure | qubit
+    random_number = int(qubit)
+    return random_number
+
+# This list is used to store our random numbers
+random_numbers_list = []
+# initialises a new quantum backend
+quantum_engine = MainEngine()
+# for loop to generate 10 random numbers
+for i in range(10):
+    # calling the random number function and append the return to the list
+    random_numbers_list.append(get_random_number(quantum_engine))
+# Flushes the quantum engine from memory
+quantum_engine.flush()
+print('Random numbers', random_numbers_list)
