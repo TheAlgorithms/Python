50946971728b7120d6a5e1052e4b208319e20135
Ravi Ranjan <ravi1120ranjan@gmail.com>
2022-10-20 10:05:48 +0530

    Add Tower of Hanoi Code

---
+++ divide_and_conquer/tower_of_hanoi.py
@@ -0,0 +1,14 @@
+# Creating a recursive function  
+def tower_of_hanoi(disks, source, auxiliary, target):  
+    if(disks == 1):  
+        print('Move disk 1 from rod {} to rod {}.'.format(source, target))  
+        return  
+    # function call itself  
+    tower_of_hanoi(disks - 1, source, target, auxiliary)  
+    print('Move disk {} from rod {} to rod {}.'.format(disks, source, target))  
+    tower_of_hanoi(disks - 1, auxiliary, source, target)  
+  
+  
+disks = int(input('Enter the number of disks: '))  
+# We are referring source as A, auxiliary as B, and target as C  
+tower_of_hanoi(disks, 'A', 'B', 'C')  # Calling the function
