32091d3955012553b734066adc589717533e812d
Modassir Afzal <60973906+Moddy2024@users.noreply.github.com>
2022-10-21 02:12:07 +0530

    Update xgboostclassifier.py

--- machine_learning/xgboostclassifier.py
+++ machine_learning/xgboostclassifier.py
@@ -24,7 +24,16 @@ def data_handling(data: dict) -> tuple:
 def xgboost(features: np.ndarray, target: np.ndarray) -> XGBClassifier:
     """
     >>> xgboost(np.array([[5.1, 3.6, 1.4, 0.2]]), np.array([0]))
-    XGBClassifier()
+    XGBClassifier(base_score=0.5, booster='gbtree', callbacks=None,
+                  colsample_bylevel=1, colsample_bynode=1, colsample_bytree=1,
+                  early_stopping_rounds=None, enable_categorical=False,
+                  eval_metric=None, gamma=0, gpu_id=-1, grow_policy='depthwise',
+                  importance_type=None, interaction_constraints='',
+                  learning_rate=0.300000012, max_bin=256, max_cat_to_onehot=4,
+                  max_delta_step=0, max_depth=6, max_leaves=0, min_child_weight=1,
+                  missing=nan, monotone_constraints='()', n_estimators=100,
+                  n_jobs=0, num_parallel_tree=1, predictor='auto', random_state=0,
+                  reg_alpha=0, reg_lambda=1)
     """
     classifier = XGBClassifier()
     classifier.fit(features, target)
