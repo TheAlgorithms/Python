8d2f4431374bcdd36e3744716e7a03424180d367
Vishal Awasthi <85893361+Vishawa@users.noreply.github.com>
2022-10-20 18:24:00 +0530

    Add files via upload

---
+++ backtracking/generate_parenthesis.py
@@ -0,0 +1,8 @@
+def generateParenthesis(self, n):
+    def generate(p, left, right):
+        if right >= left >= 0:
+            if not right:
+                yield p
+            for q in generate(p + '(', left-1, right): yield q
+            for q in generate(p + ')', left, right-1): yield q
+    return list(generate('', n, n))
