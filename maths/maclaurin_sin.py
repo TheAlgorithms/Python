921f361f00064657e19b9f5f7b645c9d71bbe2c9
Chris O <46587501+ChrisO345@users.noreply.github.com>
2022-10-21 19:58:55 +1300

    fixed pytest

--- maths/maclaurin_sin.py
+++ maths/maclaurin_sin.py
@@ -43,16 +43,20 @@ def maclaurin_sin(theta: float, accuracy: int = 30) -> float:
     """
 
     if not isinstance(theta, (int, float)):
-        raise ValueError("maclaurin_sin() requires either an int or float for theta")
+        raise ValueError(
+            "maclaurin_sin() requires either an int or float for theta"
+        )
 
     if not isinstance(accuracy, int) or accuracy <= 0:
-        raise ValueError("maclaurin_sin() requires a positive int for accuracy")
+        raise ValueError(
+            "maclaurin_sin() requires a positive int for accuracy"
+        )
 
     theta = float(theta)
     div = theta // (2 * pi)
     theta -= 2 * div * pi
     return sum(
-        ((-1) ** r) * ((theta ** (2 * r + 1)) / factorial(2 * r + 1))
+        (((-1) ** r) * ((theta ** (2 * r + 1)) / factorial(2 * r + 1))) 
         for r in range(accuracy)
     )
 
