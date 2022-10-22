67e03af4f9681d78bcb43e57b870203b3e37b93c
J <jasondevers@wireless-10-105-49-243.umd.edu>
2022-10-22 19:38:43 -0400

    added another doctest, as well as a explanation above

--- matrix/count_paths.py
+++ matrix/count_paths.py
@@ -1,4 +1,26 @@
-def dfs(grid : list(list(int)), row: int, col: int, visit: set()) -> int:
+"""
+Given a grid, where you start from the top left position [0, 0],
+you want to find how many paths you can take to get to the bottom right position.
+
+start here  ->   0  0  0  0
+                 1  1  0  0
+                 0  0  0  1
+                 0  1  0  0  <- finish here
+how many 'distinct' paths can you take to get to the finish?
+Using a recursive depth-first search algorithm below, you are able to 
+find the number of distinct unique paths (count). 
+
+'*' will demostrate a path
+In the example above, there are two distinct paths:
+1.                2.
+    *  *  *  0      *  *  *  *
+    1  1  *  0      1  1  *  *          
+    0  0  *  1      0  0  *  1
+    0  1  *  *      0  1  *  * 
+"""
+
+
+def dfs(grid: "list[list[int]]", row: int, col: int, visit: set) -> int:
     """
     Recursive Backtracking Depth First Search Algorithm
 
@@ -6,15 +28,36 @@ def dfs(grid : list(list(int)), row: int, col: int, visit: set()) -> int:
     1 represents a block (inaccessible)
     0 represents a valid space (accessible)
 
+    0  0  0  0
+    1  1  0  0
+    0  0  0  1
+    0  1  0  0
     >>> start_row = 0
     >>> start_col = 0
     >>> visited = set()
     >>> grid = [[0, 0, 0, 0], [1, 1, 0, 0], [0, 0, 0, 1], [0, 1, 0, 0]]
     >>> dfs(grid, start_row, start_col, visited)
     2
+
+    0  0  0  0  0
+    0  1  1  1  0
+    0  1  1  1  0
+    0  0  0  0  0
+    >>> start_row = 0
+    >>> start_col = 0
+    >>> visited = set()
+    >>> grid = [[0, 0, 0, 0, 0], [0, 1, 1, 1, 0], [0, 1, 1, 1, 0], [0, 0, 0, 0, 0]]
+    >>> dfs(grid, start_row, start_col, visited)
+    2
     """
     row_length, col_length = len(grid), len(grid[0])
-    if min(row, col) < 0 or row == row_length or col == col_length or (row, col) in visit or grid[row][col] == 1:
+    if (
+        min(row, col) < 0
+        or row == row_length
+        or col == col_length
+        or (row, col) in visit
+        or grid[row][col] == 1
+    ):
         return 0
     if row == row_length - 1 and col == col_length - 1:
         return 1
@@ -29,6 +72,7 @@ def dfs(grid : list(list(int)), row: int, col: int, visit: set()) -> int:
 
     visit.remove((row, col))
     return count
+
 
 if __name__ == "__main__":
     import doctest
