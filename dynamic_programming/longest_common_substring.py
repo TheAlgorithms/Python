2760b2f5457403fcc571476b77ad0191f562c104
Rohit <RohitSinghEmail@protonmail.com>
2022-10-23 19:47:26 +0530

    removed space between line

--- dynamic_programming/longest_common_substring.py
+++ dynamic_programming/longest_common_substring.py
@@ -10,7 +10,6 @@ Therefore, the length is 2.
 def longest_common_substring(text1: str, text2: str) -> int:
     """
     Finds the length of longest common substring between two strings.
-
     >>> longest_common_substring("", "")
     0
     >>> longest_common_substring("a","")
