a832ad864ed079d78195ac9c6787186b13f77844
Christian Clauss <cclauss@me.com>
2022-10-23 14:18:57 +0200

    Update and rename system_of_linear_equation_in_2_variables.py to cramers_rule_2x2.py

--- matrix/cramers_rule_2x2.py
+++ matrix/cramers_rule_2x2.py
@@ -2,80 +2,81 @@
 # https://en.wikipedia.org/wiki/Cramer%27s_rule
 
 
-def calculate_x_and_y(equation1: list[int], equation2: list[int]) -> str:
+def cramers_rule_2x2(equation1: list[int], equation2: list[int]) -> str:
     """
     Solves the system of linear equation in 2 variables.
     :param: equation1: list of 3 numbers
     :param: equation2: list of 3 numbers
     :return: String of result
     input format : [a1, b1, d1], [a2, b2, d2]
-    d --> determinant or determinant_matrix
-    dx --> determinant_x or determinant_matrix_x
-    dy --> determinant_x or determinant_matrix_y
+    determinant = [[a1, b1], [a2, b2]]
+    determinant_x = [[d1, b1], [d2, b2]]
+    determinant_y = [[a1, d1], [a2, d2]]
 
-    >>> calculate_x_and_y([1, 2, 3], [2, 4, 6])
+    >>> cramers_rule_2x2([2, 3, 0], [5, 1, 0])
+    'Trivial solution. (Consistent system) x = 0 and y = 0'
+    >>> cramers_rule_2x2([0, 4, 50], [2, 0, 26])
+    'Non-Trivial Solution (Consistent system) x = 13.0, y = 12.5'
+    >>> cramers_rule_2x2([11, 2, 30], [1, 0, 4])
+    'Non-Trivial Solution (Consistent system) x = 4.0, y = -7.0'
+    >>> cramers_rule_2x2([4, 7, 1], [1, 2, 0])
+    'Non-Trivial Solution (Consistent system) x = 2.0, y = -1.0'
+
+    >>> cramers_rule_2x2([1, 2, 3], [2, 4, 6])
     Traceback (most recent call last):
         ...
     ValueError: Infinite solutions. (Consistent system)
-    >>> calculate_x_and_y([1, 2, 3], [2, 4, 7])
+    >>> cramers_rule_2x2([1, 2, 3], [2, 4, 7])
     Traceback (most recent call last):
         ...
     ValueError: No solution. (Inconsistent system)
-    >>> calculate_x_and_y([1, 2, 3], [11, 22])
+    >>> cramers_rule_2x2([1, 2, 3], [11, 22])
     Traceback (most recent call last):
         ...
     ValueError: Please enter a valid equation.
-    >>> calculate_x_and_y([11, 2, 30], [1, 0, 4])
-    'Non-Trivial Solution (Consistent system) x = 4.0, y = -7.0'
-    >>> calculate_x_and_y([0, 1, 6], [0, 0, 3])
+    >>> cramers_rule_2x2([0, 1, 6], [0, 0, 3])
     Traceback (most recent call last):
         ...
     ValueError: No solution. (Inconsistent system)
-    >>> calculate_x_and_y([0, 0, 6], [0, 0, 3])
+    >>> cramers_rule_2x2([0, 0, 6], [0, 0, 3])
     Traceback (most recent call last):
         ...
     ValueError: Both a & b of two equations can't be zero.
-    >>> calculate_x_and_y([4, 7, 1], [1, 2, 0])
-    'Non-Trivial Solution (Consistent system) x = 2.0, y = -1.0'
-    >>> calculate_x_and_y([1, 2, 3], [1, 2, 3])
+    >>> cramers_rule_2x2([1, 2, 3], [1, 2, 3])
     Traceback (most recent call last):
         ...
     ValueError: Infinite solutions. (Consistent system)
-    >>> calculate_x_and_y([2, 3, 0], [5, 1, 0])
-    'Trivial solution. (Consistent system) x = 0 and y = 0'
-    >>> calculate_x_and_y([0, 4, 50], [2, 0, 26])
-    'Non-Trivial Solution (Consistent system) x = 13.0, y = 12.5'
-    >>> calculate_x_and_y([0, 4, 50], [0, 3, 99])
+    >>> cramers_rule_2x2([0, 4, 50], [0, 3, 99])
     Traceback (most recent call last):
         ...
     ValueError: No solution. (Inconsistent system)
     """
 
-    # Checking if the input is valid
+    # Check if the input is valid
     if not len(equation1) == len(equation2) == 3:
         raise ValueError("Please enter a valid equation.")
-    elif equation1[0] == equation1[1] == equation2[0] == equation2[1] == 0:
+    if equation1[0] == equation1[1] == equation2[0] == equation2[1] == 0:
         raise ValueError("Both a & b of two equations can't be zero.")
 
-    # Extracting the coefficients
+    # Extract the coefficients
     a1, b1, c1 = equation1
     a2, b2, c2 = equation2
 
-    # Calculating the determinant of matrix d_matrix, dx_matrix and dy_matrix
-    d = a1 * b2 - a2 * b1
-    dx = c1 * b2 - c2 * b1
-    dy = a1 * c2 - a2 * c1
+    # Calculate the determinants of the matrices
+    determinant = a1 * b2 - a2 * b1
+    determinant_x = c1 * b2 - c2 * b1
+    determinant_y = a1 * c2 - a2 * c1
 
-    # Checking if the system of linear equation has a solution (Using Cramer's rule)
-    if d == 0:
-        if dx == dy == 0:
+    # Check if the system of linear equations has a solution (using Cramer's rule)
+    if determinant == 0:
+        if determinant_x == determinant_y == 0:
             raise ValueError("Infinite solutions. (Consistent system)")
         else:
             raise ValueError("No solution. (Inconsistent system)")
     else:
-        if dx == dy == 0:
+        if determinant_x == determinant_y == 0:
             return "Trivial solution. (Consistent system) x = 0 and y = 0"
         else:
-            x = dx / d
-            y = dy / d
+            x = determinant_x / determinant
+            y = determinant_y / determinant
             return f"Non-Trivial Solution (Consistent system) x = {x}, y = {y}"
