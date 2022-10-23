c8bfc97c7d4ae657a7aa0626a660e4166130d27c
piyush <piyushrao56@gmail.com>
2022-10-23 22:04:35 +0530

    modified graph file

--- graphs/adjascency_list.py
+++ graphs/adjascency_list.py
@@ -1,46 +1,60 @@
-# Adjascency List representation in Python
+"""
+A Python program to demonstrate the adjacency
+list representation of the graph
+"""
+
+# A class to represent the adjacency list of the node
 
 
 class AdjNode:
-    def __init__(self, value):
-        self.vertex = value
-        self.next = None
+	def __init__(self, data):
+		self.vertex = data
+		self.next = None
 
 
+# A class to represent a graph. A graph
+# is the list of the adjacency lists.
+# Size of the array will be the no. of the
+# vertices "V"
 class Graph:
-    def __init__(self, num):
-        self.V = num
-        self.graph = [None] * self.V
+	def __init__(self, vertices):
+		self.V = vertices
+		self.graph = [None] * self.V
 
-    # Add edges
-    def add_edge(self, s, d):
-        node = AdjNode(d)
-        node.next = self.graph[s]
-        self.graph[s] = node
+	# Function to add an edge in an undirected graph
+	def add_edge(self, src, dest):
+		# Adding the node to the source node
+		node = AdjNode(dest)
+		node.next = self.graph[src]
+		self.graph[src] = node
 
-        node = AdjNode(s)
-        node.next = self.graph[d]
-        self.graph[d] = node
+		# Adding the source node to the destination as
+		# it is the undirected graph
+		node = AdjNode(src)
+		node.next = self.graph[dest]
+		self.graph[dest] = node
 
-    # Print the graph
-    def print_agraph(self):
-        for i in range(self.V):
-            print("Vertex " + str(i) + ":", end="")
-            temp = self.graph[i]
-            while temp:
-                print(f" -> {temp.vertex}", end="")
-                temp = temp.next
-            print(" \n")
+	# Function to print the graph
+	def print_graph(self):
+		for i in range(self.V):
+			print("Adjacency list of vertex {}\n head".format(i), end="")
+			temp = self.graph[i]
+			while temp:
+				print(" -> {}".format(temp.vertex), end="")
+				temp = temp.next
+			print(" \n")
 
 
+# Driver program to the above graph class
 if __name__ == "__main__":
-    V = 5
+	V = 5
+	graph = Graph(V)
+	graph.add_edge(0, 1)
+	graph.add_edge(0, 4)
+	graph.add_edge(1, 2)
+	graph.add_edge(1, 3)
+	graph.add_edge(1, 4)
+	graph.add_edge(2, 3)
+	graph.add_edge(3, 4)
 
-    # Create graph and edges
-    graph = Graph(V)
-    graph.add_edge(0, 1)
-    graph.add_edge(0, 2)
-    graph.add_edge(0, 3)
-    graph.add_edge(1, 2)
-
-    graph.print_agraph()
+	graph.print_graph()
