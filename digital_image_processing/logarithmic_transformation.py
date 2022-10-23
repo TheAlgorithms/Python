93e379f4a57c37b7dc93083d296dd4a7d7cd89af
Kuldeep Borkar Jr <kuldeepborkarjr765@gmail.com>
2022-10-23 17:23:46 +0530

    Added logarithmic_transformation in digital_image_processing

---
+++ digital_image_processing/logarithmic_transformation.py
@@ -0,0 +1,39 @@
+"""
+Logarithmic(Log) Transformation of an image is a type of gray level image
+transformations which replaces all pixel values in the original image
+with its logarithmic values.
+
+The formula is:
+
+S =  255 * (log(input_image_pixel_value + 1) / log(1 + max_input_image_pixel_value))
+
+The script is inspired from
+* https://www.geeksforgeeks.org/log-transformation-of-an-image-using-python-and-opencv
+
+"""
+
+import numpy as np
+from cv2 import destroyAllWindows, imread, imshow, waitKey
+
+
+def logarithmic_transform(image):
+    # Transforming pixel values
+    image_log = (np.log(image + 1) / np.log(1 + np.max(image))) * 255
+
+    # Using dtype = np.uint8 to convert float value to int
+    image_log = np.array(image_log, dtype=np.uint8)
+
+    return image_log
+
+
+if __name__ == "__main__":
+    # Read pixel values of original image
+    image = imread("image_data/lena.jpg")
+
+    # Apply log transformation
+    image_log_transform = logarithmic_transform(image)
+
+    # Show Transformed Image
+    imshow("Log transform of original image", image_log_transform)
+    waitKey(0)
+    destroyAllWindows()
