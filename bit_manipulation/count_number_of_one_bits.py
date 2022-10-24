3597d0c8fa3c5d93b4fad83b6154d7869a8001ea
JatinR05 <71865805+JatinR05@users.noreply.github.com>
2022-10-24 12:01:36 +0530

    Update count_number_of_one_bits.py
    removed the modulo operator as it is very time consuming in comparison to the and operator

--- bit_manipulation/count_number_of_one_bits.py
+++ bit_manipulation/count_number_of_one_bits.py
@@ -22,7 +22,7 @@ def get_set_bits_count(number: int) -> int:
         raise ValueError("the value of input must be positive")
     result = 0
     while number:
-        if number % 2 == 1:
+        if number & 1:
             result += 1
         number = number >> 1
     return result
