56bf6653a344313ee0e249533607ba9e3b826b97
Arjit Arora <42044030+arjitarora26@users.noreply.github.com>
2022-10-24 12:37:29 +0530

    Update boolean_algebra/xor_gate.py
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- boolean_algebra/xor_gate.py
+++ boolean_algebra/xor_gate.py
@@ -1,7 +1,6 @@
 """
-A XOR Gate is a logic gate in boolean algebra which results to True(1)
-if only one of the two inputs is 1, and False(0) if even number
-of inputs are 1.
+A XOR Gate is a logic gate in boolean algebra which results to 1 (True) if only one of the two inputs is 1, and
+0 (False) if an even number of inputs are 1.
 Following is the truth table of a XOR Gate:
     ------------------------------
     | Input 1 | Input 2 | Output |
