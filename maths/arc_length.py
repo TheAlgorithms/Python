2301ae904fd706d0d827b662fda8b4a8676346bf
Chris O <46587501+ChrisO345@users.noreply.github.com>
2022-10-25 18:29:09 +1300

    Removed redundant statement, fixed line overflow

--- maths/arc_length.py
+++ maths/arc_length.py
@@ -1,4 +1,4 @@
-import math
+from math import pi
 
 
 def arc(angle: int, rad: int) -> float:
@@ -8,14 +8,8 @@ def arc(angle: int, rad: int) -> float:
     >>> arc(120, 15)
     31.415926535897928
     """
-    pi = math.pi
-
     return 2 * pi * rad * (angle / 360)
 
 
 if __name__ == "__main__":
-    angle = 90
-    rad = 10
-    print(
-        f"The length of the arc with radius {rad} and a central angle of {angle} degrees is {arc(angle, rad)}"
-    )
+    print(arc(90, 10))
