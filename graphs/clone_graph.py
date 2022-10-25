9c55075ce163016c415dab190da8a2be30a4209c
Anshraj Shrivastava <42239140+rajansh87@users.noreply.github.com>
2022-10-25 11:12:48 +0530

    Update clone_graph.py

--- graphs/clone_graph.py
+++ graphs/clone_graph.py
@@ -1,29 +1,28 @@
 # Definition for a Node.
 class Node:
-    def __init__(self, val=0, neighbors: list = None) -> None:
+    def __init__(self, val:int = 0, neighbors: list = None) -> None:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
-
 
 # Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
 # Output: [[2,4],[1,3],[2,4],[1,3]]
 
 
-class Solution:
-    def clone_graph(self, node: "Node") -> "Node":
-        if node == None:
-            return node
-        visited = [node]
-        graph = dict()
-        graph[node.val] = Node(node.val, [])
+def clone_graph(node: Node) -> Node:
+    if node == None:
+        return node
+    visited = [node]
+    graph = dict()
+    graph[node.val] = Node(node.val, [])
 
-        while len(visited) > 0:
-            cur = visited.pop(0)
-            cur_cloned = graph[cur.val]
+    while len(visited) > 0:
+        cur = visited.pop(0)
+        cur_cloned = graph[cur.val]
 
-            for ngbr in cur.neighbors:
-                if ngbr.val not in graph:
-                    graph[ngbr.val] = Node(ngbr.val, [])
-                    visited.append(ngbr)
-                cur_cloned.neighbors.append(graph[ngbr.val])
-        return graph[node.val]
+        for ngbr in cur.neighbors:
+            if ngbr.val not in graph:
+                graph[ngbr.val] = Node(ngbr.val, [])
+                visited.append(ngbr)
+            cur_cloned.neighbors.append(graph[ngbr.val])
+    return graph[node.val]
+    
