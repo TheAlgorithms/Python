88a195720fdf44588ff6424845c5c4f0b25d157b
Anshraj Shrivastava <42239140+rajansh87@users.noreply.github.com>
2022-10-25 11:05:02 +0530

    Update clone_graph.py

--- graphs/clone_graph.py
+++ graphs/clone_graph.py
@@ -1,6 +1,6 @@
 # Definition for a Node.
 class Node:
-    def __init__(self, val=0, neighbors=None):
+    def __init__(self, val=0, neighbors=None) -> None:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 
