84a7e46e8a5705275ad16e65fd3358d494037d37
Anshraj Shrivastava <42239140+rajansh87@users.noreply.github.com>
2022-10-25 12:00:57 +0530

    Update permutations.py

--- data_structures/arrays/permutations.py
+++ data_structures/arrays/permutations.py
@@ -13,20 +13,6 @@ def permute(nums: list[int]) -> list[list[int]]:
     return result
 
 
-# Unique permutations:
-def permute_unique(nums: list[int]) -> list[list[int]]:
-    result = []
-    if len(nums) == 1:
-        return [nums.copy()]
-    for _ in range(len(nums)):
-        n = nums.pop(0)
-        permutations = permute_unique(nums)
-        for perm in permutations:
-            perm.append(n)
-        result.extend(permutations)
-        nums.append(n)
-    ans = []
-    for i in result:
-        if i not in ans:
-            ans.append(i)
-    return ans
+if __name__=="__main__":
+	nums=[1,2,3]
+	print(permute(nums))
