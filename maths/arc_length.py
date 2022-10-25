9642ff21c1d25b0710fed8070c4d2c03505dbb9f
Havish <100441982+havishs9@users.noreply.github.com>
2022-10-24 21:32:55 -0700

    Create arc_length.py

---
+++ maths/arc_length.py
@@ -0,0 +1,19 @@
+ 
+
+import math
+def arc(angle: int, r: int) -> int:
+    """
+    >>> arc(45, 5)
+    3.9269908169872414
+    >>> arc(120, 15)
+    31.415926535897928
+    """
+    pi = math.pi
+
+    return 2 * pi * r * (angle / 360)
+
+
+if __name__ == "__main__":
+    angle = 90
+    r = 10
+    print(f"The length of the arc with radius {r} and a central angle of {angle} degrees is {arc(angle, r)}")
