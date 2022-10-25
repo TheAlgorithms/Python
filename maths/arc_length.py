541db3fab0a966e4255f34e27667d9c6497d050c
Chris O <46587501+ChrisO345@users.noreply.github.com>
2022-10-25 19:03:51 +1300

    Update arc_length.py
    Changed rad to radius as not to get confused with radians

--- maths/arc_length.py
+++ maths/arc_length.py
@@ -1,14 +1,14 @@
 from math import pi
 
 
-def arc(angle: int, rad: int) -> float:
+def arc(angle: int, radius: int) -> float:
     """
     >>> arc(45, 5)
     3.9269908169872414
     >>> arc(120, 15)
     31.415926535897928
     """
-    return 2 * pi * rad * (angle / 360)
+    return 2 * pi * radius * (angle / 360)
 
 
 if __name__ == "__main__":
