00016481a01bd7f8148ba833e5647dd7d4c28b7f
Aashif Ahamed <aashifahamed0796@gmail.com>
2022-10-25 15:33:33 +0530

    Create basic_pattern_searching_algorithm.py
    Basic Pattern Searching algorithm. Searching pattern from a text

---
+++ searches/basic_pattern_searching_algorithm.py
@@ -0,0 +1,32 @@
+
+# Autor : Aashif Ahamed
+# Name : Basic Pattern Searching algorithm
+
+
+def search(pat, txt):
+    M = len(pat)
+    N = len(txt)
+ 
+    # A loop to slide pat[] one by one */
+    for i in range(N - M + 1):
+        j = 0
+ 
+        # For current index i, check
+        # for pattern match */
+        while(j < M):
+            if (txt[i + j] != pat[j]):
+                break
+            j += 1
+ 
+        if (j == M):
+            print("Pattern found at index ", i)
+ 
+ 
+# main Code
+if __name__ == '__main__':
+    txt = "AABAACAADAABAAABAA"
+    pat = "AABA"
+     
+    # Function call
+    search(pat, txt)
+ 
