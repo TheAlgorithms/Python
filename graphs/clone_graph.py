576dabab94e11e1b81f00411f529882ca14c36c1
Anshraj Shrivastava <42239140+rajansh87@users.noreply.github.com>
2022-10-25 11:02:38 +0530

    Update clone_graph.py

--- graphs/clone_graph.py
+++ graphs/clone_graph.py
@@ -1,10 +1,10 @@
-"""
+
 # Definition for a Node.
-class node:
+class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
-"""
+
 # Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
 # Output: [[2,4],[1,3],[2,4],[1,3]]
 
