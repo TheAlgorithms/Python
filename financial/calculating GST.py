7febf4cf3604fae4a824298e3c55aa9fb4aa64b2
Sushant Srivastav <63559772+sushant4191@users.noreply.github.com>
2022-10-27 02:26:46 +0530

    Update financial/calculating GST.py
    Thanks!
    
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- financial/calculating GST.py
+++ financial/calculating GST.py
@@ -1,7 +1,13 @@
 # The Program helps to calculate the net amount of a item when a certain percent of GST is applied.
 
 
-def calculate(Original_price, GST_per):
+def price_plus_tax(price: float, tax_rate: float) -> float:
+    """
+    >>> price_plus_tax(100, 0.25)
+    125
+    >>> price_plus_tax(125.50, 0.05)
+    131.775
+    """
     GST_amount = float((GST_per * Original_price) / 100)
     Price_wGST = Original_price + GST_amount
     print("Net Price after ", GST_per, "% GST is = ", Price_wGST)
