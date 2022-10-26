af2f919c4bd2f6204fbc373d7cf06ccb649f8d46
SnehalSLohar <snehallohar17@gmail.com>
2022-10-26 15:40:34 +0530

    concatenation of strings

---
+++ strings/concat.py
@@ -0,0 +1,11 @@
+var1 = "Hello"
+var2 = "World"
+
+# join() method is used to combine the strings
+print("".join([var1, var2]))
+
+# join() method is used here to combine
+# the string with a separator Space(" ")
+var3 = " ".join([var1, var2])
+
+print(var3)
