39beebd56b76438d39ab1379afbb8e8c98bb8fb2
Christian Clauss <cclauss@me.com>
2022-10-26 23:11:41 +0200

    Update price_plus_tax.py

--- financial/price_plus_tax.py
+++ financial/price_plus_tax.py
@@ -6,7 +6,7 @@ Calculate price plus tax of a good or service given its price and a tax rate.
 def price_plus_tax(price: float, tax_rate: float) -> float:
     """
     >>> price_plus_tax(100, 0.25)
-    125
+    125.0
     >>> price_plus_tax(125.50, 0.05)
     131.775
     """
