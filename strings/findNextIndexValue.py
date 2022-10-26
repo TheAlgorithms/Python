94d320612ffa110e721337330e36c7eb4b0cd010
Rohan Thapa <62699601+irohanthapa@users.noreply.github.com>
2022-10-26 22:58:37 +0530

    Created a file

---
+++ strings/findNextIndexValue.py
@@ -0,0 +1,10 @@
+
+def find_char():
+    str1 = input()
+    char = input()
+    if char in str1 and str1[-1]!=char:
+       print(str1.index(char)+1)
+    else:
+        print("not found")
+
+find_char()
