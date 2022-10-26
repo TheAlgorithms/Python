dbaa6062fc5e75642732d141bbef62dd8ba725da
JatinR05 <71865805+JatinR05@users.noreply.github.com>
2022-10-26 19:28:08 +0530

    Update minimum_squares_to_represent_a_number.py
    updated

--- dynamic_programming/minimum_squares_to_represent_a_number.py
+++ dynamic_programming/minimum_squares_to_represent_a_number.py
@@ -1,6 +1,4 @@
-import math
-import sys
-
+import math,sys
 
 def minimum_squares_to_represent_a_number(number: int) -> int:
     """
@@ -16,7 +14,7 @@ def minimum_squares_to_represent_a_number(number: int) -> int:
     >>> minimum_squares_to_represent_a_number(-1)
     Traceback (most recent call last):
         ...
-    ValueError: the value of input must be positive
+    ValueError: the value of input must not be a negative number
     >>> minimum_squares_to_represent_a_number(0)
     1
     >>> minimum_squares_to_represent_a_number(12.34)
@@ -30,17 +28,16 @@ def minimum_squares_to_represent_a_number(number: int) -> int:
         raise ValueError("the value of input must not be a negative number")
     if number == 0:
         return 1
-    answers = [-1] * (number + 1)
+    answers = [-1] * (number+1)
     answers[0] = 0
-    for i in range(1, number + 1):
+    for i in range(1,number+1):
         answer = sys.maxsize
         root = int(math.sqrt(i))
-        for j in range(1, root + 1):
-            current_answer = 1 + answers[i - (j**2)]
-            answer = min(answer, current_answer)
+        for j in range(1,root+1):
+            current_answer = 1 + answers[i-(j**2)]
+            answer = min(answer,current_answer)
         answers[i] = answer
     return answers[number]
-
 
 if __name__ == "__main__":
     import doctest
