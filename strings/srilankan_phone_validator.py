3c0c7a37f2c4a53fb1d4a4f1a14ce9a944cbe3ae
Sineth Sankalpa <66241389+sinsankio@users.noreply.github.com>
2022-10-26 19:28:43 +0530

    Update srilankan_phone_validator.py with new changes

--- strings/srilankan_phone_validator.py
+++ strings/srilankan_phone_validator.py
@@ -1,6 +1,6 @@
 import re
 
-def srilanka_phone_validator(phone: str) -> bool:
+def is_sri_lankan_phone_number(phone: str) -> bool:
     """
     Determine whether the string is a valid sri lankan phone number or not
     :param phone:
@@ -21,14 +21,11 @@ def srilanka_phone_validator(phone: str) -> bool:
     False
     """
 
-    pattern = re.compile(r'^(?:0|94|\+94|0094)?(?:(11|21|23|24|25|26|27|31|32|33|34|35|36|37|38|41|45|47|51|52|54|55|57|63|65|66|67|81|91)(0|2|3|4|5|7|9)|7(0|1|2|4|5|6|7|8)\d)\d{6}$')
-    matched = re.search(pattern, phone)
-
-    if matched:
-        return True
-    return False
+    pattern = re.compile(r'^(?:0|94|\+94|0{2}94)?(?:(1{2}|21|23|24|25|26|27|31|32|3{2}|34|35|36|37|38|41|45|47|51|52|54|5{2}|57|63|65|6{2}|67|81|91)(0|2|3|4|5|7|9)|7(0|1|2|4|5|6|7|8)\d)\d{6}$')    
+    
+    return bool(re.search(pattern, phone))
 
 if __name__ == '__main__':
-    phone = "0957651234"
+    phone = "+94475682163"
 
-    print(srilanka_phone_validator(phone))
+    print(is_sri_lankan_phone_number(phone))    
