9cad4b8b5a6033c79961bed45c816c343930ff17
Anshraj Shrivastava <42239140+rajansh87@users.noreply.github.com>
2022-10-26 18:04:09 +0530

    Update permutations.py

--- data_structures/arrays/permutations.py
+++ data_structures/arrays/permutations.py
@@ -1,6 +1,5 @@
 from itertools import permutations
 
-
 # All permutations:
 def permute(nums: list[int]) -> list[list[int]]:
     """
@@ -21,11 +20,7 @@ def permute(nums: list[int]) -> list[list[int]]:
         nums.append(n)
     return result
 
-
 if __name__ == "__main__":
     numbers = [1, 2, 3]
     print("permutations are:", permute(numbers))
-    print(
-        "isValid:",
-        all(list(nums) in permute(numbers) for nums in permutations(numbers)),
-    )
+    print("isValid:",all(list(nums) in permute(numbers) for nums in permutations(numbers)),)
