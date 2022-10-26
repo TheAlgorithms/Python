2aa0be94ececceffd5d952efcf4ab899de8edd3e
Sineth Sankalpa <66241389+sinsankio@users.noreply.github.com>
2022-10-26 21:04:53 +0530

    Update is_srilankan_phone_number.py

--- strings/is_srilankan_phone_number.py
+++ strings/is_srilankan_phone_number.py
@@ -1,6 +1,5 @@
 import re
 
-
 def is_sri_lankan_phone_number(phone: str) -> bool:
 
     """
@@ -23,15 +22,11 @@ def is_sri_lankan_phone_number(phone: str) -> bool:
     False
     """
 
-    pattern = re.compile(
-        r"^(?:0|94|\+94|0{2}94)?(?:(1{2}|21|23|24|25|26|27|31|32|3{2}|34|35|36|37|38|41|45|47|"
-        r"51|52|54|5{2}|57|63|65|6{2}|67|81|91)(0|2|3|4|5|7|9)|7(0|1|2|4|5|6|7|8)\d)\d{6}$"
-    )
-
+    pattern = re.compile(r'^(?:0|94|\+94|0{2}94)?(?:(1{2}|21|23|24|25|26|27|31|32|3{2}|34|35|36|37|38|41|45|47|51|52|54|5{2}|57|63|65|6{2}|67|81|91)(0|2|3|4|5|7|9)|7(0|1|2|4|5|6|7|8)\d)\d{6}$')    
+    
     return bool(re.search(pattern, phone))
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     phone = "+94475682163"
 
-    print(is_sri_lankan_phone_number(phone))
+    print(is_sri_lankan_phone_number(phone))    
