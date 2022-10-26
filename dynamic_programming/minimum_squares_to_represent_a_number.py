f149bb64e74cd855fc671c53855082c7618085ea
JatinR05 <71865805+JatinR05@users.noreply.github.com>
2022-10-26 09:03:53 +0530

    Update minimum_squares_to_represent_a_number.py
    I have added the appropriate checks for 0 and 12.34. 
    It would be great if you could suggest a name for the dp array

--- dynamic_programming/minimum_squares_to_represent_a_number.py
+++ dynamic_programming/minimum_squares_to_represent_a_number.py
@@ -1,7 +1,3 @@
-import math
-import sys
-
-
 def minimum_squares_to_represent_a_number(number: int) -> int:
     """
     Count the number of minimum squares to represent a number
@@ -17,10 +13,20 @@ def minimum_squares_to_represent_a_number(number: int) -> int:
     Traceback (most recent call last):
         ...
     ValueError: the value of input must be positive
+    >>> minimum_squares_to_represent_a_number(0)
+    1
+    >>> minimum_squares_to_represent_a_number(12.34)
+    Traceback (most recent call last):
+        ...
+    ValueError: the value of input must be a natural number
     """
+    if number != int(number):
+        raise ValueError ("the value of input must be a natural number")
     if number < 0:
         raise ValueError("the value of input must be positive")
-    dp = [-1 for x in range(number + 1)]
+    if number == 0:
+        return 1
+    dp = [-1] * (number + 1)
     dp[0] = 0
     for i in range(1, number + 1):
         ans = sys.maxsize
@@ -34,5 +40,3 @@ def minimum_squares_to_represent_a_number(number: int) -> int:
 
 if __name__ == "__main__":
     import doctest
-
-    doctest.testmod()
