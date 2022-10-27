7e35f7dbe4d3fe6fd3a68410cab4bad3bfa52dd3
Trisha Moitra <trishamoitra.217@gmail.com>
2022-10-28 01:15:15 +0530

    Added code for binary tree path sum
    Binary tree path sum #7135

---
+++ bt_path_sum.py
@@ -0,0 +1,10 @@
+def pathSum(self, root: TreeNode, target_sum: int) -> int:
+        def dfs(node: TreeNode, sums: List[int]) -> int:
+            if not node:
+                return 0
+            sums = [s + node.val for s in sums] + [node.val]
+            ans = sums.count(target_sum)
+            ans += dfs(node.left, sums) if node.left else 0
+            ans += dfs(node.right, sums) if node.right else 0
+            return ans
+        return dfs(root, [])
