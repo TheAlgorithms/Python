a22948b4befc73e58046e817693bfebcddf1b764
ShashankZobb <shashanknke1112@gmail.com>
2022-10-28 23:13:30 +0530

    added subtract.py

---
+++ maths/subtract.py
@@ -0,0 +1,15 @@
+
+def subtract(num1: float, num2: float) -> float:
+    """
+    >>> subtract(2, 1)
+    1
+    >>> subtract(2,4)
+    -2
+    """
+    return num1-num2
+
+
+if __name__ == "__main__":
+    num1 = 2
+    num2 = 1
+    print(f"The difference between {num1} and {num2} is {subtract(num1, num2)}")
