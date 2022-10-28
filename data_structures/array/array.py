6d875f4a738b50a391ece0cf9e54a1e3a0c1ea1c
@SainiAMAN <56934027+b19149@users.noreply.github.com>
2022-10-28 23:08:38 +0530

    created

---
+++ data_structures/array/array.py
@@ -0,0 +1,23 @@
+# Python program to demonstrate
+# Creation of Array
+
+# importing "array" for array creations
+import array as arr
+
+# creating an array with integer type
+a = arr.array('i', [1, 2, 3])
+
+# printing original array
+print ("The new created array is : ", end =" ")
+for i in range (0, 3):
+	print (a[i], end =" ")
+print()
+
+# creating an array with double type
+b = arr.array('d', [2.5, 3.2, 3.3])
+
+# printing original array
+print ("The new created array is : ", end =" ")
+for i in range (0, 3):
+	print (b[i], end =" ")
+	
