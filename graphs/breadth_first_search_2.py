e16de020ae849c033a149847f32b1584c7d95484
Andrey <Cjkjvfnby@gmail.com>
2022-10-28 20:47:56 +0300

    Add both functions and timeit

--- graphs/breadth_first_search_2.py
+++ graphs/breadth_first_search_2.py
@@ -15,6 +15,8 @@ while Q is non-empty:
 from __future__ import annotations
 
 from collections import deque
+from queue import Queue
+from timeit import timeit
 
 G = {
     "A": ["B", "C"],
@@ -31,10 +33,28 @@ def breadth_first_search(graph: dict, start: str) -> list[str]:
     >>> ''.join(breadth_first_search(G, 'A'))
     'ABCDEF'
     """
+    explored = {start}
+    result = [start]
+    queue: Queue = Queue()
+    queue.put(start)
+    while not queue.empty():
+        v = queue.get()
+        for w in graph[v]:
+            if w not in explored:
+                explored.add(w)
+                result.append(w)
+                queue.put(w)
+    return result
+
+
+def breadth_first_search_with_deque(graph: dict, start: str) -> list[str]:
+    """
+    >>> ''.join(breadth_first_search_with_deque(G, 'A'))
+    'ABCDEF'
+    """
     visited = {start}
     result = [start]
-    queue: deque = deque()
-    queue.append(start)
+    queue = deque([start])
     while queue:
         v = queue.popleft()
         for child in graph[v]:
@@ -45,8 +65,15 @@ def breadth_first_search(graph: dict, start: str) -> list[str]:
     return result
 
 
+def benchmark_function(name: str) -> None:
+    setup = f"from __main__ import G, {name}"
+    number = 10000
+    res = timeit(f"{name}(G, 'A')", setup=setup, number=number)
+    print(f"{name:<35} finished {number} runs in {res:.5f} seconds")
+
+
 if __name__ == "__main__":
     import doctest
-
     doctest.testmod()
-    print(breadth_first_search(G, "A"))
+    benchmark_function('breadth_first_search')
+    benchmark_function('breadth_first_search_with_deque')
