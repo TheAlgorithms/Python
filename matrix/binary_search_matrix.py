1cb26dd1bcc95487b6d352a9873854815113bdcc
Tapas Singhal <98687345+Shocker-lov-t@users.noreply.github.com>
2022-10-29 01:28:03 +0530

    Update binary_search_matrix.py

--- matrix/binary_search_matrix.py
+++ matrix/binary_search_matrix.py
@@ -14,9 +14,9 @@ def binary_search(array: list, lower_bound: int, upper_bound: int, value: int) -
     r = int((lower_bound + upper_bound) // 2)
     if array[r] == value:
         return r
-    if lower_bound >= upper_bound:
+    elif lower_bound >= upper_bound:
         return -1
-    if array[r] < value:
+    elif array[r] < value:
         return binary_search(array, r + 1, upper_bound, value)
     else:
         return binary_search(array, lower_bound, r - 1, value)
