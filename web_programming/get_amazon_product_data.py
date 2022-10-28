6e2712f48c33bb0a073cfa387afdc9c8b3465879
Christian Clauss <cclauss@me.com>
2022-10-28 16:17:57 +0200

    Update and rename fetch_amazon_product_data.py to get_amazon_product_data.py

--- web_programming/get_amazon_product_data.py
+++ web_programming/get_amazon_product_data.py
@@ -1,39 +1,31 @@
-""" This file provides a function which will take
-a product name as input from the user,and fetch the necessary
-information about that kind of products from Amazon like the product
-title,link to that product,price of the product,the ratings of
-the product and the discount available on the product
-and return it in the form of Pandas Dataframe"""
+"""
+This file provides a function which will take a product name as input from the user,
+and fetch from Amazon information about products of this name or category.  The product
+information will inclued title, URL, price, ratings, and the discount available.
+"""
 
 
-import itertools as it
+from itertools import zip_longest
 
-import pandas as pd
+from pandas import DataFrame
 import requests
 from bs4 import BeautifulSoup
 
 
-def get_product_info(
-    product: str = "laptop", generate_csv: bool = False
-) -> pd.DataFrame:
-    # function that will take the product as input and return the
-    # product details as output
-    # in the form of a csv file,if no input is given,it
-    # will fetch the details of laptop by default
-    url = (
-        f"https://www.amazon.in/laptop/s?k={product}"
-        # generation of search query url
-    )
+def get_amazon_product_data(product: str = "laptop") -> DataFrame:
+    """
+    Take a product name or category as input and return product information from Amazon
+    including title, URL, price, ratings, and the discount available.
+    """
+    url = f"https://www.amazon.in/laptop/s?k={product}"
     header = {
         "User-Agent": """Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36
         (KHTML, like Gecko)Chrome/44.0.2403.157 Safari/537.36""",
         "Accept-Language": "en-US, en;q=0.5",
-    }  # header that will indicate to the destination server that the
-    # request is coming from a genuine human being and not a bot
-    page = requests.get(url, headers=header)
-    page_content = page.text
-    soup = BeautifulSoup(page_content)
-    data = pd.DataFrame(
+    }
+    soup = BeautifulSoup(requests.get(url, headers=header).text)
+    # Initialize a Pandas dataframe with the column titles
+    data_frame = DataFrame(
         columns=[
             "Product Title",
             "Product Link",
@@ -42,21 +34,21 @@ def get_product_info(
             "MRP of the product",
             "Discount",
         ]
-    )  # initializing a pandas dataframe to store the requisite information
-    for i, j in it.zip_longest(
+    )
+    # Loop through each entry and store them in the dataframe
+    for item, _ in zip_longest(
         soup.find_all(
             "div",
             attrs={"class": "s-result-item", "data-component-type": "s-search-result"},
         ),
         soup.find_all("div", attrs={"class": "a-row a-size-base a-color-base"}),
-    ):  # for loop to parse through each entry and store them in the dataframe
-        # along with try....except block for handling exceptions that may arise
+    ):
         try:
-            product_title = i.h2.text
+            product_title = item.h2.text
             product_link = "https://www.amazon.in/" + i.h2.a["href"]
-            product_price = i.find("span", attrs={"class": "a-offscreen"}).text
+            product_price = item.find("span", attrs={"class": "a-offscreen"}).text
             try:
-                product_rating = i.find("span", attrs={"class": "a-icon-alt"}).text
+                product_rating = item.find("span", attrs={"class": "a-icon-alt"}).text
             except AttributeError:
                 product_rating = "Not available"
             try:
@@ -83,7 +75,7 @@ def get_product_info(
                 discount = float("nan")
         except AttributeError:
             pass
-        data.loc[len(data.index)] = [
+        data_frame.loc[len(data_frame.index)] = [
             product_title,
             product_link,
             product_price,
@@ -91,15 +83,17 @@ def get_product_info(
             product_mrp,
             discount,
         ]
-    data.loc[
-        data["Current Price of the product"] > data["MRP of the product"],
+    data_frame.loc[
+        data_frame["Current Price of the product"] > data_frame["MRP of the product"],
         "MRP of the product",
     ] = " "
-    data.loc[
-        data["Current Price of the product"] > data["MRP of the product"], "Discount"
+    data_frame.loc[
+        data_frame["Current Price of the product"] > data_frame["MRP of the product"], "Discount"
     ] = " "
-    data.index += 1
-    if generate_csv:
-        data.to_csv(f"Amazon Product Data({product}).csv")
-        # writing the data to the csv file
-    return data
+    data_frame.index += 1
+    return data_frame
+
+
+if __name__ == "__main__":
+    product = "headphones"
+    get_amazon_product_data(product).to_csv(f"Amazon Product Data for {product}.csv")
