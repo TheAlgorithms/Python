585f6314c5418acb8c516d69b5dafd548ec18928
Shuvo <38564994+0shuvo0@users.noreply.github.com>
2022-10-29 13:33:35 +0600

    Create collision_between_rectangles.py

---
+++ maths/collision_between_rectangles.py
@@ -0,0 +1,29 @@
+rect1 = {
+    "x": 10,
+    "y": 10,
+    "height": 30,
+    "width": 50
+}
+
+rect2 = {
+    "x": 20,
+    "y": 30,
+    "height": 40,
+    "width": 30
+}
+
+def check_collision(rect1, rect2) -> bool:
+    """
+    Check if two rectangle are colliding/overlaping
+    
+    >>> check_collision(rect1, rect2)
+    True
+    """
+    x_bound = rect1["x"] < rect2["x"] + rect1["width"] and rect1["x"] + rect2["width"] > rect2["x"]
+    y_bound = rect1["y"] < rect2["y"] + rect1["height"] and rect1["height"] + rect1["y"] > rect2["y"]
+    return x_bound and y_bound
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
