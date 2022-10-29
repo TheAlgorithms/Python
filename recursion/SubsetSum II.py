47db96c024ca93c372b8b3c4083e052162c6db60
Devanshe sharma <66192225+Devanshe15@users.noreply.github.com>
2022-10-30 02:06:49 +0530

    Subset sum 1

---
+++ recursion/SubsetSum II.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subsetSums(self, arr, N):
+        
+        res = []
+        
+        def helper(idx,total):
+            
+            #base case
+            if idx>=N:
+                res.append(total)
+                return
+            
+            helper(idx+1,total+arr[idx])
+            helper(idx+1,total)
+            
+            return res
+            
+            
+        return helper(0,0)
