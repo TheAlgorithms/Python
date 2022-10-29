557bc9e00aac66209621e8b3a19e9d623afde671
SwayamSahu <91021799+SwayamSahu@users.noreply.github.com>
2022-10-29 23:39:35 +0530

    added is_prime method to basic maths script

--- maths/basic_maths.py
+++ maths/basic_maths.py
@@ -1,6 +1,47 @@
 """Implementation of Basic Math in Python."""
 import math
 
+def is_prime(number: int) -> bool:
+    """Checks to see if a number is a prime in O(sqrt(n)).
+    A number is prime if it has exactly two factors: 1 and itself.
+    >>> is_prime(0)
+    False
+    >>> is_prime(1)
+    False
+    >>> is_prime(2)
+    True
+    >>> is_prime(3)
+    True
+    >>> is_prime(27)
+    False
+    >>> is_prime(87)
+    False
+    >>> is_prime(563)
+    True
+    >>> is_prime(2999)
+    True
+    >>> is_prime(67483)
+    False
+    """
+
+    # precondition
+    assert isinstance(number, int) and (
+        number >= 0
+    ), "'number' must been an int and positive"
+
+    if 1 < number < 4:
+        # 2 and 3 are primes
+        return True
+    elif number < 2 or number % 2 == 0 or number % 3 == 0:
+        # Negatives, 0, 1, all even numbers, all multiples of 3 are not primes
+        return False
+
+    # All primes number are in format of 6k +/- 1
+    for i in range(5, int((number ** (1/2)) + 1), 6):
+        if number % i == 0 or number % (i + 2) == 0:
+            return False
+    return True
+
 
 def prime_factors(n: int) -> list:
     """Find Prime Factors.
