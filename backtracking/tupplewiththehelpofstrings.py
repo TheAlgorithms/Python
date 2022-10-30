cd1842317464c78e862493821edda57227fa08a2
Arjun Rai <85013470+arjun-rai912@users.noreply.github.com>
2022-10-31 00:44:49 +0530

    Create tupplewiththehelpofstrings.py

---
+++ backtracking/tupplewiththehelpofstrings.py
@@ -0,0 +1,23 @@
+# Creating a Tuple with
+# the use of Strings
+Tuple = ('Geeks', 'For')
+print("\nTuple with the use of String: ")
+print(Tuple)
+	
+# Creating a Tuple with
+# the use of list
+list1 = [1, 2, 4, 5, 6]
+print("\nTuple using List: ")
+Tuple = tuple(list1)
+
+# Accessing element using indexing
+print("First element of tuple")
+print(Tuple[0])
+
+# Accessing element from last
+# negative indexing
+print("\nLast element of tuple")
+print(Tuple[-1])
+
+print("\nThird last element of tuple")
+print(Tuple[-3])
