0c575d3d7dc62e0993e428ae0b479d1258d72bb5
CHETAN VERMA <66515815+Chetanverma93@users.noreply.github.com>
2022-10-30 22:58:29 +0530

    adde code to add 3 numbers in python

--- maths/add.py
+++ maths/add.py
@@ -17,3 +17,23 @@ if __name__ == "__main__":
     a = 5
     b = 6
     print(f"The sum of {a} + {b} is {add(a, b)}")
+
+    
+    
+    
+    
+def add3(a: float, b: float, c: float) -> float:
+    """
+    >>> add(2, 2)
+    4
+    >>> add(2, -2)
+    0
+    """
+    return a + b + c 
+
+
+if __name__ == "__main__":
+    a = 5
+    b = 6
+    c = 7
+    print(f"The sum of {a} + {b} is {add(a, b)}")
