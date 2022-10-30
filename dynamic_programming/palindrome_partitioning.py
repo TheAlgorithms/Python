048d0232a769e6446fa36dedaa72dabcaf4e3191
Christian Clauss <cclauss@me.com>
2022-10-30 14:11:54 +0100

    is_palindromic

--- dynamic_programming/palindrome_partitioning.py
+++ dynamic_programming/palindrome_partitioning.py
@@ -23,10 +23,10 @@ def find_minimum_partitions(string: str) -> int:
     length = len(string)
     cut = [0] * length
     is_palindromic = [[False for i in range(length)] for j in range(length)]
-    for i, c in enumerate(length):
+    for i, c in enumerate(string):
         mincut = i
         for j in range(i + 1):
-            if c == string[j] and (i - j < 2 or is_palindrome[j + 1][i - 1]):
+            if c == string[j] and (i - j < 2 or is_palindromic[j + 1][i - 1]):
                 is_palindromic[j][i] = True
                 mincut = min(mincut, 0 if j == 0 else (cut[j - 1] + 1))
         cut[i] = mincut
