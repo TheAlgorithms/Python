2b8ec22e8503d2d1f325fb99eeb9501294d3c4e2
Christian Clauss <cclauss@me.com>
2022-10-30 12:19:46 +0100

    Apply suggestions from code review
    Co-authored-by: Caeden Perelli-Harris <caedenperelliharris@gmail.com>

--- maths/percent.py
+++ maths/percent.py
@@ -2,12 +2,20 @@
 Find the Percentage of two numbers
 wikipdia
 https://en.wikipedia.org/wiki/Percentage
->>> percent(30.0,75.0)
-40.0
 """
 
 
 def percent(first_num: float | int, second_num: float | int) -> float:
+    """
+    >>> percent(30.0, 75.0)
+    40.0
+    """
+    >>> percent(1, 80)
+    1.25
+    >>> percent(80, 1)
+    8000.0
+    >>> percent(80, -1)
+    8000.0
     return (first_num / second_num) * 100
 
 
