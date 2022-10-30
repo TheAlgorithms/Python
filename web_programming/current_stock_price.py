29b645902147fb1229bf1272ba192782d74e7621
Harkishan Khuva <78949167+hakiKhuva@users.noreply.github.com>
2022-10-30 18:41:38 +0530

    Updated `url`, `identifier`, etc...
    - added user agent because sometimes yahoo finance does not sent stock prices
    - `in` subdomain URL not working updated that
    - using `content` property instead of `text`
    - changed identifier from `class` to `dataset` and removed specific tag
    - added `ZOMATO.NS` stock to check for Indian stocks price

--- web_programming/current_stock_price.py
+++ web_programming/current_stock_price.py
@@ -1,14 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0"
 
-def stock_price(symbol: str = "AAPL") -> str:
-    url = f"https://in.finance.yahoo.com/quote/{symbol}?s={symbol}"
-    soup = BeautifulSoup(requests.get(url).text, "html.parser")
-    class_ = "My(6px) Pos(r) smartphone_Mt(6px)"
-    return soup.find("div", class_=class_).find("span").text
+def stock_price(symbol:str) -> str:
+    url = f"https://finance.yahoo.com/quote/{symbol}"
+    
+    page_content = requests.get(url,headers={
+        "user-agent" : USER_AGENT
+    }).content
 
+    soup = BeautifulSoup(page_content, "html.parser")
+    return soup.find(attrs={
+        "data-symbol" : symbol,
+        "data-field":"regularMarketPrice"
+    }).text
 
 if __name__ == "__main__":
-    for symbol in "AAPL AMZN IBM GOOG MSFT ORCL".split():
+    for symbol in "AAPL AMZN IBM GOOG MSFT ORCL ZOMATO.NS".split():
         print(f"Current {symbol:<4} stock price is {stock_price(symbol):>8}")
