34ead4d76dc8779ae48cf49613572b112eed52ce
Christian Clauss <cclauss@me.com>
2022-10-30 14:02:53 +0100

    Update palindrome_partitioning.py

--- dynamic_programming/palindrome_partitioning.py
+++ dynamic_programming/palindrome_partitioning.py
@@ -20,13 +20,13 @@ def find_minimum_partitions(string: str) -> int:
     >>> find_minimum_partitions("ababbbabbababa")
     3
     """
-    n = len(string)
-    cut = [0] * n
-    is_palindromic = [[False for i in range(n)] for j in range(n)]
-    for i in range(n):
+    length = len(string)
+    cut = [0] * length
+    is_palindromic = [[False for i in range(length)] for j in range(length)]
+    for i, c in enumerate(length):
         mincut = i
         for j in range(i + 1):
-            if string[i] == string[j] and (i - j < 2 or ispalindrome[j + 1][i - 1]):
+            if c == string[j] and (i - j < 2 or ispalindrome[j + 1][i - 1]):
                 is_palindromic[j][i] = True
                 mincut = min(mincut, 0 if j == 0 else (cut[j - 1] + 1))
         cut[i] = mincut
@@ -36,4 +36,4 @@ def find_minimum_partitions(string: str) -> int:
 if __name__ == "__main__":
     s = input("Enter the string: ").strip()
     ans = find_minimum_partitions(s)
-    print(f"Minimum number of partitions required for the string is {ans}")
+    print(f"Minimum number of partitions required for the '{s}' is {ans}")
