d387a78528f462ea387d72dff1e1ac3fd72b8258
Christian Clauss <cclauss@me.com>
2022-10-30 11:18:05 +0100

    Update data_structures/trie/radix_tree.py

--- data_structures/trie/radix_tree.py
+++ data_structures/trie/radix_tree.py
@@ -27,13 +27,9 @@ class RadixNode:
         >>> RadixNode("myprefix").match("mystring")
         ('my', 'prefix', 'string')
         """
-        x = 0
-        for q, w in zip(self.prefix, word):
+        for x, (q, w) in enumerate(zip(self.prefix, word)):
             if q != w:
                 break
-
-            x += 1
-
         return self.prefix[:x], self.prefix[x:], word[x:]
 
     def insert_many(self, words: list[str]) -> None:
