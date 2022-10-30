8adbc110572133cdeb3203c0cce2767483d53740
sadiqebrahim <75269485+sadiqebrahim@users.noreply.github.com>
2022-10-30 18:52:09 +0530

    Update forward_bias_current.py

--- electronics/forward_bias_current.py
+++ electronics/forward_bias_current.py
@@ -13,17 +13,17 @@ def forward_bias_current(
     This function can calculate the Forward Bias Current of a pn junction diode.
     This is calculated from the given two values.
     Examples -
-    >>> forward_bias_current(voltage=0.7, rev_sat_current=1e-12)
+    >>> forward_bias_current(forward_voltage=0.7, reverse_saturation_current=1e-12)
     0.5747545691036846
-    >>> forward_bias_current(voltage=-5, rev_sat_current=1e-12)
+    >>> forward_bias_current(forward_voltage=-5, reverse_saturation_current=1e-12)
     Traceback (most recent call last):
       ...
     ValueError: Forward voltage cannot be negative
-    >>> forward_bias_current(voltage=10, rev_sat_current=-2)
+    >>> forward_bias_current(forward_voltage=10, reverse_saturation_current=-2)
     Traceback (most recent call last):
       ...
     ValueError: Reverse saturation current should be positive
-    >>> forward_bias_current(voltage=10, rev_sat_current=1e17)
+    >>> forward_bias_current(forward_voltage=10, reverse_saturation_current=1e17)
     Traceback (most recent call last):
       ...
     ValueError: Reverse saturation current cannot be greater than 1e-5
@@ -36,8 +36,8 @@ def forward_bias_current(
     elif rev_sat_current > 1e-5:
         raise ValueError("Reverse saturation current cannot be greater than 1e-5")
     else:
-        return rev_sat_current * (
-            e ** ((voltage * physical_constants["electron volt"][0]) / (Boltzmann * T))
+        return reverse_saturation_current * (
+            e ** ((forward_voltage * physical_constants["electron volt"][0]) / (Boltzmann * T))
             - 1
         )
 
