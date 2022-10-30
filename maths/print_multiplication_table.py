ff1c1948f353819145a05c754052620c2a81d92c
Christian Clauss <cclauss@me.com>
2022-10-30 23:51:29 +0100

    Update print_multiplication_table.py

--- maths/print_multiplication_table.py
+++ maths/print_multiplication_table.py
@@ -1,14 +1,15 @@
-def print_multiplication_table(number: int, number_of_terms: int) -> None:
+def multiplication_table(number: int, number_of_terms: int) -> str:
     """
     Prints the multiplication table of a given number till the given number of terms
-    >>> print_multiplication_table(3,5)
+
+    >>> print(multiplication_table(3, 5))
     3 * 1 = 3
     3 * 2 = 6
     3 * 3 = 9
     3 * 4 = 12
     3 * 5 = 15
 
-    >>> print_multiplication_table(-4,6)
+    >>> print(multiplication_table(-4, 6))
     -4 * 1 = -4
     -4 * 2 = -8
     -4 * 3 = -12
@@ -16,11 +17,10 @@ def print_multiplication_table(number: int, number_of_terms: int) -> None:
     -4 * 5 = -20
     -4 * 6 = -24
     """
-    for i in range(1, number_of_terms + 1):
-        print(f"{number} * {i} = " + str(number * i))
+    return "\n".join(
+        f"{number} * {i} = {number * i}" for i in range(1, number_of_terms + 1)
+    )
 
 
 if __name__ == "__main__":
-    number = 5
-    number_of_terms = 10
-    print_multiplication_table(number, number_of_terms)
+    print(multiplication_table(number=5, number_of_terms=10))
