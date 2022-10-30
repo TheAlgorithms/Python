e565b12773506fe3c57fc73c67c98979e40b7fdb
Christian Clauss <cclauss@me.com>
2022-10-30 13:51:55 +0100

    Update min_distance_up_bottom.py

--- dynamic_programming/min_distance_up_bottom.py
+++ dynamic_programming/min_distance_up_bottom.py
@@ -33,23 +33,18 @@ def min_distance_up_bottom(word1: str, word2: str) -> int:
 
     @lru_cache(maxsize=None)
     def min_distance(index1: int, index2: int) -> int:
-
         # if first word index is overflow - delete all from the second word
         if index1 >= len_word1:
             return len_word2 - index2
-
         # if second word index is overflow - delete all from the first word
         if index2 >= len_word2:
             return len_word1 - index1
-        diff = int(
-            word1[index1] != word2[index2]
-        )  # diff is current letters are not identical
+        diff = int(word1[index1] != word2[index2])  # current letters not identical
         return min(
             1 + min_distance(index1 + 1, index2),
             1 + min_distance(index1, index2 + 1),
             diff + min_distance(index1 + 1, index2 + 1),
         )
-
     return min_distance(0, 0)
 
 
