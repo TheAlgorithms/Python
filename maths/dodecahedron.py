ff4477f8fed30e7e429f6980cada6725471f08da
Christian Clauss <cclauss@me.com>
2022-10-31 18:15:22 +0100

    Update dodecahedron.py

--- maths/dodecahedron.py
+++ maths/dodecahedron.py
@@ -64,7 +64,6 @@ def dodecahedron_volume(edge: float) -> float:
 
     if edge <= 0 or not isinstance(edge, int):
         raise ValueError("Length must be a positive.")
-
     return ((15 + (7 * (5 ** (1 / 2)))) / 4) * (edge**3)
 
 
