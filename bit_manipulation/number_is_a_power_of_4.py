f3b9670440d06287600c3d89843fd4f4a51922ae
melany estevez camata <elibialany@gmail.com>
2022-10-31 18:11:09 -0400

    added number is power of 4

---
+++ bit_manipulation/number_is_a_power_of_4.py
@@ -0,0 +1,13 @@
+
+def checkPowerOf4(n):
+    """
+    Return true if `n` is a power of 2, and its only
+    set bit is present at even position
+    """
+    return n and not (n & (n - 1)) and not (n & 0xAAAAAAAA)
+if __name__ == '__main__':
+    n = int(input("Please enter a starting number: ").strip())
+    if checkPowerOf4(n):
+        print(n, 'is a power of 4')
+    else:
+        print(n, 'is not a power of 4')
