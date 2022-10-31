4e7f59021c0f60a71680cb3a2fc18b1ef656e244
Christian Clauss <cclauss@me.com>
2022-10-31 18:14:41 +0100

    Apply suggestions from code review

--- maths/dodecahedron.py
+++ maths/dodecahedron.py
@@ -34,8 +34,7 @@ def dodecahedron_surface_area(edge: float) -> float:
 
     if edge <= 0 or not isinstance(edge, int):
         raise ValueError("Length must be a positive.")
-    surface_area = 3 * ((25 + 10 * (5 ** (1 / 2))) ** (1 / 2)) * (edge**2)
-    return surface_area
+    return 3 * ((25 + 10 * (5 ** (1 / 2))) ** (1 / 2)) * (edge**2)
 
 
 def dodecahedron_volume(edge: float) -> float:
@@ -66,8 +65,7 @@ def dodecahedron_volume(edge: float) -> float:
     if edge <= 0 or not isinstance(edge, int):
         raise ValueError("Length must be a positive.")
 
-    volume = ((15 + (7 * (5 ** (1 / 2)))) / 4) * (edge**3)
-    return volume
+    return ((15 + (7 * (5 ** (1 / 2)))) / 4) * (edge**3)
 
 
 if __name__ == "__main__":
