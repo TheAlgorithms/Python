72fe12b8d0fbff13643b5c6f9b3728f9abe46350
pauloguilherme0 <43881368+pauloguilherme0@users.noreply.github.com>
2022-10-31 19:37:38 -0300

    Create multiplication_table.py
    Multiplication table (from 1 to 10) using Python.

---
+++ maths/multiplication_table.py
@@ -0,0 +1,10 @@
+# To take input from the user
+num = int(input("Display multiplication table of? "))
+
+# Iterate 10 times from i = 1 to 10
+for i in range(1, 11):
+   print(num, 'x', i, '=', num*i)
+
+if __name__ == "__main__":
+  from doctest import testmod
+  testmod()
