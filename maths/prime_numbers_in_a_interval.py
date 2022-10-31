d4e44178849f92d3b7c863a24fe04968e842cd5e
pauloguilherme0 <43881368+pauloguilherme0@users.noreply.github.com>
2022-10-31 19:28:15 -0300

    Create prime_numbers_in_a_interval.py
    Python program to display all the prime numbers within an interval.

---
+++ maths/prime_numbers_in_a_interval.py
@@ -0,0 +1,13 @@
+lower = int(input("Lower number: "))
+upper = int(input("Upper number: "))
+
+print("Prime numbers between", lower, "and", upper, "are:")
+
+for num in range(lower, upper + 1):
+   # all prime numbers are greater than 1
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
