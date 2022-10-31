f3c24631b33ae09f69ee6fc6e844797ae52cc281
ZinIon16 <zain.qureshi@mbe4you.com>
2022-10-31 11:50:28 +0500

    ZinIon16 added guesser.py in other folder

---
+++ other/guesser.py
@@ -0,0 +1,25 @@
+from random import Random
+
+class Guess:
+    def makeEstimate(self, max, min = 0, trial = 1):
+        estimate = min + ((max - min) >> 1)
+        print('I guess', estimate)
+        answer = input('"lower?", "higher?", or "correct?": ')
+        if(answer == 'lower'):
+            return self.makeEstimate(estimate, min, trial + 1)
+        elif(answer == 'higher'):
+            return self.makeEstimate(max, estimate + 1, trial + 1)
+        elif(answer == 'correct'):
+            return trialgit 
+        else:
+            print('Invalid answer')
+            exit()
+
+try:
+    N = int(input('Max: '))
+except ValueError:
+    print('Value must be a number')
+    exit()
+guesser = Guess()
+trial = guesser.makeEstimate(N)
+print('Guessed the correct number in', trial, 'tries')
