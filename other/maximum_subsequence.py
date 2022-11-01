2df68a82e6ea45b353c41cc0fb912e21c24c4db7
Pronoy Mandal <lukex9442@gmail.com>
2022-11-02 00:06:45 +0530

    Update maximum_subsequence.py

--- other/maximum_subsequence.py
+++ other/maximum_subsequence.py
@@ -1,7 +1,8 @@
+from typing import Optional
 from collections.abc import Sequence
 
 
-def max_subsequence_sum(nums: Sequence[int]) -> int:
+def max_subsequence_sum(nums: Optional[Sequence[int]] = None) -> int:
     """Return the maximum possible sum amongst all non - empty subsequences.
 
     Raises:
@@ -11,8 +12,16 @@ def max_subsequence_sum(nums: Sequence[int]) -> int:
     10
     >>> max_subsequence_sum([-2, -3, -1, -4, -6])
     -1
+    >>> max_subsequence_sum([])
+    Traceback (most recent call last):
+        . . .
+    ValueError: Input sequence should not be empty
+    >>> max_subsequence_sum()
+    Traceback (most recent call last):
+        . . .
+    ValueError: Input sequence should not be empty
     """
-    if not nums:
+    if nums is None or not nums:
         raise ValueError("Input sequence should not be empty")
 
     ans = nums[0]
@@ -24,6 +33,10 @@ def max_subsequence_sum(nums: Sequence[int]) -> int:
 
 
 if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
+    # Try on a sample input from the user
     n = int(input("Enter number of elements : ").strip())
     array = list(map(int, input("\nEnter the numbers : ").strip().split()))[:n]
     print(max_subsequence_sum(array))
