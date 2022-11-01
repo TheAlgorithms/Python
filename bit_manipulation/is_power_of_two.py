1a52a9c2de75166e50791c9fbf4b225140943cb3
Christian Clauss <cclauss@me.com>
2022-11-01 18:34:36 +0100

    Deal with negative numbers

--- bit_manipulation/is_power_of_two.py
+++ bit_manipulation/is_power_of_two.py
@@ -4,7 +4,7 @@ Date    : November 1, 2022
 
 Task:
 Given a positive int number. Return True if this number is power of 2
-and False otherwise.
+or False otherwise.
 
 Implementation notes: Use bit manipulation.
 For example if the number is the power of two it's bits representation:
@@ -12,12 +12,17 @@ n     = 0..100..00
 n - 1 = 0..011..11
 
 n & (n - 1) - no intersections = 0
-
 """
 
 
 def is_power_of_two(number: int) -> bool:
     """
+    Return True if this number is power of 2 or False otherwise.
+
+    >>> is_power_of_two(0)
+    True
+    >>> is_power_of_two(1)
+    True
     >>> is_power_of_two(2)
     True
     >>> is_power_of_two(4)
@@ -28,12 +33,21 @@ def is_power_of_two(number: int) -> bool:
     True
     >>> is_power_of_two(17)
     False
-    >>> is_power_of_two(32)
-    True
-    >>> is_power_of_two(31)
-    False
-    """
+    >>> is_power_of_two(-1)
+    Traceback (most recent call last):
+        ...
+    ValueError: number must not be negitive
+    >>> is_power_of_two(1.2)
+    Traceback (most recent call last):
+        ...
+    TypeError: unsupported operand type(s) for &: 'float' and 'float'
 
+    # Test all powers of 2 from 0 to 10,000
+    >>> all(is_power_of_two(int(2 ** i)) for i in range(10000))
+    True
+    """
+    if number < 0:
+        raise ValueError("number must not be negitive")
     return number & (number - 1) == 0
 
 
