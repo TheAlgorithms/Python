29e8cd03c012c3e26ff1add18e58ed223d71c0c5
Harkishan Khuva <78949167+hakiKhuva@users.noreply.github.com>
2022-11-02 20:57:11 +0530

    updated after running black

--- other/guess_the_number_search.py
+++ other/guess_the_number_search.py
@@ -1,51 +1,51 @@
 """
-Get the random number guessed by the computer by passing the lower, higher, and the
-number to guess.
+get the random number guessed by the computer by passing the lower,higher
+and the number to guess
 
-This solution works on divide and getting the half of number of previous and current,
-this depends on the number is low or high
+this solution works on divide and getting the half of number of previous and
+current, this depends on the number is low or high
 
-If the number is more than last lower and less than to the number to guess then the
-number is assigned to it, and same but opposite for higher number.
+if the number is more than last lower and less than to the number to guess then
+the number is assigned to it, and same but opposite for higher number
 
-Suppose lower is 0, higher is 1000 and the number to guess is 355
+suppose lower is 0, higher is 1000 and the number to guess is 355
 then:
-    num = int((lower + higher) // 2)
-    for above statement the function already declared as the get_avg(a, b)
+    num = int((lower+higher)/2)
+    for above statement the function already declared as the get_avg(a,b)
 
         [1]
-    get_avg(0, 1000)  : 500
+    get_avg(0,1000)  : 500
     answer(500) : high
         Now this value is passed to the answer function and that returns the
         passed number is lower than the guess number or higher than the guess
         number and also for equality
 
         [2]
-    get_avg(0, 500) : 250
+    get_avg(0,500) : 250
     answer(250) : low
 
         [3]
-    get_avg(250, 500) : 375
+    get_avg(250,500) : 375
     answer(375) : high
 
         [4]
-    get_avg(375, 250) : 312
+    get_avg(375,250) : 312
     answer(312) : low
 
         [5]
-    get_avg(312, 375) : 343
+    get_avg(312,375) : 343
     answer(343) : low
 
         [6]
-    get_avg(343, 375) : 359
+    get_avg(343,375) : 359
     answer(359) : high
 
         [7]
-    get_avg(343, 359) : 351
+    get_avg(343,359) : 351
     answer(351) : low
 
         [8]
-    get_avg(351, 359) : 355
+    get_avg(351,359) : 355
     answer(355) : same
 
 The number is found : 355
@@ -54,6 +54,7 @@ The number is found : 355
 started...
 guess the number : 17
 details : [505, 257, 133, 71, 40, 25, 17]
+
 """
 
 
@@ -93,6 +94,7 @@ def temp_input_value(
         and isinstance(max_val, int)
         and isinstance(option, bool)
     ), "Invalid type of value(s) specified to function!"
+
     if min_val > max_val:
         raise ValueError("Invalid value for min_val or max_val (min_value < max_value)")
     return min_val if option else max_val
