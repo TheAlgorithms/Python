459133ccfb253c9bfff5af119fee72c6896fe237
Alexander Pantyukhin <apantykhin@gmail.com>
2022-11-02 23:50:46 +0400

    Update data_structures/binary_tree/is_bst.py
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- data_structures/binary_tree/is_bst.py
+++ data_structures/binary_tree/is_bst.py
@@ -6,7 +6,7 @@ Task:
 Given the root of a binary tree, determine if it is a valid binary search
 tree (BST).
 
-A valid BST is defined as follows:
+A valid binary search tree is defined as follows:
 
 - The left subtree of a node contains only nodes with keys less than the node's key.
 - The right subtree of a node contains only nodes with keys greater than the node's key.
