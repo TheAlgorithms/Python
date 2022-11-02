b98d8943a1b23b7982f0f152fd309e6e7246927b
Harkishan Khuva <78949167+hakiKhuva@users.noreply.github.com>
2022-11-02 18:17:12 +0530

    fixed the issues
    - changed type checking technique
    - updated guess value checking

--- other/guess_the_number_search.py
+++ other/guess_the_number_search.py
@@ -89,11 +89,13 @@ def temp_input_value(
     >>> temp_input_value(min_val=-5100, max_val=-100)
     -5100
     """
-    assert (
-        type(min_val) == int and type(max_val) == int and type(option) == bool
-    ), "Invalid type of value(s) specified to function!"
+    assert isinstance(min_val, int) and isinstance(max_val, int) and \
+        isinstance(option, bool), \
+        "Invalid type of value(s) specified to function!"
     if min_val > max_val:
-        raise ValueError("Invalid value for min_val or max_val (min_value < max_value)")
+        raise ValueError(
+            "Invalid value for min_val or max_val (min_value < max_value)"
+        )
     return min_val if option else max_val
 
 
@@ -148,14 +150,15 @@ def guess_the_number(lower: int, higher: int, to_guess: int) -> None:
         ...
     ValueError: argument value for lower and higher must be(lower > higher)
     """
-    assert (
-        type(lower) == int and type(higher) == int and type(to_guess) == int
-    ), 'argument values must be type of "int"'
+    assert isinstance(lower, int) and isinstance(higher, int) \
+        and isinstance(to_guess, int), "argument values must be type of \"int\""
 
     if lower > higher:
-        raise ValueError("argument value for lower and higher must be(lower > higher)")
+        raise ValueError(
+            "argument value for lower and higher must be(lower > higher)"
+        )
 
-    if to_guess < lower or to_guess > higher:
+    if not lower < to_guess < higher:
         raise ValueError(
             "guess value must be within the range of lower and higher value"
         )
