7cd5bd4a159a2310502c3b96c94ef773a7f743e0
Harkishan Khuva <78949167+hakiKhuva@users.noreply.github.com>
2022-11-06 16:59:03 +0530

    Update current_stock_price.py

--- web_programming/current_stock_price.py
+++ web_programming/current_stock_price.py
@@ -1,13 +1,14 @@
 import requests
+from typing import Union
 from bs4 import BeautifulSoup
 
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+USER_AGENT += "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
 
 
-def stock_price(symbol: str) -> str:
+def stock_price(symbol: str) -> Union[float, None]:
     """
-    fetches the stock price and return it in string
+    fetches the stock price and return it in float if found else None
     """
     assert isinstance(symbol, str), "symbol value must be type of 'str'"
 
@@ -17,7 +18,8 @@ def stock_price(symbol: str) -> str:
 
     soup = BeautifulSoup(page_content, "html.parser")
     ele = soup.find(attrs={"data-symbol": symbol, "data-field": "regularMarketPrice"})
-    return ele.text.strip() if ele is not None else ""
+
+    return float(ele.text.strip()) if ele is not None else None
 
 
 if __name__ == "__main__":
