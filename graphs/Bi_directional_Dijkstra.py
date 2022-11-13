4de4d627ff8b0d3e117e76adf1ceb2f68066379b
Swayam <74960567+practice404@users.noreply.github.com>
2022-11-13 23:42:53 +0530

    Added doctest and type hints

--- graphs/Bi_directional_Dijkstra.py
+++ graphs/Bi_directional_Dijkstra.py
@@ -1,5 +1,4 @@
 from queue import PriorityQueue
-
 import numpy as np
 
 """
@@ -11,8 +10,7 @@ Link for reference: https://www.homepages.ucl.ac.uk/~ucahmto/math/2020/05/30/bid
 
 # Author: Swayam Singh (https://github.com/practice404)
 
-
-def bidirectional_dij(source, destination, graph_forward, graph_backward) -> int:
+def bidirectional_dij(source: str or int , destination: str or int, graph_forward: dict, graph_backward: dict) -> int:
     """
     Bi-directional Dijkstra's algorithm.
     Link for reference: https://www.homepages.ucl.ac.uk/~ucahmto/math/2020/05/30/bidirectional-dijkstra.html
@@ -27,6 +25,9 @@ def bidirectional_dij(source, destination, graph_forward, graph_backward) -> int
 
     Warnings:
         If the destination is not reachable, function returns -1
+
+    >>> bidirectional_dij("E", "F", graph_fwd, graph_bwd)
+    3
     """
     shortest_path_distance = -1
 
@@ -76,18 +77,9 @@ def bidirectional_dij(source, destination, graph_forward, graph_backward) -> int
                 queue_forward.put((new_cost_f, next_forward))
                 cost_forward[next_forward] = new_cost_f
                 parent_forward[next_forward] = vertex_forward
-            if (
-                next_forward in visited_backward
-                and cost_forward[vertex_forward]
-                + d_forward
-                + cost_backward[next_forward]
-                < shortest_distance
-            ):
-                shortest_distance = (
-                    cost_forward[vertex_forward]
-                    + d_forward
-                    + cost_backward[next_forward]
-                )
+            if next_forward in visited_backward and cost_forward[vertex_forward] + d_forward + \
+                    cost_backward[next_forward] < shortest_distance:
+                shortest_distance = cost_forward[vertex_forward] + d_forward + cost_backward[next_forward]
 
         # backward pass and relaxation
         for next_backward, d_backward in graph_backward[vertex_backward]:
@@ -100,23 +92,11 @@ def bidirectional_dij(source, destination, graph_forward, graph_backward) -> int
                 cost_backward[next_backward] = new_cost_b
                 parent_backward[next_backward] = vertex_backward
 
-            if (
-                next_backward in visited_forward
-                and cost_backward[vertex_backward]
-                + d_backward
-                + cost_forward[next_backward]
-                < shortest_distance
-            ):
-                shortest_distance = (
-                    cost_backward[vertex_backward]
-                    + d_backward
-                    + cost_forward[next_backward]
-                )
+            if next_backward in visited_forward and cost_backward[vertex_backward] + d_backward + \
+                    cost_forward[next_backward] < shortest_distance:
+                shortest_distance = cost_backward[vertex_backward] + d_backward + cost_forward[next_backward]
 
-        if (
-            cost_forward[vertex_forward] + cost_backward[vertex_backward]
-            >= shortest_distance
-        ):
+        if cost_forward[vertex_forward] + cost_backward[vertex_backward] >= shortest_distance:
             break
 
     if shortest_distance == np.inf:
@@ -124,30 +104,32 @@ def bidirectional_dij(source, destination, graph_forward, graph_backward) -> int
     shortest_path_distance = shortest_distance
     return shortest_path_distance
 
+r"""
+Layout of input Graph:
+E -- 1 --> B -- 1 --> C -- 1 --> D -- 1 --> F
+ \                                         /\
+  \                                        ||
+    -------- 2 ---------> G ------- 1 ------
+"""
+
+graph_fwd = {
+    "B": [["C", 1]],
+    "C": [["D", 1]],
+    "D": [["F", 1]],
+    "E": [["B", 1], ["G", 2]],
+    "F": [],
+    "G": [["F", 1]],
+}
+graph_bwd = {
+    "B": [["E", 1]],
+    "C": [["B", 1]],
+    "D": [["C", 1]],
+    "F": [["D", 1], ["G", 1]],
+    "G": [["E", 2]]
+}
+print(bidirectional_dij("E", "F", graph_fwd, graph_bwd)) # # E -- 2 --> G -- 1 --> F == 3
 
 if __name__ == "__main__":
-    r"""
-    Layout of input Graph:
-    E -- 1 --> B -- 1 --> C -- 1 --> D -- 1 --> F
-     \                                         /\
-      \                                        ||
-        -------- 2 ---------> G ------- 1 ------
-    """
+   import doctest
+   doctest.testmod()
 
-    graph_fwd = {
-        "B": [["C", 1]],
-        "C": [["D", 1]],
-        "D": [["F", 1]],
-        "E": [["B", 1], ["G", 2]],
-        "F": [],
-        "G": [["F", 1]],
-    }
-    graph_bwd = {
-        "B": [["E", 1]],
-        "C": [["B", 1]],
-        "D": [["C", 1]],
-        "F": [["D", 1], ["G", 1]],
-        "G": [["E", 2]],
-    }
-    print(bidirectional_dij("E", "F", graph_fwd, graph_bwd))
-    # E -- 2 --> G -- 1 --> F == 3
