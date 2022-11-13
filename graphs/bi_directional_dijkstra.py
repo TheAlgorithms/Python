baabd64b146dae7ac0953c5469c4a5cb215ac58b
Swayam <74960567+practice404@users.noreply.github.com>
2022-11-14 00:21:45 +0530

    Update bi_directional_dijkstra.py

--- graphs/bi_directional_dijkstra.py
+++ graphs/bi_directional_dijkstra.py
@@ -12,10 +12,7 @@ Link for reference: https://www.homepages.ucl.ac.uk/~ucahmto/math/2020/05/30/bid
 
 # Author: Swayam Singh (https://github.com/practice404)
 
-
-def bidirectional_dij(
-    source: str, destination: str, graph_forward: dict, graph_backward: dict
-) -> int:
+def bidirectional_dij(source: str, destination: str, graph_forward: dict, graph_backward: dict) -> int:
     """
     Bi-directional Dijkstra's algorithm.
     Args:
@@ -37,8 +34,8 @@ def bidirectional_dij(
 
     visited_forward = set()
     visited_backward = set()
-    cost_forward = {source: 0}
-    cost_backward = {destination: 0}
+    cst_fwd = {source: 0}
+    cst_bwd = {destination: 0}
     parent_forward = {source: None}
     parent_backward = {destination: None}
     queue_forward: PriorityQueue[Any] = PriorityQueue()
@@ -54,81 +51,59 @@ def bidirectional_dij(
 
     while queue_forward and queue_backward:
         while not queue_forward.empty():
-            _, vertex_forward = queue_forward.get()
+            _, v_fwd = queue_forward.get()
 
-            if vertex_forward not in visited_forward:
+            if v_fwd not in visited_forward:
                 break
         else:
             break
-        visited_forward.add(vertex_forward)
+        visited_forward.add(v_fwd)
 
         while not queue_backward.empty():
-            _, vertex_backward = queue_backward.get()
+            _, v_bwd = queue_backward.get()
 
-            if vertex_backward not in visited_backward:
+            if v_bwd not in visited_backward:
                 break
         else:
             break
-        visited_backward.add(vertex_backward)
+        visited_backward.add(v_bwd)
 
         # forward pass and relaxation
-        for next_forward, d_forward in graph_forward[vertex_forward]:
-            if next_forward in visited_forward:
+        for nxt_fwd, d_forward in graph_forward[v_fwd]:
+            if nxt_fwd in visited_forward:
                 continue
-            old_cost_f = cost_forward.get(next_forward, np.inf)
-            new_cost_f = cost_forward[vertex_forward] + d_forward
+            old_cost_f = cst_fwd.get(nxt_fwd, np.inf)
+            new_cost_f = cst_fwd[v_fwd] + d_forward
             if new_cost_f < old_cost_f:
-                queue_forward.put((new_cost_f, next_forward))
-                cost_forward[next_forward] = new_cost_f
-                parent_forward[next_forward] = vertex_forward
-            if next_forward in visited_backward:
-                if (
-                    cost_forward[vertex_forward]
-                    + d_forward
-                    + cost_backward[next_forward]
-                    < shortest_distance
-                ):
-                    shortest_distance = (
-                        cost_forward[vertex_forward]
-                        + d_forward
-                        + cost_backward[next_forward]
-                    )
+                queue_forward.put((new_cost_f, nxt_fwd))
+                cst_fwd[nxt_fwd] = new_cost_f
+                parent_forward[nxt_fwd] = v_fwd
+            if nxt_fwd in visited_backward:
+                if cst_fwd[v_fwd] + d_forward + cst_bwd[nxt_fwd] < shortest_distance:
+                    shortest_distance = cst_fwd[v_fwd] + d_forward + cst_bwd[nxt_fwd]
 
         # backward pass and relaxation
-        for next_backward, d_backward in graph_backward[vertex_backward]:
-            if next_backward in visited_backward:
+        for nxt_bwd, d_backward in graph_backward[v_bwd]:
+            if nxt_bwd in visited_backward:
                 continue
-            old_cost_b = cost_backward.get(next_backward, np.inf)
-            new_cost_b = cost_backward[vertex_backward] + d_backward
+            old_cost_b = cst_bwd.get(nxt_bwd, np.inf)
+            new_cost_b = cst_bwd[v_bwd] + d_backward
             if new_cost_b < old_cost_b:
-                queue_backward.put((new_cost_b, next_backward))
-                cost_backward[next_backward] = new_cost_b
-                parent_backward[next_backward] = vertex_backward
+                queue_backward.put((new_cost_b, nxt_bwd))
+                cst_bwd[nxt_bwd] = new_cost_b
+                parent_backward[nxt_bwd] = v_bwd
 
-            if next_backward in visited_forward:
-                if (
-                    cost_backward[vertex_backward]
-                    + d_backward
-                    + cost_forward[next_backward]
-                    < shortest_distance
-                ):
-                    shortest_distance = (
-                        cost_backward[vertex_backward]
-                        + d_backward
-                        + cost_forward[next_backward]
-                    )
+            if nxt_bwd in visited_forward:
+                if cst_bwd[v_bwd] + d_backward + cst_fwd[nxt_bwd] < shortest_distance:
+                    shortest_distance = cst_bwd[v_bwd] + d_backward + cst_fwd[nxt_bwd]
 
-        if (
-            cost_forward[vertex_forward] + cost_backward[vertex_backward]
-            >= shortest_distance
-        ):
+        if cst_fwd[v_fwd] + cst_bwd[v_bwd] >= shortest_distance:
             break
 
     if shortest_distance == np.inf:
         return shortest_path_distance
     shortest_path_distance = shortest_distance
     return shortest_path_distance
-
 
 r"""
 Layout of input Graph:
@@ -151,13 +126,10 @@ graph_bwd = {
     "C": [["B", 1]],
     "D": [["C", 1]],
     "F": [["D", 1], ["G", 1]],
-    "G": [["E", 2]],
+    "G": [["E", 2]]
 }
-print(
-    bidirectional_dij("E", "F", graph_fwd, graph_bwd)
-)  # # E -- 2 --> G -- 1 --> F == 3
+print(bidirectional_dij("E", "F", graph_fwd, graph_bwd)) # # E -- 2 --> G -- 1 --> F == 3
 
 if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod()
+   import doctest
+   doctest.testmod()
