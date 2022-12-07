8e2b8abf307047368884c1f1f891ebdfdf1b14fc
Abhishek Mulik <mulikabhishek2605@gmail.com>
2022-12-07 11:23:59 +0530

    Update is_palindrome.py
    is_palindrome iterative method added, which is more efficient as it takes O(1) space, unlike string slice method which takes O(n) space

--- strings/is_palindrome.py
+++ strings/is_palindrome.py
@@ -1,4 +1,4 @@
-def is_palindrome(s: str) -> bool:
+def is_palindrome_slice_method(s: str) -> bool:
     """
     Determine if the string s is a palindrome.
 
@@ -18,10 +18,42 @@ def is_palindrome(s: str) -> bool:
     s = "".join(character for character in s.lower() if character.isalnum())
     return s == s[::-1]
 
+def is_palindorme_iterative_method(s):
+    """
+    Determine if the string s is a palindrome.
+
+    >>> is_palindrome("A man, A plan, A canal -- Panama!")
+    True
+    >>> is_palindrome("Hello")
+    False
+    >>> is_palindrome("Able was I ere I saw Elba")
+    True
+    >>> is_palindrome("racecar")
+    True
+    >>> is_palindrome("Mr. Owl ate my metal worm?")
+    True
+    """
+    end=len(s)//2
+    n=len(s)
+    """
+    We need to traverse till half of the length of string
+    as we can get access of the i'th last element from i'th index. 
+    eg: [0,1,2,3,4,5] => 4th index can be accessed with the help of 1st index (i==n-i-1) 
+    where n is length of string
+    """
+    for i in range(end):
+        if s[i]!=s[n-i-1]:
+            return False
+    return True 
 
 if __name__ == "__main__":
     s = input("Please enter a string to see if it is a palindrome: ")
-    if is_palindrome(s):
-        print(f"'{s}' is a palindrome.")
+    if is_palindrome_slice_method(s):
+        print(f"'{s}' is a palindrome,by slice method.")
     else:
         print(f"'{s}' is not a palindrome.")
+    if is_palindrome_iterative_method(s):
+        print(f"'{s}' is a palindrome,by iterative method.")
+    else:
+        print(f"'{s}' is not a palindrome.")
+        
