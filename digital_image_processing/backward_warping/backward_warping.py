21048e8d1c11ef51f91031e6da269557f6f197cd
TimelessPersona <rlgns9803@gmail.com>
2022-12-09 14:32:37 +0900

    Add backward_warping.py

--- digital_image_processing/backward_warping/backward_warping.py
+++ digital_image_processing/backward_warping/backward_warping.py
@@ -1,21 +1,31 @@
 import cv2
 import numpy as np
+import doctest
 
-
-def backward_warping(matrix: np.ndarray, img: np.ndarray) -> np.ndarray:
+def backward_warping(matrix:np.ndarray, img:np.ndarray)->np.ndarray:
     """
     Get backward_warping image
     :param matrix: 3x3 list
     :param img: np.array
     :return: np.array
+
+    Example
+    M2 is a translation matrix
+    points are moved at points*1.5
+    img[50][50] -> result img[75][75]
+    img[50][50] : 171
+    -------
+    >>> result = backward_warping(M2, img)
+    >>> result[75][75]
+    171
     """
     src_h, src_w = img.shape
     y_scale = matrix[1, 1]
     x_scale = matrix[0, 0]
 
     # result image resizing
-    dst_h = max(int(src_h * y_scale + 0.5), src_h)
-    dst_w = max(int(src_w * x_scale + 0.5), src_w)
+    dst_h = max(int(src_h*y_scale+0.5), src_h)
+    dst_w = max(int(src_w*x_scale+0.5), src_w)
     dst = np.zeros((dst_h, dst_w), img.dtype)
 
     # inverse matrix
@@ -26,8 +36,8 @@ def backward_warping(matrix: np.ndarray, img: np.ndarray) -> np.ndarray:
             # applying inverse matrix to x, y points
             temp = inv_m @ np.array([x, y, 1])
             # each point is a point applying inverse matrix
-            x_ = temp[0]
-            y_ = temp[1]
+            x_ = (temp[0])
+            y_ = (temp[1])
 
             # original image's points would be decimal points
             # using bilinear interpolation
@@ -37,11 +47,15 @@ def backward_warping(matrix: np.ndarray, img: np.ndarray) -> np.ndarray:
             # prevent overflow
             if px >= src_w - 1:
                 px = src_w - 2
+            if px < 0:
+                px = 0
             # a real point of y that left avobe estimated point
             py = int(y_)
             # prevent overflow
             if py >= src_h - 1:
                 py = src_h - 2
+            if py < 0:
+                py = 0
 
             # a distance between left real point and estimated point x
             fx1 = x_ - px
@@ -67,43 +81,47 @@ def backward_warping(matrix: np.ndarray, img: np.ndarray) -> np.ndarray:
             p4 = img[py + 1, px + 1]
 
             # a case that points are on real image size
-            if 0 <= x_ < src_w - 1 and 0 <= y_ < src_h - 1:
+            if (0 <= x_ < src_w -1 and 0 <= y_ < src_h - 1) :
                 # sum of symmetric point's value * rectangle's area
                 dst[y, x] = w1 * p1 + w2 * p2 + w3 * p3 + w4 * p4
             # a case that points are not on real image size
-            else:
+            else :
                 dst[y, x] = 0
 
     return dst
 
 
-if __name__ == "__main__":
-    img = cv2.imread("image_data/lena.jpg", cv2.IMREAD_GRAYSCALE)
-
-    M1 = np.array([[1, 0, 50], [0, 1, 100], [0, 0, 1]])
-
-    M2 = np.array([[1.5, 0, 0], [0, 1.5, 0], [0, 0, 1]])
-
+if __name__ == '__main__':
+    img = cv2.imread('image_data/lena.jpg', cv2.IMREAD_GRAYSCALE)
     angle = np.deg2rad(15)
-    M3 = np.array(
-        [
-            [np.cos(angle), -np.sin(angle), 0],
-            [np.sin(angle), np.cos(angle), 0],
-            [0, 0, 1],
-        ]
-    )
+    M1 = np.array([[1, 0, 50],
+                   [0, 1, 100],
+                   [0, 0, 1]])
 
-    M4 = np.array([[1, 0.2, 0], [0.2, 1, 0], [0, 0, 1]])
+    M2 = np.array([[1.5, 0, 0],
+                   [0, 1.5, 0],
+                   [0, 0, 1]])
+
+    M3 = np.array([[np.cos(angle), -np.sin(angle), 0],
+                   [np.sin(angle), np.cos(angle), 0],
+                   [0, 0, 1]])
+
+    M4 = np.array([[1, 0.2, 0],
+                   [0.2, 1, 0],
+                   [0, 0, 1]])
 
     dst1 = backward_warping(M1, img)
     dst2 = backward_warping(M2, img)
     dst3 = backward_warping(M3, img)
     dst4 = backward_warping(M4, img)
 
-    cv2.imshow("original", img)
-    cv2.imshow("translation", dst1)
-    cv2.imshow("scaling", dst2)
-    cv2.imshow("rotation", dst3)
-    cv2.imshow("shear", dst4)
+    doctest.testmod()
+
+    cv2.imshow('original', img)
+    cv2.imshow('translation', dst1)
+    cv2.imshow('scaling', dst2)
+    cv2.imshow('rotation', dst3)
+    cv2.imshow('shear', dst4)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
