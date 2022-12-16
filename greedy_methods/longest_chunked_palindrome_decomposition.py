459cc2a898f82db7b088495714e540b1a08e129c
alexpantyukhin <apantykhin@gmail.com>
2022-12-16 16:22:05 +0400

    add Longest Chunked Palindrome Decomposition

---
+++ greedy_methods/longest_chunked_palindrome_decomposition.py
@@ -0,0 +1,91 @@
+"""
+Author  : Alexander Pantyukhin
+Date    : December 16, 2022
+
+Task:
+
+Given a string text. You should split it to k substrings
+(subtext1, subtext2, ..., subtextk) such that:
+
+ - subtexti is a non-empty string.
+
+ - The concatenation of all the substrings is equal to text
+ (i.e., subtext1 + subtext2 + ... + subtextk == text).
+
+ - subtexti == subtextk - i + 1 for all valid values
+ of i (i.e., 1 <= i <= k).
+
+Return the largest possible value of k.
+
+Leetcode reference: 
+https://leetcode.com/problems/longest-chunked-palindrome-decomposition/description/
+
+Implementation notes:
+Use greedy approach. If it's possible to find the palindrome
+chunk for the current text start, then use it and apply recursion
+for the left path of string.
+
+Runtime: O(n*n)
+Space: O(1)
+"""
+
+def longestDecomposition(text: str) -> int:
+    """
+    >>> longestDecomposition('ghiabcdefhelloadamhelloabcdefghi')
+    7
+    >>> longestDecomposition('merchant')
+    1
+    >>> longestDecomposition('m')
+    1
+    >>> longestDecomposition('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+    46
+    >>> longestDecomposition('abcabcabcabcabcabcabcabcabcabcabcabcabcabcabc')
+    15
+    """
+
+    if text is None or len(text) == 0:
+        raise ValueError('The text should be not empty string.')
+
+    len_text = len(text)
+    
+    def substrings_are_equal(first_substring_index: int,
+                             second_substring_index: int,
+                             length: int) -> bool:
+        """
+        >>> text = 'aabaa'
+        >>> substrings_are_equal(0, 3, 2)
+        True
+        """
+
+        for i in range(length):
+            if text[first_substring_index + i] != text[second_substring_index + i]:
+                return False
+            
+        return True
+    
+    def longest_decomposition(index):
+        """
+        >>> text = 'aabaa'
+        >>> longest_decomposition(0)
+        3
+        """
+
+        if 2 * index >= len_text:
+            return 0
+        
+        ch = text[index]
+        result = 1
+
+        for i in range((len_text - 2 * index) // 2):
+            if ch == text[len_text - 1 - index - i] and \
+               substrings_are_equal(index, len_text - 1 - index - i, i + 1):
+                return max(result, 2 + longest_decomposition(index + i + 1))
+
+        return result
+
+    return longest_decomposition(0)
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
