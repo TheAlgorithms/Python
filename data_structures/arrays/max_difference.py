ac68a91d9bc3e2d9a0016364b5907bd0e702ca60
noFFENSE <orazkhanmansur@gmail.com>
2022-12-17 18:51:42 +0600

    fixed function and variable names into snake_case

--- data_structures/arrays/max_difference.py
+++ data_structures/arrays/max_difference.py
@@ -1,4 +1,4 @@
-def maxDiff(nums: list[int]) -> int:
+def max_Difference(nums: list[int]) -> int:
     """
     The function returns the max difference in the
     array where result = arr[i] - arr[j] and i > j
@@ -6,24 +6,24 @@ def maxDiff(nums: list[int]) -> int:
     Runtime: O(n)
     Space: O(1)
 
-    >>> maxDiff([2, 3, 10 , 6, 4, 8, 1])
+    >>> max_Difference(([2, 3, 10 , 6, 4, 8, 1])
     8
-    >>> maxDiff([7, 9, 5, 6, 3, 2])
+    >>> max_Difference(([7, 9, 5, 6, 3, 2])
     2
-    >>> maxDiff([10, 20, 30])
+    >>> max_Difference(([10, 20, 30])
     20
 
     """
     result = nums[1] - nums[0]
 
-    minVal = nums[0]
+    min_val = nums[0]
 
     for i in range(len(nums)):
 
-        result = max(nums[i] - minVal, result)
+        result = max(nums[i] - min_val, result)
 
-        if nums[i] < minVal:
-            minVal = nums[i]
+        if nums[i] < min_val:
+            min_val = nums[i]
 
     return result
 
