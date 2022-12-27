e591e117740786bd01367a6584987583b6677809
Erfan Alimohammadi <erfan.aa@gmail.com>
2022-12-27 10:00:19 +0100

    Update computer_vision/random_erasing_augmentation.py
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- computer_vision/random_erasing_augmentation.py
+++ computer_vision/random_erasing_augmentation.py
@@ -91,7 +91,7 @@ def random_chars(number_char: int = 32) -> str:
     32
     """
     assert number_char > 1, "The number of character should greater than 1"
-    letter_code = ascii_lowercase + digits
+    letters_and_digits = ascii_lowercase + digits
     return "".join(random.choice(letter_code) for _ in range(number_char))
 
 
