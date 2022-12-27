89b8336b47f56ee47c2225c85d270ab0c20eda9b
MohammadReza Balakhaniyan <m.balakhaniyan@gmail.com>
2022-12-27 19:50:36 +0330

    add maths/Gcd of N Numbers

--- maths/gcd_of_n_numbers.py
+++ maths/gcd_of_n_numbers.py
@@ -1,51 +1,71 @@
-from collections import Counter
-
 """
 Gcd of N Numbers
+Reference: https://en.wikipedia.org/wiki/Greatest_common_divisor
 """
 
-def get_factors(number: int, factors: dict | None = None, min_factor: int = 2) -> Counter:
+from collections import Counter
+
+
+def get_factors(number: int, factors: dict | None = None, factor: int = 2) -> Counter:
     """
-    get all factors of number:
+    this is a reccursive function for get all factors of number
     >>> get_factors(45)
     Counter({3: 2, 5: 1})
     >>> get_factors(2520)
     Counter({2: 3, 3: 2, 5: 1, 7: 1})
     >>> get_factors(23)
     Counter({23: 1})
+    
+    factor can be all numbers from 2 to number that we check if number % factor == 0
+    if it is equal to zero, we check again with number // factor else we increase factor by one
     """
-    if factors is None:
+
+    if not isinstance(number, int):
+        raise TypeError("number must be integer")
+
+    if factors is None: 
+        # first call of get factors and so there are no founded factors 
         factors = Counter()
-        
-        
-    if number == min_factor:
-        factors[min_factor] += 1
+
+    if number == factor: # break condition
+        # all numbers are factors of itself
+        factors[factor] += 1
         return factors
-    
-    if not number % min_factor:
-        factors[min_factor] += 1
-        return get_factors(number // min_factor, factors, min_factor)
-    else:
-        return get_factors(number, factors, min_factor + 1)
-    
-def get_gcd(*numbers: int) -> int:
+
+    if number % factor > 0:
+        # if it is grater than zero, so it is not a factor of number and we check next number
+        return get_factors(number, factors, factor + 1)
+
+    factors[factor] += 1
+    # else we update factors (that is Counter(dict-like) type) and check again
+    return get_factors(number // factor, factors, factor)
+
+
+def get_greatest_common_divisor(*numbers: int) -> int:
     """
     get gcd of n numbers:
-    >>> get_gcd(18, 45)
+    >>> get_greatest_common_divisor(18, 45)
     9
-    >>> get_gcd(23, 37)
+    >>> get_greatest_common_divisor(23, 37)
     1
-    >>> get_gcd(2520, 8350)
+    >>> get_greatest_common_divisor(2520, 8350)
     10
     """
+    # we just need factors, not numbers itself
     data = [get_factors(number) for number in numbers]
-    same_factors = data[0]
+    same_factors: dict = data[0]
     for d in data[1:]:
-        same_factors = same_factors & d
+        same_factors = same_factors & d 
+        # get common factor between all,`&` return common elements with smaller value (for Counter type)
+    
+    # now, same_factors is something like {2: 2, 3: 4} that means 2 * 2 * 3 * 3 * 3 * 3
     mult = 1
+    # power each factor and multiply
+    # for {2: 2, 3: 4}, it is [4, 81] and then 324
     for m in [factor ** same_factors[factor] for factor in same_factors]:
         mult *= m
     return mult
 
+
 if __name__ == "__main__":
-    print(get_gcd(18, 45)) # 9
+    print(get_greatest_common_divisor(18, 45))  # 9
