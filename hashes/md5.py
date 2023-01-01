5fadb6e79b981eee4490dfdc790fedd2f41a06a8
Tianyi Zheng <tianyizheng02@gmail.com>
2023-01-01 00:25:40 -0800

    Add more detailed comments, docs, and doctests

--- hashes/md5.py
+++ hashes/md5.py
@@ -1,42 +1,86 @@
+"""
+The MD5 algorithm is a hash function that's commonly used as a checksum to
+detect data corruption. The algorithm works by processing a given message in
+blocks of 512 bits, padding the message as needed. It uses the blocks to operate
+a 128-bit state and performs a total of 64 such operations. Note that all values
+are little-endian, so inputs are converted as needed.
+
+Although MD5 was used as a cryptographic hash function in the past, it's since
+been cracked, so it shouldn't be used for security purposes.
+
+For more info, see https://en.wikipedia.org/wiki/MD5
+"""
+
 from collections.abc import Generator
 from math import sin
 
 
-def to_little_endian(bit_string_32: str) -> str:
-    """[summary]
-    Converts the given binary string to little-endian in groups of 8 chars.
+def to_little_endian(string_32: str) -> str:
+    """
+    Converts the given string to little-endian in groups of 8 chars.
 
     Arguments:
-        bit_string_32 {[string]} -- [32 bit binary]
+        string_32 {[string]} -- [32-char string]
 
     Raises:
-        ValueError -- [if the given string not are 32 bit binary string]
+        ValueError -- [input is not 32 char]
 
     Returns:
-        [string] -- [32 bit binary string]
+        32-char little-endian string
     >>> to_little_endian('1234567890abcdfghijklmnopqrstuvw')
     'pqrstuvwhijklmno90abcdfg12345678'
+    >>> to_little_endian('1234567890')
+    Traceback (most recent call last):
+    ...
+    ValueError: Input must be of length 32
     """
+    if len(string_32) != 32:
+        raise ValueError("Input must be of length 32")
 
-    if len(bit_string_32) != 32:
-        raise ValueError("Need length 32")
     little_endian = ""
     for i in [3, 2, 1, 0]:
-        little_endian += bit_string_32[8 * i : 8 * i + 8]
+        little_endian += string_32[8 * i : 8 * i + 8]
     return little_endian
 
 
 def reformat_hex(i: int) -> str:
-    """[summary]
-    Converts the given integer into 8-char little-endian hex number.
+    """
+    Converts the given non-negative integer to hex string.
+
+    Example: Suppose the input is the following:
+        i = 1234
+
+        The input is 0x000004d2 in hex, so the little-endian hex string is
+        "d2040000".
 
     Arguments:
         i {[int]} -- [integer]
+
+    Raises:
+        ValueError -- [input is negative]
+
+    Returns:
+        8-char little-endian hex string
+
+    >>> reformat_hex(1234)
+    'd2040000'
     >>> reformat_hex(666)
     '9a020000'
+    >>> reformat_hex(0)
+    '00000000'
+    >>> reformat_hex(1234567890)
+    'd2029649'
+    >>> reformat_hex(1234567890987654321)
+    'b11c6cb1'
+    >>> reformat_hex(-1)
+    Traceback (most recent call last):
+    ...
+    ValueError: Input must be non-negative
     """
+    if i < 0:
+        raise ValueError("Input must be non-negative")
 
-    hex_rep = format(i, "08x")
+    hex_rep = format(i, "08x")[-8:]
     little_endian_hex = ""
     for i in [3, 2, 1, 0]:
         little_endian_hex += hex_rep[2 * i : 2 * i + 2]
@@ -44,40 +88,94 @@ def reformat_hex(i: int) -> str:
 
 
 def preprocess(message: str) -> str:
-    """[summary]
+    """
     Preprocesses the message string:
     - Convert message to bit string
-    - Pad bit string to a multiple of 512 bits:
+    - Pad bit string to a multiple of 512 chars:
         - Append a 1
-        - Append 0's until length = 448 (mod 512), 64 bits short of a multiple of 512
-        - Append length of original message (64 bits)
+        - Append 0's until length = 448 (mod 512)
+        - Append length of original message (64 chars)
+
+    Example: Suppose the input is the following:
+        message = "a"
+
+        The message bit string is "01100001", which is 8 bits long. Thus, the
+        bit string needs 439 bits of padding so that
+        (bit_string + "1" + padding) = 448 (mod 512).
+        The message length is "000010000...0" in 64-bit little-endian binary.
+        The combined bit string is then 512 bits long.
 
     Arguments:
-            message {[string]} -- [message string]
+        message {[string]} -- [message string]
 
     Returns:
-            [string] -- [padded bit string]
+        processed bit string padded to a multiple of 512 chars
+
+    >>> preprocess("a") == "01100001" + "1" + ("0" * 439) + "00001000" + ("0" * 56)
+    True
+    >>> preprocess("") == "1" + ("0" * 447) + ("0" * 64)
+    True
     """
     bit_string = ""
     for char in message:
         bit_string += format(ord(char), "08b")
     start_len = format(len(bit_string), "064b")
+
+    # Pad bit_string to a multiple of 512 chars
     bit_string += "1"
     while len(bit_string) % 512 != 448:
         bit_string += "0"
     bit_string += to_little_endian(start_len[32:]) + to_little_endian(start_len[:32])
+
     return bit_string
 
 
 def get_block_words(bit_string: str) -> Generator[list[int], None, None]:
-    """[summary]
-    Iterator:
-        Returns by each call a list of length 16 with the 32-bit
-        integer blocks.
+    """
+    Splits bit string into blocks of 512 chars and yields each block as a list
+    of 32-bit words
+
+    Example: Suppose the input is the following:
+        bit_string =
+            "000000000...0" +  # 0x00 (32 bits, padded to the right)
+            "000000010...0" +  # 0x01 (32 bits, padded to the right)
+            "000000100...0" +  # 0x02 (32 bits, padded to the right)
+            "000000110...0" +  # 0x03 (32 bits, padded to the right)
+            ...
+            "000011110...0"    # 0x0a (32 bits, padded to the right)
+
+        Then len(bit_string) == 512, so there'll be 1 block. The block is split
+        into 32-bit words, and each word is converted to little endian. The
+        first word is interpreted as 0 in decimal, the second word is
+        interpreted as 1 in decimal, etc.
+
+        Thus, block_words == [[0, 1, 2, 3, ..., 15]].
 
     Arguments:
-        bit_string {[string]} -- [binary string >= 512]
+        bit_string {[string]} -- [bit string with multiple of 512 as length]
+
+    Raises:
+        ValueError -- [length of bit string isn't multiple of 512]
+
+    Yields:
+        a list of 16 32-bit words
+
+    >>> test_string = "".join(format(n << 24, "032b") for n in range(16))
+    >>> list(get_block_words(test_string))
+    [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]]
+    >>> list(get_block_words(test_string * 4)) == [list(range(16))] * 4
+    True
+    >>> list(get_block_words("1" * 512)) == [[4294967295] * 16]
+    True
+    >>> list(get_block_words(""))
+    []
+    >>> list(get_block_words("1111"))
+    Traceback (most recent call last):
+    ...
+    ValueError: Input must have length that's a multiple of 512
     """
+    if len(bit_string) % 512 != 0:
+        raise ValueError("Input must have length that's a multiple of 512")
 
     for pos in range(0, len(bit_string), 512):
         block = bit_string[pos : pos + 512]
@@ -89,9 +187,35 @@ def get_block_words(bit_string: str) -> Generator[list[int], None, None]:
 
 def not_32(i: int) -> int:
     """
+    Perform bitwise NOT on given int.
+
+    Arguments:
+        i {[int]} -- [given int]
+
+    Raises:
+        ValueError -- [input is negative]
+
+    Returns:
+        Result of bitwise NOT on i
+
     >>> not_32(34)
     4294967261
+    >>> not_32(1234)
+    4294966061
+    >>> not_32(4294966061)
+    1234
+    >>> not_32(0)
+    4294967295
+    >>> not_32(1)
+    4294967294
+    >>> not_32(-1)
+    Traceback (most recent call last):
+    ...
+    ValueError: Input must be non-negative
     """
+    if i < 0:
+        raise ValueError("Input must be non-negative")
+
     i_str = format(i, "032b")
     new_str = ""
     for c in i_str:
@@ -100,19 +224,85 @@ def not_32(i: int) -> int:
 
 
 def sum_32(a: int, b: int) -> int:
+    """
+    Add two numbers as 32-bit ints.
+
+    Arguments:
+        a {[int]} -- [first given int]
+        b {[int]} -- [second given int]
+
+    Returns:
+        (a + b) as an unsigned 32-bit int
+
+    >>> sum_32(1, 1)
+    2
+    >>> sum_32(2, 3)
+    5
+    >>> sum_32(0, 0)
+    0
+    >>> sum_32(-1, -1)
+    4294967294
+    >>> sum_32(4294967295, 1)
+    0
+    """
     return (a + b) % 2**32
 
 
 def left_rotate_32(i: int, shift: int) -> int:
-    return (i << shift) ^ (i >> (32 - shift))
+    """
+    Rotate the bits of a given int left by a given amount.
+
+    Arguments:
+        i {[int]} -- [given int]
+        shift {[int]} -- [shift amount]
+
+    Raises:
+        ValueError -- [either given int or shift is negative]
+
+    Returns:
+        `i` rotated to the left by `shift` bits
+
+    >>> left_rotate_32(1234, 1)
+    2468
+    >>> left_rotate_32(1111, 4)
+    17776
+    >>> left_rotate_32(2147483648, 1)
+    1
+    >>> left_rotate_32(2147483648, 3)
+    4
+    >>> left_rotate_32(4294967295, 4)
+    4294967295
+    >>> left_rotate_32(1234, 0)
+    1234
+    >>> left_rotate_32(0, 0)
+    0
+    >>> left_rotate_32(-1, 0)
+    Traceback (most recent call last):
+    ...
+    ValueError: Input must be non-negative
+    >>> left_rotate_32(0, -1)
+    Traceback (most recent call last):
+    ...
+    ValueError: Shift must be non-negative
+    """
+    if i < 0:
+        raise ValueError("Input must be non-negative")
+    if shift < 0:
+        raise ValueError("Shift must be non-negative")
+    return ((i << shift) ^ (i >> (32 - shift))) % 2**32
 
 
 def md5_me(message: str) -> str:
-    """[summary]
-    Returns a 32-bit hash of the string 'message'
+    """
+    Returns the 32-char MD5 hash of a given message.
+
+    Reference: https://en.wikipedia.org/wiki/MD5#Algorithm
 
     Arguments:
         message {[string]} -- [message]
+
+    Returns:
+        32-char MD5 hash string
 
     >>> md5_me("")
     'd41d8cd98f00b204e9800998ecf8427e'
@@ -122,10 +312,12 @@ def md5_me(message: str) -> str:
     'e4d909c290d0fb1ca068ffaddf22cbd0'
     """
 
+    # Convert to bit string, add padding and append message length
     bit_string = preprocess(message)
 
     added_consts = [int(2**32 * abs(sin(i + 1))) for i in range(64)]
 
+    # Starting states
     a0 = 0x67452301
     b0 = 0xEFCDAB89
     c0 = 0x98BADCFE
@@ -198,18 +390,21 @@ def md5_me(message: str) -> str:
         21,
     ]
 
+    # Process bit string in chunks, each with 16 32-char words
     for block_words in get_block_words(bit_string):
         a = a0
         b = b0
         c = c0
         d = d0
+
+        # Hash current chunk
         for i in range(64):
             if i <= 15:
-                # f = (b & c) | (not_32(b) & d)
+                # f = (b & c) | (not_32(b) & d)     # Alternate definition for f
                 f = d ^ (b & (c ^ d))
                 g = i
             elif i <= 31:
-                # f = (d & b) | (not_32(d) & c)
+                # f = (d & b) | (not_32(d) & c)     # Alternate definition for f
                 f = c ^ (d & (b ^ c))
                 g = (5 * i + 1) % 16
             elif i <= 47:
@@ -223,6 +418,8 @@ def md5_me(message: str) -> str:
             d = c
             c = b
             b = sum_32(b, left_rotate_32(f, shift_amounts[i]))
+
+        # Add hashed chunk to running total
         a0 = sum_32(a0, a)
         b0 = sum_32(b0, b)
         c0 = sum_32(c0, c)
