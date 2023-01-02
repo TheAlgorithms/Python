11aaefd543834a9e1d18e58830bcb2232388a867
Tianyi Zheng <tianyizheng02@gmail.com>
2023-01-01 19:26:09 -0800

    Update sierpinski_triangle.py header doc

--- fractals/sierpinski_triangle.py
+++ fractals/sierpinski_triangle.py
@@ -1,31 +1,27 @@
-#!/usr/bin/python
+"""
+Author Anurag Kumar | anuragkumarak95@gmail.com | git/anuragkumarak95
 
-"""Author Anurag Kumar | anuragkumarak95@gmail.com | git/anuragkumarak95
+Simple example of fractal generation using recursion.
 
-Simple example of Fractal generation using recursive function.
+What is the Sierpiński Triangle?
+    The Sierpiński triangle (sometimes spelled Sierpinski), also called the
+Sierpiński gasket or Sierpiński sieve, is a fractal attractive fixed set with
+the overall shape of an equilateral triangle, subdivided recursively into
+smaller equilateral triangles. Originally constructed as a curve, this is one of
+the basic examples of self-similar sets—that is, it is a mathematically
+generated pattern that is reproducible at any magnification or reduction. It is
+named after the Polish mathematician Wacław Sierpiński, but appeared as a
+decorative pattern many centuries before the work of Sierpiński.
 
-What is Sierpinski Triangle?
->>The Sierpinski triangle (also with the original orthography Sierpinski), also called
-the Sierpinski gasket or the Sierpinski Sieve, is a fractal and attractive fixed set
-with the overall shape of an equilateral triangle, subdivided recursively into smaller
-equilateral triangles. Originally constructed as a curve, this is one of the basic
-examples of self-similar sets, i.e., it is a mathematically generated pattern that can
-be reproducible at any magnification or reduction. It is named after the Polish
-mathematician Wacław Sierpinski, but appeared as a decorative pattern many centuries
-prior to the work of Sierpinski.
+Requirements (pip): turtle
 
-Requirements(pip):
-  - turtle
+Usage: python sierpinski_triangle.py <int:depth_for_fractal>
 
-Python:
-  - 2.6
-
-Usage:
-  - $python sierpinski_triangle.py <int:depth_for_fractal>
-
-Credits: This code was written by editing the code from
-https://www.riannetrujillo.com/blog/python-fractal/
-
+Credits:
+    The above description is taken from
+    https://en.wikipedia.org/wiki/Sierpi%C5%84ski_triangle
+    This code was written by editing the code from
+    https://www.riannetrujillo.com/blog/python-fractal/
 """
 import sys
 import turtle
