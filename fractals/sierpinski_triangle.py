c27cc7676ee1b81dde75f5e49c4f0925c613a373
Tianyi Zheng <tianyizheng02@gmail.com>
2023-01-01 19:56:53 -0800

    Add type hints to sierpinski_triangle.py

--- fractals/sierpinski_triangle.py
+++ fractals/sierpinski_triangle.py
@@ -27,11 +27,16 @@ import sys
 import turtle
 
 
-def get_mid(p1, p2):
+def get_mid(p1: tuple[float, float], p2: tuple[float, float]) -> tuple[float, float]:
     return (p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2  # find midpoint
 
 
-def triangle(vertex1, vertex2, vertex3, depth):
+def triangle(
+    vertex1: tuple[float, float],
+    vertex2: tuple[float, float],
+    vertex3: tuple[float, float],
+    depth: int,
+) -> None:
     my_pen.up()
     my_pen.goto(vertex1[0], vertex1[1])
     my_pen.down()
