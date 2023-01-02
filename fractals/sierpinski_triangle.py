025f03dc9afe8d876017622e41780e9023ce4723
Tianyi Zheng <tianyizheng02@gmail.com>
2023-01-01 19:45:10 -0800

    Refactor triangle() function to not use list of vertices
    Since the number of vertices is always fixed at 3, there's no need to
    pass in the vertices as a list, and it's clearer to give the vertices
    distinct names rather than index them from the list

--- fractals/sierpinski_triangle.py
+++ fractals/sierpinski_triangle.py
@@ -26,45 +26,42 @@ Credits:
 import sys
 import turtle
 
-points = [[-175, -125], [0, 175], [175, -125]]  # size of triangle
-
 
 def get_mid(p1, p2):
-    return ((p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2)  # find midpoint
+    return (p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2  # find midpoint
 
 
-def triangle(points, depth):
-
+# def triangle(points, depth):
+def triangle(vertex1, vertex2, vertex3, depth):
     my_pen.up()
-    my_pen.goto(points[0][0], points[0][1])
+    my_pen.goto(vertex1[0], vertex1[1])
     my_pen.down()
-    my_pen.goto(points[1][0], points[1][1])
-    my_pen.goto(points[2][0], points[2][1])
-    my_pen.goto(points[0][0], points[0][1])
+    my_pen.goto(vertex2[0], vertex2[1])
+    my_pen.goto(vertex3[0], vertex3[1])
+    my_pen.goto(vertex1[0], vertex1[1])
 
     if depth > 0:
         triangle(
-            [points[0], get_mid(points[0], points[1]), get_mid(points[0], points[2])],
-            depth - 1,
+            vertex1, get_mid(vertex1, vertex2), get_mid(vertex1, vertex3), depth - 1
         )
         triangle(
-            [points[1], get_mid(points[0], points[1]), get_mid(points[1], points[2])],
-            depth - 1,
+            vertex2, get_mid(vertex1, vertex2), get_mid(vertex2, vertex3), depth - 1
         )
         triangle(
-            [points[2], get_mid(points[2], points[1]), get_mid(points[0], points[2])],
-            depth - 1,
+            vertex3, get_mid(vertex3, vertex2), get_mid(vertex1, vertex3), depth - 1
         )
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         raise ValueError(
-            "right format for using this script: "
-            "$python fractals.py <int:depth_for_fractal>"
+            "Correct format for using this script: "
+            "python fractals.py <int:depth_for_fractal>"
         )
     my_pen = turtle.Turtle()
     my_pen.ht()
     my_pen.speed(5)
     my_pen.pencolor("red")
-    triangle(points, int(sys.argv[1]))
+
+    vertices = [[-175, -125], [0, 175], [175, -125]]  # vertices of triangle
+    triangle(vertices[0], vertices[1], vertices[2], int(sys.argv[1]))
