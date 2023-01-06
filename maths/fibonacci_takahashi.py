97279dcd37890d46271befa024267cca26a77477
spirosmaggioros <51701672+spirosmaggioros@users.noreply.github.com>
2023-01-06 10:03:49 +0200

    Create fibonacci_takahashi.py

---
+++ maths/fibonacci_takahashi.py
@@ -0,0 +1,29 @@
+import gmpy2
+from gmpy2 import mpz
+
+#the takahashi fibonacci algorithm is used to compute fibonacci numbers for a large value of n.
+#more details on D.Takahashi's paper about his algorithm
+
+def fibonacci(n):
+  def fib_inner(n):
+    if n == 0:
+      return mpz(2), mpz(1)
+    m = n >> 1
+    u, v = fib_inner(m)
+    q = (2, -2)[m & 1]
+    u = u * u - q
+    v = v * v + q
+    if (n & 1):
+      return v - u, v
+    return u, v - u
+
+  m = n >> 1
+  u, v = fib_inner(m)
+  f = (2*v - u) / 5
+  if (n & 1):
+    q = (n & 2) - 1
+    return v * f - q
+  return u * f
+
+n = int(input())
+print(fibonacci(n));
