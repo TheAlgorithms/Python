681e529773369815f8ace9738dc953743ee760a5
Sundaram Kumar Jha <93595231+sundaram2021@users.noreply.github.com>
2023-01-26 13:11:31 +0530

    Updated permutations.py
    Earlier in the permutation function it is creating an infinite recursion which is causing an error 
    
    instead of that I used backtracking to get the output

--- data_structures/arrays/permutations.py
+++ data_structures/arrays/permutations.py
@@ -1,4 +1,5 @@
-def permute(nums: list[int]) -> list[list[int]]:
+from typing import List
+def permute(nums: List[int]) -> List[List[int]]:
     """
     Return all permutations.
 
@@ -7,20 +8,23 @@ def permute(nums: list[int]) -> list[list[int]]:
     >>> all(list(nums) in permute(numbers) for nums in permutations(numbers))
     True
     """
-    result = []
-    if len(nums) == 1:
-        return [nums.copy()]
-    for _ in range(len(nums)):
-        n = nums.pop(0)
-        permutations = permute(nums)
-        for perm in permutations:
-            perm.append(n)
-        result.extend(permutations)
-        nums.append(n)
-    return result
+    def backtrack(first=0):
+        if first == n:
+            output.append(nums[:])
+        for i in range(first, n):
+            nums[first], nums[i] = nums[i], nums[first]
+            backtrack(first + 1)
+            nums[first], nums[i] = nums[i], nums[first]
+
+    n = len(nums)
+    output = []
+    backtrack()
+    return output
 
 
+    # return result
 if __name__ == "__main__":
     import doctest
-
+    res = permute([1,2,3])
+    print(res)
     doctest.testmod()
