ae6d10bfcd840ff4371a5f5e335e229ff692be87
Sundaram Kumar Jha <93595231+sundaram2021@users.noreply.github.com>
2023-01-26 13:41:28 +0530

    updated permutation.py
    modified permute function which is using recursive methode
    
    to permute2 function which is using backtracking to find the output

--- data_structures/arrays/permutations.py
+++ data_structures/arrays/permutations.py
@@ -4,12 +4,27 @@ from typing import List
 def permute(nums: list[int]) -> list[list[int]]:
     """
     Return all permutations.
-
     >>> from itertools import permutations
     >>> numbers= [1,2,3]
     >>> all(list(nums) in permute(numbers) for nums in permutations(numbers))
     True
     """
+    result = []
+    if len(nums) == 1:
+        return [nums.copy()]
+    for _ in range(len(nums)):
+        n = nums.pop(0)
+        permutations = permute(nums)
+        for perm in permutations:
+            perm.append(n)
+        result.extend(permutations)
+        nums.append(n)
+    return result
+
+
+
+
+def permute2(nums: List[int]) -> List[List[int]]:
 
     def backtrack(first=0):
         if first == n:
@@ -30,6 +45,7 @@ def permute(nums: list[int]) -> list[list[int]]:
 if __name__ == "__main__":
     import doctest
 
-    res = permute([1, 2, 3])
+    # use res to print the data in permute2 function
+    res = permute2([1, 2, 3])
     print(res)
     doctest.testmod()
