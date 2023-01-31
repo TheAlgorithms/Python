f408f8c7d2ab268a417a7acd12a977a59b4396ee
Matthew Anderson <mattanderson65@outlook.com>
2023-01-31 14:40:32 -0800

    Add present_value.py

---
+++ financial/present_value.py
@@ -0,0 +1,20 @@
+# Present Value Formula
+# https://en.wikipedia.org/wiki/Present_value
+
+
+def present_value_compound(
+    future_value: float, rate: float, interest_period: float
+) -> float:
+
+    """
+    >>> present_value_compound(10000.0, 0.06, 10)
+    5583.947769151178
+    """
+
+    if future_value <= 0:
+        raise ValueError("future_value must be greater than 0")
+    if rate <= 0:
+        raise ValueError("rate must be greater than 0")
+    if interest_period <= 0:
+        raise ValueError("interest_period must be greater than 0")
+    return future_value / (1 + rate) ** interest_period
