be2c2fd35f61f2e93f84a4b524764103d8f9b0fe
domix80 <d.agresta@gmail.com>
2023-02-07 09:05:24 +0100

    Create smallest_multiple.py
    feat: Add smallest_multiple.
    Find the smallest multiple of all number between 1 and n.

---
+++ maths/smallest_multiple.py
@@ -0,0 +1,60 @@
+"""
+Smallest multiple
+Problem
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to n?
+"""
+
+def find_prime_numbers (n, l):
+  x = 2
+  while x < n:
+    y = 2
+    while y < x+1:
+      v = x // y
+      r = x % y                              
+      if v == 1 and r == 0:
+        l.append (x)
+        break
+      if r == 0:
+        break
+      y +=1
+    x +=1
+
+def find_max_power (n, l, lp):
+  result = 1
+  for e in l:
+    max_power = 0
+    value = 2
+    while value < n+1:                           
+      power = 0
+      v_actual = value
+      while True:
+        v_result = v_actual // e
+        rest = v_actual % e
+        if rest == 0:
+          power += 1
+        v_actual =v_result
+       
+        #exit condition
+        if (v_result <= e and rest == 1) or (v_result <= 1):
+          if power > max_power:
+            max_power = power
+          break
+
+      value +=1
+    result *=  pow(e, max_power)
+    lp.append ([e, max_power])
+  return(result)
+                 
+if __name__ == '__main__':
+  n = 10
+  l = []
+  lp = []
+  find_prime_numbers(n, l)
+  print("Prime numbers:")
+  print(l)
+  result = find_max_power(n, l, lp)
+  print("Max power for prime numbers:")
+  print(lp)
+  print("Smallest multiple:")
+  print (result)
