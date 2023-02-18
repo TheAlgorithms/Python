0c548d43a012a172dc216af4e734a1d607257e9d
BillXu0424 <1065602877@qq.com>
2023-02-18 18:40:48 +0100

    refactor. issue #5: refactored bi_directional_dij function in bi_directional_dijkstra.py

--- graphs/bi_directional_dijkstra.py
+++ graphs/bi_directional_dijkstra.py
@@ -52,52 +52,21 @@ def bidirectional_dij(
         return 0
 
     while queue_forward and queue_backward:
-        while not queue_forward.empty():
-            _, v_fwd = queue_forward.get()
-
-            if v_fwd not in visited_forward:
-                break
-        else:
+        # pop from forward queue
+        v_fwd = add_visited(queue_forward, visited_forward)
+        if v_fwd is None:
             break
-        visited_forward.add(v_fwd)
-
-        while not queue_backward.empty():
-            _, v_bwd = queue_backward.get()
-
-            if v_bwd not in visited_backward:
-                break
-        else:
+        
+        # pop from backward queue
+        v_bwd = add_visited(queue_backward, visited_backward)
+        if v_bwd is None:
             break
-        visited_backward.add(v_bwd)
-
+        
         # forward pass and relaxation
-        for nxt_fwd, d_forward in graph_forward[v_fwd]:
-            if nxt_fwd in visited_forward:
-                continue
-            old_cost_f = cst_fwd.get(nxt_fwd, np.inf)
-            new_cost_f = cst_fwd[v_fwd] + d_forward
-            if new_cost_f < old_cost_f:
-                queue_forward.put((new_cost_f, nxt_fwd))
-                cst_fwd[nxt_fwd] = new_cost_f
-                parent_forward[nxt_fwd] = v_fwd
-            if nxt_fwd in visited_backward:
-                if cst_fwd[v_fwd] + d_forward + cst_bwd[nxt_fwd] < shortest_distance:
-                    shortest_distance = cst_fwd[v_fwd] + d_forward + cst_bwd[nxt_fwd]
-
+        shortest_distance = forward_pass(graph_forward, v_fwd, queue_forward, visited_forward, visited_backward, parent_forward, cst_fwd, cst_bwd, shortest_distance)
+        
         # backward pass and relaxation
-        for nxt_bwd, d_backward in graph_backward[v_bwd]:
-            if nxt_bwd in visited_backward:
-                continue
-            old_cost_b = cst_bwd.get(nxt_bwd, np.inf)
-            new_cost_b = cst_bwd[v_bwd] + d_backward
-            if new_cost_b < old_cost_b:
-                queue_backward.put((new_cost_b, nxt_bwd))
-                cst_bwd[nxt_bwd] = new_cost_b
-                parent_backward[nxt_bwd] = v_bwd
-
-            if nxt_bwd in visited_forward:
-                if cst_bwd[v_bwd] + d_backward + cst_fwd[nxt_bwd] < shortest_distance:
-                    shortest_distance = cst_bwd[v_bwd] + d_backward + cst_fwd[nxt_bwd]
+        shortest_distance = backward_pass(graph_backward, v_bwd, queue_backward, visited_forward, visited_backward, parent_backward, cst_bwd, cst_bwd, shortest_distance)
 
         if cst_fwd[v_fwd] + cst_bwd[v_bwd] >= shortest_distance:
             break
@@ -105,6 +74,92 @@ def bidirectional_dij(
     if shortest_distance != np.inf:
         shortest_path_distance = shortest_distance
     return shortest_path_distance
+
+def add_visited(queue, visited) -> int:
+    """
+    pop a node from the queue and add it to visited nodes
+    
+    Args:
+        queue (PriorityQueue): priority queue of forward/backward search
+        visited (set): visited set of nodes of forward/backward search
+
+    Returns:
+        int: popped node
+    """
+    while not queue.empty():
+        _, v_fwd = queue.get()
+
+        if v_fwd not in visited:
+            break
+    else:
+        return None
+    visited.add(v_fwd)
+    return v_fwd
+
+def forward_pass(graph_forward, v_fwd, queue_forward, visited_forward, visited_backward, parent_forward, cst_fwd, cst_bwd, shortest_distance):
+    """
+    Forward pass and relaxation.
+
+    Args:
+        graph_forward (dict): graph structure used for forward pass
+        v_fwd (int): popped node from forward queue
+        queue_forward (PriorityQueue): forward queue
+        visited_forward (set): visited nodes during forward pass
+        visited_backward (set): visited nodes during backward pass
+        parent_forward (dict): dict of node and its predecessor for forward pass
+        cst_fwd (dict): dict of node and shortest distance for forward pass
+        cst_bwd (dict): dict of node and shortest distance for backward pass
+        shortest_distance (float): current shortest distance
+
+    Returns:
+        float: updated shortest distance
+    """
+    for nxt_fwd, d_forward in graph_forward[v_fwd]:
+        if nxt_fwd in visited_forward:
+            continue
+        old_cost_f = cst_fwd.get(nxt_fwd, np.inf)
+        new_cost_f = cst_fwd[v_fwd] + d_forward
+        if new_cost_f < old_cost_f:
+            queue_forward.put((new_cost_f, nxt_fwd))
+            cst_fwd[nxt_fwd] = new_cost_f
+            parent_forward[nxt_fwd] = v_fwd
+        if nxt_fwd in visited_backward:
+            if cst_fwd[v_fwd] + d_forward + cst_bwd[nxt_fwd] < shortest_distance:
+                shortest_distance = cst_fwd[v_fwd] + d_forward + cst_bwd[nxt_fwd]
+    return shortest_distance
+
+def backward_pass(graph_backward, v_bwd, queue_backward, visited_forward, visited_backward, parent_backward, cst_fwd, cst_bwd, shortest_distance):
+    """
+    Backward pass and relaxation.
+
+    Args:
+        graph_backward (dict): graph structure used for backward pass
+        v_bwd (int): popped node from backward queue
+        queue_backward (PriorityQueue): backward queue
+        visited_forward (set): visited nodes during forward pass
+        visited_backward (set): visited nodes during backward pass
+        parent_backward (dict): dict of node and its predecessor for backward pass
+        cst_fwd (dict): dict of node and shortest distance for forward pass
+        cst_bwd (dict): dict of node and shortest distance for backward pass
+        shortest_distance (float): current shortest distance
+
+    Returns:
+        float: updated shortest distance
+    """
+    for nxt_bwd, d_backward in graph_backward[v_bwd]:
+        if nxt_bwd in visited_backward:
+            continue
+        old_cost_b = cst_bwd.get(nxt_bwd, np.inf)
+        new_cost_b = cst_bwd[v_bwd] + d_backward
+        if new_cost_b < old_cost_b:
+            queue_backward.put((new_cost_b, nxt_bwd))
+            cst_bwd[nxt_bwd] = new_cost_b
+            parent_backward[nxt_bwd] = v_bwd
+
+        if nxt_bwd in visited_forward:
+            if cst_bwd[v_bwd] + d_backward + cst_fwd[nxt_bwd] < shortest_distance:
+                shortest_distance = cst_bwd[v_bwd] + d_backward + cst_fwd[nxt_bwd]
+    return shortest_distance
 
 
 graph_fwd = {
