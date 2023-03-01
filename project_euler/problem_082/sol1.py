f21e277121f50d825d39ab3050f24f522fb18b25
MaximSmolskiy <mithridatus@mail.ru>
2023-03-01 22:21:29 +0300

    Fix review issues

--- project_euler/problem_082/sol1.py
+++ project_euler/problem_082/sol1.py
@@ -21,7 +21,7 @@ a 31K text file containing an 80 by 80 matrix.
 import os
 
 
-def solution(filename: str = "p082_matrix.txt") -> int:
+def solution(filename: str = "input.txt") -> int:
     """
     Returns the minimal path sum in the matrix from the file, by starting in any cell
     in the left column and finishing in any cell in the right column,
@@ -37,23 +37,23 @@ def solution(filename: str = "p082_matrix.txt") -> int:
             for line in input_file.readlines()
         ]
 
-    m = len(matrix)
-    n = len(matrix[0])
+    rows = len(matrix)
+    cols = len(matrix[0])
 
-    minimal_path_sums = [[-1 for _ in range(n)] for _ in range(m)]
-    for i in range(m):
+    minimal_path_sums = [[-1 for _ in range(cols)] for _ in range(rows)]
+    for i in range(rows):
         minimal_path_sums[i][0] = matrix[i][0]
 
-    for j in range(1, n):
-        for i in range(m):
+    for j in range(1, cols):
+        for i in range(rows):
             minimal_path_sums[i][j] = minimal_path_sums[i][j - 1] + matrix[i][j]
 
-        for i in range(1, m):
+        for i in range(1, rows):
             minimal_path_sums[i][j] = min(
                 minimal_path_sums[i][j], minimal_path_sums[i - 1][j] + matrix[i][j]
             )
 
-        for i in range(m - 2, -1, -1):
+        for i in range(rows - 2, -1, -1):
             minimal_path_sums[i][j] = min(
                 minimal_path_sums[i][j], minimal_path_sums[i + 1][j] + matrix[i][j]
             )
