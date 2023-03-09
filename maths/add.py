7d57cd2991a1bb17382ab36f8f41b1a4988ea89b
Muhammad Suleman <88381507+supolaris@users.noreply.github.com>
2023-03-09 09:10:57 +0500

    update add.py
    I have converted the code to the dynamic input. the user now input the desired value in the variable a & b, and then these two variables will be added.

--- maths/add.py
+++ maths/add.py
@@ -14,6 +14,6 @@ def add(a: float, b: float) -> float:
 
 
 if __name__ == "__main__":
-    a = 5
-    b = 6
+    a = float(input("Enter the first value to be added: ))
+    b = float(input("Enter the second value to be added: ))
     print(f"The sum of {a} + {b} is {add(a, b)}")
