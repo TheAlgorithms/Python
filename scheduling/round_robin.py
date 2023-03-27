4f1efadc320bca8f8d3d719fe1fa825a84e8135e
muzakkirsaifi123 <mohd.saifi@knoldus.com>
2023-03-27 16:11:07 +0530

    add a loader

--- scheduling/round_robin.py
+++ scheduling/round_robin.py
@@ -4,8 +4,78 @@ In Round Robin each process is assigned a fixed time slot in a cyclic way.
 https://en.wikipedia.org/wiki/Round-robin_scheduling
 """
 from __future__ import annotations
-
 from statistics import mean
+import time
+from itertools import cycle
+from shutil import get_terminal_size
+from threading import Thread
+
+
+print("This is Round Robin Algorithm")
+
+class Loading:
+    def __init__(self, desc="processing..", points="Done..", timeout=0.2):
+        self.desc = desc
+        self.end = points
+        self.timeout = timeout
+
+        self.starting = Thread(target=self.animation_start)
+        self.steps = [
+            "⢿",
+            "....",
+            "⣻",
+            "....",
+            "⣽",
+            "....",
+            "⣾",
+            "....",
+            "⣷",
+            "....",
+            "⣯",
+            "....",
+            "⣟",
+            "....",
+            "⡿",
+            "....",
+        ]
+        self.done = False
+
+    def start(self):
+        self.starting.start()
+        return self
+
+    def animation_start(self):
+        for i in cycle(self.steps):
+            if self.done:
+                break
+            print(f"\r{self.desc} {i}", flush=True, end="")
+            time.sleep(self.timeout)
+
+    def __enter__(self):
+        self.start()
+
+    def stop(self):
+        self.done = True
+        cols = get_terminal_size((70, 30)).columns
+        print("\r " + "  " * cols, end="", flush=True)
+        print(f"\r{self.end}", flush=True)
+
+    def __exit__(self, tb, exc_value, exc_type):
+        self.stop()
+
+
+if __name__ == "__main__":
+    with Loading("Loading..."):
+        for i in range(3):
+            time.sleep(0.25)
+
+    processing = Loading(
+        "Please wait, Processing..", "oohhh!!, That was too fast!", 0.05
+    ).start()
+    for i in range(3):
+        time.sleep(0.25)
+
+    processing.stop()
 
 
 def calculate_waiting_times(burst_times: list[int]) -> list[int]:
