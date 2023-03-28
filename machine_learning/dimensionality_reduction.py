3d8c1beb2cb98b1c389318e4faad2231ac452d3a
Diego <diego.gasco99@gmail.com>
2023-03-28 22:13:00 +0200

    Added a TODO list

--- machine_learning/dimensionality_reduction.py
+++ machine_learning/dimensionality_reduction.py
@@ -3,15 +3,19 @@ import logging
 import numpy as np
 import scipy
 
+# TODO update documentation
+# TODO add comments
+# TODO update variable names
+
 
 def column_reshape(input_array: np.ndarray) -> np.ndarray:
-    """Function to reshape a row Numpy array into a column Numpy array."""
+    """Function to reshape a row Numpy array into a column Numpy array"""
 
     return input_array.reshape((input_array.size, 1))
 
 
 def covariance_within_classes(features: np.ndarray, labels: np.ndarray, classes: int) -> np.ndarray:
-    """Function to compute the covariance matrix inside each class."""
+    """Function to compute the covariance matrix inside each class"""
 
     covariance_sum = None
     for i in range(classes):
@@ -27,7 +31,7 @@ def covariance_within_classes(features: np.ndarray, labels: np.ndarray, classes:
 
 
 def covariance_between_classes(features: np.ndarray, labels: np.ndarray, classes: int) -> np.ndarray:
-    """Function to compute the covariance matrix between multiple classes."""
+    """Function to compute the covariance matrix between multiple classes"""
 
     general_data_mean = features.mean(1)
     covariance_sum = None
@@ -37,10 +41,10 @@ def covariance_between_classes(features: np.ndarray, labels: np.ndarray, classes
         data_mean = data.mean(1)
         if covariance_sum:
             covariance_sum += device_data * np.dot((column_reshape(data_mean) - column_reshape(general_data_mean),
-                                                     (column_reshape(data_mean) - column_reshape(general_data_mean)).T))
+                                                    (column_reshape(data_mean) - column_reshape(general_data_mean)).T))
         else:
             covariance_sum = device_data * np.dot((column_reshape(data_mean) - column_reshape(general_data_mean),
-                                             (column_reshape(data_mean) - column_reshape(general_data_mean)).T))
+                                                   (column_reshape(data_mean) - column_reshape(general_data_mean)).T))
 
     return covariance_sum / features.shape[1]
 
@@ -48,11 +52,10 @@ def covariance_between_classes(features: np.ndarray, labels: np.ndarray, classes
 class DimensionalityReduction:
     """Class to apply PCA and LDA techniques for the dataset dimensionality reduction.\n
     The data structures used are: \n
-    * self._features: a list of lists that contains all the packets features, where each packet has a list of features. \n
-    * self._labels: a list that contains the category labels (i.e. device labels) of each list of packet features. \n
-    * self._devices_number: an int value that specifies how many device models there are in the dataset. \n
-    * self._features_PCA: a Numpy array with the features mapped with PCA. \n
-    * self._features_LDA: a Numpy array with the features mapped with LDA."""
+    * self._features:  \n
+    * self._class_labels:  \n
+    * self._classes:  \n
+    * self._features_after_PCA: """
 
     def __init__(self, features: np.ndarray, class_labels: np.ndarray, classes: int):
         logging.basicConfig(level=logging.INFO, format='%(message)s')
@@ -62,7 +65,7 @@ class DimensionalityReduction:
         self._features_after_PCA = None
 
     def PCA(self, dimensions: int) -> np.ndarray:
-        """Principal Component Analysis with default filter parameter equal to 10."""
+        """Principal Component Analysis with default filter parameter equal to 10"""
 
         try:
             assert any(self._features) is True
@@ -83,7 +86,7 @@ class DimensionalityReduction:
         return projected_data
 
     def LDA(self, dimensions: int, pca_features=False) -> np.ndarray:
-        """Linear Discriminant Analysis with default filter parameter equal to 8."""
+        """Linear Discriminant Analysis with default filter parameter equal to 8"""
 
         try:
             if not pca_features:
@@ -97,8 +100,9 @@ class DimensionalityReduction:
                 logging.info("Linear Discriminant Analysis computed on original features")
             else:
                 assert self._features_after_PCA is not None
-                s, U = scipy.linalg.eigh(covariance_between_classes(self._features_after_PCA, self._class_labels, self._classes),
-                                         covariance_within_classes(self._features_after_PCA, self._class_labels, self._classes))
+                s, U = scipy.linalg.eigh(
+                    covariance_between_classes(self._features_after_PCA, self._class_labels, self._classes),
+                    covariance_within_classes(self._features_after_PCA, self._class_labels, self._classes))
                 W = U[:, ::-1][:, :dimensions]
                 UW, _, _ = np.linalg.svd(W)
                 U = UW[:, 0:dimensions]
