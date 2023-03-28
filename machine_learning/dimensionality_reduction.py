5476b7da71d520fbdbd2c13208c4db5d3b73e7e9
Diego <diego.gasco99@gmail.com>
2023-03-28 19:52:50 +0200

    Some bug fixies

--- machine_learning/dimensionality_reduction.py
+++ machine_learning/dimensionality_reduction.py
@@ -1,3 +1,4 @@
+import copy
 import logging
 import numpy as np
 import scipy
@@ -6,7 +7,7 @@ import scipy
 def column_reshape(input_array: np.ndarray) -> np.ndarray:
     """Function to reshape a row Numpy array into a column Numpy array."""
 
-    return v.reshape((input_array.size, 1))
+    return input_array.reshape((input_array.size, 1))
 
 
 def covariance_within_classes(features: np.ndarray, labels: np.ndarray, classes: int) -> np.ndarray:
@@ -18,11 +19,11 @@ def covariance_within_classes(features: np.ndarray, labels: np.ndarray, classes:
         data_mean = data.mean(1)
         centered_data = data - column_reshape(data_mean)
         if covariance_sum:
-            covariance_sum += + np.dot(centered_data, centered_data.T)
+            covariance_sum += np.dot(centered_data, centered_data.T)
         else:
             covariance_sum = np.dot(centered_data, centered_data.T)
 
-    return convariance_sum / features.shape[1]
+    return covariance_sum / features.shape[1]
 
 
 def covariance_between_classes(features: np.ndarray, labels: np.ndarray, classes: int) -> np.ndarray:
@@ -36,10 +37,10 @@ def covariance_between_classes(features: np.ndarray, labels: np.ndarray, classes
         data_mean = data.mean(1)
         if covariance_sum:
             covariance_sum += device_data * np.dot((column_reshape(data_mean) - column_reshape(general_data_mean),
-                                                     (column_reshape(data_mean) - column_reshape(general_data_mean)).T)
+                                                     (column_reshape(data_mean) - column_reshape(general_data_mean)).T))
         else:
             covariance_sum = device_data * np.dot((column_reshape(data_mean) - column_reshape(general_data_mean),
-                                             (column_reshape(data_mean) - column_reshape(general_data_mean)).T)
+                                             (column_reshape(data_mean) - column_reshape(general_data_mean)).T))
 
     return covariance_sum / features.shape[1]
 
@@ -58,45 +59,54 @@ class DimensionalityReduction:
         self._features = features
         self._class_labels = class_labels
         self._classes = classes
+        self._features_after_PCA = None
 
     def PCA(self, dimensions: int) -> np.ndarray:
         """Principal Component Analysis with default filter parameter equal to 10."""
 
         try:
             assert any(self._features) is True
-            data = np.array(self._features)
-            mu = data.mean(1)
-            centered_data = self._features - np.reshape(mu, (mu.size, 1))
-            covariance_matrix = np.dot(centered_data, centered_data.T) / data.shape[1]
+            data_mean = self._features.mean(1)
+            centered_data = self._features - np.reshape(data_mean, (data_mean.size, 1))
+            covariance_matrix = np.dot(centered_data, centered_data.T) / self._features.shape[1]
             s, U = np.linalg.eigh(covariance_matrix)
             # Take all the columns in the reverse order (-1), and then takes only the first columns
-            P = U[:, ::-1][:, 0:m]
+            P = U[:, ::-1][:, 0:dimensions]
             projected_data = np.dot(P.T, self._features)
-            logging.info("Principal Component Analysis computed.")
+            self._features_after_PCA = copy.deepcopy(projected_data)
+            logging.info("Principal Component Analysis computed")
         except AssertionError:
             logging.basicConfig(level=logging.ERROR, format='%(message)s', force=True)
-            logging.error("The features must be a not-empty list")
+            logging.error("Feature array is empty")
             raise AssertionError
 
         return projected_data
 
-    def LDA(self, dimensions: int) -> np.ndarray:
+    def LDA(self, dimensions: int, pca_features=False) -> np.ndarray:
         """Linear Discriminant Analysis with default filter parameter equal to 8."""
 
         try:
-            assert self._features_PCA is not None
-            data = np.array(self._features_PCA)
-            labels = np.array(self._labels)
-            s, U = scipy.linalg.eigh(covariance_between_classes(data, labels, self._devices_number),
-                                     covariance_within_classes(data, labels, self._devices_number))
-            W = U[:, ::-1][:, :m]
-            UW, _, _ = np.linalg.svd(W)
-            U = UW[:, 0:m]
-            projected_data = np.dot(U.T, self._features_PCA)
-            logging.info("Linear Discriminant Analysis computed.")
+            if not pca_features:
+                assert any(self._features) is True
+                s, U = scipy.linalg.eigh(covariance_between_classes(self._features, self._class_labels, self._classes),
+                                         covariance_within_classes(self._features, self._class_labels, self._classes))
+                W = U[:, ::-1][:, :dimensions]
+                UW, _, _ = np.linalg.svd(W)
+                U = UW[:, 0:dimensions]
+                projected_data = np.dot(U.T, self._features)
+                logging.info("Linear Discriminant Analysis computed on original features")
+            else:
+                assert self._features_after_PCA is not None
+                s, U = scipy.linalg.eigh(covariance_between_classes(self._features_after_PCA, self._class_labels, self._classes),
+                                         covariance_within_classes(self._features_after_PCA, self._class_labels, self._classes))
+                W = U[:, ::-1][:, :dimensions]
+                UW, _, _ = np.linalg.svd(W)
+                U = UW[:, 0:dimensions]
+                projected_data = np.dot(U.T, self._features)
+                logging.info("Linear Discriminant Analysis computed on features pre-processed with PCA")
         except AssertionError:
             logging.basicConfig(level=logging.ERROR, format='%(message)s', force=True)
-            logging.error("The features must be passed in PCA algorithm before!")
+            logging.error("Features array is empty!")
             raise AssertionError
 
         return projected_data
