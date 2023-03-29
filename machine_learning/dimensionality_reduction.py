24a68e935c9da302f29c1408dd6303a5bc9284df
Diego <diego.gasco99@gmail.com>
2023-03-29 16:46:10 +0200

    Finish code for dimensionality_reduction.py

--- machine_learning/dimensionality_reduction.py
+++ machine_learning/dimensionality_reduction.py
@@ -3,11 +3,6 @@ import logging
 import numpy as np
 import scipy
 
-# TODO update documentation
-# TODO add comments
-# TODO update variable names
-
-
 def column_reshape(input_array: np.ndarray) -> np.ndarray:
     """Function to reshape a row Numpy array into a column Numpy array"""
 
@@ -21,10 +16,13 @@ def covariance_within_classes(features: np.ndarray, labels: np.ndarray, classes:
     for i in range(classes):
         data = features[:, labels == i]
         data_mean = data.mean(1)
+        # Centralize the data of class i
         centered_data = data - column_reshape(data_mean)
         if covariance_sum:
+            # If covariance_sum is not None
             covariance_sum += np.dot(centered_data, centered_data.T)
         else:
+            # If covariance_sum is None (i.e. first loop)
             covariance_sum = np.dot(centered_data, centered_data.T)
 
     return covariance_sum / features.shape[1]
@@ -40,9 +38,11 @@ def covariance_between_classes(features: np.ndarray, labels: np.ndarray, classes
         device_data = data.shape[1]
         data_mean = data.mean(1)
         if covariance_sum:
+            # If covariance_sum is not None
             covariance_sum += device_data * np.dot((column_reshape(data_mean) - column_reshape(general_data_mean),
                                                     (column_reshape(data_mean) - column_reshape(general_data_mean)).T))
         else:
+            # If covariance_sum is None (i.e. first loop)
             covariance_sum = device_data * np.dot((column_reshape(data_mean) - column_reshape(general_data_mean),
                                                    (column_reshape(data_mean) - column_reshape(general_data_mean)).T))
 
@@ -52,10 +52,10 @@ def covariance_between_classes(features: np.ndarray, labels: np.ndarray, classes
 class DimensionalityReduction:
     """Class to apply PCA and LDA techniques for the dataset dimensionality reduction.\n
     The data structures used are: \n
-    * self._features:  \n
-    * self._class_labels:  \n
-    * self._classes:  \n
-    * self._features_after_PCA: """
+    * self._features: contains the features for each object as a matrix \n
+    * self._class_labels:  contains the labels associated with each object \n
+    * self._classes:  the number of classes in the dataset \n
+    * self._features_after_PCA: will contain the features mapped in a new space after PCA"""
 
     def __init__(self, features: np.ndarray, class_labels: np.ndarray, classes: int):
         logging.basicConfig(level=logging.INFO, format='%(message)s')
@@ -65,17 +65,20 @@ class DimensionalityReduction:
         self._features_after_PCA = None
 
     def PCA(self, dimensions: int) -> np.ndarray:
-        """Principal Component Analysis with default filter parameter equal to 10"""
+        """Principal Component Analysis with a certain filter parameter"""
 
         try:
+            # Check if the features have been loaded
             assert any(self._features) is True
             data_mean = self._features.mean(1)
+            # Center the dataset
             centered_data = self._features - np.reshape(data_mean, (data_mean.size, 1))
             covariance_matrix = np.dot(centered_data, centered_data.T) / self._features.shape[1]
-            s, U = np.linalg.eigh(covariance_matrix)
+            _, eigenvectors = np.linalg.eigh(covariance_matrix)
             # Take all the columns in the reverse order (-1), and then takes only the first columns
-            P = U[:, ::-1][:, 0:dimensions]
-            projected_data = np.dot(P.T, self._features)
+            filtered_eigenvectors = eigenvectors[:, ::-1][:, 0:dimensions]
+            # Project the database on the new space
+            projected_data = np.dot(filtered_eigenvectors.T, self._features)
             self._features_after_PCA = copy.deepcopy(projected_data)
             logging.info("Principal Component Analysis computed")
         except AssertionError:
@@ -86,27 +89,31 @@ class DimensionalityReduction:
         return projected_data
 
     def LDA(self, dimensions: int, pca_features=False) -> np.ndarray:
-        """Linear Discriminant Analysis with default filter parameter equal to 8"""
+        """Linear Discriminant Analysis with a certain filter parameter"""
 
         try:
             if not pca_features:
+                # Check if features have been already loaded
                 assert any(self._features) is True
-                s, U = scipy.linalg.eigh(covariance_between_classes(self._features, self._class_labels, self._classes),
-                                         covariance_within_classes(self._features, self._class_labels, self._classes))
-                W = U[:, ::-1][:, :dimensions]
-                UW, _, _ = np.linalg.svd(W)
-                U = UW[:, 0:dimensions]
-                projected_data = np.dot(U.T, self._features)
+                _, eigenvectors = scipy.linalg.eigh(
+                    covariance_between_classes(self._features, self._class_labels, self._classes),
+                    covariance_within_classes(self._features, self._class_labels, self._classes))
+                filtered_eigenvectors = eigenvectors[:, ::-1][:, :dimensions]
+                svd_matrix, _, _ = np.linalg.svd(filtered_eigenvectors)
+                filtered_svd_matrix = svd_matrix[:, 0:dimensions]
+                projected_data = np.dot(filtered_svd_matrix.T, self._features)
                 logging.info("Linear Discriminant Analysis computed on original features")
             else:
+                # Check if features mapped on PCA have been already loaded
                 assert self._features_after_PCA is not None
-                s, U = scipy.linalg.eigh(
+                _, eigenvectors = scipy.linalg.eigh(
                     covariance_between_classes(self._features_after_PCA, self._class_labels, self._classes),
                     covariance_within_classes(self._features_after_PCA, self._class_labels, self._classes))
-                W = U[:, ::-1][:, :dimensions]
-                UW, _, _ = np.linalg.svd(W)
-                U = UW[:, 0:dimensions]
-                projected_data = np.dot(U.T, self._features)
+                filtered_eigenvectors = eigenvectors[:, ::-1][:, :dimensions]
+                svd_matrix, _, _ = np.linalg.svd(filtered_eigenvectors)
+                svd_matrix_filtered = svd_matrix[:, 0:dimensions]
+                # Project the database on the new space
+                projected_data = np.dot(svd_matrix_filtered.T, self._features)
                 logging.info("Linear Discriminant Analysis computed on features pre-processed with PCA")
         except AssertionError:
             logging.basicConfig(level=logging.ERROR, format='%(message)s', force=True)
