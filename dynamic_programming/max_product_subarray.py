5437ee3fdd2e43990f6a9b95a29842ada98a5aa9
Christian Clauss <cclauss@me.com>
2023-03-31 13:12:05 +0200

    Update max_product_subarray.py

--- dynamic_programming/max_product_subarray.py
+++ dynamic_programming/max_product_subarray.py
@@ -1,4 +1,4 @@
-def max_product_subarray(numbers: list[float]) -> float:
+def max_product_subarray(numbers: list[int]) -> int:
     """
     Returns the maximum product that can be obtained by multiplying a
     contiguous subarray of the given integer list `nums`.
@@ -6,32 +6,33 @@ def max_product_subarray(numbers: list[float]) -> float:
     Example:
     >>> max_product_subarray([2, 3, -2, 4])
     6
-    >>> max_product_subarray([-2, 0, -1])
+    >>> max_product_subarray((-2, 0, -1))
     0
     >>> max_product_subarray([2, 3, -2, 4, -1])
     48
-    >>> max_product_subarray([2, 3, -2, 4.5, -1])
-    Traceback (most recent call last):
-        ...
-    ValueError: numbers must be an iterable of integers
     >>> max_product_subarray([-1])
     -1
-    >>> max_product_subarray([])
+    >>> max_product_subarray([0])
     0
-    >>> max_product_subarray("ABC")
+    >>> max_product_subarray([])
     0
     >>> max_product_subarray("")
     0
     >>> max_product_subarray(None)
     0
+    >>> max_product_subarray([2, 3, -2, 4.5, -1])
+    Traceback (most recent call last):
+        ...
+    ValueError: numbers must be an iterable of integers
+    >>> max_product_subarray("ABC")
+    Traceback (most recent call last):
+        ...
+    ValueError: numbers must be an iterable of integers
     """
-    if not isinstance(numbers, list):
+    if not numbers:
         return 0
 
-    if len(numbers) == 0:
-        return 0
-
-    if not isinstance(numbers, (list, set, tuple)) or not all(
+    if not isinstance(numbers, (list, tuple)) or not all(
         isinstance(number, int) for number in numbers
     ):
         raise ValueError("numbers must be an iterable of integers")
