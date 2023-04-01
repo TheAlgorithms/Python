d65430e65619226fd362a634b5457d4e77eebb5c
Christian Clauss <cclauss@me.com>
2023-04-01 17:08:43 +0200

    Fix

--- dynamic_programming/minimum_size_subarray_sum.py
+++ dynamic_programming/minimum_size_subarray_sum.py
@@ -5,34 +5,39 @@ def minimum_subarray_sum(target: int, numbers: list[int]) -> int:
     """
     Returns the length of the shortest contiguous subarray
      in a list of numbers whose sum is at least target.
-        >>> minimum_subarray_sum(7, [2, 3, 1, 2, 4, 3])
-        2
-        >>> minimum_subarray_sum(7, [2, 3, -1, 2, 4, -3])
-        4
-        >>> minimum_subarray_sum(11, [1, 1, 1, 1, 1, 1, 1, 1])
-        0
-        >>> minimum_subarray_sum(10, [1, 2, 3, 4, 5, 6, 7])
-        2
-        >>> minimum_subarray_sum(5, [1, 1, 1, 1, 1, 5])
-        1
-        >>> minimum_subarray_sum(0, [])
-        0
-        >>> minimum_subarray_sum(10, [10, 20, 30])
-        1
-        >>> minimum_subarray_sum(7, [1, 1, 1, 1, 1, 1, 10])
-        1
-        >>> minimum_subarray_sum(6, [])
-        0
-        >>> minimum_subarray_sum(2, [1, 2, 3])
-        1
-        >>> minimum_subarray_sum(-6, [])
-        0
-        >>> minimum_subarray_sum(8, None)
-        0
-        >>> minimum_subarray_sum(2, "ABC")
-        Traceback (most recent call last):
+     
+    >>> minimum_subarray_sum(7, [2, 3, 1, 2, 4, 3])
+    2
+    >>> minimum_subarray_sum(7, [2, 3, -1, 2, 4, -3])
+    4
+    >>> minimum_subarray_sum(11, [1, 1, 1, 1, 1, 1, 1, 1])
+    0
+    >>> minimum_subarray_sum(10, [1, 2, 3, 4, 5, 6, 7])
+    2
+    >>> minimum_subarray_sum(5, [1, 1, 1, 1, 1, 5])
+    1
+    >>> minimum_subarray_sum(0, [])
+    0
+    >>> minimum_subarray_sum(0, [1, 2, 3])
+    0
+    >>> minimum_subarray_sum(10, [10, 20, 30])
+    1
+    >>> minimum_subarray_sum(7, [1, 1, 1, 1, 1, 1, 10])
+    1
+    >>> minimum_subarray_sum(6, [])
+    0
+    >>> minimum_subarray_sum(2, [1, 2, 3])
+    1
+    >>> minimum_subarray_sum(-6, [])
+    0
+    >>> minimum_subarray_sum(-6, [3, 4, 5])
+    0
+    >>> minimum_subarray_sum(8, None)
+    0
+    >>> minimum_subarray_sum(2, "ABC")
+    Traceback (most recent call last):
         ...
-        ValueError: numbers must be an iterable of integers
+    ValueError: numbers must be an iterable of integers
     """
 
     if not numbers:
@@ -41,9 +46,9 @@ def minimum_subarray_sum(target: int, numbers: list[int]) -> int:
         isinstance(number, int) for number in numbers
     ):
         raise ValueError("numbers must be an iterable of integers")
+
     left = right = curr_sum = 0
     min_len = sys.maxsize
-
     for right, number in enumerate(numbers):
         curr_sum += number
         while curr_sum >= target:
@@ -52,3 +57,9 @@ def minimum_subarray_sum(target: int, numbers: list[int]) -> int:
             left += 1
 
     return 0 if min_len == sys.maxsize else min_len
+
+
+if __name__ == "__main__":
+    from doctest import testmod
+    
+    testmod()()
