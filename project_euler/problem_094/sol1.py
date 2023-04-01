3376a25c8be39a7505f7d9bb8b70911fd4f7eb72
MaximSmolskiy <mithridatus@mail.ru>
2023-04-01 14:34:57 +0300

    Fix

--- project_euler/problem_094/sol1.py
+++ project_euler/problem_094/sol1.py
@@ -25,20 +25,19 @@ def solution(max_perimeter: int = 10**9) -> int:
     prev_value = 1
     value = 2
 
-    sum = 0
+    perimeters_sum = 0
     i = 0
     perimeter = 0
     while perimeter <= max_perimeter:
-        sum += perimeter
+        perimeters_sum += perimeter
 
         prev_value += 2 * value
         value += prev_value
 
         perimeter = 2 * value + 2 if i % 2 == 0 else 2 * value - 2
-
         i += 1
 
-    return sum
+    return perimeters_sum
 
 
 if __name__ == "__main__":
