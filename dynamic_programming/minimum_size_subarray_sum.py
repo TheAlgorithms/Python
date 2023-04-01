ae1996c167fc7690033d3afd0089100c2804a85d
Rohan Anand <96521078+rohan472000@users.noreply.github.com>
2023-04-01 23:13:23 +0530

    Update minimum_size_subarray_sum.py

--- dynamic_programming/minimum_size_subarray_sum.py
+++ dynamic_programming/minimum_size_subarray_sum.py
@@ -39,8 +39,9 @@ def minimum_subarray_sum(target: int, numbers: list[int]) -> int:
         ...
     ValueError: numbers must be an iterable of integers
     """
-
     if not numbers:
+        return 0
+    if target == 0 and target in numbers:
         return 0
     if not isinstance(numbers, (list, tuple)) or not all(
         isinstance(number, int) for number in numbers
@@ -49,11 +50,13 @@ def minimum_subarray_sum(target: int, numbers: list[int]) -> int:
 
     left = right = curr_sum = 0
     min_len = sys.maxsize
-    for right, number in enumerate(numbers):
-        curr_sum += number
-        while curr_sum >= target:
+    
+    while right < len(numbers):
+        curr_sum += numbers[right]
+        while curr_sum >= target and left <= right:
             min_len = min(min_len, right - left + 1)
             curr_sum -= numbers[left]
             left += 1
+        right += 1
 
     return 0 if min_len == sys.maxsize else min_len
