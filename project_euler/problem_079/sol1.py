4a1f7306aeca7939cb5fbae5f6e2b2d878beb1d9
Ishab Ahmed <ishabahmed00@gmail.com>
2023-04-02 12:03:37 +0100

    Updated to follow repo standards

--- project_euler/problem_079/sol1.py
+++ project_euler/problem_079/sol1.py
@@ -12,24 +12,29 @@ The text file, keylog.txt, contains fifty successful login attempts.
 
 Given that the three characters are always asked for in order, analyse the file
 so as to determine the shortest possible secret passcode of unknown length.
-...
-
 """
 import itertools
 import os
 
 
-def solution() -> int:
+def find_secret_passcode(logins: list[str]) -> int:
     """
     Returns the shortest possible secret passcode of unknown length.
 
-    >>> solution()
-    73162890
-    """
-    with open(os.path.dirname(__file__) + "/p079_keylog.txt") as file:
-        logins = [tuple(line.strip()) for line in file]
+    >>> find_secret_passcode(["319", "680", "180", "690", "129", "620"])
+    6312980
 
-    unique_chars = {char for login in logins for char in login}
+    >>> find_secret_passcode(["135", "259", "235", "189", "690", "168", "120"])
+    132568940
+
+    >>> find_secret_passcode(["426", "281", "061", "819" "268", "406", "420"])
+    4206819
+    """
+
+    # Split each login by character e.g. '319' -> ('3', '1', '9')
+    split_logins = [tuple(login) for login in logins]
+
+    unique_chars = {char for login in split_logins for char in login}
 
     for permutation in itertools.permutations(unique_chars):
         satisfied = True
@@ -46,7 +51,18 @@ def solution() -> int:
             return int("".join(permutation))
             break
 
-    return 0
+    raise Exception("Unable to find the secret passcode")
+
+
+def solution() -> int:
+    """
+    Returns the shortest possible secret passcode of unknown length
+    for successful login attempts given by keylog.txt.
+    """
+    with open(os.path.dirname(__file__) + "/keylog.txt") as file:
+        logins = file.read().splitlines()
+
+    return find_secret_passcode(logins)
 
 
 if __name__ == "__main__":
