174ce08c731b4254a42f3ab5bea854c10c2f5caa
Isidro Arias <isidroariass@hotmail.es>
2023-04-08 17:01:16 +0200

    syntax error in dict comprehension

--- data_structures/hashing/bloom_filter.py
+++ data_structures/hashing/bloom_filter.py
@@ -30,8 +30,8 @@ because both hash functions return the same value
 
 Not added elements should return False ...
 >>> not_present_films = ("The Goodfather", "Interstellar", "Parasite", "Pulp Fiction")
->>> {film: bloom.format_hash(film) for film in not_present_films)}
-{'The Goodfather': '00011000', 'Interstellar': '00000011', 'Parasite': '00010010': 'Pulp Fiction': '10000100'}
+>>> {film: bloom.format_hash(film) for film in not_present_films}
+{'The Goodfather': '00011000', 'Interstellar': '00000011', 'Parasite': '00010010', 'Pulp Fiction': '10000100'}
 >>> any(film in bloom for film in not_present_films)
 False
 
