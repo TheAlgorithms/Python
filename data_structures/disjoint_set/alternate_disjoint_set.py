963905e5bdf2925bf8bba9f719bc2f50fd0568b6
Sukruti Mallesh <sukrutimallesh@gmail.com>
2023-04-08 21:58:29 -0700

    Changed list to dictionary

--- data_structures/disjoint_set/alternate_disjoint_set.py
+++ data_structures/disjoint_set/alternate_disjoint_set.py
@@ -2,6 +2,17 @@ class DisjointSet:
     def __init__(self, set_sizes):
         """
         Initialize a disjoint set data structure with set sizes.
+
+        Examples:
+        >>> d = DisjointSet([1, 2, 3, 4])
+        >>> d.find(0)
+        0
+        >>> d.find(1)
+        1
+        >>> d.find(2)
+        2
+        >>> d.find(3)
+        3
         """
         self.set_sizes = set_sizes
         self.parents = {i: i for i in range(len(set_sizes))}
@@ -10,6 +21,27 @@ class DisjointSet:
     def find(self, x):
         """
         Find the representative of the set containing x, with path compression.
+
+        Examples:
+        >>> d = DisjointSet([1, 2, 3, 4])
+        >>> d.find(0)
+        0
+        >>> d.union(0, 1)
+        True
+        >>> d.find(1)
+        0
+        >>> d.find(0)
+        0
+        >>> d.union(2, 3)
+        True
+        >>> d.find(2)
+        2
+        >>> d.union(1, 2)
+        True
+        >>> d.find(1)
+        0
+        >>> d.find(2)
+        0
         """
         if self.parents[x] != x:
             self.parents[x] = self.find(self.parents[x])
@@ -18,6 +50,21 @@ class DisjointSet:
     def union(self, x, y):
         """
         Merge the sets containing x and y using union by rank.
+
+        Examples:
+        >>> d = DisjointSet([1, 2, 3, 4])
+        >>> d.union(0, 1)
+        True
+        >>> d.find(0) == d.find(1)
+        True
+        >>> d.union(2, 3)
+        True
+        >>> d.find(2) == d.find(3)
+        True
+        >>> d.union(1, 2)
+        True
+        >>> d.find(0) == d.find(1) == d.find(2) == d.find(3)
+        True
         """
         x_root = self.find(x)
         y_root = self.find(y)
