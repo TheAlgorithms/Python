c6aeeb28adbaf67054f335a7d0f5a40c5a0a2336
R Shashank Singh <125629580+sh4shankk@users.noreply.github.com>
2023-04-15 22:05:46 +0530

    Update tribonacci_number.py

--- dynamic_programming/tribonacci_number.py
+++ dynamic_programming/tribonacci_number.py
@@ -1,18 +1,32 @@
 '''
-This program calculate N-th Tribonacci Number 
-for better understanding of tribonacci number 
+This program calculate N-th Tribonacci Number.
+For better understanding of tribonacci number 
 go through https://brilliant.org/wiki/tribonacci-sequence/
 '''
-class Tribonacci:
-    def tribonacci(self, n: int) -> int:
-        prev1, prev2, prev3 = 1, 1, 0
-        if n == 0: return prev3
-        if n == 1: return prev2
-        if n == 2: return prev1
-        result = 0
-        for _ in range(3, n + 1):
-            result = prev1 + prev2 + prev3
-            prev3 = prev2
-            prev2 = prev1
-            prev1 = result
-        return result
+#'n' refers to the desired n-th tribonacci number
+def tribonacci(n: int) -> int:
+    '''
+    This program takes n integer as user input which refers to the
+    desired n-th tribonacci number. Generally, the Tribonacci series
+    is initialize as 0, 1, 1......so on
+    '''
+    
+    #initializing first three tribonacci numbers
+    prev1, prev2, prev3 = 1, 1, 0         
+    
+    #evaluating edge cases
+    if n == 0: return prev3
+    if n == 1: return prev2
+    if n == 2: return prev1
+    
+    #initialzing required_tribo_num as 0 
+    required_tribo_num = 0
+    
+    #looping will start from 3 as previous numbers are evaluated above
+    for _ in range(3, n + 1):
+        result = prev1 + prev2 + prev3
+        prev3 = prev2
+        prev2 = prev1
+        prev1 = required_tribo_num
+        
+    return required_tribo_num
