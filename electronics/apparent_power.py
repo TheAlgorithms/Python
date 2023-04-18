8063be2a75de8d22f5f609c1ec8fa7e8a93407b0
Rohan Anand <96521078+rohan472000@users.noreply.github.com>
2023-04-18 10:15:20 +0530

    Update apparent_power.py

--- electronics/apparent_power.py
+++ electronics/apparent_power.py
@@ -13,9 +13,9 @@ def apparent_power(
     >>> apparent_power(100, 5, 90, 0)
     (3.061616997868383e-14+500j)
     >>> apparent_power(100, 5, -45, -60)
-    (-129.40952255126032-482.9629131445341j)
+    (-129.40952255126027-482.9629131445341j)
     >>> apparent_power(200, 10, -30, -90)
-    (-999.9999999999998-1732.0508075688776j)
+    (-999.999999999-1732.0508075688j)
     """
     # Convert angles from degrees to radians
     voltage_angle_rad = math.radians(voltage_angle)
