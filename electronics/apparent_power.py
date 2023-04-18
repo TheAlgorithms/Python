b5047cfa114c6343b92370419772b9cf0f13e634
Rohan Anand <96521078+rohan472000@users.noreply.github.com>
2023-04-18 13:00:01 +0530

    Create apparent_power.py (#8664)
    * Create apparent_power.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update apparent_power.py
    
    * Update apparent_power.py
    
    * Update apparent_power.py
    
    * Update electronics/apparent_power.py
    
    Co-authored-by: Christian Clauss <cclauss@me.com>
    
    * Update electronics/apparent_power.py
    
    Co-authored-by: Christian Clauss <cclauss@me.com>
    
    * Update apparent_power.py
    
    * Update electronics/apparent_power.py
    
    Co-authored-by: Christian Clauss <cclauss@me.com>
    
    * Update apparent_power.py
    
    * Update apparent_power.py
    
    * Update apparent_power.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update apparent_power.py
    
    * Update apparent_power.py
    
    * Update apparent_power.py
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Christian Clauss <cclauss@me.com>

---
+++ electronics/apparent_power.py
@@ -0,0 +1,35 @@
+import cmath
+import math
+
+
+def apparent_power(
+    voltage: float, current: float, voltage_angle: float, current_angle: float
+) -> complex:
+    """
+    Calculate the apparent power in a single-phase AC circuit.
+
+    >>> apparent_power(100, 5, 0, 0)
+    (500+0j)
+    >>> apparent_power(100, 5, 90, 0)
+    (3.061616997868383e-14+500j)
+    >>> apparent_power(100, 5, -45, -60)
+    (-129.40952255126027-482.9629131445341j)
+    >>> apparent_power(200, 10, -30, -90)
+    (-999.9999999999998-1732.0508075688776j)
+    """
+    # Convert angles from degrees to radians
+    voltage_angle_rad = math.radians(voltage_angle)
+    current_angle_rad = math.radians(current_angle)
+
+    # Convert voltage and current to rectangular form
+    voltage_rect = cmath.rect(voltage, voltage_angle_rad)
+    current_rect = cmath.rect(current, current_angle_rad)
+
+    # Calculate apparent power
+    return voltage_rect * current_rect
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
