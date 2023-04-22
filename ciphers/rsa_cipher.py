de1e7bb7fc7512704e9b5ef68942d374be758b8e
Rohan Anand <96521078+rohan472000@users.noreply.github.com>
2023-04-22 20:51:27 +0530

    Update rsa_cipher.py

--- ciphers/rsa_cipher.py
+++ ciphers/rsa_cipher.py
@@ -79,7 +79,7 @@ def encrypt_and_write_to_file(
             "ERROR: Block size is {} bits and key size is {} bits. The RSA cipher "
             "requires the block size to be equal to or greater than the key size. "
             "Either decrease the block size or use different keys."
-            % (block_size * 8, key_size)
+            .format(block_size * 8, key_size)
         )
 
     encrypted_blocks = [str(i) for i in encrypt_message(message, (n, e), block_size)]
@@ -104,7 +104,7 @@ def read_from_file_and_decrypt(message_filename: str, key_filename: str) -> str:
             "ERROR: Block size is {} bits and key size is {} bits. The RSA cipher "
             "requires the block size to be equal to or greater than the key size. "
             "Did you specify the correct key file and encrypted file?"
-            % (block_size * 8, key_size)
+            .format(block_size * 8, key_size)
         )
 
     encrypted_blocks = []
