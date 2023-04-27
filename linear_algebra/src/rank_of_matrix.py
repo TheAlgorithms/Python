1d026c9040347d78e822bda3e0b0651c4b866653
Rudransh Bhardwaj <115872354+rudransh61@users.noreply.github.com>
2023-04-27 09:38:27 +0530

    added more doctests in rank_of_matrix.py

--- linear_algebra/src/rank_of_matrix.py
+++ linear_algebra/src/rank_of_matrix.py
@@ -1,6 +1,4 @@
 """This is a python program that calculates the rank of a matrix"""
-""" BY - RUDRANSH BHARDWAJ"""
-
 
 def rank_of_matrix(matrix: list[list]) -> int:
     """
@@ -25,6 +23,22 @@ def rank_of_matrix(matrix: list[list]) -> int:
     ...            [9, 10, 11, 12]]
     >>> rank_of_matrix(matrix3)
     2
+    >>>rank_of_matrix([[2,3,-1,-1],
+    ...                [1,-1,-2,4],
+    ...                [3,1,3,-2],
+    ...                [6,3,0,-7]])
+    3
+    >>>rank_of_matrix([[2,1,-3,-6],
+    ...                [3,-3,1,2],
+    ...                [1,1,1,2]])
+    2
+    >>>rank_of_matrix([[2,-1,0],
+    ...                [1,3,4],
+    ...                [4,1,-3]])
+    3
+    >>>rank_of_matrix([[3,2,1],
+    ...                [-6,-4,-2]])
+    1
     """
 
     rows = len(matrix)
