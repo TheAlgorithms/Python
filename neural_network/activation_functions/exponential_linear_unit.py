dad8cf18b661d7b7d0e84b3dc623960c75b6628b
Mitra-babu <dipankar9612@gmail.com>
2023-04-28 19:50:26 +0530

    Exponential_linear_unit activation is added

--- neural_network/activation_functions/exponential_linear_unit.py
+++ neural_network/activation_functions/exponential_linear_unit.py
@@ -15,18 +15,18 @@ def exponential_linear_unit(vector: np.ndarray, alpha: float) -> np.ndarray:
     """
          Implements the ELU activation function.
          Parameters:
-             vector: np.array
-             alpha: float
+             vector: the array containing input of elu activation
+             alpha: hyper-parameter
          return:
          elu (np.array): The input numpy array after applying elu.
 
          Mathematically, f(x) = x, x>0 else (alpha * (e^x -1)), x<=0, alpha >=0
 
     Examples:
-         >>> elu_activation(vector=np.array([2.3,0.6,-2,-3.8,9]), alpha=0.3)
+         >>> exponential_linear_unit(vector=np.array([2.3,0.6,-2,-3.8,9]), alpha=0.3)
          array([ 2.3       ,  0.6       , -0.25939942, -0.29328877,  9.        ])
 
-         >>> elu_activation(vector=np.array([-9.2,-0.3,-2.45,0.45]), alpha=0.067)
+         >>> exponential_linear_unit(vector=np.array([-9.2,-0.3,-2.45,0.45]), alpha=0.067)
          array([-0.06699323, -0.01736518, -0.06121833,  0.45      ])
 
     """
