87f69477d1bb831d746d0b6496f2b6bab1e49b99
Sahil Goel <sahilgoel2013@yahoo.com>
2023-04-30 11:55:13 -0400

    Make improvements based on Rohan's suggestions

--- financial/present_value.py
+++ financial/present_value.py
@@ -9,26 +9,34 @@
 
 def present_value(discount_rate: float, cash_flows: list[float]) -> float:
     """
-    >>> round(present_value(0.13, [10, 20.70, -293, 297]), 2)
+    >>> present_value(0.13, [10, 20.70, -293, 297])
     4.69
-    >>> round(present_value(0.07, [-109129.39, 30923.23, 15098.93, 29734,39]), 2)
+    >>> present_value(0.07, [-109129.39, 30923.23, 15098.93, 29734,39])
     -42739.63
-    >>> round(present_value(0.07, [109129.39, 30923.23, 15098.93, 29734,39]), 2)
+    >>> present_value(0.07, [109129.39, 30923.23, 15098.93, 29734,39])
     175519.15
     >>> present_value(-1, [109129.39, 30923.23, 15098.93, 29734,39])
     Traceback (most recent call last):
         ...
-    ValueError: Invalid discount rate, please choose a rate other than -1
+    ValueError: Discount rate cannot be negative
+    >>> present_value(0.03, [])
+    Traceback (most recent call last):
+        ...
+    ValueError: Cash flows list cannot be empty
     """
     present_value = 0.0
 
-    if discount_rate == -1:
-        raise ValueError("Invalid discount rate, please choose a rate other than -1")
+    if discount_rate < 0:
+        raise ValueError("Discount rate cannot be negative")
+    
+    if not cash_flows:
+        raise ValueError("Cash flows list cannot be empty")
 
     for idx, cash_flow in enumerate(cash_flows):
         present_value += cash_flow / ((1 + discount_rate) ** idx)
 
-    return present_value
+    decimal_places = 2
+    return round(present_value, decimal_places)
 
 
 if __name__ == "__main__":
