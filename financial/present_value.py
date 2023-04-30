0299cfbbafbc0ec897640bd82b78e9b2da5945b3
Sahil Goel <55365655+sahilg13@users.noreply.github.com>
2023-04-30 13:15:42 -0400

    Update financial/present_value.py
    Committed third suggestion
    
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- financial/present_value.py
+++ financial/present_value.py
@@ -33,8 +33,7 @@ def present_value(discount_rate: float, cash_flows: list[float]) -> float:
 
     present_value = sum(cash_flow / ((1 + discount_rate) ** i) for i, cash_flow in enumerate(cash_flows))
 
-    decimal_places = 2
-    return round(present_value, decimal_places)
+    return round(present_value, ndigits=2)
 
 
 if __name__ == "__main__":
