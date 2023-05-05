522328afd6906d08b44129b942fbf7ca64da3104
NiyazUrazaev <niyazurazaev@gmail.com>
2023-05-05 16:53:05 +0300

    Fix after review

--- web_programming/telegram_message.py
+++ web_programming/telegram_message.py
@@ -1,18 +1,19 @@
 # Created by NiyazUrazaev on 03/05/23
+import os
 
 import requests
 
+BOT_TOKEN = os.getenv("YOUR_BOT_TOKEN", "")
 
 def send_message_to_telegram(token: str, chat_id: str, message_text: str):
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     data = {"chat_id": chat_id, "text": message_text}
     response = requests.post(url, data=data)
     if not response.ok:
-        print("Failed to send a message in Telegram:", response.text)
+        raise Exception(f"Failed to send a message in Telegram: {response.text}")
 
 
 if __name__ == "__main__":
-    token = "<YOUR BOT TOKEN>"
     chat_id = "<YOUR CHANNEL ID>"
     message_text = "<YOUR MESSAGE BODY>"
-    send_message_to_telegram(token, chat_id, message_text)
+    send_message_to_telegram(BOT_TOKEN, chat_id, message_text)
