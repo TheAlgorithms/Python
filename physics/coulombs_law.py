2387620f6a980e013c68c6906ac88d5846ee81ab
gudlu1925 <120262240+gudlu1925@users.noreply.github.com>
2023-05-09 14:32:47 +0530

    Update coulombs_law.py

--- physics/coulombs_law.py
+++ physics/coulombs_law.py
@@ -1,13 +1,12 @@
 """
-Description : The law states that the magnitude of the electrostatic force of attraction or repulsion between two point charges is directly proportional
-to the product of the magnitudes of charges and inversely proportional to the square of the distance between them.
+The law states that the magnitude of the electrostatic force of attraction or repulsion between two point charges 
+is directly proportional to the product of the magnitudes of charges and inversely proportional to the square of the distance between them.
+
 Coulomb studied the repulsive force between bodies having electrical charges of the same sign.
 
 F  = k*q1*q2/ r^2
 
-where
-
-k is proportionality constant and equals to 1/4πε0.
+k is proportionality constant and equals 1/4πε0.
 q1 is charge of first body (C)
 q2 is charge of second body (C)
 r is distance between two charged bodies (m)
