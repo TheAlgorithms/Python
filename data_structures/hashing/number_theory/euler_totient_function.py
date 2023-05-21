2af5291db2dc051e34fae3451102fae7e7ba0ca0
k3x9 <97793590+k3x9@users.noreply.github.com>
2023-05-21 11:18:25 +0530

    Created euler_totient_function.py
    Euler totient function return counts of relative prime numbers of n that lie between 1 to n.

---
+++ data_structures/hashing/number_theory/euler_totient_function.py
@@ -0,0 +1,47 @@
+def euler_totient_function(number: int) -> int:
+    """
+    Euler's totient function counts the positive integers up to a given integer n
+    that are relatively prime to n.
+
+    Relative prime numbers are numbers that have no common divisors other than 1.
+
+    >>> euler_totient_function(1)
+    1
+    >>> euler_totient_function(2)
+    1
+    >>> euler_totient_function(6)
+    2
+    >>> euler_totient_function(12)
+    4
+    >>> euler_totient_function(8)
+    4
+    >>> euler_totient_function(18)
+    6
+    >>> euler_totient_function(35)
+    24
+    >>> euler_totient_function(100)
+    40
+
+    """
+
+    if int(number) != number or number < 1:
+        raise ValueError("n must be a positive integer greater than 0")
+
+    count = number
+    X = 2
+    while X * X <= number:
+        if number % X == 0:
+            count -= count // X
+            while number % X == 0:
+                number //= X
+        X += 1
+
+    if number >= 2:
+        count -= count // number
+    return count
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
