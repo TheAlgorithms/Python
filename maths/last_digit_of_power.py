ef5fd3658b9d53c55842322030941b6bdd83e0a9
Margaret <62753112+meg-1@users.noreply.github.com>
2023-05-27 15:22:50 +0300

    adding a last_digit_of_power algorithm

---
+++ maths/last_digit_of_power.py
@@ -0,0 +1,16 @@
+def last_digit_of_pow(number1: int, number2: int) -> int:
+
+    """
+    >>> last_digit_of_pow(63, 6)
+    9
+    >>> last_digit_of_pow(724, 67521)
+    4
+    >>> last_digit_of_pow(230912350294580923559235802932358, 2034982093849324290348029429391)
+    2
+    """
+
+    return pow(number1, number2, 10)
+
+
+if __name__ == "__main__":
+    __import__("doctest").testmod()
