aca385c30dd11d077bbf963becf4c385a382e92b
Margaret <62753112+meg-1@users.noreply.github.com>
2023-05-27 15:19:08 +0300

    adding a max_sectors_of_circle algorithm

---
+++ maths/max_sectors_of_circle.py
@@ -0,0 +1,21 @@
+def max_sectors_of_circle(num_cuts: float) -> float:
+    """
+    returns the maximum amount of
+    sectors a circle can be divided
+    by if cut 'num_cuts' times
+
+    >>> max_sectors_of_circle(54)
+    1486.0
+    >>> max_sectors_of_circle(7)
+    29.0
+    >>> max_sectors_of_circle(22.5)
+    265.375
+    >>> max_sectors_of_circle(-222)
+    -1
+    """
+
+    return ((num_cuts + 2 + num_cuts**2) * (1 / 2)) if num_cuts >= 0 else -1
+
+
+if __name__ == "__main__":
+    __import__("doctest").testmod()
