5daf1c04dc0ab0a47ad28190e774c04e69f8327d
JadeKim042386 <jk042386@gmail.com>
2023-05-27 16:23:49 +0900

    refactor: delete unnecessary continue

--- graphs/greedy_best_first.py
+++ graphs/greedy_best_first.py
@@ -136,22 +136,20 @@ class GreedyBestFirst:
             pos_x = parent.pos_x + action[1]
             pos_y = parent.pos_y + action[0]
 
-            if not (0 <= pos_x <= len(grid[0]) - 1 and 0 <= pos_y <= len(grid) - 1):
-                continue
-
-            if grid[pos_y][pos_x] != 0:
-                continue
-
-            successors.append(
-                Node(
-                    pos_x,
-                    pos_y,
-                    self.target.pos_x,
-                    self.target.pos_y,
-                    parent.g_cost + 1,
-                    parent,
+            if (0 <= pos_x <= len(grid[0]) - 1
+                and 0 <= pos_y <= len(grid) - 1
+                and grid[pos_y][pos_x] == 0):
+                
+                successors.append(
+                    Node(
+                        pos_x,
+                        pos_y,
+                        self.target.pos_x,
+                        self.target.pos_y,
+                        parent.g_cost + 1,
+                        parent,
+                    )
                 )
-            )
         return successors
 
     def retrace_path(self, node: Node | None) -> Path:
