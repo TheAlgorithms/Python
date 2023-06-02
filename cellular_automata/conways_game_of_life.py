1eef60c1272bea42abcef36a7cdf1cfd1945d517
hariharan141200 <hariharan141200@gmail.com>
2023-06-02 16:51:53 +0530

    command shell grid world

--- cellular_automata/conways_game_of_life.py
+++ cellular_automata/conways_game_of_life.py
@@ -1,97 +1,108 @@
-"""
-Conway's Game of Life implemented in Python.
-https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
-"""
-from __future__ import annotations
-
-from PIL import Image
-
-# Define glider example
-GLIDER = [
-    [0, 1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0, 0, 0, 0],
-    [1, 1, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-]
-
-# Define blinker example
-BLINKER = [[0, 1, 0], [0, 1, 0], [0, 1, 0]]
+import random , os , sys , time 
 
 
-def new_generation(cells: list[list[int]]) -> list[list[int]]:
-    """
-    Generates the next generation for a given state of Conway's Game of Life.
-    >>> new_generation(BLINKER)
-    [[0, 0, 0], [1, 1, 1], [0, 0, 0]]
-    """
-    next_generation = []
-    for i in range(len(cells)):
-        next_generation_row = []
-        for j in range(len(cells[i])):
-            # Get the number of live neighbours
-            neighbour_count = 0
-            if i > 0 and j > 0:
-                neighbour_count += cells[i - 1][j - 1]
-            if i > 0:
-                neighbour_count += cells[i - 1][j]
-            if i > 0 and j < len(cells[i]) - 1:
-                neighbour_count += cells[i - 1][j + 1]
-            if j > 0:
-                neighbour_count += cells[i][j - 1]
-            if j < len(cells[i]) - 1:
-                neighbour_count += cells[i][j + 1]
-            if i < len(cells) - 1 and j > 0:
-                neighbour_count += cells[i + 1][j - 1]
-            if i < len(cells) - 1:
-                neighbour_count += cells[i + 1][j]
-            if i < len(cells) - 1 and j < len(cells[i]) - 1:
-                neighbour_count += cells[i + 1][j + 1]
+class cell:
+    def __init__(self , i , j , size , body='.' , Alive=False):  
+        #CREATING INDUVIDUL CELLS
+        self.row = i 
+        self.col = j 
+        self.size = size
+        self.body = body
+        self.Alive = Alive
+    
+        #CONVERTING 2D INTO 1D TO NAME EACH CELL
+        self.id = self.col + self.row * 10
 
-            # Rules of the game of life (excerpt from Wikipedia):
-            # 1. Any live cell with two or three live neighbours survives.
-            # 2. Any dead cell with three live neighbours becomes a live cell.
-            # 3. All other live cells die in the next generation.
-            #    Similarly, all other dead cells stay dead.
-            alive = cells[i][j] == 1
-            if (
-                (alive and 2 <= neighbour_count <= 3)
-                or not alive
-                and neighbour_count == 3
-            ):
-                next_generation_row.append(1)
+    def update(self , Neibour):
+        neibour_count = 0
+
+        #CHECKING NEIBOUR
+        if self.row > 0:
+            if Neibour[self.row - 1][self.col].body == '*':
+                neibour_count += 1
+        if self.row < self.size -1:
+            if Neibour[self.row + 1][self.col].body == '*':
+                neibour_count += 1
+        if self.col > 0:
+            if Neibour[self.row][self.col- 1].body == '*':
+                neibour_count += 1
+        if self.col < self.size -1 :
+            if Neibour[self.row][self.col + 1].body == '*':
+                neibour_count += 1
+        if self.row > 0 and self.col > 0:
+            if Neibour[self.row - 1][self.col -1].body == '*':
+                neibour_count += 1
+        if self.row < self.size - 1 and self.col < self.size - 1:
+            if Neibour[self.row + 1][self.col + 1].body == '*':
+                neibour_count += 1
+        if self.row < self.size - 1 and self.col > 0:
+            if Neibour[self.row + 1][self.col - 1].body == '*':
+                neibour_count += 1
+        if self.row > 0 and self.col < self.size - 1:
+            if Neibour[self.row - 1][self.col + 1].body == '*':
+                neibour_count += 1
+
+        #UPDATING STATE AS ALIVE OR NOT 
+        if neibour_count < 2:
+            self.Alive = False
+        elif neibour_count > 3:
+            self.Alive = False
+        elif self.body == '*' and neibour_count == 2:
+            self.Alive = True
+        elif neibour_count == 3:
+            self.Alive = True
+        else:
+            self.Alive = False
+        
+def display(life , size ):
+    #RENDERING THE GRID WORLD
+    for i in range(size):
+        for j in range(size):
+            if life[i][j].Alive:
+                print(life[i][j].body, end=' ')
             else:
-                next_generation_row.append(0)
+                print(life[i][j].body, end=' ')
+        print()
 
-        next_generation.append(next_generation_row)
-    return next_generation
+def update(life , size):
+    #UPDATING THE GRID WORLD
+    for i in range(size):
+        for j in range(size):
+            life[i][j].update(life)
+
+    for i in range(size):
+        for j in range(size):            
+            if life[i][j].Alive:
+                life[i][j].body = '*'
+            else:
+                life[i][j].body = '.'
+
+def main(size):
+    #LIST CONTAINS ALL LIFE IN INTIIAL STATE
+    life = [['*' for i in range(size)] for j in range(size)]
+    for i in range(size):
+        for j in range(size):
+            rand_pop = random.randint(1 , 2)
+            if rand_pop == 1:
+                life[i][j] = cell(i , j , size , body='*' , Alive=True)
+            else:
+                life[i][j] = cell(i , j , size)
+
+    while True:
+        #LOOP TO RENDER THE GRID WORLD
+        display(life , size)
+        update(life , size)
+        time.sleep(0.1)
+
+        if sys.platform.startswith('win'):
+            print("hi")
+            os.system('cls')
+        else:
+            os.system('clear')
+        print()
+        print()
 
 
-def generate_images(cells: list[list[int]], frames: int) -> list[Image.Image]:
-    """
-    Generates a list of images of subsequent Game of Life states.
-    """
-    images = []
-    for _ in range(frames):
-        # Create output image
-        img = Image.new("RGB", (len(cells[0]), len(cells)))
-        pixels = img.load()
-
-        # Save cells to image
-        for x in range(len(cells)):
-            for y in range(len(cells[0])):
-                colour = 255 - cells[y][x] * 255
-                pixels[x, y] = (colour, colour, colour)
-
-        # Save image
-        images.append(img)
-        cells = new_generation(cells)
-    return images
-
-
-if __name__ == "__main__":
-    images = generate_images(GLIDER, 16)
-    images[0].save("out.gif", save_all=True, append_images=images[1:])
+if __name__ == '__main__':
+    grid_size = 10
+    main(grid_size)
