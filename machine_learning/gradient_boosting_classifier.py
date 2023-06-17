2cb92abda6cc9297a8b82a60337f69e14924d8fe
Sanket Nikam <77570082+SannketNikam@users.noreply.github.com>
2023-06-17 15:38:48 +0530

    Update gradient_boosting_classifier.py
    Changed camel case to snake case

--- machine_learning/gradient_boosting_classifier.py
+++ machine_learning/gradient_boosting_classifier.py
@@ -8,92 +8,78 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_selection import SelectKBest, chi2
 import matplotlib.pyplot as plt
 
-
 def load_data():
     digits = load_digits()
-    X = digits.data
+    x = digits.data
     y = digits.target
-    return X, y
+    return x, y
 
-
-def clean_data(X, y):
+def clean_data(x, y):
     # Convert to DataFrame
-    df = pd.DataFrame(X, columns=[f"pixel_{i}" for i in range(X.shape[1])])
-    df["target"] = y
+    df = pd.DataFrame(x, columns=[f"pixel_{i}" for i in range(x.shape[1])])
+    df['target'] = y
     return df
-
 
 def feature_engineering(df):
     # Perform feature engineering (if needed)
     # For this example, we will skip this step
     return df
 
-
 def feature_selection(df):
-    X = df.drop("target", axis=1)
-    y = df["target"]
-
+    x = df.drop('target', axis=1)
+    y = df['target']
+    
     # Perform feature selection using chi-square test
     selector = SelectKBest(chi2, k=10)
-    X_new = selector.fit_transform(X, y)
-
+    x_new = selector.fit_transform(x, y)
+    
     # Update DataFrame with selected features
-    selected_features = X.columns[selector.get_support()]
+    selected_features = x.columns[selector.get_support()]
     df = df[selected_features]
-    df["target"] = y
-
+    df['target'] = y
+    
     return df
-
 
 def encode_target(df):
     le = LabelEncoder()
-    df["target"] = le.fit_transform(df["target"])
+    df['target'] = le.fit_transform(df['target'])
     return df
 
-
 def split_data(df):
-    X = df.drop("target", axis=1)
-    y = df["target"]
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42
-    )
-    return X_train, X_test, y_train, y_test
+    x = df.drop('target', axis=1)
+    y = df['target']
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+    return x_train, x_test, y_train, y_test
 
-
-def train_model(X_train, y_train):
+def train_model(x_train, y_train):
     model = GradientBoostingClassifier()
-    model.fit(X_train, y_train)
+    model.fit(x_train, y_train)
     return model
 
-
-def evaluate_model(model, X_test, y_test):
-    y_pred = model.predict(X_test)
+def evaluate_model(model, x_test, y_test):
+    y_pred = model.predict(x_test)
     accuracy = accuracy_score(y_test, y_pred)
     report = classification_report(y_test, y_pred)
     return accuracy, report
-
 
 def plot_feature_importance(model, features):
     feature_importance = model.feature_importances_
     sorted_indices = np.argsort(feature_importance)
 
     plt.figure(figsize=(10, 6))
-    plt.barh(
-        range(len(sorted_indices)), feature_importance[sorted_indices], align="center"
-    )
+    plt.barh(range(len(sorted_indices)), feature_importance[sorted_indices], align='center')
     plt.yticks(range(len(sorted_indices)), features[sorted_indices])
-    plt.xlabel("Feature Importance")
-    plt.ylabel("Features")
-    plt.title("Gradient Boosting Classifier - Feature Importance")
+    plt.xlabel('Feature Importance')
+    plt.ylabel('Features')
+    plt.title('Gradient Boosting Classifier - Feature Importance')
     plt.show()
-
 
 def main():
     # Load data
-    X, y = load_data()
+    x, y = load_data()
 
     # Clean data
-    df = clean_data(X, y)
+    df = clean_data(x, y)
 
     # Feature engineering
     df = feature_engineering(df)
@@ -105,13 +91,13 @@ def main():
     df = encode_target(df)
 
     # Split data
-    X_train, X_test, y_train, y_test = split_data(df)
+    x_train, x_test, y_train, y_test = split_data(df)
 
     # Train model
-    model = train_model(X_train, y_train)
+    model = train_model(x_train, y_train)
 
     # Evaluate model
-    accuracy, report = evaluate_model(model, X_test, y_test)
+    accuracy, report = evaluate_model(model, x_test, y_test)
 
     # Plot feature importance
     plot_feature_importance(model, df.columns[:-1])
@@ -121,5 +107,5 @@ def main():
     print(f"\nClassification Report:\n{report}")
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
