0190498d063f18d5b14c5cfd8e34ec292b287e6a
duongoku <nguyenvubinhduong@gmail.com>
2023-06-25 19:48:01 +0700

    Add more doctests

--- backtracking/power_sum.py
+++ backtracking/power_sum.py
@@ -3,36 +3,25 @@ Problem source: https://www.hackerrank.com/challenges/the-power-sum/problem
 Find the number of ways that a given integer X, can be expressed as the sum
 of the Nth powers of unique, natural numbers. For example, if X=13 and N=2.
 We have to find all combinations of unique squares adding up to 13.
-The only solution is 2^2+3^2.
+The only solution is 2^2+3^2. Constraints: 1<=X<=1000, 2<=N<=10.
 """
 
 from math import pow
 
-sum_ = 0
-count_ = 0
+sum_=count_=0
 
 
 def backtrack(x: int, n: int, i: int) -> None:
     """
-    Backtracking function to find all the possible combinations
-    of powers of natural numbers that add up to x.
-    Parameters:
-        x: The number to be expressed as the sum of
-        the nth powers of unique, natural numbers.
-        n: The power of the natural numbers.
-        i: The current natural number.
-
-    Returns:
-        None
-
-    >>> backtrack(13, 2, 1)
-
+    DO NOT CALL THIS METHOD DIRECTLY, USE solve() INSTEAD.
+    
+    >>> backtrack(13, 2, 1) is None
+    True
     """
     global sum_, count_
     i_to_n = int(pow(i, n))
     if sum_ == x:
         # If the sum of the powers is equal to x, then we have found a solution.
-        global count_
         count_ += 1
         return
     elif sum_ + i_to_n <= x:
@@ -48,20 +37,30 @@ def backtrack(x: int, n: int, i: int) -> None:
 
 def solve(x: int, n: int) -> int:
     """
-    Calculates the number of ways that x can be expressed as the sum of
-    the nth powers of unique, natural numbers.
-    Parameters:
-        x: The number to be expressed as the sum of
-        the nth powers of unique, natural numbers.
-        n: The power of the natural numbers.
-
-    Returns:
-        The number of ways that x can be expressed as the sum of
-        the nth powers of unique, natural numbers.
-
     >>> solve(13, 2)
     1
+    >>> solve(100, 2)
+    3
+    >>> solve(100, 3)
+    1
+    >>> solve(800, 2)
+    561
+    >>> solve(1000, 10)
+    0
+    >>> solve(400, 2)
+    55
+    >>> solve(50, 1)
+    Traceback (most recent call last):
+        ...
+    ValueError: Invalid input, x must be between 1 and 1000, n between 2 and 10.
+    >>> solve(-10, 5)
+    Traceback (most recent call last):
+        ...
+    ValueError: Invalid input, x must be between 1 and 1000, n between 2 and 10.
     """
+    if not (1 <= x <= 1000 and 2 <= n <= 10):
+        raise ValueError("Invalid input, x must be between 1 and 1000, n between 2 and 10.")
+
     global sum_, count_
     sum_, count_ = 0, 0
     backtrack(x, n, 1)
@@ -72,11 +71,3 @@ if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
-
-    # x = 100
-    # n = 3
-    # output = f"The number of ways that {x} can be expressed as the sum of"
-    # output += f" the {n}th powers of unique, natural numbers is {solve(x, n)}"
-    # print(output)
-    # Output: The number of ways that 100 can be expressed as the
-    # sum of the 3th powers of unique, natural numbers is 1
