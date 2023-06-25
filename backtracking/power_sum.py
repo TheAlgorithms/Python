d4e18f113f337b2fe77fbed1685ed42ee968f3d2
duongoku <nguyenvubinhduong@gmail.com>
2023-06-25 21:32:32 +0700

    Improve paramater name

--- backtracking/power_sum.py
+++ backtracking/power_sum.py
@@ -11,7 +11,7 @@ from math import pow
 sum_ = count_ = 0
 
 
-def backtrack(x: int, n: int, i: int) -> None:
+def backtrack(needed_sum: int, power: int, current_number: int) -> None:
     """
     DO NOT CALL THIS METHOD DIRECTLY, USE solve() INSTEAD.
 
@@ -21,23 +21,23 @@ def backtrack(x: int, n: int, i: int) -> None:
     True
     """
     global sum_, count_
-    i_to_n = int(pow(i, n))
-    if sum_ == x:
-        # If the sum of the powers is equal to x, then we have found a solution.
+    i_to_n = int(pow(current_number, power))
+    if sum_ == needed_sum:
+        # If the sum of the powers is equal to needed_sum, then we have found a solution.
         count_ += 1
         return
-    elif sum_ + i_to_n <= x:
-        # If the sum of the powers is less than x, then we can continue adding powers.
+    elif sum_ + i_to_n <= needed_sum:
+        # If the sum of the powers is less than needed_sum, then we can continue adding powers.
         sum_ += i_to_n
-        backtrack(x, n, i + 1)
+        backtrack(needed_sum, power, current_number + 1)
         sum_ -= i_to_n
-    if i_to_n < x:
-        # If the power of i is less than x, then we can try with the next power.
-        backtrack(x, n, i + 1)
+    if i_to_n < needed_sum:
+        # If the power of i is less than needed_sum, then we can try with the next power.
+        backtrack(needed_sum, power, current_number + 1)
     return
 
 
-def solve(x: int, n: int) -> int:
+def solve(needed_sum: int, power: int) -> int:
     """
     >>> solve(13, 2)
     1
@@ -54,20 +54,20 @@ def solve(x: int, n: int) -> int:
     >>> solve(50, 1)
     Traceback (most recent call last):
         ...
-    ValueError: Invalid input, x must be between 1 and 1000, n between 2 and 10.
+    ValueError: Invalid input, needed_sum must be between 1 and 1000, power between 2 and 10.
     >>> solve(-10, 5)
     Traceback (most recent call last):
         ...
-    ValueError: Invalid input, x must be between 1 and 1000, n between 2 and 10.
+    ValueError: Invalid input, needed_sum must be between 1 and 1000, power between 2 and 10.
     """
-    if not (1 <= x <= 1000 and 2 <= n <= 10):
+    if not (1 <= needed_sum <= 1000 and 2 <= power <= 10):
         raise ValueError(
-            "Invalid input, x must be between 1 and 1000, n between 2 and 10."
+            "Invalid input, needed_sum must be between 1 and 1000, power between 2 and 10."
         )
 
     global sum_, count_
     sum_, count_ = 0, 0
-    backtrack(x, n, 1)
+    backtrack(needed_sum, power, 1)
     return count_
 
 
