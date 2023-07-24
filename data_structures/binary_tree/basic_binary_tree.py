dc86773d6d0266ea27753bd83f757391fe2d86d6
Diwas Dahal <105503443+di-was@users.noreply.github.com>
2023-07-24 16:44:31 +0545

    Update basic_binary_tree.py

--- data_structures/binary_tree/basic_binary_tree.py
+++ data_structures/binary_tree/basic_binary_tree.py
@@ -12,6 +12,23 @@ class Node:
         self.right: Node | None = None
 
 
+def display(tree: Node | None) -> None:  # In Order traversal of the tree
+    """
+    >>> root = Node(1)
+    >>> root.left = Node(0)
+    >>> root.right = Node(2)
+    >>> display(root)
+    0
+    1
+    2
+    >>> display(root.right)
+    2
+    """
+    if tree:
+        display(tree.left)
+        print(tree.data)
+        display(tree.right)
+
 def display_using_in_order_traversal(root: Node | None, level: int = 0) -> None:
     """
     >>> root = Node(1)
@@ -93,6 +110,7 @@ def main() -> None:  # Main function for testing.
     print(depth_of_tree(tree))
     print("Tree is: ")
     display_using_in_order_traversal(tree)
+    display(tree)
 
 
 if __name__ == "__main__":
