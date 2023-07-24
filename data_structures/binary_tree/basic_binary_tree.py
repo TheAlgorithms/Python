8341098d5b74efe9217faebd8acbdab0f9b5149f
Diwas Dahal <105503443+di-was@users.noreply.github.com>
2023-07-24 16:04:58 +0545

    Update data_structures/binary_tree/basic_binary_tree.py
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- data_structures/binary_tree/basic_binary_tree.py
+++ data_structures/binary_tree/basic_binary_tree.py
@@ -12,7 +12,7 @@ class Node:
         self.right: Node | None = None
 
 
-def display_tree(root: Node | None, level: int = 0) -> None:
+def display_using_in_order_traversal(root: Node | None, level: int = 0) -> None:
     """
     >>> root = Node(1)
     >>> root.left = Node(2)
