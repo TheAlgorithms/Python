9754ed8412df9e6c805cceb9ccd7736e79ade16a
Dharsan <139860049+dharsanjilla@users.noreply.github.com>
2023-07-25 13:17:28 +0530

    Update convert_number_to_words.py
    This code is for better understanding and in previous code there were a lot of nested loops

--- web_programming/convert_number_to_words.py
+++ web_programming/convert_number_to_words.py
@@ -1,106 +1,48 @@
 import math
 
 
-def convert(number: int) -> str:
-    """
-    Given a number return the number in words.
+def int_to_en(num):
+    d = { 0 : 'zero', 1 : 'one', 2 : 'two', 3 : 'three', 4 : 'four', 5 : 'five',
+          6 : 'six', 7 : 'seven', 8 : 'eight', 9 : 'nine', 10 : 'ten',
+          11 : 'eleven', 12 : 'twelve', 13 : 'thirteen', 14 : 'fourteen',
+          15 : 'fifteen', 16 : 'sixteen', 17 : 'seventeen', 18 : 'eighteen',
+          19 : 'nineteen', 20 : 'twenty',
+          30 : 'thirty', 40 : 'forty', 50 : 'fifty', 60 : 'sixty',
+          70 : 'seventy', 80 : 'eighty', 90 : 'ninety' }
+    k = 1000
+    m = k * 1000
+    b = m * 1000
+    t = b * 1000
 
-    >>> convert(123)
-    'OneHundred,TwentyThree'
-    """
-    if number == 0:
-        words = "Zero"
-        return words
-    else:
-        digits = math.log10(number)
-        digits = digits + 1
-        singles = {}
-        singles[0] = ""
-        singles[1] = "One"
-        singles[2] = "Two"
-        singles[3] = "Three"
-        singles[4] = "Four"
-        singles[5] = "Five"
-        singles[6] = "Six"
-        singles[7] = "Seven"
-        singles[8] = "Eight"
-        singles[9] = "Nine"
+    assert(0 <= num)
 
-        doubles = {}
-        doubles[0] = ""
-        doubles[2] = "Twenty"
-        doubles[3] = "Thirty"
-        doubles[4] = "Forty"
-        doubles[5] = "Fifty"
-        doubles[6] = "Sixty"
-        doubles[7] = "Seventy"
-        doubles[8] = "Eighty"
-        doubles[9] = "Ninety"
+    if (num < 20):
+        return d[num]
 
-        teens = {}
-        teens[0] = "Ten"
-        teens[1] = "Eleven"
-        teens[2] = "Twelve"
-        teens[3] = "Thirteen"
-        teens[4] = "Fourteen"
-        teens[5] = "Fifteen"
-        teens[6] = "Sixteen"
-        teens[7] = "Seventeen"
-        teens[8] = "Eighteen"
-        teens[9] = "Nineteen"
+    if (num < 100):
+        if num % 10 == 0: return d[num]
+        else: return d[num // 10 * 10] + '-' + d[num % 10]
 
-        placevalue = {}
-        placevalue[2] = "Hundred,"
-        placevalue[3] = "Thousand,"
-        placevalue[5] = "Lakh,"
-        placevalue[7] = "Crore,"
+    if (num < k):
+        if num % 100 == 0: return d[num // 100] + ' hundred'
+        else: return d[num // 100] + ' hundred and ' + int_to_en(num % 100)
 
-        temp_num = number
-        words = ""
-        counter = 0
-        digits = int(digits)
-        while counter < digits:
-            current = temp_num % 10
-            if counter % 2 == 0:
-                addition = ""
-                if counter in placevalue and current != 0:
-                    addition = placevalue[counter]
-                if counter == 2:
-                    words = singles[current] + addition + words
-                elif counter == 0:
-                    if ((temp_num % 100) // 10) == 1:
-                        words = teens[current] + addition + words
-                        temp_num = temp_num // 10
-                        counter += 1
-                    else:
-                        words = singles[current] + addition + words
+    if (num < m):
+        if num % k == 0: return int_to_en(num // k) + ' thousand'
+        else: return int_to_en(num // k) + ' thousand, ' + int_to_en(num % k)
 
-                else:
-                    words = doubles[current] + addition + words
+    if (num < b):
+        if (num % m) == 0: return int_to_en(num // m) + ' million'
+        else: return int_to_en(num // m) + ' million, ' + int_to_en(num % m)
 
-            else:
-                if counter == 1:
-                    if current == 1:
-                        words = teens[number % 10] + words
-                    else:
-                        addition = ""
-                        if counter in placevalue:
-                            addition = placevalue[counter]
-                        words = doubles[current] + addition + words
-                else:
-                    addition = ""
-                    if counter in placevalue:
-                        if current != 0 and ((temp_num % 100) // 10) != 0:
-                            addition = placevalue[counter]
-                    if ((temp_num % 100) // 10) == 1:
-                        words = teens[current] + addition + words
-                        temp_num = temp_num // 10
-                        counter += 1
-                    else:
-                        words = singles[current] + addition + words
-            counter += 1
-            temp_num = temp_num // 10
-    return words
+    if (num < t):
+        if (num % b) == 0: return int_to_en(num // b) + ' billion'
+        else: return int_to_en(num // b) + ' billion, ' + int_to_en(num % b)
+
+    if (num % t == 0): return int_to_en(num // t) + ' trillion'
+    else: return int_to_en(num // t) + ' trillion, ' + int_to_en(num % t)
+
+    raise AssertionError('num is too large: %s' % str(num))
 
 
 if __name__ == "__main__":
