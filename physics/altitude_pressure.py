b88c7554e4d7ace296784b962138d55ae5c63c8f
Bazif Rasool <45148731+Bazifrasool@users.noreply.github.com>
2023-07-30 19:31:42 +0530

    Update physics/altitude_pressure.py
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- physics/altitude_pressure.py
+++ physics/altitude_pressure.py
@@ -31,7 +31,7 @@ def get_altitude_at_pressure(pressure: float) -> float:
     if pressure > 101325:
         raise ValueError("Value Higher than Pressure at Sea Level !")
 
-    return 44330 * (1 - (pressure / 101325) ** (1 / 5.5255))
+    return 44_330 * (1 - (pressure / 101_325) ** (1 / 5.5255))
 
 
 if __name__ == "__main__":
