4b5e5f26472960a669ddddf8fd486845687e6aed
Slava Makeev <unclearnuclear@yahoo.com>
2023-08-10 19:49:55 +0000

    added additional tests for merge_two_binary_trees and binary_tree_mirror

--- data_structures/binary_tree/binary_tree_mirror.py
+++ data_structures/binary_tree/binary_tree_mirror.py
@@ -3,7 +3,6 @@ Problem Description:
 Given a binary tree, return its mirror.
 """
 
-
 def binary_tree_mirror_dict(binary_tree_mirror_dictionary: dict, root: int):
     if not root or root not in binary_tree_mirror_dictionary:
         return
@@ -12,18 +11,26 @@ def binary_tree_mirror_dict(binary_tree_mirror_dictionary: dict, root: int):
     binary_tree_mirror_dict(binary_tree_mirror_dictionary, left_child)
     binary_tree_mirror_dict(binary_tree_mirror_dictionary, right_child)
 
-
 def binary_tree_mirror(binary_tree: dict, root: int = 1) -> dict:
     """
-    >>> binary_tree_mirror({ 1: [2,3], 2: [4,5], 3: [6,7], 7: [8,9]}, 1)
-    {1: [3, 2], 2: [5, 4], 3: [7, 6], 7: [9, 8]}
-    >>> binary_tree_mirror({ 1: [2,3], 2: [4,5], 3: [6,7], 4: [10,11]}, 1)
-    {1: [3, 2], 2: [5, 4], 3: [7, 6], 4: [11, 10]}
-    >>> binary_tree_mirror({ 1: [2,3], 2: [4,5], 3: [6,7], 4: [10,11]}, 5)
+    Return the mirror of a given binary tree.
+    
+    :param binary_tree: Dictionary representing the binary tree.
+    :param root: Root node of the binary tree.
+    :return: A new dictionary representing the mirrored binary tree.
+    
+    >>> binary_tree_mirror({ 1: [2, 3], 2: [4, 5], 3: [6, 7]}, 1)
+    {1: [3, 2], 2: [5, 4], 3: [7, 6]}
+    
+    >>> binary_tree_mirror({ 1: [2, 3], 2: [4, 5], 3: [6, 7]}, 3)
+    {3: [7, 6], 7: [None, None], 6: [None, None]}
+    
+    >>> binary_tree_mirror({ 1: [2, 3], 2: [4, 5], 3: [6, 7]}, 2)
     Traceback (most recent call last):
         ...
-    ValueError: root 5 is not present in the binary_tree
-    >>> binary_tree_mirror({}, 5)
+    ValueError: root 2 is not present in the binary_tree
+    
+    >>> binary_tree_mirror({}, 1)
     Traceback (most recent call last):
         ...
     ValueError: binary tree cannot be empty
@@ -37,9 +44,8 @@ def binary_tree_mirror(binary_tree: dict, root: int = 1) -> dict:
     binary_tree_mirror_dict(binary_tree_mirror_dictionary, root)
     return binary_tree_mirror_dictionary
 
-
 if __name__ == "__main__":
-    binary_tree = {1: [2, 3], 2: [4, 5], 3: [6, 7], 7: [8, 9]}
+    binary_tree = {1: [2, 3], 2: [4, 5], 3: [6, 7]}
     print(f"Binary tree: {binary_tree}")
-    binary_tree_mirror_dictionary = binary_tree_mirror(binary_tree, 5)
+    binary_tree_mirror_dictionary = binary_tree_mirror(binary_tree, 1)
     print(f"Binary tree mirror: {binary_tree_mirror_dictionary}")
--- data_structures/binary_tree/merge_two_binary_trees.py
+++ data_structures/binary_tree/merge_two_binary_trees.py
@@ -83,7 +83,40 @@ if __name__ == "__main__":
     tree2.right = Node(6)
     tree2.left.right = Node(9)
     tree2.right.right = Node(5)
+# Additional test case 1: Merging two empty trees
+    empty_merged_tree = merge_two_binary_trees(None, None)
+    # Expected output: None
 
+    # Additional test case 2: Merging a non-empty tree with an empty tree
+    tree3 = Node(10)
+    tree3.left = Node(20)
+    tree3.right = Node(30)
+    merged_tree_2 = merge_two_binary_trees(tree3, None)
+    # Expected output: Tree with values 10, 20, 30
+
+    # Additional test case 3: Merging two trees with no overlapping nodes
+    tree4 = Node(1)
+    tree4.left = Node(2)
+    tree4.right = Node(3)
+    tree5 = Node(4)
+    tree5.left = Node(5)
+    tree5.right = Node(6)
+    merged_tree_3 = merge_two_binary_trees(tree4, tree5)
+    # Expected output: Merged tree with values 5, 7, 9, 2, 4, 6
+
+    # Additional test case 4: Merging trees with varying depths
+    tree6 = Node(1)
+    tree6.left = Node(2)
+    tree7 = Node(3)
+    tree7.left = Node(4)
+    tree7.left.left = Node(5)
+    merged_tree_4 = merge_two_binary_trees(tree6, tree7)
+    # Expected output: Merged tree with values 4, 6, 2, 5
+
+    print("Empty Merged Tree:", print_preorder(empty_merged_tree))
+    print("Merged Tree with One Empty Tree:", print_preorder(merged_tree_2))
+    print("Merged Tree with No Overlapping Nodes:", print_preorder(merged_tree_3))
+    print("Merged Tree with Varying Depths:", print_preorder(merged_tree_4))
     print("Tree1 is: ")
     print_preorder(tree1)
     print("Tree2 is: ")
