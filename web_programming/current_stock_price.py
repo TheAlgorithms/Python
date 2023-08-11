f4ae6fde40a345d1cb0d7f1904d12d20fc8ed9e4
Suman <sumanmitra07@outlook.com>
2023-08-11 16:10:51 +0530

    updated to return the error text

--- web_programming/current_stock_price.py
+++ web_programming/current_stock_price.py
@@ -11,9 +11,7 @@ def stock_price(symbol: str = "AAPL") -> str:
     if specific_fin_streamer_tag:
         text = specific_fin_streamer_tag.get_text()
         return text
-    else:
-        print("No <fin-streamer> tag with the specified data-test attribute found.")
-    return "Not Found"
+    return "No <fin-streamer> tag with the specified data-test attribute found."
 
 # Search for the symbol at https://finance.yahoo.com/lookup
 if __name__ == "__main__":
