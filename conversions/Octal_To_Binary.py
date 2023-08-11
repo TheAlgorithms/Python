5eabc0930b7074f807added106592227d1a606c8
Bama Charan Chhandogi <b.c.chhandogi@gmail.com>
2023-08-11 16:57:08 +0530

    Delete Octal_To_Binary.py

--- conversions/Octal_To_Binary.py
+++
@@ -1,5 +0,0 @@
-octal_number = input("Enter an octal number: ")
-decimal_number = int(octal_number, 8)  # Convert octal to decimal
-binary_number = bin(decimal_number)  # Convert decimal to binary
-
-print(f"The binary representation of {octal_number} is {binary_number}")
