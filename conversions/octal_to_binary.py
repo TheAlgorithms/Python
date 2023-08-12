79eb0337264da8b6d78cf2ceb91600156d87ae73
Navanit Nandakumar <navanitnandakumar@gmail.com>
2023-08-12 07:38:03 +0530

    Updated octal_to_binary.py
    Fixed whitespaces in blank lines.

--- conversions/octal_to_binary.py
+++ conversions/octal_to_binary.py
@@ -6,7 +6,6 @@ def octal_to_binary(octal):
         decimal += (octal % 10) * pow(8, power)
         octal //= 10
         power += 1
-
     # Converting Decimal to Binary
     binary = 0
     digit_place = 1
@@ -14,10 +13,7 @@ def octal_to_binary(octal):
         binary += (decimal % 2) * digit_place
         decimal //= 2
         digit_place *= 10
-
     return binary
-
-
 octal_number = int(input("Enter octal number: "))
 binary_number = octal_to_binary(octal_number)
 print(f"The binary equivalent of {octal_number} is {binary_number}")
