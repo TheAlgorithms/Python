8c5aef0497aee2e176e1cd832f7f5362ac659a3c
Amir Hosseini <19665344+itsamirhn@users.noreply.github.com>
2023-08-14 09:02:30 +0330

    Update dynamic_programming/regex_match.py
    Co-authored-by: Tianyi Zheng <tianyizheng02@gmail.com>

--- dynamic_programming/regex_match.py
+++ dynamic_programming/regex_match.py
@@ -74,8 +74,6 @@ def dp_match(text: str, pattern: str) -> bool:
     dp = [[False for _ in range(n + 1)] for _ in range(m + 1)]
     dp[0][0] = True
 
-    for i in range(1, m + 1):
-        dp[i][0] = False
 
     for j in range(1, n + 1):
         dp[0][j] = pattern[j - 1] == "*" and dp[0][j - 2]
