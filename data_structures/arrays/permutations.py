c48d7cadb6f8dc58f28661ee7ef863ba2707ad0c
CaedenPH <caedenperelliharris@gmail.com>
2023-08-16 16:05:18 +0100

    types(permuations): Rename permute2

--- data_structures/arrays/permutations.py
+++ data_structures/arrays/permutations.py
@@ -1,17 +1,16 @@
-def permute(nums: list[int]) -> list[list[int]]:
+def permute_iterative(nums: list[int]) -> list[list[int]]:
     """
     Return all permutations.
-    >>> from itertools import permutations
-    >>> numbers= [1,2,3]
-    >>> all(list(nums) in permute(numbers) for nums in permutations(numbers))
-    True
+
+    >>> permute_backtrack([1, 2, 3])
+    [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 2, 1], [3, 1, 2]]
     """
-    result = []
+    result: list[list[int]] = []
     if len(nums) == 1:
         return [nums.copy()]
     for _ in range(len(nums)):
         n = nums.pop(0)
-        permutations = permute(nums)
+        permutations = permute_iterative(nums)
         for perm in permutations:
             perm.append(n)
         result.extend(permutations)
@@ -19,15 +18,15 @@ def permute(nums: list[int]) -> list[list[int]]:
     return result
 
 
-def permute2(nums):
+def permute_backtrack(nums: list[int]) -> list[list[int]]:
     """
     Return all permutations of the given list.
 
-    >>> permute2([1, 2, 3])
+    >>> permute_backtrack([1, 2, 3])
     [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 2, 1], [3, 1, 2]]
     """
 
-    def backtrack(start):
+    def backtrack(start: int) -> None:
         if start == len(nums) - 1:
             output.append(nums[:])
         else:
@@ -36,7 +35,7 @@ def permute2(nums):
                 backtrack(start + 1)
                 nums[start], nums[i] = nums[i], nums[start]  # backtrack
 
-    output = []
+    output: list[list[int]] = []
     backtrack(0)
     return output
 
@@ -45,6 +44,6 @@ if __name__ == "__main__":
     import doctest
 
     # use res to print the data in permute2 function
-    res = permute2([1, 2, 3])
+    res = permute_backtrack([1, 2, 3])
     print(res)
     doctest.testmod()
