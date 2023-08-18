8691180fd4ed4712eb4ac17bf83e936cb7a9c513
Guduly <133545858+Guduly@users.noreply.github.com>
2023-08-17 20:23:17 -0500

    Update arc_length.py
    followed the change request

--- maths/arc_length.py
+++ maths/arc_length.py
@@ -7,13 +7,11 @@ def arc_length(angle: int, radius: int) -> float:
     3.9269908169872414
     >>> arc_length(120, 15)
     31.415926535897928
+    >>> arc_length(90, 10)
+    15.7079632679
     """
     return 2 * pi * radius * (angle / 360)
 
 
 if __name__ == "__main__":
     print(arc_length(90, 10))
-    """
-    >>> arc_length(90, 10)
-    15.7079632679
-    """
