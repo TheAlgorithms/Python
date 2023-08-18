fa33d5cc6379890e1b363763f3a97c0769b2aa0a
BamaCharanChhandogi <b.c.chhandogi@gmailcom>
2023-08-18 11:45:30 +0530

    added documention and some test cases

--- conversions/octal_to_binary.py
+++ conversions/octal_to_binary.py
@@ -1,7 +1,31 @@
+"""
+* Author: Bama Charan Chhandogi (https://github.com/BamaCharanChhandogi)
+* Description: Convert a Octal number to Binary.
+
+reference for better understand
+
+URL: https://en.wikipedia.org/wiki/Binary_number
+URL: https://en.wikipedia.org/wiki/Octal
+"""
 def octal_to_binary(octal_number: str) -> str:
     binary_number = ""
     octal_digits = "01234567"
 
+    """
+    ValueError: String to the function
+    >>> oct_to_decimal("Av")
+    Traceback (most recent call last):
+        ...
+    ValueError: Non-octal value was passed to the function
+    >>> oct_to_decimal("90")
+    Traceback (most recent call last):
+        ...
+    ValueError: octal value was passed to the function
+    >>> oct_to_decimal("17")
+    001111
+    >>> oct_to_decimal("7")
+    111
+    """
     for digit in octal_number:
         if digit not in octal_digits:
             raise ValueError("Invalid octal digit")
