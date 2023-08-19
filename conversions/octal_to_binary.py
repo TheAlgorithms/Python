37e33918e81df01d300c2cf37b35771c7c788439
BamaCharanChhandogi <b.c.chhandogi@gmail.com>
2023-08-19 23:53:55 +0530

    documention problem solved

--- conversions/octal_to_binary.py
+++ conversions/octal_to_binary.py
@@ -7,52 +7,35 @@ https://en.wikipedia.org/wiki/Binary_number
 https://en.wikipedia.org/wiki/Octal
 """
 
-
 def octal_to_binary(octal_number: str) -> str:
     """
     Convert an Octal number to Binary.
-
-    ValueError: Non-octal value was passed to the function
+    >>> octal_to_binary("17")
+    '001111'
+    >>> octal_to_binary("7")
+    '111'
     >>> octal_to_binary("Av")
     Traceback (most recent call last):
         ...
     ValueError: Non-octal value was passed to the function
-
-    ValueError: Special Character was passed to the function
+    
     >>> octal_to_binary("@#")
     Traceback (most recent call last):
         ...
-    ValueError: Special Character was passed to the function
-
-    ValueError: Empty String was passed to the function
+    ValueError: Non-octal value was passed to the function
     >>> octal_to_binary("")
     Traceback (most recent call last):
         ...
-    ValueError: Empty String was passed to the function
-
-    ValueError: Octal value was passed to the function
-    >>> octal_to_binary("17")
-    '001111'
-
-    ValueError: Octal value was passed to the function
-    >>> octal_to_binary("7")
-    '111'
+    ValueError: Empty string was passed to the function
     """
     if not octal_number:
         raise ValueError("Empty String was passed to the function")
-
-    for digit in octal_number:
-        if not digit.isdigit():
-            raise ValueError("Special Character was passed to the function")
-
-        if digit < "0" or digit > "7":
-            raise ValueError("Non-octal value was passed to the function")
 
     binary_number = ""
     octal_digits = "01234567"
     for digit in octal_number:
         if digit not in octal_digits:
-            raise ValueError("Octal value was passed to the function")
+            raise ValueError("Non-octal value was passed to the function")
 
         binary_digit = ""
         value = int(digit)
@@ -66,5 +49,4 @@ def octal_to_binary(octal_number: str) -> str:
 
 if __name__ == "__main__":
     import doctest
-
     doctest.testmod()
