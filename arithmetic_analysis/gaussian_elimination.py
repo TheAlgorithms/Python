244ec4049fda5d5c4f52ee1a79eb547f701a7b94
Dom <97384583+tosemml@users.noreply.github.com>
2023-08-20 13:11:53 -0700

    further improvements using array slicing
    Co-authored-by: Tianyi Zheng <tianyizheng02@gmail.com>

--- arithmetic_analysis/gaussian_elimination.py
+++ arithmetic_analysis/gaussian_elimination.py
@@ -33,10 +33,7 @@ def retroactive_resolution(
 
     x: NDArray[float64] = np.zeros((rows, 1), dtype=float)
     for row in reversed(range(rows)):
-        total = np.dot(
-            [coefficients[row, col] for col in range(row + 1, columns)],
-            [x[col] for col in range(row + 1, columns)],
-        )
+        total = np.dot(coefficients[row, row + 1 :], x[row + 1 :])
         x[row, 0] = (vector[row] - total) / coefficients[row, row]
 
     return x
