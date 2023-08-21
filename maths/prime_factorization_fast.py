e36b7b764056b4a1611e98afce187e97db5ff006
Sourabh kumar verma <55890376+sourabhkv@users.noreply.github.com>
2023-08-21 23:34:15 +0530

    Update prime_factorization_fast.py

--- maths/prime_factorization_fast.py
+++ maths/prime_factorization_fast.py
@@ -1,13 +1,38 @@
 from __future__ import annotations
 from math import ceil, sqrt
 import time
+from typing import Any, List, Tuple
 
 
-def timer(func):
-    def wrapper(*args, **kwargs):
+def timer(func: callable) -> callable:
+    """
+    Decorator that measures the execution time of a function.
+
+    :param func: The function to be timed.
+    :return: The wrapped function.
+
+    >>> @timer
+    ... def test_function():
+    ...     time.sleep(1)
+    ...     return "Test completed."
+    ...
+    >>> result = test_function()
+    Test completed.
+    test_function took 1.000272 seconds to execute.
+    >>> result
+    'Test completed.'
+    """
+
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        """
+        Wraps the given function and measures its execution time.
+        
+        :param args: Positional arguments for the function.
+        :param kwargs: Keyword arguments for the function.
+        :return: The result of the wrapped function.
+        """
         start = time.time()
         result = func(*args, **kwargs)
-        print(result)
         end = time.time()
         print(f"{func.__name__} took {end - start:.6f} seconds to execute.")
         return result
@@ -16,8 +41,9 @@ def timer(func):
 
 
 @timer
-def prime_factors(n: int) -> list[int]:
+def prime_factors(number: int) -> List[int]:
     """
+    Returns prime factors of a given number as a list.
     Returns prime factors of n as a list.
 
     >>> prime_factors(0)
@@ -45,12 +71,6 @@ def prime_factors(n: int) -> list[int]:
     TypeError: '<=' not supported between instances of 'int' and 'list'
 
     """
-    if not isinstance(n, int):
-        raise TypeError("Input must be an integer")
-
-    if n <= 0:
-        return []
-
     i = 2
     factors = []
     while i * i <= n:
@@ -65,8 +85,10 @@ def prime_factors(n: int) -> list[int]:
 
 
 @timer
-def primeproduct(num: int) -> list[int]:
+def primeproduct(num: int) -> List[int]:
     """
+    Returns prime factors of a positive integer num as a list.
+
     >>> primeproduct(868)
     [2, 2, 7, 31]
     >>> primeproduct(9039423423423743)
@@ -76,10 +98,8 @@ def primeproduct(num: int) -> list[int]:
     ValueError: invalid literal for int() with base 10: '0.02'
     >>> primeproduct(-2342)
     []
-    """
-    if not isinstance(num, int):
-        raise TypeError("Input must be an integer")
 
+    """
     if num <= 1:
         return []
 
@@ -87,6 +107,7 @@ def primeproduct(num: int) -> list[int]:
     while num > 1:
         if len(prime_factors) >= 1 and prime_factors[-1] % num == 0:
             prime_factors.append(prime_factors[-1])
+
         else:
             sq = ceil(sqrt(num))
             flag = 0
@@ -98,24 +119,31 @@ def primeproduct(num: int) -> list[int]:
                         prime_factors.append(i)
                         flag = 1
                         break
+
             else:
                 while num % 2 == 0:
                     num = num // 2
                     prime_factors.append(2)
+
                 for i in range(3, sq + 1, 2):
                     if num % i == 0:
                         num = num // i
                         prime_factors.append(i)
                         flag = 1
                         break
+
             if not flag and num > 1:
                 prime_factors.append(num)
                 num = 1
                 break
+
     return prime_factors
 
 
 if __name__ == "__main__":
-    n = int(input("enter number: ").strip())
+    n = int(input("enter number: "))
     primeproduct(n)
     prime_factors(n)
+    import doctest
+
+    doctest.testmod()
