c227ff95f540a939c0a6599b9887c69299054c8d
Amirsoroush <amirsoroush.dev@gmail.com>
2023-08-21 19:09:18 +0300

    fixes #9002; improve insertion_sort algorithm

--- sorts/insertion_sort.py
+++ sorts/insertion_sort.py
@@ -40,13 +40,12 @@ def insertion_sort(collection: list) -> list:
     True
     """
 
-    for insert_index, insert_value in enumerate(collection[1:]):
-        temp_index = insert_index
-        while insert_index >= 0 and insert_value < collection[insert_index]:
-            collection[insert_index + 1] = collection[insert_index]
+    for insert_index in range(1, len(collection)):
+        insert_value = collection[insert_index]
+        while insert_index > 0 and insert_value < collection[insert_index - 1]:
+            collection[insert_index] = collection[insert_index - 1]
             insert_index -= 1
-        if insert_index != temp_index:
-            collection[insert_index + 1] = insert_value
+        collection[insert_index] = insert_value
     return collection
 
 
