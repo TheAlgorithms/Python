6d495ec50ecea23bea739f93b9a94f75860b2497
Abhay Kabdwal <abhaykabdwal@gmail.com>
2023-09-03 17:50:34 +0530

    improvised area_under_curve to work for any user input

--- maths/area_under_curve.py
+++ maths/area_under_curve.py
@@ -3,11 +3,28 @@ Approximates the area under the curve using the trapezoidal rule
 """
 from __future__ import annotations
 
-from collections.abc import Callable
+def evaluate(arr: list[float], x: float) -> float:
+    """
+    This function takes a list of coefficients
+    and a value of x as inputs and returns
+    the value of the polynomial at that point.
+    :param arr: a list containing coefficients
+    :param x: value of x
 
+    >>> arr = [5]
+    >>> f"{evaluate(arr,2)}
+    '5'
+    >>> arr = [5,1]
+    >>> f"{evaluate(arr,2)}
+    '11'
+    """
+    y = 0.0
+    for i in range(len(arr)):
+        y += arr[i] * (x ** (len(arr) - i - 1))
+    return y
 
 def trapezoidal_area(
-    fnc: Callable[[float], float],
+    fnc: list[float],
     x_start: float,
     x_end: float,
     steps: int = 100,
@@ -15,7 +32,7 @@ def trapezoidal_area(
     """
     Treats curve as a collection of linear lines and sums the area of the
     trapezium shape they form
-    :param fnc: a function which defines a curve
+    :param fnc: a list which defines a curve of polynomial
     :param x_start: left end point to indicate the start of line segment
     :param x_end: right end point to indicate end of line segment
     :param steps: an accuracy gauge; more steps increases the accuracy
@@ -32,29 +49,35 @@ def trapezoidal_area(
     >>> f"{trapezoidal_area(f, -4.0, 4.0, 10000):.4f}"
     '384.0000'
     """
-    x1 = x_start
-    fx1 = fnc(x_start)
+    dx = (x_end - x_start) / steps
     area = 0.0
-    for _ in range(steps):
-        # Approximates small segments of curve as linear and solve
-        # for trapezoidal area
-        x2 = (x_end - x_start) / steps + x1
-        fx2 = fnc(x2)
-        area += abs(fx2 + fx1) * (x2 - x1) / 2
-        # Increment step
-        x1 = x2
-        fx1 = fx2
+    for i in range(steps):
+        x1 = x_start + i * dx
+        x2 = x_start + (i + 1) * dx
+        fx1 = evaluate(fnc, x1)
+        fx2 = evaluate(fnc, x2)
+        area += abs((fx2 + fx1) * dx / 2)
     return area
 
 
 if __name__ == "__main__":
 
-    def f(x):
-        return x**3 + x**2
+    m = int(input("Enter order of polynomial: ").strip())
+    f = []
+    for j in range(m, -1, -1):
+        a_j = float(input(f"Enter coefficient of a[{j}]: ").strip())
+        f.append(a_j)
+    
+    print("The given function is :")
+    for j in range(len(f) - 1):
+        print(f"{f[j]}*x^{len(f)-j-1}", end=" + ")
+    print(f"{f[-1]}*x^{0}")
 
-    print("f(x) = x^3 + x^2")
-    print("The area between the curve, x = -5, x = 5 and the x axis is:")
+    x1 = float(input("Enter the value of x_start : "))
+    x2 = float(input("Enter the value of x_end : "))
+
+    print(f"The area between the curve, x = {x1}, x = {x2} and the x axis is:")
     i = 10
     while i <= 100000:
-        print(f"with {i} steps: {trapezoidal_area(f, -5, 5, i)}")
+        print(f"with {i} steps: area = {trapezoidal_area(f, x1, x2, i):.4f}")
         i *= 10
