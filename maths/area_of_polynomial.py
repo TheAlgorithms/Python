61dc7aaa94eb43c6858b6df940fba135f2f0692d
Abhay Kabdwal <abhaykabdwal@gmail.com>
2023-09-03 15:53:48 +0530

    added area of polynomial using trapezoidal rule

--- maths/area_of_polynomial.py
+++ maths/area_of_polynomial.py
@@ -1,9 +1,8 @@
 """
-Approximates the area under the curve of a polynomial given from the user using the trapezoidal rule
+Approximates the area of a polynomial given from the user using the trapezoidal rule
 """
-from typing import List
 
-def input_polynomial() -> List[float]:
+def input_polynomial() -> list[float]:
     # This function takes the order of the polynomial and its coefficients as inputs from the user
     # and returns a list of coefficients.
     m = int(input("Enter order of polynomial: "))
@@ -13,7 +12,7 @@ def input_polynomial() -> List[float]:
         arr.append(a_j)
     return arr
 
-def evaluate(arr: List[float], x: float) -> float:
+def evaluate(arr: list[float], x: float) -> float:
     # This function takes a list of coefficients and a value of x as inputs
     # and returns the value of the polynomial at that point.
     y = 0
@@ -22,7 +21,7 @@ def evaluate(arr: List[float], x: float) -> float:
     return y
 
 def trapezoidal_area(
-    fnc: List[float],
+    fnc: list[float],
     x_start: float,
     x_end: float,
     steps: int = 100,
@@ -44,7 +43,7 @@ if __name__ == "__main__":
     # In the main block, the code takes inputs for the polynomial, start and end points on the x-axis,
     # and calculates the area under the curve for different numbers of steps.
     f = input_polynomial()
-    print(f"The given function is :")
+    print("The given function is :")
     for j in range(len(f)-1):
         print(f"{f[j]}*x^{len(f)-j-1}", end=" + ")
     print(f"{f[-1]}*x^{0}")
