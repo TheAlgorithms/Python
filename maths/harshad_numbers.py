6ebaf2589e42e6c9f7abb64139c5fba9a994d14d
David Ekong <66387173+davidekong@users.noreply.github.com>
2023-09-05 19:38:14 +0100

    Update harshad_numbers.py

--- maths/harshad_numbers.py
+++ maths/harshad_numbers.py
@@ -1,5 +1,6 @@
 """
-A Harshad number is divisible by the sum of its digits in any base n.
+A harshad number (or more specifically an n-harshad number) is a number that's 
+divisible by the sum of its digits in some given base n.
 Reference: https://en.wikipedia.org/wiki/Harshad_number
 """
 
@@ -12,7 +13,7 @@ def int_to_base(number: int, base: int) -> str:
         raise ValueError("number must be a positive integer")
 
     while number > 0:
-        number, remainder = divmod(number, base_of_interest)
+        number, remainder = divmod(number, base)
         result = digits[remainder] + result
 
     if result == "":
@@ -58,74 +59,74 @@ def sum_of_digits(num: int, base: int) -> str:
 
 def harshad_numbers_in_base(limit: int, base: int) -> list[str]:
     """
-    Finds all Harshad numbers smaller than num in base 'base_of_interest'.
-    Where 'base_of_interest' ranges from 2 to 36.
+    Finds all Harshad numbers smaller than num in base 'base'.
+    Where 'base' ranges from 2 to 36.
 
     Examples:
-    >>> all_harshad_numbers(15, 2)
+    >>> harshad_numbers_in_base(15, 2)
     (7, ['1', '10', '100', '110', '1000', '1010', '1100'])
-    >>> all_harshad_numbers(12, 34)
+    >>> harshad_numbers_in_base(12, 34)
     (11, ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B'])
-    >>> all_harshad_numbers(12, 4)
+    >>> harshad_numbers_in_base(12, 4)
     (7, ['1', '2', '3', '10', '12', '20', '21'])
     >>> # bases beyond 36 and below 2 will error
-    >>> all_harshad_numbers(234, 37)
+    >>> harshad_numbers_in_base(234, 37)
     Traceback (most recent call last):
         ...
-    ValueError: 'base_of_interest' must be between 36 and 2 inclusive
-    >>> all_harshad_numbers(234, 1)
+    ValueError: 'base' must be between 36 and 2 inclusive
+    >>> harshad_numbers_in_base(234, 1)
     Traceback (most recent call last):
         ...
-    ValueError: 'base_of_interest' must be between 36 and 2 inclusive
+    ValueError: 'base' must be between 36 and 2 inclusive
     """
 
-    if (base_of_interest > 36) or (base_of_interest < 2):
-        raise ValueError("'base_of_interest' must be between 36 and 2 inclusive")
+    if (base > 36) or (base < 2):
+        raise ValueError("'base' must be between 36 and 2 inclusive")
 
     result = 0
     numbers = []
     if num >= 0:
         for i in range(1, num):
-            y = sum_of_digits(i, base_of_interest)
-            if i % int(y, base_of_interest) == 0:
+            y = sum_of_digits(i, base)
+            if i % int(y, base) == 0:
                 result += 1
-                numbers.append(int_to_base(i, base_of_interest))
+                numbers.append(int_to_base(i, base))
 
     return result, numbers
 
 
 def is_harshad_number_in_base(num: int, base: int) -> bool:
     """
-    Determines whether n in base 'base_of_interest' is a harshad number.
-    Where 'base_of_interest' ranges from 2 to 36.
+    Determines whether n in base 'base' is a harshad number.
+    Where 'base' ranges from 2 to 36.
 
     Examples:
-    >>> is_harshad_number(18, 10)
+    >>> is_harshad_number_in_base(18, 10)
     True
-    >>> is_harshad_number(21, 10)
+    >>> is_harshad_number_in_base(21, 10)
     True
-    >>> is_harshad_number(-21, 5)
+    >>> is_harshad_number_in_base(-21, 5)
     False
     >>> # bases beyond 36 and below 2 will error
-    >>> is_harshad_number(45, 37)
+    >>> is_harshad_number_in_base(45, 37)
     Traceback (most recent call last):
         ...
-    ValueError: 'base_of_interest' must be between 36 and 2 inclusive
-    >>> is_harshad_number(45, 1)
+    ValueError: 'base' must be between 36 and 2 inclusive
+    >>> is_harshad_number_in_base(45, 1)
     Traceback (most recent call last):
         ...
-    ValueError: 'base_of_interest' must be between 36 and 2 inclusive
+    ValueError: 'base' must be between 36 and 2 inclusive
     """
 
-    if (base_of_interest > 36) or (base_of_interest < 2):
-        raise ValueError("'base_of_interest' must be between 36 and 2 inclusive")
+    if (base > 36) or (base < 2):
+        raise ValueError("'base' must be between 36 and 2 inclusive")
 
     if num < 0:
         return False
 
-    n = int_to_base(num, base_of_interest)
-    d = sum_of_digits(num, base_of_interest)
-    return int(n, base_of_interest) % int(d, base_of_interest) == 0
+    n = int_to_base(num, base)
+    d = sum_of_digits(num, base)
+    return int(n, base) % int(d, base) == 0
 
 
 if __name__ == "__main__":
