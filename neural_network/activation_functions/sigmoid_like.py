8f37073e9aa000436f1307bf74f83ab10e9c6fc3
Alexander Dicke <adicke.math@gmail.com>
2023-09-09 09:38:33 +0200

    removes _base_activation

--- neural_network/activation_functions/sigmoid_like.py
+++ neural_network/activation_functions/sigmoid_like.py
@@ -1,26 +1,6 @@
 import numpy as np
 
 
-def _base_activation(vector: np.ndarray, alpha: float, beta: float) -> np.ndarray:
-    """
-    Base activation for sigmoid, swish, and SiLU.
-    Examples:
-    >>> result = _base_activation(np.array([0, np.log(2), np.log(5)]), 0, 1)
-    >>> np.linalg.norm(np.array([0.5, 0.66666667, 0.83333333]) - result) < 10**(-5)
-    True
-    >>> result = _base_activation(np.array([1, 2, 3]), 1, 0)
-    >>> np.linalg.norm(np.array([0.5, 1., 1.5]) - result) < 10**(-5)
-    True
-    >>> result = _base_activation(np.array([0, 1, 2]), 1, np.log(2))
-    >>> np.linalg.norm(np.array([0, 0.66666667, 1.6]) - result) < 10**(-5)
-    True
-    >>> result = _base_activation(np.array([0, 1, np.log(2)]), 1, 1)
-    >>> np.linalg.norm(np.array([0, 0.7310585, 0.462098]) - result) < 10**(-5)
-    True
-    """
-    return np.power(vector, alpha) / (1 + np.exp(-beta * vector))
-
-
 def sigmoid(vector: np.ndarray) -> np.ndarray:
     """
     The standard sigmoid function.
@@ -28,12 +8,12 @@ def sigmoid(vector: np.ndarray) -> np.ndarray:
         vector: (np.ndarray): The input array.
     Returns:
         np.ndarray: The result of the sigmoid activation applied to the input array.
-    Examples:
-    >>> result = sigmoid(vector=np.array([0, np.log(2), np.log(5)]))
-    >>> np.linalg.norm(np.array([0.5, 0.66666667, 0.83333333]) - result) < 10**(-5)
+
+    >>> np.linalg.norm(np.array([0.5, 0.66666667, 0.83333333]) \
+        - sigmoid(vector=np.array([0, np.log(2), np.log(5)]))) < 10**(-5)
     True
     """
-    return _base_activation(vector, alpha=0, beta=1)
+    return 1 / (1 + np.exp(-1 * vector))
 
 
 def swish(vector: np.ndarray, beta: float) -> np.ndarray:
@@ -44,15 +24,15 @@ def swish(vector: np.ndarray, beta: float) -> np.ndarray:
         beta: (float)
     Returns:
         np.ndarray: The result of the swish activation applied to the input array.
-    Examples:
-    >>> result = swish(np.array([1, 2, 3]), 0)
-    >>> np.linalg.norm(np.array([0.5, 1., 1.5]) - result) < 10**(-5)
+
+    >>> np.linalg.norm(np.array([0.5, 1., 1.5]) \
+            - swish(np.array([1, 2, 3]), 0)) < 10**(-5)
     True
-    >>> result = swish(np.array([0, 1, 2]), np.log(2))
-    >>> np.linalg.norm(np.array([0, 0.66666667, 1.6]) - result) < 10**(-5)
+    >>> np.linalg.norm(np.array([0, 0.66666667, 1.6]) \
+            - swish(np.array([0, 1, 2]), np.log(2))) < 10**(-5)
     True
     """
-    return _base_activation(vector, alpha=1, beta=beta)
+    return vector / (1 + np.exp(-beta * vector))
 
 
 def sigmoid_linear_unit(vector: np.ndarray) -> np.ndarray:
@@ -60,15 +40,14 @@ def sigmoid_linear_unit(vector: np.ndarray) -> np.ndarray:
     SiLU activation: https://arxiv.org/abs/1606.08415
     Args:
         vector: (np.ndarray): The input array.
-
     Returns:
         np.ndarray: The result of the sigmoid linear unit applied to the input array.
-    Examples:
-    >>> result = sigmoid_linear_unit(np.array([0, 1, np.log(2)]))
-    >>> np.linalg.norm(np.array([0, 0.7310585, 0.462098]) - result) < 10**(-5)
+
+    >>> np.linalg.norm(np.array([0, 0.7310585, 0.462098]) \
+            - sigmoid_linear_unit(np.array([0, 1, np.log(2)]))) < 10**(-5)
     True
     """
-    return swish(vector, beta=1)
+    return vector / (1 + np.exp(-1 * vector))
 
 
 if __name__ == "__main__":
