5df5d31f5e35fb733f199fdeb334b1d15189d2b2
Jaspe Micael Ingabire <micaeljaspe@gmail.com>
2023-09-10 17:03:56 +0200

    Update of the krishnamurthy number validator
    - Added the description of a Krishnamurthy number
    - Added edge cases test (exception cases)

--- maths/krishnamurthy_number_2.py
+++ maths/krishnamurthy_number_2.py
@@ -1,50 +1,81 @@
+import doctest
 
-#IMPLEMENTATION OF THE 2ND VERSION OF THE KRISHNAMURTHY NUMBER VALIDATOR
-
-import sys
+"""
+A Krishnamurthy number is a number that is equal to the sum of the factorials of its digits.
+for instance, 145 = 1! + 4! + 5! ; 40585 = 4! + 0! + 5! + 8! + 5!, but 78 != 7! + 8!
+"""
 
 def factorial(n: int) -> int: 
-    #Function to calculate the factorial of the number n
-    #n! = n*(n-1)*(n-2)*...*2*1
+    
+    # Function to calculate the factorial of a number n
+    # n! = n*(n-1)*(n-2)*...*2*1, that is the definition of the factorial of n (n!)
+
     """
+    >>> factorial(7)
+    5040
+    >>> factorial("some characters") #like '1' or 'a'
+    Traceback (most recent call last):
+      File "<stdin1>", line 1, in <module>
+    TypeError: Only integers are allowed
     >>> factorial(1)
     1
+    >>> factorial(-4)
+    Traceback (most recent call last):
+      File "<stdin>", line 1, in <module>
+    ValueError: Only positive integers are allowed
     >>> factorial(0)
     1
     >>> factorial(4)
     24
-    >>> factorial(7)
-    5040
     """
 
-    if n<2: return 1
-    else: return n*factorial(n-1)
+    if not isinstance(n,int): #If n is not an integer
+        raise TypeError("Only integers are allowed")
+
+    if n<0: #If n is a negative number
+        raise ValueError("Only positive integers are allowed")
+
+    if n<2:
+        return 1 #Since 1! = 1 and 0! = 1, this is the base case
+    else:
+        return n*factorial(n-1)
 
 def krishnamurthy_2(x: int) -> bool:
+
     """
     >>> krishnamurthy_2(5)
     False
     >>> krishnamurthy_2(2)
     True
+    >>> krishnamurthy_2("characters") #like '4' or 'c'
+    Traceback (most recent call last):
+      File "<stdin>", line 1, in <module>
+    TypeError: Only integers are allowed
     >>> krishnamurthy_2(145)
     True
     >>> krishnamurthy_2(13)
     False
     >>> krishnamurthy_2(40585)
     True
+    >>> krishnamurthy_2(-4)
+    Traceback (most recent call last):
+      File "<stdin>", line 1, in <module>
+    ValueError: Only positive integers are allowed
     """
-    if not isinstance(x,int): raise TypeError("Only integers are allowed")
-    if x<0: raise ValueError("Only positive integers are allowed")
+
+    #only int variables are allowed
+    if not isinstance(x,int):
+        raise TypeError("Only integers are allowed")
+
+    #Negative numbers are prohibited
+    if x<0:
+        raise ValueError("Only positive integers are allowed")
 
     #Calculating the factorial of each digit and storing the result in a list
     factorial_sums = (factorial(int(i)) for i in str(x))
 
-    return x==sum(factorial_sums) #True if the sum of the elements in the list is equal to the number x
+    return x==sum(factorial_sums)
 
 if __name__=='__main__':
-    x = int(input("Number: "))
-    if krishnamurthy_2(x): print(f"{x} is a krishnamurthy number")
-    else: print(f"{x} is not a krishnamurthy number")
-    sys.exit(0)
-
-
+    #To see the details about the test, add print() to the next line
+    doctest.testmod()
