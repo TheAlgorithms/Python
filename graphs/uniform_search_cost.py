fe271c3cb7b48ad9793394d9552645867a8ff06c
miltonbhowmick <miltonbhowmick6@gmail.com>
2023-09-11 11:52:20 +0600

    Added floating and 0 distance test cases as reviewer suggestions

--- graphs/uniform_search_cost.py
+++ graphs/uniform_search_cost.py
@@ -35,6 +35,27 @@ def uniform_search_cost(
     >>> start = 0
     >>> uniform_search_cost(graph_data, distance, goal, start)
     [1]
+
+    >>> graph_data = [[1, 2], [2, 3], [], [0, 2]]
+    >>> distance = {(0,1):2.91,(0,2):1.1,(1,2):3,(1,3):2.3,(3,0):2.34,(3,2):2.15}
+    >>> goal = [2]
+    >>> start = 0
+    >>> uniform_search_cost(graph_data, distance, goal, start)
+    [1.1]
+
+    >>> graph_data = [[1, 2], [2, 3], [], [0, 2]]
+    >>> distance = {(0,1):2.91,(0,2):0,(1,2):3,(1,3):2.3,(3,0):2.34,(3,2):2.15}
+    >>> goal = [2]
+    >>> start = 0
+    >>> uniform_search_cost(graph_data, distance, goal, start)
+    [0]
+
+    >>> graph_data = [[1, 2], [2, 3], [], [0, 2]]
+    >>> distance = {(0,1):1.3,(0,2):2.6,(1,2):0,(1,3):2.3,(3,0):2.34,(3,2):2.15}
+    >>> goal = [2]
+    >>> start = 0
+    >>> uniform_search_cost(graph_data, distance, goal, start)
+    [1.3]
     """
     # priority queue
     heap = []
@@ -94,7 +115,7 @@ def run() -> None:
     """
     Return nothing and prints only the output
     >>> run()
-    start: 0, goal: 6; Distance(Minimum/Lowest): 1
+    start: 0, goal: 6; Distance(Minimum/Lowest): 11
     """
     # create the graph
     #
