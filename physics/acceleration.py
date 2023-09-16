5f1f043209720a96bf6a01a7c2aaf33f62f1c951
David Scott <ds09477@georgiasouthern.edu>
2023-09-16 15:39:35 -0400

    Added acceleration.py to the physics folder, allowing for two different formulas depending on the input paramteters.

--- physics/acceleration.py
+++ physics/acceleration.py
@@ -1,25 +1,28 @@
-def calculate_acceleration(*args):
+def calculate_acceleration(force=None, mass=None, vi=None, vf=None, t=None):
     """
-    Calculate acceleration using either F = ma or a = (vf - vi) / t.
+    Calculate acceleration using either F = ma or change in velocity formula.
 
-    Arguments:
-    - If two arguments are provided: (force, mass)
-    - If three arguments are provided: (vi, vf, t)
-
+    :param force: Force in Newtons (N) (default is None).
+    :param mass: Mass in kilograms (kg) (default is None).
+    :param vi: Initial velocity in m/s (default is None).
+    :param vf: Final velocity in m/s (default is None).
+    :param t: Time in seconds (default is None).
     :return: Acceleration in meters per second squared (m/s^2).
-    """
 
-    if len(args) == 2:
-        force, mass = args
+    Examples:
+    >>> calculate_acceleration(50, 10)
+    5.0
+
+    >>> calculate_acceleration(vi=18.5, vf=46.1, t=2.47)
+    11.17408906882591
+    """
+    if force is not None and mass is not None:
         if mass == 0:
             raise ValueError("Mass cannot be zero.")
         acceleration = force / mass
-    elif len(args) == 3:
-        vi, vf, t = args
+    elif vi is not None and vf is not None and t is not None:
         acceleration = (vf - vi) / t
     else:
-        raise ValueError("Invalid number of arguments. Use either (force, mass) or (vi, vf, t) combinations.")
+        raise ValueError("Insufficient information provided to calculate acceleration.")
 
     return acceleration
-
-print(calculate_acceleration(18.5, 46.1, 2.47))
