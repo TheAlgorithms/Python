8de62d97785a8993670851e450ec82b3f8c066c1
Alexander Dicke <adicke.math@gmail.com>
2023-09-17 11:06:13 +0200

    feedback

--- neural_network/activation_functions/sigmoid_like.py
+++ neural_network/activation_functions/sigmoid_like.py
@@ -9,11 +9,11 @@ def sigmoid(vector: np.ndarray) -> np.ndarray:
     Returns:
         np.ndarray: The result of the sigmoid activation applied to the input array.
 
-    >>> np.linalg.norm(np.array([0.5, 0.66666667, 0.83333333]) \
-        - sigmoid(vector=np.array([0, np.log(2), np.log(5)]))) < 10**(-5)
+    >>> np.linalg.norm(np.array([0.5, 0.66666667, 0.83333333])
+    ... - sigmoid(vector=np.array([0, np.log(2), np.log(5)]))) < 10**(-5)
     True
     """
-    return 1 / (1 + np.exp(-1 * vector))
+    return 1 / (1 + np.exp(-vector))
 
 
 def swish(vector: np.ndarray, beta: float) -> np.ndarray:
@@ -25,11 +25,11 @@ def swish(vector: np.ndarray, beta: float) -> np.ndarray:
     Returns:
         np.ndarray: The result of the swish activation applied to the input array.
 
-    >>> np.linalg.norm(np.array([0.5, 1., 1.5]) \
-            - swish(np.array([1, 2, 3]), 0)) < 10**(-5)
+    >>> np.linalg.norm(np.array([0.5, 1., 1.5])
+    ... - swish(np.array([1, 2, 3]), 0)) < 10**(-5)
     True
-    >>> np.linalg.norm(np.array([0, 0.66666667, 1.6]) \
-            - swish(np.array([0, 1, 2]), np.log(2))) < 10**(-5)
+    >>> np.linalg.norm(np.array([0, 0.66666667, 1.6])
+    ... - swish(np.array([0, 1, 2]), np.log(2))) < 10**(-5)
     True
     """
     return vector / (1 + np.exp(-beta * vector))
@@ -43,11 +43,11 @@ def sigmoid_linear_unit(vector: np.ndarray) -> np.ndarray:
     Returns:
         np.ndarray: The result of the sigmoid linear unit applied to the input array.
 
-    >>> np.linalg.norm(np.array([0, 0.7310585, 0.462098]) \
-            - sigmoid_linear_unit(np.array([0, 1, np.log(2)]))) < 10**(-5)
+    >>> np.linalg.norm(np.array([0, 0.7310585, 0.462098])
+    ... - sigmoid_linear_unit(np.array([0, 1, np.log(2)]))) < 10**(-5)
     True
     """
-    return vector / (1 + np.exp(-1 * vector))
+    return vector / (1 + np.exp(-vector))
 
 
 if __name__ == "__main__":
