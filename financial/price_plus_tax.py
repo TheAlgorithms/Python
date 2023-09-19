69cac1c23b14af17767788fb558ea819e3acff6c
Tongcai618 <134289249+Tongcai618@users.noreply.github.com>
2023-09-19 13:40:06 -0400

    Update price_plus_tax.py
    I change the comment above to make the inputs more clear to read. Also, I add some conditions to prevent some situations that the price or tax rate is not legal.

--- financial/price_plus_tax.py
+++ financial/price_plus_tax.py
@@ -1,5 +1,7 @@
 """
-Calculate price plus tax of a good or service given its price and a tax rate.
+Calculate price plus tax of a good or service given 
+- Price
+- Tax rate
 """
 
 
@@ -10,6 +12,13 @@ def price_plus_tax(price: float, tax_rate: float) -> float:
     >>> price_plus_tax(125.50, 0.05)
     131.775
     """
+    # If the price is negative, it will raise a warning
+    if price <= 0:
+        raise Exception("The price must be >= 0")
+    # If the tax rate is negative or zero, it will raise a warning
+    if tax_rate <= 0:
+        raise Exception("The tax rate must be > 0")
+        
     return price * (1 + tax_rate)
 
 
