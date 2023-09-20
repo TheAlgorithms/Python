52e28e5ec91b432922661c2604ff8caee8e37f21
Chris O <46587501+ChrisO345@users.noreply.github.com>
2023-09-20 14:58:19 +1200

    changed maps to list comprehension

--- ciphers/base32.py
+++ ciphers/base32.py
@@ -18,7 +18,9 @@ def base32_encode(data: bytes) -> bytes:
     binary_data = "".join([bin(ord(d))[2:].zfill(8) for d in data.decode("utf-8")])
     binary_data = binary_data.ljust(5 * ((len(binary_data) // 5) + 1), "0")
     b32_chunks = map("".join, zip(*[iter(binary_data)] * 5))
-    b32_result = "".join(j for j in map(lambda _s: B32_CHARSET[int(_s, 2)], b32_chunks))
+    b32_result = "".join(
+        j for j in [B32_CHARSET[int(chunk, 2)] for chunk in b32_chunks]
+    )
     return bytes(b32_result.ljust(8 * ((len(b32_result) // 8) + 1), "="), "utf-8")
 
 
@@ -32,13 +34,13 @@ def base32_decode(data: bytes) -> bytes:
     b'some long complex string'
     """
     binary_chunks = "".join(
-        map(
-            lambda _d: bin(B32_CHARSET.index(_d))[2:].zfill(5),
-            data.decode("utf-8").strip("="),
-        )
+        [
+            bin(B32_CHARSET.index(_d))[2:].zfill(5)
+            for _d in data.decode("utf-8").strip("=")
+        ]
     )
     binary_data = list(map("".join, zip(*[iter(binary_chunks)] * 8)))
-    return bytes("".join(list(map(lambda _d: chr(int(_d, 2)), binary_data))), "utf-8")
+    return bytes("".join([chr(int(_d, 2)) for _d in binary_data]), "utf-8")
 
 
 if __name__ == "__main__":
