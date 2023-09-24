2a2015e4ad1da698d6116fc7a5d7a2936484ac7d
Chris O <46587501+ChrisO345@users.noreply.github.com>
2023-09-24 19:10:31 +1300

    Apply suggestions from code review
    Co-authored-by: Tianyi Zheng <tianyizheng02@gmail.com>

--- ciphers/base85.py
+++ ciphers/base85.py
@@ -10,7 +10,7 @@ def _base10_to_85(d: int) -> str:
 
 
 def _base85_to_10(digits: list) -> int:
-    return sum(char * 85**i for i, char in enumerate(digits[::-1]))
+    return sum(char * 85**i for i, char in enumerate(reversed(digits)))
 
 
 def ascii85_encode(data: bytes) -> bytes:
@@ -22,13 +22,11 @@ def ascii85_encode(data: bytes) -> bytes:
     >>> ascii85_encode(b"base 85")
     b'@UX=h+?24'
     """
-    binary_data = "".join([bin(ord(d))[2:].zfill(8) for d in data.decode("utf-8")])
+    binary_data = "".join(bin(ord(d))[2:].zfill(8) for d in data.decode("utf-8"))
     null_values = (32 * ((len(binary_data) // 32) + 1) - len(binary_data)) // 8
     binary_data = binary_data.ljust(32 * ((len(binary_data) // 32) + 1), "0")
     b85_chunks = [int(_s, 2) for _s in map("".join, zip(*[iter(binary_data)] * 32))]
-    result = "".join(
-        item for item in [_base10_to_85(chunk)[::-1] for chunk in b85_chunks]
-    )
+    result = "".join(_base10_to_85(chunk)[::-1] for chunk in b85_chunks)
     return bytes(result[:-null_values] if null_values % 4 != 0 else result, "utf-8")
 
 
@@ -45,13 +43,13 @@ def ascii85_decode(data: bytes) -> bytes:
     binary_data = data.decode("utf-8") + "u" * null_values
     b85_chunks = map("".join, zip(*[iter(binary_data)] * 5))
     b85_segments = [[ord(_s) - 33 for _s in chunk] for chunk in b85_chunks]
-    results = [(bin(_base85_to_10(chunk))[2::].zfill(32)) for chunk in b85_segments]
+    results = [bin(_base85_to_10(chunk))[2::].zfill(32) for chunk in b85_segments]
     char_chunks = [
         [chr(int(_s, 2)) for _s in map("".join, zip(*[iter(r)] * 8))] for r in results
     ]
     result = "".join("".join(char) for char in char_chunks)
-    offset = 0 if null_values % 5 != 0 else 1
-    return bytes(result[: -null_values + offset], "utf-8")
+    offset = int(null_values % 5 == 0)
+    return bytes(result[: offset - null_values], "utf-8")
 
 
 if __name__ == "__main__":
