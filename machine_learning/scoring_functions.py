ca085fad64373e7faaa3510aa764e0800484f072
Tongcai618 <134289249+Tongcai618@users.noreply.github.com>
2023-09-25 22:48:50 -0400

    Update scoring_functions.py

--- machine_learning/scoring_functions.py
+++ machine_learning/scoring_functions.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 """ Here I implemented the scoring functions.
-    MAE, MSE, RMSE, RMSLE are included.
+    MAE, MAPE, MSE, RMSE, RMSLE are included.
 
     Those are used for calculating differences between
     predicted values and actual values.
@@ -11,18 +11,26 @@ import numpy as np
 
     Using log and roots can be perceived as tools for penalizing big
     errors. However, using appropriate metrics depends on the situations,
-    and types of data
+    and types of data.
 """
 
 
 # Mean Absolute Error
-def mae(predict, actual):
+def mae(predict: list, actual: list) -> float:
     """
+    Function: mae
+        The MAE score is measured as the average of the absolute error values.
+        This function calculates the absolute difference between actual value 
+        and predict value. Lower MAE means that a model's predictions are
+        closer the actual value, which usually tells that the model has high 
+        forecast accuracy.
+    Parameters:
+        predict - list - the predict value
+        actual - list - the actual value
     Examples(rounded for precision):
-    >>> actual = [1,2,3];predict = [1,4,3]
+    >>> predict = [1,4,3]; actual = [1,2,3]
     >>> np.around(mae(predict,actual),decimals = 2)
     0.67
-
     >>> actual = [1,1,1];predict = [1,1,1]
     >>> mae(predict,actual)
     0.0
@@ -35,16 +43,51 @@ def mae(predict, actual):
 
     return score
 
+def mape(predict: list, actual: list) -> float:
+    """
+    Function: mape
+        The MAPE score is measured as the average of the percentage absolute 
+        error values. This function calculates the absolute percentage 
+        difference between actual value and predict value. Lower MAPE means 
+        that a model's predictions are closer the actual value, which usually 
+        tells that the model has high forecast accuracy.
+    Parameters:
+        predict - list - the predict value
+        actual - list - the actual value
+    Example(rounded for precision):
+    >>> predict = [1,4,3]; actual = [1,2,3]
+    >>> np.around(mape(predict,actual), decimals = 2)
+    0.33
+    >>> predict = [1,1,1]; actual = [1,1,1]
+    >>> mape(predict, actual)
+    0.0
+    
+    """
+    predict = np.array(predict)
+    actual = np.array(actual)
+    pct_difference = abs((predict - actual)/actual)
+    score = pct_difference.mean()
+
+    return score
+
 
 # Mean Squared Error
-def mse(predict, actual):
+def mse(predict: list, actual: list) -> float:
     """
+    Function: mse
+        The MSE score is measured as the average squared error values.
+        This function calculates the square of absolute difference 
+        between actual value and predict value. Lower MSE means that 
+        a model's predictions are closer the actual value, which usually 
+        tells that the model has high forecast accuracy.
+    Parameters:
+        predict - list - the predict value
+        actual - list - the actual value
     Examples(rounded for precision):
-    >>> actual = [1,2,3];predict = [1,4,3]
+    >>> predict = [1,4,3]; actual = [1,2,3]
     >>> np.around(mse(predict,actual),decimals = 2)
     1.33
-
-    >>> actual = [1,1,1];predict = [1,1,1]
+    >>> predict = [1,1,1]; actual = [1,1,1]
     >>> mse(predict,actual)
     0.0
     """
@@ -61,8 +104,19 @@ def mse(predict, actual):
 # Root Mean Squared Error
 def rmse(predict, actual):
     """
+    Function: mse
+        The RMSE (root-mean-square error) represents the square root
+        of MSE. The RMSE score is measured as the average difference 
+        between model's predicted value and the actual value.
+        This function calculates the average difference between actual 
+        value and predict value. Lower RMSE means that a model's 
+        predictions are closer the actual value, which usually tells 
+        that the model has high forecast accuracy.
+    Parameters:
+        predict - list - the predict value
+        actual - list - the actual value
     Examples(rounded for precision):
-    >>> actual = [1,2,3];predict = [1,4,3]
+    >>> predict = [1,4,3]; actual = [1,2,3]
     >>> np.around(rmse(predict,actual),decimals = 2)
     1.15
 
@@ -84,7 +138,7 @@ def rmse(predict, actual):
 def rmsle(predict, actual):
     """
     Examples(rounded for precision):
-    >>> actual = [10,10,30];predict = [10,2,30]
+    >>> predict = [10,2,30]; actual = [10,10,30]
     >>> np.around(rmsle(predict,actual),decimals = 2)
     0.75
 
@@ -116,12 +170,12 @@ def mbd(predict, actual):
     Example(rounded for precision):
 
     Here the model overpredicts
-    >>> actual = [1,2,3];predict = [2,3,4]
+    >>> predict = [2,3,4]; actual = [1,2,3]
     >>> np.around(mbd(predict,actual),decimals = 2)
     50.0
 
     Here the model underpredicts
-    >>> actual = [1,2,3];predict = [0,1,1]
+    >>> predict = [0,1,1]; actual = [1,2,3]
     >>> np.around(mbd(predict,actual),decimals = 2)
     -66.67
     """
@@ -139,3 +193,7 @@ def mbd(predict, actual):
 
 def manual_accuracy(predict, actual):
     return np.mean(np.array(actual) == np.array(predict))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
