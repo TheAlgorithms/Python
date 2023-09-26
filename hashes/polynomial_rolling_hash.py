a5e27f29153ba8990a15ba846b4cbc456956f377
Prasad Ashok Chavan <97186762+prasad-chavan1@users.noreply.github.com>
2023-09-26 14:36:50 +0530

    Update polynomial_rolling_hash.py

--- hashes/polynomial_rolling_hash.py
+++ hashes/polynomial_rolling_hash.py
@@ -6,26 +6,28 @@ Time Complexity: O(N)
 
 Auxiliary Space: O(1)
 
+Wikipedia link: https://en.wikipedia.org/wiki/Rolling_hash
+
 """
-
-
 class Hash:
-    def __init__(self, s: str):
+
+    def __init__(self, s=str):
         self.hash_value = 0
-        self.p, self.m = 31, 10**9 + 7
+        (self.p, self.m) = (31, 10 ** 9 + 7)
         self.length = len(s)
         hash_so_far = 0
         p_pow = 1
         for i in range(self.length):
-            hash_so_far = (hash_so_far + (1 + ord(s[i]) - ord("a")) * p_pow) % self.m
-            p_pow = (p_pow * self.p) % self.m
+            hash_so_far = (hash_so_far + (1 + ord(s[i]) - ord('a'))
+                           * p_pow) % self.m
+            p_pow = p_pow * self.p % self.m
         self.hash_value = hash_so_far
 
     def __eq__(self, other):
         return self.hash_value == other.hash_value
 
 
-if __name__ == "__main__":
-    s = "thealgorithms_python"
+if __name__ == '__main__':
+    s = 'thealgorithms_python'
     h = Hash(s)
-    print("Hash value of {} is {}".format(s, h.hash_value))
+    print('The hash value of '+ s +' is '+ h.hash_value)
