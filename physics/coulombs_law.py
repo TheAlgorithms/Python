a8bd7cc997f919f2e80073a4be68aa3f1ef3da66
Tianyi Zheng <tianyizheng02@gmail.com>
2023-09-27 01:39:32 -0400

    Update coulombs_law.py

--- physics/coulombs_law.py
+++ physics/coulombs_law.py
@@ -1,16 +1,12 @@
 """
-Description:
-Coulombs law states that the magnitude of the electrostatic force of attraction
+Coulomb's law states that the magnitude of the electrostatic force of attraction
 or repulsion between two point charges is directly proportional to the product
 of the magnitudes of charges and inversely proportional to the square of the
 distance between them.
 
-Coulomb studied the repulsive force between bodies having electrical charges
-of the same sign.
+F = k * q1 * q2 / r^2
 
-F = k*q1*q2/r^2
-
-k is Coulomb's constant and equals 1/(4π*ε0).
+k is Coulomb's constant and equals 1/(4π*ε0)
 q1 is charge of first body (C)
 q2 is charge of second body (C)
 r is distance between two charged bodies (m)
@@ -24,15 +20,15 @@ def coulombs_law(q1: float, q2: float, radius: float) -> float:
     Calculate the electrostatic force of attraction or repulsion
     between two point charges
 
-    >>> coulombs_law(15.5,20,15)
+    >>> coulombs_law(15.5, 20, 15)
     12382849136.06
-    >>> coulombs_law(1,15,5)
+    >>> coulombs_law(1, 15, 5)
     5392531075.38
-    >>> coulombs_law(20,-50,15)
+    >>> coulombs_law(20, -50, 15)
     -39944674632.44
-    >>> coulombs_law(-5,-8,10)
+    >>> coulombs_law(-5, -8, 10)
     3595020716.92
-    >>> coulombs_law(50,100,50)
+    >>> coulombs_law(50, 100, 50)
     17975103584.6
     """
     if radius <= 0:
@@ -43,4 +39,4 @@ def coulombs_law(q1: float, q2: float, radius: float) -> float:
 if __name__ == "__main__":
     import doctest
 
-    doctest.testmod(verbose=True)
+    doctest.testmod()
