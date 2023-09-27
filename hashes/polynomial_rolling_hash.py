d699d85c2fdc5b2b343311c95888b51b5ca89a4e
Prasad Ashok Chavan <97186762+prasad-chavan1@users.noreply.github.com>
2023-09-27 08:10:42 +0530

    Update polynomial_rolling_hash.py

--- hashes/polynomial_rolling_hash.py
+++ hashes/polynomial_rolling_hash.py
@@ -1,6 +1,7 @@
 """
 Polynomial Rolling Hash Algo -->
-Polynomial rolling hash function is a hash function that uses only multiplications and additions.
+Polynomial rolling hash function is a hash 
+function that uses only multiplications and additions.
 
 Time Complexity: O(N)
 
@@ -12,14 +13,14 @@ Wikipedia link: https://en.wikipedia.org/wiki/Rolling_hash
 
 
 class Hash:
-    def __init__(self, s: str):
+    def __init__(self, input_str: str) ->None:
         self.hash_value = 0
         self.p, self.m = 31, 10**9 + 7
-        self.length = len(s)
+        self.length = len(input_str)
         hash_so_far = 0
         p_pow = 1
         for i in range(self.length):
-            hash_so_far = (hash_so_far + (1 + ord(s[i]) - ord("a")) * p_pow) % self.m
+            hash_so_far = (hash_so_far + (1 + ord(input_str[i]) - ord("a")) * p_pow) % self.m
             p_pow = (p_pow * self.p) % self.m
         self.hash_value = hash_so_far
 
