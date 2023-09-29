27b2fb5144c8c6ec2303249dad8a4100cbfef724
Kiran Borge <borgekiran123@gmail.com>
2023-09-29 07:00:28 -0700

    Add Reverse Sort Algorithm
    This contribution adds a valuable sorting algorithm to our collection, providing users with an option for sorting elements in reverse order.
    
    The Reverse Sort algorithm can be beneficial in situations where descending order is a requirement.

---
+++ sorts/reverse_sort.py
@@ -0,0 +1,17 @@
+def reverse_sort(arr):
+    n = len(arr)
+    sorted = False
+
+    while not sorted:
+        sorted = True  
+        for i in range(n - 1):
+            if arr[i] < arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                sorted = False  
+
+
+my_list = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+print("Original List:", my_list)
+
+reverse_sort(my_list)
+print("Reverse Sorted List:", my_list)
