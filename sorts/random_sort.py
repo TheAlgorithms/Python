803166e2b753be1240a9afab7bc91786ba1f390c
Kiran Borge <borgekiran123@gmail.com>
2023-09-29 07:42:44 -0700

    updated

--- sorts/random_sort.py
+++ sorts/random_sort.py
@@ -1,14 +1,11 @@
 import random
 
-
 def random_sort(arr):
-    sorted_arr = sorted(arr)
-    while arr != sorted_arr:
+    while any(arr[i] > arr[i + 1] for i in range(len(arr) - 1)):
         random.shuffle(arr)
 
-
-my_list = [4, 2, 7, 1, 9, 5]
+my_list = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
 print("Original List:", my_list)
 
 random_sort(my_list)
-print("Sorted List:", my_list)
+print("Randomly Sorted List:", my_list)
