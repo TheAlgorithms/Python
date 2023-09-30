d9e9cf6ac7ff0f81b4b899f28132bc3ab95866b2
Nandhika Jhansi <91628798+Nandhika2003@users.noreply.github.com>
2023-10-01 02:41:48 +0530

    Create Word Break Problem.py

---
+++ backtracking/Word Break Problem.py
@@ -0,0 +1,24 @@
+def dictionaryContains(word):
+	dictionary = {"mobile", "samsung", "sam", "sung", "man",
+				"mango", "icecream", "and", "go", "i", "love", "ice", "cream"}
+	return word in dictionary
+
+def wordBreak(string):
+	wordBreakUtil(string, len(string), "")
+  
+def wordBreakUtil(string, n, result):
+	for i in range(1, n + 1):
+		prefix = string[:i]
+		if dictionaryContains(prefix):
+			if i == n:
+				result += prefix
+				print(result)
+				return
+			wordBreakUtil(string[i:], n - i, result+prefix+" ")
+      
+if __name__ == "__main__":
+	print("First Test:")
+	wordBreak("iloveicecreamandmango")
+
+	print("\nSecond Test:")
+	wordBreak("ilovesamsungmobile")
