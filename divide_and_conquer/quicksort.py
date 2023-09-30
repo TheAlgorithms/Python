067c2ac957f5dac6733ea810bf7dd99304188058
Alok Kumar Verma <as1492@srmist.edu.in>
2023-10-01 00:29:13 +0530

    Added QuickSort under Divide and Conquer
    Quicksort is a divide-and-conquer algorithm. Overall, it is slightly faster than merge sort and heapsort for randomized data, particularly on larger distributions.[

---
+++ divide_and_conquer/quicksort.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+def quick_sort(array: list) -> list:
+    """Returns a list of sorted array elements using quick sort.
+
+    >>> from random import shuffle
+    >>> array = [-2, 3, -10, 11, 99, 100000, 100, -200]
+    >>> shuffle(array)
+    >>> quick_sort(array)
+    [-200, -10, -2, 3, 11, 99, 100, 100000]
+
+    >>> shuffle(array)
+    >>> quick_sort(array)
+    [-200, -10, -2, 3, 11, 99, 100, 100000]
+
+    >>> array = [-200]
+    >>> quick_sort(array)
+    [-200]
+
+    >>> array = [-2, 3, -10, 11, 99, 100000, 100, -200]
+    >>> shuffle(array)
+    >>> sorted(array) == quick_sort(array)
+    True
+
+    >>> array = [-2]
+    >>> quick_sort(array)
+    [-2]
+
+    >>> array = []
+    >>> quick_sort(array)
+    []
+
+    >>> array = [10000000, 1, -1111111111, 101111111112, 9000002]
+    >>> sorted(array) == quick_sort(array)
+    True
+    """
+    if len(array) <= 1:
+        return array
+    
+    pivot = array[len(array) // 2]
+    left = [x for x in array if x < pivot]
+    middle = [x for x in array if x == pivot]
+    right = [x for x in array if x > pivot]
+    
+    return quick_sort(left) + middle + quick_sort(right)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
