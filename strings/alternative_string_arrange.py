f7a45d20cbd8951e355d11b6b0f527058eb68a05
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-09-30 19:09:44 +0530

    Add comments for alternative_string_arrange.py

--- strings/alternative_string_arrange.py
+++ strings/alternative_string_arrange.py
@@ -13,17 +13,29 @@ def alternative_string_arrange(first_str: str, second_str: str) -> str:
     >>> alternative_string_arrange("ABC", "")
     'ABC'
     """
+    # Calculate the lengths of the input strings.
     first_str_length: int = len(first_str)
     second_str_length: int = len(second_str)
+
+    # Determine the max length between the two input strings.
     abs_length: int = (
         first_str_length if first_str_length > second_str_length else second_str_length
     )
+
+    # Initialize an empty list to store the characters of the combined string.
     output_list: list = []
+
+    # Iterate through the characters up to the max length.
     for char_count in range(abs_length):
+        # Append a character from the first string if it exists at this position.
         if char_count < first_str_length:
             output_list.append(first_str[char_count])
+
+        # Append a character from the second string if it exists at this position.
         if char_count < second_str_length:
             output_list.append(second_str[char_count])
+
+    # Join the characters in the list to form the final combined string.
     return "".join(output_list)
 
 
