fc2644b43443afb9428693fecfb44104bd1b9504
ParamThakkar123 <paramthakar864@gmail.com>
2023-09-30 23:07:41 +0530

    added minimum_platforms.py file

--- greedy_methods/minimum_platforms.py
+++ greedy_methods/minimum_platforms.py
@@ -1,30 +1,29 @@
-
 from typing import List
- 
+
 # Function to find the minimum number of platforms required.
 def find_platform_optimized(arr: List[int], dep: List[int], n: int) -> int:
- 
+
     count = 0
     max_platforms = 0
- 
+
     # Find the maximum departure time
     max_departure_time = max(dep)
- 
+
     # Create a list to store the count of trains at each time
     v = [0] * (max_departure_time + 2)
- 
+
     # Increment the count at the arrival time and decrement at the departure time
     for i in range(n):
         v[arr[i]] += 1
         v[dep[i] + 1] -= 1
- 
+
     # Iterate over the list and keep track of the maximum sum seen so far
     for i in range(max_departure_time + 2):
         count += v[i]
         max_platforms = max(max_platforms, count)
- 
+
     return max_platforms
- 
+
 # Driver Code
 if __name__ == '__main__':
     arr = []
