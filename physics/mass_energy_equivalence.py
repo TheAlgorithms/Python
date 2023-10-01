91b1445a908e0a12e1494c3e5b7398acfbc98101
mhsuhail00 <112853656+mhsuhail00@users.noreply.github.com>
2023-10-02 00:48:01 +0530

    Update mass_energy_equivalence.py

--- physics/mass_energy_equivalence.py
+++ physics/mass_energy_equivalence.py
@@ -4,14 +4,14 @@ Reference: https://en.wikipedia.org/wiki/Mass%E2%80%93energy_equivalence
 
 Here it defines a Function to calculate energy(in joules) equivalent of a given
 mass(in kilograms) using Einstein's Mass-energy equivalence equation E=mc^2
-where m is mass and c is the speed of light in vacuum
-(a constant value) in meter per second.
+where m is mass and c = 299,792,458 meter per second is the speed of light
+in vacuum (a constant value).
 
 The function takes a single argument mass(in kg) having datatype float and
 returns the energy(in joule) having datatype float
 """
 
-
+speed_of_light = 299792458
 def calculate_energy(mass: float) -> float:
     """
     get energy equivalent of given mass in joules:
@@ -29,7 +29,6 @@ def calculate_energy(mass: float) -> float:
     """
     if mass < 0:
         raise TypeError("Mass cannot be negative")
-    speed_of_light = 299792458
     energy = mass * speed_of_light**2
     return energy
 
