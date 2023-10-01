90f562fc5252157a64f0bb91f3bcfb8cb0d48a7a
mhsuhail00 <112853656+mhsuhail00@users.noreply.github.com>
2023-10-01 14:49:06 +0530

    Update mass_energy_equivalence.py

--- physics/mass_energy_equivalence.py
+++ physics/mass_energy_equivalence.py
@@ -2,12 +2,9 @@
 Mass energy Equivalene
 Reference: https://en.wikipedia.org/wiki/Mass%E2%80%93energy_equivalence
 """
-import math
-
+import math as m
 """ Function to calculate energy(in joules) equivalent of a given
 mass(in kilograms) using Einstein's equation E=mc^2"""
-
-
 def calculate_energy(mass: float) -> float:
     """
     get energy equivalent of given mass in joules:
@@ -28,11 +25,9 @@ def calculate_energy(mass: float) -> float:
     # Speed of light in meters per second (approximately)
     speed_of_light = 299792458
     # Calculate energy using the equation E=mc^2
-    energy = mass * math.pow(speed_of_light, 2)
+    energy = mass * m.pow(speed_of_light, 2)
     # return value has unit Joules
     return energy
-
-
 if __name__ == "__main__":
     # Input the mass from the user
     mass = float(input("Enter mass (in kilograms): "))
