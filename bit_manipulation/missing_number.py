1a207520934b84e5922fbd540ac5a15e93cc8a14
Christian Clauss <cclauss@me.com>
2023-10-01 13:23:47 +0200

    Update bit_manipulation/missing_number.py

--- bit_manipulation/missing_number.py
+++ bit_manipulation/missing_number.py
@@ -1,12 +1,12 @@
-def find_missing_number(nums):
+def find_missing_number(nums: list[int]) -> int:
     """
     Finds the missing number in a list of consecutive integers.
 
     Args:
-        nums (List[int]): A list of integers.
+        nums: A list of integers.
 
     Returns:
-        int: The missing number.
+        The missing number.
 
     Example:
         >>> find_missing_number([0, 1, 3, 4])
