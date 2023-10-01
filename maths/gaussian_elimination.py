74b559739a1de935db1518e5fd47bb438a5954b0
milindaShehan <milinda.msd@gmail.com>
2023-10-02 02:14:44 +0530

    Added Gaussian Elimination Algorithm

--- maths/gaussian_elimination.py
+++ maths/gaussian_elimination.py
@@ -1,35 +1,22 @@
-from typing import List
-
-
-def get_matrix_input() -> List[List[float]]:
+def get_matrix_input() -> list[list[float]]:
     """
     Gets input for the coefficient matrix augmented with constants.
 
     Returns:
         A list of lists representing the matrix.
-
-    Example:
-    >>> input_data = "2\\n2 -1 1 8\\n-3 3 2 1"
-    >>> from io import StringIO
-    >>> import sys
-    >>> sys.stdin = StringIO(input_data)
-    >>> matrix = get_matrix_input()
-    >>> matrix
-    [[2.0, -1.0, 1.0, 8.0], [-3.0, 3.0, 2.0, 1.0]]
     """
     print("Enter the number of equations:", end=" ")
     num_eq = int(input())
     print(f"Enter coefficients for {num_eq} equations (one row at a time):")
-
+    
     matrix = []
     for _ in range(num_eq):
         row = list(map(float, input().split()))
         matrix.append(row)
-
+    
     return matrix
 
-
-def gaussian_elimination(matrix: List[List[float]]) -> List[float]:
+def gaussian_elimination(matrix: list[list[float]]) -> list[float]:
     """
     This function applies the Gaussian Elimination method to solve a system of linear equations.
 
@@ -38,12 +25,6 @@ def gaussian_elimination(matrix: List[List[float]]) -> List[float]:
 
     Returns:
         A list containing the solutions to the system of equations.
-
-    Example:
-    >>> matrix = [[2.0, -1.0, 1.0, 8.0], [-3.0, 3.0, 2.0, 1.0]]
-    >>> solutions = gaussian_elimination(matrix)
-    >>> solutions
-    [3.8888888888888875, 2.4444444444444438]
     """
     n = len(matrix)
 
@@ -65,7 +46,6 @@ def gaussian_elimination(matrix: List[List[float]]) -> List[float]:
     solutions = [row[-1] for row in matrix]
 
     return solutions
-
 
 if __name__ == "__main__":
     matrix = get_matrix_input()
