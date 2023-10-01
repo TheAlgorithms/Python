18e2c7cf70550b26bf95d6c088eeff65012f02f1
kosuri-indu <kosurilindu@gmail.com>
2023-10-01 09:51:15 +0530

    ruff errors sorted

--- project_euler/problem_095/sol1.py
+++ project_euler/problem_095/sol1.py
@@ -3,10 +3,10 @@ Problem 95
 Url: https://projecteuler.net/problem=95
 Statement:
 The proper divisors of a number are all the divisors excluding the number itself.
-For example, the proper divisors of  28 is 1,2,4,7 and 14 
+For example, the proper divisors of  28 is 1,2,4,7 and 14.
 As the sum of these divisors is equal to 28, we call it a perfect number.
 
-Interestingly the sum of the proper divisors of 220 is 284
+Interestingly the sum of the proper divisors of 220 is 284.
 The sum of the proper divisors of 284 is 220 forming a chain of two numbers.
 For this reason, 220 and 284 are called an amicable pair.
 Perhaps less well known are longer chains.
