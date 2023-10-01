f2888356e711e8cca0baf309d9764b8a8c5abfb2
sanju2728 <sanjanabhat2002@gmail.com>
2023-10-01 12:56:43 +0530

    added stringfunctions.py

--- strings/stringfunctions.py
+++ strings/stringfunctions.py
@@ -1,96 +1,83 @@
-''' These are the implementations of the basic string functions in python, 
-like isupper(), islower() and so on'''
-
-
-
 def is_upper(string):
-    '''
-    This function checks if a given string contains only upper case characters.
-    '''
-    if not string: 
-        return False
-    for character in string:
-        if ord(character) > 96 and ord(character) < 123: 
-            return False
-    return True
-
+    """
+    Check if all characters in the string are uppercase letters.
+    
+    Args:
+        string (str): The input string.
+        
+    Returns:
+        bool: True if all characters in the string are uppercase letters, False otherwise.
+    """
+    return all(ord(char) >= 65 and ord(char) <= 90 for char in string)
 
 def is_lower(string):
-    '''
-    This function checks if a given string contains only lower case characters.
-    '''
-    for character in string:
-        if ord(character) in range(65, 97):
-            return False
-    return True
-
+    """
+    Check if all characters in the string are lowercase letters.
+    
+    Args:
+        string (str): The input string.
+        
+    Returns:
+        bool: True if all characters in the string are lowercase letters, False otherwise.
+    """
+    return all(ord(char) >= 97 and ord(char) <= 122 for char in string)
 
 def is_alpha(string):
-    '''
-    This function checks whether all characters in a string are alphabetical
-    '''
-    if(not string): 
-        return False
-    for character in string:
-        if ((ord(character) in range(65, 91)) or (ord(character) in range(97, 123))):
-            continue
-        else:
-            return False
-    return True
-
+    """
+    Check if all characters in the string are alphabetical (letters).
+    
+    Args:
+        string (str): The input string.
+        
+    Returns:
+        bool: True if all characters in the string are letters, False otherwise.
+    """
+    return all(char.isalpha() for char in string)
 
 def is_alnum(string):
-    '''
-    This function checks if all the characters in a string are alphanumeric
-    '''
-    if(not string): return False
-    for character in string:
-        if ((ord(character) in range(65, 91)) or (ord(character) in range(97, 123)) or (ord(character) in range(48, 58))):
-            continue
-        else:
-            return False
-    return True
-
+    """
+    Check if all characters in the string are alphanumeric (letters or digits).
+    
+    Args:
+        string (str): The input string.
+        
+    Returns:
+        bool: True if all characters in the string are letters or digits, False otherwise.
+    """
+    return all(char.isalnum() for char in string)
 
 def is_decimal(string):
-    '''
-    This function returns true if all of the characters in the string are digits
-    '''
-    if(not string): return False
-    for character in string:
-        if(ord(character) not in range(48, 58)):
-            return False 
-    return True
-
+    """
+    Check if all characters in the string are decimal digits.
+    
+    Args:
+        string (str): The input string.
+        
+    Returns:
+        bool: True if all characters in the string are decimal digits, False otherwise.
+    """
+    return all(char.isdigit() for char in string)
 
 def is_space(string):
-    '''
-    This function checks for the strings constituing only of spaces, tabs and newlines
-    '''
-    if(not string): return False
-    for character in string:
-        if(character != ' '):        # solve for newline pending
-            return False
-    return True
-
+    """
+    Check if all characters in the string are whitespace characters.
+    
+    Args:
+        string (str): The input string.
+        
+    Returns:
+        bool: True if all characters in the string are whitespace characters, False otherwise.
+    """
+    return all(char.isspace() for char in string)
 
 def is_title(string):
-    '''
-    This function checks that every word starts with an uppercase letter.
-    '''
-    for substring in string.split():
-        if(ord(substring[0]) not in range(65, 91)):
-            return False
-        for character in range(1, len(substring)):
-            if(is_upper(substring[character])): 
-                return False
-    return True
-
-print(is_upper("HEY"))                          # True
-print(is_lower("there"))                        # True
-print(is_alpha("you"))                          # True
-print(is_alnum("are3"))                         # True
-print(is_decimal("400"))                        # True
-print(is_space("   "))                          # True
-print(is_title("Times Pretty"))                 # True
-            
+    """
+    Check if the string is in title case (first letter of each word is capital).
+    
+    Args:
+        string (str): The input string.
+        
+    Returns:
+        bool: True if the string is in title case, False otherwise.
+    """
+    return string.istitle()
