d8b39f77ff0e44636d68a68c908846e7c44c3f57
Prithu-Nagar <56398437+Prithu-Nagar@users.noreply.github.com>
2023-10-01 12:04:51 +0530

    Update quick_sort.py

--- sorts/quick_sort.py
+++ sorts/quick_sort.py
@@ -43,6 +43,10 @@ def quick_sort(collection: list) -> list:
 
 
 if __name__ == "__main__":
+    import time
     user_input = input("Enter numbers separated by a comma:\n").strip()
     unsorted = [int(item) for item in user_input.split(",")]
+    start = time.process_time()
     print(quick_sort(unsorted))
+    stop = time.process_time()
+    print(f"Processing time: {(stop-start)%1e9 + 7}")
