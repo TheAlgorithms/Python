4fc0e09a17c63ba6cbd824652030e9ba1cd973a7
dwarkadhish kamthane <dwarkakamthane@gmail.com>
2023-10-01 22:11:39 +0530

    Einstein's Energy mass Equivalence
    This code returns the energy when mass is inputed

--- physics/Einstein's Energy-Mass Equivalence.py
+++ physics/Einstein's Energy-Mass Equivalence.py
@@ -1,15 +1,16 @@
-def energy_equivalence(mass_kg):
+c= 299792458
+def energy_equivalence(mass):
    
     # Check if the mass is non-negative
     if mass_kg < 0:
         raise ValueError("mass should be positive")
 
     # Check if the speed of light is positive
-    if speed_of_light_m_per_s <= 0:
+    if c <= 0:
         raise ValueError("Speed of light must be positive")
 
     # Calculate energy using the mass-energy equivalence equation
-    energy = mass_kg * speed_of_light_m_per_s**2
+    energy = mass * c **2
     return energy
 
 if __name__ == "__main__":
