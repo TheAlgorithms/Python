3ddfcdc25163bae76af33e4afb67f5a92fca29a4
kosuri-indu <kosurilindu@gmail.com>
2023-10-01 09:47:53 +0530

    examples

--- project_euler/problem_095/sol1.py
+++ project_euler/problem_095/sol1.py
@@ -1,13 +1,30 @@
 """
 Problem 95
 Url: https://projecteuler.net/problem=95
-"""
+Statement:
+The proper divisors of a number are all the divisors excluding the number itself.
+For example, the proper divisors of  28 is 1,2,4,7 and 14 and .
+As the sum of these divisors is equal to 28, we call it a perfect number.
 
+Interestingly the sum of the proper divisors of 220 is 284 
+The sum of the proper divisors of 284 is 220 forming a chain of two numbers.
+For this reason, 220 and 284 are called an amicable pair.
+
+Perhaps less well known are longer chains. 
+For example, starting with 12496, we form a chain of five numbers:
+12496 -> 14288 -> 15472 -> 14536 -> 14264(->12496 -> ....)
+Since this chain returns to its starting point, it is called an amicable chain.
+
+Find the smallest member of the longest amicable chain with no element exceeding one million.
+"""
 
 def solution(number : int = 10**6) -> int:
     """
     Returns the smallest member when n = 1000000
-    >> 14316
+    >>> solution(1000000)
+    14316
+    >>> solution(5000)
+    220
     """
 
     sum_of_div = [0] * (number + 1)
