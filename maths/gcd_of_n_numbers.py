3f4faaff752a7d224ccf2f8702319e7c1e390133
Dev <devsoni0711@gmail.com>
2023-10-01 22:16:59 +0530

    mypy-syntax-error-in-maths/gcd_of_n_numbers

--- maths/gcd_of_n_numbers.py
+++ maths/gcd_of_n_numbers.py
@@ -1,5 +1,5 @@
 """
-Gcd of N Numbers
+GCD of N Numbers
 Reference: https://en.wikipedia.org/wiki/Greatest_common_divisor
 """
 
@@ -10,29 +10,13 @@ def get_factors(
     number: int, factors: Counter | None = None, factor: int = 2
 ) -> Counter:
     """
-    this is a recursive function for get all factors of number
-    >>> get_factors(45)
-    Counter({3: 2, 5: 1})
-    >>> get_factors(2520)
-    Counter({2: 3, 3: 2, 5: 1, 7: 1})
-    >>> get_factors(23)
-    Counter({23: 1})
-    >>> get_factors(0)
-    Traceback (most recent call last):
-        ...
-    TypeError: number must be integer and greater than zero
-    >>> get_factors(-1)
-    Traceback (most recent call last):
-        ...
-    TypeError: number must be integer and greater than zero
-    >>> get_factors(1.5)
-    Traceback (most recent call last):
-        ...
-    TypeError: number must be integer and greater than zero
+    This is a recursive function to get all factors of a number.
 
-    factor can be all numbers from 2 to number that we check if number % factor == 0
-    if it is equal to zero, we check again with number // factor
-    else we increase factor by one
+    :param number: The number to find factors for.
+    :param factors: A Counter object to store factors.
+    :param factor: The current factor to check.
+    :return: A Counter object containing the factors.
+    :raises TypeError: If the number is not an integer or is less than or equal to zero.
     """
 
     if not isinstance(number, int) or number <= 0:
@@ -40,47 +24,31 @@ def get_factors(
 
     factors = factors or Counter()
 
-    if number == factor:  # break condition
-        # all numbers are factors of itself
+    if number == factor:  # Break condition
+        # All numbers are factors of itself
         factors[factor] += 1
         return factors
 
     if number % factor > 0:
-        # if it is greater than zero
-        # so it is not a factor of number and we check next number
+        # If it is greater than zero, it is not a factor of the number,
+        # so we check the next number
         return get_factors(number, factors, factor + 1)
 
     factors[factor] += 1
-    # else we update factors (that is Counter(dict-like) type) and check again
+    # Otherwise, we update factors and check again
     return get_factors(number // factor, factors, factor)
 
 
 def get_greatest_common_divisor(*numbers: int) -> int:
     """
-    get gcd of n numbers:
-    >>> get_greatest_common_divisor(18, 45)
-    9
-    >>> get_greatest_common_divisor(23, 37)
-    1
-    >>> get_greatest_common_divisor(2520, 8350)
-    10
-    >>> get_greatest_common_divisor(-10, 20)
-    Traceback (most recent call last):
-        ...
-    Exception: numbers must be integer and greater than zero
-    >>> get_greatest_common_divisor(1.5, 2)
-    Traceback (most recent call last):
-        ...
-    Exception: numbers must be integer and greater than zero
-    >>> get_greatest_common_divisor(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    1
-    >>> get_greatest_common_divisor("1", 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    Traceback (most recent call last):
-        ...
-    Exception: numbers must be integer and greater than zero
+    Get the greatest common divisor (GCD) of n numbers.
+
+    :param numbers: A variable number of integers to find the GCD for.
+    :return: The GCD of the input numbers.
+    :raises Exception: If any input number is not an integer or is less than or equal to zero.
     """
 
-    # we just need factors, not numbers itself
+    # We just need factors, not numbers themselves
     try:
         same_factors, *factors = map(get_factors, numbers)
     except TypeError as e:
@@ -88,13 +56,12 @@ def get_greatest_common_divisor(*numbers: int) -> int:
 
     for factor in factors:
         same_factors &= factor
-        # get common factor between all
-        # `&` return common elements with smaller value (for Counter type)
+        # Get the common factor between all using bitwise AND
 
-    # now, same_factors is something like {2: 2, 3: 4} that means 2 * 2 * 3 * 3 * 3 * 3
+    # Now, same_factors is something like {2: 2, 3: 4}, which means 2 * 2 * 3 * 3 * 3 * 3
     mult = 1
-    # power each factor and multiply
-    # for {2: 2, 3: 4}, it is [4, 81] and then 324
+    # Power each factor and multiply
+    # For {2: 2, 3: 4}, it is [4, 81] and then 324
     for m in [factor**power for factor, power in same_factors.items()]:
         mult *= m
     return mult
