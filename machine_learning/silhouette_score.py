24a4d008549c7b857014d3e8f5c922ba7d829708
swayam patil <swayampatil7803@gmail.com>
2023-10-01 12:24:08 +0530

    Update silhouette_score.py

--- machine_learning/silhouette_score.py
+++ machine_learning/silhouette_score.py
@@ -21,7 +21,7 @@ from typing import Union
 
 def pairwise_distances(X: np.ndarray) -> np.ndarray:
     """
-    
+
     Compute pairwise distances between data points in the input array X.
 
     Parameters:
@@ -30,6 +30,17 @@ def pairwise_distances(X: np.ndarray) -> np.ndarray:
     Returns:
     numpy.ndarray: A square matrix of pairwise distances where distances[i, j] represents
     the Euclidean distance between X[i] and X[j].
+
+    Example:
+    >>> import numpy as np
+    >>> from machine_learning.silhouette_score import pairwise_distances  # Replace 'your_module' with the actual module name
+    >>> data_points = np.array([[1, 2], [2, 3], [3, 4], [4, 5]])
+    >>> distances = pairwise_distances(data_points)
+    >>> distances
+    array([[0.        , 1.41421356, 2.82842712, 4.24264069],
+           [1.41421356, 0.        , 1.41421356, 2.82842712],
+           [2.82842712, 1.41421356, 0.        , 1.41421356],
+           [4.24264069, 2.82842712, 1.41421356, 0.        ]])
     """
     n = X.shape[0]
     distances = np.zeros((n, n))
@@ -39,7 +50,7 @@ def pairwise_distances(X: np.ndarray) -> np.ndarray:
             distances[j, i] = distances[i, j]
     return distances
 
-def silhouette_score(X: np.ndarray, cluster_labels: Union[list, np.ndarray]) -> float:
+def silhouette_score(data_points: np.ndarray, cluster_labels: Union[list, np.ndarray]) -> float:
     """
     Calculate the silhouette score for a set of data points and their corresponding cluster labels.
 
@@ -47,7 +58,7 @@ def silhouette_score(X: np.ndarray, cluster_labels: Union[list, np.ndarray]) -> 
     is to its own cluster compared to other clusters.
 
     Parameters:
-    X (numpy.ndarray): The input data array of shape (n_samples, n_features).
+    data_points (numpy.ndarray): The input data array of shape (n_samples, n_features).
     cluster_labels (list or numpy.ndarray): List of cluster labels assigned to each data point in X.
 
     Returns:
@@ -57,14 +68,14 @@ def silhouette_score(X: np.ndarray, cluster_labels: Union[list, np.ndarray]) -> 
     Example:
     >>> import numpy as np
     >>> from machine_learning.silhouette_score import silhouette_score
-    >>> X = np.array([[1, 2], [2, 3], [3, 4], [4, 5]])
+    >>> data_points = np.array([[1, 2], [2, 3], [3, 4], [4, 5]])
     >>> cluster_labels = [0, 0, 1, 1]
-    >>> score = silhouette_score(X, cluster_labels)
+    >>> score = silhouette_score(data_points, cluster_labels)
     >>> round(score, 2)
     0.36
     """
-    distances = pairwise_distances(X)
-    n = len(X)
+    distances = pairwise_distances(data_points)
+    n = len(data_points)
     silhouette_values = []
 
     for i in range(n):
