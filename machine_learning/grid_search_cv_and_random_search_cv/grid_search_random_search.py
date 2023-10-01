71e14e1671843cebeac22a4816be76f513a735f6
Shubham Nagure <nagure.shubham@gmail.com>
2023-10-01 07:54:30 +0000

    corrections as per contribution rules

--- machine_learning/grid_search_cv_and_random_search_cv/grid_search_random_search.py
+++ machine_learning/grid_search_cv_and_random_search_cv/grid_search_random_search.py
@@ -1,7 +1,9 @@
 # Import necessary libraries
-from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
-from sklearn.ensemble import RandomForestClassifier
+# Random Search Cross-Validation
+from scipy.stats import randint
 from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 
 # Load a dataset (replace this with your dataset)
 data = load_iris()
@@ -13,10 +15,10 @@ model = RandomForestClassifier()
 
 # Hyperparameters to tune
 param_grid = {
-    'n_estimators': [10, 50, 100, 200],
-    'max_depth': [None, 10, 20, 30],
-    'min_samples_split': [2, 5, 10],
-    'min_samples_leaf': [1, 2, 4]
+    "n_estimators": [10, 50, 100, 200],
+    "max_depth": [None, 10, 20, 30],
+    "min_samples_split": [2, 5, 10],
+    "min_samples_leaf": [1, 2, 4],
 }
 
 # Grid Search Cross-Validation
@@ -27,16 +29,16 @@ grid_search.fit(X, y)
 print("Grid Search Best Hyperparameters:", grid_search.best_params_)
 print("Grid Search Best Score:", grid_search.best_score_)
 
-# Random Search Cross-Validation
-from scipy.stats import randint
 param_dist = {
-    'n_estimators': randint(10, 200),
-    'max_depth': [None] + list(randint(10, 30, 4)),
-    'min_samples_split': randint(2, 10),
-    'min_samples_leaf': randint(1, 4)
+    "n_estimators": randint(10, 200),
+    "max_depth": [None] + list(randint(10, 30, 4)),
+    "min_samples_split": randint(2, 10),
+    "min_samples_leaf": randint(1, 4),
 }
 
-random_search = RandomizedSearchCV(model, param_distributions=param_dist, n_iter=100, cv=5, n_jobs=-1)
+random_search = RandomizedSearchCV(
+    model, param_distributions=param_dist, n_iter=100, cv=5, n_jobs=-1
+)
 random_search.fit(X, y)
 
 # Print the best hyperparameters and corresponding score
