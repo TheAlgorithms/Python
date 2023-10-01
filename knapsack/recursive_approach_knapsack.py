995cb5348581b4a9510873f829650147936ab5ec
Balkishan <143352599+balkishan69@users.noreply.github.com>
2023-10-01 17:05:21 +0530

    Update recursive_approach_knapsack.py
    DP Memoization Approach for 0/1 Knapsack Problem

--- knapsack/recursive_approach_knapsack.py
+++ knapsack/recursive_approach_knapsack.py
@@ -17,35 +17,51 @@ be carried.
 """
 
 
-def knapsack(
-    weights: list, values: list, number_of_items: int, max_weight: int, index: int
-) -> int:
-    """
-    Function description is as follows-
-    :param weights: Take a list of weights
-    :param values: Take a list of profits corresponding to the weights
-    :param number_of_items: number of items available to pick from
-    :param max_weight: Maximum weight that could be carried
-    :param index: the element we are looking at
-    :return: Maximum expected gain
-    >>> knapsack([1, 2, 4, 5], [5, 4, 8, 6], 4, 5, 0)
-    13
-    >>> knapsack([3 ,4 , 5], [10, 9 , 8], 3, 25, 0)
-    27
-    """
-    if index == number_of_items:
-        return 0
-    ans1 = 0
-    ans2 = 0
-    ans1 = knapsack(weights, values, number_of_items, max_weight, index + 1)
-    if weights[index] <= max_weight:
-        ans2 = values[index] + knapsack(
-            weights, values, number_of_items, max_weight - weights[index], index + 1
-        )
-    return max(ans1, ans2)
+# Memoization Approach for 0/1 Knapsack Problem:
+# Note: It should be noted that the above function using recursion computes the same subproblems again and again. See the following recursion tree, K(1, 1) is being evaluated twice. 
+
+# In the following recursion tree, K() refers  to knapSack(). The two parameters indicated in the following recursion tree are n and W.
+# The recursion tree is for following sample inputs.
+# weight[] = {1, 1, 1}, W = 2, profit[] = {10, 20, 30}  
+
+# Recursion tree for Knapsack capacity 2 units and 3 items of 1 unit weight.
 
 
-if __name__ == "__main__":
-    import doctest
+# This is the memoization approach of
+# 0 / 1 Knapsack in Python in simple
+# we can say recursion + memoization = DP
 
-    doctest.testmod()
+
+def knapsack(wt, val, W, n):
+
+	# base conditions
+	if n == 0 or W == 0:
+		return 0
+	if t[n][W] != -1:
+		return t[n][W]
+
+	# choice diagram code
+	if wt[n-1] <= W:
+		t[n][W] = max(
+			val[n-1] + knapsack(
+				wt, val, W-wt[n-1], n-1),
+			knapsack(wt, val, W, n-1))
+		return t[n][W]
+	elif wt[n-1] > W:
+		t[n][W] = knapsack(wt, val, W, n-1)
+		return t[n][W]
+
+# Driver code
+if __name__ == '__main__':
+	profit = [60, 100, 120]
+	weight = [10, 20, 30]
+	W = 50
+	n = len(profit)
+	
+	# We initialize the matrix with -1 at first.
+	t = [[-1 for i in range(W + 1)] for j in range(n + 1)]
+	print(knapsack(weight, profit, W, n))
+
+# Time Complexity: O(N * W). As redundant calculations of states are avoided.
+# Auxiliary Space: O(N * W) + O(N). The use of a 2D array data structure for storing intermediate states and O(N) auxiliary stack space(ASS) has been used for recursion stack
+
