93015b51270f12158e0b9bb579df528b3fbe960b
lakshit kava <lakshit1kava0@gmail.com>
2023-10-01 14:45:50 +0530

    update patternMatching.py

--- data_structures/string_algorithms/pattern_matching.py
+++ data_structures/string_algorithms/pattern_matching.py
@@ -1,30 +1,30 @@
 # Author : Lakshit21 (https://github.com/Lakshit21)
 # Check Code on : https://cses.fi/problemset/result/5753335/
 # Date : 1st Oct 2023
-# Time : 1:00 PM IST
 
-class StringMatch():
-    def __init__(self, function_method_name='Z_FUNCTION', separator='\uFFFF'):
+
+class StringMatch:
+    def __init__(self, function_method_name="Z_FUNCTION", separator="\uFFFF"):
         """
         Initialize the StringMatch class.
 
         Args:
-            function_method_name (str): The string matching algorithm to use ('Z_FUNCTION' or 'PI_FUNCTION').
+            function_method_name (str): The string matching algorithm to use.
             separator (str): The separator character to merge text and pattern.
 
         Note:
             You can specify the matching algorithm using 'function_method_name'.
         """
-        self.__separator = separator # default is '\uFFFF' least used Unicode
+        self.__separator = separator  # default is '\uFFFF' least used Unicode
 
         # Dictionary mapping method names to corresponding functions
         self.__function_dictionary = {
-            'Z_FUNCTION': self.__zfun_match,
-            'PI_FUNCTION': self.__pifun_match,
+            "Z_FUNCTION": self.__zfun_match,
+            "PI_FUNCTION": self.__pifun_match,
         }
         # to be added...
 
-        if function_method_name in self.__function_dictionary.keys():
+        if function_method_name in self.__function_dictionary:
             self.Function = self.__function_dictionary[function_method_name]
         else:  # Default to Z-function
             self.Function = self.__zfun_match
@@ -40,17 +40,20 @@ class StringMatch():
         Returns:
             tuple: A tuple containing the occurrence count and occurrence indices.
         """
+
         def is_valid_text():
-            for char in text:
-                if char == self.__separator:
-                    return False
-            return True
+            return all(char != self.separator for char in text)
+            # for char in text:
+            #     if char == self.__separator:
+            #         return False
+            # return True
 
         def is_valid_pattern():
-            for char in text:
-                if char == self.__separator:
-                    return False
-            return True
+            return all(char != self.separator for char in pattern)
+            # for char in text:
+            #     if char == self.__separator:
+            #         return False
+            # return True
 
         if not is_valid_text():
             print("Err : Text ")
@@ -60,8 +63,7 @@ class StringMatch():
             print("Err : Pattern ")
             return
 
-        occurrence_count, occurrence_indices = self.Function(
-            text=text, pattern=pattern)
+        occurrence_count, occurrence_indices = self.Function(text=text, pattern=pattern)
 
         return occurrence_count, occurrence_indices
 
@@ -76,8 +78,7 @@ class StringMatch():
         Returns:
             tuple: A tuple containing the occurrence count and occurrence indices.
         """
-        merged_string = self.__merge_text_and_pattern(
-            text=text, pattern=pattern)
+        merged_string = self.__merge_text_and_pattern(text=text, pattern=pattern)
 
         z = self.__z_function(merged_string)
 
@@ -104,8 +105,7 @@ class StringMatch():
         Returns:
             tuple: A tuple containing the occurrence count and occurrence indices.
         """
-        merged_string = self.__merge_text_and_pattern(
-            text=text, pattern=pattern)
+        merged_string = self.__merge_text_and_pattern(text=text, pattern=pattern)
 
         pi = self.__pi_function(merged_string=merged_string)
 
@@ -117,7 +117,7 @@ class StringMatch():
             j = i + offset
             if pi[j] == len(pattern):
                 pattern_count += 1
-                pattern_indices.append(j - 2*(len(pattern)))
+                pattern_indices.append(j - 2 * (len(pattern)))
 
         return pattern_count, pattern_indices
 
@@ -143,7 +143,7 @@ class StringMatch():
         Calculate the Z-function values for a given string.
 
         Args:
-            merged_string (str): The string for which Z-function values are to be calculated.
+            merged_string (str): The string for which Z-function.
 
         Returns:
             list: A list of Z-function values for the input string.
@@ -156,16 +156,16 @@ class StringMatch():
 
         l, r = 0, 0
 
-        def isEqual(index_1: int, index_2: int):
+        def isequal(index_1: int, index_2: int):
             return s[index_1] == s[index_2]
 
         for i in range(1, n):
             if i < r:
-                z[i] = min(r-i, z[i-l])
-            while (i+z[i] < n and isEqual(z[i], i+z[i])):
+                z[i] = min(r - i, z[i - l])
+            while i + z[i] < n and isequal(z[i], i + z[i]):
                 z[i] += 1
-            if i+z[i] > r:
-                l, r = i, i+z[i]
+            if i + z[i] > r:
+                l, r = i, i + z[i]
 
         return z
 
@@ -174,7 +174,7 @@ class StringMatch():
         Calculate the Pi-function values for a given string.
 
         Args:
-            merged_string (str): The string for which Pi-function values are to be calculated.
+            merged_string (str): The string for which Pi-function.
 
         Returns:
             list: A list of Pi-function values for the input string.
@@ -185,14 +185,14 @@ class StringMatch():
 
         pi = [0] * n
 
-        def isEqual(index_1: int, index_2: int):
+        def isequal(index_1: int, index_2: int):
             return s[index_1] == s[index_2]
 
         for i in range(1, n):
-            j = pi[i-1]
-            while j > 0 and not isEqual(i, j):
-                j = pi[j-1]
-            if isEqual(i, j):
+            j = pi[i - 1]
+            while j > 0 and not isequal(i, j):
+                j = pi[j - 1]
+            if isequal(i, j):
                 j += 1
             pi[i] = j
 
@@ -204,7 +204,7 @@ usage:
 
 0. import file :)
 
-1. Create Object Of the Class StringMatch with custom function_method_selection and separator
+1. Create Object Of the Class StringMatch with custom fields
 
 obj = StringMatch(function_method_name='PI_FUNCTION', separator'$' )
 
