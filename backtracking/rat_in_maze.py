3e0190382429f26e18ab5f36c900a39596b8deb8
Saptadeep Banerjee <69459134+imSanko@users.noreply.github.com>
2023-10-01 16:01:41 +0530

    Added New Rat Maze in Backtracking

--- backtracking/rat_in_maze.py
+++ backtracking/rat_in_maze.py
@@ -60,7 +60,7 @@ def solve_maze(maze: list[list[int]]) -> bool:
     False
     """
     size = len(maze)
-    # We need to create solution object to save path.
+    # We need to create a solution object to save the path.
     solutions = [[0 for _ in range(size)] for _ in range(size)]
     solved = run_maze(maze, 0, 0, solutions)
     if solved:
@@ -74,41 +74,43 @@ def run_maze(maze: list[list[int]], i: int, j: int, solutions: list[list[int]]) 
     """
     This method is recursive starting from (i, j) and going in one of four directions:
     up, down, left, right.
-    If a path is found to destination it returns True otherwise it returns False.
+    If a path is found to the destination, it returns True; otherwise, it returns False.
     Parameters:
         maze(2D matrix) : maze
-        i, j : coordinates of matrix
+        i, j : coordinates of the matrix
         solutions(2D matrix) : solutions
     Returns:
-        Boolean if path is found True, Otherwise False.
+        Boolean if the path is found, True; Otherwise, False.
     """
     size = len(maze)
-    # Final check point.
+    # Final checkpoint.
     if i == j == (size - 1):
         solutions[i][j] = 1
         return True
 
-    lower_flag = (not i < 0) and (not j < 0)  # Check lower bounds
-    upper_flag = (i < size) and (j < size)  # Check upper bounds
+    # Check bounds
+    if (
+        i >= 0
+        and j >= 0
+        and i < size
+        and j < size
+        and solutions[i][j] == 0
+        and maze[i][j] == 1
+    ):
+        solutions[i][j] = 1
 
-    if lower_flag and upper_flag:
-        # check for already visited and block points.
-        block_flag = (not solutions[i][j]) and (not maze[i][j])
-        if block_flag:
-            # check visited
-            solutions[i][j] = 1
+        # Explore in four directions: up, down, left, right
+        if (
+            run_maze(maze, i + 1, j, solutions)
+            or run_maze(maze, i, j + 1, solutions)
+            or run_maze(maze, i - 1, j, solutions)
+            or run_maze(maze, i, j - 1, solutions)
+        ):
+            return True
 
-            # check for directions
-            if (
-                run_maze(maze, i + 1, j, solutions)
-                or run_maze(maze, i, j + 1, solutions)
-                or run_maze(maze, i - 1, j, solutions)
-                or run_maze(maze, i, j - 1, solutions)
-            ):
-                return True
+        # If none of the directions lead to a solution, backtrack by marking the current position as 0
+        solutions[i][j] = 0
 
-            solutions[i][j] = 0
-            return False
     return False
 
 
