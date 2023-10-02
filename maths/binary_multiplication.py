2c4994cb09c6b77c379412e79445283943d49acb
AasheeshLikePanner <ashishlearningsoftware@gmail.com>
2023-10-02 22:54:16 +0530

    Adding doctests and changing file name

--- maths/binary_multiplication.py
+++ maths/binary_multiplication.py
@@ -12,6 +12,32 @@
 
 
 def b_expo(a: int, b: int) -> int:
+    """
+        Calculate the result of multiplying 'a' and 'b' using bitwise multiplication.
+
+        Parameters:
+        a (int): The first number.
+        b (int): The second number.
+
+        Returns:
+        int: The result of 'a' multiplied by 'b'.
+
+        Examples:
+        >>> b_expo(2, 3)
+        6
+        >>> b_expo(5, 0)
+        0
+        >>> b_expo(3, 4)
+        12
+        >>> b_expo(10, 5)
+        50
+        >>> b_expo(0, 5)
+        0
+        >>> b_expo(2, 1)
+        2
+        >>> b_expo(1, 10)
+        10
+        """
     res = 0
     while b > 0:
         if b & 1:
@@ -24,6 +50,35 @@ def b_expo(a: int, b: int) -> int:
 
 
 def b_expo_mod(a: int, b: int, c: int) -> int:
+    """
+       Calculate the result of (a * b) % c using binary exponentiation and modular arithmetic.
+
+       Parameters:
+       a (int): The first number.
+       b (int): The second number.
+       c (int): The modulus.
+
+       Returns:
+       int: The result of (a * b) % c.
+
+       Examples:
+       >>> b_expo_mod(2, 3, 5)
+       1
+       >>> b_expo_mod(5, 0, 7)
+       0
+       >>> b_expo_mod(3, 4, 6)
+       0
+       >>> b_expo_mod(10, 5, 13)
+       8
+       >>> b_expo_mod(2, 1, 5)
+       2
+       >>> b_expo_mod(1, 10, 3)
+       1
+       >>> b_expo_mod(7, 3, 4)
+       1
+       >>> b_expo_mod(8, 2, 10)
+       6
+       """
     res = 0
     while b > 0:
         if b & 1:
@@ -34,6 +89,9 @@ def b_expo_mod(a: int, b: int, c: int) -> int:
 
     return res
 
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
 
 """
 * Wondering how this method works !
