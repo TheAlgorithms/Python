94e5edeac85a330d28b4d6229d9f807e7458b8f3
MihirRajeshPanchal <mihirpanchal5400@gmail.com>
2023-10-02 22:36:18 +0530

    added doctests

--- sorts/library_sort.py
+++ sorts/library_sort.py
@@ -26,7 +26,24 @@ def library_sort(collection: list) -> list:
     [-45, -5, -2]
     """
 
-    def insertion_sort(arr):
+    def insertion_sort(arr: list) -> None:
+        """
+        Sorts a list using the Insertion Sort algorithm in place.
+
+        :param arr: A mutable list containing comparable items.
+        :return: None
+
+        Examples:
+        >>> arr = [5, 2, 9, 1, 5]
+        >>> insertion_sort(arr)
+        >>> arr
+        [1, 2, 5, 5, 9]
+
+        >>> arr = [4, 3, 2, 1]
+        >>> insertion_sort(arr)
+        >>> arr
+        [1, 2, 3, 4]
+        """
         for i in range(1, len(arr)):
             key = arr[i]
             j = i - 1
@@ -34,6 +51,35 @@ def library_sort(collection: list) -> list:
                 arr[j + 1] = arr[j]
                 j -= 1
             arr[j + 1] = key
+
+    def insertion_sort(arr: list) -> list:
+        """
+        Sorts a list using the Insertion Sort algorithm and returns a new sorted list.
+
+        :param arr: A list containing comparable items.
+        :return: A new sorted list.
+
+        Examples:
+        >>> arr = [5, 2, 9, 1, 5]
+        >>> sorted_arr = insertion_sort(arr)
+        >>> sorted_arr
+        [1, 2, 5, 5, 9]
+
+        >>> arr = [4, 3, 2, 1]
+        >>> sorted_arr = insertion_sort(arr)
+        >>> sorted_arr
+        [1, 2, 3, 4]
+        """
+        sorted_arr = arr.copy()
+        for i in range(1, len(sorted_arr)):
+            key = sorted_arr[i]
+            j = i - 1
+            while j >= 0 and key < sorted_arr[j]:
+                sorted_arr[j + 1] = sorted_arr[j]
+                j -= 1
+            sorted_arr[j + 1] = key
+
+        return sorted_arr
 
     if len(collection) <= 1:
         return collection
@@ -52,11 +98,21 @@ def library_sort(collection: list) -> list:
 
 def library(left: list, right: list) -> list:
     """
-    library two sorted collections (left and right) into a single sorted collection.
+    Merge two sorted collections (left and right) into a single sorted collection.
 
     :param left: A sorted collection
     :param right: A sorted collection
-    :return: A libraryd sorted collection
+    :return: A merged sorted collection.
+
+    Examples:
+    >>> library([1, 3, 5], [2, 4, 6])
+    [1, 2, 3, 4, 5, 6]
+
+    >>> library([0, 2, 4], [1, 3, 5])
+    [0, 1, 2, 3, 4, 5]
+
+    >>> library([7, 9, 11], [8, 10, 12])
+    [7, 8, 9, 10, 11, 12]
     """
     result = []
     i = j = 0
