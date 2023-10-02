8786aa3ef2e464a80bf0b1b4efa31925b0320ae8
dimonalik <114773527+dimonalik@users.noreply.github.com>
2023-10-02 14:30:28 +0300

    Update blacklistipchecker.py
    Rewritten without IPy module

--- other/blacklistipchecker.py
+++ other/blacklistipchecker.py
@@ -4,16 +4,15 @@
 # to make it work (it`s free 30 IPs per month)
 
 import json
-import logging
 import sys
-
+from typing import Any
+import re
 import requests
-from IPy import IP
 
 api_key = "key_CAMF5HI5t4ZzkmgGkioI1tius"
 
 
-def test_ip(ip: str) -> tuple[str, list] | tuple[str, str] | None:
+def test_ip(ip: str) -> Any:
     result = requests.get(
         "https://api.blacklistchecker.com/check/" + ip, auth=(api_key, "")
     )
@@ -32,20 +31,26 @@ def test_ip(ip: str) -> tuple[str, list] | tuple[str, str] | None:
             if n.get("detected") is True:
                 detects.append(n.get("name"))
         return (res, detects)
-    else:
-        return ()
 
+def isip(ip: str) -> bool:
+    match = re.match(\
+        r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}', ip\
+        )
+    if not bool(match):
+        return False
+    octets = ip.split(".")
+    for b in octets:
+        if int(b) < 0 or int(b) > 255:
+            return False
+    return True
+    
 
 res_txt = "IP blacklists check returned next results:\n"
 send_param = 0
 if len(sys.argv) > 1:
     for x in sys.argv[1:]:
-        try:
-            IP(x)
-        except Exception as ex:
-            print("Argument isn`t a valid ip(" + x + ")\n")
-            print(f"{ex}\n Skipping argument")
-            logging.exception("Caught an error")
+        if isip(x) is not True:
+            print(f"IP {x} is incorrect\n")
         else:
             if test_ip(x) is not None:
                 res, detec = test_ip(x)
@@ -57,26 +62,15 @@ if len(sys.argv) > 1:
 else:
     print("Type in ip to check")
     x = input()
-    try:
-        IP(x)
-    except Exception as ex:
-        print("Argument isn`t a valid ip(" + x + ")")
-        print(str(ex) + "\n")
-        logging.exception("Caught an error")
-        sys.exit()
+    if isip(x) is not True:
+        print(f"IP {x} is incorrect\n")
     else:
         if test_ip(x) is not None:
             res, detec = test_ip(x)
             if res == "limit exceeded":
                 res_txt = res_txt + "Limit Exceeded!\n"
             else:
-                res_txt = (
-                    res_txt
-                    + str(res)
-                    + " is detected in "
-                    + str(detec)
-                    + " blacklists \n \n"
-                )
+                res_txt += f"{res} is detected in {detec} blacklists \n \n"
 if len(res_txt) <= 43:
     print("IPs not blacklisted!")
     print(res_txt)
