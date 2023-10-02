2bad9c1ad889c0bff8673875aa7a39fbe9e14f75
BamaCharanChhandogi <b.c.chhandogi@gmail.com>
2023-10-02 12:54:22 +0530

    improve code

--- data_structures/arrays/median_two_array.py
+++ data_structures/arrays/median_two_array.py
@@ -1,46 +1,53 @@
-class Solution:
-    def find_median_sorted_arrays(self, nums1: list[int], nums2: list[int]) -> float:
-        """
-        Find the median of two sorted arrays.
+def find_median_sorted_arrays(nums1: list[int], nums2: list[int]) -> float:
+    """
+    Find the median of two arrays.
 
-        Args:
-            nums1: The first sorted array.
-            nums2: The second sorted array.
+    Args:
+        nums1: The first array.
+        nums2: The second array.
 
-        Returns:
-            The median of the two sorted arrays.
+    Returns:
+    The median of the two arrays.
 
-        Example:
-        >>> sol = Solution()
-        >>> sol.find_median_sorted_arrays([1, 3], [2])
+    Examples:
+        >>> find_median_sorted_arrays([1, 3], [2])
         2.0
 
-        >>> sol.find_median_sorted_arrays([1, 2], [3, 4])
+        >>> find_median_sorted_arrays([1, 2], [3, 4])
         2.5
 
-        >>> sol.find_median_sorted_arrays([0, 0], [0, 0])
+        >>> find_median_sorted_arrays([0, 0], [0, 0])
         0.0
-        """
-        # Merge the arrays into a single sorted array.
-        merged = nums1 + nums2
 
-        # Sort the merged array.
-        merged.sort()
+        >>> find_median_sorted_arrays([], [])
+        Traceback (most recent call last):
+            ...
+        ValueError: Both input arrays are empty.
 
-        # Calculate the total number of elements in the merged array.
-        total = len(merged)
+        >>> find_median_sorted_arrays([], [1])
+        1.0
 
-        if total % 2 == 1:
-            # If the total number of elements is odd, return the
-            # middle element as the median.
-            return float(merged[total // 2])
-        else:
-            # If the total number of elements is even, calculate
-            # the average of the two middle elements as the median.
-            middle1 = merged[total // 2 - 1]
-            middle2 = merged[total // 2]
-            return (float(middle1) + float(middle2)) / 2.0
+        >>> find_median_sorted_arrays([-1000], [1000])
+        0.0
 
+        >>> find_median_sorted_arrays([-1.1, -2.2], [-3.3, -4.4])
+        -2.75
+    """
+    if not nums1 and not nums2:
+        raise ValueError("Both input arrays are empty.")
+
+    # Merge the arrays into a single sorted array.
+    merged = sorted(nums1 + nums2)
+    total = len(merged)
+
+    if total % 2 == 1:  # If the total number of elements is odd
+        return float(merged[total // 2])  # then return the middle element
+
+    # If the total number of elements is even, calculate
+    # the average of the two middle elements as the median.
+    middle1 = merged[total // 2 - 1]
+    middle2 = merged[total // 2]
+    return (float(middle1) + float(middle2)) / 2.0
 
 if __name__ == "__main__":
     import doctest
