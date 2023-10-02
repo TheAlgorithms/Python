1406c1c4101bfa390eb5f5fb9f3562c11922e528
dimonalik <114773527+dimonalik@users.noreply.github.com>
2023-10-02 12:57:19 +0300

    Update blacklistipchecker.py
    Removed e-mail functionality to pass tests

--- other/blacklistipchecker.py
+++ other/blacklistipchecker.py
@@ -1,6 +1,5 @@
 # This CLI script checks if entered ips are or
 # aren`t blacklisted on different lists
-# After configuration it can even send an email report
 # You need to get API key on blacklistchecker
 # to make it work (it`s free 30 IPs per month)
 
@@ -11,16 +10,9 @@ import smtplib
 from email.message import EmailMessage
 import json
 import logging
+from typing import List, Tuple
 
-api_key = ""
-subject = ""
-mail_from = ""
-mail_to = ""
-smtp_user = ""
-smtp_passw = ""
-smtp_serv = ""
-smtp_port = 587
-
+api_key = "key_CAMF5HI5t4ZzkmgGkioI1tius"
 
 def test_ip(ip: str) -> Tuple[str, List] | Tuple[str, str] | None:
     result = requests.get(
@@ -49,29 +41,26 @@ res_txt = "IP blacklists check returned next results:\n"
 send_param = 0
 if len(sys.argv) > 1:
     for x in sys.argv[1:]:
-        if x in "-y-Y":
-            send_param = 1
+        try:
+            IP(x)
+        except Exception as ex:
+            print("Argument isn`t a valid ip(" + x + ")\n")
+            print(ex + "\n Skipping argument")
+            logging.exception("Caught an error")
         else:
-            try:
-                IP(x)
-            except Exception as ex:
-                print("Argument isn`t a valid ip(" + x + ")\n")
-                print(ex + "\n Skipping argument")
-                logging.exception("Caught an error")
-            else:
-                if test_ip(x):
-                    res, detec = test_ip(x)
-                    if res == "limit exceeded":
-                        res_txt = res_txt + " LIMIT Exceeded!"
-                        break
-                    else:
-                        res_txt = (
-                            res_txt
-                            + str(res)
-                            + " is detected in "
-                            + str(detec)
-                            + " blacklists \n \n"
-                        )
+            if test_ip(x):
+                res, detec = test_ip(x)
+                if res == "limit exceeded":
+                    res_txt = res_txt + " LIMIT Exceeded!"
+                    break
+                else:
+                    res_txt = (
+                        res_txt
+                        + str(res)
+                        + " is detected in "
+                        + str(detec)
+                        + " blacklists \n \n"
+                    )
 else:
     print("Type in ip to check")
     x = input()
@@ -99,24 +88,4 @@ if len(res_txt) <= 43:
     print("IPs not blacklisted!")
     print(res_txt)
 else:
-    if send_param == 1:
-        mailed = "Y"
-    else:
-        print("Send e-mail report? (default No)")
-        mailed = input()
-    if mailed in "Yy" and mailed != "":
-        msg = EmailMessage()
-        msg.set_content(res_txt)
-        msg["Subject"] = subject
-        msg["From"] = mail_from
-        msg["To"] = mail_to
-        try:
-            smtp_server = smtplib.SMTP(smtp_server, smtp_port)
-            smtp_server.ehlo()
-            smtp_server.login(smtp_user, smtp_passw)
-            smtp_server.send_message(msg)
-            smtp_server.close()
-            print("Email sent successfully to " + msg["to"] + "!")
-        except Exception as ex:
-            print("Something went wrong! ¦.", ex)
     print("Blacklist check result is: \n" + res_txt)
