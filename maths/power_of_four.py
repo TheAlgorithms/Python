cdfd5e055e3b10ec46203a629be03eeabb672396
Your Name <you@example.com>
2023-10-02 07:53:22 +0600

    Added Problem Statement link #9331

--- maths/power_of_four.py
+++ maths/power_of_four.py
@@ -1,4 +1,4 @@
-# Problem Statement:
+# Problem Statement: Link : https://leetcode.com/problems/power-of-four/description/
 # Given an integer n, return true if it is a power of four. Otherwise, return false.
 # An integer n is a power of four, if there exists an integer x such that n == 4x.
 # Example 1:
