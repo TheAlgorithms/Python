383efa1e22ae5f38719fbd6952f5fac04dbc59b2
Samiksha Mishra <38784342+mishrasamiksha@users.noreply.github.com>
2023-10-02 13:18:18 +0530

    Update alternate_bits_check.py

--- bit_manipulation/alternate_bits_check.py
+++ bit_manipulation/alternate_bits_check.py
@@ -1,7 +1,7 @@
 # function to check if all the bits
 # are set or not in the binary
 # representation of 'x'
-def allbitsareset(x):
+def allbitsareset(x) -> bool:
     # if true, then all bits are set
     if ((x + 1) & x) == 0:
         return True
@@ -12,8 +12,14 @@ def allbitsareset(x):
 
 # Function to check if a number
 # has bits in alternate pattern
-def bitsareinlltorder(x):
+def bitsareinlltorder(x) -> bool:
     num = x ^ (x >> 1)
 
     # To check if all bits are set in 'num'
     return allbitsareset(num)
+
+"""
+>>> Input :  15
+>>> Output :  No
+>>> Explanation: Binary representation of 15 is 1111.
+"""
