4bed08335bfd94a5c64473c5dcf7505ff986bc0e
Samiksha Mishra <38784342+mishrasamiksha@users.noreply.github.com>
2023-10-02 13:00:22 +0530

    Update alternate_bits_check.py

--- bit_manipulation/alternate_bits_check.py
+++ bit_manipulation/alternate_bits_check.py
@@ -1,7 +1,7 @@
 # function to check if all the bits
 # are set or not in the binary
 # representation of 'x'
-def allBitsAreSet(x):
+def allbitsareset(x):
     # if true, then all bits are set
     if ((x + 1) & x) == 0:
         return True
@@ -12,8 +12,8 @@ def allBitsAreSet(x):
 
 # Function to check if a number
 # has bits in alternate pattern
-def bitsAreInAltOrder(x):
+def bitsareinlltorder(x):
     num = x ^ (x >> 1)
 
     # To check if all bits are set in 'num'
-    return allBitsAreSet(num)
+    return allbitsareset(num)
