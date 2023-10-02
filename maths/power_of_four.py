28c55b9769b30fb744bc84f1e5cf89010a30d848
Pronay Debnath <pronayd4455@gmail.com>
2023-10-02 11:21:43 +0530

    Update power_of_four.py

--- maths/power_of_four.py
+++ maths/power_of_four.py
@@ -2,20 +2,18 @@
 # Date:- 2/10/2023
 # Power of four using logarithm
 
-
 import math
 import doctest
 
-
-def is_power_of_four(n: int) -> bool:
+def is_power_of_four(number_to_check: int) -> bool:
     """
     Determine if an integer is a power of four.
 
     Args:
-    n (int): The integer to check.
+    number_to_check (int): The integer to check.
 
     Returns:
-    bool: True if n is a power of four, False otherwise.
+    bool: True if number_to_check is a power of four, False otherwise.
 
     Examples:
     >>> is_power_of_four(16)
@@ -27,15 +25,14 @@ def is_power_of_four(n: int) -> bool:
     >>> is_power_of_four(0)
     False
     """
-    return n > 0 and math.log(n, 4).is_integer()
-
+    return number_to_check > 0 and math.log(number_to_check, 4).is_integer()
 
 if __name__ == "__main__":
     doctest.testmod()
-
-    n = int(input("Enter an integer: "))
-    result = is_power_of_four(n)
-
+    
+    number_to_check = int(input("Enter an integer: "))
+    result = is_power_of_four(number_to_check)
+    
     if result:
         print("The given integer is a power of four.")
     else:
