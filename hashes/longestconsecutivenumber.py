db1ff0ca3b2ce0a77429ec8a21215f36dcdb9c61
Kaushik <kaushikkudchadkar5@gmail.com>
2023-10-02 17:28:34 +0530

    Added Longest Consecutive Finder Algorithm using Hash Set

---
+++ hashes/longestconsecutivenumber.py
@@ -0,0 +1,22 @@
+#using hash set
+
+def longestConsecutive(nums):
+    if len(nums) == 0:
+        return 0
+    set = set(nums)
+    count = 1
+    for num in nums:
+        if num - 1 not in set:
+            tempCount = 1
+            while num + 1 in set:
+                tempCount += 1
+                num += 1
+            count = max(count, tempCount)
+        if count > len(nums) / 2:
+            break
+    return count
+
+size = int(input())
+nums = list(map(int, input().split()))
+nums.sort()
+print(longestConsecutive(nums))
