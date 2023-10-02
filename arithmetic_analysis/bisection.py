8a1f84ef11be8c9158f4a21008a504adb06c2cc1
w2kzx80 <68893211+w2kzx80@users.noreply.github.com>
2023-10-03 01:44:34 +0300

    Update bisection.py
    alittle optimization?)

--- arithmetic_analysis/bisection.py
+++ arithmetic_analysis/bisection.py
@@ -19,23 +19,26 @@ def bisection(function: Callable[[float], float], a: float, b: float) -> float:
         ...
     ValueError: could not find root in given interval.
     """
-    start: float = a
-    end: float = b
-    if function(a) == 0:  # one of the a or b is a root for the function
+    fna: float = function(a)
+    if fna == 0:  # one of the a or b is a root for the function
         return a
     elif function(b) == 0:
         return b
     elif (
-        function(a) * function(b) > 0
+        fna * function(b) > 0
     ):  # if none of these are root and they are both positive or negative,
         # then this algorithm can't find the root
         raise ValueError("could not find root in given interval.")
     else:
+        start: float = a
+        end: float = b
         mid: float = start + (end - start) / 2.0
+        fnmid: float = 0
         while abs(start - mid) > 10**-7:  # until precisely equals to 10^-7
-            if function(mid) == 0:
+            fnmid = function(mid)
+            if fnmid == 0:
                 return mid
-            elif function(mid) * function(start) < 0:
+            elif fnmid * function(start) < 0:
                 end = mid
             else:
                 start = mid
