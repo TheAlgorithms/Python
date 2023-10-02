1cb94c19036e2dfcfbbe2775bae4a5d461ee2133
lrazcs <109248041+lrazcs@users.noreply.github.com>
2023-10-01 22:13:02 -0400

    Update test_knapsack.py

--- knapsack/tests/test_knapsack.py
+++ knapsack/tests/test_knapsack.py
@@ -9,33 +9,33 @@ class Test(unittest.TestCase):
         val = [0]
         w = [0]
         c = len(val)
-        self.assertEqual(k.knapsack(cap, w, val, c, permit_repetition=False), 0)
+        self.assertEqual(k(cap, w, val, c, False), 0)
 
         val = [60]
         w = [10]
         c = len(val)
-        self.assertEqual(k.knapsack(cap, w, val, c, permit_repetition=False), 0)
+        self.assertEqual(k(cap, w, val, c, False), 0)
 
     def test_easy_case(self):
         cap = 3
         val = [1, 2, 3]
         w = [3, 2, 1]
         c = len(val)
-        self.assertEqual(k.knapsack(cap, w, val, c, permit_repetition=False), 5)
+        self.assertEqual(k(cap, w, val, c, False), 5)
 
     def test_knapsack(self):
         cap = 50
         val = [60, 100, 120]
         w = [10, 20, 30]
         c = len(val)
-        self.assertEqual(k.knapsack(cap, w, val, c, permit_repetition=False), 220)
+        self.assertEqual(k(cap, w, val, c, False), 220)
 
     def test_knapsack_repetition(self):
         cap = 50
         val = [60, 100, 120]
         w = [10, 20, 30]
         c = len(val)
-        self.assertEqual(k.knapsack(cap, w, val, c, permit_repetition=True), 300)
+        self.assertEqual(k(cap, w, val, c, True), 300)
 
 
 if __name__ == "__main__":
