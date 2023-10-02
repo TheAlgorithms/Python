93646f7439bcb56ab78596507419f762f85d3095
MihirRajeshPanchal <mihirpanchal5400@gmail.com>
2023-10-02 22:40:23 +0530

    added doctests properly

--- sorts/library_sort.py
+++ sorts/library_sort.py
@@ -26,23 +26,20 @@ def library_sort(collection: list) -> list:
     [-45, -5, -2]
     """
 
-    def insertion_sort(arr: list) -> None:
+    def insertion_sort(arr):
         """
-        Sorts a list using the Insertion Sort algorithm in place.
+        Sorts a list using the Insertion Sort algorithm.
 
-        :param arr: A mutable list containing comparable items.
-        :return: None
+        :param arr: A mutable list with comparable items.
+        :return: The same list ordered in ascending order.
 
         Examples:
-        >>> arr = [5, 2, 9, 1, 5]
-        >>> insertion_sort(arr)
-        >>> arr
-        [1, 2, 5, 5, 9]
-
-        >>> arr = [4, 3, 2, 1]
-        >>> insertion_sort(arr)
-        >>> arr
-        [1, 2, 3, 4]
+        >>> insertion_sort([5, 3, 2, 4, 1])
+        [1, 2, 3, 4, 5]
+        >>> insertion_sort([0])
+        [0]
+        >>> insertion_sort([])
+        []
         """
         for i in range(1, len(arr)):
             key = arr[i]
@@ -51,35 +48,6 @@ def library_sort(collection: list) -> list:
                 arr[j + 1] = arr[j]
                 j -= 1
             arr[j + 1] = key
-
-    def insertion_sort(arr: list) -> list:
-        """
-        Sorts a list using the Insertion Sort algorithm and returns a new sorted list.
-
-        :param arr: A list containing comparable items.
-        :return: A new sorted list.
-
-        Examples:
-        >>> arr = [5, 2, 9, 1, 5]
-        >>> sorted_arr = insertion_sort(arr)
-        >>> sorted_arr
-        [1, 2, 5, 5, 9]
-
-        >>> arr = [4, 3, 2, 1]
-        >>> sorted_arr = insertion_sort(arr)
-        >>> sorted_arr
-        [1, 2, 3, 4]
-        """
-        sorted_arr = arr.copy()
-        for i in range(1, len(sorted_arr)):
-            key = sorted_arr[i]
-            j = i - 1
-            while j >= 0 and key < sorted_arr[j]:
-                sorted_arr[j + 1] = sorted_arr[j]
-                j -= 1
-            sorted_arr[j + 1] = key
-
-        return sorted_arr
 
     if len(collection) <= 1:
         return collection
@@ -96,24 +64,21 @@ def library_sort(collection: list) -> list:
 
     return library(left, right)
 
-
 def library(left: list, right: list) -> list:
     """
-    Merge two sorted collections (left and right) into a single sorted collection.
+    Merges two sorted collections into a single sorted collection.
 
     :param left: A sorted collection
     :param right: A sorted collection
-    :return: A merged sorted collection.
+    :return: A merged and sorted collection
 
     Examples:
     >>> library([1, 3, 5], [2, 4, 6])
     [1, 2, 3, 4, 5, 6]
-
-    >>> library([0, 2, 4], [1, 3, 5])
-    [0, 1, 2, 3, 4, 5]
-
-    >>> library([7, 9, 11], [8, 10, 12])
-    [7, 8, 9, 10, 11, 12]
+    >>> library([1], [2])
+    [1, 2]
+    >>> library([], [1, 2, 3])
+    [1, 2, 3]
     """
     result = []
     i = j = 0
@@ -130,7 +95,6 @@ def library(left: list, right: list) -> list:
     result.extend(right[j:])
 
     return result
-
 
 if __name__ == "__main__":
     import doctest
