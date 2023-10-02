6c25ef0aad286f6f4c9e84d9a0f16587439349b8
Dev Agrawal <devagrawal04@gmail.com>
2023-10-02 05:38:21 +0530

    Update sum_bit_manipulation.py
    Updated variable names

--- maths/sum_bit_manipulation.py
+++ maths/sum_bit_manipulation.py
@@ -1,16 +1,16 @@
-def add_numbers(a: int, b: int) -> int:
+def add_numbers(int_1: int, int_2: int) -> int:
     """
     Add two integers using bitwise operations.
 
     Args:
-        a (int): The first integer.
-        b (int): The second integer.
+        int_1 (int): The first integer.
+        int_2 (int): The second integer.
 
     Returns:
         int: The sum of the two integers.
     """
-    while b > 0:
-        carry = a & b
-        a = a ^ b  # Sum
-        b = carry << 1
-    return a
+    while int_2 > 0:
+        carry = int_1 & int_2
+        int_1 = int_1 ^ int_2  # Sum
+        int_2 = carry << 1
+    return int_1
