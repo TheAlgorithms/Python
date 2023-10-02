94d0ba7ac0e6fa006a3bd3b75d740f8a619a6262
Lieann Razon <109248041+lrazcs@users.noreply.github.com>
2023-10-01 21:01:14 -0400

    update test_knapsack.py

--- knapsack/tests/test_knapsack.py
+++ knapsack/tests/test_knapsack.py
@@ -1,11 +1,3 @@
-"""
-Created on Fri Oct 16 09:31:07 2020
-
-@author: Dr. Tobias Schröder
-@license: MIT-license
-
-This file contains the test-suite for the knapsack problem.
-"""
 import unittest
 
 from knapsack import knapsack as k
@@ -13,40 +5,39 @@ from knapsack import knapsack as k
 
 class Test(unittest.TestCase):
     def test_base_case(self):
-        """
-        test for the base case
-        """
         cap = 0
         val = [0]
         w = [0]
         c = len(val)
-        self.assertEqual(k.knapsack(cap, w, val, c), 0)
+        self.assertEqual(k.knapsack(cap, w, val, c, permit_repetition=False), 0)
 
         val = [60]
         w = [10]
         c = len(val)
-        self.assertEqual(k.knapsack(cap, w, val, c), 0)
+        self.assertEqual(k.knapsack(cap, w, val, c, permit_repetition=False), 0)
 
     def test_easy_case(self):
-        """
-        test for the base case
-        """
         cap = 3
         val = [1, 2, 3]
         w = [3, 2, 1]
         c = len(val)
-        self.assertEqual(k.knapsack(cap, w, val, c), 5)
+        self.assertEqual(k.knapsack(cap, w, val, c, permit_repetition=False), 5)
 
     def test_knapsack(self):
-        """
-        test for the knapsack
-        """
         cap = 50
         val = [60, 100, 120]
         w = [10, 20, 30]
         c = len(val)
-        self.assertEqual(k.knapsack(cap, w, val, c), 220)
+        self.assertEqual(k.knapsack(cap, w, val, c, permit_repetition=False), 220)
+
+    def test_knapsack_repetition(self):
+        cap = 50
+        val = [60, 100, 120]
+        w = [10, 20, 30]
+        c = len(val)
+        self.assertEqual(k.knapsack(cap, w, val, c, permit_repetition=True), 300)
 
 
 if __name__ == "__main__":
     unittest.main()
+
