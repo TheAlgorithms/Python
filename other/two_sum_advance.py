0199e39bfa81c25d85b9c06a417a0db71893ca1a
Divya Gambhir <114723756+Diald@users.noreply.github.com>
2023-10-02 10:41:19 +0530

    Update two_sum_advance.py

--- other/two_sum_advance.py
+++ other/two_sum_advance.py
@@ -1,16 +1,4 @@
 """Pointer Solution
-
-Given a 1-indexed array of integers numbers that is
-already sorted
-in non-decreasing order, find two numbers such that they
-add up to a specific target number.
-Let these two numbers be numbers[index1] and numbers[index2]
-where 1 <= index1 < index2 < numbers.length.
-Return the indices of the two numbers, index1 and index2,
-added by one as an integer array [index1, index2] of length 2.
-The tests are generated such that there is exactly one solution.
-You may not use the same element twice.
-Your solution must use only constant extra space.
 Example 1:
 
 Input: numbers = [2,7,11,15], target = 9
