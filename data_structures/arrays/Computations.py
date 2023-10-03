c82682bb76a0c46b39a96e72de303fcdd1578f6f
XDFrost <pratham0gupta@gmail.com>
2023-10-03 23:59:31 +0530

    changed k to length

--- data_structures/arrays/Computations.py
+++ data_structures/arrays/Computations.py
@@ -1,26 +1,24 @@
 from itertools import combinations
 from typing import List
 
-
-def combine_recursive(nums: List[int], k: int) -> List[List[int]]:
+def combine_recursive(nums: List[int], length: int) -> List[List[int]]:
     """
-    Return all combinations of length k.
+    Return all combinations of length length.
 
     >>> combine_recursive([1, 2, 3], 2)
     [[1, 2], [1, 3], [2, 3]]
     """
     result: List[List[int]] = []
-
-    def combine_helper(current_combination: List[int], start: int, k: int) -> None:
-        if k == 0:
+    
+    def combine_helper(current_combination: List[int], start: int, length: int) -> None:
+        if length == 0:
             result.append(current_combination)
             return
         for i in range(start, len(nums)):
-            combine_helper(current_combination + [nums[i]], i + 1, k - 1)
+            combine_helper(current_combination + [nums[i]], i + 1, length - 1)
 
-    combine_helper([], 0, k)
+    combine_helper([], 0, length)
     return result
-
 
 if __name__ == "__main__":
     import doctest
