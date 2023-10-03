5923c9290dc765458cd698a20ca72e8e3c80645d
AVAniketh0905 <adimoolamaniketh@gmail.com>
2023-10-03 23:03:02 +0530

    format files

--- data_structures/binary_tree/serialize_deserialize_binary_tree.py
+++ data_structures/binary_tree/serialize_deserialize_binary_tree.py
@@ -1,31 +1,33 @@
 class TreeNode:
     """
     A binary tree node has a value, left child, and right child.
-    
+
     Props:
         val(int): The value of the node.
         left: The left child of the node.
         right: The right child of the node.
     """
-    def __init__(self, val:int = 0, left = None, right = None) -> None:
+
+    def __init__(self, val: int = 0, left=None, right=None) -> None:
         if not isinstance(val, int):
             raise TypeError("Value must be an integer.")
         self.val = val
         self.left = left
         self.right = right
 
+
 # Helper functions
 def are_trees_identical(root1: TreeNode, root2: TreeNode) -> bool:
     """
     Check if two binary trees are identical.
-    
+
     Args:
         root1 (TreeNode): Tree 1
         root2 (TreeNode): Tree 2
 
     Returns:
         bool: True if the trees are identical, False otherwise.
-    
+
     >>> root1 = TreeNode(1)
     >>> root1.left = TreeNode(2)
     >>> root1.right = TreeNode(3)
@@ -43,29 +45,30 @@ def are_trees_identical(root1: TreeNode, root2: TreeNode) -> bool:
     >>> are_trees_identical(root1, root2)
     False
     """
-    
+
     if not root1 and not root2:
         return True
     if not root1 or not root2:
         return False
-    
+
     return (
         root1.val == root2.val
         and are_trees_identical(root1.left, root2.left)
         and are_trees_identical(root1.right, root2.right)
     )
 
+
 # Main functions
 def serialize(root: TreeNode) -> str:
     """
     Serialize a binary tree to a string using preorder traversal.
-    
+
     Args:
         root(TreeNode): The root of the binary tree.
-    
+
     Returns:
         A string representation of the binary tree.
-        
+
     >>> root = TreeNode(1)
     >>> root.left = TreeNode(2)
     >>> root.right = TreeNode(3)
@@ -77,23 +80,24 @@ def serialize(root: TreeNode) -> str:
     >>> serialize(root)
     '1,null,null'
     """
-    
+
     # Use "null" to represent empty nodes in the serialization
     if not root:
-        return "null"  
-    
+        return "null"
+
     return str(root.val) + "," + serialize(root.left) + "," + serialize(root.right)
+
 
 def deserialize(data: str) -> TreeNode:
     """
     Deserialize a string to a binary tree.
-    
-    Args: 
+
+    Args:
         data(str): The serialized string.
-    
+
     Returns:
         The root of the binary tree.
-        
+
     >>> root = TreeNode(1)
     >>> root.left = TreeNode(2)
     >>> root.right = TreeNode(3)
@@ -110,25 +114,26 @@ def deserialize(data: str) -> TreeNode:
     >>> are_trees_identical(root, deserialized)
     False
     """
-    
+
     # Split the serialized string by comma to get node values
-    nodes = data.split(",") 
-    
+    nodes = data.split(",")
+
     def build_tree():
         # Get the next value from the list
-        val = nodes.pop(0)  
-        
+        val = nodes.pop(0)
+
         if val == "null":
             return None
-        
+
         node = TreeNode(int(val))
-        node.left = build_tree() # Recursively build left subtree
-        node.right = build_tree() # Recursively build right subtree
-        
+        node.left = build_tree()  # Recursively build left subtree
+        node.right = build_tree()  # Recursively build right subtree
+
         return node
 
     return build_tree()
-        
+
+
 if __name__ == "__main__":
     import doctest
 
