b0f71e9112463d78c6e76a102fc77d9fd42d8a94
ArunSiva <Arunsiva003@users.noreply.github.com>
2023-10-03 19:01:01 +0530

    newly updated infix_to_prefix_3

--- conversions/infix_to_prefix_conversions.py
+++ conversions/infix_to_prefix_conversions.py
@@ -14,7 +14,7 @@ from typing import TypeVar
 T = TypeVar("T", bound=Enum)
 
 
-def infix_to_prefix(expression) -> str:
+def infix_to_prefix(expression: str) -> str:
     """
     Convert an infix expression to a prefix expression.
     Args:
@@ -34,14 +34,72 @@ def infix_to_prefix(expression) -> str:
     '+*ab/cd'
     """
 
-    def precedence(operator) -> int:
+    def precedence(operator:str) -> int:
+        """
+        Get the precedence of an operator.
+        Args:
+            operator (str): The operator.
+        Returns:
+            int: The precedence value.
+        
+        >>> precedence("+")
+        1
+        >>> precedence("-")
+        1
+        >>> precedence("*")
+        2
+        >>> precedence("/")
+        2
+        >>> precedence("^")
+        3
+        >>> precedence("%")
+        0
+        """
         precedence_dict = {"+": 1, "-": 1, "*": 2, "/": 2, "^": 3}
         return precedence_dict.get(operator, 0)
 
-    def is_operator(char) -> bool:
+    def is_operator(char:str) -> bool:
+        """
+        Check if a character is an operator.
+        Args:
+            char (str): The character to check.
+        Returns:
+            bool: True if the character is an operator, False otherwise.
+
+        >>> is_operator("+")
+        True
+        >>> is_operator("-")
+        True
+        >>> is_operator("*")
+        True
+        >>> is_operator("/")
+        True
+        >>> is_operator("^")
+        True
+        >>> is_operator("a")
+        False
+        """
         return char in "+-*/^"
 
-    def infix_to_prefix(infix_expr) -> str:
+    def infix_to_prefix(infix_expr:str) -> str:
+        """
+        Convert an infix expression to a prefix expression.
+        Args:
+            expression (str): The infix expression to convert.
+        Returns:
+            str: The converted prefix expression.
+
+        >>> infix_to_prefix("2+2")
+        '+22'
+        >>> infix_to_prefix("(1+2)*3")
+        '*+123'
+        >>> infix_to_prefix("a*(b+c)")
+        '*a+bc'
+        >>> infix_to_prefix("1+2*3")
+        '+1*23'
+        >>> infix_to_prefix("a * b + c / d")
+        '+*ab/cd'
+        """
         postfix = []
         stack = []
         for char in infix_expr:
