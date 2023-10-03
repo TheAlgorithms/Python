e0a90325cd96e7a56379a316491561a30ed6bdc7
Rishabh-Saket <87156299+Rishabh-Saket@users.noreply.github.com>
2023-10-03 13:09:07 +0530

    Adding a alogorithm to reverse linked list in a certiain condition

---
+++ data_structures/linked_list/reverse_linked_list_II.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        i=1
+        arr=[]
+        h=head
+        while head:
+            if left==i:
+                arr.append(head.val)
+            elif left<i<right:
+                arr.append(head.val)
+            elif i==right:
+                arr.append(head.val)
+            i+=1
+            head=head.next
+        j=1
+        head=h
+        while head:
+            if j==left:
+                head.val=arr.pop(-1)
+            elif left<j<right:
+                head.val=arr.pop(-1)
+            elif j==right:
+                head.val=arr.pop(-1)
+            head=head.next
+            j+=1
+        return h
