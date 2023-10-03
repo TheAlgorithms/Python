a9ff2cf5e76a5d514b116c604d6aa0cd6e0f970b
ARYA SINGH <notaryasingh@gmail.com>
2023-10-03 22:55:18 +0530

    gKickstart_matrix_pattern_printing

---
+++ matrix/gkickstart_matrix_pattern_printing.py
@@ -0,0 +1,56 @@
+po = int(input("ENTER NUMBER OF TRIALS : "))
+for _l in range(po):
+    i = str(input("ENTER NUMBER OF ROWS AND COLUMNS : ").strip())
+    kpo = str(i).split(" ")
+    a = int(kpo[0])
+    b = int(kpo[1])
+
+    def matrix(r: int, c: int):
+        _symbols = [".", "+", "-", "|"]
+        t = c * 2 + 1
+        # first two lines
+        print(_symbols[0] * 2, end="")
+        c = 2
+        for _i in range(t - 2):
+            if c % 2 == 0:
+                print(_symbols[1], end="")
+            else:
+                print(_symbols[2], end="")
+            c = c + 1
+        print()
+        # second row
+        m = 2
+        print(_symbols[0] * 2, end="")
+        for _i in range(t - 2):
+            if m % 2 == 0:
+                print(_symbols[3], end="")
+            else:
+                print(_symbols[0], end="")
+            m = m + 1
+        print()
+        # third row onwards
+
+        f = 2 * r + 1
+        j = 2
+        for _i in range(f - 2):
+            if j % 2 == 0:
+                p = 2
+                for _ka in range(t):
+                    if p % 2 == 0:
+                        print(_symbols[1], end="")
+                    else:
+                        print(_symbols[2], end="")
+                    p = p + 1
+            else:
+                o = 2
+                for _kam in range(t):
+                    if o % 2 == 0:
+                        print(_symbols[3], end="")
+                    else:
+                        print(_symbols[0], end="")
+                    o = o + 1
+
+            j = j + 1
+            print()
+
+    matrix(a, b)
