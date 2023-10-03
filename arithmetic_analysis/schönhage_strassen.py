cea203d8c49d79c8455756fb169c09f8dab1ccd9
Wrrikk <86593125+Wrrikk@users.noreply.github.com>
2023-10-03 09:38:24 +0530

    Update schönhage_strassen.py

--- arithmetic_analysis/schönhage_strassen.py
+++ arithmetic_analysis/schönhage_strassen.py
@@ -1,3 +1,5 @@
+import cmath
+
 def fast_fourier_transform(poly: list) -> list:
     """
     Compute the Fast Fourier Transform of a polynomial.
@@ -23,7 +25,6 @@ def fast_fourier_transform(poly: list) -> list:
         combined[i + n//2] = even_terms[i] - t
     return combined
 
-
 def inverse_fast_fourier_transform(poly: list) -> list:
     """
     Compute the Inverse Fast Fourier Transform of a polynomial.
@@ -41,6 +42,64 @@ def inverse_fast_fourier_transform(poly: list) -> list:
     poly = fast_fourier_transform([x.conjugate() for x in poly])
     return [x.conjugate()/n for x in poly]
 
+def multiply_using_fft(poly1: list, poly2: list) -> list:
+    """
+    Multiply two polynomials using FFT.
+    
+    Args:
+    - poly1: First polynomial represented as a list of complex coefficients.
+    - poly2: Second polynomial represented as a list of complex coefficients.
+    
+    Returns:
+    - A list of complex coefficients representing the product polynomial.
+    """
+    n = len(poly1) + len(poly2) - 1
+    m = 1
+    while m < n:
+        m *= 2
+    poly1 += [0] * (m - len(poly1))
+    poly2 += [0] * (m - len(poly2))
 
-# Re-running the doctests
-doctest.testmod()
+    transformed_poly1 = fast_fourier_transform(poly1)
+    transformed_poly2 = fast_fourier_transform(poly2)
+    return [a*b for a, b in zip(transformed_poly1, transformed_poly2)]
+
+def perform_carry_propagation(values: list) -> list:
+    """
+    Convert the complex values obtained after inverse FFT into integers by rounding and carry propagation.
+    
+    Args:
+    - values: List of complex numbers.
+    
+    Returns:
+    - List of integers after performing carry propagation.
+    """
+    result = []
+    carry = 0
+    for val in values:
+        current_value = int(val.real + 0.5) + carry
+        result.append(current_value % 10)
+        carry = current_value // 10
+    while carry:
+        result.append(carry % 10)
+        carry //= 10
+    return result[::-1]
+
+def fast_multiply_numbers(num1: int, num2: int) -> int:
+    """
+    Multiply two integers using FFT-based polynomial multiplication.
+    
+    Args:
+    - num1: First integer.
+    - num2: Second integer.
+    
+    Returns:
+    - The product of the two integers.
+    
+    >>> fast_multiply_numbers(12345, 6789)
+    83810205
+    """
+    poly1 = [int(digit) for digit in str(num1)][::-1]
+    poly2 = [int(digit) for digit in str(num2)][::-1]
+    product_poly = inverse_fast_fourier_transform(multiply_using_fft(poly1, poly2))
+    return int(''.join(map(str, perform_carry_propagation(product_poly))))
