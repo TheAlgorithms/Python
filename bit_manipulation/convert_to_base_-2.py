cb397016c2758352f21e28d34f48935fddf63a66
Tanish <ktanish2002@gmail.com>
2023-10-03 18:27:58 +0530

    My commit for #9588

---
+++ bit_manipulation/convert_to_base_-2.py
@@ -0,0 +1,95 @@
+def baseNeg2(n: int) -> str:
+    """
+    Converts an integer 'n' to its negative base (-2) representation and returns it as a string.
+
+    The negative base (-2) representation is a positional numeral system in which:
+    - The base is -2.
+    - The only digits allowed are '0' and '1'.
+    - The value of the number is calculated as follows:
+        - Let's say the number is 'n'.
+        - Starting from the rightmost digit, the value at each position is determined by (-2)^position * digit.
+        - The sum of these values at all positions gives the decimal value of the number 'n'.
+
+    Parameters:
+    - n (int): The integer to be converted to its negative base (-2) representation.
+
+    Returns:
+    - str: The negative base (-2) representation of the input integer 'n' as a string.
+
+    Examples:
+    >>> baseNeg2(0)
+    '0'
+    
+    >>> baseNeg2(1)
+    '1'
+
+    >>> baseNeg2(2)
+    '110'
+
+    >>> baseNeg2(3)
+    '111'
+
+    >>> baseNeg2(-3)
+    '1101'
+
+    >>> baseNeg2(10)
+    '11110'
+
+    >>> baseNeg2(-10)
+    '1010'
+
+    Edge Cases:
+    - If the input 'n' is 0, the function returns "0" since the negative base (-2) representation of 0 is "0".
+
+    Errors for Incorrect Input:
+    1. If the input 'n' is not an integer, a TypeError will be raised, as the function expects 'n' to be an integer.
+    >>> baseNeg2("abc")
+    Traceback (most recent call last):
+        ...
+    TypeError: Input 'n' must be an integer
+
+    2. If 'n' is not in the expected range for the negative base (-2) representation, the result may not be valid, but no specific error will be raised.
+    >>> baseNeg2(0.5)
+    Traceback (most recent call last):
+        ...
+    TypeError: Input 'n' must be an integer
+    
+    >>> baseNeg2(-0.5)
+    Traceback (most recent call last):
+        ...
+    TypeError: Input 'n' must be an integer
+    
+    >>> baseNeg2(255)
+    '100000011'
+    """
+    
+    # Check if the input 'n' is an integer
+    if not isinstance(n, int):
+        raise TypeError("Input 'n' must be an integer")
+    
+    # Handle the case when 'n' is 0
+    if n == 0:
+        return "0"
+    
+    result = []
+    while n != 0:
+        # Calculate the remainder when dividing by -2
+        remainder = n % (-2)
+        # Perform integer division by -2
+        n //= (-2)
+        
+        # If the remainder is negative, adjust it and increment 'n' accordingly
+        if remainder < 0:
+            remainder += 2
+            n += 1
+        
+        # Append the remainder (0 or 1) as a string to the result
+        result.append(str(remainder))
+    
+    # Reverse the result list and join the elements to form the final negative base (-2) representation
+    return ''.join(result[::-1])
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
