753da5a45d10115bf2f1e45a8aab49d6dfbe4ef3
SiddhantTotade <siddhanttotade.1994@gmail.com>
2023-10-04 00:48:10 +0530

    docs : add comment in circular_linked_list.py and swap_nodes.py

--- data_structures/linked_list/circular_linked_list.py
+++ data_structures/linked_list/circular_linked_list.py
@@ -1,4 +1,4 @@
-from __future__ import annotations
+from __future__ import annotations  # Enables forward annotations for type hinting
 
 from collections.abc import Iterator
 from typing import Any
@@ -6,16 +6,31 @@ from typing import Any
 
 class Node:
     def __init__(self, data: Any):
+        """
+        Initialize a new Node with the given data.
+
+        Args:
+            data (Any): The data to be stored in the node.
+        """
         self.data: Any = data
-        self.next: Node | None = None
+        self.next: Node | None = None  # Reference to the next node
 
 
 class CircularLinkedList:
     def __init__(self):
-        self.head = None
-        self.tail = None
+        """
+        Initialize an empty Circular Linked List.
+        """
+        self.head = None  # Reference to the head (first node)
+        self.tail = None  # Reference to the tail (last node)
 
     def __iter__(self) -> Iterator[Any]:
+        """
+        Iterator method to allow iterating through the Circular Linked List.
+
+        Yields:
+            Iterator[Any]: The data of each node in the list.
+        """
         node = self.head
         while self.head:
             yield node.data
@@ -24,25 +39,59 @@ class CircularLinkedList:
                 break
 
     def __len__(self) -> int:
+        """
+        Get the length (number of nodes) in the Circular Linked List.
+
+        Returns:
+            int: The number of nodes in the list.
+        """
         return sum(1 for _ in self)
 
     def __repr__(self):
+        """
+        Generate a string representation of the Circular Linked List.
+
+        Returns:
+            str: A string representation of the list in the format "data1->data2->...->dataN".
+        """
         return "->".join(str(item) for item in iter(self))
 
     def insert_tail(self, data: Any) -> None:
+        """
+        Insert a node with the given data at the end of the Circular Linked List.
+
+        Args:
+            data (Any): The data to be inserted.
+        """
         self.insert_nth(len(self), data)
 
     def insert_head(self, data: Any) -> None:
+        """
+        Insert a node with the given data at the beginning of the Circular Linked List.
+
+        Args:
+            data (Any): The data to be inserted.
+        """
         self.insert_nth(0, data)
 
     def insert_nth(self, index: int, data: Any) -> None:
+        """
+        Insert a node with the given data at the specified index in the Circular Linked List.
+
+        Args:
+            index (int): The index at which to insert the data.
+            data (Any): The data to be inserted.
+
+        Raises:
+            IndexError: If the index is out of range.
+        """
         if index < 0 or index > len(self):
             raise IndexError("list index out of range.")
         new_node = Node(data)
         if self.head is None:
-            new_node.next = new_node  # first node points itself
+            new_node.next = new_node  # First node points to itself
             self.tail = self.head = new_node
-        elif index == 0:  # insert at head
+        elif index == 0:  # Insert at the head
             new_node.next = self.head
             self.head = self.tail.next = new_node
         else:
@@ -51,22 +100,52 @@ class CircularLinkedList:
                 temp = temp.next
             new_node.next = temp.next
             temp.next = new_node
-            if index == len(self) - 1:  # insert at tail
+            if index == len(self) - 1:  # Insert at the tail
                 self.tail = new_node
 
     def delete_front(self):
+        """
+        Delete and return the data of the node at the front of the Circular Linked List.
+
+        Returns:
+            Any: The data of the deleted node.
+
+        Raises:
+            IndexError: If the list is empty.
+        """
         return self.delete_nth(0)
 
     def delete_tail(self) -> Any:
+        """
+        Delete and return the data of the node at the end of the Circular Linked List.
+
+        Returns:
+            Any: The data of the deleted node.
+
+        Raises:
+            IndexError: If the list is empty.
+        """
         return self.delete_nth(len(self) - 1)
 
     def delete_nth(self, index: int = 0) -> Any:
+        """
+        Delete and return the data of the node at the specified index in the Circular Linked List.
+
+        Args:
+            index (int): The index of the node to be deleted. Defaults to 0.
+
+        Returns:
+            Any: The data of the deleted node.
+
+        Raises:
+            IndexError: If the index is out of range.
+        """
         if not 0 <= index < len(self):
             raise IndexError("list index out of range.")
         delete_node = self.head
-        if self.head == self.tail:  # just one node
+        if self.head == self.tail:  # Just one node
             self.head = self.tail = None
-        elif index == 0:  # delete head node
+        elif index == 0:  # Delete head node
             self.tail.next = self.tail.next.next
             self.head = self.head.next
         else:
@@ -75,16 +154,24 @@ class CircularLinkedList:
                 temp = temp.next
             delete_node = temp.next
             temp.next = temp.next.next
-            if index == len(self) - 1:  # delete at tail
+            if index == len(self) - 1:  # Delete at tail
                 self.tail = temp
         return delete_node.data
 
     def is_empty(self) -> bool:
+        """
+        Check if the Circular Linked List is empty.
+
+        Returns:
+            bool: True if the list is empty, False otherwise.
+        """
         return len(self) == 0
 
 
 def test_circular_linked_list() -> None:
     """
+    Test cases for the CircularLinkedList class.
+
     >>> test_circular_linked_list()
     """
     circular_linked_list = CircularLinkedList()
