e38a34b7fb1bdc3536e2ebee15c42d3e85ab5ed7
Jallepalli Harsha Vardhan <87539405+harshajv7981@users.noreply.github.com>
2023-10-03 11:10:07 +0530

    Update genetic_algorithm_optimisation.py

--- genetic_algorithm/genetic_algorithm_optimisation.py
+++ genetic_algorithm/genetic_algorithm_optimisation.py
@@ -1,55 +1,33 @@
-import random
+#import random
 
-# Define the fitness function (objective function)
-def fitness_function(x):
-    return x**2 + 3*x + 2
+def fitness_function(x: float) -> float:
+    """
+    Calculate the fitness (objective) function value for a given input.
 
-# Genetic Algorithm parameters
-population_size = 100
-mutation_rate = 0.01
-num_generations = 100
+    Args:
+        x (float): The input value for which the fitness is calculated.
 
-# Initialize the population with random individuals
-def initialize_population(size):
-    return [random.uniform(-10, 10) for _ in range(size)]
+    Returns:
+        float: The fitness value calculated for the input.
 
-# Select individuals for reproduction based on their fitness
-def select_parents(population):
-    fitness_scores = [fitness_function(x) for x in population]
-    total_fitness = sum(fitness_scores)
-    probabilities = [score / total_fitness for score in fitness_scores]
-    return random.choices(population, probabilities, k=2)
+    Raises:
+        ValueError: If the input is not a valid floating-point number.
 
-# Create a new individual through crossover (blend two parents)
-def crossover(parent1, parent2):
-    alpha = random.uniform(0, 1)
-    child = alpha * parent1 + (1 - alpha) * parent2
-    return child
+    Example:
+    >>> fitness_function(2.5)
+    12.25
+    >>> fitness_function(-1.0)
+    6.0
+    """
+    if not isinstance(x, (int, float)):
+        raise ValueError("Input must be a valid number.")
 
-# Mutate an individual
-def mutate(individual):
-    if random.random() < mutation_rate:
-        return individual + random.uniform(-0.1, 0.1)
-    else:
-        return individual
+    # Define your fitness function here (e.g., x^2, or any other function)
+    return x**2 + 3 * x + 2
 
-# Genetic Algorithm
-population = initialize_population(population_size)
 
-for generation in range(num_generations):
-    new_population = []
-    for _ in range(population_size // 2):
-        parent1, parent2 = select_parents(population)
-        child1 = crossover(parent1, parent2)
-        child2 = crossover(parent1, parent2)
-        child1 = mutate(child1)
-        child2 = mutate(child2)
-        new_population.extend([child1, child2])
-    population = new_population
-
-# Find the best individual in the final population
-best_individual = max(population, key=fitness_function)
-best_fitness = fitness_function(best_individual)
-
-print(f"Best individual: {best_individual}")
-print(f"Best fitness: {best_fitness}")
+if __name__ == "__main__":
+    # Example usage
+    x_value = float(input("Enter the value of x: ").strip())
+    fitness = fitness_function(x_value)
+    print(f"The fitness for x = {x_value} is {fitness}.")
