c637530ccf6facf9008d49cb0fce34a2700c617a
Saikat Dey <59501072+saikat2955@users.noreply.github.com>
2023-10-03 18:48:22 +0530

    Create Law_of_Cosines.py
    Added Law of Cosines rule in python integration

---
+++ maths/Law_of_Cosines.py
@@ -0,0 +1,28 @@
+import math
+
+def law_of_cosines(a, b, angle_c):
+    """
+    Calculate the length of the third side of a triangle using the Law of Cosines.
+
+    :param a: Length of side 'a'
+    :param b: Length of side 'b'
+    :param angle_c: Measure of angle 'C' in degrees
+    :return: Length of side 'c'
+    """
+    # Convert the angle from degrees to radians
+    angle_c_radians = math.radians(angle_c)
+    
+    # Use the Law of Cosines formula
+    c_squared = a**2 + b**2 - 2 * a * b * math.cos(angle_c_radians)
+    
+    # Take the square root to find the length of side 'c'
+    c = math.sqrt(c_squared)
+    
+    return c
+
+# Example usage:
+a = 5
+b = 7
+angle_C_degrees = 60
+c = law_of_cosines(a, b, angle_C_degrees)
+print(f"The length of side 'c' is {c:.2f}")
