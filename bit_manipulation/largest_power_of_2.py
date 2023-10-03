d0012f9172c6bd54455afcaf484c256b178e6c6b
siddwarr <siddharthwarrier2@gmail.com>
2023-10-03 13:07:59 +0530

    updated largest_power_of_2

--- bit_manipulation/largest_power_of_2.py
+++ bit_manipulation/largest_power_of_2.py
@@ -1,8 +1,11 @@
 def largest_power_of_2(number: int) -> int:
     """
-    returns the largest power of 2 that is less than or equal to the given number
-    The way this works is that we shift the binary form of the number to the right until we reach the last set bit
-    Using the number of times we had to shift to find the last set bit, we find the 2**(no of times shifted) which will be the ans
+    returns the largest power of 2 that is less than
+    or equal to the given number
+    The way this works is that we shift the binary form of the number
+    to the right until we reach the last set bit
+    Using the number of times we had to shift to find the last set bit,
+    we find the 2**(no of times shifted) which will be the ans
     >>> largest_power_of_2(0)
     0
     >>> largest_power_of_2(1)
