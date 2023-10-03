531419e6c5bf70cdeb95661914f606e157c6d1d5
Saikat Dey <59501072+saikat2955@users.noreply.github.com>
2023-10-03 19:16:44 +0530

    Update law_of_cosines.py
    I've updated the code to include type hints, a more descriptive parameter name, and added doctests for the law_of_cosines function. Please note that you would typically save this code in a Python file (e.g., law_of_cosines.py) and import it as needed for testing and use in other scripts.

--- maths/law_of_cosines.py
+++ maths/law_of_cosines.py
@@ -3,34 +3,33 @@
 
 import math
 
-
-def law_of_cosines(a, b, angle_c):
+def law_of_cosines(a: float, b: float, angle_c: float) -> float:
     """
     Calculate the length of the third side of a triangle using the Law of Cosines.
 
-    :param a: Length of side 'a'
-    :param b: Length of side 'b'
-    :param angle_c: Measure of angle 'C' in degrees
-    :return: Length of side 'c'
+    :param a: The length of side 'a' (float).
+    :param b: The length of side 'b' (float).
+    :param angle_c: The measure of angle 'C' in degrees (float).
+    
+    :return: The length of side 'c' (float).
+    
+    >>> law_of_cosines(5, 7, 60)
+    5.830951894845301
+    >>> law_of_cosines(3, 4, 90)
+    5.0
     """
     # Convert the angle from degrees to radians
     angle_c_radians = math.radians(angle_c)
-
+    
     # Use the Law of Cosines formula
     c_squared = a**2 + b**2 - 2 * a * b * math.cos(angle_c_radians)
-
+    
     # Take the square root to find the length of side 'c'
     c = math.sqrt(c_squared)
-
+    
     return c
 
-
 if __name__ == "__main__":
-    # Input values from the user or other sources
-    a = float(input("Enter the length of side 'a': "))
-    b = float(input("Enter the length of side 'b': "))
-    angle_C_degrees = float(input("Enter the measure of angle 'C' in degrees: "))
+    import doctest
+    doctest.testmod()
 
-    # Calculate the length of side 'c'
-    c = law_of_cosines(a, b, angle_C_degrees)
-    print(f"The length of side 'c' is {c:.2f}")
