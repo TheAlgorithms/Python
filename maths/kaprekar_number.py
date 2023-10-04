c7d8d77c4983fa7a99c3baadea3a6ce166b9a8df
ParalallHimself <112560813+ParalallHimself@users.noreply.github.com>
2023-10-04 11:51:55 +0530

    Created a kaprekar_number.py
    Python program to check for Kaprekar number

---
+++ maths/kaprekar_number.py
@@ -0,0 +1,45 @@
+# Python program to check if a number is Kaprekar number or not
+
+import math
+
+# Returns true if n is a Kaprekar number, else false
+def iskaprekar(n):
+	if n == 1 :
+		return True
+	
+	#Counting number of digits in square
+	sq_n = n * n
+	count_digits = 1
+	while not sq_n == 0 :
+		count_digits = count_digits + 1
+		sq_n = sq_n // 10
+	
+	sq_n = n*n # Recompute square as it was changed
+	
+	# Split the square at different points and see if sum
+	# of any pair of splitted numbers is equal to n.
+	r_digits = 0
+	while r_digits< count_digits :
+		r_digits = r_digits + 1
+		eq_parts = (int) (math.pow(10, r_digits))
+		
+		# To avoid numbers like 10, 100, 1000 (These are not
+		# Kaprekar numbers
+		if eq_parts == n :
+			continue
+		
+		# Find sum of current parts and compare with n
+		
+		sum = sq_n//eq_parts + sq_n % eq_parts
+		if sum == n :
+			return True
+	
+	# compare with original number
+	return False
+	
+# Driver method
+i=1
+while i<10000 :
+	if (iskaprekar(i)) :
+		print (i,end=" ")
+	i = i + 1
