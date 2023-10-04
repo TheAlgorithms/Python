eea53f39a1de7ca1cad821fb8b6ae07b7975af6c
Manas <99756067+scienmanas@users.noreply.github.com>
2023-10-04 10:40:51 +0530

    Create main.py

---
+++ web_programming/Insta-Follower-Bot/main.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from login_and_perform import Login
+import pandas
+
+AccountsToFollow_Df = pandas.read_csv(r"Day 52\Insta-Folllower-Bot\Data\accounts_list.csv")
+AccountsToFollow = AccountsToFollow_Df.account.tolist()
+
+option = webdriver.ChromeOptions()
+option.add_experimental_option('detach', True)
+
+driver = webdriver.Chrome(options=option)
+
+loginn = Login(driver)
+loginn.logintoinsta()
+for account in AccountsToFollow :
+    loginn.followerBot(account)
+    
+driver.quit()
