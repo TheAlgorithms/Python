61d2f59d4f6468ddeba8910d8692a2a144a2b084
Manas <99756067+scienmanas@users.noreply.github.com>
2023-10-04 16:46:26 +0530

    Update Spiral_Matrix.py

--- data_structures/arrays/Spiral_Matrix.py
+++ data_structures/arrays/Spiral_Matrix.py
@@ -44,6 +44,6 @@ def spiral_order(matrix):
 if __name__ == "__main__":
     import doctest
 
-    result = spiral_roder([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+    result = spiral_order([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
     print(result)
     doctest.testmod()
