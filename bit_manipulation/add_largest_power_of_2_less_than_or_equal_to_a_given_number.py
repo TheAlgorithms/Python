f518c1ff60509087ab5cc9ee273b1f7ceb4713f3
Sharnabh Banerjee <banerjeesharnabh@gmail.com>
2023-10-04 20:15:49 +0530

    Fixes #9374
    Fixes #9374

--- bit_manipulation/add_largest_power_of_2_less_than_or_equal_to_a_given_number.py
+++ bit_manipulation/add_largest_power_of_2_less_than_or_equal_to_a_given_number.py
@@ -24,11 +24,15 @@ def add_largest(num: int) -> int:
     1511
 
     """
+
+    # Checks if Float or not
     if isinstance(num, float):
         raise TypeError("num must be an integer !!")
+    # Checks if negative or Zero
     if num <= 0:
         return 0
     res = 1
+    # Left Bit Shift till it res is less than or equal to the given number
     while (res << 1) <= num:
         res <<= 1
     res += num
