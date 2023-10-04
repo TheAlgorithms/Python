9ea0e1d5ea039a81ceda0ebb762b999035f4974a
parth <parthkhandenath123@gmail.com>
2023-10-05 00:57:40 +0530

    added dp algorithm to count distinct subsequences of a string in another string

---
+++ dynamic_programming/distinct_subsequences.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+
+def subsequence_counting(s1: str, s2: str, n: int, m: int) -> int:
+    """
+    Uses bottom-up dynamic programming/tabulation
+    to count the number of distinct
+    subsequences of string s2 in string s1
+
+    >>> s1 = "babgbag"
+    >>> s2 = "bag"
+    >>> subsequence_counting(s1, s2, len(s1), len(s2))
+    1
+    """
+    # Initialize a DP table to store
+    # the count of distinct subsequences
+    dp = [[0 for i in range(m + 1)] for j in range(n + 1)]
+
+    # Base case: There is exactly one subsequence
+    # of an empty string s2 in s1
+    for i in range(n + 1):
+        dp[i][0] = 1
+
+    # Initialize dp[0][i] to 0 for i > 0
+    # since an empty s1 cannot have
+    # a non-empty subsequence of s2
+    for i in range(1, m + 1):
+        dp[0][i] = 0
+
+    # Fill in the DP table using dynamic programming
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            # If the current characters match, we have two choices:
+            # 1. Include the current character in
+            #    both s1 and s2 (dp[i-1][j-1])
+            # 2. Skip the current character in s1 (dp[i-1][j])
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
+            else:
+                dp[i - 1][j]
+
+    # The final value in dp[n][m] is the count of distinct subsequences
+    return dp[n][m]
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
+    s1 = "babgbag"
+    s2 = "bag"
+
+    # Find the number of distinct subsequences of string s2 in string s1
+    print("The Count of Distinct Subsequences is ",end="")
+    print(subsequence_counting(s1, s2, len(s1), len(s2)))
