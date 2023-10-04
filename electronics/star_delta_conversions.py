5ce34622db9153b22ccdf3e731223def1be78b98
Yosef Sirak <yosefsirak6@gmail.com>
2023-10-04 10:27:23 -0700

    updated to handle ac impedances

--- electronics/star_delta_conversions.py
+++ electronics/star_delta_conversions.py
@@ -2,73 +2,106 @@
 # https://en.wikipedia.org/wiki/Y-%CE%94_transform
 
 
+
 def star_to_delta(
-    resistor_a: float,
-    resistor_b: float,
-    resistor_c: float,
-) -> list[float]:
+    impedance_a: complex,
+    impedance_b: complex,
+    impedance_c: complex,
+) -> list[complex]:
     """
 
-    Convert's star resistor arrangement to delta
-
+    Convert's star impedance arrangement to delta
+    
     Examples:
-    >>> star_to_delta(5,7.5,3.0)
-    [15.0, 10.0, 25.0]
-    >>> star_to_delta(5,75,12)
-    [267.0, 17.8, 111.25]
-    >>> star_to_delta(-5,75,12)
-    Traceback (most recent call last):
-      ...
-    ValueError: resistance value is zero or negative
-    """
+    1.) Impedance with resistances and zero reactance
+    >>> star_to_delta(complex(2,0),complex(1,0),complex(1,0)) 
+    [(2.5+0j), (5+0j), (5+0j)]
 
-    if resistor_a <= 0 or resistor_b <= 0 or resistor_c <= 0:
-        raise ValueError("resistance value is zero or negative")
+    2.) Impedance with resistance and reactance
+    >>> star_to_delta(complex(2,0),complex(1,2),complex(1,6))
+    [(-3.5+12j), (8.2+7.6j), (3.702702702702702+1.7837837837837838j)]
+
+    3.) Zero impedance
+    >>> star_to_delta(complex(0,0),complex(1,2),complex(1,6))
+    Traceback (most recent call last):
+       ...
+    ValueError: entered impedance value is zero
+    
+    4.) Negative resistance
+    >>> star_to_delta(complex(12,0),complex(-1,2),complex(1,6))
+    Traceback (most recent call last):
+       ...
+    ValueError: entered resistance value is negative
+    """
+    
+    if abs(impedance_a) == 0 or abs(impedance_b) == 0 or abs(impedance_c) == 0:
+        raise ValueError("entered impedance value is zero")
+
+    if impedance_a.real < 0 or impedance_b.real < 0 or impedance_c.real < 0:
+        raise ValueError("entered resistance value is negative")
+    
+
 
     delta_arrangement = []
-    resistance_chain = (
-        resistor_a * resistor_b + resistor_b * resistor_c + resistor_c * resistor_a
+    impedance_chain = (
+        impedance_a * impedance_b + impedance_b * impedance_c + impedance_c * impedance_a
     )
-    delta_arrangement.append(resistance_chain / resistor_a)
-    delta_arrangement.append(resistance_chain / resistor_b)
-    delta_arrangement.append(resistance_chain / resistor_c)
+    delta_arrangement.append(impedance_chain / impedance_a)
+    delta_arrangement.append(impedance_chain / impedance_b)
+    delta_arrangement.append(impedance_chain / impedance_c)
 
     return delta_arrangement
 
 
 def delta_to_star(
-    resistor_a: float,
-    resistor_b: float,
-    resistor_c: float,
-) -> list[float]:
-    """
+    impedance_a: complex,
+    impedance_b: complex,
+    impedance_c: complex,
+) -> list[complex]:
+     """
+     
+     Convert's delta impedance arrangement to star
+    
+     Examples:
+     1.) Impedance with resistances and zero reactance
+     >>> delta_to_star(complex(3,0),complex(5,0),complex(7,0)) 
+     [(2.3333333333333335+0j), (1.4+0j), (1+0j)]
 
-    Convert's delta resistor arrangement to star
-
-    Examples:
-    >>> delta_to_star(10,25,15)
-    [5.0, 7.5, 3.0]
-    >>> delta_to_star(6,6,6)
-    [2.0, 2.0, 2.0]
-    >>> delta_to_star(-6,5,6)
-    Traceback (most recent call last):
+     2.) Impedance with resistance and reactance
+     >>> delta_to_star(complex(1,3),complex(2,0),complex(0,-3))
+     [-2j, (3-1j), (0.6666666666666666+2j)]
+    
+     3.) Zero impedance
+     >>> delta_to_star(complex(0,0),complex(1,2),complex(1,6))
+     Traceback (most recent call last):
        ...
-    ValueError: resistance value is zero or negative
-    """
+     ValueError: entered impedance value is zero
+    
+     4.) Negative resistance
+     >>> delta_to_star(complex(12,0),complex(-1,2),complex(1,6))
+     Traceback (most recent call last):
+       ...
+     ValueError: entered resistance value is negative
+     """
+     if abs(impedance_a) == 0 or abs(impedance_b) == 0 or abs(impedance_c) == 0:
+        raise ValueError("entered impedance value is zero")
 
-    if resistor_a <= 0 or resistor_b <= 0 or resistor_c <= 0:
-        raise ValueError("resistance value is zero or negative")
+     if impedance_a.real < 0 or impedance_b.real < 0 or impedance_c.real <0:
+        raise ValueError("entered resistance value is negative")
+    
+     star_arrangement = []
+     impedance_sum = impedance_a + impedance_b + impedance_c
+     star_arrangement.append(( impedance_b  *  impedance_c) / impedance_sum)
+     star_arrangement.append(( impedance_c  *  impedance_a) / impedance_sum)
+     star_arrangement.append(( impedance_a  *  impedance_b) / impedance_sum)
+    
+    
 
-    star_arrangement = []
-    resistance_sum = resistor_a + resistor_b + resistor_c
-    star_arrangement.append((resistor_a * resistor_b) / resistance_sum)
-    star_arrangement.append((resistor_b * resistor_c) / resistance_sum)
-    star_arrangement.append((resistor_c * resistor_a) / resistance_sum)
-
-    return star_arrangement
+     return star_arrangement
 
 
 if __name__ == "__main__":
     import doctest
-
+    
+ 
     doctest.testmod()
