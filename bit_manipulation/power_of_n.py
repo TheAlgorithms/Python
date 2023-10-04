8c1116084d3f7c5270ce69a0d317d7f2cd657d9d
Himanshu <ghimanshugupta70@gmail.com>
2023-10-04 23:57:25 +0530

    Added power_of_n.py and fixed naming convention

--- bit_manipulation/power_of_n.py
+++ bit_manipulation/power_of_n.py
@@ -3,38 +3,38 @@ __author__ = "Himanshu Gupta"
 __version__ = "1.0.0"
 __date__ = "2023-09-03"
 
-def binaryExponentiation(x: float, n: int) -> float:
+def binary_exponentiation(number: float, power: int) -> float:
     """
-    Function to calculate x raised to the power n (i.e., x^n) where x is a float number and n is an integer and it will return float value
+    Function to calculate number raised to the power (i.e., number^power) where number is a float number and power is an integer and it will return float value
 
-    >>> binaryExponentiation(2.00000, 10)
+    >>> binary_exponentiation(2.00000, 10)
     1024.0
 
-    >>> binaryExponentiation(2.10000, 3)
+    >>> binary_exponentiation(2.10000, 3)
     9.261000000000001
 
-    >>> binaryExponentiation(2.00000, -2)
+    >>> binary_exponentiation(2.00000, -2)
     0.25
     """
 
-    if n == 0:
+    if power == 0:
         return 1
 
-    # Handle case where, n < 0.
-    if n < 0:
-        n = -1 * n
-        x = 1.0 / x
+    # Handle case where, power < 0.
+    if power < 0:
+        power = -1 * power
+        number = 1.0 / number
 
     # Perform Binary Exponentiation.
     result = 1
-    while n != 0:
-        # If 'n' is odd we multiply result with 'x' and reduce 'n' by '1'.
-        if n % 2 == 1:
-            result *= x
-            n -= 1
-        # We square 'x' and reduce 'n' by half, x^n => (x^2)^(n/2).
-        x *= x
-        n //= 2
+    while power != 0:
+        # If 'power' is odd we multiply result with 'number' and reduce 'power' by '1'.
+        if power % 2 == 1:
+            result *= number
+            power -= 1
+        # We square 'number' and reduce 'power' by half, number^power => (number^2)^(power/2).
+        number *= number
+        power //= 2
     return result
 
 
