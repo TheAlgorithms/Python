bcff50cf1cead9eaf2c0d7a3c0b3df6ae28971e1
Manas <99756067+scienmanas@users.noreply.github.com>
2023-10-04 16:34:33 +0530

    Create Spiral_Matrix.py

---
+++ data_structures/arrays/Spiral_Matrix.py
@@ -0,0 +1,42 @@
+def spiralOrder(matrix):
+    ans = []
+    Row = len(matrix)
+    Column = len(matrix[0])
+    count = 0
+    total = Row * Column
+    
+    startingRow = 0
+    startingcol = 0
+    endingRow = Row - 1
+    endingColumn = Column - 1
+    while count < total:
+        
+        for i in range(startingcol, endingColumn + 1):
+            if count >= total:
+                break
+            ans.append(matrix[startingRow][i])
+            count += 1
+        startingRow += 1
+        
+        for i in range(startingRow, endingRow + 1):
+            if count >= total:
+                break
+            ans.append(matrix[i][endingColumn])
+            count += 1
+        endingColumn -= 1
+        
+        for i in range(endingColumn, startingcol - 1, -1):
+            if count >= total:
+                break
+            ans.append(matrix[endingRow][i])
+            count += 1
+        endingRow -= 1
+        
+        for i in range(endingRow, startingRow - 1, -1):
+            if count >= total:
+                break
+            ans.append(matrix[i][startingcol])
+            count += 1
+        startingcol += 1
+        
+    return ans
