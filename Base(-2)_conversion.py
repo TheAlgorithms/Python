0a43d074c241e441e2cc7b3327fb447a00aabbee
Abhay Pandey <116704975+Aloneking789@users.noreply.github.com>
2023-10-04 07:24:10 +0530

    Create Base(-2)_conversion.py
    Fixes #9588 
    
    This code defines a decimal_to_base_minus_2 function that takes a decimal number as input and converts it to base -2.

---
+++ Base(-2)_conversion.py
@@ -0,0 +1,22 @@
+def decimal_to_base_minus_2(n):
+    if n == 0:
+        return "0"
+    
+    result = ""
+    
+    while n != 0:
+        remainder = n % (-2)
+        n = -(n // (-2))
+        
+        if remainder < 0:
+            remainder += 2
+            n += 1
+        
+        result = str(remainder) + result
+    
+    return result
+
+# Example usage:
+decimal_number = 13
+base_minus_2 = decimal_to_base_minus_2(decimal_number)
+print(f"{decimal_number} in base -2 is {base_minus_2}")
