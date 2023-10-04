5a75bb60bc574642a905093fc93fcb68b1b3f71b
bravo1goingdark <kumarashutosh34169@gmail.com>
2023-10-05 00:23:57 +0530

    added doctest and return type to function as per the checklist

--- data_structures/heap/two_way_heaps_method.py
+++ data_structures/heap/two_way_heaps_method.py
@@ -15,49 +15,79 @@ https://stephenjoel2k.medium.com/two-heaps-min-heap-max-heap-c3d32cbb671d
 
 
 import heapq
+from typing import Union
 
 class MedianFinder:
-
-
-    def __init__(self):
+    def __init__(self) -> None:
+        """
+        Initialize the MedianFinder with empty max and min heaps.
+        
+        Examples:
+            >>> median_finder = MedianFinder()
+        """
         self.maxheap = []
         self.minheap = []
 
+    def insert(self, value) -> None:
+        """
+        Insert a value into the max and min heaps.
 
-    def insert(self , value):
+        Args:
+            value (int): A number to be inserted in the heap.
+
+        Examples:
+            >>> median_finder = MedianFinder()
+            >>> median_finder.insert(10)
+            >>> median_finder.insert(20)
+        """
         if not self.maxheap or value <= -self.maxheap[0]:
-            heapq.heappush(self.maxheap , -value)
-        else :
-            heapq.heappush(self.minheap , value)        
+            heapq.heappush(self.maxheap, -value)
+        else:
+            heapq.heappush(self.minheap, value)
 
         if len(self.maxheap) - len(self.minheap) > 1:
-            heapq.heappush(self.minheap , -heapq.heappop(self.maxheap))    
-
+            heapq.heappush(self.minheap, -heapq.heappop(self.maxheap))
         elif len(self.minheap) - len(self.maxheap) > 1:
-            heapq.heappush(self.maxheap , -heapq.heappop(self.minheap))
-    
+            heapq.heappush(self.maxheap, -heapq.heappop(self.minheap))
 
-    def findMedian(self):
+    def find_Median(self) -> Union[int, float]:
+        """
+        Find the median of the stream of numbers.
+
+        Returns:
+            Union[int, float]: The median of the stream.
+
+        Examples:
+            >>> median_finder = MedianFinder()
+            >>> median_finder.insert(5)
+            >>> median_finder.insert(8)
+            >>> median_finder.insert(2)
+            >>> median_finder.insert(10)
+            >>> median_finder.insert(1)
+            >>> median_finder.insert(7)
+            >>> median_finder.insert(6)
+            >>> median_finder.find_Median()
+            6
+        """
         if len(self.maxheap) == len(self.minheap):
             return (-self.maxheap[0] + self.minheap[0]) / 2.0
-        
         elif len(self.maxheap) > len(self.minheap):
             return -self.maxheap[0]
-        
         return self.minheap[0]
 
+def main() -> None:
+    """
+    Run the example with a stream of numbers.
 
-def main():
-    
-
+    Returns:
+        None
+    """
     median_finder = MedianFinder()
 
-    stream_of_number = [5, 8, 2, 10, 1, 7, 6]
+    stream_of_numbers = [5, 8, 2, 10, 1, 7, 6]
 
-    for value in stream_of_number:
+    for value in stream_of_numbers:
         median_finder.insert(value)
 
-    median = median_finder.findMedian()
-    print("Median:" , median)    # Median = 6
-
-main()
+    median = median_finder.find_Median()
+    print("Median:", median)  # Median = 6
