0023b3c454e50f0948ccc1cf40b14c803b3a15e9
Salik <salikashraf987@gmail.com>
2023-10-04 15:02:18 +0500

    addingNLP_Algo

---
+++ other/tokenization.py
@@ -0,0 +1,11 @@
+#Tokenization: Splitting text into words or sentences.
+#https://www.tutorialspoint.com/natural_language_processing/natural_language_processing_python.htm
+import nltk
+nltk.download('punkt')
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+text = "NLTK is a leading platform for building Python programs to work with human language data."
+words = word_tokenize(text)
+sentences = sent_tokenize(text)
+print(words)
+print(sentences)
