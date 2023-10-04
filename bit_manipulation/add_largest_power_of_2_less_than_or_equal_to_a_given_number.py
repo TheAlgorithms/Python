0037ffe0599b71783bc78a81e0ea316892ad90f2
Sharnabh Banerjee <banerjeesharnabh@gmail.com>
2023-10-04 20:10:54 +0530

    Fixes #9374
    Fixes #9374

---
+++ bit_manipulation/add_largest_power_of_2_less_than_or_equal_to_a_given_number.py
@@ -0,0 +1,41 @@
+"""
+Fixes #9374
+Name : Sharnabh Banerjee
+
+"""
+
+
+def add_largest(num: int) -> int:
+    """
+    Add Largest Power of 2 less then or equal to a given number
+    >>> add_largest(5.3)
+    Traceback (most recent call last):
+        ...
+    TypeError: num must be an integer !!
+    >>> add_largest(5)
+    9
+    >>> add_largest(10)
+    18
+    >>> add_largest(99)
+    163
+    >>> add_largest(-10)
+    0
+    >>> add_largest(999)
+    1511
+
+    """
+    if isinstance(num, float):
+        raise TypeError("num must be an integer !!")
+    if num <= 0:
+        return 0
+    res = 1
+    while (res << 1) <= num:
+        res <<= 1
+    res += num
+    return res
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
