62656c76748ebc1500e9cbd936f3d54034bf1e37
shivamk23 <shivamdeveloper23@gmail.com>
2023-10-04 21:45:49 +0530

    Improve heap Sort code

--- sorts/heap_sort1.py
+++ sorts/heap_sort1.py
@@ -1,4 +1,6 @@
-def heapify(arr, n, i):
+from typing import List
+
+def heapify(arr: List[int], n: int, i: int) -> None:
     largest = i
     left, right = 2 * i + 1, 2 * i + 2
 
@@ -12,7 +14,7 @@ def heapify(arr, n, i):
         arr[i], arr[largest] = arr[largest], arr[i]
         heapify(arr, n, largest)
 
-def heap_sort(arr):
+def heap_sort(arr: List[int]) -> None:
     n = len(arr)
 
     # Build max heap
@@ -28,3 +30,20 @@ def heap_sort(arr):
 arr = [12, 11, 13, 5, 6, 7]
 heap_sort(arr)
 print("Sorted array:", arr)
+
+def heap_sort(arr: List[int]) -> None:
+    """
+    Sorts the input list in-place using the Heap Sort algorithm.
+
+    >>> arr = [12, 11, 13, 5, 6, 7]
+    >>> heap_sort(arr)
+    >>> arr
+    [5, 6, 7, 11, 12, 13]
+    """
+    n = len(arr)
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
