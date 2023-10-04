243b4d4bf73361b6577388a5d7a6a07145c88593
Rohan Sardar <77870108+RohanSardar@users.noreply.github.com>
2023-10-04 21:20:32 +0530

    Program to convert a given string to Pig Latin
    This is a program to convert a user given string to its respective Pig Latin form
    
    As per wikipedia (link: https://en.wikipedia.org/wiki/Pig_Latin#Rules)
    For words that begin with consonant sounds, all letters before the initial vowel are placed at the end of the word sequence. Then, "ay" is added, as in the following examples:
    "pig" = "igpay"
    "latin" = "atinlay"
    "banana" = "ananabay"
    
    When words begin with consonant clusters (multiple consonants that form one sound), the whole sound is added to the end when speaking or writing.
    "friends" = "iendsfray"
    "smile" = "ilesmay"
    "string" = "ingstray"
    
    For words that begin with vowel sounds, one just adds "hay", "way" or "yay" to the end. Examples are:
    "eat" = "eatway"
    "omelet" = "omeletway"
    "are" = "areway"

---
+++ strings/pig_latin.py
@@ -0,0 +1,16 @@
+def pig_latin(sentence):
+    sentence = sentence.lower()
+    length = len(sentence)
+    if sentence[0] in "aeiou":
+        result = sentence + "way"
+    else:
+        for i in range(length):
+            if sentence[i] in "aeiou":
+                break
+        result = sentence[i:] + sentence[:i] + "ay"
+    return result
+
+statement = input("Enter a string: ")
+answer = pig_latin(statement)
+
+print("The PIG LATIN of the entered string is \"{}\"".format(answer))
