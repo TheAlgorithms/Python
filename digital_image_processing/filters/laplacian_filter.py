0ce40a23f3719128f1e1f433ff0fc8bbe429a371
ojas-wani <waniojas@gmail.com>
2023-10-05 15:53:49 -0700

    update laplacian_filter.py

--- digital_image_processing/filters/laplacian_filter.py
+++ digital_image_processing/filters/laplacian_filter.py
@@ -4,8 +4,8 @@
 
 from cv2 import (
     BORDER_DEFAULT,
-    COLOR_BGR2GRAY,
     CV_64F,
+    COLOR_BGR2GRAY,
     cvtColor,
     filter2D,
     imread,
@@ -15,8 +15,8 @@ from cv2 import (
 from gaussian_filter import gaussian_filter
 import numpy as np
 
-def my_laplacian(ksize: int, src: np.ndarray) -> np.ndarray:
 
+def my_laplacian(ksize: int, src: np.ndarray) -> np.ndarray:
     """
     :param src: the source image, which should be a grayscale or color image.
     :param ksize: the size of the kernel used to compute the Laplacian filter,
