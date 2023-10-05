8325b6435994eff8ac4814f1558ac4b0999ae376
ojas-wani <waniojas@gmail.com>
2023-10-05 15:03:29 -0700

    updated laplacian_filter.py

--- digital_image_processing/filters/laplacian_filter.py
+++ digital_image_processing/filters/laplacian_filter.py
@@ -8,18 +8,18 @@ import numpy as np
 
 def my_laplacian(src, ddepth=-1, ksize=3, scale=1, delta=0, bordertype='default') -> np.ndarray:
 
-  """
-  :param src: the source image, which should be a grayscale or color image.
-  :param ddepth: the desired depth of the destination image,
-                -1 or one of np.uint8, np.uint16, np.int16, np.float32 or np.float64.
-  :param ksize: the size of the kernel used to compute the Laplacian filter,
-                which can be 1, 3, 5 or 7.
-  :param scale: an optional scaling factor applied to the computed Laplacian values,
-                which can be used to enhance or reduce the effect of the filter.
-  :param delta: an optional value added to the computed Laplacian values,
-                which can be used to shift the output image intensity range.
-  :param bordertype: an optional flag that specifies how to handle the image borders,
-                    which can be one of 'default', 'reflect', or 'constant'.
+    """
+    :param src: the source image, which should be a grayscale or color image.
+    :param ddepth: the desired depth of the destination image,
+                  -1 or one of np.uint8, np.uint16, np.int16, np.float32 or np.float64.
+    :param ksize: the size of the kernel used to compute the Laplacian filter,
+                  which can be 1, 3, 5 or 7.
+    :param scale: an optional scaling factor applied to the computed Laplacian values,
+                  which can be used to enhance or reduce the effect of the filter.
+    :param delta: an optional value added to the computed Laplacian values,
+                  which can be used to shift the output image intensity range.
+    :param bordertype: an optional flag that specifies how to handle the image borders,
+                      which can be one of 'default', 'reflect', or 'constant'.
 
     """
 
