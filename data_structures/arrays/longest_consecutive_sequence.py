5baa5000d6c5d4e8f519e73d50a837ac4b872065
Sarthak950 <panditsamsar950@gmail.com>
2023-10-05 13:50:09 +0530

    removed the function from the class and fixed the indentation #9686

--- data_structures/arrays/longest_consecutive_sequence.py
+++ data_structures/arrays/longest_consecutive_sequence.py
@@ -5,47 +5,39 @@ Longest Consecutive Sequence Problem from LeetCode
 
 """
 
-
-    def longestConsecutiveSequence(self, nums: List[int]) -> int:
-        # Create a map of all numbers in the array to their index
-        nmap = defaultdict(int)
-        for i in range(len(nums)):
-            if nums[i] not in nmap:
-                nmap[nums[i]] = i
-
-        # Create a seen array to keep track of whether a number has been seen before
-        seen, ans = [0] * len(nums), 0
-
-        # Iterate through each number in the array
-        for n in nums:
-            # Set curr to the current number, and count to 1
-            curr, count = n, 1
-
-            # If we've already seen this number, skip it
-            if seen[nmap[n]]:
-                continue
-
-            # Otherwise, iterate through all consecutive numbers after curr
-            while curr + 1 in nmap:
-                # Increment curr
-                curr += 1
-
-                # Check if we've seen this number before
-                ix = nmap[curr]
-                if seen[ix]:
-                    # If we have, add it to the count and break out of the loop
-                    count += seen[ix]
-                    break
-                else:
-                    # Otherwise, add it to the seen array and increment the count
-                    seen[ix] = 1
-                    count += 1
-
-            # Add the count to the seen array and update the answer
-            seen[nmap[n]], ans = count, max(ans, count)
-
-        # Return the answer
-        return ans
+def longestConsecutiveSequence(self, nums: List[int]) -> int:
+    # Create a map of all numbers in the array to their index
+    nmap = defaultdict(int)
+    for i in range(len(nums)):
+        if nums[i] not in nmap:
+            nmap[nums[i]] = i
+    # Create a seen array to keep track of whether a number has been seen before
+    seen, ans = [0] * len(nums), 0
+    # Iterate through each number in the array
+    for n in nums:
+        # Set curr to the current number, and count to 1
+        curr, count = n, 1
+        # If we've already seen this number, skip it
+        if seen[nmap[n]]:
+            continue
+        # Otherwise, iterate through all consecutive numbers after curr
+        while curr + 1 in nmap:
+            # Increment curr
+            curr += 1
+            # Check if we've seen this number before
+            ix = nmap[curr]
+            if seen[ix]:
+                # If we have, add it to the count and break out of the loop
+                count += seen[ix]
+                break
+            else:
+                # Otherwise, add it to the seen array and increment the count
+                seen[ix] = 1
+                count += 1
+        # Add the count to the seen array and update the answer
+        seen[nmap[n]], ans = count, max(ans, count)
+    # Return the answer
+    return ans
 
 
 """
