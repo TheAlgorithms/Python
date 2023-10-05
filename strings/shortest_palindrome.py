88907f181459e5d0ca71fd665dbc0ed80360c50d
jeevaramanthan.m <jeevaramanathan.m@infosys.com>
2023-10-05 18:45:48 +0530

    wordings fix

--- strings/shortest_palindrome.py
+++ strings/shortest_palindrome.py
@@ -3,7 +3,7 @@ Program to find the shortest palindrome in the given string
 """
 
 def shortestpalindrome(s: str) -> str:
-    
+
     """
     >>> shortestPalindrome("aacecaaa")
     'aaacecaaa'
@@ -11,21 +11,21 @@ def shortestpalindrome(s: str) -> str:
     'dcbabcd'
     """
 
-    # Reverse the input string 's' and store it in 'r'.
+    # Reverse the input string 's' and store it in 'r'
     r = s[::-1]
 
-    # Check if the original string 's' is already a palindrome.
+    # Check if the original string 's' is already a palindrome
     if s == s[::-1]:
         return s
-    
+
     # Iterate through the characters of 's'.
     for i in range(len(s) + 1):
-        # Check if the substring of 's' starting from index 'i' matches with the reversed string 'r'.
+        # If substring of 's' starting from index 'i' matches with reversed string 'r'
         if s.startswith(r[i:]):
-            # If palindrome found, construct shortest palindrome by concatenating reversed prefix 'r[:i]' with original string 's'.
+            #construct shortest palindrome by concatenating reversed prefix & string
             return r[:i] + s
 
 if __name__ == "__main__":
     from doctest import testmod
 
-    testmod()          
+    testmod()
