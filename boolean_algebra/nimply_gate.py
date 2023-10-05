077398e6c1a97a7a67a662dfa8f8c3e7ff7c6b3b
Daniela Large <133594563+dannylarge144@users.noreply.github.com>
2023-10-05 17:54:27 +0100

    Add files via upload

---
+++ boolean_algebra/nimply_gate.py
@@ -0,0 +1,51 @@
+"""
+An NIMPLY Gate is a logic gate in boolean algebra which results to 0 if 
+either input 1 is 0, or if input 1 is 1, then it is 0 only if input 2 is 1. 
+It is true is input 1 implies input 2.
+
+Following is the truth table of an IMPLY Gate:
+    ------------------------------
+    | Input 1 | Input 2 | Output |
+    ------------------------------
+    |    0    |    0    |    0   |
+    |    0    |    1    |    0   |
+    |    1    |    0    |    1   |
+    |    1    |    1    |    0   |
+    ------------------------------
+
+Refer - https://en.wikipedia.org/wiki/NIMPLY_gate
+"""
+
+
+def nimply_gate(input_1: int, input_2: int) -> int:
+    """
+    Calculate NIMPLY of the input values
+
+    >>> nimply_gate(0, 0)
+    0
+    >>> nimply_gate(0, 1)
+    0
+    >>> nimply_gate(1, 0)
+    1
+    >>> nimply_gate(1, 1)
+    0
+    """
+    return int((input_1 == 1) and (input_2 == 0))
+
+
+def test_nimply_gate() -> None:
+    """
+    Tests the and_gate function
+    """
+    assert nimply_gate(0, 0) == 0
+    assert nimply_gate(0, 1) == 0
+    assert nimply_gate(1, 0) == 1
+    assert nimply_gate(1, 1) == 0
+
+
+if __name__ == "__main__":
+    test_nimply_gate()
+    print(nimply_gate(0, 0))
+    print(nimply_gate(0, 1))
+    print(nimply_gate(1, 0))
+    print(nimply_gate(1, 1))
