2bcd208278779bdee7507a1e76f066fdd62fade1
Sarvjeet Singh <63469455+aazad20@users.noreply.github.com>
2023-10-06 01:31:55 +0530

    Update majority_vote_algorithm.py

--- other/majority_vote_algorithm.py
+++ other/majority_vote_algorithm.py
@@ -8,7 +8,7 @@ URL : https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_majority_vote_algorithm
 """
 
 
-def majority_element(nums: List[int], k: int) -> List[int]:
+def majority_element(nums: List[int], partition: int) -> List[int]:
     """
     >>> majority_element([1, 2, 2, 3, 1, 3, 2],3)
     [2]
@@ -16,10 +16,10 @@ def majority_element(nums: List[int], k: int) -> List[int]:
     ctr = collections.Counter()
     for n in nums:
         ctr[n] += 1
-        if len(ctr) == k:
+        if len(ctr) == partition:
             ctr -= collections.Counter(set(ctr))
     ctr = collections.Counter(n for n in nums if n in ctr)
-    return [n for n in ctr if ctr[n] > len(nums) / k]
+    return [n for n in ctr if ctr[n] > len(nums) / partition]
 
 
 if __name__ == "__main__":
