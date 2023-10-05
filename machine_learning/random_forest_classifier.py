de5eedb73b6f12a1b19bed8c07af7ce9f4454329
Rajkanwar Singh <rajkanwarsingh15@gmail.com>
2023-10-05 14:50:36 +0530

    fixed errors run ruff

--- machine_learning/random_forest_classifier.py
+++ machine_learning/random_forest_classifier.py
@@ -16,57 +16,59 @@ class DecisionTree:
     def __init__(self, max_depth: Optional[int] = None) -> None:
         self.max_depth = max_depth
 
-    def fit(self, X: List[np.ndarray], y: List[int]) -> None:
+    def fit(self, features: List[np.ndarray], labels: List[int]) -> None:
         """
         Fit the decision tree to the training data.
 
         Parameters:
-            X (List[numpy.ndarray]): The input features.
-            y (List[int]): The target labels.
+            features (List[numpy.ndarray]): The input features.
+            labels (List[int]): The target labels.
 
         Returns:
             None
         """
-        self.tree = self._build_tree(X, y, depth=0)
+        self.tree = self._build_tree(features, labels, depth=0)
 
-    def _build_tree(self, X: List[np.ndarray], y: List[int], depth: int) -> tuple:
+    def _build_tree(
+        self, features: List[np.ndarray], labels: List[int], depth: int
+    ) -> tuple:
         """
         Recursively build the decision tree.
 
         Parameters:
-            X (List[numpy.ndarray]): The input features.
-            y (List[int]): The target labels.
+            features (List[numpy.ndarray]): The input features.
+            labels (List[int]): The target labels.
             depth (int): The current depth of the tree.
 
         Returns:
             tuple: The decision tree structure.
         """
-        if depth == self.max_depth or len(np.unique(y)) == 1:
-            return (np.bincount(y).argmax(),)
+        if depth == self.max_depth or len(np.unique(labels)) == 1:
+            return (np.bincount(labels).argmax(),)
 
-        num_features = len(X[0])
+        num_features = len(features[0])
         best_split_feature = None
         best_split_value = None
         best_split_score = np.inf
 
         for feature in range(num_features):
-            unique_values = np.unique(np.array(X)[:, feature])
+            unique_values = np.unique(np.array(features)[:, feature])
             for value in unique_values:
-                left_mask = np.array(X)[:, feature] <= value
-                right_mask = np.array(X)[:, feature] > value
+                left_mask = np.array(features)[:, feature] <= value
+                right_mask = np.array(features)[:, feature] > value
 
                 if (
-                    len(np.array(y)[left_mask]) == 0
-                    or len(np.array(y)[right_mask]) == 0
+                    len(np.array(labels)[left_mask]) == 0
+                    or len(np.array(labels)[right_mask]) == 0
                 ):
                     continue
 
-                left_score = self._calculate_gini(np.array(y)[left_mask])
-                right_score = self._calculate_gini(np.array(y)[right_mask])
+                left_score = self._calculate_gini(np.array(labels)[left_mask])
+                right_score = self._calculate_gini(np.array(labels)[right_mask])
                 weighted_score = (
-                    len(np.array(y)[left_mask]) * left_score
-                    + len(np.array(y)[right_mask]) * right_score
-                ) / len(y)
+                    len(np.array(labels)[left_mask]) * left_score
+                    + len(np.array(labels)[right_mask]) * right_score
+                ) / len(labels)
 
                 if weighted_score < best_split_score:
                     best_split_score = weighted_score
@@ -74,54 +76,70 @@ class DecisionTree:
                     best_split_value = value
 
         if best_split_feature is None:
-            return (np.bincount(y).argmax(),)
+            return (np.bincount(labels).argmax(),)
 
         left_split = self._build_tree(
-            [np.array(X)[np.array(X)[:, best_split_feature] <= best_split_value]],
-            [np.array(y)[np.array(X)[:, best_split_feature] <= best_split_value]],
+            [
+                np.array(features)[
+                    np.array(features)[:, best_split_feature] <= best_split_value
+                ]
+            ],
+            [
+                np.array(labels)[
+                    np.array(features)[:, best_split_feature] <= best_split_value
+                ]
+            ],
             depth + 1,
         )
         right_split = self._build_tree(
-            [np.array(X)[np.array(X)[:, best_split_feature] > best_split_value]],
-            [np.array(y)[np.array(X)[:, best_split_feature] > best_split_value]],
+            [
+                np.array(features)[
+                    np.array(features)[:, best_split_feature] > best_split_value
+                ]
+            ],
+            [
+                np.array(labels)[
+                    np.array(features)[:, best_split_feature] > best_split_value
+                ]
+            ],
             depth + 1,
         )
 
         return (best_split_feature, best_split_value, left_split, right_split)
 
-    def _calculate_gini(self, y: List[int]) -> float:
+    def _calculate_gini(self, labels: List[int]) -> float:
         """
         Calculate the Gini impurity for a given set of labels.
 
         Parameters:
-            y (List[int]): A list of labels.
+            labels (List[int]): A list of labels.
 
         Returns:
             float: The Gini impurity.
         """
-        if len(y) == 0:
+        if len(labels) == 0:
             return 0
-        p_i = np.bincount(y) / len(y)
+        p_i = np.bincount(labels) / len(labels)
         return 1 - np.sum(p_i**2)
 
-    def predict(self, X: List[np.ndarray]) -> List[int]:
+    def predict(self, features: List[np.ndarray]) -> List[int]:
         """
         Make predictions for input features.
 
         Parameters:
-            X (List[numpy.ndarray]): The input features.
+            features (List[numpy.ndarray]): The input features.
 
         Returns:
             List[int]: Predicted labels.
         """
-        return [self._predict_tree(x, self.tree) for x in X]
+        return [self._predict_tree(data_point, self.tree) for data_point in features]
 
-    def _predict_tree(self, x: np.ndarray, tree: tuple) -> int:
+    def _predict_tree(self, data_point: np.ndarray, tree: tuple) -> int:
         """
         Recursively traverse the decision tree to make predictions.
 
         Parameters:
-            x (numpy.ndarray): Input features for a single data point.
+            data_point (numpy.ndarray): Input features for a single data point.
             tree (tuple): The decision tree structure.
 
         Returns:
@@ -130,10 +148,10 @@ class DecisionTree:
         if len(tree) == 1:
             return tree[0]
         feature, value, left, right = tree
-        if x[feature] <= value:
-            return self._predict_tree(x, left)
+        if data_point[feature] <= value:
+            return self._predict_tree(data_point, left)
         else:
-            return self._predict_tree(x, right)
+            return self._predict_tree(data_point, right)
 
 
 if __name__ == "__main__":
