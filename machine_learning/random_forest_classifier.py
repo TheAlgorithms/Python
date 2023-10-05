78fae70a2b53742f737270aae86a1bfd5c617b50
Rajkanwar Singh <rajkanwarsingh15@gmail.com>
2023-10-05 08:42:16 +0530

    added doctests to my random forest classifier

--- machine_learning/random_forest_classifier.py
+++ machine_learning/random_forest_classifier.py
@@ -1,19 +1,55 @@
 import numpy as np
 
-# Define a decision tree class
 class DecisionTree:
+    """
+    Decision Tree classifier.
+
+    Parameters:
+        max_depth (int): Maximum depth of the tree. If None, the tree grows until pure nodes or min_samples_split is reached.
+
+    Attributes:
+        tree (tuple): The decision tree structure.
+
+    Examples:
+        >>> np.random.seed(42)
+        >>> X = np.random.rand(100, 2)
+        >>> y = (X[:, 0] + X[:, 1] > 1).astype(int)
+        >>> tree = DecisionTree(max_depth=3)
+        >>> tree.fit(X, y)
+        >>> predictions = tree.predict(np.array([[0.7, 0.3], [0.2, 0.8]]))
+    """
+
     def __init__(self, max_depth=None):
         self.max_depth = max_depth
 
     def fit(self, X, y):
+        """
+        Fit the decision tree to the training data.
+
+        Parameters:
+            X (numpy.ndarray): The input features.
+            y (numpy.ndarray): The target labels.
+
+        Returns:
+            None
+        """
         self.tree = self._build_tree(X, y, depth=0)
 
     def _build_tree(self, X, y, depth):
-        # Check termination conditions
-        if depth == self.max_depth or len(np.unique(y)) == 1:
-            return (np.bincount(y).argmax(),)  # Return a tuple with the class label
+        """
+        Recursively build the decision tree.
 
-        # Find the best split
+        Parameters:
+            X (numpy.ndarray): The input features.
+            y (numpy.ndarray): The target labels.
+            depth (int): The current depth of the tree.
+
+        Returns:
+            tuple: The decision tree structure.
+        """
+        if depth == self.max_depth or len(np.unique(y)) == 1:
+            return (np.bincount(y).argmax(),)
+
         num_features = X.shape[1]
         best_split_feature = None
         best_split_value = None
@@ -30,9 +66,7 @@ class DecisionTree:
 
                 left_score = self._calculate_gini(y[left_mask])
                 right_score = self._calculate_gini(y[right_mask])
-                weighted_score = (
-                    len(y[left_mask]) * left_score + len(y[right_mask]) * right_score
-                ) / len(y)
+                weighted_score = (len(y[left_mask]) * left_score + len(y[right_mask]) * right_score) / len(y)
 
                 if weighted_score < best_split_score:
                     best_split_score = weighted_score
@@ -40,78 +74,76 @@ class DecisionTree:
                     best_split_value = value
 
         if best_split_feature is None:
-            return (np.bincount(y).argmax(),)  # Return a tuple with the class label
+            return (np.bincount(y).argmax(),)
 
-        left_split = self._build_tree(
-            X[X[:, best_split_feature] <= best_split_value],
-            y[X[:, best_split_feature] <= best_split_value],
-            depth + 1,
-        )
-        right_split = self._build_tree(
-            X[X[:, best_split_feature] > best_split_value],
-            y[X[:, best_split_feature] > best_split_value],
-            depth + 1,
-        )
+        left_split = self._build_tree(X[X[:, best_split_feature] <= best_split_value], y[X[:, best_split_feature] <= best_split_value], depth + 1)
+        right_split = self._build_tree(X[X[:, best_split_feature] > best_split_value], y[X[:, best_split_feature] > best_split_value], depth + 1)
 
         return (best_split_feature, best_split_value, left_split, right_split)
 
     def _calculate_gini(self, y):
+        """
+        Calculate the Gini impurity for a given set of labels.
+
+        Parameters:
+            y (numpy.ndarray): An array of labels.
+
+        Returns:
+            float: The Gini impurity.
+        """
         if len(y) == 0:
             return 0
         p_i = np.bincount(y) / len(y)
         return 1 - np.sum(p_i**2)
 
     def predict(self, X):
+        """
+        Make predictions for input features.
+
+        Parameters:
+            X (numpy.ndarray): The input features.
+
+        Returns:
+            numpy.ndarray: Predicted labels.
+        """
         return np.array([self._predict_tree(x, self.tree) for x in X])
 
     def _predict_tree(self, x, tree):
+        """
+        Recursively traverse the decision tree to make predictions.
+
+        Parameters:
+            x (numpy.ndarray): Input features for a single data point.
+            tree (tuple): The decision tree structure.
+
+        Returns:
+            int: Predicted label.
+        """
         if len(tree) == 1:
-            return tree[0]  # Leaf node, return class label
+            return tree[0]
         feature, value, left, right = tree
         if x[feature] <= value:
             return self._predict_tree(x, left)
         else:
             return self._predict_tree(x, right)
 
-
-# Random Forest Classifier
-class RandomForestClassifier:
-    def __init__(self, n_estimators=100, max_depth=None):
-        self.n_estimators = n_estimators
-        self.max_depth = max_depth
-        self.trees = []
-
-    def fit(self, X, y):
-        for _ in range(self.n_estimators):
-            # Randomly sample data with replacement
-            indices = np.random.choice(len(X), len(X), replace=True)
-            X_subset = X[indices]
-            y_subset = y[indices]
-
-            tree = DecisionTree(max_depth=self.max_depth)
-            tree.fit(X_subset, y_subset)
-            self.trees.append(tree)
-
-    def predict(self, X):
-        predictions = np.array([tree.predict(X) for tree in self.trees])
-        # Use majority vote for classification
-        return np.apply_along_axis(
-            lambda x: np.bincount(x).argmax(), axis=0, arr=predictions
-        )
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
 
 
 # Example usage:
-if __name__ == "__main__":
+#if __name__ == "__main__":
     # Generate some random data for demonstration
-    np.random.seed(42)
-    X = np.random.rand(100, 2)
-    y = (X[:, 0] + X[:, 1] > 1).astype(int)
+    #np.random.seed(42)
+    #X = np.random.rand(100, 2)
+    #y = (X[:, 0] + X[:, 1] > 1).astype(int)
 
     # Create and train a Random Forest classifier
-    rf_classifier = RandomForestClassifier(n_estimators=100, max_depth=None)
-    rf_classifier.fit(X, y)
+    #rf_classifier = RandomForestClassifier(n_estimators=100, max_depth=None)
+    #rf_classifier.fit(X, y)
 
     # Make predictions
-    new_data = np.array([[0.7, 0.3], [0.2, 0.8]])
-    predictions = rf_classifier.predict(new_data)
-    print(predictions)
+    #new_data = np.array([[0.7, 0.3], [0.2, 0.8]])
+    #predictions = rf_classifier.predict(new_data)
+    #print(predictions)
