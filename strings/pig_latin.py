76b3f7e14675adf6959f420e58f910ec2188c031
Rohan Sardar <77870108+RohanSardar@users.noreply.github.com>
2023-10-05 08:20:56 +0530

    Update pig_latin.py

--- strings/pig_latin.py
+++ strings/pig_latin.py
@@ -1,4 +1,31 @@
-def pig_latin(sentence):
+# pig_latin.py
+
+def pig_latin(sentence:str) -> str:
+
+    """Compute the piglatin of a given string.
+
+    Usage examples:
+    >>> pig_latin("pig")
+    'igpay'
+    >>> pig_latin("latin")
+    'atinlay'
+    >>> pig_latin("banana")
+    'ananabay'
+    >>> pig_latin("friends")
+    'iendsfray'
+    >>> pig_latin("smile")
+    'ilesmay'
+    >>> pig_latin("string")
+    'ingstray'
+    >>> pig_latin("eat")
+    'eatway'
+    >>> pig_latin("omelet")
+    'omeletway'
+    >>> pig_latin("are")
+    'areway'
+
+    """
+
     sentence = sentence.lower()
     length = len(sentence)
     if sentence[0] in "aeiou":
@@ -10,8 +37,7 @@ def pig_latin(sentence):
         result = sentence[i:] + sentence[:i] + "ay"
     return result
 
-
 statement = input("Enter a string: ")
 answer = pig_latin(statement)
 
-print(f'The PIG LATIN of the entered string is "{answer}"')
+print(f"The PIG LATIN of the entered string is \"{answer}\"")
