9399cf51179eeb10a19a19cd2f620554c4de48cc
Jeshma-Ullas <jeshmarajiullas@gmail.com>
2023-10-05 16:53:16 +0400

    Added kth largest element using heap

---
+++ data_structures/heap/kth_largest.py
@@ -0,0 +1,14 @@
+from heapq import heappush, heappop #to import heappop and heappush
+l=input().split(" ") 
+k=int(input())
+for i in range(len(l)):
+    l[i]=int(l[i])
+minheap=[]
+count=0
+for i in l:
+    heappush(minheap,i)
+    count+=1
+    if(count>k):
+        heappop(minheap)
+        count-=1
+print(minheap[0])
