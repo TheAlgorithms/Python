5f9331a63b5b0c11e3716360bdd6b3c063fe2e15
ojas-wani <waniojas@gmail.com>
2023-10-05 15:35:17 -0700

    required changes to laplacian file

--- digital_image_processing/filters/laplacian_filter.py
+++ digital_image_processing/filters/laplacian_filter.py
@@ -1,37 +1,18 @@
 # @Author  : ojas-wani
 # @File    : laplacian_filter.py
-# @Time    : 10/04/2023
+# @Date    : 10/04/2023
 
-from cv2 import BORDER_DEFAULT, cvtColor, CV_64F, COLOR_BGR2GRAY,  filter2D, GaussianBlur, imread, imshow, waitKey
+from cv2 import BORDER_DEFAULT, CV_64F, COLOR_BGR2GRAY, cvtColor, filter2D, imread, imshow, waitKey
+from gaussian_filter import gaussian_filter
 import numpy as np
 
-
-def my_laplacian(src, ddepth=-1, ksize=3, scale=1, delta=0, bordertype='default') -> np.ndarray:
+def my_laplacian(src:np.ndarray, ksize:int) -> np.ndarray:
 
     """
     :param src: the source image, which should be a grayscale or color image.
-    :param ddepth: the desired depth of the destination image,
-                  -1 or one of np.uint8, np.uint16, np.int16, np.float32 or np.float64.
     :param ksize: the size of the kernel used to compute the Laplacian filter,
                   which can be 1, 3, 5 or 7.
-    :param scale: an optional scaling factor applied to the computed Laplacian values,
-                  which can be used to enhance or reduce the effect of the filter.
-    :param delta: an optional value added to the computed Laplacian values,
-                  which can be used to shift the output image intensity range.
-    :param bordertype: an optional flag that specifies how to handle the image borders,
-                      which can be one of 'default', 'reflect', or 'constant'.
-
     """
-
-    # Convert the source image to a numpy array
-    src = np.array(src)
-
-    # Get the shape and depth of the source image
-    src_depth = src.dtype
-
-    # If ddepth is -1, use the same depth as the source image
-    if ddepth == -1:
-        ddepth = src_depth
 
     # Create a Laplacian kernel matrix according to the ksize
     if ksize == 1:
@@ -63,7 +44,7 @@ def my_laplacian(src, ddepth=-1, ksize=3, scale=1, delta=0, bordertype='default'
 
     # Apply the Laplacian kernel using convolution
     laplacian_result = filter2D(
-        src, ddepth, kernel, delta, borderType=BORDER_DEFAULT, anchor=(0, 0)
+        src, CV_64F, kernel, 0, borderType=BORDER_DEFAULT, anchor=(0, 0)
     )
 
     return laplacian_result
@@ -71,16 +52,16 @@ def my_laplacian(src, ddepth=-1, ksize=3, scale=1, delta=0, bordertype='default'
 
 if __name__ == "__main__":
     # read original image
-    img = imread(r"../image_data/lena.jpg")
+    img = imread(r"digital_image_processing/image_data/lena.jpg")
 
     # turn image in gray scale value
     gray = cvtColor(img, COLOR_BGR2GRAY)
 
     # Applying gaussian filter
-    blur_image = GaussianBlur(gray, (3, 3), 0, 0)
+    blur_image = gaussian_filter(gray,3, sigma=1)
 
     # Apply multiple Kernel to detect edges
-    laplacian_image = my_laplacian(blur_image, ddepth=CV_64F, ksize=3)
+    laplacian_image = my_laplacian(blur_image, ksize=3)
 
     imshow("Original image", img)
     imshow("Deteced edges using laplacian filter", laplacian_image)
