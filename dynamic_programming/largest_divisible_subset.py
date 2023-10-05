46928270dd5ac63fd8b101463195086e126ba7fe
Parth <100679824+pa-kh039@users.noreply.github.com>
2023-10-05 20:32:29 +0530

    update
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- dynamic_programming/largest_divisible_subset.py
+++ dynamic_programming/largest_divisible_subset.py
@@ -67,7 +67,8 @@ if __name__ == "__main__":
 
     testmod()
 
-    array = [1, 16, 7, 8, 4]
+    items = [1, 16, 7, 8, 4]
+    print(f"The longest divisible subset of {items} is {largest_divisible_subset(items)}.")
 
     answer = largest_divisible_subset(array)
 
