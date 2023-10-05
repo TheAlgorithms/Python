6d53919ac0581a01e04b194c61f69d29ea3a8c2c
jeevaramanthan.m <jeevaramanathan.m@infosys.com>
2023-10-06 04:54:42 +0530

    made the requested changes

--- data_structures/binary_tree/symmetric_tree.py
+++ data_structures/binary_tree/symmetric_tree.py
@@ -4,58 +4,65 @@ Given the root of a binary tree, check whether it is a mirror of itself
 
 Leetcode reference: https://leetcode.com/problems/symmetric-tree/
 """
+from __future__ import annotations
+from dataclasses import dataclass
 
-
+@dataclass
 class Node:
     """
     A Node has data variable and pointers to Nodes to its left and right.
     """
+    data: int
+    left: Node | None = None
+    right: Node | None = None
 
-    def __init__(self, data: int) -> None:
-        self.data = data
-        self.left: Node | None = None
-        self.right: Node | None = None
+def make_symmetric_tree() -> Node:
+        r"""
+        Create a symmetric tree for testing.
+        The tree looks like this:
+               1
+             /   \
+            2     2
+          / \    / \
+         3   4   4  3
+        """
+        tree = Node(1)
+        tree.left = Node(2)
+        tree.right = Node(2)
+        tree.left.left = Node(3)
+        tree.left.right = Node(4)
+        tree.right.left = Node(4)
+        tree.right.right = Node(3)
+        return tree
 
+def make_asymmetric_tree() -> Node:
+        r"""
+        Create a asymmetric tree for testing.
+        The tree looks like this:
+               1
+             /   \
+            2     2
+          / \    / \
+         3   4   3  4
+        """
+        tree = Node(1)
+        tree.left = Node(2)
+        tree.right = Node(2)
+        tree.left.left = Node(3)
+        tree.left.right = Node(4)
+        tree.right.left = Node(3)
+        tree.right.right = Node(4)
+        return tree
 
 class SymmetricTree:
     def is_symmetric_tree(self, tree: Node) -> bool:
         r"""
-        Test cases for is_symmetric_tree function.
+        Test cases for is_symmetric_tree function        
 
-        The below tree looks like this
-               1
-             /   \
-            2      2
-           / \    / \
-          3   4   4  3
-
-        >>> tree = Node(1)
-        >>> tree.left = Node(2)
-        >>> tree.right = Node(2)
-        >>> tree.left.left = Node(3)
-        >>> tree.left.right = Node(4)
-        >>> tree.right.left = Node(4)
-        >>> tree.right.right = Node(3)
-
-        >>> SymmetricTree().is_symmetric_tree(tree)
+        >>> SymmetricTree().is_symmetric_tree(make_symmetric_tree())
         True
 
-        The below tree looks like this
-              1
-            /   \
-          2      2
-         / \    / \
-        3   4   3  4
-
-        >>> tree2 = Node(1)
-        >>> tree2.left = Node(2)
-        >>> tree2.right = Node(2)
-        >>> tree2.left.left = Node(3)
-        >>> tree2.left.right = Node(4)
-        >>> tree2.right.left = Node(3)
-        >>> tree2.right.right = Node(4)
-
-        >>> SymmetricTree().is_symmetric_tree(tree2)
+        >>> SymmetricTree().is_symmetric_tree(make_asymmetric_tree())
         False
         """
         if tree is None:
@@ -65,15 +72,14 @@ class SymmetricTree:
 
     def is_mirror(self, left: Node | None, right: Node | None) -> bool:
         """
-        >>> tree1 = Node(1)
-        >>> tree1.left = Node(2)
-        >>> tree1.right = Node(2)
-        >>> tree1.left.left = Node(3)
-        >>> tree1.left.right = Node(4)
-        >>> tree1.right.left = Node(4)
+        >>> tree1 = make_symmetric_tree()
         >>> tree1.right.right = Node(3)
         >>> SymmetricTree().is_mirror(tree1.left, tree1.right)
         True
+
+        >>> tree2 = make_asymmetric_tree()
+        >>> SymmetricTree().is_mirror(tree2.left, tree2.right)
+        False
         """
         if left is None and right is None:
             # Both sides are empty, which is symmetric.
