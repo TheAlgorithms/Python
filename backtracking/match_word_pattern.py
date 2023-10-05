c1680baee148748a0410552d90b7705e29ff1a03
Abul Hasan <33129246+haxkd@users.noreply.github.com>
2023-10-06 00:05:28 +0530

    fix: issue #9844

--- backtracking/match_word_pattern.py
+++ backtracking/match_word_pattern.py
@@ -1,4 +1,4 @@
-def matchWordPattern(pattern :str, input_string: str) -> bool:
+def match_word_pattern(pattern :str, input_string: str) -> bool:
     """
     Determine if a given pattern matches a string using backtracking.
 
@@ -6,13 +6,13 @@ def matchWordPattern(pattern :str, input_string: str) -> bool:
     input_string: The string to match against the pattern.
     return: True if the pattern matches the string, False otherwise.
 
-    >>> matchWordPattern("aba", "GraphTreesGraph")
+    >>> match_word_pattern("aba", "GraphTreesGraph")
     True
 
-    >>> matchWordPattern("xyx", "PythonRubyPython")
+    >>> match_word_pattern("xyx", "PythonRubyPython")
     True
 
-    >>> matchWordPattern("GG", "PythonJavaPython")
+    >>> match_word_pattern("GG", "PythonJavaPython")
     False
     """
     def backtrack(pattern_index, str_index):
@@ -20,31 +20,24 @@ def matchWordPattern(pattern :str, input_string: str) -> bool:
             return True
         if pattern_index == len(pattern) or str_index == len(input_string):
             return False
-        
         char = pattern[pattern_index]
-        
         if char in pattern_map:
             mapped_str = pattern_map[char]
             if input_string.startswith(mapped_str, str_index):
                 return backtrack(pattern_index + 1, str_index + len(mapped_str))
             else:
                 return False
-        
         for end in range(str_index + 1, len(input_string) + 1):
             substr = input_string[str_index:end]
             if substr in str_map:
                 continue
-            
             pattern_map[char] = substr
             str_map[substr] = char
-            
             if backtrack(pattern_index + 1, end):
                 return True
-            
             del pattern_map[char]
             del str_map[substr]
         return False
-    
     pattern_map = {}
     str_map = {}
     return backtrack(0, 0)
