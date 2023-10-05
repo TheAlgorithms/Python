259fdbbcaf2cee6d6973d0dde65d1f99f81ca2e3
Richards <96327922+Richards-Richie@users.noreply.github.com>
2023-10-05 07:09:59 +0530

    Update camel_to_snake_case.py

--- strings/camel_to_snake_case.py
+++ strings/camel_to_snake_case.py
@@ -1,7 +1,7 @@
 import re
 
 
-def camel_with_numbers_to_snake(camel_case):
+def camel_with_numbers_to_snake(camel_case)-> str:
     words = re.findall("[a-zA-Z][^A-Z]*", camel_case)
     return "_".join(w.lower() for w in words)
 
