02073549e1b96df92d5e9b5156ae13b9055f8ee5
Christian Clauss <cclauss@me.com>
2023-10-05 15:08:30 +0200

    Update gas_station.py

--- greedy_methods/gas_station.py
+++ greedy_methods/gas_station.py
@@ -1,5 +1,3 @@
-from dataclasses import dataclass
-
 """
 Task:
 There are n gas stations along a circular route, where the amount of gas
@@ -11,7 +9,7 @@ You begin the journey with an empty tank at one of the gas stations.
 
 Given two integer arrays gas_quantities and costs, return the starting
 gas station's index if you can travel around the circuit once
-in the clockwise direction, otherwise return -1.
+in the clockwise direction otherwise, return -1.
 If there exists a solution, it is guaranteed to be unique
 
 Reference: https://leetcode.com/problems/gas-station/description
@@ -25,6 +23,7 @@ If the net gain ever goes below 0 while iterating through the stations,
 start checking from the next station.
 
 """
+from dataclasses import dataclass
 
 
 @dataclass
@@ -33,65 +32,60 @@ class GasStation:
     cost: int
 
 
-def get_gas_stations(gas_quantities: list[int], costs: list[int]) -> list[GasStation]:
+def get_gas_stations(gas_quantities: list[int], costs: list[int]) -> tuple[GasStation]:
     """
-    This function returns a list of gas stations.
+    This function returns a tuple of gas stations.
 
     Args:
-        gas_quantities [list]: Amount of gas available at each station
-        cost [list]: The cost of gas required to move from a station to the next
+        gas_quantities: Amount of gas available at each station
+        costs: The cost of gas required to move from one station to the next
 
     Returns:
-        gas_stations [list]: a list of gas stations
-
-    Examples:
-    >>> get_gas_stations([1, 2, 3, 4, 5], [3, 4, 5, 1, 2])
-    [GasStation(gas_quantity=1, cost=3), GasStation(gas_quantity=2, cost=4), \
-GasStation(gas_quantity=3, cost=5), GasStation(gas_quantity=4, cost=1), \
-GasStation(gas_quantity=5, cost=2)]
+        A tuple of gas stations
+    
+    >>> gas_stations = get_gas_stations([1, 2, 3, 4, 5], [3, 4, 5, 1, 2])
+    >>> len(gas_stations)
+    5
+    >>> gas_stations[0]
+    GasStation(gas_quantity=1, cost=3)
+    >>> gas_stations[-1]
+    GasStation(gas_quantity=5, cost=2)
     """
-    gas_stations = [
-        GasStation(gas_quantity, cost)
-        for (gas_quantity, cost) in zip(gas_quantities, costs)
-    ]
-    return gas_stations
+    return tuple(
+        GasStation(quantity, cost) for quantity, cost in zip(gas_quantities, costs)
+    )
 
 
-def can_complete_journey(gas_quantities: list[int], costs: list[int]) -> int:
+def can_complete_journey(gas_stations: tuple[GasStation]) -> int:
     """
     This function returns the index from which to start the journey
     in order to reach the end.
 
     Args:
         gas_quantities [list]: Amount of gas available at each station
-        cost [list]: The cost of gas required to move from a station to the next
+        cost [list]: The cost of gas required to move from one station to the next
 
     Returns:
         start [int]: start index needed to complete the journey
 
     Examples:
-    >>> can_complete_journey([1, 2, 3, 4, 5], [3, 4, 5, 1, 2])
+    >>> can_complete_journey(get_gas_stations([1, 2, 3, 4, 5], [3, 4, 5, 1, 2]))
     3
-    >>> can_complete_journey([2, 3, 4], [3, 4, 3])
+    >>> can_complete_journey(get_gas_stations([2, 3, 4], [3, 4, 3]))
     -1
-
     """
-    total_gas = sum(gas_quantities)
-    total_cost = sum(costs)
-
+    total_gas = sum(gas_station.gas_quantity for gas_station in gas_stations)
+    total_cost = sum(gas_station.cost for gas_station in gas_stations)
     if total_gas < total_cost:
         return -1
 
     start = 0
     net = 0
-    gas_stations = get_gas_stations(gas_quantities, costs)
-
     for i, gas_station in enumerate(gas_stations):
         net += gas_station.gas_quantity - gas_station.cost
         if net < 0:
             start = i + 1
             net = 0
-
     return start
 
 
