e3580d4c1b096b7b50125c9d8d13e910911d7ccd
Sarvjeet Singh <63469455+aazad20@users.noreply.github.com>
2023-10-06 01:28:26 +0530

    Update and rename majorityvotealgorithm.py to majority_vote_algorithm.py

--- other/majority_vote_algorithm.py
+++ other/majority_vote_algorithm.py
@@ -1,13 +1,18 @@
+import collections
 """
 This is Booyer-Moore Majority Vote Algorithm. The problem statement goes like Given an integer array of size n, find all elements that appear more than ⌊ n/k ⌋ times.
 We have to solve in O(n) time and O(1) Space.
 URL : https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_majority_vote_algorithm
 
 """
-import collections
 
 
-def majorityElement(self, nums: List[int]) -> List[int]:
+
+def majority_element(nums,k):
+    """
+    >>> majority_element([1, 2, 2, 3, 1, 3, 2],3)
+    [2]
+    """
     ctr = collections.Counter()
     for n in nums:
         ctr[n] += 1
@@ -17,10 +22,7 @@ def majorityElement(self, nums: List[int]) -> List[int]:
     return [n for n in ctr if ctr[n] > len(nums) / k]
 
 
-nums = [1, 2, 2, 3, 1, 3, 2]
-k = 3
-print(majorityElement(nums, k))
 
-"""
-Output: [2]
-"""
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
