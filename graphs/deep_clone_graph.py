ebf333a0cc486a154773ff6e2937959f6efde032
dbring <dbring@gmail.com>
2023-10-05 10:17:02 -0700

    Fixed doctests

--- graphs/deep_clone_graph.py
+++ graphs/deep_clone_graph.py
@@ -16,16 +16,36 @@ class Node:
         self.value = value
         self.neighbors = neighbors or []
 
+    def __repr__(self):
+        return f"Node({self.value}, {self.neighbors})"
+
 
 def clone_graph(node: Node | None) -> Node | None:
     """
     This function returns a clone of a connected undirected graph.
-    >>> clone_graph(Node(1))
-    Node(1)
-    >>> clone_graph(Node(1, [Node(2)]))
-    Node(1, [Node(2)])
-    >>> clone_graph(None)
-    None
+    >>> node1 = Node(1)
+    >>> node2 = Node(2)
+    >>> node3 = Node(3)
+    >>> node4 = Node(4)
+    >>> node1.neighbors = [node2, node4]
+    >>> node2.neighbors = [node1, node3]
+    >>> node3.neighbors = [node2, node4]
+    >>> node4.neighbors = [node1, node3]
+    >>> clone1 = clone_graph(node1)
+    >>> clone1.value
+    1
+    >>> clone2 = clone1.neighbors[0]
+    >>> clone2.value
+    2
+    >>> clone4 = clone1.neighbors[1]
+    >>> clone4.value
+    4
+    >>> clone3 = clone2.neighbors[1]
+    >>> clone3.value
+    3
+    >>> clone = clone_graph(None)
+    >>> clone is None
+    True
     """
     if not node:
         return None
