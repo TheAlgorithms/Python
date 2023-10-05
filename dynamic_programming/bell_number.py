f9437492b5bc9667fd83e16035f3ec687521eaaf
Jeshma-Ullas <jeshmarajiullas@gmail.com>
2023-10-05 17:21:24 +0400

    Code for Bell number added

---
+++ dynamic_programming/bell_number.py
@@ -0,0 +1,37 @@
+from functools import lru_cache
+
+@lru_cache
+def bell_number(n):
+    """
+    Calculate the Bell Number for a given integer n.
+
+    >>> bell_number(5)
+    52
+    >>> bell_number(0)
+    1
+    >>> bell_number(-1)
+    Traceback (most recent call last):
+        ...
+    ValueError: Number should not be negative.
+    """
+    if n < 0:
+        raise ValueError("Number should not be negative.")
+    
+    if n == 0:
+        return 1
+    
+    # Initialize the Bell Number for n=1
+    bell_n = [0] * (n + 1)
+    bell_n[0] = 1
+    
+    for i in range(1, n + 1):
+        bell_n[i] = 0
+        for j in range(i):
+            bell_n[i] += bell_n[j]
+    
+    return bell_n[n]
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
