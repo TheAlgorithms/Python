7be691758aa4272786c8fc20d1290f670186ae34
VIVEK KUMAR SAHU <80661612+Vivekkumarsahu07@users.noreply.github.com>
2023-10-05 21:15:19 +0530

    gravitational_search_algorithm

---
+++ machine_learning/gravitational_search_algorithm.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+"""
+Gravitational Search Algorithm
+
+Author - Vivek Kumar Sahu
+Email- vivekkumarsahu979@gmail.com
+
+Requirements:
+  - numpy
+  - matplotlib (for visualization, optional)
+Python:
+  - 3.6 or higher
+Inputs:
+  - n_agents, number of agents (int)
+  - n_dimensions, number of problem dimensions (int)
+  - n_iterations, number of iterations for the algorithm (int)
+  - search_space, a list of tuples defining the search space for each dimension
+  - objective_function, a function that takes an agent's position and returns its fitness value
+  - G0, initial gravitational constant (float)
+  - alpha, gravitational scaling factor (float)
+  - beta, attraction scaling factor (float)
+Usage:
+  1. Define the problem by specifying the search space and objective function.
+  2. Initialize the algorithm with parameters: n_agents, n_dimensions, n_iterations, search_space, objective_function, G0, alpha, beta.
+  3. Run the algorithm using the `run_gsa` function.
+  4. Access the best solution and fitness value after the algorithm converges.
+"""
+from typing import List, Tuple, Callable
+import numpy as np
+
+def objective_function(x: np.ndarray) -> float:
+    """
+    Objective function to minimize.
+
+    Parameters:
+    - x (numpy.ndarray): Agent's position.
+
+    Returns:
+    - float: Fitness value.
+    """
+    return np.sum(x**2)  # Minimize the sum of squares
+
+def initialize_agents(n_agents: int, n_dimensions: int, search_space: List[Tuple[float, float]]) -> np.ndarray:
+    """
+    Initialize agents with random positions within the search space.
+
+    Parameters:
+    - n_agents (int): Number of agents.
+    - n_dimensions (int): Number of problem dimensions.
+    - search_space (List[Tuple[float, float]]): Search space for each dimension.
+
+    Returns:
+    - numpy.ndarray: Array of agents' positions.
+    """
+    agents = np.random.rand(n_agents, n_dimensions)
+    for i, dimension in enumerate(search_space):
+        agents[:, i] = agents[:, i] * (dimension[1] - dimension[0]) + dimension[0]
+    return agents
+
+def calculate_fitness(agents: np.ndarray, objective_function: Callable[[np.ndarray], float]) -> np.ndarray:
+    """
+    Calculate fitness values for each agent based on the objective function.
+
+    Parameters:
+    - agents (numpy.ndarray): Array of agents' positions.
+    - objective_function (Callable[[np.ndarray], float]): Objective function to optimize.
+
+    Returns:
+    - numpy.ndarray: Array of fitness values.
+    """
+    fitness = np.array([objective_function(agent) for agent in agents])
+    return fitness
+
+def update_agents(
+    agents: np.ndarray,
+    G: float,
+    alpha: float,
+    beta: float
+) -> np.ndarray:
+    """
+    Update agent positions based on gravitational forces.
+
+    Parameters:
+    - agents (numpy.ndarray): Array of agents' positions.
+    - G (float): Gravitational constant.
+    - alpha (float): Scaling factor.
+    - beta (float): Scaling factor.
+
+    Returns:
+    - numpy.ndarray: Updated agent positions.
+    """
+    # ...
+
+def run_gsa(
+    n_agents: int,
+    n_dimensions: int,
+    n_iterations: int,
+    search_space: List[Tuple[float, float]],
+    objective_function: Callable[[np.ndarray], float],
+    G0: float,
+    alpha: float,
+    beta: float,
+) -> Tuple[np.ndarray, float]:
+    """
+    Run the Gravitational Search Algorithm (GSA) optimization.
+
+    Parameters:
+    - n_agents (int): Number of agents.
+    - n_dimensions (int): Number of problem dimensions.
+    - n_iterations (int): Number of iterations.
+    - search_space (List[Tuple[float, float]]): Search space for each dimension.
+    - objective_function (Callable[[np.ndarray], float]): Objective function to optimize.
+    - G0 (float): Gravitational constant.
+    - alpha (float): Scaling factor.
+    - beta (float): Scaling factor.
+
+    Returns:
+    - Tuple[np.ndarray, float]: Best agent and its fitness value.
+    """
+    agents = initialize_agents(n_agents, n_dimensions, search_space)
+    best_agent = None
+    best_fitness = float("inf")
+
+    for _ in range(n_iterations):
+        fitness = calculate_fitness(agents, objective_function)
+        best_index = np.argmin(fitness)
+        if fitness[best_index] < best_fitness:
+            best_agent = agents[best_index]
+            best_fitness = fitness[best_index]
+
+        G = update_gravitational_constant(G0, alpha, beta)
+        agents = update_agents(agents, G, alpha, beta)
+
+    return best_agent, best_fitness
+
+def update_gravitational_constant(G0: float, alpha: float, beta: float) -> float:
+    """
+    Update the gravitational constant G based on iterations.
+
+    Parameters:
+    - G0 (float): Initial gravitational constant.
+    - alpha (float): Scaling factor for G.
+    - beta (float): Scaling factor for G.
+
+    Returns:
+    - float: Updated gravitational constant.
+    """
+    # ...
+
+# Doctests
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
