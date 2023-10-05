62b4a80dda70e5b2b7b883cbaf2fa9d61b36f966
Christian Clauss <cclauss@me.com>
2023-10-06 01:56:50 +0200

    Update symmetric_tree.py

--- data_structures/binary_tree/symmetric_tree.py
+++ data_structures/binary_tree/symmetric_tree.py
@@ -22,116 +22,77 @@ class Node:
 
 def make_symmetric_tree() -> Node:
     r"""
-        Create a symmetric tree for testing.
-        The tree looks like this:
-               1
-             /   \
-            2     2
-          / \    / \
-         3   4   4  3
-
-        >>> tree = make_symmetric_tree()
-        >>> tree.data
-        1
-        >>> tree.left.data
-        2
-        >>> tree.right.data
-        2
-        >>> tree.left.left.data
-        3
-        >>> tree.left.right.data
-        4
-        >>> tree.right.left.data
-        4
-        >>> tree.right.right.data
-        3
+    Create a symmetric tree for testing.
+    The tree looks like this:
+           1
+         /   \
+        2     2
+      / \    / \
+     3   4   4  3
     """
-    tree = Node(1)
-    tree.left = Node(2)
-    tree.right = Node(2)
-    tree.left.left = Node(3)
-    tree.left.right = Node(4)
-    tree.right.left = Node(4)
-    tree.right.right = Node(3)
-    return tree
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(2)
+    root.left.left = Node(3)
+    root.left.right = Node(4)
+    root.right.left = Node(4)
+    root.right.right = Node(3)
+    return root
 
 
 def make_asymmetric_tree() -> Node:
     r"""
-        Create a asymmetric tree for testing.
-        The tree looks like this:
-               1
-             /   \
-            2     2
-          / \    / \
-         3   4   3  4
-
-        >>> tree = make_asymmetric_tree()
-        >>> tree.data
-        1
-        >>> tree.left.data
-        2
-        >>> tree.right.data
-        2
-        >>> tree.left.left.data
-        3
-        >>> tree.left.right.data
-        4
-        >>> tree.right.left.data
-        3
-        >>> tree.right.right.data
-        4
+    Create a asymmetric tree for testing.
+    The tree looks like this:
+           1
+         /   \
+        2     2
+      / \    / \
+     3   4   3  4
     """
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(2)
+    root.left.left = Node(3)
+    root.left.right = Node(4)
+    root.right.left = Node(3)
+    root.right.right = Node(4)
+    return root
 
-    tree = Node(1)
-    tree.left = Node(2)
-    tree.right = Node(2)
-    tree.left.left = Node(3)
-    tree.left.right = Node(4)
-    tree.right.left = Node(3)
-    tree.right.right = Node(4)
-    return tree
+
+def is_symmetric_tree(tree: Node) -> bool:
+    """
+    Test cases for is_symmetric_tree function
+    >>> is_symmetric_tree(make_symmetric_tree())
+    True
+    >>> is_symmetric_tree(make_asymmetric_tree())
+    False
+    """
+    if tree:
+        return is_mirror(tree.left, tree.right)
+    return True  # An empty tree is considered symmetric.
 
 
-class SymmetricTree:
-    def is_symmetric_tree(self, tree: Node) -> bool:
-        r"""
-        Test cases for is_symmetric_tree function
-
-        >>> SymmetricTree().is_symmetric_tree(make_symmetric_tree())
-        True
-
-        >>> SymmetricTree().is_symmetric_tree(make_asymmetric_tree())
-        False
-        """
-        if tree is None:
-            # An empty tree is considered symmetric.
-            return True
-        return self.is_mirror(tree.left, tree.right)
-
-    def is_mirror(self, left: Node | None, right: Node | None) -> bool:
-        """
-        >>> tree1 = make_symmetric_tree()
-        >>> tree1.right.right = Node(3)
-        >>> SymmetricTree().is_mirror(tree1.left, tree1.right)
-        True
-
-        >>> tree2 = make_asymmetric_tree()
-        >>> SymmetricTree().is_mirror(tree2.left, tree2.right)
-        False
-        """
-        if left is None and right is None:
-            # Both sides are empty, which is symmetric.
-            return True
-        if left is None or right is None:
-            # One side is empty while the other is not, which is not symmetric.
-            return False
-        if left.data == right.data:
-            # The values match, so check the subtree
-            return self.is_mirror(left.left, right.right) and self.is_mirror(
-                left.right, right.left
-            )
+def is_mirror(left: Node | None, right: Node | None) -> bool:
+    """
+    >>> tree1 = make_symmetric_tree()
+    >>> tree1.right.right = Node(3)
+    >>> is_mirror(tree1.left, tree1.right)
+    True
+    >>> tree2 = make_asymmetric_tree()
+    >>> is_mirror(tree2.left, tree2.right)
+    False
+    """
+    if left is None and right is None:
+        # Both sides are empty, which is symmetric.
+        return True
+    if left is None or right is None:
+        # One side is empty while the other is not, which is not symmetric.
         return False
+    if left.data == right.data:
+        # The values match, so check the subtree
+        return is_mirror(left.left, right.right) and is_mirror(left.right, right.left)
+    return False
 
 
 if __name__ == "__main__":
