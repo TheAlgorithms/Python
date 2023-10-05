d4e5af40d6d1dd326a02ab337a74486eda834115
Richards <96327922+Richards-Richie@users.noreply.github.com>
2023-10-05 07:16:11 +0530

    Update camel_to_snake_case.py

--- strings/camel_to_snake_case.py
+++ strings/camel_to_snake_case.py
@@ -6,10 +6,6 @@ def camel_with_numbers_to_snake(camel_case) -> str:
     return "_".join(w.lower() for w in words)
 
 
-# Example usage
-# camel_with_numbers_string = "exampleVariableName123"
-# snake_case_with_numbers_string = camel_with_numbers_to_snake(camel_with_numbers_string)
-# print(snake_case_with_numbers_string)  # Output: "example_variable_name123"
 if __name__ == "__main__":
     from doctest import testmod
 
