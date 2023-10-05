fba5ecd51930c1514d087adbf5628eca04876cc2
Anuj Mishra <100562544+Anuj-gr8@users.noreply.github.com>
2023-10-05 12:43:53 +0530

    Update contour_detection_opencv.py
    Added some modifications

--- computer_vision/contour_detection_opencv.py
+++ computer_vision/contour_detection_opencv.py
@@ -11,19 +11,18 @@ Date: 05-10-2023
 import cv2
 import numpy as np
 
-
 class ContourDetector:
-    def __init__(self, image_path):
+    def __init__(self, image_path: str):
         """
         Initialize the ContourDetector with the path to an image.
-
+        
         Args:
             image_path (str): Path to the input image.
         """
         self.image_path = image_path
         self.image = cv2.imread(image_path)
 
-    def preprocess_image(self):
+    def preprocess_image(self) -> None:
         """
         Preprocess the input image by converting it to grayscale and applying Gaussian blur.
         """
@@ -33,7 +32,7 @@ class ContourDetector:
         # Apply Gaussian blur to reduce noise and improve contour detection
         self.blurred = cv2.GaussianBlur(self.gray, (5, 5), 0)
 
-    def detect_contours(self):
+    def detect_contours(self) -> None:
         """
         Detect contours in the preprocessed image using the Canny edge detection algorithm.
         """
@@ -41,14 +40,12 @@ class ContourDetector:
         self.edges = cv2.Canny(self.blurred, 50, 150)
 
         # Find contours in the edged image
-        self.contours, _ = cv2.findContours(
-            self.edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
-        )
+        self.contours, _ = cv2.findContours(self.edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    def draw_contours(self):
+    def draw_contours(self) -> np.ndarray:
         """
         Draw the detected contours on a copy of the original image.
-
+        
         Returns:
             np.ndarray: Image with contours drawn.
         """
@@ -56,20 +53,19 @@ class ContourDetector:
         cv2.drawContours(image_with_contours, self.contours, -1, (0, 255, 0), 2)
         return image_with_contours
 
-    def save_image_with_contours(self, output_path):
+    def save_image_with_contours(self, output_path: str) -> None:
         """
         Save the image with detected contours to the specified output path.
-
+        
         Args:
             output_path (str): Path to save the output image.
         """
         image_with_contours = self.draw_contours()
         cv2.imwrite(output_path, image_with_contours)
 
-
 if __name__ == "__main__":
-    image_path = "path_to_image.jpg"
-    output_path = "output_image_with_contours.jpg"
+    image_path = 'path_to_image.jpg'
+    output_path = 'output_image_with_contours.jpg'
 
     detector = ContourDetector(image_path)
     detector.preprocess_image()
