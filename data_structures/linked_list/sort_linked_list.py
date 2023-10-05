3f443fc7d70c0706b5de141b5db584b09c675679
AkankshaNarula <64724811+AkankshaNarula@users.noreply.github.com>
2023-10-05 18:49:32 +0530

    Create sort_linked_list.py

---
+++ data_structures/linked_list/sort_linked_list.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+ class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+  if head is None or head.next is None:
+            return head
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        mid = slow.next
+        slow.next = None
+        left = self.sortList(head)
+        right = self.sortList(mid)
+        dummy = ListNode(0)
+        curr = dummy
+        while left and right:
+            if left.val < right.val:
+                curr.next = left
+                left = left.next
+            else:
+                curr.next = right
+                right = right.next
+            curr = curr.next
+        curr.next = left or right
+        return dummy.next
+        
