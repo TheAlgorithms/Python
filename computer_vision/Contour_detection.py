070ea6dd56f4932c1f0640d830add564a4051e8c
Anuj Mishra <anujmishra282003@gmail.com>
2023-10-06 21:15:25 +0530

    formatted code

--- computer_vision/Contour_detection.py
+++ computer_vision/Contour_detection.py
@@ -8,6 +8,7 @@ Date: 05-10-2023
 import cv2
 import numpy as np
 
+
 class ContourDetector:
     def __init__(self, image_path: str):
         self.image_path = image_path
@@ -23,14 +24,18 @@ class ContourDetector:
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         return gray_image
 
-    def detect_contours(self, gray_image: np.ndarray, lower_threshold: int = 50, upper_threshold: int = 150) -> np.ndarray:
+    def detect_contours(self, gray_image: np.ndarray,
+                        lower_threshold: int = 50,
+                        upper_threshold: int = 150) -> np.ndarray:
         """
         Detects contours in the grayscale image using the Canny edge detector.
 
         Args:
             gray_image (np.ndarray): The grayscale input image.
-            lower_threshold (int): The lower threshold for Canny edge detection (default: 50).
-            upper_threshold (int): The upper threshold for Canny edge detection (default: 150).
+            lower_threshold (int): The lower threshold for Canny edge detection
+            (default: 50).
+            upper_threshold (int): The upper threshold for Canny edge detection
+            (default: 150).
 
         Returns:
             np.ndarray: The binary image with detected edges.
@@ -49,14 +54,16 @@ class ContourDetector:
         Returns:
             np.ndarray: The color image with drawn contours.
         """
-        contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        
+        contours, _ = cv2.findContours(edges,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
         contour_image = image.copy()
-        cv2.drawContours(contour_image, contours, -1, (0, 255, 0), 2)  # Green color for contours
-        
+        cv2.drawContours(contour_image, contours, -1, (0, 255, 0), 2)
+        # Green color for contours
+
         return contour_image
 
-    def save_image_with_contours(self, output_path: str, contour_image: np.ndarray) -> None:
+    def save_image_with_contours(self, output_path: str,
+                                 contour_image: np.ndarray) -> None:
         """
         Saves the image with drawn contours to the specified output path.
 
@@ -69,7 +76,7 @@ class ContourDetector:
 if __name__ == "__main__":
     detector = ContourDetector('path_to_image.jpg')
     gray_image = detector.preprocess_image()
-    edges = detector.detect_contours(gray_image, lower_threshold=50, upper_threshold=150)
+    edges = detector.detect_contours(gray_image,lower_threshold=50,upper_threshold=150)
     image = cv2.imread(detector.image_path)
     contour_image = detector.draw_contours(image, edges)
     detector.save_image_with_contours('output_image_with_contours.jpg', contour_image)
