6ff610a1ac2668f7ecd51ef49badda45f1025e9e
Sarvjeet Singh <63469455+aazad20@users.noreply.github.com>
2023-10-06 16:33:25 +0530

    renaming variables majority_vote_algorithm.py

--- other/majority_vote_algorithm.py
+++ other/majority_vote_algorithm.py
@@ -1,29 +1,28 @@
 import collections
-
 """
 This is Booyer-Moore Majority Vote Algorithm. The problem statement goes like:
 Given an integer array of size n, find all elements that appear more than ⌊ n/k ⌋ times.
 We have to solve in O(n) time and O(1) Space.
 URL : https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_majority_vote_algorithm
-
 """
 
 
-def majority_element(nums: list[int], partition: int) -> list[int]:
+def majority_element(total_votes: list[int], min_votes_required: int) -> list[int]:
     """
     >>> majority_element([1, 2, 2, 3, 1, 3, 2],3)
     [2]
     """
-    ctr = collections.Counter()
-    for n in nums:
-        ctr[n] += 1
-        if len(ctr) == partition:
-            ctr -= collections.Counter(set(ctr))
-    ctr = collections.Counter(n for n in nums if n in ctr)
-    return [n for n in ctr if ctr[n] > len(nums) / partition]
+    majority_candidate_counter : Dict[int, int] = collections.Counter()
+    for vote in total_votes:
+        majority_candidate_counter[vote] += 1
+        if len(majority_candidate_counter) == min_votes_required:
+            majority_candidate_counter-=collections.Counter(set(majority_candidate_counter))
+    majority_candidate_counter = collections.Counter(vote for vote in total_votes 
+                                                     if vote in majority_candidate_counter)
+    return [vote for vote in majority_candidate_counter 
+            if majority_candidate_counter[vote] > len(total_votes) / min_votes_required]
 
 
 if __name__ == "__main__":
     import doctest
-
     doctest.testmod()
