a314c4509ba1c2d6fe029b443c6a7788d6110fd2
Abul Hasan <33129246+haxkd@users.noreply.github.com>
2023-10-06 13:29:36 +0530

    Added: Project Euler - 60 solution #9895

--- project_euler/problem_060/sol1.py
+++ project_euler/problem_060/sol1.py
@@ -1,22 +1,26 @@
 import random
-from typing import List
 
 """
-The primes 3, 7, 109 and 673 are quite remarkable. By taking any two primes and concatenating them in any order the result will always be prime. For example, taking 7 and 109 both 7109 and 1097 are prime. The sum of these four primes, 792, represents the lowest sum for a set of four primes with this property.
-Find the lowest sum for a set of five primes for which any two primes concatenate to produce another prime.
+The primes 3, 7, 109 and 673 are quite remarkable. By taking any 
+two primes and concatenating them in any order the result will always be prime. 
+For example, taking 7 and 109 both 7109 and 1097 are prime. 
+The sum of these four primes, 792, represents the lowest sum 
+for a set of four primes with this property.
+Find the lowest sum for a set of five primes 
+for which any two primes concatenate to produce another prime.
 """
 
 
-# Sieve of Eratosthenes to generate prime numbers
-def sieve_of_eratosthenes(limit: int) -> List[int]:
+def sieve_of_eratosthenes(limit: int) -> list[int]:
     """
-    Generates prime numbers up to the specified limit using the Sieve of Eratosthenes algorithm.
+    Generates prime numbers up to the specified 
+    limit using the Sieve of Eratosthenes algorithm.
 
     Parameters:
     limit (int): The upper limit for prime number generation.
 
     Returns:
-    List[int]: A list of prime numbers up to the given limit.
+    list[int]: A list of prime numbers up to the given limit.
 
     >>> sieve_of_eratosthenes(2)
     [2]
@@ -24,25 +28,21 @@ def sieve_of_eratosthenes(limit: int) -> List[int]:
     [2, 3]
     """
     is_prime = [True] * limit
-    if limit > 2:
+    if (limit > 2):
         is_prime[0] = False
         is_prime[1] = False
         is_prime[2] = True
-
     for number in range(3, int(limit**0.5) + 1, 2):
         if is_prime[number]:
             for multiple in range(number * 2, limit, number):
                 is_prime[multiple] = False
-
     primes = [2]
     for number in range(3, limit, 2):
         if is_prime[number]:
             primes.append(number)
-
     return primes
 
 
-# Miller–Rabin primality test
 def is_prime(number: int, k: int = 3) -> bool:
     """
     Tests if a given number is prime using the Miller-Rabin primality test.
@@ -84,7 +84,6 @@ def is_prime(number: int, k: int = 3) -> bool:
         return True
 
 
-# Check if two numbers form a prime pair
 def forms_prime_pair(num1: int, num2: int) -> bool:
     """
     Checks if two numbers form a prime pair when concatenated in any order.
@@ -107,44 +106,42 @@ def forms_prime_pair(num1: int, num2: int) -> bool:
     num2_num1_concat = int(num2_str + num1_str)
     return is_prime(num1_num2_concat) and is_prime(num2_num1_concat)
 
-
 # Finding prime numbers up to a given limit
-def find_primes(limit: int) -> List[int]:
+
+
+def find_primes(limit: int) -> list[int]:
     """
-    Finds prime numbers up to the specified limit using the Sieve of Eratosthenes algorithm.
+    Finds prime numbers up to the specified limit 
+    using the Sieve of Eratosthenes algorithm.
 
     Parameters:
     limit (int): The upper limit for prime number generation.
 
     Returns:
-    List[int]: A list of prime numbers up to the given limit.
+    list[int]: A list of prime numbers up to the given limit.
     >>> find_primes(2)
     [2]
     >>> find_primes(5)
     [2, 3]
-
-
     """
     return sieve_of_eratosthenes(limit)
 
 
-# Problem solution
-def find_lowest_sum_of_five_primes(primes: List[int]) -> int:
+def find_lowest_sum_of_five_primes(primes: list[int]) -> int:
     """
-    Finds the lowest sum of five prime numbers that form prime pairs when concatenated in any order.
+    Finds the lowest sum of five prime numbers that 
+    form prime pairs when concatenated in any order.
 
     Parameters:
-    primes (List[int]): A list of prime numbers.
+    primes (list[int]): A list of prime numbers.
 
     Returns:
     int: The lowest sum of five prime numbers that satisfy the condition.
-
 
     >>> find_lowest_sum_of_five_primes([2, 3])
     0
     >>> find_lowest_sum_of_five_primes([2, 3, 5])
     0
-
     """
     for prime1 in primes:
         for prime2 in primes:
@@ -154,35 +151,24 @@ def find_lowest_sum_of_five_primes(primes: List[int]) -> int:
                 for prime3 in primes:
                     if prime3 < prime2:
                         continue
-                    if forms_prime_pair(prime1, prime3) and forms_prime_pair(
-                        prime2, prime3
-                    ):
+                    if forms_prime_pair(prime1, prime3) and forms_prime_pair(prime2, prime3):
                         for prime4 in primes:
                             if prime4 < prime3:
                                 continue
-                            if (
-                                forms_prime_pair(prime1, prime4)
-                                and forms_prime_pair(prime2, prime4)
-                                and forms_prime_pair(prime3, prime4)
-                            ):
+                            if forms_prime_pair(prime1, prime4) and forms_prime_pair(prime2, prime4) and forms_prime_pair(prime3, prime4):
                                 for prime5 in primes:
                                     if prime5 < prime4:
                                         continue
-                                    if (
-                                        forms_prime_pair(prime1, prime5)
-                                        and forms_prime_pair(prime2, prime5)
-                                        and forms_prime_pair(prime3, prime5)
-                                        and forms_prime_pair(prime4, prime5)
-                                    ):
-                                        return (
-                                            prime1 + prime2 + prime3 + prime4 + prime5
-                                        )
+                                    if forms_prime_pair(prime1, prime5) and forms_prime_pair(prime2, prime5) and forms_prime_pair(prime3, prime5) and forms_prime_pair(prime4, prime5):
+                                        return prime1 + prime2 + prime3 + prime4 + prime5
     return 0
 
 
 def solution(primes_limit: int = 10000) -> int:
     """
-    the lowest sum for a set of five primes for which any two primes concatenate to produce another prime.
+    the lowest sum for a set of five primes for which 
+    any two primes concatenate to produce another prime.
+    
     Parameters:
     primes_limit (int): The upper limit for prime number generation.
 
@@ -192,14 +178,11 @@ def solution(primes_limit: int = 10000) -> int:
     0
     >>> solution(10000)
     26033
-
     """
-    primes_list: List[int] = find_primes(primes_limit)
-    # Run the function and print the output
+    primes_list: list[int] = find_primes(primes_limit)
     return find_lowest_sum_of_five_primes(primes_list)
 
 
 if __name__ == "__main__":
     import doctest
-
     doctest.testmod()
