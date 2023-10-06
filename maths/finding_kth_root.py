7376e3b7fdde7eaa63d782e178c322a7e996a279
hollowcrust <72879387+hollowcrust@users.noreply.github.com>
2023-10-06 23:42:11 +0800

    Update finding_kth_root.py

--- maths/finding_kth_root.py
+++ maths/finding_kth_root.py
@@ -1,5 +1,6 @@
 """
-Finding the real root of a given degree of a given number, rounding to the nearest given number of decimal places without using **
+Finding the real root of a given degree of a given number, 
+rounding to the nearest given number of decimal places without using **
 
 Input:
     - number: 3
@@ -13,7 +14,8 @@ Output:
 def finding_kth_root(number: float, deg: int, decimal_place: int) -> float:
     """
     Implementing binary search to find the kth root of a number
-    Negative numbers are treated the same way as positive numbers, with the minus sign added at the end
+    Negative numbers are treated the same way as positive numbers,
+    with the minus sign added at the end
 
     >>> finding_kth_root(3, 2, 5)
     1.73205
@@ -22,7 +24,7 @@ def finding_kth_root(number: float, deg: int, decimal_place: int) -> float:
     >>> finding_kth_root(5, 1, 3)
     5.0
     >>> finding_kth_root(0, 2, 2)
-    0.0
+    0
     >>> finding_kth_root(125.7, 2, 0)
     11.0
     >>> finding_kth_root(-123, 5, 3)
@@ -50,13 +52,13 @@ def finding_kth_root(number: float, deg: int, decimal_place: int) -> float:
         hi = -number
 
     error = 1
-    for i in range(decimal_place + 1):
+    for _i in range(decimal_place + 1):
         error /= 10
 
     while hi - lo >= error:  # Precision is not reached, continue looping
         mid = (hi + lo) / 2
         product = 1
-        for i in range(deg):
+        for _i in range(deg):
             product *= mid
 
         if product > abs(number):  # Overestimation, higher bound decreases to mid
