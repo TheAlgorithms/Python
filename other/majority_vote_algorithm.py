c0a3f88fc344dbe589137e3a7c3997e93884b25a
Christian Clauss <cclauss@me.com>
2023-10-06 15:42:41 +0200

    Update majority_vote_algorithm.py

--- other/majority_vote_algorithm.py
+++ other/majority_vote_algorithm.py
@@ -1,5 +1,5 @@
 """
-This is Booyer-Moore Majority Vote Algorithm. The problem statement goes like:
+This is Booyer-Moore Majority Vote Algorithm. The problem statement goes like this:
 Given an integer array of size n, find all elements that appear more than ⌊ n/k ⌋ times.
 We have to solve in O(n) time and O(1) Space.
 URL : https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_majority_vote_algorithm
@@ -7,13 +7,13 @@ URL : https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_majority_vote_algorithm
 from collections import Counter
 
 
-def majority_element(votes: list[int], votes_needed_to_win: int) -> list[int]:
+def majority_vote(votes: list[int], votes_needed_to_win: int) -> list[int]:
     """
-    >>> majority_element([1, 2, 2, 3, 1, 3, 2], 3)
+    >>> majority_vote([1, 2, 2, 3, 1, 3, 2], 3)
     [2]
-    >>> majority_element([1, 2, 2, 3, 1, 3, 2], 2)
+    >>> majority_vote([1, 2, 2, 3, 1, 3, 2], 2)
     []
-    >>> majority_element([1, 2, 2, 3, 1, 3, 2], 4)
+    >>> majority_vote([1, 2, 2, 3, 1, 3, 2], 4)
     [1, 2, 3]
     """
     majority_candidate_counter: Counter[int] = Counter()
