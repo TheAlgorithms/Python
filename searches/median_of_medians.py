c767c6641d09811c44d620c33bddf1eaf907c071
Achal Jain <achaljain232@gmail.com>
2023-10-06 08:19:55 +0530

    Update median_of_medians.py as per requested changes

--- searches/median_of_medians.py
+++ searches/median_of_medians.py
@@ -8,40 +8,31 @@ deterministically
 https://en.wikipedia.org/wiki/Median_of_medians
 """
 
-"""
->>> quick_select([2, 4, 5, 7, 899, 54, 32], 5)
-32
->>> quick_select([2, 4, 5, 7, 899, 54, 32], 1)
-2
->>> quick_select([5, 4, 3, 2], 2)
-3
->>> quick_select([3, 5, 7, 10, 2, 12], 3)
-5
-"""
-
-
 def median_of_five(arr: list) -> int:
     """
+    Return the median of the input list
+    :param arr: Array to find median of
+    :return: median of arr
+
     >>> median_of_five([2, 4, 5, 7, 899])
     5
     >>> median_of_five([5, 7, 899, 54, 32])
     32
     >>> median_of_five([5, 4, 3, 2])
-    3
+    4
     >>> median_of_five([3, 5, 7, 10, 2])
     5
     """
-    """
-    Return the median of the input list
-    :param arr: Array to find median of
-    :return: median of arr
-    """
-    arr = sorted(arr)
+    arr=sorted(arr)
     return arr[len(arr) // 2]
-
 
 def median_of_medians(arr: list) -> int:
     """
+    Return a pivot to partition data on by calculating
+    Median of medians of input data
+    :param arr: The data to be checked (a list)
+    :return: median of medians of input array
+
     >>> median_of_medians([2, 4, 5, 7, 899, 54, 32])
     54
     >>> median_of_medians([5, 7, 899, 54, 32])
@@ -51,12 +42,7 @@ def median_of_medians(arr: list) -> int:
     >>> median_of_medians([3, 5, 7, 10, 2, 12])
     12
     """
-    """
-    Return a pivot to partition data on by calculating
-    Median of medians of input data
-    :param arr: The data to be checked (a list)
-    :return: median of medians of input array
-    """
+
     if len(arr) <= 5:
         return median_of_five(arr)
     medians = []
@@ -69,9 +55,14 @@ def median_of_medians(arr: list) -> int:
         i += 5
     return median_of_medians(medians)
 
-
 def quick_select(arr: list, target: int) -> int:
     """
+    Two way partition the data into smaller and greater lists,
+    in relationship to the pivot
+    :param arr: The data to be searched (a list)
+    :param target: The rank to be searched
+    :return: element at rank target
+    
     >>> quick_select([2, 4, 5, 7, 899, 54, 32], 5)
     32
     >>> quick_select([2, 4, 5, 7, 899, 54, 32], 1)
@@ -80,14 +71,6 @@ def quick_select(arr: list, target: int) -> int:
     3
     >>> quick_select([3, 5, 7, 10, 2, 12], 3)
     5
-    """
-
-    """
-    Two way partition the data into smaller and greater lists,
-    in relationship to the pivot
-    :param arr: The data to be searched (a list)
-    :param target: The rank to be searched
-    :return: element at rank target
     """
 
     # Invalid Input
@@ -109,10 +92,12 @@ def quick_select(arr: list, target: int) -> int:
         else:
             right.append(arr[i])
     rank_x = len(left) + 1
-    if rank_x == target:
+    if(rank_x == target):
         answer = x
     elif rank_x > target:
         answer = quick_select(left, target)
     elif rank_x < target:
         answer = quick_select(right, target - rank_x)
     return answer
+
+print(median_of_five([5, 4, 3, 2]))
