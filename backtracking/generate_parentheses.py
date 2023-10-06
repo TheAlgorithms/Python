c05de071f049b47eb1f11e7f1be3c7f49acb032a
kosuri-indu <kosurilindu@gmail.com>
2023-10-06 22:16:16 +0530

    changes

--- backtracking/generate_parentheses.py
+++ backtracking/generate_parentheses.py
@@ -1,7 +1,6 @@
 """
 Given 'n' pairs of parentheses,
 this program generates all combinations of parentheses.
-
 Example, n = 3 :
 [
    "((()))",
@@ -10,23 +9,17 @@ Example, n = 3 :
    "()(())",
    "()()()"
 ]
-
 This problem can be solved using the concept of "BACKTRACKING".
-
 By adding an open parenthesis to the solution and
 recursively add more till we get 'n' open parentheses.
-
 Then we start adding close parentheses until the solution is valid
 (open parenthesis is closed).
-
 If we reach a point where we can not add more parentheses to the solution,
 we backtrack to the previous step and try a different path.
 """
 
-
-def generate_parentheses(number : int = 3):
+def generate_parentheses(number : int = 3) -> list:
     """
-
     >>> generate_parentheses(3)
     ['((()))', '(()())', '(())()', '()(())', '()()()']
 
@@ -38,10 +31,8 @@ def generate_parentheses(number : int = 3):
 
     >>> generate_parentheses(0)
     ['']
-
     """
-    
-    def backtrack(x : str = "", left : int = 0, right : int = 0):
+    def backtrack(x : str = "", left : int = 0, right : int = 0) -> None:
         
         if len(x) == 2 * number:
             result.append(x)
@@ -54,7 +45,6 @@ def generate_parentheses(number : int = 3):
     result = []
     backtrack()
     return result
-
 
 if __name__ == "__main__":
     import doctest
