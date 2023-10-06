7c4b1ba10cf7f75e305372f94d41f5049a58c936
parth <parthkhandenath123@gmail.com>
2023-10-06 17:54:00 +0530

    update

--- graphs/floyd_warshall.py
+++ graphs/floyd_warshall.py
@@ -1,6 +1,5 @@
 import sys
 
-
 """
 The famous Floyd Warshall Alogirthm to
 find the shortest distance between all
@@ -13,8 +12,8 @@ def floyd_warshall(adjacency_matrix: list[list[int]], n: int) -> list[list[int]]
     """
     Given adjacency matrix, returns a matrix with
     shortest distance between each pair of vertices
-    >>> floyd_warshall([[-1, 2, -1, -1], [1, -1, 3, -1], [-1, -1, -1, -1], [3, 5, 4, -1]],4)
-    [[0, 2, 5, -1], [1, 0, 3, -1], [-1, -1, 0, -1], [3, 5, 4, 0]]
+    >>> floyd_warshall([[-1, 2, 2, -1], [1, 2, 3, -1], [-1, 2, 2, 1], [3, 5, 4, 1]],4)
+    [[0, 2, 2, 3], [1, 0, 3, 4], [3, 2, 0, 1], [3, 5, 4, 0]]
     >>> floyd_warshall([[-1, 4, -1, 2], [1, 7, -1, 14], [-1, 2, 4, 9], [10, 5, 1, 2]],4)
     [[0, 4, 3, 2], [1, 0, 4, 3], [3, 2, 0, 5], [4, 3, 1, 0]]
     >>> floyd_warshall([[1, 2], [1, 11]],2)
@@ -46,10 +45,9 @@ def floyd_warshall(adjacency_matrix: list[list[int]], n: int) -> list[list[int]]
     for intermediate_vertex in range(n):
         for vertex1 in range(n):
             for vertex2 in range(n):
-                if (
-                    shortest_distance[vertex1][intermediate_vertex] != sys.maxsize
-                    and shortest_distance[intermediate_vertex][vertex2] != sys.maxsize
-                ):
+                check1=(shortest_distance[vertex1][intermediate_vertex] != sys.maxsize)
+                check2=(shortest_distance[intermediate_vertex][vertex2] != sys.maxsize)
+                if (check1 and check2):
                     shortest_distance[vertex1][vertex2] = min(
                         shortest_distance[vertex1][vertex2],
                         shortest_distance[vertex1][intermediate_vertex]
@@ -76,24 +74,24 @@ if __name__ == "__main__":
             row.append(-1)
         adjacency_matrix.append(row)
     # directed graph
-    adjacency_matrix[0][
-        1
-    ] = 2  # vertex 1 can be reached from vertex 0 at cost/distance of 2
-    adjacency_matrix[1][
-        0
-    ] = 1  # vertex 0 can be reached from vertex 1 at cost/distance of 1
-    adjacency_matrix[1][
-        2
-    ] = 3  # vertex 2 can be reached from vertex 1 at cost/distance of 3
-    adjacency_matrix[3][
-        0
-    ] = 3  # vertex 0 can be reached from vertex 3 at cost/distance of 3
-    adjacency_matrix[3][
-        1
-    ] = 5  # vertex 1 can be reached from vertex 3 at cost/distance of 5
-    adjacency_matrix[3][
-        2
-    ] = 4  # vertex 2 can be reached from vertex 3 at cost/distance of 4
+    adjacency_matrix[0][1] = 2
+    # vertex 1 can be reached from vertex 0 at cost/distance of 2
+
+    adjacency_matrix[1][0] = 1
+    # vertex 0 can be reached from vertex 1 at cost/distance of 1
+
+    adjacency_matrix[1][2] = 3
+    # vertex 2 can be reached from vertex 1 at cost/distance of 3
+
+    adjacency_matrix[3][0] = 3
+    # vertex 0 can be reached from vertex 3 at cost/distance of 3
+
+    adjacency_matrix[3][1] = 5
+    # vertex 1 can be reached from vertex 3 at cost/distance of 5
+
+    adjacency_matrix[3][2] = 4
+    # vertex 2 can be reached from vertex 3 at cost/distance of 4
+
     # print(adjacency_matrix)
     # if matrix[v1][v2] is -1, then v2 can't be reached from v1
     shortest_distance_matrix = floyd_warshall(adjacency_matrix, total_vertices)
