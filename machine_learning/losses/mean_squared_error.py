ba41942528a76242eda00fc4f0f6677098e0a2cf
Arnav Kohli <95236897+THEGAMECHANGER416@users.noreply.github.com>
2023-10-07 15:36:00 +0530

    Update mean_squared_error.py

--- machine_learning/losses/mean_squared_error.py
+++ machine_learning/losses/mean_squared_error.py
@@ -1,3 +1,18 @@
+"""
+Mean Squared Error (MSE) Loss Function
+
+This script defines the Mean Squared Error (MSE) loss function, which is commonly used for regression problems.
+
+Description:
+The Mean Squared Error (MSE) measures the average squared difference between the true values (ground truth) and the predicted values produced by a model. It is widely used in regression tasks and serves as a measure of the model's accuracy.
+
+Formula:
+MSE = (1/n) * Σ(y_true - y_pred)^2
+
+Source:
+- [Wikipedia - Mean squared error](https://en.wikipedia.org/wiki/Mean_squared_error)
+"""
+
 import numpy as np
 
 
