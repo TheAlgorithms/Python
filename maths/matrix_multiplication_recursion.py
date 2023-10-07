2cf909075444f95663e32f42f49f15b6975a26fc
ojas-wani <waniojas@gmail.com>
2023-10-07 10:57:31 -0700

    update matrix_multiplication

--- maths/matrix_multiplication_recursion.py
+++ maths/matrix_multiplication_recursion.py
@@ -24,10 +24,11 @@ def matrix_multiply_recursive(matrix_a: list, matrix_b: list) -> list:
         raise ValueError("Invalid matrix dimensions")
 
     # Initialize the result matrix with zeros
-    result = [[0 for _ in range(len(matrix_b[0]))] for _ in range(len(matrix_a))]
+    result = [[0 for _ in range(len(matrix_b[0]))]
+              for _ in range(len(matrix_a))]
 
     # Recursive multiplication of matrices
-    def multiply(i_loop, j_loop, k_loop, matrix_a, matrix_b, result):
+    def multiply(i_loop: int, j_loop: int, k_loop: int, matrix_a: list, matrix_b: list, result: list) -> function:
         """
         :param matrix_a: Input matrices.
         :param matrix_b: Input matrices where length of matrices is
@@ -44,7 +45,8 @@ def matrix_multiply_recursive(matrix_a: list, matrix_b: list) -> list:
             return multiply(matrix_a, matrix_b, result, i_loop + 1, 0, 0)
         if k_loop >= len(matrix_b):
             return multiply(matrix_a, matrix_b, result, i_loop, j_loop + 1, 0)
-        result[i_loop][j_loop] += matrix_a[i_loop][k_loop] * matrix_b[k_loop][j_loop]
+        result[i_loop][j_loop] += matrix_a[i_loop][k_loop] * \
+            matrix_b[k_loop][j_loop]
         multiply(matrix_a, matrix_b, result, i_loop, j_loop, k_loop + 1)
 
     # Perform matrix multiplication
