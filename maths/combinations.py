79c7e38bb6fe6ffdd99ddd81d0ccc9b1957be283
SubhranShu2332 <124662904+SubhranShu2332@users.noreply.github.com>
2023-10-07 21:53:11 +0530

    In place of calculating the factorial several times we can run a loop k times to calculate the combination
    for example:
    5 C 3 = 5! / (3! * (5-3)! )
    = (5*4*3*2*1)/[(3*2*1)*(2*1)]
    =(5*4*3)/(3*2*1)
    so running a loop k times will reduce the time complexity to O(k)

--- maths/combinations.py
+++ maths/combinations.py
@@ -1,8 +1,6 @@
 """
 https://en.wikipedia.org/wiki/Combination
 """
-from math import factorial
-
 
 def combinations(n: int, k: int) -> int:
     """
@@ -35,7 +33,12 @@ def combinations(n: int, k: int) -> int:
     # to calculate a factorial of a negative number, which is not possible
     if n < k or k < 0:
         raise ValueError("Please enter positive integers for n and k where n >= k")
-    return factorial(n) // (factorial(k) * factorial(n - k))
+    res=1
+    for i in range(k):
+        res = res * (n - i)
+        res = res // (i + 1)
+    return res
+            
 
 
 if __name__ == "__main__":
