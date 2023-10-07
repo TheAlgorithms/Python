16803afd55521a379540c4e88ed198beee5dd9fc
dahhou ilyas <dahhou.ilyas@gmail.com>
2023-10-07 21:07:48 +0100

    add new programme in dynamique programming wildcard_matching

--- dynamic_programming/wildcard_matching.py
+++ dynamic_programming/wildcard_matching.py
@@ -32,7 +32,7 @@ tz_maroc = pytz.timezone("Africa/Casablanca")
 now_maroc = datetime.datetime.now(tz=tz_maroc)
 
 
-def is_match(s: str, p: str) -> bool:
+def is_match(string: str, pattern: str) -> bool:
     """
     >>> isMatch("aa", "a")
     False
@@ -77,7 +77,7 @@ def is_match(s: str, p: str) -> bool:
     True
 
     """
-    m, n = len(s), len(p)
+    m, n = len(string), len(pattern)
     dp = [[False] * (n + 1) for _ in range(m + 1)]
 
     # Base case
@@ -85,15 +85,15 @@ def is_match(s: str, p: str) -> bool:
 
     # Fill in the first row
     for j in range(1, n + 1):
-        if p[j - 1] == "*":
+        if pattern[j - 1] == "*":
             dp[0][j] = dp[0][j - 1]
 
     # Fill in the rest of the DP table
     for i in range(1, m + 1):
         for j in range(1, n + 1):
-            if p[j - 1] == "?" or s[i - 1] == p[j - 1]:
+            if pattern[j - 1] == "?" or string[i - 1] == pattern[j - 1]:
                 dp[i][j] = dp[i - 1][j - 1]
-            elif p[j - 1] == "*":
+            elif pattern[j - 1] == "*":
                 dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
 
     return dp[m][n]
