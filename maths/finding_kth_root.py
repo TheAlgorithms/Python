30daa416490ef75b897bf8e543f579c553deab16
hollowcrust <72879387+hollowcrust@users.noreply.github.com>
2023-10-07 21:52:25 +0800

    Update finding_kth_root.py

--- maths/finding_kth_root.py
+++ maths/finding_kth_root.py
@@ -2,6 +2,8 @@
 Finding the real root of a given degree of a given number
 rounding to the nearest given number of decimal places without using ** or pow()
 
+[Problem link] https://www.geeksforgeeks.org/calculating-n-th-real-root-using-binary-search/
+
 Input:
     - number: 3
     - deg: 2
