277a681ad63c7c284cb17daa67e5ba8187183e2f
Arnav Kohli <95236897+THEGAMECHANGER416@users.noreply.github.com>
2023-10-07 11:00:51 +0530

    Update mean_squared_error.py

--- machine_learning/losses/mean_squared_error.py
+++ machine_learning/losses/mean_squared_error.py
@@ -13,10 +13,11 @@ def mean_squared_error(y_true: np.ndarray, y_pred: np.ndarray) -> float:
     - mse: The Mean Squared Error between y_true and y_pred.
 
     Example usage:
-    true_values = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
-    predicted_values = np.array([0.8, 2.1, 2.9, 4.2, 5.2])
-    mse = mean_squared_error(true_values, predicted_values)
-    print(f"Mean Squared Error: {mse}")
+    >>> true_values = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
+    >>> predicted_values = np.array([0.8, 2.1, 2.9, 4.2, 5.2])
+    >>> mse = mean_squared_error(true_values, predicted_values)
+    >>> mse
+    0.10599999999999987
     """
     if len(y_true) != len(y_pred):
         raise ValueError("Input arrays must have the same length.")
