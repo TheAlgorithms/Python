847721fde60ca92362fe4d11aff936a8cb8ad354
Souhardyo <souhardyodasgupta@gmail.com>
2023-10-08 14:04:49 +0530

    Added Doctest

--- data_structures/stacks/TwoQueuedStack.py
+++ data_structures/stacks/TwoQueuedStack.py
@@ -1,64 +1,73 @@
 class StackUsingQueues:
-    def __init__(self):
+    def __init__(self) -> None:
         """
-        Initialising required queues using lists data structure
+        Initialize the stack using two queues.
+
+        >>> stack = StackUsingQueues()
         """
         self.queue1 = []
         self.queue2 = []
 
-    def push(self,item):
-        while len(self.queue1)!= 0:
-            """
-            enqueuing queue 2 using the values from queue 1 bringing them out in First
-            In First Out (F.I.F.O) order.
-            """
-            self.queue2.append(self.queue1[0])
-            self.queue1 .pop(0)
-            """
-            adding the new value to queue 1
-            """
-        self.queue1.append(item)
+    def push(self, item) -> None:
+        """
+        Push an item onto the stack.
 
+        >>> stack = StackUsingQueues()
+        >>> stack.push(1)
+        >>> stack.push(2)
+        >>> stack.push(3)
+        >>> stack.peek()
+        3
         """
-        returning the values from queue 2 to queue 1 so as
-        to replicate the stack data structure
-        """
-        while len(self.queue2)!= 0:
+        while len(self.queue1) != 0:
+            self.queue2.append(self.queue1[0])
+            self.queue1.pop(0)
+        self.queue1.append(item)
+        while len(self.queue2) != 0:
             self.queue1.append(self.queue2[0])
             self.queue2.pop(0)
 
+    def pop(self) -> int:
+        """
+        Pop the top item from the stack and return it.
 
-
-
-    def pop(self):
+        >>> stack = StackUsingQueues()
+        >>> stack.push(1)
+        >>> stack.push(2)
+        >>> stack.pop()
+        2
+        >>> stack.pop()
+        1
+        >>> stack.pop()
+        0
+        """
         if len(self.queue1) != 0:
-            """
-            The first value of queue 1 is being popped here as it
-            has been implemented in a way that the
-            first value of the queue which gets popped is the last value of the stack.
-            And since stack follows Last In First Out (L.I.F.O) order the
-            following has been implemented.
-            """
             popped = self.queue1.pop(0)
             return popped
         else:
-            return None
+            return 0
 
-
-
-
-
-
-
-    def peek(self):
+    def peek(self) -> int:
         """
-        Function to see the last value inserted in the stack
-        implemented using queue here, which has the last
-        value of the stack as its first.
+        Return the top item from the stack without removing it.
+
+        >>> stack = StackUsingQueues()
+        >>> stack.push(1)
+        >>> stack.push(2)
+        >>> stack.peek()
+        2
+        >>> stack.pop()
+        2
+        >>> stack.peek()
+        1
+        >>> stack.pop()
+        1
+        >>> stack.peek()
+        0
         """
         if len(self.queue1) != 0:
             return self.queue1[0]
         else:
-            return None
+            return 0
 
 
