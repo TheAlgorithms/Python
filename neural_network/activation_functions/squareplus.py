d9397c353e82e6c60f6aede512dffdae20c0c036
kausthub-kannan <kausthubkannan961@gmail.com>
2023-10-08 19:13:11 +0530

    Fixed Squareplus Function

--- neural_network/activation_functions/squareplus.py
+++ neural_network/activation_functions/squareplus.py
@@ -20,16 +20,16 @@ def squareplus(vector: np.ndarray, beta: float) -> np.ndarray:
     Returns:
         np.ndarray: The input array after applying the SquarePlus activation.
 
-    Formula: f(x) = x^2 if x > 0 else f(x) = 0
+    Formula: f(x) = ( x + (x^2 + b)**0.5 ) / 2
 
     Examples:
     >>> squareplus(np.array([2.3, 0.6, -2, -3.8]), beta=2)
-    array([5.29, 0.36, 0.  , 0.  ])
+    array([2.5       , 1.06811457, 0.22474487, 0.12731349])
 
     >>> squareplus(np.array([-9.2, -0.3, 0.45, -4.56]), beta=3)
-    array([0.      , 0.      , 0.091125, 0.      ])
+    array([0.0808119 , 0.72891979, 1.11977651, 0.15893419])
     """
-    return np.where(vector > 0, vector**beta, 0)
+    return (vector + np.sqrt(vector**2 + beta)) / 2
 
 
 if __name__ == "__main__":
