faf2975ea7a8e7692db6c88da97fb4a9c58f3a88
Jibesh Roy <jibeshr60@gmail.com>
2023-10-08 09:29:12 +0530

    Added new file Generate Parenthesis

---
+++ backtracking/generate_parenthesis.py
@@ -0,0 +1,48 @@
+"""
+   Given 'n' Pairs of parenthesis,
+   this program generates all combinations of parenthesis 
+   Example, n=3 :
+   [
+       "((()))",
+       "(()())",
+       "(())()",
+       "()(())",
+       "()()()"
+   ]
+   This problem is solved using Backtracking 
+"""
+def solve(op,open,close, ans):
+    if(open == 0 and close == 0):
+        ans.append(op)
+        return
+    if(open == close):
+        op1 = op
+        op1+=('(')
+        solve(op1, open-1, close, ans)
+    elif(open == 0):
+        op1 = op
+        op1+=(')')
+        solve(op1, open, close-1, ans)
+    elif(close == 0):
+        op1 = op
+        op1+=('(')
+        solve(op1, open-1, close, ans)
+    else:
+        op1 = op
+        op2 = op
+        op1+=('(')
+        op2+=(')')
+        solve(op1, open-1, close, ans)
+        solve(op2, open, close-1, ans)
+
+
+def generateParenthesis(n):
+    open = n
+    close = n
+    ans=[]
+    op = ""
+    solve(op, open, close, ans)
+    return ans
+    
+
+print(generateParenthesis(3))
