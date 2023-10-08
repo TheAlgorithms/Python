d5b0f8e5963d803c745610f090868460fec21daf
hallowshaw <rhitam.rit54@gmail.com>
2023-10-08 22:09:18 +0530

    ArmStrong Number No While Loop

--- maths/armstrong_numbers.py
+++ maths/armstrong_numbers.py
@@ -15,82 +15,49 @@ FAILING: tuple = (-153, -1, 0, 1.2, 200, "A", [], {}, None)
 def armstrong_number(n: int) -> bool:
     """
     Return True if n is an Armstrong number or False if it is not.
-
-    >>> all(armstrong_number(n) for n in PASSING)
-    True
-    >>> any(armstrong_number(n) for n in FAILING)
-    False
     """
     if not isinstance(n, int) or n < 1:
         return False
 
-    # Initialization of sum and number of digits.
-    total = 0
-    number_of_digits = 0
-    temp = n
-    # Calculation of digits of the number
-    while temp > 0:
-        number_of_digits += 1
-        temp //= 10
-    # Dividing number into separate digits and find Armstrong number
-    temp = n
-    while temp > 0:
-        rem = temp % 10
-        total += rem**number_of_digits
-        temp //= 10
+    num_str = str(n)
+    num_digits = len(num_str)
+    total = sum(int(digit) ** num_digits for digit in num_str)
     return n == total
 
 
 def pluperfect_number(n: int) -> bool:
-    """Return True if n is a pluperfect number or False if it is not
-
-    >>> all(armstrong_number(n) for n in PASSING)
-    True
-    >>> any(armstrong_number(n) for n in FAILING)
-    False
-    """
+    """Return True if n is a pluperfect number or False if it is not."""
     if not isinstance(n, int) or n < 1:
         return False
 
-    # Init a "histogram" of the digits
-    digit_histogram = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    digit_total = 0
-    total = 0
-    temp = n
-    while temp > 0:
-        temp, rem = divmod(temp, 10)
-        digit_histogram[rem] += 1
-        digit_total += 1
+    num_str = str(n)
+    num_digits = len(num_str)
+    digit_histogram = [0] * 10
 
-    for cnt, i in zip(digit_histogram, range(len(digit_histogram))):
-        total += cnt * i**digit_total
+    for digit in num_str:
+        digit_histogram[int(digit)] += 1
 
+    total = sum(cnt * (i ** num_digits) for i, cnt in enumerate(digit_histogram))
     return n == total
 
 
 def narcissistic_number(n: int) -> bool:
-    """Return True if n is a narcissistic number or False if it is not.
-
-    >>> all(armstrong_number(n) for n in PASSING)
-    True
-    >>> any(armstrong_number(n) for n in FAILING)
-    False
-    """
+    """Return True if n is a narcissistic number or False if it is not."""
     if not isinstance(n, int) or n < 1:
         return False
-    expo = len(str(n))  # the power that all digits will be raised to
-    # check if sum of each digit multiplied expo times is equal to number
-    return n == sum(int(i) ** expo for i in str(n))
+
+    num_str = str(n)
+    expo = len(num_str)
+    total = sum(int(digit) ** expo for digit in num_str)
+    return n == total
 
 
 def main():
     """
-    Request that user input an integer and tell them if it is Armstrong number.
+    Request that user input an integer and tell them if it is an Armstrong number.
     """
     num = int(input("Enter an integer to see if it is an Armstrong number: ").strip())
     print(f"{num} is {'' if armstrong_number(num) else 'not '}an Armstrong number.")
-    print(f"{num} is {'' if narcissistic_number(num) else 'not '}an Armstrong number.")
-    print(f"{num} is {'' if pluperfect_number(num) else 'not '}an Armstrong number.")
 
 
 if __name__ == "__main__":
@@ -98,3 +65,4 @@ if __name__ == "__main__":
 
     doctest.testmod()
     main()
+
