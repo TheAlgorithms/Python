41b1a1028855edac280a2f0441e56499f87b24a3
Bhargav Shirin Nalamati <bnalamat@gitam.in>
2023-10-08 16:41:49 +0530

    Update train a basic linear regression model.py

--- machine_learning/train a basic linear regression model.py
+++ machine_learning/train a basic linear regression model.py
@@ -1,3 +1,4 @@
+# Import necessary libraries
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
@@ -9,15 +10,14 @@ X = np.random.rand(100, 1)  # Feature (input)
 y = 2 * X + 1 + 0.1 * np.random.randn(100, 1)  # Target (output) with some noise
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+# Create and train a Linear Regression model
 model = LinearRegression()
-model.fit(X_train, y_train)
+model.fit(x_train, y_train)
 
 # Make predictions on the test data
-y_pred = model.predict(X_test)
+y_pred = model.predict(x_test)
 
 # Calculate the Mean Squared Error (MSE) to evaluate the model's performance
 mse = mean_squared_error(y_test, y_pred)
