76a64a008a9d873609e8bfae4bdb0af16c8f79ba
Jibesh10101011 <114723590+Jibesh10101011@users.noreply.github.com>
2023-10-08 11:34:03 +0530

    Update generate_parenthesis.py

--- backtracking/generate_parenthesis.py
+++ backtracking/generate_parenthesis.py
@@ -1,50 +1,51 @@
 """
-Given 'n' Pairs of parenthesis,
-this program generates all combinations of parenthesis
-Example, n=3 :
-[
-    "((()))",
-    "(()())",
-    "(())()",
-    "()(())",
-    "()()()"
-]
-This problem is solved using Backtracking
+Given, 'n' pairs of parenthesis,
+this program will generate all combinations of valid parenthesis 
 """
-
-
-def solve(op, open, close, ans) -> int:
-    if open == 0 and close == 0:
-        ans.append(op)
-        return
-    if open == close:
-        op1 = op
-        op1 += "("
-        solve(op1, open - 1, close, ans)
-    elif open == 0:
-        op1 = op
-        op1 += ")"
-        solve(op1, open, close - 1, ans)
-    elif close == 0:
-        op1 = op
-        op1 += "("
-        solve(op1, open - 1, close, ans)
+def solve(op : str,open : int ,close : int , ans : list[str]) -> int :
+    if(open == 0 and close == 0):
+      ans.append(op)
+      return
+    if(open == close):
+      op1 : str = op
+      op1+=('(')
+      solve(op1, open-1, close, ans)
+    elif(open == 0):
+      op1 : str = op
+      op1+=(')')
+      solve(op1, open, close-1, ans)
+    elif(close == 0):
+      op1 : str= op
+      op1+=('(')
+      solve(op1, open-1, close, ans)
     else:
-        op1 = op
-        op2 = op
-        op1 += "("
-        op2 += ")"
-        solve(op1, open - 1, close, ans)
-        solve(op2, open, close - 1, ans)
+      op1 : str = op
+      op2 : str = op
+      op1+=('(')
+      op2+=(')')
+      solve(op1, open-1, close, ans)
+      solve(op2, open, close-1, ans)
 
 
-def generate_parenthesis(n) -> list[str]:
-    open = n
-    close = n
-    ans = []
-    op = ""
-    solve(op, open, close, ans)
-    return ans
+def generate_parenthesis(n : int) -> list[str] :
+   """
+   >>> generate_parenthesis(3)
+   ['((()))', '(()())', '(())()', '()(())', '()()()']
+   >>> generate_parenthesis(4)
+   ['(((())))', '((()()))', '((())())', '((()))()', '(()(()))', '(()()())', '(()())()', '(())(())', '(())()()', '()((()))', '()(()())', '()(())()', '()()(())', '()()()()']
+   >>> generate_parenthesis(5)
+   ['((((()))))', '(((()())))', '(((())()))', '(((()))())', '(((())))()', '((()(())))', '((()()()))', '((()())())', '((()()))()', '((())(()))', '((())()())', '((())())()', '((()))(())', '((()))()()', '(()((())))', '(()(()()))', '(()(())())', '(()(()))()', '(()()(()))', '(()()()())', '(()()())()', '(()())(())', '(()())()()', '(())((()))', '(())(()())', '(())(())()', '(())()(())', '(())()()()', '()(((())))', '()((()()))', '()((())())', '()((()))()', '()(()(()))', '()(()()())', '()(()())()', '()(())(())', '()(())()()', '()()((()))', '()()(()())', '()()(())()', '()()()(())', '()()()()()']
+   """
+   open : int = n
+   close : int = n
+   ans : list[str]=[] 
+   op : str = ""
+   solve(op, open, close, ans)
+   return ans
+    
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
 
 
-print(generate_parenthesis(3))
