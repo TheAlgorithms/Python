70b6e06a3efd2b628eb315965d1de769b6389715
Tianyi Zheng <tianyizheng02@gmail.com>
2023-10-08 15:02:41 -0400

    Update maths/carmichael_number.py

--- maths/carmichael_number.py
+++ maths/carmichael_number.py
@@ -5,7 +5,7 @@ satisfies the following modular arithmetic condition:
 
     power(b, n-1) MOD n = 1,
     for all b ranging from 1 to n such that b and
-    n are relatively prime, i.e, greatest_common_divisor(b, n) = 1
+    n are relatively prime, i.e, gcd(b, n) = 1
 
 Examples of Carmichael Numbers: 561, 1105, ...
 https://en.wikipedia.org/wiki/Carmichael_number
