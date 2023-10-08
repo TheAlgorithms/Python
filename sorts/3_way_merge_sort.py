eae6f65e9b3979bd0ed766d195835071b323d41a
Piyush Agrawal <85010286+piyush31009@users.noreply.github.com>
2023-10-08 12:28:27 +0530

    merge sort 3 way

--- sorts/3_way_merge_sort.py
+++ sorts/3_way_merge_sort.py
@@ -1,109 +1,87 @@
-
-def merge(gArray, low, mid1, mid2, high, destArray):
+def merge(garray, low, mid1, mid2, high, destarray):
     i = low
     j = mid1
     k = mid2
     l = low
- 
     while ((i < mid1) and (j < mid2) and (k < high)):
-        if(gArray[i] < gArray[j]):
-            if(gArray[i] < gArray[k]):
-                destArray[l] = gArray[i]
+        if(garray[i] < garray[j]):
+            if(garray[i] < garray[k]):
+                destarray[l] = garray[i]
                 l += 1
                 i += 1
             else:
-                destArray[l] = gArray[k]
+                destarray[l] = garray[k]
                 l += 1
                 k += 1
         else:
-            if(gArray[j] < gArray[k]):
-                destArray[l] = gArray[j]
+            if(garray[j] < garray[k]):
+                destarray[l] = garray[j]
                 l += 1
                 j += 1
             else:
-                destArray[l] = gArray[k]
+                destarray[l] = garray[k]
                 l += 1
                 k += 1
- 
     while ((i < mid1) and (j < mid2)):
-        if(gArray[i] < gArray[j]):
-            destArray[l] = gArray[i]
+        if(garray[i] < garray[j]):
+            destarray[l] = garray[i]
             l += 1
             i += 1
         else:
-            destArray[l] = gArray[j]
+            destarray[l] = garray[j]
             l += 1
             j += 1
- 
     while ((j < mid2) and (k < high)):
-        if(gArray[j] < gArray[k]):
-            destArray[l] = gArray[j]
+        if(garray[j] < garray[k]):
+            destarray[l] = garray[j]
             l += 1
             j += 1
         else:
-            destArray[l] = gArray[k]
+            destarray[l] = garray[k]
             l += 1
             k += 1
     while ((i < mid1) and (k < high)):
-        if(gArray[i] < gArray[k]):
-            destArray[l] = gArray[i]
+        if(garray[i] < garray[k]):
+            destarray[l] = garray[i]
             l += 1
             i += 1
         else:
-            destArray[l] = gArray[k]
+            destarray[l] = garray[k]
             l += 1
-            k += 1
- 
+            k += 1 
     while (i < mid1):
-        destArray[l] = gArray[i]
+        destarray[l] = garray[i]
         l += 1
         i += 1
- 
     while (j < mid2):
-        destArray[l] = gArray[j]
+        destarray[l] = garray[j]
         l += 1
         j += 1
- 
     while (k < high):
-        destArray[l] = gArray[k]
+        destarray[l] = garray[k]
         l += 1
         k += 1
- 
- 
-""" Performing the merge sort algorithm on the 
+""" Performin the merge sort algorithm on the 
 given array of values in the rangeof indices 
 [low, high). low is minimum index, high is 
 maximum index (exclusive) """
- 
- 
 def mergeSort3WayRec(gArray, low, high, destArray):
     if (high - low < 2):
         return
- 
     mid1 = low + ((high - low) // 3)
     mid2 = low + 2 * ((high - low) // 3) + 1
- 
     mergeSort3WayRec(destArray, low, mid1, gArray)
     mergeSort3WayRec(destArray, mid1, mid2, gArray)
     mergeSort3WayRec(destArray, mid2, high, gArray)
- 
     merge(destArray, low, mid1, mid2, high, gArray)
- 
- 
 def mergeSort3Way(gArray, n):
     if (n == 0):
-        return
- 
+        return 
     fArray = []
     fArray = gArray.copy()
- 
     mergeSort3WayRec(fArray, 0, n, gArray)
- 
     gArray = fArray.copy()
- 
     return gArray
- 
- 
 data =[]
 for i in range(10):
     data.append(int(input()))
