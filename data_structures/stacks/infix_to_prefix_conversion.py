2d7fba0ebd6a9af921a941ad7f0976dd55de4aa0
parth <parthkhandenath123@gmail.com>
2023-10-08 18:58:59 +0530

    update2

--- data_structures/stacks/infix_to_prefix_conversion.py
+++ data_structures/stacks/infix_to_prefix_conversion.py
@@ -20,6 +20,12 @@ def infix_2_postfix(infix: str) -> str:
     receives the infix notation as a string
     and returns the postfix notation as a string
     >>> infix_2_postfix("1+2")
+    Symbol  |  Stack  | Postfix
+    ----------------------------
+       1     |         | 1
+       +     | +       | 1
+       2     | +       | 12
+             |         | 12+
     '12+'
     """
     stack = []
@@ -84,6 +90,12 @@ def infix_2_prefix(infix: str) -> str:
     receives infix notation as a string
     and returns prefix notation as a string
     >>> infix_2_prefix("1+2")
+    Symbol  |  Stack  | Postfix
+    ----------------------------
+       2     |         | 2
+       +     | +       | 2
+       1     | +       | 21
+             |         | 21+
     '+12'
     """
     infix_list = []
