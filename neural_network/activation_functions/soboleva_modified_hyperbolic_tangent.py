6a604d2c87f602ca25750d66f86ccdb7ae5800ab
Saahil Mahato <115351000+saahil-mahato@users.noreply.github.com>
2023-10-09 04:35:24 +0545

    Update and rename sobovela_modified_hyperbolic_tangent.py to soboleva_modified_hyperbolic_tangent.py

--- neural_network/activation_functions/soboleva_modified_hyperbolic_tangent.py
+++ neural_network/activation_functions/soboleva_modified_hyperbolic_tangent.py
@@ -1,7 +1,7 @@
 """
-This script implements the Sobovela Modified Hyperbolic Tangent function.
+This script implements the Soboleva Modified Hyperbolic Tangent function.
 
-The function applies the Sobovela Modified Hyperbolic Tangent function
+The function applies the Soboleva Modified Hyperbolic Tangent function
 to each element of the vector.
 
 More details about the activation function can be found on:
@@ -12,22 +12,24 @@ https://en.wikipedia.org/wiki/Soboleva_modified_hyperbolic_tangent
 import numpy as np
 
 
-def sobovela_modified_hyperbolic_tangent(
+def soboleva_modified_hyperbolic_tangent(
     vector: np.ndarray, a_value: float, b_value: float, c_value: float, d_value: float
 ) -> np.ndarray:
     """
-    Implements the Sobovela Modified Hyperbolic Tangent function
+    Implements the Soboleva Modified Hyperbolic Tangent function
 
     Parameters:
         vector (ndarray): A vector that consitis of numeric values
-        alpha (float): alpha value
-        beta (float): beta value
+        a_value (float): paramenter a of the equation
+        b_value (float): parameter b of the equation
+        c_value (float): parameter c of the equation        
+        d_value (float): parameter d of the equation
 
     Returns:
         vector (ndarray): Input array after applying SMHT function
 
     >>> vector = np.array([5.4, -2.4, 6.3, -5.23, 3.27, 0.56])
-    >>> sobovela_modified_hyperbolic_tangent(vector, 0.2, 0.4, 0.6, 0.8)
+    >>> soboleva_modified_hyperbolic_tangent(vector, 0.2, 0.4, 0.6, 0.8)
     array([ 0.11075085, -0.28236685,  0.07861169, -0.1180085 ,  0.22999056,
             0.1566043 ])
     """
