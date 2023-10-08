948036503d714b82ccf1bba6db0abde49cca4a4d
Saahil Mahato <115351000+saahil-mahato@users.noreply.github.com>
2023-10-09 04:44:36 +0545

    fix: algorithm bug and docs

--- neural_network/activation_functions/binary_step.py
+++ neural_network/activation_functions/binary_step.py
@@ -1,9 +1,8 @@
 """
 This script demonstrates the implementation of the Binary Step function.
 
-It's an activation function in which if the input to the
-activation function is greater than a threshold, the neuron is
-activated, else it is deactivated
+It's an activation function in which the neuron is activated if the input is positive,
+else it is deactivated
 
 It's a simple activation function which is mentioned in this wikipedia article:
 https://en.wikipedia.org/wiki/Activation_function
@@ -18,17 +17,17 @@ def binary_step(vector: np.ndarray) -> np.ndarray:
     Implements the binary step function
 
     Parameters:
-        vector (ndarray): A vector that consitis of numeric values
+        vector (ndarray): A vector that consists of numeric values
 
     Returns:
-        vector (ndarray): A vector that consitis of values 0 or 1
+        vector (ndarray): Input vector after applying binary step function
 
     >>> vector = np.array([-1.2, 0, 2, 1.45, -3.7, 0.3])
     >>> binary_step(vector)
-    array([0, 0, 1, 1, 0, 1])
+    array([0, 1, 1, 1, 0, 1])
     """
 
-    return np.where(vector > 0, 1, 0)
+    return np.where(vector >= 0, 1, 0)
 
 
 if __name__ == "__main__":
