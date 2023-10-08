41029ba380080d1e73135bf497a8c4002ed65c1f
Anshul shrivas <128448038+anshul-132002@users.noreply.github.com>
2023-10-08 10:03:56 +0530

    Added parentheses using backtracking

---
+++ backtracking/parentheses.py
@@ -0,0 +1,18 @@
+def generateParenthesis(n):
+    def backtrack(combination, open_count, close_count):
+        if len(combination) == 2 * n:
+            valid_combinations.append(combination)
+            return
+        if open_count < n:
+            backtrack(combination + '(', open_count + 1, close_count)
+        if close_count < open_count:
+            backtrack(combination + ')', open_count, close_count + 1)
+
+    valid_combinations = []
+    backtrack('', 0, 0)
+    return valid_combinations
+
+n = 3  # You can change the value of n to generate combinations for a different number of pairs of parentheses
+result = generateParenthesis(n)
+for combination in result:
+    print(combination)
