2a4989dc6c4fc8845220cb2b07e28ece8f7caf03
Arnav Kohli <95236897+THEGAMECHANGER416@users.noreply.github.com>
2023-10-08 13:00:47 +0530

    Update mean_squared_error.py

--- machine_learning/loss_functions/mean_squared_error.py
+++ machine_learning/loss_functions/mean_squared_error.py
@@ -31,6 +31,12 @@ def mean_squared_error(y_true: np.ndarray, y_pred: np.ndarray) -> float:
     >>> predicted_values = np.array([0.8, 2.1, 2.9, 4.2, 5.2])
     >>> mean_squared_error(true_values, predicted_values)
     0.028000000000000032
+    >>> true_labels = np.array([0, 1, 1, 0, 1])
+    >>> predicted_probs = np.array([0.3, 0.8, 0.9, 0.2])
+    >>> binary_cross_entropy(true_labels, predicted_probs)
+    Traceback (most recent call last):
+        ...
+    ValueError: Input arrays must have the same length.
     """
     if len(y_true) != len(y_pred):
         raise ValueError("Input arrays must have the same length.")
