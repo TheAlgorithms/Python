341df1882f9b1c280fd31a804a5f3cab4c4d7144
Arnav Kohli <95236897+THEGAMECHANGER416@users.noreply.github.com>
2023-10-09 12:42:53 +0530

    Update machine_learning/loss_functions/categorical_cross_entropy.py
    Co-authored-by: Tianyi Zheng <tianyizheng02@gmail.com>

--- machine_learning/loss_functions/categorical_cross_entropy.py
+++ machine_learning/loss_functions/categorical_cross_entropy.py
@@ -14,7 +14,7 @@ Resources:
 import numpy as np
 
 
-def categorical_crossentropy(
+def categorical_cross_entropy(
     y_true: np.ndarray, y_pred: np.ndarray, epsilon: float = 1e-15
 ) -> float:
     """
