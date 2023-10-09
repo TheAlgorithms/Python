1dfce0617e5b321179d3eb6876720d88e72a5dac
AasheeshLikePanner <ashishlearningsoftware@gmail.com>
2023-10-09 19:57:42 +0530

    Adding doctest in subset_generation.py

--- dynamic_programming/subset_generation.py
+++ dynamic_programming/subset_generation.py
@@ -3,42 +3,50 @@
 
 def combination_util(arr, n, r, index, data, i):
     """
-    Current combination is ready to be printed, print it
-    arr[]  ---> Input Array
-    data[] ---> Temporary array to store current combination
-    start & end ---> Staring and Ending indexes in arr[]
-    index  ---> Current index in data[]
-    r ---> Size of a combination to be printed
+    Generate and print all combinations of 'r' elements from the input list 'arr'.
+    Args:
+    arr (list): The input list from which combinations are generated.
+    n (int): The total number of elements in the input list 'arr'.
+    r (int): The size of the combinations to be generated.
+    index (int): The current index in the 'data' array.
+    data (list): Temporary array to store the current combination being generated.
+    i (int): The current index in the input list 'arr'.
+    Returns:
+    None: This function prints the combinations but does not return a value.
+    Examples:
+    >>> arr = [1, 2, 3, 4]
+    >>> n = len(arr)
+    >>> r = 2
+    >>> data = [0] * r
+    >>> combination_util(arr, n, r, 0, data, 0)
+    1 2
+    1 3
+    1 4
+    2 3
+    2 4
+    3 4
     """
     if index == r:
         for j in range(r):
             print(data[j], end=" ")
         print(" ")
         return
-    #  When no more elements are there to put in data[]
     if i >= n:
         return
-    # current is included, put next at next location
     data[index] = arr[i]
     combination_util(arr, n, r, index + 1, data, i + 1)
-    # current is excluded, replace it with
-    # next (Note that i+1 is passed, but
-    # index is not changed)
     combination_util(arr, n, r, index, data, i + 1)
-    # The main function that prints all combinations
-    # of size r in arr[] of size n. This function
-    # mainly uses combinationUtil()
 
 
 def print_combination(arr, n, r):
-    # A temporary array to store all combination one by one
     data = [0] * r
-    # Print all combination using temporary array 'data[]'
     combination_util(arr, n, r, 0, data, 0)
 
 
 if __name__ == "__main__":
-    # Driver code to check the function above
     arr = [10, 20, 30, 40, 50]
     print_combination(arr, len(arr), 3)
-    # This code is contributed by Ambuj sahu
+
+    import doctest
+
+    doctest.testmod()
