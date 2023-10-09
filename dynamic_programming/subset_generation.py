9ca12cec12ed39b54dc00fa3ddd6e3c98d85887d
AasheeshLikePanner <ashishlearningsoftware@gmail.com>
2023-10-09 20:39:25 +0530

    build fail fixing

--- dynamic_programming/subset_generation.py
+++ dynamic_programming/subset_generation.py
@@ -1,24 +1,23 @@
 # Print all subset combinations of n element in given set of r element.
 
 
-def combinations(arr, n, r, index, data, i):
+def combinations(arr, n, r, index=0, data=None, i=0):
     """
     Generate and print all combinations of 'r' elements from the input list 'arr'.
     Args:
     arr (list): The input list from which combinations are generated.
     n (int): The total number of elements in the input list 'arr'.
     r (int): The size of the combinations to be generated.
-    index (int): The current index in the 'data' array.
-    data (list): Temporary array to store the current combination being generated.
-    i (int): The current index in the input list 'arr'.
+    index (int, optional): The current index in the 'data' array. Defaults to 0.
+    data (list, optional): Temporary array to store the current combination being generated. Defaults to None.
+    i (int, optional): The current index in the input list 'arr'. Defaults to 0.
     Returns:
     None: This function prints the combinations but does not return a value.
     Examples:
     >>> arr = [1, 2, 3, 4]
     >>> n = len(arr)
     >>> r = 2
-    >>> data = [0] * r
-    >>> combinations(arr, n, r, 0, data, 0)
+    >>> combinations(arr, n, r)
     1 2
     1 3
     1 4
@@ -26,6 +25,9 @@ def combinations(arr, n, r, index, data, i):
     2 4
     3 4
     """
+    if data is None:
+        data = [0] * r
+
     if index == r:
         for j in range(r):
             print(data[j], end=" ")
