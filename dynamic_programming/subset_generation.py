dde13de591cb5af22bc263ea76f826d0f8ee1f8d
AasheeshLikePanner <ashishlearningsoftware@gmail.com>
2023-10-09 21:50:43 +0530

    Changing the code to return tuple

--- dynamic_programming/subset_generation.py
+++ dynamic_programming/subset_generation.py
@@ -1,44 +1,22 @@
-# Print all subset combinations of n element in given set of r element.
+def subset_combinations_dp(elements, n):
+    r = len(elements)
 
+    dp = [[] for _ in range(r + 1)]
 
-def combination_util(arr, n, r, index, data, i):
-    """
-    Current combination is ready to be printed, print it
-    arr[]  ---> Input Array
-    data[] ---> Temporary array to store current combination
-    start & end ---> Staring and Ending indexes in arr[]
-    index  ---> Current index in data[]
-    r ---> Size of a combination to be printed
-    """
-    if index == r:
-        for j in range(r):
-            print(data[j], end=" ")
-        print(" ")
-        return
-    #  When no more elements are there to put in data[]
-    if i >= n:
-        return
-    # current is included, put next at next location
-    data[index] = arr[i]
-    combination_util(arr, n, r, index + 1, data, i + 1)
-    # current is excluded, replace it with
-    # next (Note that i+1 is passed, but
-    # index is not changed)
-    combination_util(arr, n, r, index, data, i + 1)
-    # The main function that prints all combinations
-    # of size r in arr[] of size n. This function
-    # mainly uses combinationUtil()
+    dp[0].append(())
 
+    for i in range(1, r + 1):
+        for j in range(i, 0, -1):
+            for prev_combination in dp[j - 1]:
+                dp[j].append(tuple(prev_combination) + (elements[i - 1],))
 
-def print_combination(arr, n, r):
-    # A temporary array to store all combination one by one
-    data = [0] * r
-    # Print all combination using temporary array 'data[]'
-    combination_util(arr, n, r, 0, data, 0)
+    combinations = dp[n]
+
+    return combinations
 
 
 if __name__ == "__main__":
-    # Driver code to check the function above
-    arr = [10, 20, 30, 40, 50]
-    print_combination(arr, len(arr), 3)
-    # This code is contributed by Ambuj sahu
+    elements = [10, 20, 30, 40]
+    n = 2
+    result = subset_combinations_dp(elements, n)
+    print(result)
