7840a7b5c15ad0ec286d2d44f21e6b7a88634c2e
ArunSiva <Arunsiva003@users.noreply.github.com>
2023-10-09 17:30:39 +0530

    count_pairs_with_given_sum

--- data_structures/arrays/count_pairs_with_givensum.py
+++ data_structures/arrays/count_pairs_with_givensum.py
@@ -6,9 +6,9 @@ Reference: https://www.geeksforgeeks.org/count-pairs-with-given-sum/
 Author : Arunkumar (Arunsiva003)
 Date: 9th October 2023
 """
-from typing import List, Dict
 
-def count_pairs_with_given_sum(arr: List[int], target_sum: int) -> int:
+
+def count_pairs_with_given_sum(arr: list[int], target_sum: int) -> int:
     """
     Count pairs in the array that sum up to the given target sum.
 
@@ -26,30 +26,32 @@ def count_pairs_with_given_sum(arr: List[int], target_sum: int) -> int:
         3
     """
     # Create a dictionary to store the frequency of each element in the array
-    num_freq: Dict[int, int] = {}
-    
+    num_freq: dict[int, int] = {}
+
     # Initialize the count of pairs to 0
     pair_count = 0
-    
+
     # Iterate through the array
     for num in arr:
         # Calculate the difference needed to reach the target sum
         diff = target_sum - num
-        
+
         # If the difference is in the dictionary, increment the pair count
         if diff in num_freq:
             pair_count += num_freq[diff]
-        
+
         # Update the frequency of the current number in the dictionary
         if num in num_freq:
             num_freq[num] += 1
         else:
             num_freq[num] = 1
-    
+
     # The pair count is twice the actual count, so divide by 2
     return pair_count
 
+
 if __name__ == "__main__":
-    input_arr = [1,2,3,4,5,6]
+    input_arr = [1, 2, 3, 4, 5, 6]
     target_sum = 6
-    print(f"Pairs that sum up to {target_sum}: {count_pairs_with_given_sum(input_arr, target_sum)}")
+    result = count_pairs_with_given_sum(input_arr, target_sum)
+    print(f"Pairs that sum up to {target_sum}: {result}")
