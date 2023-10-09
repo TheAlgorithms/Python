885a98ff1a109b924ded0920c6c9b307b1adff8a
Rohan Sardar <77870108+RohanSardar@users.noreply.github.com>
2023-10-10 00:55:34 +0530

    Update seconds_timeConversion.py

--- maths/seconds_timeConversion.py
+++ maths/seconds_timeConversion.py
@@ -1,19 +1,19 @@
-def timeConversion(sec: int) -> int:
+def timeconversion(sec: int) -> int:
     """
     This simple python program converts seconds to days, hours, minutes and seconds
 
     Usage examples:
-    >>> timeConversion(458964)
+    >>> timeconversion(458964)
     (5, 7, 29, 24)
-    >>> timeConversion(185683)
+    >>> timeconversion(185683)
     (2, 3, 34, 43)
-    >>> timeConversion(76896)
+    >>> timeconversion(76896)
     (0, 21, 21, 36)
-    >>> timeConversion(7896543)
+    >>> timeconversion(7896543)
     (91, 9, 29, 3)
-    >>> timeConversion(1000000)
+    >>> timeconversion(1000000)
     (11, 13, 46, 40)
-    >>> timeConversion(256)
+    >>> timeconversion(256)
     (0, 0, 4, 16)
     """
     days = sec // 86400
@@ -29,6 +29,6 @@ def timeConversion(sec: int) -> int:
 if __name__ == "__main__":
     seconds = 256  # assign the input here
     print(
-        f"{timeConversion(seconds)[0]} days {timeConversion(seconds)[1]} hours\
- {timeConversion(seconds)[2]} minutes {timeConversion(seconds)[3]} seconds"
+        f"{timeconversion(seconds)[0]} days {timeconversion(seconds)[1]} hours\
+ {timeconversion(seconds)[2]} minutes {timeconversion(seconds)[3]} seconds"
     )
