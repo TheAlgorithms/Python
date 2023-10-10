df219a0d03529ad73b9ffb19b9135059d7a8afa9
IBR-41379 <sayandeep369@gmail.com>
2023-10-10 23:51:17 +0530

    Update simple_adaline.py

--- neural_network/simple_adaline.py
+++ neural_network/simple_adaline.py
@@ -1,76 +1,64 @@
-from typing import List, Tuple
-
-
-def weight_change_or(
-    weight: List[float], threshold: float, learning_rate: float
-) -> List[float]:
-    output = weight[0] * 0 + weight[1] * 0
+# This program implements the AND and OR gates using the Adaline algorithm.
+def weight_change_or(weight: list[float], threshold: float, learning_rate: float) -> list[float]:
+    output = weight[0]*0 + weight[1]*0
     if output <= threshold:
-        output_left = weight[0] * 0 + weight[1] * 1
+        output_left = weight[0]*0 + weight[1]*1
         if output_left >= threshold:
-            output_left_down = weight[0] * 1 + weight[1] * 0
+            output_left_down = weight[0]*1 + weight[1]*0
             if output_left_down >= threshold:
-                output_all = weight[0] * 1 + weight[1] * 1
+                output_all = weight[0]*1 + weight[1]*1
                 if output_all >= threshold:
                     return weight
                 else:
-                    weight[0] = weight[0] + learning_rate * 1 * 1
-                    weight[1] = weight[1] + learning_rate * 1 * 1
+                    weight[0] = weight[0] + learning_rate*1*1
+                    weight[1] = weight[1] + learning_rate*1*1
                     return weight_change_or(weight, threshold, learning_rate)
             else:
-                weight[0] = weight[0] + learning_rate * 1 * 1
-                weight[1] = weight[1] + learning_rate * 1 * 0
+                weight[0] = weight[0] + learning_rate*1*1
+                weight[1] = weight[1] + learning_rate*1*0
                 return weight_change_or(weight, threshold, learning_rate)
         else:
-            weight[0] = weight[0] + learning_rate * 1 * 0
-            weight[1] = weight[1] + learning_rate * 1 * 1
+            weight[0] = weight[0] + learning_rate*1*0
+            weight[1] = weight[1] + learning_rate*1*1
             return weight_change_or(weight, threshold, learning_rate)
     else:
         threshold += learning_rate
         return weight_change_or(weight, threshold, learning_rate)
 
 
-def weight_change_and(
-    weight: List[float], threshold: float, learning_rate: float
-) -> List[float]:
-    output = weight[0] * 0 + weight[1] * 0
+def weight_change_and(weight: list[float], threshold: float, learning_rate: float) -> list[float]:
+    output = weight[0]*0 + weight[1]*0
     if output <= threshold:
-        output_left = weight[0] * 0 + weight[1] * 1
+        output_left = weight[0]*0 + weight[1]*1
         if output_left <= threshold:
-            output_left_down = weight[0] * 1 + weight[1] * 0
+            output_left_down = weight[0]*1 + weight[1]*0
             if output_left_down <= threshold:
-                output_all = weight[0] * 1 + weight[1] * 1
+                output_all = weight[0]*1 + weight[1]*1
                 if output_all >= threshold:
                     return weight
                 else:
-                    weight[0] = weight[0] + (learning_rate * 1 * 1)
-                    weight[1] = weight[1] + (learning_rate * 1 * 1)
+                    weight[0] = weight[0] + (learning_rate*1*1)
+                    weight[1] = weight[1] + (learning_rate*1*1)
                     return weight_change_and(weight, threshold, learning_rate)
             else:
-                weight[0] = weight[0] - (learning_rate * 1 * 1)
-                weight[1] = weight[1] - (learning_rate * 1 * 0)
+                weight[0] = weight[0] - (learning_rate*1*1)
+                weight[1] = weight[1] - (learning_rate*1*0)
                 return weight_change_and(weight, threshold, learning_rate)
         else:
-            weight[0] = weight[0] - (learning_rate * 1 * 0)
-            weight[1] = weight[1] - (learning_rate * 1 * 1)
+            weight[0] = weight[0] - (learning_rate*1*0)
+            weight[1] = weight[1] - (learning_rate*1*1)
             return weight_change_and(weight, threshold, learning_rate)
     else:
         threshold += learning_rate
         return weight_change_and(weight, threshold, learning_rate)
 
 
-def and_gate(
-    weight: List[float],
-    input_a: int,
-    input_b: int,
-    threshold: float,
-    learning_rate: float,
-) -> int:
+def and_gate(weight: list[float], input_a: int, input_b: int, threshold: float, learning_rate: float) -> int:
     """
     This function implements the AND gate using the Adaline algorithm.
 
     Args:
-        weight (List[float]): The weights for the Adaline algorithm.
+        weight (list[float]): The weights for the Adaline algorithm.
         input_a (int): The first input value.
         input_b (int): The second input value.
         threshold (float): The threshold value for the Adaline algorithm.
@@ -80,25 +68,19 @@ def and_gate(
         int: The output of the AND gate.
     """
     weight = weight_change_and(weight, threshold, learning_rate)
-    output = weight[0] * input_a + weight[1] * input_b
+    output = weight[0]*input_a + weight[1]*input_b
     if output >= threshold:
         return 1
     else:
         return 0
 
 
-def or_gate(
-    weight: List[float],
-    input_a: int,
-    input_b: int,
-    threshold: float,
-    learning_rate: float,
-) -> int:
+def or_gate(weight: list[float], input_a: int, input_b: int, threshold: float, learning_rate: float) -> int:
     """
     This function implements the OR gate using the Adaline algorithm.
 
     Args:
-        weight (List[float]): The weights for the Adaline algorithm.
+        weight (list[float]): The weights for the Adaline algorithm.
         input_a (int): The first input value.
         input_b (int): The second input value.
         threshold (float): The threshold value for the Adaline algorithm.
@@ -108,7 +90,7 @@ def or_gate(
         int: The output of the OR gate.
     """
     weight = weight_change_or(weight, threshold, learning_rate)
-    output = weight[0] * input_a + weight[1] * input_b
+    output = weight[0]*input_a + weight[1]*input_b
     if output >= threshold:
         return 1
     else:
@@ -122,11 +104,5 @@ learning_rate = 0.5
 input_a, input_b = input("Input the value of A and B:").split()
 input_a = int(input_a)
 input_b = int(input_b)
-print(
-    "\nThe output of OR is:",
-    or_gate(weight, input_a, input_b, threshold, learning_rate),
-)
-print(
-    "\nThe output of AND is:",
-    and_gate(weight2, input_a, input_b, threshold, learning_rate),
-)
+print("\nThe output of OR is:", or_gate(weight, input_a, input_b, threshold, learning_rate))
+print("\nThe output of AND is:", and_gate(weight2, input_a, input_b, threshold, learning_rate))
