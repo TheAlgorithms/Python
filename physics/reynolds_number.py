ef67be4a4ee9066217ccfa3a5f144fb0c8981e2e
Tianyi Zheng <tianyizheng02@gmail.com>
2023-10-10 01:20:33 -0400

    Apply suggestions from code review

--- physics/reynolds_number.py
+++ physics/reynolds_number.py
@@ -24,7 +24,7 @@ are classified according to a range. Laminar flow falls below Reynolds
 number of 1100 and turbulent falls in a range greater than 2200.
 Laminar flow is the type of flow in which the fluid travels smoothly in
 regular paths. Conversely, turbulent flow isn't smooth and follows an
-irregular math with lots of mixing.
+irregular path with lots of mixing.
 
 Reference : https://byjus.com/physics/reynolds-number/
 """
@@ -34,25 +34,25 @@ def reynolds_number(
     density: float, velocity: float, diameter: float, viscosity: float
 ) -> float:
     """
-    >>> reynolds_number(900,2.5,0.05,0.4)
+    >>> reynolds_number(900, 2.5, 0.05, 0.4)
     281.25
-    >>> reynolds_number(450,3.86,0.078,0.23)
+    >>> reynolds_number(450, 3.86, 0.078, 0.23)
     589.0695652173912
-    >>> reynolds_number(234,-4.5,0.3,0.44)
+    >>> reynolds_number(234, -4.5, 0.3, 0.44)
     717.9545454545454
-    >>> reynolds_number(-90,2,0.045,1)
+    >>> reynolds_number(-90, 2, 0.045, 1)
     Traceback (most recent call last):
         ...
-    ValueError: please ensure that density, diameter and viscosity are non negative
-    >>> reynolds_number(0,2,-0.4,-2)
+    ValueError: please ensure that density, diameter and viscosity are positive
+    >>> reynolds_number(0, 2, -0.4, -2)
     Traceback (most recent call last):
         ...
-    ValueError: please ensure that density, diameter and viscosity are non negative
+    ValueError: please ensure that density, diameter and viscosity are positive
     """
 
     if density <= 0 or diameter <= 0 or viscosity <= 0:
         raise ValueError(
-            "please ensure that density, diameter and viscosity are non negative"
+            "please ensure that density, diameter and viscosity are positive"
         )
     return (density * abs(velocity) * diameter) / viscosity
 
