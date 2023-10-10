8f9070663e5815fd6823e438ee349ce02123497b
AADITYAG2002 <aadityag2002@gmail.com>
2023-10-11 01:13:27 +0530

    feat: added implementation of stalin sort

---
+++ sorts/stalin_sort.py
@@ -0,0 +1,36 @@
+"""
+This is a Python implementation of stalin sort algorithm.
+Stalin sort removes the lement that is not in ascending order
+
+For doctests run following command:
+python3 -m doctest -v stalin_sort.py
+
+For manual testing run:
+python circle_sort.py
+"""
+
+def stalin_sort(collection: list) -> list:
+    """Pure implementation of bogosort algorithm in Python
+    :param collection: some mutable ordered collection with heterogeneous
+    comparable items inside
+    :return: the same collection ordered by ascending
+    Examples:
+    >>> stalin_sort([1, 2, 5, 3, 6, 4, 10])
+    [1, 2, 5, 6, 10]
+    >>> stalin_sort([])
+    []
+    >>> stalin_sort([6, 5, 4, 3, 2, 1])
+    [6]
+    """
+    i = 1
+    while i < len(collection):
+        if collection[i] < collection[i-1]:
+            collection.pop(i)
+        else:
+            i += 1
+    return collection
+
+if __name__ == "__main__":
+    user_input = input("Enter numbers separated by a comma:\n").strip()
+    unsorted = [int(item) for item in user_input.split(",")]
+    print(stalin_sort(unsorted))
