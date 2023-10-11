a8bb6a0d056f25a6b9ce909b2a7b66f416b84222
Vishal Mishra <mishravishal150@gmail.com>
2023-10-11 22:45:53 +0530

    python graphics of a 3D circle and a geometric pattern of benzene

--- graphics/Rainbow_Benzene.py
+++ graphics/Rainbow_Benzene.py
@@ -2,7 +2,20 @@
 # Rainbow Benzene
 # using Turtle Programming
 import turtle
+pen = turtle.Turtle()
+pen.speed(0)
+#it will make a 3D look like circle 
+pen.fillcolor("white")
+pen.begin_fill()
+ 
+ #Radius of circle 
+pen.circle(100)
+pen.end_fill()
+pen.hideturtle()
+
+#for the colour of rainbow
 colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow']
+#to generate the benzene 
 t = turtle.Pen()
 turtle.bgcolor('black')
 for x in range(360):
