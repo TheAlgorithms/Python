1c475350304bc7ceab3f72020cf39de9a241441c
shaheryar3 <62328879+shaheryar3@users.noreply.github.com>
2023-10-11 22:37:46 +0530

    changes made

--- maths/polynomials/polynomial_visualiser.py
+++ maths/polynomials/polynomial_visualiser.py
@@ -3,24 +3,37 @@ import matplotlib.pyplot as plt
 
 # Define your polynomial coefficients
 # You can change these coefficients as per your needs
-coefficients = [-2,0,1] #enter the polunomial coeffecients lowest first
+coefficients = [-2, 0, 1]  # Enter the polynomial coefficients from lowest to highest degree
 
 # Function to evaluate the polynomial
-def evaluate_polynomial(x):
+def evaluate_polynomial(x_value: float) -> float:
+    """
+    Evaluate a polynomial with the given coefficients at a specific x value.
+
+    Args:
+        x_value (float): The value of x at which to evaluate the polynomial.
+
+    Returns:
+        float: The result of evaluating the polynomial at the given x value.
+
+    Example:
+        >>> evaluate_polynomial(2)
+        2.0
+    """
     result = 0
     for i, coef in enumerate(coefficients):
-        result += coef * (x ** i)
+        result += coef * (x_value ** i)
     return result
 
 # Create an array of x-values using np.linspace
-x = np.linspace(-10, 10, 400)  # Adjust the range and number of points as needed
+x_values = np.linspace(-10, 10, 400)  # Adjust the range and number of points as needed
 
 # Evaluate the polynomial for the x-values
-y = evaluate_polynomial(x)
+y_values = evaluate_polynomial(x_values)
 
 # Create the matplotlib plot
 plt.figure(figsize=(8, 6))
-plt.plot(x, y, label="Polynomial Visulaiser", color='blue')
+plt.plot(x_values, y_values, label="Polynomial Visualizer", color='blue')
 plt.axhline(0, color='red', linewidth=1)
 plt.axvline(0, color='red', linewidth=1)
 
