03e5235607604f5c4f8afda1c1451bc5c27c8cb3
IBR-41379 <sayandeep369@gmail.com>
2023-10-11 23:24:24 +0530

    Update machine_learning/decision_tree_churn/churn_cal.py
    suggested changes changed
    
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- machine_learning/decision_tree_churn/churn_cal.py
+++ machine_learning/decision_tree_churn/churn_cal.py
@@ -30,21 +30,17 @@ def calculate_churn_rate() -> np.ndarray:
     ].values
     y = churn_md["Exited"]
 
-    # Splitting the dataset into training and testing data
+    # Split the dataset into training and testing data
     x_test, x_train, y_test, y_train = model_selection.train_test_split(
         x, y, test_size=0.3, random_state=3
     )
 
-    # Creating the decision tree classifier
+    # Create the decision tree classifier
     decision_tree = tree.DecisionTreeClassifier(criterion="entropy", max_depth=4)
     decision_tree.fit(x_train, y_train)
 
-    # Predicting the values
-    pred_tree = decision_tree.predict(x_test)
-
     # Returning the predicted values
-
-    return pred_tree[0:5]
+    return decision_tree.predict(x_test)
 
 
 if __name__ == "__main__":
