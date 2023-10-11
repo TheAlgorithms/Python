86af749adf31d5bc2a4fe339ecfa47293a54c14d
IBR-41379 <sayandeep369@gmail.com>
2023-10-11 11:00:28 +0530

    Made changes to pass the test

--- machine_learning/decision_tree_churn/churn_cal.py
+++ machine_learning/decision_tree_churn/churn_cal.py
@@ -1,47 +1,54 @@
-# Predicting Customer Churn rate using Decision Tree
-# Dataset from Kaggle: https://www.kaggle.com/code/korfanakis/predicting-customer-churn-with-machine-learning/input
+import doctest
+
 import pandas as pd
-from sklearn import metrics, model_selection, tree
+from sklearn import model_selection, tree
 
-df = pd.read_csv("churn_modelling.csv")
 
-# prints the first 5 rows of the dataset
-# print(df.head())
+def calculate_churn_rate():
+    """
+    Calculates the churn rate of customers using a decision tree classifier.
 
-# Sorting the dependent and independent values
-x = df[
-    [
-        "CreditScore",
-        "Age",
-        "Tenure",
-        "Balance",
-        "NumOfProducts",
-        "HasCrCard",
-        "IsActiveMember",
-        "EstimatedSalary",
-    ]
-].values
-y = df["Exited"]
+    >>> calculate_churn_rate()
+    array([0, 0, 0, 0, 1])
+    """
 
-# Splitting the dataset into training and testing data
-x_test, x_train, y_test, y_train = model_selection.train_test_split(
-    x, y, test_size=0.3, random_state=3
-)
+    df = pd.read_csv("churn_modelling.csv")
 
-# Creating the decision tree classifier
-decision_tree = tree.DecisionTreeClassifier(criterion="entropy", max_depth=4)
-decision_tree.fit(x_train, y_train)
+    # Sorting the dependent and independent values
+    x = df[
+        [
+            "CreditScore",
+            "Age",
+            "Tenure",
+            "Balance",
+            "NumOfProducts",
+            "HasCrCard",
+            "IsActiveMember",
+            "EstimatedSalary",
+        ]
+    ].values
+    y = df["Exited"]
 
-# Predicting the values
-pred_tree = decision_tree.predict(x_test)
-print("Accuracy of the model: ", metrics.accuracy_score(y_test, pred_tree))
+    # Splitting the dataset into training and testing data
+    x_test, x_train, y_test, y_train = model_selection.train_test_split(
+        x, y, test_size=0.3, random_state=3
+    )
 
-# Comparing the first five predicted and actual values
-print("Predicted values: ", pred_tree[0:5])
-print("Actual values: \n", y_test[0:5])
+    # Creating the decision tree classifier
+    decision_tree = tree.DecisionTreeClassifier(criterion="entropy", max_depth=4)
+    decision_tree.fit(x_train, y_train)
 
-# Error rate
-print("Error rate: ", metrics.mean_squared_error(y_test, pred_tree))
-# Visualizing the decision tree
-# tree.plot_tree(decision_tree)
-# plt.show()
+    # Predicting the values
+    pred_tree = decision_tree.predict(x_test)
+
+    # Returning the predicted values
+
+    return pred_tree[0:5]
+
+
+if __name__ == "__main__":
+    # Predicting the churn rate
+    pred_tree = calculate_churn_rate()
+    doctest.testmod()
+    # Printing the predicted values
+    print(pred_tree[0:5])
