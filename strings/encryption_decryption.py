0f0d0e0db733deb3b2b10b4f9ff3120bec164f3d
Adarsh Joshi <121653448+joshiadarshx@users.noreply.github.com>
2023-10-11 07:01:41 +0545

    Update encryption_decryption.py

--- strings/encryption_decryption.py
+++ strings/encryption_decryption.py
@@ -1,8 +1,6 @@
 #secreat_code_generator
 str1 = "axz"
 str2 = "byz"
-
-
 def code() -> None:
     print("\n***YOU CAME TO CODING SECTION***\n")
     code = input(" enter string to be encripted :  ")
@@ -14,8 +12,6 @@ def code() -> None:
         print(f" \n generated code is  {new_code}\n")
     else:
         print(f"\n code is  : {code[::-1]} \n")
-
-
 def decode() -> None:  # def function() -> None:
     print("\n***YOU CAME TO DeCODING SECTION***\n")
     code = input(" \nenter string to be decripted :  ")
@@ -25,10 +21,6 @@ def decode() -> None:  # def function() -> None:
         print(f" \n generated code is :  {''.join(code)}\n")
     else:
         print(f"\n code is  : {code[::-1]} \n")
-
-    pass
-
-
 while True:
     print(" \n1.Coding \n2. Decoding \n 3.exit")
     choice = int(input(" Please enter Ur choice"))
