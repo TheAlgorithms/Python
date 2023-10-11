94e542d2e0d2202d9126c4c9ebc3667d45884450
IBR-41379 <sayandeep369@gmail.com>
2023-10-12 01:02:38 +0530

    appropriate changes made to both the files

--- machine_learning/decision_tree_churn/churn_cal.py
+++ machine_learning/decision_tree_churn/churn_cal.py
@@ -8,14 +8,12 @@ from sklearn import model_selection, tree
 def calculate_churn_rate(filename: str = "churn_modelling.csv") -> np.ndarray:
     """
     Calculate the churn rate of customers using a decision tree classifier.
-
     >>> calculate_churn_rate()
     array([0, 0, 0, 0, 1])
     """
-
     churn_md = pd.read_csv(Path(__file__).with_name(filename).absolute())
 
-    # Sort the dependent and independent values
+    # Sorting the dependent and independent values
     x = churn_md[
         [
             "CreditScore",
@@ -30,21 +28,24 @@ def calculate_churn_rate(filename: str = "churn_modelling.csv") -> np.ndarray:
     ].values
     y = churn_md["Exited"]
 
-    # Split the dataset into training and testing data
+    # Splitting the dataset into training and testing data
     x_test, x_train, y_test, y_train = model_selection.train_test_split(
         x, y, test_size=0.3, random_state=3
     )
 
-    # Create the decision tree classifier
+    # Creating the decision tree classifier
     decision_tree = tree.DecisionTreeClassifier(criterion="entropy", max_depth=4)
     decision_tree.fit(x_train, y_train)
 
+    # Predicting the values
+    pred_tree = decision_tree.predict(x_test)
+
     # Returning the predicted values
-    return decision_tree.predict(x_test)
+
+    return pred_tree[0:5]
 
 
 if __name__ == "__main__":
     from doctest import testmod
-
     testmod()
     print(f"{calculate_churn_rate('churn_modelling.csv') = }")
