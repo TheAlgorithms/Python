fecb1e26b1ac3c4d44cc002eaf5d539d8266183d
Saptadeep Banerjee <saptadeep555@gmail.com>
2023-10-12 08:13:29 +0530

    I added a docstring to the partition function

--- dynamic_programming/integer_partition.py
+++ dynamic_programming/integer_partition.py
@@ -7,15 +7,30 @@ into k parts. These two facts together are used for this algorithm.
 
 
 def partition(m: int) -> int:
+    """
+    Calculate the number of ways to partition a positive integer into distinct positive integers.
+
+    Args:
+        m (int): The positive integer to partition.
+
+    Returns:
+        int: The number of ways to partition the integer.
+
+    Examples:
+        >>> partition(5)
+        2
+        >>> partition(6)
+        3
+    """
     memo: list[list[int]] = [[0 for _ in range(m)] for _ in range(m + 1)]
     for i in range(m + 1):
         memo[i][0] = 1
 
-    for n in range(m + 1):
-        for k in range(1, m):
-            memo[n][k] += memo[n][k - 1]
-            if n - k > 0:
-                memo[n][k] += memo[n - k - 1][k]
+    for total in range(m + 1):
+        for largest_num in range(1, m):
+            memo[total][largest_num] += memo[total][largest_num - 1]
+            if total - largest_num > 0:
+                memo[total][largest_num] += memo[total - largest_num - 1][largest_num]
 
     return memo[m][m - 1]
 
@@ -25,13 +40,19 @@ if __name__ == "__main__":
 
     if len(sys.argv) == 1:
         try:
-            n = int(input("Enter a number: ").strip())
-            print(partition(n))
+            n = int(input("Enter a positive integer: ").strip())
+            if n <= 0:
+                print("Please enter a positive integer.")
+            else:
+                print("Number of ways to partition:", partition(n))
         except ValueError:
-            print("Please enter a number.")
+            print("Please enter a valid positive integer.")
     else:
         try:
             n = int(sys.argv[1])
-            print(partition(n))
+            if n <= 0:
+                print("Please pass a positive integer.")
+            else:
+                print("Number of ways to partition:", partition(n))
         except ValueError:
-            print("Please pass a number.")
+            print("Please pass a valid positive integer.")
