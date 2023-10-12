d9f8025a28f9859a8a359ad9b2eae2248803a7dd
Amarelfaith <abubakarelfaith@gmail.com>
2023-10-12 14:36:43 +0100

    Added an MD5 hashing algorithm that hashes any given string

--- hashes/hashing.py
+++ hashes/hashing.py
@@ -4,13 +4,13 @@ To run this script, you must have Python 3 installed.
 To run this script, type the following command:
 python hashing.py
 '''
-import hashlib
+import hashlib as hl
 
 # Define the string to be hashed
 string_to_hash = input("Enter the string to be hashed: ")
 
 # Create an instance of the MD5 hashing algorithm
-hash_object = hashlib.md5()
+hash_object = hl.md5()
 
 # Update the hash object with the string to be hashed
 hash_object.update(string_to_hash.encode())
