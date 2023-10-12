a7ac76d49b4d5438fea9e6407302d5b82682443d
DHRUVKADAM22 <147514863+DHRUVKADAM22@users.noreply.github.com>
2023-10-12 15:13:09 +0530

    Create bernoulli_principle.py

---
+++ physics/bernoulli_principle.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+
+v = np.linspace(0, 10, 100)  # Velocity range from 0 to 10
+
+# Define constants for the Bernoulli equation
+pressure_constant = 1.0  # A constant for simplicity
+
+# Calculate pressure using the Bernoulli equation
+pressure = pressure_constant - 0.5 * v**2
+
+# Create a plot
+plt.figure(figsize=(8, 6))
+plt.plot(v, pressure)
+plt.title("Bernoulli Principle Visualization")
+plt.xlabel("Velocity")
+plt.ylabel("Pressure")
+plt.grid(True)
+
+# Show the plot
+plt.show()
