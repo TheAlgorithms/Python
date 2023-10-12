7ff9f19e8ab1e720f88af85da3d002e55f88f2f9
Iyiola <48067557+ialoko@users.noreply.github.com>
2023-10-12 16:02:38 -0400

    Update binary_exponentiation.py

--- maths/binary_exponentiation.py
+++ maths/binary_exponentiation.py
@@ -5,6 +5,18 @@
 
 
 def binary_exponentiation(a: int, n: int) -> int:
+    """
+    >>> binary_exponentiation(-1, 3)
+    -1
+    >>> binary_exponentiation(-1, 4)
+    1
+    >>> binary_exponentiation(2, 2)
+    4
+    >>> binary_exponentiation(5e3, 1)
+    5000.0
+    >>> binary_exponentiation(-5e3, 1)
+    -5000.0
+    """
     if n == 0:
         return 1
 
@@ -18,10 +30,14 @@ def binary_exponentiation(a: int, n: int) -> int:
 
 if __name__ == "__main__":
     try:
-        BASE = int(input("Enter Base : ").strip())
+        BASE = int(float(input("Enter Base : ").strip()))
         POWER = int(input("Enter Power : ").strip())
     except ValueError:
         print("Invalid literal for integer")
 
     RESULT = binary_exponentiation(BASE, POWER)
     print(f"{BASE}^({POWER}) : {RESULT}")
+
+    import doctest
+
+    doctest.testmod()
