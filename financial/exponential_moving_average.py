68c4bb87d24081238a2a7c5ddc942f8a35243cb8
Poojan Smart <44301271+PoojanSmart@users.noreply.github.com>
2023-10-12 17:28:50 +0530

    readibility of code and documentation

--- financial/exponential_moving_average.py
+++ financial/exponential_moving_average.py
@@ -1,7 +1,8 @@
 """
-    Calculates exponential moving average (EMA) on the series of numbers
+    Calculate the exponential moving average (EMA) on the series of numbers.
     Wikipedia Reference: https://en.wikipedia.org/wiki/Exponential_smoothing
-    Reference: https://www.investopedia.com/terms/e/ema.asp#toc-what-is-an-exponential-moving-average-ema
+    https://www.investopedia.com/terms/e/ema.asp#toc-what-is-an-exponential
+    -moving-average-ema
 
     Exponential moving average is used in finance to analyze changes stock prices.
     EMA is used in conjunction with Simple moving average (SMA), EMA reacts to the
@@ -12,22 +13,27 @@ from collections.abc import Iterator
 
 
 def exponential_moving_average(
-    series_generator: Iterator[float], window_size: int
+    series: Iterator[float], window_size: int
 ) -> Iterator[float]:
     """
     Returns the generator which generates exponential moving average of the given
-    series generator
-    >>> list(exponential_moving_average((ele for ele in [2, 5, 3, 8.2, 6, 9, 10]), 3))
+    series
+    >>> list(exponential_moving_average(iter([2, 5, 3, 8.2, 6, 9, 10]), 3))
     [2.0, 3.5, 3.25, 5.725, 5.8625, 7.43125, 8.715625]
 
-    :param series_generator: Generator which generates numbers
+    :param series: Generator which generates numbers
     :param window_size: Window size for calculating average (window_size > 0)
     :return: Returns generator of which returns exponentially averaged numbers
 
     Formula:
 
     st = alpha * xt + (1 - alpha) * st_prev
-    alpha = 2/(1 + window_size) - smoothing factor
+
+    Where,
+    st : Exponential moving average at timestamp t
+    xt : Datapoint in series at timestamp t
+    st_prev : Exponential moving average at timestamp t-1
+    alpha : 2/(1 + window_size) - smoothing factor
 
     Exponential moving average (EMA) is a rule of thumb technique for
     smoothing time series data using the exponential window function.
@@ -40,22 +46,28 @@ def exponential_moving_average(
     alpha = 2 / (1 + window_size)
 
     # Defining timestamp t
-    t = 0
+    timestamp = 0
 
     # Exponential average at timestamp t
-    st = 0.0
+    exponential_average = 0.0
 
-    for xt in series_generator:
-        if t <= window_size:
+    for datapoint in series:
+        if timestamp <= window_size:
             # Assigning simple moving average till the window_size for the first time
             # is reached
-            st = float(xt) if t == 0 else (st + xt) * 0.5
+            exponential_average = (
+                float(datapoint)
+                if timestamp == 0
+                else (exponential_average + datapoint) * 0.5
+            )
         else:
             # Calculating exponential moving average based on current timestamp data
             # point and previous exponential average value
-            st = (alpha * xt) + ((1 - alpha) * st)
-        t += 1
-        yield st
+            exponential_average = (alpha * datapoint) + (
+                (1 - alpha) * exponential_average
+            )
+        timestamp += 1
+        yield exponential_average
 
 
 if __name__ == "__main__":
@@ -64,9 +76,10 @@ if __name__ == "__main__":
     doctest.testmod()
 
     test_series = [2, 5, 3, 8.2, 6, 9, 10]
-    test_generator = (ele for ele in test_series)
     test_window_size = 3
-    result = exponential_moving_average(test_generator, test_window_size)
+    result = exponential_moving_average(
+        series=iter(test_series), window_size=test_window_size
+    )
     print("Test series: ", test_series)
     print("Window size: ", test_window_size)
     print("Result: ", list(result))
