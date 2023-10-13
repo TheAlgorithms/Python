02ce739570e2a4be08beed219f7fd1b891c23859
Riya Khandelwal <riyakhandelwal@jklu.edu.in>
2023-10-13 20:14:04 +0530

    Update largest_smallest_words.py

--- strings/largest_smallest_words.py
+++ strings/largest_smallest_words.py
@@ -1,9 +1,7 @@
-from typing import Tuple, Optional
 
+import doctest
 
-def find_smallest_and_largest_words(
-    input_string: str,
-) -> Tuple[Optional[str], Optional[str]]:
+def find_smallest_and_largest_words(input_string: str):
     """
     Find the smallest and largest words in a given input string based on their length.
 
@@ -11,35 +9,38 @@ def find_smallest_and_largest_words(
         input_string (str): The input string to analyze.
 
     Returns:
-        Tuple[Optional[str], Optional[str]]: A tuple containing the smallest and largest words found.
-        If no words are found, both values in the tuple will be None.
+        tuple of (str, str): A tuple containing the smallest and largest words found.
+        If no words are found, both values in the tuple will be empty strings.
 
-    Example:
+    Examples:
     >>> find_smallest_and_largest_words("My name is abc")
     ('My', 'name')
 
+    >>> find_smallest_and_largest_words("Hello guys")
+    ('guys', 'Hello')
+
+    >>> find_smallest_and_largest_words("OnlyOneWord")
+    ('OnlyOneWord', 'OnlyOneWord')
+
     >>> find_smallest_and_largest_words("")
-    (None, None)
+    ('', '')
     """
     words = input_string.split()
     if not words:
-        return None, None
+        return '', ''
 
     smallest_word = min(words, key=len)
     largest_word = max(words, key=len)
 
     return smallest_word, largest_word
 
-
 if __name__ == "__main__":
-    import doctest
-
     doctest.testmod()
 
     input_string = input("Enter a sentence:\n").strip()
     smallest, largest = find_smallest_and_largest_words(input_string)
 
-    if smallest is not None and largest is not None:
+    if smallest and largest:
         print(f"The smallest word in the given sentence is '{smallest}'")
         print(f"The largest word in the given sentence is '{largest}'")
     else:
