71b174dbf3bacf5d6d401ebf4cdfa1f9ba749c8e
airwaks <114548361+airwakz@users.noreply.github.com>
2023-10-13 21:52:41 +0530

    Update subset_generation.py

--- dynamic_programming/subset_generation.py
+++ dynamic_programming/subset_generation.py
@@ -1,8 +1,5 @@
-# Print all subset combinations of n element in given set of r element.
-
-
 def combination_util(arr, n, r, index, data, i):
-    """
+        """
     Current combination is ready to be printed, print it
     arr[]  ---> Input Array
     data[] ---> Temporary array to store current combination
@@ -11,34 +8,25 @@ def combination_util(arr, n, r, index, data, i):
     r ---> Size of a combination to be printed
     """
     if index == r:
-        for j in range(r):
-            print(data[j], end=" ")
-        print(" ")
+        # Instead of printing, append the combination to the list
+        result.append(tuple(data[:r]))
         return
-    #  When no more elements are there to put in data[]
+         #  When no more elements are there to put in data[]
     if i >= n:
         return
-    # current is included, put next at next location
     data[index] = arr[i]
     combination_util(arr, n, r, index + 1, data, i + 1)
-    # current is excluded, replace it with
-    # next (Note that i+1 is passed, but
-    # index is not changed)
     combination_util(arr, n, r, index, data, i + 1)
-    # The main function that prints all combinations
-    # of size r in arr[] of size n. This function
-    # mainly uses combinationUtil()
-
 
 def print_combination(arr, n, r):
-    # A temporary array to store all combination one by one
     data = [0] * r
-    # Print all combination using temporary array 'data[]'
+    result = []  # Initialize an empty list to store the combinations
     combination_util(arr, n, r, 0, data, 0)
+    return result  # Return the list of tuples
 
-
-if __name__ == "__main__":
-    # Driver code to check the function above
+if __name__ == "__main":
     arr = [10, 20, 30, 40, 50]
-    print_combination(arr, len(arr), 3)
-    # This code is contributed by Ambuj sahu
+    combinations = print_combination(arr, len(arr), 3)
+    for combo in combinations:
+        print(combo)
+#The Code is contributed by Airwakz
