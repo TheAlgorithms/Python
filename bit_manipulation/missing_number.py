1f3b829dca3a6f235ca78f351bbc5c7eb83e2a37
Abílio Azevedo <abilio10@gmail.com>
2023-10-13 19:36:22 -0300

    test: adding more tests to missing number algorithm

--- bit_manipulation/missing_number.py
+++ bit_manipulation/missing_number.py
@@ -11,11 +11,25 @@ def find_missing_number(nums: list[int]) -> int:
     Example:
         >>> find_missing_number([0, 1, 3, 4])
         2
+        >>> find_missing_number([4, 3, 1, 0])
+        2
+        >>> find_missing_number([-2, 0, 1, 3, 4])
+        Traceback (most recent call last):
+        ...
+        ValueError: negative values not supported
     """
     n = len(nums)
     missing_number = n
 
     for i in range(n):
+        if nums[i] < 0:
+            raise ValueError("negative values not supported")
         missing_number ^= i ^ nums[i]
 
     return missing_number
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
