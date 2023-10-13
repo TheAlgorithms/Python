5b30676055595c2b5f670f78edac300749e19da6
Christian Clauss <cclauss@me.com>
2023-10-13 15:14:26 +0200

    Update damerau_levenshtein_distance.py

--- strings/damerau_levenshtein_distance.py
+++ strings/damerau_levenshtein_distance.py
@@ -30,26 +30,23 @@ def damerau_levenshtein_distance(first_string: str, second_string: str) -> int:
     2
     >>> damerau_levenshtein_distance("container", "containment")
     3
+    >>> damerau_levenshtein_distance("container", "containment")
+    3
     """
-
-    length_of_first_string = len(first_string)
-    length_of_second_string = len(second_string)
-
     # Create a dynamic programming matrix to store the distances
-    dp_matrix = [
-        [0] * (length_of_second_string + 1) for _ in range(length_of_first_string + 1)
-    ]
+    dp_matrix = [[0] * (len(second_string) + 1) for _ in range(len(first_string) + 1)]
 
     # Initialize the matrix
-    for i in range(length_of_first_string + 1):
+    for i in range(len(first_string) + 1):
         dp_matrix[i][0] = i
-    for j in range(length_of_second_string + 1):
+    for j in range(len(second_string) + 1):
         dp_matrix[0][j] = j
 
     # Fill the matrix
-    for i in range(1, length_of_first_string + 1):
-        for j in range(1, length_of_second_string + 1):
-            cost = 0 if first_string[i - 1] == second_string[j - 1] else 1
+    # for i in range(1, length_of_first_string + 1):
+    for i, first_char in enumerate(first_string, start=1):
+        for j, second_char in enumerate(second_string, start=1):
+            cost = int(first_char != second_char)
 
             dp_matrix[i][j] = min(
                 dp_matrix[i - 1][j] + 1,  # Deletion
@@ -63,11 +60,10 @@ def damerau_levenshtein_distance(first_string: str, second_string: str) -> int:
                 and first_string[i - 1] == second_string[j - 2]
                 and first_string[i - 2] == second_string[j - 1]
             ):
-                dp_matrix[i][j] = min(
-                    dp_matrix[i][j], dp_matrix[i - 2][j - 2] + cost
-                )  # Transposition
+                # Transposition
+                dp_matrix[i][j] = min(dp_matrix[i][j], dp_matrix[i - 2][j - 2] + cost)
 
-    return dp_matrix[length_of_first_string][length_of_second_string]
+    return dp_matrix[-1][-1]
 
 
 if __name__ == "__main__":
