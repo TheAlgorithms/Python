2a4ab5e42b53efe68bcee53a64070cf678a62d38
airwaks <114548361+airwakz@users.noreply.github.com>
2023-10-13 22:04:35 +0530

    Update subset_generation.py

--- dynamic_programming/subset_generation.py
+++ dynamic_programming/subset_generation.py
@@ -1,32 +1,21 @@
-def combination_util(arr, n, r, index, data, i):
-        """
-    Current combination is ready to be printed, print it
-    arr[]  ---> Input Array
-    data[] ---> Temporary array to store current combination
-    start & end ---> Staring and Ending indexes in arr[]
-    index  ---> Current index in data[]
-    r ---> Size of a combination to be printed
-    """
+def combination_util(arr, n, r, index, data, i, result):
     if index == r:
-        # Instead of printing, append the combination to the list
-        result.append(tuple(data[:r]))
+        result.append(tuple(data[:r]))  # Append the combination to the result list
         return
-         #  When no more elements are there to put in data[]
     if i >= n:
         return
     data[index] = arr[i]
-    combination_util(arr, n, r, index + 1, data, i + 1)
-    combination_util(arr, n, r, index, data, i + 1)
+    combination_util(arr, n, r, index + 1, data, i + 1, result)
+    combination_util(arr, n, r, index, data, i + 1, result)
 
 def print_combination(arr, n, r):
     data = [0] * r
     result = []  # Initialize an empty list to store the combinations
-    combination_util(arr, n, r, 0, data, 0)
+    combination_util(arr, n, r, 0, data, 0, result)
     return result  # Return the list of tuples
 
-if __name__ == "__main":
+if __name__ == "__main__":
     arr = [10, 20, 30, 40, 50]
     combinations = print_combination(arr, len(arr), 3)
     for combo in combinations:
         print(combo)
-#The Code is contributed by Airwakz
