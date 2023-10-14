886679ec1e4116890f38b84a9ce6c8507691b57d
Aryan Gandhi <aryandgandhi@gmail.com>
2023-10-14 15:43:41 -0500

    doctest test

--- dynamic_programming/subset_generation.py
+++ dynamic_programming/subset_generation.py
@@ -9,6 +9,23 @@ def combination_util(arr, n, r, index, data, i):
     start & end ---> Staring and Ending indexes in arr[]
     index  ---> Current index in data[]
     r ---> Size of a combination to be printed
+
+    >>> combination_util([1, 2, 3], 3, 2, 0, [0, 0], 0)
+    1 2
+    1 3
+    2 3
+
+    >>> combination_util([-1,-2,-3], 3, 2, 0, [0, 0], 0)
+    -1 -2
+    -1 -3
+    -2 -3
+
+    both of these should return nothing
+    >>> combination_util([], 3, 2, 0, [0, 0], 0)
+
+    >>> combination_util([1], 3, 2, 0, [0, 0], 0)
+
+
     """
     if index == r:
         for j in range(r):
@@ -32,7 +49,25 @@ def combination_util(arr, n, r, index, data, i):
 
 
 def print_combination(arr, n, r):
+    """
+    >>> print_combination([1, 2, 3], 3, 2, 0, [0, 0], 0)
+    1 2
+    1 3
+    2 3
+
+    >>> print_combination([-1,-2,-3], 3, 2, 0, [0, 0], 0)
+    -1 -2
+    -1 -3
+    -2 -3
+
+    both of these should return nothing
+    >>> print_combination([], 3, 2, 0, [0, 0], 0)
+
+    >>> print_combination([1], 3, 2, 0, [0, 0], 0)"""
     # A temporary array to store all combination one by one
+    # returns nothing if n < r
+    if n < r:
+        return
     data = [0] * r
     # Print all combination using temporary array 'data[]'
     combination_util(arr, n, r, 0, data, 0)
@@ -40,6 +75,7 @@ def print_combination(arr, n, r):
 
 if __name__ == "__main__":
     # Driver code to check the function above
+    # arr = [10,20,30,40,50]
     arr = [10, 20, 30, 40, 50]
     print_combination(arr, len(arr), 3)
     # This code is contributed by Ambuj sahu
