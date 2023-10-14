cbc0a17b62671f7cee1dc39a13de9de3ae9246c6
Sheetal Neeraj <42382485+sheetalneeraj@users.noreply.github.com>
2023-10-14 09:04:26 +0530

    Update kalman_filter.py

--- audio_filters/kalman_filter.py
+++ audio_filters/kalman_filter.py
@@ -1,18 +1,17 @@
 """
 Kalman Filter in Python
 
-This script demonstrates the implementation of a one-dimensional Kalman filter using the 'filterpy' library.
-The Kalman filter is used for estimating the state of a linear dynamic system that is subject to Gaussian noise.
+This script demonstrates the implementation of a one-dimensional Kalman
+filter using the 'filterpy' library.
+The Kalman filter is used for estimating the state of a linear 
+dynamic system that is subject to Gaussian noise.
 
 The steps involved in the Kalman filter algorithm are as follows:
 1. Initialization of the state and covariance.
-2. Prediction of the next state and covariance based on the state transition matrix and process noise covariance.
-3. Update of the state estimate based on the measurement received and the measurement noise covariance.
-
-The 'filterpy' library provides tools for Kalman filtering.
-
-Make sure to install the 'filterpy' library first by using pip:
-    pip install filterpy
+2. Prediction of the next state and covariance based on the state
+transition matrix and process noise covariance.
+3. Update of the state estimate based on the measurement
+received and the measurement noise covariance.
 """
 
 import numpy as np
