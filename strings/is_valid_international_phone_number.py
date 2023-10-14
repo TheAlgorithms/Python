04d089bc6e2e125f37ed248ddd03d4cb563902d0
Saurabh Mahapatra <98408932+its-100rabh@users.noreply.github.com>
2023-10-14 14:40:58 +0530

    Created is_valid_international_phone_number.py

---
+++ strings/is_valid_international_phone_number.py
@@ -0,0 +1,23 @@
+import re
+
+def is_valid_international_phone_number(phone: str) -> bool:
+    """
+    Determine whether the string is a valid international phone number or not.
+
+    >>> is_valid_international_phone_number("+919136812895")
+    True
+    >>> is_valid_international_phone_number("+91 9136812895")
+    True
+    >>> is_valid_international_phone_number("+123 123456")
+    True
+    >>> is_valid_international_phone_number("654294563")
+    False
+    """
+
+    pattern = r"^\+(?:\d{6,15}|\d{2}\s\d{6,15})$"
+
+    return bool(re.match(pattern, phone))
+
+if __name__ == '__main':
+    str1 = "+919132896815"
+    print(is_valid_international_phone_number(str1))
