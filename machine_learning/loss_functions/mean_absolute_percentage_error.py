138ad8a2d42dbdc41bfbc480e1dd030c867e3645
Humza <abbasisaud99@gmail.com>
2023-10-15 00:24:33 +0500

    added mean_absolute_percentage_error

--- machine_learning/loss_functions/mean_absolute_percentage_error.py
+++ machine_learning/loss_functions/mean_absolute_percentage_error.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 """
-Mean Absolute Percentage Error (MAPE):
-MAPE calculates the average of the absolute percentage differences between the
+Mean Absolute Percentage Error :
+It calculates the average of the absolute percentage differences between the
 predicted and true values.
 
-MAPE = (Σ|y_true[i]-Y_pred[i]/y_true[i]|)/n
+Formula = (Σ|y_true[i]-Y_pred[i]/y_true[i]|)/n
 
 https://stephenallwright.com/good-mape-score/
 
@@ -14,14 +14,14 @@ https://stephenallwright.com/good-mape-score/
 
 def mean_absolute_percentage_error(y_true: np.ndarray, y_pred: np.ndarray) -> float:
     """
-    Calculate the Mean Absolute Percentage Error (MAPE) between y_true and y_pred.
+    Calculate the Mean Absolute Percentage Error between y_true and y_pred.
 
     Parameters:
     y_true (np.ndarray): Numpy array containing true/target values.
     y_pred (np.ndarray): Numpy array containing predicted values.
 
     Returns:
-    float: The MAPE between y_true and y_pred.
+    float: The Mean Absolute Percentage error between y_true and y_pred.
 
     Examples:
     >>> y_true = np.array([10, 20, 30, 40])
@@ -41,17 +41,17 @@ def mean_absolute_percentage_error(y_true: np.ndarray, y_pred: np.ndarray) -> fl
     """
     try:
         if len(y_true) != len(y_pred):
-            error_message = "the lenght of the two arrays should be same."
+            error_message = "the length of the two arrays should be same."
             raise ValueError(error_message)
 
         # Calculate the absolute percentage difference between y_true and y_pred
-        # addded 1e-9 to avoid divison by 0 (smoothing).
+        # added 1e-9 to avoid division by 0 (smoothing).
         absolute_percentage_diff = np.abs((y_true - y_pred) / (y_true + 1e-9))
 
         # Calculate the mean and multiply by 100 for percentage.
-        mape = np.mean(absolute_percentage_diff) * 100
+        error = np.mean(absolute_percentage_diff) * 100
 
-        return mape
+        return error
 
     except ValueError as e:
         raise e
