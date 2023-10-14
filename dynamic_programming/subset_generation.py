c2f28f5e9edcac6bcd9d188aadf23caa97bfdc7f
Aryan Gandhi <aryandgandhi@gmail.com>
2023-10-14 15:52:51 -0500

    fixing 4 tests

--- dynamic_programming/subset_generation.py
+++ dynamic_programming/subset_generation.py
@@ -14,16 +14,14 @@ def combination_util(arr, n, r, index, data, i):
     1 2
     1 3
     2 3
-
     >>> combination_util([-1,-2,-3], 3, 2, 0, [0, 0], 0)
     -1 -2
     -1 -3
     -2 -3
-
     both of these should return nothing
-    >>> combination_util([], 3, 2, 0, [0, 0], 0)
+    >>> combination_util([], 0, 2, 0, [0, 0], 0)
 
-    >>> combination_util([1], 3, 2, 0, [0, 0], 0)
+    >>> combination_util([1], 1, 2, 0, [0, 0], 0)
 
 
     """
@@ -36,6 +34,7 @@ def combination_util(arr, n, r, index, data, i):
     if i >= n:
         return
     # current is included, put next at next location
+
     data[index] = arr[i]
     combination_util(arr, n, r, index + 1, data, i + 1)
     # current is excluded, replace it with
@@ -50,23 +49,21 @@ def combination_util(arr, n, r, index, data, i):
 
 def print_combination(arr, n, r):
     """
-    >>> print_combination([1, 2, 3], 3, 2, 0, [0, 0], 0)
+    >>> print_combination([1, 2, 3], 3, 2)
     1 2
     1 3
     2 3
-
-    >>> print_combination([-1,-2,-3], 3, 2, 0, [0, 0], 0)
+    >>> print_combination([-1,-2,-3], 3, 2)
     -1 -2
     -1 -3
     -2 -3
 
-    both of these should return nothing
-    >>> print_combination([], 3, 2, 0, [0, 0], 0)
+    >>> print_combination([], 3, 2)
 
-    >>> print_combination([1], 3, 2, 0, [0, 0], 0)"""
+    >>> print_combination([1], 3, 2)"""
     # A temporary array to store all combination one by one
     # returns nothing if n < r
-    if n < r:
+    if len(arr) < r:
         return
     data = [0] * r
     # Print all combination using temporary array 'data[]'
@@ -76,6 +73,10 @@ def print_combination(arr, n, r):
 if __name__ == "__main__":
     # Driver code to check the function above
     # arr = [10,20,30,40,50]
+    import doctest
+
+    doctest.testmod()
+
     arr = [10, 20, 30, 40, 50]
     print_combination(arr, len(arr), 3)
     # This code is contributed by Ambuj sahu
