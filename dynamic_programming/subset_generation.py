4b53f932f125269c4d4c4be605fb6bec17c5785e
Keshav Kumar Jha <110764833+TheRealSaiTama@users.noreply.github.com>
2023-10-14 17:29:27 +0000

    Update subset_generation.py

--- dynamic_programming/subset_generation.py
+++ dynamic_programming/subset_generation.py
@@ -1,43 +1,23 @@
-from typing import List, Tuple, Union
+from typing import List, Tuple
 
-def combination_util(arr: list, n: int, r: int, index: int, data: list, i: int) -> Union[Tuple[int, ...], None]:
+def get_combinations(arr: List[int], n: int) -> List[Tuple[int, ...]]:
     """
-    Generate all combinations of 'r' elements from a given set of 'n' elements.
+    Returns all possible combinations of n elements from the input array.
 
-    :param arr: List of input elements
-    :param n: Number of elements in the input list
-    :param r: Size of the combination to be generated
-    :param index: Current index in the 'data' list
-    :param data: Temporary list to store the current combination
-    :param i: Current index in the 'arr' list
-    :return: A tuple representing a combination of 'r' elements, or None if no more combinations are possible.
+    :param arr: The input array
+    :param n: The number of elements to combine
+    :return: A list of tuples, where each tuple contains n elements from the input array
     """
-    if index == r:
-        return tuple(data)
-    if i >= n:
-        return None
-    data[index] = arr[i]
-    res1 = combination_util(arr, n, r, index + 1, data, i + 1)
-    res2 = combination_util(arr, n, r, index, data, i + 1)
-    if res1 is None:
-        return res2
-    elif res2 is None:
-        return res1
+    if n == 1:
+        return [(x,) for x in arr]
     else:
-        return res1, res2
+        combinations = []
+        for i in range(len(arr)):
+            for combination in get_combinations(arr[i+1:], n-1):
+                combinations.append((arr[i],) + combination)
+        return combinations
 
-def get_combinations(arr: list, n: int, r: int) -> List[Tuple[int, ...]]:
-    """
-    Generate all combinations of 'r' elements from the given list of elements.
-
-    :param arr: List of input elements
-    :param n: Number of elements in the input list
-    :param r: Size of the combination to be generated
-    :return: A list of tuples representing combinations of 'r' elements.
-    """
-    data = [0] * r
-    return combination_util(arr, n, r, 0, data, 0)
-
-if __name__ == "__main__":
-    arr = [10, 20, 30, 40, 50]
-    print(get_combinations(arr, len(arr), 3))
+# Test the function
+arr = [1, 2, 3, 4, 5]
+n = 3
+print(get_combinations(arr, n))
