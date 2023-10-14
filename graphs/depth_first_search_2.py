718d90f0e067ca7f1b6583d130e44ac9b1ed6995
Salman Siddiq <salmansiddiq566@gmail.com>
2023-10-14 19:14:35 +0500

    added doctests for DFS2

--- graphs/depth_first_search_2.py
+++ graphs/depth_first_search_2.py
@@ -1,65 +1,83 @@
-#!/usr/bin/python
+""" 
+Author: OMKAR PATHAK 
 
-""" Author: OMKAR PATHAK """
-
+Class representing a directed graph.
+"""
 
 class Graph:
     def __init__(self):
+        """
+        Initialize a graph.
+        """
         self.vertex = {}
 
-    # for printing the Graph vertices
     def print_graph(self) -> None:
-        print(self.vertex)
-        for i in self.vertex:
-            print(i, " -> ", " -> ".join([str(j) for j in self.vertex[i]]))
+        """
+        Print the graph vertices.
 
-    # for adding the edge between two vertices
+        Example:
+        >>> g = Graph()
+        >>> g.add_edge(0, 1)
+        >>> g.add_edge(0, 2)
+        >>> g.add_edge(1, 2)
+        >>> g.add_edge(2, 0)
+        >>> g.add_edge(2, 3)
+        >>> g.add_edge(3, 3)
+        >>> g.print_graph()
+        0  ->  1 -> 2
+        1  ->  2
+        2  ->  0 -> 3
+        3  ->  3
+        """
+
     def add_edge(self, from_vertex: int, to_vertex: int) -> None:
-        # check if vertex is already present,
-        if from_vertex in self.vertex:
-            self.vertex[from_vertex].append(to_vertex)
-        else:
-            # else make a new vertex
-            self.vertex[from_vertex] = [to_vertex]
+        """
+        Add an edge between two vertices.
+
+        Example:
+        >>> g = Graph()
+        >>> g.add_edge(0, 1)
+        >>> g.vertex
+        {0: [1]}
+
+        >>> g.add_edge(0, 2)
+        >>> g.vertex
+        {0: [1, 2]}
+        """
 
     def dfs(self) -> None:
-        # visited array for storing already visited nodes
-        visited = [False] * len(self.vertex)
+        """
+        Perform Depth-First Search on the graph.
 
-        # call the recursive helper function
-        for i in range(len(self.vertex)):
-            if not visited[i]:
-                self.dfs_recursive(i, visited)
+        Example:
+        >>> g = Graph()
+        >>> g.add_edge(0, 1)
+        >>> g.add_edge(0, 2)
+        >>> g.add_edge(1, 2)
+        >>> g.add_edge(2, 0)
+        >>> g.add_edge(2, 3)
+        >>> g.add_edge(3, 3)
+        >>> g.dfs()
+        0 1 2 3 
+        """
 
     def dfs_recursive(self, start_vertex: int, visited: list) -> None:
-        # mark start vertex as visited
-        visited[start_vertex] = True
+        """
+        Perform Depth-First Search recursively from a given vertex.
 
-        print(start_vertex, end=" ")
-
-        # Recur for all the vertices that are adjacent to this node
-        for i in self.vertex:
-            if not visited[i]:
-                self.dfs_recursive(i, visited)
-
+        Example:
+        >>> g = Graph()
+        >>> g.add_edge(0, 1)
+        >>> g.add_edge(0, 2)
+        >>> g.add_edge(1, 2)
+        >>> g.add_edge(2, 0)
+        >>> g.add_edge(2, 3)
+        >>> g.add_edge(3, 3)
+        >>> visited = [False] * len(g.vertex)
+        >>> g.dfs_recursive(0, visited)
+        0 1 2 3 
+        """
 
 if __name__ == "__main__":
-    g = Graph()
-    g.add_edge(0, 1)
-    g.add_edge(0, 2)
-    g.add_edge(1, 2)
-    g.add_edge(2, 0)
-    g.add_edge(2, 3)
-    g.add_edge(3, 3)
-
-    g.print_graph()
-    print("DFS:")
-    g.dfs()
-
-    # OUTPUT:
-    # 0  ->  1 -> 2
-    # 1  ->  2
-    # 2  ->  0 -> 3
-    # 3  ->  3
-    # DFS:
-    #  0 1 2 3
+    import doctest
+    doctest.testmod()
