cd26f8d6c7394e61442394d542b4cd4701933c39
Saurabh Mahapatra <98408932+its-100rabh@users.noreply.github.com>
2023-10-14 22:06:18 +0530

    Update upper.py

--- strings/upper.py
+++ strings/upper.py
@@ -14,7 +14,8 @@ def upper(word: str) -> str:
 
     # Converting to ASCII value, obtaining the integer representation
     # and checking to see if the character is a lowercase letter.
-    # If it is a lowercase letter, it is shifted by 32, making it an uppercase letter.
+    # If it is a lowercase letter,
+    # it is shifted by 32, making it an uppercase letter.
     return "".join(chr(ord(char) - 32) if "a" <= char <= "z" else char for char in word)
 
 
