23049645111d1222098e68c10913ca0333781117
Sheetal Neeraj <42382485+sheetalneeraj@users.noreply.github.com>
2023-10-14 08:03:19 +0530

    Create bitwise_rotation.py
    Performs logical left rotation and logical right rotation on a binary number, as specified by the rotation_amount. They account for cases when the rotation amount is greater than the length of the binary number.

---
+++ bit_manipulation/bitwise_rotation.py
@@ -0,0 +1,59 @@
+def logical_left_rotation(number: int, rotation_amount: int) -> str:
+    """
+    Take in 2 positive integers.
+    'number' is the integer to be logically left rotated 'rotation_amount' times.
+    Return the rotated binary representation.
+
+    >>> logical_left_rotation(0b1100, 1)
+    '0b1001'
+    >>> logical_left_rotation(0b1100, 2)
+    '0b0110'
+    >>> logical_left_rotation(0b1010, 3)
+    '0b0101'
+    >>> logical_left_rotation(0b1100, 4)
+    '0b1100'
+    >>> logical_left_rotation(0b1010, 1)
+    '0b0101'
+    >>> logical_left_rotation(0b1100, -1)
+    Traceback (most recent call last):
+        ...
+    ValueError: both inputs must be positive integers
+    """
+    if number < 0 or rotation_amount < 0:
+        raise ValueError("both inputs must be positive integers")
+
+    binary_number = bin(number)[2:]  # Convert to binary and remove '0b'
+    rotated_number = binary_number[rotation_amount % len(binary_number):] + binary_number[:rotation_amount % len(binary_number)]
+    return '0b' + rotated_number.zfill(len(binary_number))
+
+def logical_right_rotation(number: int, rotation_amount: int) -> str:
+    """
+    Take in 2 positive integers.
+    'number' is the integer to be logically right rotated 'rotation_amount' times.
+    Return the rotated binary representation.
+
+    >>> logical_right_rotation(0b1100, 1)
+    '0b0110'
+    >>> logical_right_rotation(0b1100, 2)
+    '0b0011'
+    >>> logical_right_rotation(0b1010, 3)
+    '0b0101'
+    >>> logical_right_rotation(0b1100, 4)
+    '0b1100'
+    >>> logical_right_rotation(0b1010, 1)
+    '0b0101'
+    >>> logical_right_rotation(0b1100, -1)
+    Traceback (most recent call last):
+        ...
+    ValueError: both inputs must be positive integers
+    """
+    if number < 0 or rotation_amount < 0:
+        raise ValueError("both inputs must be positive integers")
+
+    binary_number = bin(number)[2:]  # Convert to binary and remove '0b'
+    rotated_number = binary_number[-rotation_amount % len(binary_number):] + binary_number[:-rotation_amount % len(binary_number)]
+    return '0b' + rotated_number.zfill(len(binary_number))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
