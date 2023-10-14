00a3759800013d805f8d6af2fcbb1eb388130589
Jyoti Rani <102273107+Jranii@users.noreply.github.com>
2023-10-14 12:17:08 +0530

    Update all_operations_linked_list.py

--- data_structures/linked_list/all_operations_linked_list.py
+++ data_structures/linked_list/all_operations_linked_list.py
@@ -1,23 +1,40 @@
 # Define the structure of a node in the linked list
 class Node:
-    def __init__(self, data):
+    def __init__(self, data) -> None:
+        """
+        Initialize a new node with the given data.
+
+        :param data: Data to be stored in the node.
+        :type data: int
+        """
         self.data = data
         self.next = None
 
-
 # Define the singly linked list class
 class SinglyLinkedList:
-    def __init__(self):
+
+    def __init__(self) -> None:
+        """
+        Initialize an empty singly linked list.
+        """
         self.head = None
 
     # Function to insert a node at the beginning of the list
-    def insert_at_beginning(self, data):
+    def insert_at_beginning(self, data) -> None:
+        """
+        :param data: Data to be inserted.
+        :type data: int
+        """
         new_node = Node(data)
         new_node.next = self.head
         self.head = new_node
 
     # Function to insert a node at the end of the list
-    def insert_at_end(self, data):
+    def insert_at_end(self, data) -> None:
+        """
+        :param data: Data to be inserted.
+        :type data: int
+        """
         new_node = Node(data)
         if not self.head:
             self.head = new_node
@@ -28,7 +45,16 @@ class SinglyLinkedList:
         current.next = new_node
 
     # Function to insert a node at a specified position (middle)
-    def insert_at_middle(self, data, position):
+    def insert_at_middle(self, data, position) -> None:
+        """
+        Insert a new node at the specified position in the linked list.
+
+        :param data: Data to be inserted.
+        :type data: int
+        :param position: Position at which the data should be inserted.
+        :type position: int
+        :raise ValueError: If the position is invalid.
+        """
         if position <= 0:
             print("Invalid position.")
             return
@@ -48,14 +74,20 @@ class SinglyLinkedList:
         current.next = new_node
 
     # Function to delete a node at the beginning of the list
-    def delete_at_beginning(self):
+    def delete_at_beginning(self) -> None:
+        """
+        :raise ValueError: If the list is empty.
+        """
         if not self.head:
             print("List is empty. Nothing to delete.")
             return
         self.head = self.head.next
 
     # Function to delete a node at the end of the list
-    def delete_at_end(self):
+    def delete_at_end(self) -> None:
+        """
+        :raise ValueError: If the list is empty.
+        """
         if not self.head:
             print("List is empty. Nothing to delete.")
             return
@@ -68,7 +100,12 @@ class SinglyLinkedList:
         current.next = None
 
     # Function to delete a node at a specified position (middle)
-    def delete_at_middle(self, position):
+    def delete_at_middle(self, position) -> None:
+        """
+        :param position: Position of the node to be deleted.
+        :type position: int
+        :raise ValueError: If the position is invalid.
+        """
         if position <= 0:
             print("Invalid position.")
             return
@@ -86,56 +123,57 @@ class SinglyLinkedList:
         current.next = current.next.next
 
     # Function to display the linked list
-    def display(self):
+    def display(self) -> None:
         current = self.head
         while current:
             print(current.data, end=" -> ")
             current = current.next
         print("None")
 
-
 # Main program
 if __name__ == "__main__":
     linked_list = SinglyLinkedList()
 
-    while True:
-        print("\nLinked List Operations:")
-        print("1. Insert at Beginning")
-        print("2. Insert at End")
-        print("3. Insert at Middle")
-        print("4. Delete at Beginning")
-        print("5. Delete at End")
-        print("6. Delete at Middle")
-        print("7. Display")
-        print("8. Exit")
-        choice = int(input("Enter your choice: "))
+    # Insert nodes
+    linked_list.insert_at_beginning(10)
+    print(linked_list.display())
+    """
+    >>> linked_list.display()
+    '10 -> None'
+    """
 
-        if choice == 1:
-            data = int(input("Enter data to insert at the beginning: "))
-            linked_list.insert_at_beginning(data)
-            linked_list.display()
-        elif choice == 2:
-            data = int(input("Enter data to insert at the end: "))
-            linked_list.insert_at_end(data)
-            linked_list.display()
-        elif choice == 3:
-            data = int(input("Enter data to insert: "))
-            position = int(input("Enter position to insert: "))
-            linked_list.insert_at_middle(data, position)
-            linked_list.display()
-        elif choice == 4:
-            linked_list.delete_at_beginning()
-            linked_list.display()
-        elif choice == 5:
-            linked_list.delete_at_end()
-            linked_list.display()
-        elif choice == 6:
-            position = int(input("Enter position to delete: "))
-            linked_list.delete_at_middle(position)
-            linked_list.display()
-        elif choice == 7:
-            linked_list.display()
-        elif choice == 8:
-            break
-        else:
-            print("Invalid choice. Please try again.")
+    linked_list.insert_at_end(20)
+    print(linked_list.display())
+    """
+    >>> linked_list.display()
+    10 -> 20 -> None
+    """
+
+    linked_list.insert_at_middle(15, 2)
+    print(linked_list.display())
+    """
+    >>> linked_list.display()
+    '10 -> 15 -> 20 -> None
+    """
+
+    # Delete nodes
+    linked_list.delete_at_beginning()
+    print(linked_list.display())
+    """
+    >>> linked_list.display()
+    15 -> 20 -> None
+    """
+
+    linked_list.delete_at_end()
+    print(linked_list.display())
+    """
+    >>> linked_list.display()
+    15 -> None
+    """
+
+    linked_list.delete_at_middle(1)
+    print(linked_list.display())
+    """
+    >>> linked_list.display()
+    None
+    """
