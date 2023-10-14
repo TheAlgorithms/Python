1cda63d15fba22ee2b1c95ab884919d63611692b
Tharindu Dilshan Sooriyaarachchi <tdilshan2010@gmail.com>
2023-10-15 02:59:49 +0530

    Added TimSort.py

---
+++ sorts/TimSort.py
@@ -0,0 +1,85 @@
+MIN_MERGE = 32
+
+
+def calcMinRun(n):
+    r = 0
+    while n >= MIN_MERGE:
+        r |= n & 1
+        n >>= 1
+    return n + r
+
+def insertionSort(arr, left, right):
+    for i in range(left + 1, right + 1):
+        j = i
+        while j > left and arr[j] < arr[j - 1]:
+            arr[j], arr[j - 1] = arr[j - 1], arr[j]
+            j -= 1
+
+def merge(arr, l, m, r):
+    len1, len2 = m - l + 1, r - m
+    left, right = [], []
+    for i in range(0, len1):
+        left.append(arr[l + i])
+    for i in range(0, len2):
+        right.append(arr[m + 1 + i])
+
+    i, j, k = 0, 0, l
+
+    while i < len1 and j < len2:
+        if left[i] <= right[j]:
+            arr[k] = left[i]
+            i += 1
+
+        else:
+            arr[k] = right[j]
+            j += 1
+
+        k += 1
+
+    while i < len1:
+        arr[k] = left[i]
+        k += 1
+        i += 1
+
+    while j < len2:
+        arr[k] = right[j]
+        k += 1
+        j += 1
+
+def timSort(arr):
+    n = len(arr)
+    minRun = calcMinRun(n)
+
+    for start in range(0, n, minRun):
+        end = min(start + minRun - 1, n - 1)
+        insertionSort(arr, start, end)
+
+    size = minRun
+    while size < n:
+        for left in range(0, n, 2 * size):
+
+            mid = min(n - 1, left + size - 1)
+            right = min((left + 2 * size - 1), (n - 1))
+
+            if mid < right:
+                merge(arr, left, mid, right)
+
+        size = 2 * size
+
+if __name__ == "__main__":
+
+    arr = [-2, 7, 15, -14, 0, 15, 0,
+           7, -7, -4, -13, 5, 8, -14, 12]
+
+    print("Given Array is")
+    print(arr)
+
+    timSort(arr)
+
+    print("After Sorting Array is")
+    print(arr)
+
+#Given Array is
+#-2  7  15  -14  0  15  0  7  -7  -4  -13  5  8  -14  12  
+#After Sorting Array is
+#-14  -14  -13  -7  -4  -2  0  0  5  7  7  8  12  15  15  
