173e249acf4fbb4829ffb3a21a551214265cef08
SiddhantTotade <siddhanttotade.1994@gmail.com>
2023-10-14 10:28:32 +0530

    docs : add comments in slack_message.py and test_fetch_github_info.py

--- web_programming/daily_horoscope.py
+++ web_programming/daily_horoscope.py
@@ -2,12 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 
 
+# Function to retrieve the daily horoscope for a zodiac sign and day.
 def horoscope(zodiac_sign: int, day: str) -> str:
     url = (
         "https://www.horoscope.com/us/horoscopes/general/"
         f"horoscope-general-daily-{day}.aspx?sign={zodiac_sign}"
     )
+
+    # Fetch and parse the HTML content of the webpage.
     soup = BeautifulSoup(requests.get(url).content, "html.parser")
+
+    # Extract and return the daily horoscope text from the parsed content.
     return soup.find("div", class_="main-horoscope").p.text
 
 
@@ -28,8 +33,12 @@ if __name__ == "__main__":
         "11. Aquarius\n",
         "12. Pisces\n",
     )
+
+    # Prompt the user to select a zodiac sign and day for the horoscope.
     zodiac_sign = int(input("number> ").strip())
     print("choose some day:\n", "yesterday\n", "today\n", "tomorrow\n")
     day = input("enter the day> ")
+
+    # Fetch and display the horoscope text.
     horoscope_text = horoscope(zodiac_sign, day)
     print(horoscope_text)
--- web_programming/test_fetch_github_info.py
+++ web_programming/test_fetch_github_info.py
@@ -5,7 +5,9 @@ import requests
 from .fetch_github_info import AUTHENTICATED_USER_ENDPOINT, fetch_github_info
 
 
+# Test function with mock response to test 'fetch_github_info'.
 def test_fetch_github_info(monkeypatch):
+    # Define a fake response class for testing purposes.
     class FakeResponse:
         def __init__(self, content) -> None:
             assert isinstance(content, (bytes, str))
@@ -14,6 +16,7 @@ def test_fetch_github_info(monkeypatch):
         def json(self):
             return json.loads(self.content)
 
+    # Define a mock response function for 'requests.get'.
     def mock_response(*args, **kwargs):
         assert args[0] == AUTHENTICATED_USER_ENDPOINT
         assert "Authorization" in kwargs["headers"]
@@ -21,7 +24,10 @@ def test_fetch_github_info(monkeypatch):
         assert "Accept" in kwargs["headers"]
         return FakeResponse(b'{"login":"test","id":1}')
 
+    # Use 'monkeypatch' to replace 'requests.get' with the mock response.
     monkeypatch.setattr(requests, "get", mock_response)
+
+    # Call 'fetch_github_info' with a token and assert results.
     result = fetch_github_info("token")
     assert result["login"] == "test"
     assert result["id"] == 1
