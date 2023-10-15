5ee16c91473455d606b2c254f10219d79496f1ca
grpathak22 <guruprasadpathak22@gmail.com>
2023-10-15 22:39:49 +0530

    added and fixed travelling salesman problem in dynamic_programming
    Signed-off-by: grpathak22 <guruprasadpathak22@gmail.com>

--- dynamic_programming/travelling_salesman.py
+++ dynamic_programming/travelling_salesman.py
@@ -1,13 +1,19 @@
 import numpy as np
 
-def travelling_salesman(city):
+"""
+Author: grpathak22
+Date: 15 October 2023
+Travelling Salesman Problem using Dynamic Programming
+"""
+def travelling_salesman(tsp_graph,city) -> None:
     """Solve the traveling salesman problem using dynamic programming.
 
   Args:
     distance_matrix: A square matrix of distances between cities.
-
+    city: Starting city which is initially 0(index)
+    
   Returns:
-    A list of city coordinates in the shortest possible tour.
+    None
   """
     global cost
     adjacent_vertex,min_val = 999,999
@@ -25,7 +31,7 @@ def travelling_salesman(city):
         print((adjacent_vertex + 1), end=" ")
         cost = cost + tsp_graph[city][adjacent_vertex]
         return
-    travelling_salesman(adjacent_vertex)
+    travelling_salesman(tsp_graph,adjacent_vertex)
 
 """
 1)Initialize a set of visited cities to be empty.
@@ -35,9 +41,11 @@ def travelling_salesman(city):
     Visit the closest unvisited city and add it to the set of visited cities.
     Set the current city to be the closest unvisited city.
 4)Return to the starting city.
-The code also keeps track of the total distance traveled. The total distance is calculated by adding up the distances between each pair of visited cities.
+The code also keeps track of the total distance traveled,
+The total distance is calculated by adding up the distances between each pair of visited cities.
 
-Once the code has finished executing, it will print the shortest possible path and the total distance of the path."""
+Once the code has finished executing, 
+it will print the shortest possible path and the total distance of the path."""
 
 cost = 0
 tsp_graph = np.array([
@@ -47,7 +55,8 @@ tsp_graph = np.array([
   [30, 35, 40, 0]
 ])
 num_cities = len(tsp_graph[0])
+city = 0
 visited = np.zeros(num_cities, dtype=int)
-print(f"Shortest Path:  \n", end=" ")
-travelling_salesman(0)
+print("Shortest Path:  \n", end=" ")
+travelling_salesman(tsp_graph,city)
 print(f"\nMinimum Cost: {cost} ", end=" ")
