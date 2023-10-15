574b82740f8bc1d009f046c859a5696751a18aa6
Khushi <139145505+Khushi-51@users.noreply.github.com>
2023-10-15 22:26:07 +0530

    Create subtracting elements of a matrix.py

---
+++ subtracting elements of a matrix.py
@@ -0,0 +1,19 @@
+
+# importing numpy as np
+import numpy as np
+ 
+ 
+# creating first matrix
+A = np.array([[1, 2], [3, 4]])
+ 
+# creating second matrix
+B = np.array([[4, 5], [6, 7]])
+ 
+print("Printing elements of first matrix")
+print(A)
+print("Printing elements of second matrix")
+print(B)
+ 
+# subtracting two matrix
+print("Subtraction of two matrix")
+print(np.subtract(A, B))
