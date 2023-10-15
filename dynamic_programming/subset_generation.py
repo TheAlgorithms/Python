9088b31e60de6f927be8e182d59fe0ce62cb1865
AasheeshLikePanner <ashishlearningsoftware@gmail.com>
2023-10-15 23:49:51 +0530

    Update stock_span_problem.py

--- dynamic_programming/subset_generation.py
+++ dynamic_programming/subset_generation.py
@@ -9,7 +9,7 @@ def subset_combinations_dp(elements: list, n: int) -> list:
     Returns:
         list: A list of tuples, each representing a combination of n elements.
         >>> subset_combinations_dp(elements=[10, 20, 30, 40], n=2)
-        [(10, 20), (10, 30), (20, 30), (10, 40), (20, 40), (30, 40)]
+        [(10, 20), (10, 30), (10, 40), (20, 30), (20, 40), (30, 40)]
         >>> subset_combinations_dp(elements=[1, 2, 3], n=1)
         [(1,), (2,), (3,)]
         >>> subset_combinations_dp(elements=[1, 2, 3], n=3)
@@ -21,13 +21,15 @@ def subset_combinations_dp(elements: list, n: int) -> list:
         >>> subset_combinations_dp(elements=[10, 20, 30, 40, 50], n=0)
         [()]
         >>> subset_combinations_dp(elements=[1, 2, 3, 4], n=2)
-        [(1, 2), (1, 3), (2, 3), (1, 4), (2, 4), (3, 4)]
+        [(1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 4)]
         >>> subset_combinations_dp(elements=[1, 'apple', 3.14], n=2)
         [(1, 'apple'), (1, 3.14), ('apple', 3.14)]
         >>> subset_combinations_dp(elements=['single'], n=0)
         [()]
     """
     r = len(elements)
+    if n > r:
+        return []
 
     dp: list[list[tuple]] = [[] for _ in range(r + 1)]
 
@@ -38,12 +40,27 @@ def subset_combinations_dp(elements: list, n: int) -> list:
             for prev_combination in dp[j - 1]:
                 dp[j].append(tuple(prev_combination) + (elements[i - 1],))
 
-    combinations = dp[n]
+    element_type = type(elements[0])
+    if all(isinstance(e, element_type) for e in elements):
+        combinations = sorted(dp[n])
+    else:
+        combinations = dp[n]
+
 
     return combinations
 
 
 if __name__ == "__main__":
+    from itertools import combinations
+
+    for items, n in (
+            ([10, 20, 30, 40], 2), ([1, 2, 3], 1), ([1, 2, 3], 3), ([42], 1),
+            ([6, 7, 8, 9], 4), ([10, 20, 30, 40, 50], 1), ([1, 2, 3, 4], 2),
+            ([1, 'apple', 3.14], 2), (['single'], 0), ([], 9)
+    ):
+        actual = subset_combinations_dp(items, n)
+        expected = list(combinations(items, n))
+        assert actual == expected, f"items, n: {actual} != {expected}"
     print(f"{subset_combinations_dp(elements=[10, 20, 30, 40], n=2) = }")
 
     import doctest
