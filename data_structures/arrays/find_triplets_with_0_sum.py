3c08bebe8f82cf1a869366249abbe86e2708e935
Sujal369 <130586852+Sujal369@users.noreply.github.com>
2023-10-15 12:25:06 +0530

    Thank you for giving me the chance to  contribute in open source
    This code finds unique triplets with a sum of 0 in the given list while preserving their original order. It also avoids duplicates and improves the time complexity to O(n^2), where n is the length of the input list.

--- data_structures/arrays/find_triplets_with_0_sum.py
+++ data_structures/arrays/find_triplets_with_0_sum.py
@@ -1,7 +1,3 @@
-from itertools import combinations
-
-
-def find_triplets_with_0_sum(nums: list[int]) -> list[list[int]]:
     """
     Given a list of integers, return elements a, b, c such that a + b + c = 0.
     Args:
@@ -18,7 +14,36 @@ def find_triplets_with_0_sum(nums: list[int]) -> list[list[int]]:
         >>> find_triplets_with_0_sum([1, 2, 3, 0, -1, -2, -3])
         [[-3, 0, 3], [-3, 1, 2], [-2, -1, 3], [-2, 0, 2], [-1, 0, 1]]
     """
-    return [
-        list(x)
-        for x in sorted({abc for abc in combinations(sorted(nums), 3) if not sum(abc)})
-    ]
+from itertools import combinations
+
+def find_triplets_with_0_sum(nums):
+    triplets = []
+    nums.sort()  # Sort the input list in ascending order
+    
+    for i in range(len(nums) - 2):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue  # Skip duplicates to avoid duplicate triplets
+        
+        left, right = i + 1, len(nums) - 1
+        
+        while left < right:
+            total = nums[i] + nums[left] + nums[right]
+            if total == 0:
+                triplets.append([nums[i], nums[left], nums[right]])
+                while left < right and nums[left] == nums[left + 1]:
+                    left += 1  # Skip duplicates on the left
+                while left < right and nums[right] == nums[right - 1]:
+                    right -= 1  # Skip duplicates on the right
+                left += 1
+                right -= 1
+            elif total < 0:
+                left += 1
+            else:
+                right -= 1
+    
+    return triplets
+
+# Example usage:
+result = find_triplets_with_0_sum([-1, 0, 1, 2, -1, -4])
+print(result)  # Output: [[-1, -1, 2], [-1, 0, 1]]
+
