5163d265509da8f1fedc1d47fab3e8497a73b611
Wei Jiang <weijiang9715@gmail.com>
2023-10-15 14:04:41 +0200

    add monotonic queue algorithm

--- data_structures/queue/monotonic_queue.py
+++ data_structures/queue/monotonic_queue.py
@@ -1,26 +1,26 @@
 from __future__ import annotations
 
-from double_ended_queue import Deque
+from .double_ended_queue import Deque
 
 arr = [1, 3, -1, -3, 5, 3, 6, 7]
-k = 3
+window_size = 3
 expect = [3, 3, 5, 5, 6, 7]
 
 
-def max_sliding_window(arr: list[float], k: int) -> list[float]:
+def max_sliding_window(arr: list[float], window_size: int) -> list[float]:
     """
     Given an array of integers nums, there is a sliding window of size k which is moving
     from the very left of the array to the very right.
-    Each time the sliding window of length k moves right by one position.
+    Each time the sliding window of length window_size moves right by one position.
     Return the max sliding window.
-    >>> max_sliding_window(arr, k) == expect
+    >>> max_sliding_window(arr, window_size) == expect
     True
     """
     max_val = []
     queue = Deque()
     for i in range(len(arr)):
         # pop the element if the index is outside the window size k
-        if queue and i - queue._front.val >= k:
+        if queue and i - queue._front.val >= window_size:
             queue.popleft()
         # keep the queue monotonically decreasing
         # so that the max value is always on the top
@@ -28,7 +28,7 @@ def max_sliding_window(arr: list[float], k: int) -> list[float]:
             queue.pop()
         queue.append(i)
         # the maximum value is the first element in queue
-        if i >= k - 1:
+        if i >= window_size - 1:
             max_val.append(arr[queue._front.val])
     return max_val
 
