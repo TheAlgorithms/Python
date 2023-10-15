2327d60b36463fde2be3f4f9498061d348075c09
Vinayak Mali <66154908+malivinayak@users.noreply.github.com>
2023-10-15 19:06:41 +0530

    Adding test

--- scheduling/fixed_priority_pre_emptive_scheduling.py
+++ scheduling/fixed_priority_pre_emptive_scheduling.py
@@ -1,25 +1,23 @@
 # Reference - https://en.wikipedia.org/wiki/Fixed-priority_pre-emptive_scheduling
-
+# https://en.wikipedia.org/wiki/Scheduling_(computing)
 from __future__ import annotations
 
 
-def calculate_waiting_times(
-    duration_times: list[int], priorities: list[int]
-) -> list[int]:
+def calculate_waiting_times(duration_times: list[int]) -> list[int]:
     """
     This function calculates the waiting time of some processes that have a
-    specified duration time and priority.
+    specified duration time.
         Return: The waiting time for each process.
+    >>> calculate_waiting_times([5, 10, 15])
+    [0, 5, 15]
+    >>> calculate_waiting_times([1, 2, 3, 4, 5])
+    [0, 1, 3, 6, 10]
+    >>> calculate_waiting_times([10, 3])
+    [0, 10]
     """
     waiting_times = [0] * len(duration_times)
-    sorted_processes = sorted(enumerate(duration_times), key=lambda x: priorities[x[0]])
-
-    for i in range(1, len(sorted_processes)):
-        process_id, duration_time = sorted_processes[i]
-        waiting_times[process_id] = (
-            waiting_times[sorted_processes[i - 1][0]] + sorted_processes[i - 1][1]
-        )
-
+    for i in range(1, len(duration_times)):
+        waiting_times[i] = duration_times[i - 1] + waiting_times[i - 1]
     return waiting_times
 
 
@@ -31,6 +29,12 @@ def calculate_turnaround_times(
         Return: The time difference between the completion time and the
                 arrival time.
                 Practically waiting_time + duration_time
+    >>> calculate_turnaround_times([5, 10, 15], [0, 5, 15])
+    [5, 15, 30]
+    >>> calculate_turnaround_times([1, 2, 3, 4, 5], [0, 1, 3, 6, 10])
+    [1, 3, 6, 10, 15]
+    >>> calculate_turnaround_times([10, 3], [0, 10])
+    [10, 13]
     """
     return [
         duration_time + waiting_times[i]
@@ -42,6 +46,12 @@ def calculate_average_turnaround_time(turnaround_times: list[int]) -> float:
     """
     This function calculates the average of the turnaround times
         Return: The average of the turnaround times.
+    >>> calculate_average_turnaround_time([0, 5, 16])
+    7.0
+    >>> calculate_average_turnaround_time([1, 5, 8, 12])
+    6.5
+    >>> calculate_average_turnaround_time([10, 24])
+    17.0
     """
     return sum(turnaround_times) / len(turnaround_times)
 
@@ -50,6 +60,12 @@ def calculate_average_waiting_time(waiting_times: list[int]) -> float:
     """
     This function calculates the average of the waiting times
         Return: The average of the waiting times.
+    >>> calculate_average_waiting_time([0, 5, 16])
+    7.0
+    >>> calculate_average_waiting_time([1, 5, 8, 12])
+    6.5
+    >>> calculate_average_waiting_time([10, 24])
+    17.0
     """
     return sum(waiting_times) / len(waiting_times)
 
@@ -63,17 +79,16 @@ if __name__ == "__main__":
         print("Zero amount of processes")
         raise SystemExit(0)
 
-    # duration time and priority of all processes
+    # duration time of all processes
     duration_times = [19, 8, 9]
-    priorities = [2, 1, 3]
 
-    # ensure we can match each id to a duration time and priority
-    if len(duration_times) != len(processes) or len(priorities) != len(processes):
-        print("Unable to match all id's with their duration time or priority")
+    # ensure we can match each id to a duration time
+    if len(duration_times) != len(processes):
+        print("Unable to match all id's with their duration time")
         raise SystemExit(0)
 
     # get the waiting times and the turnaround times
-    waiting_times = calculate_waiting_times(duration_times, priorities)
+    waiting_times = calculate_waiting_times(duration_times)
     turnaround_times = calculate_turnaround_times(duration_times, waiting_times)
 
     # get the average times
