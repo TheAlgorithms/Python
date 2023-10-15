01be1ead8ddb1a26059513050033e5fdbd8b4204
hollowcrust <72879387+hollowcrust@users.noreply.github.com>
2023-10-16 00:38:03 +0800

    Fix typo

--- data_structures/arrays/sparse_table.py
+++ data_structures/arrays/sparse_table.py
@@ -1,9 +1,9 @@
 """
     Sparse table is a data structure that allows answering range queries on
-    a static number_listay, i.e. the elements do not change throughout all the queries.
+    a static number list, i.e. the elements do not change throughout all the queries.
 
     The implementation below will solve the problem of Range Minimum Query:
-    Finding the minimum value of a subset [L..R] of a static number_listay.
+    Finding the minimum value of a subset [L..R] of a static number list.
 
     Overall time complexity: O(nlogn)
     Overall space complexity: O(nlogn)
