a6f5f73f37cc527f5d18530b918f4e2d2fe737b6
SiddhantTotade <siddhanttotade.1994@gmail.com>
2023-10-15 10:25:08 +0530

    docs : add comments

--- web_programming/test_fetch_github_info.py
+++ web_programming/test_fetch_github_info.py
@@ -5,6 +5,7 @@ import requests
 from .fetch_github_info import AUTHENTICATED_USER_ENDPOINT, fetch_github_info
 
 
+# Define a test case to fetch and validate GitHub user information.
 def test_fetch_github_info(monkeypatch):
     class FakeResponse:
         def __init__(self, content) -> None:
@@ -14,6 +15,7 @@ def test_fetch_github_info(monkeypatch):
         def json(self):
             return json.loads(self.content)
 
+    # Mock the response from a GitHub API request with a fake response.
     def mock_response(*args, **kwargs):
         assert args[0] == AUTHENTICATED_USER_ENDPOINT
         assert "Authorization" in kwargs["headers"]
@@ -22,6 +24,10 @@ def test_fetch_github_info(monkeypatch):
         return FakeResponse(b'{"login":"test","id":1}')
 
     monkeypatch.setattr(requests, "get", mock_response)
+
+    # Call the function to fetch GitHub information with a mock response.
     result = fetch_github_info("token")
+
+    # Assert that the fetched data matches the expected values.
     assert result["login"] == "test"
     assert result["id"] == 1
