78c0f81968e3e690ba2de8a1f6cdf8b79e1ee26f
Christian Clauss <cclauss@me.com>
2023-10-15 22:09:53 +0200

    Update sparse_table.py

--- data_structures/arrays/sparse_table.py
+++ data_structures/arrays/sparse_table.py
@@ -10,6 +10,7 @@
 
     Wikipedia link: https://en.wikipedia.org/wiki/Range_minimum_query
 """
+from math import log2
 
 
 def build_sparse_table(number_list: list[int]) -> list[list[int]]:
@@ -26,27 +27,20 @@ def build_sparse_table(number_list: list[int]) -> list[list[int]]:
     ...
     ValueError: empty number list not allowed
     """
-    import math
-
-    if number_list == []:
+    if not number_list:
         raise ValueError("empty number list not allowed")
 
     length = len(number_list)
-    """
-    Initialise sparse_table
-
-    sparse_table[j][i] represents the minimum value of
-    the subset of length (2 ** j) of number_list, starting from index i.
-    """
+    # Initialise sparse_table -- sparse_table[j][i] represents the minimum value of the
+    # subset of length (2 ** j) of number_list, starting from index i.
 
     # smallest power of 2 subset length that fully covers number_list
-    row = int(math.log2(length)) + 1
+    row = int(log2(length)) + 1
     sparse_table = [[0 for i in range(length)] for j in range(row)]
 
     # minimum of subset of length 1 is that value itself
     for i, value in enumerate(number_list):
         sparse_table[0][i] = value
-
     j = 1
 
     # compute the minimum value for all intervals with size (2 ** j)
@@ -58,11 +52,8 @@ def build_sparse_table(number_list: list[int]) -> list[list[int]]:
             sparse_table[j][i] = min(
                 sparse_table[j - 1][i + (1 << (j - 1))], sparse_table[j - 1][i]
             )
-
             i += 1
-
         j += 1
-
     return sparse_table
 
 
@@ -72,33 +63,27 @@ def query(sparse_table: list[list[int]], left_bound: int, right_bound: int) -> i
     0
     >>> query(build_sparse_table([8, 1, 0, 3, 4, 9, 3]), 4, 6)
     3
-    >>> query(build_sparse_table([8, 1, 0, 3, 4, 9, 3]), 0, 11)
-    Traceback (most recent call last):
-    ...
-    IndexError: list index out of range
-
     >>> query(build_sparse_table([3, 1, 9]), 2, 2)
     9
     >>> query(build_sparse_table([3, 1, 9]), 0, 1)
     1
-
+    >>> query(build_sparse_table([8, 1, 0, 3, 4, 9, 3]), 0, 11)
+    Traceback (most recent call last):
+    ...
+    IndexError: list index out of range
     >>> query(build_sparse_table([]), 0, 0)
     Traceback (most recent call last):
     ...
     ValueError: empty number list not allowed
     """
-    import math
-
     if left_bound < 0 or right_bound >= len(sparse_table[0]):
         raise IndexError("list index out of range")
 
     # highest subset length of power of 2 that is within range [left_bound, right_bound]
-    j = int(math.log2(right_bound - left_bound + 1))
+    j = int(log2(right_bound - left_bound + 1))
 
-    """
-    minimum of 2 overlapping smaller subsets: [left_bound, left_bound + 2 ** j - 1]
-    and [right_bound - 2 ** j + 1, right_bound]
-    """
+    # minimum of 2 overlapping smaller subsets:
+    # [left_bound, left_bound + 2 ** j - 1] and [right_bound - 2 ** j + 1, right_bound]
     return min(sparse_table[j][right_bound - (1 << j) + 1], sparse_table[j][left_bound])
 
 
@@ -106,3 +91,4 @@ if __name__ == "__main__":
     from doctest import testmod
 
     testmod()
+    print(f"{query(build_sparse_table([3, 1, 9]), 2, 2) = }")
