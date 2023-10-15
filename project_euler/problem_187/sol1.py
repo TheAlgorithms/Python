4dff7f0c6892e1c9531e542bfa78cbc616118f44
Manpreet Singh <63737630+ManpreetSingh2004@users.noreply.github.com>
2023-10-15 14:22:09 +0530

    Separate while_solution and new solution

--- project_euler/problem_187/sol1.py
+++ project_euler/problem_187/sol1.py
@@ -85,6 +85,29 @@ def slow_solution(max_number: int = 10**8) -> int:
     return semiprimes_count
 
 
+def while_solution(max_number: int = 10**8) -> int:
+    """
+    Returns the number of composite integers below max_number have precisely two,
+    not necessarily distinct, prime factors.
+
+    >>> while_solution(30)
+    10
+    """
+
+    prime_numbers = calculate_prime_numbers(max_number // 2)
+
+    semiprimes_count = 0
+    left = 0
+    right = len(prime_numbers) - 1
+    while left <= right:
+        while prime_numbers[left] * prime_numbers[right] >= max_number:
+            right -= 1
+        semiprimes_count += right - left + 1
+        left += 1
+
+    return semiprimes_count
+
+
 def solution(max_number: int = 10**8) -> int:
     """
     Returns the number of composite integers below max_number have precisely two,
