58856c9c584c6c9a14d24ff37914d13c3c64486b
kosuri-indu <kosurilindu@gmail.com>
2023-10-15 11:14:43 +0530

    changes as recommended

--- greedy_methods/job_sequence.py
+++ greedy_methods/job_sequence.py
@@ -13,43 +13,38 @@ which is by scheduling the tasks with rewards 40, 20
 This problem can be solved using the concept of "GREEDY ALGORITHM".
 Time Complexity - O(n log n)
 
+URL : https://www.google.com/imgres?imgurl=https%3A%2F%2Fmiro.medium.com%2Fv2%2Fresize%3Afit%3A648%2F1*fCm4EwGIm_KFvHrniNR_yw.jpeg&tbnid=YzfEiVyu7FIWQM&vet=12ahUKEwjFi5i2rPeBAxV7a2wGHbghCd8QMygCegQIARBh..i&imgrefurl=https%3A%2F%2Fmedium.com%2F%40nihardudhat2000%2Fjob-sequencing-with-deadline-17ddbb5890b5&docid=wcDisrtMy41SsM&w=648&h=328&q=job%20sequence&client=firefox-b-d&ved=2ahUKEwjFi5i2rPeBAxV7a2wGHbghCd8QMygCegQIARBh
+
 We iterate over the tasks array once, sorting it in descending order of reward.
 Then we iterate over the sorted tasks array once more, scheduling each
 task if its deadline is greater than the current time.The greedy choice at
 each point is to either schedule the current task if its deadline is greater
 than the current time, or skip it otherwise.
 """
-
 from typing import Any
+from dataclasses import dataclass
 
-
+@dataclass
 class Task:
-    def __init__(self, task_id: Any, deadline: int, reward: int) -> None:
-        self.task_id = task_id
-        self.deadline = deadline
-        self.reward = reward
+    task_id: Any
+    deadline: int
+    reward: int
 
-
-def max_tasks(tasks_info: list[tuple[int]]) -> int:
+def max_tasks(tasks_info: list[tuple[int]]) -> list:
     """
     >>> max_tasks([(4, 20), (1, 10), (1, 40), (1, 30)])
-    (2, [2, 0])
+    [2, 0]
     >>> max_tasks([(1, 10), (2, 20), (3, 30), (2, 40)])
-    (2, [3, 2])
+    [3, 2]
+
     """
     tasks = [Task(i, d, p) for i, (d, p) in enumerate(tasks_info)]
 
     tasks.sort(key=lambda task: task.reward, reverse=True)
 
-    schedule = []
-    current_time = 0
+    schedule = [task.task_id for current_time, task in enumerate(tasks, start=1) if task.deadline >= current_time]
 
-    for task in tasks:
-        if task.deadline > current_time:
-            schedule.append(task.task_id)
-            current_time += 1
-
-    return len(schedule), schedule
+    return schedule
 
 
 if __name__ == "__main__":
