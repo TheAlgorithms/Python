95f2fb1f5f3e18c5e5aec3ae7541b178894bef6f
Aroson <74296409+Aroson1@users.noreply.github.com>
2023-10-15 16:13:15 +0530

    Update ic_555_timer.py

--- electronics/ic_555_timer.py
+++ electronics/ic_555_timer.py
@@ -3,9 +3,9 @@ from __future__ import annotations
 
 """
     This function can calculate the frequency and duty cycle of an astable 555 timer
-    The function takes in the value of the external resistances (in OHMS) and
+    The function takes in the value of the external resistances (in OHMS) and 
     capacitance (in microFARADS), and calculates the following:
-
+    
     -------------------------------------
     | Freq = 1.44 /[( R1+ 2 x R2) x C1] |               ... in Hz
     -------------------------------------
@@ -19,24 +19,24 @@ from __future__ import annotations
     ------------------------------------------------
     where R1 is the first resisitance,
           R2 is the second resistance,
-
+          
 """
 
 
-def astable_mode(
+def astable_frequency(
     resistance_1: float, resistance_2: float, capacitance: float
-) -> dict[str:float, str:float]:
+) -> float:
     """
     Usage examples:
-    >>> astable_mode(resistance_1=45, resistance_2=45, capacitance=7)
-    {'Frequency': 1523.8095238095239, 'Duty_Cycle': 66.66666666666666}
-    >>> astable_mode(resistance_1=356, resistance_2=234, capacitance=976)
-    {'Frequency': 1.7905459175553078, 'Duty_Cycle': 71.60194174757282}
-    >>> astable_mode(resistance_1=2, resistance_2=-1, capacitance=2)
+    >>> astable_frequency(resistance_1=45, resistance_2=45, capacitance=7)
+    1523.8095238095239
+    >>> astable_frequency(resistance_1=356, resistance_2=234, capacitance=976)
+    1.7905459175553078
+    >>> astable_frequency(resistance_1=2, resistance_2=-1, capacitance=2)
     Traceback (most recent call last):
         ...
     ValueError: All values must be positive
-    >>> astable_mode(resistance_1=0, resistance_2=0, capacitance=2)
+    >>> astable_frequency(resistance_1=0, resistance_2=0, capacitance=2)
     Traceback (most recent call last):
         ...
     ValueError: All values must be positive
@@ -46,11 +46,33 @@ def astable_mode(
         raise ValueError("All values must be positive")
     else:
         frequency = (1.44 / ((resistance_1 + 2 * resistance_2) * capacitance)) * 10**6
+        return frequency
 
+
+def astable_duty_cycle(resistance_1: float, resistance_2: float) -> float:
+    """
+    Usage examples:
+    >>> astable_duty_cycle(resistance_1=45, resistance_2=45)
+    66.66666666666666
+    >>> astable_duty_cycle(resistance_1=356, resistance_2=234)
+    71.60194174757282
+    >>> astable_duty_cycle(resistance_1=2, resistance_2=-1)
+    Traceback (most recent call last):
+        ...
+    ValueError: All values must be positive
+    >>> astable_duty_cycle(resistance_1=0, resistance_2=0)
+    Traceback (most recent call last):
+        ...
+    ValueError: All values must be positive
+    """
+
+    if resistance_1 <= 0 or resistance_2 <= 0:
+        raise ValueError("All values must be positive")
+    else:
         duty_cycle = (
             (resistance_1 + resistance_2) / (resistance_1 + 2 * resistance_2) * 100
         )
-        return {"Frequency": frequency, "Duty_Cycle": duty_cycle}
+        return duty_cycle
 
 
 if __name__ == "__main__":
