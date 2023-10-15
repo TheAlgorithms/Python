afd8d566495bf33bf221cb7583d965ffd1ed8a96
Christian Clauss <cclauss@me.com>
2023-10-15 21:50:24 +0200

    Update subset_generation.py

--- dynamic_programming/subset_generation.py
+++ dynamic_programming/subset_generation.py
@@ -1,7 +1,4 @@
-# return all subset combinations of n element in given set of r element.
-
-
-def subset_combinations_dp(elements: list[int], n: int) -> list[tuple[int, ...]]:
+def subset_combinations(elements: list[int], n: int) -> list:
     """
     Compute n-element combinations from a given list using dynamic programming.
     Args:
@@ -9,22 +6,33 @@ def subset_combinations_dp(elements: list[int], n: int) -> list[tuple[int, ...]]
         n (int): The number of elements in each combination.
     Returns:
         list: A list of tuples, each representing a combination of n elements.
-        >>> subset_combinations_dp(elements=[10, 20, 30, 40], n=2)
+        >>> subset_combinations(elements=[10, 20, 30, 40], n=2)
         [(10, 20), (10, 30), (10, 40), (20, 30), (20, 40), (30, 40)]
-        >>> subset_combinations_dp(elements=[1, 2, 3], n=1)
+        >>> subset_combinations(elements=[1, 2, 3], n=1)
         [(1,), (2,), (3,)]
-        >>> subset_combinations_dp(elements=[1, 2, 3], n=3)
+        >>> subset_combinations(elements=[1, 2, 3], n=3)
         [(1, 2, 3)]
-        >>> subset_combinations_dp(elements=[42], n=1)
+        >>> subset_combinations(elements=[42], n=1)
         [(42,)]
-        >>> subset_combinations_dp(elements=[6, 7, 8, 9], n=4)
+        >>> subset_combinations(elements=[6, 7, 8, 9], n=4)
         [(6, 7, 8, 9)]
-        >>> subset_combinations_dp(elements=[10, 20, 30, 40, 50], n=0)
+        >>> subset_combinations(elements=[10, 20, 30, 40, 50], n=0)
         [()]
-        >>> subset_combinations_dp(elements=[1, 2, 3, 4], n=2)
+        >>> subset_combinations(elements=[1, 2, 3, 4], n=2)
         [(1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 4)]
-        >>> subset_combinations_dp(elements=[1, 'apple', 3.14], n=2)
+        >>> subset_combinations(elements=[1, 'apple', 3.14], n=2)
         [(1, 'apple'), (1, 3.14), ('apple', 3.14)]
+        >>> subset_combinations(elements=['single'], n=0)
+        [()]
+        >>> subset_combinations(elements=[], n=9)
+        []
+        >>> from itertools import combinations
+        >>> all(subset_combinations(items, n) == list(combinations(items, n))
+        ...     for items, n in (
+        ...         ([10, 20, 30, 40], 2), ([1, 2, 3], 1), ([1, 2, 3], 3), ([42], 1),
+        ...         ([6, 7, 8, 9], 4), ([10, 20, 30, 40, 50], 1), ([1, 2, 3, 4], 2),
+        ...         ([1, 'apple', 3.14], 2), (['single'], 0), ([], 9)))
+        True
     """
     r = len(elements)
     if n > r:
@@ -39,18 +47,14 @@ def subset_combinations_dp(elements: list[int], n: int) -> list[tuple[int, ...]]
             for prev_combination in dp[j - 1]:
                 dp[j].append(tuple(prev_combination) + (elements[i - 1],))
 
-    element_type = type(elements[0])
-    if all(isinstance(e, element_type) for e in elements):
-        combinations = sorted(dp[n])
-    else:
-        combinations = dp[n]
-
-    return combinations
+    try:
+        return sorted(dp[n])
+    except TypeError:
+        return dp[n]
 
 
 if __name__ == "__main__":
-    print(f"{subset_combinations_dp(elements=[10, 20, 30, 40], n=2) = }")
+    from doctest import testmod
 
-    import doctest
-
-    doctest.testmod()
+    testmod()
+    print(f"{subset_combinations(elements=[10, 20, 30, 40], n=2) = }")
