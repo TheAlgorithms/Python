73e90bff1b75d3d81e6db95d6f8377d3fce0497e
Aditya-K-Pathak <90629430+Aditya-K-Pathak@users.noreply.github.com>
2023-10-15 10:59:58 +0530

    Create sort_array_of_0s_1s_2s.py
    Added a new algorithm to sort array of 0s, 1s and 2s.

---
+++ data_structures/arrays/sort_array_of_0s_1s_2s.py
@@ -0,0 +1,28 @@
+"""
+Author: Aditya Pathak
+Date: 15th Oct 2023
+Given an array of integers consisting of 0s, 1s and 2s only, we need to 
+return the sorted array in O(n) complexity i.e., without actually sorting.
+
+https://practice.geeksforgeeks.org/problems/sort-an-array-o f-0s-1s-and-2s4231/
+"""
+
+def sort012(arr: list[int]) -> list[int]:
+    """
+    args: arr -> An array of integer
+    return: list -> an Array of integer
+
+    >>> sort012([1, 2, 0, 0, 2])
+    [0, 0, 1, 2, 2]
+    >>> sort012([0, 0, 2])
+    [0, 0, 2]
+    >>> sort012([])
+    []
+    """
+    return arr.count(0) * [0] + arr.count(1) * [1] + arr.count(2) * [2]
+
+
+if __name__ == "__main__":
+    from doctest import testmod
+
+    testmod()
