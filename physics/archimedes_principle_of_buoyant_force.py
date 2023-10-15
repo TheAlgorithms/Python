802a381223132b3789f07d213dcf003e105c95b0
Christian Clauss <cclauss@me.com>
2023-10-15 19:05:09 +0200

    Update and rename archimedes_principle.py to archimedes_principle_of_buoyant_force.py

--- physics/archimedes_principle_of_buoyant_force.py
+++ physics/archimedes_principle_of_buoyant_force.py
@@ -1,17 +1,16 @@
 """
-Calculates buoyant force on object submerged within static fluid.
-Discovered by greek mathematician, Archimedes. The principle is named after him.
+Calculate the buoyant force of any body completely or partially submerged in a static
+fluid.  This principle was discovered by the Greek mathematician Archimedes.
 
 Equation for calculating buoyant force:
 Fb = ρ * V * g
 
-Source:
-- https://en.wikipedia.org/wiki/Archimedes%27_principle
+https://en.wikipedia.org/wiki/Archimedes%27_principle
 """
 
 
 # Acceleration Constant on Earth (unit m/s^2)
-g = 9.80665
+g = 9.80665  # Also available in scipy.constants.g
 
 
 def archimedes_principle(
@@ -20,12 +19,14 @@ def archimedes_principle(
     """
     Args:
         fluid_density: density of fluid (kg/m^3)
-        volume: volume of object / liquid being displaced by object
+        volume: volume of object / liquid being displaced by object (m^3)
         gravity: Acceleration from gravity. Gravitational force on system,
             Default is Earth Gravity
     returns:
         buoyant force on object in Newtons
 
+    >>> archimedes_principle(fluid_density=500, volume=4, gravity=9.8)
+    19600.0
     >>> archimedes_principle(fluid_density=997, volume=0.5, gravity=9.8)
     4885.3
     >>> archimedes_principle(fluid_density=997, volume=0.7)
@@ -49,9 +50,9 @@ def archimedes_principle(
     if fluid_density <= 0:
         raise ValueError("Impossible fluid density")
     if volume <= 0:
-        raise ValueError("Impossible Object volume")
+        raise ValueError("Impossible object volume")
     if gravity < 0:
-        raise ValueError("Impossible Gravity")
+        raise ValueError("Impossible gravity")
 
     return fluid_density * gravity * volume
 
@@ -59,5 +60,4 @@ def archimedes_principle(
 if __name__ == "__main__":
     import doctest
 
-    # run doctest
     doctest.testmod()
