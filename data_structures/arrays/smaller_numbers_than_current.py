9ab19b4c4a12f0f13c877e11759c1f13d1f52b7d
Sathvik <sathvikng56@gmail.com>
2023-10-15 20:45:03 +0530

    Added Finding Smaller Numbers than Current Numbers following python conventions

--- data_structures/arrays/smaller_numbers_than_current.py
+++ data_structures/arrays/smaller_numbers_than_current.py
@@ -1,7 +1,7 @@
 from typing import List
 
-class Solution:
-    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+class smallerNumbersThanCurrent:
+    def smaller_numbers_than_current(self, nums: List[int]) -> List[int]:
         """
         Count the number of elements smaller than each element in the input list.
 
@@ -12,12 +12,12 @@ class Solution:
             List[int]: A list containing the count of elements smaller than each element in the input list.
 
         Examples:
-            >>> solution = Solution()
-            >>> solution.smallerNumbersThanCurrent([8, 1, 2, 2, 3])
+            >>> solution = smallerNumbersThanCurrent()
+            >>> solution.smaller_numbers_than_current([8, 1, 2, 2, 3])
             [4, 0, 1, 1, 3]
-            >>> solution.smallerNumbersThanCurrent([6, 5, 4, 8])
+            >>> solution.smaller_numbers_than_current([6, 5, 4, 8])
             [2, 1, 0, 3]
-            >>> solution.smallerNumbersThanCurrent([7, 7, 7, 7])
+            >>> solution.smaller_numbers_than_current([7, 7, 7, 7])
             [0, 0, 0, 0]
         """
         count = [0] * 101
