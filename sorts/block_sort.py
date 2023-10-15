4503729bae16eb5fc7d607f94cd43b7ad1a2447b
Pavan Sriram Kannuri <86733057+SportingDot1024@users.noreply.github.com>
2023-10-15 13:27:13 +0530

    Update block_sort.py

--- sorts/block_sort.py
+++ sorts/block_sort.py
@@ -1,34 +1,4 @@
-def block_sort(lst: list) -> list:
-    """
-    Sorts a list using the Block Sort algorithm.
-
-    The Block Sort algorithm works by dividing the input list into blocks of size
-    sqrt(n), sorting each block, and then merging the sorted blocks into a single
-    sorted list.
-
-    Args:
-        lst (list[int]): The unsorted list to be sorted.
-
-    Returns:
-        list[int]: The sorted list.
-
-    Examples:
-        >>> block_sort([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5])
-        [1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9]
-        >>> block_sort([5, 4, 3, 2, 1])
-        [1, 2, 3, 4, 5]
-        >>> block_sort([])
-        []
-    """
-    if not lst:
-        return []
-
-    block_size = int(len(lst) ** 0.5)
-    blocks = [lst[i : i + block_size] for i in range(0, len(lst), block_size)]
-    for block in blocks:
-        block.sort()
-
-    def block_sort(lst: List[int]) -> List[int]:
+def block_sort(lst: List[int]) -> List[int]:
     """
     Sorts a list using the Block Sort algorithm.
 
@@ -64,10 +34,10 @@ def block_sort(lst: list) -> list:
     while blocks:
         # Find the block with the minimum first element.
         min_block = min(blocks, key=lambda block: block[0])
-
+    
         # Append the minimum element to the sorted result.
         sorted_result.append(min_block.pop(0))
-
+    
         # If the block is empty after the pop operation, remove it from the list of blocks.
         if not min_block:
             blocks.remove(min_block)
@@ -76,12 +46,4 @@ def block_sort(lst: list) -> list:
 
 if __name__ == "__main__":
     import doctest
-    doctest.testmod()
-
-    return sorted_lst
-
-
-if __name__ == "__main__":
-    import doctest
-
     doctest.testmod()
