ac76e17035fa3cc42c9163b27ffb0d87fc492a8c
Christian Clauss <cclauss@me.com>
2023-10-16 18:39:27 +0200

    Update diameter_of_binary_tree.py

--- data_structures/binary_tree/diameter_of_binary_tree.py
+++ data_structures/binary_tree/diameter_of_binary_tree.py
@@ -1,74 +1,73 @@
 """
-The diameter/width of a tree is defined as the number of nodes on the longest path between two end nodes.
+The diameter/width of a tree is defined as the number of nodes on the longest path
+between two end nodes.
 """
+from __future__ import annotations
 
-# A binary tree Node
+from dataclasses import dataclass
 
 
+@dataclass
 class Node:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
+    data: int
+    left: Node | None = None
+    right: Node | None = None
 
+    def depth(self) -> int:
+        """
+        >>> root = Node(1)
+        >>> root.depth()
+        1
+        >>> root.left = Node(2)
+        >>> root.depth()
+        2
+        >>> root.left.depth()
+        1
+        >>> root.right = Node(3)
+        >>> root.depth()
+        2
+        """
+        left_depth = self.left.depth() if self.left else 0
+        right_depth = self.right.depth() if self.right else 0
+        return max(left_depth, right_depth) + 1
 
-# utility class to pass height object
-class Height:
-    def __init__(self):
-        self.h = 0
+    def diameter(self) -> int:
+        """
+        >>> root = Node(1)
+        >>> root.diameter()
+        1
+        >>> root.left = Node(2)
+        >>> root.diameter()
+        2
+        >>> root.left.diameter()
+        1
+        >>> root.right = Node(3)
+        >>> root.diameter()
+        3
+        """
+        left_depth = self.left.depth() if self.left else 0
+        right_depth = self.right.depth() if self.right else 0
+        return left_depth + right_depth + 1
 
-
-# Function to calculate diameter of the given binary tree
-def calculate_diameter(root: Node, height: int) -> int:
-    # to store height of left and right subtree
-    lh = Height()
-    rh = Height()
-
-    # base condition- when binary tree is empty
-    if root is None:
-        height.h = 0
-        return 0
-
-    # ldiameter --> diameter of left subtree
-    # rdiameter --> diameter of right subtree
-
-    # height of left subtree and right subtree is obtained from lh and rh and returned value of function is stored in ldiameter and rdiameter
-
-    ldiameter = calculate_diameter(root.left, lh)
-    rdiameter = calculate_diameter(root.right, rh)
-
-    # height of tree will be max of left subtree
-    # height and right subtree height plus1
-
-    height.h = max(lh.h, rh.h) + 1
-
-    # return maximum of the following
-    # 1)left diameter
-    # 2)right diameter
-    # 3)left height + right height + 1
-    return max(lh.h + rh.h + 1, max(ldiameter, rdiameter))
-
-
-def diameter(root: Node) -> int:
-    height = Height()
-    return calculate_diameter(root, height)
 
 
 if __name__ == "__main__":
+    from doctest import testmod
+
+    testmod()
     root = Node(1)
     root.left = Node(2)
     root.right = Node(3)
     root.left.left = Node(4)
     root.left.right = Node(5)
-
-    """
-Constructed binary tree is
+    r"""
+    Constructed binary tree is
 		1
-	   / \
-	  2	  3
-	 / \
+       / \
+      2	  3
+     / \
 	4	 5
-"""
-
-    print("The diameter of the binary tree is:", end=" ")
-    print(diameter(root))
+    """
+    print(f"{root.diameter() = }")
+    print(f"{root.left.diameter() = }")
+    print(f"{root.right.diameter() = }")
