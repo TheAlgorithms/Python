2537edf82f7263eb421f8aed34b1f9a506ed671f
Tanishka Garg <115347881+gargtanishka895@users.noreply.github.com>
2023-10-16 11:38:27 +0530

    Update huffman.py
    This code follows PEP 8 style guidelines, uses Counter for counting frequencies, and is more concise and maintainable. It should also run faster because it compresses the bitstrings into a more compact form.

--- compression/huffman.py
+++ compression/huffman.py
@@ -1,46 +1,29 @@
-from __future__ import annotations
-
+from collections import Counter
 import sys
-
 
 class Letter:
     def __init__(self, letter: str, freq: int):
         self.letter: str = letter
         self.freq: int = freq
-        self.bitstring: dict[str, str] = {}
+        self.bitstring: str = ""
 
     def __repr__(self) -> str:
         return f"{self.letter}:{self.freq}"
 
-
 class TreeNode:
-    def __init__(self, freq: int, left: Letter | TreeNode, right: Letter | TreeNode):
+    def __init__(self, freq: int, left: Letter | 'TreeNode', right: Letter | 'TreeNode'):
         self.freq: int = freq
-        self.left: Letter | TreeNode = left
-        self.right: Letter | TreeNode = right
-
+        self.left: Letter | 'TreeNode' = left
+        self.right: Letter | 'TreeNode' = right
 
 def parse_file(file_path: str) -> list[Letter]:
-    """
-    Read the file and build a dict of all letters and their
-    frequencies, then convert the dict into a list of Letters.
-    """
-    chars: dict[str, int] = {}
     with open(file_path) as f:
-        while True:
-            c = f.read(1)
-            if not c:
-                break
-            chars[c] = chars[c] + 1 if c in chars else 1
-    return sorted((Letter(c, f) for c, f in chars.items()), key=lambda x: x.freq)
-
+        text = f.read()
+        char_freqs = Counter(text)
+    return [Letter(c, f) for c, f in char_freqs.items()]
 
 def build_tree(letters: list[Letter]) -> Letter | TreeNode:
-    """
-    Run through the list of Letters and build the min heap
-    for the Huffman Tree.
-    """
-    response: list[Letter | TreeNode] = letters  # type: ignore
+    response = letters
     while len(response) > 1:
         left = response.pop(0)
         right = response.pop(0)
@@ -50,43 +33,30 @@ def build_tree(letters: list[Letter]) -> Letter | TreeNode:
         response.sort(key=lambda x: x.freq)
     return response[0]
 
-
 def traverse_tree(root: Letter | TreeNode, bitstring: str) -> list[Letter]:
-    """
-    Recursively traverse the Huffman Tree to set each
-    Letter's bitstring dictionary, and return the list of Letters
-    """
     if isinstance(root, Letter):
-        root.bitstring[root.letter] = bitstring
+        root.bitstring = bitstring
         return [root]
-    treenode: TreeNode = root  # type: ignore
+    treenode = root
     letters = []
     letters += traverse_tree(treenode.left, bitstring + "0")
     letters += traverse_tree(treenode.right, bitstring + "1")
     return letters
 
-
 def huffman(file_path: str) -> None:
-    """
-    Parse the file, build the tree, then run through the file
-    again, using the letters dictionary to find and print out the
-    bitstring for each letter.
-    """
     letters_list = parse_file(file_path)
     root = build_tree(letters_list)
-    letters = {
-        k: v for letter in traverse_tree(root, "") for k, v in letter.bitstring.items()
-    }
-    print(f"Huffman Coding  of {file_path}: ")
+    letters = {k: v for letter in traverse_tree(root, "") for k, v in letter.bitstring.items()}
+    print(f"Huffman Coding of {file_path}:")
+    
     with open(file_path) as f:
-        while True:
-            c = f.read(1)
-            if not c:
-                break
-            print(letters[c], end=" ")
+        text = f.read()
+        encoded_text = ''.join(letters[c] for c in text)
+        print(encoded_text)
     print()
 
-
 if __name__ == "__main__":
-    # pass the file path to the huffman function
+    if len(sys.argv) != 2:
+        print("Usage: python huffman.py <file_path>")
+        sys.exit(1)
     huffman(sys.argv[1])
