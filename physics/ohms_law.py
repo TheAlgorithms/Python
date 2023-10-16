5573bb8970b1595a2577ca1a81d0fe3019175734
Anjali Rajendran <84264801+AnjaliRaj015@users.noreply.github.com>
2023-10-16 17:55:08 +0530

    edited

--- physics/ohms_law.py
+++ physics/ohms_law.py
@@ -1,9 +1,9 @@
 """
 Description :
-Calculate one of the parameters (current, voltage, or resistance)
+Calculate one of the parameters (current, voltage, or resistance) 
 in an electrical circuit based on Ohm's law.
-Ohm's law(Ω) states that the current through a
-conductor between two points is directly
+Ohm's law(Ω) states that the current through a 
+conductor between two points is directly 
 proportional to the voltage across the two points.
 V is proportional to I.
 V = I*R (where R is a proportionality constant)
@@ -21,10 +21,7 @@ Source :
 
 from typing import Union
 
-
-def ohms_law(
-    voltage: float = None, current: float = None, resistance: float = None
-) -> Union[float, None]:
+def ohms_law(voltage: float, current: float, resistance: float) -> Union[float, None]:
     """
     Calculate one of the parameters (voltage, current, or resistance) in an electrical circuit
     based on Ohm's law.
@@ -35,47 +32,51 @@ def ohms_law(
     - resistance: Resistance in Ohms (Ω)
 
     Returns:
-    The calculated parameter (voltage, current, or resistance) as a float.
-    Returns None if all parameters are None.
+    A dictionary with the calculated parameter and its value.
 
     Example Usages:
-    ohms_law(voltage=12, current=3)
-    # Output: 4.0
-
     ohms_law(current=2, resistance=4)
-    # Output: 8.0
+    # Output: {'voltage': 8.0}
+
+    ohms_law(voltage=12, current=3)
+    # Output: {'resistance': 4.0}
 
     ohms_law(voltage=9, resistance=3)
-    # Output: 3.0
+    # Output: {'current': 3.0}
 
     Note:
     - Only two out of the three parameters should be provided.
     - Negative values for current, voltage, and resistance are not allowed.
-    - Providing all three parameters will return None.
+    - Providing all three parameters will raise an error.
     """
     # Check that exactly two parameters are provided
-    if sum(param is not None for param in [voltage, current, resistance]) != 2:
-        raise ValueError("Exactly two parameters (voltage, current, or resistance) must be provided.")
+    if sum(param is not None for param in [current, voltage, resistance]) != 2:
+        raise ValueError(
+            "Exactly two parameters (current, voltage, or resistance) must be provided."
+        )
 
     # Check for negative values
-    if current is not None and current < 0:
+    if current < 0:
         raise ValueError("Current cannot be negative.")
-    if voltage is not None and voltage < 0:
+    if voltage < 0:
         raise ValueError("Voltage cannot be negative.")
-    if resistance is not None and resistance < 0:
+    if resistance < 0:
         raise ValueError("Resistance cannot be negative.")
 
     # Calculate the missing parameter
     if current is None:
-        return voltage / resistance
+        current = voltage / resistance
+        return {"current": current}
     elif voltage is None:
-        return current * resistance
+        voltage = current * resistance
+        return {"voltage": voltage}
     elif resistance is None:
-        return voltage / current
+        resistance = voltage / current
+        return {"resistance": resistance}
     else:
         # All parameters are None, return None
         return None
-# Run Doctest
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
