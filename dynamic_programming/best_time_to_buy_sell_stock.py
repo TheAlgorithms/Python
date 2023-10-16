072312baccbb4694a0bddf031e9e963f6330105c
YashKesh <89805020+YashKesh@users.noreply.github.com>
2023-10-16 13:14:40 +0530

    The best time to buy and sell stock is added in DP section with annotations and test cases(TheAlgorithms#7104)

--- dynamic_programming/best_time_to_buy_sell_stock.py
+++ dynamic_programming/best_time_to_buy_sell_stock.py
@@ -7,6 +7,11 @@
 # from typing import list
 
 
+import unittest
+
+# from typing import list
+
+
 class Solution:
     def solve(
         self, prices: list[int], index: int, fee: int, buy: int, dp: list[list[int]]
@@ -43,17 +48,38 @@ class Solution:
 
     def maxprofit(self, prices: list[int], fee: int) -> int:
         """
-        Calculate the maximum profit achievable when buying and
-        selling stocks with a fee.
-
+        Calculate the maximum profit achievable when buying
+        and selling stocks with a fee.
         :param prices: list of stock prices for each day.
         :param fee: Transaction fee.
         :return: Maximum profit achievable.
+        Example:
+        >>> s = Solution()
+        >>> s.maxprofit([1, 3, 2, 8, 4, 9], 2)
+        8
+        >>> s.maxprofit([1, 4, 3, 6, 8, 2, 7], 3)
+        6
         """
         n = len(prices)
         dp = [[-1] * 2 for _ in range(n)]
 
         return self.solve(prices, 0, fee, 1, dp)
+
+
+class TestSolution(unittest.TestCase):
+    def test_maxprofit(self):
+        s = Solution()
+
+        # Test case 1: Example input
+        # Test case 1: Example input
+        prices1 = [1, 3, 2, 8, 4, 9]
+        fee1 = 2
+        assert s.maxprofit(prices1, fee1) == 8  # Use assert instead of self.assertEqual
+
+        # Test case 2: Another example
+        prices2 = [1, 4, 3, 6, 8, 2, 7]
+        fee2 = 3
+        assert s.maxprofit(prices2, fee2) == 6
 
 
 def main():
@@ -73,4 +99,4 @@ if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
-    main()
+    unittest.main()
