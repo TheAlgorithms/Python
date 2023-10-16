4b8acda830db24dd9b0ea6d01a2614b610d0f5d2
Anjali Rajendran <84264801+AnjaliRaj015@users.noreply.github.com>
2023-10-16 13:53:25 +0530

    adding code for ohms law to python physics
    I am adding a new code for calculating ohms law Ω a physics problem to python physics code solutions.

---
+++ physics/ohms_law.py
@@ -0,0 +1,58 @@
+"""
+Calculate one of the parameters (current, voltage, or resistance) in an electrical circuit based on Ohm's law.
+Ohm's law(Ω) states that the current through a conductor between two points is directly proportional to the voltage across the two points.
+V is proportional to I.
+V = I*R (where R is a proportionality constant)
+
+    Input Parameters:
+    - current: Current in Amperes (A)
+    - voltage: Voltage in Volts (V)
+    - resistance: Resistance in Ohms (Ω)
+
+    Returns:
+    A dictionary with the calculated parameter and its value.
+
+"""
+def ohms_law(current=None, voltage=None, resistance=None):
+    """
+    Example Usages:
+    ohms_law(current=2, resistance=4)
+    # Output: {'voltage': 8.0}
+
+    ohms_law(voltage=12, current=3)
+    # Output: {'resistance': 4.0}
+
+    ohms_law(voltage=9, resistance=3)
+    # Output: {'current': 3.0}
+
+    Note:
+    - Only two out of the three parameters should be provided.
+    - Negative values for current, voltage, and resistance are not allowed.
+    - Providing all three parameters will raise an error.
+    """
+    # Check that exactly two parameters are provided
+    if sum(param is not None for param in [current, voltage, resistance]) != 2:
+        raise ValueError("Exactly two parameters (current, voltage, or resistance) must be provided.")
+
+    # Check for negative values
+    if current is not None and current < 0:
+        raise ValueError("Current cannot be negative.")
+    if voltage is not None and voltage < 0:
+        raise ValueError("Voltage cannot be negative.")
+    if resistance is not None and resistance < 0:
+        raise ValueError("Resistance cannot be negative.")
+
+    # Calculate the missing parameter
+    if current is None:
+        current = voltage / resistance
+        return {'current': current}
+    elif voltage is None:
+        voltage = current * resistance
+        return {'voltage': voltage}
+    elif resistance is None:
+        resistance = voltage / current
+        return {'resistance': resistance}
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
