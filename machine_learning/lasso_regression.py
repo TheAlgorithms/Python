016e45e87711fef8bce391118914bdb11d25d28c
Svraj <74090764+Svrajj@users.noreply.github.com>
2023-10-16 18:40:33 +0530

    Create lasso_regression.py

---
+++ machine_learning/lasso_regression.py
@@ -0,0 +1,50 @@
+'''
+Lasso regression, a vital predictive analysis technique, helps identify pivotal features within datasets. These features profoundly impact prediction accuracy. In our code, we use a CS:GO dataset to link Average Damage per Round (ADR) to a player's Rating. Lasso's special touch is feature selection; it trims irrelevant features by nudging some coefficients to zero, fine-tuning the model's accuracy. In essence, Lasso elegantly balances feature significance, refining predictive power in various fields, from finance to healthcare, to harness valuable insights and improve predictions.
+
+'''
+import numpy as np
+import requests
+
+def collect_dataset():
+    response = requests.get(
+        "https://raw.githubusercontent.com/yashLadha/The_Math_of_Intelligence/"
+        "master/Week1/ADRvsRating.csv"
+    )
+    lines = response.text.splitlines()
+    data = []
+    for item in lines:
+        item = item.split(",")
+        data.append(item)
+    data.pop(0)  # Remove labels
+    dataset = np.array(data, dtype=float)
+    return dataset
+
+def lasso_regression(X, y, alpha, max_iterations, learning_rate):
+    n_samples, n_features = X.shape
+    weights = np.zeros(n_features)
+    
+    for _ in range(max_iterations):
+        y_pred = X.dot(weights)
+        gradient = -(1/n_samples) * X.T.dot(y - y_pred) + alpha * np.sign(weights)
+        weights -= learning_rate * gradient
+
+    return weights
+
+def main():
+    data = collect_dataset()
+
+    data_x = data[:, :-1]
+    data_y = data[:, -1]
+
+    alpha = 0.01  # Regularization strength
+    max_iterations = 100000
+    learning_rate = 0.0001550
+
+    feature_weights = lasso_regression(data_x, data_y, alpha, max_iterations, learning_rate)
+
+    print("Feature weights after Lasso regression:")
+    for i, weight in enumerate(feature_weights):
+        print(f"Feature {i}: {weight:.5f}")
+
+if __name__ == "__main__":
+    main()
