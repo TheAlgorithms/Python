3ba3cf77a2439b8a480fa5239eb31c4ab08ed74a
AasheeshLikePanner <ashishlearningsoftware@gmail.com>
2023-10-16 20:26:15 +0530

    Update stock_span_problem.py

--- data_structures/stacks/stock_span_problem.py
+++ data_structures/stacks/stock_span_problem.py
@@ -16,28 +16,30 @@ def calculation_span(price: list[float]) -> list[float]:
     Returns:
     >>> price = [10, 4, 5, 90, 120, 80]
     >>> calculation_span(price)
-    [1.0, 1.0, 2.0, 4.0, 5.0, 6.0]
+    [1.0, 1.0, 2.0, 4.0, 5.0, 1.0]
     >>> price = [100, 50, 60, 70, 80, 90]
     >>> calculation_span(price)
     [1.0, 1.0, 2.0, 3.0, 4.0, 5.0]
     >>> price = [5, 4, 3, 2, 1]
     >>> calculation_span(price)
-    [1.0, 1.0, 2.0, 3.0, 4.0]
+    [1.0, 1.0, 1.0, 1.0, 1.0]
     >>> price = [1, 2, 3, 4, 5]
     >>> calculation_span(price)
     [1.0, 2.0, 3.0, 4.0, 5.0]
     >>> price = [10, 20, 30, 40, 50]
     >>> calculation_span(price)
     [1.0, 2.0, 3.0, 4.0, 5.0]
+    >>> calculation_span(price=[100, 80, 60, 70, 60, 75, 85])
+    [1.0, 1.0, 1.0, 2.0, 1.0, 4.0, 6.0]
     """
     n = len(price)
     st = [0]
-    s = [0.0] * n
-    s[0] = 1.0
+    s = [1.0]
     for i in range(1, n):
-        while len(st) > 0 and price[st[0]] <= price[i]:
+        while st and price[st[-1]] <= price[i]:
             st.pop()
-        s[i] = float(i + 1) if len(st) <= 0 else float(i - st[0])
+        s.append(float(i - st[-1] if st else i + 1))
+        st.append(i)
     return s
 
 
