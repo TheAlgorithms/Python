46b0f05c22ec0c94b6cd9067fb88e665ffd61580
SandeepaDilshanAlagiyawanna <sandeepa.d.alagiyawanna@gmail.com>
2023-10-17 11:15:17 +0530

    Function is Full Optimized with lambda removed and def added

--- project_euler/problem_001/sol2.py
+++ project_euler/problem_001/sol2.py
@@ -1,13 +1,12 @@
 """
-Project Euler Problem 1: https://projecteuler.net/problem=1
-
-Multiples of 3 and 5
+Project Euler Problem 1: Multiples of 3 and 5
 
 If we list all the natural numbers below 10 that are multiples of 3 or 5,
-we get 3, 5, 6 and 9. The sum of these multiples is 23.
+we get 3, 5, 6, and 9. The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
+
 def sum_of_multiples(k):
     """
     Calculate the sum of the first 'k' natural numbers,
@@ -25,14 +24,11 @@ def solution(n: int = 1000) -> int:
     """
     Returns the sum of all the multiples of 3 or 5 below n.
 
-    >>> solution(3)
-    0
-    >>> solution(4)
-    3
-    >>> solution(10)
-    23
-    >>> solution(600)
-    83700
+    Args:
+    n (int): The upper limit for finding multiples.
+
+    Returns:
+    int: The sum of multiples of 3 or 5 below n.
     """
     n -= 1  # We subtract 1 to consider numbers below n
     terms_3 = n // 3
