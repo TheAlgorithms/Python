c7ea86e4e45dc8d95fa437fee4e1d892940b3705
Adam Ross <14985050+R055A@users.noreply.github.com>
2023-10-17 21:47:57 +0200

    Refactor bubble sort func signature, doctest, timer

--- sorts/bubble_sort.py
+++ sorts/bubble_sort.py
@@ -11,6 +11,12 @@ def bubble_sort_iterative(collection: list[Any]) -> list[Any]:
     Examples:
     >>> bubble_sort_iterative([0, 5, 2, 3, 2])
     [0, 2, 2, 3, 5]
+    >>> bubble_sort_iterative([])
+    []
+    >>> bubble_sort_iterative([-2, -45, -5])
+    [-45, -5, -2]
+    >>> bubble_sort_iterative([-23, 0, 6, -4, 34])
+    [-23, -4, 0, 6, 34]
     >>> bubble_sort_iterative([0, 5, 2, 3, 2]) == sorted([0, 5, 2, 3, 2])
     True
     >>> bubble_sort_iterative([]) == sorted([])
@@ -21,13 +27,19 @@ def bubble_sort_iterative(collection: list[Any]) -> list[Any]:
     True
     >>> bubble_sort_iterative(['d', 'a', 'b', 'e']) == sorted(['d', 'a', 'b', 'e'])
     True
+    >>> bubble_sort_iterative(['z', 'a', 'y', 'b', 'x', 'c'])
+    ['a', 'b', 'c', 'x', 'y', 'z']
+    >>> bubble_sort_iterative([1.1, 3.3, 5.5, 7.7, 2.2, 4.4, 6.6])
+    [1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7]
+    >>> bubble_sort_iterative([1, 3.3, 5, 7.7, 2, 4.4, 6])
+    [1, 2, 3.3, 4.4, 5, 6, 7.7]
     >>> import random
-    >>> collection = random.sample(range(-50, 50), 100)
-    >>> bubble_sort_iterative(collection) == sorted(collection)
+    >>> collection_arg = random.sample(range(-50, 50), 100)
+    >>> bubble_sort_iterative(collection_arg) == sorted(collection_arg)
     True
     >>> import string
-    >>> collection = random.choices(string.ascii_letters + string.digits, k=100)
-    >>> bubble_sort_iterative(collection) == sorted(collection)
+    >>> collection_arg = random.choices(string.ascii_letters + string.digits, k=100)
+    >>> bubble_sort_iterative(collection_arg) == sorted(collection_arg)
     True
     """
     length = len(collection)
@@ -42,59 +54,78 @@ def bubble_sort_iterative(collection: list[Any]) -> list[Any]:
     return collection
 
 
-def bubble_sort_recursive(collection: list[Any]), length: int = 0) -> list[Any]:
-    """
-    It is similar is bubble sort but recursive.
-    :param list_data: mutable ordered sequence of elements
-    :param length: length of list data
+def bubble_sort_recursive(collection: list[Any]) -> list[Any]:
+    """It is similar iterative bubble sort but recursive.
+
+    :param collection: mutable ordered sequence of elements
     :return: the same list in ascending order
 
-    >>> bubble_sort_recursive([0, 5, 2, 3, 2], 5)
+    Examples:
+    >>> bubble_sort_recursive([0, 5, 2, 3, 2])
     [0, 2, 2, 3, 5]
-
-    >>> bubble_sort_recursive([], 0)
+    >>> bubble_sort_iterative([])
     []
-
-    >>> bubble_sort_recursive([-2, -45, -5], 3)
+    >>> bubble_sort_recursive([-2, -45, -5])
     [-45, -5, -2]
-
-    >>> bubble_sort_recursive([-23, 0, 6, -4, 34], 5)
+    >>> bubble_sort_recursive([-23, 0, 6, -4, 34])
     [-23, -4, 0, 6, 34]
-
-    >>> bubble_sort_recursive([-23, 0, 6, -4, 34], 5) == sorted([-23, 0, 6, -4, 34])
+    >>> bubble_sort_recursive([0, 5, 2, 3, 2]) == sorted([0, 5, 2, 3, 2])
     True
-
-    >>> bubble_sort_recursive(['z','a','y','b','x','c'], 6)
+    >>> bubble_sort_recursive([]) == sorted([])
+    True
+    >>> bubble_sort_recursive([-2, -45, -5]) == sorted([-2, -45, -5])
+    True
+    >>> bubble_sort_recursive([-23, 0, 6, -4, 34]) == sorted([-23, 0, 6, -4, 34])
+    True
+    >>> bubble_sort_recursive(['d', 'a', 'b', 'e']) == sorted(['d', 'a', 'b', 'e'])
+    True
+    >>> bubble_sort_recursive(['z', 'a', 'y', 'b', 'x', 'c'])
     ['a', 'b', 'c', 'x', 'y', 'z']
-
     >>> bubble_sort_recursive([1.1, 3.3, 5.5, 7.7, 2.2, 4.4, 6.6])
     [1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7]
+    >>> bubble_sort_recursive([1, 3.3, 5, 7.7, 2, 4.4, 6])
+    [1, 2, 3.3, 4.4, 5, 6, 7.7]
+    >>> import random
+    >>> collection_arg = random.sample(range(-50, 50), 100)
+    >>> bubble_sort_recursive(collection_arg) == sorted(collection_arg)
+    True
+    >>> import string
+    >>> collection_arg = random.choices(string.ascii_letters + string.digits, k=100)
+    >>> bubble_sort_recursive(collection_arg) == sorted(collection_arg)
+    True
     """
-    length = length or len(list_data)
+    length = len(collection)
     swapped = False
     for i in range(length - 1):
-        if list_data[i] > list_data[i + 1]:
-            list_data[i], list_data[i + 1] = list_data[i + 1], list_data[i]
+        if collection[i] > collection[i + 1]:
+            collection[i], collection[i + 1] = collection[i + 1], collection[i]
             swapped = True
 
-    return list_data if not swapped else bubble_sort_recursive(list_data, length - 1)
+    return collection if not swapped else bubble_sort_recursive(collection)
 
 
 if __name__ == "__main__":
     import doctest
-    import time
+    from timeit import timeit
 
     doctest.testmod()
 
     user_input = input("Enter numbers separated by a comma:").strip()
-    unsorted = [int(item) for item in user_input.split(",")]
+    unsorted = [int(item) if isinstance(item, int) else float(item)
+                for item in user_input.split(",")]
 
-    print("Iterative bubble sort:")
-    start = time.process_time()
+    num_runs = 1000
+    timer_iterative = timeit('bubble_sort_iterative(unsorted[:])',
+                             globals=globals(),
+                             number=num_runs)
+    timer_recursive = timeit('bubble_sort_recursive(unsorted[:])',
+                             globals=globals(),
+                             number=num_runs)
+
+    print("\nIterative bubble sort:")
     print(*bubble_sort_iterative(unsorted), sep=",")
-    print(f"Processing time (iterative): {(time.process_time() - start) % 1e9 + 7}")
+    print(f"Processing time (iterative): {timer_iterative:.5f} s for {num_runs} runs")
 
     print("\nRecursive bubble sort:")
-    start = time.process_time()
-    print(bubble_sort_recursive(unsorted, len(unsorted)))
-    print(f"Processing time (recursive): {(time.process_time() - start) % 1e9 + 7}")
+    print(*bubble_sort_recursive(unsorted), sep=",")
+    print(f"Processing time (recursive): {timer_recursive:.5f} s for {num_runs} runs")
