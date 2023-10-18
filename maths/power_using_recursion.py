9780df114139ad1f38718b7e5ba2e06f2f0a3ae7
Saptadeep Banerjee <saptadeep555@gmail.com>
2023-10-18 12:28:41 +0530

    fixed

--- maths/power_using_recursion.py
+++ maths/power_using_recursion.py
@@ -11,7 +11,6 @@
     Output -->
         2 to the power of 0 is 1
 """
-import pytest
 
 
 def power(base: int, exponent: int) -> float:
@@ -19,14 +18,12 @@ def power(base: int, exponent: int) -> float:
     Calculate the power of a base raised to an exponent.
 
     Examples:
-    >>> power(3, 4)  # Positive exponent
+    >>> power(3, 4)
     81
-    >>> power(2, 0)  # Zero exponent
+    >>> power(2, 0)
     1
-    >>> power(5, -3)  # Negative exponent
-    0.008
     >>> all(power(base, exponent) == pow(base, exponent)
-    ...     for base in range(-10, 10) for exponent in range(10))  # Check against built-in pow() function
+    ...     for base in range(-10, 10) for exponent in range(10))
     True
     """
     if exponent == 0:
@@ -37,35 +34,8 @@ def power(base: int, exponent: int) -> float:
         return base * power(base, exponent - 1)
 
 
-@pytest.mark.parametrize("base, exponent", [(1, 2), (2, 3), (3, 4)])
-def test_power_positive_exponent(base, exponent):
-    assert power(base, exponent) == pow(base, exponent)
-
-
-@pytest.mark.parametrize("base, exponent", [(1, -2), (2, -3), (3, -4)])
-def test_power_negative_exponent(base, exponent):
-    assert power(base, exponent) == 1 / pow(base, -exponent)
-
-
-def test_power_zero_exponent():
-    assert power(1, 0) == 1
-    assert power(2, 0) == 1
-    assert power(3, 0) == 1
-
-
-def test_power_with_non_integer_exponent():
-    assert power(2, 0.5) == pytest.approx(1.4142135623730951)
-    assert power(5, -1.5) == pytest.approx(0.4)
-    assert power(3, 1.3) == pytest.approx(5.916079783099617)
-
-
-def test_power_raises_type_error():
-    with pytest.raises(TypeError):
-        power("abc", 2)
-    with pytest.raises(TypeError):
-        power(2, "def")
-
-
-def test_power_raises_value_error():
-    witSh pytest.raises(ValueError):
-        power(2, -10)
+if __name__ == "__main__":
+    base = int(input("Enter the base: ").strip())
+    exponent = int(input("Enter the exponent: ").strip())
+    result = power(base, exponent)
+    print(f"{base} to the power of {exponent} is {result}")
