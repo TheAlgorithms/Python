fbe06e3d64f162848cd9aa72a2da6f1a124a58f5
Ankit Avinash <ankit.avinash@gmail.com>
2023-10-18 18:42:55 +0530

    Fixed bug

--- machine_learning/gradient_descent_without_loops.py
+++ machine_learning/gradient_descent_without_loops.py
@@ -82,7 +82,8 @@ def test_gradient_descent():
     predictions = predict(test_x, weights, bias)
     print("Testing: ")
     for i in range(test_x.shape[-1]):
-        print(f'Actual output value: {test_y[0, i]} \tPredicted output value: {predictions[0, i]}')
+        print(f'Actual output value: {test_y[0, i]}\t\
+              Predicted output value: {predictions[0, i]}')
     print()
 
 if __name__ == '__main__':
