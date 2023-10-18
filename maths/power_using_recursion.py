ff09802015c98fe036684eb62ea84972486ac392
Saptadeep Banerjee <saptadeep555@gmail.com>
2023-10-18 12:02:33 +0530

    Added new tests!

--- maths/power_using_recursion.py
+++ maths/power_using_recursion.py
@@ -11,43 +11,61 @@
     Output -->
         2 to the power of 0 is 1
 """
+import pytest
 
 
 def power(base: int, exponent: int) -> float:
     """
-    >>> power(3, 4)
+    Calculate the power of a base raised to an exponent.
+
+    Examples:
+    >>> power(3, 4)  # Positive exponent
     81
-    >>> power(2, 0)
+    >>> power(2, 0)  # Zero exponent
     1
+    >>> power(5, -3)  # Negative exponent
+    0.008
     >>> all(power(base, exponent) == pow(base, exponent)
-    ...     for base in range(-10, 10) for exponent in range(10))
+    ...     for base in range(-10, 10) for exponent in range(10))  # Check against built-in pow() function
     True
-    >>> power('a', 1)
-    'a'
-    >>> power('a', 2)
-    Traceback (most recent call last):
-        ...
-    TypeError: can't multiply sequence by non-int of type 'str'
-    >>> power('a', 'b')
-    Traceback (most recent call last):
-        ...
-    TypeError: unsupported operand type(s) for -: 'str' and 'int'
-    >>> power(2, -1)
-    Traceback (most recent call last):
-        ...
-    RecursionError: maximum recursion depth exceeded
     """
-    return base * power(base, (exponent - 1)) if exponent else 1
+    if exponent == 0:
+        return 1
+    elif exponent < 0:
+        return 1 / power(base, -exponent)
+    else:
+        return base * power(base, exponent - 1)
 
 
-if __name__ == "__main__":
-    from doctests import testmod
+@pytest.mark.parametrize("base, exponent", [(1, 2), (2, 3), (3, 4)])
+def test_power_positive_exponent(base, exponent):
+    assert power(base, exponent) == pow(base, exponent)
 
-    testmod()
-    print("Raise base to the power of exponent using recursion...")
-    base = int(input("Enter the base: ").strip())
-    exponent = int(input("Enter the exponent: ").strip())
-    result = power(base, abs(exponent))
-    if exponent < 0:  # power() does not properly deal w/ negative exponents
-        result = 1 / result
-    print(f"{base} to the power of {exponent} is {result}")
+
+@pytest.mark.parametrize("base, exponent", [(1, -2), (2, -3), (3, -4)])
+def test_power_negative_exponent(base, exponent):
+    assert power(base, exponent) == 1 / pow(base, -exponent)
+
+
+def test_power_zero_exponent():
+    assert power(1, 0) == 1
+    assert power(2, 0) == 1
+    assert power(3, 0) == 1
+
+
+def test_power_with_non_integer_exponent():
+    assert power(2, 0.5) == pytest.approx(1.4142135623730951)
+    assert power(5, -1.5) == pytest.approx(0.4)
+    assert power(3, 1.3) == pytest.approx(5.916079783099617)
+
+
+def test_power_raises_type_error():
+    with pytest.raises(TypeError):
+        power("abc", 2)
+    with pytest.raises(TypeError):
+        power(2, "def")
+
+
+def test_power_raises_value_error():
+    with pytest.raises(ValueError):
+        power(2, -10)
