ce28b1c583903e7a78fa87ebf625cb9e0fc0b06c
Anupamaraie <91787285+Anupamaraie@users.noreply.github.com>
2023-10-20 00:58:24 +0545

    Update vernam_cipher.py

--- ciphers/vernam_cipher.py
+++ ciphers/vernam_cipher.py
@@ -39,3 +39,8 @@ print("\n\n")
 print("Plaintext:", plaintext)
 print("Encrypted:", encrypted_text)
 print("Decrypted:", decrypted_text)
+
+if __name__ == "__main__":
+    import doctest
+    
+    doctest.testmod()
