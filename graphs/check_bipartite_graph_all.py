783e2ae04f41eba3630bea8724941dab9946354d
shivaparihar6119 <shivaayp999@gmail.com>
2023-10-19 23:04:00 +0530

    Fixes#8098

--- graphs/check_bipartite_graph_all.py
+++ graphs/check_bipartite_graph_all.py
@@ -17,6 +17,8 @@ from queue import Queue
         >>> is_bipartite(defaultdict(list, {0: [1, 2], 1: [0, 2], 2: [0, 1]}))
         True"""
 
+
+
 def check_bipartite(graph:defaultdict[int, list[int]]) -> bool:
     queue = Queue()
     visited = [False] * len(graph)
