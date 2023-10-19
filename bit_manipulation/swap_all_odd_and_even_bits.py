a941570decad9dcc801858fb295ee19ad348938c
Christian Clauss <cclauss@me.com>
2023-10-19 19:28:19 +0200

    Update swap_all_odd_and_even_bits.py

--- bit_manipulation/swap_all_odd_and_even_bits.py
+++ bit_manipulation/swap_all_odd_and_even_bits.py
@@ -1,31 +1,53 @@
-def swap_odd_even_bits(num):
-    # Get all even bits
-    even_bits = (
-        num & 0xAAAAAAAA
-    )  # 0xAAAAAAAA is a 32-bit number with all even bits set to 1
-
-    # Get all odd bits
-    odd_bits = (
-        num & 0x55555555
-    )  # 0x55555555 is a 32-bit number with all odd bits set to 1
-
-    # Right shift even bits by 1 and left shift odd bits by 1 to swap them
-    even_bits >>= 1
-    odd_bits <<= 1
-
-    # Combine the swapped even and odd bits
-    result = even_bits | odd_bits
-
-    return result
+def show_bits(before: int, after: int) -> str:
+    return f"{before}: {before:08b}\n{after}: {after:08b}"
 
 
-# Example usage:
-num = 23  # Binary: 10111
-swapped_num = swap_odd_even_bits(num)  # Result: 43 (Binary: 101011)
-print(swapped_num)
+def swap_odd_even_bits(num: int) -> int:
+    """
+    1. We use bitwise AND operations to separate the even bits (0, 2, 4, 6, etc.) and
+       odd bits (1, 3, 5, 7, etc.) in the input number.
+    2. We then right-shift the even bits by 1 position and left-shift the odd bits by
+       1 position to swap them.
+    3. Finally, we combine the swapped even and odd bits using a bitwise OR operation
+       to obtain the final result.
+    >>> print(show_bits(0, swap_odd_even_bits(0)))
+    0: 00000000
+    0: 00000000
+    >>> print(show_bits(1, swap_odd_even_bits(1)))
+    1: 00000001
+    2: 00000010
+    >>> print(show_bits(2, swap_odd_even_bits(2)))
+    2: 00000010
+    1: 00000001
+    >>> print(show_bits(3, swap_odd_even_bits(3)))
+    3: 00000011
+    3: 00000011
+    >>> print(show_bits(4, swap_odd_even_bits(4)))
+    4: 00000100
+    8: 00001000
+    >>> print(show_bits(5, swap_odd_even_bits(5)))
+    5: 00000101
+    10: 00001010
+    >>> print(show_bits(6, swap_odd_even_bits(6)))
+    6: 00000110
+    5: 00000101
+    >>> print(show_bits(23, swap_odd_even_bits(23)))
+    23: 00010111
+    43: 00101011
+    """
+    # Get all even bits - 0xAAAAAAAA is a 32-bit number with all even bits set to 1
+    even_bits = num & 0xAAAAAAAA
 
-# In this code:
+    # Get all odd bits - 0x55555555 is a 32-bit number with all odd bits set to 1
+    odd_bits = num & 0x55555555
 
-# 1. We use bitwise AND operations to separate the even bits (0, 2, 4, 6, etc.) and odd bits (1, 3, 5, 7, etc.) in the input number.
-# 2. We then right-shift the even bits by 1 position and left-shift the odd bits by 1 position to swap them.
-# 3. Finally, we combine the swapped even and odd bits using a bitwise OR operation to obtain the final result.
+    # Right shift even bits and left shift odd bits and swap them
+    return even_bits >> 1 | odd_bits << 1
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
+    for i in (-1, 0, 1, 2, 3, 4, 23, 24):
+        print(show_bits(i, swap_odd_even_bits(i)), '\n')
