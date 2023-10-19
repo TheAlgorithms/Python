322854c70dd6b3949cca156a2d255dbcb3fb952f
Anupamaraie <91787285+Anupamaraie@users.noreply.github.com>
2023-10-20 01:05:25 +0545

    Update vernam_cipher.py

--- ciphers/vernam_cipher.py
+++ ciphers/vernam_cipher.py
@@ -28,11 +28,6 @@ def vernam_decrypt(ciphertext: str, key: str) -> str:
         decrypted_text += chr(65 + ct)
     return decrypted_text
 
-
-import doctest
-
-doctest.testmod()
-
 # Example usage
 plaintext = input("Enter the message: ").upper()
 key = input("Enter the key: ").upper()
