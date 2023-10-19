ccccc95c8a0fbe1fe5afc11f6ef88d7f56a49ad2
Christian Clauss <cclauss@me.com>
2023-10-19 19:48:21 +0200

    Update swap_all_odd_and_even_bits.py

--- bit_manipulation/swap_all_odd_and_even_bits.py
+++ bit_manipulation/swap_all_odd_and_even_bits.py
@@ -1,5 +1,10 @@
 def show_bits(before: int, after: int) -> str:
-    return f"{before}: {before:08b}\n{after}: {after:08b}"
+    """
+    >>> print(show_bits(0, 0xFFFF))
+        0: 00000000
+    65535: 1111111111111111
+    """
+    return f"{before:>5}: {before:08b}\n{after:>5}: {after:08b}"
 
 
 def swap_odd_even_bits(num: int) -> int:
@@ -11,29 +16,29 @@ def swap_odd_even_bits(num: int) -> int:
     3. Finally, we combine the swapped even and odd bits using a bitwise OR operation
        to obtain the final result.
     >>> print(show_bits(0, swap_odd_even_bits(0)))
-    0: 00000000
-    0: 00000000
+        0: 00000000
+        0: 00000000
     >>> print(show_bits(1, swap_odd_even_bits(1)))
-    1: 00000001
-    2: 00000010
+        1: 00000001
+        2: 00000010
     >>> print(show_bits(2, swap_odd_even_bits(2)))
-    2: 00000010
-    1: 00000001
+        2: 00000010
+        1: 00000001
     >>> print(show_bits(3, swap_odd_even_bits(3)))
-    3: 00000011
-    3: 00000011
+        3: 00000011
+        3: 00000011
     >>> print(show_bits(4, swap_odd_even_bits(4)))
-    4: 00000100
-    8: 00001000
+        4: 00000100
+        8: 00001000
     >>> print(show_bits(5, swap_odd_even_bits(5)))
-    5: 00000101
-    10: 00001010
+        5: 00000101
+       10: 00001010
     >>> print(show_bits(6, swap_odd_even_bits(6)))
-    6: 00000110
-    9: 00001001
+        6: 00000110
+        9: 00001001
     >>> print(show_bits(23, swap_odd_even_bits(23)))
-    23: 00010111
-    43: 00101011
+       23: 00010111
+       43: 00101011
     """
     # Get all even bits - 0xAAAAAAAA is a 32-bit number with all even bits set to 1
     even_bits = num & 0xAAAAAAAA
@@ -50,4 +55,4 @@ if __name__ == "__main__":
 
     doctest.testmod()
     for i in (-1, 0, 1, 2, 3, 4, 23, 24):
-        print(show_bits(i, swap_odd_even_bits(i)), "\n")
+        print(show_bits(i, swap_odd_even_bits(i)), '\n')
