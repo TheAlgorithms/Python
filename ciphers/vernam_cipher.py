0dcffec8e0d861a467a4a540d26d7a4d14c70c2b
Anupamaraie <91787285+Anupamaraie@users.noreply.github.com>
2023-10-20 00:53:48 +0545

    Update vernam_cipher.py

--- ciphers/vernam_cipher.py
+++ ciphers/vernam_cipher.py
@@ -1,30 +1,32 @@
 def vernam_encrypt(plaintext: str, key: str) -> str:
+    """
+    >>>vernam_encrypt("HELLO","KEY")
+    'RIJVS'
+    """
     ciphertext = ""
     for i in range(len(plaintext)):
         ct = ord(key[i % len(key)]) - 65 + ord(plaintext[i]) - 65
         while ct > 25:
             ct = ct - 26
         ciphertext += chr(65 + ct)
-    """
-    >>>vernam_encrypt("HELLO","KEY")
-    'RIJVS'
-    """
     return ciphertext
 
 
 def vernam_decrypt(ciphertext: str, key: str) -> str:
+    """
+    >>>vernam_decrypt("RIJVS","KEY")
+    'HELLO'
+    """
     decrypted_text = ""
     for i in range(len(ciphertext)):
         ct = ord(ciphertext[i]) - ord(key[i % len(key)])
         while ct < 0:
             ct = 26 + ct
         decrypted_text += chr(65 + ct)
-    """
-    >>>vernam_decrypt("RIJVS","KEY")
-    'HELLO'
-    """
     return decrypted_text
 
+import doctest
+doctest.testmod()
 
 # Example usage
 plaintext = input("Enter the message: ").upper()
@@ -35,7 +37,3 @@ print("\n\n")
 print("Plaintext:", plaintext)
 print("Encrypted:", encrypted_text)
 print("Decrypted:", decrypted_text)
-
-import doctest
-
-doctest.testmod()
