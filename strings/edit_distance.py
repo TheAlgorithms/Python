45e67f1f7c4bd7c39e486c0d60f9fb0b3a6fd3f3
Tianyi Zheng <tianyizheng02@gmail.com>
2023-10-19 08:33:39 -0400

    Update edit_distance.py

--- strings/edit_distance.py
+++ strings/edit_distance.py
@@ -1,17 +1,17 @@
 def edit_distance(source: str, target: str) -> int:
     """
-    Edit distance algorithm is a string metric, i.e., it is a way of quantifying
-    how dissimilar two strings are to one another. It is measured by
-    counting the minimum number of operations required to transform one string
-    into another.
+    Edit distance algorithm is a string metric, i.e., it is a way of quantifying how
+    dissimilar two strings are to one another. It is measured by counting the minimum
+    number of operations required to transform one string into another.
 
     This implementation assumes that the cost of operations (insertion, deletion and
     substitution) is always 1
 
     Args:
-    source: the initial string with respect to which we are calculating the edit distance
-        for the target
+    source: the initial string with respect to which we are calculating the edit
+        distance for the target
     target: the target string, formed after performing n operations on the source string
+    
     >>> edit_distance("GATTIC", "GALTIC")
     1
     """
