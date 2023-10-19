e6e49e9a2da5703ed200fcfbb3850661e442ad9b
Ankit Singh <97951167+ankitsinghhh@users.noreply.github.com>
2023-10-19 11:35:24 +0530

    Update climbing_stairs.py
    Refactor code for better readability and documentation
    
    - Updated the docstring to provide a more detailed description of the problem and an example.
    - Improved the assertion error message for input validation.

--- dynamic_programming/climbing_stairs.py
+++ dynamic_programming/climbing_stairs.py
@@ -1,42 +1,41 @@
 #!/usr/bin/env python3
 
-
 def climb_stairs(number_of_steps: int) -> int:
     """
-    LeetCdoe No.70: Climbing Stairs
-    Distinct ways to climb a number_of_steps staircase where each time you can either
-    climb 1 or 2 steps.
+    Calculate the number of distinct ways to climb a staircase with a given number of steps.
+    
+    This problem is from LeetCode No. 70: Climbing Stairs.
 
     Args:
-        number_of_steps: number of steps on the staircase
+        number_of_steps (int): The number of steps on the staircase.
 
     Returns:
-        Distinct ways to climb a number_of_steps staircase
+        int: The number of distinct ways to climb the staircase.
 
     Raises:
-        AssertionError: number_of_steps not positive integer
+        AssertionError: If number_of_steps is not a positive integer.
 
-    >>> climb_stairs(3)
-    3
-    >>> climb_stairs(1)
-    1
-    >>> climb_stairs(-7)  # doctest: +ELLIPSIS
-    Traceback (most recent call last):
-        ...
-    AssertionError: number_of_steps needs to be positive integer, your input -7
+    Example:
+        >>> climb_stairs(3)
+        3
+        >>> climb_stairs(1)
+        1
+        >>> climb_stairs(-7)  # doctest: +ELLIPSIS
+        Traceback (most recent call last):
+            ...
+        AssertionError: number_of_steps needs to be a positive integer, your input: -7
     """
-    assert (
-        isinstance(number_of_steps, int) and number_of_steps > 0
-    ), f"number_of_steps needs to be positive integer, your input {number_of_steps}"
+    assert isinstance(number_of_steps, int) and number_of_steps > 0, f"number_of_steps needs to be a positive integer, your input: {number_of_steps}"
+    
     if number_of_steps == 1:
         return 1
+    
     previous, current = 1, 1
     for _ in range(number_of_steps - 1):
         current, previous = current + previous, current
+    
     return current
-
 
 if __name__ == "__main__":
     import doctest
-
     doctest.testmod()
