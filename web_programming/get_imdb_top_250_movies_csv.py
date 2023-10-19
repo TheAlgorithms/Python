f820c2ef33b44a3c9c183e49344ce4a79c98c81c
Ritik Kumar <ritik.iit21@gmail.com>
2023-10-19 23:45:29 +0530

    added year of release

--- web_programming/get_imdb_top_250_movies_csv.py
+++ web_programming/get_imdb_top_250_movies_csv.py
@@ -11,9 +11,10 @@ def get_imdb_top_250_movies(url: str = "") -> dict[str, float]:
     soup = BeautifulSoup(requests.get(url).text, "html.parser")
     titles = soup.find_all("td", attrs="titleColumn")
     ratings = soup.find_all("td", class_="ratingColumn imdbRating")
+    years = soup.find_all("td", class_ ="yearColumn releasingYear")
     return {
-        title.a.text: float(rating.strong.text)
-        for title, rating in zip(titles, ratings)
+        title.a.text: float(rating.strong.text): int(year.strong.text)
+        for title, rating, year in zip(titles, ratings, years)
     }
 
 
@@ -21,9 +22,9 @@ def write_movies(filename: str = "IMDb_Top_250_Movies.csv") -> None:
     movies = get_imdb_top_250_movies()
     with open(filename, "w", newline="") as out_file:
         writer = csv.writer(out_file)
-        writer.writerow(["Movie title", "IMDb rating"])
-        for title, rating in movies.items():
-            writer.writerow([title, rating])
+        writer.writerow(["Movie title", "IMDb rating", "Year of Release"])
+        for title, rating, year in movies.items():
+            writer.writerow([title, rating, year])
 
 
 if __name__ == "__main__":
