d89a0b9979d5884a2790b60e1660081c3239da7f
Saptadeep Banerjee <saptadeep555@gmail.com>
2023-10-20 13:38:30 +0530

    Few things added

--- maths/signum.py
+++ maths/signum.py
@@ -5,13 +5,13 @@ Signum function -- https://en.wikipedia.org/wiki/Sign_function
 
 def signum(num: float) -> int:
     """
-     Applies signum function on the number
+    Applies signum function on the number
 
     Custom test cases:
     >>> signum(-20)
-    -2
+    -1
     >>> signum(20)
-    2
+    1
     >>> signum(0)
     0
     """
@@ -27,9 +27,15 @@ def test_signum() -> None:
     assert signum(5) == 1
     assert signum(-5) == -1
     assert signum(0) == 0
+    assert signum(10.5) == 1
+    assert signum(-10.5) == -1
+    assert signum(1e-6) == 1
+    assert signum(-1e-6) == -1
+    assert signum(123456789) == 1
+    assert signum(-123456789) == -1
 
 
-if __name__ == "__main__":
+if __name__ == "__main":
     print(signum(12))
     print(signum(-12))
     print(signum(0))
