a4abc098a6fc865cf8cb74ccc0d681fd9291d5b0
Bubu-Droid <fufudadw@gmail.com>
2023-10-20 10:00:07 +0530

    Update code according to guidelines

--- digital_image_processing/proj_transform.py
+++ digital_image_processing/proj_transform.py
@@ -3,13 +3,13 @@ import cv2
 import numpy as np
 
 # read the image using opencv
-imgInput = cv2.imread('./image_data/lena.jpg')
+img_input = cv2.imread('./image_data/lena.jpg')
 
 # set the width and height of the output image in px
 # width, height = 718, 718
 
-height = imgInput.shape[0]
-width = imgInput.shape[1]
+height = img_input.shape[0]
+width = img_input.shape[1]
 
 # set the initial coordinates of the four points
 pts1 = np.float32([[0,height], [width,height], [width,0], [0,0]])
@@ -18,15 +18,15 @@ pts1 = np.float32([[0,height], [width,height], [width,0], [0,0]])
 pts2 = np.float32([[142,142], [362,32], [418,358], [127,461]])
 
 # get the projective transformation matrix
-trnsMatrix = cv2.getPerspectiveTransform(pts1,pts2)
+trns_matrix = cv2.getPerspectiveTransform(pts1,pts2)
 
 
 # apply the projective transformation
-imgOutput = cv2.warpPerspective(imgInput, trnsMatrix, (width,height))
+img_output = cv2.warpPerspective(img_input, trns_matrix, (width,height))
 
 # display the original and the final image
-cv2.imshow("Original Image", imgInput)
-cv2.imshow("Final Image", imgOutput)
+cv2.imshow("Original Image", img_input)
+cv2.imshow("Final Image", img_output)
 
 # end the program on pressing some key
 cv2.waitKey(0)
