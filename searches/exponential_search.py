dbbe1358044fff9acf7c56a2c829d25f25250ca1
kiarash8112 <kiarash8112hajian@gmail.com>
2023-10-20 14:47:42 -0400

    replace binary_search with binary_search_recursion

--- searches/exponential_search.py
+++ searches/exponential_search.py
@@ -1,17 +1,12 @@
-from binary_search import binary_search
+from binary_search import binary_search_by_recursion
 
 
 def exponential_search(sorted_collection: list[int], item: int) -> int | None:
     bound = 1
     while bound < len(sorted_collection) and sorted_collection[bound] < item:
         bound *= 2
-    if bound < len(sorted_collection) and sorted_collection[bound] == item:
-        return bound
-    last_bound = bound / 2
-    sorted_collection = sorted_collection[int(last_bound) : bound + 1]
-    index_in_optimized_list = binary_search(
-        sorted_collection=sorted_collection, item=item
+    left = bound / 2
+    right = min(bound, len(sorted_collection) - 1)
+    return binary_search_by_recursion(
+        sorted_collection=sorted_collection, item=item, left=left, right=right
     )
-    if index_in_optimized_list is None:
-        return None
-    return int(index_in_optimized_list) + int(bound / 2)
