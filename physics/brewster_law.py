54793fe1b508df2bcc25355f3c87ab9c308c033b
kbadal19 <k.badal19@gmail.com>
2023-10-21 20:22:34 +0530

    Added Brewster's law in physics directory

--- physics/brewster_law.py
+++ physics/brewster_law.py
@@ -1,17 +1,18 @@
 import math
 
 """
-    Brewster’s law, relationship for light waves stating that the maximum polarization (vibration in one plane only) of a
-    ray of light may be achieved by letting the ray fall on a surface of a transparent medium in such a way that the 
-    refracted ray makes an angle of 90° with the reflected ray. The law is named after a Scottish physicist, Sir David 
-    Brewster, who first proposed it in 1811.
+Brewster’s law, relationship for light waves stating that the maximum polarization (vibration in one plane only) of a
+ray of light may be achieved by letting the ray fall on a surface of a transparent medium in such a way that the 
+refracted ray makes an angle of 90° with the reflected ray. The law is named after a Scottish physicist, Sir David 
+Brewster, who first proposed it in 1811.
 """
 
 """
-     Brewster’s law also states that the tangent of the angle of polarization, p, for a wavelength of light passing from 
-     one substance to another is equal to the ratio of the refractive indices, n1 and n2, of the two contacting mediums: 
-     tan p = n2/n1.
+Brewster’s law also states that the tangent of the angle of polarization, p, for a wavelength of light passing from 
+one substance to another is equal to the ratio of the refractive indices, n1 and n2, of the two contacting mediums: 
+tan p = n2/n1.
 """
+
 # Function to calculate Brewster's angle in radians and degrees
 def brewsters_angle(n1, n2):
     # Check if refractive indices are positive
@@ -24,6 +25,7 @@ def brewsters_angle(n1, n2):
     theta_b_degrees = math.degrees(theta_b)
 
     return theta_b, theta_b_degrees
+
 if __name__ == "__main":
     # Get user input for refractive indices
     n1 = float(input("Enter the refractive index of the initial medium (n1): "))
