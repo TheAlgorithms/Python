8dc484bb031ba191dcf8f5ae7628d3ad0d12193f
Saswat Susmoy <72549122+Saswatsusmoy@users.noreply.github.com>
2023-10-21 15:42:48 +0530

    Updated peak_signal_to_noise_ratio.py

--- compression/peak_signal_to_noise_ratio.py
+++ compression/peak_signal_to_noise_ratio.py
@@ -1,24 +1,24 @@
-"""
-Peak signal-to-noise ratio - PSNR
-    https://en.wikipedia.org/wiki/Peak_signal-to-noise_ratio
-Source:
-https://tutorials.techonical.com/how-to-calculate-psnr-value-of-two-images-using-python
-"""
-
 import math
 import os
+
+import numpy as np
 
 PIXEL_MAX = 255.0
 
 
-def peak_signal_to_noise_ratio(original, contrast):
-    mse = 0
-    height, width, _ = original.shape
-    for i in range(height):
-        for j in range(width):
-            for k in range(3):
-                mse += (original[i, j, k] - contrast[i, j, k]) ** 2
-    mse /= height * width * 3
+def peak_signal_to_noise_ratio(original: np.ndarray, contrast: np.ndarray) -> float:
+    """
+    Calculate peak signal to noise ratio (PSNR) between two images.
+    >>> import numpy as np
+    >>> a = np.array([[0, 0], [255, 255]])
+    >>> b = np.array([[0, 0], [255, 255]])
+    >>> peak_signal_to_noise_ratio(a, b)
+    inf
+    >>> c = np.array([[0, 0], [254, 254]])
+    >>> round(peak_signal_to_noise_ratio(a, c), 2)
+    48.13
+    """
+    mse = np.mean((original - contrast) ** 2)
 
     if mse == 0:
         return 100
@@ -26,23 +26,23 @@ def peak_signal_to_noise_ratio(original, contrast):
     return 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
 
 
-def read_image(image_path):
+def read_image(image_path: str) -> np.ndarray:
+    """Read image from file."""
     with open(image_path, "rb") as f:
         height = int.from_bytes(f.read(2), byteorder="big")
         width = int.from_bytes(f.read(2), byteorder="big")
-        image = []
-        for _i in range(height):
-            row = []
-            for _j in range(width):
+        image = np.empty((height, width, 3), dtype=np.uint8)
+        for i in range(height):
+            for j in range(width):
                 b = f.read(1)[0]
                 g = f.read(1)[0]
                 r = f.read(1)[0]
-                row.append([r, g, b])
-            image.append(row)
+                image[i, j] = [r, g, b]
     return image
 
 
 def main():
+    """Main function."""
     dir_path = os.path.dirname(os.path.realpath(__file__))
 
     original = read_image(os.path.join(dir_path, "image_data/original_image.png"))
