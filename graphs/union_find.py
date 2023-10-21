a269eea7ef5e018f1ff822a84eef6aed56168676
龙龙 <51738561+08183080@users.noreply.github.com>
2023-10-21 16:14:30 +0800

    Update union_find.py

--- graphs/union_find.py
+++ graphs/union_find.py
@@ -1,41 +1,45 @@
 """
-2023/10/21 shining in shanghai
-unionfind
+Implementation of a union-find!
+If you want to do doctest:
+python3 -m doctest -v union_find.py
 """
 
+
 class UnionFind:
-    """
-    initialize a unionfind
-    """
-    def __init__(self, n):
+    def __init__(self, n: int) -> None:
+        """
+        Initialize a unionfind
+        n is the numbers of the initial trees
+        """
         self.pa = list(range(n))
-        self.size = [1] * n  # size of the current 
-    
-    """
-    Returns the parent of the element x (root element).
-    >>> uf = UnionFind(3)
-    >>> uf.find(2)
-    2
-    >>> uf.find(1)
-    1
-    """
-    def find(self, x):
+        self.size = [1] * n  # size of the current
+
+    def find(self, x: int) -> int:
+        """
+        Returns the parent of the element x (root element).
+        >>> uf = UnionFind(3)
+        >>> uf.find(2)
+        2
+        >>> uf.find(1)
+        1
+        """
         if self.pa[x] != x:
             self.pa[x] = self.find(self.pa[x])
         return self.pa[x]
-    
-    """
-    Returns the union of tree x and tree y.
-    we connect a tree with fewer nodes to another tree.
-    >>> uf = UnionFind(9)
-    >>> uf.union(2,3)
-    >>> uf.find(3)
-    2
-    >>> uf.union(3,4)
-    >>> uf.union(4)
-    2
-    """
-    def union(self, x, y):
+
+    def union(self, x: int, y: int) -> None:
+        """
+        x and y are trees.
+        Returns the union of tree x and tree y.
+        we connect a tree with fewer nodes to another tree.
+        >>> uf = UnionFind(9)
+        >>> uf.union(2,3)
+        >>> uf.find(3)
+        2
+        >>> uf.union(3,4)
+        >>> uf.find(4)
+        2
+        """
         x, y = self.find(x), self.find(y)
         if x == y:
             return
@@ -44,23 +48,23 @@ class UnionFind:
         self.pa[y] = x
         self.size[x] += self.size[y]
 
-    """
-    Returns the size of tree x that contains its children
-    >>> uf.UnionFind(4)
-    >>> uf.getSize(1)
-    1
-    >>> uf.union(1,2)
-    >>> uf.getSize(1)
-    2
-    """
-    def getSize(self, x):
+    def get_size(self, x: int) -> int:
+        """
+        Returns the size of tree x that contains its children
+        x is the tree that you want to get its size
+        >>> uf = UnionFind(4)
+        >>> uf.get_size(1)
+        1
+        >>> uf.union(1,2)
+        >>> uf.get_size(1)
+        2
+        """
         return self.size[x]
+
 
 if __name__ == "__main__":
     # test
     uf = UnionFind(6)
-    uf.union(1,2)
-    uf.union(2,4)
-    assert uf.getSize(1) == 3
-
-    
+    uf.union(1, 2)
+    uf.union(2, 4)
+    assert uf.get_size(1) == 3
