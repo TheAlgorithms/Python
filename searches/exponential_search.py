f8862a85ea2e33d700327444e60d496244a74d18
SumitBr <sumitbrsumit@gmail.com>
2023-10-21 19:02:14 +0600

    'exponential_search_added'

--- searches/exponential_search.py
+++ searches/exponential_search.py
@@ -11,56 +11,56 @@ For manual testing run:
 python3 expontial_search.py
 """
 
-def binarySearch( arr, l, r, x):
-    if r >= l:
-        mid = l + ( r-l ) // 2
-         
-        # If the element is present at 
-        # the middle itself
-        if arr[mid] == x:
-            return mid
-         
-        # If the element is smaller than mid, 
-        # then it can only be present in the 
-        # left subarray
-        if arr[mid] > x:
-            return binarySearch(arr, l, 
-                                mid - 1, x)
-         
-        # Else he element can only be
-        # present in the right
-        return binarySearch(arr, mid + 1, r, x)
-         
-    # We reach here if the element is not present
-    return -1
- 
-# Returns the position of first
-# occurrence of x in array
+def binary_search(arr, left, right, target) -> int:
+    """
+    Binary search function to find the position of the target in the array.
 
-def exponentialSearch(arr, n, x):
-    # IF x is present at first 
-    # location itself
+    Args:
+        arr (list): The sorted array to search in.
+        left (int): The left index of the subarray to search.
+        right (int): The right index of the subarray to search.
+        target (int): The target element to find.
+
+    Returns:
+        int: The index of the target element if found, or -1 if not found.
+    """
+    while left <= right:
+        mid = left + (right - left) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+def exponential_search(arr, n, x) -> int:
+    """
+    Exponential search function to find the position of the target in the array.
+
+    Args:
+        arr (list): The sorted array to search in.
+        n (int): The number of elements in the array.
+        x (int): The target element to find.
+
+    Returns:
+        int: The index of the target element if found, or -1 if not found.
+    """
     if arr[0] == x:
         return 0
-         
-    # Find range for binary search 
-    # j by repeated doubling
+
     i = 1
     while i < n and arr[i] <= x:
-        i = i * 2
-     
-    # Call binary search for the found range
-    return binarySearch( arr, i // 2, min(i, n-1), x)
-     
- 
-# Driver Code
+        i *= 2
 
+    return binary_search(arr, i // 2, min(i, n - 1), x)
+
+# Driver Code
 arr = [2, 3, 4, 10, 40]
 n = len(arr)
 x = 10
-result = exponentialSearch(arr, n, x)
+result = exponential_search(arr, n, x)
 if result == -1:
-    print ("Element not found in the array")
+    print("Element not found in the array")
 else:
-    print ("Element is present at index %d" %result)
-    
+    print(f"Element is present at index {result}")
