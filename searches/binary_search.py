afbd45858b09350e8bc934c225c6bfbf8ed0abbf
Kiarash Hajian <133909368+kiarash8112@users.noreply.github.com>
2023-10-21 03:40:49 -0400

    use // instread of int() convert  searches/binary_search.py
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- searches/binary_search.py
+++ searches/binary_search.py
@@ -325,7 +325,7 @@ def exponential_search(sorted_collection: list[int], item: int) -> int:
     bound = 1
     while bound < len(sorted_collection) and sorted_collection[bound] < item:
         bound *= 2
-    left = int(bound / 2)
+    left = bound // 2
     right = min(bound, len(sorted_collection) - 1)
     return binary_search_by_recursion(
         sorted_collection=sorted_collection, item=item, left=left, right=right
