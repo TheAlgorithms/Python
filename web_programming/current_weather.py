b4cc5e460f225f3ea022591518cafed14ce337a7
Ritik Kumar Parvat <115374818+RitikKumarParvat@users.noreply.github.com>
2023-10-21 21:23:05 +0530

    Update current_weather.py

--- web_programming/current_weather.py
+++ web_programming/current_weather.py
@@ -20,11 +20,24 @@ def weather_onecall(lat: float = 55.68, lon: float = 12.57, appid: str = APPID) 
 
 
 if __name__ == "__main__":
-    from pprint import pprint
 
     while True:
         location = input("Enter a location:").strip()
         if location:
-            pprint(current_weather(location))
+            json_data = current_weather(location)
+            condition = json_data['weather'][0]['main']
+            description=json_data['weather'][0]['description']
+            temp = int(json_data['main']['temp'] - 273.15)
+            pressure = json_data['main']['pressure']
+            humidity = json_data['main']['humidity']
+            wind = json_data['wind']['speed']
+
+            print("\n Condition:",condition)
+            print("\n description:",description)
+            print("\n temp:",temp)
+            print("\n pressure:",pressure)
+            print("\n humidity:",humidity)
+            print("\n wind:",wind)
+            
         else:
             break
