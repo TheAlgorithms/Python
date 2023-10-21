6ca2bc599ca8c7b7bc16cd7927b8e2bba6c37f07
Kamalakar Satapathi <103712713+Sgvkamalakar@users.noreply.github.com>
2023-10-21 19:58:20 +0530

    Update top_view_of_binary_tree.py

--- data_structures/binary_tree/top_view_of_binary_tree.py
+++ data_structures/binary_tree/top_view_of_binary_tree.py
@@ -1,44 +1,42 @@
 from collections import deque
 
-
-class node:
+class TreeNode:
     def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
-        self.horiz_dist = 0
-
+        self.data=data
+        self.left=None
+        self.right=None
+        self.horiz_dist=0
 
 def topview(root):
     if not root:
         return []
+    
+    d={}
+    queue=deque()
+    root.horiz_dist=0
+    queue.append(root)
 
-    d = {}
-    q = deque()
-    root.horiz_dist = 0
-    q.append(root)
-    while q:
-        cur = q.popleft()
-        horiz_dist = cur.horiz_dist
+    while queue:
+        cur=queue.popleft()
+        horiz_dist=cur.horiz_dist
         if horiz_dist not in d:
-            d[horiz_dist] = cur.data
+            d[horiz_dist]=cur.data
         if cur.left:
-            cur.left.horiz_dist = horiz_dist - 1
-            q.append(cur.left)
+            cur.left.horiz_dist=horiz_dist-1
+            queue.append(cur.left)
         if cur.right:
-            cur.right.horiz_dist = horiz_dist + 1
-            q.append(cur.right)
-    top_view_nodes = [d[key] for key in sorted(d)]
+            cur.right.horiz_dist=horiz_dist+1
+            queue.append(cur.right)
+    top_view_nodes=[d[key] for key in sorted(d)]
     return top_view_nodes
 
+root=TreeNode(1)
+root.left=TreeNode(2)
+root.right=TreeNode(3)
+root.left.left=TreeNode(4)
+root.left.right=TreeNode(5)
+root.right.left=TreeNode(6)
+root.right.right=TreeNode(7)
 
-root = node(1)
-root.left = node(2)
-root.right = node(3)
-root.left.left = node(4)
-root.left.right = node(5)
-root.right.left = node(6)
-root.right.right = node(7)
-
-result = topview(root)
-print(result)
+result=topview(root)
+print(result) 
