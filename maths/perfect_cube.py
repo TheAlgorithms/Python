a8266089a0dc1aa4c497597e831da25c2701d943
Saptadeep Banerjee <saptadeep555@gmail.com>
2023-10-22 12:26:30 +0530

    [Add]: NEW test cases for CUBE

--- maths/perfect_cube.py
+++ maths/perfect_cube.py
@@ -1,11 +1,21 @@
 def perfect_cube(n: int) -> bool:
     """
-    Check if a number is a perfect cube or not.
+    C Check if a number is a perfect cube or not.
 
     >>> perfect_cube(27)
     True
     >>> perfect_cube(4)
     False
+    >>> perfect_cube(8)
+    False
+    >>> perfect_cube(125)
+    True
+    >>> perfect_cube(64)
+    True
+    >>> perfect_cube(0)
+    True
+    >>> perfect_cube(1)
+    Tru
     """
     val = n ** (1 / 3)
     return (val * val * val) == n
