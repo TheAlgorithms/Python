9f16d42374b690e8f2d28a5695d855df2ba674e6
Saptadeep Banerjee <saptadeep555@gmail.com>
2023-10-22 09:02:01 +0530

    Added SOME TESTS for the Perfect_Cube

--- maths/perfect_cube.py
+++ maths/perfect_cube.py
@@ -6,11 +6,32 @@ def perfect_cube(n: int) -> bool:
     True
     >>> perfect_cube(4)
     False
+    >>> perfect_cube(2)
+    False
+    >>> perfect_cube(0)
+    True
+    >>> perfect_cube(1)
+    True
+    >>> perfect_cube(-27)
+    True
+    >>> perfect_cube(-4)
+    False
+    >>> perfect_cube(-2)
+    False
+
     """
-    val = n ** (1 / 3)
-    return (val * val * val) == n
+    if n == 0:
+        return True
+    val = abs(n) ** (1 / 3)
+    return abs(round(val) ** 3) == abs(n)
 
 
 if __name__ == "__main__":
     print(perfect_cube(27))
     print(perfect_cube(4))
+    print(perfect_cube(2))
+    print(perfect_cube(0))
+    print(perfect_cube(1))
+    print(perfect_cube(-27))
+    print(perfect_cube(-4))
+    print(perfect_cube(-2))
