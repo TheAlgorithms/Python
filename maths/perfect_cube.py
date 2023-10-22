dd34e97dabaff68393830b3894fe45157a37b92f
Saptadeep Banerjee <saptadeep555@gmail.com>
2023-10-22 13:20:51 +0530

    Inserted few lines

--- maths/perfect_cube.py
+++ maths/perfect_cube.py
@@ -1,21 +1,27 @@
 def perfect_cube(n: int) -> bool:
     """
-    Check if a number is a perfect cube or not.
+      Check if a number is a perfect cube or not.
 
     >>> perfect_cube(27)
-    True
-    >>> perfect_cube(4)
-    False
-    >>> perfect_cube(8)
-    False
-    >>> perfect_cube(125)
-    True
-    >>> perfect_cube(64)
-    True
-    >>> perfect_cube(0)
-    True
-    >>> perfect_cube(1)
-    Tru
+      True
+      >>> perfect_cube(4)
+      False
+      >>> perfect_cube(8)
+      False
+      >>> perfect_cube(125)
+      True
+      >>> perfect_cube(64)
+      True
+      >>> perfect_cube(0)
+      True
+      >>> perfect_cube(1)
+      True
+      >>> perfect_cube(3.375)
+      False
+      >>> perfect_cube("64")
+      False
+      >>> perfect_cube([27])
+      False
     """
     val = n ** (1 / 3)
     return (val * val * val) == n
