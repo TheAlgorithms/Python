c43480310b28044a55a0bd1e5ab2d5001ceeb117
Maulik Parmar <109577079+Maulikatgit@users.noreply.github.com>
2023-10-23 20:08:05 +0530

    Update merge_sort.py

--- dsa/all_sorting/merge_sort.py
+++ dsa/all_sorting/merge_sort.py
@@ -53,15 +53,15 @@ def merge(arr, l, m, r):
 # sub-array of arr to be sorted
 
 
-def mergeSort(arr, l, r):
+def mergesort(arr, l, r):
     if l < r:
         # Same as (l+r)//2, but avoids overflow for
         # large l and h
         m = l + (r - l) // 2
 
         # Sort first and second halves
-        mergeSort(arr, l, m)
-        mergeSort(arr, m + 1, r)
+        mergesort(arr, l, m)
+        mergesort(arr, m + 1, r)
         merge(arr, l, m, r)
 
 
@@ -72,7 +72,7 @@ print("Given array is")
 for i in range(n):
     print("%d" % arr[i], end=" ")
 
-mergeSort(arr, 0, n - 1)
+mergesort(arr, 0, n - 1)
 print("\n\nSorted array is")
 for i in range(n):
     print("%d" % arr[i], end=" ")
