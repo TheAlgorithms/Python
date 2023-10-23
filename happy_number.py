4d31a43e886819f9ba959fffc54f80d3b2486104
Devashri Deulkar <95555641+Devadeut@users.noreply.github.com>
2023-10-23 22:08:33 +0530

    Add files via upload

---
+++ happy_number.py
@@ -0,0 +1,37 @@
+def is_happy_number(n: int) -> bool:
+    """
+    Check if a number is a happy number.
+    https://en.wikipedia.org/wiki/Happy_number
+     A happy number is defined by the following process:
+    1. Starting with any positive integer, replace the number by
+       the sum of the squares of its digits.
+    2. Repeat the process until the number equals 1 (happy) or
+       it loops endlessly in a cycle (not happy).
+
+    Args:
+        n (int): The number to check for happiness.
+
+    Returns:
+        bool: True if the number is a happy number, False otherwise.
+
+    Examples:
+    >>> is_happy_number(19)
+    True
+    >>> is_happy_number(4)
+    False
+    >>> is_happy_number(23)
+    True
+    """
+
+    # Create a set to store seen numbers and detect cycles
+    seen = set()
+
+    while n != 1 and n not in seen:
+        seen.add(n)
+        n = sum(int(digit) ** 2 for digit in str(n))
+
+    return n == 1
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
