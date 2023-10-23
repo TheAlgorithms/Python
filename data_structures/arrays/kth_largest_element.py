fb5c506e10af815eb99c345ab9be5527061eb55e
neha3423 <akellaneha@gmail.com>
2023-10-23 20:23:52 +0530

    neha3423

--- data_structures/arrays/kth_largest_element.py
+++ data_structures/arrays/kth_largest_element.py
@@ -41,7 +41,7 @@ def partition(arr: list[int], low: int, high: int) -> int:
     return i + 1
 
 
-def kth_largest_element(arr: list[int], k: int) -> int:
+def kth_largest_element(arr: list[int], position: int) -> int:
     """
     Finds the kth largest element in a list.
 
@@ -84,18 +84,18 @@ def kth_largest_element(arr: list[int], k: int) -> int:
     """
     if not arr:
         return -1
-    if not isinstance(k, int):
-        raise ValueError("'k' should be an integer")
-    if not 1 <= k <= len(arr):
-        raise ValueError("Invalid value of 'k'")
+    if not isinstance(position, int):
+        raise ValueError("The position should be an integer")
+    if not 1 <= position <= len(arr):
+        raise ValueError("Invalid value of 'position'")
     low, high = 0, len(arr) - 1
     while low <= high:
         if low > len(arr) - 1 or high < 0:
             return -1
         pivot_index = partition(arr, low, high)
-        if pivot_index == k - 1:
+        if pivot_index == position - 1:
             return arr[pivot_index]
-        elif pivot_index > k - 1:
+        elif pivot_index > position - 1:
             high = pivot_index - 1
         else:
             low = pivot_index + 1
