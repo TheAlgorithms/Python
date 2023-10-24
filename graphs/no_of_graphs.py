b93b93a8685225bcf290d2e521f155dc1ba116bc
kratiagrawal100 <kratiagrawal100@gmail.com>
2023-10-24 23:43:33 +0530

    adding no of graphs program given n vertices for undirected graph

---
+++ graphs/no_of_graphs.py
@@ -0,0 +1,10 @@
+def no_of_graphs(n):
+    ## no of edges for given n vertices would be: n*(n-1)/2 (undirected graph)
+    edges=n*(n-1)/2
+    return pow(2,edges)
+
+if __name__ == "__main__":
+    n=input("Enter no of vertices")
+    n=int(n)
+    graphs=no_of_graphs(n)
+    print(f"possible graphs for {n} vertices are {graphs}:")
