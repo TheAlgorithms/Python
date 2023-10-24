3c7c9200358f9e718a1ead1d7d282b5a9c9544aa
Hussnain Fareed <14870938+husnainfareed@users.noreply.github.com>
2023-10-24 23:03:07 +0200

    Create recursive_binary_search.py

---
+++ searches/recursive_binary_search.py
@@ -0,0 +1,22 @@
+def binary_search(arr, target, low, high):
+    if low > high:
+        return -1  # Element not found
+
+    mid = (low + high) // 2
+
+    if arr[mid] == target:
+        return mid  # Element found at index 'mid'
+    elif arr[mid] > target:
+        return binary_search(arr, target, low, mid - 1)  # Search in the left half
+    else:
+        return binary_search(arr, target, mid + 1, high)  # Search in the right half
+
+# Example usage
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+target = 5
+result = binary_search(arr, target, 0, len(arr) - 1)
+
+if result != -1:
+    print(f"Element found at index {result}")
+else:
+    print("Element not found")
