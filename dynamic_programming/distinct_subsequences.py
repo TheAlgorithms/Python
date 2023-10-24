ae6278f0e34a8716572b8c84015c1ae6dd36e41f
Aayush <aayushsoni424@gmail.com>
2023-10-24 18:43:09 +0530

    Add: Distinct Subsequences

--- dynamic_programming/distinct_subsequences.py
+++ dynamic_programming/distinct_subsequences.py
@@ -7,20 +7,22 @@ Output: 5
 Leetcode link: https://leetcode.com/problems/distinct-subsequences/description/
 """
 
-
-def count_distinct_subsequences(s: str, t: str) -> int:
+def count_distinct_subsequences(input_str: str, target_str: str) -> int:
     """
-    Calculate the number of distinct subsequences of `s` that equal `t`.
+    Calculate the number of distinct subsequences of `input_str`
+    that equal `target_str`.
 
-    :param s: The input string `s`.
-    :param t: The target string `t`.
-    :return: The number of distinct subsequences of `s` that equal `t`.
+    :param input_str: The input string `input_str`.
+    :param target_str: The target string `target_str`.
+    :return: The number of distinct subsequences of `input_str`
+    that equal `target_str`.
 
     This function uses dynamic programming to solve the problem.
 
-    Time complexity: O(m * n).
+    Time complexity: O(m * n),
     Space complexity: O(m * n).
-    where `m` is the length of `t`, and `n` is the length of `s`.
+    where `m` is the length of `target_str`, and `n` is the length of
+    `input_str`.
 
     >>> count_distinct_subsequences("babgbag", "bag")
     5
@@ -28,31 +30,32 @@ def count_distinct_subsequences(s: str, t: str) -> int:
     3
     """
 
-    t_len = len(t)  # Length of target string `t`
-    s_len = len(s)  # Length of input string `s`
+    target_len = len(target_str)  # Length of target string `target_str`
+    input_len = len(input_str)  # Length of input string `input_str`
 
-    if t_len > s_len:
+    if target_len > input_len:
         return 0
 
     # Create a matrix to store counts of subsequences.
-    # dp[i][j] stores the count of occurrences of `t[1..i]` in `s[1..j]`.
-    dp = [[0 for _ in range(s_len + 1)] for __ in range(t_len + 1)]
+    # dp[i][j] stores the count of occurrences of `target_str[1..i]` in
+    # `input_str[1..j]`.
+    dp = [[0 for _ in range(input_len + 1)] for __ in range(target_len + 1]
 
     # Initialize the matrix.
-    for i in range(t_len + 1):
+    for i in range(target_len + 1):
         dp[i][0] = 0
-    for j in range(s_len + 1):
+    for j in range(input_len + 1):
         dp[0][j] = 1
 
     # Fill `dp` using dynamic programming
-    for i in range(1, t_len + 1):
-        for j in range(1, s_len + 1):
-            if t[i - 1] != s[j - 1]:
+    for i in range(1, target_len + 1):
+        for j in range(1, input_len + 1):
+            if target_str[i - 1] != input_str[j - 1]:
                 dp[i][j] = dp[i][j - 1]
             else:
                 dp[i][j] = dp[i][j - 1] + dp[i - 1][j - 1]
 
-    return dp[t_len][s_len]
+    return dp[target_len][input_len]
 
 
 if __name__ == "__main__":
