a23dd7ecbea89be8f6b3c7fcf214425274db0d02
SEIKH NABAB UDDIN <93948993+nababuddin@users.noreply.github.com>
2023-10-24 18:42:32 +0530

    Change from only weatherstack to both (#10882)
    * Update current_weather.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update current_weather.py
    
    * Update current_weather.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update current_weather.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update current_weather.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update current_weather.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update current_weather.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update current_weather.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update current_weather.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update current_weather.py
    
    * Update current_weather.py
    
    * Update current_weather.py
    
    * Update current_weather.py
    
    * import requests
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- web_programming/current_weather.py
+++ web_programming/current_weather.py
@@ -1,30 +1,50 @@
 import requests
 
-APPID = ""  # <-- Put your OpenWeatherMap appid here!
-URL_BASE = "https://api.openweathermap.org/data/2.5/"
+# Put your API key(s) here
+OPENWEATHERMAP_API_KEY = ""
+WEATHERSTACK_API_KEY = ""
+
+# Define the URL for the APIs with placeholders
+OPENWEATHERMAP_URL_BASE = "https://api.openweathermap.org/data/2.5/weather"
+WEATHERSTACK_URL_BASE = "http://api.weatherstack.com/current"
 
 
-def current_weather(q: str = "Chicago", appid: str = APPID) -> dict:
-    """https://openweathermap.org/api"""
-    return requests.get(URL_BASE + "weather", params=locals()).json()
-
-
-def weather_forecast(q: str = "Kolkata, India", appid: str = APPID) -> dict:
-    """https://openweathermap.org/forecast5"""
-    return requests.get(URL_BASE + "forecast", params=locals()).json()
-
-
-def weather_onecall(lat: float = 55.68, lon: float = 12.57, appid: str = APPID) -> dict:
-    """https://openweathermap.org/api/one-call-api"""
-    return requests.get(URL_BASE + "onecall", params=locals()).json()
+def current_weather(location: str) -> list[dict]:
+    """
+    >>> current_weather("location")
+    Traceback (most recent call last):
+        ...
+    ValueError: No API keys provided or no valid data returned.
+    """
+    weather_data = []
+    if OPENWEATHERMAP_API_KEY:
+        params_openweathermap = {"q": location, "appid": OPENWEATHERMAP_API_KEY}
+        response_openweathermap = requests.get(
+            OPENWEATHERMAP_URL_BASE, params=params_openweathermap
+        )
+        weather_data.append({"OpenWeatherMap": response_openweathermap.json()})
+    if WEATHERSTACK_API_KEY:
+        params_weatherstack = {"query": location, "access_key": WEATHERSTACK_API_KEY}
+        response_weatherstack = requests.get(
+            WEATHERSTACK_URL_BASE, params=params_weatherstack
+        )
+        weather_data.append({"Weatherstack": response_weatherstack.json()})
+    if not weather_data:
+        raise ValueError("No API keys provided or no valid data returned.")
+    return weather_data
 
 
 if __name__ == "__main__":
     from pprint import pprint
 
-    while True:
-        location = input("Enter a location:").strip()
+    location = "to be determined..."
+    while location:
+        location = input("Enter a location (city name or latitude,longitude): ").strip()
         if location:
-            pprint(current_weather(location))
-        else:
-            break
+            try:
+                weather_data = current_weather(location)
+                for forecast in weather_data:
+                    pprint(forecast)
+            except ValueError as e:
+                print(repr(e))
+                location = ""
