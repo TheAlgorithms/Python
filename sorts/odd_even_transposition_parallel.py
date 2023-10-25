4c4571c5b4ea502efef0252b4f08e2ec0ad2d299
RaymondDashWu <raymond31670@hotmail.com>
2023-10-25 13:13:48 -0700

    list(range(10)[::-1]) test uncommented

--- sorts/odd_even_transposition_parallel.py
+++ sorts/odd_even_transposition_parallel.py
@@ -73,8 +73,8 @@ arr = the list to be sorted
 
 def odd_even_transposition(arr):
     """
-    # >>> odd_even_transposition(list(range(10)[::-1])) == sorted(list(range(10)[::-1]))
-    # True
+    >>> odd_even_transposition(list(range(10)[::-1])) == sorted(list(range(10)[::-1]))
+    True
     # >>> odd_even_transposition(["a", "x", "c"]) == sorted(["x", "a", "c"])
     # True
     # >>> odd_even_transposition([1.9, 42.0, 2.8]) == sorted([1.9, 42.0, 2.8])
