902664320f4684f64c6aeac0d06fe3bba1973959
Tanmay Admuthe <101891282+tanmayadmuthe@users.noreply.github.com>
2023-10-25 12:18:42 +0530

    Update mirror_binary_tree.py

--- data_structures/binary_tree/mirror_binary_tree.py
+++ data_structures/binary_tree/mirror_binary_tree.py
@@ -1,23 +1,15 @@
-"""
-Given the root of a binary tree, mirror the tree, and return its root.
-
-Leetcode problem reference: https://leetcode.com/problems/mirror-binary-tree/
-"""
 from __future__ import annotations
-
 from collections.abc import Iterator
 from dataclasses import dataclass
-
+from typing import Optional, List
 
 @dataclass
 class Node:
-    """
-    A Node has value variable and pointers to Nodes to its left and right.
-    """
+    """A Node has a value and pointers to its left and right Nodes."""
 
     value: int
-    left: Node | None = None
-    right: Node | None = None
+    left: Optional[Node] = None
+    right: Optional[Node] = None
 
     def __iter__(self) -> Iterator[int]:
         if self.left:
@@ -29,44 +21,26 @@ class Node:
     def __len__(self) -> int:
         return sum(1 for _ in self)
 
-    def mirror(self) -> Node:
+    def mirror(self, inplace: bool = True) -> Node:
         """
         Mirror the binary tree rooted at this node by swapping left and right children.
 
-        >>> tree = Node(0)
-        >>> list(tree)
-        [0]
-        >>> list(tree.mirror())
-        [0]
-        >>> tree = Node(1, Node(0), Node(3, Node(2), Node(4, None, Node(5))))
-        >>> tuple(tree)
-        (0, 1, 2, 3, 4, 5)
-        >>> tuple(tree.mirror())
-        (5, 4, 3, 2, 1, 0)
-        """
-        self.left, self.right = self.right, self.left
-        if self.left:
-            self.left.mirror()
-        if self.right:
-            self.right.mirror()
-        return self
+        Args:
+            inplace (bool): If True, mirror the tree in place. If False, create a new mirrored tree.
 
+        Returns:
+            Node: The mirrored tree root node.
+        """
+        if inplace:
+            self.left, self.right = self.right, self.left
+        else:
+            mirrored_node = Node(self.value)
+            mirrored_node.left = self.right.mirror(False) if self.right else None
+            mirrored_node.right = self.left.mirror(False) if self.left else None
+            return mirrored_node
 
 def make_tree_seven() -> Node:
-    r"""
-    Return a binary tree with 7 nodes that looks like this:
-           1
-         /   \
-        2     3
-       / \   / \
-      4   5 6   7
-
-    >>> tree_seven = make_tree_seven()
-    >>> len(tree_seven)
-    7
-    >>> list(tree_seven)
-    [4, 2, 5, 1, 6, 3, 7]
-    """
+    """Return a binary tree with 7 nodes."""
     tree = Node(1)
     tree.left = Node(2)
     tree.right = Node(3)
@@ -76,24 +50,8 @@ def make_tree_seven() -> Node:
     tree.right.right = Node(7)
     return tree
 
-
 def make_tree_nine() -> Node:
-    r"""
-    Return a binary tree with 9 nodes that looks like this:
-          1
-         /  \
-        2    3
-       / \    \
-      4   5    6
-     / \   \
-    7   8   9
-
-    >>> tree_nine = make_tree_nine()
-    >>> len(tree_nine)
-    9
-    >>> list(tree_nine)
-    [7, 4, 8, 2, 5, 9, 1, 3, 6]
-    """
+    """Return a binary tree with 9 nodes."""
     tree = Node(1)
     tree.left = Node(2)
     tree.right = Node(3)
@@ -105,49 +63,17 @@ def make_tree_nine() -> Node:
     tree.left.right.right = Node(9)
     return tree
 
-
 def main() -> None:
-    r"""
-    Mirror binary trees with the given root and returns the root
-
-    >>> tree = make_tree_nine()
-    >>> tuple(tree)
-    (7, 4, 8, 2, 5, 9, 1, 3, 6)
-    >>> tuple(tree.mirror())
-    (6, 3, 1, 9, 5, 2, 8, 4, 7)
-
-    nine_tree:
-          1
-         /  \
-        2    3
-       / \    \
-      4   5    6
-     / \   \
-    7   8   9
-
-    The mirrored tree looks like this:
-          1
-         /  \
-        3    2
-       /    / \
-      6    5   4
-          /   / \
-         9   8   7
-    """
-    trees = {"zero": Node(0), "seven": make_tree_seven(), "nine": make_tree_nine()}
+    """Mirror binary trees and print the results."""
+    trees = {
+        "zero": Node(0),
+        "seven": make_tree_seven(),
+        "nine": make_tree_nine(),
+    }
     for name, tree in trees.items():
-        print(f"      The {name} tree: {tuple(tree)}")
-        # (0,)
-        # (4, 2, 5, 1, 6, 3, 7)
-        # (7, 4, 8, 2, 5, 9, 1, 3, 6)
-        print(f"Mirror of {name} tree: {tuple(tree.mirror())}")
-        # (0,)
-        # (7, 3, 6, 1, 5, 2, 4)
-        # (6, 3, 1, 9, 5, 2, 8, 4, 7)
-
+        print(f"The {name} tree: {list(tree)}")
+        mirrored_tree = tree.mirror(False)
+        print(f"Mirror of {name} tree: {list(mirrored_tree)}")
 
 if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod()
     main()
