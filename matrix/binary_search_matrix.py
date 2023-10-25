8d13824f45487c8e6be68ddc8803427b1e0bc777
Tanmay Admuthe <101891282+tanmayadmuthe@users.noreply.github.com>
2023-10-25 11:54:24 +0530

    Update binary_search_matrix.py

--- matrix/binary_search_matrix.py
+++ matrix/binary_search_matrix.py
@@ -1,55 +1,40 @@
-def binary_search(array: list, lower_bound: int, upper_bound: int, value: int) -> int:
-    """
-    This function carries out Binary search on a 1d array and
-    return -1 if it do not exist
-    array: A 1d sorted array
-    value : the value meant to be searched
-    >>> matrix = [1, 4, 7, 11, 15]
-    >>> binary_search(matrix, 0, len(matrix) - 1, 1)
-    0
-    >>> binary_search(matrix, 0, len(matrix) - 1, 23)
-    -1
-    """
+from typing import List, Tuple, Union
 
-    r = int((lower_bound + upper_bound) // 2)
-    if array[r] == value:
-        return r
-    if lower_bound >= upper_bound:
-        return -1
-    if array[r] < value:
-        return binary_search(array, r + 1, upper_bound, value)
-    else:
-        return binary_search(array, lower_bound, r - 1, value)
-
-
-def mat_bin_search(value: int, matrix: list) -> list:
+def binary_search(array: List[int], lower_bound: int, upper_bound: int, value: int) -> int:
     """
-    This function loops over a 2d matrix and calls binarySearch on
-    the selected 1d array and returns [-1, -1] is it do not exist
-    value : value meant to be searched
-    matrix = a sorted 2d matrix
-    >>> matrix = [[1, 4, 7, 11, 15],
-    ...           [2, 5, 8, 12, 19],
-    ...           [3, 6, 9, 16, 22],
-    ...           [10, 13, 14, 17, 24],
-    ...           [18, 21, 23, 26, 30]]
-    >>> target = 1
-    >>> mat_bin_search(target, matrix)
-    [0, 0]
-    >>> target = 34
-    >>> mat_bin_search(target, matrix)
-    [-1, -1]
+    Binary search in a sorted 1D array.
+    Returns the index of the value if found, or -1 if not found.
     """
-    index = 0
-    if matrix[index][0] == value:
-        return [index, 0]
-    while index < len(matrix) and matrix[index][0] < value:
-        r = binary_search(matrix[index], 0, len(matrix[index]) - 1, value)
-        if r != -1:
-            return [index, r]
-        index += 1
-    return [-1, -1]
+    while lower_bound <= upper_bound:
+        mid = (lower_bound + upper_bound) // 2
+        if array[mid] == value:
+            return mid
+        elif array[mid] < value:
+            lower_bound = mid + 1
+        else:
+            upper_bound = mid - 1
+    return -1
 
+def search_2d_matrix(value: int, matrix: List[List[int]]) -> Union[List[int], Tuple[int, int]]:
+    """
+    Search for a value in a sorted 2D matrix.
+    Returns the index [row, col] of the value if found, or (-1, -1) if not found.
+    """
+    if not matrix or not matrix[0]:
+        return (-1, -1)
+
+    rows, cols = len(matrix), len(matrix[0])
+    row, col = 0, cols - 1
+
+    while row < rows and col >= 0:
+        if matrix[row][col] == value:
+            return (row, col)
+        elif matrix[row][col] < value:
+            row += 1
+        else:
+            col -= 1
+
+    return (-1, -1)
 
 if __name__ == "__main__":
     import doctest
