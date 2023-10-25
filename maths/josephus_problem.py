f0d09207bfea9fb7095a37fdc5325031cef1d283
Aqib Javid Bhat <aqib.m31.dev@gmail.com>
2023-10-25 19:11:27 +0530

    Add iterative implementation of Josephus Problem

--- maths/josephus_problem.py
+++ maths/josephus_problem.py
@@ -15,9 +15,9 @@ https://en.wikipedia.org/wiki/Josephus_problem
 """
 
 
-def josephus(n: int, k: int) -> int:
+def josephus_recursive(n: int, k: int) -> int:
     """
-    Solve the Josephus problem for n people and a step size of k.
+    Solve the Josephus problem for n people and a step size of k recursively.
 
     Args:
         n (int): Number of people.
@@ -27,15 +27,15 @@ def josephus(n: int, k: int) -> int:
         int: The position of the last person remaining.
 
     Examples:
-        >>> josephus(7, 3)
+        >>> josephus_recursive(7, 3)
         3
-        >>> josephus(10, 2)
+        >>> josephus_recursive(10, 2)
         4
     """
     if n == 1:
         return 0
 
-    return (josephus(n - 1, k) + k) % n
+    return (josephus_recursive(n - 1, k) + k) % n
 
 
 def find_winner(n: int, k: int) -> int:
@@ -55,7 +55,34 @@ def find_winner(n: int, k: int) -> int:
         >>> find_winner(10, 2)
         5
     """
-    return josephus(n, k) + 1
+    return josephus_recursive(n, k) + 1
+
+
+def josephus_iterative(n: int, k: int) -> int:
+    """
+    Solve the Josephus problem for n people and a step size of k iteratively.
+
+    Args:
+        n (int): The number of people in the circle.
+        k (int): The number of steps to take before eliminating someone.
+
+    Returns:
+        int: The position of the last person standing.
+
+    Examples:
+        >>> josephus_iterative(5, 2)
+        3
+        >>> josephus_iterative(7, 3)
+        4
+    """
+    circle = list(range(1, n + 1))
+    current = 0
+
+    while len(circle) > 1:
+        current = (current + k - 1) % len(circle)
+        circle.pop(current)
+
+    return circle[0]
 
 
 if __name__ == "__main__":
