4781e0b3e15a0b1f2a7f6d283b69a5c24d3215ce
Khushi Shukla <tusharika.shukla05@gmail.com>
2023-10-26 23:59:15 +0530

    Update crossword_puzzle_solver.py

--- backtracking/crossword_puzzle_solver.py
+++ backtracking/crossword_puzzle_solver.py
@@ -1,18 +1,16 @@
 # https://www.geeksforgeeks.org/solve-crossword-puzzle/
-def solve_crossword(puzzle, words) -> None:
+from typing import List, Optional
+
+def solve_crossword(puzzle: List[List[str]], words: List[str]) -> Optional[List[List[str]]]:
     rows, cols = len(puzzle), len(puzzle[0])
 
-    def is_valid(word, r, c, direction):
+    def is_valid(word: str, r: int, c: int, direction: str) -> bool:
         if direction == "across":
-            return c + len(word) <= cols and all(
-                puzzle[r][c + i] in ("", word[i]) for i in range(len(word))
-            )
+            return c + len(word) <= cols and all(puzzle[r][c + i] in ("", word[i]) for i in range(len(word)))
         else:  # direction == "down"
-            return r + len(word) <= rows and all(
-                puzzle[r + i][c] in ("", word[i]) for i in range(len(word))
-            )
+            return r + len(word) <= rows and all(puzzle[r + i][c] in ("", word[i]) for i in range(len(word)))
 
-    def place_word(word, r, c, direction) -> None:
+    def place_word(word: str, r: int, c: int, direction: str) -> None:
         if direction == "across":
             for i in range(len(word)):
                 puzzle[r][c + i] = word[i]
@@ -20,7 +18,7 @@ def solve_crossword(puzzle, words) -> None:
             for i in range(len(word)):
                 puzzle[r + i][c] = word[i]
 
-    def remove_word(word, r, c, direction) -> None:
+    def remove_word(word: str, r: int, c: int, direction: str) -> None:
         if direction == "across":
             for i in range(len(word)):
                 puzzle[r][c + i] = ""
@@ -28,7 +26,7 @@ def solve_crossword(puzzle, words) -> None:
             for i in range(len(word)):
                 puzzle[r + i][c] = ""
 
-    def backtrack(puzzle, words):
+    def backtrack(puzzle: List[List[str]], words: List[str]) -> bool:
         for r in range(rows):
             for c in range(cols):
                 if puzzle[r][c] == "":
@@ -53,21 +51,17 @@ def solve_crossword(puzzle, words) -> None:
     else:
         return None
 
-
 # Example usage:
 puzzle = [
-    ["#", "#", "c", "#", "#", "#", "#", "#", "#"],
-    ["#", "#", "a", "t", "#", "t", "r", "a", "#"],
-    ["#", "#", "o", "#", "#", "#", "o", "#", "#"],
-    ["#", "#", "w", "#", "c", "a", "r", "#", "#"],
-    ["#", "#", "#", "#", "#", "a", "#", "#", "#"],
-    ["#", "#", "t", "#", "t", "a", "x", "i", "#"],
-    ["#", "#", "e", "#", "#", "n", "#", "#", "#"],
-    ["#", "#", "n", "#", "a", "t", "e", "x", "#"],
-    ["#", "#", "#", "#", "x", "#", "#", "#", "#"],
+    ["#", "#", "c", "#", "#", "#", "#"],
+    ["#", "#", "r", "a", "c", "k", "#"],
+    ["#", "#", "o", "#", "#", "#", "#"],
+    ["#", "#", "r", "#", "b", "#", "#"],
+    ["#", "#", "a", "a", "t", "a", "x"],
+    ["#", "#", "t", "#", "i", "n", "#"],
+    ["#", "#", "e", "#", "n", "#", "#"],
 ]
-
-words = ["car", "cat", "tax", "rat", "ate", "axe", "won"]
+words = ["car", "rack", "bat", "cat", "rat", "in", "tax", "eat"]
 solution = solve_crossword(puzzle, words)
 
 if solution:
