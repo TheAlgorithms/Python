9ee7ebe29187687f2534dc4d90d27d0fa1c3152f
Tianyi Zheng <tianyizheng02@gmail.com>
2023-10-26 03:53:57 -0400

    Update lens_formulae.py

--- physics/lens_formulae.py
+++ physics/lens_formulae.py
@@ -1,53 +1,50 @@
 """
-    This module has functions which calculate focal length of lens, distance of
-    image from the lens and
-    distance of object from the lens.
-    The above is calculated uding the lens formula.
+This module has functions which calculate focal length of lens, distance of
+image from the lens and distance of object from the lens.
+The above is calculated using the lens formula.
 
-    In optics, the relationship between the distance of the image (v),
-    the distance of the object (u), and
-    the focal length (f) of the lens is given by the formula known as the Lens formula.
-    The Lens formula is applicable for convex as well as concave lenses. The formula
-    is given as follows:
+In optics, the relationship between the distance of the image (v),
+the distance of the object (u), and
+the focal length (f) of the lens is given by the formula known as the Lens formula.
+The Lens formula is applicable for convex as well as concave lenses. The formula
+is given as follows:
 
-    -------------------
-    | 1/f = 1/v + 1/u |
-    -------------------
+-------------------
+| 1/f = 1/v + 1/u |
+-------------------
 
-    Where,
-        f = focal length of the lens in meters.
-        v = distance of the image from the lens in meters.
-        u = distance of the object from the lens in meters.
+Where
+    f = focal length of the lens in meters.
+    v = distance of the image from the lens in meters.
+    u = distance of the object from the lens in meters.
 
-    To make our calculations easy few assumptions are made while deriving the formula
-    which are important to keep in mind before solving this equation.
-    The assumptions are as follows:
-        1.The object O is a point object lying somewhere on the principle axis.
-        2.The lens is thin.
-        3.The aperture of the lens taken must be small.
-        4.The angles of incidence and angle of refraction should be small.
+To make our calculations easy few assumptions are made while deriving the formula
+which are important to keep in mind before solving this equation.
+The assumptions are as follows:
+    1. The object O is a point object lying somewhere on the principle axis.
+    2. The lens is thin.
+    3. The aperture of the lens taken must be small.
+    4. The angles of incidence and angle of refraction should be small.
 
-    Sign convention is a set of rules to set signs for image distance, object distance,
-    focal length, etc
-    for mathematical analysis of image formation. According to it:
-        1.Object is always placed to the left of lens.
-        2.All distances are measured from the optical centre of the mirror.
-        3.Distances measured in the direction of the incident ray are positive and
-        the distances measured in the direction opposite
-        to that of the incident rays are negative.
-        4.Distances measured along y-axis above the principal axis are positive and
-        that measured along y-axis below the principal
-        axis are negative.
+Sign convention is a set of rules to set signs for image distance, object distance,
+focal length, etc
+for mathematical analysis of image formation. According to it:
+    1. Object is always placed to the left of lens.
+    2. All distances are measured from the optical centre of the mirror.
+    3. Distances measured in the direction of the incident ray are positive and
+    the distances measured in the direction opposite
+    to that of the incident rays are negative.
+    4. Distances measured along y-axis above the principal axis are positive and
+    that measured along y-axis below the principal
+    axis are negative.
 
-        Note: Sign convention can be reversed and will still give the correct results.
+Note: Sign convention can be reversed and will still give the correct results.
 
-        (Reference for Sign convention
-        https://www.toppr.com/ask/content/concept/sign-convention-for-lenses-210246/)
+Reference for Sign convention:
+https://www.toppr.com/ask/content/concept/sign-convention-for-lenses-210246/
 
-        (Reference for assumptions
-        https://testbook.com/physics/derivation-of-lens-maker-formula#:~:text=Assumptions%20of%20Lens%20Maker%27s%20Formula&text=The%20object%20O%20is%20a,of%20refraction%20should%20be%20small.)
-
-
+Reference for assumptions:
+https://testbook.com/physics/derivation-of-lens-maker-formula
 """
 
 
