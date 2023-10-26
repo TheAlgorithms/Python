8dfea512235161403d1894950940e9a3f6ece580
Akash_Jambulkar <97665573+Akash-Jambulkar@users.noreply.github.com>
2023-10-26 13:56:29 +0530

    Update cocktail_shaker_sort.py
    Added a docstring with clear explanations of the function and its parameters.
    
    Changed variable names i, start, and end for better readability.
    
    Improved comments to describe the purpose of each section of the algorithm.
    
    Adjusted the loop ranges to make the code more concise and readable.
    
    Removed redundant comments and variable assignments.
    
    Provided a clear message when printing the sorted list.

--- sorts/cocktail_shaker_sort.py
+++ sorts/cocktail_shaker_sort.py
@@ -1,8 +1,5 @@
 """ https://en.wikipedia.org/wiki/Cocktail_shaker_sort """
-
-
-def cocktail_shaker_sort(unsorted: list) -> list:
-    """
+"""
     Pure implementation of the cocktail shaker sort algorithm in Python.
     >>> cocktail_shaker_sort([4, 5, 2, 1, 2])
     [1, 2, 2, 4, 5]
@@ -18,27 +15,53 @@ def cocktail_shaker_sort(unsorted: list) -> list:
 
     >>> cocktail_shaker_sort([-4, -5, -24, -7, -11])
     [-24, -11, -7, -5, -4]
+"""
+def cocktail_shaker_sort(arr):
     """
-    for i in range(len(unsorted) - 1, 0, -1):
+    Sorts a list using the Cocktail Shaker Sort algorithm.
+    
+    :param arr: List of elements to be sorted.
+    :return: Sorted list.
+    """
+    start, end = 0, len(arr) - 1
+
+    while start < end:
         swapped = False
 
-        for j in range(i, 0, -1):
-            if unsorted[j] < unsorted[j - 1]:
-                unsorted[j], unsorted[j - 1] = unsorted[j - 1], unsorted[j]
-                swapped = True
-
-        for j in range(i):
-            if unsorted[j] > unsorted[j + 1]:
-                unsorted[j], unsorted[j + 1] = unsorted[j + 1], unsorted[j]
+        # Pass from left to right
+        for i in range(start, end):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
                 swapped = True
 
         if not swapped:
             break
-    return unsorted
 
+        end -= 1  # Decrease the end pointer after each pass
+
+        # Pass from right to left
+        for i in range(end, start, -1):
+            if arr[i] < arr[i - 1]:
+                arr[i], arr[i - 1] = arr[i - 1], arr[i]
+                swapped = True
+
+        if not swapped:
+            break
+
+        start += 1  # Increase the start pointer after each pass
+
+    return arr
 
 if __name__ == "__main__":
     import doctest
+
+    doctest.testmod()
+    
+    user_input = input("Enter numbers separated by a comma:\n").strip()
+    unsorted = [int(item) for item in user_input.split(",")]
+    sorted_list = cocktail_shaker_sort(unsorted)
+    print(f"Sorted list: {sorted_list}")
+
 
     doctest.testmod()
     user_input = input("Enter numbers separated by a comma:\n").strip()
