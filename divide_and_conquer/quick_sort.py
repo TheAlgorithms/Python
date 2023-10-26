52a6f1b079c5961805045394aa5cc957dbe5db32
Saksham <perfectdude429@gmail.com>
2023-10-26 15:38:31 +0545

    sorting algorithm using divide and conquer

--- divide_and_conquer/quick_sort.py
+++ divide_and_conquer/quick_sort.py
@@ -1,23 +1,19 @@
-"""
-    Sorting Algorithm using Divide and Conquer method
-    Quick_Sort
-"""
-def quicksort(arr, left, right):
+def partition(arr: list[float], left: int, right: int) -> int:
     """
-        Parameter: 
-        arr: list
-        left: start index for sorting
-        right: end index for sorting
+        Picks a random (right most) element and sorts only the element.
+
+            Parameters: 
+                    arr (list[float]): List of numbers to be sorted.
+                    left (int): starting index of list to be sorted.
+                    right (int): ending index of list to be sorted.
+                
+            Returns:
+                    index (int): Index of the sorted element. 
+
+        >>> partition([23,12,55,20,32], 0, 4)
+        3
     """
-    if(left<right):
-        partition_pos = partition(arr, left, right)
-        quicksort(arr, left, partition_pos-1)
-        quicksort(arr, partition_pos+1, right)
-        
-def partition(arr, left, right):
-    """
-        Return a sorted index for pivot element
-    """
+
     i = left
     j = right-1
     pivot = arr[right]
@@ -31,3 +27,30 @@ def partition(arr, left, right):
     if(arr[i] > pivot):
         arr[i], arr[right] = arr[right], arr[i]
     return i
+
+def quicksort(arr: list[float], left: int, right: int) -> list[float]:
+    """
+        Sorts a list of numbers using divide and conquer.
+        For more information, https://en.wikipedia.org/wiki/Quicksort.
+
+        Parameter: 
+                arr (list[float]): list
+                left (int): start index for sorting
+                right (int): end index for sorting
+
+        Returns:
+                list[float]: sorted list
+
+        >>>quicksort([23,12,55,20,32], 0, 4)
+        [12,20,23,32,55]
+    """
+
+    if(left<right):
+        partition_pos = partition(arr, left, right)
+        quicksort(arr, left, partition_pos-1)
+        quicksort(arr, partition_pos+1, right)
+        return arr
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
