edf58a98592c218fd14eddf186c49b617797523c
Tapas Singhal <98687345+Shocker-lov-t@users.noreply.github.com>
2023-10-26 22:42:13 +0530

    Create have_different_signs.py

---
+++ bit_manipulation/have_different_signs.py
@@ -0,0 +1,27 @@
+# https://www.geeksforgeeks.org/detect-if-two-integers-have-opposite-signs/
+
+def have_different_signs(number1: int, number2: int) -> bool:
+    """
+    Check if two integers have different signs.
+
+    >>> have_different_signs(5, -3)
+    True
+    >>> have_different_signs(-2, 8)
+    True
+    >>> have_different_signs(0, -5)
+    True
+    >>> have_different_signs(3, 7)
+    False
+    >>> have_different_signs(-10, -20)
+    False
+    >>> have_different_signs(0, 0)
+    False
+    """
+
+    # Check if the signs of the two numbers are different
+    return (number1 < 0 and number2 >= 0) or (number1 >= 0 and number2 < 0)
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
