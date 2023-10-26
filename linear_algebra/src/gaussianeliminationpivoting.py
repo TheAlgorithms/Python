b1bc7ff5fcd30ba4a4180b6f56421080eb1ee7d9
Mohammad Esfandiyar <padraesfandiyar@gmail.com>
2023-10-26 23:49:14 +0330

    Update gaussianeliminationpivoting.py

--- linear_algebra/src/gaussianeliminationpivoting.py
+++ linear_algebra/src/gaussianeliminationpivoting.py
@@ -1,44 +1,40 @@
 import numpy as np
-
-
-def custom_pivoting(matrix: np.ndarray, n: int, i: int) -> int:
+def custom_pivoting(matrix: np.ndarray, num_rows: int, 
+                    column_index: int) -> int:
     """
-    Selects the index of the minimum absolute
-    value in the i-th column of a matrix.
+    Selects the index of the minimum absolute 
+    value in the specified column of a matrix.
 
     Parameters:
     - matrix (np.ndarray): The input matrix.
-    - n (int): The size of the matrix.
-    - i (int): The column index.
+    - num_rows (int): The number of rows in the matrix.
+    - column_index (int): The index of the column.
 
     Returns:
-    - int: The index of the minimum absolute value in the
-    i-th column.
+    - int: The index of the minimum absolute value in the 
+    specified column.
 
     Example:
-    >>> a_matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]],
-    dtype=float)
+    >>> a_matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]], dtype=float)
     >>> custom_pivoting(a_matrix, 3, 1)
     0
-    """
-    min_index = i
-    for index in range(i + 1, n):
-        if abs(matrix[index][i]) < abs(matrix[min_index][i]):
+    """    
+    min_index = column_index
+    for index in range(column_index + 1, num_rows):
+        if abs(matrix[index][column_index]) < abs(matrix[min_index][column_index]):
             min_index = index
     return min_index
-
-
 def custom_gauss_elimination_pivoting(
-    coeff_matrix: list, const_vector: list, n: int
+    coeff_matrix: list, const_vector: list, num_equations: int
 ) -> list:
     """
-    Solves a system of linear equations
+    Solves a system of linear equations 
     using Gaussian elimination with partial pivoting.
 
     Parameters:
     - coeff_matrix (list): The coefficient matrix.
     - const_vector (list): The constant vector.
-    - n (int): The size of the system.
+    - num_equations (int): The number of equations in the system.
 
     Returns:
     - list: The solution vector.
@@ -50,23 +46,22 @@ def custom_gauss_elimination_pivoting(
     [1.0, 2.0, 3.0]
     """
     result = []
-    for i in range(n - 1):
-        new_index = custom_pivoting(coeff_matrix, n, i)
-        coeff_matrix[i], coeff_matrix[new_index] = (coeff_matrix[new_index],)
-        coeff_matrix[i]
-        const_vector[i], const_vector[new_index] = (const_vector[new_index],)
-        const_vector[i]
+    for i in range(num_equations - 1):
+        new_index = custom_pivoting(coeff_matrix, num_equations, i)
+        coeff_matrix[i], coeff_matrix[new_index] = coeff_matrix[new_index], coeff_matrix[i]
+        const_vector[i], const_vector[new_index] = const_vector[new_index], const_vector[i]
         pivot = coeff_matrix[i][i]
-        for j in range(i + 1, n):
+        for j in range(i + 1, num_equations):
             m = -1 * coeff_matrix[j][i] / pivot
-            for k in range(n):
+            for k in range(num_equations):
                 coeff_matrix[j][k] += m * coeff_matrix[i][k]
             const_vector[j] += m * const_vector[i]
-    for p in range(n - 1, -1, -1):
-        result.append(const_vector[p] / coeff_matrix[p][p])
-        for q in range(p - 1, -1, -1):
-            const_vector[q] = const_vector[q] - result[n - p - 1] * coeff_matrix[q][p]
+    for row_index in range(num_equations - 1, -1, -1):
+        result.append(const_vector[row_index] / coeff_matrix[row_index][row_index])
+        for q in range(row_index - 1, -1, -1):
+            const_vector[q] = const_vector[q] - result[num_equations - row_index - 1] * coeff_matrix[q][row_index]
     return result
+
 
 
 # Example usage:
