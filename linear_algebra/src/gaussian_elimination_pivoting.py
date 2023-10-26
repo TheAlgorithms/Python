94acda96b61fc1385c1e80212c0921c9775b983e
Mohammad Esfandiyar <padraesfandiyar@gmail.com>
2023-10-27 01:44:48 +0330

    Update gaussian_elimination_pivoting.py

--- linear_algebra/src/gaussian_elimination_pivoting.py
+++ linear_algebra/src/gaussian_elimination_pivoting.py
@@ -1,85 +1,79 @@
 import numpy as np
+import sys
+import time
+
+matrixAB = np.loadtxt('matrix.txt')
+B = np.copy(matrixAB[:, matrixAB.shape[1] - 1])
 
 
-def custom_pivoting(matrix: np.ndarray, num_rows: int, column_index: int) -> int:
-    """
-    Selects the index of the minimum absolute
-    value in the specified column of a matrix.
+def foo(matrix):
+    start = time.process_time()
+    AB = np.copy(matrix)
+    numOfRows = AB.shape[0]
+    numOfColumns = AB.shape[1] - 1
+    xLst = []
 
-    Parameters:
-    - matrix (np.ndarray): The input matrix.
-    - num_rows (int): The number of rows in the matrix.
-    - column_index (int): The index of the column.
+    """"Lead element search"""
+    print("Matrix before leading coefficient search: ")
+    print(AB)
+    print(" ")
 
-    Returns:
-    - int: The index of the minimum absolute value
-    in the specified column.
+    """Upper triangular matrix"""
 
-    Example:
-    >>> a_matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]], dtype=float)
-    >>> custom_pivoting(a_matrix, 3, 1)
-    0
-    """
-    min_index = column_index
-    for index in range(column_index + 1, num_rows):
-        if abs(matrix[index][column_index]) < abs(matrix[min_index][column_index]):
-            min_index = index
-    return min_index
+    for columnNum in range(numOfRows):
+        for i in range(columnNum, numOfColumns):
+            if abs(AB[i][columnNum]) > abs(AB[columnNum][columnNum]):
+                AB[[columnNum, i]] = AB[[i, columnNum]]
+                if AB[columnNum, columnNum] == 0.0:
+                    sys.exit("Matrix is not correct")
+            else:
+                pass
+        if columnNum != 0:
+            for i in range(columnNum, numOfRows):
+                AB[i, :] = AB[i, :] - AB[i, columnNum - 1] / AB[columnNum - 1, columnNum - 1] * AB[columnNum - 1, :]
+
+    print("Upper triangular matrix: ")
+    print(AB.round(3))
+    print(" ")
+
+    """Find x vector"""
+    columnNum = numOfRows
+    while columnNum != 0:
+        columnNum -= 1
+        lineOfX = AB[columnNum, numOfRows]
+        if columnNum + 1 != numOfRows:
+            for y in range(1, numOfRows - columnNum):
+                lineOfX += -AB[columnNum, numOfRows - y] * xLst[y - 1]
+        x = lineOfX / AB[columnNum, columnNum]
+        xLst.append(x)
+
+    stop = time.process_time()
+    xLst.reverse()
+    print("x vector: ")
+    print(xLst)
+    print(" ")
+    print("Start time: ", start, "End time: ", stop)
+    print("Elapsed time during the whole function in seconds:", stop - start)
+
+    return np.asarray(xLst)
 
 
-def custom_gauss_elimination_pivoting(
-    coeff_matrix: list, const_vector: list, num_equations: int
-) -> list:
-    """
-    Solves a system of linear equations using
-    Gaussian elimination with partial pivoting.
+vectorOfXAlpha = foo(matrixAB)
 
-    Parameters:
-    - coeff_matrix (list): The coefficient matrix.
-    - const_vector (list): The constant vector.
-    - num_equations (int): The number of equations in the system.
+"""Cond(A)"""
+modifiedB = np.copy(B)
+modifiedB[np.argmax(abs(B))] = B[np.argmax(abs(B))] / 100 * 101
 
-    Returns:
-    - list: The solution vector.
+matrixAB[:, matrixAB.shape[1] - 1] = modifiedB
+print()
+print("Cond(A) check: ")
+vectorOfXBeta = foo(matrixAB)
 
-    Example:
-    >>> a_matrix = [[2, 3, 4], [1, -2, 3], [3, 4, 5]]
-    >>> b_vector = [20, 9, 11]
-    >>> custom_gauss_elimination_pivoting(a_matrix, b_vector, 3)
-    [1.0, 2.0, 3.0]
-    """
-    result: List[float] = []
-
-    # Forward elimination
-    for i in range(num_equations - 1):
-        new_index = custom_pivoting(coeff_matrix, num_equations, i)
-        coeff_matrix[i], coeff_matrix[new_index] = (
-            coeff_matrix[new_index],
-            coeff_matrix[i],
-        )
-        const_vector[i], const_vector[new_index] = (
-            const_vector[new_index],
-            const_vector[i],
-        )
-        pivot = coeff_matrix[i][i]
-        for j in range(i + 1, num_equations):
-            m = -1 * coeff_matrix[j][i] / pivot
-            for k in range(num_equations):
-                coeff_matrix[j][k] += m * coeff_matrix[i][k]
-            const_vector[j] += m * const_vector[i]
-
-    # Backward substitution
-    for row_index in range(num_equations - 1, -1, -1):
-        temp_sum = sum(
-            coeff_matrix[row_index][col] * result[col]
-            for col in range(row_index + 1, num_equations)
-        )
-        result.append(
-            (const_vector[row_index] - temp_sum) / coeff_matrix[row_index][row_index]
-        )
-
-    result.reverse()
-    return result
+deltaB = modifiedB - B
+deltaX = vectorOfXAlpha - vectorOfXBeta
+print(" ")
+condA = abs(np.sum(deltaX) / np.sum(vectorOfXAlpha)) * (np.sum(B) / np.sum(deltaB))
+print("Cond(A) =< {:03f}".format(condA))
 
 
 # Example usage:
