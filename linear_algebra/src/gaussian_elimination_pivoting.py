a7267330519cd869d01b029283832458473d2b5b
Mohammad Esfandiyar <padraesfandiyar@gmail.com>
2023-10-27 01:51:44 +0330

    Update gaussian_elimination_pivoting.py

--- linear_algebra/src/gaussian_elimination_pivoting.py
+++ linear_algebra/src/gaussian_elimination_pivoting.py
@@ -1,84 +1,84 @@
-import numpy as np
 import sys
 import time
+import numpy as np
 
-matrixAB = np.loadtxt("matrix.txt")
-B = np.copy(matrixAB[:, matrixAB.shape[1] - 1])
+matrixab = np.loadtxt("matrix.txt")
+B = np.copy(matrixab[:, matrixab.shape[1] - 1])
 
 
 def foo(matrix):
     start = time.process_time()
-    AB = np.copy(matrix)
-    numOfRows = AB.shape[0]
-    numOfColumns = AB.shape[1] - 1
-    xLst = []
+    ab = np.copy(matrix)
+    numofrows = ab.shape[0]
+    numofcolumns = ab.shape[1] - 1
+    xlst = []
 
     """"Lead element search"""
     print("Matrix before leading coefficient search: ")
-    print(AB)
+    print(ab)
     print(" ")
 
     """Upper triangular matrix"""
 
-    for columnNum in range(numOfRows):
-        for i in range(columnNum, numOfColumns):
-            if abs(AB[i][columnNum]) > abs(AB[columnNum][columnNum]):
-                AB[[columnNum, i]] = AB[[i, columnNum]]
-                if AB[columnNum, columnNum] == 0.0:
+    for columnnum in range(numofrows):
+        for i in range(columnnum, numofcolumns):
+            if abs(ab[i][columnnum]) > abs(ab[columnnum][columnnum]):
+                ab[[columnnum, i]] = ab[[i, columnnum]]
+                if ab[columnnum, columnnum] == 0.0:
                     sys.exit("Matrix is not correct")
             else:
                 pass
-        if columnNum != 0:
-            for i in range(columnNum, numOfRows):
-                AB[i, :] = (
-                    AB[i, :]
-                    - AB[i, columnNum - 1]
-                    / AB[columnNum - 1, columnNum - 1]
-                    * AB[columnNum - 1, :]
+        if columnnum != 0:
+            for i in range(columnnum, numofrows):
+                ab[i, :] = (
+                    ab[i, :]
+                    - ab[i, columnnum - 1]
+                    / ab[columnnum - 1, columnnum - 1]
+                    * ab[columnnum - 1, :]
                 )
 
     print("Upper triangular matrix: ")
-    print(AB.round(3))
+    print(ab.round(3))
     print(" ")
 
     """Find x vector"""
-    columnNum = numOfRows
-    while columnNum != 0:
-        columnNum -= 1
-        lineOfX = AB[columnNum, numOfRows]
-        if columnNum + 1 != numOfRows:
-            for y in range(1, numOfRows - columnNum):
-                lineOfX += -AB[columnNum, numOfRows - y] * xLst[y - 1]
-        x = lineOfX / AB[columnNum, columnNum]
-        xLst.append(x)
+    columnnum = numofrows
+    while columnnum != 0:
+        columnnum -= 1
+        lineofx = ab[columnnum, numofrows]
+        if columnnum + 1 != numofrows:
+            for y in range(1, numofrows - columnnum):
+                lineofx += -ab[columnnum, numofrows - y] * xlst[y - 1]
+        x = lineofx / ab[columnnum, columnnum]
+        xlst.append(x)
 
     stop = time.process_time()
-    xLst.reverse()
+    xlst.reverse()
     print("x vector: ")
-    print(xLst)
+    print(xlst)
     print(" ")
     print("Start time: ", start, "End time: ", stop)
     print("Elapsed time during the whole function in seconds:", stop - start)
 
-    return np.asarray(xLst)
+    return np.asarray(xlst)
 
 
-vectorOfXAlpha = foo(matrixAB)
+vectorofxalpha = foo(matrixab)
 
 """Cond(A)"""
-modifiedB = np.copy(B)
-modifiedB[np.argmax(abs(B))] = B[np.argmax(abs(B))] / 100 * 101
+modifiedb = np.copy(B)
+modifiedb[np.argmax(abs(B))] = B[np.argmax(abs(B))] / 100 * 101
 
-matrixAB[:, matrixAB.shape[1] - 1] = modifiedB
+matrixab[:, matrixab.shape[1] - 1] = modifiedb
 print()
 print("Cond(A) check: ")
-vectorOfXBeta = foo(matrixAB)
+vectorofxbeta = foo(matrixab)
 
-deltaB = modifiedB - B
-deltaX = vectorOfXAlpha - vectorOfXBeta
+deltab = modifiedb - B
+deltax = vectorofxalpha - vectorofxbeta
 print(" ")
-condA = abs(np.sum(deltaX) / np.sum(vectorOfXAlpha)) * (np.sum(B) / np.sum(deltaB))
-print("Cond(A) =< {:03f}".format(condA))
+conda = abs(np.sum(deltax) / np.sum(vectorofxalpha)) * (np.sum(B) / np.sum(deltab))
+print("Cond(A) =< {:03f}".f(conda))
 
 
 # Example usage:
