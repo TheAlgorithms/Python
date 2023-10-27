4d1d54c984e925ba4d93e11bd78396ba6624db53
shaman <shamanbusiness2004@gmail.com>
2023-10-27 23:05:52 +0530

    graph color

--- divide_and_conquer/graphcolor.py
+++ divide_and_conquer/graphcolor.py
@@ -1,16 +1,16 @@
 from collections import defaultdict
-
+from typing import Dict, List
 
 class Graph:
-    def __init__(self, subjects):
+    def __init__(self, subjects: List[str]) -> None:
         self.subjects = subjects
         self.graph = defaultdict(list)
 
-    def add_edge(self, subject1, subject2):
+    def add_edge(self, subject1: str, subject2: str) -> None:
         self.graph[subject1].append(subject2)
         self.graph[subject2].append(subject1)
 
-    def graph_coloring(self):
+    def graph_coloring(self) -> Dict[str, int]:
         color_map = {}
         available_colors = set(range(1, len(self.subjects) + 1))
         for subject in self.subjects:
@@ -21,15 +21,15 @@ class Graph:
                     used_colors.add(color_map[neighbor])
 
             available_color = available_colors - used_colors
-        if available_color:
-            color_map[subject] = min(available_color)
-        else:
-            # If no available color, assign a new color
-            color_map[subject] = len(available_colors) + 1
-            available_colors.add(color_map[subject])
+            if available_color:
+                color_map[subject] = min(available_color)
+            else:
+                # If no available color, assign a new color
+                color_map[subject] = len(available_colors) + 1
+                available_colors.add(color_map[subject])
         return color_map
 
-    def get_minimum_time_slots(self):
+    def get_minimum_time_slots(self) -> int:
         color_map = self.graph_coloring()
         return max(color_map.values())
 
@@ -47,7 +47,6 @@ graph.add_edge("Math", "Physics")
 graph.add_edge("Math", "Chemistry")
 graph.add_edge("Physics", "Chemistry")
 graph.add_edge("Physics", "Biology")
-
 
 minimum_time_slots = graph.get_minimum_time_slots()
 print(f"Minimum time slots needed: {minimum_time_slots}")
