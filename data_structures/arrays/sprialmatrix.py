4353e93f1269a88524497bb8b700d7374b7d8750
Vinay Mallela <69084915+MD571@users.noreply.github.com>
2023-10-27 10:56:19 +0530

    Update and rename sprial_matrix.py to sprialmatrix.py

--- data_structures/arrays/sprialmatrix.py
+++ data_structures/arrays/sprialmatrix.py
@@ -3,16 +3,16 @@ Given a 2D array, print it in spiral form.
 
 Examples:
 
-Input:  	{{1,    2,   3,   4},
-              	 {5,    6,   7,   8},
-             	 {9,   10,  11,  12},
-            	 {13,  14,  15,  16 }}
+Input:  {{1,    2,   3,   4},
+              {5,    6,   7,   8},
+             {9,   10,  11,  12},
+            {13,  14,  15,  16 }}
 Output: 1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10
 Explanation: The output is matrix in spiral format.
 """
 
 
-def spiralOrder(matrix):
+def spiralorder(matrix):
     ans = []
 
     if len(matrix) == 0:
@@ -29,12 +29,13 @@ def spiralOrder(matrix):
 
     # Iterate from 0 to R * C - 1
     for i in range(m * n):
+        i=i
         ans.append(matrix[x][y])
         seen[x][y] = True
         cr = x + dr[di]
         cc = y + dc[di]
 
-        if 0 <= cr and cr < m and 0 <= cc and cc < n and not (seen[cr][cc]):
+        if cr>=0 and cr < m and cc>=0 and cc < n and not (seen[cr][cc]):
             x = cr
             y = cc
         else:
@@ -49,6 +50,6 @@ if __name__ == "__main__":
     a = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
 
     # Function call
-    for x in spiralOrder(a):
+    for x in spiralorder(a):
         print(x, end=" ")
     print()
