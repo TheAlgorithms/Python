82b4c59f94befbbff589d36d0ff5e3b4d3471aa7
Khushi Shukla <tusharika.shukla05@gmail.com>
2023-10-27 21:02:45 +0530

    Update monotonic_array.py

--- data_structures/arrays/monotonic_array.py
+++ data_structures/arrays/monotonic_array.py
@@ -16,6 +16,8 @@ def is_monotonic(nums: list[int]) -> bool:
 
 
 # Test the function with your examples
-print(is_monotonic([1, 2, 2, 3]))  # Output: True
-print(is_monotonic([6, 5, 4, 4]))  # Output: True
-print(is_monotonic([1, 3, 2]))  # Output: False
+if __name__ == "__main__":
+    # Test the function with your examples
+    print(is_monotonic([1, 2, 2, 3]))  # Output: True
+    print(is_monotonic([6, 5, 4, 4]))  # Output: True
+    print(is_monotonic([1, 3, 2]))  # Output: False
