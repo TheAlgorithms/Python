1b621e1fd40adb557c7a0457cbf8b746438097ad
SEIKH NABAB UDDIN <93948993+nababuddin@users.noreply.github.com>
2023-10-27 10:59:44 +0530

    Update digital_differential_analyzer_line_drawing.py

--- graphics/digital_differential_analyzer_line_drawing.py
+++ graphics/digital_differential_analyzer_line_drawing.py
@@ -1,7 +1,25 @@
 import matplotlib.pyplot as plt
+from typing import List, Tuple
 
+def digital_differential_analyzer_line(x1: int, y1: int, x2: int, y2: int) -> Tuple[List[int], List[int]]:
+    """
+    Draw a line using the Digital Differential Analyzer (DDA) algorithm.
 
-def dda_line(x1, y1, x2, y2):
+    Parameters:
+        x1 (int): The x-coordinate of the starting point.
+        y1 (int): The y-coordinate of the starting point.
+        x2 (int): The x-coordinate of the ending point.
+        y2 (int): The y-coordinate of the ending point.
+
+    Returns:
+        Tuple[List[int], List[int]]: A tuple of two lists, where the first list contains
+        the x-coordinates of the line points, and the second list contains the y-coordinates.
+
+    Example:
+        >>> digital_differential_analyzer_line(0, 0, 3, 2)
+        ([0, 1, 2, 3], [0, 1, 1, 2])
+    """
+
     # Calculate the differences in x and y coordinates
     dx = x2 - x1
     dy = y2 - y1
@@ -28,20 +46,19 @@ def dda_line(x1, y1, x2, y2):
 
     return x_points, y_points
 
-
 if __name__ == "__main__":
     # Input the coordinates of the two endpoints of the line
-    x1 = int(input("Enter x1: "))
-    y1 = int(input("Enter y1: "))
-    x2 = int(input("Enter x2: "))
-    y2 = int(input("Enter y2: "))
+    x1 = int(input("Enter the x-coordinate of the starting point: "))
+    y1 = int(input("Enter the y-coordinate of the starting point: "))
+    x2 = int(input("Enter the x-coordinate of the ending point: "))
+    y2 = int(input("Enter the y-coordinate of the ending point: "))
 
-    # Calculate the points using DDA algorithm
-    x_points, y_points = dda_line(x1, y1, x2, y2)
+    # Calculate the points using the Digital Differential Analyzer (DDA) algorithm
+    x_points, y_points = digital_differential_analyzer_line(x1, y1, x2, y2)
 
     # Plot the line using Matplotlib
     plt.plot(x_points, y_points, marker="o")
-    plt.title("DDA Line Drawing Algorithm")
+    plt.title("Digital Differential Analyzer Line Drawing Algorithm")
     plt.xlabel("X-axis")
     plt.ylabel("Y-axis")
     plt.grid()
