58a987a09a59c5ca49a0b9595540989002032ec2
Ankit Kumar Singh <57264717+hacquees@users.noreply.github.com>
2023-10-28 04:27:05 +0530

    Update lagrange_interpolation.py

--- maths/numerical_analysis/lagrange_interpolation.py
+++ maths/numerical_analysis/lagrange_interpolation.py
@@ -1,30 +1,25 @@
-import math
-from typing import List
-
-def lagrange_interpolation(x: List[float], y: List[float], value: float) -> float:
+def lagrange_interpolation(x: list[float], y: list[float], value: float) -> float:
     """
     Calculate the Lagrange interpolation of a function based on provided data points.
 
     Parameters:
-        x (List[float]): List of x-values (independent variable).
-        y (List[float]): List of corresponding y-values (dependent variable).
+        x (list[float]): List of x-values (independent variable).
+        y (list[float]): List of corresponding y-values (dependent variable).
         value (float): The value at which to interpolate the function.
 
     Returns:
         float: The interpolated value of the function at the specified 'value'.
 
-    for eg.:
-        
-        >>> x = [5, 6, 9, 11]
-        >>> y = [12, 13, 14, 16]
-        >>> lagrange_interpolation(x, y, 10)
+    Examples:
+        >>> x = [5.0, 6.0, 9.0, 11.0]
+        >>> y = [12.0, 13.0, 14.0, 16.0]
+        >>> lagrange_interpolation(x, y, 10.0)
         14.666666666666666
-
     """
     ans = 0
     n = len(x)
 
-    for i in range(n):  
+    for i in range(n):
         term = y[i]
         for j in range(n):
             if i != j:
@@ -36,17 +31,22 @@ def lagrange_interpolation(x: List[float], y: List[float], value: float) -> floa
 def main():
     """
     Main function for performing Lagrange interpolation.
-    Takes user input for data points and the value to interpolate, then displays the interpolated result.
+    Takes user input for data points and the value to interpolate,
+    then displays the interpolated result.
     """
     n = int(input("Enter the number of values: "))
     x = []
     y = []
 
-    print("Enter the values of x in a list: ")
-    x = list(map(float, input().split()))
+    print("Enter the values of x in a list:")
+    for i in range(n):
+        value = float(input())
+        x.append(value)
 
-    print("Enter the values of corresponding y: ")
-    y = list(map(float, input().split()))
+    print("Enter the values of corresponding y:")
+    for j in range(n):
+        value = float(input())
+        y.append(value)
 
     value_to_interpolate = float(input("Enter the value to interpolate:"))
 
