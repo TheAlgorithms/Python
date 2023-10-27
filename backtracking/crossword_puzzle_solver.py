236b159f2f881159d88159c69f6952b809ede1e0
Khushi Shukla <tusharika.shukla05@gmail.com>
2023-10-27 13:04:01 +0530

    Update crossword_puzzle_solver.py

--- backtracking/crossword_puzzle_solver.py
+++ backtracking/crossword_puzzle_solver.py
@@ -1,25 +1,33 @@
 # https://www.geeksforgeeks.org/solve-crossword-puzzle/
 
-
 def is_valid(puzzle: list[list[str]], word: str, row: int, col: int) -> bool:
     """
     Check if a word can be placed at the given position.
 
-    >>> puzzle = [['', '', '', ''], ['', '', '', ''], ['', '', '', ''], ['', '', '', '']]
+    >>> puzzle = [
+    ...     ['', '', '', ''],
+    ...     ['', '', '', ''],
+    ...     ['', '', '', ''],
+    ...     ['', '', '', '']
+    ... ]
     >>> is_valid(puzzle, 'word', 0, 0)
     True
     """
     for i in range(len(word)):
-        if row + i >= len(puzzle) or puzzle[row + i][col] != "":
+        if row + i >= len(puzzle) or puzzle[row + i][col] != '':
             return False
     return True
-
 
 def place_word(puzzle: list[list[str]], word: str, row: int, col: int) -> None:
     """
     Place a word at the given position.
 
-    >>> puzzle = [['', '', '', ''], ['', '', '', ''], ['', '', '', ''], ['', '', '', '']]
+    >>> puzzle = [
+    ...     ['', '', '', ''],
+    ...     ['', '', '', ''],
+    ...     ['', '', '', ''],
+    ...     ['', '', '', '']
+    ... ]
     >>> place_word(puzzle, 'word', 0, 0)
     >>> puzzle
     [['w', '', '', ''], ['o', '', '', ''], ['r', '', '', ''], ['d', '', '', '']]
@@ -27,25 +35,34 @@ def place_word(puzzle: list[list[str]], word: str, row: int, col: int) -> None:
     for i in range(len(word)):
         puzzle[row + i][col] = word[i]
 
-
 def remove_word(puzzle: list[list[str]], word: str, row: int, col: int) -> None:
     """
     Remove a word from the given position.
 
-    >>> puzzle = [['w', '', '', ''], ['o', '', '', ''], ['r', '', '', ''], ['d', '', '', '']]
+    >>> puzzle = [
+    ...     ['w', '', '', ''],
+    ...     ['o', '', '', ''],
+    ...     ['r', '', '', ''],
+    ...     ['d', '', '', '']
+    ... ]
     >>> remove_word(puzzle, 'word', 0, 0)
     >>> puzzle
     [['', '', '', ''], ['', '', '', ''], ['', '', '', ''], ['', '', '', '']]
     """
     for i in range(len(word)):
-        puzzle[row + i][col] = ""
-
+        puzzle[row + i][col] = ''
 
 def solve_crossword(puzzle: list[list[str]], words: list[str]) -> bool:
     """
     Solve the crossword puzzle using backtracking.
 
-    >>> puzzle = [['', '', '', ''], ['', '', '', ''], ['', '', '', ''], ['', '', '', '']]
+    >>> puzzle = [
+    ...     ['', '', '', ''],
+    ...     ['', '', '', ''],
+    ...     ['', '', '', ''],
+    ...     ['', '', '', '']
+    ... ]
+    
     >>> words = ['word', 'another', 'more', 'last']
     >>> solve_crossword(puzzle, words)
     True
