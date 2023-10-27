20f1e85b0f738847e74899f487783b7985976a20
Vinay Mallela <69084915+MD571@users.noreply.github.com>
2023-10-27 11:17:26 +0530

    added doctest  to sprialmatrix.py

--- data_structures/arrays/sprialmatrix.py
+++ data_structures/arrays/sprialmatrix.py
@@ -1,47 +1,49 @@
-def spiralorder(matrix):
+from typing import List
+
+def spiralOrder(matrix: List[List[int]]) -> List[int]:
     """
     The spiralorder function returns a list containing the values of matrix in spiral order.
-    Given a 2D array, print it in spiral form.
-    Examples:
+    Get the elements of a 2D matrix in spiral order.
 
-    Input:  {{1,    2,   3,   4},
-              {5,    6,   7,   8},
-             {9,   10,  11,  12},
-            {13,  14,  15,  16 }}
-    Output: 1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10
-    Explanation: The output is matrix in spiral format.
+    Args:
+    matrix (List[List[int]]): The input 2D matrix.
 
-    reference:
-    -https://en.wikipedia.org/wiki/Spiral_array_model
+    Returns:
+    List[int]: A list containing the elements of the matrix in spiral order.
+
+    Example:
+    >>> a = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+    >>> spiralOrder(a)
+    [1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10]
     """
     ans = []
 
-    if len(matrix) == 0:
+    if not matrix:
         return ans
 
     m = len(matrix)
     n = len(matrix[0])
-    seen = [[0 for i in range(n)] for j in range(m)]
+    seen = [[False for _ in range(n)] for _ in range(m)]
     dr = [0, 1, 0, -1]
     dc = [1, 0, -1, 0]
     x = 0
     y = 0
     di = 0
 
-    # Iterate from 0 to R * C - 1
     for i in range(m * n):
         ans.append(matrix[x][y])
         seen[x][y] = True
         cr = x + dr[di]
         cc = y + dc[di]
 
-        if cr >= 0 and cr < m and cc >= 0 and cc < n and not (seen[cr][cc]):
+        if 0 <= cr < m and 0 <= cc < n and not seen[cr][cc]:
             x = cr
             y = cc
         else:
             di = (di + 1) % 4
             x += dr[di]
             y += dc[di]
+
     return ans
 
 
