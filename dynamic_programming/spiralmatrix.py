975d6af087321adc81e8964dab0eeddd46db2381
shaman <shamanbusiness2004@gmail.com>
2023-10-27 17:18:38 +0530

    spiralmatrix

---
+++ dynamic_programming/spiralmatrix.py
@@ -0,0 +1,43 @@
+def spiral_order(matrix):
+    result = []
+    if not matrix:
+        return result
+
+    rows, cols = len(matrix), len(matrix[0])
+    top, bottom, left, right = 0, rows - 1, 0, cols - 1
+
+    while top <= bottom and left <= right:
+        # Traverse from left to right
+        for i in range(left, right + 1):
+            result.append(matrix[top][i])
+        top += 1
+
+        # Traverse from top to bottom
+        for i in range(top, bottom + 1):
+            result.append(matrix[i][right])
+        right -= 1
+
+        if top <= bottom:
+            # Traverse from right to left
+            for i in range(right, left - 1, -1):
+                result.append(matrix[bottom][i])
+            bottom -= 1
+
+        if left <= right:
+            # Traverse from bottom to top
+            for i in range(bottom, top - 1, -1):
+                result.append(matrix[i][left])
+            left += 1
+
+    return result
+
+# Example usage:
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+]
+
+result = spiral_order(matrix)
+print(' '.join(map(str, result)))
