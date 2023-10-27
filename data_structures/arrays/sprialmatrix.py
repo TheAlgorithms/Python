ce41af1bb06332d3c3364e0791d4c38731432c7b
Vinay Mallela <69084915+MD571@users.noreply.github.com>
2023-10-27 11:08:29 +0530

    Update sprialmatrix.py

--- data_structures/arrays/sprialmatrix.py
+++ data_structures/arrays/sprialmatrix.py
@@ -1,18 +1,19 @@
-"""
-Given a 2D array, print it in spiral form.
+def spiralorder(matrix):
+    """
+    Given a 2D array, print it in spiral form.
 
-Examples:
+    Examples:
 
-Input:  {{1,    2,   3,   4},
+    Input:  {{1,    2,   3,   4},
               {5,    6,   7,   8},
              {9,   10,  11,  12},
             {13,  14,  15,  16 }}
-Output: 1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10
-Explanation: The output is matrix in spiral format.
-"""
+    Output: 1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10
+    Explanation: The output is matrix in spiral format.
 
-
-def spiralorder(matrix):
+    reference: 
+    -https://en.wikipedia.org/wiki/Spiral_array_model
+    """
     ans = []
 
     if len(matrix) == 0:
