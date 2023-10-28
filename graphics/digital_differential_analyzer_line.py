93ed88bc563c69e7143233792ff2ad764a0f6629
SEIKH NABAB UDDIN <93948993+nababuddin@users.noreply.github.com>
2023-10-28 12:20:40 +0530

    Update digital_differential_analyzer_line.py

--- graphics/digital_differential_analyzer_line.py
+++ graphics/digital_differential_analyzer_line.py
@@ -1,12 +1,11 @@
 import matplotlib.pyplot as plt
 
-
 def digital_differential_analyzer_line(
     x1: int, y1: int, x2: int, y2: int
 ) -> list[tuple[int, int]]:
     """
-    Draws a line between two points using the Digital Differential Analyzer (DDA) algorithm.
-
+    Draws a line between two points using the DDA algorithm.
+    
     Args:
     - x1, y1: Coordinates of the starting point.
     - x2, y2: Coordinates of the ending point.
@@ -17,7 +16,7 @@ def digital_differential_analyzer_line(
     >>> digital_differential_analyzer_line(1, 1, 4, 4)
     [(2, 2), (3, 3), (4, 4)]
     """
-
+    
     dx = x2 - x1
     dy = y2 - y1
     steps = max(abs(dx), abs(dy))
@@ -32,12 +31,10 @@ def digital_differential_analyzer_line(
         coordinates.append((int(round(x)), int(round(y))))
     return coordinates
 
-
 if __name__ == "__main__":
     import doctest
-
     doctest.testmod()
-
+    
     x1 = int(input("Enter the x-coordinate of the starting point: "))
     y1 = int(input("Enter the y-coordinate of the starting point: "))
     x2 = int(input("Enter the x-coordinate of the ending point: "))
