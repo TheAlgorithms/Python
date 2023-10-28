e36a6488d536b7aee6c77d594d6d23c51d4a5e94
Aryansh Bhargavan <aryanshbhargavan@outlook.com>
2023-10-28 14:39:18 +0530

    Feat: Added Fast inverse square root

---
+++ maths/fast_inverse_sqrt.py
@@ -0,0 +1,34 @@
+"""
+Fast inverse square root.
+Reference: https://en.wikipedia.org/wiki/Fast_inverse_square_root
+"""
+
+import struct
+
+
+def fastInvSqrt(x):
+    """
+    Calculate the fast inverse square root of a number.
+
+    This function computes the fast inverse square root of a floating-point number
+    using the famous Quake III algorithm, originally developed by id Software.
+
+    :param float x: The input number for which to calculate the inverse square root.
+    :return float: The fast inverse square root of the input number.
+
+    Example:
+    >>> fastInvSqrt(10)
+    0.3156857923527257
+
+    >>> fastInvSqrt(4)
+    0.49915357479239103
+    """
+    i = struct.unpack('>i', struct.pack('>f', x))[0]
+    i = 0x5f3759df - (i >> 1)
+    y = struct.unpack('>f', struct.pack('>i', i))[0]
+    return y * (1.5 - 0.5 * x * y * y)
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod(verbose=True)
