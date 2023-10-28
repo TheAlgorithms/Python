6569aeaac09510b08ef7990c2597cb343e78cf1f
sudi050 <sudi050@gmail.com>
2023-10-29 02:09:42 +0530

    Update Ridge.py

--- machine_learning/Ridge.py
+++ machine_learning/Ridge.py
@@ -4,7 +4,7 @@ import time as t
 
 
 class RidgeRegression:
-    def __init__(self, learning_rate, iterations, l2_penality):
+    def __init__(self,X,Y):
         self.objvals = []
 
         # no_of_training_examples, no_of_features
@@ -19,16 +19,16 @@ class RidgeRegression:
 
         # gradient descent learning
 
-        for i in range(self.iterations):
+        for _ in range(self.iterations):
             Y_pred = self.predict(self.X)
-            hh = self.objval(Y, Y_pred, X)
+            hh = self.objval(Y, Y_pred)
 
             start = t.time()
             self.update_weights()
             end = t.time() - start
             self.timeiter.append(end)
             Y_pred = self.predict(self.X)
-            hj = self.objval(Y, Y_pred, X)
+            hj = self.objval(Y, Y_pred)
             self.objvals.append(hh)
             if abs(hh - hj) <= 0.0001e-05:
                 break
@@ -42,16 +42,16 @@ class RidgeRegression:
     # Helper function to update weights in gradient descent
 
     def update_weights(self):
-        Y_pred = self.predict(self.X)
+        y_pred = self.predict(self.X)
 
         # calculate gradients
-        dW = (
-            -(2 * (self.X.T).dot(self.Y - Y_pred)) + (2 * self.l2_penality * self.W)
+        dw = (
+            -(2 * (self.X.T).dot(self.Y - y_pred)) + (2 * self.l2_penality * self.W)
         ) / self.m
-        db = -2 * np.sum(self.Y - Y_pred) / self.m
+        db = -2 * np.sum(self.Y - y_pred) / self.m
 
         # update weights
-        self.W = self.W - self.learning_rate * dW
+        self.W = self.W - self.learning_rate * dw
         self.b = self.b - self.learning_rate * db
         return self
 
@@ -59,8 +59,7 @@ class RidgeRegression:
     def predict(self, X):
         return X.dot(self.W) + self.b
 
-    def objval(self, Y, pred, X):
-        h_1 = sum((Y - pred) ** 2)
+    def objval(self, Y, pred):
         h_2 = la.norm(self.W, 2) ** 2
         h_3 = self.l2_penality * h_2
         h = h_3 / self.m
