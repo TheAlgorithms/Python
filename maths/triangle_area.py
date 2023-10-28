2aa99c3411467570f4b5f3a893d384953f71e34a
HarshvMahawar <114311884+HarshvMahawar@users.noreply.github.com>
2023-10-29 04:39:12 +0530

    Create triangle_area.py

---
+++ maths/triangle_area.py
@@ -0,0 +1,18 @@
+# Function to calculate the area of a triangle
+def calculate_triangle_area(base, height):
+    area = 0.5 * base * height
+    return area
+
+# Get input for the base length and height from the user
+try:
+    base = float(input("Enter the base length of the triangle: "))
+    height = float(input("Enter the height of the triangle: "))
+    
+    # Check if both base and height are positive numbers
+    if base > 0 and height > 0:
+        triangle_area = calculate_triangle_area(base, height)
+        print(f"The area of the triangle is: {triangle_area}")
+    else:
+        print("Base and height must be positive numbers.")
+except ValueError:
+    print("Invalid input. Please enter valid numbers for the base and height.")
