672a6b8731876e876f5320422bf2d99cc71affac
Tapas Singhal <98687345+Shocker-lov-t@users.noreply.github.com>
2023-10-29 04:55:56 +0530

    Create magic_square.py

---
+++ data_structures/arrays/magic_square.py
@@ -0,0 +1,46 @@
+# https://www.codingninjas.com/studio/library/check-if-the-given-matrix-is-a-magic-square-or-not
+
+def is_magic_square(matrix):
+    """
+    Check if a given 3x3 matrix is a magic square.
+
+    Parameters:
+    - matrix (list): A 3x3 square matrix.
+
+    Returns:
+    - bool: True if it's a magic square, False otherwise.
+
+    >>> is_magic_square([[2, 7, 6], [9, 5, 1], [4, 3, 8]])
+    True
+    >>> is_magic_square([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    False
+    >>> is_magic_square([[16, 23, 17], [78, 32, 21], [17, 16, 15]])
+    False
+    """
+
+    # Check if the matrix is a 3x3 square matrix
+    if len(matrix) != 3 or any(len(row) != 3 for row in matrix):
+        return False
+
+    # Calculate the sum of the first row as the expected sum
+    expected_sum = sum(matrix[0])
+
+    # Check rows and columns
+    for i in range(3):
+        row_sum = sum(matrix[i])
+        col_sum = sum(matrix[j][i] for j in range(3))
+        if row_sum != expected_sum or col_sum != expected_sum:
+            return False
+
+    # Check diagonals
+    diagonal1_sum = sum(matrix[i][i] for i in range(3))
+    diagonal2_sum = sum(matrix[i][2 - i] for i in range(3))
+    if diagonal1_sum != expected_sum or diagonal2_sum != expected_sum:
+        return False
+
+    return True
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
