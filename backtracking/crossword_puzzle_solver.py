77863f63e4a041d832ff8fb94c21ced9f385da14
Christian Clauss <cclauss@me.com>
2023-10-28 18:58:49 +0200

    Update backtracking/crossword_puzzle_solver.py

--- backtracking/crossword_puzzle_solver.py
+++ backtracking/crossword_puzzle_solver.py
@@ -70,6 +70,9 @@ def solve_crossword(puzzle: list[list[str]], words: list[str]) -> bool:
     >>> words = ['word', 'four', 'more', 'last']
     >>> solve_crossword(puzzle, words)
     True
+    >>> words = ['word', 'four', 'more', 'paragraphs']
+    >>> solve_crossword(puzzle, words)
+    False
     """
     for row in range(len(puzzle)):
         for col in range(len(puzzle[0])):
