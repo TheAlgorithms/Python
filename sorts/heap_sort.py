e0027e6c725df0502b8ec400b166d3c7292184ea
Christian Clauss <cclauss@me.com>
2023-10-29 18:51:41 +0100

    Update heap_sort.py

--- sorts/heap_sort.py
+++ sorts/heap_sort.py
@@ -1,72 +1,70 @@
 """
-This is a pure Python implementation of the heap sort algorithm.
-
-For doctests run following command:
-python -m doctest -v heap_sort.py
-or
-python3 -m doctest -v heap_sort.py
-
-For manual testing run:
-python heap_sort.py
+A pure Python implementation of the heap sort algorithm.
 """
 
 
-def heapify(unsorted_list: list[int], index: int, heap_size: int) -> None:
+def heapify(unsorted: list[int], index: int, heap_size: int) -> None:
     """
-
-    :param unsorted_list: unsorted list containing integers numbers
+    :param unsorted: unsorted list containing integers numbers
     :param index: index
     :param heap_size: size of the heap
     :return: None
+    >>> unsorted = [1, 4, 3, 5, 2]
+    >>> heapify(unsorted, 0, len(unsorted))
+    >>> unsorted
+    [4, 5, 3, 1, 2]
+    >>> heapify(unsorted, 0, len(unsorted))
+    >>> unsorted
+    [5, 4, 3, 1, 2]
     """
     largest = index
     left_index = 2 * index + 1
     right_index = 2 * index + 2
-    if left_index < heap_size and unsorted_list[left_index] > unsorted_list[largest]:
+    if left_index < heap_size and unsorted[left_index] > unsorted[largest]:
         largest = left_index
 
-    if right_index < heap_size and unsorted_list[right_index] > unsorted_list[largest]:
+    if right_index < heap_size and unsorted[right_index] > unsorted[largest]:
         largest = right_index
 
     if largest != index:
-        unsorted_list[largest], unsorted_list[index] = (
-            unsorted_list[index],
-            unsorted_list[largest],
+        unsorted[largest], unsorted[index] = (
+            unsorted[index],
+            unsorted[largest],
         )
-        heapify(unsorted_list, largest, heap_size)
+        heapify(unsorted, largest, heap_size)
 
 
-def heap_sort(unsorted_list: list[int]) -> list[int]:
+def heap_sort(unsorted: list[int]) -> list[int]:
     """
-    Pure implementation of the heap sort algorithm in Python
+    A pure Python implementation of the heap sort algorithm
 
-    :param collection: some mutable ordered collection with heterogeneous
-    comparable items inside
+    :param collection: a mutable ordered collection of heterogeneous comparable items
     :return: the same collection ordered by ascending
 
     Examples:
     >>> heap_sort([0, 5, 3, 2, 2])
     [0, 2, 2, 3, 5]
-
     >>> heap_sort([])
     []
-
     >>> heap_sort([-2, -5, -45])
     [-45, -5, -2]
-
     >>> heap_sort([3, 7, 9, 28, 123, -5, 8, -30, -200, 0, 4])
     [-200, -30, -5, 0, 3, 4, 7, 8, 9, 28, 123]
     """
-    n = len(unsorted_list)
+    n = len(unsorted)
     for i in range(n // 2 - 1, -1, -1):
-        heapify(unsorted_list, i, n)
+        heapify(unsorted, i, n)
     for i in range(n - 1, 0, -1):
-        unsorted_list[0], unsorted_list[i] = unsorted_list[i], unsorted_list[0]
-        heapify(unsorted_list, 0, i)
-    return unsorted_list
+        unsorted[0], unsorted[i] = unsorted[i], unsorted[0]
+        heapify(unsorted, 0, i)
+    return unsorted
 
 
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
+    user_input = input("Enter numbers separated by a comma:\n").strip()
+    if user_input:
+        unsorted = [int(item) for item in user_input.split(",")]
+        print(f"{heap_sort(unsorted) = }")
