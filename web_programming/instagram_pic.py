6636eaef27920814814f2c3d4a3dd008e8987dd4
SEIKH NABAB UDDIN <93948993+nababuddin@users.noreply.github.com>
2023-10-29 11:22:04 +0530

    Update instagram_pic.py

--- web_programming/instagram_pic.py
+++ web_programming/instagram_pic.py
@@ -1,12 +1,48 @@
 from datetime import datetime
+from typing import Optional
 import requests
 from bs4 import BeautifulSoup
 
 
-def download_image(url):
+def validate_url(url: str) -> bool:
+    """
+    Validates the given URL.
+    >>> validate_url("https://www.example.com")
+    True
+    """
+    # Add URL validation logic here
+    return True
+
+
+def download_image_data(image_url: str) -> Optional[bytes]:
+    """
+    Downloads image data from the given URL.
+    >>> download_image_data("https://www.example.com/image.jpg")  # This is a hypothetical example; actual output may vary.
+    b'...'
+    """
+    try:
+        return requests.get(image_url).content
+    except requests.exceptions.RequestException:
+        return None
+
+
+def save_image(image_data: bytes, file_name: str) -> None:
+    """
+    Saves the image data to a file.
+    """
+    with open(file_name, "wb") as file:
+        file.write(image_data)
+
+
+def download_image(url: str) -> str:
+    """
+    Downloads an image from the given URL and saves it to a file.
+    >>> download_image("https://www.example.com")  # This is a hypothetical example; actual output may vary.
+    'Image downloaded and saved as image_2023-10-29_12:34:56.jpg'
+    """
     try:
         response = requests.get(url)
-        response.raise_for_status()  # Check for HTTP errors
+        response.raise_for_status()
 
         soup = BeautifulSoup(response.text, "html.parser")
         image_meta_tag = soup.find("meta", {"property": "og:image"})
@@ -14,24 +50,19 @@ def download_image(url):
         if image_meta_tag:
             image_url = image_meta_tag.get("content")
             if image_url:
-                image_data = requests.get(image_url).content
-
+                image_data = download_image_data(image_url)
                 if image_data:
-                    file_name = f"{datetime.now():%Y-%m-%d_%H:%M:%S}.jpg"
-                    with open(file_name, "wb") as file:
-                        file.write(image_data)
-                    print(f"Image downloaded and saved as {file_name}")
-                else:
-                    print("Failed to download the image.")
-            else:
-                print("Image URL not found in the meta tag.")
-        else:
-            print("No meta tag with 'og:image' property found.")
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred during the HTTP request: {e}")
+                    file_name = f"image_{datetime.now():%Y-%m-%d_%H:%M:%S}.jpg"
+                    save_image(image_data, file_name)
+                    return f"Image downloaded and saved as {file_name}"
+    except requests.exceptions.RequestException:
+        return "An error occurred during the HTTP request."
 
 
 if __name__ == "__main__":
     url = input("Enter image URL: ").strip()
-    print(f"Downloading image from {url} ...")
-    download_image(url)
+    if validate_url(url):
+        result = download_image(url)
+        print(result)
+    else:
+        print("Invalid URL. Please try again.")
