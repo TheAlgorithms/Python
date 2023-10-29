6fac36f200a69595fde9fb8eac69701c0ac18d5d
Christian Clauss <cclauss@me.com>
2023-10-29 18:07:45 +0100

    The power of enumerate()

--- dynamic_programming/wildcard_matching.py
+++ dynamic_programming/wildcard_matching.py
@@ -3,31 +3,17 @@ Author  : ilyas dahhou
 Date    : Oct 7, 2023
 
 Task:
-Given an input string (s) and a pattern (p), implement wildcard
-pattern matching with support for '?' and '*' where:
-
+Given an input string and a pattern, implement wildcard pattern matching with support
+for '?' and '*' where:
 '?' matches any single character.
 '*' matches any sequence of characters (including the empty sequence).
 The matching should cover the entire input string (not partial).
-
-Implementation notes:
-implementation Dynamic Programming up bottom approach.
 
 Runtime complexity:O(m * n)
 
 The implementation was tested on the
 leetcode: https://leetcode.com/problems/wildcard-matching/
-
-
-wildcard matching
-Dynamic Programming: top -> down.
-
 """
-from datetime import datetime, timedelta, timezone
-
-country_timezone = timezone(timedelta(hours=1))
-
-current_time_in_country = datetime.now(tz=country_timezone)
 
 
 def is_match(string: str, pattern: str) -> bool:
@@ -62,26 +48,25 @@ def is_match(string: str, pattern: str) -> bool:
     >>> is_match("", "")
     True
     """
-    m, n = len(string), len(pattern)
-    dp = [[False] * (n + 1) for _ in range(m + 1)]
+    dp = [[False] * (len(pattern) + 1) for _ in range(len(string) + 1)]
 
     # Base case
     dp[0][0] = True
 
     # Fill in the first row
-    for j in range(1, n + 1):
-        if pattern[j - 1] == "*":
+    for j, char in enumerate(pattern, 1):
+        if char == "*":
             dp[0][j] = dp[0][j - 1]
 
     # Fill in the rest of the DP table
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
-            if pattern[j - 1] == "?" or string[i - 1] == pattern[j - 1]:
+    for i, s_char in enumerate(string, 1):
+        for j, p_char in enumerate(pattern, 1):
+            if p_char in (s_char, "?"):
                 dp[i][j] = dp[i - 1][j - 1]
             elif pattern[j - 1] == "*":
                 dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
 
-    return dp[m][n]
+    return dp[len(string)][len(pattern)]
 
 
 if __name__ == "__main__":
