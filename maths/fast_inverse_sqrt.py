2a8fda4d226dea668ac371e9d3e2afcebb160b17
Aryansh Bhargavan <aryanshbhargavan@outlook.com>
2023-10-29 19:51:23 +0530

    Fix: Added doctests that break the code, changed var name

--- maths/fast_inverse_sqrt.py
+++ maths/fast_inverse_sqrt.py
@@ -6,27 +6,39 @@ Reference: https://en.wikipedia.org/wiki/Fast_inverse_square_root
 import struct
 
 
-def fastInvSqrt(x: float) -> float:
+def fast_inverse_sqrt(number: float) -> float:
     """
     Calculate the fast inverse square root of a number.
 
     This function computes the fast inverse square root of a floating-point number
     using the famous Quake III algorithm, originally developed by id Software.
 
-    :param float x: The input number for which to calculate the inverse square root.
+    :param float number: The input number for which to calculate the inverse square root.
     :return float: The fast inverse square root of the input number.
 
     Example:
-    >>> fastInvSqrt(10)
+    >>> fast_inverse_sqrt(10)
     0.3156857923527257
 
-    >>> fastInvSqrt(4)
+    >>> fast_inverse_sqrt(4)
     0.49915357479239103
+
+    >>> fast_inverse_sqrt(0)
+    Traceback (most recent call last):
+        ...
+    ValueError: Input must be a positive number.
+
+    >>> fast_inverse_sqrt(-1)
+    Traceback (most recent call last):
+        ...
+    ValueError: Input must be a positive number.
     """
-    i = struct.unpack('>i', struct.pack('>f', x))[0]
+    if number<=0:
+        raise ValueError("Input must be a positive number.")
+    i = struct.unpack('>i', struct.pack('>f', number))[0]
     i = 0x5f3759df - (i >> 1)
     y = struct.unpack('>f', struct.pack('>i', i))[0]
-    return y * (1.5 - 0.5 * x * y * y)
+    return y * (1.5 - 0.5 * number * y * y)
 
 if __name__ == "__main__":
     import doctest
