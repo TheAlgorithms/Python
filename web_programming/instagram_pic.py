c8f89ee2a10f72e926828a41ebcda367911a91be
Christian Clauss <cclauss@me.com>
2023-10-29 08:23:06 +0100

    Fast fail instead of nested ifs and PEP8: Keep try/except blocks small

--- web_programming/instagram_pic.py
+++ web_programming/instagram_pic.py
@@ -1,8 +1,7 @@
 from datetime import datetime
 
-from bs4 import BeautifulSoup
-
 import requests
+from bs4 import BeautifulSoup
 
 
 def download_image(url: str) -> str:
@@ -10,38 +9,39 @@ def download_image(url: str) -> str:
     Download an image from a given URL by scraping the 'og:image' meta tag.
 
     Parameters:
-        url (str): The URL to scrape.
+        url: The URL to scrape.
 
     Returns:
-        str: A message indicating the result of the operation.
+        A message indicating the result of the operation.
     """
     try:
         response = requests.get(url)
         response.raise_for_status()
-
-        soup = BeautifulSoup(response.text, "html.parser")
-        image_meta_tag = soup.find("meta", {"property": "og:image"})
-
-        if image_meta_tag:
-            image_url = image_meta_tag.get("content")
-            if image_url:
-                image_data = requests.get(image_url).content
-                if image_data:
-                    file_name = f"{datetime.now():%Y-%m-%d_%H:%M:%S}.jpg"
-                    with open(file_name, "wb") as file:
-                        file.write(image_data)
-                    return f"Image downloaded and saved as {file_name}"
-                else:
-                    return "Failed to download the image."
-            else:
-                return "Image URL not found in the meta tag."
-        else:
-            return "No meta tag with 'og:image' property found."
     except requests.exceptions.RequestException as e:
-        return f"An error occurred during the HTTP request: {e}"
+        return f"An error occurred during the HTTP request to {url}: {e!r}"
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    image_meta_tag = soup.find("meta", {"property": "og:image"})
+    if not image_meta_tag:
+        return "No meta tag with property 'og:image' was found."
+
+    image_url = image_meta_tag.get("content")
+    if not image_url:
+        return f"Image URL not found in meta tag {image_meta_tag}."
+
+    try:
+        image_data = requests.get(image_url).content
+    except requests.exceptions.RequestException as e:
+        return f"An error occurred during the HTTP request to {image_url}: {e!r}"
+    if not image_data:
+        return f"Failed to download the image from {image_url}."
+
+    file_name = f"{datetime.now():%Y-%m-%d_%H:%M:%S}.jpg"
+    with open(file_name, "wb") as file:
+        file.write(image_data)
+    return f"Image downloaded and saved in the file {file_name}"
 
 
 if __name__ == "__main__":
-    url = input("Enter image URL: ").strip()
-    result = download_image(url)
-    print(result)
+    url = input("Enter image URL: ").strip() or "https://www.instagram.com"
+    print(f"download_image({url}): {download_image(url)}")
