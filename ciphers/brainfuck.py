41c94b23b4099da874eec3741843a70d62df7322
RasButAss <samarth3tankasali@gmail.com>
2023-10-29 13:16:23 +0530

    Added wikipedia link to description

--- ciphers/brainfuck.py
+++ ciphers/brainfuck.py
@@ -14,6 +14,7 @@ import traceback
     Notable for its extreme minimalism, the language consists of only eight simple commands, a data pointer and an instruction pointer. While it is fully Turing complete, it is not intended for practical use, but to challenge and amuse programmers. Brainfuck requires one to break commands into microscopic steps.
 
     The language's name is a reference to the slang term brainfuck, which refers to things so complicated or unusual that they exceed the limits of one's understanding, as it was not meant or made for designing actual software but to challenge the boundaries of computer programming.
+    (https://en.wikipedia.org/wiki/Brainfuck)
 """
 
 
