aeae0a809aa5cc07321f1f0cdddaf77b3de17006
Christian Clauss <cclauss@me.com>
2023-10-29 21:54:19 +0100

    Update fast_inverse_sqrt.py

--- maths/fast_inverse_sqrt.py
+++ maths/fast_inverse_sqrt.py
@@ -1,6 +1,7 @@
 """
-Fast inverse square root.
+Fast inverse square root (1/sqrt(x)) using the Quake III algorithm.
 Reference: https://en.wikipedia.org/wiki/Fast_inverse_square_root
+Accuracy: https://en.wikipedia.org/wiki/Fast_inverse_square_root#Accuracy
 """
 
 import struct
@@ -8,10 +9,8 @@ import struct
 
 def fast_inverse_sqrt(number: float) -> float:
     """
-    Calculate the fast inverse square root of a number.
-
-    This function computes the fast inverse square root of a floating-point number
-    using the famous Quake III algorithm, originally developed by id Software.
+    Compute the fast inverse square root of a floating-point number using the famous
+    Quake III algorithm.
 
     :param float number: Input number for which to calculate the inverse square root.
     :return float: The fast inverse square root of the input number.
@@ -19,19 +18,22 @@ def fast_inverse_sqrt(number: float) -> float:
     Example:
     >>> fast_inverse_sqrt(10)
     0.3156857923527257
-
     >>> fast_inverse_sqrt(4)
     0.49915357479239103
-
+    >>> fast_inverse_sqrt(4.1)
+    0.4932849504615651
     >>> fast_inverse_sqrt(0)
     Traceback (most recent call last):
         ...
     ValueError: Input must be a positive number.
-
     >>> fast_inverse_sqrt(-1)
     Traceback (most recent call last):
         ...
     ValueError: Input must be a positive number.
+    >>> from math import isclose, sqrt
+    >>> all(isclose(fast_inverse_sqrt(i), 1 / sqrt(i), rel_tol=0.00132)
+    ...     for i in range(50, 60))
+    True
     """
     if number <= 0:
         raise ValueError("Input must be a positive number.")
@@ -42,6 +44,11 @@ def fast_inverse_sqrt(number: float) -> float:
 
 
 if __name__ == "__main__":
-    import doctest
+    from doctest import testmod
 
-    doctest.testmod(verbose=True)
+    testmod()
+    # https://en.wikipedia.org/wiki/Fast_inverse_square_root#Accuracy
+    from math import sqrt
+
+    for i in range(5, 101, 5):
+        print(f"{i:>3}: {(1 / sqrt(i)) - fast_inverse_sqrt(i):.5f}")
