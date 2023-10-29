7b8af150b1312f9f028ead1aefeca7b1ae311073
Christian Clauss <cclauss@me.com>
2023-10-29 11:21:00 +0100

    Update find_previous_power_of_two.py

--- bit_manipulation/find_previous_power_of_two.py
+++ bit_manipulation/find_previous_power_of_two.py
@@ -1,20 +1,10 @@
-# https://stackoverflow.com/questions/1322510/given-an-integer-how-do-i-find-the-next-largest-power-of-two-using-bit-twiddlin
-
-
 def find_previous_power_of_two(number: int) -> int:
     """
     Find the largest power of two that is less than or equal to a given integer.
+    https://stackoverflow.com/questions/1322510
 
-    >>> find_previous_power_of_two(10)
-    8
-    >>> find_previous_power_of_two(16)
-    16
-    >>> find_previous_power_of_two(5)
-    4
-    >>> find_previous_power_of_two(1)
-    1
-    >>> find_previous_power_of_two(0)
-    0
+    >>> [find_previous_power_of_two(i) for i in range(18)]
+    [0, 1, 2, 2, 4, 4, 4, 4, 8, 8, 8, 8, 8, 8, 8, 8, 16, 16]
     >>> find_previous_power_of_two(-5)
     Traceback (most recent call last):
         ...
@@ -22,22 +12,15 @@ def find_previous_power_of_two(number: int) -> int:
     >>> find_previous_power_of_two(10.5)
     Traceback (most recent call last):
         ...
-    TypeError: Input must be an integer
+    ValueError: Input must be a non-negative integer
     """
-
-    if not isinstance(number, int):
-        raise TypeError("Input must be an integer")
-
-    if number < 0:
+    if not isinstance(number, int) or number < 0:
         raise ValueError("Input must be a non-negative integer")
-
     if number == 0:
         return 0
-
     power = 1
     while power <= number:
         power <<= 1  # Equivalent to multiplying by 2
-
     return power >> 1 if number > 1 else 1
 
 
