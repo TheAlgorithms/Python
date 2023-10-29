61219b40fc9380a6cc00c6049ac4d879472ece21
Manpreet Singh <63737630+ManpreetSingh2004@users.noreply.github.com>
2023-10-29 17:54:24 +0530

    Add prime helper fn in prime_sieve_eratosthenes.py

--- maths/prime_sieve_eratosthenes.py
+++ maths/prime_sieve_eratosthenes.py
@@ -11,6 +11,9 @@ you can read in detail about this at
 https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
 """
 
+import numpy as np
+from math import isqrt
+
 
 def prime_sieve_eratosthenes(num: int) -> list[int]:
     """
@@ -43,6 +46,34 @@ def prime_sieve_eratosthenes(num: int) -> list[int]:
         p += 1
 
     return [prime for prime in range(2, num + 1) if primes[prime]]
+
+
+def np_prime_sieve_eratosthenes(max_number: int) -> list[int]:
+    """
+    Returns prime numbers below max_number.
+    See: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+
+    >>> np_prime_sieve_eratosthenes(10)
+    [2, 3, 5, 7]
+    >>> np_prime_sieve_eratosthenes(2)
+    [2]
+    >>> np_prime_sieve_eratosthenes(1)
+    []
+    """
+    if max_number < 2:
+        return []
+
+    # List containing a bool value for every odd number below max_number/2
+    is_prime = np.ones((max_number + 1) // 2, dtype=bool)
+
+    for i in range(3, isqrt(max_number - 1) + 1, 2):
+        if is_prime[i // 2]:
+            # Mark all multiple of i as not prime using list slicing
+            is_prime[i**2 // 2 :: i] = False
+
+    primes = np.where(is_prime)[0] * 2 + 1
+    primes[0] = 2
+    return primes.tolist()
 
 
 if __name__ == "__main__":
