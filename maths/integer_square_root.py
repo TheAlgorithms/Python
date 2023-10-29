53a10dfdd862385a5ce0188002f7fd84e3f46cc0
Christian Clauss <cclauss@me.com>
2023-10-29 11:51:45 +0100

    Update integer_square_root.py

--- maths/integer_square_root.py
+++ maths/integer_square_root.py
@@ -1,11 +1,8 @@
 """
-Integer Square Root Algorithm
-
-This module provides an efficient method to calculate the square root of a
-non-negative integer 'num' rounded down to the nearest integer. It uses
-a binary search approach to find the integer square root without using any
-built-in exponent functions or operators.
-
+Integer Square Root Algorithm -- An efficient method to calculate the square root of a
+non-negative integer 'num' rounded down to the nearest integer. It uses a binary search
+approach to find the integer square root without using any built-in exponent functions
+or operators.  https://en.wikipedia.org/wiki/Integer_square_root
 Note:
     - This algorithm is designed for non-negative integers only.
     - The result is rounded down to the nearest integer.
@@ -17,44 +14,50 @@ Note:
 def integer_square_root(num: int) -> int:
     """
     Returns the integer square root of a non-negative integer num.
-
     Args:
         num: A non-negative integer.
-
     Returns:
         The integer square root of num.
-
     Raises:
-        ValueError: If num is negative.
-
-    >>> integer_square_root(0)
-    0
-    >>> integer_square_root(1)
-    1
-    >>> integer_square_root(4)
-    2
+        ValueError: If num is not an integer or is negative.
+    >>> [integer_square_root(i) for i in range(18)]
+    [0, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4]
     >>> integer_square_root(625)
     25
-    >>> integer_square_root(9)
-    3
-    >>> integer_square_root(10)
-    3
+    >>> integer_square_root(2_147_483_647)
+    46340
+    >>> from math import sqrt
+    >>> all(integer_square_root(i) == int(sqrt(i)) for i in range(20))
+    True
+    >>> integer_square_root(-1)
+    Traceback (most recent call last):
+        ...
+    ValueError: num must be non-negative integer
+    >>> integer_square_root(1.5)
+    Traceback (most recent call last):
+        ...
+    ValueError: num must be non-negative integer
+    >>> integer_square_root("0")
+    Traceback (most recent call last):
+        ...
+    ValueError: num must be non-negative integer
     """
-    if num < 0:
-        raise ValueError("num must be non-negative")
+    if not isinstance(num, int) or num < 0:
+        raise ValueError("num must be non-negative integer")
 
     if num < 2:
         return num
 
-    left_bound, right_bound = 0, num // 2
+    left_bound = 0
+    right_bound = num // 2
 
     while left_bound <= right_bound:
         mid = left_bound + (right_bound - left_bound) // 2
         mid_squared = mid * mid
-
         if mid_squared == num:
             return mid
-        elif mid_squared < num:
+
+        if mid_squared < num:
             left_bound = mid + 1
         else:
             right_bound = mid - 1
