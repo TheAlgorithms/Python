650e8f972c62c958d0d3277265beead45b2a0e98
Shreya123714 <95279016+Shreya123714@users.noreply.github.com>
2023-10-30 11:28:02 +0530

    Update fast_fourier_transform.py
    reduce the comment size, by dividing it in two lines, lowercased the variable

--- physics/fast_fourier_transform.py
+++ physics/fast_fourier_transform.py
@@ -1,8 +1,10 @@
 """
 # Fast Fourier Transform (FFT) Implementation in Python
 
-This Python script provides a basic demonstration of the Fast Fourier Transform (FFT) algorithm without the use of external libraries like NumPy.
-The FFT is a mathematical algorithm used to transform a signal from the time domain to the frequency domain, revealing its frequency components.
+This Python script provides a basic demonstration of the Fast Fourier Transform (FFT)
+algorithm without the use of external libraries like NumPy.
+The FFT is a mathematical algorithm used to transform a signal from the time domain
+to the frequency domain, revealing its frequency components.
 
 """
 
@@ -11,16 +13,16 @@ import math
 
 # Define a function to compute the DFT
 def dft(signal):
-    N = len(signal)
+    n = len(signal)
     dft_result = []
 
-    for k in range(N):
-        Xk_real, Xk_imag = 0, 0
+    for k in range(n):
+        xk_real, xk_imag = 0, 0
         for n in range(N):
             angle = 2 * math.pi * k * n / N
-            Xk_real += signal[n] * math.cos(angle)
-            Xk_imag -= signal[n] * math.sin(angle)
-        dft_result.append((Xk_real, Xk_imag))
+            xk_real += signal[n] * math.cos(angle)
+            xk_imag -= signal[n] * math.sin(angle)
+        dft_result.append((xk_real, xk_imag))
 
     return dft_result
 
