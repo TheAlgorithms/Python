ea7b31a4eb60bf83e3406c63d02a2a1861297b74
SAIMADHURI KOLA <110275881+SaimadhuriKola@users.noreply.github.com>
2023-10-30 22:45:30 +0530

    Create greedy.py

---
+++ greedy_methods/greedy.py
@@ -0,0 +1,49 @@
+A greedy algorithm is an algorithmic paradigm that follows the problem-solving heuristic of making the locally optimal choice at each stage with the hope of finding a global optimum. In other words, it makes the best choice at each step without considering the long-term consequences. Greedy algorithms are often used for optimization problems, where the goal is to find the best solution among a set of possible solutions.
+
+Here's a detailed explanation and an example of a greedy algorithm in Python:
+
+**Example: The Coin Change Problem**
+
+The Coin Change Problem is a classic example of a problem that can be solved using a greedy algorithm. Given a set of coin denominations and a target amount, the goal is to find the minimum number of coins required to make up that amount.
+
+```python
+def greedy_coin_change(coins, target_amount):
+    # Sort the coins in descending order (if not already sorted).
+    coins.sort(reverse=True)
+    
+    num_coins = 0
+    change = []
+
+    for coin in coins:
+        while target_amount >= coin:
+            target_amount -= coin
+            num_coins += 1
+            change.append(coin)
+
+    if target_amount == 0:
+        return num_coins, change
+    else:
+        return "No exact change possible."
+
+# Example usage:
+coins = [25, 10, 5, 1]
+target_amount = 63
+
+min_coins, change = greedy_coin_change(coins, target_amount)
+print(f"Minimum number of coins: {min_coins}")
+print(f"Change: {change}")
+```
+
+Explanation:
+
+1. Sort the list of coin denominations in descending order. This is a crucial step in the greedy algorithm to ensure you use the largest possible coin at each step.
+
+2. Initialize variables `num_coins` to keep track of the number of coins used and `change` to store the actual coins used.
+
+3. Iterate through the sorted coins. While the target amount is greater than or equal to the current coin denomination, subtract the coin from the target amount, increment the `num_coins`, and add the coin to the `change` list.
+
+4. If the target amount becomes zero, it means you've made exact change using the greedy approach. Return the number of coins used and the list of coins used.
+
+5. If there's a remaining amount after iterating through all the coins, it means that the exact change is not possible using the available coin denominations. Return an appropriate message.
+
+In the provided example, the greedy algorithm will return the minimum number of coins and the actual coins used to make change for the given target amount. This is just one of many examples where a greedy algorithm can be applied to solve optimization problems.
