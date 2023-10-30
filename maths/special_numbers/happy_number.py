5ebf0991c34f933af549ffa748f1a8ffd427a7d8
Devashri Deulkar <95555641+Devadeut@users.noreply.github.com>
2023-10-30 14:21:51 +0530

    Update happy_number.py
    added new changes

--- maths/special_numbers/happy_number.py
+++ maths/special_numbers/happy_number.py
@@ -2,14 +2,14 @@ def is_happy_number(number: int) -> bool:
     """
     Check if a number is a happy number.
     https://en.wikipedia.org/wiki/Happy_number
-     A happy number is defined by the following process:
+    A happy number is defined by the following process:
     1. Starting with any positive integer, replace the number by
        the sum of the squares of its digits.
     2. Repeat the process until the number equals 1 (happy) or
        it loops endlessly in a cycle (not happy).
 
     Args:
-        n (int): The number to check for happiness.
+        number (int): The number to check for happiness.
 
     Returns:
         bool: True if the number is a happy number, False otherwise.
@@ -22,13 +22,13 @@ def is_happy_number(number: int) -> bool:
     >>> is_happy_number(23)
     True
     >>> is_happy_number(0)
-    ?
+    False
     >>> is_happy_number(-19)
-    ?
+    False
     >>> is_happy_number(19.1)
-    ?
+    False
     >>> is_happy_number("Happy")
-    ?
+    False
     """
 
     # Create a set to store seen numbers and detect cycles
