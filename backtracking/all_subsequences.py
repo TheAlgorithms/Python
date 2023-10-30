44f76bf2c6848380c9a0fd8ed8285c020a31f29a
Abhignya Tayi <abhignyatayi@Abhignyas-MacBook-Air.local>
2023-10-30 17:20:43 +0530

    Your meaningful commit message here

--- backtracking/all_subsequences.py
+++ backtracking/all_subsequences.py
@@ -1,37 +1,52 @@
 """
-In this problem, we want to determine all possible subsequences
-of the given sequence. We use backtracking to solve this problem.
+In this problem, we find all subsequences of a given list using backtracking.
+Time Complexity: O(2^n), where n is the list's length.
 
-Time complexity: O(2^n),
-where n denotes the length of the given sequence.
+Functions:
+- generate_all_subsequences: Initiates backtracking and prints subsequences.
+- create_state_space_tree: Recursively builds a state-space tree for subsequences.
 """
-from __future__ import annotations
 
+from __future__ import annotations
 from typing import Any
 
-
 def generate_all_subsequences(sequence: list[Any]) -> None:
-    create_state_space_tree(sequence, [], 0)
+    """Kickstarts the backtracking algorithm.
 
+    Args:
+    - sequence (list[Any]): Input list of elements.
+
+    Returns:
+    None: Outputs all subsequences to stdout.
+    """
+    create_state_space_tree(sequence, [], 0)
 
 def create_state_space_tree(
     sequence: list[Any], current_subsequence: list[Any], index: int
 ) -> None:
-    """
-    Creates a state space tree to iterate through each branch using DFS.
-    We know that each state has exactly two children.
-    It terminates when it reaches the end of the given sequence.
-    """
+    """Recursively constructs a state-space tree for subsequences.
 
+    Args:
+    - sequence (list[Any]): Original list.
+    - current_subsequence (list[Any]): Current subsequence.
+    - index (int): Current index in the original list.
+
+    Returns:
+    None: Outputs subsequences when reaching the end of the list.
+    """
     if index == len(sequence):
         print(current_subsequence)
         return
 
+    # Exclude current element and proceed.
     create_state_space_tree(sequence, current_subsequence, index + 1)
+    
+    # Include current element and proceed.
     current_subsequence.append(sequence[index])
     create_state_space_tree(sequence, current_subsequence, index + 1)
+    
+    # Backtrack.
     current_subsequence.pop()
-
 
 if __name__ == "__main__":
     seq: list[Any] = [3, 1, 2, 4]
