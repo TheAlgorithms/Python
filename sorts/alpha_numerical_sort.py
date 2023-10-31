43a313c40c8112c9c9102e195786a1e02f43f91d
Caio Emmanuel <87953966+cemmanuelsr@users.noreply.github.com>
2023-10-31 08:41:07 -0300

    Making lines shorter

--- sorts/alpha_numerical_sort.py
+++ sorts/alpha_numerical_sort.py
@@ -1,20 +1,24 @@
 """
-    https://en.wikipedia.org/wiki/Natural_sort_order
+https://en.wikipedia.org/wiki/Natural_sort_order
 
-    In computing, natural sort order (or natural sorting) is the ordering of strings in alphabetical order,
-    except that multi-digit numbers are treated atomically, i.e., as if they were a single character. Natural sort order
-    has been promoted as being more human-friendly ("natural") than machine-oriented, pure alphabetical sort order.
+In computing, natural sort order (or natural sorting) is the ordering
+of strings in alphabetical order, except that multi-digit numbers are
+treated atomically, i.e., as if they were a single character.
+Natural sort order has been promoted as being more human-friendly
+("natural") than machine-oriented, pure alphabetical sort order.
 
-    For example, in alphabetical sorting, "z11" would be sorted before "z2" because the "1" in the first string is sorted as smaller
-    than "2", while in natural sorting "z2" is sorted before "z11" because "2" is treated as smaller than "11".
+For example, in alphabetical sorting, "z11" would be sorted before
+"z2" because the "1" in the first string is sorted as smaller
+than "2", while in natural sorting "z2" is sorted before "z11"
+because "2" is treated as smaller than "11".
 
-    Alphabetical sorting:
-    1.z11
-    2.z2
+Alphabetical sorting:
+1.z11
+2.z2
 
-    Natural sorting:
-    1. z2
-    2. z11
+Natural sorting:
+1. z2
+2. z11
 """
 
 from collections.abc import Iterable
@@ -22,7 +26,8 @@ from collections.abc import Iterable
 
 def alpha_numerical_sort(unsorted: Iterable) -> Iterable:
     """
-    This function is used to alpha numerical sort an unsorted iterable as a list or a set
+    This function is used to alpha numerical sort an unsorted
+    iterable as a list or a set
     Args:
         unsorted: Iterable object
     Returns:
