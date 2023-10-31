c052db7cd6b9d537ada65082fef10d22116936b8
Abhignya Tayi <abhignyatayi@Abhignyas-MacBook-Air.local>
2023-10-31 20:52:23 +0530

    Updated the readability of code

--- strings/frequency_finder.py
+++ strings/frequency_finder.py
@@ -2,9 +2,10 @@
 
 import string
 
-# frequency taken from https://en.wikipedia.org/wiki/Letter_frequency
+# Frequency of each English letter taken from https://en.wikipedia.org/wiki/Letter_frequency
 english_letter_freq = {
-    "E": 12.70,
+    # Letter frequencies in percentages
+     "E": 12.70,
     "T": 9.06,
     "A": 8.17,
     "O": 7.51,
@@ -31,73 +32,80 @@ english_letter_freq = {
     "Q": 0.10,
     "Z": 0.07,
 }
+
+# Ordering of English letters from most to least frequent
 ETAOIN = "ETAOINSHRDLCUMWFGYPBVKJXQZ"
 LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-
 def get_letter_count(message: str) -> dict[str, int]:
+    """
+    Count the occurrence of each letter in the given message.
+    """
+    # Initialize a dictionary to store the count of each letter
     letter_count = {letter: 0 for letter in string.ascii_uppercase}
+
+    # Count each uppercase letter in the message
     for letter in message.upper():
         if letter in LETTERS:
             letter_count[letter] += 1
 
     return letter_count
 
-
 def get_item_at_index_zero(x: tuple) -> str:
+    """
+    Helper function to get the first item in a tuple.
+    """
     return x[0]
-
 
 def get_frequency_order(message: str) -> str:
     """
-    Get the frequency order of the letters in the given string
-    >>> get_frequency_order('Hello World')
-    'LOWDRHEZQXJKVBPYGFMUCSNIAT'
-    >>> get_frequency_order('Hello@')
-    'LHOEZQXJKVBPYGFWMUCDRSNIAT'
-    >>> get_frequency_order('h')
-    'HZQXJKVBPYGFWMUCLDRSNIOATE'
+    Get the frequency order of the letters in the given message.
     """
+    # Get the letter count in the message
     letter_to_freq = get_letter_count(message)
-    freq_to_letter: dict[int, list[str]] = {
-        freq: [] for letter, freq in letter_to_freq.items()
-    }
+
+    # Create a dictionary to map frequency to letters
+    freq_to_letter = {freq: [] for letter, freq in letter_to_freq.items()}
+
+    # Populate the dictionary
     for letter in LETTERS:
         freq_to_letter[letter_to_freq[letter]].append(letter)
 
-    freq_to_letter_str: dict[int, str] = {}
-
+    # Create a dictionary to store sorted letters for each frequency
+    freq_to_letter_str = {}
     for freq in freq_to_letter:
         freq_to_letter[freq].sort(key=ETAOIN.find, reverse=True)
         freq_to_letter_str[freq] = "".join(freq_to_letter[freq])
 
+    # Sort by frequency
     freq_pairs = list(freq_to_letter_str.items())
     freq_pairs.sort(key=get_item_at_index_zero, reverse=True)
 
-    freq_order: list[str] = [freq_pair[1] for freq_pair in freq_pairs]
+    # Create a string of letters sorted by their frequency in the message
+    freq_order = [freq_pair[1] for freq_pair in freq_pairs]
 
     return "".join(freq_order)
 
-
 def english_freq_match_score(message: str) -> int:
     """
-    >>> english_freq_match_score('Hello World')
-    1
+    Calculate a match score based on how many of the six most frequent
+    and six least frequent letters are present in the message.
     """
     freq_order = get_frequency_order(message)
     match_score = 0
+
+    # Check how many of the six most common English letters are in the most common letters of the message
     for common_letter in ETAOIN[:6]:
         if common_letter in freq_order[:6]:
             match_score += 1
 
+    # Check how many of the six least common English letters are in the least common letters of the message
     for uncommon_letter in ETAOIN[-6:]:
         if uncommon_letter in freq_order[-6:]:
             match_score += 1
 
     return match_score
 
-
 if __name__ == "__main__":
     import doctest
-
     doctest.testmod()
