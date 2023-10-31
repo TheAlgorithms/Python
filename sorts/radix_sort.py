7ee4dea9fc64baa1119126878c95676be3e08835
Peilin Li <61273074+Ross-Li@users.noreply.github.com>
2023-10-31 11:51:38 -0400

    Use a separate flatten function
    Use a separate flatten function to increase performance, readability and modularity.

--- sorts/radix_sort.py
+++ sorts/radix_sort.py
@@ -2,11 +2,18 @@
 This is a pure Python implementation of the radix sort algorithm
 
 Source: https://en.wikipedia.org/wiki/Radix_sort
+Source of flatten_extend(): How to Flatten a List of Lists in Python 
+https://realpython.com/python-flatten-list/#considering-performance-while-flattening-your-lists
 """
 from __future__ import annotations
 
 RADIX = 10
 
+def flatten_extend(list_of_lists: list[list]) -> list[int]:
+    flat_list = []
+    for l in list_of_lists:
+        flat_list.extend(row)
+    return flat_list
 
 def radix_sort(list_of_ints: list[int]) -> list[int]:
     """
@@ -30,13 +37,9 @@ def radix_sort(list_of_ints: list[int]) -> list[int]:
         for i in list_of_ints:
             tmp = int((i / placement) % RADIX)
             buckets[tmp].append(i)
-        # put each buckets' contents into list_of_ints
-        a = 0
-        for b in range(RADIX):
-            for i in buckets[b]:
-                list_of_ints[a] = i
-                a += 1
-        # move to next
+        # Flatten each buckets' contents back into list_of_ints
+        list_of_ints = flatten_extend(buckets)
+        # move to next 
         placement *= RADIX
     return list_of_ints
 
