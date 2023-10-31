021731114bf80db20c270e13ca3ecbd4309234a2
Abhignya Tayi <abhignyatayi@Abhignyas-MacBook-Air.local>
2023-10-31 13:59:22 +0530

    Update aho_corasick.py
    Updated the documentation

--- strings/aho_corasick.py
+++ strings/aho_corasick.py
@@ -1,96 +1,55 @@
 from __future__ import annotations
 
-from collections import deque
+import collections
+import pprint
+from pathlib import Path
 
+# Function to sort the characters in a given word
+def signature(word: str) -> str:
+    """
+    Sorts the characters in the given word.
+    
+    >>> signature("test")
+    'estt'
+    >>> signature("this is a test")
+    '   aehiisssttt'
+    >>> signature("finaltest")
+    'aefilnstt'
+    """
+    return "".join(sorted(word))
 
-class Automaton:
-    def __init__(self, keywords: list[str]):
-        self.adlist: list[dict] = []
-        self.adlist.append(
-            {"value": "", "next_states": [], "fail_state": 0, "output": []}
-        )
+# Function to find anagrams of a given word
+def anagram(my_word: str) -> list[str]:
+    """
+    Finds all the anagrams of the given word.
+    
+    >>> anagram('test')
+    ['sett', 'stet', 'test']
+    >>> anagram('this is a test')
+    []
+    >>> anagram('final')
+    ['final']
+    """
+    return word_by_signature[signature(my_word)]
 
-        for keyword in keywords:
-            self.add_keyword(keyword)
-        self.set_fail_transitions()
+# Read the word list from the file
+data: str = Path(__file__).parent.joinpath("words.txt").read_text(encoding="utf-8")
+# Sort and remove duplicates
+word_list = sorted({word.strip().lower() for word in data.splitlines()})
 
-    def find_next_state(self, current_state: int, char: str) -> int | None:
-        for state in self.adlist[current_state]["next_states"]:
-            if char == self.adlist[state]["value"]:
-                return state
-        return None
+# Create a dictionary to hold the anagrams
+word_by_signature = collections.defaultdict(list)
 
-    def add_keyword(self, keyword: str) -> None:
-        current_state = 0
-        for character in keyword:
-            next_state = self.find_next_state(current_state, character)
-            if next_state is None:
-                self.adlist.append(
-                    {
-                        "value": character,
-                        "next_states": [],
-                        "fail_state": 0,
-                        "output": [],
-                    }
-                )
-                self.adlist[current_state]["next_states"].append(len(self.adlist) - 1)
-                current_state = len(self.adlist) - 1
-            else:
-                current_state = next_state
-        self.adlist[current_state]["output"].append(keyword)
+# Populate the dictionary
+for word in word_list:
+    word_by_signature[signature(word)].append(word)
 
-    def set_fail_transitions(self) -> None:
-        q: deque = deque()
-        for node in self.adlist[0]["next_states"]:
-            q.append(node)
-            self.adlist[node]["fail_state"] = 0
-        while q:
-            r = q.popleft()
-            for child in self.adlist[r]["next_states"]:
-                q.append(child)
-                state = self.adlist[r]["fail_state"]
-                while (
-                    self.find_next_state(state, self.adlist[child]["value"]) is None
-                    and state != 0
-                ):
-                    state = self.adlist[state]["fail_state"]
-                self.adlist[child]["fail_state"] = self.find_next_state(
-                    state, self.adlist[child]["value"]
-                )
-                if self.adlist[child]["fail_state"] is None:
-                    self.adlist[child]["fail_state"] = 0
-                self.adlist[child]["output"] = (
-                    self.adlist[child]["output"]
-                    + self.adlist[self.adlist[child]["fail_state"]]["output"]
-                )
-
-    def search_in(self, string: str) -> dict[str, list[int]]:
-        """
-        >>> A = Automaton(["what", "hat", "ver", "er"])
-        >>> A.search_in("whatever, err ... , wherever")
-        {'what': [0], 'hat': [1], 'ver': [5, 25], 'er': [6, 10, 22, 26]}
-        """
-        result: dict = {}  # returns a dict with keywords and list of its occurrences
-        current_state = 0
-        for i in range(len(string)):
-            while (
-                self.find_next_state(current_state, string[i]) is None
-                and current_state != 0
-            ):
-                current_state = self.adlist[current_state]["fail_state"]
-            next_state = self.find_next_state(current_state, string[i])
-            if next_state is None:
-                current_state = 0
-            else:
-                current_state = next_state
-                for key in self.adlist[current_state]["output"]:
-                    if key not in result:
-                        result[key] = []
-                    result[key].append(i - len(key) + 1)
-        return result
-
-
+# Main execution
 if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod()
+    # Create a dictionary of all anagrams where the word has more than one anagram
+    all_anagrams = {word: anagram(word) for word in word_list if len(anagram(word)) > 1}
+    
+    # Save the anagrams to a text file
+    with open("anagrams.txt", "w") as file:
+        file.write("all_anagrams = \n")
+        file.write(pprint.pformat(all_anagrams))
