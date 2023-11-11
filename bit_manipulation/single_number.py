e6624519e8ed0aec445e78bfab1396eba541d9c8
eukub <ekublin@gmail.com>
2023-11-11 14:02:49 +0300

    Algorithm solving problem "136. Single Number" from Leetcode (https://leetcode.com/problems/single-number/)

--- bit_manipulation/single_number.py
+++ bit_manipulation/single_number.py
@@ -1,6 +1,6 @@
 """
     Given a non-empty array of integers nums, every element
-    appears twice except for one. Find that single one.
+    appears twice except for one. Find that single one
 
     You must implement a solution with a linear runtime complexity
     and use only constant extra space.
@@ -14,6 +14,17 @@ def single_number(nums: list) -> int:
     :param nums: A non-empty array of any integers nums,
                  every element appears twice except for one.
     :return: element that appears only one time
+
+    Examples:
+        Example 1
+            Input: nums = [1, 3, 3, 2, 6, 2, 1]
+            Output: 6
+        Example 2
+            Input: nums = [12, 1, 1, 7, 1, 12, 1]
+            Output: 7
+        Example 3
+            Input: nums = [6]
+            Output: 6
     """
     result = 0
 
