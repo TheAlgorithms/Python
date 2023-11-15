602bca66c589ca381252247847adca7111d27db3
Harsh Kumar <61012869+cyrixninja@users.noreply.github.com>
2023-11-15 16:17:55 +0530

    Fixed Issues

--- maths/spearman_rank_correlation_coefficient.py
+++ maths/spearman_rank_correlation_coefficient.py
@@ -1,28 +1,29 @@
-def rankdata(arr):
+from typing import List
+
+def assign_ranks(data: List[float]) -> List[int]:
     """
     Assigns ranks to elements in the array.
 
-    :param arr: List of floats.
+    :param data: List of floats.
     :return: List of ints representing the ranks.
 
     Example:
-    >>> rankdata([3.2, 1.5, 4.0, 2.7, 5.1])
+    >>> assign_ranks([3.2, 1.5, 4.0, 2.7, 5.1])
     [3, 1, 4, 2, 5]
 
-    >>> rankdata([10.5, 8.1, 12.4, 9.3, 11.0])
+    >>> assign_ranks([10.5, 8.1, 12.4, 9.3, 11.0])
     [3, 1, 5, 2, 4]
-
     """
-    ranked = sorted((value, index) for index, value in enumerate(arr))
-    ranks = [0] * len(arr)
+    ranked_data = sorted((value, index) for index, value in enumerate(data))
+    ranks = [0] * len(data)
 
-    for pos, (_, index) in enumerate(ranked):
-        ranks[index] = pos + 1
+    for position, (_, index) in enumerate(ranked_data):
+        ranks[index] = position + 1
 
     return ranks
 
 
-def spearman_rank_correlation(x, y):
+def calculate_spearman_rank_correlation(x: List[float], y: List[float]) -> float:
     """
     Calculates Spearman's rank correlation coefficient.
 
@@ -30,33 +31,32 @@ def spearman_rank_correlation(x, y):
     :param y: List of floats representing the second variable.
     :return: Spearman's rank correlation coefficient.
 
-    Example Usage :
+    Example Usage:
 
     >>> x = [1, 2, 3, 4, 5]
     >>> y = [5, 4, 3, 2, 1]
-    >>> spearman_rank_correlation(x, y)
+    >>> calculate_spearman_rank_correlation(x, y)
     -1.0
 
     >>> x = [1, 2, 3, 4, 5]
     >>> y = [2, 4, 6, 8, 10]
-    >>> spearman_rank_correlation(x, y)
+    >>> calculate_spearman_rank_correlation(x, y)
     1.0
 
     >>> x = [1, 2, 3, 4, 5]
     >>> y = [5, 1, 2, 9, 5]
-    >>> spearman_rank_correlation(x, y)
+    >>> calculate_spearman_rank_correlation(x, y)
     0.6
-
     """
     n = len(x)
-    rank_x = rankdata(x)
-    rank_y = rankdata(y)
+    rank_x = assign_ranks(x)
+    rank_y = assign_ranks(y)
 
     # Calculate differences of ranks
     d = [rx - ry for rx, ry in zip(rank_x, rank_y)]
 
     # Calculate the sum of squared differences
-    d_squared = sum(di**2 for di in d)
+    d_squared = sum(di ** 2 for di in d)
 
     # Calculate the Spearman's rank correlation coefficient
     rho = 1 - (6 * d_squared) / (n * (n**2 - 1))
@@ -72,15 +72,15 @@ if __name__ == "__main__":
     # Example usage:
     x1 = [1, 2, 3, 4, 5]
     y1 = [2, 4, 6, 8, 10]
-    rho1 = spearman_rank_correlation(x1, y1)
+    rho1 = calculate_spearman_rank_correlation(x1, y1)
     print(f"Spearman's rank correlation coefficient (Example 1): {rho1}")
 
     x2 = [1, 2, 3, 4, 5]
     y2 = [5, 4, 3, 2, 1]
-    rho2 = spearman_rank_correlation(x2, y2)
+    rho2 = calculate_spearman_rank_correlation(x2, y2)
     print(f"Spearman's rank correlation coefficient (Example 2): {rho2}")
 
     x3 = [1, 2, 3, 4, 5]
     y3 = [5, 1, 2, 9, 5]
-    rho3 = spearman_rank_correlation(x3, y3)
+    rho3 = calculate_spearman_rank_correlation(x3, y3)
     print(f"Spearman's rank correlation coefficient (Example 3): {rho3}")
