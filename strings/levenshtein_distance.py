f6bf88b6ed9d70435bd6c29d06c5abfd28151536
Pedram_Mohajer <48964282+pedram-mohajer@users.noreply.github.com>
2023-11-20 22:57:14 -0500

    Update levenshtein_distance.py

--- strings/levenshtein_distance.py
+++ strings/levenshtein_distance.py
@@ -1,42 +1,48 @@
 """
-This is a Python implementation of the levenshtein distance.
-Levenshtein distance is a string metric for measuring the
-difference between two sequences.
+This is an optimized Python implementation of the Levenshtein distance algorithm.
+Levenshtein distance is a metric for measuring differences between sequences.
 
-For doctests run following command:
-python -m doctest -v levenshtein-distance.py
+For doctests, run the following command:
+python -m doctest -v levenshtein_distance.py
 or
-python3 -m doctest -v levenshtein-distance.py
+python3 -m doctest -v levenshtein_distance.py
 
-For manual testing run:
-python levenshtein-distance.py
+For manual testing, run:
+python levenshtein_distance.py
 """
 
 
-def levenshtein_distance(first_word: str, second_word: str) -> int:
-    """Implementation of the levenshtein distance in Python.
-    :param first_word: the first word to measure the difference.
-    :param second_word: the second word to measure the difference.
-    :return: the levenshtein distance between the two words.
+def levenshtein_distance_optimized(first_word: str, second_word: str) -> int:
+    """
+    Compute the Levenshtein distance between two words (strings).
+
+    The function is optimized for efficiency by modifying rows in place.
+
+    Parameters:
+    first_word (str): The first word to measure the difference.
+    second_word (str): The second word to measure the difference.
+
+    Returns:
+    int: The Levenshtein distance between the two words.
+
     Examples:
-    >>> levenshtein_distance("planet", "planetary")
+    >>> levenshtein_distance_optimized("planet", "planetary")
     3
-    >>> levenshtein_distance("", "test")
+    >>> levenshtein_distance_optimized("", "test")
     4
-    >>> levenshtein_distance("book", "back")
+    >>> levenshtein_distance_optimized("book", "back")
     2
-    >>> levenshtein_distance("book", "book")
+    >>> levenshtein_distance_optimized("book", "book")
     0
-    >>> levenshtein_distance("test", "")
+    >>> levenshtein_distance_optimized("test", "")
     4
-    >>> levenshtein_distance("", "")
+    >>> levenshtein_distance_optimized("", "")
     0
-    >>> levenshtein_distance("orchestration", "container")
+    >>> levenshtein_distance_optimized("orchestration", "container")
     10
     """
-    # The longer word should come first
     if len(first_word) < len(second_word):
-        return levenshtein_distance(second_word, first_word)
+        return levenshtein_distance_optimized(second_word, first_word)
 
     if len(second_word) == 0:
         return len(first_word)
@@ -44,21 +50,16 @@ def levenshtein_distance(first_word: str, second_word: str) -> int:
     previous_row = list(range(len(second_word) + 1))
 
     for i, c1 in enumerate(first_word):
-        current_row = [i + 1]
+        current_row = [i + 1] + [0] * len(second_word)
 
         for j, c2 in enumerate(second_word):
-            # Calculate insertions, deletions and substitutions
             insertions = previous_row[j + 1] + 1
             deletions = current_row[j] + 1
             substitutions = previous_row[j] + (c1 != c2)
+            current_row[j + 1] = min(insertions, deletions, substitutions)
 
-            # Get the minimum to append to the current row
-            current_row.append(min(insertions, deletions, substitutions))
-
-        # Store the previous row
         previous_row = current_row
 
-    # Returns the last element (distance)
     return previous_row[-1]
 
 
@@ -66,5 +67,5 @@ if __name__ == "__main__":
     first_word = input("Enter the first word:\n").strip()
     second_word = input("Enter the second word:\n").strip()
 
-    result = levenshtein_distance(first_word, second_word)
+    result = levenshtein_distance_optimized(first_word, second_word)
     print(f"Levenshtein distance between {first_word} and {second_word} is {result}")
