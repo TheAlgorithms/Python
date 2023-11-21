b8c258145db2246cbaf5fd18d1e8b6fa8a0d3e62
Pedram_Mohajer <48964282+pedram-mohajer@users.noreply.github.com>
2023-11-20 23:24:43 -0500

    Update edit_distance.py

--- strings/edit_distance.py
+++ strings/edit_distance.py
@@ -1,32 +1,48 @@
 def edit_distance(source: str, target: str) -> int:
     """
-    Edit distance algorithm is a string metric, i.e., it is a way of quantifying how
-    dissimilar two strings are to one another. It is measured by counting the minimum
-    number of operations required to transform one string into another.
+    Calculate the edit distance between two strings.
 
-    This implementation assumes that the cost of operations (insertion, deletion and
-    substitution) is always 1
+    The edit distance is the minimum number of operations (insertions, deletions,
+    or substitutions) required to transform the source string into the target string.
 
     Args:
-    source: the initial string with respect to which we are calculating the edit
-        distance for the target
-    target: the target string, formed after performing n operations on the source string
+        source (str): The source string.
+        target (str): The target string.
 
-    >>> edit_distance("GATTIC", "GALTIC")
-    1
+    Returns:
+        int: The edit distance between the source and target strings.
+
+    Examples:
+    >>> edit_distance("kitten", "sitting")
+    3
+    >>> edit_distance("GATTACA", "GALACTICA")
+    4
+    >>> edit_distance("", "test")
+    4
+    >>> edit_distance("book", "back")
+    2
+    >>> edit_distance("", "")
+    0
     """
-    if len(source) == 0:
-        return len(target)
-    elif len(target) == 0:
-        return len(source)
+    m, n = len(source), len(target)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
 
-    delta = int(source[-1] != target[-1])  # Substitution
-    return min(
-        edit_distance(source[:-1], target[:-1]) + delta,
-        edit_distance(source, target[:-1]) + 1,
-        edit_distance(source[:-1], target) + 1,
-    )
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0:
+                dp[i][j] = j
+            elif j == 0:
+                dp[i][j] = i
+            elif source[i - 1] == target[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            else:
+                dp[i][j] = 1 + min(
+                    dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]  # Remove  # Insert
+                )  # Replace
+
+    return dp[m][n]
 
 
 if __name__ == "__main__":
+    # Example usage
     print(edit_distance("ATCGCTG", "TAGCTAA"))  # Answer is 4
