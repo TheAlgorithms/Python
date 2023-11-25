32a74df2bdde4a96ab6db7a5b9467e57e3992368
Christian Clauss <cclauss@me.com>
2023-11-25 14:18:54 +0100

    Update backtracking/all_combinations.py

--- backtracking/all_combinations.py
+++ backtracking/all_combinations.py
@@ -36,8 +36,12 @@ def generate_all_combinations(n: int, k: int) -> list[list[int]]:
     ...     for n in range(1, 6) for k in range(1, 6))
     True
     """
-    if k < 0 or n < 0 or k > n:
-        return []  # Return an empty list for invalid input
+    if k < 0:
+        raise ValueError("k must not be negative")
+    if n < 0:
+        raise ValueError("n must not be negative")
+    if k > n:
+        raise ValueError("k must less that or equal to n")
 
     result: list[list[int]] = []
     create_all_state(1, n, k, [], result)
