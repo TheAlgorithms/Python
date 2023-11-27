a8381809509dacda2b1a78db38deefed07b2adf2
moaldeen <132774635+moaldeen@users.noreply.github.com>
2023-11-27 17:22:55 -0500

    Update first_missing_positive.py

--- data_structures/arrays/first_missing_positive.py
+++ data_structures/arrays/first_missing_positive.py
@@ -1,4 +1,4 @@
-def firstmissingpositive(nums):
+def first_missing_positive(nums: list[int]) -> int:
     """
     Given an unsorted integer array nums, returns the smallest missing positive integer.
 
@@ -9,20 +9,17 @@ def firstmissingpositive(nums):
     - Iterate through the array to find the first position where nums[i] != i + 1,
       indicating the missing positive integer.
 
-    Args:
-    - nums (List[int]): The input unsorted integer array.
-
-    Returns:
-    - int: The smallest missing positive integer.
+    :param nums: The input unsorted integer array.
+    :return: The smallest missing positive integer.
 
     Examples:
-    >>> firstmissingpositive([1, 2, 0])
+    >>> first_missing_positive([1, 2, 0])
     3
 
-    >>> firstmissingpositive([3, 4, -1, 1])
+    >>> first_missing_positive([3, 4, -1, 1])
     2
 
-    >>> firstmissingpositive([7, 8, 9, 11, 12])
+    >>> first_missing_positive([7, 8, 9, 11, 12])
     1
     """
     n = len(nums)
@@ -47,17 +44,17 @@ if __name__ == "__main__":
 
     # Add more tests if needed
     nums1 = [1, 2, 0]
-    print(firstmissingpositive(nums1))  # Output: 3
+    print(first_missing_positive(nums1))  # Output: 3
 
     nums2 = [3, 4, -1, 1]
-    print(firstmissingpositive(nums2))  # Output: 2
+    print(first_missing_positive(nums2))  # Output: 2
 
     nums3 = [7, 8, 9, 11, 12]
-    print(firstmissingpositive(nums3))  # Output: 1
+    print(first_missing_positive(nums3))  # Output: 1
 
     # Additional test
     nums4 = [2, 5, -10, 8, 4, 6, 1, 9]
-    print(firstmissingpositive(nums4))  # Output: 3
+    print(first_missing_positive(nums4))  # Output: 3
 
     # Run the doctests
     doctest.testmod()
