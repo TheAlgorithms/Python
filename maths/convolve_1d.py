09031e828bbf3f34eb8716e4814dbbc820ae74fb
AlexB <acbucha@clemson.edu>
2023-12-07 00:30:26 -0500

    addressing automated concerns

--- maths/convolve_1d.py
+++ maths/convolve_1d.py
@@ -17,9 +17,9 @@ class Signal:
     A discrete representation of a signal as a n-dimensional vector
 
     >>> Signal([1,2,3,4,5])
-    Signal(signal=[1,2,3,4,5],n=5)
+    Signal(signal=[1, 2, 3, 4, 5], n=5)
     >>> Signal([1.0,3.0,2.0,-1.0])
-    Signal(signal=[1.0,3.0,2.0,-1.0],n=4)
+    Signal(signal=[1.0, 3.0, 2.0, -1.0], n=4)
     """
 
     signal: list[float] = field(default_factory=list)
@@ -41,7 +41,7 @@ class DiscreteConvolve1D:
     >>> s1 = Signal([1,2,3,4,5])
     >>> s2 = Signal([1,-1,2,-3])
     >>> DiscreteConvolve1D(s1,s2)
-    DiscreteConvolve1D(kern=Signal( # doctest: +NORMALIZE_WHITESPACE
+    DiscreteConvolve1D(kern=Signal(# doctest: +NORMALIZE_WHITESPACE
     signal=[1, 2, 3, 4, 5], n=5), sig=Signal(signal=[1, -1, 2, -3], n=4))
     """
 
