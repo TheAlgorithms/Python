7d55e21dae73cb2d4dfcdfe8ef6cc6d0612ecb57
Charlie Miller <35689954+MarlieChiller@users.noreply.github.com>
2024-01-02 10:49:06 +0000

    Update get_top_billionaires.py

--- web_programming/get_top_billionaires.py
+++ web_programming/get_top_billionaires.py
@@ -31,7 +31,7 @@ def calculate_age(unix_date: float) -> str:
     >>> int(calculate_age(-657244800000)) - years_since_create
     72
     >>> int(calculate_age(46915200000)) - years_since_create
-    51
+    50
     """
     # Convert date from milliseconds to seconds
     unix_date /= 1000
