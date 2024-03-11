ee241403f9637271bdb7ca603f5974ddf9e62e9c
MaximSmolskiy <mithridatus@mail.ru>
2024-03-11 11:31:00 +0300

    Try to fix ruff

--- neural_network/input_data.py
+++ neural_network/input_data.py
@@ -28,8 +28,10 @@ from tensorflow.python.framework import dtypes, random_seed
 from tensorflow.python.platform import gfile
 from tensorflow.python.util.deprecation import deprecated
 
-_Datasets = typing.NamedTuple(
-    "_Datasets", [("train", _DataSet), ("validation", _DataSet), ("test", _DataSet)])
+class _Datasets(typing.NamedTuple):
+    train:  "_DataSet"
+    validation:  "_DataSet"
+    test:  "_DataSet"
 
 # CVDF mirror of http://yann.lecun.com/exdb/mnist/
 DEFAULT_SOURCE_URL = "https://storage.googleapis.com/cvdf-datasets/mnist/"
