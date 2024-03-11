d37672a2c8db65c7d9b24756edced70627e84a19
MaximSmolskiy <mithridatus@mail.ru>
2024-03-11 10:56:53 +0300

    Try to fix ruff

--- ciphers/rsa_cipher.py
+++ ciphers/rsa_cipher.py
@@ -76,11 +76,9 @@ def encrypt_and_write_to_file(
     key_size, n, e = read_key_file(key_filename)
     if key_size < block_size * 8:
         sys.exit(
-            "ERROR: Block size is {} bits and key size is {} bits. The RSA cipher "
-            "requires the block size to be equal to or greater than the key size. "
-            "Either decrease the block size or use different keys.".format(
-                block_size * 8, key_size
-            )
+            f"ERROR: Block size is {block_size * 8} bits and key size is {key_size} "
+            "bits. The RSA cipher requires the block size to be equal to or greater "
+            "than the key size. Either decrease the block size or use different keys."
         )
 
     encrypted_blocks = [str(i) for i in encrypt_message(message, (n, e), block_size)]
@@ -102,11 +100,10 @@ def read_from_file_and_decrypt(message_filename: str, key_filename: str) -> str:
 
     if key_size < block_size * 8:
         sys.exit(
-            "ERROR: Block size is {} bits and key size is {} bits. The RSA cipher "
-            "requires the block size to be equal to or greater than the key size. "
-            "Did you specify the correct key file and encrypted file?".format(
-                block_size * 8, key_size
-            )
+            f"ERROR: Block size is {block_size * 8} bits and key size is {key_size} "
+            "bits. The RSA cipher requires the block size to be equal to or greater "
+            "than the key size. Did you specify the correct key file and encrypted "
+            "file?"
         )
 
     encrypted_blocks = []
