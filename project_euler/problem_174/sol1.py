96897955d9592bdf141b01b353b8613e67b85921
MaximSmolskiy <mithridatus@mail.ru>
2024-03-12 19:42:24 +0300

    Fix project_euler/problem_174/sol1.py

--- project_euler/problem_174/sol1.py
+++ project_euler/problem_174/sol1.py
@@ -45,7 +45,7 @@ def solution(t_limit: int = 1000000, n_limit: int = 10) -> int:
         for hole_width in range(hole_width_lower_bound, outer_width - 1, 2):
             count[outer_width * outer_width - hole_width * hole_width] += 1
 
-    return sum(1 for n in count.values() if 1 <= n <= 10)
+    return sum(1 for n in count.values() if 1 <= n <= n_limit)
 
 
 if __name__ == "__main__":
