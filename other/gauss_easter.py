102e9a31b673e5444678fd55640a0038b6a16a9d
Maxim Smolskiy <mithridatus@mail.ru>
2024-03-25 10:43:24 +0300

    Enable ruff DTZ001 rule (#11326)
    * updating DIRECTORY.md
    
    * Enable ruff DTZ001 rule
    
    * Fix other/gauss_easter.py
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    ---------
    
    Co-authored-by: MaximSmolskiy <MaximSmolskiy@users.noreply.github.com>

--- other/gauss_easter.py
+++ other/gauss_easter.py
@@ -3,7 +3,7 @@ https://en.wikipedia.org/wiki/Computus#Gauss'_Easter_algorithm
 """
 
 import math
-from datetime import datetime, timedelta
+from datetime import UTC, datetime, timedelta
 
 
 def gauss_easter(year: int) -> datetime:
@@ -11,16 +11,16 @@ def gauss_easter(year: int) -> datetime:
     Calculation Gregorian easter date for given year
 
     >>> gauss_easter(2007)
-    datetime.datetime(2007, 4, 8, 0, 0)
+    datetime.datetime(2007, 4, 8, 0, 0, tzinfo=datetime.timezone.utc)
 
     >>> gauss_easter(2008)
-    datetime.datetime(2008, 3, 23, 0, 0)
+    datetime.datetime(2008, 3, 23, 0, 0, tzinfo=datetime.timezone.utc)
 
     >>> gauss_easter(2020)
-    datetime.datetime(2020, 4, 12, 0, 0)
+    datetime.datetime(2020, 4, 12, 0, 0, tzinfo=datetime.timezone.utc)
 
     >>> gauss_easter(2021)
-    datetime.datetime(2021, 4, 4, 0, 0)
+    datetime.datetime(2021, 4, 4, 0, 0, tzinfo=datetime.timezone.utc)
     """
     metonic_cycle = year % 19
     julian_leap_year = year % 4
@@ -45,11 +45,11 @@ def gauss_easter(year: int) -> datetime:
     ) % 7
 
     if days_to_add == 29 and days_from_phm_to_sunday == 6:
-        return datetime(year, 4, 19)
+        return datetime(year, 4, 19, tzinfo=UTC)
     elif days_to_add == 28 and days_from_phm_to_sunday == 6:
-        return datetime(year, 4, 18)
+        return datetime(year, 4, 18, tzinfo=UTC)
     else:
-        return datetime(year, 3, 22) + timedelta(
+        return datetime(year, 3, 22, tzinfo=UTC) + timedelta(
             days=int(days_to_add + days_from_phm_to_sunday)
         )
 
