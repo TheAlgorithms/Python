a0f8b6fba7dc80be9f87ade6373fdb71c5522db0
MaximSmolskiy <mithridatus@mail.ru>
2024-04-02 00:09:10 +0300

    Fix

--- data_structures/stacks/balanced_parentheses.py
+++ data_structures/stacks/balanced_parentheses.py
@@ -19,11 +19,7 @@ def balanced_parentheses(parentheses: str) -> bool:
     for bracket in parentheses:
         if bracket in bracket_pairs:
             stack.push(bracket)
-        elif (
-            bracket in (")", "]", "}")
-            and stack.is_empty()
-            or bracket_pairs[stack.pop()] != bracket
-        ):
+        elif bracket in (")", "]", "}") and (stack.is_empty() or bracket_pairs[stack.pop()] != bracket):
             return False
     return stack.is_empty()
 
