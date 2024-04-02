8f2b3c0216ff9f719a06261fbd5a062736f7e83d
MaximSmolskiy <mithridatus@mail.ru>
2024-04-02 20:51:18 +0300

    Fix

--- compression/huffman.py
+++ compression/huffman.py
@@ -40,7 +40,7 @@ def build_tree(letters: list[Letter]) -> Letter | TreeNode:
     Run through the list of Letters and build the min heap
     for the Huffman Tree.
     """
-    response: list[Letter | TreeNode] = letters.copy()
+    response: list[Letter | TreeNode] = list(letters)
     while len(response) > 1:
         left = response.pop(0)
         right = response.pop(0)
