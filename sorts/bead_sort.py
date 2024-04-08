33f4a498980179b8685be08f8a6e9e580210bc8d
MaximSmolskiy <mithridatus@mail.ru>
2024-04-09 02:07:39 +0300

    Revert "Update sorts/bead_sort.py"
    This reverts commit 2c1816bf102eeec5aa39cb2f1806afb64b672d14.

--- sorts/bead_sort.py
+++ sorts/bead_sort.py
@@ -31,9 +31,9 @@ def bead_sort(sequence: list) -> list:
     if any(not isinstance(x, int) or x < 0 for x in sequence):
         raise TypeError("Sequence must be list of non-negative integers")
     for _ in range(len(sequence)):
-        for i, (rod_upper, rod_lower) in enumerate(pairwise(sequence)):
-            if rod_upper > rod_lower:
-                sequence[i], sequence[i + 1] = rod_lower, rod_upper
+        for i in range(len(sequence) - 1):
+            if sequence[i] > sequence[i + 1]:
+                sequence[i], sequence[i + 1] = sequence[i + 1], sequence[i]
     return sequence
 
 
