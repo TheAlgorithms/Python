04d853ab9d0fb199af95f23e3b227d1ca24cd66f
Christian Clauss <cclauss@me.com>
2024-04-20 16:16:29 +0200

    Update sorts/bead_sort.py

--- sorts/bead_sort.py
+++ sorts/bead_sort.py
@@ -31,9 +31,10 @@ def bead_sort(sequence: list) -> list:
     if any(not isinstance(x, int) or x < 0 for x in sequence):
         raise TypeError("Sequence must be list of non-negative integers")
     for _ in range(len(sequence)):
-        for i in range(len(sequence) - 1):
-            if sequence[i] > sequence[i + 1]:
-                sequence[i], sequence[i + 1] = sequence[i + 1], sequence[i]
+        for i, (rod_upper, rod_lower) in enumerate(zip(sequence, sequence[1:])):  # noqa: RUF007
+            if rod_upper > rod_lower:
+                sequence[i] -= rod_upper - rod_lower
+                sequence[i + 1] += rod_upper - rod_lower
     return sequence
 
 
