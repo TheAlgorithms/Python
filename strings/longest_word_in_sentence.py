1c7710de382cf9648dedfda740593b0440f8b1b1
Saurabh Mahapatra <98408932+its-100rabh@users.noreply.github.com>
2024-07-05 10:29:48 +0530

    Create longest_word_in_sentence.py

---
+++ strings/longest_word_in_sentence.py
@@ -0,0 +1,19 @@
+def longest_word(sentence: str) -> str:
+    """
+    Finds the longest word in a sentence.
+
+    >>> longest_word("The quick brown fox jumped over the lazy dog")
+    'jumped'
+    >>> longest_word("Python is amazing")
+    'amazing'
+    >>> longest_word("")
+    ''
+    >>> longest_word("a")
+    'a'
+    """
+    words = sentence.split()
+    return max(words, key=len) if words else ''
+
+if __name__ == "__main__":
+    from doctest import testmod
+    testmod()
