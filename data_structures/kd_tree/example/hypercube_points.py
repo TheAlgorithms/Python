1668d73a43abd9c6e6ab4f20c93fbbcd5f8600af
Ramy-Badr-Ahmed <ramy.badr@hotmail.com>
2024-08-28 19:47:09 +0200

    Replaced legacy `np.random.rand` call with `np.random.Generator` in kd_tree/hypercube_points.py

--- data_structures/kd_tree/example/hypercube_points.py
+++ data_structures/kd_tree/example/hypercube_points.py
@@ -2,4 +2,5 @@ import numpy as np
 
 
 def hypercube_points(num_points, hypercube_size, num_dimensions):
-    return hypercube_size * np.random.rand(num_points, num_dimensions)
+    rng = np.random.default_rng()
+    return hypercube_size * rng.random((num_points, num_dimensions))
