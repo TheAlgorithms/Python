87909da36b8890e1c0a8433d135ddaf16650e8a0
Ramy-Badr-Ahmed <ramy.badr@hotmail.com>
2024-09-07 22:40:59 +0200

    Considering ruff part-2

--- data_structures/suffix_tree/suffix_tree_node.py
+++ data_structures/suffix_tree/suffix_tree_node.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 class SuffixTreeNode:
     def __init__(
         self,
-        children: dict[str, "SuffixTreeNode"] = None,
+        children: dict[str, SuffixTreeNode] = None,
         is_end_of_string: bool = False,
         start: int | None = None,
         end: int | None = None,
@@ -14,12 +14,12 @@ class SuffixTreeNode:
         Initializes a suffix tree node.
 
         Parameters:
-            children (Dict[str, SuffixTreeNode]|None): The children of this node.
+            children (dict[str, SuffixTreeNode] | None): The children of this node.
             is_end_of_string (bool): Indicates if this node represents
                                      the end of a string.
-            start (int|None): The start index of the suffix in the text.
-            end (int|None): The end index of the suffix in the text.
-            suffix_link (SuffixTreeNode|None): Link to another suffix tree node.
+            start (int | None): The start index of the suffix in the text.
+            end (int | None): The end index of the suffix in the text.
+            suffix_link (SuffixTreeNode | None): Link to another suffix tree node.
         """
         self.children = children or {}
         self.is_end_of_string = is_end_of_string
