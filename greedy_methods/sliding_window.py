6a8bfb08783034d756bc211058903735f3dc5e41
OmMahajan29 <112854335+OmMahajan29@users.noreply.github.com>
2024-09-25 07:31:14 +0530

    Update sliding_window.py
    changed input parameter

--- greedy_methods/sliding_window.py
+++ greedy_methods/sliding_window.py
@@ -1,10 +1,10 @@
-def sliding_window(s: str) -> int:
+def sliding_window(input_string: str) -> int:
     """
     This function takes a string and returns the length of the longest substring
     without repeating characters using the sliding window algorithm.
 
     Args:
-        s: A string input.
+        input_string: A string input.
 
     Returns:
         max_len: Length of the longest substring without repeating characters.
@@ -28,21 +28,19 @@ def sliding_window(s: str) -> int:
     max_len = 0
 
     # Traverse the string with a right pointer
-    for right, char in enumerate(s):
+    for right, char in enumerate(input_string):
         if char in char_index_map and char_index_map[char] >= left:
             # Move the left pointer to avoid repeating characters
             left = char_index_map[char] + 1
-
+        
         # Update the latest index of the character
         char_index_map[char] = right
-
+        
         # Calculate the current length of the window
         max_len = max(max_len, right - left + 1)
-
+    
     return max_len
-
 
 if __name__ == "__main__":
     import doctest
-
     doctest.testmod()
