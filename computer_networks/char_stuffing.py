5e8d8c1fb3f531fe607ef4771f93e251221724f0
harsheenkohli <harsheenkohli2003@gmail.com>
2024-10-02 01:46:41 +0530

    Added Character Stuffing in Computer Networks

--- computer_networks/char_stuffing.py
+++ computer_networks/char_stuffing.py
@@ -10,7 +10,9 @@ def char_stuffing(s: str) -> str:
     >>> char_stuffing("a#b#c")
     "a##b##c"
     """
-    arr = [i for i in s]
+    arr = list()
+    for i in s:
+        arr.append(i)
     for i in range(len(arr)):
         if arr[i] == FLAG and not (i == 0 or i == len(arr) - 1):
             arr[i] = ESC + arr[i]
