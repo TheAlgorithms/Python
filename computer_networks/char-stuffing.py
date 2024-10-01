097ce6e75bb9fd72cfbdfa723067389f8310aac8
harsheenkohli <harsheenkohli2003@gmail.com>
2024-10-02 01:34:01 +0530

    Character Stuffing in Computer Networks

---
+++ computer_networks/char-stuffing.py
@@ -0,0 +1,23 @@
+FLAG = '~'
+ESC = '#'
+
+
+def char_stuffing(s: str) -> str:
+    """
+    Return the char stuffed message
+    >>> char_stuffing("abc")
+    "abc"
+    >>> char_stuffing("a#b#c")
+    "a##b##c"
+    """
+    arr = [i for i in s]
+    for i in range(len(arr)):
+        if arr[i] == FLAG and not (i == 0 or i == len(arr) - 1):
+            arr[i] = ESC + arr[i]
+        elif arr[i] == ESC:
+            arr[i] += ESC
+    return ''.join(arr)
+
+
+s = '~abc#~cde~ab~'
+print(char_stuffing(s))
