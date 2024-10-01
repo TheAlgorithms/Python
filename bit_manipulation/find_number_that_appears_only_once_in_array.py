6b3bbfe3d4b2cfb61ec368c333c8ec19b12983fe
Irfan Ansari <71096605+IRFANSARI@users.noreply.github.com>
2024-10-01 17:14:19 +0530

    Update find_number_that_appears_only_once_in_array.py

--- bit_manipulation/find_number_that_appears_only_once_in_array.py
+++ bit_manipulation/find_number_that_appears_only_once_in_array.py
@@ -1,16 +1,40 @@
-###
 
-###
+"""
+This problem comes from LeetCode 136: Single Number
 
+Problem Statement: You are given an list of number where each number appears twice in the list but there is a single number that appears once only. Find that single number.
 
-def findDuplicate(nums):
-    duplicate_num = 0
+Intuition:
+- Hence we know that a ^ a = 0 (^ is XOR binary operator)
+- We can XOR all numbers, then we will get the only single number
+- Example: let's take nums = [1, 2, 4, 1, 4, 3, 2]
+    - We can see all numbers appears twice except 3
+    - If we take XOR of all element, we will get 3, let me demonstrate -
+        - 1 ^ 2 ^ 4 ^ 1 ^ 4 ^ 3 ^ 2
+    - Since we know a ^ a = 0, we can cancel them out -
+        - 1 ^ 1 ^ 2 ^ 2 ^ 4 ^ 4 ^ 3
+        - 0 ^ 0 ^ 0 ^ 3
+    - We also know a ^ 0 = a, then we will get our answer as 3
+        - 0 ^ 0 ^ 0 ^ 3
+        - 0 ^ 0 ^ 3
+        - 0 ^ 3
+        - 3
+
+Complexities:
+- Time: O(n)
+- Space: O(1)
+"""
+
+def findSingleNumber(nums):
+    single_num = 0
     
     for i in nums:
-        duplicate_num = duplicate_num ^ i;
+        single_num = single_num ^ i;
     
-    return duplicate_num
+    return single_num
 
 if __name__ == "__main__":
     nums = [1, 4, 1, 7, 9, 2, 9, 7, 2]
-    print(findDuplicate(nums))
+    print(single_num(nums))
+
+
