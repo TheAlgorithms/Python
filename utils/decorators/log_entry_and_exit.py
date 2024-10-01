33f23e5fc777cff9b018b171ea21330db5ce9017
Dheeraj-Nalapat <dheerajnalapat3@gmail.com>
2024-10-01 17:25:16 +0530

    Added fixes according to ruff

--- utils/decorators/log_entry_and_exit.py
+++ utils/decorators/log_entry_and_exit.py
@@ -1,27 +1,29 @@
-from functools import wraps
 import logging
+import tzlocal
+from functools import wraps
 from datetime import datetime
 
 
 def log_entry_and_exit(func):
-    """Log starting time and finish time of funciton"""
+    '''Log starting time and finish time of funciton'''
 
+    # Setiing local timezone
+    local_tz = tzlocal.get_localzone()
     # Configure logging
     logging.basicConfig(
         level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
     )
 
     @wraps(func)
-    def wrapper(*args, **kwargs):
-        start_time = datetime.now()
+    def wrapper(*args,**kwargs):
+        start_time = datetime.now(tz=local_tz)
         logging.info(f"Function '{func.__name__}' started at {start_time}")
-        return_value = func(*args, *kwargs)
-        end_time = datetime.now()
+        return_value = func(*args,*kwargs)
+        end_time = datetime.now(tz=local_tz)
         logging.info(f"Function '{func.__name__}' ended at {end_time}")
         return return_value
 
     return wrapper
-
 
 """
 Example usage:
@@ -33,6 +35,5 @@ Example usage:
 def my_func(name):
     print(f"My name is {name}")
 
-
-# Fucntion call
+#Fucntion call
 my_func("John Doe")
