506161a4cf3f878bcc7fb9a5b42af0e5d917af6a
Tejaswi Tyagi <98461855+tejaswi0910@users.noreply.github.com>
2024-10-03 18:06:25 +0000

    line length shortened

--- dynamic_programming/longest_arithmetic_subsequence.py
+++ dynamic_programming/longest_arithmetic_subsequence.py
@@ -1,12 +1,12 @@
 """
-Longest Arithmetic Subsequence Problem: Given an array nums of integers, return the length
-of the longest arithmetic subsequence in nums.
+Longest Arithmetic Subsequence Problem: Given an array nums of integers, 
+return the length of the longest arithmetic subsequence in nums.
 
 Note that:
-- A subsequence is an array that can be derived from another array by deleting some or no
-  elements without changing the order of the remaining elements.
-- A sequence seq is arithmetic if seq[i + 1] - seq[i] are all the same value (for 0 <= i <
-  seq.length - 1).
+- A subsequence is an array that can be derived from another array by 
+deleting some or no elements without changing the order of the remaining elements.
+- A sequence seq is arithmetic if seq[i + 1] - seq[i] are all the same value 
+(for 0 <= i < seq.length - 1).
 """
 
 
