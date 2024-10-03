9fd5b47b7b0ded19ac6d289b41bf626a83bd1eec
grpathak22 <guruprasadpathak22@gmail.com>
2024-10-03 23:50:01 +0530

    made changes for ruff tests
    Signed-off-by: grpathak22 <guruprasadpathak22@gmail.com>

--- ciphers/aes_128.py
+++ ciphers/aes_128.py
@@ -1,6 +1,19 @@
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad, unpad
 import base64
+from Crypto.Cipher import AES 
+from Crypto.Util.Padding import pad, unpad
+
+
+
+'''
+AES (Advanced Encryption Standard) is a symmetric encryption algorithm used 
+    for secure data encryption. AES-128 uses a 128-bit key to encrypt and decrypt 
+    data blocks of 128 bits. This implementation uses Cipher Block Chaining (CBC) mode 
+    with PKCS7 padding for encrypting messages.
+
+    For more details, visit: 
+    https://en.wikipedia.org/wiki/Advanced_Encryption_Standard
+'''
+
 
 def aes_encrypt(plaintext: str, key: str) -> str:
     """
