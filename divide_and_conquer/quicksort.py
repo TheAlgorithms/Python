a59c1c02e868cb9172591a60a517f5702243a72f
Rohan Sardar <77870108+RohanSardar@users.noreply.github.com>
2024-10-04 18:59:14 +0530

    Update quicksort.py

--- divide_and_conquer/quicksort.py
+++ divide_and_conquer/quicksort.py
@@ -9,8 +9,9 @@ def quicksort(arr: list) -> list:
     >>> quicksort([3, 6, 8, 10, 1, 2, 1, 3, 2, 8])
     [1, 1, 2, 2, 3, 3, 6, 8, 8, 10]
     """
-
-    # If the length of the array is less than or equal to 1, then there's nothing to sort, so return the given array
+    
+    # If the length of the array is less than or equal to 1, then there's
+    # nothing to sort, so return the given array
     if len(arr) <= 1:
         return arr
 
@@ -18,17 +19,17 @@ def quicksort(arr: list) -> list:
     # In this case let the pivot be the first element
     pivot = arr[0]
 
-    # Using list comprehension creating three list object: smaller_elemnts, pivot_elements
-    # & larger_elements based on the comparison with the pivot element
+    # Using list comprehension creating three list object: smaller_elemnts,
+    # pivot_elements & larger_elements
+    # based on the comparison with the pivot element
     smaller_elements = [x for x in arr if x < pivot]
     pivot_elements = [x for x in arr if x == pivot]
     larger_elements = [x for x in arr if x > pivot]
 
-    # Recursively splitting the list object to determine the correct position of the element
-    # return quicksort(smaller_elements) + pivot_elements + quicksort(larger_elements)
-
+    # Recursively splitting the list object to determine the correct
+    # position of the element
+    return quicksort(smaller_elements) + pivot_elements + quicksort(larger_elements)
 
 if __name__ == "__main__":
     import doctest
-
     doctest.testmod()
