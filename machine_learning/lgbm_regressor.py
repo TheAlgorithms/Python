f0ceb04ca90276e6601507af3e4a2b376ab4b5be
Archisman Karmakar <92569441+ArchismanKarmakar@users.noreply.github.com>
2024-10-04 23:53:32 +0530

    Update lgbm_regressor.py

--- machine_learning/lgbm_regressor.py
+++ machine_learning/lgbm_regressor.py
@@ -1,9 +1,9 @@
 # LGBM Regressor Example using Bank Marketing Dataset
 import numpy as np
+from lightgbm import LGBMRegressor
 from sklearn.datasets import fetch_openml
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from sklearn.model_selection import train_test_split
-from lightgbm import LGBMRegressor
 
 
 def data_handling(data: dict) -> tuple:
@@ -17,9 +17,8 @@ def data_handling(data: dict) -> tuple:
     return (data["data"], data["target"])
 
 
-def lgbm_regressor(
-    features: np.ndarray, target: np.ndarray, test_features: np.ndarray
-) -> np.ndarray:
+def lgbm_regressor(features: np.ndarray, target: np.ndarray,
+                   test_features: np.ndarray) -> np.ndarray:
     """
     >>> lgbm_regressor(np.array([[0.12, 0.02, 0.01, 0.25, 0.09]]),
     ... np.array([1]), np.array([[0.11, 0.03, 0.02, 0.28, 0.08]]))
@@ -40,7 +39,7 @@ def main() -> None:
     Bank Marketing Dataset is used to demonstrate the algorithm.
     """
     # Load Bank Marketing dataset
-    bank_data = fetch_openml(name="bank-marketing", version=1, as_frame=False)
+    bank_data = fetch_openml(name='bank-marketing', version=1, as_frame=False)
     data, target = data_handling(bank_data)
     x_train, x_test, y_train, y_test = train_test_split(
         data, target, test_size=0.25, random_state=1
