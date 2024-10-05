95d06d95f4243287e897a8bfad57773a03cbd0d6
sshsrijanr <srijan.ramavat@gmail.com>
2024-10-05 20:13:43 +0530

    added remove_nth_node_from_end.py in linked list Data Structure

---
+++ data_structures/linked_list/remove_nth_node_from_end.py
@@ -0,0 +1,37 @@
+class Node():
+
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+def remove(head, n):
+    extra = Node(0)
+    extra.next = head
+    fast = extra
+    slow = extra
+    for _ in range(n):
+        fast = fast.next
+
+    while fast.next:
+        fast = fast.next
+        slow = slow.next
+
+    slow.next = slow.next.next
+
+    return extra.next
+
+def print_list(head):
+    curr = head
+    while curr:
+        print(curr.val, end=" ")
+        curr = curr.next
+
+head = Node(1)
+head.next = Node(2)
+head.next.next = Node(3)
+head.next.next.next = Node(4)
+head.next.next.next.next = Node(5)
+print_list(head)
+head = remove(head, 5)
+print_list(head)
