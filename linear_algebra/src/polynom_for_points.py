a6b6f2b3394ee0839cec5d9b98c11c46b3ac46e5
zeelrupapara <zeelrupapara@gmail.com>
2024-10-06 12:39:48 +0530

    feat: add testcase of polynom_for_points

--- linear_algebra/src/polynom_for_points.py
+++ linear_algebra/src/polynom_for_points.py
@@ -27,6 +27,12 @@ def points_to_polynomial(coordinates: list[list[int]]) -> str:
     f(x)=x^2*-1.0+x^1*-0.0+x^0*-2.0
     >>> print(points_to_polynomial([[1, 5], [2, 2], [3, 9]]))
     f(x)=x^2*5.0+x^1*-18.0+x^0*18.0
+    >>> print(points_to_polynomial([[1, 1], [1, 2], [1, 3]]))
+    x=1
+    >>> print(points_to_polynomial([[1, 1], [2, 2], [2, 2]]))
+    Traceback (most recent call last):
+        ...
+    ValueError: The program cannot work out a fitting polynomial.
     """
     if len(coordinates) == 0 or not all(len(pair) == 2 for pair in coordinates):
         raise ValueError("The program cannot work out a fitting polynomial.")
