f1c5829f78ddcc77b0964713a498e35c277c9b21
1227haran <hariharan.deivasagayam@genesys.com>
2024-10-06 23:48:03 +0530

    Updated code

--- data_structures/binary_tree/maximum_sum_bst.py
+++ data_structures/binary_tree/maximum_sum_bst.py
@@ -44,7 +44,7 @@ def max_sum_bst(root: TreeNode) -> int:
     """
     ans: int = 0
 
-    def solver(node: TreeNode | None) -> tuple[bool, int, int, int]:
+    def solver(node: 'TreeNode' | None) -> tuple[bool, int, int, int]:
         """
         Returns the maximum sum by making recursive calls
         >>> t1 = TreeNode(1)
