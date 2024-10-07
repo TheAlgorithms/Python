1141964e131fb3acaf7544c6ae474408b8220ca4
Satyam Kumar <isatyamks@gmail.com>
2024-10-07 21:31:18 +0530

    Update shapley_value.py

--- game_theory/shapley_value.py
+++ game_theory/shapley_value.py
@@ -7,22 +7,11 @@ def shapley_value(payoff_matrix):
             if (s & (1 << i)) == 0:  # i not in S
                 continue
 
-<<<<<<< HEAD
             s_without_i = s & ~(1 << i)
             marginal_contribution = payoff_matrix[s][i] - (payoff_matrix[s_without_i][i] if s_without_i else 0)
             shapley_values[i] += marginal_contribution / (len(bin(s)) - 2)  # Normalize by size of S
-=======
-            S_without_i = S & ~(1 << i)
-            marginal_contribution = payoff_matrix[S][i] - (
-                payoff_matrix[S_without_i][i] if S_without_i else 0
-            )
-            shapley_values[i] += marginal_contribution / (
-                len(bin(S)) - 2
-            )  # Normalize by size of S
->>>>>>> 51cf80c355f4a1fbfba6aa04bbb0fdf1292dcb2f
 
     return shapley_values
-
 
 # Example usage
 payoff_matrix = np.array([[1, 2], [3, 4]])
