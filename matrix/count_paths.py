772d5dad0d807fcdabfa4ace98fa768411371482
Vinay Sanwal <vinaysanwal@Vinays-MacBook-Pro.local>
2024-10-07 20:31:54 +0530

    Interval Scheduling Algorithm

--- matrix/count_paths.py
+++ matrix/count_paths.py
@@ -20,54 +20,17 @@ In the example above, there are two distinct paths:
 """
 
 
-def depth_first_search(grid: list[list[int]], row: int, col: int, visit: set) -> int:
-    """
-    Recursive Backtracking Depth First Search Algorithm
-
-    Starting from top left of a matrix, count the number of
-    paths that can reach the bottom right of a matrix.
-    1 represents a block (inaccessible)
-    0 represents a valid space (accessible)
-
-    0  0  0  0
-    1  1  0  0
-    0  0  0  1
-    0  1  0  0
-    >>> grid = [[0, 0, 0, 0], [1, 1, 0, 0], [0, 0, 0, 1], [0, 1, 0, 0]]
-    >>> depth_first_search(grid, 0, 0, set())
-    2
-
-    0  0  0  0  0
-    0  1  1  1  0
-    0  1  1  1  0
-    0  0  0  0  0
-    >>> grid = [[0, 0, 0, 0, 0], [0, 1, 1, 1, 0], [0, 1, 1, 1, 0], [0, 0, 0, 0, 0]]
-    >>> depth_first_search(grid, 0, 0, set())
-    2
-    """
-    row_length, col_length = len(grid), len(grid[0])
-    if (
-        min(row, col) < 0
-        or row == row_length
-        or col == col_length
-        or (row, col) in visit
-        or grid[row][col] == 1
-    ):
-        return 0
-    if row == row_length - 1 and col == col_length - 1:
-        return 1
-
-    visit.add((row, col))
-
-    count = 0
-    count += depth_first_search(grid, row + 1, col, visit)
-    count += depth_first_search(grid, row - 1, col, visit)
-    count += depth_first_search(grid, row, col + 1, visit)
-    count += depth_first_search(grid, row, col - 1, visit)
-
-    visit.remove((row, col))
-    return count
-
+intervals = [(4, 5), (0, 2), (2, 7), (1, 3), (0, 4)]  
+intervals.sort(key=lambda x: x[1])  
+count = 0 
+end = 0 
+answer = [] 
+  
+for interval in intervals:  
+    if(end <= interval[0]): 
+        end = interval[1] 
+        count += 1
+        answer.append(interval) 
 
 if __name__ == "__main__":
     import doctest
