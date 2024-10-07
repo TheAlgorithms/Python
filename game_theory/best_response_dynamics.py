b143bfc57be1eefa518c282881b00baf853890a3
Satyam Kumar <isatyamks@gmail.com>
2024-10-07 21:19:26 +0530

    updated to lowercase

--- game_theory/best_response_dynamics.py
+++ game_theory/best_response_dynamics.py
@@ -1,27 +1,26 @@
-def best_response_dynamics(payoff_matrix_A, payoff_matrix_B, iterations=10):
-    n = payoff_matrix_A.shape[0]
-    m = payoff_matrix_A.shape[1]
+def best_response_dynamics(payoff_matrix_a, payoff_matrix_b, iterations=10):
+    n = payoff_matrix_a.shape[0]
+    m = payoff_matrix_a.shape[1]
 
     # Initialize strategies
-    strategy_A = np.ones(n) / n
-    strategy_B = np.ones(m) / m
+    strategy_a = np.ones(n) / n
+    strategy_b = np.ones(m) / m
 
     for _ in range(iterations):
         # Update strategy A
-        response_A = np.argmax(payoff_matrix_A @ strategy_B)
-        strategy_A = np.zeros(n)
-        strategy_A[response_A] = 1
+        response_a = np.argmax(payoff_matrix_a @ strategy_b)
+        strategy_a = np.zeros(n)
+        strategy_a[response_a] = 1
 
         # Update strategy B
-        response_B = np.argmax(payoff_matrix_B.T @ strategy_A)
-        strategy_B = np.zeros(m)
-        strategy_B[response_B] = 1
+        response_b = np.argmax(payoff_matrix_b.T @ strategy_a)
+        strategy_b = np.zeros(m)
+        strategy_b[response_b] = 1
 
-    return strategy_A, strategy_B
-
+    return strategy_a, strategy_b
 
 # Example usage
-payoff_A = np.array([[3, 0], [5, 1]])
-payoff_B = np.array([[2, 4], [0, 2]])
-strategies = best_response_dynamics(payoff_A, payoff_B)
+payoff_a = np.array([[3, 0], [5, 1]])
+payoff_b = np.array([[2, 4], [0, 2]])
+strategies = best_response_dynamics(payoff_a, payoff_b)
 print("Final strategies:", strategies)
--- game_theory/shapley_value.py
+++ game_theory/shapley_value.py
@@ -3,10 +3,15 @@ def shapley_value(payoff_matrix):
     shapley_values = np.zeros(n)
 
     for i in range(n):
-        for S in range(1 << n):  # All subsets of players
-            if (S & (1 << i)) == 0:  # i not in S
+        for s in range(1 << n):  # All subsets of players
+            if (s & (1 << i)) == 0:  # i not in S
                 continue
 
+<<<<<<< HEAD
+            s_without_i = s & ~(1 << i)
+            marginal_contribution = payoff_matrix[s][i] - (payoff_matrix[s_without_i][i] if s_without_i else 0)
+            shapley_values[i] += marginal_contribution / (len(bin(s)) - 2)  # Normalize by size of S
+=======
             S_without_i = S & ~(1 << i)
             marginal_contribution = payoff_matrix[S][i] - (
                 payoff_matrix[S_without_i][i] if S_without_i else 0
@@ -14,6 +19,7 @@ def shapley_value(payoff_matrix):
             shapley_values[i] += marginal_contribution / (
                 len(bin(S)) - 2
             )  # Normalize by size of S
+>>>>>>> 51cf80c355f4a1fbfba6aa04bbb0fdf1292dcb2f
 
     return shapley_values
 
