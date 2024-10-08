e9a65c70d0a8cc4509cfdded13c1645ea39f41b2
AyushChakraborty <chakrabortyayush83@gmail.com>
2024-10-08 09:41:55 +0530

    fix: Update elif to else for more general case

--- bit_manipulation/hamming_code_generator.py
+++ bit_manipulation/hamming_code_generator.py
@@ -83,7 +83,7 @@ def hamming_15_11(number: str) -> str:
 
         return "".join([str(i) for i in hamming_digits])
 
-    elif len(number) != 11 or not all(bit in '01' for bit in number):
+    else:
         return "Input must be an 11-bit binary string containing only '0's and '1's."
 
 if __name__ == "__main__":
