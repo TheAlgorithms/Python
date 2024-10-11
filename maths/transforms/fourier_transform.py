45b18a8669656ebc317b76a39b438d575012d16c
Nimitt Sharma <84977996+Nimittxo@users.noreply.github.com>
2024-10-12 01:06:50 +0530

    Update fourier_transform.py

--- maths/transforms/fourier_transform.py
+++ maths/transforms/fourier_transform.py
@@ -1,28 +1,28 @@
-import numpy as np
 from typing import List, Union
+import numpy as np
 
-def fourier_transform(signal: List[Union[int, float]]) -> List[complex]:
+def fourier_transform(signal: list[int | float]) -> list[complex]:
     """
     Compute the discrete Fourier transform (DFT) of a signal.
 
     Args:
-    signal (List[Union[int, float]]): A list of numerical values representing the input signal.
+        signal (list[int | float]): A list of numerical values representing the input signal.
 
     Returns:
-    List[complex]: The Fourier transform of the input signal as a list of complex numbers.
+        list[complex]: The Fourier transform of the input signal as a list of complex numbers.
 
     Example:
-    >>> fourier_transform([1, 2, 3, 4])
-    [(10+0j), (-2+2j), (-2+0j), (-2-2j)]
+        >>> fourier_transform([1, 2, 3, 4])
+        [(10+0j), (-2+2j), (-2+0j), (-2-2j)]
 
     Note:
-    This is a basic implementation of the DFT and can be optimized using FFT for larger datasets.
+        This is a basic implementation of the DFT and can be optimized using FFT for larger datasets.
     """
     n = len(signal)
     result = []
-    for k in range(n): 
-        summation = 0 + 0j  
-        for t in range(n):  
+    for k in range(n):
+        summation = 0 + 0j
+        for t in range(n):
             angle = -2j * np.pi * t * k / n
             summation += signal[t] * np.exp(angle)
         result.append(summation)
