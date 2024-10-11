835603223fff4ed651fc8e2343b2eaadb8ece0aa
Nimitt Sharma <84977996+Nimittxo@users.noreply.github.com>
2024-10-12 00:59:11 +0530

    fourier_transform.py
    create a new directory called ``` transforms ``` for transforms in calculus added first transform named fourier transform

---
+++ maths/transforms/fourier_transform.py
@@ -0,0 +1,34 @@
+import numpy as np
+from typing import List, Union
+
+def fourier_transform(signal: List[Union[int, float]]) -> List[complex]:
+    """
+    Compute the discrete Fourier transform (DFT) of a signal.
+
+    Args:
+    signal (List[Union[int, float]]): A list of numerical values representing the input signal.
+
+    Returns:
+    List[complex]: The Fourier transform of the input signal as a list of complex numbers.
+
+    Example:
+    >>> fourier_transform([1, 2, 3, 4])
+    [(10+0j), (-2+2j), (-2+0j), (-2-2j)]
+
+    Note:
+    This is a basic implementation of the DFT and can be optimized using FFT for larger datasets.
+    """
+    n = len(signal)
+    result = []
+    for k in range(n): 
+        summation = 0 + 0j  
+        for t in range(n):  
+            angle = -2j * np.pi * t * k / n
+            summation += signal[t] * np.exp(angle)
+        result.append(summation)
+    return result
+
+if __name__ == "__main__":
+    sample_signal = [1, 2, 3, 4]
+    result = fourier_transform(sample_signal)
+    print("Fourier Transform of the signal:", result)
