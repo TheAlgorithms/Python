671c995bf4ec021fcfe67c57bc0e8686c5a81bcd
NILADITYA KAR <niladityakar.it2022@nsec.ac.in>
2024-10-12 23:41:02 +0530

    Create Egyptian Fraction.py
    Added a Python Program on Egyptian Fraction Representation

---
+++ maths/Egyptian Fraction.py
@@ -0,0 +1,11 @@
+def egyptian_fraction(numerator, denominator):
+    fractions = []
+    while numerator != 0:
+        x = denominator // numerator + 1
+        fractions.append(f"1/{x}")
+        numerator = x * numerator - denominator
+        denominator *= x
+    return fractions
+
+num, den = 6, 14
+print(f"Egyptian Fraction of {num}/{den}: {egyptian_fraction(num, den)}")
