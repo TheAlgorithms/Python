8f4d745e4e823a0e38872ad79bb686841426fd3d
Aditya wadhwani <127688598+adityawadhwani03@users.noreply.github.com>
2024-10-13 21:30:19 +0530

    Create bubble.py

---
+++ bubble.py
@@ -0,0 +1,16 @@
+def bubble_sort(arr):
+    n = len(arr)
+    # Traverse through all array elements
+    for i in range(n):
+        # Last i elements are already sorted
+        for j in range(0, n-i-1):
+            # Traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                
+# Example usage
+arr = [64, 34, 25, 12, 22, 11, 90]
+bubble_sort(arr)
+print("Sorted array is:", arr)
