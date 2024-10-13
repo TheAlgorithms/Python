28aecb84d96b7dbaadaca6937d2ff00212c2d099
Tatiparthi Chenchu Jahnavi <85797034+Jahnavi998@users.noreply.github.com>
2024-10-13 18:58:55 +0530

    Create swapcase.py

---
+++ strings/swapcase.py
@@ -0,0 +1,32 @@
+"""
+Example to show how this method works
+>>> swap_case("Hello")
+hELLO
+>>> swap_case("water")
+WATER
+
+"""
+
+def swap_case(input_string:str):
+    list_representation=list(input_string)
+    for i in range(len(list_representation)):
+        if list_representation[i]>="A" and list_representation[i]<="Z":
+            m2=ord(list_representation[i])
+            m1=m2+32
+            list_representation[i]=chr(m1)
+        elif list_representation[i]>="a" and list_representation[i]<="z":
+            m3=ord(list_representation[i])
+            m4=m3-32
+            list_representation[i]=chr(m4)
+        else:
+            pass
+    swapped_list="".join(list_representation)
+    return swapped_list
+
+if __name__ == "__main__":
+    # sample test 
+    input_string="Hello"
+    swapped_string = swap_case(input_string)
+    print(swapped_string)
+
+        
