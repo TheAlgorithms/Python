fc7f2dfb693b1db23a7abe0d1319ebdaeb90bc2e
silkirai1812 <86019976+silkirai1812@users.noreply.github.com>
2024-10-15 07:26:22 +0530

    Create floor|CeilUsingBinarySearch.py
    Given an sorted array 'arr' of n integers and an integer x. Find the floor and ceiling of x in arr[0..n-1]

---
+++ searches/floor|CeilUsingBinarySearch.py
@@ -0,0 +1,47 @@
+def findFloor(arr, n, x):
+    low = 0
+    high = n - 1
+    ans = -1
+
+    while low <= high:
+        mid = (low + high) // 2
+        # maybe an answer
+        if arr[mid] <= x:
+            ans = arr[mid]
+            # look for smaller index on the left
+            low = mid + 1
+        else:
+            high = mid - 1  # look on the right
+
+    return ans
+
+
+def findCeil(arr, n, x):
+    low = 0
+    high = n - 1
+    ans = -1
+
+    while low <= high:
+        mid = (low + high) // 2
+        # maybe an answer
+        if arr[mid] >= x:
+            ans = arr[mid]
+            # look for smaller index on the left
+            high = mid - 1
+        else:
+            low = mid + 1  # look on the right
+
+    return ans
+
+
+def getFloorAndCeil(arr, n, x):
+    f = findFloor(arr, n, x)
+    c = findCeil(arr, n, x)
+    return (f, c)
+
+# sample test cas
+# arr = [3, 4, 4, 7, 8, 10]
+# n = 6
+# x = 5
+# ans = getFloorAndCeil(arr, n, x)
+# print("The floor and ceil are:", ans[0], ans[1])
