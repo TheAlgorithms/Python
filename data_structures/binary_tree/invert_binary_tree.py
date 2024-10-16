c159f6edb7c6f4c03df62eb4ad460b4e230cb809
Ronald Ngounou <74538524+ronaldngounou@users.noreply.github.com>
2024-10-15 23:57:26 -0400

    create preorder function

--- data_structures/binary_tree/invert_binary_tree.py
+++ data_structures/binary_tree/invert_binary_tree.py
@@ -26,6 +26,59 @@ class TreeNode:
         self.right: TreeNode | None = None 
     
 
-    @property 
-    def mirror_binary_tree(self, root):
-        pass 
+class MirrorBinaryTree:
+    def mirror_binary_tree(self, root : TreeNode):
+        """
+        Invert a binary tree and return the new root.
+
+        Returns the root of the mirrored binary tree.
+
+        >>> tree = TreeNode(0)
+        >>> tree.left = TreeNode(10)
+        >>> tree.right = TreeNode(20)
+        >>> result_tree = MirrorBinaryTree().mirror_binary_tree(tree)
+        >>> print_preorder(result_tree)
+        0
+        20
+        10
+        """ 
+        
+        if not root:
+            return None 
+
+        if root.left:
+            self.mirror_binary_tree(root.left)
+        
+        if root.right:
+            self.mirror_binary_tree(root.right)
+        
+        root.left, root.right = root.right, root.left 
+
+        return root
+
+def print_preorder(root: TreeNode | None) -> None:
+    """
+    Print pre-order traversal of the tree .
+
+    >>> root = TreeNode(1)
+    >>> root.left = TreeNode(2)
+    >>> root.right = TreeNode(3)
+    >>> print_preorder(root)
+    1
+    2
+    3
+    >>> print_preorder(root.right)
+    3
+    """
+    if not root:
+        return None 
+    if root:
+        print(root.data)
+        print_preorder(root.left)
+        print_preorder(root.right)
+        
+
+if __name__ == "__main__":
+    import doctest 
+    doctest.testmod()
+    
