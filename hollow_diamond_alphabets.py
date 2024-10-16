1f307e8e963d834ecbac39d3ea9292cfc71f2fb0
Manoj Kumar Pendem <manojpendem19@gmail.com>
2024-10-16 07:43:15 +0530

    Update hollow_diamond_alphabets.py
    The hollow_diamond_alphabet function prints a hollow diamond pattern using uppercase alphabet characters based on the specified size. The parameter n determines the number of rows in the diamond. The function utilizes spaces for alignment and hollow spaces within the diamond structure, creating a visually appealing output.

--- hollow_diamond_alphabets.py
+++ hollow_diamond_alphabets.py
@@ -1,25 +1,25 @@
-def hollow_diamond_alphabet(n: int) -> None:
+def hollow_diamond_alphabet(diamond_size: int):
     """
     Prints a hollow diamond pattern using alphabet characters.
 
     Parameters:
-    n (int): The size of the diamond. Determines the number of rows.
+    diamond_size (int): The size of the diamond. Determines the number of rows.
 
     Example:
     >>> hollow_diamond_alphabet(5)
-        A
-       B C
-      D   E
-     F     G
-    H       I
-     F     G
-      D   E
-       B C
-        A
+            A
+           B C
+          D   E
+         F     G
+        H       I
+         F     G
+          D   E
+           B C
+            A
     """
     alpha = 64
-    for i in range(1, n + 1):
-        left_spaces = " " * (n - i)
+    for i in range(1, diamond_size + 1):
+        left_spaces = " " * (diamond_size - i)
         hollow_spaces = " " * (((i - 1) * 2) - 1)
         if i == 1:
             print(left_spaces + chr(alpha + 1))
@@ -28,8 +28,8 @@ def hollow_diamond_alphabet(n: int) -> None:
         alpha += 2
 
     alpha -= 2
-    for i in range(n - 1, 0, -1):
-        left_spaces = " " * (n - i)
+    for i in range(diamond_size - 1, 0, -1):
+        left_spaces = " " * (diamond_size - i)
         hollow_spaces = " " * (((i - 1) * 2) - 1)
         if i == 1:
             print(left_spaces + chr(alpha - 1))
@@ -37,6 +37,6 @@ def hollow_diamond_alphabet(n: int) -> None:
             print(left_spaces + chr(alpha - 2) + hollow_spaces + chr(alpha - 1))
         alpha -= 2
 
-
-n = int(input("Enter the size of the diamond: "))
-hollow_diamond_alphabet(n)
+# Example usage
+diamond_size = int(input("Enter the diamond size: "))
+hollow_diamond_alphabet(diamond_size)
