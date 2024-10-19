fb520a32412507b364d792dcea0c85fa4372b6c2
Kumar Abhishek <abhishekfrayne@gmail.com>
2024-10-19 17:54:38 +0530

    check_anagram.py

---
+++ strings/check_anagram.py
@@ -0,0 +1,22 @@
+str1 = "Race"
+str2 = "Care"
+
+# convert both the strings into lowercase
+str1 = str1.lower()
+str2 = str2.lower()
+
+# check if length is same
+if(len(str1) == len(str2)):
+
+    # sort the strings
+    sorted_str1 = sorted(str1)
+    sorted_str2 = sorted(str2)
+
+    # if sorted char arrays are same
+    if(sorted_str1 == sorted_str2):
+        print(str1 + " and " + str2 + " are anagram.")
+    else:
+        print(str1 + " and " + str2 + " are not anagram.")
+
+else:
+    print(str1 + " and " + str2 + " are not anagram.")
