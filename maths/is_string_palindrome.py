e9a779ab2dffa3f43eb0a9bbbd3ca5f9cdeda06a
sarveshsrinath <sarveshsrinath@gmail.com>
2024-10-19 16:10:36 +0530

    Added doctests

--- maths/is_string_palindrome.py
+++ maths/is_string_palindrome.py
@@ -42,5 +42,6 @@ def is_string_palindrome(words: str) -> bool:
     return cleaned_phrase == cleaned_phrase[::-1]
 
 if __name__ == "__main__":
-    n=input("Enter a word:-")
-    print(is_string_palindrome(n))
+    import doctest
+
+    doctest.testmod()
