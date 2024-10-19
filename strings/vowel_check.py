1d6cb1dc77ed75067fbc3ba035de14c2bb60c193
Kumar Abhishek <abhishekfrayne@gmail.com>
2024-10-19 17:48:51 +0530

    Create vowel_check.py

---
+++ strings/vowel_check.py
@@ -0,0 +1,14 @@
+String = input('Enter the string :')
+count = 0
+#to check for less conditions
+#keep string in lowercase
+String = String.lower()
+for i in String:
+    if i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u':
+        #if True
+        count+=1
+#check if any vowel found
+if count == 0:
+    print('No vowels found')
+else:
+    print('Total vowels are :' + str(count))
