65c1eae3249055f4d30e0c9e0f7e3bf8123d5509
MRGAMING666709 <minesh.24bcs10029@sst.scaler.com>
2024-10-21 21:24:34 +0530

    Add files via upload

---
+++ butterflyPattern.py
@@ -0,0 +1,14 @@
+def butterflyPattern(n):
+    for i in range(n//2):
+        print("* "*(i+1),end="")
+        print(" "*(((n+1)//2 - 2)*4 - 4*i + 1),end="")
+        print(" *"*(i+1))
+    print("* "*(n//2),end="*")
+    print(" *"*(n//2))
+    for i in range(n//2):
+        print("* "*(n//2 - i),end="")
+        print(" "*(1 + 4*i),end="")
+        print(" *"*(n//2 - i))
+
+n = int(input())
+butterflyPattern(n)
