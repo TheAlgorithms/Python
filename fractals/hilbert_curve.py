4a6642a5b7af6b93657838a67989f1bedc7121e7
Simon Waldherr <git@simon.waldherr.eu>
2024-10-24 22:25:59 +0200

    better coding style

--- fractals/hilbert_curve.py
+++ fractals/hilbert_curve.py
@@ -12,10 +12,20 @@ Requirements:
     - numpy
 """
 
+import doctest
+
 import matplotlib.pyplot as plt
 
 
-def hilbert_curve(order, x0=0, y0=0, xi=1, xj=0, yi=0, yj=1):
+def hilbert_curve(
+    order: int,
+    x0: float = 0,
+    y0: float = 0,
+    xi: float = 1,
+    xj: float = 0,
+    yi: float = 0,
+    yj: float = 1,
+) -> list[tuple[float, float]]:
     """
     Generates the points for the Hilbert curve of the specified order.
 
@@ -27,6 +37,9 @@ def hilbert_curve(order, x0=0, y0=0, xi=1, xj=0, yi=0, yj=1):
     - x0, y0: the starting coordinates
     - xi, xj: the transformation matrix for x coordinates
     - yi, yj: the transformation matrix for y coordinates
+
+    Returns:
+    - A list of (x, y) coordinates representing the Hilbert curve.
 
     >>> len(hilbert_curve(1))
     5
@@ -65,9 +78,15 @@ def hilbert_curve(order, x0=0, y0=0, xi=1, xj=0, yi=0, yj=1):
     return points
 
 
-def plot_hilbert_curve(points):
+def plot_hilbert_curve(points: list[tuple[float, float]]) -> None:
     """
     Plots the Hilbert curve using matplotlib.
+
+    Parameters:
+    - points: A list of (x, y) coordinates representing the Hilbert curve.
+
+    Returns:
+    - None
     """
     x, y = zip(*points)
     plt.plot(x, y)
@@ -77,6 +96,8 @@ def plot_hilbert_curve(points):
 
 
 if __name__ == "__main__":
+    doctest.testmod()
+
     order = 5  # Order of the Hilbert curve
     curve_points = hilbert_curve(order)
     plot_hilbert_curve(curve_points)
