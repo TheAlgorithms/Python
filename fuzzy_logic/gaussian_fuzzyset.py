8735fb0e663ee080f49d4f8505df188d42b3a9b7
Shreya <95279016+Shreya123714@users.noreply.github.com>
2024-10-30 17:47:21 +0530

    Update gaussian_fuzzyset.py
    Fix membership method to return standard float type

--- fuzzy_logic/gaussian_fuzzyset.py
+++ fuzzy_logic/gaussian_fuzzyset.py
@@ -60,7 +60,7 @@ class GaussianFuzzySet:
 
         membership_value = np.exp(-0.5 * ((member - self.mean) / self.std_dev) ** 2)
         # Directly return for non-complement or return 1 - membership for complement
-        return membership_value if not self.is_complement else 1 - membership_value
+        return float(membership_value) if not self.is_complement else 1 - float(membership_value)
 
     def complement(self) -> GaussianFuzzySet:
         """
