5ffd5aa71bf6ac7e55bc371d19e6ee441364365c
RajdeepBakolia2004 <144157867+RajdeepBakolia2004@users.noreply.github.com>
2024-12-13 19:30:57 +0530

    fixed the issue in strings/join.py

--- strings/join.py
+++ strings/join.py
@@ -37,18 +37,43 @@ def join(separator: str, separated: list[str]) -> str:
     'apple-banana-cherry'
     """
 
-    joined = ""
-    for word_or_phrase in separated:
-        if not isinstance(word_or_phrase, str):
+    joined : str = ""
+    """
+    The last element of the list is not followed by the separator.
+    So, we need to iterate through the list and join each element
+    with the separator except the last element.
+    """
+    last_index : int = len(separated)-1
+    """
+    Iterate through the list and join each element with the separator.
+    Except the last element, all other elements are followed by the separator.
+    """
+    for index in range(last_index):
+        """
+        If the element is not a string, raise an exception.
+        """
+        if not isinstance( separated[index] ,str):
             raise Exception("join() accepts only strings")
-        joined += word_or_phrase + separator
-
-    # Remove the trailing separator
-    # by stripping it from the result
-    return joined.strip(separator)
+        """
+        join the element with the separator.
+        """
+        joined += separated[index] + separator
+    """
+    If the list is not empty, join the last element.
+    """
+    if (separated != []) :
+        """
+        If the last element is not a string, raise an exception.
+        """
+        if not isinstance(separated[len(separated)-1], str):
+            raise Exception("join() accepts only strings")
+        joined += separated[len(separated)-1]
+    """
+    RETURN the joined string.
+    """
+    return joined
 
 
 if __name__ == "__main__":
     from doctest import testmod
-
     testmod()
