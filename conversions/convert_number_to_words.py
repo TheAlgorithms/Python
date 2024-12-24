1653e382fea24a185efc68249fd7fd3bc0d89f10
Christian Clauss <cclauss@me.com>
2024-12-23 22:10:05 -0500

    Update convert_number_to_words.py

--- conversions/convert_number_to_words.py
+++ conversions/convert_number_to_words.py
@@ -54,7 +54,7 @@ class NumberingSystem(Enum):
 
 
 class NumberWords(Enum):
-    ONES: ClassVar[dict[int, str]] = {
+    ONES = {
         0: "",
         1: "one",
         2: "two",
@@ -67,7 +67,7 @@ class NumberWords(Enum):
         9: "nine",
     }
 
-    TEENS: ClassVar[dict[int, str]] = {
+    TEENS = {
         0: "ten",
         1: "eleven",
         2: "twelve",
@@ -80,7 +80,7 @@ class NumberWords(Enum):
         9: "nineteen",
     }
 
-    TENS: ClassVar[dict[int, str]] = {
+    TEN = {
         2: "twenty",
         3: "thirty",
         4: "forty",
