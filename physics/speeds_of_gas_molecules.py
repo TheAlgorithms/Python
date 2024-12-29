f19c044bd99a86084dbecd6ec6f9d097b9bb5fd5
MaximSmolskiy <mithridatus@mail.ru>
2024-12-29 21:52:30 +0300

    Fix

--- physics/speeds_of_gas_molecules.py
+++ physics/speeds_of_gas_molecules.py
@@ -22,25 +22,25 @@ https://en.wikipedia.org/wiki/Maxwell%E2%80%93Boltzmann_distribution
 The average speed can be calculated by integrating the Maxwell-Boltzmann distribution
 from 0 to infinity and dividing by the total number of molecules. The result is::
 
-        ---------------------
+        ----------------------
         | v_avg = √(8RT/πM)  |
-        ---------------------
+        ----------------------
 
 The most probable speed is the speed at which the Maxwell-Boltzmann distribution
 is at its maximum. This can be found by differentiating the Maxwell-Boltzmann
 distribution with respect to ``v`` and setting the result equal to zero. The result is::
 
-        ---------------------
+        ----------------------
         | v_mp = √(2RT/M)    |
-        ---------------------
+        ----------------------
 
 The root-mean-square speed is another measure of the average speed
 of the molecules in a gas. It is calculated by taking the square root
 of the average of the squares of the speeds of the molecules. The result is::
 
-        ---------------------
+        ----------------------
         | v_rms = √(3RT/M)   |
-        ---------------------
+        ----------------------
 
 Here we have defined functions to calculate the average and
 most probable speeds of molecules in a gas given the
