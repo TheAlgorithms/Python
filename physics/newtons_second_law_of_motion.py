ed4f7429a6f4b11a71e8d6a972c4bcc1033f0f2d
MaximSmolskiy <mithridatus@mail.ru>
2024-12-29 17:59:06 +0300

    Fix

--- physics/newtons_second_law_of_motion.py
+++ physics/newtons_second_law_of_motion.py
@@ -2,8 +2,8 @@ r"""
 Description:
     Newton's second law of motion pertains to the behavior of objects for which
     all existing forces are not balanced.
-    The second law states that the acceleration of an object is dependent upon two variables
-    - the net force acting upon the object and the mass of the object.
+    The second law states that the acceleration of an object is dependent upon
+    two variables - the net force acting upon the object and the mass of the object.
     The acceleration of an object depends directly
     upon the net force acting upon the object,
     and inversely upon the mass of the object.
