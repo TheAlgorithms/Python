67f713314afe18c0815605ea2d310c7bacfe37cb
Christian Clauss <cclauss@me.com>
2024-12-30 13:21:09 +0100

    Update conversions/rectangular_to_polar.py

--- conversions/rectangular_to_polar.py
+++ conversions/rectangular_to_polar.py
@@ -22,7 +22,6 @@ def rectangular_to_polar(real: float, img: float) -> tuple[float, float]:
     """
 
     mod = round(math.sqrt((real**2) + (img**2)), 2)
-
     ang = round(math.degrees(math.atan2(img, real)), 2)
     return (mod, ang)
 
