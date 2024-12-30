58faf1d823932fe16d6eb14bdfa7adc8d7dd5c45
Christian Clauss <cclauss@me.com>
2024-12-30 13:27:56 +0100

    Update conversions/rectangular_to_polar.py

--- conversions/rectangular_to_polar.py
+++ conversions/rectangular_to_polar.py
@@ -5,19 +5,19 @@ def rectangular_to_polar(real: float, img: float) -> tuple[float, float]:
     """
     https://en.wikipedia.org/wiki/Polar_coordinate_system
 
-    >>> rec_to_pol(5,-5)
+    >>> rectangular_to_polar(5,-5)
     (7.07, -45.0)
-    >>> rec_to_pol(-1,1)
+    >>> rectangular_to_polar(-1,1)
     (1.41, 135.0)
-    >>> rec_to_pol(-1,-1)
+    >>> rectangular_to_polar(-1,-1)
     (1.41, -135.0)
-    >>> rec_to_pol(1e-10,1e-10)
+    >>> rectangular_to_polar(1e-10,1e-10)
     (0.0, 45.0)
-    >>> rec_to_pol(-1e-10,1e-10)
+    >>> rectangular_to_polar(-1e-10,1e-10)
     (0.0, 135.0)
-    >>> rec_to_pol(9.75,5.93)
+    >>> rectangular_to_polar(9.75,5.93)
     (11.41, 31.31)
-    >>> rec_to_pol(10000,99999)
+    >>> rectangular_to_polar(10000,99999)
     (100497.76, 84.29)
     """
 
