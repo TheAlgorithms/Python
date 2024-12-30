3b2b09bcae6bd8c6c5d654768d5f57e294bd36ef
Maxim Smolskiy <mithridatus@mail.ru>
2024-12-30 13:18:32 +0300

    Update is_ip_v4_address_valid.py

--- maths/is_ip_v4_address_valid.py
+++ maths/is_ip_v4_address_valid.py
@@ -2,12 +2,10 @@
 wiki: https://en.wikipedia.org/wiki/IPv4
 
 Is IP v4 address valid?
-A valid IP address must be four numbers in the form of A.B.C.D,
-where A,B,C and D are numbers from 0-255
-
-for example:
-    192.168.23.1, 172.254.254.254 are valid IP address
-    192.168.01.0, 256.192.3.121 are invalid IP address
+A valid IP address must be four octets in the form of A.B.C.D,
+where A, B, C and D are numbers from 0-255
+for example: 192.168.23.1, 172.255.255.255 are valid IP address
+             192.168.256.0, 256.192.3.121 are invalid IP address
 """
 
 
@@ -20,16 +18,13 @@ def is_ip_v4_address_valid(ip: str) -> bool:
     >>> is_ip_v4_address_valid("192.168.0.23")
     True
 
-    >>> is_ip_v4_address_valid("192.255.15.8")
-    True
+    >>> is_ip_v4_address_valid("192.256.15.8")
+    False
 
     >>> is_ip_v4_address_valid("172.100.0.8")
     True
 
-    >>> is_ip_v4_address_valid("0.0.0.0")
-    True
-
-    >>> is_ip_v4_address_valid("254.256.0.255")
+    >>> is_ip_v4_address_valid("255.256.0.256")
     False
 
     >>> is_ip_v4_address_valid("1.2.33333333.4")
@@ -47,27 +42,28 @@ def is_ip_v4_address_valid(ip: str) -> bool:
     >>> is_ip_v4_address_valid("1.2.A.4")
     False
 
+    >>> is_ip_v4_address_valid("0.0.0.0")
+    True
+
+    >>> is_ip_v4_address_valid("1.2.3.")
+    False
+
     >>> is_ip_v4_address_valid("1.2.3.05")
     False
     """
-
-    parts = ip.split(".")
-
-    if len(parts) != 4:
+    octets = ip.split(".")
+    if len(octets) != 4:
         return False
 
-    for part in parts:
-        if not part:
+    for octet in octets:
+        if not octet.isdigit():
             return False
 
-        for i in range(len(part)):
-            if not part[i].isdigit():
-                return False
-
-        if part[0] == "0" and len(part) > 1:
+        number = int(octet)
+        if len(str(number)) != len(octet):
             return False
 
-        if not (0 <= int(part) <= 255):
+        if not 0 <= number <= 255:
             return False
 
     return True
@@ -75,5 +71,5 @@ def is_ip_v4_address_valid(ip: str) -> bool:
 
 if __name__ == "__main__":
     ip = input().strip()
-    result = "valid" if is_ip_v4_address_valid(ip) else "invalid"
-    print(f"{ip} is a {result} IPv4 address.")
+    valid_or_invalid = "valid" if is_ip_v4_address_valid(ip) else "invalid"
+    print(f"{ip} is a {valid_or_invalid} IPv4 address.")
