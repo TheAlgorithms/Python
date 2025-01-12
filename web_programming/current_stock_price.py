b579604d6d7f61f9bb2f5953817642c7079191c5
MaximSmolskiy <mithridatus@mail.ru>
2025-01-12 04:07:10 +0300

    Fix

--- web_programming/current_stock_price.py
+++ web_programming/current_stock_price.py
@@ -24,12 +24,12 @@ def stock_price(symbol: str = "AAPL") -> str:
         url, headers={"USER-AGENT": "Mozilla/5.0"}, timeout=10
     ).text
     soup = BeautifulSoup(yahoo_finance_source, "html.parser")
-    specific_fin_streamer_tag = soup.find("fin-streamer", {"data-testid": "qsp-price"})
+    specific_fin_streamer_tag = soup.find("span", {"data-testid": "qsp-price"})
 
     if specific_fin_streamer_tag:
         text = specific_fin_streamer_tag.get_text()
         return text
-    return "No <fin-streamer> tag with the specified data-test attribute found."
+    return "No <fin-streamer> tag with the specified data-testid attribute found."
 
 
 # Search for the symbol at https://finance.yahoo.com/lookup
