9a8e2c7fa318b46bd0563b432fc9dc83d84fce0e
Christian Clauss <cclauss@me.com>
2025-01-13 21:48:08 +0100

    Update current_stock_price.py

--- web_programming/current_stock_price.py
+++ web_programming/current_stock_price.py
@@ -15,7 +15,7 @@ Current ORCL stock price is   188.87
 def stock_price(symbol: str = "AAPL") -> str:
     """
     >>> stock_price("EEEE")
-    'No <fin-streamer> tag with the specified data-test attribute found.'
+    '- '
     >>> isinstance(float(stock_price("GOOG")),float)
     True
     """
@@ -24,12 +24,10 @@ def stock_price(symbol: str = "AAPL") -> str:
         url, headers={"USER-AGENT": "Mozilla/5.0"}, timeout=10
     ).text
     soup = BeautifulSoup(yahoo_finance_source, "html.parser")
-    specific_fin_streamer_tag = soup.find("fin-streamer", {"data-testid": "qsp-price"})
 
-    if specific_fin_streamer_tag:
-        text = specific_fin_streamer_tag.get_text()
-        return text
-    return "No <fin-streamer> tag with the specified data-test attribute found."
+    if specific_fin_streamer_tag := soup.find("span", {"data-testid": "qsp-price"}):
+        return specific_fin_streamer_tag.get_text()
+    return "No <fin-streamer> tag with the specified data-testid attribute found."
 
 
 # Search for the symbol at https://finance.yahoo.com/lookup
