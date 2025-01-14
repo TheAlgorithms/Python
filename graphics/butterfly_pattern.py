f00a5f7673ee4f39da969116928517bb7ce04ae1
Maxim Smolskiy <mithridatus@mail.ru>
2025-01-15 00:21:42 +0300

    Update butterfly_pattern.py

--- graphics/butterfly_pattern.py
+++ graphics/butterfly_pattern.py
@@ -8,15 +8,28 @@ def butterfly_pattern(n: int) -> str:
     *****
     ** **
     *   *
+    >>> print(butterfly_pattern(5))
+    *       *
+    **     **
+    ***   ***
+    **** ****
+    *********
+    **** ****
+    ***   ***
+    **     **
+    *       *
     """
     result = []
 
     # Upper part
-    for i in range(1, n + 1):
+    for i in range(1, n):
         left_stars = "*" * i
         spaces = " " * (2 * (n - i) - 1)
         right_stars = "*" * i
         result.append(left_stars + spaces + right_stars)
+
+    # Middle part
+    result.append("*" * (2 * n - 1))
 
     # Lower part
     for i in range(n - 1, 0, -1):
