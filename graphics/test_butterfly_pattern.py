de046d25ecc742d1c11d113d6760374ff7c30d47
Maxim Smolskiy <mithridatus@mail.ru>
2025-01-14 23:52:25 +0300

    Delete graphics/test_butterfly_pattern.py

--- graphics/test_butterfly_pattern.py
+++
@@ -1,8 +0,0 @@
-from graphics.butterfly_pattern import butterfly_pattern
-
-
-def test_butterfly_pattern():
-    expected_output = (
-        "*        *\n" "**      **\n" "***    ***\n" "**      **\n" "*        *"
-    )
-    assert butterfly_pattern(3) == expected_output
