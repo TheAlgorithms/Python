8da5b0a9b618cdf50c3d1d8d0c7a213917321a24
Aditya Kumar Mishra <154746713+adityakrmishra@users.noreply.github.com>
2025-01-31 16:43:30 +0530

    Fix linting errors (E501, F401) in recursive knapsack implementation

--- knapsack/recursive_approach_knapsack.py
+++ knapsack/recursive_approach_knapsack.py
@@ -1,40 +1,31 @@
 """
 A shopkeeper has bags of wheat that each have different weights and different profits.
 Example:
-no_of_items = 4
-profit = [5, 4, 8, 6]
-weight = [1, 2, 4, 5]
-max_weight = 5
+    no_of_items = 4
+    profit = [5, 4, 8, 6]
+    weight = [1, 2, 4, 5]
+    max_weight = 5
 
 Constraints:
-- max_weight > 0
-- profit[i] >= 0
-- weight[i] >= 0
+    max_weight > 0
+    profit[i] >= 0
+    weight[i] >= 0
 
-Calculate the maximum profit the shopkeeper can make given the maximum weight that can be carried.
+Calculate the maximum profit the shopkeeper can make given the maximum weight 
+that can be carried.
 """
 
-from functools import lru_cache
-
-
 def knapsack(
-    weights: list,
-    values: list,
-    number_of_items: int,
-    max_weight: int,
-    index: int,
-    memo=None,
+    weights: list, values: list, number_of_items: int, max_weight: int, index: int
 ) -> int:
     """
-    Optimized Recursive Knapsack with Memoization.
-
+    Function description is as follows:
     :param weights: List of item weights
-    :param values: List of corresponding item profits
-    :param number_of_items: Total number of available items
-    :param max_weight: Maximum weight capacity of the knapsack
-    :param index: Current item index being considered
-    :param memo: Dictionary to store computed results for optimization
-    :return: Maximum profit possible
+    :param values: List of item profits corresponding to the weights
+    :param number_of_items: Number of available items
+    :param max_weight: Maximum weight that can be carried
+    :param index: The item index currently being checked
+    :return: Maximum expected gain
 
     >>> knapsack([1, 2, 4, 5], [5, 4, 8, 6], 4, 5, 0)
     13
@@ -44,31 +35,14 @@ def knapsack(
     if index == number_of_items:
         return 0
 
-    if memo is None:
-        memo = {}
-
-    # If already computed, return stored value
-    if (index, max_weight) in memo:
-        return memo[(index, max_weight)]
-
-    # Case 1: Skip the current item
-    ans1 = knapsack(weights, values, number_of_items, max_weight, index + 1, memo)
-
-    # Case 2: Include the current item (if weight allows)
+    ans1 = knapsack(weights, values, number_of_items, max_weight, index + 1)
     ans2 = 0
     if weights[index] <= max_weight:
         ans2 = values[index] + knapsack(
-            weights,
-            values,
-            number_of_items,
-            max_weight - weights[index],
-            index + 1,
-            memo,
+            weights, values, number_of_items, max_weight - weights[index], index + 1
         )
-
-    # Store and return the maximum value obtained
-    memo[(index, max_weight)] = max(ans1, ans2)
-    return memo[(index, max_weight)]
+    
+    return max(ans1, ans2)
 
 
 if __name__ == "__main__":
