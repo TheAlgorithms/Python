d0986a4941dde68fc25d3a1d4f05e8b9a3441830
Maxim Smolskiy <mithridatus@mail.ru>
2025-02-20 23:37:57 +0300

    Update n_queens.py

--- backtracking/n_queens.py
+++ backtracking/n_queens.py
@@ -33,15 +33,15 @@ def is_safe(board: list[list[int]], row: int, column: int) -> bool:
 
     n = len(board)  # Size of the board
 
-    # Check if there is any queen in the same row, column,
-    # left upper diagonal, and right upper diagonal
+    # Check if there is any queen in the same upper column,
+    # left upper diagonal and left lower diagonal
     return (
-        all(board[i][j] != 1 for i, j in zip(range(row, -1, -1), range(column, n)))
+        all(board[i][j] != 1 for i, j in zip(range(row), [column] * row))
         and all(
-            board[i][j] != 1 for i, j in zip(range(row, -1, -1), range(column, -1, -1))
+            board[i][j] != 1 for i, j in zip(range(row - 1, -1, -1), range(column - 1, -1, -1))
         )
         and all(
-            board[i][j] != 1 for i, j in zip(range(row, -1, -1), [column] * (row + 1))
+            board[i][j] != 1 for i, j in zip(range(row - 1, -1, -1), range(column + 1, n))
         )
     )
 
