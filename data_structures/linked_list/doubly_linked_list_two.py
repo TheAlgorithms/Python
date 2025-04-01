091b5cebfefd3b8b307dde780e8cc8ed6a9c4296
MaximSmolskiy <mithridatus@mail.ru>
2025-04-01 23:41:56 +0300

    Fix

--- data_structures/linked_list/doubly_linked_list_two.py
+++ data_structures/linked_list/doubly_linked_list_two.py
@@ -241,9 +241,13 @@ def create_linked_list() -> None:
     >>> linked_list
     LinkedList(head=Node(data=10, previous=None, next=None), tail=Node(data=10, previous=None, next=None))
     >>> linked_list.insert_at_position(position=2, value=20)
+    >>> linked_list
     >>> linked_list.insert_at_position(position=1, value=30)
+    >>> linked_list
     >>> linked_list.insert_at_position(position=2, value=40)
+    >>> linked_list
     >>> linked_list.insert_at_position(position=3, value=50)
+    >>> linked_list
     """
 
 
