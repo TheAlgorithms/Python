c7b54a07a666c2d47395fac93e921b4de0447c52
SaicoBys <JacobMoronta0000@gmail.com>
2025-04-04 15:43:01 -0500

    Add is_palindrome algorithm under strings category

---
+++ strings/is_palindrome.py
@@ -0,0 +1,15 @@
+def is_palindrome(txt):
+    """
+    Checks if a given text is a palindrome.
+
+    A palindrome is a word or phrase that reads the same forwards and backwards.
+
+    Parameters:
+    txt (str): The text to check.
+
+    Returns:
+    bool: True if the text is a palindrome, False otherwise.
+    """
+    return txt == txt[::-1]
+    
+print(is_palindrome("radar"))
