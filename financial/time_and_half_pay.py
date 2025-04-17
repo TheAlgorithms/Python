2d56591107cf30015350d6d3cf3e66e6f517cc74
Maxim Smolskiy <mithridatus@mail.ru>
2025-04-18 02:26:56 +0300

    Update time_and_half_pay.py

--- financial/time_and_half_pay.py
+++ financial/time_and_half_pay.py
@@ -1,6 +1,5 @@
 """
 Calculate time and a half pay
-
 """
 
 
@@ -9,6 +8,7 @@ def pay(hours_worked: float, pay_rate: float, hours: float = 40) -> float:
     hours_worked = The total hours worked
     pay_rate = Ammount of money per hour
     hours = Number of hours that must be worked before you recieve time and a half
+
     >>> pay(41, 1)
     41.5
     >>> pay(65, 19)
@@ -17,23 +17,20 @@ def pay(hours_worked: float, pay_rate: float, hours: float = 40) -> float:
     10.0
     """
     # Check that all input parameters are float or integer
-    assert type(hours_worked) is float or type(hours_worked) is int, (
+    assert isinstance(hours_worked, float) or isinstance(hours_worked, int) (
         "Parameter 'hours_worked' must be of type 'int' or 'float'"
     )
-    assert type(pay_rate) is float or type(pay_rate) is int, (
-        "Parameter 'hours_worked' must be of type 'int' or 'float'"
+    assert isinstance(pay_rate, float) or isinstance(pay_rate, int) (
+        "Parameter 'pay_rate' must be of type 'int' or 'float'"
     )
-    assert type(hours) is float or type(hours) is int, (
-        "Parameter 'hours_worked' must be of type 'int' or 'float'"
+    assert isinstance(hours, float) or isinstance(hours, int) (
+        "Parameter 'hours' must be of type 'int' or 'float'"
     )
 
     normal_pay = hours_worked * pay_rate
-    over_time = hours_worked - hours
-    # Another way
-    """over_time_pay = ((over_time * ((over_time + (over_time ** 2) ** 0.5) / (2 * over_time))) / 2) * pay_rate"""
-    over_time_pay = (max(0, over_time) / 2) * pay_rate
-    total_pay = normal_pay + over_time_pay
-    return total_pay
+    over_time = max(0, hours_worked - hours)
+    over_time_pay = over_time * pay_rate / 2
+    return normal_pay + over_time_pay
 
 
 if __name__ == "__main__":
