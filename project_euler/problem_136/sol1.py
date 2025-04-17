28c86013debe775f5d77286b6f147c6c42765ee7
Maxim Smolskiy <mithridatus@mail.ru>
2025-04-18 01:50:38 +0300

    Update sol1.py

--- project_euler/problem_136/sol1.py
+++ project_euler/problem_136/sol1.py
@@ -4,10 +4,12 @@ Project Euler Problem 136: https://projecteuler.net/problem=136
 Singleton Difference
 
 The positive integers, x, y, and z, are consecutive terms of an arithmetic progression.
-Given that n is a positive integer, the equation, x^2 - y^2 - z^2 = n, has exactly one solution when n = 20:
-                                    13^2 - 10^2 - 7^2 = 20.
+Given that n is a positive integer, the equation, x^2 - y^2 - z^2 = n,
+has exactly one solution when n = 20:
+                              13^2 - 10^2 - 7^2 = 20.
 
-In fact there are twenty-five values of n below one hundred for which the equation has a unique solution.
+In fact there are twenty-five values of n below one hundred for which
+the equation has a unique solution.
 
 How many values of n less than fifty million have exactly one solution?
 
