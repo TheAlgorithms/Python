c5c57529cf86505d1a3e89d91c0c2a91518f2367
Maxim Smolskiy <mithridatus@mail.ru>
2025-05-05 10:06:40 +0300

    Update sol1.py

--- project_euler/problem_190/sol1.py
+++ project_euler/problem_190/sol1.py
@@ -5,9 +5,11 @@ Maximising a Weighted Product
 
 Let S_m = (x_1, x_2, ..., x_m) be the m-tuple of positive real numbers with
 x_1 + x_2 + ... + x_m = m for which P_m = x_1 * x_2^2 * ... * x_m^m is maximised.
-For example, it can be verified that |_P_10_| = 4112
-(|__| is the integer part function).
-Find Sum_{m=2}^15 = |_P_m_|.
+
+For example, it can be verified that |_ P_10 _| = 4112
+(|_ _| is the integer part function).
+
+Find Sum_{m=2}^15 = |_ P_m _|.
 
 Solution:
 - Fix x_1 = m - x_2 - ... - x_m.
@@ -15,10 +17,6 @@ Solution:
   x_2 = 2 * x_1, x_3 = 3 * x_1, ..., x_m = m * x_1.
 - Calculate partial second order derivatives of P_m wrt the x_2, ..., x_m.
   By plugging in the values from the previous step, can verify that solution is maximum.
-
->>> solution(5)
-10
-
 """
 
 
@@ -32,7 +30,8 @@ def solution(n: int = 15) -> int:
     2
     >>> solution(4)
     4
-
+    >>> solution(5)
+    10
     """
 
     ans = 0
