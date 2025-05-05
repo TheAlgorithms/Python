687726d05e3f0fa821d99de32bc8ec2994fc0460
Maxim Smolskiy <mithridatus@mail.ru>
2025-05-05 09:57:41 +0300

    Update sol1.py

--- project_euler/problem_164/sol1.py
+++ project_euler/problem_164/sol1.py
@@ -14,8 +14,8 @@ def solve(
     digit: int, prev1: int, prev2: int, sum_max: int, first: bool, cache: dict[str, int]
 ) -> int:
     """
-    Solve for remaining 'digit' digits, with previous 'prev1' number, and
-    previous-previous 'prev2' number, total sum of 'sum_max'.
+    Solve for remaining 'digit' digits, with previous 'prev1' digit, and
+    previous-previous 'prev2' digit, total sum of 'sum_max'.
     Pass around 'cache' to store/reuse intermediate results.
 
     >>> solve(digit=1, prev1=0, prev2=0, sum_max=9, first=True, cache={})
@@ -25,21 +25,25 @@ def solve(
     """
     if digit == 0:
         return 1
-    comb = 0
+
     cache_str = f"{digit},{prev1},{prev2}"
     if cache_str in cache:
         return cache[cache_str]
-    for v in range(sum_max - prev1 - prev2 + 1):
-        if first and v == 0:
+
+    comb = 0
+    for curr in range(sum_max - prev1 - prev2 + 1):
+        if first and curr == 0:
             continue
+
         comb += solve(
             digit=digit - 1,
-            prev1=v,
+            prev1=curr,
             prev2=prev1,
             sum_max=sum_max,
             first=False,
             cache=cache,
         )
+
     cache[cache_str] = comb
     return comb
 
