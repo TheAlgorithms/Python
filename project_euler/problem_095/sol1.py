091b592df0eed897013dfb28a9508224b42dec89
Maxim Smolskiy <mithridatus@mail.ru>
2025-05-06 22:54:20 +0300

    Update sol1.py

--- project_euler/problem_095/sol1.py
+++ project_euler/problem_095/sol1.py
@@ -3,16 +3,19 @@ Project Euler Problem 95: https://projecteuler.net/problem=95
 
 Amicable Chains
 
+The proper divisors of a number are all the divisors excluding the number itself. For example, the proper divisors of 28 are 1, 2, 4, 7, and 14. As the sum of these divisors is equal to 28, we call it a perfect number.
+Interestingly the sum of the proper divisors of 220 is 284 and the sum of the proper divisors of 284 is 220, forming a chain of two numbers. For this reason, 220 and 284 are called an amicable pair.
+Perhaps less well known are longer chains. For example, starting with $12496$, we form a chain of five numbers:
+12496 -> 14288 -> 15472 -> 14536 -> 14264 (-> 12496 -> ...)
+Since this chain returns to its starting point, it is called an amicable chain.
+Find the smallest member of the longest amicable chain with no element exceeding one million.
+
 Solution is doing the following:
 - Get relevant prime numbers
 - Iterate over product combination of prime numbers to generate all non-prime
 numbers up to max number, by keeping track of prime factors
 - Calculate the sum of factors for each number
 - Iterate over found some factors to find longest chain
-
->>> solution(200000)
-12496
-
 """
 
 from numpy import sqrt
@@ -113,6 +116,8 @@ def solution(n_max: int = 1000000) -> int:
 
     >>> solution(10)
     6
+    >>> solution(200000)
+    12496
     """
 
     primes = generate_primes(n_max)
