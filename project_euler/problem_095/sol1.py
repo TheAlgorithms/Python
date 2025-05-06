bded10419793551686f124182fc220c9d46f66da
Maxim Smolskiy <mithridatus@mail.ru>
2025-05-06 23:25:55 +0300

    Update sol1.py

--- project_euler/problem_095/sol1.py
+++ project_euler/problem_095/sol1.py
@@ -56,9 +56,9 @@ def generate_primes(num: int) -> list[int]:
     """
     are_primes = [True] * (num + 1)
     are_primes[0] = are_primes[1] = False
-    for i in range(start=2, stop=isqrt(num) + 1):
+    for i in range(2, isqrt(num) + 1):
         if are_primes[i]:
-            for j in range(start=i * i, stop=num + 1, step=i):
+            for j in range(i * i, num + 1, i):
                 are_primes[j] = False
 
     return [prime for prime, is_prime in enumerate(are_primes) if is_prime]
