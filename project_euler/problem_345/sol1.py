dc88339ecee48cfc7234ca665e0e97eb86f4efb2
Maxim Smolskiy <mithridatus@mail.ru>
2025-05-06 22:24:07 +0300

    Update sol1.py

--- project_euler/problem_345/sol1.py
+++ project_euler/problem_345/sol1.py
@@ -6,7 +6,8 @@ Matrix Sum
 We define the Matrix Sum of a matrix as the maximum possible sum of
 matrix elements such that none of the selected elements share the same row or column.
 
-For example, the Matrix Sum of the matrix below equals 3315 ( = 863 + 383 + 343 + 959 + 767):
+For example, the Matrix Sum of the matrix below equals
+3315 ( = 863 + 383 + 343 + 959 + 767):
       7  53 183 439 863
     497 383 563  79 973
     287  63 343 169 583
