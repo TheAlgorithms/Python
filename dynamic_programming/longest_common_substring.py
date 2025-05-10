d8dfc28a50e7f02fda5a858f7ab31795cf6ee663
Maxim Smolskiy <mithridatus@mail.ru>
2025-05-10 14:55:14 +0300

    Update longest_common_substring.py

--- dynamic_programming/longest_common_substring.py
+++ dynamic_programming/longest_common_substring.py
@@ -46,16 +46,20 @@ def longest_common_substring(text1: str, text2: str) -> str:
     if not text1 or not text2:
         return ""
 
-    dp = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
-    max_length, end_pos = 0, 0
+    text1_length = len(text1)
+    text2_length = len(text2)
+    
+    dp = [[0] * (text2_length + 1) for _ in range(text1_length + 1)]
+    end_pos = 0
+    max_length = 0
 
-    for i in range(1, len(text1) + 1):
-        for j in range(1, len(text2) + 1):
+    for i in range(1, text1_length + 1):
+        for j in range(1, text2_length + 1):
             if text1[i - 1] == text2[j - 1]:
-                dp[i][j] = dp[i - 1][j - 1] + 1
+                dp[i][j] = 1 + dp[i - 1][j - 1]
                 if dp[i][j] > max_length:
-                    max_length = dp[i][j]
                     end_pos = i
+                    max_length = dp[i][j]
 
     return text1[end_pos - max_length : end_pos]
 
