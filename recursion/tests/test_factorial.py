9696330432148d50b29d08e699c41ce1e462a7dc
Khushi Pal <114483555+khushipy@users.noreply.github.com>
2025-06-19 17:03:47 +0530

    Update test_factorial.py

--- recursion/tests/test_factorial.py
+++ recursion/tests/test_factorial.py
@@ -1,18 +1,15 @@
-import unittest
+import pytest
+
 from recursion.factorial import factorial
 
 
-class TestFactorial(unittest.TestCase):
-    def test_factorial_valid_inputs(self):
-        self.assertEqual(factorial(0), 1)
-        self.assertEqual(factorial(1), 1)
-        self.assertEqual(factorial(5), 120)
-        self.assertEqual(factorial(10), 3628800)
-
-    def test_factorial_invalid_input(self):
-        with self.assertRaises(ValueError):
-            factorial(-1)
+def test_factorial_valid_inputs() -> None:
+    assert factorial(0) == 1
+    assert factorial(1) == 1
+    assert factorial(5) == 120
+    assert factorial(10) == 3628800
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_factorial_invalid_input() -> None:
+    with pytest.raises(ValueError):
+        factorial(-1)
