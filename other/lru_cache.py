c674736c80fbd46a88f7bb293195d1e53fbac328
lighting9999 <lighting0909@yeah.net>
2025-06-22 14:29:12 +0800

    Update lru_cache.py

--- other/lru_cache.py
+++ other/lru_cache.py
@@ -1,13 +1,9 @@
 from __future__ import annotations
 
 from collections.abc import Callable
-from typing import Generic, TypeVar
-
-T = TypeVar("T")
-U = TypeVar("U")
 
 
-class DoubleLinkedListNode(Generic[T, U]):
+class DoubleLinkedListNode[T, U]:
     """
     Double Linked List Node built specifically for LRU Cache
 
@@ -28,7 +24,7 @@ class DoubleLinkedListNode(Generic[T, U]):
         )
 
 
-class DoubleLinkedList(Generic[T, U]):
+class DoubleLinkedList[T, U]:
     """
     Double Linked List built specifically for LRU Cache
 
@@ -143,7 +139,7 @@ class DoubleLinkedList(Generic[T, U]):
         return node
 
 
-class LRUCache(Generic[T, U]):
+class LRUCache[T, U]:
     """
     LRU Cache to store a given capacity of data. Can be used as a stand-alone object
     or as a function decorator.
