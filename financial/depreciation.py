02ec03b330c906cb7a3d03380135113302a363f2
Michael Roberts <michaelnathanroberts@gmail.com>
2025-06-25 21:41:09 -0400

    Reformat, pt. 2

--- financial/depreciation.py
+++ financial/depreciation.py
@@ -90,7 +90,9 @@ def straight_line_depreciation(
 
     return list_of_depreciation_expenses
 
-def declining_balance_depreciation(useful_years: int,
+
+def declining_balance_depreciation(
+    useful_years: int,
     purchase_value: float,
     residual_value: float = 0.0,) -> list[float]:
     """
@@ -141,10 +143,12 @@ def declining_balance_depreciation(useful_years: int,
         new_book_value = book_value * (1 - depreciation_rate)
         list_of_depreciation_expenses.append(round(book_value - new_book_value, 2))
         book_value = new_book_value
-    
+
     return list_of_depreciation_expenses
 
-def sum_of_years_digits_depreciation(useful_years: int,
+
+def sum_of_years_digits_depreciation(
+    useful_years: int,
     purchase_value: float,
     residual_value: float = 0.0,) -> list[float]:
     """
@@ -190,18 +194,13 @@ def sum_of_years_digits_depreciation(useful_years: int,
 
     list_of_depreciation_expenses = []
 
-    for i in range(1, useful_years+1):
-        depreciation_value = (useful_years - (i - 1)) / digits_sum * (purchase_value - residual_value)
+    for i in range(1, useful_years + 1):
+        depreciation_value = (
+            (useful_years - (i - 1)) / digits_sum * (purchase_value - residual_value)
+        )
         list_of_depreciation_expenses.append(round(depreciation_value, 2))
 
-    
     return list_of_depreciation_expenses
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
