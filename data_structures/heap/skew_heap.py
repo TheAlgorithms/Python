a35036fbd857896e4bc333da235d7914c09038a2
lighting9999 <lighting0909@yeah.net>
2025-06-29 14:37:19 +0800

    fix l001 skew_heap.py

--- data_structures/heap/skew_heap.py
+++ data_structures/heap/skew_heap.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
+
 from __future__ import annotations
 
-from collections.abc import Iterable, Iterator, Callable
+from collections.abc import Callable, Iterable, Iterator
 from typing import Any
 
 
