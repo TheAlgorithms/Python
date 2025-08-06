d48e8a3d0a4fcfe07cb0d81b52b35ab6d776c576
hiiamkarati <dwivedi.prakriti11@gmail.com>
2025-08-06 18:28:07 +0530

    Two-Sum to match target

---
+++ interview-bootcamp/Two-Sum.py
@@ -0,0 +1,20 @@
+def to_sum(nums, target):
+    """
+    This function finds two numbers in the list 'nums' that add up to 'target'.
+    
+    :param nums: List of integers
+    :param target: Integer target sum
+    :return: Tuple of the two numbers that add up to target, or None if no such pair exists
+    """
+    num_set = set()
+    
+    for num in nums:
+        complement = target - num
+        if complement in num_set:
+            return (complement, num)
+        num_set.add(num)
+    
+    return None
+
+if __name__ == "__main__":
+    print(to_sum([2, 7, 11, 15], 9)) 
