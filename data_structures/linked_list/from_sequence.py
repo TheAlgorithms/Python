a46a25341fe4800e54803eeb23492a481bd0ba40
Maxim Smolskiy <mithridatus@mail.ru>
2025-08-24 17:46:29 +0300

    Update from_sequence.py

--- data_structures/linked_list/from_sequence.py
+++ data_structures/linked_list/from_sequence.py
@@ -25,18 +25,32 @@ def make_linked_list(elements_list: list | tuple) -> Node:
     Creates a Linked List from the elements of the given sequence
     (list/tuple) and returns the head of the Linked List.
 
-    >>> make_linked_list([1, 3, 5, 32, 44, 12, 43])
-    <1> ---> <3> ---> <5> ---> <32> ---> <44> ---> <12> ---> <43> ---> <END>
+    >>> make_linked_list([])
+    Traceback (most recent call last):
+        ...
+    ValueError: The Elements List is empty
+    >>> make_linked_list(())
+    Traceback (most recent call last):
+        ...
+    ValueError: The Elements List is empty
     >>> make_linked_list([1])
     <1> ---> <END>
     >>> make_linked_list((1,))
     <1> ---> <END>
+    >>> make_linked_list([1, 3, 5, 32, 44, 12, 43])
+    <1> ---> <3> ---> <5> ---> <32> ---> <44> ---> <12> ---> <43> ---> <END>
+    >>> make_linked_list((1, 3, 5, 32, 44, 12, 43))
+    <1> ---> <3> ---> <5> ---> <32> ---> <44> ---> <12> ---> <43> ---> <END>
     """
 
+    # if elements_list is empty
     if not elements_list:
-        raise Exception("The Elements List is empty")
+        raise ValueError("The Elements List is empty")
+
+    # Set first element as Head
     head = Node(elements_list[0])
     current = head
+    # Loop through elements from position 1
     for data in elements_list[1:]:
         current.next = Node(data)
         current = current.next
