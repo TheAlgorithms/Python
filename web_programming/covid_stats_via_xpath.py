fe15df30edaae174372b05290cfd7217c34eef3f
Maxim Smolskiy <mithridatus@mail.ru>
2025-08-26 01:43:05 +0300

    Update covid_stats_via_xpath.py

--- web_programming/covid_stats_via_xpath.py
+++ web_programming/covid_stats_via_xpath.py
@@ -1,5 +1,5 @@
 """
-This is to show simple COVID19 info fetching from worldometers site using lxml
+This is to show simple COVID19 info fetching from worldometers archive site using lxml
 * The main motivation to use lxml in place of bs4 is that it is faster and therefore
 more convenient to use in Python web projects (e.g. Django or Flask-based)
 """
@@ -9,7 +9,6 @@ more convenient to use in Python web projects (e.g. Django or Flask-based)
 # dependencies = [
 #     "httpx",
 #     "lxml",
-#     "requests_html",
 # ]
 # ///
 
@@ -17,22 +16,16 @@ from typing import NamedTuple
 
 import httpx
 from lxml import html
-from requests_html import HTMLSession
 
 
 class CovidData(NamedTuple):
-    cases: int
-    deaths: int
-    recovered: int
+    cases: str
+    deaths: str
+    recovered: str
 
 
-def covid_stats(url: str = "https://www.worldometers.info/coronavirus/") -> CovidData:
+def covid_stats(url: str = "https://web.archive.org/web/20250825095350/https://www.worldometers.info/coronavirus/") -> CovidData:
     xpath_str = '//div[@class = "maincounter-number"]/span/text()'
-    session = HTMLSession()
-    r = session.get(url)
-    print(r.html.html)
-    r.html.render()
-    print(r.html.html)
     return CovidData(
         *html.fromstring(httpx.get(url, timeout=10).content).xpath(xpath_str)
     )
