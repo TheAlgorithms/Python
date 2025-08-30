381f069d61566307d471fee315d8360947bdaf98
Maxim Smolskiy <mithridatus@mail.ru>
2025-08-30 03:21:31 +0300

    Update matrix_class.py

--- matrix/matrix_class.py
+++ matrix/matrix_class.py
@@ -204,11 +204,9 @@ class Matrix:
         return Matrix(
             [
                 [
-                    (
-                        self.minors().rows[row][column]
-                        if (row + column) % 2 == 0
-                        else self.minors().rows[row][column] * -1
-                    )
+                    self.minors().rows[row][column]
+                    if (row + column) % 2 == 0
+                    else self.minors().rows[row][column] * -1
                     for column in range(self.minors().num_columns)
                 ]
                 for row in range(self.minors().num_rows)
