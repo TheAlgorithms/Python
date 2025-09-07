9b016fe5d914a6504c5e1421213fe18c4b91c5e7
Maxim Smolskiy <mithridatus@mail.ru>
2025-09-08 02:24:18 +0300

    Update proth_number.py

--- maths/special_numbers/proth_number.py
+++ maths/special_numbers/proth_number.py
@@ -29,11 +29,11 @@ def proth(number: int) -> int:
     """
 
     if not isinstance(number, int):
-        msg = f"Input value of [number={number}] must be an integer"
+        msg = f"Input value of [{number=}] must be an integer"
         raise TypeError(msg)
 
     if number < 1:
-        msg = f"Input value of [number={number}] must be > 0"
+        msg = f"Input value of [{number=}] must be > 0"
         raise ValueError(msg)
     elif number == 1:
         return 3
