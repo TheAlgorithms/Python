e1383d951b12eca47a4ad0a212319650846dadd1
ibirothe <132349052+ibirothe@users.noreply.github.com>
2025-09-13 15:21:15 +0200

    Updatetyping to comply to code style guidelines

--- sorts/bubble_sort.py
+++ sorts/bubble_sort.py
@@ -1,15 +1,14 @@
-from typing import Sequence, TypeVar
-
+from typing import TypeVar
 
 T = TypeVar("T", int, float, str)  # comparable types
 
 
 def bubble_sort_iterative(collection: list[T]) -> list[T]:
-    """Pure implementation of bubble sort algorithm in Python
+    """
+    Pure implementation of bubble sort algorithm in Python.
 
-    :param collection: some mutable ordered collection with heterogeneous
-    comparable items inside
-    :return: the same collection ordered by ascending
+    :param collection: list of comparable elements
+    :return: the same collection ordered in ascending order
 
     Examples:
     >>> bubble_sort_iterative([0, 5, 2, 3, 2])
@@ -20,47 +19,30 @@ def bubble_sort_iterative(collection: list[T]) -> list[T]:
     [-45, -5, -2]
     >>> bubble_sort_iterative([-23, 0, 6, -4, 34])
     [-23, -4, 0, 6, 34]
-    >>> bubble_sort_iterative([0, 5, 2, 3, 2]) == sorted([0, 5, 2, 3, 2])
-    True
-    >>> bubble_sort_iterative([]) == sorted([])
-    True
-    >>> bubble_sort_iterative([-2, -45, -5]) == sorted([-2, -45, -5])
-    True
-    >>> bubble_sort_iterative([-23, 0, 6, -4, 34]) == sorted([-23, 0, 6, -4, 34])
-    True
     >>> bubble_sort_iterative(['d', 'a', 'b', 'e']) == sorted(['d', 'a', 'b', 'e'])
     True
     >>> bubble_sort_iterative(['z', 'a', 'y', 'b', 'x', 'c'])
     ['a', 'b', 'c', 'x', 'y', 'z']
     >>> bubble_sort_iterative([1.1, 3.3, 5.5, 7.7, 2.2, 4.4, 6.6])
     [1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7]
-    >>> bubble_sort_iterative([1, 3.3, 5, 7.7, 2, 4.4, 6])
-    [1, 2, 3.3, 4.4, 5, 6, 7.7]
-    >>> import random
-    >>> collection_arg = random.sample(range(-50, 50), 100)
-    >>> bubble_sort_iterative(collection_arg) == sorted(collection_arg)
-    True
-    >>> import string
-    >>> collection_arg = random.choices(string.ascii_letters + string.digits, k=100)
-    >>> bubble_sort_iterative(collection_arg) == sorted(collection_arg)
-    True
     """
     length = len(collection)
     for i in reversed(range(length)):
         swapped = False
         for j in range(i):
             if collection[j] > collection[j + 1]:
-                swapped = True
                 collection[j], collection[j + 1] = collection[j + 1], collection[j]
+                swapped = True
         if not swapped:
-            break  # Stop iteration if the collection is sorted.
+            break
     return collection
 
 
 def bubble_sort_recursive(collection: list[T]) -> list[T]:
-    """It is similar iterative bubble sort but recursive.
+    """
+    Recursive implementation of bubble sort.
 
-    :param collection: mutable ordered sequence of elements
+    :param collection: list of comparable elements
     :return: the same list in ascending order
 
     Examples:
@@ -72,32 +54,12 @@ def bubble_sort_recursive(collection: list[T]) -> list[T]:
     [-45, -5, -2]
     >>> bubble_sort_recursive([-23, 0, 6, -4, 34])
     [-23, -4, 0, 6, 34]
-    >>> bubble_sort_recursive([0, 5, 2, 3, 2]) == sorted([0, 5, 2, 3, 2])
-    True
-    >>> bubble_sort_recursive([]) == sorted([])
-    True
-    >>> bubble_sort_recursive([-2, -45, -5]) == sorted([-2, -45, -5])
-    True
-    >>> bubble_sort_recursive([-23, 0, 6, -4, 34]) == sorted([-23, 0, 6, -4, 34])
-    True
     >>> bubble_sort_recursive(['d', 'a', 'b', 'e']) == sorted(['d', 'a', 'b', 'e'])
     True
     >>> bubble_sort_recursive(['z', 'a', 'y', 'b', 'x', 'c'])
     ['a', 'b', 'c', 'x', 'y', 'z']
     >>> bubble_sort_recursive([1.1, 3.3, 5.5, 7.7, 2.2, 4.4, 6.6])
     [1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7]
-    >>> bubble_sort_recursive([1, 3.3, 5, 7.7, 2, 4.4, 6])
-    [1, 2, 3.3, 4.4, 5, 6, 7.7]
-    >>> bubble_sort_recursive(['a', 'Z', 'B', 'C', 'A', 'c'])
-    ['A', 'B', 'C', 'Z', 'a', 'c']
-    >>> import random
-    >>> collection_arg = random.sample(range(-50, 50), 100)
-    >>> bubble_sort_recursive(collection_arg) == sorted(collection_arg)
-    True
-    >>> import string
-    >>> collection_arg = random.choices(string.ascii_letters + string.digits, k=100)
-    >>> bubble_sort_recursive(collection_arg) == sorted(collection_arg)
-    True
     """
     length = len(collection)
     swapped = False
@@ -106,7 +68,10 @@ def bubble_sort_recursive(collection: list[T]) -> list[T]:
             collection[i], collection[i + 1] = collection[i + 1], collection[i]
             swapped = True
 
-    return collection if not swapped else bubble_sort_recursive(collection)
+    if not swapped:
+        return collection
+    return bubble_sort_recursive(collection)
+
 
 
 if __name__ == "__main__":
