f488a07b8317c97639cb1b1f37a746bc2214cdc9
AryanJadhav15 <aryanjadhav54321@gmail.com>
2025-10-01 19:57:33 +0530

    Fix ruff check for sorts/bead_sort.py:34:52

--- sorts/bead_sort.py
+++ sorts/bead_sort.py
@@ -28,13 +28,13 @@ def bead_sort(sequence: list) -> list:
         ...
     TypeError: Sequence must be list of non-negative integers
     """
+    from itertools import pairwise
     if any(not isinstance(x, int) or x < 0 for x in sequence):
         raise TypeError("Sequence must be list of non-negative integers")
-    for _ in range(len(sequence)):
-        for i, (rod_upper, rod_lower) in enumerate(zip(sequence, sequence[1:])):
-            if rod_upper > rod_lower:
-                sequence[i] -= rod_upper - rod_lower
-                sequence[i + 1] += rod_upper - rod_lower
+    for i, (rod_upper, rod_lower) in enumerate(pairwise(sequence)):
+        if rod_upper > rod_lower:
+            sequence[i] -= rod_upper - rod_lower
+            sequence[i + 1] += rod_upper - rod_lower
     return sequence
 
 
