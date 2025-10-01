fbf3da5cdc49a377e88b457f7928c56251608478
ananya-research <ananya.303.singh@gmail.com>
2025-09-30 21:01:51 -0400

    Create triangle.py
    LeetCode ques 120, utilizing DP

---
+++ dynamic_programming/triangle.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        for row in range(1, len(triangle)):
+            for col in range(row + 1):
+                smallest_above = math.inf
+                if col > 0:
+                    smallest_above = triangle[row - 1][col - 1]
+                if col < row:
+                    smallest_above = min(smallest_above, triangle[row - 1][col])
+                triangle[row][col] += smallest_above
+        return min(triangle[-1])
