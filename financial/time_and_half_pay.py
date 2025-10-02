3c023b236d44c75a87883307a03e73259b31191b
arvinder004 <asdhoul004@gmail.com>
2025-10-02 13:07:09 +0530

    added maximum subarray sum (kadane's algorithm) in /data_structures/arrays/

--- financial/time_and_half_pay.py
+++ financial/time_and_half_pay.py
@@ -17,15 +17,15 @@ def pay(hours_worked: float, pay_rate: float, hours: float = 40) -> float:
     10.0
     """
     # Check that all input parameters are float or integer
-    assert isinstance(hours_worked, (float, int)), (
-        "Parameter 'hours_worked' must be of type 'int' or 'float'"
-    )
-    assert isinstance(pay_rate, (float, int)), (
-        "Parameter 'pay_rate' must be of type 'int' or 'float'"
-    )
-    assert isinstance(hours, (float, int)), (
-        "Parameter 'hours' must be of type 'int' or 'float'"
-    )
+    assert isinstance(
+        hours_worked, (float, int)
+    ), "Parameter 'hours_worked' must be of type 'int' or 'float'"
+    assert isinstance(
+        pay_rate, (float, int)
+    ), "Parameter 'pay_rate' must be of type 'int' or 'float'"
+    assert isinstance(
+        hours, (float, int)
+    ), "Parameter 'hours' must be of type 'int' or 'float'"
 
     normal_pay = hours_worked * pay_rate
     over_time = max(0, hours_worked - hours)
