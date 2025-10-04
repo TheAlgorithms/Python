16cf4623c1a957c1ff7e5de286e99bd6c6fcaa79
Wfiq <145265758+Fiqqar@users.noreply.github.com>
2025-10-04 19:04:03 +0700

    Update tree.py

--- height_of_tree/tree.py
+++ height_of_tree/tree.py
@@ -1,18 +1,32 @@
-class Node:
-    def __init__(self, info):
-        self.info = info
-        self.left = None
-        self.right = None
+from typing import Optional, List
 
-    def __str__(self):
+class Node:
+    def __init__(self, info: int) -> None:
+        self.info: int = info
+        self.left: Optional["Node"] = None
+        self.right: Optional["Node"] = None
+
+    def __str__(self) -> str:
+        """
+        >>> str(Node(5))
+        '5'
+        """
         return str(self.info)
 
-
 class BinarySearchTree:
-    def __init__(self):
-        self.root = None
+    def __init__(self) -> None:
+        self.root: Optional[Node] = None
 
-    def create(self, val):
+    def create(self, val: int) -> None:
+        """
+        >>> bst = BinarySearchTree()
+        >>> bst.create(10)
+        >>> bst.root.info
+        10
+        >>> bst.create(5)
+        >>> bst.root.left.info
+        5
+        """
         if self.root is None:
             self.root = Node(val)
         else:
@@ -33,21 +47,37 @@ class BinarySearchTree:
                 else:
                     break
 
-
-def height(node):
+def height(node: Optional[Node]) -> int:
+    """
+    >>> height(None)
+    -1
+    >>> n = Node(3)
+    >>> height(n)
+    0
+    >>> n.left = Node(2)
+    >>> n.right = Node(5)
+    >>> n.right.right = Node(6)
+    >>> height(n)
+    2
+    """
     if node is None:
         return -1
     return 1 + max(height(node.left), height(node.right))
 
-
-def tree_height_from_list(data):
+def tree_height_from_list(data: List[int]) -> int:
+    """
+    >>> tree_height_from_list([3,2,5,6])
+    2
+    >>> tree_height_from_list([1])
+    0
+    >>> tree_height_from_list([5,1,10,15,7])
+    3
+    """
     bst = BinarySearchTree()
     for x in data:
         bst.create(x)
     return height(bst.root)
 
-
 if __name__ == "__main__":
     import doctest
-
     doctest.testmod()
