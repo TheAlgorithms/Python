0db3f495327f6d06c35f1be9d18d06a4e673f45d
Ch.R.Bhardwaj <131685680+RamachandraBhardwaj@users.noreply.github.com>
2025-10-06 10:08:30 +0530

    Refactor longest_palindromic_substring function

--- dynamic_programming/longest_palindromic_substring.py
+++ dynamic_programming/longest_palindromic_substring.py
@@ -1,9 +1,10 @@
 # reference :https://www.geeksforgeeks.org/dsa/longest-palindromic-substring/
 
-
-def longest_palindromic_substring(s: str) -> str:
+def longest_palindromic_substring(s:str)->str:
     """
-    This function returns longest palindromic substring in a string using DP
+    This function returns longest palindromic substring in a string
+    using Dynamic Programming.
+
     s : input string
     >>> longest_palindromic_substring("babad")
     'aba'
@@ -16,37 +17,44 @@ def longest_palindromic_substring(s: str) -> str:
     >>> longest_palindromic_substring("")
     ''
     """
-    n = len(s)
+    n=len(s)
 
-    dp = [[False for i in range(n)] for j in range(n)]
-    start = 0
-    max_length = 1
+    dp=[[False for i in range(n)] for j in range(n)]
+    start=0
+    max_length=1
 
     for i in range(n):
-        dp[i][i] = True
-
+        dp[i][i]=True
     # for length 2 palindrome check
-    for i in range(n - 1):
-        if s[i] == s[i + 1]:
-            dp[i][i + 1] = True
-            start = i
-            max_length = 2
+    for i in range(n-1):
+        if s[i]==s[i+1]:
+            dp[i][i+1]=True
+            start=i
+            max_length=2
 
     # for length 3 and above
-    for length in range(3, n + 1):
-        for i in range(n - length + 1):
-            j = i + length - 1
-            if s[i] == s[j] and dp[i + 1][j - 1]:
-                dp[i][j] = True
-                start = i
-                max_length = length
+    for length in range(3,n+1):
+        for i in range(n-length+1):
+            j=i+length-1
+            if s[i]==s[j] and dp[i+1][j-1]:
+                dp[i][j]=True
+                start=i
+                max_length=length
 
-    return s[start : start + max_length]
+    return s[start:start+max_length]
 
-
-def manacher_algorithm(s: str) -> str:
+def manacher_algorithm(s:str)->str:
     """
-    This function returns the longest palindromic substring in a string using Manacher's algorithm
+    This function returns the longest palindromic substring in a string 
+    using Manacher's algorithm.
+
+    s : input string
+    t : transformed string
+    p : array to store length of palindrome radius around each center
+    c : center of the current rightmost palindrome
+    r : right edge of the current rightmost palindrome
+    n : length of transformed string
+    
     >>> longest_palindromic_substring("babad")
     'aba'
     >>> longest_palindromic_substring("cbbd")
@@ -58,28 +66,27 @@ def manacher_algorithm(s: str) -> str:
     >>> longest_palindromic_substring("")
     ''
     """
-    T = "^#" + "#".join(s) + "#$"
-    n = len(T)
+    t='^#'+'#'.join(s)+'#$'
+    n=len(t)
 
-    p = [0] * n
-    c = 0
-    r = 0
+    p=[0]*n
+    c=0
+    r=0
 
-    for i in range(1, n - 1):
-        mirror = 2 * c - i
-        if i < r:
-            p[i] = min(r - i, p[mirror])
-        while T[i + (1 + p[i])] == T[i - (1 + p[i])]:
-            p[i] += 1
-        if i + p[i] > r:
-            c = i
-            r = i + p[i]
+    for i in range(1,n-1):
+        mirror=2*c-i
+        if i<r:
+            p[i]=min(r-i,p[mirror])
+        while t[i+(1+p[i])]==t[i-(1+p[i])]:
+            p[i]+=1
+        if i+p[i]>r:
+            c=i
+            r=i+p[i]
 
-    max_length = max(p)
-    max_center = p.index(max_length)
-    start = (max_center - max_length) // 2
-    return s[start : start + max_length]
-
+    max_length=max(p)
+    max_center=p.index(max_length)
+    start=(max_center-max_length)//2
+    return s[start:start+max_length]
 
 if __name__ == "__main__":
     import doctest
