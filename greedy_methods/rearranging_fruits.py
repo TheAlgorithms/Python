0ad56962e529e3d7c5bbfc589efec9e919ff4079
Krishna Singh <nagarkotikrishna01@gmail.com>
2025-10-07 23:28:34 +0530

    Update rearranging_fruits.py

--- greedy_methods/rearranging_fruits.py
+++ greedy_methods/rearranging_fruits.py
@@ -1,12 +1,3 @@
-# it's a leetcode question no. 2561 where You have two fruit baskets containing n fruits each. You are given two 0-indexed integer arrays basket1 and basket2 representing the cost of fruit in each basket.
-# You want to make both baskets equal. To do so, you can use the following operation as many times as you want:
-
-# Chose two indices i and j, and swap the ith fruit of basket1 with the jth fruit of basket2.
-# The cost of the swap is min(basket1[i],basket2[j]).
-# Two baskets are considered equal if sorting them according to the fruit cost makes them exactly the same baskets.
-
-# Return the minimum cost to make both the baskets equal or -1 if impossible.
-
 from typing import List
 from collections import defaultdict
 
