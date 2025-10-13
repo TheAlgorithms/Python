0fbe0dbbd09044e9411504c2634cd73ba2daf988
megha-66 <vaswanimegha09@gmail.com>
2025-10-13 09:56:52 +0000

    Added a new algorithm

--- maths/tribonacci.py
+++ maths/tribonacci.py
@@ -1,15 +1,16 @@
 """
-This program calculates the "Nth Tribonacci number in a series. 
+This program calculates the "Nth Tribonacci number in a series.
 
-The Tribonacci sequence Tn is defined as follows :- 
+The Tribonacci sequence Tn is defined as follows :-
 
-T(0) = 0; T(1) = 1; T(2) = 1; and T(n+3) = T(n) + T(n+1) + T(n+3) for n>=0 
+T(0) = 0; T(1) = 1; T(2) = 1; and T(n+3) = T(n) + T(n+1) + T(n+3) for n>=0
 
-In this program, we assume an integer 'n' is given and we have to calculate nth Tribinacci number 
+In this program, we assume an integer 'n' is given and
+we have to calculate nth Tribinacci number
 
 """
 
-def tribonacci(self, n : int) -> int :
+def tribonacci(n : int) -> int :
     trib = [0,1,1]
     for i in range(3,n+1):
         x = trib[i-1] + trib[i-2] + trib[i-3]
@@ -18,4 +19,5 @@ def tribonacci(self, n : int) -> int :
 
 
 if __name__ == "__main__" :
-    print(tribonacci(25)) #prints 1389537
+    import doctest
+    doctest.testmod()
