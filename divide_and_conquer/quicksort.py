a34bc4fd92b296b9412f1aa38992db4f78be86ed
Love Sikka <lovesikka062@gmail.com>
2025-10-14 18:50:23 +0530

    Added Quick Sort

---
+++ divide_and_conquer/quicksort.py
@@ -0,0 +1,13 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+
+arr = [12, 4, 5, 6, 7, 3, 1, 15]
+sorted_arr = quick_sort(arr)
+print(sorted_arr)
