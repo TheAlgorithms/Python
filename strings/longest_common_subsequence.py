9b5b565b20ff58c951a3cd93e79ffa47494bb06e
jaimin45-art <jmodi7396@gmail.com>
2025-10-16 18:48:57 +0530

    fix: changed variable names to lowercase as per ruff style guide

--- strings/longest_common_subsequence.py
+++ strings/longest_common_subsequence.py
@@ -3,14 +3,14 @@
 # using Dynamic Programming and also prints the actual LCS string.
 
 
-def longest_common_subsequence(X: str, Y: str):
-    m, n = len(X), len(Y)
+def longest_common_subsequence(x: str, y: str):
+    m, n = len(x), len(y)
     dp = [[0] * (n + 1) for _ in range(m + 1)]
 
     # Fill the dp table
     for i in range(1, m + 1):
         for j in range(1, n + 1):
-            if X[i - 1] == Y[j - 1]:
+            if x[i - 1] == y[j - 1]:
                 dp[i][j] = 1 + dp[i - 1][j - 1]
             else:
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
@@ -19,8 +19,8 @@ def longest_common_subsequence(X: str, Y: str):
     i, j = m, n
     lcs = []
     while i > 0 and j > 0:
-        if X[i - 1] == Y[j - 1]:
-            lcs.append(X[i - 1])
+        if x[i - 1] == y[j - 1]:
+            lcs.append(x[i - 1])
             i -= 1
             j -= 1
         elif dp[i - 1][j] > dp[i][j - 1]:
