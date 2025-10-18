a6289175d13d80dbb0c2be8830ca1bc218d7b888
chintansood <chintansood9@gmail.com>
2025-10-18 19:56:24 +0530

    Added Heap Sort algorithm in Python for Hacktoberfest 2025

---
+++ sorts/heap_sorting.py
@@ -0,0 +1,26 @@
+def heapify(arr, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+
+    if l < n and arr[l] > arr[largest]:
+        largest = l
+    if r < n and arr[r] > arr[largest]:
+        largest = r
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+def heap_sort(arr):
+    n = len(arr)
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+if __name__ == "__main__":
+    arr = [12, 11, 13, 5, 6, 7]
+    print("Original array:", arr)
+    heap_sort(arr)
+    print("Sorted array:", arr)
