1acb2ffe43b46d05d52e074eb57829a8180cd161
Arunanshu Dey <arunanshudey7@gmail.com>
2025-10-18 13:36:36 +0530

    Removed Trailing whitespace and fix unindent does not match any outer indentation level
    Refactor helper function to improve readability and maintainability. Adjusted bit manipulation logic and added comments for clarity.

--- bit_manipulation/maximize_the_number_of_partitions_after_operations.py
+++ bit_manipulation/maximize_the_number_of_partitions_after_operations.py
@@ -1,12 +1,14 @@
 class Solution:
     def __init__(self):
         self.dp = {}
+
     def count_set_bits(self, n):
         count = 0
         while n > 0:
             count += n & 1
             n >>= 1
         return count
+
     def helper(self, s, k, i, mask, is_change):
         n = len(s)
         if i == n:
@@ -14,15 +16,17 @@ class Solution:
         curr_state = (i << 27) | (mask << 1) | is_change
         if curr_state in self.dp:
             return self.dp[curr_state]
+
         result = 0
         val = ord(s[i]) - ord('a')
         count = self.count_set_bits(mask | (1 << val))
-        temp = 0
+
         if count > k:
             temp = 1 + self.helper(s, k, i + 1, 1 << val, is_change)
         else:
             temp = self.helper(s, k, i + 1, mask | (1 << val), is_change)
         result = max(result, temp)
+
         if is_change == 0:
             for j in range(26):
                 count = self.count_set_bits(mask | (1 << j))
@@ -31,10 +35,12 @@ class Solution:
                 else:
                     temp = self.helper(s, k, i + 1, mask | (1 << j), 1)
                 result = max(result, temp)
+
         self.dp[curr_state] = result
-        return result    
+        return result
+
     def max_partitions_after_operations(self, s, k):
-         """
+        """
         :type s: str
         :type k: int
         :rtype: int
