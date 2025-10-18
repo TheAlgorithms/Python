6f95e10cbe75f1089c360140ea5d909287f621b3
Abhi <anu01857@gmail.com>
2025-10-18 15:38:07 +0530

    added comments

--- maths/all_divisor_of_a_number.py
+++ maths/all_divisor_of_a_number.py
@@ -4,11 +4,16 @@ list1 = []
 
 
 def divisors_of_number(number):
+    # by observation we can go till i reaches square root of n
     while (i**2 <= number):
         if number % i == 0:
-            list1.appenumberd(i)
+            list1.append(i)
+            # added the number to list
             if i != number//i:
+                # we know that if i is the divisor then n/i will also be a divsior but n/i should not be equal to i
                 list1.append(number//i)
+                # added the number to list
         i = i + 1
+        # i is increased by 1
     list1.sort()
     return list1
