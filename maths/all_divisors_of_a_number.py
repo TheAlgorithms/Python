569376a17078d4f20e8e87c1f9c9092fe5525457
Abhi <anu01857@gmail.com>
2025-10-18 16:29:13 +0530

    Fix ruff format

--- maths/all_divisors_of_a_number.py
+++ maths/all_divisors_of_a_number.py
@@ -5,13 +5,13 @@ def divisors_of_number(number):
     i = 1
     list1 = []
     # by observation we can go till i reaches square root of n
-    while (i**2 <= number):
+    while i**2 <= number:
         if number % i == 0:
             list1.append(i)
             # added the number to list
-            if i != number//i:
+            if i != number // i:
                 # if i is the divisor then n/i will also be a divsior
-                list1.append(number//i)
+                list1.append(number // i)
                 # added the number to list
         i = i + 1
         # i is increased by 1
