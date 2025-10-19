3f9ec25ebd71a3c7d2734efb264380b7e15c3a2c
VarunArora24 <varunarora242424@gmail.com>
2025-10-19 19:55:21 +0530

    Add Python function to check Disarium numbers

---
+++ maths/disarium.py
@@ -0,0 +1,21 @@
+def is_disarium(num: int) -> bool:
+    """
+    Check if a number is Disarium.
+
+    >>> is_disarium(89)
+    True
+    >>> is_disarium(75)
+    False
+    >>> is_disarium(135)
+    True
+    """
+    digits = list(str(num))
+    total = 0
+    for i in range(len(digits)):
+        total += int(digits[i]) ** (i + 1)
+    return total == num
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
