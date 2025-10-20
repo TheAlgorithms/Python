65065313889264304af549811f13c7149562cecc
Naman-Vasudev <naman24vasudev@gmail.com>
2025-10-20 13:55:19 +0530

    Create sieve_of_eratosthenes.py
    Created a new folder of primality under Python/math and added a suitable algorithm

---
+++ maths/primality/sieve_of_eratosthenes.py
@@ -0,0 +1,33 @@
+# Sieve of Eratosthenes: an efficient algorithm to compute all prime numbers up to n.
+# It repeatedly marks multiples of each prime as non-prime, starting from 2.
+# This method is suitable for n up to about 10**7 on typical hardware.
+
+def sieve_of_erastosthenes(n):
+   
+    #Boolean list to track prime status of numbers
+    prime = [True] * (n + 1)
+    p = 2
+
+    # Main Algorithm
+    while p * p <= n:
+        if prime[p]:
+            
+            # All multiples of p will be non-prime hence delcare them False.
+
+            for i in range(p * p, n + 1, p):
+                prime[i] = False
+        p += 1
+
+    # Store all primes.
+    result = []
+    for p in range(2, n + 1):
+        if prime[p]:
+            result.append(p)
+    
+    return result
+
+if __name__ == "__main__":
+    n = 35
+    result = sieve_of_erastosthenes(n)
+    for num in result:
+        print(num, end=' ')
