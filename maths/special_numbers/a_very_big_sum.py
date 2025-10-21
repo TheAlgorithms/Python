4c6121d84508d56583e52238b33f4dbb4885b801
Komal Harshita <komal.sony234@gmail.com>
2025-10-21 12:42:24 +0530

    Add a_very_big_sum algorithm to maths folder

---
+++ maths/special_numbers/a_very_big_sum.py
@@ -0,0 +1,20 @@
+from typing import List
+
+def a_very_big_sum(arr: List[int]) -> int:
+    """
+    Return the sum of all integers in the input array.
+
+    >>> a_very_big_sum([2, 4, 6, 2, 4, 6, 3])
+    27
+    >>> a_very_big_sum([])
+    0
+    >>> a_very_big_sum([1000000000, 2000000000])
+    3000000000
+    """
+    if not all(isinstance(x, int) for x in arr):
+        raise ValueError("All elements in the array must be integers")
+
+    total = 0
+    for number in arr:
+        total += number
+    return total
