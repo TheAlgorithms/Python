d917069ba3713faffad404376f177f0633ce8672
devdandekar24 <dandekardev365@gmail.com>
2025-10-24 13:43:58 +0530

    Fix issues and improve divide_two_integers algorithm

--- bit_manipulation/divide_two_integers.py
+++ bit_manipulation/divide_two_integers.py
@@ -47,7 +47,8 @@ class Solution:
         while d >= n:
             cnt = 0
 
-            # Inner loop: find largest power-of-two multiple of divisor that fits in dividend
+            # Inner loop: find largest power-of-two multiple of divisor 
+            # that fits in dividend
             while d >= (n << (cnt + 1)):
                 cnt += 1
 
