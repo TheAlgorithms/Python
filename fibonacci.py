39676c67e15263ebd5d5af73b530f0d9ab8af487
Akruti Tiwari <akrutitiwari24@gmail.com>
2025-10-27 20:52:52 +0530

    Add fibonacci algorithm in maths folder

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+def fibonacci(n: int) -> list[int]:
+    """
+    Return first n Fibonacci numbers.
+    Example:
+    >>> fibonacci(5)
+    [0, 1, 1, 2, 3]
+    """
+    if n <= 0:
+        return []
+    fib_seq = [0, 1]
+    for i in range(2, n):
+        fib_seq.append(fib_seq[i-1] + fib_seq[i-2])
+    return fib_seq[:n]
