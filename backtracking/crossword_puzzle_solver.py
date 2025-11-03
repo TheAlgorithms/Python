7625d7a1a46ac7b64d5fa45de7edaea84ee5c554
khanhkhanhlele <namkhanh2172@gmail.com>
2025-11-03 20:25:02 +0700

    backtracking/crossword_puzzle_solver.py change doc

--- backtracking/crossword_puzzle_solver.py
+++ backtracking/crossword_puzzle_solver.py
@@ -1,5 +1,8 @@
-# https://www.geeksforgeeks.org/solve-crossword-puzzle/
-
+"""
+Crossword Puzzle Solver using Backtracking.
+Reference:
+https://www.geeksforgeeks.org/solve-crossword-puzzle/
+"""
 
 def is_valid(
     puzzle: list[list[str]], word: str, row: int, col: int, vertical: bool
