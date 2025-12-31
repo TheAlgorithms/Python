09b298415195097ed130512b6cb0831ce2463e2b
BhumikaB1 <bhumikapatrange077@gmail.com>
2025-12-31 23:28:25 +0530

    Add simple is_even algorithm

---
+++ maths/is_even.py
@@ -0,0 +1,8 @@
+def is_even(n: int) -> bool:
+    """
+    Check whether a number is even.
+
+    :param n: Integer number
+    :return: True if even, False otherwise
+    """
+    return n % 2 == 0
