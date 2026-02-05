8a6d45ac6e54136714798ac49f9d05fc379c86fa
Tithi Joshi <tithijoshi27@gmail.com>
2026-02-05 10:19:22 +0530

    Improve isogram check by ignoring non-alphabetic characters
    Updated docstring to clarify isogram definition and added example with non-alphabetic characters. Modified letter extraction to ignore non-alphabetic characters.

--- strings/is_isogram.py
+++ strings/is_isogram.py
@@ -5,26 +5,23 @@ wiki: https://en.wikipedia.org/wiki/Heterogram_(literature)#Isograms
 
 def is_isogram(string: str) -> bool:
     """
-    An isogram is a word in which no letter is repeated.
-    Examples of isograms are uncopyrightable and ambidextrously.
+    An isogram is a word or phrase in which no letter is repeated.
+    Non-alphabetic characters are ignored.
+
     >>> is_isogram('Uncopyrightable')
     True
     >>> is_isogram('allowance')
     False
+    >>> is_isogram('six-year-old')
+    True
     >>> is_isogram('copy1')
-    Traceback (most recent call last):
-     ...
-    ValueError: String must only contain alphabetic characters.
+    True
     """
-    if not all(x.isalpha() for x in string):
-        raise ValueError("String must only contain alphabetic characters.")
-
-    letters = sorted(string.lower())
+    letters = [char.lower() for char in string if char.isalpha()]
     return len(letters) == len(set(letters))
 
 
 if __name__ == "__main__":
     input_str = input("Enter a string ").strip()
-
     isogram = is_isogram(input_str)
     print(f"{input_str} is {'an' if isogram else 'not an'} isogram.")
