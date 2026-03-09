4d4298c62a841129ee759582d19cf651bbf84024
kvadrik <41710943+kvadrik@users.noreply.github.com>
2026-03-09 15:50:27 +0200

    relativistic_velocity_summation function parameters explained

--- physics/relativistic_velocity_summation.py
+++ physics/relativistic_velocity_summation.py
@@ -6,6 +6,10 @@ of an object moving at speed v1 relative to a frame that is itself moving at vel
 relative to an observer. I take the last one to be strictly lower than the speed of
 light.
 The formula is v2 = (v1 + v)/(1 + v1 * v / c^2)
+v1 - speed of the object relative to a moving frame
+v - speed of the moving frame
+c - speed of light in the vacuum
+v2 - speed of the object relative to an observer
 """
 
 
