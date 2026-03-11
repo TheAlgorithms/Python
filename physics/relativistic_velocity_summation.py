eb2b0602b95d52c312114cc680dc603eec7412fa
kvadrik <41710943+kvadrik@users.noreply.github.com>
2026-03-11 18:40:33 +0200

    Put # in comments

--- physics/relativistic_velocity_summation.py
+++ physics/relativistic_velocity_summation.py
@@ -1,17 +1,14 @@
-"""
-The relativistic velocity summation formula calculates the combined velocity v2
-of an object moving at speed v1 relative to a frame that is itself moving at velocity v
-relative to an observer. I take the last one to be strictly lower than the speed of
-light.
-The formula is v2 = (v1 + v)/(1 + v1 * v / c**2)
-v1 - speed of the object relative to a moving frame
-v - speed of the moving frame
-c - speed of light in the vacuum
-v2 - speed of the object relative to an observer
-"""
+# The relativistic velocity summation formula calculates the combined velocity v2
+# of an object moving at speed v1 relative to a frame that is itself moving at velocity v
+# relative to an observer. I take the last one to be strictly lower than the speed of
+# light.
+# The formula is v2 = (v1 + v)/(1 + v1 * v / c**2)
+# v1 - speed of the object relative to a moving frame
+# v - speed of the moving frame
+# c - speed of light in the vacuum
+# v2 - speed of the object relative to an observer
 
 c = 299792458
-
 
 def relativistic_velocity_summation(
     object_velocity: float, frame_velocity: float
