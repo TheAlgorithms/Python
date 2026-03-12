6a6e66218c7d11252c5c3fa3f832137958c5b1be
Maxim Smolskiy <mithridatus@mail.ru>
2026-03-12 23:45:15 +0300

    Update sol2.py

--- project_euler/problem_015/sol2.py
+++ project_euler/problem_015/sol2.py
@@ -6,9 +6,6 @@ the right and down, there are exactly 6 routes to the bottom right corner.
 How many such routes are there through a 20x20 grid?
 """
 
-from numpy import integer, ones
-
-
 def solution(n: int = 20) -> int:
     """
     Solve by explicitly counting the paths with dynamic programming.
@@ -21,13 +18,13 @@ def solution(n: int = 20) -> int:
     2
     """
 
-    counts = ones((n + 1, n + 1), dtype=integer)
+    counts = [[1 for _ in range(n + 1)] for _ in range(n + 1)]
 
     for i in range(1, n + 1):
         for j in range(1, n + 1):
             counts[i][j] = counts[i - 1][j] + counts[i][j - 1]
 
-    return int(counts[n][n])
+    return counts[n][n]
 
 
 if __name__ == "__main__":
