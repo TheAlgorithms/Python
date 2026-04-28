6bd17df2d661751ee13cf014322f7e3e058f3cd3
Labib-Bin-Salam <Labibsalam0@gmail.com>
2026-04-29 00:07:03 +0100

    Refine Shell Sort implementation and documentation

--- sorts/shell_sort.py
+++ sorts/shell_sort.py
@@ -1,4 +1,5 @@
-"""Shell sort implementation.
+"""
+Shell sort implementation.
 
 Reference:
 https://en.wikipedia.org/wiki/Shellsort#Pseudocode
@@ -6,9 +7,10 @@ https://en.wikipedia.org/wiki/Shellsort#Pseudocode
 
 
 def shell_sort(collection: list[int]) -> list[int]:
-    """Sort a list of integers in ascending order using shell sort.
+    """
+    Sort a list of integers in ascending order using Shell sort.
 
-    :param collection: A list of integers
+    :param collection: A list of integers to sort
     :return: The same list sorted in ascending order
 
     >>> shell_sort([0, 5, 3, 2, 2])
@@ -17,18 +19,24 @@ def shell_sort(collection: list[int]) -> list[int]:
     []
     >>> shell_sort([-2, -5, -45])
     [-45, -5, -2]
+    >>> shell_sort([3, "a"])  # doctest: +IGNORE_EXCEPTION_DETAIL
+    Traceback (most recent call last):
+    TypeError
     """
     gaps = [701, 301, 132, 57, 23, 10, 4, 1]
 
     for gap in gaps:
-        for i in range(gap, len(collection)):
-            insert_value = collection[i]
-            j = i
+        for index in range(gap, len(collection)):
+            insert_value = collection[index]
+            current_index = index
 
-            while j >= gap and collection[j - gap] > insert_value:
-                collection[j] = collection[j - gap]
-                j -= gap
+            while (
+                current_index >= gap
+                and collection[current_index - gap] > insert_value
+            ):
+                collection[current_index] = collection[current_index - gap]
+                current_index -= gap
 
-            collection[j] = insert_value
+            collection[current_index] = insert_value
 
     return collection
