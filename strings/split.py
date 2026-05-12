012dd18518429cfc8f0980664d649981b6bad487
Rishon Pravin <rishonpravin@gmail.com>
2026-05-12 18:39:27 +0530

    Issue #14649

--- strings/split.py
+++ strings/split.py
@@ -1,33 +1,38 @@
-def split(string: str, separator: str = " ") -> list:
+def split(string: str, separator: str = " ") -> list[str]:
     """
-    Will split the string up into all the values separated by the separator
-    (defaults to spaces)
+    Splits a string by the specified separator and returns a list of substrings.
 
-    >>> split("apple#banana#cherry#orange",separator='#')
+    :param string: The string to be split.
+    :param separator: The separator to split the string by (defaults to space).
+    :return: List of substrings split by the separator.
+
+    Examples:
+
+    >>> split("apple#banana#cherry#orange", separator='#')
     ['apple', 'banana', 'cherry', 'orange']
 
     >>> split("Hello there")
     ['Hello', 'there']
 
-    >>> split("11/22/63",separator = '/')
+    >>> split("11/22/63", separator='/')
     ['11', '22', '63']
 
-    >>> split("12:43:39",separator = ":")
+    >>> split("12:43:39", separator=":")
     ['12', '43', '39']
 
     >>> split(";abbb;;c;", separator=';')
     ['', 'abbb', '', 'c', '']
     """
-
     split_words = []
-
     last_index = 0
+
     for index, char in enumerate(string):
         if char == separator:
             split_words.append(string[last_index:index])
             last_index = index + 1
-        if index + 1 == len(string):
-            split_words.append(string[last_index : index + 1])
+
+    # Append the remaining part of the string after the last separator
+    split_words.append(string[last_index:])
     return split_words
 
 
