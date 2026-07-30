890ffda11444a746512875fefff03fc2072d2b82
Zann <141020257+Fauzanhandriansyah@users.noreply.github.com>
2026-07-30 09:02:53 +0700

    Update anagrams.py

--- strings/anagrams.py
+++ strings/anagrams.py
@@ -23,7 +23,7 @@ def signature(word: str) -> str:
 
 
 def anagram(my_word: str) -> list[str]:
-"""
+    """
     An anagram is a word or phrase formed by rearranging the letters of a
     different word or phrase, typically using all the original letters exactly
     once.
