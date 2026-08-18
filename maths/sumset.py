6e104dd8417530feff6b5af316cb7b2f06fc0fdf
Sanchi Agarwal <sanchiagarwal715@gmail.com>
2026-08-18 16:39:07 +0530

    Add empty set test for sumset

--- maths/sumset.py
+++ maths/sumset.py
@@ -23,6 +23,8 @@ def sumset(set_a: set, set_b: set) -> set:
     Traceback (most recent call last):
     ...
     AssertionError: The input value of [set_b=3] is not a set
+    >>> sumset(set(), {1, 2, 3})
+    set()
     """
     assert isinstance(set_a, set), f"The input value of [set_a={set_a}] is not a set"
     assert isinstance(set_b, set), f"The input value of [set_b={set_b}] is not a set"
