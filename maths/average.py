43235607e0a972ca496fbc30e4a511658ff0d824
Blaze__0318 <anujmhatre125@gmail.com>
2026-09-01 20:30:51 +0530

    Add maths/average.py

---
+++ maths/average.py
@@ -0,0 +1,20 @@
+"""Compute average of a list of numbers."""
+
+
+def average(numbers: list[float]) -> float:
+    """
+    Compute the arithmetic mean.
+
+    >>> average([1, 2, 3, 4, 5])
+    3.0
+    >>> average([10, 20])
+    15.0
+    """
+    if not numbers:
+        raise ValueError("List is empty")
+    return sum(numbers) / len(numbers)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
