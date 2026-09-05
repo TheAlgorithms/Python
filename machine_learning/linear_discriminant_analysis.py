45dfaf2a3fb456d21d10e76ae877ede87c0b7234
Christian Clauss <cclauss@me.com>
2026-09-06 00:11:42 +0200

    Apply batched suggestions from code review
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- machine_learning/linear_discriminant_analysis.py
+++ machine_learning/linear_discriminant_analysis.py
@@ -252,7 +252,7 @@ def accuracy(actual_y: list, predicted_y: list) -> float:
 num = TypeVar("num")
 
 
-def valid_input[num](
+def valid_input(
     input_type: Callable[[object], num],  # Usually float or int
     input_msg: str,
     err_msg: str,
