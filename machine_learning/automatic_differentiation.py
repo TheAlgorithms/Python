Fix gradient accumulation type handling

machine_learning/automatic_differentiation.py
@@ -258,9 +258,7 @@ class GradientTracker:
         """
 
         # partial derivatives with respect to target
-        partial_deriv: defaultdict[Variable, np.ndarray] = defaultdict(
-            lambda: np.array(0)
-        )
+        partial_deriv: dict[Variable, np.ndarray] = {}
         partial_deriv[target] = np.ones_like(target.to_ndarray())
 
         # iterating through each operations in the computation graph
@@ -272,7 +270,7 @@ class GradientTracker:
                 # of variables with respect to the target
                 dparam_doutput = self.derivative(param, operation)
                 dparam_dtarget = dparam_doutput * partial_deriv[operation.output]
-                partial_deriv[param] += dparam_dtarget
+                partial_deriv[param] = partial_deriv.get(param, np.zeros_like(dparam_dtarget)) + dparam_dtarget
 
                 if param.result_of and param.result_of != OpType.NOOP:
                     operation_queue.append(param.result_of)