db52ade16297dd92b036d42a47a2dc0c8094dc30
bpiper02 <pbrent135@gmail.com>
2026-09-09 19:14:54 -0400

    Make gnome sort generic over comparable items

--- tests/test_sorts.py
+++ tests/test_sorts.py
@@ -96,6 +96,7 @@ def test_sort_matches_builtin(sort, case):
         bubble_sort_iterative,
         bubble_sort_recursive,
         circle_sort,
+        gnome_sort,
         insertion_sort,
         merge_sort,
         selection_sort,
