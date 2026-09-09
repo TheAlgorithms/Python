625acaf275a7e5a3c3a5ba05807f31ddc910654a
Christian Clauss <cclauss@me.com>
2026-09-10 00:02:55 +0200

    Fix grammar in merge sort docstring
    Corrected minor grammatical errors in docstring.

--- sorts/merge_sort.py
+++ sorts/merge_sort.py
@@ -1,11 +1,11 @@
 """
-This is a pure Python implementation of the merge sort algorithm.
+The merge sort algorithm.
 
-For doctests run following command:
+For doctests, run the following command:
 python -m doctest -v merge_sort.py
 or
 python3 -m doctest -v merge_sort.py
-For manual testing run:
+For manual testing, run:
 python merge_sort.py
 """
 
