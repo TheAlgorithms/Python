a0b8c6b1abcaa0b122d06c01a08bd021eb585efb
Christian Clauss <cclauss@me.com>
2026-09-12 09:56:54 +0200

    Fix typos
    Updated the docstring for the haversine_distance function to improve clarity and fix minor grammatical issues.

--- geodesy/haversine_distance.py
+++ geodesy/haversine_distance.py
@@ -5,18 +5,18 @@ EARTH_RADIUS = 6371000
 
 def haversine_distance(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
     """
-    Calculate great circle distance between two points on a sphere,
+    Calculate great-circle distance between two points on a sphere,
     given longitudes and latitudes https://en.wikipedia.org/wiki/Haversine_formula
 
     We know that the globe is "sort of" spherical, so a path between two points
     isn't exactly a straight line. We need to account for the Earth's curvature
     when calculating distance from point A to B. This effect is negligible for
     small distances but adds up as distance increases. The Haversine method treats
-    the earth as a sphere which allows us to "project" the two points A and B
+    the Earth as a sphere, which allows us to "project" the two points A and B
     onto the surface of that sphere and approximate the spherical distance between
     them. Since the Earth is not a perfect sphere, other methods which model the
-    Earth's ellipsoidal nature are more accurate but a quick and modifiable
-    computation like Haversine can be handy for shorter range distances.
+    Earth's ellipsoidal nature are more accurate, but a quick and modifiable
+    computation like Haversine can be handy for shorter-range distances.
 
     Args:
         lat1: latitude of coordinate 1 in degrees
