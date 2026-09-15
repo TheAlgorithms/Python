8b2877a81e892d87801193ff057ac54c6a6466ab
Efe furkan two <137198431+RDEV521@users.noreply.github.com>
2026-09-15 08:07:06 +0300

    Fix welcome message to reflect RDEV521

---
+++ scripts/rdev521_hello.py
@@ -0,0 +1,8 @@
+def hello_rocy() -> None:
+    """
+    Prints a welcome message from RDEV521.
+    """
+    print("Hello Open Source World from RDEV521!")
+
+if __name__ == "__main__":
+    hello_rocy()
