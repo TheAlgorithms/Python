273af2cd31b973e2498d571f2cb961c87da62d3f
Christian Clauss <cclauss@me.com>
2026-09-16 20:08:44 +0200

    Bool don't work

--- conversions/roman_numerals.py
+++ conversions/roman_numerals.py
@@ -63,7 +63,7 @@ def int_to_roman(number: int) -> str:
         ...
     TypeError: int_to_roman only accepts integers, got bool
     """
-    if not isinstance(number, (bool, int)):
+    if not isinstance(number, int):
         msg = f"int_to_roman only accepts integers, got {type(number).__name__}"
         raise TypeError(msg)
     if not 1 <= number <= 3999:
