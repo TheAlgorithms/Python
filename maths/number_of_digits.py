a0b2ef538a265ca2b3baff14acc34190bb945e77
Abhiranjan Kumar <2400030581@kluniversity.in>
2026-09-22 11:12:30 +0530

    Fix incorrect doctest and improve log handling in num_digits functions (#13850)
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- maths/number_of_digits.py
+++ maths/number_of_digits.py
@@ -21,7 +21,6 @@ def num_digits(n: int) -> int:
         ...
     TypeError: Input must be an integer
     """
-
     if not isinstance(n, int):
         raise TypeError("Input must be an integer")
 
@@ -59,22 +58,15 @@ def num_digits_fast(n: int) -> int:
         ...
     TypeError: Input must be an integer
     """
-
     if not isinstance(n, int):
         raise TypeError("Input must be an integer")
-
-    if n == 0:
-        return 1
-
-    abs_n = abs(n)
-    digits = math.floor(math.log10(abs_n)) + 1
-    return digits + 1 if 10**digits <= abs_n else digits
+    return 1 if n == 0 else math.floor(math.log10(abs(n)) + 1)
 
 
 def num_digits_faster(n: int) -> int:
     """
     Find the number of digits in a number.
-    abs() is used for negative numbers
+    abs() is used for negative numbers.
 
     >>> num_digits_faster(12345)
     5
@@ -91,7 +83,6 @@ def num_digits_faster(n: int) -> int:
         ...
     TypeError: Input must be an integer
     """
-
     if not isinstance(n, int):
         raise TypeError("Input must be an integer")
 
@@ -100,14 +91,14 @@ def num_digits_faster(n: int) -> int:
 
 def benchmark() -> None:
     """
-    Benchmark multiple functions, with three different length int values.
+    Benchmark multiple functions with three different integer sizes.
     """
     from collections.abc import Callable
 
     def benchmark_a_function(func: Callable, value: int) -> None:
         call = f"{func.__name__}({value})"
         timing = timeit(f"__main__.{call}", setup="import __main__")
-        print(f"{call}: {func(value)} -- {timing} seconds")
+        print(f"{call}: {func(value)} -- {timing:.6f} seconds")
 
     for value in (262144, 1125899906842624, 1267650600228229401496703205376):
         for func in (num_digits, num_digits_fast, num_digits_faster):
