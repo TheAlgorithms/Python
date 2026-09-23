e3238c9e018708f89ac9ede5121eaddc4373adb6
Christian Clauss <cclauss@me.com>
2026-09-23 17:19:38 +0200

    Rename function to annuity_due_future_value

--- financial/annuity_due_future_value.py
+++ financial/annuity_due_future_value.py
@@ -6,19 +6,19 @@ for each term is $1000, then the cash flow is as follows:
 
     0: $1000 --- 1: $1000 --- 2: $1000 --- 3: no payment
 
-The following function, get_annuity_due_future_value, gives the future value of
+The following function, annuity_due_future_value, gives the future value of
 the given annuity due. In the example above, the function should return
 the sum of each payment's future value at the end of term 3.
 
 More info on: https://www.investopedia.com/retirement/calculating-present-and-future-value-of-annuities
 
-This function can help to understand how much you will receive
+This function can help you understand how much you will receive
 if you make a regular deposit at the beginning of each term
-for saving with a fix period and interest rate.
+for saving with a fixed period and interest rate.
 """
 
 
-def get_annuity_due_future_value(
+def annuity_due_future_value(
     term_payment: float, number_of_payments: int, term_interest_rate: float
 ) -> float:
     """
@@ -29,13 +29,13 @@ def get_annuity_due_future_value(
     :return: the future value (maturity value) of the given annuity due
 
     Examples:
-    >>> round(get_annuity_due_future_value(500, 10, 0.05), 2)
+    >>> round(annuity_due_future_value(500, 10, 0.05), 2)
     6603.39
-    >>> round(get_annuity_due_future_value(1000, 10, 0.05), 2)
+    >>> round(annuity_due_future_value(1000, 10, 0.05), 2)
     13206.79
-    >>> round(get_annuity_due_future_value(1000, 10, 0.10), 2)
+    >>> round(annuity_due_future_value(1000, 10, 0.10), 2)
     17531.17
-    >>> round(get_annuity_due_future_value(1000, 20, 0.10), 2)
+    >>> round(annuity_due_future_value(1000, 20, 0.10), 2)
     63002.5
     """
 
@@ -47,11 +47,14 @@ def get_annuity_due_future_value(
 
 
 if __name__ == "__main__":
+    from doctest import testmod
+
+    testmod()
     user_input_amount = float(input("How much money will be deposited each term?\n> "))
     user_input_payment_number = int(input("How many payments will be made?\n> "))
     term_interest_rate = float(input("What is the interest rate per term?\n> "))
     print(
-        get_annuity_due_future_value(
+        annuity_due_future_value(
             user_input_amount, user_input_payment_number, term_interest_rate
         )
     )
