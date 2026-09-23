2bf6db4605a7940dbb187fa17ab247272168de74
Christian Clauss <cclauss@me.com>
2026-09-23 17:52:49 +0200

    Rename function to ordinary_annuity_future_value

--- financial/ordinary_annuity_future_value.py
+++ financial/ordinary_annuity_future_value.py
@@ -6,20 +6,20 @@ for each term is $1000, then the cash flow is as follows:
 
     0: no payment --- 1: $1000 --- 2: $1000 --- 3: $1000
 
-The function, get_ordinary_annuity_future_value, calculates the future value of
+The function, ordinary_annuity_future_value, calculates the future value of
 the given ordinary annuity. In the example above, the function should return
 the sum of each payment's future value at the end of term 3,
 which means the sum is returned as soon as the last payment is made.
 
 More info on: https://www.investopedia.com/retirement/calculating-present-and-future-value-of-annuities
 
-This function can help to understand how much you will receive
-if you make a regular deposit at the end of each term
-for saving with a fix period and interest rate.
+This function can help you understand how much you will receive if you make a
+regular deposit at the end of each term for saving with a fixed period and
+interest rate.
 """
 
 
-def get_ordinary_annuity_future_value(
+def ordinary_annuity_future_value(
     term_payment: float, number_of_payments: int, term_interest_rate: float
 ) -> float:
     """
@@ -30,13 +30,13 @@ def get_ordinary_annuity_future_value(
     :return: the future value (maturity value) of the given ordinary annuity
 
     Examples:
-    >>> round(get_ordinary_annuity_future_value(500, 10, 0.05), 2)
+    >>> round(ordinary_annuity_future_value(500, 10, 0.05), 2)
     6288.95
-    >>> round(get_ordinary_annuity_future_value(1000, 10, 0.05), 2)
+    >>> round(ordinary_annuity_future_value(1000, 10, 0.05), 2)
     12577.89
-    >>> round(get_ordinary_annuity_future_value(1000, 10, 0.10), 2)
+    >>> round(ordinary_annuity_future_value(1000, 10, 0.10), 2)
     15937.42
-    >>> round(get_ordinary_annuity_future_value(1000, 20, 0.10), 2)
+    >>> round(ordinary_annuity_future_value(1000, 20, 0.10), 2)
     57275.0
     """
 
@@ -52,7 +52,7 @@ if __name__ == "__main__":
     user_input_payment_number = int(input("How many payments will be made?\n> "))
     term_interest_rate = float(input("What is the interest rate per term?\n> "))
     print(
-        get_ordinary_annuity_future_value(
+        ordinary_annuity_future_value(
             user_input_amount, user_input_payment_number, term_interest_rate
         )
     )
