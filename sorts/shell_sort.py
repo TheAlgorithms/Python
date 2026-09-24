53c2134117b6e4c2a99b38575a39e9e3620369db
Christian Clauss <cclauss@me.com>
2026-09-24 22:22:55 +0200

    Refactor shell_sort to use Comparable protocol
    Refactor shell_sort function to use a Comparable protocol for type hinting. Update sorting logic to improve clarity.

--- sorts/shell_sort.py
+++ sorts/shell_sort.py
@@ -1,31 +1,56 @@
 """
+https://en.wikipedia.org/wiki/Shellsort
 https://en.wikipedia.org/wiki/Shellsort#Pseudocode
 """
 
+from typing import Any, Protocol
 
-def shell_sort(collection: list[int]) -> list[int]:
-    """
-    Sort a list of integers using the Shell Sort algorithm.
 
-    Reference:
-    https://en.wikipedia.org/wiki/Shellsort
+class Comparable(Protocol):
+    def __lt__(self, other: Any, /) -> bool: ...
 
+
+def shell_sort[T: Comparable](collection: list[T]) -> list[T]:
+    """Pure implementation of shell sort algorithm in Python.
+
+    :param collection:  Some mutable ordered collection with heterogeneous
+    comparable items inside
+    :return:  the same collection ordered in ascending order
+
+    Examples:
     >>> shell_sort([0, 5, 3, 2, 2])
     [0, 2, 2, 3, 5]
     >>> shell_sort([])
     []
     >>> shell_sort([-2, -5, -45])
     [-45, -5, -2]
+    >>> shell_sort(["c", "a", "b"])
+    ['a', 'b', 'c']
+    >>> shell_sort([2.5, -1.0, 0.0])
+    [-1.0, 0.0, 2.5]
+    >>> shell_sort([0, 5, 3, 2, 2]) == sorted([0, 5, 3, 2, 2])
+    True
+    >>> shell_sort(["c", "a", "b"]) == sorted(["c", "a", "b"])
+    True
     """
+    # Marcin Ciura's gap sequence
     gaps = [701, 301, 132, 57, 23, 10, 4, 1]
-
     for gap in gaps:
         for i in range(gap, len(collection)):
             insert_value = collection[i]
             j = i
-            while j >= gap and collection[j - gap] > insert_value:
+            while j >= gap and insert_value < collection[j - gap]:
                 collection[j] = collection[j - gap]
                 j -= gap
-            collection[j] = insert_value
-
+            if j != i:
+                collection[j] = insert_value
     return collection
+
+
+if __name__ == "__main__":
+    from doctest import testmod
+
+    testmod()
+    user_input = input("Enter numbers separated by a comma:\n").strip()
+    unsorted = [int(item) for item in user_input.split(",")]
+    print(shell_sort(unsorted))
