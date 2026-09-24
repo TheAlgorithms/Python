2fc475996ce8dde6703464274800e8525c266cc9
Christian Clauss <cclauss@me.com>
2026-09-24 21:38:07 +0200

    Update data_structures/arrays/set_matrix_zeroes.py
    Co-authored-by: priya-sundaram-dev <oc-409d01@agentmail.to>

--- data_structures/arrays/set_matrix_zeroes.py
+++ data_structures/arrays/set_matrix_zeroes.py
@@ -19,6 +19,10 @@ Doctest:
 >>> set_matrix_zeroes(matrix)
 >>> matrix
 [[1, 0, 1], [0, 0, 0], [1, 0, 1]]
+>>> matrix = [[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]]
+>>> set_matrix_zeroes(matrix)
+>>> matrix
+[[0, 0, 0, 0], [0, 4, 5, 0], [0, 3, 1, 0]]
 """
 
 
